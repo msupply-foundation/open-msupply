5af0a4fa31cf20fd87fef507a999944fd425fd4e
wlthomson <will.tho@outlook.com>
2021-05-25 16:26:02 +1200

    Update random port for test robustness

--- server/tests/health_check.rs
+++ server/tests/health_check.rs
@@ -2,14 +2,15 @@
 
 use rust_server::run;
 
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -18,7 +19,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
