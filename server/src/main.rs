5c7fa1889659d2d6376436e15e2e68b86fb413d1
wlthomson <will.tho@outlook.com>
2021-05-25 14:49:49 +1200

    Refactor project as crate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,7 @@
-//! src/main.rs
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+//! main.rs
+use rust_server::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run().await
 }
