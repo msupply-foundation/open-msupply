97b87ca71ced4951fab5cc28be51aab210231704
wlthomson <will.tho@outlook.com>
2021-05-25 15:46:35 +1200

    Add integration testing
    Source: https://github.com/msupply-foundation/remote-server/commit/6fca527cafbeb96c433a2c13d37a88b7b6a93795

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,13 +1,15 @@
 //! src/lib.rs
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run();
+    Ok(server)
 }
