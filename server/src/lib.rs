d5a0fea7e9e30ebb8bf96907a8a3cab8a4d35790
wlthomson <will.tho@outlook.com>
2021-05-25 14:49:49 +1200

    Refactor project as crate
    Source: https://github.com/msupply-foundation/remote-server/commit/8b7500914d3bbc5206c20804cc4a047cf2f0141e

---
+++ server/src/lib.rs
@@ -0,0 +1,13 @@
+//! src/lib.rs
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
