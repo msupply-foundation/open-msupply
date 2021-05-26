29980143ddc85dfd52b86e2daed0206f4057f01e
wlthomson <will.tho@outlook.com>
2021-05-26 15:52:46 +1200

    Add requisition table

---
+++ server/scripts/init_db.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -x
+set -eo pipefail
+
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=omsupply}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    echo >&2 "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
+sqlx migrate run
+
+echo >&2 "Postgres has been migrated, ready to go!"
