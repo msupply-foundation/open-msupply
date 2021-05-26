6f4cc1806c90f42b250b81d1cf4512dc289c077f
wlthomson <will.tho@outlook.com>
2021-05-26 18:43:08 +1200

    Format rs files
    Source: https://github.com/msupply-foundation/remote-server/commit/97b1c71d7be4c01acfb182e7e5c65a75793b0579

--- server/src/configuration.rs
+++ server/src/configuration.rs
@@ -2,7 +2,7 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -12,6 +12,15 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
