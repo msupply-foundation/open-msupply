5b4f2a096742b3fb829f1840aeacd48b65a946a3
wlthomson <will.tho@outlook.com>
2021-05-26 18:04:47 +1200

    Add configuration file
    Source: https://github.com/msupply-foundation/remote-server/commit/b32a164045b87612357c8e560e9547ffb4602630

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,13 @@
 //! src/main.rs
 use rust_server::startup::run;
+use rust_server::configuration::get_configuration;
 
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
