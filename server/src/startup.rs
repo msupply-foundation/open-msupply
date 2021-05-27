a874f44e0c07400fd404d8730fb43e2a72bce058
wlthomson <will.tho@outlook.com>
2021-05-27 12:44:35 +1200

    Add database application state

--- server/src/startup.rs
+++ server/src/startup.rs
@@ -3,14 +3,18 @@ use crate::routes::{health_check, post_requisition};
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::{Connection, PgConnection};
 
 use std::net::TcpListener;
+use std::sync::Arc;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = Arc::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/requisition", web::post().to(post_requisition))
+            .data(connection.clone())
     })
     .listen(listener)?
     .run();
