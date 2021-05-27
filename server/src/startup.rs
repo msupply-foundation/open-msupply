e17d20fbd2bc436ee6f174ce45f7a60dd3b6d77d
wlthomson <will.tho@outlook.com>
2021-05-27 16:28:35 +1200

    Add requisition insert query

--- server/src/startup.rs
+++ server/src/startup.rs
@@ -1,20 +1,17 @@
 //! src/startup.rs
 use crate::routes::{health_check, post_requisition};
-
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::{Connection, PgConnection};
-
+use sqlx::PgPool;
 use std::net::TcpListener;
-use std::sync::Arc;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
-    let connection = Arc::new(connection);
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
+    let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/requisition", web::post().to(post_requisition))
-            .data(connection.clone())
+            .app_data(connection_pool.clone())
     })
     .listen(listener)?
     .run();
