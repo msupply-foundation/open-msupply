613aa57ffc394923b178985fae26e0cf933509ca
wlthomson <will.tho@outlook.com>
2021-05-27 16:28:35 +1200

    Add requisition insert query
    Source: https://github.com/msupply-foundation/remote-server/commit/162ba87ff35805ba4c4ed97017fe8ae033e57be7

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,16 @@
 //! src/main.rs
 use rust_server::configuration::get_configuration;
 use rust_server::startup::run;
-
-use sqlx::{Connection, PgConnection};
-
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
--- server/src/routes/requisition.rs
+++ server/src/routes/requisition.rs
@@ -1,13 +1,33 @@
 //! src/routes/requisition.rs
 use actix_web::{web, HttpResponse};
+use uuid::Uuid;
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct RequisitionData {
-    id: String,
-    from_id: String,
-    to_id: String,
+    id: Uuid,
+    from_id: Uuid,
+    to_id: Uuid
 }
 
-pub async fn post_requisition(_requisition_data: web::Form<RequisitionData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn post_requisition(
+    requisition: web::Form<RequisitionData>,
+    connection_pool: web::Data<PgPool>,
+) -> Result<HttpResponse, HttpResponse> {
+    sqlx::query!(
+        r#"
+        INSERT INTO requisition (id, from_id, to_id)
+        VALUES ($1, $2, $3)
+        "#,
+        requisition.id,
+        requisition.from_id,
+        requisition.to_id
+    )
+    .execute(connection_pool.get_ref())
+    .await
+    .map_err(|e| {
+        eprintln!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
+    Ok(HttpResponse::Ok().finish())
 }
