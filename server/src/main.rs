08bac0bade608064a99544de8d2dfcc0f6e464cc
wlthomson <will.tho@outlook.com>
2021-05-27 12:44:35 +1200

    Add database application state
    Source: https://github.com/msupply-foundation/remote-server/commit/c608592c531e56a3977f1960739accd8c02383bb

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,12 +2,17 @@
 use rust_server::configuration::get_configuration;
 use rust_server::startup::run;
 
+use sqlx::{Connection, PgConnection};
+
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
