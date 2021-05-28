2fad5614c8e29327895563fd616c6d4e677963f9
wlthomson <will.tho@outlook.com>
2021-05-28 16:51:51 +1200

    Add hierarchical configuration

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,7 +14,10 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
