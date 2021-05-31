546ceec09a5862a5210676e2a6f65ccb9ce5b8de
wlthomson <will.tho@outlook.com>
2021-05-31 23:39:36 +1200

    Revert existing server implementation

--- server/migrations/20210526031245_create_requisition_table.sql
+++
@@ -1,8 +0,0 @@
--- migrations/20210526031245_create_requisition_table.sql
--- Create requisition table
-CREATE TABLE requisition (
-    id uuid NOT NULL,
-    PRIMARY KEY (id),
-    from_id uuid NOT NULL,
-    to_id uuid NOT NULL
-);
--- server/src/routes/mod.rs
+++
@@ -1,6 +0,0 @@
-//! src/routes/mod.rs
-mod health_check;
-mod requisition;
-
-pub use health_check::*;
-pub use requisition::*;
