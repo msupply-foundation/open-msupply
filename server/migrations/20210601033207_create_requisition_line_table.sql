4296fd657b6630e579cfe9842b172df69b1ab2fc
wlthomson <will.tho@outlook.com>
2021-06-01 18:00:03 +1200

    Add sqlx db migrations

---
+++ server/migrations/20210601033207_create_requisition_line_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+-- Create requisition line table 
+CREATE TABLE requisition_line (
+    id TEXT NOT NULL,
+    CONSTRAINT pk_requisition_line PRIMARY KEY(id),
+    requisition_id TEXT NOT NULL,
+    CONSTRAINT fk_requisition FOREIGN KEY(requisition_id) REFERENCES requisition(id),
+    item_name TEXT NOT NULL,
+    item_quantity REAL NOT NULL
+)
