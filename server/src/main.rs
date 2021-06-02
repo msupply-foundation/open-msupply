8e470c0cbe25fc18d5e0bef9afef1a93705f87b4
wlthomson <will.tho@outlook.com>
2021-06-02 19:17:04 +1200

    Add minimal sqlx integration

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,12 @@
 //! src/main.rs
 
-use std::{collections::HashMap, env};
+use std::{env};
 
 use actix_cors::Cors;
 use actix_web::{http::header, middleware, web, App, Error, HttpResponse, HttpServer};
 use juniper::{graphql_object, EmptyMutation, EmptySubscription, GraphQLObject, RootNode};
 use juniper_actix::{graphql_handler};
+use sqlx::PgPool;
 
 #[derive(Clone, GraphQLObject)]
 // A requisition.
@@ -25,87 +26,136 @@ pub struct RequisitionLine {
     item_quantity: f64,
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct Database {
-    // Mock database connection with hash lookups.
-    requisitions: HashMap<String, Requisition>,
+    connection_pool: PgPool,
 }
 
-pub fn generate_requisitions() -> HashMap<String, Requisition> {
-    let mut requisitions = HashMap::new();
+struct RequisitionRow { 
+    id: String,
+    from_id: String,
+    to_id: String
+}
 
-    requisitions.insert(
-        "requisition_a".to_string(),
-        Requisition {
-            id: "requisition_a".to_string(),
-            from_id: "store_a".to_string(),
-            to_id: "store_b".to_string(),
-            requisition_lines: vec![
-                RequisitionLine {
-                    id: "requisition_line_a".to_string(),
-                    requisition_id: "requisition_a".to_string(),
-                    item_name: "item_a".to_string(),
-                    item_quantity: 1.0,
-                },
-                RequisitionLine {
-                    id: "requisition_line_b".to_string(),
-                    requisition_id: "requisition_a".to_string(),
-                    item_name: "item_b".to_string(),
-                    item_quantity: 2.0,
-                },
-            ],
-        },
-    );
+struct RequisitionLineRow {
+    id: String,
+    requisition_id: String,
+    item_name: String,
+    item_quantity: f32
+}
 
-    requisitions.insert(
-        "requisition_b".to_string(),
-        Requisition {
-            id: "requisition_b".to_string(),
-            from_id: "store_a".to_string(),
-            to_id: "store_c".to_string(),
-            requisition_lines: vec![
-                RequisitionLine {
-                    id: "requisition_line_c".to_string(),
-                    requisition_id: "requisition_b".to_string(),
-                    item_name: "item_a".to_string(),
-                    item_quantity: 3.0,
-                },
-                RequisitionLine {
-                    id: "requisition_line_d".to_string(),
-                    requisition_id: "requisition_b".to_string(),
-                    item_name: "item_b".to_string(),
-                    item_quantity: 4.0,
-                },
-            ],
-        },
-    );
+async fn select_requisition(connection_pool: &PgPool, id: String) -> Result<RequisitionRow, sqlx::Error> {
+    let requisition_row = sqlx::query_as!(RequisitionRow,
+        r#"
+        SELECT id, from_id, to_id
+        FROM requisition
+        WHERE id = $1
+        "#, id
+    )
+    .fetch_one(connection_pool)
+    .await?;
 
-    requisitions.insert(
-        "requisition_c".to_string(),
-        Requisition {
-            id: "requisition_c".to_string(),
-            from_id: "store_b".to_string(),
-            to_id: "store_c".to_string(),
-            requisition_lines: vec![RequisitionLine {
-                id: "requisition_line_d".to_string(),
-                requisition_id: "requisition_c".to_string(),
-                item_name: "item_a".to_string(),
-                item_quantity: 5.0,
-            }],
-        },
-    );
+    Ok(requisition_row)
+}
 
-    requisitions
+#[allow(dead_code)]
+async fn select_requisition_line(connection_pool: &PgPool, id: String) -> Result<RequisitionLineRow, sqlx::Error> {
+    let requisition_line = sqlx::query_as!(RequisitionLineRow,
+        r#"
+        SELECT id, requisition_id, item_name, item_quantity
+        FROM requisition_line
+        WHERE id = $1
+        "#, id
+    )
+    .fetch_one(connection_pool)
+    .await?;
+
+    Ok(requisition_line)
+}
+
+async fn select_requisition_lines(connection_pool: &PgPool, requisition_id: String) -> Result<Vec<RequisitionLineRow>, sqlx::Error> {
+    let requisition_lines = sqlx::query_as!(RequisitionLineRow,
+        r#"
+        SELECT id, requisition_id, item_name, item_quantity
+        FROM requisition_line 
+        WHERE requisition_id = $1
+        "#, requisition_id
+    )
+    .fetch_all(connection_pool)
+    .await?;
+
+    Ok(requisition_lines)
+}
+
+async fn insert_requisition(connection_pool: &PgPool, id: String, from_id: String, to_id: String) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO requisition (id, from_id, to_id)
+        VALUES ($1, $2, $3)
+        "#,
+        id,
+        from_id,
+        to_id
+    )
+    .execute(connection_pool)
+    .await?;
+
+    Ok(())
+}
+
+async fn insert_requisition_line(connection_pool: &PgPool, id: String, requisition_id: String, item_name: String, item_quantity: f32) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO requisition_line (id, requisition_id, item_name, item_quantity)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        id,
+        requisition_id,
+        item_name,
+        item_quantity,
+    )
+    .execute(connection_pool)
+    .await?;
+
+    Ok(())
+}
+
+async fn insert_mock_data(connection_pool: &PgPool) -> Result<(), sqlx::Error> {
+    insert_requisition(connection_pool, "requisition_a".to_string(), "store_a".to_string(), "store_b".to_string()).await?;
+    insert_requisition(connection_pool, "requisition_b".to_string(), "store_a".to_string(), "store_c".to_string()).await?;
+    insert_requisition(connection_pool, "requisition_c".to_string(), "store_b".to_string(), "store_c".to_string()).await?;
+
+    insert_requisition_line(connection_pool, "requisition_line_a".to_string(), "requisition_a".to_string(), "item_a".to_string(), 1.0).await?;
+    insert_requisition_line(connection_pool, "requisition_line_b".to_string(), "requisition_a".to_string(), "item_b".to_string(), 2.0).await?;
+    insert_requisition_line(connection_pool, "requisition_line_c".to_string(), "requisition_b".to_string(), "item_a".to_string(), 3.0).await?;
+    insert_requisition_line(connection_pool, "requisition_line_d".to_string(), "requisition_b".to_string(), "item_b".to_string(), 4.0).await?;
+    insert_requisition_line(connection_pool, "requisition_line_e".to_string(), "requisition_c".to_string(), "item_a".to_string(), 5.0).await?;
+
+    Ok(())
 }
 
 impl Database {
-    pub fn new() -> Database {
-        let requisitions = generate_requisitions();
-        Database { requisitions }
+    pub async fn new(connection_pool: PgPool) -> Database {
+        Database { connection_pool }
     }
 
-    pub fn get_requisition(&self, id: String) -> Option<&Requisition> {
-        self.requisitions.get(&id)
+    pub async fn get_requisition(&self, id: String) -> Result<Requisition, sqlx::Error> {
+        let requisition_row = select_requisition(&self.connection_pool, id.to_string()).await?;
+        let requisition_line_rows = select_requisition_lines(&self.connection_pool, id.to_string()).await?;
+
+        let requisition = Requisition {
+            id: requisition_row.id,
+            from_id: requisition_row.from_id,
+            to_id: requisition_row.to_id,
+            requisition_lines: requisition_line_rows.into_iter().map(|line| RequisitionLine {
+                id: line.id,
+                requisition_id: line.requisition_id,
+                item_name: line.item_name,
+                item_quantity: line.item_quantity as f64
+            }).collect()
+        };
+
+        Ok(requisition)
     }
 }
 
@@ -119,8 +169,8 @@ impl Query {
     }
 
     #[graphql(arguments(id(description = "id of the requisition")))]
-    fn requisition(database: &Database, id: String) -> Option<&Requisition> {
-        database.get_requisition(id)
+    async fn requisition(database: &Database, id: String) -> Requisition {
+        database.get_requisition(id).await.unwrap()
     }
 }
 
@@ -139,7 +189,11 @@ async fn graphql_route(
     payload: actix_web::web::Payload,
     schema: web::Data<Schema>,
 ) -> Result<HttpResponse, Error> {
-    let context = Database::new();
+    let connection_pool =
+        PgPool::connect("postgres://postgres:password@localhost/omsupply-database")
+            .await
+            .expect("Failed to connect to database");
+    let context = Database::new(connection_pool).await;
     graphql_handler(&schema, &context, req, payload).await
 }
 
@@ -147,6 +201,15 @@ async fn graphql_route(
 async fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
+
+    let connection_pool =
+    PgPool::connect("postgres://postgres:password@localhost/omsupply-database")
+        .await
+        .expect("Failed to connect to database");
+
+    insert_mock_data(&connection_pool)
+        .await
+        .expect("Failed to insert mock data");
 
     let server = HttpServer::new(move || {
         App::new()
