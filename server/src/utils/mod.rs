539ba28c20033ea0994afece0cf9e06b7103148a
wlthomson <will.tho@outlook.com>
2021-06-06 18:04:14 +1200

    Add settings structs

--- server/src/utils/mod.rs
+++ server/src/utils/mod.rs
@@ -2,6 +2,8 @@
 
 pub mod configuration;
 pub mod mock;
+pub mod settings;
 
 pub use configuration::*;
 pub use mock::*;
+pub use settings::*;
---
+++ server/src/utils/settings.rs
@@ -0,0 +1,37 @@
+//! src/utils/settings.rs
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub server: ServerSettings,
+    pub database: DatabaseSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ServerSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+impl ServerSettings {
+    pub fn address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.port, self.host, self.database_name
+        )
+    }
+}
