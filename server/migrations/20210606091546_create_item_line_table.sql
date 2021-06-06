99387447ea0f19b2dad4239ca74b7fa9c3bc8a23
wlthomson <will.tho@outlook.com>
2021-06-06 21:20:10 +1200

    Add item_line migration script

---
+++ server/migrations/20210606091546_create_item_line_table.sql
@@ -0,0 +1,9 @@
+-- Create item_line table 
+CREATE TABLE item_line (
+    id TEXT NOT NULL,
+    CONSTRAINT pk_item_line PRIMARY KEY(id),
+    item_id TEXT NOT NULL,
+    CONSTRAINT fk_item FOREIGN KEY(item_id) REFERENCES item(id)
+    batch TEXT NOT NULL,
+    quantity REAL NOT NULL
+)
