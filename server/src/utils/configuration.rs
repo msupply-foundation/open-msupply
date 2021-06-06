02d8446b9ad93c2edf2da696884b07d6f9bc3331
wlthomson <will.tho@outlook.com>
2021-06-06 18:10:23 +1200

    Add configuration getter
    Source: https://github.com/msupply-foundation/remote-server/commit/9694e9c0a8ede6593c1b67ea733cdb2e60172b6f

--- server/src/utils/configuration.rs
+++ server/src/utils/configuration.rs
@@ -1,1 +1,26 @@
+//! src/utils/configuration.rs
 
+use crate::utils::{Environment, Settings};
+
+use std::convert::TryInto;
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    settings.merge(config::File::with_name("configuration"))?;
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+
+    settings.try_into()
+}
