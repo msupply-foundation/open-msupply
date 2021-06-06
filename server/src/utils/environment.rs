547d768dcc10b485975a4d8bf0890ed45e94ce7c
wlthomson <will.tho@outlook.com>
2021-06-06 18:09:12 +1200

    Add environment enum
    Source: https://github.com/msupply-foundation/remote-server/commit/05f1bfd6bc4f73cd123b0115a1cb73f4721b7b61

---
+++ server/src/utils/environment.rs
@@ -0,0 +1,31 @@
+//! src/utils/environment.rs
+
+use std::convert::TryFrom;
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
