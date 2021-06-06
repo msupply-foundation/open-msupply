4338e9ccf69b691007a4e3d895ae6030406f8afd
wlthomson <will.tho@outlook.com>
2021-06-06 20:39:07 +1200

    Add item mocks

--- server/src/utils/mod.rs
+++ server/src/utils/mod.rs
@@ -2,10 +2,8 @@
 
 pub mod configuration;
 pub mod environment;
-pub mod mock;
 pub mod settings;
 
 pub use self::configuration::*;
 pub use self::environment::*;
-pub use self::mock::*;
 pub use self::settings::*;
