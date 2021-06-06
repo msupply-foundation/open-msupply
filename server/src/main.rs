345a97b15ffc1854ae1d69c197de2747384493ec
wlthomson <will.tho@outlook.com>
2021-06-06 18:15:22 +1200

    Remove hardcoded config strings
    Source: https://github.com/msupply-foundation/remote-server/commit/40a946c7d8a7b0bd0601e1e483d637111d806fcb

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,23 +4,26 @@ mod database;
 mod server;
 mod utils;
 
-use std::{env, io};
 use std::net::TcpListener;
+use std::{env, io};
+
+use utils::get_configuration;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let listener =
-        TcpListener::bind("127.0.0.1:8080").expect("Failed to bind server to address");
+    let configuration = get_configuration().expect("Failed to parse configuration settings");
 
-    let database = database::DatabaseConnection::new(
-        sqlx::PgPool::connect("postgres://postgres:password@localhost/omsupply-database")
-            .await
-            .expect("Failed to connect to omsupply-database"),
-    )
-    .await;
+    let listener = TcpListener::bind(configuration.server.address())
+        .expect("Failed to bind server to address");
+
+    let pool = sqlx::PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to omsupply-database");
+
+    let database = database::DatabaseConnection::new(pool).await;
 
     // TODO: replace mock data with tests
     database
