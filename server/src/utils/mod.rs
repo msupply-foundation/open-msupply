49a120c6a17b99a1e84872a9a271c08843d09aa1
wlthomson <will.tho@outlook.com>
2021-06-06 18:09:12 +1200

    Add environment enum

--- server/src/utils/mod.rs
+++ server/src/utils/mod.rs
@@ -1,9 +1,11 @@
 //! src/utils/mod.rs
 
 pub mod configuration;
+pub mod environment;
 pub mod mock;
 pub mod settings;
 
 pub use configuration::*;
+pub use environment::*;
 pub use mock::*;
 pub use settings::*;
