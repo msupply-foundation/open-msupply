c66185311c4fff40e6496b3344d3cde2d171e30b
wlthomson <will.tho@outlook.com>
2021-06-06 21:40:46 +1200

    Fix item_line sql typo
    Source: https://github.com/msupply-foundation/remote-server/commit/94678bbd233497ec6abd930022870a7525402fae

--- server/migrations/20210606091546_create_item_line_table.sql
+++ server/migrations/20210606091546_create_item_line_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE item_line (
     id TEXT NOT NULL,
     CONSTRAINT pk_item_line PRIMARY KEY(id),
     item_id TEXT NOT NULL,
-    CONSTRAINT fk_item FOREIGN KEY(item_id) REFERENCES item(id)
+    CONSTRAINT fk_item FOREIGN KEY(item_id) REFERENCES item(id),
     batch TEXT NOT NULL,
     quantity REAL NOT NULL
 )
