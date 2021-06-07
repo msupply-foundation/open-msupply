5a9b55b8c00951567bf9518f515b2394a8947b6b
wlthomson <will.tho@outlook.com>
2021-06-07 18:32:41 +1200

    Add requisition_line foreign key to item

--- server/migrations/20210601033207_create_requisition_line_table.sql
+++ server/migrations/20210601033207_create_requisition_line_table.sql
@@ -1,9 +1,7 @@
 -- Create requisition line table 
 CREATE TABLE requisition_line (
-    id TEXT NOT NULL,
-    CONSTRAINT pk_requisition_line PRIMARY KEY(id),
+    id TEXT NOT NULL PRIMARY KEY,
     requisition_id TEXT NOT NULL,
-    CONSTRAINT fk_requisition FOREIGN KEY(requisition_id) REFERENCES requisition(id),
-    item_name TEXT NOT NULL,
+    item_id TEXT NOT NULL,
     item_quantity REAL NOT NULL
 )
