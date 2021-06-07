b07d5f485e792cd307cab5db2933ddf903927a75
wlthomson <will.tho@outlook.com>
2021-06-07 18:48:36 +1200

    Add transaction foreign key to name
    Source: https://github.com/msupply-foundation/remote-server/commit/fa7bc738ae0d57cda593f854409108af4307d9e9

--- server/migrations/20210607021315_create_transaction_table.sql
+++ server/migrations/20210607021315_create_transaction_table.sql
@@ -1,8 +1,6 @@
 -- Create transaction table
 CREATE TABLE transaction (
-    id TEXT NOT NULL,
-    constraint pk_transaction PRIMARY KEY(id),
-    -- TODO: add foreign key to name table
+    id TEXT NOT NULL PRIMARY KEY,
     name_id TEXT NOT NULL,
     invoice_number INTEGER NOT NULL
 )
