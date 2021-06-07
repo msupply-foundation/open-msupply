0ae61aebd6786e6e8e35eafc9191f57e5af45118
wlthomson <will.tho@outlook.com>
2021-06-07 22:28:13 +1200

    Update requisition_line mock data

--- server/src/database/mocks.rs
+++ server/src/database/mocks.rs
@@ -45,35 +45,35 @@ pub fn mock_requisition_lines() -> Vec<RequisitionLineRow> {
     let requisition_line_a = RequisitionLineRow {
         id: "requisition_line_a".to_string(),
         requisition_id: "requisition_a".to_string(),
-        item_name: "item_a".to_string(),
+        item_id: "item_a".to_string(),
         item_quantity: 1.0,
     };
 
     let requisition_line_b = RequisitionLineRow {
         id: "requisition_line_b".to_string(),
         requisition_id: "requisition_a".to_string(),
-        item_name: "item_b".to_string(),
+        item_id: "item_b".to_string(),
         item_quantity: 2.0,
     };
 
     let requisition_line_c = RequisitionLineRow {
         id: "requisition_line_c".to_string(),
         requisition_id: "requisition_b".to_string(),
-        item_name: "item_a".to_string(),
+        item_id: "item_a".to_string(),
         item_quantity: 3.0,
     };
 
     let requisition_line_d = RequisitionLineRow {
         id: "requisition_line_d".to_string(),
         requisition_id: "requisition_b".to_string(),
-        item_name: "item_b".to_string(),
+        item_id: "item_b".to_string(),
         item_quantity: 4.0,
     };
 
     let requisition_line_e = RequisitionLineRow {
         id: "requisition_line_e".to_string(),
         requisition_id: "requisition_c".to_string(),
-        item_name: "item_a".to_string(),
+        item_id: "item_a".to_string(),
         item_quantity: 5.0,
     };
 
