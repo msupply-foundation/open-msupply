2708267573e84bbca1208c020c2ac5c0ba06466f
wlthomson <will.tho@outlook.com>
2021-06-08 09:52:04 +1200

    Add mock item_line data
    Source: https://github.com/msupply-foundation/remote-server/commit/f8b40211e593245986ea9132ff6a15c439b4e222

--- server/src/database/mocks.rs
+++ server/src/database/mocks.rs
@@ -1,4 +1,4 @@
-use crate::database::schema::{ItemRow, RequisitionLineRow, RequisitionRow};
+use crate::database::schema::{ItemRow, ItemLineRow, RequisitionLineRow, RequisitionRow};
 
 pub fn mock_items() -> Vec<ItemRow> {
     let item_a = ItemRow {
@@ -17,6 +17,52 @@ pub fn mock_items() -> Vec<ItemRow> {
     };
 
     vec![item_a, item_b, item_c]
+}
+
+pub fn mock_item_lines() -> Vec<ItemLineRow> {
+    let item_line_a = ItemLineRow {
+        id: "item_a_line_a".to_string(),
+        item_id: "item_a".to_string(),
+        batch: "item_a_batch_a".to_string(),
+        quantity: 1.0,
+    };
+
+    let item_line_b = ItemLineRow {
+        id: "item_a_line_b".to_string(),
+        item_id: "item_a".to_string(),
+        batch: "item_a_batch_a".to_string(),
+        quantity: 2.0,
+    };
+
+    let item_line_c = ItemLineRow {
+        id: "item_b_line_a".to_string(),
+        item_id: "item_b".to_string(),
+        batch: "item_b_batch_a".to_string(),
+        quantity: 3.0,
+    };
+
+    let item_line_d = ItemLineRow {
+        id: "item_b_line_b".to_string(), 
+        item_id: "item_b".to_string(),
+        batch: "item_b_batch_b".to_string(),
+        quantity: 4.0,
+    };
+
+    let item_line_e = ItemLineRow {
+        id: "item_c_line_a".to_string(),
+        item_id: "item_c".to_string(),
+        batch: "item_c_batch_a".to_string(),
+        quantity: 5.0,
+    };
+
+    let item_line_f = ItemLineRow {
+        id: "item_c_line_a".to_string(),
+        item_id: "item_c".to_string(),
+        batch: "item_c_batch_a".to_string(),
+        quantity: 6.0,
+    };
+
+    vec![item_line_a, item_line_b, item_line_c, item_line_d, item_line_e, item_line_f]
 }
 
 pub fn mock_requisitions() -> Vec<RequisitionRow> {
