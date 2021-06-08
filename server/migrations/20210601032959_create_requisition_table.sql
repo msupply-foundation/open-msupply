000fd75d376ef019d6bb2e6178c4efafd177a406
wlthomson <will.tho@outlook.com>
2021-06-08 13:49:11 +1200

    Update requisition migration script
    Source: https://github.com/msupply-foundation/remote-server/commit/1bf744601d6387f44fe01e52daa9af3029eb9a28

--- server/migrations/20210601032959_create_requisition_table.sql
+++ server/migrations/20210601032959_create_requisition_table.sql
@@ -1,6 +1,17 @@
--- Create requisition table
+-- Create requisition table.
+--
+-- CREATE TABLE requisition (
+--   ID ALPHA PRIMARY KEY,
+--   name_ID ALPHA,
+--   store_ID ALPHA
+-- );
+--
+-- ID: unique id of the requisition.
+-- name_ID: id of the customer associated with the requisition.
+-- store_ID: id of the supplier associated with the requisition.
+
 CREATE TABLE requisition (
-    id TEXT NOT NULL PRIMARY KEY,
-    name_id TEXT NOT NULL,
-    store_id TEXT NOT NULL
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name_id VARCHAR(255) NOT NULL,
+    store_id VARCHAR(255) NOT NULL
 )
--- server/migrations/20210601033207_create_requisition_line_table.sql
+++ server/migrations/20210601033207_create_requisition_line_table.sql
@@ -1,7 +1,23 @@
--- Create requisition line table 
+-- Create requisition line table.
+--
+-- CREATE TABLE requisition_line (
+--   ID ALPHA PRIMARY KEY,
+--   requisition_ID ALPHA,
+--   item_ID ALPHA,
+--   actualQuan REAL,
+--   suggested_quantity REAL
+-- );
+--
+-- ID: unique id of the requisition_line.
+-- requisition_ID: id of the parent requisition.
+-- item_ID: id of the item associated with the requisition_line.
+-- actualQuan: requested quantity of item.
+-- suggested_quantity: calculated suggested quantity of item.
+
 CREATE TABLE requisition_line (
-    id TEXT NOT NULL PRIMARY KEY,
-    requisition_id TEXT NOT NULL,
-    item_id TEXT NOT NULL,
-    item_quantity REAL NOT NULL
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    requisition_id VARCHAR(255) NOT NULL,
+    item_id VARCHAR(255) NOT NULL,
+    actual_quantity DOUBLE PRECISION NOT NULL,
+    suggested_quantity DOUBLE PRECISION NOT NULL, 
 )
