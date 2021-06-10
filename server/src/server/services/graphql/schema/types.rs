996710c1f39f4a53d7d2ef6130bed4642468b4a0
wlthomson <will.tho@outlook.com>
2021-06-10 16:45:32 +1200

    Refactor requisition line gql object

--- server/src/server/services/graphql/schema/types.rs
+++ server/src/server/services/graphql/schema/types.rs
@@ -111,31 +111,119 @@ impl Requisition {
     }
 }
 
-#[derive(Clone, GraphQLObject)]
+#[derive(Clone)]
 // A requisition line.
 pub struct RequisitionLine {
-    pub id: String,
-    pub item_id: String,
-    pub actual_quantity: f64,
-    pub suggested_quantity: f64,
+    pub requisition_line_row: RequisitionLineRow
 }
 
-#[derive(Clone, GraphQLObject)]
+#[graphql_object(Context = DatabaseConnection)]
+impl RequisitionLine {
+    pub fn id(&self) -> String {
+        self.requisition_line_row.id.clone()
+    }
+
+    pub async fn item(&self, database: &DatabaseConnection) -> Item {
+        let item_row = database
+        .get_item(self.requisition_line_row.item_id.clone())
+        .await
+        .unwrap_or_else(|_| panic!("Failed to get item for item line {}", self.requisition_line_row.id));
+
+        Item { 
+            item_row
+        }
+    }
+
+    pub fn actual_quantity(&self) -> f64 {
+        self.requisition_line_row.actual_quantity
+    }
+
+    pub fn suggested_quantity(&self) -> f64 {
+        self.requisition_line_row.suggested_quantity
+    }
+}
+
+#[derive(Clone)]
 // A transaction.
 pub struct Transact {
-    pub id: String,
-    pub name_id: String,
-    pub invoice_number: i32,
-    pub trans_lines: Vec<TransLine>,
+    pub transact_row: TransactRow
 }
 
-#[derive(Clone, GraphQLObject)]
+#[graphql_object(Context = DatabaseConnection)]
+impl Transact {
+    pub fn id(&self) -> String {
+        self.transact_row.id.to_string()
+    }
+
+    pub async fn name(&self, database: &DatabaseConnection) -> Name {
+        let name_row = database
+            .get_name(self.transact_row.name_id.clone())
+            .await
+            .unwrap_or_else(|_| panic!("Failed to get name for transact {}", self.transact_row.id));
+
+        Name { name_row }
+    } 
+
+    pub fn invoice_number(&self) -> i32 {
+        self.transact_row.invoice_number
+    }    
+
+    pub async fn trans_lines(&self, database: &DatabaseConnection) -> Vec<TransLine> {
+        let trans_line_rows: Vec<TransLineRow> = database
+            .get_trans_lines(self.transact_row.id.clone())
+            .await
+            .unwrap_or_else(|_| panic!("Failed to get trans_lines for transact {}", self.transact_row.id));
+
+        trans_line_rows
+            .into_iter()
+            .map(|trans_line_row| TransLine { trans_line_row })
+            .collect()
+    }
+}
+
+#[derive(Clone)]
 // A transaction line
 pub struct TransLine {
-    pub id: String,
-    pub transaction_id: String,
-    pub item_id: String,
-    pub item_line_id: String,
+    pub trans_line_row: TransLineRow
+}
+
+#[graphql_object(Context = DatabaseConnection)]
+impl TransLine {
+    pub fn id(&self) -> String {
+        self.trans_line_row.id.clone()
+    }
+
+    pub async fn transaction(&self, database: &DatabaseConnection) -> Transact {
+        let transact_row: TransactRow = database
+            .get_transact(self.trans_line_row.transaction_id.clone())
+            .await
+            .unwrap_or_else(|_| panic!("Failed to get transact for trans_line {}", self.trans_line_row.id));
+
+        Transact { transact_row }
+    }
+
+    pub async fn item(&self, database: &DatabaseConnection) -> Item {
+        let item_row = database
+            .get_item(self.trans_line_row.item_id.clone())
+            .await
+            .unwrap_or_else(|_| panic!("Failed to get item for trans_line {}", self.trans_line_row.id));
+
+        Item { 
+            item_row
+        }
+    }
+
+    pub async fn item_line(&self, database: &DatabaseConnection) -> ItemLine {
+        // Handle optional item_line_id correctly.
+        let item_line_row = database
+            .get_item_line(self.trans_line_row.item_line_id.clone().unwrap())
+            .await
+            .unwrap_or_else(|_| panic!("Failed to get item_line for trans_line {}", self.trans_line_row.id));
+
+        ItemLine { 
+            item_line_row
+        }
+    }
 }
 
 #[derive(Clone, GraphQLInputObject)]
