37592dffdb2c73fe2c525f196a127f8ae605e003
wlthomson <will.tho@outlook.com>
2021-06-22 17:11:14 +1200

    Extract auth header in gql handler
    Source: https://github.com/msupply-foundation/remote-server/commit/3821aae8d07445461adc64c56a0e6dfd4d443940

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -1,7 +1,9 @@
+pub mod authorization;
 pub mod middleware;
 pub mod services;
 pub mod startup;
 
+pub use self::authorization::*;
 pub use self::middleware::*;
 pub use self::services::*;
 pub use self::startup::*;
