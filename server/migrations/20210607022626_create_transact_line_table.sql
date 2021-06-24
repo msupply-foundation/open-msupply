2285e01b7ae3d04a2cd58718a191d662290101ba
wlthomson <will.tho@outlook.com>
2021-06-24 20:02:16 +1200

    Add transact_line table type_of field
    Source: https://github.com/msupply-foundation/remote-server/commit/f3373acff1e2aaf9ce3124d13eea4e84b61b540d

--- server/migrations/20210607022626_create_transact_line_table.sql
+++ server/migrations/20210607022626_create_transact_line_table.sql
@@ -1,11 +1,15 @@
 -- Create transact_line table.
 
+CREATE TYPE transact_line_type AS ENUM ('stock_out', 'stock_in', 'placeholder', 'cash_in', 'cash_out', 'non_stock', 'service');
+
 CREATE TABLE transact_line (
     -- Unique id assigned to each transact_line.
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     -- Id of the parent transact.
     transact_id VARCHAR(255) NOT NULL,
-    -- Id of the item associated with the transact_line.
+    -- Type of the transact_line.
+    type_of transact_line_type NOT NULL,
+-- Id of the item associated with the transact_line.
     item_id VARCHAR(255) NOT NULL,
     -- Id of the item_line associated with the transact_line (null for placeholders, service items).
     item_line_id VARCHAR(255)
