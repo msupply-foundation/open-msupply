1335be1def1f1e09f9296e875dfad8c1c3734723
wlthomson <will.tho@outlook.com>
2021-06-24 21:02:16 +1200

    Add item table type_of field
    Source: https://github.com/msupply-foundation/remote-server/commit/8a97f724b3e91d2f3fe1ed4b997e4bbb00d133e8

--- server/migrations/20210606073020_create_item_table.sql
+++ server/migrations/20210606073020_create_item_table.sql
@@ -1,8 +1,11 @@
 -- Create item table.
 
+CREATE TYPE item_type AS ENUM ('general', 'service', 'cross_reference');
+
 CREATE TABLE item (
     -- Unique id assigned to each item.
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     -- Name of the item.
-    item_name VARCHAR(255) NOT NULL
+    item_name VARCHAR(255) NOT NULL,
+    type_of item_type NOT NULL
 )
