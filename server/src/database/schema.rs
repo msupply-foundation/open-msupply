4ed28d46ebd738ef8814e103b8988929d9a36ca8
wlthomson <will.tho@outlook.com>
2021-06-24 21:03:10 +1200

    Add ItemRow type_of enum

--- server/src/database/schema.rs
+++ server/src/database/schema.rs
@@ -113,10 +113,23 @@ pub struct TransactLineRow {
     pub type_of: TransactLineRowType,
 }
 
+#[derive(sqlx::Type)]
+#[sqlx(rename = "item_type")]
+#[derive(Clone)]
+pub enum ItemRowType {
+    #[sqlx(rename = "general")]
+    General,
+    #[sqlx(rename = "service")]
+    Service,
+    #[sqlx(rename = "cross_reference")]
+    CrossReference,
+}
+
 #[derive(Clone)]
 pub struct ItemRow {
     pub id: String,
     pub item_name: String,
+    pub type_of: ItemRowType,
 }
 
 #[derive(Clone)]
