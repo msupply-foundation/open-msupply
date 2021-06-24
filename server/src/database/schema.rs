89f0c6b46807b3ae6c0155a6e3c62832f2365453
wlthomson <will.tho@outlook.com>
2021-06-24 20:02:43 +1200

    Add TransactLineRow type_of enum

--- server/src/database/schema.rs
+++ server/src/database/schema.rs
@@ -84,12 +84,33 @@ pub struct TransactRow {
     pub type_of: TransactRowType,
 }
 
+#[derive(sqlx::Type)]
+#[sqlx(rename = "transact_line_type")]
+#[derive(Clone)]
+pub enum TransactLineRowType {
+    #[sqlx(rename = "stock_out")]
+    StockOut,
+    #[sqlx(rename = "stock_in")]
+    StockIn,
+    #[sqlx(rename = "placeholder")]
+    Placeholder,
+    #[sqlx(rename = "cash_in")]
+    CashIn,
+    #[sqlx(rename = "cash_out")]
+    CashOut,
+    #[sqlx(rename = "non_stock")]
+    NonStock,
+    #[sqlx(rename = "service")]
+    Service
+}
+
 #[derive(Clone)]
 pub struct TransactLineRow {
     pub id: String,
     pub transact_id: String,
     pub item_id: String,
     pub item_line_id: Option<String>,
+    pub type_of: TransactLineRowType,
 }
 
 #[derive(Clone)]
