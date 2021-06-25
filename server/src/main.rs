b076695d7eb8f07977eaaf56e4b5177b85da8e5f
wlthomson <will.tho@outlook.com>
2021-06-25 17:17:52 +1200

    Refactor project as library
    Source: https://github.com/msupply-foundation/remote-server/commit/a635a38047524d299846ef47c149e24a0f4921e1

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,19 @@
-mod database;
-mod server;
-mod utils;
-
-use std::net::TcpListener;
-use std::{env, io};
-
-use utils::get_configuration;
-
 #[actix_web::main]
-async fn main() -> io::Result<()> {
-    env::set_var("RUST_LOG", "info");
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let configuration = get_configuration().expect("Failed to parse configuration settings");
+    let configuration = remote_server::util::configuration::get_configuration()
+        .expect("Failed to parse configuration settings");
 
-    let listener = TcpListener::bind(configuration.server.address())
+    let listener = std::net::TcpListener::bind(configuration.server.address())
         .expect("Failed to bind server to address");
 
     let pool = sqlx::PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to omsupply-database");
 
-    let database = database::DatabaseConnection::new(pool).await;
+    let database = remote_server::database::connection::DatabaseConnection::new(pool).await;
 
     // TODO: replace mock data with tests
     database
@@ -29,5 +21,5 @@ async fn main() -> io::Result<()> {
         .await
         .expect("Failed to insert mock data");
 
-    server::run(listener, database)?.await
+    remote_server::run(listener, database)?.await
 }
