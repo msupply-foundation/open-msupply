de99b25ac48fd7f93dc174264d200a5164c6734d
wlthomson <will.tho@outlook.com>
2021-06-27 16:25:11 +1200

    Add example health_check integration test
    Source: https://github.com/msupply-foundation/remote-server/commit/81439b0fb7f8a8ed8a4ecc2752faf3d7e245d467

---
+++ server/tests/rest.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod rest {
+    use remote_server::server;
+
+    #[actix_rt::test]
+    async fn health_check_returns_200() {
+        let mut app = actix_web::test::init_service(
+            actix_web::App::new().configure(server::services::rest::config),
+        )
+        .await;
+
+        let req = actix_web::test::TestRequest::get()
+            .uri("/health_check")
+            .to_request();
+
+        let res = actix_web::test::call_service(&mut app, req).await;
+
+        assert!(res.status().is_success());
+    }
+}
