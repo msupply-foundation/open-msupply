92012f84d3363bc8ee2cd21c4f5f916463341ea1
wlthomson <will.tho@outlook.com>
2021-07-08 11:29:41 +1200

    Update RepositoryRegistry struct with optional fields

--- server/src/server/data/registry.rs
+++ server/src/server/data/registry.rs
@@ -5,15 +5,17 @@ use std::sync::Arc;
 #[derive(Clone)]
 pub struct RepositoryRegistry {
     // TODO: implement with hash maps to dynamic trait objects.
-    // pub repositories: CHashMap<String, Arc<dyn database::repository::Repository>>
-    pub customer_invoice_repository: Arc<dyn database::repository::CustomerInvoiceRepository>,
-    pub item_repository: Arc<dyn database::repository::ItemRepository>,
-    pub item_line_repository: Arc<dyn database::repository::ItemLineRepository>,
-    pub name_repository: Arc<dyn database::repository::NameRepository>,
-    pub requisition_repository: Arc<dyn database::repository::RequisitionRepository>,
-    pub requisition_line_repository: Arc<dyn database::repository::RequisitionLineRepository>,
-    pub store_repository: Arc<dyn database::repository::StoreRepository>,
-    pub transact_repository: Arc<dyn database::repository::TransactRepository>,
-    pub transact_line_repository: Arc<dyn database::repository::TransactLineRepository>,
-    pub user_account_repository: Arc<dyn database::repository::UserAccountRepository>,
+    // pub repositories: HashMap<String, Arc<dyn database::repository::Repository>>
+    pub customer_invoice_repository:
+        Option<Arc<dyn database::repository::CustomerInvoiceRepository>>,
+    pub item_repository: Option<Arc<dyn database::repository::ItemRepository>>,
+    pub item_line_repository: Option<Arc<dyn database::repository::ItemLineRepository>>,
+    pub name_repository: Option<Arc<dyn database::repository::NameRepository>>,
+    pub requisition_repository: Option<Arc<dyn database::repository::RequisitionRepository>>,
+    pub requisition_line_repository:
+        Option<Arc<dyn database::repository::RequisitionLineRepository>>,
+    pub store_repository: Option<Arc<dyn database::repository::StoreRepository>>,
+    pub transact_repository: Option<Arc<dyn database::repository::TransactRepository>>,
+    pub transact_line_repository: Option<Arc<dyn database::repository::TransactLineRepository>>,
+    pub user_account_repository: Option<Arc<dyn database::repository::UserAccountRepository>>,
 }
