80c8f022b08545e4367a0e3a224f40589c9bcb85
Chris Morgan <me@chrismorgan.info>
2021-07-12 16:10:59 +1200

    cargo fmt

--- server/src/server/service/rest/mod.rs
+++ server/src/server/service/rest/mod.rs
@@ -1,11 +1,15 @@
 pub mod routes;
 
 pub fn config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(actix_web::web::scope("/").route(
-        routes::paths::HEALTH_CHECK,
-        actix_web::web::get().to(routes::handlers::health_check),
-    ).route(
-        routes::paths::SCHEDULE_SYNC,
-        actix_web::web::get().to(routes::handlers::schedule_sync),
-    ));
+    cfg.service(
+        actix_web::web::scope("/")
+            .route(
+                routes::paths::HEALTH_CHECK,
+                actix_web::web::get().to(routes::handlers::health_check),
+            )
+            .route(
+                routes::paths::SCHEDULE_SYNC,
+                actix_web::web::get().to(routes::handlers::schedule_sync),
+            ),
+    );
 }
