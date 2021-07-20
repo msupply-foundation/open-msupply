21aa09a2569411cac0f18b44fdfcb4f02531055a
wlthomson <will.tho@outlook.com>
2021-07-21 11:48:39 +1200

    Update configuration constants

--- server/src/util/environment.rs
+++ server/src/util/environment.rs
@@ -1,5 +1,8 @@
 use std::str::FromStr;
 
+pub static LOCAL: &str = "local";
+pub static PRODUCTION: &str = "production";
+
 pub enum Environment {
     Local,
     Production,
@@ -8,8 +11,8 @@ pub enum Environment {
 impl Environment {
     pub fn as_str(&self) -> &'static str {
         match self {
-            Environment::Local => "local",
-            Environment::Production => "production",
+            Environment::Local => LOCAL,
+            Environment::Production => PRODUCTION,
         }
     }
 }
@@ -18,14 +21,14 @@ impl FromStr for Environment {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.eq_ignore_ascii_case("local") {
+        if s.eq_ignore_ascii_case(LOCAL) {
             Ok(Self::Local)
-        } else if s.eq_ignore_ascii_case("production") {
+        } else if s.eq_ignore_ascii_case(PRODUCTION) {
             Ok(Self::Production)
         } else {
             Err(format!(
-                "{} is not a supported environment. Use either `local` or `production`.",
-                s
+                "{} is not a supported environment. Use either `{}` or `{}`.",
+                s, LOCAL, PRODUCTION
             ))
         }
     }
