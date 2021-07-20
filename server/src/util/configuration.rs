7fd667453efb3c2a56f8420098e9ed34204b19b7
wlthomson <will.tho@outlook.com>
2021-07-21 11:48:39 +1200

    Update configuration constants
    Source: https://github.com/msupply-foundation/remote-server/commit/dd9dc4697c694ef8e4e0588487f8f551065d51c4

--- server/src/util/configuration.rs
+++ server/src/util/configuration.rs
@@ -1,18 +1,21 @@
 use crate::util::settings::Settings;
+use crate::util::environment::LOCAL as LOCAL_ENVIRONMENT;
 use config::{Config, ConfigError, File};
 
-static CONFIGURATION_FOLDER: &str = "configuration";
-static BASE_FILE: &str = "base";
-static DEFAULT_EXTRA_FILE: &str = "local";
+static CONFIGURATION_DIRECTORY_PATH: &str = "configuration";
+static BASE_CONFIGURATION_FILE_PATH: &str = "base";
+
+static APP_ENVIRONMENT_VAR: &str = "APP_ENVIRONMENT";
+static DEFAULT_APP_ENVIRONMENT: &str = LOCAL_ENVIRONMENT;
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
     let mut settings = Config::default();
     let base_path = std::env::current_dir().map_err(|err| ConfigError::Message(err.to_string()))?;
-    let configuration_directory = base_path.join(CONFIGURATION_FOLDER);
+    let configuration_directory = base_path.join(CONFIGURATION_DIRECTORY_PATH);
 
-    settings.merge(File::from(configuration_directory.join(BASE_FILE)).required(true))?;
+    settings.merge(File::from(configuration_directory.join(BASE_CONFIGURATION_FILE_PATH)).required(true))?;
 
-    let extra_file = std::env::var("APP_ENVIRONMENT").unwrap_or_else(|_| DEFAULT_EXTRA_FILE.into());
+    let extra_file = std::env::var(APP_ENVIRONMENT_VAR).unwrap_or_else(|_| DEFAULT_APP_ENVIRONMENT.into());
 
     settings.merge(File::from(configuration_directory.join(extra_file)).required(true))?;
 
