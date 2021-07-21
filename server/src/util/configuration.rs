e33c00b3c09ce04833117e470d2ffe0c0e335597
wlthomson <will.tho@outlook.com>
2021-07-21 13:16:32 +1200

    Add SettingsError enum
    Source: https://github.com/msupply-foundation/remote-server/commit/4aafc5f603e5c088476ac7268ba1aa90f7189316

--- server/src/util/configuration.rs
+++ server/src/util/configuration.rs
@@ -1,8 +1,8 @@
-use crate::util::settings::Settings;
+use crate::util::settings::{Settings, SettingsError};
 use crate::util::environment::{Environment, LOCAL as LOCAL_ENVIRONMENT, PRODUCTION as PRODUCTION_ENVIRONMENT};
 
-use config::{Config, ConfigError, File};
-use std::borrow::Cow;
+use config::{Config, File};
+use std::{borrow::Cow, env};
 
 static CONFIGURATION_DIRECTORY_PATH: &str = "configuration";
 static BASE_CONFIGURATION_FILE_PATH: &str = "base";
@@ -12,19 +12,19 @@ static PRODUCTION_CONFIGURATION_FILE_PATH: &str = PRODUCTION_ENVIRONMENT;
 static APP_ENVIRONMENT_VAR: &str = "APP_ENVIRONMENT";
 static DEFAULT_APP_ENVIRONMENT: &str = LOCAL_ENVIRONMENT;
 
-pub fn get_configuration() -> Result<Settings, ConfigError> {
+pub fn get_configuration() -> Result<Settings, SettingsError> {
     let mut configuration = Config::default();
 
-    let current_directory_path = std::env::current_dir().map_err(|err| ConfigError::Message(err.to_string()))?;
+    let current_directory_path = env::current_dir().map_err(|err| SettingsError::Path(err.to_string()))?;
 
     let configuration_directory = current_directory_path.join(CONFIGURATION_DIRECTORY_PATH);
 
     configuration.merge(File::from(configuration_directory.join(BASE_CONFIGURATION_FILE_PATH)).required(true))?;
 
-    let environment: Environment = std::env::var(APP_ENVIRONMENT_VAR)
+    let environment: Environment = env::var(APP_ENVIRONMENT_VAR)
         .map(Cow::from)
         .unwrap_or_else(|_| DEFAULT_APP_ENVIRONMENT.into())
-        .parse().map_err(|err: String| ConfigError::Message(err))?;
+        .parse()?;
 
     let app_environment_file_path = match environment {
         Environment::Local => LOCAL_CONFIGURATION_FILE_PATH,
@@ -33,5 +33,5 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
 
     configuration.merge(File::from(configuration_directory.join(app_environment_file_path)).required(true))?;
 
-    configuration.try_into()
+    configuration.try_into().map_err(|err| SettingsError::Config(err))
 }
