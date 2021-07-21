f52b36c6e5b69d712667837028dfc189d9b8d0e2
wlthomson <will.tho@outlook.com>
2021-07-21 13:17:11 +1200

    Fix unformatted configuration code
    Source: https://github.com/msupply-foundation/remote-server/commit/cecc914b31d21124974e5b7dc00e6ae99e1ef56b

--- server/src/util/configuration.rs
+++ server/src/util/configuration.rs
@@ -1,5 +1,7 @@
+use crate::util::environment::{
+    Environment, LOCAL as LOCAL_ENVIRONMENT, PRODUCTION as PRODUCTION_ENVIRONMENT,
+};
 use crate::util::settings::{Settings, SettingsError};
-use crate::util::environment::{Environment, LOCAL as LOCAL_ENVIRONMENT, PRODUCTION as PRODUCTION_ENVIRONMENT};
 
 use config::{Config, File};
 use std::{borrow::Cow, env};
@@ -15,11 +17,14 @@ static DEFAULT_APP_ENVIRONMENT: &str = LOCAL_ENVIRONMENT;
 pub fn get_configuration() -> Result<Settings, SettingsError> {
     let mut configuration = Config::default();
 
-    let current_directory_path = env::current_dir().map_err(|err| SettingsError::Path(err.to_string()))?;
+    let current_directory_path =
+        env::current_dir().map_err(|err| SettingsError::Path(err.to_string()))?;
 
     let configuration_directory = current_directory_path.join(CONFIGURATION_DIRECTORY_PATH);
 
-    configuration.merge(File::from(configuration_directory.join(BASE_CONFIGURATION_FILE_PATH)).required(true))?;
+    configuration.merge(
+        File::from(configuration_directory.join(BASE_CONFIGURATION_FILE_PATH)).required(true),
+    )?;
 
     let environment: Environment = env::var(APP_ENVIRONMENT_VAR)
         .map(Cow::from)
@@ -31,7 +36,11 @@ pub fn get_configuration() -> Result<Settings, SettingsError> {
         Environment::Production => PRODUCTION_CONFIGURATION_FILE_PATH,
     };
 
-    configuration.merge(File::from(configuration_directory.join(app_environment_file_path)).required(true))?;
+    configuration.merge(
+        File::from(configuration_directory.join(app_environment_file_path)).required(true),
+    )?;
 
-    configuration.try_into().map_err(|err| SettingsError::Config(err))
+    configuration
+        .try_into()
+        .map_err(|err| SettingsError::Config(err))
 }
