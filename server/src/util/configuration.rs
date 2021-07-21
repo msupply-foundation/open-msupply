108c5033512d1c84ae0e484ac3563b9da9875c88
wlthomson <will.tho@outlook.com>
2021-07-21 12:24:35 +1200

    Revert removed configuration Environment parsing
    Source: https://github.com/msupply-foundation/remote-server/commit/fdb918a94abc716ebfeec862a42bb66109ddc03e

--- server/src/util/configuration.rs
+++ server/src/util/configuration.rs
@@ -1,9 +1,13 @@
 use crate::util::settings::Settings;
-use crate::util::environment::LOCAL as LOCAL_ENVIRONMENT;
+use crate::util::environment::{Environment, LOCAL as LOCAL_ENVIRONMENT, PRODUCTION as PRODUCTION_ENVIRONMENT};
+
 use config::{Config, ConfigError, File};
+use std::borrow::Cow;
 
 static CONFIGURATION_DIRECTORY_PATH: &str = "configuration";
 static BASE_CONFIGURATION_FILE_PATH: &str = "base";
+static LOCAL_CONFIGURATION_FILE_PATH: &str = LOCAL_ENVIRONMENT;
+static PRODUCTION_CONFIGURATION_FILE_PATH: &str = PRODUCTION_ENVIRONMENT;
 
 static APP_ENVIRONMENT_VAR: &str = "APP_ENVIRONMENT";
 static DEFAULT_APP_ENVIRONMENT: &str = LOCAL_ENVIRONMENT;
@@ -17,7 +21,15 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
 
     configuration.merge(File::from(configuration_directory.join(BASE_CONFIGURATION_FILE_PATH)).required(true))?;
 
-    let app_environment_file_path = std::env::var(APP_ENVIRONMENT_VAR).unwrap_or_else(|_| DEFAULT_APP_ENVIRONMENT.into());
+    let environment: Environment = std::env::var(APP_ENVIRONMENT_VAR)
+        .map(Cow::from)
+        .unwrap_or_else(|_| DEFAULT_APP_ENVIRONMENT.into())
+        .parse().map_err(|err: String| ConfigError::Message(err))?;
+
+    let app_environment_file_path = match environment {
+        Environment::Local => LOCAL_CONFIGURATION_FILE_PATH,
+        Environment::Production => PRODUCTION_CONFIGURATION_FILE_PATH,
+    };
 
     configuration.merge(File::from(configuration_directory.join(app_environment_file_path)).required(true))?;
 
