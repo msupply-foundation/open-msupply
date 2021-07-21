8760d34b672759a2583bb1c3c64e71e5d5e2f9a0
wlthomson <will.tho@outlook.com>
2021-07-21 13:16:32 +1200

    Add SettingsError enum

--- server/src/util/environment.rs
+++ server/src/util/environment.rs
@@ -1,4 +1,7 @@
-use std::str::FromStr;
+use std::{
+    fmt::{Debug, Display, Formatter, Result as FmtResult},
+    str::FromStr,
+};
 
 pub static LOCAL: &str = "local";
 pub static PRODUCTION: &str = "production";
@@ -7,6 +10,9 @@ pub enum Environment {
     Local,
     Production,
 }
+
+#[derive(Debug)]
+pub struct EnvironmentError(String);
 
 impl Environment {
     pub fn as_str(&self) -> &'static str {
@@ -17,8 +23,14 @@ impl Environment {
     }
 }
 
+impl Display for EnvironmentError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.0)
+    }
+}
+
 impl FromStr for Environment {
-    type Err = String;
+    type Err = EnvironmentError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s.eq_ignore_ascii_case(LOCAL) {
@@ -26,10 +38,10 @@ impl FromStr for Environment {
         } else if s.eq_ignore_ascii_case(PRODUCTION) {
             Ok(Self::Production)
         } else {
-            Err(format!(
+            Err(EnvironmentError(format!(
                 "{} is not a supported environment. Use either `{}` or `{}`.",
                 s, LOCAL, PRODUCTION
-            ))
+            )))
         }
     }
 }
--- server/src/util/settings.rs
+++ server/src/util/settings.rs
@@ -1,3 +1,8 @@
+use crate::util::environment::EnvironmentError;
+
+use config::ConfigError;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub server: ServerSettings,
@@ -38,5 +43,43 @@ impl DatabaseSettings {
             "postgres://{}:{}@{}:{}",
             self.username, self.password, self.host, self.port
         )
+    }
+}
+
+pub enum SettingsError {
+    Config(ConfigError),
+    Environment(EnvironmentError),
+    Path(String),
+}
+
+impl Debug for SettingsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            SettingsError::Config(err) => write!(f, "{:?}", err),
+            SettingsError::Environment(err) => write!(f, "{:?}", err),
+            SettingsError::Path(s) => write!(f, "{:?}", s),
+        }
+    }
+}
+
+impl Display for SettingsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            SettingsError::Config(err) => write!(f, "{}", err),
+            SettingsError::Environment(err) => write!(f, "{}", err),
+            SettingsError::Path(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl From<EnvironmentError> for SettingsError {
+    fn from(err: EnvironmentError) -> SettingsError {
+        SettingsError::Environment(err)
+    }
+}
+
+impl From<ConfigError> for SettingsError {
+    fn from(err: ConfigError) -> SettingsError {
+        SettingsError::Config(err)
     }
 }
