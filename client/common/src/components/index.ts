03f92ac21d25dd37677206efc7ce885c302ace32
Mark Prins <mark@malcolmprins.net>
2021-07-26 09:21:16 +1200

    organise imports

---
+++ client/common/src/components/index.ts
@@ -0,0 +1,10 @@
+import Typography from '@material-ui/core/Typography';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+export { ListItemIcon, Typography };
+export { LoadingApp } from './LoadingApp';
+export { DataGrid } from '@material-ui/data-grid';
+export { RemoteComponent } from './RemoteComponent';
+
+export * from './icons';
+export * from './layout';
