2312d6c40022e41a4713aa81d861bfcd95b93309
Joshua Griffin <griffinjoshua5@gmail.com>
2021-07-25 23:22:59 +1200

    Add test commit
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/01a3a1047fe88cc73eaa4dc2a763bcff6e8c7003

--- client/dashboard/src/DashboardService.tsx
+++ client/dashboard/src/DashboardService.tsx
@@ -8,6 +8,10 @@ import React from 'react';
 import Widget from './Widget';
 import { useServiceContext } from 'host/Service';
 
+// const RecentInvoices = React.lazy(() => import('invoices/RecentInvoicesWidget'));
+// const SalesDeposits = React.lazy(() => import('requisitions/DepositsWidget'));
+// const SalesToday = React.lazy(() => import('requisitions/TodayWidget'));
+
 const RecentInvoiceWidget = () => (
   <Widget height="500px">
     <Typography>RecentInvoices</Typography>
