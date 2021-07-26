aed6192e4379380cf6f58500a8a257846117acaa
wlthomson <will.tho@outlook.com>
2021-07-27 11:23:13 +1200

    Add stubs for missing mock data
    Mock data is very limited at the moment; the idea here is to clarify
    how mock datasets will be exposed to the main application. Mock data
    needs to be added to cover test cases for existing graphql queries and
    mutations as well as upcoming sync use-cases.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/68172994450eb3c7aac782067fbc6f321e81d55e

---
+++ server/src/database/mock/transact_line.rs
@@ -0,0 +1,5 @@
+use crate::database::schema::TransactLineRow;
+
+pub fn mock_transact_lines() -> Vec<TransactLineRow> {
+    vec![]
+}
---
+++ server/src/database/mock/user_account.rs
@@ -0,0 +1,5 @@
+use crate::database::schema::UserAccountRow;
+
+pub fn mock_user_accounts() -> Vec<UserAccountRow> {
+    vec![]
+}
