c8a87ead96e967c28fcbafff334b009faa68b8c6
wlthomson <will.tho@outlook.com>
2021-07-27 11:14:24 +1200

    Migrate mock repositories to DatabaseRow
    Migrates existing mock repository structs to handle `DatabaseRow`
    enums, e.g. `RequisitionRow` -> `DatabaseRow::Requisition(row)`.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/de5d101c7f18b2977ceacfe8fe47f118973ab15d

--- server/src/database/repository/mock/transact_line.rs
+++ server/src/database/repository/mock/transact_line.rs
@@ -1,32 +1,31 @@
 use crate::database::repository::RepositoryError;
-use crate::database::schema::TransactLineRow;
+use crate::database::schema::{DatabaseRow, TransactLineRow};
 
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 #[derive(Clone)]
 pub struct TransactLineRepository {
-    mock_data: Arc<Mutex<HashMap<String, TransactLineRow>>>,
+    mock_data: Arc<Mutex<HashMap<String, DatabaseRow>>>,
 }
 
 impl TransactLineRepository {
-    pub fn new(mock_data: Arc<Mutex<HashMap<String, TransactLineRow>>>) -> TransactLineRepository {
+    pub fn new(mock_data: Arc<Mutex<HashMap<String, DatabaseRow>>>) -> TransactLineRepository {
         TransactLineRepository { mock_data }
     }
 
     pub async fn insert_one(&self, transact_line: &TransactLineRow) -> Result<(), RepositoryError> {
         self.mock_data.lock().unwrap().insert(
-            String::from(transact_line.id.clone()),
-            transact_line.clone(),
+            transact_line.id.to_string(),
+            DatabaseRow::TransactLine(transact_line.clone()),
         );
-
         Ok(())
     }
 
     pub async fn find_one_by_id(&self, id: &str) -> Result<TransactLineRow, RepositoryError> {
-        match self.mock_data.lock().unwrap().get(&String::from(id)) {
-            Some(transact_line) => Ok(transact_line.clone()),
-            None => Err(RepositoryError {
+        match self.mock_data.lock().unwrap().get(&id.to_string()) {
+            Some(DatabaseRow::TransactLine(transact_line)) => Ok(transact_line.clone()),
+            _ => Err(RepositoryError {
                 msg: String::from(format!("Failed to find transact_line {}", id)),
             }),
         }
@@ -37,12 +36,18 @@ impl TransactLineRepository {
         transact_id: &str,
     ) -> Result<Vec<TransactLineRow>, RepositoryError> {
         let mut transact_lines = vec![];
-        for (_id, transact_line) in self.mock_data.lock().unwrap().clone().into_iter() {
-            if transact_line.transact_id == transact_id {
-                transact_lines.push(transact_line);
-            }
-        }
-
+        self.mock_data
+            .lock()
+            .unwrap()
+            .clone()
+            .into_iter()
+            .for_each(|(_id, row)| {
+                if let DatabaseRow::TransactLine(transact_line) = row {
+                    if transact_line.transact_id == transact_id {
+                        transact_lines.push(transact_line);
+                    }
+                }
+            });
         Ok(transact_lines)
     }
 }
