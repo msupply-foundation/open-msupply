904938bab8fd18a5c3285a35ead0bfafbbff10f6
Joshua Griffin <griffinjoshua5@gmail.com>
2021-07-26 22:29:13 +1200

    Add updated import/exports

--- client/common/src/components/layout/index.ts
+++ client/common/src/components/layout/index.ts
@@ -1,10 +1,27 @@
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 
-export { AppBar, Box, Divider, Drawer, List, ListItem, ListItemText, Toolbar };
+export {
+  AppBar,
+  Box,
+  Container,
+  Divider,
+  Drawer,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Toolbar,
+};
