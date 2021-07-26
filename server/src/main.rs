10dd4f28ce0d8793263b6f8ac758581209a6086b
wlthomson <will.tho@outlook.com>
2021-07-27 11:40:30 +1200

    Enable compiling server with mock feature
    Updates application conditional compilation to enable running server
    with `cargo run --features mock`. When running with `mock` feature,
    `pgsqlx` repositories are replaced by `mock` repositories, with mock
    data injected and with CRUD operations simulated using a shared
    `HashMap`.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/c9df775c6cfd2f7e4dab75e0b4bfb70651764888

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,40 @@
 #![allow(where_clauses_object_safety)]
-#![cfg_attr(feature = "mock", allow(unused_imports))]
 
 use remote_server::database;
 use remote_server::server;
 use remote_server::util;
+#[cfg(not(feature = "mock"))]
+use sqlx::PgPool;
+
+#[cfg(feature = "mock")]
+use {
+    remote_server::database::{
+        mock,
+        schema::{
+            DatabaseRow, ItemLineRow, ItemRow, NameRow, RequisitionLineRow, RequisitionRow,
+            StoreRow, TransactLineRow, TransactRow, UserAccountRow,
+        },
+    },
+    std::{collections::HashMap, sync::Mutex},
+};
+
+use remote_server::{
+    database::repository::{
+        CustomerInvoiceRepository, ItemLineRepository, ItemRepository, NameRepository,
+        RequisitionLineRepository, RequisitionRepository, StoreRepository, TransactLineRepository,
+        TransactRepository, UserAccountRepository,
+    },
+    server::{
+        data::{RepositoryMap, RepositoryRegistry},
+        middleware::{compress as compress_middleware, logger as logger_middleware},
+        service::{graphql::config as graphql_config, rest::config as rest_config},
+    },
+    util::{
+        configuration,
+        settings::Settings,
+        sync::{self, SyncReceiverActor, SyncSenderActor},
+    },
+};
 
 use log::info;
 use tokio::sync::mpsc::channel;
@@ -11,24 +42,125 @@ use tokio::sync::mpsc::error::TrySendError;
 use tokio::time::{delay_for, interval, Duration};
 
 use std::sync::Arc;
+#[cfg(not(feature = "mock"))]
+async fn get_repositories(settings: &Settings) -> RepositoryMap {
+    let pool: PgPool = PgPool::connect(&settings.database.connection_string())
+        .await
+        .expect("Failed to connect to database");
 
-#[cfg(feature = "mock")]
-fn main() -> Result<(), &'static str> {
-    Err("Compiled with the mock feature enabled, server not supported in this mode")
+    let mut repositories: RepositoryMap = RepositoryMap::new();
+
+    repositories.insert(CustomerInvoiceRepository::new(pool.clone()));
+    repositories.insert(ItemRepository::new(pool.clone()));
+    repositories.insert(ItemLineRepository::new(pool.clone()));
+    repositories.insert(NameRepository::new(pool.clone()));
+    repositories.insert(RequisitionRepository::new(pool.clone()));
+    repositories.insert(RequisitionLineRepository::new(pool.clone()));
+    repositories.insert(StoreRepository::new(pool.clone()));
+    repositories.insert(TransactRepository::new(pool.clone()));
+    repositories.insert(TransactLineRepository::new(pool.clone()));
+    repositories.insert(UserAccountRepository::new(pool.clone()));
+
+    repositories
 }
 
-#[cfg(not(feature = "mock"))]
+#[cfg(feature = "mock")]
+pub fn get_repositories() -> RepositoryMap {
+    let mut mock_data: HashMap<String, DatabaseRow> = HashMap::new();
+
+    let mock_names: Vec<NameRow> = mock::mock_names();
+    for name in mock_names {
+        mock_data.insert(name.id.to_string(), DatabaseRow::Name(name.clone()));
+    }
+
+    let mock_items: Vec<ItemRow> = mock::mock_items();
+    for item in mock_items {
+        mock_data.insert(item.id.to_string(), DatabaseRow::Item(item.clone()));
+    }
+
+    let mock_item_lines: Vec<ItemLineRow> = mock::mock_item_lines();
+    for item_line in mock_item_lines {
+        mock_data.insert(
+            item_line.id.to_string(),
+            DatabaseRow::ItemLine(item_line.clone()),
+        );
+    }
+
+    let mock_requisitions: Vec<RequisitionRow> = mock::mock_requisitions();
+    for requisition in mock_requisitions {
+        mock_data.insert(
+            requisition.id.to_string(),
+            DatabaseRow::Requisition(requisition.clone()),
+        );
+    }
+
+    let mock_requisition_lines: Vec<RequisitionLineRow> = mock::mock_requisition_lines();
+    for requisition_line in mock_requisition_lines {
+        mock_data.insert(
+            requisition_line.id.to_string(),
+            DatabaseRow::RequisitionLine(requisition_line.clone()),
+        );
+    }
+
+    let mock_stores: Vec<StoreRow> = mock::mock_stores();
+    for store in mock_stores {
+        mock_data.insert(store.id.to_string(), DatabaseRow::Store(store.clone()));
+    }
+
+    let mock_transacts: Vec<TransactRow> = mock::mock_transacts();
+    for transact in mock_transacts {
+        mock_data.insert(
+            transact.id.to_string(),
+            DatabaseRow::Transact(transact.clone()),
+        );
+    }
+
+    let mock_transact_lines: Vec<TransactLineRow> = mock::mock_transact_lines();
+    for transact_line in mock_transact_lines {
+        mock_data.insert(
+            transact_line.id.to_string(),
+            DatabaseRow::TransactLine(transact_line.clone()),
+        );
+    }
+
+    let mock_user_accounts: Vec<UserAccountRow> = mock::mock_user_accounts();
+    for user_account in mock_user_accounts {
+        mock_data.insert(
+            user_account.id.to_string(),
+            DatabaseRow::UserAccount(user_account.clone()),
+        );
+    }
+
+    let mut repositories: RepositoryMap = RepositoryMap::new();
+    let mock_data: Arc<Mutex<HashMap<String, DatabaseRow>>> = Arc::new(Mutex::new(mock_data));
+
+    repositories.insert(CustomerInvoiceRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(ItemRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(ItemLineRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(NameRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(RequisitionRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(RequisitionLineRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(StoreRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(TransactRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(TransactLineRepository::new(Arc::clone(&mock_data)));
+    repositories.insert(UserAccountRepository::new(Arc::clone(&mock_data)));
+
+    repositories
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let configuration =
-        util::configuration::get_configuration().expect("Failed to parse configuration settings");
+    let settings: Settings =
+        configuration::get_configuration().expect("Failed to parse configuration settings");
 
-    let pool: sqlx::PgPool = sqlx::PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to database");
+    #[cfg(not(feature = "mock"))]
+    let repositories: RepositoryMap = get_repositories(&settings).await;
+
+    #[cfg(feature = "mock")]
+    let repositories: RepositoryMap = get_repositories();
 
     let mut repositories = anymap::Map::new();
     repositories.insert(database::repository::CustomerInvoiceRepository::new(
