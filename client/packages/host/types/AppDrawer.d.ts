a357d10c8c695e5a9516ff60d6e3f96233b5d4a5
Mark Prins <mark@malcolmprins.net>
2021-08-02 11:47:19 +1200

    delete type defs
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/9bb20d0f351781c4de14039ccfaffd807e1f2033

--- client/packages/host/types/AppDrawer.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface AppDrawerProps {
-    drawer: any;
-    open?: boolean;
-}
-declare const AppDrawer: React.FC<AppDrawerProps>;
-export default AppDrawer;
-//# sourceMappingURL=AppDrawer.d.ts.map
