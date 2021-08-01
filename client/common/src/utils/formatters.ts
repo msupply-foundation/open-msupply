7aa3a881e53dcc5b19c7661ef7dfa968c7895f16
Mark Prins <mark@malcolmprins.net>
2021-08-02 09:44:07 +1200

    remove definitions
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/bfcd849674200d9e484dad627f9bc81f16624a87

--- client/common/src/utils/formatters.ts
+++ client/common/src/utils/formatters.ts
@@ -1,2 +1,2 @@
-export const formatDate = (date: Date) =>
+export const formatDate = (date: Date): string =>
   `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
--- client/common/types/common/src/store/ApiSlice.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const ApiSlice: import("@reduxjs/toolkit/query/react").Api<({ document, variables }: any) => {
-    data: Promise<any>;
-    error?: undefined;
-} | {
-    error: any;
-    data?: undefined;
-}, {}, "api", never, typeof import("@reduxjs/toolkit/dist/query/core/module").coreModuleName | typeof import("@reduxjs/toolkit/dist/query/react/module").reactHooksModuleName>;
-//# sourceMappingURL=ApiSlice.d.ts.map
