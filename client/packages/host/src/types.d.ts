7fbc4dfe3ab19f9874f05af746380623f9051fce
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-02 23:39:56 +1200

    Add TransactionService to host

--- client/packages/host/src/types.d.ts
+++ client/packages/host/src/types.d.ts
@@ -1,3 +1,7 @@
 declare module 'dashboard/DashboardService' {
   export default function (): JSX.Element;
 }
+
+declare module 'transactions/TransactionService' {
+  export default function (): JSX.Element;
+}
