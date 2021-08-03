78a47aa2d996b72625fa28d176abec501c5c3004
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-03 23:17:32 +1200

    Add Transactions component

--- client/packages/transactions/src/TransactionService.tsx
+++ client/packages/transactions/src/TransactionService.tsx
@@ -1,6 +1,14 @@
 import React, { FC } from 'react';
 import { request } from 'graphql-request';
-import { query, mutation, useDraftDocument } from './api';
+import { getQuery, mutation, useDraftDocument } from './api';
+import {
+  Routes,
+  Route,
+  useQuery,
+  DataGrid,
+  useNavigate,
+  useParams,
+} from '@openmsupply-client/common';
 
 interface Transaction {
   customer: string;
@@ -10,26 +18,25 @@ interface Transaction {
   date: string;
 }
 
-const queryFn = async (): Promise<Transaction> => {
-  const result = await request('http://localhost:4000', query);
+const queryFn = (id: string) => async (): Promise<Transaction> => {
+  const result = await request('http://localhost:4000', getQuery(), { id });
   const { transaction } = result;
-
   return transaction;
 };
 
 const mutationFn = async (updated: Transaction): Promise<Transaction> => {
   const patch = { transactionPatch: updated };
   const result = await request('http://localhost:4000', mutation, patch);
-
   const { updateTransaction } = result;
-
   return updateTransaction;
 };
 
-const TransactionService: FC = () => {
+const Transaction: FC = () => {
+  const { id } = useParams();
+
   const { draft, setDraft, save } = useDraftDocument<Transaction>(
-    'transaction',
-    queryFn,
+    ['transaction', id],
+    queryFn(id ?? ''),
     mutationFn
   );
 
@@ -51,6 +58,86 @@ const TransactionService: FC = () => {
       </div>
     </div>
   ) : null;
+};
+
+const listQuery = async () => {
+  const { transactions } = await request(
+    'http://localhost:4000',
+    `
+    query Query {
+    transactions {
+      id
+      date
+      customer
+      supplier
+      total
+    }
+}
+  `
+  );
+
+  return transactions;
+};
+
+const columns = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    flex: 1,
+  },
+  {
+    field: 'date',
+    headerName: 'Date',
+    flex: 1,
+  },
+  {
+    field: 'customer',
+    headerName: 'Customer',
+    flex: 1,
+  },
+  {
+    field: 'supplier',
+    headerName: 'Supplier',
+    flex: 1,
+  },
+  {
+    field: 'total',
+    headerName: 'Total',
+    flex: 1,
+  },
+];
+
+const Transactions = () => {
+  const { data, isLoading } = useQuery(['transaction', 'list'], listQuery, {
+    enabled: true,
+  });
+  const navigate = useNavigate();
+
+  return (
+    !isLoading && (
+      <div style={{ minWidth: '100%' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          hideFooterPagination
+          hideFooterRowCount
+          hideFooterSelectedRowCount
+          onRowClick={params => {
+            navigate(`/transactions/${params.id}`);
+          }}
+        />
+      </div>
+    )
+  );
+};
+
+const TransactionService: FC = () => {
+  return (
+    <Routes>
+      <Route path="*" element={<Transactions />} />
+      <Route path=":id" element={<Transaction />} />
+    </Routes>
+  );
 };
 
 export default TransactionService;
