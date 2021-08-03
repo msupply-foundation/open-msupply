1a6f7bfa0c9860ef521fc6f1ba390781a512b8ad
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-03 21:49:07 +1200

    Add shift of react-router-dom to shared package
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/8bac58d86e0f99500ee0fe0cb5e749ae59584c3e

--- client/packages/common/src/index.ts
+++ client/packages/common/src/index.ts
@@ -1,12 +1,16 @@
 import { request, gql } from 'graphql-request';
 import * as utils from './utils';
 
+import { Link as ReactRouterLink } from 'react-router-dom';
+
 export * from './components';
 export * from './hooks';
 export * from './store';
 export * from 'react-query';
 export * from 'react-query/devtools';
 export * from './styles';
+export * from 'react-router-dom';
 
 export { utils };
 export { request, gql };
+export { ReactRouterLink };
