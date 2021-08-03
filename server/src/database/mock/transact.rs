550ed2802e3854e8f2e9f1f96e868389bbeeb591
wlthomson <will.tho@outlook.com>
2021-08-03 16:40:25 +1200

    Add mock transact data
    Adds some (very) basic mock transact data. For now just consists of
    customer and supplier invoices; other transact types can be added as
    and when needed.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/e69eb0b4d6e2c8228a70d71f5b156214857e6c75

--- server/src/database/mock/transact.rs
+++ server/src/database/mock/transact.rs
@@ -1,5 +1,58 @@
-use crate::database::schema::TransactRow;
+use crate::database::schema::{TransactRow, TransactRowType};
+
+pub fn mock_customer_invoice_a() -> TransactRow {
+    TransactRow {
+        id: String::from("customer_invoice_a"),
+        name_id: String::from("name_store_a"),
+        store_id: String::from("store_b"),
+        invoice_number: 1,
+        type_of: TransactRowType::CustomerInvoice,
+    }
+}
+
+pub fn mock_customer_invoice_b() -> TransactRow {
+    TransactRow {
+        id: String::from("customer_invoice_b"),
+        name_id: String::from("name_store_a"),
+        store_id: String::from("store_c"),
+        invoice_number: 3,
+        type_of: TransactRowType::CustomerInvoice,
+    }
+}
+
+pub fn mock_supplier_invoice_a() -> TransactRow {
+    TransactRow {
+        id: String::from("supplier_invoice_a"),
+        name_id: String::from("name_store_b"),
+        store_id: String::from("store_a"),
+        invoice_number: 2,
+        type_of: TransactRowType::SupplierInvoice,
+    }
+}
+
+pub fn mock_supplier_invoice_b() -> TransactRow {
+    TransactRow {
+        id: String::from("supplier_invoice_b"),
+        name_id: String::from("name_store_c"),
+        store_id: String::from("store_a"),
+        invoice_number: 4,
+        type_of: TransactRowType::SupplierInvoice,
+    }
+}
+
+pub fn mock_customer_invoices() -> Vec<TransactRow> {
+    vec![mock_customer_invoice_a(), mock_customer_invoice_b()]
+}
+
+pub fn mock_supplier_invoices() -> Vec<TransactRow> {
+    vec![mock_supplier_invoice_a(), mock_supplier_invoice_b()]
+}
 
 pub fn mock_transacts() -> Vec<TransactRow> {
-    vec![]
+    let mut mock_transacts: Vec<TransactRow> = Vec::new();
+
+    mock_transacts.extend(mock_customer_invoices());
+    mock_transacts.extend(mock_supplier_invoices());
+
+    mock_transacts
 }
