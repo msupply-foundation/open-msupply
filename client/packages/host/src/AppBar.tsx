382c5d8b678f82a13baa00c72a16c42d332e7b77
Mark Prins <mark@malcolmprins.net>
2021-08-03 17:19:10 +1200

    app bar and toggle on msupply guy

--- client/packages/host/src/AppBar.tsx
+++ client/packages/host/src/AppBar.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  makeStyles,
-  IconButton,
-  MenuIcon,
-  Toolbar,
-  Typography,
-} from '@openmsupply-client/common';
+import { makeStyles, Toolbar, Typography } from '@openmsupply-client/common';
 import clsx from 'clsx';
 import { useServiceContext } from './Service';
 
@@ -14,6 +8,7 @@ const useStyles = makeStyles(theme => ({
     paddingRight: 24,
   },
   appBar: {
+    left: 72,
     position: 'absolute',
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
@@ -22,18 +17,12 @@ const useStyles = makeStyles(theme => ({
     }),
   },
   appBarShift: {
-    marginLeft: 200,
+    marginLeft: 128,
     width: `calc(100% - 200px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
   },
   title: {
     flexGrow: 1,
@@ -42,8 +31,6 @@ const useStyles = makeStyles(theme => ({
 
 interface Drawer {
   open: boolean | null;
-  openDrawer: () => void;
-  closeDrawer: () => void;
 }
 interface AppBarProps {
   drawer: Drawer;
@@ -58,18 +45,6 @@ const AppBar: React.FC<AppBarProps> = props => {
       className={clsx(classes.appBar, props.drawer.open && classes.appBarShift)}
     >
       <Toolbar className={classes.toolbar}>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={props.drawer.openDrawer}
-          className={clsx(
-            classes.menuButton,
-            props.drawer.open && classes.menuButtonHidden
-          )}
-        >
-          <MenuIcon />
-        </IconButton>
         <Typography
           component="h1"
           variant="h6"
