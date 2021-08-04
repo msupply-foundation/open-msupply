7d4e996ff1a748bc76ce0bf7252fbacccd996ed8
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-04 12:11:19 +1200

    Remove unused import
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/d993add8595d728046c2bc50872a1dedb655d211

--- client/packages/transactions/src/TransactionService.tsx
+++ client/packages/transactions/src/TransactionService.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { request } from 'graphql-request';
 import { getQuery, mutation, useDraftDocument } from './api';
 import {
