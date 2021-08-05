5ff42eeb4be7d29fcb3169b938729d65321f4101
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-06 09:03:12 +1200

    Add NavLink component

--- client/packages/common/src/ui/components/NavLink.tsx
+++ client/packages/common/src/ui/components/NavLink.tsx
@@ -1,25 +1,13 @@
 import React, { FC } from 'react';
-import { useMatch, Link } from 'react-router-dom';
+
 import clsx from 'clsx';
 import { makeStyles, ListItem, ListItemText } from '@material-ui/core';
-import { Theme } from '../../styles/theme';
+import { useMatch, Link } from 'react-router-dom';
+import { AppTheme } from '../../styles/theme';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  toolbarIcon: {
-    display: 'flex',
-    height: 90,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  drawerMenu: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    justifyContent: 'space-between',
-    paddingLeft: 15,
-  },
+const ReactRouterLink = Link;
+
+const useStyles = makeStyles((theme: AppTheme) => ({
   drawerMenuItem: {
     height: 32,
     margin: '20px 0',
@@ -33,36 +21,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     backgroundColor: `${theme.palette.background?.white}!important`,
     boxShadow: theme.shadows[4],
   },
-  drawerPaper: {
-    backgroundColor: theme.palette.background?.drawer,
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: 200,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    borderRadius: 8,
-    boxShadow: theme.shadows[7],
-    '& li > a': { borderRadius: 16, padding: '4px 8px', width: 168 },
-    '& li > a > div': { marginLeft: 8 },
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-    '& li > a': { borderRadius: 20, height: 40, padding: 10, width: 40 },
-    '& li > a > div': { display: 'none' },
-    '& ul > hr': { display: 'none' },
-  },
-  mSupplyGuy: { height: 60, width: 45 },
-  mSupplyGuySmall: { height: 40, width: 30 },
 }));
 
 interface ListItemLinkProps {
@@ -73,19 +31,19 @@ interface ListItemLinkProps {
 }
 
 export const AppNavLink: FC<ListItemLinkProps> = props => {
-  useStyles();
+  const classes = useStyles();
   const selected = useMatch({ path: props.to + '/*' });
 
   const CustomLink = React.useMemo(
     () =>
       React.forwardRef<HTMLAnchorElement>((linkProps, ref) => (
-        <Link ref={ref} to={props.to} {...linkProps} />
+        <ReactRouterLink ref={ref} to={props.to} {...linkProps} />
       )),
     [props.to]
   );
   const className = clsx(
-    props.classes['drawerMenuItem'],
-    !!selected && props.classes['drawerMenuItemSelected']
+    classes['drawerMenuItem'],
+    !!selected && classes['drawerMenuItemSelected']
   );
 
   return (
