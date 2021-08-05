08449aa51db0900f5ecf5bc27484ff9c917d3336
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-05 22:50:18 +1200

    Add updated common/index exports

--- client/packages/common/src/index.ts
+++ client/packages/common/src/index.ts
@@ -1,16 +1,9 @@
-import { request, gql } from 'graphql-request';
-import * as utils from './utils';
+export * from 'graphql-request';
+export * from 'react-query';
+export * from 'react-query/devtools';
 
-import { Link as ReactRouterLink } from 'react-router-dom';
-
+export * from './utils';
 export * from './ui';
 export * from './hooks';
 export * from './store';
-export * from 'react-query';
-export * from 'react-query/devtools';
 export * from './styles';
-export * from 'react-router-dom';
-
-export { utils };
-export { request, gql };
-export { ReactRouterLink };
