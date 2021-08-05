a0549cd005712e16d11fa036ae85c465a61c69c0
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-05 18:38:42 +1200

    Add shift loose components into ui/components
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/f4f0353cd5550aa629dcd67d3d5dc6106f11ff74

--- client/packages/common/src/ui/components/LoadingApp.tsx
+++ client/packages/common/src/ui/components/LoadingApp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { CircularProgress, Typography } from './index';
-import { makeStyles } from '../styles';
+import { CircularProgress, Typography } from '../index';
+import { makeStyles } from '../../styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
