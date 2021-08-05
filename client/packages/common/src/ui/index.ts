dc423d30eebe44f2ca7f2f0b7085c20be0d9054b
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-05 21:23:30 +1200

    Add adjustment to LoadingApp and ui imports, add NavLink component

--- client/packages/common/src/ui/index.ts
+++ client/packages/common/src/ui/index.ts
@@ -2,10 +2,11 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import ListItemText from '@material-ui/core/ListItemText';
+import clsx from 'clsx';
 
-export { LoadingApp } from './components/LoadingApp';
 export { DataGrid } from '@material-ui/data-grid';
-export { RemoteComponent } from './components/RemoteComponent';
+
+export * from './components';
 export * from './icons';
 export * from './layout';
-export { CircularProgress, Divider, ListItemText, Typography };
+export { clsx, CircularProgress, Divider, ListItemText, Typography };
