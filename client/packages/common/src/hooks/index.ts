676c0d8346c5c5889a842b98b8f5f1c44d924dcf
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-05 14:37:11 +1200

    Revert "Add hooking up of a Nav component from Customers -> Host/AppDrawer"
    This reverts commit 3f407eadba73a404165cfbedfbf3846413181251.
    
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/882376a9ad67deafbc82572460f2b5620b491d9b

--- client/packages/common/src/hooks/index.ts
+++ client/packages/common/src/hooks/index.ts
@@ -1,4 +1,2 @@
 export { useRemoteFn } from './useRemoteFn';
 export { useRemoteScript } from './useRemoteScript';
-export { useDrawer } from './useDrawer';
-export { useLocalStorageSync } from './useLocalStorageSync';
