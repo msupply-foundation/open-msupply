009e5e31bb41844727487c98f0244eac278e3fc2
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-06 09:15:08 +1200

    Remove commented out
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/e6181bbb7c2a80d692b559faa6db65f5c0f1a20e

--- client/packages/common/src/styles/index.ts
+++ client/packages/common/src/styles/index.ts
@@ -1,5 +1,3 @@
-// import { makeStyles } from '@material-ui/core';
-// import { CssBaseline } from '@material-ui/core';
 import AppThemeProvider from './ThemeProvider';
 
 export { AppThemeProvider };
