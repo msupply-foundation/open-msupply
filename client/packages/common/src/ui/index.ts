a93aba79b2707bca45905feddce883121745012f
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-05 18:38:42 +1200

    Add shift loose components into ui/components

--- client/packages/common/src/ui/index.ts
+++ client/packages/common/src/ui/index.ts
@@ -3,9 +3,9 @@ import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import ListItemText from '@material-ui/core/ListItemText';
 
-export { LoadingApp } from './LoadingApp';
+export { LoadingApp } from './components/LoadingApp';
 export { DataGrid } from '@material-ui/data-grid';
-export { RemoteComponent } from './RemoteComponent';
+export { RemoteComponent } from './components/RemoteComponent';
 export * from './icons';
 export * from './layout';
 export { CircularProgress, Divider, ListItemText, Typography };
