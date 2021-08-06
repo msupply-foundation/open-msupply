bfe64f55f365495fc8d3c18057c2dd528ed1cb28
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-06 12:11:48 +1200

    Remove full import/export of all of material ui replacing with explicit imports
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/843fe1456d96a769456831314af11def6be4c6f5

--- client/packages/common/src/styles/index.ts
+++ client/packages/common/src/styles/index.ts
@@ -1,3 +1,5 @@
 import AppThemeProvider from './ThemeProvider';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-export { AppThemeProvider };
+export { AppThemeProvider, makeStyles, CssBaseline };
--- client/packages/common/src/ui/components/index.ts
+++ client/packages/common/src/ui/components/index.ts
@@ -1,3 +1,8 @@
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 export * from './LoadingApp';
 export * from './RemoteComponent';
 export { AppNavLink } from './NavLink';
+export { Typography, CircularProgress, Divider };
