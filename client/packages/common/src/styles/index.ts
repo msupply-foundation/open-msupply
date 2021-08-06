596d4ef63696a3db2e7643ead92fbe75e928b985
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-06 13:32:44 +1200

    Add type augmentation for theme

--- client/packages/common/src/styles/index.ts
+++ client/packages/common/src/styles/index.ts
@@ -2,7 +2,4 @@ import AppThemeProvider from './ThemeProvider';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import { ApplicationTheme } from './theme';
-type AppTheme = ApplicationTheme;
-
-export { AppThemeProvider, makeStyles, CssBaseline, AppTheme };
+export { AppThemeProvider, makeStyles, CssBaseline };
