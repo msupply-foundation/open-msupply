eb9f365f92786591b11fa1476ddb23a809db2afd
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-17 20:14:21 +1200

    Add TestingProvider component

--- client/packages/common/src/intl/IntlTestProvider.tsx
+++ client/packages/common/src/intl/IntlTestProvider.tsx
@@ -1,10 +1,26 @@
 import React, { FC } from 'react';
 import { IntlProvider as ReactIntlProvider } from 'react-intl';
+import { LocaleMessages, SupportedLocales } from './intlHelpers';
 import en from './locales/en.json';
+import fr from './locales/fr.json';
+import pt from './locales/pt.json';
 
-export const IntlTestProvider: FC = ({ children }) => {
+const locales: Record<SupportedLocales, LocaleMessages> = {
+  en,
+  fr,
+  pt,
+};
+
+interface IntlTestProviderProps {
+  locale: SupportedLocales;
+}
+
+export const IntlTestProvider: FC<IntlTestProviderProps> = ({
+  children,
+  locale,
+}) => {
   return (
-    <ReactIntlProvider locale="en" messages={en}>
+    <ReactIntlProvider locale="en" messages={locales[locale]}>
       {children}
     </ReactIntlProvider>
   );
