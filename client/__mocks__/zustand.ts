afb88c0b9c11770a51b3344ec8b4d1043cad6d65
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-17 12:19:41 +1200

    Add zustand mock and jest setup for local storage

---
+++ client/__mocks__/zustand.ts
@@ -0,0 +1,24 @@
+import actualCreate from 'zustand';
+import { act } from 'react-dom/test-utils';
+
+/**
+ * This mock adds functionality to standard zustand by resetting to the
+ * initial state after each test.
+ */
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const stores = new Set<Function>();
+
+const create: typeof actualCreate = createState => {
+  const store = actualCreate(createState);
+  const initialState = store.getState();
+  stores.add(() => act(() => store.setState(initialState, true)));
+
+  return store;
+};
+
+afterEach(() => {
+  stores.forEach(resetFn => resetFn());
+});
+
+export default create;
--- client/jest-setup.ts
+++ client/jest-setup.ts
@@ -1,1 +1,21 @@
 import '@testing-library/jest-dom';
+
+let mockStorage: Record<string, string> = {};
+
+beforeAll(() => {
+  global.Storage.prototype.setItem = jest.fn((key, value) => {
+    mockStorage[key] = value;
+  });
+  global.Storage.prototype.getItem = jest.fn(key => {
+    return mockStorage[key] ?? null;
+  });
+});
+
+afterAll(() => {
+  (global.Storage.prototype.setItem as jest.Mock).mockReset();
+  (global.Storage.prototype.getItem as jest.Mock).mockReset();
+});
+
+beforeEach(() => {
+  mockStorage = {};
+});
