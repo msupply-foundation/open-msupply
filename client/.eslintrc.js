2052c2ab1336f3b086bc9bbfcbdf4b257701c05b
Joshua Griffin <griffinjoshua5@gmail.com>
2021-08-17 13:21:36 +1200

    Add eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'plugin:jest-dom/recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'google',
