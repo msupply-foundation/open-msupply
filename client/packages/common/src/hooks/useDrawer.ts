092e22a733ed9fa6e1937f1dba72d6d051a2be33
Mark Prins <mark@malcolmprins.net>
2021-08-19 12:11:54 +1200

    retain drawer state

--- client/packages/common/src/hooks/useDrawer.ts
+++ client/packages/common/src/hooks/useDrawer.ts
@@ -1,17 +1,23 @@
 import create from 'zustand';
 
+const localStorageKey = '@openmsupply-client/appdrawer/open';
+
 type DrawerController = {
   isOpen: boolean;
+  isSet: boolean;
   open: () => void;
   close: () => void;
   toggle: () => void;
 };
 
-export const useDrawer = create<DrawerController>(set => {
-  return {
-    isOpen: true,
-    open: () => set(state => ({ ...state, isOpen: true })),
-    close: () => set(state => ({ ...state, isOpen: false })),
-    toggle: () => set(state => ({ ...state, isOpen: !state.isOpen })),
-  };
+export const useDrawer = create<DrawerController>(set => ({
+  isOpen: !!JSON.parse(localStorage.getItem(localStorageKey) ?? 'false'),
+  isSet: localStorage.getItem(localStorageKey) !== null,
+  open: () => set(state => ({ ...state, isOpen: true })),
+  close: () => set(state => ({ ...state, isOpen: false })),
+  toggle: () => set(state => ({ ...state, isOpen: !state.isOpen })),
+}));
+
+useDrawer.subscribe(({ isOpen }) => {
+  localStorage.setItem(localStorageKey, JSON.stringify(isOpen));
 });
