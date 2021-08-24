63b4820ea93e6cd4a0fb37bbb21eeee4e30bacc1
wlthomson <will.tho@outlook.com>
2021-08-24 22:22:33 +1200

    Remove get_loaders helpers from main
    Refactors `get_loaders` and associated conditional compilation logic
    from `main.rs` into `loaders/mock.rs` and `loaders/pgsqlx.rs` (see
    50bcf38e371f7cff451bf14460d9cc5a6d9ceca6 for background).

--- server/src/database/repository/mod.rs
+++ server/src/database/repository/mod.rs
@@ -1,14 +1,14 @@
+#[cfg_attr(feature = "mock", path = "mock/mod.rs")]
+#[cfg_attr(not(feature = "mock"), path = "pgsqlx/mod.rs")]
+mod repository;
+
+pub use repository::{
+    get_repositories, CustomerInvoiceRepository, ItemLineRepository, ItemRepository,
+    NameRepository, RequisitionLineRepository, RequisitionRepository, StoreRepository,
+    TransactLineRepository, TransactRepository, UserAccountRepository,
+};
+
 #[derive(Clone, Debug)]
 pub struct RepositoryError {
     msg: String,
 }
-
-#[cfg_attr(feature = "mock", path = "mock/mod.rs")]
-#[cfg_attr(not(feature = "mock"), path = "pgsqlx/mod.rs")]
-pub mod repository;
-
-pub use repository::{
-    CustomerInvoiceRepository, ItemLineRepository, ItemRepository, NameRepository,
-    RequisitionLineRepository, RequisitionRepository, StoreRepository, TransactLineRepository,
-    TransactRepository, UserAccountRepository,
-};
