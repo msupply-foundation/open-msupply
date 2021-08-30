d814c42cb6f27685666b63b2add4c823abc9ea80
Mark Prins <mark@malcolmprins.net>
2021-08-30 15:45:44 +1200

    stories
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/e8265c5f731e0b49e0a08788b6d717b0903cdc50

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,18 @@
+import React from 'react';
+
+import AppThemeProvider from '../packages/common/src/styles/ThemeProvider';
+import { IntlProvider } from '../packages/common/src/intl/IntlProvider';
+
+export const decorators = [
+  Story => (
+    <IntlProvider locale="en">
+      <AppThemeProvider>
+        <Story />
+      </AppThemeProvider>
+    </IntlProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
