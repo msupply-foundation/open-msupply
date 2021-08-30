d1be93ef89b8c2233de038e9a406f4340fbba6eb
Mark Prins <mark@malcolmprins.net>
2021-08-30 15:45:44 +1200

    stories

---
+++ client/packages/common/src/ui/components/buttons/TextButton.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TextButton } from '.';
+
+const Template: ComponentStory<typeof TextButton> = () => (
+  <TextButton labelKey="button.docs" onClick={() => console.info('clicked')} />
+);
+
+export const Primary = Template.bind({});
+
+export default {
+  title: 'Buttons/TextButton',
+  component: TextButton,
+} as ComponentMeta<typeof TextButton>;
