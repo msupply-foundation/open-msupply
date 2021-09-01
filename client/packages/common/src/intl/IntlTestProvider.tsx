261b0638e7a13ba8c24f8b51df6bcf969a859a62
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-02 10:49:34 +1200

    Add storybook upgrades

--- client/packages/common/src/intl/IntlTestProvider.tsx
+++ client/packages/common/src/intl/IntlTestProvider.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { IntlProvider as ReactIntlProvider } from 'react-intl';
 import { LocaleMessages, SupportedLocales } from './intlHelpers';
 import en from './locales/en.json';
 import fr from './locales/fr.json';
 import pt from './locales/pt.json';
 import ar from './locales/ar.json';
+import { useHostContext } from '../hooks';
 
 const locales: Record<SupportedLocales, LocaleMessages> = {
   en,
@@ -21,6 +22,12 @@ export const IntlTestProvider: FC<IntlTestProviderProps> = ({
   children,
   locale,
 }) => {
+  const { locale: currentLocale, setLocale } = useHostContext();
+
+  useEffect(() => {
+    if (currentLocale !== locale) setLocale(locale);
+  }, [locale]);
+
   return (
     <ReactIntlProvider locale="en" messages={locales[locale]}>
       {children}
