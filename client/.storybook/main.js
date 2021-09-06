e0342ab0896b2ffe16c5a0d05150ca261141bd63
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-07 09:02:15 +1200

    Add updated resolutions for same emotion version

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,23 @@
+const path = require('path');
+const toPath = filePath => path.join(process.cwd(), filePath);
+
 module.exports = {
   stories: [
     '../packages/**/*.stories.mdx',
     '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+        },
+      },
+    };
+  },
 };
