892040834e59d23bf28593daaf24c03cfdaa4277
Mark Prins <mark@malcolmprins.net>
2021-09-08 13:03:45 +1200

    use icons for checkbox

--- client/packages/common/src/ui/icons/CheckboxEmpty.tsx
+++ client/packages/common/src/ui/icons/CheckboxEmpty.tsx
@@ -4,7 +4,7 @@ import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon';
 export const CheckboxEmpty = (props: SvgIconProps): JSX.Element => {
   const combinedProps: SvgIconProps = { color: 'primary', ...props };
   return (
-    <SvgIcon {...combinedProps} viewBox="0 0 24 30">
+    <SvgIcon {...combinedProps} viewBox="3 3 20 20">
       <path d="M17,5a2.00228,2.00228,0,0,1,2,2V17a2.00228,2.00228,0,0,1-2,2H7a2.00228,2.00228,0,0,1-2-2V7A2.00228,2.00228,0,0,1,7,5H17m0-1H7A3,3,0,0,0,4,7V17a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V7a3,3,0,0,0-3-3Z" />
     </SvgIcon>
   );
