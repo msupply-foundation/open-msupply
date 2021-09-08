9d45e9fed2be0f31e13c1a935bb886ebd7eafc02
Clemens Zeidler <clemens@msupply.foundation>
2021-09-08 20:10:55 +1200

    Remove needless to_string()
    Source: https://github.com/msupply-foundation/remote-server/commit/32fead1ca48ee3b7f3341f65f0a0b0a671fa0a55

--- server/migrations/sqlite/00000104_create_item_table/up.sql
+++ server/migrations/sqlite/00000104_create_item_table/up.sql
@@ -3,5 +3,5 @@
 CREATE TABLE item (
     id TEXT NOT NULL PRIMARY KEY,
     item_name TEXT NOT NULL,
-    type_of TEXT CHECK(type_of IN ('general', 'service', 'cross_reference')) NOT NULL
+    type_of TEXT CHECK(type_of IN ('general', 'service', 'cross_reference', 'none_stock')) NOT NULL
 )
