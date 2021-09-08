8f4fe76e58e94c6c93e380957fbf499cdcbc4637
Mark Prins <mark@malcolmprins.net>
2021-09-08 13:03:45 +1200

    use icons for checkbox
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/a52662cce08184b095dd7b49ab97d48798029a29

--- client/packages/common/src/ui/icons/CheckboxDisabled.tsx
+++ client/packages/common/src/ui/icons/CheckboxDisabled.tsx
@@ -4,7 +4,7 @@ import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon';
 export const CheckboxDisabled = (props: SvgIconProps): JSX.Element => {
   const combinedProps: SvgIconProps = { color: 'primary', ...props };
   return (
-    <SvgIcon {...combinedProps} viewBox="0 0 24 30">
+    <SvgIcon {...combinedProps} viewBox="3 3 20 20">
       <path d="M17,4H7A3,3,0,0,0,4,7V17a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V7A3,3,0,0,0,17,4Zm-1,9H8a1,1,0,0,1,0-2h8a1,1,0,0,1,0,2Z" />
     </SvgIcon>
   );
