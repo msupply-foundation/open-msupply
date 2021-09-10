69e055fc74511cfd7269676079dc1ea19d664cc4
andreievg <andrei.evguenov@gmail.com>
2021-09-11 01:55:01 +1200

    dry out logic of test (while keeping test readable)
    Source: https://github.com/msupply-foundation/remote-server/commit/d5bc16b85fbfce905206e1c554f6ce02a4396bab

--- server/src/util/sync/translation/mod.rs
+++ server/src/util/sync/translation/mod.rs
@@ -108,6 +108,62 @@ mod tests {
         TestSyncRecord,
     };
 
+    #[actix_rt::test]
+    async fn test_store_translation_insert() {
+        let settings = test_db::get_test_settings("omsupply-database-translation-insert");
+
+        test_db::setup(&settings.database).await;
+        let registry = RepositoryRegistry {
+            repositories: get_repositories(&settings).await,
+        };
+
+        let mut records = Vec::new();
+        // Need to be in order of reference dependency
+        records.append(&mut get_test_name_records());
+        records.append(&mut get_test_store_records());
+        records.append(&mut get_test_item_records());
+
+        import_sync_records(&registry, &extract_sync_records(records))
+            .await
+            .unwrap();
+
+        let mut records = Vec::new();
+        records.append(&mut get_test_name_records());
+        records.append(&mut get_test_store_records());
+        records.append(&mut get_test_item_records());
+        // Asserts inside this method, to avoid repetition
+        check_records_against_database(&registry, records).await;
+    }
+
+    #[actix_rt::test]
+    async fn test_store_translation_upsert() {
+        let settings = test_db::get_test_settings("omsupply-database-translation-upsert");
+
+        test_db::setup(&settings.database).await;
+        let registry = RepositoryRegistry {
+            repositories: get_repositories(&settings).await,
+        };
+
+        let mut records = Vec::new();
+        records.append(&mut get_test_name_records());
+        records.append(&mut get_test_item_records());
+        records.append(&mut get_test_item_upsert_records());
+        records.append(&mut get_test_name_upsert_records());
+
+        import_sync_records(&registry, &extract_sync_records(records))
+            .await
+            .unwrap();
+
+        let mut records = Vec::new();
+        records.append(&mut get_test_item_upsert_records());
+        records.append(&mut get_test_name_upsert_records());
+
+        // Asserts inside this method, to avoid repetition
+        check_records_against_database(&registry, records).await;
+    }
+
+    // DB query will return NotFound error for record that's not found
+    // while test data has None for records that shouldn't be integrated
     fn from_option_to_db_result<T>(option: Option<T>) -> Result<T, RepositoryError> {
         match option {
             Some(record) => Ok(record),
@@ -122,32 +178,10 @@ mod tests {
             .collect()
     }
 
-    #[actix_rt::test]
-    async fn test_store_translation_insert() {
-        let settings = test_db::get_test_settings("omsupply-database-translation-insert");
-
-        test_db::setup(&settings.database).await;
-        let registry = RepositoryRegistry {
-            repositories: get_repositories(&settings).await,
-        };
-
-        import_sync_records(&registry, &extract_sync_records(get_test_name_records()))
-            .await
-            .unwrap();
-
-        import_sync_records(&registry, &extract_sync_records(get_test_store_records()))
-            .await
-            .unwrap();
-
-        import_sync_records(&registry, &extract_sync_records(get_test_item_records()))
-            .await
-            .unwrap();
-
-        let mut records = Vec::new();
-        records.append(&mut get_test_store_records());
-        records.append(&mut get_test_name_records());
-        records.append(&mut get_test_item_records());
-
+    async fn check_records_against_database(
+        registry: &RepositoryRegistry,
+        records: Vec<TestSyncRecord>,
+    ) {
         for record in records {
             match record.translated_record {
                 TestSyncDataRecord::Store(comparison_record) => {
@@ -177,66 +211,6 @@ mod tests {
                         from_option_to_db_result(comparison_record)
                     )
                 }
-            }
-        }
-    }
-
-    #[actix_rt::test]
-    async fn test_store_translation_upsert() {
-        let settings = test_db::get_test_settings("omsupply-database-translation-upsert");
-
-        test_db::setup(&settings.database).await;
-        let registry = RepositoryRegistry {
-            repositories: get_repositories(&settings).await,
-        };
-
-        import_sync_records(&registry, &extract_sync_records(get_test_name_records()))
-            .await
-            .unwrap();
-
-        import_sync_records(&registry, &extract_sync_records(get_test_item_records()))
-            .await
-            .unwrap();
-
-        import_sync_records(
-            &registry,
-            &extract_sync_records(get_test_name_upsert_records()),
-        )
-        .await
-        .unwrap();
-
-        import_sync_records(
-            &registry,
-            &extract_sync_records(get_test_item_upsert_records()),
-        )
-        .await
-        .unwrap();
-
-        let mut records = Vec::new();
-        records.append(&mut get_test_item_upsert_records());
-        records.append(&mut get_test_name_upsert_records());
-
-        for record in records {
-            match record.translated_record {
-                TestSyncDataRecord::Name(comparison_record) => {
-                    assert_eq!(
-                        registry
-                            .get::<NameRepository>()
-                            .find_one_by_id(&record.sync_record.record_id)
-                            .await,
-                        from_option_to_db_result(comparison_record)
-                    )
-                }
-                TestSyncDataRecord::Item(comparison_record) => {
-                    assert_eq!(
-                        registry
-                            .get::<ItemRepository>()
-                            .find_one_by_id(&record.sync_record.record_id)
-                            .await,
-                        from_option_to_db_result(comparison_record)
-                    )
-                }
-                _ => panic!("not implemented"),
             }
         }
     }
