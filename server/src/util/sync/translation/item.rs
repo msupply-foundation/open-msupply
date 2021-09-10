33e502591981977e61a75c7f10b1add648b5e7f4
andreievg <andrei.evguenov@gmail.com>
2021-09-11 01:23:54 +1200

    refactor tests into unit tests and a semi integration test

--- server/src/util/sync/translation/item.rs
+++ server/src/util/sync/translation/item.rs
@@ -100,52 +100,25 @@ impl LegacyItemRow {
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        database::repository::{repository::get_repositories, ItemRepository},
-        server::data::RepositoryRegistry,
-        util::{
-            sync::translation::{import_sync_records, SyncRecord, SyncType},
-            test_db,
-        },
+    use crate::util::sync::translation::{
+        item::LegacyItemRow,
+        test_data::{item::get_test_item_records, TestSyncDataRecord},
     };
 
-    #[actix_rt::test]
-    async fn test_item_translation() {
-        let settings = test_db::get_test_settings("omsupply-database-item-translation");
-        test_db::setup(&settings.database).await;
-        let repositories = get_repositories(&settings).await;
-        let registry = RepositoryRegistry { repositories };
-        let item_repo = registry.get::<ItemRepository>();
-
-        let record = SyncRecord {
-            sync_type: SyncType::Insert,
-            record_type: "item".to_string(),
-            data: r#"
-            {"ID":"8F252B5884B74888AAB73A0D42C09E7F","item_name":"Non stock items","start_of_year_date":"0000-00-00","manufacture_method":"","default_pack_size":1,"dose_picture":"[object Picture]","atc_category":"","medication_purpose":"","instructions":"","user_field_7":false,"flags":"","ddd_value":"","code":"NSI","other_names":"","type_of":"non_stock","price_editable":false,"margin":0,"barcode_spare":"","spare_ignore_for_orders":false,"sms_pack_size":0,"expiry_date_mandatory":false,"volume_per_pack":0,"department_ID":"","weight":0,"essential_drug_list":false,"catalogue_code":"","indic_price":0,"user_field_1":"","spare_hold_for_issue":false,"builds_only":false,"reference_bom_quantity":0,"use_bill_of_materials":false,"description":"","spare_hold_for_receive":false,"Message":"","interaction_group_ID":"","spare_pack_to_one_on_receive":false,"cross_ref_item_ID":"","strength":"","user_field_4":false,"user_field_6":"","spare_internal_analysis":0,"user_field_2":"","user_field_3":"","ddd factor":0,"account_stock_ID":"52923505A91447B9923BA34A4F332014","account_purchases_ID":"330ACC81721C4126BD5DD6769466C5C4","account_income_ID":"EF34ADD07C014AB8914E30CA2E3FEA8D","unit_ID":"","outer_pack_size":0,"category_ID":"","ABC_category":"","warning_quantity":0,"user_field_5":0,"print_units_in_dis_labels":false,"volume_per_outer_pack":0,"normal_stock":false,"critical_stock":false,"spare_non_stock":false,"non_stock_name_ID":"","is_sync":false,"sms_code":"","category2_ID":"","category3_ID":"","buy_price":0,"VEN_category":"","universalcodes_code":"","universalcodes_name":"","kit_data":null,"custom_data":null,"doses":0,"is_vaccine":false,"restricted_location_type_ID":""}
-            "#.to_string(),
-        };
-        let records = vec![record];
-        import_sync_records(&registry, &records).await.unwrap();
-        let entry = item_repo
-            .find_one_by_id("8F252B5884B74888AAB73A0D42C09E7F")
-            .await
-            .unwrap();
-        assert_eq!(entry.name, "Non stock items");
-
-        // should be able to upsert
-        let record = SyncRecord {
-            sync_type: SyncType::Insert,
-            record_type: "item".to_string(),
-            data: r#"
-            {"ID":"8F252B5884B74888AAB73A0D42C09E7F","item_name":"Non stock items 2","start_of_year_date":"0000-00-00","manufacture_method":"","default_pack_size":1,"dose_picture":"[object Picture]","atc_category":"","medication_purpose":"","instructions":"","user_field_7":false,"flags":"","ddd_value":"","code":"NSI","other_names":"","type_of":"general","price_editable":false,"margin":0,"barcode_spare":"","spare_ignore_for_orders":false,"sms_pack_size":0,"expiry_date_mandatory":false,"volume_per_pack":0,"department_ID":"","weight":0,"essential_drug_list":false,"catalogue_code":"","indic_price":0,"user_field_1":"","spare_hold_for_issue":false,"builds_only":false,"reference_bom_quantity":0,"use_bill_of_materials":false,"description":"","spare_hold_for_receive":false,"Message":"","interaction_group_ID":"","spare_pack_to_one_on_receive":false,"cross_ref_item_ID":"","strength":"","user_field_4":false,"user_field_6":"","spare_internal_analysis":0,"user_field_2":"","user_field_3":"","ddd factor":0,"account_stock_ID":"52923505A91447B9923BA34A4F332014","account_purchases_ID":"330ACC81721C4126BD5DD6769466C5C4","account_income_ID":"EF34ADD07C014AB8914E30CA2E3FEA8D","unit_ID":"","outer_pack_size":0,"category_ID":"","ABC_category":"","warning_quantity":0,"user_field_5":0,"print_units_in_dis_labels":false,"volume_per_outer_pack":0,"normal_stock":false,"critical_stock":false,"spare_non_stock":false,"non_stock_name_ID":"","is_sync":false,"sms_code":"","category2_ID":"","category3_ID":"","buy_price":0,"VEN_category":"","universalcodes_code":"","universalcodes_name":"","kit_data":null,"custom_data":null,"doses":0,"is_vaccine":false,"restricted_location_type_ID":""}
-            "#.to_string(),
-        };
-        let records = vec![record];
-        import_sync_records(&registry, &records).await.unwrap();
-        let entry = item_repo
-            .find_one_by_id("8F252B5884B74888AAB73A0D42C09E7F")
-            .await
-            .unwrap();
-        assert_eq!(entry.name, "Non stock items 2");
+    #[test]
+    fn test_item_translation() {
+        for record in get_test_item_records() {
+            match record.translated_record {
+                TestSyncDataRecord::Item(translated_record) => {
+                    assert_eq!(
+                        LegacyItemRow::try_translate(&record.sync_record).unwrap(),
+                        translated_record,
+                        "{}",
+                        record.identifier
+                    )
+                }
+                _ => panic!("Testing wrong record type {:#?}", record.translated_record),
+            }
+        }
     }
 }
