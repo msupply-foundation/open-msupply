0943eddb1d7b057b0dc29e7a3dd0956f51af13f3
Clemens Zeidler <clemens@msupply.foundation>
2021-09-14 16:55:17 +1200

    Add master_list, master_list_line and master_list_name_join tables

---
+++ server/src/database/repository/diesel/master_list_line.rs
@@ -0,0 +1,57 @@
+use super::{DBBackendConnection, DBConnection};
+
+use crate::database::{
+    repository::{repository::get_connection, RepositoryError},
+    schema::MasterListLineRow,
+};
+
+use diesel::{
+    prelude::*,
+    r2d2::{ConnectionManager, Pool},
+};
+
+#[derive(Clone)]
+pub struct MasterListLineRepository {
+    pool: Pool<ConnectionManager<DBBackendConnection>>,
+}
+
+impl MasterListLineRepository {
+    pub fn new(pool: Pool<ConnectionManager<DBBackendConnection>>) -> Self {
+        MasterListLineRepository { pool }
+    }
+
+    #[cfg(feature = "postgres")]
+    pub fn upsert_one_tx(
+        connection: &DBConnection,
+        row: &MasterListLineRow,
+    ) -> Result<(), RepositoryError> {
+        use crate::database::schema::diesel_schema::master_list_line::dsl::*;
+
+        diesel::insert_into(master_list_line)
+            .values(row)
+            .on_conflict(id)
+            .do_update()
+            .set(row)
+            .execute(connection)?;
+        Ok(())
+    }
+
+    #[cfg(feature = "sqlite")]
+    pub fn upsert_one_tx(
+        connection: &DBConnection,
+        row: &MasterListLineRow,
+    ) -> Result<(), RepositoryError> {
+        use crate::database::schema::diesel_schema::master_list_line::dsl::*;
+        diesel::replace_into(master_list_line)
+            .values(row)
+            .execute(connection)?;
+        Ok(())
+    }
+
+    pub async fn find_one_by_id(&self, id: &str) -> Result<MasterListLineRow, RepositoryError> {
+        use crate::database::schema::diesel_schema::master_list_line::dsl::*;
+        let connection = get_connection(&self.pool)?;
+        let result = master_list_line.filter(id.eq(id)).first(&connection)?;
+        Ok(result)
+    }
+}
