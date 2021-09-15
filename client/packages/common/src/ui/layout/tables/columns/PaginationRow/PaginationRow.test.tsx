d2ca8bccddbd96794684a267df7db4a8be8208a6
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-15 12:56:18 +1200

    Add PaginationRow component

---
+++ client/packages/common/src/ui/layout/tables/columns/PaginationRow/PaginationRow.test.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { PaginationRow } from './PaginationRow';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+
+describe('PaginationRow', () => {
+  it('Renders a string : Showing X-Y of Z', () => {
+    const offset = 0;
+    const first = 10;
+    const total = 20;
+
+    const { getByText } = render(
+      <PaginationRow
+        offset={offset}
+        total={total}
+        first={first}
+        onChange={jest.fn()}
+      />
+    );
+
+    const node1 = getByText(/showing/i);
+    const node2 = getByText(`${offset + 1}-${first + offset}`);
+    const node3 = getByText(/of/i);
+    const node4 = getByText(`${total}`);
+
+    expect(node1).toBeInTheDocument();
+    expect(node2).toBeInTheDocument();
+    expect(node3).toBeInTheDocument();
+    expect(node4).toBeInTheDocument();
+  });
+
+  it('Renders at least one page button', () => {
+    const { getByRole } = render(
+      <PaginationRow offset={1} total={1} first={1} onChange={jest.fn()} />
+    );
+
+    const node = getByRole('button', { name: 'page 1' });
+
+    expect(node).toBeInTheDocument();
+  });
+
+  it('Renders the first 5 pages and the last page', () => {
+    const offset = 0;
+    const first = 10;
+    const total = 20000;
+
+    const { getByRole } = render(
+      <PaginationRow
+        offset={offset}
+        total={total}
+        first={first}
+        onChange={jest.fn()}
+      />
+    );
+
+    const node1 = getByRole('button', { name: /page 1/i });
+    const node2 = getByRole('button', { name: /page 2$/i });
+    const node3 = getByRole('button', { name: /page 3/i });
+    const node4 = getByRole('button', { name: /page 4/i });
+    const node5 = getByRole('button', { name: /page 5/i });
+    const node6 = getByRole('button', { name: /page 2000/i });
+
+    expect(node1).toBeInTheDocument();
+    expect(node2).toBeInTheDocument();
+    expect(node3).toBeInTheDocument();
+    expect(node4).toBeInTheDocument();
+    expect(node5).toBeInTheDocument();
+    expect(node6).toBeInTheDocument();
+  });
+
+  it('Renders nothing when the total is zero', () => {
+    const offset = 0;
+    const first = 10;
+    const total = 0;
+
+    const { queryByRole, queryByText } = render(
+      <PaginationRow
+        offset={offset}
+        total={total}
+        first={first}
+        onChange={jest.fn()}
+      />
+    );
+
+    const node1 = queryByRole('button', { name: /page 1/i });
+    const node2 = queryByText('showing');
+
+    expect(node1).not.toBeInTheDocument();
+    expect(node2).not.toBeInTheDocument();
+  });
+
+  it('Triggers the callback when a page button is pressed, with the page, zero indexed', () => {
+    const offset = 0;
+    const first = 10;
+    const total = 20000;
+    const onChange = jest.fn();
+
+    const { getByRole } = render(
+      <PaginationRow
+        offset={offset}
+        total={total}
+        first={first}
+        onChange={onChange}
+      />
+    );
+
+    const node = getByRole('button', { name: /page 1/i });
+
+    act(() => {
+      userEvent.click(node);
+    });
+
+    expect(onChange).toBeCalledTimes(1);
+    expect(onChange).toBeCalledWith(0);
+  });
+
+  it('Renders nothing when the total is zero', () => {
+    const offset = 0;
+    const first = 10;
+    const total = 0;
+
+    const { queryByRole, queryByText } = render(
+      <PaginationRow
+        offset={offset}
+        total={total}
+        first={first}
+        onChange={jest.fn()}
+      />
+    );
+
+    const node1 = queryByRole('button', { name: /page 1/i });
+    const node2 = queryByText('showing');
+
+    expect(node1).not.toBeInTheDocument();
+    expect(node2).not.toBeInTheDocument();
+  });
+});
