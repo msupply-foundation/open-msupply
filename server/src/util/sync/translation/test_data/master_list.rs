327e0fe517c92a28080a1d717cc7cee35be899f7
Clemens Zeidler <clemens@msupply.foundation>
2021-09-15 19:53:37 +1200

    Fix master list translation
    Source: https://github.com/msupply-foundation/remote-server/commit/88c0da4e7483954da5d2d073170d5081d6c8f884

--- server/src/util/sync/translation/test_data/master_list.rs
+++ server/src/util/sync/translation/test_data/master_list.rs
@@ -12,9 +12,9 @@ pub fn get_test_master_list_records() -> Vec<TestSyncRecord> {
     vec![TestSyncRecord {
         translated_record: TestSyncDataRecord::MasterList(Some(MasterListRow {
             id: "87027C44835B48E6989376F42A58F7E3".to_owned(),
-            name: "".to_owned(),
+            name: "District Store".to_owned(),
             code: "".to_owned(),
-            description: "District Store".to_owned(),
+            description: "note 1".to_owned(),
         })),
         identifier: "Master list",
         sync_record: SyncRecord {
@@ -24,7 +24,7 @@ pub fn get_test_master_list_records() -> Vec<TestSyncRecord> {
                 "description": "District Store",
                 "date_created": "2017-08-17",
                 "created_by_user_ID": "0763E2E3053D4C478E1E6B6B03FEC207",
-                "note": "",
+                "note": "note 1",
                 "gets_new_items": false,
                 "tags": null,
                 "isProgram": false,
@@ -45,9 +45,9 @@ pub fn get_test_master_list_upsert_records() -> Vec<TestSyncRecord> {
     vec![TestSyncRecord {
         translated_record: TestSyncDataRecord::MasterList(Some(MasterListRow {
             id: "87027C44835B48E6989376F42A58F7E3".to_owned(),
-            name: "".to_owned(),
+            name: "District Store 2".to_owned(),
             code: "".to_owned(),
-            description: "District Store 2".to_owned(),
+            description: "note 2".to_owned(),
         })),
         identifier: "Master list upsert",
         sync_record: SyncRecord {
@@ -57,7 +57,7 @@ pub fn get_test_master_list_upsert_records() -> Vec<TestSyncRecord> {
                 "description": "District Store 2",
                 "date_created": "2017-08-17",
                 "created_by_user_ID": "0763E2E3053D4C478E1E6B6B03FEC207",
-                "note": "",
+                "note": "note 2",
                 "gets_new_items": false,
                 "tags": null,
                 "isProgram": false,
