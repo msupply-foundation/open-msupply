aa439e2e306293ee5a712f3eb82c7e5391f018d0
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-15 22:26:50 +1200

    Add update for tests
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/79e84720e6221a1996fa7adb2471217afaed7aa4

--- client/jest-setup.ts
+++ client/jest-setup.ts
@@ -58,3 +58,28 @@ window.resizeTo = (width, height) => {
     outerHeight: height,
   }).dispatchEvent(new Event('resize'));
 };
+
+class ResizeObserver {
+  fn: ResizeObserverCallback;
+
+  constructor(fn: ResizeObserverCallback) {
+    this.fn = fn;
+  }
+
+  observe() {
+    const entries: ResizeObserverEntry[] = [];
+    this.fn(entries, this);
+  }
+
+  disconnect() {}
+
+  unobserve() {}
+}
+
+beforeEach(() => {
+  Object.assign(window, { ResizeObserver });
+});
+
+afterEach(() => {
+  Object.assign(window, { ResizeObserver: null });
+});
