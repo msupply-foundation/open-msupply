f2b482104832dc3ea91138f769d7d413142fab59
wlthomson <will.tho@outlook.com>
2021-09-15 20:27:30 +1200

    Update central sync logic with persisted cursor
    Updates `pull_central_records` to read and write sync cursor from
    database.

--- server/src/database/repository/diesel/central_sync_buffer.rs
+++ server/src/database/repository/diesel/central_sync_buffer.rs
@@ -19,6 +19,18 @@ impl CentralSyncBufferRepository {
         CentralSyncBufferRepository { pool }
     }
 
+    pub async fn insert_one(
+        &self,
+        central_sync_buffer_row: &CentralSyncBufferRow,
+    ) -> Result<(), RepositoryError> {
+        use crate::database::schema::diesel_schema::central_sync_buffer::dsl::*;
+        let connection = get_connection(&self.pool)?;
+        diesel::insert_into(central_sync_buffer)
+            .values(central_sync_buffer_row)
+            .execute(&*connection)?;
+        Ok(())
+    }
+
     pub async fn insert_many(
         &self,
         central_sync_buffer_rows: &Vec<CentralSyncBufferRow>,
