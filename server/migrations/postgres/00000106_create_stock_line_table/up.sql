b3b8b69778568c0656c8528176ecda049dbe4e5f
andreievg <andrei.evguenov@gmail.com>
2021-09-17 03:53:20 +1200

    changing stock line schema

---
+++ server/migrations/postgres/00000106_create_stock_line_table/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE stock_line (
+    id TEXT NOT NULL PRIMARY KEY,
+    item_id TEXT NOT NULL REFERENCES item(id),
+    store_id TEXT NOT NULL REFERENCES store(id),
+    batch TEXT,
+    expiry_date TEXT,
+    cost_price_per_pack DECIMAL NOT NULL,
+    sell_price_per_pack DECIMAL NOT NULL,
+    available_number_of_packs INTEGER NOT NULL,
+    total_number_of_packs INTEGER NOT NULL,
+    pack_size INTEGER NOT NULL
+)
---
+++ server/src/database/schema/stock_line.rs
@@ -0,0 +1,16 @@
+use super::diesel_schema::stock_line;
+
+#[derive(Clone, Queryable, Insertable, Debug, PartialEq)]
+#[table_name = "stock_line"]
+pub struct StockLineRow {
+    pub id: String,
+    pub item_id: String,
+    pub store_id: String,
+    pub batch: Option<String>,
+    pub pack_size: i32,
+    pub cost_price_per_pack: f64,
+    pub sell_price_per_pack: f64,
+    pub available_number_of_packs: i32,
+    pub total_number_of_packs: i32,
+    pub expiry_date: Option<String>,
+}
