f4955d1d00a2cd39872af28dd2d83b38c48c845e
wlthomson <will.tho@outlook.com>
2021-09-16 20:00:12 +1200

    Update CentralSyncBufferRow deserialization
    Removes duplicate `cursor_id` field from `central_sync_buffer_row` and
    adds custom derializer for reading central sync record data as
    `String`.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/f88a9ad8b09f234b2951d76ed562385457f4d7b2

--- server/src/database/schema/central_sync_buffer.rs
+++ server/src/database/schema/central_sync_buffer.rs
@@ -1,20 +1,27 @@
 use super::diesel_schema::central_sync_buffer;
 
-use serde::{self, Deserialize, Serialize};
+use serde::{self, Deserialize, Deserializer, Serialize};
 use std::fmt::{self, Debug, Display};
 
 #[derive(Clone, Queryable, Deserialize, Serialize, Insertable, Debug, PartialEq, Eq)]
 #[table_name = "central_sync_buffer"]
 pub struct CentralSyncBufferRow {
     #[serde(rename = "ID")]
-    pub id: String,
-    #[serde(rename = "ID")]
-    pub cursor_id: i32,
+    pub id: i32,
     #[serde(rename = "tableName")]
     pub table_name: String,
     #[serde(rename = "recordId")]
     pub record_id: String,
+    #[serde(deserialize_with = "map_as_string")]
     pub data: String,
+}
+
+fn map_as_string<'de, D>(deserializer: D) -> Result<String, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    serde_json::to_string_pretty(&serde_json::Value::deserialize(deserializer)?)
+        .map_err(|err| serde::de::Error::custom(err.to_string()))
 }
 
 impl Display for CentralSyncBufferRow {
