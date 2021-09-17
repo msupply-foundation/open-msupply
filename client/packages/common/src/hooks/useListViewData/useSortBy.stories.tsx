77db1de4e1655d87f93d1d19dbff7747ad3b405d
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-17 16:44:35 +1200

    Add useSortBy hook
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/cb763e27e43c60959e6054572aac99eb49d6a00e

---
+++ client/packages/common/src/hooks/useListViewData/useSortBy.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSortBy } from './useSortBy';
+import { Story } from '@storybook/react';
+
+export default {
+  title: 'Hooks/useSortBy',
+};
+
+interface TestSortBy {
+  id: string;
+  quantity: number;
+}
+
+const Template: Story = () => {
+  const { sortBy, onChangeSortBy } = useSortBy<TestSortBy>({
+    key: 'id',
+    isDesc: true,
+  });
+
+  return (
+    <div>
+      <div>
+        <span> Two buttons to sort by two different keys, ID or Quantity.</span>
+        <button onClick={() => onChangeSortBy('id')}>Sort by ID!</button>
+        <button onClick={() => onChangeSortBy('quantity')}>
+          Sort by Quantity!
+        </button>
+      </div>
+
+      <p style={{ whiteSpace: 'pre-line' }}>
+        {JSON.stringify(sortBy, null, 2)}
+      </p>
+    </div>
+  );
+};
+
+export const Primary = Template.bind({});
