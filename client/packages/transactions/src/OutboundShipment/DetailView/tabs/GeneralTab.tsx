995a6f9cb000c5cc540372d6a766cbc1ed8c6c95
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-18 19:49:28 +1200

    Add basic general tab with rendering datatable

---
+++ client/packages/transactions/src/OutboundShipment/DetailView/tabs/GeneralTab.tsx
@@ -0,0 +1,72 @@
+import {
+  ColumnDefinition,
+  Item,
+  RemoteDataTable,
+  useColumns,
+  useDataTableApi,
+} from '@openmsupply-client/common';
+import React, { FC } from 'react';
+
+interface GeneralTabProps<T> {
+  data: T[];
+}
+
+const defaultColumns: ColumnDefinition<Item>[] = [
+  {
+    label: 'label.name',
+    key: 'id',
+    width: 100,
+    minWidth: 100,
+    maxWidth: 100,
+    align: 'left',
+  },
+  {
+    label: 'label.type',
+    key: 'code',
+    width: 100,
+    minWidth: 100,
+    maxWidth: 100,
+    align: 'left',
+  },
+  {
+    label: 'label.type',
+    key: 'name',
+    width: 100,
+    minWidth: 100,
+    maxWidth: 100,
+    align: 'left',
+  },
+  {
+    label: 'label.type',
+    key: 'packSize',
+    width: 100,
+    minWidth: 100,
+    maxWidth: 100,
+    align: 'left',
+  },
+  {
+    label: 'label.type',
+    key: 'quantity',
+    width: 100,
+    minWidth: 100,
+    maxWidth: 100,
+    align: 'left',
+  },
+];
+
+export const GeneralTab: FC<GeneralTabProps<Item>> = ({ data }) => {
+  const columns = useColumns<Item>(defaultColumns);
+  const tableApi = useDataTableApi<Item>();
+
+  return (
+    <RemoteDataTable
+      sortBy={[{ id: 'quantity' }]}
+      pagination={{ first: 0, offset: 0, total: 10 }}
+      tableApi={tableApi}
+      columns={columns}
+      data={data}
+      onSortBy={() => {}}
+      onChangePage={() => {}}
+    />
+  );
+};
