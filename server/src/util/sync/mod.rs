44c0e8d8512e7c30d577eb67728931bbc4589512
Clemens Zeidler <clemens@msupply.foundation>
2021-09-21 07:41:20 +1200

    Integerate all records in one transaction
    Source: https://github.com/msupply-foundation/remote-server/commit/1708a277d8ebb4ccbb73c6aee75169ec0a4160e9

--- server/src/util/sync/mod.rs
+++ server/src/util/sync/mod.rs
@@ -15,8 +15,9 @@ pub use remote::{
 pub use server::SyncServer;
 
 use crate::{
-    database::repository::{
-        CentralSyncBufferRepository, CentralSyncCursorRepository, SyncRepository,
+    database::{
+        repository::{CentralSyncBufferRepository, CentralSyncCursorRepository, SyncRepository},
+        schema::CentralSyncBufferRow,
     },
     server::data::RepositoryRegistry,
 };
@@ -179,18 +180,26 @@ impl SyncReceiverActor {
     ) -> Result<(), String> {
         let central_sync_buffer_repository: &CentralSyncBufferRepository =
             repositories.get::<CentralSyncBufferRepository>();
+
+        // load all sync buffer entries into memory
+        let mut records: Vec<CentralSyncBufferRow> = Vec::new();
+        for table_name in TRANSLATION_RECORDS {
+            let mut buffer_rows = central_sync_buffer_repository
+                .get_sync_entries(table_name)
+                .await
+                .map_err(|_| "Failed to read central sync entries".to_string())?;
+            records.append(&mut buffer_rows);
+        }
+
+        // write all entries to the DB
         let sync_session = repositories
             .get::<SyncRepository>()
             .new_sync_session()
             .await
             .unwrap();
-        for table_name in TRANSLATION_RECORDS {
-            let buffer_rows = central_sync_buffer_repository
-                .get_sync_entries(table_name)
-                .await
-                .map_err(|_| "Failed to read central sync entries".to_string())?;
-            import_sync_records(&sync_session, repositories, &buffer_rows).await?;
-        }
+        import_sync_records(&sync_session, repositories, &records).await?;
+
+        // clear the sync buffer
         central_sync_buffer_repository
             .remove_all()
             .await
