f4432b6f39d0a5398af63be4ae350b89a801a9d1
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-26 12:52:15 +1300

    Add test fixes and shift default header/cell
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/5292a3033b9ffbb788b684911ffed2560370067f

---
+++ client/packages/common/src/ui/layout/tables/components/index.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from 'react';
+import { CellProps, HeaderProps } from '../columns/types';
+import { DomainObject } from '../../../../types';
+import { useTranslation } from '../../../../intl';
+
+export * from './DataRow';
+
+export const BasicCell = <T extends DomainObject>({
+  column,
+  rowData,
+}: CellProps<T>): ReactElement => {
+  return <>{column.accessor(rowData)}</>;
+};
+
+export const BasicHeader = <T extends DomainObject>({
+  column,
+}: HeaderProps<T>): ReactElement => {
+  const t = useTranslation();
+  const header = column.label === '' ? column.label : t(column.label);
+
+  return <>{header}</>;
+};
