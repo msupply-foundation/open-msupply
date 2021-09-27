46e3dff71d0cccdfa815c5e6f7f854055ab4498d
Clemens Zeidler <clemens@msupply.foundation>
2021-09-28 09:18:15 +1300

    Use TIMESTAMP for the expiry_date (stock_line and invoice_line)
    Source: https://github.com/msupply-foundation/remote-server/commit/43d39b770751a468d9fa38e5a19dd26aed56ee6a

--- server/migrations/postgres/00000106_create_stock_line_table/up.sql
+++ server/migrations/postgres/00000106_create_stock_line_table/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE stock_line (
     item_id TEXT NOT NULL REFERENCES item(id),
     store_id TEXT NOT NULL REFERENCES store(id),
     batch TEXT,
-    expiry_date TEXT,
+    expiry_date TIMESTAMP,
     cost_price_per_pack DOUBLE PRECISION NOT NULL,
     sell_price_per_pack DOUBLE PRECISION NOT NULL,
     available_number_of_packs INTEGER NOT NULL,
