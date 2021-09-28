6d21633344bf098a2fa5b8f166e51af0a2becee8
Joshua Griffin <griffinjoshua5@gmail.com>
2021-09-29 09:47:01 +1300

    Add useIsMountedRef hook
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/c5f6ae7970fc7242367ad2d088d133f66c524d1a

---
+++ client/packages/common/src/hooks/useIsMountedRef/useIsMountedRef.test.tsx
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useIsMountedRef } from '.';
+
+describe('useIsMountedRef', () => {
+  it('is set to true once a component has mounted', () => {
+    const { result } = renderHook(useIsMountedRef);
+
+    expect(result.current.current).toBe(true);
+  });
+
+  it('is set to false once a component has unmounted', () => {
+    const { result, unmount } = renderHook(useIsMountedRef);
+
+    unmount();
+
+    expect(result.current.current).toBe(false);
+  });
+});
