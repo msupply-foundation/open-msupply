9dd1dc4a6cc7a272224d66e2184e498d78dd39bc
Clemens Zeidler <clemens@msupply.foundation>
2021-09-30 19:31:30 +1300

    Use TEXT type for dates in sqlite
    Source: https://github.com/msupply-foundation/remote-server/commit/ebc1a80a210a1cab99c994c007062190c0fa7d9e

--- server/migrations/sqlite/00000105_create_stock_line_table/up.sql
+++ server/migrations/sqlite/00000105_create_stock_line_table/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE stock_line (
     item_id TEXT NOT NULL REFERENCES item(id),
     store_id TEXT NOT NULL REFERENCES store(id),
     batch TEXT,
-    expiry_date DATE,
+    expiry_date TEXT,
     cost_price_per_pack REAL NOT NULL,
     sell_price_per_pack REAL NOT NULL,
     available_number_of_packs INTEGER NOT NULL,
