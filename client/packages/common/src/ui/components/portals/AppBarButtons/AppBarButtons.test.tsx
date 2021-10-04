8b2053643bec154a61a9916bb598c1e259a2738a
Joshua Griffin <griffinjoshua5@gmail.com>
2021-10-04 22:40:31 +1300

    Add AppBarButtons portal

---
+++ client/packages/common/src/ui/components/portals/AppBarButtons/AppBarButtons.test.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from 'react';
+import { act, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AppBarButtons, AppBarButtonsPortal } from './AppBarButtons';
+
+describe('AppBarContent', () => {
+  const TestAppBarButtons: FC<{ initialShow: boolean }> = ({ initialShow }) => {
+    const [show, setShow] = React.useState(initialShow);
+
+    return (
+      <>
+        <button onClick={() => setShow(state => !state)} />
+        {show && (
+          <div id="source">
+            <AppBarButtons />
+          </div>
+        )}
+
+        <div data-testid="2">
+          <AppBarButtonsPortal>
+            <span>josh</span>
+          </AppBarButtonsPortal>
+        </div>
+      </>
+    );
+  };
+
+  it('Portal children are rendered under the source', () => {
+    const { getByText } = render(<TestAppBarButtons initialShow />);
+
+    const node = getByText(/josh/);
+
+    expect(node.parentNode).not.toHaveAttribute('data-testid', '2');
+    expect(node.closest('#source')).toBeInTheDocument();
+  });
+
+  it('Portal children dismount if the portal dismounts', () => {
+    const { queryByText, getByRole } = render(
+      <TestAppBarButtons initialShow />
+    );
+
+    const button = getByRole(/button/);
+
+    act(() => {
+      userEvent.click(button);
+    });
+
+    const node = queryByText(/josh/);
+
+    expect(node).not.toBeInTheDocument();
+  });
+
+  it('The portal children are not rendered if the source is not rendered', () => {
+    const { queryByText } = render(<TestAppBarButtons initialShow={false} />);
+
+    const node = queryByText(/josh/);
+
+    expect(node).not.toBeInTheDocument();
+  });
+});
