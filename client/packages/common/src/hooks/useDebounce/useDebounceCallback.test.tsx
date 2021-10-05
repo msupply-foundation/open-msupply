363cf43fc2d0836f20e58d7c47c7743d573eddca
Joshua Griffin <griffinjoshua5@gmail.com>
2021-10-05 16:01:20 +1300

    Add useDebounceCallback hook
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/fafd977525f1c235c16f80592d683dc39dd91904

---
+++ client/packages/common/src/hooks/useDebounce/useDebounceCallback.test.tsx
@@ -0,0 +1,164 @@
+import { waitFor } from '@testing-library/dom';
+import { renderHook } from '@testing-library/react-hooks';
+import { useDebounceCallback } from './useDebounceCallback';
+
+describe('useDebounceCallback', () => {
+  it('returns a function which debounces consecutive calls', async () => {
+    // Setting up a simple value for incrementing and a function which is a closure
+    // over the value which will be incremented. The side effect makes this  somewhat flakey
+    // when creating tests, but allows us to access the real state (i) to compare against
+    // returned values from the callback to ensure they're the same.
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+
+    const { result } = renderHook(() => {
+      const cb = useDebounceCallback(func, []);
+
+      return cb;
+    });
+
+    result.current();
+    result.current();
+    await result.current();
+
+    await waitFor(() => {
+      expect(i).toBe(1);
+    });
+  });
+
+  it('calls the function again correctly after a wait period has completed', async () => {
+    // Ensuring that after a wait period, we can just call the function again and the normal behaviour
+    // is resumed.
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+
+    const { result } = renderHook(() => {
+      const cb = useDebounceCallback(func, [], 10);
+
+      return cb;
+    });
+
+    result.current();
+    result.current();
+    await result.current();
+    result.current();
+    await result.current();
+    await result.current();
+
+    await waitFor(() => {
+      expect(i).toBe(3);
+    });
+  });
+
+  it('returns a function which resolves to the final value', async () => {
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+    // Here we set up a debounced callback and call it multiple times and compare that the
+    // resolved value from each invocation aligns with with what the 'state' of i "should be",
+    // which will mean that the resolved values are equal to the final state of the consecutive calls.
+    const { result } = renderHook(() => {
+      const cb = useDebounceCallback(func, [], 10);
+
+      return cb;
+    });
+
+    const val1 = result.current();
+    const val2 = result.current();
+    const val3 = result.current();
+
+    await waitFor(() => {
+      expect(val1).resolves.toBe(1);
+      expect(val2).resolves.toBe(1);
+      expect(val3).resolves.toBe(1);
+    });
+  });
+
+  it('is unaffected by re-renders', async () => {
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+    // Since this is a hook, we want some render resilience, such that if a render occurs
+    // in the middle of consecutive calls, the behaviour is stable and debouncing still
+    // happens as per normal.
+    const { result, rerender } = renderHook(() => {
+      const cb = useDebounceCallback(func, []);
+      return cb;
+    });
+
+    result.current();
+    rerender();
+    result.current();
+    rerender();
+
+    await result.current();
+
+    await waitFor(() => {
+      expect(i).toBe(1);
+    });
+  });
+
+  it('creates a new debounced function whenever the deps list changes', async () => {
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+    // We also have a dependency array where the desired behaviour would be that
+    // whenever a dependency changes, a new debounced function is created. So
+    // this test will re-render with a different dependency in the deps list, causing
+    // a new debounced function to be created after each invocation, so consecutive calls should
+    // NOT be debounced
+    const { result, rerender } = renderHook(props => {
+      const cb = useDebounceCallback(func, [props]);
+      return cb;
+    });
+
+    result.current();
+    rerender({});
+    result.current();
+    rerender({});
+
+    await result.current();
+
+    await waitFor(() => {
+      expect(i).toBe(3);
+    });
+  });
+
+  it('creates debounces normally if a dependency in the deps list has not changed', async () => {
+    let i = 0;
+    const func = () => {
+      i = i + 1;
+      return i;
+    };
+    // just making sure that if a dependency in the deps list doesn't actually change, we still keep our
+    // stable debounce function, by passing a primitive to the deps list. Note that the initial props has
+    // been set, and if removed, will mean a change from undefined -> !undefined
+    const { result, rerender } = renderHook((props = 1) => {
+      const cb = useDebounceCallback(func, [props]);
+      return cb;
+    });
+
+    result.current();
+    rerender(1);
+    result.current();
+    rerender(1);
+
+    await result.current();
+
+    await waitFor(() => {
+      expect(i).toBe(1);
+    });
+  });
+});
--- client/packages/common/src/hooks/useDebounce/useDebouncedValue.stories.tsx
+++ client/packages/common/src/hooks/useDebounce/useDebouncedValue.stories.tsx
@@ -3,12 +3,13 @@ import { Story } from '@storybook/react';
 import { useDebouncedValue } from './useDebouncedValue';
 import { TextField, Typography, Button } from '@mui/material';
 import { Box } from '@mui/system';
+import { useDebounceCallback } from '.';
 
 export default {
-  title: 'Hooks/useDebouncedValue',
+  title: 'Hooks/useDebounce',
 };
 
-const Template: Story = () => {
+const Value: Story = () => {
   const [debounceTime, setDebounceTime] = useState(500);
   const [number, setNumber] = useState(0);
   const debouncedNumber = useDebouncedValue(number, debounceTime);
@@ -42,4 +43,33 @@ const Template: Story = () => {
   );
 };
 
-export const Primary = Template.bind({});
+const Callback: Story = () => {
+  const [debounceTime, setDebounceTime] = useState(500);
+  const [number, setNumber] = useState(0);
+  const cb = useDebounceCallback(setNumber, [], debounceTime);
+
+  return (
+    <div>
+      <p>{`State number: ${number}`}</p>
+      <Box gap={2} flexDirection="row" display="flex" alignItems="center">
+        <Typography> Debounced timer: </Typography>
+        <TextField
+          value={debounceTime}
+          onChange={e => setDebounceTime(Number(e.target.value))}
+        />
+      </Box>
+      <Box gap={2} mt={2} flexDirection="row" display="flex">
+        <Button variant="contained" onClick={() => cb(state => (state += 1))}>
+          Increment
+        </Button>
+        <Button variant="contained" onClick={() => cb(state => (state -= 1))}>
+          Decrement
+        </Button>
+      </Box>
+    </div>
+  );
+};
+
+export const UseDebounceValue = Value.bind({});
+
+export const UseDebounceCallback = Callback.bind({});
