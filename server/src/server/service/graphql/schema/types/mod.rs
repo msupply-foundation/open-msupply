702f3e19d527532d254b312d169cc19e9124e07e
andreievg <andrei.evguenov@gmail.com>
2021-10-08 03:32:24 +1300

    generalise connector types
    Source: https://github.com/msupply-foundation/remote-server/commit/bfb7fee3df8c3d526e6e578d827df9b7a5ed0ea8

--- server/src/server/service/graphql/schema/types/mod.rs
+++ server/src/server/service/graphql/schema/types/mod.rs
@@ -2,18 +2,21 @@ use crate::{
     database::{
         loader::{InvoiceLoader, StoreLoader},
         repository::{
-            CustomerInvoiceRepository, InvoiceLineRepository, RequisitionLineRepository,
-            StorageConnectionManager,
+            CustomerInvoiceRepository, InvoiceLineRepository, RepositoryError,
+            RequisitionLineRepository, StorageConnectionManager,
         },
         schema::{
             InvoiceLineRow, InvoiceRow, RequisitionLineRow, RequisitionRow, RequisitionRowType,
             StoreRow,
         },
     },
+    domain::PaginationOption,
     server::service::graphql::ContextExt,
+    service::{ListError, ListResult},
 };
 
-use async_graphql::{dataloader::DataLoader, Context, Enum, InputObject, Object};
+use async_graphql::*;
+use dataloader::DataLoader;
 
 // M1 speced API is moved to their own files
 // Types defined here are prototype types and should be removed before M1 release to avoid confusion (for consumers and devs)
@@ -34,6 +37,131 @@ pub use self::invoices_query::*;
 
 pub mod sort_filter_types;
 pub use self::sort_filter_types::*;
+
+/// Generic Connector
+#[derive(SimpleObject)]
+#[graphql(concrete(name = "NameConnector", params(NameNode)))]
+pub struct Connector<T: OutputType> {
+    total_count: u32,
+    nodes: Vec<T>,
+}
+
+/// Convert from ListResult to Generic Connector
+impl<DomainType, GQLType> From<ListResult<DomainType>> for Connector<GQLType>
+where
+    GQLType: From<DomainType> + OutputType,
+{
+    fn from(ListResult { count, rows }: ListResult<DomainType>) -> Self {
+        Connector {
+            total_count: count,
+            nodes: rows.into_iter().map(GQLType::from).collect(),
+        }
+    }
+}
+
+/// Generic Pagination Input
+#[derive(InputObject)]
+pub struct PaginationInput {
+    pub first: Option<u32>,
+    pub offset: Option<u32>,
+}
+
+impl From<PaginationInput> for PaginationOption {
+    fn from(PaginationInput { first, offset }: PaginationInput) -> Self {
+        PaginationOption {
+            limit: first,
+            offset,
+        }
+    }
+}
+
+/// Generic Error Wrapper
+#[derive(SimpleObject)]
+#[graphql(concrete(name = "ConnectorError", params(ConnectorErrorInterface)))]
+pub struct ErrorWrapper<T: OutputType> {
+    error: T,
+}
+
+// Generic Error Interface
+#[derive(Interface)]
+#[graphql(field(name = "description", type = "&str"))]
+pub enum ConnectorErrorInterface {
+    DBError(DBError),
+    PaginationError(PaginationError),
+}
+
+impl From<ListError> for ErrorWrapper<ConnectorErrorInterface> {
+    fn from(error: ListError) -> Self {
+        let error = match error {
+            ListError::DBError(error) => ConnectorErrorInterface::DBError(DBError(error)),
+            ListError::LimitBelowMin { limit, min } => {
+                ConnectorErrorInterface::PaginationError(PaginationError {
+                    out_of_range: FirstOutOfRange::Min(min),
+                    first: limit,
+                })
+            }
+            ListError::LimitAboveMax { limit, max } => {
+                ConnectorErrorInterface::PaginationError(PaginationError {
+                    out_of_range: FirstOutOfRange::Max(max),
+                    first: limit,
+                })
+            }
+        };
+
+        ErrorWrapper { error }
+    }
+}
+
+// Generic Errors
+pub struct DBError(pub RepositoryError);
+
+#[Object]
+impl DBError {
+    pub async fn description(&self) -> &'static str {
+        "Dabase Error"
+    }
+
+    pub async fn full_error(&self) -> String {
+        format!("{:#}", self.0)
+    }
+}
+
+pub enum FirstOutOfRange {
+    Max(u32),
+    Min(u32),
+}
+pub struct PaginationError {
+    out_of_range: FirstOutOfRange,
+    first: u32,
+}
+
+#[Object]
+impl PaginationError {
+    pub async fn description(&self) -> &'static str {
+        match &self.out_of_range {
+            FirstOutOfRange::Max(_) => "First is too big",
+            FirstOutOfRange::Min(_) => "First is too low",
+        }
+    }
+
+    pub async fn max(&self) -> Option<u32> {
+        match &self.out_of_range {
+            FirstOutOfRange::Max(max) => Some(max.clone()),
+            _ => None,
+        }
+    }
+
+    pub async fn min(&self) -> Option<u32> {
+        match &self.out_of_range {
+            FirstOutOfRange::Min(min) => Some(min.clone()),
+            _ => None,
+        }
+    }
+
+    pub async fn first(&self) -> u32 {
+        self.first
+    }
+}
 
 #[derive(Clone)]
 pub struct Store {
