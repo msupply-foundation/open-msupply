85a24f291694050cfb53178d74b48900032cef64
andreievg <andrei.evguenov@gmail.com>
2021-10-08 04:28:20 +1300

    use service in invoice queries and removing conflicting queries
    Source: https://github.com/msupply-foundation/remote-server/commit/6715bc532f1f87e44e58f632621b892abd518ee7

--- server/src/server/service/graphql/schema/queries/mod.rs
+++ server/src/server/service/graphql/schema/queries/mod.rs
@@ -1,24 +1,22 @@
 pub mod pagination;
 
-use crate::database::repository::{
-    InvoiceLineRepository, InvoiceQueryRepository, RepositoryError, RequisitionRepository,
-    StorageConnectionManager, StoreRepository,
-};
-use crate::database::schema::{InvoiceLineRow, RequisitionRow, StoreRow};
+use crate::database::repository::StorageConnectionManager;
+
+use crate::domain::invoice::InvoiceFilter;
 use crate::domain::item::ItemFilter;
 use crate::domain::name::NameFilter;
 use crate::domain::PaginationOption;
-use crate::server::service::graphql::schema::types::{InvoiceLine, Requisition, Store};
 use crate::server::service::graphql::ContextExt;
+use crate::service::invoice::{get_invoice, get_invoices};
 use crate::service::item::get_items;
 use crate::service::name::get_names;
 
 use super::types::{
-    convert_sort, InvoiceFilterInput, InvoiceList, InvoiceNode, InvoiceSortInput, ItemFilterInput,
-    ItemSortInput, ItemsResponse, NameFilterInput, NameSortInput, NamesResponse, PaginationInput,
+    convert_sort, InvoiceFilterInput, InvoiceResponse, InvoiceSortInput, InvoicesResponse,
+    ItemFilterInput, ItemSortInput, ItemsResponse, NameFilterInput, NameSortInput, NamesResponse,
+    PaginationInput,
 };
 use async_graphql::{Context, Object};
-use pagination::Pagination;
 pub struct Queries;
 
 #[Object]
@@ -36,9 +34,9 @@ impl Queries {
         #[graphql(desc = "sort options (only first sort input is evaluated for this endpoint)")]
         sort: Option<Vec<NameSortInput>>,
     ) -> NamesResponse {
-        let connection_pool = ctx.get_repository::<StorageConnectionManager>();
+        let connection_manager = ctx.get_repository::<StorageConnectionManager>();
         get_names(
-            connection_pool,
+            connection_manager,
             page.map(PaginationOption::from),
             filter.map(NameFilter::from),
             convert_sort(sort),
@@ -54,9 +52,9 @@ impl Queries {
         #[graphql(desc = "sort options (only first sort input is evaluated for this endpoint)")]
         sort: Option<Vec<ItemSortInput>>,
     ) -> ItemsResponse {
-        let connection_pool = ctx.get_repository::<StorageConnectionManager>();
+        let connection_manager = ctx.get_repository::<StorageConnectionManager>();
         get_items(
-            connection_pool,
+            connection_manager,
             page.map(PaginationOption::from),
             filter.map(ItemFilter::from),
             convert_sort(sort),
@@ -64,80 +62,30 @@ impl Queries {
         .into()
     }
 
-    // TODO return better error
     pub async fn invoice(
         &self,
         ctx: &Context<'_>,
         #[graphql(desc = "id of the invoice")] id: String,
-    ) -> Result<InvoiceNode, RepositoryError> {
+    ) -> InvoiceResponse {
         let connection_manager = ctx.get_repository::<StorageConnectionManager>();
-        let connection = connection_manager.connection().unwrap();
-
-        let repository = InvoiceQueryRepository::new(&connection);
-        let invoice = repository.find_one_by_id(id.as_str())?;
-        Ok(InvoiceNode::from(invoice))
+        get_invoice(connection_manager, id).into()
     }
 
     pub async fn invoices(
         &self,
-        _ctx: &Context<'_>,
-        #[graphql(desc = "pagination (first and offset)")] page: Option<Pagination>,
+        ctx: &Context<'_>,
+        #[graphql(desc = "pagination (first and offset)")] page: Option<PaginationInput>,
         #[graphql(desc = "filters option")] filter: Option<InvoiceFilterInput>,
         #[graphql(desc = "sort options (only first sort input is evaluated for this endpoint)")]
         sort: Option<Vec<InvoiceSortInput>>,
-    ) -> InvoiceList {
-        InvoiceList {
-            pagination: page,
-            filter,
-            sort,
-        }
-    }
-
-    pub async fn store(
-        &self,
-        ctx: &Context<'_>,
-        #[graphql(desc = "id of the store")] id: String,
-    ) -> Store {
+    ) -> InvoicesResponse {
         let connection_manager = ctx.get_repository::<StorageConnectionManager>();
-        let connection = connection_manager.connection().unwrap();
-
-        let store_repository = StoreRepository::new(&connection);
-        let store_row: StoreRow = store_repository
-            .find_one_by_id(&id)
-            .unwrap_or_else(|_| panic!("Failed to get store {}", id));
-
-        Store { store_row }
-    }
-
-    pub async fn invoice_line(
-        &self,
-        ctx: &Context<'_>,
-        #[graphql(desc = "id of the invoice line")] id: String,
-    ) -> InvoiceLine {
-        let connection_manager = ctx.get_repository::<StorageConnectionManager>();
-        let connection = connection_manager.connection().unwrap();
-        let invoice_line_repository = InvoiceLineRepository::new(&connection);
-
-        let invoice_line_row: InvoiceLineRow = invoice_line_repository
-            .find_one_by_id(&id)
-            .unwrap_or_else(|_| panic!("Failed to get invoice line {}", id));
-
-        InvoiceLine { invoice_line_row }
-    }
-
-    pub async fn requisition(
-        &self,
-        ctx: &Context<'_>,
-        #[graphql(desc = "id of the requisition")] id: String,
-    ) -> Requisition {
-        let connection_manager = ctx.get_repository::<StorageConnectionManager>();
-        let connection = connection_manager.connection().unwrap();
-        let requisition_repository = RequisitionRepository::new(&connection);
-
-        let requisition_row: RequisitionRow = requisition_repository
-            .find_one_by_id(&id)
-            .unwrap_or_else(|_| panic!("Failed to get requisition {}", id));
-
-        Requisition { requisition_row }
+        get_invoices(
+            connection_manager,
+            page.map(PaginationOption::from),
+            filter.map(InvoiceFilter::from),
+            convert_sort(sort),
+        )
+        .into()
     }
 }
