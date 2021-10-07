1c0ffee31b6fab5893f7414293d48d3b518edac1
Clemens Zeidler <clemens@msupply.foundation>
2021-10-08 12:08:29 +1300

    Refactor existing tests to use new gql helpers

--- server/src/database/mock/mod.rs
+++ server/src/database/mock/mod.rs
@@ -1,19 +1,19 @@
+mod invoice;
+mod invoice_line;
 mod item;
-mod stock_line;
 mod name;
 mod requisition;
 mod requisition_line;
+mod stock_line;
 mod store;
-mod invoice;
-mod invoice_line;
 mod user_account;
 
+pub use invoice::{mock_customer_invoices, mock_invoices};
+pub use invoice_line::mock_invoice_lines;
 pub use item::mock_items;
-pub use stock_line::mock_stock_lines;
 pub use name::mock_names;
 pub use requisition::mock_requisitions;
 pub use requisition_line::mock_requisition_lines;
+pub use stock_line::mock_stock_lines;
 pub use store::mock_stores;
-pub use invoice::mock_invoices;
-pub use invoice_line::mock_invoice_lines;
 pub use user_account::mock_user_accounts;
