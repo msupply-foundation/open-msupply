5e59c99ff394d2af98ae4716350db642439d68b3
andreievg <andrei.evguenov@gmail.com>
2021-10-09 02:12:42 +1300

    use domain in gql type
    Source: https://github.com/msupply-foundation/remote-server/commit/ef6b40b05aaf2068e3c429db1a9f01d4cdd596c9

--- server/src/server/service/graphql/schema/types/invoice_line.rs
+++ server/src/server/service/graphql/schema/types/invoice_line.rs
@@ -1,74 +1,90 @@
+use super::{Connector, ConnectorError};
+use crate::domain::invoice_line::{InvoiceLine, StockLine};
 use async_graphql::*;
 use chrono::NaiveDate;
-use dataloader::DataLoader;
 
-use crate::{
-    database::{loader::InvoiceLineQueryLoader, repository::InvoiceLineQueryJoin},
-    server::service::graphql::ContextExt,
-};
-
-#[derive(PartialEq, Debug)]
-struct InvoiceLines {
-    invoice_id: String,
+pub struct InvoiceLineNode {
+    invoice_line: InvoiceLine,
 }
 
 #[Object]
-impl InvoiceLines {
-    async fn nodes(&self, ctx: &Context<'_>) -> Vec<InvoiceLineNode> {
-        let loader = ctx.get_loader::<DataLoader<InvoiceLineQueryLoader>>();
-
-        let lines = loader
-            .load_one(self.invoice_id.to_string())
-            .await
-            // TODO handle error:
-            .unwrap()
-            .map_or(Vec::new(), |v| v);
-        lines.into_iter().map(InvoiceLineNode::from).collect()
+impl InvoiceLineNode {
+    pub async fn id(&self) -> &str {
+        &self.invoice_line.id
+    }
+    pub async fn item_id(&self) -> &str {
+        &self.invoice_line.item_id
+    }
+    pub async fn item_name(&self) -> &str {
+        &self.invoice_line.item_name
+    }
+    pub async fn item_code(&self) -> &str {
+        &self.invoice_line.item_code
+    }
+    pub async fn pack_size(&self) -> i32 {
+        self.invoice_line.pack_size
+    }
+    pub async fn number_of_packs(&self) -> i32 {
+        self.invoice_line.number_of_packs
+    }
+    pub async fn cost_price_per_pack(&self) -> f64 {
+        self.invoice_line.cost_price_per_pack
+    }
+    pub async fn sell_price_per_pack(&self) -> f64 {
+        self.invoice_line.sell_price_per_pack
+    }
+    pub async fn batch(&self) -> &Option<String> {
+        &self.invoice_line.batch
+    }
+    pub async fn expiry_date(&self) -> &Option<NaiveDate> {
+        &self.invoice_line.expiry_date
+    }
+    pub async fn stock_line(&self) -> StockLineNode {
+        self.invoice_line.stock_line.clone().into()
     }
 }
 
-#[derive(SimpleObject, PartialEq, Debug)]
-#[graphql(name = "InvoiceQueryLineNode")]
-pub struct InvoiceLineNode {
-    id: String,
-    item_id: String,
-    item_name: String,
-    item_code: String,
-    pack_size: i32,
-    number_of_packs: i32,
-    cost_price_per_pack: f64,
-    sell_price_per_pack: f64,
-    batch: Option<String>,
-    expiry_date: Option<NaiveDate>,
+pub struct StockLineNode {
     stock_line: StockLine,
 }
 
-impl From<InvoiceLineQueryJoin> for InvoiceLineNode {
-    fn from((invoice_line, item, stock_line): InvoiceLineQueryJoin) -> Self {
-        // TODO: is that correct:
-        let invoice_number_of_packs = invoice_line.available_number_of_packs;
-        InvoiceLineNode {
-            id: invoice_line.id,
-            item_id: item.id,
-            item_name: item.name,
-            item_code: item.code,
-            pack_size: invoice_line.pack_size,
-            number_of_packs: invoice_number_of_packs,
-            cost_price_per_pack: invoice_line.cost_price_per_pack,
-            sell_price_per_pack: invoice_line.sell_price_per_pack,
-            batch: invoice_line.batch,
-            expiry_date: invoice_line.expiry_date,
-            // TODO resolve stock_line on demand:
-            stock_line: StockLine {
-                available_number_of_packs: stock_line.available_number_of_packs
-                    + invoice_number_of_packs,
-            },
+#[Object]
+impl StockLineNode {
+    pub async fn available_number_of_packs(&self) -> i32 {
+        self.stock_line.available_number_of_packs
+    }
+}
+
+type CurrentConnector = Connector<InvoiceLineNode>;
+
+#[derive(Union)]
+pub enum InvoiceLinesResponse {
+    Error(ConnectorError),
+    Response(CurrentConnector),
+}
+
+impl<T, E> From<Result<T, E>> for InvoiceLinesResponse
+where
+    CurrentConnector: From<T>,
+    ConnectorError: From<E>,
+{
+    fn from(result: Result<T, E>) -> Self {
+        match result {
+            Ok(response) => InvoiceLinesResponse::Response(response.into()),
+            Err(error) => InvoiceLinesResponse::Error(error.into()),
         }
     }
 }
 
-#[derive(SimpleObject, PartialEq, Debug)]
-pub struct StockLine {
+impl From<InvoiceLine> for InvoiceLineNode {
     /// number of pack available for a batch ("includes" numberOfPacks in this line)
-    available_number_of_packs: i32,
+    fn from(invoice_line: InvoiceLine) -> Self {
+        InvoiceLineNode { invoice_line }
+    }
+}
+
+impl From<StockLine> for StockLineNode {
+    fn from(stock_line: StockLine) -> Self {
+        StockLineNode { stock_line }
+    }
 }
