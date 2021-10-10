af6f67995b1470f6360897e6594efe9bf7843e2f
andreievg <andrei.evguenov@gmail.com>
2021-10-11 03:38:55 +1300

    add mutation as service
    Source: https://github.com/msupply-foundation/remote-server/commit/1ab4aa3919103421e668f0d7de07803f7cdc5fcc

---
+++ server/src/service/invoice/supplier_invoice/insert/mod.rs
@@ -0,0 +1,36 @@
+use crate::{
+    database::repository::{InvoiceRepository, RepositoryError, StorageConnectionManager},
+    domain::{name::Name, supplier_invoice::InsertSupplierInvoice},
+};
+
+pub mod generate;
+pub mod validate;
+
+use generate::generate;
+use validate::validate;
+
+pub fn insert_supplier_invoice(
+    connection_manager: &StorageConnectionManager,
+    input: InsertSupplierInvoice,
+) -> Result<String, InsertSupplierInvoiceError> {
+    let connection = connection_manager.connection()?;
+    // TODO do inside transaction
+    validate(&input, &connection)?;
+    let new_invoice = generate(input, &connection)?;
+    InvoiceRepository::new(&connection).insert_one(&new_invoice)?;
+
+    Ok(new_invoice.id)
+}
+
+pub enum InsertSupplierInvoiceError {
+    InvoiceAlreadyExists,
+    DatabaseError(RepositoryError),
+    OtherPartyDoesNotExists,
+    OtherPartyNotASupplier(Name),
+}
+
+impl From<RepositoryError> for InsertSupplierInvoiceError {
+    fn from(error: RepositoryError) -> Self {
+        InsertSupplierInvoiceError::DatabaseError(error)
+    }
+}
