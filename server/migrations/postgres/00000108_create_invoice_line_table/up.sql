f6290ae225aca1bf7e503df4e5a252bc088a1381
andreievg <andrei.evguenov@gmail.com>
2021-10-12 12:06:51 +1300

    revert extra space in sql files

--- server/migrations/postgres/00000108_create_invoice_line_table/up.sql
+++ server/migrations/postgres/00000108_create_invoice_line_table/up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE invoice_line (
     id TEXT NOT NULL PRIMARY KEY,
-    invoice_id TEXT NOT NULL REFERENCES invoice (id),
-    item_id TEXT NOT NULL REFERENCES item (id),
+    invoice_id TEXT NOT NULL REFERENCES invoice(id),
+    item_id TEXT NOT NULL REFERENCES item(id),
     item_name TEXT NOT NULL,
     item_code TEXT NOT NULL,
-    stock_line_id TEXT REFERENCES stock_line (id),
+    stock_line_id TEXT REFERENCES stock_line(id),
     batch TEXT,
     expiry_date DATE,
     cost_price_per_pack DOUBLE PRECISION NOT NULL,
