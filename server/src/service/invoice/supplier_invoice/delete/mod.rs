353d33d16061bf210217113e1ea097799379de2c
andreievg <andrei.evguenov@gmail.com>
2021-10-11 14:34:52 +1300

    add delete service
    Source: https://github.com/msupply-foundation/remote-server/commit/e00a7e6c45b7fe98faab4cde9c63e8846b1f8ee0

---
+++ server/src/service/invoice/supplier_invoice/delete/mod.rs
@@ -0,0 +1,36 @@
+use crate::{
+    database::repository::{InvoiceRepository, RepositoryError, StorageConnectionManager},
+    domain::invoice_line::InvoiceLine,
+};
+
+pub mod validate;
+
+use validate::validate;
+
+pub fn delete_supplier_invoice(
+    connection_manager: &StorageConnectionManager,
+    id: String,
+) -> Result<String, DeleteSupplierInvoiceError> {
+    let connection = connection_manager.connection()?;
+    // TODO do inside transaction
+    validate(&id, &connection)?;
+
+    InvoiceRepository::new(&connection).delete(&id)?;
+
+    Ok(id)
+}
+
+pub enum DeleteSupplierInvoiceError {
+    InvoiceDoesNotExists,
+    DatabaseError(RepositoryError),
+    NotASupplierInvoice,
+    NotThisStoreInvoice,
+    CannotEditFinalised,
+    InvoiceLinesExists(Vec<InvoiceLine>),
+}
+
+impl From<RepositoryError> for DeleteSupplierInvoiceError {
+    fn from(error: RepositoryError) -> Self {
+        DeleteSupplierInvoiceError::DatabaseError(error)
+    }
+}
