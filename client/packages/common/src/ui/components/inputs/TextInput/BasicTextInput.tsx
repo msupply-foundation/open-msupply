e013ec33c183eac5c6998cfd24a89906495ddd0a
Joshua Griffin <griffinjoshua5@gmail.com>
2021-10-12 13:52:54 +1300

    Add BasicTextInput component

---
+++ client/packages/common/src/ui/components/inputs/TextInput/BasicTextInput.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import { StandardTextFieldProps, TextField } from '@mui/material';
+
+/**
+ * Very basic TextInput component with some simple styling applied where you can
+ * build your input on top.
+ */
+
+export const BasicTextInput: FC<StandardTextFieldProps> = ({
+  sx,
+  InputProps,
+  ...props
+}) => {
+  return (
+    <TextField
+      sx={{
+        backgroundColor: theme => theme.palette.background.menu,
+        borderRadius: '8px',
+        padding: '4px 8px',
+        '& .MuiInput-underline:before': { borderBottomWidth: 0 },
+        '& .MuiInput-input': { color: theme => theme.palette.darkGrey },
+        ...sx,
+      }}
+      size="small"
+      variant="standard"
+      InputProps={{
+        disableUnderline: true,
+        ...InputProps,
+      }}
+      {...props}
+    />
+  );
+};
