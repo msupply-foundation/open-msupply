4c1f3f625a8f05ab8438460f64a80688a2d7a538
andreievg <andrei.evguenov@gmail.com>
2021-10-13 00:04:33 +1300

    add domain input for update supplier invoice line

--- server/src/domain/supplier_invoice.rs
+++ server/src/domain/supplier_invoice.rs
@@ -29,3 +29,15 @@ pub struct InsertSupplierInvoiceLine {
     pub expiry_date: Option<NaiveDate>,
     pub number_of_packs: u32,
 }
+
+pub struct UpdateSupplierInvoiceLine {
+    pub id: String,
+    pub invoice_id: String,
+    pub item_id: Option<String>,
+    pub pack_size: Option<u32>,
+    pub batch: Option<String>,
+    pub cost_price_per_pack: Option<f64>,
+    pub sell_price_per_pack: Option<f64>,
+    pub expiry_date: Option<NaiveDate>,
+    pub number_of_packs: Option<u32>,
+}
