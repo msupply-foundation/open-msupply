efb78195120bf4d2e6208ae4d78223a6151ca1d6
andreievg <andrei.evguenov@gmail.com>
2021-10-12 19:55:38 +1300

    add invoice line query service
    Source: https://github.com/msupply-foundation/remote-server/commit/e399b97a41bae57e46b0dfa43d898bed0af17941

---
+++ server/src/service/invoice_line/mod.rs
@@ -0,0 +1,5 @@
+pub mod query;
+pub use self::query::*;
+
+pub mod supplier_invoice_line;
+pub use self::supplier_invoice_line::*;
---
+++ server/src/service/invoice_line/supplier_invoice_line/insert/validate.rs
@@ -0,0 +1,75 @@
+use crate::{
+    database::{
+        repository::{InvoiceLineRepository, RepositoryError, StorageConnection},
+        schema::{InvoiceRow, ItemRow},
+    },
+    domain::supplier_invoice::InsertSupplierInvoiceLine,
+    service::{
+        invoice::{
+            check_invoice_exists, check_invoice_finalised, check_invoice_type,
+            CommonError as CommonInvoiceError,
+        },
+        invoice_line::{
+            supplier_invoice_line::{check_item, check_number_of_packs, check_pack_size},
+            CommonError as CommonLineError,
+        },
+    },
+};
+
+use super::InsertSupplierInvoiceLineError;
+
+pub fn validate(
+    input: &InsertSupplierInvoiceLine,
+    connection: &StorageConnection,
+) -> Result<(ItemRow, InvoiceRow), InsertSupplierInvoiceLineError> {
+    check_line_does_not_exists(&input.id, connection)?;
+    check_pack_size(Some(input.pack_size))?;
+    check_number_of_packs(Some(input.number_of_packs))?;
+    let item = check_item(&input.item_id, connection)?;
+
+    let invoice = check_invoice_exists(&input.invoice_id, connection)?;
+    // check_store(invoice, connection)?; InvoiceDoesNotBelongToCurrentStore
+    check_invoice_type(&invoice)?;
+    check_invoice_finalised(&invoice)?;
+
+    Ok((item, invoice))
+}
+
+fn check_line_does_not_exists(
+    id: &str,
+    connection: &StorageConnection,
+) -> Result<(), InsertSupplierInvoiceLineError> {
+    let result = InvoiceLineRepository::new(connection).find_one_by_id(id);
+
+    if let Err(RepositoryError::NotFound) = &result {
+        Ok(())
+    } else if let Err(error) = result {
+        Err(error.into())
+    } else {
+        Err(InsertSupplierInvoiceLineError::LineAlreadyExists)
+    }
+}
+
+impl From<CommonLineError> for InsertSupplierInvoiceLineError {
+    fn from(error: CommonLineError) -> Self {
+        use InsertSupplierInvoiceLineError::*;
+        match error {
+            CommonLineError::PackSizeBelowOne => PackSizeBelowOne,
+            CommonLineError::NumberOfPacksBelowOne => NumberOfPacksBelowOne,
+            CommonLineError::ItemNotFound => ItemNotFound,
+            CommonLineError::DatabaseError(error) => DatabaseError(error),
+        }
+    }
+}
+
+impl From<CommonInvoiceError> for InsertSupplierInvoiceLineError {
+    fn from(error: CommonInvoiceError) -> Self {
+        use InsertSupplierInvoiceLineError::*;
+        match error {
+            CommonInvoiceError::InvoiceDoesNotExists => InvoiceDoesNotExist,
+            CommonInvoiceError::DatabaseError(error) => DatabaseError(error),
+            CommonInvoiceError::InvoiceIsFinalised => CannotEditFinalised,
+            CommonInvoiceError::NotASupplierInvoice => NotASupplierInvoice,
+        }
+    }
+}
