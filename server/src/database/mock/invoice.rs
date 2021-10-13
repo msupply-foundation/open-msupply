67517a605259b1de2142954475877410e91c3d83
andreievg <andrei.evguenov@gmail.com>
2021-10-14 04:29:03 +1300

    add more mocks to helpers
    Source: https://github.com/msupply-foundation/remote-server/commit/c94616ff6644c1e5553111548dce38b35fe448c6

--- server/src/database/mock/invoice.rs
+++ server/src/database/mock/invoice.rs
@@ -23,7 +23,7 @@ pub fn mock_customer_invoice_b() -> InvoiceRow {
         id: String::from("customer_invoice_b"),
         name_id: String::from("name_store_a"),
         store_id: String::from("store_c"),
-        invoice_number: 3,
+        invoice_number: 2,
         r#type: InvoiceRowType::CustomerInvoice,
         status: InvoiceRowStatus::Finalised,
         comment: Some(String::from("")),
@@ -55,7 +55,7 @@ pub fn mock_supplier_invoice_a() -> InvoiceRow {
         id: String::from("supplier_invoice_a"),
         name_id: String::from("name_store_b"),
         store_id: String::from("store_a"),
-        invoice_number: 2,
+        invoice_number: 4,
         r#type: InvoiceRowType::SupplierInvoice,
         status: InvoiceRowStatus::Confirmed,
         comment: Some(String::from("")),
@@ -71,7 +71,7 @@ pub fn mock_supplier_invoice_b() -> InvoiceRow {
         id: String::from("supplier_invoice_b"),
         name_id: String::from("name_store_c"),
         store_id: String::from("store_a"),
-        invoice_number: 4,
+        invoice_number: 5,
         r#type: InvoiceRowType::SupplierInvoice,
         status: InvoiceRowStatus::Finalised,
         comment: Some(String::from("")),
@@ -87,7 +87,23 @@ pub fn mock_supplier_invoice_c() -> InvoiceRow {
         id: String::from("supplier_invoice_c"),
         name_id: String::from("name_store_c"),
         store_id: String::from("store_a"),
-        invoice_number: 4,
+        invoice_number: 6,
+        r#type: InvoiceRowType::SupplierInvoice,
+        status: InvoiceRowStatus::Draft,
+        comment: Some(String::from("")),
+        their_reference: Some(String::from("")),
+        entry_datetime: NaiveDate::from_ymd(1970, 1, 4).and_hms_milli(21, 30, 0, 0),
+        confirm_datetime: None,
+        finalised_datetime: None,
+    }
+}
+
+pub fn mock_empty_draft_supplier_invoice() -> InvoiceRow {
+    InvoiceRow {
+        id: String::from("empty_draft_supplier_invoice"),
+        name_id: String::from("name_store_c"),
+        store_id: String::from("store_a"),
+        invoice_number: 7,
         r#type: InvoiceRowType::SupplierInvoice,
         status: InvoiceRowStatus::Draft,
         comment: Some(String::from("")),
@@ -111,6 +127,7 @@ pub fn mock_supplier_invoices() -> Vec<InvoiceRow> {
         mock_supplier_invoice_a(),
         mock_supplier_invoice_b(),
         mock_supplier_invoice_c(),
+        mock_empty_draft_supplier_invoice(),
     ]
 }
 
