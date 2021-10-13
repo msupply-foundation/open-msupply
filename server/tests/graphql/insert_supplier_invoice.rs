7aee126bc152a4596abdbc6fc2610dd0447a2bee
andreievg <andrei.evguenov@gmail.com>
2021-10-14 01:00:39 +1300

    insert supplier invoice to use graphql_client
    Source: https://github.com/msupply-foundation/remote-server/commit/35500d6a3b08601438c9195de449f156046e43ea

--- server/tests/graphql/insert_supplier_invoice.rs
+++ server/tests/graphql/insert_supplier_invoice.rs
@@ -1,168 +1,157 @@
 mod graphql {
-    use std::{fs, path::Path};
-
-    use crate::graphql::assert_gql_query;
+    use crate::graphql::get_gql_result;
     use chrono::{Duration, Utc};
+    use graphql_client::{GraphQLQuery, Response};
     use remote_server::{
         database::{
-            mock::{mock_items, mock_name_store_joins, mock_names, mock_stores},
-            repository::{
-                InvoiceQueryRepository, ItemRepository, NameRepository, NameStoreJoinRepository,
-                StoreRepository,
-            },
-            schema::{ItemRow, NameRow, NameStoreJoinRow, StoreRow},
+            mock::MockDataInserts,
+            repository::{InvoiceQueryRepository, NameQueryRepository},
         },
-        domain::{invoice::InvoiceFilter, DatetimeFilter, Pagination},
-        server::service::graphql::schema::types::{InvoiceNodeStatus, InvoiceNodeType},
+        domain::{
+            invoice::InvoiceFilter,
+            name::{Name, NameFilter},
+            DatetimeFilter, Pagination,
+        },
         util::test_db,
     };
-    use serde::Serialize;
-    use serde_json::json;
+    use uuid::Uuid;
 
-    #[derive(Serialize)]
-    #[serde(rename_all = "camelCase")]
-    struct NameNode {
-        id: String,
-        name: String,
-        code: String,
-        is_customer: bool,
-        is_supplier: bool,
-    }
+    use crate::graphql::{
+        insert_supplier_invoice_full as full, insert_supplier_invoice_partial as partial,
+        InsertSupplierInvoiceFull as Full, InsertSupplierInvoicePartial as Partial,
+    };
 
-    impl From<(&NameStoreJoinRow, &NameRow)> for NameNode {
-        fn from((nj, n): (&NameStoreJoinRow, &NameRow)) -> Self {
-            NameNode {
-                id: n.id.to_string(),
-                name: n.name.to_string(),
-                code: n.code.to_string(),
-                is_customer: nj.name_is_customer,
-                is_supplier: nj.name_is_supplier,
+    impl From<Name> for full::NameNode {
+        fn from(n: Name) -> Self {
+            full::NameNode {
+                code: n.code,
+                id: n.id,
+                is_customer: n.is_customer,
+                is_supplier: n.is_supplier,
+                name: n.name,
             }
         }
     }
 
     #[actix_rt::test]
     async fn test_insert_supplier_invoice() {
-        let (settings, connection) = test_db::setup_all("test_insert_supplier_invoice_query").await;
+        let (_, connection, settings) =
+            test_db::setup_all("test_insert_supplier_invoice_query", MockDataInserts::all()).await;
 
-        let name_repository = NameRepository::new(&connection);
-        let store_repository = StoreRepository::new(&connection);
-        let name_store_repository = NameStoreJoinRepository::new(&connection);
-
-        let mock_names: Vec<NameRow> = mock_names();
-        let mock_stores: Vec<StoreRow> = mock_stores();
-        let mock_items: Vec<ItemRow> = mock_items();
-        let mock_name_store_joins: Vec<NameStoreJoinRow> = mock_name_store_joins();
-        let item_repository = ItemRepository::new(&connection);
-
-        for name in &mock_names {
-            name_repository.insert_one(&name).await.unwrap();
-        }
-
-        for store in &mock_stores {
-            store_repository.insert_one(&store).await.unwrap();
-        }
-
-        for name_store_join in &mock_name_store_joins {
-            name_store_repository.upsert_one(name_store_join).unwrap();
-        }
-
-        for item in mock_items {
-            item_repository.insert_one(&item).await.unwrap();
-        }
-
-        let query = fs::read_to_string(Path::new(
-            "tests/graphql/queries/insert_supplier_invoice_full.graphql",
-        ))
-        .unwrap();
-        // Test ForeingKeyError
-        let variables = Some(json!({
-          "id": "new_invoice",
-          "otherPartyId": "invalid",
-          "status": InvoiceNodeStatus::Draft,
-        }));
-        let expected = json!( {
-            "insertSupplierInvoice": {
-              "__typename": "InsertSupplierInvoiceError",
-              "error": {
-                "__typename": "ForeignKeyError",
-                "description": "FK record doesn't exist",
-                "key": "OTHER_PARTY_ID"
-              }
-            }
-        });
-        assert_gql_query(&settings, &query, &variables, &expected).await;
-
-        // Test OtherPartyNotASupplier
-        let not_supplier_name_join = mock_name_store_joins
-            .iter()
-            .find(|nsj| !nsj.name_is_supplier)
-            .unwrap();
-        let not_supplier_name = mock_names
-            .iter()
-            .find(|n| n.id == not_supplier_name_join.name_id)
-            .unwrap();
-        let variables = Some(json!({
-          "id": "new_invoice",
-          "otherPartyId": not_supplier_name_join.name_id,
-          "status": InvoiceNodeStatus::Draft,
-        }));
-
-        let expected = json!( {
-          "insertSupplierInvoice": {
-            "__typename": "InsertSupplierInvoiceError",
-            "error": {
-              "__typename": "OtherPartyNotASupplier",
-              "description": "Other party name is not a supplier",
-              "otherParty": NameNode::from((not_supplier_name_join, not_supplier_name))
-            }
-          }
-        });
-
-        assert_gql_query(&settings, &query, &variables, &expected).await;
-        // Test Success
+        // Setup
         let start = Utc::now().naive_utc();
         let end = Utc::now()
             .naive_utc()
             .checked_add_signed(Duration::seconds(5))
             .unwrap();
-        let invoice_id = "new_invoice";
-
-        let supplier_name_join = mock_name_store_joins
-            .iter()
-            .find(|nsj| nsj.name_is_supplier)
+        let id1 = Uuid::new_v4().to_string();
+        let id2 = Uuid::new_v4().to_string();
+        let id3 = Uuid::new_v4().to_string();
+        let id4 = Uuid::new_v4().to_string();
+        let id5 = Uuid::new_v4().to_string();
+        let comment = "some comment";
+        let their_reference = "some reference";
+        let not_supplier = NameQueryRepository::new(&connection)
+            .query(
+                Pagination::one(),
+                Some(NameFilter::new().match_is_supplier(false)),
+                None,
+            )
+            .unwrap()
+            .pop()
             .unwrap();
-        let supplier_name = mock_names
-            .iter()
-            .find(|n| n.id == supplier_name_join.name_id)
+        let supplier = NameQueryRepository::new(&connection)
+            .query(
+                Pagination::one(),
+                Some(NameFilter::new().match_is_supplier(true)),
+                None,
+            )
+            .unwrap()
+            .pop()
             .unwrap();
 
-        let variables = Some(json!({
-          "id": invoice_id,
-          "otherPartyId": supplier_name_join.name_id,
-          "status": InvoiceNodeStatus::Draft,
-        }));
+        // Test ForeingKeyError
 
-        let expected = json!( {
-          "insertSupplierInvoice": {
-            "id": invoice_id,
-            "status": InvoiceNodeStatus::Draft,
-            "type": InvoiceNodeType::SupplierInvoice
-          }
+        let current_id = id1;
+
+        let query = Full::build_query(full::Variables {
+            id: current_id,
+            other_party_id: "invalid".to_string(),
+            status: full::InvoiceNodeStatus::Draft,
+            comment: None,
+            their_reference: None,
         });
+        let response: Response<full::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = full::ResponseData {
+            insert_supplier_invoice:
+                full::InsertSupplierInvoiceResponse::InsertSupplierInvoiceError(
+                    full::InsertSupplierInvoiceError {
+                        error: full::InsertSupplierInvoiceErrorInterface::ForeignKeyError(
+                            full::ForeignKeyError {
+                                description: "FK record doesn't exist".to_string(),
+                                key: full::ForeignKey::OtherPartyId,
+                            },
+                        ),
+                    },
+                ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
 
-        let query = fs::read_to_string(Path::new(
-            "tests/graphql/queries/insert_supplier_invoice_partial.graphql",
-        ))
-        .unwrap();
-        assert_gql_query(&settings, &query, &variables, &expected).await;
+        // Test OtherPartyNotASupplier
 
+        let current_id = id2;
+
+        let query = Full::build_query(full::Variables {
+            id: current_id,
+            other_party_id: not_supplier.id.clone(),
+            status: full::InvoiceNodeStatus::Draft,
+            comment: None,
+            their_reference: None,
+        });
+        let response: Response<full::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = full::ResponseData {
+            insert_supplier_invoice:
+                full::InsertSupplierInvoiceResponse::InsertSupplierInvoiceError(
+                    full::InsertSupplierInvoiceError {
+                        error: full::InsertSupplierInvoiceErrorInterface::OtherPartyNotASupplier(
+                            full::OtherPartyNotASupplier {
+                                description: "Other party name is not a supplier".to_string(),
+                                other_party: not_supplier.into(),
+                            },
+                        ),
+                    },
+                ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
+
+        // Test Success
+
+        let current_id = id3.clone();
+
+        let query = Partial::build_query(partial::Variables {
+            id: current_id.clone(),
+            other_party_id: supplier.id.clone(),
+            status: partial::InvoiceNodeStatus::Draft,
+            comment: None,
+            their_reference: None,
+        });
+        let response: Response<partial::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = partial::ResponseData {
+            insert_supplier_invoice: partial::InsertSupplierInvoiceResponsePartial::InvoiceNode(
+                partial::PartialInvoiceNode {
+                    id: current_id.clone(),
+                    status: partial::InvoiceNodeStatus::Draft,
+                    type_: partial::InvoiceNodeType::SupplierInvoice,
+                },
+            ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
         let invoice = InvoiceQueryRepository::new(&connection)
             .query(
                 Pagination::one(),
                 Some(
                     InvoiceFilter::new()
-                        .match_id(invoice_id)
+                        .match_id(&current_id)
                         .set_entry_datetime(DatetimeFilter::date_range(start, end)),
                 ),
                 None,
@@ -170,62 +159,63 @@ mod graphql {
             .unwrap()
             .pop()
             .unwrap();
-
-        assert_eq!(invoice.id, invoice_id);
-        assert_eq!(invoice.other_party_name, supplier_name.name);
+        assert_eq!(invoice.id, current_id);
+        assert_eq!(invoice.other_party_name, supplier.name);
         assert_eq!(invoice.confirm_datetime, None);
         assert_eq!(invoice.finalised_datetime, None);
 
         // Test RecordAlreadyExist
-        let expected = json!( {
-          "insertSupplierInvoice": {
-            "__typename": "InsertSupplierInvoiceError",
-            "error": {
-              "__typename": "RecordAlreadyExist",
-              "description": "Record already exists",
-            }
-          }
+
+        let duplicated_id = id3;
+        let query = Full::build_query(full::Variables {
+            id: duplicated_id,
+            other_party_id: supplier.id.clone(),
+            status: full::InvoiceNodeStatus::Draft,
+            comment: None,
+            their_reference: None,
         });
-        let query = fs::read_to_string(Path::new(
-            "tests/graphql/queries/insert_supplier_invoice_full.graphql",
-        ))
-        .unwrap();
-        assert_gql_query(&settings, &query, &variables, &expected).await;
+        let response: Response<full::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = full::ResponseData {
+            insert_supplier_invoice:
+                full::InsertSupplierInvoiceResponse::InsertSupplierInvoiceError(
+                    full::InsertSupplierInvoiceError {
+                        error: full::InsertSupplierInvoiceErrorInterface::RecordAlreadyExist(
+                            full::RecordAlreadyExist {
+                                description: "Record already exists".to_string(),
+                            },
+                        ),
+                    },
+                ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
 
         // Test Confirmed
-        let start = Utc::now().naive_utc();
-        let end = Utc::now()
-            .naive_utc()
-            .checked_add_signed(Duration::seconds(5))
-            .unwrap();
-        let invoice_id = "new_invoice2";
 
-        let variables = Some(json!({
-          "id": invoice_id,
-          "otherPartyId": supplier_name_join.name_id,
-          "status": InvoiceNodeStatus::Confirmed,
-        }));
-
-        let expected = json!( {
-          "insertSupplierInvoice": {
-            "id": invoice_id,
-            "status": InvoiceNodeStatus::Confirmed,
-            "type": InvoiceNodeType::SupplierInvoice
-          }
+        let current_id = id4;
+        let query = Partial::build_query(partial::Variables {
+            id: current_id.clone(),
+            other_party_id: supplier.id.clone(),
+            status: partial::InvoiceNodeStatus::Confirmed,
+            comment: None,
+            their_reference: None,
         });
-
-        let query = fs::read_to_string(Path::new(
-            "tests/graphql/queries/insert_supplier_invoice_partial.graphql",
-        ))
-        .unwrap();
-        assert_gql_query(&settings, &query, &variables, &expected).await;
-
+        let response: Response<partial::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = partial::ResponseData {
+            insert_supplier_invoice: partial::InsertSupplierInvoiceResponsePartial::InvoiceNode(
+                partial::PartialInvoiceNode {
+                    id: current_id.clone(),
+                    status: partial::InvoiceNodeStatus::Confirmed,
+                    type_: partial::InvoiceNodeType::SupplierInvoice,
+                },
+            ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
         let invoice = InvoiceQueryRepository::new(&connection)
             .query(
                 Pagination::one(),
                 Some(
                     InvoiceFilter::new()
-                        .match_id(invoice_id)
+                        .match_id(&current_id)
                         .set_entry_datetime(DatetimeFilter::date_range(start, end))
                         .set_confirm_datetime(DatetimeFilter::date_range(start, end)),
                 ),
@@ -235,44 +225,38 @@ mod graphql {
             .pop()
             .unwrap();
 
-        assert_eq!(invoice.id, invoice_id);
+        assert_eq!(invoice.id, current_id);
         assert_eq!(invoice.finalised_datetime, None);
         assert_eq!(invoice.comment, None);
         assert_eq!(invoice.their_reference, None);
 
         // Test Finaized, comment and thier_reference
-        let start = Utc::now().naive_utc();
-        let end = Utc::now()
-            .naive_utc()
-            .checked_add_signed(Duration::seconds(5))
-            .unwrap();
-        let invoice_id = "new_invoice3";
-        let comment = "comment";
-        let their_reference = "reference";
-        let variables = Some(json!({
-          "id": invoice_id,
-          "otherPartyId": supplier_name_join.name_id,
-          "status": InvoiceNodeStatus::Finalised,
-          "comment": comment,
-          "theirReference": their_reference,
-        }));
 
-        let expected = json!( {
-          "insertSupplierInvoice": {
-            "id": invoice_id,
-            "status": InvoiceNodeStatus::Finalised,
-            "type": InvoiceNodeType::SupplierInvoice
-          }
+        let current_id = id5;
+        let query = Partial::build_query(partial::Variables {
+            id: current_id.clone(),
+            other_party_id: supplier.id.clone(),
+            status: partial::InvoiceNodeStatus::Finalised,
+            comment: Some(comment.to_string()),
+            their_reference: Some(their_reference.to_string()),
         });
-
-        assert_gql_query(&settings, &query, &variables, &expected).await;
-
+        let response: Response<partial::ResponseData> = get_gql_result(&settings, query).await;
+        let expected = partial::ResponseData {
+            insert_supplier_invoice: partial::InsertSupplierInvoiceResponsePartial::InvoiceNode(
+                partial::PartialInvoiceNode {
+                    id: current_id.clone(),
+                    status: partial::InvoiceNodeStatus::Finalised,
+                    type_: partial::InvoiceNodeType::SupplierInvoice,
+                },
+            ),
+        };
+        assert_eq!(response.data.unwrap(), expected);
         let invoice = InvoiceQueryRepository::new(&connection)
             .query(
                 Pagination::one(),
                 Some(
                     InvoiceFilter::new()
-                        .match_id(invoice_id)
+                        .match_id(&current_id)
                         .set_entry_datetime(DatetimeFilter::date_range(start, end))
                         .set_confirm_datetime(DatetimeFilter::date_range(start, end))
                         .set_finalised_datetime(DatetimeFilter::date_range(start, end)),
@@ -282,8 +266,7 @@ mod graphql {
             .unwrap()
             .pop()
             .unwrap();
-
-        assert_eq!(invoice.id, invoice_id);
+        assert_eq!(invoice.id, current_id);
         assert_eq!(invoice.comment, Some(comment.to_string()));
         assert_eq!(invoice.their_reference, Some(their_reference.to_string()));
     }
