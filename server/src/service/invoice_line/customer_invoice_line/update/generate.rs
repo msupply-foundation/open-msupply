b1d6bf0d74cda76364db38b43facb4841d974ec8
andreievg <andrei.evguenov@gmail.com>
2021-10-15 11:24:55 +1300

    add stock reduction and validation logic to update
    Source: https://github.com/msupply-foundation/remote-server/commit/8e84b933a53e1362c8370365552f39bf687ade7f

--- server/src/service/invoice_line/customer_invoice_line/update/generate.rs
+++ server/src/service/invoice_line/customer_invoice_line/update/generate.rs
@@ -1,23 +1,76 @@
 use crate::{
-    database::schema::{InvoiceLineRow, InvoiceRow, ItemRow, StockLineRow},
+    database::schema::{InvoiceLineRow, InvoiceRow, InvoiceRowStatus, ItemRow, StockLineRow},
     domain::customer_invoice::UpdateCustomerInvoiceLine,
     service::u32_to_i32,
 };
 
-use super::UpdateCustomerInvoiceLineError;
+use super::{BatchPair, UpdateCustomerInvoiceLineError};
 
 pub fn generate(
     input: UpdateCustomerInvoiceLine,
     existing_line: InvoiceLineRow,
     item_row: ItemRow,
-    batch: StockLineRow,
-    _: InvoiceRow,
-) -> Result<InvoiceLineRow, UpdateCustomerInvoiceLineError> {
-    let new_line = generate_line(input, existing_line, item_row, batch);
+    batch_pair: BatchPair,
+    invoice: InvoiceRow,
+) -> Result<(InvoiceLineRow, BatchPair), UpdateCustomerInvoiceLineError> {
+    let adjust_total_number_of_packs = invoice.status == InvoiceRowStatus::Confirmed;
 
-    // Adjust stock line
+    let batch_pair = BatchPair {
+        main_batch: generate_batch_update(
+            &input,
+            &existing_line,
+            &batch_pair,
+            adjust_total_number_of_packs,
+        ),
+        previous_batch_option: generate_previous_batch_update(
+            &existing_line,
+            batch_pair.previous_batch_option,
+            adjust_total_number_of_packs,
+        ),
+    };
 
-    Ok(new_line)
+    let new_line = generate_line(
+        input,
+        existing_line,
+        item_row,
+        batch_pair.main_batch.clone(),
+    );
+
+    Ok((new_line, batch_pair))
+}
+
+fn generate_batch_update(
+    input: &UpdateCustomerInvoiceLine,
+    existing_line: &InvoiceLineRow,
+    batch_pair: &BatchPair,
+    adjust_total_number_of_packs: bool,
+) -> StockLineRow {
+    let mut update_batch = batch_pair.main_batch.clone();
+
+    let reduction = batch_pair.get_main_batch_reduction(input, existing_line);
+
+    update_batch.available_number_of_packs -= reduction;
+    if adjust_total_number_of_packs {
+        update_batch.total_number_of_packs -= reduction;
+    }
+
+    update_batch
+}
+fn generate_previous_batch_update(
+    existing_line: &InvoiceLineRow,
+    previous_batch_option: Option<StockLineRow>,
+    adjust_total_number_of_packs: bool,
+) -> Option<StockLineRow> {
+    // If previous batch is present, this means batch was changes thus:
+    // - release stock of the batch
+    previous_batch_option.map(|mut previous_batch| {
+        let addition = existing_line.number_of_packs;
+        previous_batch.available_number_of_packs += addition;
+        if adjust_total_number_of_packs {
+            previous_batch.total_number_of_packs += addition;
+        }
+        previous_batch
+    })
 }
 
 fn generate_line(
