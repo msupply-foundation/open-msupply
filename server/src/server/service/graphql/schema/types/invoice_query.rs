981519a3557c7d8053a387b96f88cc2466df5258
Clemens Zeidler <clemens@msupply.foundation>
2021-10-14 20:24:58 +1300

    Improve docs
    Source: https://github.com/msupply-foundation/remote-server/commit/998ecbf63543bc5f7df7ef3b4604706f52394332

--- server/src/server/service/graphql/schema/types/invoice_query.rs
+++ server/src/server/service/graphql/schema/types/invoice_query.rs
@@ -70,11 +70,11 @@ pub enum InvoiceNodeType {
 #[graphql(remote = "crate::domain::invoice::InvoiceStatus")]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")] // only needed to be comparable in tests
 pub enum InvoiceNodeStatus {
-    /// In DRAFT mode only the available_number_of_packs in a stock line gets updated when items are
-    /// added to the invoice.
-    Draft,
-    /// When an invoice is CONFIRMED available_number_of_packs and total_number_of_packs gets
+    /// For customer invoices: In DRAFT mode only the available_number_of_packs in a stock line gets
     /// updated when items are added to the invoice.
+    Draft,
+    /// For customer invoices: When an invoice is CONFIRMED available_number_of_packs and
+    /// total_number_of_packs get updated when items are added to the invoice.
     Confirmed,
     /// A FINALISED invoice can't be edited nor deleted.
     Finalised,
