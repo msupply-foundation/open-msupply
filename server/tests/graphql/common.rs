6635dc07835e147d22a499d1c947246993500a47
andreievg <andrei.evguenov@gmail.com>
2021-10-17 21:03:17 +1300

    add common test helpers
    Source: https://github.com/msupply-foundation/remote-server/commit/05c1dcc4a37a34b33024a62ef96925a32f9d43a9

---
+++ server/tests/graphql/common.rs
@@ -0,0 +1,60 @@
+use serde::{de::DeserializeOwned, Serialize};
+
+macro_rules! get_invoice_inline {
+    ($filter:expr, $connection:expr) => {{
+        remote_server::database::repository::InvoiceQueryRepository::new($connection)
+            .query(Pagination::one(), Some($filter), None)
+            .unwrap()
+            .pop()
+            .unwrap()
+    }};
+}
+
+macro_rules! get_invoice_lines_inline {
+    ($invoice_id:expr, $connection:expr) => {{
+        remote_server::database::repository::InvoiceLineRepository::new($connection)
+            .find_many_by_invoice_id($invoice_id)
+            .unwrap()
+    }};
+}
+
+macro_rules! assert_unwrap_enum {
+    ($enum:ident, $variant:path) => {{
+        let debug = format!("{:#?}", $enum);
+
+        if let $variant(result) = $enum {
+            result
+        } else {
+            panic!("\ncannot unwrap {} from\n {}", stringify!($variant), debug);
+        }
+    }};
+}
+
+macro_rules! assert_unwrap_optional_key {
+    ($option:ident, $key:ident) => {{
+        let debug = format!("{:#?}", $option);
+
+        if let Some(result) = $option.$key {
+            result
+        } else {
+            panic!(
+                "\ncannot unwrap Some(value) of key '{}' in\n {}",
+                stringify!($key),
+                debug
+            );
+        }
+    }};
+}
+
+pub fn convert_graphql_client_type<FROM, TO>(f: FROM) -> TO
+where
+    FROM: Serialize,
+    TO: DeserializeOwned,
+{
+    serde_json::from_str(&serde_json::to_string(&f).unwrap()).unwrap()
+}
+
+pub(crate) use assert_unwrap_enum;
+pub(crate) use assert_unwrap_optional_key;
+pub(crate) use get_invoice_inline;
+pub(crate) use get_invoice_lines_inline;
