7d340784c1e07f4c8404a168570f374097fedec6
Joshua Griffin <griffinjoshua5@gmail.com>
2021-10-19 11:56:51 +1300

    Add status handling utils and add to mock server
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/fbf728c956f7aa453420cb37878b5fe691692a1e

---
+++ client/packages/invoices/src/OutboundShipment/utils.ts
@@ -0,0 +1,57 @@
+import { LocaleKey } from '@openmsupply-client/common/src/intl/intlHelpers';
+import { OutboundShipmentStatus } from '@openmsupply-client/common';
+import { OutboundShipment } from './DetailView/types';
+
+const outboundStatuses: OutboundShipmentStatus[] = [
+  'draft',
+  'allocated',
+  'picked',
+  'shipped',
+  'delivered',
+];
+
+const NextStatusButtonTranslation: Record<OutboundShipmentStatus, LocaleKey> = {
+  draft: 'label.draft',
+  allocated: 'label.allocation',
+  picked: 'label.picked',
+  shipped: 'label.shipped',
+  delivered: 'label.delivered',
+};
+
+const StatusTranslation: Record<OutboundShipmentStatus, LocaleKey> = {
+  draft: 'label.draft',
+  allocated: 'label.allocated',
+  picked: 'label.picked',
+  shipped: 'label.shipped',
+  delivered: 'label.delivered',
+};
+
+const getNextOutboundStatus = (
+  currentStatus: OutboundShipmentStatus
+): OutboundShipmentStatus | undefined => {
+  const currentStatusIdx = outboundStatuses.findIndex(
+    status => currentStatus === status
+  );
+
+  return outboundStatuses[currentStatusIdx + 1];
+};
+
+export const getNextOutboundStatusButtonTranslation = (
+  currentStatus: OutboundShipmentStatus
+): LocaleKey | undefined => {
+  const nextStatus = getNextOutboundStatus(currentStatus);
+
+  if (nextStatus) return NextStatusButtonTranslation[nextStatus];
+
+  return undefined;
+};
+
+export const getStatusTranslation = (
+  currentStatus: OutboundShipmentStatus
+): LocaleKey | undefined => {
+  return StatusTranslation[currentStatus];
+};
+
+export const isInvoiceSaveable = (outbound: OutboundShipment): boolean => {
+  return outbound.status !== 'shipped' && outbound.status !== 'delivered';
+};
