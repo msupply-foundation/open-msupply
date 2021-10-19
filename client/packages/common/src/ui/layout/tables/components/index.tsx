ca897c4c8817098993fa29b0f2d8612f5dc25004
Joshua Griffin <griffinjoshua5@gmail.com>
2021-10-19 13:38:11 +1300

    Add passing of translators into formatters
    Unsure about this one, but have made it mandatory when calling formatter() that the translators must also be passed. It makes it a bit  easier to write a quick formatter for your column without having to import more dependencies in. Additionally, it is somewhat rare for formatter() to actually be called. Really, it is only going to be called in quite generic places. If for instance, there is a specific column, then it is most likely that you would have a custom Cell component, in which case you would just do your formatting within the cell itself.

--- client/packages/common/src/ui/layout/tables/components/index.tsx
+++ client/packages/common/src/ui/layout/tables/components/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { CellProps, HeaderProps } from '../columns/types';
 import { DomainObject } from '../../../../types';
-import { useTranslation } from '../../../../intl';
+import { useTranslation, useFormatDate } from '../../../../intl';
 
 export * from './DataRow';
 
@@ -9,7 +9,10 @@ export const BasicCell = <T extends DomainObject>({
   column,
   rowData,
 }: CellProps<T>): ReactElement => {
-  return <>{column.formatter(column.accessor(rowData))}</>;
+  const t = useTranslation();
+  const d = useFormatDate();
+
+  return <>{column.formatter(column.accessor(rowData), { t, d })}</>;
 };
 
 export const BasicHeader = <T extends DomainObject>({
