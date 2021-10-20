ee58dfe92f2827d7df4354d1b5caabfb780ddada
andreievg <andrei.evguenov@gmail.com>
2021-10-21 02:15:18 +1300

    make sure docker logs all statement and remove wait for db start
    Source: https://github.com/msupply-foundation/remote-server/commit/79a9ed24c6318528a27554896434956ca5ec9151

--- server/scripts/init_db.sh
+++ server/scripts/init_db.sh
@@ -14,21 +14,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 fi
 
 $DOCKER_CMD run \
+    --name postgres_docker \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres \
-    postgres -N 1000
-
-# Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-    >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1
-done
-
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
-
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
-sqlx database create
+    postgres -N 1000 -c log_statement=all -c logging_collector=on
