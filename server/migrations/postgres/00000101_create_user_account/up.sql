785ac1f9c6e0fa805c314c878dbc9ede7878a136
Clemens Zeidler <clemens@msupply.foundation>
2021-10-29 08:25:52 +1300

    Add user account service (currently can only create a user)
    Source: https://github.com/msupply-foundation/remote-server/commit/5c0060e06f11cfaf2b24457e6945c7ab274d2d56

--- server/migrations/postgres/00000101_create_user_account/up.sql
+++ server/migrations/postgres/00000101_create_user_account/up.sql
@@ -1,11 +1,9 @@
 -- Create user_account table.
 
-CREATE EXTENSION pgcrypto;
-
 CREATE TABLE user_account (
     id TEXT NOT NULL PRIMARY KEY,
     username TEXT NOT NULL,
-    -- Password is stored as salted MD5 hash.
+    -- Hashed password
     password TEXT NOT NULL,
     email TEXT
 )
