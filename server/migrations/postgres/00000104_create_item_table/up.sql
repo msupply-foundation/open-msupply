d4ca6512e8d3c05021303a2bd3988eb25ccc3801
andreievg <andrei.evguenov@gmail.com>
2021-11-03 22:48:57 +1300

    Add unit table in schemas

--- server/migrations/postgres/00000104_create_item_table/up.sql
+++ server/migrations/postgres/00000104_create_item_table/up.sql
@@ -3,5 +3,6 @@
 CREATE TABLE item (
     id TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
-    code TEXT NOT NULL
+    code TEXT NOT NULL,
+    unit_id TEXT REFERENCES unit(id)
 )
---
+++ server/migrations/sqlite/00000103_create_unit_table/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE unit (
+    id TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    "index" INTEGER NOT NULL
+);
+
