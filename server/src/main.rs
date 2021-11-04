8f4ced29e4aa1e2eb5673293af31089de8e0b423
Clemens Zeidler <clemens@msupply.foundation>
2021-11-04 13:50:58 +1300

    Introduce AuthData to store shared auth data (jwt token and bucket)
    Source: https://github.com/msupply-foundation/remote-server/commit/aca3a51c03bd3114d1d83b111ac27cc46f85f47e

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,10 +4,14 @@ use actix_cors::Cors;
 use remote_server::{
     database::{loader::get_loaders, repository::get_repositories},
     server::{
-        data::{ActorRegistry, LoaderMap, LoaderRegistry, RepositoryMap, RepositoryRegistry},
+        data::{
+            auth::AuthData, ActorRegistry, LoaderMap, LoaderRegistry, RepositoryMap,
+            RepositoryRegistry,
+        },
         middleware::{compress as compress_middleware, logger as logger_middleware},
         service::{graphql::config as graphql_config, rest::config as rest_config},
     },
+    service::token_bucket::TokenBucket,
     util::{
         configuration,
         settings::Settings,
@@ -19,7 +23,7 @@ use actix_web::{web::Data, App, HttpServer};
 use std::{
     env,
     net::TcpListener,
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, RwLock},
     time::Duration,
 };
 
@@ -31,6 +35,12 @@ async fn main() -> std::io::Result<()> {
     let settings: Settings =
         configuration::get_configuration().expect("Failed to parse configuration settings");
 
+    let auth_data = Data::new(AuthData {
+        auth_token_secret: "TODO: get secret from somewhere else, e.g. DB table?".to_string(),
+        token_bucket: RwLock::new(TokenBucket::new()),
+        // TODO: configure ssl
+        debug_no_ssl: true,
+    });
     let repositories: RepositoryMap = get_repositories(&settings).await;
     let loaders: LoaderMap = get_loaders(&settings).await;
     let (mut sync_sender, mut sync_receiver): (SyncSenderActor, SyncReceiverActor) =
@@ -55,6 +65,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(repository_registry_data_app.clone())
             .app_data(actor_registry_data.clone())
+            .app_data(auth_data.clone())
             .wrap(logger_middleware())
             .wrap(Cors::permissive())
             .wrap(compress_middleware())
