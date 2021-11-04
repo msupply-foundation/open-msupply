fd6092e63434d60e0c1494d2102fd4febe0e3c4c
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-05 08:01:50 +1300

    Add provider to host
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/4393c3e3f6e8ce3b45a37e9610727bfdfbaa9da2

--- client/packages/common/src/api/index.ts
+++ client/packages/common/src/api/index.ts
@@ -1,14 +1,1 @@
-import { GraphQLClient } from 'graphql-request';
-import { getSdk } from '..';
-
 export * from './OmSupplyApiContext';
-
-export const createOmSupplyApi = (
-  url: string
-): { api: OmSupplyApi; client: GraphQLClient } => {
-  const client = new GraphQLClient(url);
-  const api = getSdk(client);
-  return { client, api };
-};
-
-export type OmSupplyApi = ReturnType<typeof getSdk>;
