319889a714f31682bc47d70c454264764d8d45bb
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-05 07:32:33 +1300

    Add OmSupply types
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/34c6f1d306f05fcf236fc523b9821737d14cf38d

--- client/packages/common/src/api/index.ts
+++ client/packages/common/src/api/index.ts
@@ -1,1 +1,9 @@
+import { GraphQLClient } from 'graphql-request';
+import { getSdk } from '..';
+
 export * from './context';
+
+export const createOmSupplyApi = (url: string): OmSupplyApi =>
+  getSdk(new GraphQLClient(url));
+
+export type OmSupplyApi = ReturnType<typeof getSdk>;
