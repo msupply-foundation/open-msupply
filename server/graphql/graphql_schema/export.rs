f3647a5717621cebf9bb562504111d34d729e958
Clemens Zeidler <clemens@msupply.foundation>
2021-11-10 07:54:28 +1300

    Fix graphql_export and allow it to run in the root and graphql dir
    Source: https://github.com/msupply-foundation/remote-server/commit/ed35fa088d5cb404f6403e93cc66a2b1203549cd

--- server/graphql/graphql_schema/export.rs
+++ server/graphql/graphql_schema/export.rs
@@ -1,7 +1,21 @@
-use remote_server::server::service::graphql::build_schema;
+use graphql::build_schema;
 use std::fs;
 //  Entry point for export_graphql target, see cargo.toml and reademe.md test section
 fn main() {
+    // Try to detect if we are in the graphql crate or in the root crate and adjust the output
+    // path accordingly.
+    let has_schema_dir = fs::read_dir(".").unwrap().find_map(|entry| {
+        let dir = entry.unwrap();
+        Some(dir.file_type().unwrap().is_dir() && dir.file_name() == "graphql_schema")
+    });
+    let output_path = if has_schema_dir.unwrap() {
+        // assume we are in graphql dir
+        "graphql_schema/schema.graphql"
+    } else {
+        // assume we are in the project root dir
+        "graphql/graphql_schema/schema.graphql"
+    };
+
     let schema = build_schema().finish();
-    fs::write("graphql/graphql_schema/schema.graphql", &schema.sdl()).unwrap();
+    fs::write(output_path, &schema.sdl()).unwrap();
 }
