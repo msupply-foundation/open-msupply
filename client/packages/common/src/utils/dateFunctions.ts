8218bef013b42fa39529ef0136f90ccb17419aca
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-11 10:27:40 +1300

    Add note to gql queries, add missing fields to created invoice line and fix date formatting within tables
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/b8a3c515064b83baa2430f77e5bc1048f1101cfa

--- client/packages/common/src/utils/dateFunctions.ts
+++ client/packages/common/src/utils/dateFunctions.ts
@@ -1,4 +1,10 @@
-import { differenceInMonths, isPast, isThisWeek, isToday } from 'date-fns';
+import {
+  isValid,
+  differenceInMonths,
+  isPast,
+  isThisWeek,
+  isToday,
+} from 'date-fns';
 
 export const MINIMUM_EXPIRY_MONTHS = 3;
 
@@ -6,5 +12,10 @@ export const isAlmostExpired = (expiryDate: Date): boolean =>
   differenceInMonths(expiryDate, Date.now()) <= MINIMUM_EXPIRY_MONTHS;
 
 export const isExpired = (expiryDate: Date): boolean => isPast(expiryDate);
+
+export const getDateOrNull = (date: string): Date | null => {
+  const maybeDate = new Date(date);
+  return isValid(maybeDate) ? maybeDate : null;
+};
 
 export { isThisWeek, isToday };
