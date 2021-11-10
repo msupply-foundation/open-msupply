0117ac42ff7d16d21cc1e2a78051437059682968
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-10 19:54:58 +1300

    Add updated test cases for deletion

--- client/packages/invoices/src/OutboundShipment/DetailView/reducer.test.ts
+++ client/packages/invoices/src/OutboundShipment/DetailView/reducer.test.ts
@@ -127,7 +127,6 @@ describe('DetailView reducer', () => {
       isDesc: isDesc,
       direction: isDesc ? 'desc' : 'asc',
     },
-    deletedLines: [],
   });
 
   const createLine = (
@@ -294,14 +293,109 @@ describe('DetailView reducer', () => {
     });
   });
 
-  it('transfers deleted lines from the draft to the deleted lines cache', () => {
+  it('deleted lines are tagged as such', () => {
     const lineToDelete = createLine('1');
     const state = callReducer(OutboundAction.deleteLine(lineToDelete));
 
     const line = state.draft.lines.find(({ id }) => lineToDelete.id === id);
 
-    expect(line).toBeFalsy();
-    expect(state.deletedLines[0]).toEqual(lines[0]);
+    expect(line).toEqual(expect.objectContaining({ isDeleted: true }));
+  });
+
+  it('a line which is created, then deleted, is removed from state completely', () => {
+    const lineToDelete = createLine('99');
+    const state1 = callReducer(OutboundAction.upsertLine(lineToDelete));
+    const state2 = callReducer(OutboundAction.deleteLine(lineToDelete));
+
+    // Find the line in the first state, ensuring we did create it
+    const lineCreated = state1.draft.lines.find(
+      ({ id }) => lineToDelete.id === id
+    );
+    // Should not find the line in the second state
+    const lineDeleted = state2.draft.lines.find(
+      ({ id }) => lineToDelete.id === id
+    );
+
+    expect(lineCreated).toBeTruthy();
+    expect(lineDeleted).toBeFalsy();
+  });
+
+  it('inserting a line for an item which already has an invoice line in state for the same item, reuses the same invoice line', () => {
+    // Simulate the user adding a line
+    const lineToCreate = createLine('999', {
+      itemId: 'item1',
+      numberOfPacks: 999,
+      isCreated: true,
+    });
+    const state1 = callReducer(OutboundAction.upsertLine(lineToCreate));
+    const lineCreated1 = state1.draft.lines.find(
+      // The line should exist in the draft with the same ID and have the isCreated tag.
+      ({ id, isCreated }) => lineToCreate.id === id && isCreated
+    );
+    expect(lineCreated1).toBeTruthy();
+
+    // Simulate the user deleting a line.
+    const state2 = callReducer(OutboundAction.deleteLine(lineToCreate));
+    const lineDeleted1 = state2.draft.lines.find(
+      // The line should exist in the draft with the same ID and have the isDeleted tag AND the isCreated tag.
+      ({ id, isDeleted, isCreated }) =>
+        lineToCreate?.id === id && isDeleted && isCreated
+    );
+    expect(lineDeleted1).toBeFalsy();
+
+    // Simulate the user re-adding an invoice line for the same item
+    const lineToCreate2 = createLine('998', {
+      itemId: 'item1',
+      numberOfPacks: 999,
+      isCreated: true,
+    });
+    const state3 = callReducer(OutboundAction.upsertLine(lineToCreate2));
+    const lineDeleted2 = state3.draft.lines.find(
+      // The originally added line should NOT be in the draft, instead replaced by the newly added invoice line.
+      ({ id, isCreated, isDeleted }) =>
+        lineToCreate?.id === id && isCreated && isDeleted
+    );
+    const lineCreated2 = state3.draft.lines.find(
+      ({ id, isCreated, isDeleted }) =>
+        lineToCreate?.id === id && isCreated && isDeleted
+    );
+    expect(lineDeleted2).toBeFalsy();
+    expect(lineCreated2).toBeTruthy();
+  });
+
+  it('inserting a line for an item which has an existing, persisted, but deleted, line, reuses that existing line', () => {
+    // Simulate the user adding a line
+    const lineToCreate1 = createLine('999', {
+      itemId: 'item1',
+      numberOfPacks: 999,
+      isCreated: false,
+      isDeleted: true,
+    });
+    const state1 = callReducer(
+      OutboundAction.upsertLine(lineToCreate1),
+      getState({ defaultLines: [lineToCreate1] })
+    );
+    const lineCreated1 = state1.draft.lines.find(
+      // The line should exist in the draft with the same ID and have the isDeleted tag but not the isCreated tag,
+      // which is the state of a line which is persisted on the server, but deleted on the client.
+      ({ id, isCreated, isDeleted }) =>
+        lineToCreate1.id === id && !isCreated && isDeleted
+    );
+    expect(lineCreated1).toBeTruthy();
+
+    // Simulate the user adding a new line for the same item.
+    const lineToCreate2 = createLine('998', {
+      itemId: 'item1',
+      numberOfPacks: 999,
+    });
+    const state2 = callReducer(OutboundAction.upsertLine(lineToCreate2));
+    const lineCreated2 = state2.draft.lines.find(
+      // The line should exist in the draft with the same ID and have the isDeleted tag AND the isCreated tag.
+      // Not that we are matching on the original line id, not the new line id.
+      ({ id, isDeleted, isCreated }) =>
+        lineToCreate1?.id === id && !isDeleted && isCreated
+    );
+    expect(lineCreated2).toBeTruthy();
   });
 
   it('updates an existing line when upserting', () => {
