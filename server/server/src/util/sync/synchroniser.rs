c50b0857e9c4495137ec13f8d00a2e9b7d2fd633
Clemens Zeidler <clemens@msupply.foundation>
2021-11-11 13:32:14 +1300

    Fix test (don't rely on actual config files)
    Source: https://github.com/msupply-foundation/remote-server/commit/1d18dac74286f4feccfabdbec773ceffd67058f4

--- server/server/src/util/sync/synchroniser.rs
+++ server/server/src/util/sync/synchroniser.rs
@@ -272,12 +272,9 @@ mod tests {
 
     #[actix_rt::test]
     async fn test_integrate_central_records() {
-        let settings: Settings =
-            configuration::get_configuration().expect("Failed to parse configuration settings");
-
+        let settings = get_test_settings("omsupply-database-integrate_central_records");
         let sync_connection = SyncConnection::new(&settings.sync);
 
-        let settings = get_test_settings("omsupply-database-integrate_central_records");
         test_db::setup(&settings.database).await;
         let connection_manager = get_storage_connection_manager(&settings.database);
 
