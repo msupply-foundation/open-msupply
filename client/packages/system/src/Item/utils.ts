0c247c1fe1f9045395e84da4a5b2d470c86e2bc0
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-14 07:40:53 +1300

    Add useStockLines, useItemsList and useItem hooks

---
+++ client/packages/system/src/Item/utils.ts
@@ -0,0 +1,36 @@
+import {
+  ConnectorError,
+  StockLineConnector,
+  ItemSortFieldInput,
+  Item,
+  ItemsWithStockLinesQuery,
+  StockLineNode,
+  ItemNode,
+} from '@openmsupply-client/common';
+
+export const itemsGuard = (
+  itemsQuery: ItemsWithStockLinesQuery
+): { nodes: ItemNode[]; totalCount: number } => {
+  if (itemsQuery.items.__typename === 'ItemConnector') {
+    return itemsQuery.items;
+  } else {
+    throw new Error(itemsQuery.items.error.description);
+  }
+};
+
+export const availableBatchesGuard = (
+  availableBatches: StockLineConnector | ConnectorError
+): StockLineNode[] => {
+  if (availableBatches.__typename === 'StockLineConnector') {
+    return availableBatches.nodes;
+  } else if (availableBatches.__typename === 'ConnectorError') {
+    throw new Error(availableBatches.error.description);
+  }
+
+  throw new Error('Unknown');
+};
+
+export const getItemSortField = (sortField: keyof Item): ItemSortFieldInput => {
+  if (sortField === 'name') return ItemSortFieldInput.Name;
+  return ItemSortFieldInput.Code;
+};
