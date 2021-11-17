952b3a7cec0160e6cde4a078313d389e4394c714
Mark Prins <mark@malcolmprins.net>
2021-11-18 10:21:29 +1300

    move locales
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/b4b50ff3e36aadab5306163c2e4aa5cc67908043

--- client/packages/common/src/intl/IntlProvider.tsx
+++ client/packages/common/src/intl/IntlProvider.tsx
@@ -5,7 +5,6 @@ import {
   SupportedLocales,
   importMessages,
 } from './intlHelpers';
-
 export const IntlProvider: React.FC<
   Omit<React.ComponentProps<typeof ReactIntlProvider>, 'messages'> & {
     messages?: LocaleMessages;
@@ -22,10 +21,54 @@ export const IntlProvider: React.FC<
   // to ensure local date formatting, use the full locale otherwise 'en' === 'en-US'
   const re = new RegExp(`^${locale}-[A-Z]+$`);
   const intlLocale = re.test(navigator.language) ? navigator.language : locale;
-
   return messages ? (
     <ReactIntlProvider locale={intlLocale} messages={messages} key={locale}>
       {props.children}
     </ReactIntlProvider>
   ) : null;
 };
+
+// import React from 'react';
+// import i18next from 'i18next';
+// import Backend from 'i18next-chained-backend';
+// import LocalStorageBackend from 'i18next-localstorage-backend';
+// import HttpApi from 'i18next-http-backend';
+// import { I18nextProvider, initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+
+// export const defaultNS = 'app';
+// export const IntlProvider: React.FC = ({ children }) => {
+//   React.useEffect(() => {
+//     i18next
+//       .use(initReactI18next) // passes i18n down to react-i18next
+//       .use(Backend)
+//       .use(LanguageDetector)
+//       .init({
+//         backend: {
+//           backends: [
+//             LocalStorageBackend, // primary backend
+//             HttpApi, // fallback backend
+//           ],
+//           backendOptions: [
+//             {
+//               /* options for primary backend */
+//             },
+//             {
+//               /* options for secondary backend */
+//               loadPath: '/locales/{{lng}}/{{ns}}.json',
+//             },
+//           ],
+//         },
+//         debug: true,
+//         defaultNS,
+//         ns: defaultNS, // behaving as I expect defaultNS should. Without specifying ns here, a request is made to 'translation.json'
+//         fallbackLng: 'en',
+//         fallbackNS: 'common',
+//         interpolation: {
+//           escapeValue: false, // not needed for react!!
+//         },
+//       });
+//   }, []);
+
+//   return <I18nextProvider i18n={i18next}>{children}</I18nextProvider>;
+// };
