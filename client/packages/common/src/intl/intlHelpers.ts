0aa57b313be91dc70c49581233962ab8cb159234
Mark Prins <mark@malcolmprins.net>
2021-11-18 11:02:18 +1300

    remove locale from Host

--- client/packages/common/src/intl/intlHelpers.ts
+++ client/packages/common/src/intl/intlHelpers.ts
@@ -33,102 +33,11 @@ export const useTranslation = (ns?: Namespace): TypedTFunction<LocaleKey> => {
   return (key, options) => (key ? t(key, options) : '');
 };
 
-export const useTranslationPrevious = (): ((
-  id?: LocaleKey, // only accepts valid keys, not any string
-  values?: LocaleProps
-) => string) => {
-  const intl = useIntl();
-  return (id, values) =>
-    id ? intl.formatMessage({ id: id as string }, values) : '';
-};
-
-export const useFormatDatePrevious = (): ((
-  value: number | Date,
-  options?: Intl.DateTimeFormatOptions & { format?: string }
-) => string) => {
-  const intl = useIntl();
-  return (value, options) => intl.formatDate(value, options);
-};
-export const useFormatNumberPrevious = (): ((
-  value: number | bigint,
-  options?: FormatNumberOptions
-) => string) => {
-  const intl = useIntl();
-  return (value, options) => intl.formatNumber(value, options);
-};
-export const useRtlPrevious = (): boolean => {
+const useRtlPrevious = (): boolean => {
   const { locale } = useHostContext();
   const isRtl = locale === 'ar';
   return isRtl;
 };
-
-// // return type on this signature enforces that all languages have the same translations defined
-// export const importMessages = (
-//   locale: SupportedLocales
-// ): Promise<LocaleMessages> => {
-//   switch (locale) {
-//     case 'en':
-//       return import(
-//         /* webpackMode: "lazy", webpackChunkName: "en_json" */
-//         './locales/en/common.json'
-//       );
-//     case 'fr':
-//       return import(
-//         /* webpackMode: "lazy", webpackChunkName: "fr_json" */
-//         './locales/en/common.json'
-//       );
-//     case 'ar':
-//       return import(
-//         /* webpackMode: "lazy", webpackChunkName: "ab_json" */
-//         './locales/en/common.json'
-//       );
-//   }
-// };
-
-// import { Namespace, useTranslation as useTranslationNext } from 'react-i18next';
-// import { i18n, TFunctionResult, TOptions } from 'i18next';
-// import { LocaleKey } from './locales';
-
-// export type SupportedLocales = 'en' | 'fr' | 'ar';
-// export type LocaleProps = TOptions<Record<string, unknown>> | string;
-// export { LocaleKey };
-// export interface TypedTFunction<Keys> {
-//   // basic usage
-//   <TKeys extends Keys, TResult extends TFunctionResult = string>(
-//     key: TKeys | TKeys[],
-//     options?: LocaleProps
-//   ): TResult;
-//   // overloaded usage
-//   <TKeys extends Keys, TResult extends TFunctionResult = string>(
-//     key: TKeys | TKeys[],
-//     defaultValue?: string,
-//     options?: LocaleProps
-//   ): TResult;
-// }
-
-// export const useTranslation = (ns?: Namespace): TypedTFunction<LocaleKey> => {
-//   const { t } = useTranslationNext(ns);
-//   return (key, options) => (key ? t(key, options) : '');
-// };
-
-// export const useI18N = (): i18n => {
-//   const { i18n } = useTranslationNext();
-//   return i18n;
-// };
-
-// // export const useTranslationWithFallback = (): ((
-// //   id: LocaleKey, // only accepts valid keys, not any string
-// //   fallback: string,
-// //   values?: Record<string, PrimitiveType>
-// // ) => string) => {
-// //   const intl = useIntl();
-// //   return (id, fallback, values) => {
-// //     if (!id) return '';
-// //     if (Object.keys(intl.messages).every(key => id !== key)) return fallback;
-
-// //     return intl.formatMessage({ id: id as string }, values);
-// //   };
-// // };
 
 export const useFormatDate = (): ((
   value: number | Date,
