ace12ca3a6e178cd6bf00fae030284fa944ae545
Mark Prins <mark@malcolmprins.net>
2021-11-18 11:44:08 +1300

    update intl test provider

--- client/packages/common/src/intl/IntlTestProvider.tsx
+++ client/packages/common/src/intl/IntlTestProvider.tsx
@@ -1,12 +1,11 @@
-import React, { FC } from 'react';
-// import React, { FC, useEffect } from 'react';
-// import {
-//   I18nextProvider,
-//   initReactI18next,
-//   useTranslation,
-// } from 'react-i18next';
+import React, { FC, useEffect } from 'react';
+import {
+  I18nextProvider,
+  initReactI18next,
+  useTranslation,
+} from 'react-i18next';
 import { SupportedLocales } from './intlHelpers';
-// import { resources } from './locales';
+import { resources } from './locales';
 
 interface IntlTestProviderProps {
   locale: SupportedLocales;
@@ -16,26 +15,20 @@ export const IntlTestProvider: FC<IntlTestProviderProps> = ({
   children,
   locale,
 }) => {
-  // const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
-  // useEffect(() => {
-  //   i18n.changeLanguage(locale);
-  // }, [locale]);
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+  }, [locale]);
 
-  // i18n.use(initReactI18next).init({
-  //   resources,
-  //   lng: locale,
-  //   fallbackLng: 'en',
-  //   interpolation: {
-  //     escapeValue: false,
-  //   },
-  // });
+  i18n.use(initReactI18next).init({
+    resources,
+    lng: locale,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
-  // return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
-  return (
-    <div>
-      {locale}
-      {children}
-    </div>
-  );
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 };
