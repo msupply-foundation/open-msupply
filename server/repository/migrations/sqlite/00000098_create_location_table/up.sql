02fe4c8f991bf55eac36610048c149ce98d5a08f
Andrei <andrei.evguenov@gmail.com>
2021-11-18 10:56:41 +1300

    Ass store_id to location schema
    Source: https://github.com/msupply-foundation/remote-server/commit/4766269f8433ac55fba41be038de842ac9af060a

--- server/repository/migrations/sqlite/00000098_create_location_table/up.sql
+++ server/repository/migrations/sqlite/00000098_create_location_table/up.sql
@@ -2,6 +2,7 @@ CREATE TABLE location (
     id TEXT NOT NULL PRIMARY KEY,
     code TEXT NOT NULL,
     name TEXT NOT NULL,
-    on_hold BOOLEAN NOT NULL
+    on_hold BOOLEAN NOT NULL,
+    store_id TEXT NOT NULL REFERENCES store(id)
 );
 
