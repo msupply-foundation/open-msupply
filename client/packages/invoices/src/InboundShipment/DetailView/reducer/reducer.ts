43a70748203a618c5246044ac92db2f6052b2a89
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-20 10:40:22 +1300

    Add inbound detail view reducer

---
+++ client/packages/invoices/src/InboundShipment/DetailView/reducer/reducer.ts
@@ -0,0 +1,231 @@
+import {
+  getUnitQuantity,
+  getSumOfKeyReducer,
+  produce,
+  Column,
+  DocumentActionSet,
+  DocumentActionType,
+  SortBy,
+  ifTheSameElseDefault,
+  Item,
+  arrayToRecord,
+} from '@openmsupply-client/common';
+import { placeholderInbound } from '../../../utils';
+import {
+  ActionType,
+  OutboundShipmentAction,
+  Invoice,
+  InvoiceLine,
+  InboundShipment,
+  InboundShipmentItem,
+  InboundShipmentRow,
+} from '../../../types';
+
+const getExistingLine = (
+  items: InboundShipmentItem[],
+  line: InboundShipmentRow
+): {
+  existingSummaryItem?: InboundShipmentItem;
+  existingRow?: InboundShipmentRow;
+} => {
+  const existingSummaryItem = items.find(
+    item => !!item.batches[line.id] || item.itemId === line.itemId
+  );
+
+  if (!existingSummaryItem) return {};
+
+  let existingRow = existingSummaryItem.batches[line.id];
+
+  if (existingRow) return { existingRow, existingSummaryItem };
+
+  existingRow = Object.values(existingSummaryItem.batches).find(
+    ({ stockLineId }) => stockLineId === line.stockLineId
+  );
+
+  return { existingRow, existingSummaryItem };
+};
+
+export const recalculateSummary = (
+  summaryItem: InboundShipmentItem
+): { unitQuantity: number; numberOfPacks: number } => {
+  const unitQuantity = Object.values<InboundShipmentRow>(
+    summaryItem.batches
+  ).reduce(getUnitQuantity, 0);
+
+  const numberOfPacks = Object.values<InboundShipmentRow>(
+    summaryItem.batches
+  ).reduce(getSumOfKeyReducer('numberOfPacks'), 0);
+
+  return { unitQuantity, numberOfPacks };
+};
+
+export const InboundAction = {
+  upsertLine: (line: InboundShipmentRow): OutboundShipmentAction => ({
+    type: ActionType.UpsertLine,
+    payload: { line },
+  }),
+
+  deleteLine: (line: InboundShipmentRow): OutboundShipmentAction => ({
+    type: ActionType.DeleteLine,
+    payload: { line },
+  }),
+  updateInvoice: <K extends keyof Invoice>(
+    key: K,
+    value: Invoice[K]
+  ): OutboundShipmentAction => ({
+    type: ActionType.UpdateInvoice,
+    payload: { key, value },
+  }),
+
+  onSortBy: (column: Column<InboundShipmentItem>): OutboundShipmentAction => ({
+    type: ActionType.SortBy,
+    payload: { column },
+  }),
+};
+
+export interface InboundShipmentStateShape {
+  draft: InboundShipment;
+  sortBy: SortBy<InboundShipmentItem>;
+}
+
+export const itemToSummaryItem = (item: Item): InboundShipmentItem => {
+  return {
+    id: item.id,
+    itemId: item.id,
+    itemName: item.name,
+    itemCode: item.code,
+    itemUnit: item.unitName,
+    batches: {},
+    unitQuantity: 0,
+    numberOfPacks: 0,
+  };
+};
+
+export const createSummaryItem = (
+  itemId: string,
+  batches: InboundShipmentRow[] = []
+): InboundShipmentItem => {
+  const item: InboundShipmentItem = {
+    id: itemId,
+    itemId: itemId,
+    itemName: ifTheSameElseDefault(batches, 'itemName', ''),
+    itemCode: ifTheSameElseDefault(batches, 'itemCode', ''),
+    itemUnit: ifTheSameElseDefault(batches, 'itemUnit', ''),
+    batches: arrayToRecord(batches),
+    unitQuantity: batches.reduce(getUnitQuantity, 0),
+    numberOfPacks: batches.reduce(getSumOfKeyReducer('numberOfPacks'), 0),
+    locationDescription: ifTheSameElseDefault(
+      batches,
+      'locationDescription',
+      undefined
+    ),
+
+    batch: ifTheSameElseDefault(batches, 'batch', '[multiple]'),
+    // TODO: Likely should just be a string.
+    sellPrice: ifTheSameElseDefault(batches, 'sellPricePerPack', undefined),
+    // TODO: Likely should just be a string.
+    packSize: ifTheSameElseDefault(batches, 'packSize', undefined),
+  };
+
+  return item;
+};
+
+export const getInitialState = (): InboundShipmentStateShape => ({
+  draft: placeholderInbound,
+  sortBy: { key: 'numberOfPacks', isDesc: true, direction: 'asc' },
+});
+
+export const reducer = (
+  data: Invoice = placeholderInbound
+): ((
+  state: InboundShipmentStateShape | undefined,
+  action: DocumentActionSet<OutboundShipmentAction>
+) => InboundShipmentStateShape) =>
+  produce(
+    (
+      state: InboundShipmentStateShape = getInitialState(),
+      action: DocumentActionSet<OutboundShipmentAction>
+    ) => {
+      switch (action.type) {
+        case DocumentActionType.Init: {
+          return state;
+        }
+
+        case DocumentActionType.Merge: {
+          const { draft } = state;
+
+          Object.keys(draft).forEach(key => {
+            // TODO: Sometimes we want to keep the user entered values?
+            if (key === 'items' || key === 'lines') return;
+            draft[key] = data[key];
+          });
+
+          draft.items = data.lines?.reduce((itemsArray, serverLine) => {
+            const InboundShipmentRow = createLine(serverLine, draft);
+
+            const { existingRow, existingSummaryItem } = getExistingLine(
+              itemsArray,
+              InboundShipmentRow
+            );
+
+            const summaryItem =
+              existingSummaryItem ??
+              createSummaryItem(serverLine.itemId, [InboundShipmentRow]);
+
+            if (existingRow) {
+              delete summaryItem.batches[existingRow.id];
+              const newLine = mergeLines(serverLine, existingRow);
+              summaryItem.batches[newLine.id] = newLine;
+            } else {
+              summaryItem.batches[InboundShipmentRow.id] = InboundShipmentRow;
+            }
+
+            const { unitQuantity, numberOfPacks } =
+              recalculateSummary(summaryItem);
+
+            if (!existingSummaryItem) {
+              itemsArray.push({
+                ...summaryItem,
+                unitQuantity,
+                numberOfPacks,
+              });
+            } else {
+              existingSummaryItem.unitQuantity = unitQuantity;
+              existingSummaryItem.numberOfPacks = numberOfPacks;
+            }
+
+            return itemsArray;
+          }, draft.items);
+
+          break;
+        }
+      }
+      return state;
+    }
+  );
+
+const mergeLines = (
+  serverLine: InvoiceLine,
+  clientLine: InboundShipmentRow
+) => {
+  const newLine = {
+    ...clientLine,
+    ...serverLine,
+    isUpdated: true,
+    isCreated: false,
+    isDeleted: false,
+  };
+
+  return newLine;
+};
+
+const createLine = (
+  line: InvoiceLine,
+  draft: InboundShipment
+): InboundShipmentRow => {
+  return {
+    ...line,
+    stockLineId: line.stockLine?.id ?? '',
+    invoiceId: draft.id,
+  };
+};
