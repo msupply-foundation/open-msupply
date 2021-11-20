bdebdbbd557086c4f03141bbe44a0960cd049eb6
Mark Prins <mark@malcolmprins.net>
2021-11-20 16:21:56 +1300

    correct imports of language types

--- client/packages/common/src/intl/locales/index.ts
+++ client/packages/common/src/intl/locales/index.ts
@@ -1,20 +1,11 @@
 // "import type" ensures en messages aren't bundled by default
-import app from './en/app.json';
-import common from './en/common.json';
-import dashboard from './en/dashboard.json';
-import outboundShipment from './en/outbound-shipment.json';
+import * as app from './en/app.json';
+import * as common from './en/common.json';
+import * as dashboard from './en/dashboard.json';
+import * as outboundShipment from './en/outbound-shipment.json';
 
 export type LocaleKey =
   | keyof typeof app
   | keyof typeof dashboard
   | keyof typeof common
   | keyof typeof outboundShipment;
-
-export const resources = {
-  en: {
-    app,
-    dashboard,
-    common,
-    'outbound-shipment': outboundShipment,
-  },
-} as const;
