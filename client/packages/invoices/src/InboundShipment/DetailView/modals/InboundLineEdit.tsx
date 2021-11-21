fa3254991c7470094b6ab85a2cb9a54cdb24996f
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-21 13:17:37 +1300

    Add props to InboundLineEdit

--- client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit.tsx
@@ -1,5 +1,12 @@
 import { FC } from 'react';
+import { Item } from '@openmsupply-client/common';
+import { InboundShipmentItem } from '../../../types';
+interface InboundLineEditProps {
+  item: InboundShipmentItem | null;
+  onUpsert: (item: InboundShipmentItem) => void;
+  onChangeItem: (item: Item | null) => void;
+}
 
-export const InboundLineEdit: FC = () => {
+export const InboundLineEdit: FC<InboundLineEditProps> = () => {
   return null;
 };
