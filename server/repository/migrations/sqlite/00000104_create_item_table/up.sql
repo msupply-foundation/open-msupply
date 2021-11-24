ebfc1714354c9238dbfc9337c9a302d145739749
Clemens Zeidler <clemens@msupply.foundation>
2021-11-25 10:00:24 +1300

    Add an item type
    Source: https://github.com/msupply-foundation/remote-server/commit/477fbfd96915ef3a1f30795f501f231ecc245bd8

--- server/repository/migrations/sqlite/00000104_create_item_table/up.sql
+++ server/repository/migrations/sqlite/00000104_create_item_table/up.sql
@@ -4,5 +4,6 @@ CREATE TABLE item (
     id TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     code TEXT NOT NULL,
-    unit_id TEXT REFERENCES unit(id)
+    unit_id TEXT REFERENCES unit(id),
+    type TEXT NOT NULL
 )
