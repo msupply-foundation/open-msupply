19daf92079c58053eb999733c5d6fa22c9aa395e
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-26 13:45:45 +1300

    Add minor style updates

--- client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/InboundLineEdit.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/InboundLineEdit.tsx
@@ -9,6 +9,10 @@ import {
   Tab,
   TabPanel,
   styled,
+  BaseButton,
+  useTranslation,
+  alpha,
+  useIsMediumScreen,
 } from '@openmsupply-client/common';
 import { InboundShipment, InboundShipmentItem } from '../../../../types';
 import { flattenInboundItems } from '../../../../utils';
@@ -16,6 +20,7 @@ import { ModalMode } from '../../DetailView';
 import { BatchTable, PricingTable } from './TabTables';
 import { createInboundShipmentBatch, wrapInboundShipmentItem } from './utils';
 import { InboundLineEditForm } from './InboundLineEditForm';
+import { Box } from '@mui/system';
 
 const StyledTabPanel = styled(TabPanel)({
   height: '100%',
@@ -39,6 +44,8 @@ export const InboundLineEdit: FC<InboundLineEditProps> = ({
   mode,
   draft,
 }) => {
+  const t = useTranslation('distribution');
+  const isMediumScreen = useIsMediumScreen();
   const [currentTab, setCurrentTab] = React.useState<Tabs>(Tabs.Batch);
   const wrappedInbound = item
     ? wrapInboundShipmentItem(item, onChangeItem)
@@ -70,18 +77,39 @@ export const InboundLineEdit: FC<InboundLineEditProps> = ({
         onChangeItem={onChangeItem}
       />
       <Divider margin={5} />
-      {wrappedInbound && (
+      {wrappedInbound ? (
         <TabContext value={currentTab}>
-          <TabList
-            value={currentTab}
-            centered
-            onChange={(_, v) => setCurrentTab(v)}
-          >
-            <Tab value={Tabs.Batch} label={Tabs.Batch} />
-            <Tab value={Tabs.Pricing} label={Tabs.Pricing} />
-          </TabList>
+          <Box flex={1} display="flex" justifyContent="space-between">
+            <Box flex={1} />
+            <Box flex={1}>
+              <TabList
+                value={currentTab}
+                centered
+                onChange={(_, v) => setCurrentTab(v)}
+              >
+                <Tab value={Tabs.Batch} label={Tabs.Batch} />
+                <Tab value={Tabs.Pricing} label={Tabs.Pricing} />
+              </TabList>
+            </Box>
+            <Box flex={1} justifyContent="flex-end" display="flex">
+              <BaseButton
+                color="primary"
+                variant="outlined"
+                onClick={onAddBatch}
+              >
+                {t('label.add-batch')}
+              </BaseButton>
+            </Box>
+          </Box>
 
-          <TableContainer sx={{ height: 400 }}>
+          <TableContainer
+            sx={{
+              height: isMediumScreen ? 300 : 400,
+              marginTop: 2,
+              backgroundColor: theme => alpha(theme.palette.gray.pale, 0.5),
+              borderRadius: '20px',
+            }}
+          >
             <StyledTabPanel value={Tabs.Batch}>
               <BatchTable batches={batches} />
             </StyledTabPanel>
@@ -90,20 +118,9 @@ export const InboundLineEdit: FC<InboundLineEditProps> = ({
               <PricingTable batches={batches} />
             </StyledTabPanel>
           </TableContainer>
-          <Fab
-            sx={{
-              alignSelf: 'flex-end',
-              margin: '10px',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-            color="secondary"
-            aria-label="add"
-            onClick={onAddBatch}
-          >
-            <PlusCircleIcon />
-          </Fab>
         </TabContext>
+      ) : (
+        <Box sx={{ height: isMediumScreen ? 400 : 500 }} />
       )}
     </>
   );
