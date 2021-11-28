b81cca429506684d99e4a129864ae3aa85a6b3c9
Joshua Griffin <griffinjoshua5@gmail.com>
2021-11-28 22:27:18 +1300

    Add inventory package
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/20d3aafa92191c56f536acb8e38859a344dbe902

---
+++ client/packages/inventory/src/Stocktake/ListView/index.ts
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const ListView: FC = () => {
+  return null;
+};
---
+++ client/packages/inventory/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  ListView as SupplierRequisitionListView,
+  DetailView as SupplierRequisitionDetailView,
+} from './Stocktake';
+export {
+  ListView as CustomerRequisitionListView,
+  DetailView as CustomerRequisitionDetailView,
+} from './CustomerRequisition';
+
+export { InventoryService } from './InventoryService';
