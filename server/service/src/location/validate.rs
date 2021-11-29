d2f4ded1eefa171a08cc0dd7ffdcf191fcf168d2
Andrei <andrei.evguenov@gmail.com>
2021-11-30 11:53:21 +1300

    Use one check_location_code_is_unique for insert and update

--- server/service/src/location/validate.rs
+++ server/service/src/location/validate.rs
@@ -6,18 +6,21 @@ use crate::{current_store_id, service_provider::ServiceConnection, WithDBError};
 pub struct LocationWithCodeAlreadyExists;
 
 pub fn check_location_code_is_unique(
-    code: &String,
+    id: &String,
+    code_option: &Option<String>,
     connection: &ServiceConnection,
 ) -> Result<(), WithDBError<LocationWithCodeAlreadyExists>> {
-    let locations = LocationRepository::new(connection).query_by_filter(
-        LocationFilter::new()
-            .code(|f| f.equal_to(code))
-            .store_id(|f| f.equal_to(&current_store_id())),
-    )?;
+    if let Some(code) = code_option {
+        let locations = LocationRepository::new(connection).query_by_filter(
+            LocationFilter::new()
+                .code(|f| f.equal_to(code))
+                .id(|f| f.not_equal_to(id))
+                .store_id(|f| f.equal_to(&current_store_id())),
+        )?;
 
-    if locations.len() > 0 {
-        Err(WithDBError::Error(LocationWithCodeAlreadyExists {}))
-    } else {
-        Ok(())
+        if locations.len() > 0 {
+            return Err(WithDBError::Error(LocationWithCodeAlreadyExists {}));
+        }
     }
+    Ok(())
 }
