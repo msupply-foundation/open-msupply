5a5d679534dc6b1159bcc5753d7a34350f1cf4a4
Joshua Griffin <griffinjoshua5@gmail.com>
2021-12-01 12:53:43 +1300

    Always start the service worker
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/e2807e33d1a3b09788afb7593b849958b589552e

--- client/packages/host/src/bootstrap.tsx
+++ client/packages/host/src/bootstrap.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  const {
-    setupMockWorker,
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-  } = require('@openmsupply-client/mock-server/src/worker/client');
-  const worker = setupMockWorker();
+const {
+  setupMockWorker,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require('@openmsupply-client/mock-server/src/worker/client');
+const worker = setupMockWorker();
 
-  worker.start();
-}
+worker.start();
 
 ReactDOM.render(<App />, document.getElementById('root'));
