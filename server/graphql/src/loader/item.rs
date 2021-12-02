99ea85feba4b6ffff0c0845fb61e3657e82860af
Clemens Zeidler <clemens@msupply.foundation>
2021-12-03 09:39:16 +1300

    Change ItemLoader to query Item instead of ItemRow
    Source: https://github.com/msupply-foundation/remote-server/commit/ba7ead3e84326e8ea1f2408d8c74942e5dcf5251

--- server/graphql/src/loader/item.rs
+++ server/graphql/src/loader/item.rs
@@ -1,4 +1,7 @@
-use repository::{schema::ItemRow, ItemRepository, RepositoryError, StorageConnectionManager};
+use domain::item::{Item, ItemFilter};
+use domain::{EqualFilter, Pagination};
+use repository::ItemQueryRepository;
+use repository::{RepositoryError, StorageConnectionManager};
 
 use async_graphql::dataloader::*;
 use async_graphql::*;
@@ -10,20 +13,31 @@ pub struct ItemLoader {
 
 #[async_trait::async_trait]
 impl Loader<String> for ItemLoader {
-    type Value = ItemRow;
+    type Value = Item;
     type Error = RepositoryError;
 
     async fn load(&self, keys: &[String]) -> Result<HashMap<String, Self::Value>, Self::Error> {
         let connection = self.connection_manager.connection()?;
-        let repo = ItemRepository::new(&connection);
+        let repo = ItemQueryRepository::new(&connection);
         let result = repo
-            .find_many_by_id(keys)
-            .unwrap()
+            .query(
+                Pagination {
+                    limit: keys.len() as u32,
+                    offset: 0,
+                },
+                Some(ItemFilter {
+                    id: Some(EqualFilter::equal_any(keys.to_vec())),
+                    name: None,
+                    code: None,
+                    is_visible: None,
+                }),
+                None,
+            )?
             .iter()
-            .map(|item: &ItemRow| {
-                let item_id = item.id.clone();
+            .map(|item: &Item| {
+                let id = item.id.clone();
                 let item = item.clone();
-                (item_id, item)
+                (id, item)
             })
             .collect();
         Ok(result)
