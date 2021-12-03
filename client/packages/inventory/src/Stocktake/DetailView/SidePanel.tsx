ab9a5d78f7456e7ce956d0a3c42a012c2fb44774
Joshua Griffin <griffinjoshua5@gmail.com>
2021-12-04 11:14:15 +1300

    Add shift description input to toolbar rather than side panel
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/9d19c0f0fb0d6f69f2670dca6469143fe834aad9

--- client/packages/inventory/src/Stocktake/DetailView/SidePanel.tsx
+++ client/packages/inventory/src/Stocktake/DetailView/SidePanel.tsx
@@ -24,12 +24,6 @@ const AdditionalInfoSection: FC<SidePanelProps> = ({ draft }) => {
   return (
     <DetailPanelSection title={t('heading.additional-info')}>
       <Grid container gap={0.5} key="additional-info">
-        <PanelLabel>{t('heading.description')}</PanelLabel>
-        <TextArea
-          disabled={!isStocktakeEditable(draft)}
-          onChange={e => draft.update('description', e.target.value)}
-          value={draft.description}
-        />
         <PanelLabel>{t('heading.comment')}</PanelLabel>
         <TextArea
           disabled={!isStocktakeEditable(draft)}
