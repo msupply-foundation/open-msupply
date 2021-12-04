b8ac55fe877a02772b09dfd20eaf0bbc530af801
Joshua Griffin <griffinjoshua5@gmail.com>
2021-12-05 08:48:25 +1300

    Add item columns and summary items to stocktake detail view
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/edb795f37fafb19d32777b684682eb9aa2d11cfc

--- client/packages/common/src/utils/arrays/utils.ts
+++ client/packages/common/src/utils/arrays/utils.ts
@@ -1,15 +1,13 @@
-export const ifTheSameElseDefault = <
-  T,
-  K extends keyof T,
-  J extends T[K] | undefined
->(
+export const ifTheSameElseDefault = <T, K extends keyof T, J>(
   someEntities: T[],
   key: K,
   defaultValue: J
-): J => {
-  const value = someEntities[0]?.[key] as J;
-  const allTheSame = someEntities.every(entity => entity[key] === value);
-  return allTheSame && value ? value : defaultValue;
+): J | T[K] => {
+  const value = someEntities[0]?.[key];
+  const allTheSame = someEntities.every(entity => {
+    return entity[key] === value;
+  });
+  return allTheSame && value != undefined ? value : defaultValue;
 };
 
 export const arrayToRecord = <T extends { id: string }>(
