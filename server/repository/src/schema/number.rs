0e2f8e6a8541158580302d4086c0e1b25bbc2671
Clemens Zeidler <clemens@msupply.foundation>
2021-12-06 14:23:45 +1300

    Add store_id to the number table

--- server/repository/src/schema/number.rs
+++ server/repository/src/schema/number.rs
@@ -5,4 +5,6 @@ use super::diesel_schema::number;
 pub struct NumberRow {
     pub id: String,
     pub value: i64,
+    /// Note, store id will be needed mainly for sync.
+    pub store_id: String,
 }
