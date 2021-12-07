66f04f08ebb4957d9a24761daf4a64569ae64e16
Mark Prins <mark@malcolmprins.net>
2021-12-08 10:19:48 +1300

    jest and storybook

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,6 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
@@ -6,5 +9,13 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   moduleNameMapper: {
     'react-i18next': '<rootDir>/__mocks__/react-i18next.ts',
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>',
+    }),
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: true,
+    },
   },
 };
