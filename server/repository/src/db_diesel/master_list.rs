c54a3576ebd5366a01635c5500bbb7628639b88b
Andrei <andrei.evguenov@gmail.com>
2021-12-09 23:05:56 +1300

    Add master list repository

--- server/repository/src/db_diesel/master_list.rs
+++ server/repository/src/db_diesel/master_list.rs
@@ -1,8 +1,24 @@
-use super::StorageConnection;
+use crate::{
+    diesel_macros::{apply_equal_filter, apply_simple_string_filter, apply_sort_no_case},
+    repository_error::RepositoryError,
+    schema::{
+        diesel_schema::{
+            master_list, master_list::dsl as master_list_dsl,
+            master_list_name_join::dsl as master_list_name_join_dsl, name::dsl as name_dsl,
+        },
+        MasterListRow,
+    },
+};
+use domain::{
+    master_list::{MasterListFilter, MasterListSort, MasterListSortField},
+    EqualFilter, Pagination,
+};
 
-use crate::{repository_error::RepositoryError, schema::MasterListRow};
+use super::{DBType, StorageConnection};
 
 use diesel::prelude::*;
+
+pub type MasterList = MasterListRow;
 
 pub struct MasterListRepository<'a> {
     connection: &'a StorageConnection,
@@ -13,33 +29,86 @@ impl<'a> MasterListRepository<'a> {
         MasterListRepository { connection }
     }
 
-    #[cfg(feature = "postgres")]
-    pub fn upsert_one(&self, row: &MasterListRow) -> Result<(), RepositoryError> {
-        use crate::schema::diesel_schema::master_list::dsl::*;
+    pub fn count(&self, filter: Option<MasterListFilter>) -> Result<i64, RepositoryError> {
+        // TODO (beyond M1), check that store_id matches current store
+        let query = create_filtered_query(filter, self.connection)?;
 
-        diesel::insert_into(master_list)
-            .values(row)
-            .on_conflict(id)
-            .do_update()
-            .set(row)
-            .execute(&self.connection.connection)?;
-        Ok(())
+        Ok(query.count().get_result(&self.connection.connection)?)
     }
 
-    #[cfg(not(feature = "postgres"))]
-    pub fn upsert_one(&self, row: &MasterListRow) -> Result<(), RepositoryError> {
-        use crate::schema::diesel_schema::master_list::dsl::*;
-        diesel::replace_into(master_list)
-            .values(row)
-            .execute(&self.connection.connection)?;
-        Ok(())
+    pub fn query_by_filter(
+        &self,
+        filter: MasterListFilter,
+    ) -> Result<Vec<MasterList>, RepositoryError> {
+        self.query(Pagination::new(), Some(filter), None)
     }
 
-    pub async fn find_one_by_id(&self, item_id: &str) -> Result<MasterListRow, RepositoryError> {
-        use crate::schema::diesel_schema::master_list::dsl::*;
-        let result = master_list
-            .filter(id.eq(item_id))
-            .first(&self.connection.connection)?;
+    pub fn query(
+        &self,
+        pagination: Pagination,
+        filter: Option<MasterListFilter>,
+        sort: Option<MasterListSort>,
+    ) -> Result<Vec<MasterList>, RepositoryError> {
+        // TODO (beyond M1), check that store_id matches current store
+        let mut query = create_filtered_query(filter, self.connection)?;
+
+        if let Some(sort) = sort {
+            match sort.key {
+                MasterListSortField::Name => {
+                    apply_sort_no_case!(query, sort, master_list_dsl::name);
+                }
+                MasterListSortField::Code => {
+                    apply_sort_no_case!(query, sort, master_list_dsl::code);
+                }
+                MasterListSortField::Description => {
+                    apply_sort_no_case!(query, sort, master_list_dsl::description);
+                }
+            }
+        } else {
+            query = query.order(master_list_dsl::id.asc())
+        }
+
+        let result = query
+            .offset(pagination.offset as i64)
+            .limit(pagination.limit as i64)
+            .load::<MasterListRow>(&self.connection.connection)?;
+
         Ok(result)
     }
+}
+
+type BoxedMasterListQuery = master_list::BoxedQuery<'static, DBType>;
+
+fn create_filtered_query(
+    filter: Option<MasterListFilter>,
+    connection: &StorageConnection,
+) -> Result<BoxedMasterListQuery, RepositoryError> {
+    let mut query = master_list_dsl::master_list.into_boxed();
+
+    if let Some(f) = filter {
+        apply_equal_filter!(query, f.id, master_list_dsl::id);
+        apply_simple_string_filter!(query, f.name, master_list_dsl::name);
+        apply_simple_string_filter!(query, f.code, master_list_dsl::code);
+        apply_simple_string_filter!(query, f.description, master_list_dsl::description);
+
+        // Result master list should be unique, which would need extra logic if we were to join
+        // name table through master_list_name_join, thus query seperatly and use resulting
+        // master_list_ids in 'any' filter
+        if f.exists_for_name.is_some() || f.exists_for_name_id.is_some() {
+            let mut name_join_query = master_list_name_join_dsl::master_list_name_join
+                .select(master_list_name_join_dsl::master_list_id)
+                .left_join(name_dsl::name)
+                .into_boxed();
+
+            apply_simple_string_filter!(name_join_query, f.exists_for_name, name_dsl::name_);
+            apply_equal_filter!(name_join_query, f.exists_for_name_id, name_dsl::id);
+
+            let master_list_ids = name_join_query.load::<String>(&connection.connection)?;
+
+            let filter = Some(EqualFilter::equal_any(master_list_ids));
+            apply_equal_filter!(query, filter, master_list_dsl::id);
+        }
+    }
+
+    Ok(query)
 }
