076757814bbb5669aeaa1e6bb38f7cb162fe8aad
Andrei <andrei.evguenov@gmail.com>
2021-12-10 02:26:05 +1300

    Add master list line graphql type and use it with loader in master list query
    Source: https://github.com/msupply-foundation/remote-server/commit/8a82ccd306742ab7c1fa80fd0119027413a96c4f

--- server/graphql/src/schema/types/master_list.rs
+++ server/graphql/src/schema/types/master_list.rs
@@ -1,32 +1,54 @@
-use async_graphql::*;
+use async_graphql::{dataloader::DataLoader, Context, Object};
 use repository::MasterList;
+
+use crate::{
+    loader::MasterListLineByMasterListId, standard_graphql_error::StandardGraphqlError, ContextExt,
+};
+
+use super::MasterListLineConnector;
 
 #[derive(PartialEq, Debug)]
 pub struct MasterListNode {
-    masterlist: MasterList,
+    master_list: MasterList,
 }
 
 #[Object]
 impl MasterListNode {
     pub async fn id(&self) -> &str {
-        &self.masterlist.id
+        &self.master_list.id
     }
 
     pub async fn name(&self) -> &str {
-        &self.masterlist.name
+        &self.master_list.name
     }
 
     pub async fn code(&self) -> &str {
-        &self.masterlist.code
+        &self.master_list.code
     }
 
     pub async fn description(&self) -> &str {
-        &self.masterlist.description
+        &self.master_list.description
+    }
+
+    pub async fn lines(
+        &self,
+        ctx: &Context<'_>,
+    ) -> Result<MasterListLineConnector, StandardGraphqlError> {
+        let loader = ctx.get_loader::<DataLoader<MasterListLineByMasterListId>>();
+
+        let lines_option = loader.load_one(self.master_list.id.clone()).await?;
+
+        let result = match lines_option {
+            None => MasterListLineConnector::empty(),
+            Some(lines) => MasterListLineConnector::from_domain_vec(lines),
+        };
+
+        Ok(result)
     }
 }
 
 impl MasterListNode {
-    pub fn from_domain(masterlist: MasterList) -> Self {
-        MasterListNode { masterlist }
+    pub fn from_domain(master_list: MasterList) -> Self {
+        MasterListNode { master_list }
     }
 }
