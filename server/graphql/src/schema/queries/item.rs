2342d4fd5cb4ad400d5e8fed19fd54efffa598d9
Clemens Zeidler <clemens@msupply.foundation>
2021-12-10 13:35:48 +1300

    Move item query function into its own file
    Source: https://github.com/msupply-foundation/remote-server/commit/b1cfddd1cb4db927990f6a90e70639d76dce3761

---
+++ server/graphql/src/schema/queries/item.rs
@@ -0,0 +1,64 @@
+use async_graphql::*;
+use domain::{item::ItemFilter, PaginationOption, SimpleStringFilter};
+use service::item::get_items;
+
+use crate::{
+    schema::types::{
+        sort_filter_types::{
+            convert_sort, EqualFilterBoolInput, SimpleStringFilterInput, SortInput,
+        },
+        Connector, ConnectorError, ItemNode, PaginationInput,
+    },
+    ContextExt,
+};
+
+#[derive(Enum, Copy, Clone, PartialEq, Eq)]
+#[graphql(remote = "domain::item::ItemSortField")]
+#[graphql(rename_items = "camelCase")]
+pub enum ItemSortFieldInput {
+    Name,
+    Code,
+}
+pub type ItemSortInput = SortInput<ItemSortFieldInput>;
+
+#[derive(InputObject, Clone)]
+pub struct ItemFilterInput {
+    pub name: Option<SimpleStringFilterInput>,
+    pub code: Option<SimpleStringFilterInput>,
+    pub is_visible: Option<EqualFilterBoolInput>,
+}
+
+impl From<ItemFilterInput> for ItemFilter {
+    fn from(f: ItemFilterInput) -> Self {
+        ItemFilter {
+            id: None,
+            name: f.name.map(SimpleStringFilter::from),
+            code: f.code.map(SimpleStringFilter::from),
+            is_visible: f.is_visible.and_then(|filter| filter.equal_to),
+        }
+    }
+}
+
+#[derive(Union)]
+pub enum ItemsResponse {
+    Error(ConnectorError),
+    Response(Connector<ItemNode>),
+}
+
+pub fn items(
+    ctx: &Context<'_>,
+    page: Option<PaginationInput>,
+    filter: Option<ItemFilterInput>,
+    sort: Option<Vec<ItemSortInput>>,
+) -> ItemsResponse {
+    let connection_manager = ctx.get_connection_manager();
+    match get_items(
+        connection_manager,
+        page.map(PaginationOption::from),
+        filter.map(ItemFilter::from),
+        convert_sort(sort),
+    ) {
+        Ok(items) => ItemsResponse::Response(items.into()),
+        Err(error) => ItemsResponse::Error(error.into()),
+    }
+}
