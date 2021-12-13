8ee754adf9b69d71f3ce658bd59328507033eeee
Andrei <andrei.evguenov@gmail.com>
2021-12-14 03:24:10 +1300

    Move logic of number generation to service
    Source: https://github.com/msupply-foundation/remote-server/commit/8514535c2885abb60bd5ccfe18971a7318355759

--- server/service/src/number.rs
+++ server/service/src/number.rs
@@ -1,19 +1,17 @@
 use repository::{
     schema::{NumberRow, NumberRowType},
-    NumberRowRepository, RepositoryError,
+    NumberRowRepository, RepositoryError, StorageConnection,
 };
 use util::uuid::uuid;
 
-use crate::service_provider::ServiceContext;
-
 pub fn next_number(
-    ctx: &ServiceContext,
+    connection: &StorageConnection,
     r#type: &NumberRowType,
     store_id: &str,
-) -> Result<i64, RepositoryError> {
+) -> Result<i32, RepositoryError> {
     // Should be done in transaction
-    let next_number = ctx.transaction(|ctx| {
-        let repo = NumberRowRepository::new(&ctx.connection);
+    let next_number = connection.transaction_sync(|connection_tx| {
+        let repo = NumberRowRepository::new(&connection_tx);
 
         let updated_number_row = match repo.find_one_by_type_and_store(r#type, store_id)? {
             Some(mut row) => {
@@ -37,4 +35,45 @@ pub fn next_number(
         Ok(updated_number_row.value)
     })?;
     Ok(next_number)
+}
+
+#[cfg(test)]
+mod test {
+    use repository::{
+        mock::{
+            mock_inbound_shipment_number_store_a, mock_outbound_shipment_number_store_a,
+            MockDataInserts,
+        },
+        schema::NumberRowType,
+        test_db::setup_all,
+    };
+
+    use crate::number::next_number;
+
+    #[actix_rt::test]
+    async fn test_number_service() {
+        let (_, connection, _, _) = setup_all("test_number_service", MockDataInserts::all()).await;
+
+        let inbound_shipment_store_a_number = mock_inbound_shipment_number_store_a();
+        let outbound_shipment_store_b_number = mock_outbound_shipment_number_store_a();
+
+        // Test existing
+
+        let result = next_number(&connection, &NumberRowType::InboundShipment, "store_a").unwrap();
+        assert_eq!(result, inbound_shipment_store_a_number.value + 1);
+
+        let result = next_number(&connection, &NumberRowType::InboundShipment, "store_a").unwrap();
+        assert_eq!(result, inbound_shipment_store_a_number.value + 2);
+
+        let result = next_number(&connection, &NumberRowType::OutboundShipment, "store_a").unwrap();
+        assert_eq!(result, outbound_shipment_store_b_number.value + 1);
+
+        // Test new
+
+        let result = next_number(&connection, &NumberRowType::OutboundShipment, "store_b").unwrap();
+        assert_eq!(result, 1);
+
+        let result = next_number(&connection, &NumberRowType::OutboundShipment, "store_b").unwrap();
+        assert_eq!(result, 2);
+    }
 }
