0e693fee86b26845962182ccd93314490b24f930
Clemens Zeidler <clemens@msupply.foundation>
2021-12-16 08:47:41 +1300

    Add stock take status constraints

--- server/repository/migrations/postgres/00000119_stock_take_table/up.sql
+++ server/repository/migrations/postgres/00000119_stock_take_table/up.sql
@@ -1,9 +1,14 @@
+CREATE TYPE stock_take_status AS ENUM (
+    'NEW',
+    'FINALIZED'
+);
+
 CREATE TABLE stock_take (
     id TEXT NOT NULL PRIMARY KEY,
     store_id TEXT NOT NULL REFERENCES store(id),
     comment	TEXT,
     description TEXT,
-    status TEXT,
+    status stock_take_status NOT NULL,
     created_datetime TIMESTAMP NOT NULL,
     finalised_datetime TIMESTAMP,
     inventory_additions_id TEXT REFERENCES invoice(id),
