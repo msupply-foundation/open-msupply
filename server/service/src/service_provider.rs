fd8f7e9078930ad36f27feb34d859b4db688f97d
Clemens Zeidler <clemens@msupply.foundation>
2021-12-20 14:09:45 +1300

    Make PermissionSerivce configurable
    Return Admin user as a default for the time being.
    
    Source: https://github.com/msupply-foundation/remote-server/commit/7ab4c9ebd89456dfdaeee97e0388e57a60670211

--- server/service/src/service_provider.rs
+++ server/service/src/service_provider.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use repository::{RepositoryError, StorageConnection, StorageConnectionManager};
 
 use crate::{
@@ -8,12 +10,14 @@ use crate::{
     location::{LocationService, LocationServiceTrait},
     master_list::{MasterListService, MasterListServiceTrait},
     permission_validation::{ValidationService, ValidationServiceTrait},
+    permissions::{PermissionService, PermissionServiceTrait},
     stock_take::{StockTakeService, StockTakeServiceTrait},
     stock_take_line::{StockTakeLineService, StockTakeLineServiceTrait},
 };
 
 pub struct ServiceProvider {
     pub connection_manager: StorageConnectionManager,
+    pub permission_service: Arc<dyn PermissionServiceTrait>,
     pub validation_service: Box<dyn ValidationServiceTrait>,
 
     pub location_service: Box<dyn LocationServiceTrait>,
@@ -31,9 +35,11 @@ pub struct ServiceContext {
 
 impl ServiceProvider {
     pub fn new(connection_manager: StorageConnectionManager) -> Self {
+        let permission_service = Arc::new(PermissionService::new());
         ServiceProvider {
             connection_manager,
-            validation_service: Box::new(ValidationService::new()),
+            permission_service: permission_service.clone(),
+            validation_service: Box::new(ValidationService::new(permission_service)),
             location_service: Box::new(LocationService {}),
             master_list_service: Box::new(MasterListService {}),
             invoice_count_service: Box::new(InvoiceCountService {}),
