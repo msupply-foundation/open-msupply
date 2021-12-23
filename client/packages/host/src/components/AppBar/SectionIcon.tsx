14f3a958dca07d18734018017d5106ff7ad67bed
Mark Prins <mark@malcolmprins.net>
2021-12-23 19:21:57 +1300

    use object in sectionIcon

--- client/packages/host/src/components/AppBar/SectionIcon.tsx
+++ client/packages/host/src/components/AppBar/SectionIcon.tsx
@@ -14,39 +14,9 @@ import {
 } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
 
-const getSection = () => {
-  const routes = [
-    AppRoute.Catalogue,
-    AppRoute.Distribution,
-    AppRoute.Inventory,
-    AppRoute.Replenishment,
-    AppRoute.Reports,
-  ];
-  const location = useLocation();
-
-  for (let i = 0; i < routes.length; i++) {
-    const route = routes[i];
-    const match = matchPath(
-      RouteBuilder.create(route ?? '')
-        .addWildCard()
-        .build(),
-      location.pathname
-    );
-    if (!!match) return route;
-  }
-  return undefined;
-};
-
-export const SectionIcon: React.FC = () => {
-  const t = useTranslation('app');
-  const section = getSection();
-  const icon = getIcon(section);
-
-  return icon ? (
-    <Tooltip title={t(section as LocaleKey)}>
-      <div>{icon}</div>
-    </Tooltip>
-  ) : null;
+type Section = {
+  icon?: JSX.Element;
+  titleKey: LocaleKey;
 };
 
 const getIcon = (section?: AppRoute) => {
@@ -64,4 +34,42 @@ const getIcon = (section?: AppRoute) => {
     default:
       return undefined;
   }
+};
+
+const getSection = (): Section | undefined => {
+  const routes = [
+    AppRoute.Catalogue,
+    AppRoute.Distribution,
+    AppRoute.Inventory,
+    AppRoute.Replenishment,
+    AppRoute.Reports,
+  ];
+  const location = useLocation();
+
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[i];
+    const match = matchPath(
+      RouteBuilder.create(route ?? '')
+        .addWildCard()
+        .build(),
+      location.pathname
+    );
+    if (!!match)
+      return {
+        icon: getIcon(route),
+        titleKey: route as LocaleKey,
+      };
+  }
+  return undefined;
+};
+
+export const SectionIcon: React.FC = () => {
+  const t = useTranslation('app');
+  const section = getSection();
+
+  return section?.icon ? (
+    <Tooltip title={t(section?.titleKey)}>
+      <div>{section.icon}</div>
+    </Tooltip>
+  ) : null;
 };
