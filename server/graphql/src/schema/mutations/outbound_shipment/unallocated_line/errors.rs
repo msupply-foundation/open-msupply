b666f03c5af5ecd78877e21044e90ec4684e63e6
Andrei <andrei.evguenov@gmail.com>
2022-01-05 17:27:26 +1300

    Relocate errors from mod.rs to errors.rs

---
+++ server/graphql/src/schema/mutations/outbound_shipment/unallocated_line/errors.rs
@@ -0,0 +1,15 @@
+pub struct UnallocatedLinesOnlyEditableInNewInvoice;
+#[Object]
+impl UnallocatedLinesOnlyEditableInNewInvoice {
+    pub async fn description(&self) -> &'static str {
+        "Can only insert or edit unallocated lines in new invoice"
+    }
+}
+
+pub struct UnallocatedLineForItemAlreadyExists;
+#[Object]
+impl UnallocatedLineForItemAlreadyExists {
+    pub async fn description(&self) -> &'static str {
+        "Unallocated line already exists for this item"
+    }
+}
