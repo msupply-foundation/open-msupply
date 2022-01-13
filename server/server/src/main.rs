36df815ab01d0dc330f7dcbb7dc5a56a1ba56b6e
Clemens Zeidler <clemens@msupply.foundation>
2022-01-14 12:50:48 +1300

    Add option to shut down the remote server
    Source: https://github.com/msupply-foundation/remote-server/commit/0c120c1e319a5eeda7957b47fbe4b27fa66801b1

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -3,6 +3,7 @@
 use std::env;
 
 use server::{configuration, settings::Settings, start_server};
+use tokio::sync::oneshot;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,5 +13,6 @@ async fn main() -> std::io::Result<()> {
     let settings: Settings =
         configuration::get_configuration().expect("Failed to parse configuration settings");
 
-    start_server(settings).await
+    let (_, off_switch) = oneshot::channel();
+    start_server(settings, off_switch).await
 }
