89fe06adf00842addd1f02d7abce79fb074e2428
Mark Prins <mark@malcolmprins.net>
2022-01-24 16:21:26 +1300

    custom theme support
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/36ff2c6abb6740d988f50f985554d1e2fc47ee6a

--- client/packages/common/src/styles/useAppTheme.ts
+++ client/packages/common/src/styles/useAppTheme.ts
@@ -1,11 +1,17 @@
 import { Direction } from '@mui/material/styles';
 import { Theme } from '@mui/material';
 import { useState, useEffect } from 'react';
-import theme from './theme';
+import { themeOptions, createTheme } from './theme';
 import { useRtl } from '@common/intl';
+import { useLocalStorage } from '../localStorage';
+import merge from 'lodash/merge';
 
 export const useAppTheme = (): Theme => {
-  const [currentTheme, setTheme] = useState(theme);
+  const [customTheme] = useLocalStorage('/theme/custom');
+  const appTheme = customTheme
+    ? createTheme(merge(themeOptions, customTheme))
+    : createTheme(themeOptions);
+  const [currentTheme, setTheme] = useState(appTheme);
   const isRtl = useRtl();
 
   const setDirection = (newDirection: Direction) => {
