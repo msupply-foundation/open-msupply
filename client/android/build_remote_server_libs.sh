d033e6705086d2931c274a95c45cb679e57a517d
Clemens Zeidler <clemens@msupply.foundation>
2022-01-26 10:27:39 +1300

    Simplify android build setup
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/e20c7e86987c3c391f546772f92919c8ec5c35d0

--- client/android/build_remote_server_libs.sh
+++ client/android/build_remote_server_libs.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
+set -e
 
+# Path the the remote server dir
+REMOTE_SERVER_DIR="../../remote-server"
+# The dir where the android project is:
+ANDROID_DIR="."
+
+# Make the remote server libs:
+CURRENT_PWD=`pwd`
+cd $REMOTE_SERVER_DIR/android
+make android
+cd $CURRENT_PWD
+
+
+# Copy the libs over to the Android dir:
 LIB_NAME=libremote_server_android
 
-ANDROID_DIR="../android"
 JNILIBS="$ANDROID_DIR/app/src/main/jniLibs"
 
 mkdir -p $JNILIBS
@@ -10,7 +23,7 @@ mkdir -p $JNILIBS/x86
 mkdir -p $JNILIBS/arm64-v8a
 mkdir -p $JNILIBS/armeabi-v7a
 
-RUST_OUTPUT="./remote-server/target"
+RUST_OUTPUT="$REMOTE_SERVER_DIR/target"
 cp $RUST_OUTPUT/i686-linux-android/release/$LIB_NAME.so $JNILIBS/x86/$LIB_NAME.so
 cp $RUST_OUTPUT/aarch64-linux-android/release/$LIB_NAME.so $JNILIBS/arm64-v8a/$LIB_NAME.so
 cp $RUST_OUTPUT/armv7-linux-androideabi/release/$LIB_NAME.so $JNILIBS/armeabi-v7a/$LIB_NAME.so
