2cbf9ee3e24722235fcf6827b3a82c248e591e44
Andrei <andrei.evguenov@gmail.com>
2022-01-28 02:40:45 +1300

    Change visibility and type of requisition line fields

--- server/repository/src/schema/requisition_line.rs
+++ server/repository/src/schema/requisition_line.rs
@@ -1,16 +1,16 @@
 use super::diesel_schema::requisition_line;
 
-#[derive(Clone, Queryable, Insertable, Debug, PartialEq)]
+#[derive(Clone, Queryable, AsChangeset, Insertable, Debug, PartialEq)]
 #[table_name = "requisition_line"]
 pub struct RequisitionLineRow {
     pub id: String,
     pub requisition_id: String,
     pub item_id: String,
-    pub requested_quantity: f64,
-    pub calculated_quantity: f64,
-    pub supply_quantity: f64,
-    request_stock_on_hand: i32,
-    request_average_monthly_consumption: i32,
-    response_stock_on_hand: i32,
-    response_average_monthly_consumption: i32,
+    pub requested_quantity: i32,
+    pub calculated_quantity: i32,
+    pub supply_quantity: i32,
+    pub request_stock_on_hand: i32,
+    pub request_average_monthly_consumption: i32,
+    pub response_stock_on_hand: i32,
+    pub response_average_monthly_consumption: i32,
 }
