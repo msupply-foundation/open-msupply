74099a8c08e0018bab86a73cd82b1df320f97eaf
Andrei <andrei.evguenov@gmail.com>
2022-01-27 16:47:45 +1300

    Add database changes to diesel schema
    This reverts commit 86dc5bd161cfe7008967def938eddfb53f428019.

--- server/repository/src/schema/requisition.rs
+++ server/repository/src/schema/requisition.rs
@@ -1,21 +1,39 @@
 use super::diesel_schema::requisition;
+use chrono::NaiveDateTime;
 use diesel_derive_enum::DbEnum;
 
 #[derive(DbEnum, Debug, Clone, PartialEq, Eq)]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
 pub enum RequisitionRowType {
-    Imprest,
-    StockHistory,
     Request,
     Response,
-    Supply,
-    Report,
+}
+#[derive(DbEnum, Debug, Clone, PartialEq, Eq)]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
+pub enum RequisitionRowStatus {
+    Draft,
+    New,
+    Sent,
+    Finalised,
 }
 
-#[derive(Clone, Queryable, Insertable, Debug, PartialEq, Eq)]
+#[derive(Clone, Queryable, Insertable, Debug, PartialEq)]
 #[table_name = "requisition"]
 pub struct RequisitionRow {
     pub id: String,
+    pub requisition_number: i64,
     pub name_id: String,
     pub store_id: String,
-    pub type_of: RequisitionRowType,
+    #[column_name = "type_"]
+    pub r#type: RequisitionRowType,
+    pub status: RequisitionRowStatus,
+    pub created_datetime: NaiveDateTime,
+    pub sent_datetime: Option<NaiveDateTime>,
+    pub finalised_datetime: Option<NaiveDateTime>,
+    pub color: Option<String>,
+    pub comment: Option<String>,
+    pub their_reference: Option<String>,
+    pub max_months_of_stock: f64,
+    pub threshold_months_of_stock: f64,
+    pub linked_requisition_id: Option<String>,
 }
