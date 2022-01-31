49b5adc5c83d2484d200b555d6a87ac97b0edc64
Andrei <andrei.evguenov@gmail.com>
2022-01-31 16:47:41 +1300

    Add InvoiceLineForRequisitionLine loader
    Source: https://github.com/msupply-foundation/remote-server/commit/7bbda172bd87df3841a0d24e2858a203f79be795

--- server/graphql/src/loader/invoice_line_query.rs
+++ server/graphql/src/loader/invoice_line_query.rs
@@ -6,6 +6,10 @@ use std::collections::HashMap;
 
 use service::ListError;
 
+use crate::standard_graphql_error::StandardGraphqlError;
+
+use super::{extract_unique_requisition_and_item_id, RequisitionAndItemId};
+
 pub struct InvoiceLineQueryLoader {
     pub connection_manager: StorageConnectionManager,
 }
@@ -34,6 +38,49 @@ impl Loader<String> for InvoiceLineQueryLoader {
                 .entry(line.invoice_id.clone())
                 .or_insert_with(|| Vec::<InvoiceLine>::new());
             list.push(line);
+        }
+        Ok(map)
+    }
+}
+
+pub struct InvoiceLineForRequisitionLine {
+    pub connection_manager: StorageConnectionManager,
+}
+
+#[async_trait::async_trait]
+impl Loader<RequisitionAndItemId> for InvoiceLineForRequisitionLine {
+    type Value = Vec<InvoiceLine>;
+    type Error = async_graphql::Error;
+
+    async fn load(
+        &self,
+        requisition_and_item_id: &[RequisitionAndItemId],
+    ) -> Result<HashMap<RequisitionAndItemId, Self::Value>, Self::Error> {
+        let connection = self.connection_manager.connection()?;
+        let repo = InvoiceLineRepository::new(&connection);
+
+        let (requisition_ids, item_ids) =
+            extract_unique_requisition_and_item_id(requisition_and_item_id);
+
+        let all_invoice_lines = repo
+            .query_by_filter(
+                InvoiceLineFilter::new()
+                    .requisition_id(EqualFilter::equal_any(requisition_ids))
+                    .item_id(EqualFilter::equal_any(item_ids)),
+            )
+            .map_err(StandardGraphqlError::from_repository_error)?;
+
+        let mut map: HashMap<RequisitionAndItemId, Vec<InvoiceLine>> = HashMap::new();
+        for line in all_invoice_lines {
+            if let Some(requisition_id) = &line.requisition_id {
+                let list = map
+                    .entry(RequisitionAndItemId {
+                        item_id: line.item_id.clone(),
+                        requisition_id: requisition_id.clone(),
+                    })
+                    .or_insert_with(|| Vec::<InvoiceLine>::new());
+                list.push(line);
+            }
         }
         Ok(map)
     }
