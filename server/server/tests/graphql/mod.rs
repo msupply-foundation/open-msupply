5fe1ae9715a120dd135a695e5291fc52c533ec83
Andrei <andrei.evguenov@gmail.com>
2022-01-31 16:45:16 +1300

    Add loaders to registry and add service provider to get_loaders
    Source: https://github.com/msupply-foundation/remote-server/commit/f62df1d0bddb2c79e8056db0235df8f3f0ebbf57

--- server/server/tests/graphql/mod.rs
+++ server/server/tests/graphql/mod.rs
@@ -37,7 +37,6 @@ mod outbound_shipment_line_insert;
 mod outbound_shipment_line_update;
 mod outbound_shipment_update;
 mod pagination;
-mod requisition;
 mod stock_take_delete;
 mod stock_take_insert;
 mod stock_take_line_delete;
@@ -53,12 +52,13 @@ where
     OUT: DeserializeOwned,
 {
     let connection_manager = get_storage_connection_manager(&settings.database);
-    let loaders = get_loaders(&connection_manager).await;
-
     let connection_manager_data = actix_web::web::Data::new(connection_manager.clone());
-    let loader_registry = actix_web::web::Data::new(LoaderRegistry { loaders });
+
     let service_provider_data =
         actix_web::web::Data::new(ServiceProvider::new(connection_manager.clone()));
+
+    let loaders = get_loaders(&connection_manager, service_provider_data.clone()).await;
+    let loader_registry = actix_web::web::Data::new(LoaderRegistry { loaders });
 
     let auth_data = Data::new(AuthData {
         auth_token_secret: settings.auth.token_secret.to_owned(),
@@ -105,14 +105,15 @@ async fn run_gql_query(
     service_provider_override: Option<ServiceProvider>,
 ) -> serde_json::Value {
     let connection_manager = get_storage_connection_manager(&settings.database);
-    let loaders = get_loaders(&connection_manager).await;
-
     let connection_manager_data = actix_web::web::Data::new(connection_manager.clone());
-    let loader_registry = actix_web::web::Data::new(LoaderRegistry { loaders });
+
     let service_provider_data = actix_web::web::Data::new(match service_provider_override {
         Some(service_provider) => service_provider,
         None => ServiceProvider::new(connection_manager.clone()),
     });
+
+    let loaders = get_loaders(&connection_manager, service_provider_data.clone()).await;
+    let loader_registry = actix_web::web::Data::new(LoaderRegistry { loaders });
 
     let auth_data = Data::new(AuthData {
         auth_token_secret: settings.auth.token_secret.to_owned(),
