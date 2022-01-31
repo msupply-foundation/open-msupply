b5a57f1891a5f2820f3ff584508cf4eccb9e1171
Andrei <andrei.evguenov@gmail.com>
2022-01-31 16:43:47 +1300

    Make store_id optional in requisition service

--- server/service/src/requisition/mod.rs
+++ server/service/src/requisition/mod.rs
@@ -3,7 +3,9 @@ use self::query::{get_requisition, get_requisition_by_number, get_requisitions};
 use super::{ListError, ListResult};
 use crate::service_provider::ServiceContext;
 use domain::PaginationOption;
-use repository::{schema::RequisitionRowType, Requisition, RequisitionFilter, RequisitionSort};
+use repository::{
+    schema::RequisitionRowType, RepositoryError, Requisition, RequisitionFilter, RequisitionSort,
+};
 
 pub mod query;
 
@@ -11,21 +13,21 @@ pub trait RequisitionServiceTrait: Sync + Send {
     fn get_requisitions(
         &self,
         ctx: &ServiceContext,
-        store_id: &str,
+        store_id_option: Option<&str>,
         pagination: Option<PaginationOption>,
         filter: Option<RequisitionFilter>,
         sort: Option<RequisitionSort>,
     ) -> Result<ListResult<Requisition>, ListError> {
-        get_requisitions(ctx, store_id, pagination, filter, sort)
+        get_requisitions(ctx, store_id_option, pagination, filter, sort)
     }
 
     fn get_requisition(
         &self,
         ctx: &ServiceContext,
-        store_id: &str,
+        store_id_option: Option<&str>,
         id: &str,
-    ) -> Result<Option<Requisition>, ListError> {
-        get_requisition(ctx, store_id, id)
+    ) -> Result<Option<Requisition>, RepositoryError> {
+        get_requisition(ctx, store_id_option, id)
     }
 
     fn get_requisition_by_number(
@@ -34,7 +36,7 @@ pub trait RequisitionServiceTrait: Sync + Send {
         store_id: &str,
         requisition_number: u32,
         r#type: RequisitionRowType,
-    ) -> Result<Option<Requisition>, ListError> {
+    ) -> Result<Option<Requisition>, RepositoryError> {
         get_requisition_by_number(ctx, store_id, requisition_number, r#type)
     }
 }
