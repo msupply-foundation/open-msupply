0a9e844f0d5a9f14157e494627dd1b732f0accf9
Andrei <andrei.evguenov@gmail.com>
2022-01-31 16:51:12 +1300

    Add base implementation of ItemStats node
    Source: https://github.com/msupply-foundation/remote-server/commit/280412a1d4d13bc7dd4481b8caeca2c9459aef14

--- server/graphql/src/schema/types/item_stats.rs
+++ server/graphql/src/schema/types/item_stats.rs
@@ -1,17 +1,20 @@
 use async_graphql::*;
-pub struct ItemStats {}
+pub struct ItemStats {
+    pub average_monthly_consumption: i32,
+    pub stock_on_hand: i32,
+}
 
 #[Object]
 impl ItemStats {
-    pub async fn average_monthly_consumption(&self) -> f64 {
-        todo!()
+    pub async fn average_monthly_consumption(&self) -> i32 {
+        self.average_monthly_consumption
     }
 
-    pub async fn stock_on_hand(&self) -> u32 {
-        todo!()
+    pub async fn stock_on_hand(&self) -> i32 {
+        self.stock_on_hand
     }
 
     pub async fn months_of_stock(&self) -> f64 {
-        todo!()
+        self.stock_on_hand as f64 / self.average_monthly_consumption as f64
     }
 }
