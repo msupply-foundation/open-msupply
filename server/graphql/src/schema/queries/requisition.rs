0a1c5abb3c32d24c23d4449a0d64f06e1fdfa3a0
Andrei <andrei.evguenov@gmail.com>
2022-01-31 16:25:56 +1300

    Implement requisition graphql query

--- server/graphql/src/schema/queries/requisition.rs
+++ server/graphql/src/schema/queries/requisition.rs
@@ -1,8 +1,19 @@
 use async_graphql::*;
+use domain::{DatetimeFilter, EqualFilter, PaginationOption, SimpleStringFilter};
+use repository::{RequisitionFilter, RequisitionSort, RequisitionSortField};
+use service::permission_validation::{Resource, ResourceAccessRequest};
 
-use crate::schema::types::{
-    sort_filter_types::{DatetimeFilterInput, EqualFilterBigNumberInput, EqualFilterInput},
-    PaginationInput, RequisitionNode, RequisitionNodeStatus, RequisitionNodeType,
+use crate::{
+    schema::types::{
+        sort_filter_types::{
+            DatetimeFilterInput, EqualFilterBigNumberInput, EqualFilterInput,
+            SimpleStringFilterInput,
+        },
+        PaginationInput, RecordNotFound, RequisitionConnector, RequisitionNode,
+        RequisitionNodeStatus, RequisitionNodeType,
+    },
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
 };
 
 use super::EqualFilterStringInput;
@@ -37,17 +48,11 @@ pub struct RequisitionFilterInput {
     pub created_datetime: Option<DatetimeFilterInput>,
     pub sent_datetime: Option<DatetimeFilterInput>,
     pub finalised_datetime: Option<DatetimeFilterInput>,
-    pub other_party_name: Option<EqualFilterStringInput>,
+    pub other_party_name: Option<SimpleStringFilterInput>,
     pub other_party_id: Option<EqualFilterStringInput>,
     pub colour: Option<EqualFilterStringInput>,
-    pub their_reference: Option<EqualFilterStringInput>,
-    pub comment: Option<EqualFilterStringInput>,
-}
-
-#[derive(SimpleObject)]
-pub struct RequisitionConnector {
-    total_count: u32,
-    nodes: Vec<RequisitionNode>,
+    pub their_reference: Option<SimpleStringFilterInput>,
+    pub comment: Option<SimpleStringFilterInput>,
 }
 
 #[derive(Union)]
@@ -55,34 +60,154 @@ pub enum RequisitionsResponse {
     Response(RequisitionConnector),
 }
 
-pub fn get_requisitions(
-    _ctx: &Context<'_>,
-    _store_id: Option<String>,
-    _page: Option<PaginationInput>,
-    _filter: Option<RequisitionFilterInput>,
-    _sort: Option<Vec<RequisitionSortInput>>,
-) -> Result<RequisitionsResponse> {
-    todo!()
-}
-
 #[derive(Union)]
 pub enum RequisitionResponse {
+    Error(RecordNotFound),
     Response(RequisitionNode),
 }
 
-pub fn get_requisition(
-    _ctx: &Context<'_>,
-    _store_id: Option<String>,
-    _id: String,
-) -> Result<RequisitionResponse> {
-    todo!()
+pub fn get_requisition(ctx: &Context<'_>, store_id: &str, id: &str) -> Result<RequisitionResponse> {
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::QueryRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    let requisition_option = service_provider.requisition_service.get_requisition(
+        &service_context,
+        Some(store_id),
+        id,
+    )?;
+
+    let response = match requisition_option {
+        Some(requisition) => {
+            RequisitionResponse::Response(RequisitionNode::from_domain(requisition))
+        }
+        None => RequisitionResponse::Error(RecordNotFound {}),
+    };
+
+    Ok(response)
+}
+
+pub fn get_requisitions(
+    ctx: &Context<'_>,
+    store_id: &str,
+    page: Option<PaginationInput>,
+    filter: Option<RequisitionFilterInput>,
+    sort: Option<Vec<RequisitionSortInput>>,
+) -> Result<RequisitionsResponse> {
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::QueryRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    let requisitions = service_provider
+        .requisition_service
+        .get_requisitions(
+            &service_context,
+            Some(&store_id),
+            page.map(PaginationOption::from),
+            filter.map(|filter| filter.to_domain()),
+            // Currently only one sort option is supported, use the first from the list.
+            sort.map(|mut sort_list| sort_list.pop())
+                .flatten()
+                .map(|sort| sort.to_domain()),
+        )
+        .map_err(StandardGraphqlError::from_list_error)?;
+
+    Ok(RequisitionsResponse::Response(
+        RequisitionConnector::from_domain(requisitions),
+    ))
 }
 
 pub fn get_requisition_by_number(
-    _ctx: &Context<'_>,
-    _store_id: Option<String>,
-    _requisition_number: u32,
-    _type: RequisitionNodeType,
+    ctx: &Context<'_>,
+    store_id: &str,
+    requisition_number: u32,
+    r#type: RequisitionNodeType,
 ) -> Result<RequisitionResponse> {
-    todo!()
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::QueryRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    let requisition_option = service_provider
+        .requisition_service
+        .get_requisition_by_number(
+            &service_context,
+            store_id,
+            requisition_number,
+            r#type.to_domain(),
+        )?;
+
+    let response = match requisition_option {
+        Some(requisition) => {
+            RequisitionResponse::Response(RequisitionNode::from_domain(requisition))
+        }
+        None => RequisitionResponse::Error(RecordNotFound {}),
+    };
+
+    Ok(response)
+}
+
+impl RequisitionSortInput {
+    pub fn to_domain(self) -> RequisitionSort {
+        use RequisitionSortField as to;
+        use RequisitionSortFieldInput as from;
+        let key = match self.key {
+            from::RequisitionNumber => to::RequisitionNumber,
+            from::Type => to::Type,
+            from::Status => to::Status,
+            from::OtherPartyName => to::OtherPartyName,
+            from::SentDatetime => to::SentDatetime,
+            from::CreatedDatetime => to::CreatedDatetime,
+            from::FinalisedDatetime => to::FinalisedDatetime,
+        };
+
+        RequisitionSort {
+            key,
+            desc: self.desc,
+        }
+    }
+}
+
+impl RequisitionFilterInput {
+    pub fn to_domain(self) -> RequisitionFilter {
+        RequisitionFilter {
+            id: self.id.map(EqualFilter::from),
+            requisition_number: self.requisition_number.map(EqualFilter::from),
+            r#type: self
+                .r#type
+                .map(|t| t.map_to_domain(RequisitionNodeType::to_domain)),
+            status: self
+                .status
+                .map(|t| t.map_to_domain(RequisitionNodeStatus::to_domain)),
+            created_datetime: self.created_datetime.map(DatetimeFilter::from),
+            sent_datetime: self.sent_datetime.map(DatetimeFilter::from),
+            finalised_datetime: self.finalised_datetime.map(DatetimeFilter::from),
+            name: self.other_party_name.map(SimpleStringFilter::from),
+            name_id: self.other_party_id.map(EqualFilter::from),
+            colour: self.colour.map(EqualFilter::from),
+            their_reference: self.their_reference.map(SimpleStringFilter::from),
+            comment: self.comment.map(SimpleStringFilter::from),
+            store_id: None,
+        }
+    }
 }
