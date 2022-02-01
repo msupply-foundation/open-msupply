105ce1a0f5b3a13c27937d715ad3a708aa4ca662
Andrei <andrei.evguenov@gmail.com>
2022-02-01 23:24:10 +1300

    Use item stats in add from master list

--- server/service/src/requisition/request_requisition/generate.rs
+++ server/service/src/requisition/request_requisition/generate.rs
@@ -1,3 +1,10 @@
+use domain::EqualFilter;
+use repository::{
+    schema::{RequisitionLineRow, RequisitionRow},
+    ItemStatsFilter, ItemStatsRepository, RepositoryError, StorageConnection,
+};
+use util::uuid::uuid;
+
 pub struct GenerateCalculatedQuantity {
     pub average_monthly_consumption: i32,
     pub stock_on_hand: i32,
@@ -22,5 +29,44 @@ pub fn generate_calculated_quantity(
         return 0;
     }
 
-    (max_months_of_stock - months_of_stock * average_monthly_consumption as f64) as i32
+    ((max_months_of_stock - months_of_stock) * average_monthly_consumption as f64) as i32
+}
+
+pub fn generate_requisition_lines(
+    connection: &StorageConnection,
+    store_id: &str,
+    requisition_row: &RequisitionRow,
+    item_ids: Vec<String>,
+) -> Result<Vec<RequisitionLineRow>, RepositoryError> {
+    let repository = ItemStatsRepository::new(&connection);
+
+    let filter = ItemStatsFilter::new().item_id(EqualFilter::equal_any(item_ids));
+    let item_stats_rows = repository.query(store_id, None, Some(filter))?;
+
+    let result = item_stats_rows
+        .into_iter()
+        .map(|item_stats| {
+            let average_monthly_consumption = item_stats.average_monthly_consumption();
+            let stock_on_hand = item_stats.stock_on_hand();
+            let calculated_quantity = generate_calculated_quantity(GenerateCalculatedQuantity {
+                average_monthly_consumption: average_monthly_consumption.clone(),
+                stock_on_hand: stock_on_hand.clone(),
+                threshold_months_of_stock: requisition_row.threshold_months_of_stock.clone(),
+                max_months_of_stock: requisition_row.max_months_of_stock.clone(),
+            });
+
+            RequisitionLineRow {
+                id: uuid(),
+                requisition_id: requisition_row.id.clone(),
+                item_id: item_stats.item_id,
+                calculated_quantity,
+                requested_quantity: 0,
+                supply_quantity: 0,
+                stock_on_hand,
+                average_monthly_consumption,
+            }
+        })
+        .collect();
+
+    Ok(result)
 }
