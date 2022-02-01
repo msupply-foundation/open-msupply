7b42592f3543811ab64c85b97ebcd44d45849d4f
Andrei <andrei.evguenov@gmail.com>
2022-02-01 23:22:40 +1300

    Add item stats views

---
+++ server/repository/migrations/postgres/2021-12-25T10:00_item_stats/down.sql
@@ -0,0 +1,2 @@
+DROP VIEW IF EXISTS item_stats CASCADE;
+
---
+++ server/repository/src/schema/item_stats.rs
@@ -0,0 +1,18 @@
+use chrono::NaiveDateTime;
+
+#[derive(Clone, Queryable, Debug, PartialEq)]
+pub struct ConsumptionRow {
+    pub id: String,
+    pub item_id: String,
+    pub store_id: String,
+    pub consumption_quantity: i32,
+    pub consumption_datetime: Option<NaiveDateTime>,
+}
+
+#[derive(Clone, Queryable, Debug, PartialEq)]
+pub struct StockInfoRow {
+    pub id: String,
+    pub item_id: String,
+    pub store_id: String,
+    pub stock_on_hand: i64,
+}
