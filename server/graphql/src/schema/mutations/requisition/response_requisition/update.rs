df3e9f0e01bfd1dd2e4cd019bf283c496f8c1cc7
Andrei <andrei.evguenov@gmail.com>
2022-02-02 15:11:11 +1300

    Add response requisition service to graphql mapping
    Source: https://github.com/msupply-foundation/remote-server/commit/4d5d8310cc24b241b342ab6d68e1e12ab4641231

--- server/graphql/src/schema/mutations/requisition/response_requisition/update.rs
+++ server/graphql/src/schema/mutations/requisition/response_requisition/update.rs
@@ -1,7 +1,18 @@
+use crate::{
+    schema::{
+        mutations::{requisition::errors::CannotEditRequisition, RecordDoesNotExist},
+        types::RequisitionNode,
+    },
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
 use async_graphql::*;
-
-use crate::schema::{
-    mutations::requisition::errors::CannotEditRequisition, types::RequisitionNode,
+use service::{
+    permission_validation::{Resource, ResourceAccessRequest},
+    requisition::response_requisition::{
+        UpdateResponseRequisition as ServiceInput, UpdateResponseRequisitionError as ServiceError,
+        UpdateResponseRequstionStatus,
+    },
 };
 
 #[derive(InputObject)]
@@ -23,6 +34,7 @@ pub enum UpdateResponseRequisitionStatusInput {
 #[graphql(name = "UpdateResponseRequisitionErrorInterface")]
 #[graphql(field(name = "description", type = "String"))]
 pub enum UpdateErrorInterface {
+    RecordDoesNotExist(RecordDoesNotExist),
     CannotEditRequisition(CannotEditRequisition),
 }
 
@@ -39,10 +51,82 @@ pub enum UpdateResponse {
     Response(RequisitionNode),
 }
 
-pub fn update(
-    _ctx: &Context<'_>,
-    _store_id: Option<String>,
-    _input: UpdateInput,
-) -> Result<UpdateResponse> {
-    todo!()
+pub fn update(ctx: &Context<'_>, store_id: &str, input: UpdateInput) -> Result<UpdateResponse> {
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::EditRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    let response = match service_provider
+        .requisition_service
+        .update_response_requisition(&service_context, store_id, input.to_domain())
+    {
+        Ok(requisition) => UpdateResponse::Response(RequisitionNode::from_domain(requisition)),
+        Err(error) => UpdateResponse::Error(UpdateError {
+            error: map_error(error)?,
+        }),
+    };
+
+    Ok(response)
+}
+
+impl UpdateInput {
+    fn to_domain(self) -> ServiceInput {
+        let UpdateInput {
+            id,
+            colour,
+            their_reference,
+            comment,
+            status,
+        } = self;
+
+        ServiceInput {
+            id,
+            colour,
+            their_reference,
+            comment,
+            status: status.map(|status| status.to_domain()),
+        }
+    }
+}
+
+fn map_error(error: ServiceError) -> Result<UpdateErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Structured Errors
+        ServiceError::RequisitionDoesNotExist => {
+            return Ok(UpdateErrorInterface::RecordDoesNotExist(
+                RecordDoesNotExist {},
+            ))
+        }
+        ServiceError::CannotEditRequisition => {
+            return Ok(UpdateErrorInterface::CannotEditRequisition(
+                CannotEditRequisition {},
+            ))
+        }
+        // Standard Graphql Errors
+        ServiceError::NotThisStoreRequisition => BadUserInput(formatted_error),
+        ServiceError::NotAResponseRequisition => BadUserInput(formatted_error),
+        ServiceError::UpdatedRequisitionDoesNotExist => InternalError(formatted_error),
+        ServiceError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
+}
+
+impl UpdateResponseRequisitionStatusInput {
+    pub fn to_domain(self) -> UpdateResponseRequstionStatus {
+        use UpdateResponseRequisitionStatusInput::*;
+        match self {
+            Finalised => UpdateResponseRequstionStatus::Finalised,
+        }
+    }
 }
