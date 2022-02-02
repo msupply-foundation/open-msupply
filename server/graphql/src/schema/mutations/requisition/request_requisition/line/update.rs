28e30b782fa35c49034677d3725327edd7d20a4e
Andrei <andrei.evguenov@gmail.com>
2022-02-02 16:01:07 +1300

    Add response and request requisition line service to graphql mapping
    Source: https://github.com/msupply-foundation/remote-server/commit/f69849d899d204147fecaa500517ab2f528b6e43

--- server/graphql/src/schema/mutations/requisition/request_requisition/line/update.rs
+++ server/graphql/src/schema/mutations/requisition/request_requisition/line/update.rs
@@ -1,8 +1,21 @@
+use crate::{
+    schema::{
+        mutations::{
+            requisition::errors::CannotEditRequisition, ForeignKey, ForeignKeyError,
+            RecordDoesNotExist,
+        },
+        types::RequisitionLineNode,
+    },
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
 use async_graphql::*;
-
-use crate::schema::{
-    mutations::{requisition::errors::CannotEditRequisition, RecordDoesNotExist},
-    types::RequisitionLineNode,
+use service::{
+    permission_validation::{Resource, ResourceAccessRequest},
+    requisition_line::request_requisition_line::{
+        UpdateRequestRequisitionLine as ServiceInput,
+        UpdateRequestRequisitionLineError as ServiceError,
+    },
 };
 
 #[derive(InputObject)]
@@ -17,6 +30,7 @@ pub struct UpdateInput {
 #[graphql(field(name = "description", type = "String"))]
 pub enum UpdateErrorInterface {
     RecordDoesNotExist(RecordDoesNotExist),
+    RequisitionDoesNotExist(ForeignKeyError),
     CannotEditRequisition(CannotEditRequisition),
 }
 
@@ -32,11 +46,74 @@ pub enum UpdateResponse {
     Error(UpdateError),
     Response(RequisitionLineNode),
 }
+pub fn update(ctx: &Context<'_>, store_id: &str, input: UpdateInput) -> Result<UpdateResponse> {
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::EditRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
 
-pub fn update(
-    _ctx: &Context<'_>,
-    _store_id: Option<String>,
-    _input: UpdateInput,
-) -> Result<UpdateResponse> {
-    todo!()
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    let response = match service_provider
+        .requisition_line_service
+        .update_request_requisition_line(&service_context, store_id, input.to_domain())
+    {
+        Ok(requisition_line) => {
+            UpdateResponse::Response(RequisitionLineNode::from_domain(requisition_line))
+        }
+        Err(error) => UpdateResponse::Error(UpdateError {
+            error: map_error(error)?,
+        }),
+    };
+
+    Ok(response)
+}
+
+impl UpdateInput {
+    fn to_domain(self) -> ServiceInput {
+        let UpdateInput {
+            id,
+            requested_quantity,
+        } = self;
+
+        ServiceInput {
+            id,
+            requested_quantity,
+        }
+    }
+}
+
+fn map_error(error: ServiceError) -> Result<UpdateErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Structured Errors
+        ServiceError::RequisitionLineDoesNotExist => {
+            return Ok(UpdateErrorInterface::RecordDoesNotExist(
+                RecordDoesNotExist {},
+            ))
+        }
+        ServiceError::RequisitionDoesNotExist => {
+            return Ok(UpdateErrorInterface::RequisitionDoesNotExist(
+                ForeignKeyError(ForeignKey::RequisitionId),
+            ))
+        }
+        ServiceError::CannotEditRequisition => {
+            return Ok(UpdateErrorInterface::CannotEditRequisition(
+                CannotEditRequisition {},
+            ))
+        }
+        // Standard Graphql Errors
+        ServiceError::NotThisStoreRequisition => BadUserInput(formatted_error),
+        ServiceError::NotARequestRequisition => BadUserInput(formatted_error),
+        ServiceError::UpdatedRequisitionLineDoesNotExist => InternalError(formatted_error),
+        ServiceError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
 }
