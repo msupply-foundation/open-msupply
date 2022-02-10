59224bfa3b29b4e45e0eaf74656435567bdc2ff0
Mark Prins <mark@malcolmprins.net>
2022-02-10 17:17:32 +1300

    readme updates for mac; and folder move

--- client/packages/host/public/config.js
+++ client/packages/host/public/config.js
@@ -1,5 +1,6 @@
 window.env = {
   // API_URL: 'http://localhost:4000', // - default URL for the mock graphql server
   // API_URL: 'http://localhost:8000/graphql', // - default URL for the backend graphql server
-  API_URL: 'https://demo-open.msupply.org:8000/graphql', // Demo - site URL
+  API_URL: 'http://localhost:8082/graphql',
+  // API_URL: 'https://demo-open.msupply.org:8000/graphql', // Demo - site URL
 };
