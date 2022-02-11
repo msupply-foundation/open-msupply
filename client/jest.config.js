c75853c5014a66a841979a0458a7342f9061349c
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-11 21:54:52 +1300

    Add inline source maps for jest debugging

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,12 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest', { sourceMaps: 'inline' }],
   },
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
