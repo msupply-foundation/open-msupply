3e2e092163038e5941ca84e22ebc36fd20545019
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-15 11:24:18 +1300

    Add changes to SplitButton to make the component less stateful

--- client/packages/common/src/ui/components/buttons/SplitButton.tsx
+++ client/packages/common/src/ui/components/buttons/SplitButton.tsx
@@ -6,23 +6,25 @@ import { ChevronDownIcon } from '../../icons';
 import { ButtonWithIcon, ButtonWithIconProps } from './standard/ButtonWithIcon';
 import { ShrinkableBaseButton } from '@common/components';
 
-interface SplitButtonOption {
+export interface SplitButtonOption<T> {
   label: string;
-  value?: string;
+  value?: T;
   isDisabled?: boolean;
 }
 
-interface SplitButtonProps {
+export interface SplitButtonProps<T> {
   color?: ButtonWithIconProps['color'];
   ariaLabel?: string;
   ariaControlLabel?: string;
-  options: SplitButtonOption[];
-  onClick: (option: SplitButtonOption) => void;
+  options: SplitButtonOption<T>[];
+  onClick: (option: SplitButtonOption<T>) => void;
   Icon?: ButtonWithIconProps['Icon'];
   isDisabled?: boolean;
+  selectedOption: SplitButtonOption<T>;
+  onSelectOption: (option: SplitButtonOption<T>) => void;
 }
 
-export const SplitButton = ({
+export const SplitButton = <T,>({
   color = 'secondary',
   ariaLabel,
   ariaControlLabel,
@@ -30,85 +32,79 @@ export const SplitButton = ({
   onClick,
   Icon = null,
   isDisabled = false,
-}: SplitButtonProps) => {
+  selectedOption,
+  onSelectOption,
+}: SplitButtonProps<T>) => {
+  console.log(selectedOption);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-
-  const buttonLabel = options[selectedIndex]?.label ?? '';
+  const buttonLabel = selectedOption.label;
   const open = !!anchorEl;
 
   return (
-    <>
-      <ButtonGroup color={color} variant="outlined" aria-label={ariaLabel}>
-        <ButtonWithIcon
-          color={color}
-          disabled={isDisabled}
-          sx={{
-            borderRadius: 0,
-            borderStartStartRadius: '24px',
-            borderEndStartRadius: '24px',
-          }}
-          onClick={() => {
-            const selectedOption = options[selectedIndex];
-            if (!selectedOption) {
-              throw new Error('The selected index for an option is invalid');
-            }
+    <ButtonGroup color={color} variant="outlined" aria-label={ariaLabel}>
+      <ButtonWithIcon
+        color={color}
+        disabled={isDisabled}
+        sx={{
+          borderRadius: 0,
+          borderStartStartRadius: '24px',
+          borderEndStartRadius: '24px',
+        }}
+        onClick={() => {
+          onClick(selectedOption);
+        }}
+        label={buttonLabel}
+        Icon={Icon}
+      />
 
-            onClick(selectedOption);
-          }}
-          label={buttonLabel}
-          Icon={Icon}
-        />
-
-        <ShrinkableBaseButton
-          shrink
-          disabled={isDisabled}
-          color={color}
-          size="small"
-          aria-controls={open ? ariaControlLabel : undefined}
-          aria-expanded={open ? 'true' : undefined}
-          aria-label={ariaLabel}
-          aria-haspopup="menu"
-          onClick={e => {
-            setAnchorEl(e.currentTarget);
-          }}
-          sx={{
-            borderRadius: 0,
-            borderStartEndRadius: '24px',
-            borderEndEndRadius: '24px',
-          }}
-        >
-          <ChevronDownIcon />
-        </ShrinkableBaseButton>
-        <Menu
-          anchorEl={anchorEl}
-          open={open}
-          onClose={() => setAnchorEl(null)}
-          elevation={5}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          transformOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
-        >
-          {options.map((option, index) => (
-            <MenuItem
-              key={option.label}
-              disabled={option?.isDisabled}
-              selected={index === selectedIndex}
-              onClick={() => {
-                setSelectedIndex(index);
-                setAnchorEl(null);
-              }}
-            >
-              {option.label}
-            </MenuItem>
-          ))}
-        </Menu>
-      </ButtonGroup>
-    </>
+      <ShrinkableBaseButton
+        shrink
+        disabled={isDisabled}
+        color={color}
+        size="small"
+        aria-controls={open ? ariaControlLabel : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-label={ariaLabel}
+        aria-haspopup="menu"
+        onClick={e => {
+          setAnchorEl(e.currentTarget);
+        }}
+        sx={{
+          borderRadius: 0,
+          borderStartEndRadius: '24px',
+          borderEndEndRadius: '24px',
+        }}
+      >
+        <ChevronDownIcon />
+      </ShrinkableBaseButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => setAnchorEl(null)}
+        elevation={5}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        {options.map(option => (
+          <MenuItem
+            key={option.label}
+            disabled={option?.isDisabled}
+            selected={option.value === selectedOption.value}
+            onClick={() => {
+              onSelectOption(option);
+              setAnchorEl(null);
+            }}
+          >
+            {option.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </ButtonGroup>
   );
 };
