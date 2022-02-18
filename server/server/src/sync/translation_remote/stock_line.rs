2367bdbb314ffb28d6b8dbb41a34a4e62b08c6ae
Clemens Zeidler <clemens@msupply.foundation>
2022-02-18 15:04:06 +1300

    Use RemotePullTranslation trait and add a test
    Source: https://github.com/msupply-foundation/remote-server/commit/0636d81959d9a9685065d106749b5903c81fddcb

--- server/server/src/sync/translation_remote/stock_line.rs
+++ server/server/src/sync/translation_remote/stock_line.rs
@@ -5,7 +5,9 @@ use serde::{Deserialize, Deserializer};
 
 use crate::sync::translation_central::SyncTranslationError;
 
-use super::TRANSLATION_RECORD_ITEM_LINE;
+use super::{
+    IntegrationRecord, IntegrationUpsertRecord, RemotePullTranslation, TRANSLATION_RECORD_ITEM_LINE,
+};
 
 fn empty_str_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<String>, D::Error> {
     let s: Option<String> = Option::deserialize(d)?;
@@ -20,7 +22,7 @@ fn zero_date_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<NaiveDa
 
 #[allow(non_snake_case)]
 #[derive(Deserialize)]
-pub struct LegacyStockLineRow {
+struct LegacyStockLineRow {
     ID: String,
     store_ID: String,
     item_ID: String,
@@ -39,10 +41,12 @@ pub struct LegacyStockLineRow {
     note: Option<String>,
 }
 
-impl LegacyStockLineRow {
-    pub fn try_translate_pull(
+pub struct StockLineTranslation {}
+impl RemotePullTranslation for StockLineTranslation {
+    fn try_translate_pull(
+        &self,
         sync_record: &RemoteSyncBufferRow,
-    ) -> Result<Option<StockLineRow>, SyncTranslationError> {
+    ) -> Result<Option<super::IntegrationRecord>, SyncTranslationError> {
         let table_name = TRANSLATION_RECORD_ITEM_LINE;
 
         if sync_record.table_name != table_name {
@@ -58,46 +62,44 @@ impl LegacyStockLineRow {
                 }
             })?;
 
-        Ok(Some(StockLineRow {
-            id: data.ID.clone(),
-            store_id: data.store_ID.clone(),
-            item_id: data.item_ID.clone(),
-            location_id: data.location_ID,
-            batch: data.batch,
-            pack_size: data.pack_size,
-            cost_price_per_pack: data.cost_price,
-            sell_price_per_pack: data.sell_price,
-            available_number_of_packs: data.available,
-            total_number_of_packs: data.quantity,
-            expiry_date: data.expiry_date,
-            on_hold: data.hold,
-            note: data.note,
-        }))
+        Ok(Some(IntegrationRecord::from_upsert(
+            IntegrationUpsertRecord::StockLine(StockLineRow {
+                id: data.ID.clone(),
+                store_id: data.store_ID.clone(),
+                item_id: data.item_ID.clone(),
+                location_id: data.location_ID,
+                batch: data.batch,
+                pack_size: data.pack_size,
+                cost_price_per_pack: data.cost_price,
+                sell_price_per_pack: data.sell_price,
+                available_number_of_packs: data.available,
+                total_number_of_packs: data.quantity,
+                expiry_date: data.expiry_date,
+                on_hold: data.hold,
+                note: data.note,
+            }),
+        )))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::sync::translation_remote::{
-        stock_line::LegacyStockLineRow,
-        test_data::{stock_line::get_test_stock_line_records, TestSyncDataRecord},
+        stock_line::StockLineTranslation, test_data::stock_line::get_test_stock_line_records,
+        RemotePullTranslation,
     };
 
     #[test]
     fn test_stock_line_translation() {
         for record in get_test_stock_line_records() {
-            match record.translated_record {
-                TestSyncDataRecord::StockLine(translated_record) => {
-                    assert_eq!(
-                        LegacyStockLineRow::try_translate_pull(&record.remote_sync_buffer_row)
-                            .unwrap(),
-                        translated_record,
-                        "{}",
-                        record.identifier
-                    )
-                }
-                _ => panic!("Testing wrong record type {:#?}", record.translated_record),
-            }
+            assert_eq!(
+                StockLineTranslation {}
+                    .try_translate_pull(&record.remote_sync_buffer_row)
+                    .unwrap(),
+                record.translated_record,
+                "{}",
+                record.identifier
+            );
         }
     }
 }
