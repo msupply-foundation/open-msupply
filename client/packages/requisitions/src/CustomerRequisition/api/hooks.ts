408a44fc171c5b2264250b73bed9474fb5d9a9ac
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-19 21:06:15 +1300

    Add response requisition queries

--- client/packages/requisitions/src/CustomerRequisition/api/hooks.ts
+++ client/packages/requisitions/src/CustomerRequisition/api/hooks.ts
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import {
+  useHostContext,
   RequisitionNodeStatus,
   useParams,
   useOmSupplyApi,
@@ -13,51 +14,67 @@ import {
   usePagination,
   getDataSorter,
 } from '@openmsupply-client/common';
-import { Requisition, RequisitionLine } from '../../types';
-import { CustomerRequisitionApi } from './api';
+import { ResponseRequisitionQueries } from './api';
+import {
+  getSdk,
+  ResponseRequisitionFragment,
+  ResponseRequisitionLineFragment,
+} from './operations.generated';
 
-export const useCustomerRequisition = (): UseQueryResult<Requisition> => {
-  const { id = '' } = useParams();
-  const { api } = useOmSupplyApi();
-  return useQuery(['requisition', id], () =>
-    CustomerRequisitionApi.get.byId(api)(id)
-  );
+export const useResponseRequisitionApi = () => {
+  const { client } = useOmSupplyApi();
+  return getSdk(client);
 };
 
+export const useCustomerRequisition =
+  (): UseQueryResult<ResponseRequisitionFragment> => {
+    const { id = '' } = useParams();
+    const { store } = useHostContext();
+    const api = useResponseRequisitionApi();
+    return useQuery(['requisition', id], () =>
+      ResponseRequisitionQueries.get.byNumber(api)(Number(id), store.id)
+    );
+  };
+
 export const useCustomerRequisitionFields = <
-  KeyOfRequisition extends keyof Requisition
+  KeyOfRequisition extends keyof ResponseRequisitionFragment
 >(
   keys: KeyOfRequisition | KeyOfRequisition[]
-): FieldSelectorControl<Requisition, KeyOfRequisition> => {
+): FieldSelectorControl<ResponseRequisitionFragment, KeyOfRequisition> => {
+  const { store } = useHostContext();
   const { id = '' } = useParams();
-  const { api } = useOmSupplyApi();
+  const api = useResponseRequisitionApi();
   return useFieldsSelector(
     ['requisition', id],
-    () => CustomerRequisitionApi.get.byId(api)(id),
-    (patch: Partial<Requisition>) =>
-      CustomerRequisitionApi.update(api)({ ...patch, id }),
+    () => ResponseRequisitionQueries.get.byNumber(api)(Number(id), store.id),
+    (patch: Partial<ResponseRequisitionFragment>) =>
+      ResponseRequisitionQueries.update(api, store.id)({ ...patch, id }),
     keys
   );
 };
 
 interface UseCustomerRequisitionLinesController
-  extends SortController<RequisitionLine>,
+  extends SortController<ResponseRequisitionLineFragment>,
     PaginationState {
-  lines: RequisitionLine[];
+  lines: ResponseRequisitionLineFragment[];
 }
 
 export const useCustomerRequisitionLines =
   (): UseCustomerRequisitionLinesController => {
-    const { sortBy, onChangeSortBy } = useSortBy<RequisitionLine>({
-      key: 'itemName',
-      isDesc: false,
-    });
+    const { sortBy, onChangeSortBy } =
+      useSortBy<ResponseRequisitionLineFragment>({
+        key: 'itemName',
+        isDesc: false,
+      });
     const pagination = usePagination(20);
     const { lines } = useCustomerRequisitionFields('lines');
 
     const sorted = useMemo(() => {
-      const sorted = [...(lines ?? [])].sort(
-        getDataSorter(sortBy.key as keyof RequisitionLine, !!sortBy.isDesc)
+      const sorted = [...(lines.nodes ?? [])].sort(
+        getDataSorter(
+          sortBy.key as keyof ResponseRequisitionLineFragment,
+          !!sortBy.isDesc
+        )
       );
 
       return sorted.slice(
