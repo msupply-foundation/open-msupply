ba22f86df27bf3477b55d7785b29202bced4e8bc
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-20 21:19:45 +1300

    Add updates for compilaton

--- client/packages/invoices/src/OutboundShipment/api/operations.generated.ts
+++ client/packages/invoices/src/OutboundShipment/api/operations.generated.ts
@@ -1,4 +1,4 @@
-import * as Types from '../../../../../../packages/common/src/types/schema';
+import * as Types from '@openmsupply-client/common';
 
 import { GraphQLClient } from 'graphql-request';
 import * as Dom from 'graphql-request/dist/types.dom';
