416de831482f3be6584d0a784b62b3bc9e0b73c1
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-22 22:51:50 +1300

    Add stocktake line deletion

--- client/packages/inventory/src/Stocktake/DetailView/Toolbar.tsx
+++ client/packages/inventory/src/Stocktake/DetailView/Toolbar.tsx
@@ -11,32 +11,18 @@ import {
   useBufferState,
   InputWithLabelRow,
 } from '@openmsupply-client/common';
-import { useStocktakeFields, useIsStocktakeDisabled } from '../api';
+import {
+  useStocktakeFields,
+  useIsStocktakeDisabled,
+  useDeleteSelectedLines,
+} from '../api';
 
 export const Toolbar: FC = () => {
   const t = useTranslation(['distribution', 'common', 'inventory']);
-  const { success, info } = useNotification();
   const isDisabled = useIsStocktakeDisabled();
   const { description, update } = useStocktakeFields('description');
+  const { onDelete } = useDeleteSelectedLines();
   const [descriptionBuffer, setDescriptionBuffer] = useBufferState(description);
-
-  // const { selectedRows } = useTableStore(state => ({
-  //   selectedRows: Object.keys(state.rowState)
-  //     .filter(id => state.rowState[id]?.isSelected)
-  //     .map(selectedId => draft.lines.find(({ id }) => selectedId === id))
-  //     .filter(Boolean) as StocktakeItem[],
-  // }));
-  const selectedRows: unknown[] = [];
-
-  const deleteAction = () => {
-    if (selectedRows && selectedRows?.length > 0) {
-      const successSnack = success(`Deleted ${selectedRows?.length} lines`);
-      successSnack();
-    } else {
-      const infoSnack = info(t('label.select-rows-to-delete-them'));
-      infoSnack();
-    }
-  };
 
   return (
     <AppBarContentPortal sx={{ display: 'flex', flex: 1, marginBottom: 1 }}>
@@ -66,7 +52,7 @@ export const Toolbar: FC = () => {
         </Grid>
         <Grid item>
           <DropdownMenu disabled={isDisabled} label={t('label.select')}>
-            <DropdownMenuItem IconComponent={DeleteIcon} onClick={deleteAction}>
+            <DropdownMenuItem IconComponent={DeleteIcon} onClick={onDelete}>
               {t('button.delete-lines', { ns: 'distribution' })}
             </DropdownMenuItem>
           </DropdownMenu>
