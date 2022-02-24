bf10f4459643fd4fb512642b39b1a5d8a31b0b48
Clemens Zeidler <clemens@msupply.foundation>
2022-02-25 08:46:57 +1300

    Add site_id and central_server_id to the SyncSettings
    Source: https://github.com/msupply-foundation/remote-server/commit/54596d3a8df7603613e5938e3df0fb52b420ef19

--- server/server/src/settings.rs
+++ server/server/src/settings.rs
@@ -26,6 +26,8 @@ pub struct SyncSettings {
     pub username: String,
     pub password: String,
     pub interval: u64,
+    pub central_server_site_id: u32,
+    pub site_id: u32,
 }
 
 impl ServerSettings {
