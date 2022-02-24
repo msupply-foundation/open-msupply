e8c952c38e83488e8bfa90c67011ddf1633eb547
Clemens Zeidler <clemens@msupply.foundation>
2022-02-25 08:45:07 +1300

    Improve sync api v3 interface

--- server/server/src/sync/sync_api_v3.rs
+++ server/server/src/sync/sync_api_v3.rs
@@ -15,16 +15,28 @@ pub struct SyncApiV3 {
     credentials: SyncCredentials,
 }
 
-fn extra_headers(side_id: &str) -> anyhow::Result<HeaderMap> {
+fn extra_headers(side_id: u32) -> anyhow::Result<HeaderMap> {
     let mut headers = HeaderMap::new();
-    headers.insert("msupply-site-uuid", side_id.parse()?);
+    headers.insert("msupply-site-uuid", format!("{}", side_id).parse()?);
     headers.insert(CONTENT_LENGTH, "application/json".parse()?);
     headers.insert(ACCEPT, "application/json".parse()?);
     Ok(headers)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct RemoteSyncRecordV3 {
+pub enum SyncTypeV3 {
+    #[serde(rename = "I")]
+    Insert,
+    #[serde(rename = "U")]
+    Update,
+    #[serde(rename = "D")]
+    Delete,
+    #[serde(rename = "M")]
+    Merge,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RemoteGetRecordV3 {
     #[serde(rename = "RecordType")]
     pub record_type: String,
     #[serde(rename = "SyncID")]
@@ -32,16 +44,33 @@ pub struct RemoteSyncRecordV3 {
     #[serde(rename = "KeyFieldID")]
     pub key_field_id: i64,
     #[serde(rename = "mergeIDtokeep")]
-    pub merge_id_tokeep: String,
+    pub merge_id_to_keep: String,
     #[serde(rename = "StoreID")]
     pub store_id: String,
     #[serde(rename = "RecordID")]
     pub record_id: String,
     #[serde(rename = "SyncType")]
-    pub sync_type: String, // e.g. "U"
+    pub sync_type: SyncTypeV3,
     #[serde(rename = "mergeIDtodelete")]
-    pub merge_id_todelete: String,
+    pub merge_id_to_delete: String,
     pub data: serde_json::Value,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RemotePostRecordV3 {
+    #[serde(rename = "SyncID")]
+    pub sync_id: String,
+    #[serde(rename = "RecordType")]
+    pub record_type: String,
+    #[serde(rename = "RecordID")]
+    pub record_id: String,
+    #[serde(rename = "SyncType")]
+    pub sync_type: SyncTypeV3,
+    #[serde(rename = "StoreID")]
+    pub store_id: Option<String>,
+    // if sync type is Delete data is None
+    #[serde(rename = "Data")]
+    pub data: Option<serde_json::Value>,
 }
 
 #[derive(Debug, Serialize)]
@@ -55,7 +84,7 @@ impl SyncApiV3 {
         server_url: Url,
         credentials: SyncCredentials,
         client: Client,
-        site_id: &str,
+        site_id: u32,
     ) -> anyhow::Result<Self> {
         Ok(SyncApiV3 {
             server_url,
@@ -67,13 +96,10 @@ impl SyncApiV3 {
 
     pub async fn get_initial_dump(
         &self,
-        from_site: &str,
-        to_site: &str,
+        from_site: u32,
+        to_site: u32,
     ) -> anyhow::Result<serde_json::Value> {
-        let query = [
-            ("from_site", &from_site.to_string()),
-            ("to_site", &to_site.to_string()),
-        ];
+        let query = [("from_site", from_site), ("to_site", to_site)];
 
         let response = self
             .client
@@ -94,14 +120,14 @@ impl SyncApiV3 {
 
     pub async fn get_queued_records(
         &self,
-        from_site: &str,
-        to_site: &str,
+        from_site: u32,
+        to_site: u32,
         limit: u32,
-    ) -> anyhow::Result<Vec<RemoteSyncRecordV3>> {
+    ) -> anyhow::Result<Vec<RemoteGetRecordV3>> {
         let query = [
-            ("from_site", &from_site.to_string()),
-            ("to_site", &to_site.to_string()),
-            ("limit", &limit.to_string()),
+            ("from_site", from_site),
+            ("to_site", to_site),
+            ("limit", limit),
         ];
 
         let response = self
@@ -123,14 +149,11 @@ impl SyncApiV3 {
 
     pub async fn post_queued_records(
         &self,
-        from_site: &str,
-        to_site: &str,
-        records: &RemoteSyncRecordV3,
+        from_site: u32,
+        to_site: u32,
+        records: &Vec<RemotePostRecordV3>,
     ) -> anyhow::Result<serde_json::Value> {
-        let query = [
-            ("from_site", &from_site.to_string()),
-            ("to_site", &to_site.to_string()),
-        ];
+        let query = [("from_site", from_site), ("to_site", to_site)];
 
         let response = self
             .client
@@ -152,14 +175,11 @@ impl SyncApiV3 {
 
     pub async fn post_acknowledged_records(
         &self,
-        from_site: &str,
-        to_site: &str,
+        from_site: u32,
+        to_site: u32,
         records: &RemoteSyncAckV3,
     ) -> anyhow::Result<serde_json::Value> {
-        let query = [
-            ("from_site", &from_site.to_string()),
-            ("to_site", &to_site.to_string()),
-        ];
+        let query = [("from_site", from_site), ("to_site", to_site)];
 
         let response = self
             .client
