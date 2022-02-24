d163d8987559bc0443e5f323ca467d1da4bbb0b9
Mark Prins <mark@malcolmprins.net>
2022-02-25 12:31:29 +1300

    add stock level queries

---
+++ client/packages/dashboard/src/api/operations.generated.ts
@@ -0,0 +1,103 @@
+import * as Types from '@openmsupply-client/common';
+
+import { GraphQLClient } from 'graphql-request';
+import * as Dom from 'graphql-request/dist/types.dom';
+import gql from 'graphql-tag';
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
+export type ItemStatsFragment = { __typename: 'ItemNode', stats: { __typename: 'ItemStatsNode', averageMonthlyConsumption: number, availableStockOnHand: number, availableMonthsOfStockOnHand: number } };
+
+export type StockCountsQueryVariables = Types.Exact<{
+  daysTillExpired?: Types.InputMaybe<Types.Scalars['Int']>;
+  timezoneOffset?: Types.InputMaybe<Types.Scalars['Int']>;
+}>;
+
+
+export type StockCountsQuery = { __typename: 'Queries', stockCounts: { __typename: 'StockCounts', expired: number, expiringSoon: number } };
+
+export type ItemStatsQueryVariables = Types.Exact<{
+  storeId: Types.Scalars['String'];
+}>;
+
+
+export type ItemStatsQuery = { __typename: 'Queries', items: { __typename: 'ItemConnector', totalCount: number, nodes: Array<{ __typename: 'ItemNode', stats: { __typename: 'ItemStatsNode', averageMonthlyConsumption: number, availableStockOnHand: number, availableMonthsOfStockOnHand: number } }> } };
+
+export const ItemStatsFragmentDoc = gql`
+    fragment ItemStats on ItemNode {
+  stats(storeId: $storeId) {
+    averageMonthlyConsumption
+    availableStockOnHand
+    availableMonthsOfStockOnHand
+  }
+}
+    `;
+export const StockCountsDocument = gql`
+    query stockCounts($daysTillExpired: Int, $timezoneOffset: Int) {
+  stockCounts(daysTillExpired: $daysTillExpired, timezoneOffset: $timezoneOffset) {
+    expired
+    expiringSoon
+  }
+}
+    `;
+export const ItemStatsDocument = gql`
+    query itemStats($storeId: String!) {
+  items(filter: {isVisible: {equalTo: true}}) {
+    ... on ItemConnector {
+      nodes {
+        ...ItemStats
+      }
+      totalCount
+    }
+  }
+}
+    ${ItemStatsFragmentDoc}`;
+
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string) => Promise<T>;
+
+
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
+  return {
+    stockCounts(variables?: StockCountsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<StockCountsQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<StockCountsQuery>(StockCountsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'stockCounts');
+    },
+    itemStats(variables: ItemStatsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<ItemStatsQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<ItemStatsQuery>(ItemStatsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'itemStats');
+    }
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
+
+/**
+ * @param resolver a function that accepts a captured request and may return a mocked response.
+ * @see https://mswjs.io/docs/basics/response-resolver
+ * @example
+ * mockStockCountsQuery((req, res, ctx) => {
+ *   const { daysTillExpired, timezoneOffset } = req.variables;
+ *   return res(
+ *     ctx.data({ stockCounts })
+ *   )
+ * })
+ */
+export const mockStockCountsQuery = (resolver: ResponseResolver<GraphQLRequest<StockCountsQueryVariables>, GraphQLContext<StockCountsQuery>, any>) =>
+  graphql.query<StockCountsQuery, StockCountsQueryVariables>(
+    'stockCounts',
+    resolver
+  )
+
+/**
+ * @param resolver a function that accepts a captured request and may return a mocked response.
+ * @see https://mswjs.io/docs/basics/response-resolver
+ * @example
+ * mockItemStatsQuery((req, res, ctx) => {
+ *   const { storeId } = req.variables;
+ *   return res(
+ *     ctx.data({ items })
+ *   )
+ * })
+ */
+export const mockItemStatsQuery = (resolver: ResponseResolver<GraphQLRequest<ItemStatsQueryVariables>, GraphQLContext<ItemStatsQuery>, any>) =>
+  graphql.query<ItemStatsQuery, ItemStatsQueryVariables>(
+    'itemStats',
+    resolver
+  )
--- client/packages/dashboard/src/widgets/StockWidget.tsx
+++ client/packages/dashboard/src/widgets/StockWidget.tsx
@@ -2,24 +2,31 @@ import React from 'react';
 import {
   Grid,
   StatsPanel,
-  useOmSupplyApi,
-  useQuery,
   useTranslation,
   Widget,
 } from '@openmsupply-client/common';
-import { getStockCountQueryFn } from '../api';
+import { useItemStats, useStockCounts } from '../api';
+
+const LOW_MOS_THRESHOLD = 3;
 
 export const StockWidget: React.FC = () => {
-  const { api } = useOmSupplyApi();
-  const t = useTranslation(['app', 'dashboard']);
-  const { data, isLoading } = useQuery(
-    ['stock', 'count'],
-    getStockCountQueryFn(api),
-    { retry: false }
-  );
+  const t = useTranslation(['dashboard']);
+  const { data: expiryData, isLoading: isExpiryLoading } = useStockCounts();
+  const { data: itemStatsData, isLoading: isItemStatsLoading } = useItemStats();
+
+  const lowStockItemsCount =
+    itemStatsData?.filter(
+      item =>
+        item.stats.availableStockOnHand > 0 &&
+        item.stats.availableMonthsOfStockOnHand <= LOW_MOS_THRESHOLD
+    ).length || 0;
+
+  const noStockItemsCount =
+    itemStatsData?.filter(item => item.stats.availableStockOnHand === 0)
+      .length || 0;
 
   return (
-    <Widget title={t('stock')}>
+    <Widget title={t('heading-stock')}>
       <Grid
         container
         justifyContent="flex-start"
@@ -28,16 +35,34 @@ export const StockWidget: React.FC = () => {
       >
         <Grid item>
           <StatsPanel
-            isLoading={isLoading}
+            isLoading={isExpiryLoading}
             title={t('heading.expiring-stock')}
             stats={[
               {
                 label: t('label.expired', { ns: 'dashboard' }),
-                value: data?.expired || 0,
+                value: expiryData?.expired || 0,
               },
               {
                 label: t('label.expiring-soon'),
-                value: data?.expiringSoon || 0,
+                value: expiryData?.expiringSoon || 0,
+              },
+            ]}
+          />
+          <StatsPanel
+            isLoading={isItemStatsLoading}
+            title={t('heading.stock-levels')}
+            stats={[
+              {
+                label: t('label.total-items', { ns: 'dashboard' }),
+                value: itemStatsData?.length || 0,
+              },
+              {
+                label: t('label.items-no-stock'),
+                value: noStockItemsCount,
+              },
+              {
+                label: t('label.low-stock-items'),
+                value: lowStockItemsCount,
               },
             ]}
           />
