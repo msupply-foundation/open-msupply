eaf73ede775f288aa19b90633a32b2d93d522e2f
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-25 14:04:39 +1300

    Add quantity tests

--- client/packages/common/src/utils/quantities.ts
+++ client/packages/common/src/utils/quantities.ts
@@ -1,14 +1,14 @@
 export const suggestedQuantity = (amc: number, soh: number, mos: number) => {
   // If there is no consumption, don't suggest any
-  if (!amc) return 0;
+  if (!amc || amc < 0) return 0;
   // If there is no months of stock to order to, don't suggest stock to be ordered
-  if (!mos) return 0;
+  if (!mos || amc < 0) return 0;
 
   // Total amount to potentially order
   const total = amc * mos;
 
   // Subtract the available stock on hand
-  const suggested = total - soh;
+  const suggested = total - Math.max(soh, 0);
 
-  return suggested;
+  return Math.max(suggested, 0);
 };
