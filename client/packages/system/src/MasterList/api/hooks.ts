2ab82875f50faac58bc8adc6b913265909253367
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-27 22:23:34 +1300

    Add masterlist query relocation

---
+++ client/packages/system/src/MasterList/api/hooks.ts
@@ -0,0 +1,37 @@
+import {
+  useQuery,
+  useOmSupplyApi,
+  useAuthState,
+  useQueryParams,
+} from '@openmsupply-client/common';
+import { MasterListRow } from './../types';
+import { getSdk } from './operations.generated';
+import { getMasterListQueries } from './api';
+
+export const useMasterListApi = () => {
+  const { client } = useOmSupplyApi();
+  const { storeId } = useAuthState();
+  const sdk = getSdk(client);
+  const queries = getMasterListQueries(sdk, storeId);
+
+  return { ...queries, storeId };
+};
+
+export const useMasterLists = () => {
+  const queryParams = useQueryParams<MasterListRow>({
+    initialSortBy: { key: 'name' },
+  });
+  const api = useMasterListApi();
+
+  return {
+    ...useQuery(['master-list', 'list', api.storeId, queryParams], () =>
+      api.get.list({
+        first: queryParams.first,
+        offset: queryParams.offset,
+        sortBy: queryParams.sortBy,
+        filterBy: queryParams.filter.filterBy,
+      })
+    ),
+    ...queryParams,
+  };
+};
