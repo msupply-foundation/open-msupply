e8d1ba76051836bb9bb950666996adf191eee30b
Joshua Griffin <griffinjoshua5@gmail.com>
2022-02-27 21:12:22 +1300

    Add shift of stock queries to own package
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/4e40268aa854ad6677d0e3fc095b2d46205cc8e5

---
+++ client/packages/system/src/Stock/api/operations.generated.ts
@@ -0,0 +1,94 @@
+import * as Types from '@openmsupply-client/common';
+
+import { GraphQLClient } from 'graphql-request';
+import * as Dom from 'graphql-request/dist/types.dom';
+import gql from 'graphql-tag';
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
+export type ItemsWithStockLinesQueryVariables = Types.Exact<{
+  first?: Types.InputMaybe<Types.Scalars['Int']>;
+  offset?: Types.InputMaybe<Types.Scalars['Int']>;
+  key: Types.ItemSortFieldInput;
+  desc?: Types.InputMaybe<Types.Scalars['Boolean']>;
+  filter?: Types.InputMaybe<Types.ItemFilterInput>;
+  storeId: Types.Scalars['String'];
+}>;
+
+
+export type ItemsWithStockLinesQuery = { __typename: 'Queries', items: { __typename: 'ItemConnector', totalCount: number, nodes: Array<{ __typename: 'ItemNode', code: string, id: string, isVisible: boolean, name: string, unitName?: string | null, availableBatches: { __typename: 'StockLineConnector', totalCount: number, nodes: Array<{ __typename: 'StockLineNode', availableNumberOfPacks: number, batch?: string | null, costPricePerPack: number, expiryDate?: string | null, id: string, itemId: string, packSize: number, sellPricePerPack: number, totalNumberOfPacks: number, onHold: boolean, note?: string | null, storeId: string, locationName?: string | null }> } }> } };
+
+
+export const ItemsWithStockLinesDocument = gql`
+    query itemsWithStockLines($first: Int, $offset: Int, $key: ItemSortFieldInput!, $desc: Boolean, $filter: ItemFilterInput, $storeId: String!) {
+  items(
+    storeId: $storeId
+    page: {first: $first, offset: $offset}
+    sort: {key: $key, desc: $desc}
+    filter: $filter
+  ) {
+    __typename
+    ... on ItemConnector {
+      __typename
+      totalCount
+      nodes {
+        __typename
+        code
+        id
+        isVisible
+        name
+        unitName
+        availableBatches(storeId: $storeId) {
+          __typename
+          totalCount
+          nodes {
+            __typename
+            availableNumberOfPacks
+            batch
+            costPricePerPack
+            expiryDate
+            id
+            itemId
+            packSize
+            sellPricePerPack
+            totalNumberOfPacks
+            onHold
+            note
+            storeId
+            locationName
+          }
+        }
+      }
+    }
+  }
+}
+    `;
+
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string) => Promise<T>;
+
+
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
+  return {
+    itemsWithStockLines(variables: ItemsWithStockLinesQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<ItemsWithStockLinesQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<ItemsWithStockLinesQuery>(ItemsWithStockLinesDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'itemsWithStockLines');
+    }
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
+
+/**
+ * @param resolver a function that accepts a captured request and may return a mocked response.
+ * @see https://mswjs.io/docs/basics/response-resolver
+ * @example
+ * mockItemsWithStockLinesQuery((req, res, ctx) => {
+ *   const { first, offset, key, desc, filter, storeId } = req.variables;
+ *   return res(
+ *     ctx.data({ items })
+ *   )
+ * })
+ */
+export const mockItemsWithStockLinesQuery = (resolver: ResponseResolver<GraphQLRequest<ItemsWithStockLinesQueryVariables>, GraphQLContext<ItemsWithStockLinesQuery>, any>) =>
+  graphql.query<ItemsWithStockLinesQuery, ItemsWithStockLinesQueryVariables>(
+    'itemsWithStockLines',
+    resolver
+  )
