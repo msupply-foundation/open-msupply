6debf865fe2c7ddc6bf2396757ee97e24f2d11d6
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-02 13:22:45 +1300

    Remove unused hooks

--- client/packages/common/src/hooks/useDrawer/useDrawer.test.tsx
+++ client/packages/common/src/hooks/useDrawer/useDrawer.test.tsx
@@ -2,7 +2,7 @@ import { useDrawer } from './useDrawer';
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import LocalStorage from '../localStorage/LocalStorage';
+import LocalStorage from '../../localStorage/LocalStorage';
 
 describe('useDrawer', () => {
   const UseDrawerExample = () => {
