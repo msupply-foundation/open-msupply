d8cbfcb7ec166d80b90952837b6852c64b67c767
Andrei <andrei.evguenov@gmail.com>
2022-03-04 13:48:42 +1300

    Destructure input when inline_init is used
    Source: https://github.com/msupply-foundation/remote-server/commit/4f751984a358c967725d1fbb464389f2f718cf4a

--- server/service/src/invoice_line/inbound_shipment_service_line/insert/generate.rs
+++ server/service/src/invoice_line/inbound_shipment_service_line/insert/generate.rs
@@ -4,19 +4,28 @@ use util::inline_init;
 use super::{InsertInboundShipmentServiceLine, InsertInboundShipmentServiceLineError};
 
 pub fn generate(
-    input: InsertInboundShipmentServiceLine,
+    InsertInboundShipmentServiceLine {
+        id,
+        invoice_id,
+        item_id: _,
+        name,
+        total_before_tax,
+        total_after_tax,
+        tax,
+        note,
+    }: InsertInboundShipmentServiceLine,
     item: ItemRow,
 ) -> Result<InvoiceLineRow, InsertInboundShipmentServiceLineError> {
     let new_line = inline_init(|r: &mut InvoiceLineRow| {
-        r.id = input.id;
-        r.invoice_id = input.invoice_id;
+        r.id = id;
+        r.invoice_id = invoice_id;
         r.item_id = item.id;
         r.item_code = item.code;
-        r.item_name = input.name.unwrap_or(item.name);
-        r.total_before_tax = input.total_before_tax;
-        r.total_after_tax = input.total_after_tax;
-        r.tax = input.tax;
-        r.note = input.note;
+        r.item_name = name.unwrap_or(item.name);
+        r.total_before_tax = total_before_tax;
+        r.total_after_tax = total_after_tax;
+        r.tax = tax;
+        r.note = note;
         r.r#type = InvoiceLineRowType::Service;
     });
 
