7c1036bb7cc9c05bf8f99245b2acbed4a6b99e7d
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-04 13:22:38 +1300

    Remove origin from window.open call
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/ced92f4e607413c4d35c16fd0da80abb753d509f

--- client/packages/common/src/hooks/useOpenInNewTab/useOpenInNewTab.ts
+++ client/packages/common/src/hooks/useOpenInNewTab/useOpenInNewTab.ts
@@ -2,14 +2,12 @@ import { useNavigate } from 'react-router';
 
 export const useOpenInNewTab = () => {
   const navigate = useNavigate();
-  const { origin } = window.location;
   return (url: string) => {
-    const to = `${origin}${url}`;
-    const win = window.open(to, '_blank');
+    const win = window.open(url, '_blank');
     if (win) {
       win.focus();
     } else {
-      navigate(to);
+      navigate(url);
     }
   };
 };
