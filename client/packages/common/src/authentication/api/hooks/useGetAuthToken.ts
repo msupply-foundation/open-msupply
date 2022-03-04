65d25b7e2669630441635ae1d2b392eed40cb294
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-04 17:12:53 +1300

    Add auth query key factory

--- client/packages/common/src/authentication/api/hooks/useGetAuthToken.ts
+++ client/packages/common/src/authentication/api/hooks/useGetAuthToken.ts
@@ -1,20 +1,9 @@
 import { useMutation } from 'react-query';
 import { useAuthApi } from './useAuthApi';
-import { AuthenticationResponse, AuthQueries } from '../api';
-
-interface LoginCredentials {
-  username: string;
-  password: string;
-}
 
 export const useGetAuthToken = () => {
   const api = useAuthApi();
-  const { mutate, ...rest } = useMutation<
-    AuthenticationResponse,
-    unknown,
-    LoginCredentials,
-    unknown
-  >(AuthQueries.get.authToken(api));
+  const { mutate, ...rest } = useMutation(api.get.authToken);
 
   return { mutate, ...rest };
 };
--- client/packages/common/src/authentication/api/hooks/useGetRefreshToken.ts
+++ client/packages/common/src/authentication/api/hooks/useGetRefreshToken.ts
@@ -1,11 +1,10 @@
 import { useAuthApi } from './useAuthApi';
 import { useQuery } from 'react-query';
-import { AuthQueries } from '../api';
 import { COOKIE_LIFETIME_MINUTES } from '../../AuthContext';
 
 export const useGetRefreshToken = (token: string) => {
   const api = useAuthApi();
-  return useQuery(['refresh-token', token], AuthQueries.get.refreshToken(api), {
+  return useQuery(api.keys.refresh(token), () => api.get.refreshToken(), {
     refetchInterval: Math.max(COOKIE_LIFETIME_MINUTES - 1, 1) * 60 * 1000,
     enabled: !!token,
   });
