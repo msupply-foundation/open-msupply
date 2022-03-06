be0b107416d7b88c10c57e206af4c00db4a129e1
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-06 21:38:59 +1300

    Add CheckBox cell component
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/c5458f159f7cb098ff2d8107b6067a539992d787

---
+++ client/packages/common/src/ui/layout/tables/components/Cells/CheckboxCell/CheckboxCell.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CellProps } from '../../../columns';
+import { Checkbox } from '@common/components';
+import { RecordWithId } from '@common/types';
+import { useBufferState } from '@common/hooks';
+
+export const CheckboxCell = <T extends RecordWithId>({
+  rowData,
+  column,
+  rows,
+}: CellProps<T>): React.ReactElement<CellProps<T>> => {
+  const [buffer, setBuffer] = useBufferState(
+    column.accessor({ rowData, rows })
+  );
+
+  return (
+    <Checkbox
+      checked={!!buffer}
+      size="small"
+      onChange={e => {
+        const newValue = e.target.checked;
+        setBuffer(newValue);
+        column.setter({ ...rowData, [column.key]: newValue });
+      }}
+    />
+  );
+};
