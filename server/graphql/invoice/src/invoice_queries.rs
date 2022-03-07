c0ce21fa87ed5c308e43b030f462ad11d54e42e0
Andrei <andrei.evguenov@gmail.com>
2022-03-08 02:49:22 +1300

    Add otherPartyName filter and change nameId to otherPartyId filter on InvoiceFilter

--- server/graphql/invoice/src/invoice_queries.rs
+++ server/graphql/invoice/src/invoice_queries.rs
@@ -70,7 +70,8 @@ pub struct EqualFilterInvoiceStatusInput {
 pub struct InvoiceFilterInput {
     pub id: Option<EqualFilterStringInput>,
     pub invoice_number: Option<EqualFilterBigNumberInput>,
-    pub name_id: Option<EqualFilterStringInput>,
+    pub other_party_name: Option<SimpleStringFilterInput>,
+    pub other_party_id: Option<EqualFilterStringInput>,
     pub store_id: Option<EqualFilterStringInput>,
     pub r#type: Option<EqualFilterInvoiceTypeInput>,
     pub status: Option<EqualFilterInvoiceStatusInput>,
@@ -164,7 +165,8 @@ impl InvoiceFilterInput {
         InvoiceFilter {
             id: self.id.map(EqualFilter::from),
             invoice_number: self.invoice_number.map(EqualFilter::from),
-            name_id: self.name_id.map(EqualFilter::from),
+            name_id: self.other_party_id.map(EqualFilter::from),
+            name: self.other_party_name.map(SimpleStringFilter::from),
             store_id: self.store_id.map(EqualFilter::from),
             r#type: self
                 .r#type
