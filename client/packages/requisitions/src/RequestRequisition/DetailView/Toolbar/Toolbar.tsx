5e139a612ca24d823674d9b5b8adf2efa4271338
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-07 23:35:11 +1300

    Add factoring out components and translations
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/a932326388691a480ac4747c96dc9869c86a8e72

---
+++ client/packages/requisitions/src/RequestRequisition/DetailView/Toolbar/Toolbar.tsx
@@ -0,0 +1,78 @@
+import React, { FC } from 'react';
+import {
+  AppBarContentPortal,
+  Box,
+  InputWithLabelRow,
+  BufferedTextInput,
+  Grid,
+  useTranslation,
+} from '@openmsupply-client/common';
+import { NameSearchInput } from '@openmsupply-client/system';
+import { useRequestFields, useIsRequestDisabled } from '../../api';
+import { ToolbarDropDown } from './ToolbarDropDown';
+import { ToolbarActions } from './ToolbarActions';
+
+export const Toolbar: FC = () => {
+  const t = useTranslation('replenishment');
+
+  const isDisabled = useIsRequestDisabled();
+  const { theirReference, update, otherParty } = useRequestFields([
+    'theirReference',
+    'otherParty',
+  ]);
+
+  return (
+    <AppBarContentPortal sx={{ display: 'flex', flex: 1, marginBottom: 1 }}>
+      <Grid
+        container
+        flexDirection="row"
+        display="flex"
+        flex={1}
+        alignItems="flex-end"
+      >
+        <Grid item display="flex" flex={1}>
+          <Box display="flex" flexDirection="row" gap={4}>
+            <Box display="flex" flex={1} flexDirection="column" gap={1}>
+              {otherParty && (
+                <InputWithLabelRow
+                  label={t('label.supplier-name')}
+                  Input={
+                    <NameSearchInput
+                      type="supplier"
+                      disabled={isDisabled}
+                      value={otherParty ?? null}
+                      onChange={otherParty => {
+                        update({ otherPartyId: otherParty.id });
+                      }}
+                    />
+                  }
+                />
+              )}
+              <InputWithLabelRow
+                label={t('label.supplier-ref')}
+                Input={
+                  <BufferedTextInput
+                    disabled={isDisabled}
+                    size="small"
+                    sx={{ width: 250 }}
+                    value={theirReference}
+                    onChange={e => update({ theirReference: e.target.value })}
+                  />
+                }
+              />
+            </Box>
+          </Box>
+        </Grid>
+        <Box
+          flexDirection="column"
+          alignItems="flex-end"
+          display="flex"
+          gap={2}
+        >
+          <ToolbarActions />
+          <ToolbarDropDown />
+        </Box>
+      </Grid>
+    </AppBarContentPortal>
+  );
+};
---
+++ client/packages/requisitions/src/RequestRequisition/DetailView/Toolbar/ToolbarActions.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Autocomplete,
+  InputWithLabelRow,
+  useTranslation,
+  useConfirmationModal,
+} from '@openmsupply-client/common';
+import { useRequestFields, useIsRequestDisabled } from '../../api';
+
+const months = [1, 2, 3, 4, 5, 6];
+
+export const ToolbarActions = () => {
+  const t = useTranslation('replenishment');
+  const isDisabled = useIsRequestDisabled();
+  const { minMonthsOfStock, update } = useRequestFields('minMonthsOfStock');
+  const getConfirmation = useConfirmationModal({
+    title: t('heading.are-you-sure'),
+    message: t('messages.changing-min-mos'),
+  });
+
+  return (
+    <InputWithLabelRow
+      labelWidth="150px"
+      labelProps={{ sx: { fontSize: 12, fontWeight: 500 } }}
+      label={t('label.min-months-of-stock')}
+      Input={
+        <Autocomplete
+          disabled={isDisabled}
+          clearIcon={null}
+          isOptionEqualToValue={(a, b) => a.value === b.value}
+          value={{
+            label: `${minMonthsOfStock} months`,
+            value: minMonthsOfStock,
+          }}
+          width="150px"
+          options={months.map(numberOfMonths => ({
+            label: t('label.number-months', { numberOfMonths }),
+            value: numberOfMonths,
+          }))}
+          onChange={(_, option) =>
+            option &&
+            getConfirmation({
+              onConfirm: () => update({ minMonthsOfStock: option.value }),
+            })
+          }
+        />
+      }
+    />
+  );
+};
