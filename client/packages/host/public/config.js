e086aaa7bb8ebd546b8d850fbab066af4142f211
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-08 16:00:04 +1300

    Revert config change

--- client/packages/host/public/config.js
+++ client/packages/host/public/config.js
@@ -1,5 +1,5 @@
 window.env = {
-  API_URL: 'http://localhost:8000/graphql', // - default URL for the backend graphql server
+  // API_URL: 'http://localhost:8000/graphql', // - default URL for the backend graphql server
   // API_URL: 'http://localhost:8082/graphql', // URL when building android app
-  // API_URL: 'https://demo-open.msupply.org:8000/graphql', // Demo - site URL
+  API_URL: 'https://demo-open.msupply.org:8000/graphql', // Demo - site URL
 };
