c2fb11bd7599006eb25d3a83570fa63534cb6657
Andrei <andrei.evguenov@gmail.com>
2022-03-09 00:39:17 +1300

    Add sort and extra filter to store queries
    * Add store service to general service
    * Use store service in loader
    * Join name to store in store repository
    * Expose sort in store query with Code, Name and NameCode
    * Add extra fitlers in store query for code, name_code, and name
    
    Source: https://github.com/msupply-foundation/remote-server/commit/166ce01062a31ebf303f92f7c3b495d80ff45eeb

--- server/service/src/service_provider.rs
+++ server/service/src/service_provider.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use repository::{
     Name, NameFilter, NameSort, PaginationOption, RepositoryError, StorageConnection,
-    StorageConnectionManager,
+    StorageConnectionManager, Store, StoreFilter, StoreSort,
 };
 
 use crate::{
@@ -22,7 +22,7 @@ use crate::{
     requisition_line::{RequisitionLineService, RequisitionLineServiceTrait},
     stocktake::{StocktakeService, StocktakeServiceTrait},
     stocktake_line::{StocktakeLineService, StocktakeLineServiceTrait},
-    store::{StoreService, StoreServiceTrait},
+    store::get_stores,
     ListError, ListResult,
 };
 
@@ -36,7 +36,6 @@ pub struct ServiceProvider {
     pub master_list_service: Box<dyn MasterListServiceTrait>,
     pub stocktake_service: Box<dyn StocktakeServiceTrait>,
     pub stocktake_line_service: Box<dyn StocktakeLineServiceTrait>,
-    pub store_service: Box<dyn StoreServiceTrait>,
     pub invoice_line_service: Box<dyn InvoiceLineServiceTrait>,
     pub requisition_service: Box<dyn RequisitionServiceTrait>,
     pub requisition_line_service: Box<dyn RequisitionLineServiceTrait>,
@@ -61,7 +60,6 @@ impl ServiceProvider {
             validation_service: Box::new(ValidationService::new(permission_service)),
             location_service: Box::new(LocationService {}),
             master_list_service: Box::new(MasterListService {}),
-            store_service: Box::new(StoreService {}),
             invoice_line_service: Box::new(InvoiceLineService {}),
             invoice_count_service: Box::new(InvoiceCountService {}),
             invoice_service: Box::new(InvoiceService {}),
@@ -98,6 +96,16 @@ pub trait GeneralServiceTrait: Sync + Send {
         sort: Option<NameSort>,
     ) -> Result<ListResult<Name>, ListError> {
         get_names(ctx, store_id, pagination, filter, sort)
+    }
+
+    fn get_stores(
+        &self,
+        ctx: &ServiceContext,
+        pagination: Option<PaginationOption>,
+        filter: Option<StoreFilter>,
+        sort: Option<StoreSort>,
+    ) -> Result<ListResult<Store>, ListError> {
+        get_stores(ctx, pagination, filter, sort)
     }
 }
 
--- server/service/src/store.rs
+++ server/service/src/store.rs
@@ -1,27 +1,21 @@
-use repository::PaginationOption;
-use repository::{schema::StoreRow, StoreFilter, StoreRepository, StoreSort};
+use repository::{PaginationOption, Store};
+use repository::{StoreFilter, StoreRepository, StoreSort};
 
 use crate::{
     get_default_pagination, i64_to_u32, service_provider::ServiceContext, ListError, ListResult,
 };
 
-pub trait StoreServiceTrait: Sync + Send {
-    fn get_stores(
-        &self,
-        ctx: &ServiceContext,
-        pagination: Option<PaginationOption>,
-        filter: Option<StoreFilter>,
-        sort: Option<StoreSort>,
-    ) -> Result<ListResult<StoreRow>, ListError> {
-        let pagination = get_default_pagination(pagination, u32::MAX, 1)?;
-        let repository = StoreRepository::new(&ctx.connection);
+pub fn get_stores(
+    ctx: &ServiceContext,
+    pagination: Option<PaginationOption>,
+    filter: Option<StoreFilter>,
+    sort: Option<StoreSort>,
+) -> Result<ListResult<Store>, ListError> {
+    let pagination = get_default_pagination(pagination, u32::MAX, 1)?;
+    let repository = StoreRepository::new(&ctx.connection);
 
-        Ok(ListResult {
-            rows: repository.query(pagination, filter.clone(), sort)?,
-            count: i64_to_u32(repository.count(filter)?),
-        })
-    }
+    Ok(ListResult {
+        rows: repository.query(pagination, filter.clone(), sort)?,
+        count: i64_to_u32(repository.count(filter)?),
+    })
 }
-
-pub struct StoreService;
-impl StoreServiceTrait for StoreService {}
