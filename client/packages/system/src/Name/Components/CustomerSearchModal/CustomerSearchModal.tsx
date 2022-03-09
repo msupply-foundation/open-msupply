aa52f836505e5db8593993597e58cf2bda710a2a
Joshua Griffin <35858975+joshxg@users.noreply.github.com>
2022-03-10 11:05:19 +1300

    Update packages/system/src/Name/Components/CustomerSearchModal/CustomerSearchModal.tsx
    Co-authored-by: Mark Prins <9192912+mark-prins@users.noreply.github.com>

--- client/packages/system/src/Name/Components/CustomerSearchModal/CustomerSearchModal.tsx
+++ client/packages/system/src/Name/Components/CustomerSearchModal/CustomerSearchModal.tsx
@@ -17,7 +17,7 @@ export const CustomerSearchModal: FC<NameSearchProps> = ({
       open={open}
       options={data?.nodes ?? []}
       onClose={onClose}
-      title={t('suppliers')}
+      title={t('customers')}
       optionKey="name"
       onChange={(_, name: NameRowFragment | null) => {
         if (name) onChange(name);
