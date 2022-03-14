9f256939196e7f7392d7453e0005328e44d84958
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-14 22:43:35 +1300

    Add localisation string and refactor to show 'no related documents'

--- client/packages/invoices/src/InboundShipment/DetailView/SidePanel/RelatedDocumentsSection.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/SidePanel/RelatedDocumentsSection.tsx
@@ -19,37 +19,41 @@ export const RelatedDocumentsSectionComponent = () => {
   const d = useFormatDate();
   const { requisition } = useInboundFields('requisition');
 
-  if (!requisition) {
-    return <PanelLabel>{t('messages.no-related-documents')}</PanelLabel>;
-  }
-
-  const { createdDatetime, user, requisitionNumber } = requisition;
-
-  let tooltip = t('messages.internal-order-created-on', {
-    date: d(new Date(createdDatetime)),
-  });
-  if (user?.username && user?.username !== 'unknown') {
-    tooltip += ` ${t('messages.by-user', { username: user.username })}`;
+  let tooltip = '';
+  if (requisition) {
+    const { user, createdDatetime } = requisition;
+    tooltip = t('messages.internal-order-created-on', {
+      date: d(new Date(createdDatetime)),
+    });
+    if (user?.username && user.username !== 'unknown') {
+      tooltip += ` ${t('messages.by-user', {
+        username: user?.username,
+      })}`;
+    }
   }
 
   return (
     <DetailPanelSection title={t('heading.related-documents')}>
       <Grid item direction="column" gap={0.5}>
-        <Tooltip title={tooltip}>
-          <Grid item>
-            <PanelRow>
-              <PanelLabel>{t('label.requisition')}</PanelLabel>
-              <PanelField>
-                <Link
-                  to={RouteBuilder.create(AppRoute.Replenishment)
-                    .addPart(AppRoute.InternalOrder)
-                    .addPart(String(requisitionNumber))
-                    .build()}
-                >{`#${requisitionNumber}`}</Link>
-              </PanelField>
-            </PanelRow>
-          </Grid>
-        </Tooltip>
+        {!requisition ? (
+          <PanelLabel>{t('messages.no-related-documents')}</PanelLabel>
+        ) : (
+          <Tooltip title={tooltip}>
+            <Grid item>
+              <PanelRow>
+                <PanelLabel>{t('label.requisition')}</PanelLabel>
+                <PanelField>
+                  <Link
+                    to={RouteBuilder.create(AppRoute.Replenishment)
+                      .addPart(AppRoute.InternalOrder)
+                      .addPart(String(requisition?.requisitionNumber))
+                      .build()}
+                  >{`#${requisition?.requisitionNumber}`}</Link>
+                </PanelField>
+              </PanelRow>
+            </Grid>
+          </Tooltip>
+        )}
       </Grid>
     </DetailPanelSection>
   );
--- client/packages/invoices/src/OutboundShipment/DetailView/SidePanel/RelatedDocumentsSection.tsx
+++ client/packages/invoices/src/OutboundShipment/DetailView/SidePanel/RelatedDocumentsSection.tsx
@@ -19,37 +19,41 @@ const RelatedDocumentsSectionComponent = () => {
   const d = useFormatDate();
   const { requisition } = useOutboundFields('requisition');
 
-  if (!requisition) {
-    return <PanelLabel>{t('messages.no-related-documents')}</PanelLabel>;
-  }
-
-  const { createdDatetime, user, requisitionNumber } = requisition;
-
-  let tooltip = t('messages.customer-requisition-created-on', {
-    date: d(new Date(createdDatetime)),
-  });
-  if (user && user?.username !== 'unknown') {
-    tooltip += ` ${t('messages.by-user', { user: user?.username })}`;
+  let tooltip = '';
+  if (requisition) {
+    const { user, createdDatetime } = requisition;
+    tooltip = t('messages.customer-requisition-created-on', {
+      date: d(new Date(createdDatetime)),
+    });
+    if (user && user?.username !== 'unknown') {
+      tooltip += ` ${t('messages.by-user', {
+        user: user?.username,
+      })}`;
+    }
   }
 
   return (
     <DetailPanelSection title={t('heading.related-documents')}>
       <Grid item direction="column" container gap={0.5}>
-        <Tooltip title={tooltip}>
-          <Grid item>
-            <PanelRow>
-              <PanelLabel>{t('label.requisition')}</PanelLabel>
-              <PanelField>
-                <Link
-                  to={RouteBuilder.create(AppRoute.Distribution)
-                    .addPart(AppRoute.CustomerRequisition)
-                    .addPart(String(requisition.requisitionNumber))
-                    .build()}
-                >{`#${requisitionNumber}`}</Link>
-              </PanelField>
-            </PanelRow>
-          </Grid>
-        </Tooltip>
+        {!requisition ? (
+          <PanelLabel>{t('messages.no-related-documents')}</PanelLabel>
+        ) : (
+          <Tooltip title={tooltip}>
+            <Grid item>
+              <PanelRow>
+                <PanelLabel>{t('label.requisition')}</PanelLabel>
+                <PanelField>
+                  <Link
+                    to={RouteBuilder.create(AppRoute.Distribution)
+                      .addPart(AppRoute.CustomerRequisition)
+                      .addPart(String(requisition?.requisitionNumber))
+                      .build()}
+                  >{`#${requisition?.requisitionNumber}`}</Link>
+                </PanelField>
+              </PanelRow>
+            </Grid>
+          </Tooltip>
+        )}
       </Grid>
     </DetailPanelSection>
   );
