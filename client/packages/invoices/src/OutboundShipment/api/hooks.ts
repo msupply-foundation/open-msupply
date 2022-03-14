8b7974234bc21dfc38df77303496e9cdf6d3bd65
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-14 20:37:30 +1300

    Add adjustments to react-query props

--- client/packages/invoices/src/OutboundShipment/api/hooks.ts
+++ client/packages/invoices/src/OutboundShipment/api/hooks.ts
@@ -77,8 +77,15 @@ export const useOutbound = (): UseQueryResult<OutboundFragment> => {
   const outboundNumber = useOutboundNumber();
   const api = useOutboundApi();
 
-  return useQuery(api.keys.detail(outboundNumber), () =>
-    api.get.byNumber(outboundNumber)
+  return useQuery(
+    api.keys.detail(outboundNumber),
+    () => api.get.byNumber(outboundNumber),
+    // Don't refetch when the edit modal opens, for example. But, don't cache data when this query
+    // is inactive. For example, when navigating away from the page and back again, refetch.
+    {
+      refetchOnMount: false,
+      cacheTime: 0,
+    }
   );
 };
 
--- client/packages/requisitions/src/RequestRequisition/api/hooks.ts
+++ client/packages/requisitions/src/RequestRequisition/api/hooks.ts
@@ -98,8 +98,15 @@ export const useUpdateRequest = () => {
 export const useRequest = (): UseQueryResult<RequestFragment> => {
   const requestNumber = useRequestNumber();
   const api = useRequestApi();
-  return useQuery(api.keys.detail(requestNumber), () =>
-    api.get.byNumber(requestNumber)
+  return useQuery(
+    api.keys.detail(requestNumber),
+    () => api.get.byNumber(requestNumber),
+    // Don't refetch when the edit modal opens, for example. But, don't cache data when this query
+    // is inactive. For example, when navigating away from the page and back again, refetch.
+    {
+      refetchOnMount: false,
+      cacheTime: 0,
+    }
   );
 };
 
