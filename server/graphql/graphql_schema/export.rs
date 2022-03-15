39196fdd6f8a81f010362fedc2ae0d6605988370
Clemens Zeidler <clemens@msupply.foundation>
2022-03-15 14:44:18 +1300

    Add graphql self request

--- server/graphql/graphql_schema/export.rs
+++ server/graphql/graphql_schema/export.rs
@@ -1,7 +1,7 @@
-use graphql::build_schema;
+use graphql::schema_builder;
 use std::fs;
 //  Entry point for export_graphql target, see cargo.toml and reademe.md test section
 fn main() {
-    let schema = build_schema().finish();
+    let schema = schema_builder().finish();
     fs::write("graphql/graphql_schema/schema.graphql", &schema.sdl()).unwrap();
 }
