da9e42fadb4b8664c5d9456472c6ddbc5de7f30c
Andrei <andrei.evguenov@gmail.com>
2022-03-15 22:15:24 +1300

    Add transport_reference to outbound shipment mutation
    Source: https://github.com/msupply-foundation/remote-server/commit/49d3011fe5da9a196fc12a60823ef579af21e2eb

--- server/service/src/invoice/outbound_shipment/update/mod.rs
+++ server/service/src/invoice/outbound_shipment/update/mod.rs
@@ -28,6 +28,7 @@ pub struct UpdateOutboundShipment {
     pub comment: Option<String>,
     pub their_reference: Option<String>,
     pub colour: Option<String>,
+    pub transport_reference: Option<String>,
 }
 
 #[derive(Debug)]
@@ -115,6 +116,15 @@ impl UpdateOutboundShipmentStatus {
             UpdateOutboundShipmentStatus::Shipped => InvoiceRowStatus::Shipped,
         }
     }
+
+    pub fn full_status_option(
+        status: &Option<UpdateOutboundShipmentStatus>,
+    ) -> Option<InvoiceRowStatus> {
+        match status {
+            Some(status) => Some(status.full_status()),
+            None => None,
+        }
+    }
 }
 
 impl UpdateOutboundShipment {
@@ -123,5 +133,90 @@ impl UpdateOutboundShipment {
             Some(status) => Some(status.full_status()),
             None => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use repository::{
+        mock::{mock_name_a, mock_store_a, MockData, MockDataInserts},
+        schema::{InvoiceRow, InvoiceRowType},
+        test_db::setup_all_with_data,
+        InvoiceRepository,
+    };
+    use util::{inline_edit, inline_init};
+
+    use crate::{
+        invoice::outbound_shipment::update::UpdateOutboundShipment,
+        service_provider::ServiceProvider,
+    };
+
+    #[actix_rt::test]
+    async fn update_outbound_shipment_success() {
+        fn invoice() -> InvoiceRow {
+            inline_init(|r: &mut InvoiceRow| {
+                r.id = "test_invoice_pricing".to_string();
+                r.name_id = mock_name_a().id;
+                r.store_id = mock_store_a().id;
+                r.r#type = InvoiceRowType::OutboundShipment;
+            })
+        }
+
+        let (_, connection, connection_manager, _) = setup_all_with_data(
+            "update_outbound_shipment_success",
+            MockDataInserts::all(),
+            Some(inline_init(|r: &mut MockData| {
+                r.invoices = vec![invoice()];
+            })),
+        )
+        .await;
+
+        let service_provider = ServiceProvider::new(connection_manager);
+        let context = service_provider.context().unwrap();
+        let service = service_provider.invoice_service;
+
+        // Test all basic fields
+        fn get_update() -> UpdateOutboundShipment {
+            UpdateOutboundShipment {
+                id: invoice().id,
+                other_party_id: None,
+                status: None,
+                on_hold: Some(true),
+                comment: Some("comment".to_string()),
+                their_reference: Some("their_reference".to_string()),
+                colour: Some("colour".to_string()),
+                transport_reference: Some("transport_reference".to_string()),
+            }
+        }
+
+        let result = service.update_outbound_shipment(&context, "store_a", get_update());
+
+        assert!(matches!(result, Ok(_)), "Not Ok(_) {:#?}", result);
+
+        let updated_record = InvoiceRepository::new(&connection)
+            .find_one_by_id(&invoice().id)
+            .unwrap();
+
+        assert_eq!(
+            updated_record,
+            inline_edit(&invoice(), |mut u| {
+                let UpdateOutboundShipment {
+                    id: _,
+                    other_party_id: _,
+                    status: _,
+                    on_hold,
+                    comment,
+                    their_reference,
+                    colour,
+                    transport_reference,
+                } = get_update();
+                u.on_hold = on_hold.unwrap();
+                u.comment = comment;
+                u.their_reference = their_reference;
+                u.colour = colour;
+                u.transport_reference = transport_reference;
+                u
+            })
+        );
     }
 }
