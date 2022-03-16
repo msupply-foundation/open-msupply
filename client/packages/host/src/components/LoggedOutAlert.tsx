3b04dcd56c15d7da30e963a068b5c23eee55aa3b
Mark Prins <mark@malcolmprins.net>
2022-03-16 14:57:39 +1300

    logout alert

---
+++ client/packages/host/src/components/LoggedOutAlert.tsx
@@ -0,0 +1,44 @@
+import { useToggle } from '@common/hooks';
+import { AppRoute } from 'packages/config/src';
+import React, { useEffect } from 'react';
+import {
+  matchPath,
+  RouteBuilder,
+  useAuthContext,
+  useLocation,
+  useNavigate,
+} from '@openmsupply-client/common';
+import { AlertModal } from '@common/components';
+import { useTranslation } from '@common/intl';
+
+export const LoggedOutAlert = () => {
+  const navigate = useNavigate();
+  const { isOn, toggleOff, toggleOn } = useToggle();
+  const t = useTranslation('app');
+  const location = useLocation();
+  const { token, user } = useAuthContext();
+
+  useEffect(() => {
+    if (!user) toggleOn();
+    return () => toggleOff();
+  }, [user]);
+
+  if (
+    matchPath(
+      RouteBuilder.create(AppRoute.Login).addWildCard().build(),
+      location.pathname
+    )
+  )
+    return null;
+
+  return (
+    <AlertModal
+      open={isOn}
+      title={t('auth.logged-out-title')}
+      message={t('auth.logged-out-message')}
+      onOk={() => {
+        navigate(AppRoute.Login);
+      }}
+    />
+  );
+};
