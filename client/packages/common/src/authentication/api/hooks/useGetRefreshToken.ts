15ecab9448e5693f6b106bf1324c53bb5012c6c1
Mark Prins <mark@malcolmprins.net>
2022-03-16 19:04:26 +1300

    allow dev login!!

--- client/packages/common/src/authentication/api/hooks/useGetRefreshToken.ts
+++ client/packages/common/src/authentication/api/hooks/useGetRefreshToken.ts
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { useAuthApi } from './useAuthApi';
 import { useQuery } from 'react-query';
 import { COOKIE_LIFETIME_MINUTES } from '../../AuthContext';
+import { useDebounceCallback } from '@common/hooks';
 
 export const useGetRefreshToken = (token: string) => {
   const api = useAuthApi();
+  const [enabled, setEnabled] = useState(false);
+  const refetchInterval = Math.max(COOKIE_LIFETIME_MINUTES - 1, 1) * 60 * 1000;
+  const debouncedSetEnabled = useDebounceCallback(
+    setEnabled,
+    [token],
+    refetchInterval
+  );
+
+  debouncedSetEnabled(!!token);
+
   return useQuery(api.keys.refresh(token), api.get.refreshToken, {
-    refetchInterval: Math.max(COOKIE_LIFETIME_MINUTES - 1, 1) * 60 * 1000,
-    enabled: !!token,
+    refetchInterval,
+    enabled,
   });
 };
