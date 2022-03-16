bd30bd399cf3c5cfa120f3644cad1ee1bffc6eda
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-17 00:05:56 +1300

    Add split into folders/files with compilation

--- client/packages/common/src/intl/index.ts
+++ client/packages/common/src/intl/index.ts
@@ -1,5 +1,12 @@
-export * from './intlHelpers';
-export * from './IntlProvider';
+// export * from './intlHelpers';
+// export * from './IntlProvider';
 export * from './IntlTestProvider';
 export { LocaleKey } from './locales';
+
 export * from './currency';
+export * from './util';
+export * from './strings';
+export * from './number';
+export * from './date';
+export * from './currency';
+export * from './context';
