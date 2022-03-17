7670e310b539806959197779ab555d4e8105c187
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-03-18 09:27:18 +1300

    i18n-unused-config

---
+++ client/i18n-unused.config.js
@@ -0,0 +1,18 @@
+// Info: https://www.npmjs.com/package/i18n-unused
+
+module.exports = {
+  localesPath: 'packages/common/src/intl/locales',
+  srcPath: 'packages',
+  ignorePaths: [
+    'packages/common/node_modules',
+    'packages/android/node_modules',
+    'packages/config/node_modules',
+    'packages/dashboard/node_modules',
+    'packages/host/node_modules',
+    'packages/inventory/node_modules',
+    'packages/invoices/node_modules',
+    'packages/requisitions/node_modules',
+    'packages/system/node_modules',
+    'packages/template/node_modules',
+  ],
+};
