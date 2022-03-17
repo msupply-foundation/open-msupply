b634099519299c9da3c6cf116ebdea66fda8203a
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-03-17 17:35:12 +1300

    Update Breadcrumbs.tsx
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/fb1554ebc1ed36e6b775d6dab1feb875672b2445

--- client/packages/common/src/ui/components/navigation/Breadcrumbs/Breadcrumbs.tsx
+++ client/packages/common/src/ui/components/navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -11,6 +11,8 @@ const Breadcrumb = styled(Link)({
   textDecoration: 'none',
 });
 
+// Small change
+
 export const Breadcrumbs: React.FC = () => {
   const t = useTranslation(['app', 'common']);
   const { urlParts, navigateUpOne } = useBreadcrumbs();
