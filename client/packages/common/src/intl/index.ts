5aa546402f894e1eb051b4e8c982f07ca99d9672
Joshua Griffin <griffinjoshua5@gmail.com>
2022-03-17 21:51:46 +1300

    Add crazy intl method back
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/f0dede2e59c361c668d93bdb1030877e3d7789e4

--- client/packages/common/src/intl/index.ts
+++ client/packages/common/src/intl/index.ts
@@ -7,3 +7,15 @@ export * from './number';
 export * from './date';
 export * from './currency';
 export * from './context';
+
+/* 
+Removing this unused method breaks things. Some components in
+Host start to become undefined and aren't resolved.
+Something with the imports and circular dependencies, probably.
+If you can fix it, you will be rewarded.
+*/
+import { useAuthContext } from '../authentication';
+export const useUserName = (): string => {
+  const { user } = useAuthContext();
+  return user?.name ?? '';
+};
