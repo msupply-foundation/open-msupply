b4d2d0ecf0509b41b8d30df65b1664af59e68589
Clemens Zeidler <clemens@msupply.foundation>
2022-03-17 16:08:03 +1300

    Add support to specify header and footer templates
    Source: https://github.com/msupply-foundation/remote-server/commit/e790acb8fd1da2534a9515805bd7b5c9f1c64b56

--- server/service/src/report/html_printing.rs
+++ server/service/src/report/html_printing.rs
@@ -2,30 +2,35 @@ use std::fs;
 
 use headless_chrome::{protocol::page::PrintToPdfOptions, Browser, LaunchOptionsBuilder};
 
-pub fn html_to_pdf(document: &String, document_id: &str) -> Result<Vec<u8>, failure::Error> {
-    let pdf_options=  Some(PrintToPdfOptions {
-    display_header_footer: Some(true),
-    prefer_css_page_size: Some(false),
-    landscape: Some(true),
-    print_background: None,
-    scale: None,
-    // Assuming 96 DPI (dots per inch)
-    paper_width: Some(8.0),
-    paper_height: Some(11.0),
-    margin_top: Some(1.0),
-    margin_bottom: Some(1.0),
-    margin_left: Some(0.0),
-    margin_right: Some(0.0),
-    page_ranges: None,
-    ignore_invalid_page_ranges: None,
-    header_template: Some(r#"<div style="font-size: 15px; padding-top: 8px; text-align: center; width: 100%;"><span>Some Rad Store's Header here. </div>"#.to_string()),
-    footer_template: Some(r#"<div style="font-size: 15px; padding-top: 8px; text-align: center; width: 100%;"><span>Footer here.</span> Page number: <span class="pageNumber"></span></div>"#.to_string()),
-});
+use super::report_service::GeneratedReport;
+
+pub fn html_to_pdf(
+    document: &GeneratedReport,
+    document_id: &str,
+) -> Result<Vec<u8>, failure::Error> {
+    let pdf_options = Some(PrintToPdfOptions {
+        display_header_footer: Some(true),
+        prefer_css_page_size: Some(false),
+        landscape: Some(true),
+        print_background: None,
+        scale: None,
+        // Assuming 96 DPI (dots per inch)
+        paper_width: Some(8.0),
+        paper_height: Some(11.0),
+        margin_top: Some(1.0),
+        margin_bottom: Some(1.0),
+        margin_left: Some(0.0),
+        margin_right: Some(0.0),
+        page_ranges: None,
+        ignore_invalid_page_ranges: None,
+        header_template: document.header.clone(),
+        footer_template: document.footer.clone(),
+    });
 
     let document_name = format!("{}.html", document_id);
     let temp_html_doc_path = std::env::current_dir()?.join(document_name);
     //let file_path = PathBuf::from_str(&document_name)?.canonicalize()?;
-    fs::write(&temp_html_doc_path, document)?;
+    fs::write(&temp_html_doc_path, &document.document)?;
 
     // create a new browser and a tab in that browser using headless-chrome
     let launch_options = LaunchOptionsBuilder::default()
