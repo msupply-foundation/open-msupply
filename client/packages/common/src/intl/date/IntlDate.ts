e34596058aea75a03844be1a8e7bdacfb107aee7
Mark Prins <mark@malcolmprins.net>
2022-03-18 17:48:26 +1300

    date format type
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/d8eef4180a783f037c36e833945371da4d1060b4

--- client/packages/common/src/intl/date/IntlDate.ts
+++ client/packages/common/src/intl/date/IntlDate.ts
@@ -2,7 +2,9 @@ import { useTranslation as useTranslationNext } from 'react-i18next';
 
 export const useFormatDate = (): ((
   value: number | Date,
-  options?: Intl.DateTimeFormatOptions & { format?: string }
+  options?: Intl.DateTimeFormatOptions & { format?: string } & {
+    val: { month?: string; day?: string; year?: string; weekday?: string };
+  }
 ) => string) => {
   const { t } = useTranslationNext('app');
   return (val, formatParams) => t('intl.datetime', { val, formatParams });
