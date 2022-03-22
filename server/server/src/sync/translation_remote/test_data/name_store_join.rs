3845c9436fd8d9889580da27f0bb8767d5109ea6
Clemens Zeidler <clemens@msupply.foundation>
2022-03-23 11:06:47 +1300

    Add new mSupply fields to name_store_join translation
    Source: https://github.com/msupply-foundation/remote-server/commit/0651cd2b0acd75b68cc5bd0cda31131ed1758f83

--- server/server/src/sync/translation_remote/test_data/name_store_join.rs
+++ server/server/src/sync/translation_remote/test_data/name_store_join.rs
@@ -46,7 +46,7 @@ fn name_store_join_1_pull_record() -> TestSyncRecord {
         },
     }
 }
-fn name_store_join1_push_record() -> TestSyncPushRecord {
+fn name_store_join_1_push_record() -> TestSyncPushRecord {
     TestSyncPushRecord {
         change_log: ChangelogRow {
             id: 2,
@@ -57,7 +57,9 @@ fn name_store_join1_push_record() -> TestSyncPushRecord {
         push_data: json!(LegacyNameStoreJoinRow {
             ID: NAME_STORE_JOIN_1.0.to_string(),
             store_ID: "store_a".to_string(),
-            name_ID: "name_store_a".to_string()
+            name_ID: "name_store_a".to_string(),
+            name_is_customer: None,
+            name_is_supplier: None,
         }),
     }
 }
@@ -106,7 +108,63 @@ fn name_store_join_2_push_record() -> TestSyncPushRecord {
         push_data: json!(LegacyNameStoreJoinRow {
             ID: NAME_STORE_JOIN_2.0.to_string(),
             store_ID: "store_b".to_string(),
-            name_ID: "name_store_b".to_string()
+            name_ID: "name_store_b".to_string(),
+            name_is_customer: None,
+            name_is_supplier: None,
+        }),
+    }
+}
+
+// same as NAME_STORE_JOIN_2 but with new om fields
+const NAME_STORE_JOIN_3: (&'static str, &'static str) = (
+    "BE65A4A05E4D47E88303D6105A7872C2",
+    r#"{
+      "ID": "BE65A4A05E4D47E88303D6105A7872C2",
+      "inactive": false,
+      "name_ID": "name_store_b",
+      "spare_Category_ID": 0,
+      "spare_Category_optional2_id": 0,
+      "spare_Category_optional_id": 0,
+      "store_ID": "store_b",
+      "name_is_customer": true,
+      "name_is_supplier": true
+  }"#,
+);
+fn name_store_join_3_pull_record() -> TestSyncRecord {
+    TestSyncRecord {
+        translated_record: Some(IntegrationRecord::from_upsert(
+            IntegrationUpsertRecord::NameStoreJoin(NameStoreJoinRow {
+                id: NAME_STORE_JOIN_3.0.to_string(),
+                store_id: "store_b".to_string(),
+                name_id: "name_store_b".to_string(),
+                name_is_customer: true,
+                name_is_supplier: true,
+            }),
+        )),
+        identifier: "Name store join 3",
+        remote_sync_buffer_row: RemoteSyncBufferRow {
+            id: "name_store_join_3".to_string(),
+            table_name: TRANSLATION_RECORD_NAME_STORE_JOIN.to_string(),
+            record_id: NAME_STORE_JOIN_3.0.to_string(),
+            data: NAME_STORE_JOIN_3.1.to_string(),
+            action: RemoteSyncBufferAction::Update,
+        },
+    }
+}
+fn name_store_join_3_push_record() -> TestSyncPushRecord {
+    TestSyncPushRecord {
+        change_log: ChangelogRow {
+            id: 2,
+            table_name: ChangelogTableName::NameStoreJoin,
+            row_id: NAME_STORE_JOIN_3.0.to_string(),
+            row_action: ChangelogAction::Upsert,
+        },
+        push_data: json!(LegacyNameStoreJoinRow {
+            ID: NAME_STORE_JOIN_3.0.to_string(),
+            store_ID: "store_b".to_string(),
+            name_ID: "name_store_b".to_string(),
+            name_is_customer: Some(true),
+            name_is_supplier: Some(true),
         }),
     }
 }
@@ -116,13 +174,15 @@ pub fn get_test_name_store_join_records() -> Vec<TestSyncRecord> {
     vec![
         name_store_join_1_pull_record(),
         name_store_join_2_pull_record(),
+        name_store_join_3_pull_record(),
     ]
 }
 
 #[allow(dead_code)]
 pub fn get_test_push_name_store_join_records() -> Vec<TestSyncPushRecord> {
     vec![
-        name_store_join1_push_record(),
+        name_store_join_1_push_record(),
         name_store_join_2_push_record(),
+        name_store_join_3_push_record(),
     ]
 }
