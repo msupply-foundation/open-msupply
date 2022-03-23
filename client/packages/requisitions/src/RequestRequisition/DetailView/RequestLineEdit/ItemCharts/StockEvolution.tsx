c7e78dab330ba0ff5241697faba26b023fc25564
Mark Prins <mark@malcolmprins.net>
2022-03-24 10:59:09 +1300

    no data for charts
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/2329fdc7547fbfbc3e837f3107ddfaf5540c7bac

--- client/packages/requisitions/src/RequestRequisition/DetailView/RequestLineEdit/ItemCharts/StockEvolution.tsx
+++ client/packages/requisitions/src/RequestRequisition/DetailView/RequestLineEdit/ItemCharts/StockEvolution.tsx
@@ -86,76 +86,80 @@ export const StockEvolution: React.FC<StockEvolutionProps> = ({
         </Typography>
       </Box>
       <Box>
-        <ComposedChart width={450} height={255} data={data}>
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="date"
-            axisLine={false}
-            tickFormatter={dateFormatter}
-            tick={{ fontSize: 12 }}
-          />
-          <YAxis axisLine={false} tick={{ fontSize: 12 }} />
-          <ChartTooltip
-            formatter={tooltipFormatter}
-            labelFormatter={tooltipLabelFormatter}
-            labelStyle={{ fontWeight: 700 }}
-          />
-          <Legend
-            payload={[
-              {
-                value: t('label.past'),
-                type: 'rect',
-                id: '1',
-                color: theme.palette.gray.main,
-              },
-              {
-                value: t('label.projected'),
-                type: 'rect',
-                id: '2',
-                color: theme.palette.primary.light,
-              },
-              {
-                value: t('label.min'),
-                type: 'line',
-                id: '3',
-                color: theme.palette.error.main,
-              },
-              {
-                value: t('label.max'),
-                type: 'line',
-                id: '4',
-                color: theme.palette.success.main,
-              },
-            ]}
-          />
-          <Bar dataKey="value">
-            {data.map(entry => (
-              <Cell
-                key={entry.date.toISOString()}
-                fill={
-                  entry.isHistoric
-                    ? theme.palette.gray.main
-                    : theme.palette.primary.light
-                }
-              />
-            ))}
-          </Bar>
-          <Line
-            dataKey="max"
-            stroke={theme.palette.success.main}
-            strokeDasharray="4"
-            dot={false}
-            strokeWidth={2}
-          />
+        {data.length === 0 ? (
+          <Typography width={450}>{t('error.no-data')}</Typography>
+        ) : (
+          <ComposedChart width={450} height={255} data={data}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              axisLine={false}
+              tickFormatter={dateFormatter}
+              tick={{ fontSize: 12 }}
+            />
+            <YAxis axisLine={false} tick={{ fontSize: 12 }} />
+            <ChartTooltip
+              formatter={tooltipFormatter}
+              labelFormatter={tooltipLabelFormatter}
+              labelStyle={{ fontWeight: 700 }}
+            />
+            <Legend
+              payload={[
+                {
+                  value: t('label.past'),
+                  type: 'rect',
+                  id: '1',
+                  color: theme.palette.gray.main,
+                },
+                {
+                  value: t('label.projected'),
+                  type: 'rect',
+                  id: '2',
+                  color: theme.palette.primary.light,
+                },
+                {
+                  value: t('label.min'),
+                  type: 'line',
+                  id: '3',
+                  color: theme.palette.error.main,
+                },
+                {
+                  value: t('label.max'),
+                  type: 'line',
+                  id: '4',
+                  color: theme.palette.success.main,
+                },
+              ]}
+            />
+            <Bar dataKey="value">
+              {data.map(entry => (
+                <Cell
+                  key={entry.date.toISOString()}
+                  fill={
+                    entry.isHistoric
+                      ? theme.palette.gray.main
+                      : theme.palette.primary.light
+                  }
+                />
+              ))}
+            </Bar>
+            <Line
+              dataKey="max"
+              stroke={theme.palette.success.main}
+              strokeDasharray="4"
+              dot={false}
+              strokeWidth={2}
+            />
 
-          <Line
-            dataKey="min"
-            stroke={theme.palette.error.main}
-            dot={false}
-            strokeWidth={2}
-            strokeDasharray="4"
-          />
-        </ComposedChart>
+            <Line
+              dataKey="min"
+              stroke={theme.palette.error.main}
+              dot={false}
+              strokeWidth={2}
+              strokeDasharray="4"
+            />
+          </ComposedChart>
+        )}
       </Box>
     </Box>
   );
