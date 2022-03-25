8b7c5914acd09f1ac0a552916d74bece3709a307
Andrei <andrei.evguenov@gmail.com>
2022-03-26 00:40:41 +1300

    Adjust graphql schema
    * Rename fields and tables
    * Make date on consumption not nullable timestamp
    * Add invoice line = stock out condition to consumption view

--- server/repository/src/schema/item_stats.rs
+++ server/repository/src/schema/item_stats.rs
@@ -1,18 +1,32 @@
-use chrono::NaiveDateTime;
+use chrono::NaiveDate;
+use util::Defaults;
 
 #[derive(Clone, Queryable, Debug, PartialEq)]
 pub struct ConsumptionRow {
     pub id: String,
     pub item_id: String,
     pub store_id: String,
-    pub consumption_quantity: i32,
-    pub consumption_datetime: Option<NaiveDateTime>,
+    pub quantity: i32,
+    pub date: NaiveDate,
 }
 
 #[derive(Clone, Queryable, Debug, PartialEq)]
-pub struct StockInfoRow {
+pub struct StockOnHandRow {
     pub id: String,
     pub item_id: String,
     pub store_id: String,
     pub available_stock_on_hand: i64,
+}
+
+impl Default for ConsumptionRow {
+    fn default() -> Self {
+        Self {
+            date: Defaults::naive_date(),
+            // Default
+            id: Default::default(),
+            item_id: Default::default(),
+            store_id: Default::default(),
+            quantity: Default::default(),
+        }
+    }
 }
