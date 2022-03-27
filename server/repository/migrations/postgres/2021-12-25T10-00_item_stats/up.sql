ec2f7ba814a03fc917f6bd3987ed88710e8d9448
Andrei <andrei.evguenov@gmail.com>
2022-03-28 02:49:15 +1300

    Add stock movement view and repository
    Source: https://github.com/msupply-foundation/remote-server/commit/c6facd2dc1a50a40a015f692393fb2830b16eafe

--- server/repository/migrations/postgres/2021-12-25T10-00_item_stats/up.sql
+++ server/repository/migrations/postgres/2021-12-25T10-00_item_stats/up.sql
@@ -1,26 +1,70 @@
+-- https://github.com/sussol/msupply/blob/master/Project/Sources/Methods/aggregator_stockMovement.4dm
+-- TODO are all of sc, ci, si type transactions synced, and are all of the dates set correctly ?
+CREATE VIEW outbound_shipment_stock_movement AS
+SELECT 
+    'n/a' as id,
+    number_of_packs * pack_size * -1 as quantity,
+	item_id,
+	store_id,
+	picked_datetime as datetime
+FROM invoice_line 
+JOIN invoice
+	ON invoice_line.invoice_id = invoice.id
+WHERE invoice.type = 'OUTBOUND_SHIPMENT' 
+	AND picked_datetime IS NOT NULL
+	AND invoice_line.number_of_packs > 0
+	AND invoice_line.type = 'STOCK_OUT';
+		
+CREATE VIEW inbound_shipment_stock_movement AS
+SELECT 
+    'n/a' as id,
+    number_of_packs * pack_size as quantity,
+	item_id,
+	store_id,
+	delivered_datetime as datetime
+FROM invoice_line 
+JOIN invoice
+	ON invoice_line.invoice_id = invoice.id
+WHERE invoice.type = 'INBOUND_SHIPMENT' 
+	AND delivered_datetime IS NOT NULL
+	AND invoice_line.number_of_packs > 0
+	AND invoice_line.type = 'STOCK_IN';
+		
+CREATE VIEW inventory_adjustment_stock_movement AS
+SELECT 
+    'n/a' as id,
+    CASE
+	 WHEN invoice_line.type = 'STOCK_IN' THEN number_of_packs * pack_size
+	 WHEN invoice_line.type = 'STOCK_OUT' THEN number_of_packs * pack_size * -1
+	END as quantity,
+	item_id,
+	store_id,
+	verified_datetime as datetime
+FROM invoice_line 
+JOIN invoice
+	ON invoice_line.invoice_id = invoice.id
+WHERE invoice.type = 'INVENTORY_ADJUSTMENT' 
+	AND verified_datetime IS NOT NULL
+	AND invoice_line.number_of_packs > 0;
+		
+CREATE VIEW stock_movement AS
+SELECT * FROM outbound_shipment_stock_movement
+UNION SELECT * from inbound_shipment_stock_movement
+UNION SELECT * from inventory_adjustment_stock_movement;
+
+-- https://github.com/sussol/msupply/blob/master/Project/Sources/Methods/aggregator_stockConsumption.4dm
+-- TODO sc type ?
 CREATE VIEW consumption AS
 SELECT 
     'n/a' as id,
     items_and_stores.item_id AS item_id, 
     items_and_stores.store_id AS store_id,
-	COALESCE(consumption.quantity, 0) AS quantity,
-	consumption.date::date AS date
+	-- @ = absolute value
+	@ COALESCE(consumption.quantity, 0) AS quantity,
+	consumption.datetime::date AS date
 FROM
    (SELECT item.id AS item_id, store.id AS store_id FROM item, store) as items_and_stores
-LEFT OUTER JOIN 
-	(SELECT 
-	 	item_id, 
-	 	store_id, 
-	 	picked_datetime as date,
-	 	COALESCE(pack_size * number_of_packs, 0) as quantity
-	 FROM invoice_line 
-	 JOIN invoice
-	    ON invoice_line.invoice_id = invoice.id
-	 WHERE invoice.type = 'OUTBOUND_SHIPMENT' 
-	 	AND picked_datetime IS NOT NULL
-		AND invoice_line.number_of_packs > 0
-		AND invoice_line.type = 'STOCK_OUT'
-	) AS consumption 
+LEFT OUTER JOIN outbound_shipment_stock_movement as consumption
 	ON consumption.item_id = items_and_stores.item_id 
 		AND consumption.store_id = items_and_stores.store_id;
 
@@ -43,3 +87,7 @@ LEFT OUTER JOIN
 	) AS stock
 	ON stock.item_id = items_and_stores.item_id 
 		AND stock.store_id = items_and_stores.store_id
+
+-- https://github.com/sussol/msupply/blob/master/Project/Sources/Methods/aggregator_stockMovement.4dm
+-- TODO are all of sc, ci, si type transactions synced, and are all of the dates set correctly ?
+
--- server/repository/src/schema/item_stats.rs
+++ server/repository/src/schema/item_stats.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveDate;
+use chrono::{NaiveDate, NaiveDateTime};
 use util::Defaults;
 
 #[derive(Clone, Queryable, Debug, PartialEq)]
@@ -18,10 +18,32 @@ pub struct StockOnHandRow {
     pub available_stock_on_hand: i64,
 }
 
+#[derive(Clone, Queryable, Debug, PartialEq)]
+pub struct StockMovementRow {
+    pub id: String,
+    pub item_id: String,
+    pub store_id: String,
+    pub quantity: i32,
+    pub datetime: NaiveDateTime,
+}
+
 impl Default for ConsumptionRow {
     fn default() -> Self {
         Self {
             date: Defaults::naive_date(),
+            // Default
+            id: Default::default(),
+            item_id: Default::default(),
+            store_id: Default::default(),
+            quantity: Default::default(),
+        }
+    }
+}
+
+impl Default for StockMovementRow {
+    fn default() -> Self {
+        Self {
+            datetime: Defaults::naive_date_time(),
             // Default
             id: Default::default(),
             item_id: Default::default(),
