8f3d7da3e890d16753e6626b9ab9d0e971b48726
Mark Prins <mark@malcolmprins.net>
2022-03-30 14:25:18 +1300

    prevent duplicate alert modals
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/9396ebb71bd27d04cf3498d14aba0bbbce771118

--- client/packages/common/src/ui/components/modals/AlertModal/AlertModalContext.tsx
+++ client/packages/common/src/ui/components/modals/AlertModal/AlertModalContext.tsx
@@ -1,28 +1,32 @@
-import { createContext } from 'react';
+import React, { createContext } from 'react';
 
 export interface AlertModalState {
   open: boolean;
-  message: string;
+  message: string | React.ReactNode;
   title: string;
   iconType?: 'alert';
   onOk?: () => void;
+  important?: boolean;
 }
 
 export interface AlertModalControllerState extends AlertModalState {
   setState: (state: AlertModalState) => void;
   setOpen: (open: boolean) => void;
-  setMessage: (message: string) => void;
+  setMessage: (message: string | React.ReactNode) => void;
   setTitle: (title: string) => void;
   setOnOk: (onOk: () => void) => void;
+  setImportant: (important: boolean) => void;
 }
 
 export const AlertModalContext = createContext<AlertModalControllerState>({
   open: false,
   message: '',
   title: '',
+  important: false,
   setOpen: () => {},
   setState: () => {},
   setMessage: () => {},
   setTitle: () => {},
   setOnOk: () => {},
+  setImportant: () => {},
 });
