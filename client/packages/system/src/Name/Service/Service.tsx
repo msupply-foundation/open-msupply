442f021a04be8ed10ab7d302ac10e81781966309
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-03-31 20:01:38 +1300

    Fix link

--- client/packages/system/src/Name/Service/Service.tsx
+++ client/packages/system/src/Name/Service/Service.tsx
@@ -2,27 +2,16 @@ import React, { FC } from 'react';
 import { RouteBuilder, Routes, Route } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
 import { NameListView } from '../ListView';
-// import { NameDetailView } from '../DetailModal';
 
 export const Service: FC = () => {
   const customersRoute = RouteBuilder.create(AppRoute.Customer).build();
 
-  // const customerRoute = RouteBuilder.create(AppRoute.Customer)
-  //   .addPart(':id')
-  //   .build();
-
   const suppliersRoute = RouteBuilder.create(AppRoute.Suppliers).build();
-
-  // const supplierRoute = RouteBuilder.create(AppRoute.Suppliers)
-  //   .addPart(':id')
-  //   .build();
 
   return (
     <Routes>
       <Route path={customersRoute} element={<NameListView type="customer" />} />
-      {/* <Route path={customerRoute} element={<NameDetailView />} /> */}
       <Route path={suppliersRoute} element={<NameListView type="supplier" />} />
-      {/* <Route path={supplierRoute} element={<NameDetailView />} /> */}
     </Routes>
   );
 };
