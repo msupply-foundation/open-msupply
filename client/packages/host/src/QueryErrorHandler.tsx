91a1a8b85aa135bd4bc9589386547f436ba41e10
Mark Prins <mark@malcolmprins.net>
2022-04-01 17:44:10 +1300

    error notification for queries

---
+++ client/packages/host/src/QueryErrorHandler.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useNotification } from '@common/hooks';
+import { useTranslation } from '@common/intl';
+import { useQueryClient } from 'react-query';
+
+export const QueryErrorHandler = () => {
+  const client = useQueryClient();
+  const { error } = useNotification();
+  const t = useTranslation();
+
+  const generalError = t('error.general-query-error');
+
+  useEffect(() => {
+    const currentDefaults = client.getDefaultOptions();
+    client.setDefaultOptions({
+      queries: {
+        ...currentDefaults.queries,
+        onError: e => error((e as Error).message || generalError)(),
+      },
+      mutations: currentDefaults.mutations,
+    });
+  }, []);
+
+  return <></>;
+};
