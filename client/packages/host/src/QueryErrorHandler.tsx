dd3a69cc34d7cd39ec4e3b17f1ee4d6df881f414
Mark Prins <mark@malcolmprins.net>
2022-04-05 16:15:34 +1200

    re-toast on change of location

--- client/packages/host/src/QueryErrorHandler.tsx
+++ client/packages/host/src/QueryErrorHandler.tsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useNotification } from '@common/hooks';
 import { useTranslation } from '@common/intl';
 import { useQueryClient } from 'react-query';
+import { useLocation } from '@openmsupply-client/common';
 
 export const QueryErrorHandler = () => {
   const client = useQueryClient();
   const { error } = useNotification();
   const t = useTranslation();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
+  const location = useLocation();
   const generalError = t('error.general-query-error');
 
   useEffect(() => {
     if (!!errorMessage) {
       error(errorMessage)();
     }
-  }, [errorMessage]);
+  }, [errorMessage, location.pathname]);
 
   useEffect(() => {
     const currentDefaults = client.getDefaultOptions();
