210cc2aa083aa48a26182a544b816f78a240b365
Mark Prins <mark@malcolmprins.net>
2022-04-08 13:57:27 +1200

    change comment column
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/02e4f503d95196e1e802491dfe9777545d084df9

---
+++ client/packages/common/src/ui/layout/tables/columns/CommentPopoverColumn/CommentPopoverColumn.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { RecordWithId } from '@common/types';
+import { ColumnAlign, ColumnDefinition } from '../types';
+import { MessageSquareIcon } from '@common/icons';
+import { PaperHoverPopover, PaperPopoverSection } from '@common/components';
+import { useTranslation } from '@common/intl';
+
+export const getCommentPopoverColumn = <T extends RecordWithId>(
+  label?: string
+): ColumnDefinition<T> => ({
+  key: 'comment',
+  sortable: false,
+  align: ColumnAlign.Center,
+  width: 60,
+  Header: () => {
+    return null;
+  },
+
+  Cell: ({ column, rowData, rows }) => {
+    const t = useTranslation('common');
+    const value = column.accessor({ rowData, rows });
+
+    return value ? (
+      <PaperHoverPopover
+        width={400}
+        Content={
+          <PaperPopoverSection label={label ?? t('label.comment')}>
+            {String(value)}
+          </PaperPopoverSection>
+        }
+      >
+        <MessageSquareIcon sx={{ fontSize: 16 }} color="primary" />
+      </PaperHoverPopover>
+    ) : null;
+  },
+});
