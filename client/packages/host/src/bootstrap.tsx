92b5326881320f6c38fd6d205fccbde5f7c6a906
Mark Prins <mark@malcolmprins.net>
2022-04-20 18:26:07 +1200

    remove comments

--- client/packages/host/src/bootstrap.tsx
+++ client/packages/host/src/bootstrap.tsx
@@ -6,9 +6,3 @@ import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(<App />);
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
