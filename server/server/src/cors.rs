0cae59ecbda5e0d06007e16d68df7bd83e8ab195
James Brunskill <james@msupply.foundation>
2022-05-09 17:20:51 +1200

    Allow same-site and same-origin CORS requests
    Source: https://github.com/msupply-foundation/remote-server/commit/b8d1ebe219a0404ef69b8c2fc74d5f9118a0eb6e

---
+++ server/server/src/cors.rs
@@ -0,0 +1,42 @@
+use crate::settings::Settings;
+use actix_cors::Cors;
+use actix_web::http::header;
+
+pub fn cors_policy(config_settings: &Settings) -> Cors {
+    let cors = if config_settings.server.develop && config_settings.server.debug_cors_permissive {
+        Cors::permissive()
+    } else {
+        let mut cors_tmp = Cors::default()
+            .supports_credentials()
+            .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+            .allowed_headers(vec![
+                header::AUTHORIZATION,
+                header::ACCEPT,
+                header::CONTENT_TYPE,
+            ])
+            .max_age(3600);
+        for origin in config_settings.server.cors_origins.iter() {
+            cors_tmp = cors_tmp.allowed_origin(origin);
+        }
+        cors_tmp = cors_tmp.allowed_origin_fn(|_header, req| {
+            //allow requests where Sec-Fetch-Site is set to same-origin, same-site or none
+            let sec_fetch_site_header = req
+                .headers
+                .get("Sec-Fetch-Site")
+                .map(header::HeaderValue::as_bytes);
+
+            match sec_fetch_site_header {
+                None => false,
+                Some(bytes) => match bytes {
+                    b"cross-site" => false,
+                    b"same-origin" => true,
+                    b"same-site" => true,
+                    b"none" => true,
+                    _ => false,
+                },
+            }
+        });
+        cors_tmp
+    };
+    cors
+}
