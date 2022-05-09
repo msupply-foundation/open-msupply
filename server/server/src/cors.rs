d40bb8d61c86037eae7fade5750d827f4552ba98
James Brunskill <james@msupply.foundation>
2022-05-10 08:48:36 +1200

    Rename cors_tmp

--- server/server/src/cors.rs
+++ server/server/src/cors.rs
@@ -6,7 +6,7 @@ pub fn cors_policy(config_settings: &Settings) -> Cors {
     let cors = if config_settings.server.develop && config_settings.server.debug_cors_permissive {
         Cors::permissive()
     } else {
-        let mut cors_tmp = Cors::default()
+        let mut cors = Cors::default()
             .supports_credentials()
             .allowed_methods(vec!["GET", "POST", "OPTIONS"])
             .allowed_headers(vec![
@@ -16,9 +16,9 @@ pub fn cors_policy(config_settings: &Settings) -> Cors {
             ])
             .max_age(3600);
         for origin in config_settings.server.cors_origins.iter() {
-            cors_tmp = cors_tmp.allowed_origin(origin);
+            cors = cors.allowed_origin(origin);
         }
-        cors_tmp = cors_tmp.allowed_origin_fn(|_header, req| {
+        cors = cors.allowed_origin_fn(|_header, req| {
             //allow requests where Sec-Fetch-Site is set to same-origin, same-site or none
             let sec_fetch_site_header = req
                 .headers
@@ -36,7 +36,7 @@ pub fn cors_policy(config_settings: &Settings) -> Cors {
                 },
             }
         });
-        cors_tmp
+        cors
     };
     cors
 }
