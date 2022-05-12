ab5053d78074c7056d626adb7626ae082821c366
Clemens Zeidler <clemens@msupply.foundation>
2022-05-12 13:35:52 +1200

    Add a CertPlugin for making requests to the remote server
    Source: https://github.com/msupply-foundation/openmsupply-client/commit/ab8af275bc7b4e59e409946e4aa909c908d0637c

--- client/packages/android/app/src/main/java/org/openmsupply/client/MainActivity.java
+++ client/packages/android/app/src/main/java/org/openmsupply/client/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 
+import org.openmsupply.client.certplugin.CertPlugin;
+
 public class MainActivity extends BridgeActivity {
     RemoteServer server = new RemoteServer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        registerPlugin(CertPlugin.class);
 
         String path = getFilesDir().getAbsolutePath();
         String cache = getCacheDir().getAbsolutePath();
