8fc0fb59f75c99877aa5f1200d8a2b4178fc9205
Andrei <andrei.evguenov@gmail.com>
2022-05-24 20:09:19 +1200

    Update electron client UI
    Source: https://github.com/msupply-foundation/open-msupply/commit/ca299cc96146d8155f5e5a0b636256039aa324cb

---
+++ client/packages/electron/src/discovery/ServerDiscovery.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  useElectronClient,
+  InlineSpinner,
+  formatServer,
+  BaseButton,
+  AlertIcon,
+  Box,
+  Typography,
+  useTranslation,
+} from '@openmsupply-client/common';
+
+import { ServerDiscoveryLayout } from './ServerDiscoveryLayout';
+
+export const ServerDiscovery = () => {
+  const { servers, discoveryTimedOut } = useElectronClient(true);
+  const t = useTranslation('app');
+
+  return (
+    <ServerDiscoveryLayout
+      LoadingIndicator={
+        servers.length === 0 && <InlineSpinner color="secondary" showText />
+      }
+      ServerNodes={servers.map(server => {
+        const display = formatServer(server);
+
+        return (
+          <BaseButton
+            key={display}
+            onClick={() => {
+              window.electronAPI.connectToServer(server);
+            }}
+          >
+            {display}
+          </BaseButton>
+        );
+      })}
+      ErrorFindingServer={
+        discoveryTimedOut && (
+          <Box display="flex" sx={{ color: 'error.main' }} gap={1}>
+            <Box>
+              <AlertIcon />
+            </Box>
+            <Box>
+              <Typography sx={{ color: 'inherit' }}>
+                {t('error.finding-servers')}
+              </Typography>
+            </Box>
+          </Box>
+        )
+      }
+    />
+  );
+};
