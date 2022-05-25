7cfdfb5fa2f3058ad1ba5871c45a6daa5d9e06c1
Andrei <andrei.evguenov@gmail.com>
2022-05-25 13:05:02 +1200

    Reinstate ability to run local and remote

--- client/packages/config/src/config.ts
+++ client/packages/config/src/config.ts
@@ -1,12 +1,16 @@
 import { Capacitor } from '@capacitor/core';
 
+
+declare const API_HOST: string;
+
 // For production we get URL relative to web app
-const { port, hostname, protocol } = window.location;
-
 const isProductionBuild = process.env['NODE_ENV'] === 'production';
+// http://localhost:8000 is default for remote server
+// API_HOST is available through webpack.DefinePlugin (i.e. webpack server --env API_HOST='http://localhost:8001')
+const devServerURL = API_HOST || 'http://localhost:8000';
 
-const devServerURL = 'https://demo-open.msupply.org:8000'; // Demo - site URL
 // const devServerURL = 'http://localhost:8000'; // - default URL for the backend graphql server
+const { port, hostname, protocol } = window.location;
 
 // For mobile always use https://localhost:8000, as per MainActivity.java
 const productionServerUrl =
@@ -15,7 +19,7 @@ const productionServerUrl =
     : 'https://localhost:8000';
 
 const config = {
-  API_HOST: isProductionBuild ? productionServerUrl : devServerURL,
+  API_HOST: isProductionBuild ? productionServerUrl : devServerURL
 };
 
 export default config;
