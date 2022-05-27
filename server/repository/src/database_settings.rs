37721da5cb5a139196dfc7abd40ef41119510367
James Brunskill <james@msupply.foundation>
2022-05-27 17:56:27 +1200

    WIP tests for sqlite concurrency issue

--- server/repository/src/database_settings.rs
+++ server/repository/src/database_settings.rs
@@ -1,7 +1,15 @@
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::{
+    connection::SimpleConnection,
+    r2d2::{ConnectionManager, Pool},
+    SqliteConnection,
+};
 use serde;
 
 use crate::db_diesel::{DBBackendConnection, StorageConnectionManager};
+
+//WAIT up to 5 SECONDS for lock in SQLITE(https://www.sqlite.org/c3ref/busy_timeout.html)
+#[cfg(not(feature = "postgres"))]
+const SQLITE_LOCKWAIT_MS: u32 = 5000;
 
 #[derive(serde::Deserialize, Clone)]
 pub struct DatabaseSettings {
@@ -56,10 +64,64 @@ impl DatabaseSettings {
     }
 }
 
+// feature sqlite
+#[cfg(not(feature = "postgres"))]
+#[derive(Debug)]
+pub struct SqliteConnectionOptions {
+    pub enable_wal: bool,
+    pub enable_foreign_keys: bool,
+    pub busy_timeout_ms: Option<u32>,
+}
+// feature sqlite
+#[cfg(not(feature = "postgres"))]
+impl diesel::r2d2::CustomizeConnection<SqliteConnection, diesel::r2d2::Error>
+    for SqliteConnectionOptions
+{
+    fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), diesel::r2d2::Error> {
+        (|| {
+            if self.enable_wal {
+                // println!("Adding WAL");
+                let r =
+                    conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL;");
+                println!("{:?}", r);
+            }
+            if self.enable_foreign_keys {
+                // println!("Adding foreign_keys");
+                let r = conn.batch_execute("PRAGMA foreign_keys = ON;");
+                println!("{:?}", r);
+            }
+            if let Some(d) = self.busy_timeout_ms {
+                // println!("Adding Busy Timeout");
+                let r = conn.batch_execute(&format!("PRAGMA busy_timeout = {};", d));
+                println!("{:?}", r);
+            }
+            Ok(())
+        })()
+        .map_err(diesel::r2d2::Error::QueryError)
+    }
+}
+
+// feature postgres
+#[cfg(feature = "postgres")]
 pub fn get_storage_connection_manager(settings: &DatabaseSettings) -> StorageConnectionManager {
-    // TODO fix connection string for sqlite
     let connection_manager =
         ConnectionManager::<DBBackendConnection>::new(&settings.connection_string());
     let pool = Pool::new(connection_manager).expect("Failed to connect to database");
+    StorageConnectionManager::new(pool)
+}
+
+// feature sqlite
+#[cfg(not(feature = "postgres"))]
+pub fn get_storage_connection_manager(settings: &DatabaseSettings) -> StorageConnectionManager {
+    let connection_manager =
+        ConnectionManager::<DBBackendConnection>::new(&settings.connection_string());
+    let pool = Pool::builder()
+        .connection_customizer(Box::new(SqliteConnectionOptions {
+            enable_wal: true,
+            enable_foreign_keys: true,
+            busy_timeout_ms: Some(SQLITE_LOCKWAIT_MS),
+        }))
+        .build(connection_manager)
+        .expect("Failed to connect to database");
     StorageConnectionManager::new(pool)
 }
