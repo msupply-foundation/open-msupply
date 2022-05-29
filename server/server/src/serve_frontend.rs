57656bd746f288a5a9fa4a685a676bb51fb87398
Andrei <andrei.evguenov@gmail.com>
2022-05-30 11:53:23 +1200

    Fix rust compilation
    Source: https://github.com/msupply-foundation/open-msupply/commit/6c366ab0bb4a147fe76bf8662f8f2dc7d6c19f73

--- server/server/src/serve_frontend.rs
+++ server/server/src/serve_frontend.rs
@@ -1,5 +1,8 @@
-use actix_web::{get, web::ServiceConfig, HttpRequest, HttpResponse, Responder};
-use mime_guess::from_path;
+use actix_web::{
+    get, http::header::ContentType, web::ServiceConfig, HttpRequest, HttpResponse, Responder,
+};
+use mime_guess::{from_path, mime};
+use reqwest::StatusCode;
 use rust_embed::RustEmbed;
 
 #[derive(RustEmbed)]
