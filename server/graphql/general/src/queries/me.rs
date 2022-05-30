a68f4fd9a96ede958677d1d3e7522fb319f0683c
roxy-codes <roxy@msupply.foundation>
2022-05-30 14:41:22 +1200

    Add site_id filter to me endpoint
    Source: https://github.com/msupply-foundation/open-msupply/commit/7a92ce132f74df8e5eeeab49aacdf3c5d0338057

--- server/graphql/general/src/queries/me.rs
+++ server/graphql/general/src/queries/me.rs
@@ -5,6 +5,7 @@ use graphql_core::{
 };
 
 use graphql_types::types::UserNode;
+use repository::{KeyValueStoreRepository, KeyValueType};
 use service::auth::{Resource, ResourceAccessRequest};
 use service::user_account::UserAccountService;
 
@@ -25,7 +26,13 @@ pub fn me(ctx: &Context<'_>) -> Result<UserResponse> {
     let service_provider = ctx.service_provider();
     let service_ctx = service_provider.context()?;
     let user_service = UserAccountService::new(&service_ctx.connection);
-    let user = match user_service.find_user(&user.user_id) {
+
+    let key_value_store = KeyValueStoreRepository::new(&service_ctx.connection);
+    let site_id = key_value_store
+        .get_i32(KeyValueType::SettingsSyncSiteId)?
+        .unwrap();
+
+    let user = match user_service.find_user(&user.user_id, site_id) {
         Ok(Some(user)) => user,
         Ok(None) => {
             return Err(StandardGraphqlError::InternalError(
