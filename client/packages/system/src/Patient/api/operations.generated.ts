7610251e470ac4a7dbbcda22054db31a2f8621a7
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-06-08 12:42:22 +1200

    WIP
    Source: https://github.com/msupply-foundation/open-msupply/commit/a591fa30f16cf363252abaf89fc97997a5fcfc1d

--- client/packages/system/src/Patient/api/operations.generated.ts
+++ client/packages/system/src/Patient/api/operations.generated.ts
@@ -3,43 +3,10 @@ import * as Types from '@openmsupply-client/common';
 import { GraphQLClient } from 'graphql-request';
 import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
-import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw';
-export type PatientRowFragment = {
-  __typename: 'NameNode';
-  id: string;
-  code: string;
-  firstName?: string | null;
-  lastName?: string | null;
-  name: string;
-  dateOfBirth?: string | null;
-};
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
+export type PatientRowFragment = { __typename: 'NameNode', id: string, code: string, firstName?: string | null, lastName?: string | null, name: string, dateOfBirth?: string | null };
 
-export type PatientFragment = {
-  __typename: 'NameNode';
-  address1?: string | null;
-  address2?: string | null;
-  chargeCode?: string | null;
-  code: string;
-  comment?: string | null;
-  country?: string | null;
-  createdDatetime?: string | null;
-  // dateOfBirth?: string | null;
-  email?: string | null;
-  firstName?: string | null;
-  id: string;
-  isCustomer: boolean;
-  isDonor: boolean;
-  isManufacturer: boolean;
-  isOnHold: boolean;
-  isSupplier: boolean;
-  isSystemName: boolean;
-  isVisible: boolean;
-  name: string;
-  phone?: string | null;
-  type: Types.NameNodeType;
-  website?: string | null;
-  store?: { __typename: 'StoreNode'; id: string; code: string } | null;
-};
+export type PatientFragment = { __typename: 'NameNode', address1?: string | null, address2?: string | null, chargeCode?: string | null, code: string, comment?: string | null, country?: string | null, createdDatetime?: string | null, email?: string | null, firstName?: string | null, id: string, isCustomer: boolean, isDonor: boolean, isManufacturer: boolean, isOnHold: boolean, isSupplier: boolean, isSystemName: boolean, isVisible: boolean, name: string, phone?: string | null, type: Types.NameNodeType, website?: string | null, store?: { __typename: 'StoreNode', id: string, code: string } | null };
 
 export type PatientsQueryVariables = Types.Exact<{
   storeId: Types.Scalars['String'];
@@ -50,188 +17,101 @@ export type PatientsQueryVariables = Types.Exact<{
   filter?: Types.InputMaybe<Types.NameFilterInput>;
 }>;
 
-export type PatientsQuery = {
-  __typename: 'FullQuery';
-  names: {
-    __typename: 'NameConnector';
-    totalCount: number;
-    nodes: Array<{
-      __typename: 'NameNode';
-      id: string;
-      code: string;
-      firstName?: string | null;
-      lastName?: string | null;
-      name: string;
-      // dateOfBirth?: string | null;
-    }>;
-  };
-};
+
+export type PatientsQuery = { __typename: 'FullQuery', names: { __typename: 'NameConnector', totalCount: number, nodes: Array<{ __typename: 'NameNode', id: string, code: string, firstName?: string | null, lastName?: string | null, name: string, dateOfBirth?: string | null }> } };
 
 export type PatientByIdQueryVariables = Types.Exact<{
   storeId: Types.Scalars['String'];
   nameId: Types.Scalars['String'];
 }>;
 
-export type PatientByIdQuery = {
-  __typename: 'FullQuery';
-  names: {
-    __typename: 'NameConnector';
-    totalCount: number;
-    nodes: Array<{
-      __typename: 'NameNode';
-      address1?: string | null;
-      address2?: string | null;
-      chargeCode?: string | null;
-      code: string;
-      comment?: string | null;
-      country?: string | null;
-      createdDatetime?: string | null;
-      // dateOfBirth?: string | null;
-      email?: string | null;
-      firstName?: string | null;
-      id: string;
-      isCustomer: boolean;
-      isDonor: boolean;
-      isManufacturer: boolean;
-      isOnHold: boolean;
-      isSupplier: boolean;
-      isSystemName: boolean;
-      isVisible: boolean;
-      name: string;
-      phone?: string | null;
-      type: Types.NameNodeType;
-      website?: string | null;
-      store?: { __typename: 'StoreNode'; id: string; code: string } | null;
-    }>;
-  };
-};
+
+export type PatientByIdQuery = { __typename: 'FullQuery', names: { __typename: 'NameConnector', totalCount: number, nodes: Array<{ __typename: 'NameNode', address1?: string | null, address2?: string | null, chargeCode?: string | null, code: string, comment?: string | null, country?: string | null, createdDatetime?: string | null, email?: string | null, firstName?: string | null, id: string, isCustomer: boolean, isDonor: boolean, isManufacturer: boolean, isOnHold: boolean, isSupplier: boolean, isSystemName: boolean, isVisible: boolean, name: string, phone?: string | null, type: Types.NameNodeType, website?: string | null, store?: { __typename: 'StoreNode', id: string, code: string } | null }> } };
 
 export const PatientRowFragmentDoc = gql`
-  fragment PatientRow on NameNode {
-    id
-    code
-    firstName
-    lastName
-    name
-    # dateOfBirth
-  }
-`;
+    fragment PatientRow on NameNode {
+  id
+  code
+  firstName
+  lastName
+  name
+  dateOfBirth
+}
+    `;
 export const PatientFragmentDoc = gql`
-  fragment Patient on NameNode {
-    address1
-    address2
-    chargeCode
-    code
-    comment
-    country
-    createdDatetime
-    # dateOfBirth
-    email
-    firstName
+    fragment Patient on NameNode {
+  address1
+  address2
+  chargeCode
+  code
+  comment
+  country
+  createdDatetime
+  email
+  firstName
+  id
+  isCustomer
+  isDonor
+  isManufacturer
+  isOnHold
+  isSupplier
+  isSystemName
+  isVisible
+  name
+  phone
+  type
+  website
+  store {
     id
-    isCustomer
-    isDonor
-    isManufacturer
-    isOnHold
-    isSupplier
-    isSystemName
-    isVisible
-    name
-    phone
-    type
-    website
-    store {
-      id
-      code
-    }
+    code
   }
-`;
+}
+    `;
 export const PatientsDocument = gql`
-  query patients(
-    $storeId: String!
-    $key: NameSortFieldInput!
-    $desc: Boolean
-    $first: Int
-    $offset: Int
-    $filter: NameFilterInput
+    query patients($storeId: String!, $key: NameSortFieldInput!, $desc: Boolean, $first: Int, $offset: Int, $filter: NameFilterInput) {
+  names(
+    storeId: $storeId
+    page: {first: $first, offset: $offset}
+    sort: {key: $key, desc: $desc}
+    filter: $filter
   ) {
-    names(
-      storeId: $storeId
-      page: { first: $first, offset: $offset }
-      sort: { key: $key, desc: $desc }
-      filter: $filter
-    ) {
-      ... on NameConnector {
-        __typename
-        nodes {
-          ...PatientRow
-        }
-        totalCount
+    ... on NameConnector {
+      __typename
+      nodes {
+        ...PatientRow
       }
+      totalCount
     }
   }
-  ${PatientRowFragmentDoc}
-`;
+}
+    ${PatientRowFragmentDoc}`;
 export const PatientByIdDocument = gql`
-  query patientById($storeId: String!, $nameId: String!) {
-    names(storeId: $storeId, filter: { id: { equalTo: $nameId } }) {
-      ... on NameConnector {
-        __typename
-        nodes {
-          ...Patient
-        }
-        totalCount
+    query patientById($storeId: String!, $nameId: String!) {
+  names(storeId: $storeId, filter: {id: {equalTo: $nameId}}) {
+    ... on NameConnector {
+      __typename
+      nodes {
+        ...Patient
       }
+      totalCount
     }
   }
-  ${PatientFragmentDoc}
-`;
+}
+    ${PatientFragmentDoc}`;
 
-export type SdkFunctionWrapper = <T>(
-  action: (requestHeaders?: Record<string, string>) => Promise<T>,
-  operationName: string,
-  operationType?: string
-) => Promise<T>;
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
 
-const defaultWrapper: SdkFunctionWrapper = (
-  action,
-  _operationName,
-  _operationType
-) => action();
 
-export function getSdk(
-  client: GraphQLClient,
-  withWrapper: SdkFunctionWrapper = defaultWrapper
-) {
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationType) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
-    patients(
-      variables: PatientsQueryVariables,
-      requestHeaders?: Dom.RequestInit['headers']
-    ): Promise<PatientsQuery> {
-      return withWrapper(
-        wrappedRequestHeaders =>
-          client.request<PatientsQuery>(PatientsDocument, variables, {
-            ...requestHeaders,
-            ...wrappedRequestHeaders,
-          }),
-        'patients',
-        'query'
-      );
+    patients(variables: PatientsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<PatientsQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<PatientsQuery>(PatientsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'patients', 'query');
     },
-    patientById(
-      variables: PatientByIdQueryVariables,
-      requestHeaders?: Dom.RequestInit['headers']
-    ): Promise<PatientByIdQuery> {
-      return withWrapper(
-        wrappedRequestHeaders =>
-          client.request<PatientByIdQuery>(PatientByIdDocument, variables, {
-            ...requestHeaders,
-            ...wrappedRequestHeaders,
-          }),
-        'patientById',
-        'query'
-      );
-    },
+    patientById(variables: PatientByIdQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<PatientByIdQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<PatientByIdQuery>(PatientByIdDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'patientById', 'query');
+    }
   };
 }
 export type Sdk = ReturnType<typeof getSdk>;
@@ -247,13 +127,11 @@ export type Sdk = ReturnType<typeof getSdk>;
  *   )
  * })
  */
-export const mockPatientsQuery = (
-  resolver: ResponseResolver<
-    GraphQLRequest<PatientsQueryVariables>,
-    GraphQLContext<PatientsQuery>,
-    any
-  >
-) => graphql.query<PatientsQuery, PatientsQueryVariables>('patients', resolver);
+export const mockPatientsQuery = (resolver: ResponseResolver<GraphQLRequest<PatientsQueryVariables>, GraphQLContext<PatientsQuery>, any>) =>
+  graphql.query<PatientsQuery, PatientsQueryVariables>(
+    'patients',
+    resolver
+  )
 
 /**
  * @param resolver a function that accepts a captured request and may return a mocked response.
@@ -266,14 +144,8 @@ export const mockPatientsQuery = (
  *   )
  * })
  */
-export const mockPatientByIdQuery = (
-  resolver: ResponseResolver<
-    GraphQLRequest<PatientByIdQueryVariables>,
-    GraphQLContext<PatientByIdQuery>,
-    any
-  >
-) =>
+export const mockPatientByIdQuery = (resolver: ResponseResolver<GraphQLRequest<PatientByIdQueryVariables>, GraphQLContext<PatientByIdQuery>, any>) =>
   graphql.query<PatientByIdQuery, PatientByIdQueryVariables>(
     'patientById',
     resolver
-  );
+  )
