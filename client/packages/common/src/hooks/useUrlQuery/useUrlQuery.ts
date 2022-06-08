1f1001607560b3961fccf1cef8025c2780f1e253
Mark Prins <mark@malcolmprins.net>
2022-06-09 09:51:28 +1200

    fix filter

--- client/packages/common/src/hooks/useUrlQuery/useUrlQuery.ts
+++ client/packages/common/src/hooks/useUrlQuery/useUrlQuery.ts
@@ -4,6 +4,17 @@ interface UrlQueryObject {
   [key: string]: string | number | boolean;
 }
 
+// here you can override the parsers for specific query params
+// an example is the filter, which must allow filtering by numeric codes
+// if this is parsed as numeric, the query param changes filter=0300 to filter=300
+// which then does not match against codes, as the filter is usually a 'startsWith'
+const DefaultParsers: Record<
+  string,
+  (value: string) => string | boolean | number
+> = {
+  filter: (value: string) => value,
+};
+
 export const useUrlQuery = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -30,6 +41,8 @@ export const useUrlQuery = () => {
 const parseSearchParams = (searchParams: URLSearchParams) =>
   Object.fromEntries(
     Array.from(searchParams.entries()).map(([key, value]) => {
+      const parser = DefaultParsers[key]; // written out longhand to avoid TS complaint
+      if (parser) return [key, parser(value)];
       if (!isNaN(Number(value))) return [key, Number(value)];
       if (value === 'true') return [key, true];
       if (value === 'false') return [key, false];
