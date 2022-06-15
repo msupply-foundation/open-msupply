dc52055bedac036a8c572a14457e24d58e74856a
roxy-codes <roxy@msupply.foundation>
2022-06-15 17:22:56 +1200

    Orphan line + refactor
    Source: https://github.com/msupply-foundation/open-msupply/commit/9fb7e3ad23d415d009f1531f880bacabdb00ca23

--- server/service/src/app_data.rs
+++ server/service/src/app_data.rs
@@ -1,7 +1,6 @@
 use std::{
     fs::File,
     io::{Error, Read, Write},
-    path::PathBuf,
 };
 
 use serde::{Deserialize, Serialize};
@@ -11,9 +10,11 @@ pub struct AppData {
     pub site_hardware_id: String,
 }
 
+const APP_DATA_FILE: &str = "settings_app_data.yaml";
+
 impl AppData {
     pub fn load_from_file() -> Result<AppData, Error> {
-        let mut file = File::open("settings_app_data.yaml")?;
+        let mut file = File::open(APP_DATA_FILE)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
         let app_data: AppData = serde_yaml::from_str(&contents).expect("Failed to parse app data");
@@ -24,15 +25,11 @@ impl AppData {
         self.site_hardware_id.is_empty()
     }
 
-    pub fn write_to_file(path: &PathBuf, hardware_id: String) -> Result<Self, Error> {
-        let mut file = File::open(path)?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        let mut app_data: AppData =
-            serde_yaml::from_str(&contents).expect("Failed to parse app data");
+    pub fn write_to_file(hardware_id: String) -> Result<Self, Error> {
+        let mut app_data = AppData::load_from_file()?;
         if app_data.site_hardware_id.is_empty() {
             app_data.site_hardware_id = hardware_id;
-            let mut file = File::create(path)?;
+            let mut file = File::create(APP_DATA_FILE)?;
             file.write_all(serde_yaml::to_string(&app_data).unwrap().as_bytes())?;
         }
         Ok(app_data)
