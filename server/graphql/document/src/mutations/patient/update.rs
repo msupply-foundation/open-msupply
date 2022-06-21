1110da007b2949736319e5b30f75eb6f2a61d345
Clemens Zeidler <clemens@msupply.foundation>
2022-06-22 09:53:51 +1200

    Add update patient endpoint
    Source: https://github.com/msupply-foundation/open-msupply/commit/e94e1f6dc69e9db371a3ce65f1f1136e52c2e399

---
+++ server/graphql/document/src/mutations/patient/update.rs
@@ -0,0 +1,80 @@
+use async_graphql::*;
+use graphql_core::{
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    document::patient::{UpdatePatient, UpdatePatientError},
+};
+
+use crate::queries::PatientNode;
+
+#[derive(InputObject)]
+pub struct UpdatePatientInput {
+    /// Patient document data
+    pub data: serde_json::Value,
+    /// The schema id used for the patient data
+    pub schema_id: Option<String>,
+    pub parent: String,
+}
+
+#[derive(Union)]
+pub enum UpdatePatientResponse {
+    Response(PatientNode),
+}
+
+pub fn update_patient(
+    ctx: &Context<'_>,
+    store_id: String,
+    input: UpdatePatientInput,
+) -> Result<UpdatePatientResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::MutateLocation,
+            store_id: Some(store_id.clone()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context()?;
+
+    match service_provider.patient_service.update_patient(
+        &service_context,
+        service_provider,
+        store_id,
+        &user.user_id,
+        UpdatePatient {
+            data: input.data,
+            schema_id: input.schema_id,
+            parent: Some(input.parent),
+        },
+    ) {
+        Ok(patient) => Ok(UpdatePatientResponse::Response(PatientNode { patient })),
+        Err(error) => {
+            let formatted_error = format!("{:#?}", error);
+            let std_err = match error {
+                UpdatePatientError::InvalidDataSchema(_) => {
+                    StandardGraphqlError::BadUserInput(formatted_error)
+                }
+                UpdatePatientError::InternalError(_) => {
+                    StandardGraphqlError::InternalError(formatted_error)
+                }
+                UpdatePatientError::DatabaseError(_) => {
+                    StandardGraphqlError::InternalError(formatted_error)
+                }
+                UpdatePatientError::InvalidPatientId => {
+                    StandardGraphqlError::BadUserInput(formatted_error)
+                }
+                UpdatePatientError::PatientExists => {
+                    StandardGraphqlError::BadUserInput(formatted_error)
+                }
+                UpdatePatientError::InvalidParentId => {
+                    StandardGraphqlError::BadUserInput(formatted_error)
+                }
+            };
+            Err(std_err.extend())
+        }
+    }
+}
