401631bf9f6a7b0be42c438abe2c9218bd8c8578
Clemens Zeidler <clemens@msupply.foundation>
2022-06-29 13:10:34 +1200

    Add basic patient search API (exact match only)
    Source: https://github.com/msupply-foundation/open-msupply/commit/ff0b29ff19e1c2cf5b343ea9dedb66dcd4e710a4

--- server/graphql/document/src/queries/mod.rs
+++ server/graphql/document/src/queries/mod.rs
@@ -6,5 +6,7 @@ pub mod json_schema;
 pub use self::json_schema::*;
 pub mod patient;
 pub use self::patient::*;
+pub mod patient_search;
+pub use self::patient_search::*;
 pub mod document_registry;
 pub use self::document_registry::*;
