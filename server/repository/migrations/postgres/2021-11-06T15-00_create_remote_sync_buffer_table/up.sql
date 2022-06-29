91657a6af007b5031b5ba2e54536cff78e1a599a
Andrei <andrei.evguenov@gmail.com>
2022-06-29 21:56:46 +1200

    Remove remote and central sync buffers
    Source: https://github.com/msupply-foundation/open-msupply/commit/6d9fce86ae803ca7f0510f23e7b527b58883da9c

--- server/repository/migrations/postgres/2021-11-06T15-00_create_remote_sync_buffer_table/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE remote_sync_buffer (
-    id TEXT NOT NULL PRIMARY KEY,
-    table_name TEXT NOT NULL,
-    record_id TEXT NOT NULL,
-    action TEXT NOT NULL,
-    data TEXT NOT NULL
-)
