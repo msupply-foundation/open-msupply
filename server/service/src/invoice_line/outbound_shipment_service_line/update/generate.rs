b3010c336a69770260c00f4834a14e7ef8e9604b
roxy-codes <roxy@msupply.foundation>
2022-07-07 11:53:24 +1200

    Refactor total after tax

--- server/service/src/invoice_line/outbound_shipment_service_line/update/generate.rs
+++ server/service/src/invoice_line/outbound_shipment_service_line/update/generate.rs
@@ -1,5 +1,7 @@
 use repository::{InvoiceLineRow, ItemRow};
 
+use crate::invoice::common::total_after_tax;
+
 use super::{UpdateOutboundShipmentServiceLine, UpdateOutboundShipmentServiceLineError};
 
 pub fn generate(
@@ -53,8 +55,7 @@ pub fn generate(
         update_line.note = Some(note);
     }
 
-    update_line.total_after_tax =
-        update_line.total_before_tax * (f64::from(1) + update_line.tax.unwrap_or_default() / 100.0);
+    update_line.total_after_tax = total_after_tax(update_line.total_before_tax, update_line.tax);
 
     Ok(update_line)
 }
