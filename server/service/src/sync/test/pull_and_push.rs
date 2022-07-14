f23ab2ad7b9e4f0154c6ed3f5c76324da25d102a
Andrei <andrei.evguenov@gmail.com>
2022-07-15 11:33:40 +1200

    Split check_records_against_database to allow for testing against IntegrationRecords
    Source: https://github.com/msupply-foundation/open-msupply/commit/74bf2cdee24a0c49b121790f40817f3f2d0f26df

--- server/service/src/sync/test/pull_and_push.rs
+++ server/service/src/sync/test/pull_and_push.rs
@@ -2,25 +2,34 @@ use crate::sync::{
     remote_data_synchroniser::translate_changelogs_to_push_records,
     synchroniser::integrate_and_translate_sync_buffer,
     test::{
-        check_records_against_database, extract_sync_buffer_rows,
-        test_data::{get_all_pull_upsert_test_records, get_all_push_test_records},
+        check_test_records_against_database, extract_sync_buffer_rows,
+        test_data::get_all_push_test_records,
     },
     translations::table_name_to_central,
 };
 use repository::{mock::MockDataInserts, test_db, SyncBufferRow, SyncBufferRowRepository};
-use util::{init_logger, LogLevel};
 
-use super::{insert_all_extra_data, test_data::get_all_pull_delete_test_records};
+use super::{
+    insert_all_extra_data,
+    test_data::{
+        get_all_pull_delete_central_test_records, get_all_pull_delete_remote_test_records,
+        get_all_pull_upsert_central_test_records, get_all_pull_upsert_remote_test_records,
+    },
+};
 
 #[actix_rt::test]
 async fn test_sync_pull_and_push() {
-    init_logger(LogLevel::Warn);
+    // util::init_logger(util::LogLevel::Warn);
 
     let (_, connection, _, _) =
         test_db::setup_all("test_sync_pull_and_push", MockDataInserts::all()).await;
 
     // Test Pull Upsert
-    let test_records = get_all_pull_upsert_test_records();
+    let test_records = vec![
+        get_all_pull_upsert_central_test_records(),
+        get_all_pull_upsert_remote_test_records(),
+    ]
+    .concat();
     insert_all_extra_data(&test_records, &connection).await;
     let sync_reords: Vec<SyncBufferRow> = extract_sync_buffer_rows(&test_records);
 
@@ -30,30 +39,33 @@ async fn test_sync_pull_and_push() {
 
     integrate_and_translate_sync_buffer(&connection).unwrap();
 
-    check_records_against_database(&connection, test_records).await;
+    check_test_records_against_database(&connection, test_records).await;
 
     // Test Push
     let test_records = get_all_push_test_records();
-    for record in test_records {
-        let expected_row_id = record.change_log.row_id.to_string();
-        let expected_table_name = table_name_to_central(&record.change_log.table_name);
+    for test_record in test_records {
+        let expected_row_id = test_record.change_log.row_id.to_string();
+        let expected_table_name = table_name_to_central(&test_record.change_log.table_name);
         let mut result =
-            translate_changelogs_to_push_records(&connection, vec![record.change_log.clone()])
+            translate_changelogs_to_push_records(&connection, vec![test_record.change_log.clone()])
                 .unwrap();
         // we currently only have one entry in the data_list
         let result = result
             .pop()
-            .expect(&format!("Could not translate {:#?}", record));
-        // tests only do upsert right now, so there must be Some data:
-        let data = result.data.unwrap();
+            .expect(&format!("Could not translate {:#?}", test_record));
+        let record = result.record;
 
-        assert_eq!(result.record_id, expected_row_id);
-        assert_eq!(result.record_type, expected_table_name);
-        assert_eq!(data, record.push_data);
+        assert_eq!(record.record_id, expected_row_id);
+        assert_eq!(record.table_name, expected_table_name);
+        assert_eq!(record.data, test_record.push_data);
     }
 
     // Test Pull Delete
-    let test_records = get_all_pull_delete_test_records();
+    let test_records = vec![
+        get_all_pull_delete_central_test_records(),
+        get_all_pull_delete_remote_test_records(),
+    ]
+    .concat();
     insert_all_extra_data(&test_records, &connection).await;
     let sync_reords: Vec<SyncBufferRow> = extract_sync_buffer_rows(&test_records);
 
@@ -63,5 +75,5 @@ async fn test_sync_pull_and_push() {
 
     integrate_and_translate_sync_buffer(&connection).unwrap();
 
-    check_records_against_database(&connection, test_records).await;
+    check_test_records_against_database(&connection, test_records).await;
 }
