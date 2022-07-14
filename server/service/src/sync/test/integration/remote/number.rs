755df1eb4843fd72ece9b8cf6c3912e6feb1d79d
Andrei <andrei.evguenov@gmail.com>
2022-07-15 11:23:24 +1200

    Relocate remote integration tests and use SyncRecordTest plus add deletes

---
+++ server/service/src/sync/test/integration/remote/number.rs
@@ -0,0 +1,100 @@
+use crate::sync::{
+    test::integration::{
+        central_server_configurations::NewSiteProperties, SyncRecordTester, TestStepData,
+    },
+    translations::{IntegrationRecords, PullUpsertRecord},
+};
+use rand::{thread_rng, Rng};
+use repository::{NumberRow, NumberRowType};
+use serde_json::json;
+use util::uuid::uuid;
+
+fn gen_i32() -> i32 {
+    thread_rng().gen::<i32>()
+}
+
+pub struct NumberRecordTester;
+impl SyncRecordTester for NumberRecordTester {
+    fn test_step_data(&self, new_site_properties: &NewSiteProperties) -> Vec<TestStepData> {
+        let mut result = Vec::new();
+        // STEP 1 - insert
+        let store_id = &new_site_properties.store_id;
+
+        let mut row_0 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::InboundShipment,
+        };
+
+        let mut row_1 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::OutboundShipment,
+        };
+
+        let mut row_2 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::InventoryAdjustment,
+        };
+
+        let mut row_3 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::RequestRequisition,
+        };
+
+        let mut row_4 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::ResponseRequisition,
+        };
+
+        let mut row_5 = NumberRow {
+            id: uuid(),
+            value: gen_i32() as i64,
+            store_id: store_id.to_string(),
+            r#type: NumberRowType::Stocktake,
+        };
+
+        result.push(TestStepData {
+            central_upsert: json!({}),
+            central_delete: json!({}),
+            integration_records: IntegrationRecords::from_upserts(vec![
+                PullUpsertRecord::Number(row_0.clone()),
+                PullUpsertRecord::Number(row_1.clone()),
+                PullUpsertRecord::Number(row_2.clone()),
+                PullUpsertRecord::Number(row_3.clone()),
+                PullUpsertRecord::Number(row_4.clone()),
+                PullUpsertRecord::Number(row_5.clone()),
+            ]),
+        });
+        // STEP 2 - mutate
+        row_0.value = gen_i32() as i64;
+        row_1.value = gen_i32() as i64;
+        row_2.value = gen_i32() as i64;
+        row_3.value = gen_i32() as i64;
+        row_4.value = gen_i32() as i64;
+        row_5.value = gen_i32() as i64;
+
+        result.push(TestStepData {
+            central_upsert: json!({}),
+            central_delete: json!({}),
+            integration_records: IntegrationRecords::from_upserts(vec![
+                PullUpsertRecord::Number(row_0),
+                PullUpsertRecord::Number(row_1),
+                PullUpsertRecord::Number(row_2),
+                PullUpsertRecord::Number(row_3),
+                PullUpsertRecord::Number(row_4),
+                PullUpsertRecord::Number(row_5),
+            ]),
+        });
+
+        result
+    }
+}
---
+++ server/service/src/sync/test/integration/remote/stock_line.rs
@@ -0,0 +1,89 @@
+use crate::sync::{
+    test::integration::{
+        central_server_configurations::NewSiteProperties, SyncRecordTester, TestStepData,
+    },
+    translations::{IntegrationRecords, PullUpsertRecord},
+};
+use chrono::NaiveDate;
+use repository::{LocationRow, StockLineRow};
+use serde_json::json;
+use util::{inline_edit, uuid::uuid};
+pub struct StockLineRecordTester;
+impl SyncRecordTester for StockLineRecordTester {
+    fn test_step_data(&self, new_site_properties: &NewSiteProperties) -> Vec<TestStepData> {
+        let mut result = Vec::new();
+        // STEP 1 - insert
+        let store_id = &new_site_properties.store_id;
+        // create test location
+        let location_row = LocationRow {
+            id: uuid(),
+            name: "LoationName".to_string(),
+            code: "LocationCode".to_string(),
+            on_hold: false,
+            store_id: store_id.to_string(),
+        };
+
+        let stock_line_row = StockLineRow {
+            id: uuid(),
+            item_id: uuid(),
+            store_id: store_id.to_string(),
+            location_id: Some(location_row.id.clone()),
+            batch: Some("some remote sync test batch".to_string()),
+            pack_size: 5,
+            cost_price_per_pack: 10.0,
+            sell_price_per_pack: 15.0,
+            available_number_of_packs: 100,
+            total_number_of_packs: 150,
+            expiry_date: Some(NaiveDate::from_ymd(2021, 03, 21)),
+            on_hold: true,
+            note: Some("some remote sync test note".to_string()),
+        };
+
+        result.push(TestStepData {
+            central_upsert: json!({"item": [{
+                "ID": stock_line_row.item_id,
+                "type_of": "general"
+            }]}),
+            central_delete: json!({}),
+            integration_records: IntegrationRecords::from_upserts(vec![
+                PullUpsertRecord::Location(location_row),
+                PullUpsertRecord::StockLine(stock_line_row.clone()),
+            ]),
+        });
+        // STEP 2 - mutate
+        let stock_line_row = inline_edit(&stock_line_row, |mut d| {
+            d.item_id = uuid();
+            d.location_id = None;
+            d.batch = Some("some remote sync test batch 2".to_string());
+            d.pack_size = 10;
+            d.cost_price_per_pack = 15.0;
+            d.sell_price_per_pack = 20.0;
+            d.available_number_of_packs = 110;
+            d.total_number_of_packs = 160;
+            d.expiry_date = Some(NaiveDate::from_ymd(2021, 03, 22));
+            d.on_hold = false;
+            d.note = Some("some remote sync test note 2".to_string());
+            d
+        });
+        result.push(TestStepData {
+            central_upsert: json!({"item": [{
+                "ID": stock_line_row.item_id,
+                "type_of": "general"
+            }]}),
+            central_delete: json!({}),
+            integration_records: IntegrationRecords::from_upserts(vec![
+                PullUpsertRecord::StockLine(stock_line_row.clone()),
+            ]),
+        });
+        // STEP 3 - delete
+        result.push(TestStepData {
+            central_upsert: json!({}),
+            central_delete: json!({}),
+            integration_records: IntegrationRecords::from_delete(
+                &stock_line_row.id,
+                crate::sync::translations::PullDeleteRecordTable::StockLine,
+            ),
+        });
+        result
+    }
+}
