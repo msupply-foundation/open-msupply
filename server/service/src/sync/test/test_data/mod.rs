9af3c72aa924e6cb4b59a1311b0cdc1f86ecebaa
Andrei <andrei.evguenov@gmail.com>
2022-07-15 11:34:23 +1200

    Split test data to centra and remote data
    Source: https://github.com/msupply-foundation/open-msupply/commit/9997b473c581887270d6f43a490984672c08829b

--- server/service/src/sync/test/test_data/mod.rs
+++ server/service/src/sync/test/test_data/mod.rs
@@ -20,32 +20,37 @@ pub(crate) mod stocktake_line;
 pub(crate) mod store;
 pub(crate) mod unit;
 
-pub(crate) fn get_all_pull_upsert_test_records() -> Vec<TestSyncPullRecord> {
+pub(crate) fn get_all_pull_upsert_central_test_records() -> Vec<TestSyncPullRecord> {
     let mut test_records = Vec::new();
     test_records.append(&mut item::test_pull_upsert_records());
-    test_records.append(&mut location::test_pull_upsert_records());
     test_records.append(&mut master_list_line::test_pull_upsert_records());
     test_records.append(&mut master_list_name_join::test_pull_upsert_records());
     test_records.append(&mut master_list::test_pull_upsert_records());
-    test_records.append(&mut name_store_join::test_pull_upsert_records());
     test_records.append(&mut name::test_pull_upsert_records());
-    test_records.append(&mut number::test_pull_upsert_records());
     test_records.append(&mut report::test_pull_upsert_records());
+    test_records.append(&mut store::test_pull_upsert_records());
+    test_records.append(&mut unit::test_pull_upsert_records());
+    // Central but site specific
+    test_records.append(&mut name_store_join::test_pull_upsert_records());
+    test_records.append(&mut special::name_to_name_store_join::test_pull_upsert_records());
+    test_records
+}
+
+pub(crate) fn get_all_pull_upsert_remote_test_records() -> Vec<TestSyncPullRecord> {
+    let mut test_records = Vec::new();
+    test_records.append(&mut location::test_pull_upsert_records());
+    test_records.append(&mut number::test_pull_upsert_records());
     test_records.append(&mut requisition_line::test_pull_upsert_records());
     test_records.append(&mut requisition::test_pull_upsert_records());
     test_records.append(&mut stock_line::test_pull_upsert_records());
     test_records.append(&mut stocktake_line::test_pull_upsert_records());
     test_records.append(&mut stocktake::test_pull_upsert_records());
-    test_records.append(&mut store::test_pull_upsert_records());
     test_records.append(&mut invoice_line::test_pull_upsert_records());
     test_records.append(&mut invoice::test_pull_upsert_records());
-    test_records.append(&mut unit::test_pull_upsert_records());
-    test_records.append(&mut special::name_to_name_store_join::test_pull_upsert_records());
-
     test_records
 }
 
-pub(crate) fn get_all_pull_delete_test_records() -> Vec<TestSyncPullRecord> {
+pub(crate) fn get_all_pull_delete_central_test_records() -> Vec<TestSyncPullRecord> {
     let mut test_records = Vec::new();
     test_records.append(&mut unit::test_pull_delete_records());
     test_records.append(&mut item::test_pull_delete_records());
@@ -56,7 +61,13 @@ pub(crate) fn get_all_pull_delete_test_records() -> Vec<TestSyncPullRecord> {
     test_records.append(&mut report::test_pull_delete_records());
     test_records.append(&mut store::test_pull_delete_records());
     test_records.append(&mut unit::test_pull_delete_records());
+    // Central but site specific
     test_records.append(&mut name_store_join::test_pull_delete_records());
+    test_records
+}
+
+pub(crate) fn get_all_pull_delete_remote_test_records() -> Vec<TestSyncPullRecord> {
+    let mut test_records = Vec::new();
     test_records.append(&mut requisition::test_pull_delete_records());
     test_records.append(&mut requisition_line::test_pull_delete_records());
     test_records.append(&mut invoice::test_pull_delete_records());
