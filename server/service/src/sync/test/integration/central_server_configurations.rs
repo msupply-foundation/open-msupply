e45bc5a19a2e67a21b114609ca022641e280f4dc
Andrei <andrei.evguenov@gmail.com>
2022-07-15 12:58:23 +1200

    Simplify central_server_configurations

--- server/service/src/sync/test/integration/central_server_configurations.rs
+++ server/service/src/sync/test/integration/central_server_configurations.rs
@@ -11,6 +11,8 @@ use crate::sync::{
     SyncCredentials,
 };
 
+use super::with_retry;
+
 impl SyncApiV5 {
     pub(crate) async fn upsert_central_records(
         &self,
@@ -43,10 +45,6 @@ pub(crate) struct CreateSyncSiteResult {
 }
 
 impl ConfigureCentralServer {
-    // SYNC_SITE_PASSWORD="pass" SYNC_SITE_NAME="demo" SYNC_URL="http://localhost:2048" NEW_SITE_PASSWORD="pass" cargo test sync_integration_test --features integration_test
-    // OR in VSCODE settings if using rust analyzer:
-    // "rust-analyzer.runnableEnv": { "SYNC_URL": "http://localhost:2048", "SYNC_SITE_NAME": "demo","SYNC_SITE_PASSWORD": "pass", "NEW_SITE_PASSWORD": "pass"}
-    // "rust-analyzer.cargo.features": ["integration_test"]
     pub(crate) fn from_env() -> ConfigureCentralServer {
         let password =
             env::var("SYNC_SITE_PASSWORD").expect("SYNC_SITE_PASSWORD env variable missing");
@@ -71,32 +69,15 @@ impl ConfigureCentralServer {
     }
 
     pub(crate) async fn upsert_records(&self, records: serde_json::Value) -> anyhow::Result<()> {
-        Ok(self.api.upsert_central_records(&records).await?)
+        Ok(with_retry(|| self.api.upsert_central_records(&records)).await?)
     }
 
     pub(crate) async fn delete_records(&self, records: serde_json::Value) -> anyhow::Result<()> {
-        Ok(self.api.delete_central_records(&records).await?)
+        Ok(with_retry(|| self.api.delete_central_records(&records)).await?)
     }
 
     pub(crate) async fn create_sync_site(&self) -> anyhow::Result<CreateSyncSiteResult> {
-        self.create_sync_site_with_extra_data(|_| json!({})).await
-    }
-
-    pub(crate) async fn create_sync_site_with_extra_data<F>(
-        &self,
-        // Before site is inserted and linked to store
-        pre_site_creation_data: F,
-    ) -> anyhow::Result<CreateSyncSiteResult>
-    where
-        F: Fn(&NewSiteProperties) -> serde_json::Value,
-    {
         let new_site_properties = NewSiteProperties::new(&self.new_site_password);
-        self.api
-            .upsert_central_records(&new_site_properties.preliminary_data())
-            .await?;
-        self.api
-            .upsert_central_records(&pre_site_creation_data(&new_site_properties))
-            .await?;
 
         self.api
             .upsert_central_records(&new_site_properties.site_data())
@@ -119,7 +100,6 @@ impl ConfigureCentralServer {
 pub(crate) struct NewSiteProperties {
     pub(crate) store_id: String,
     pub(crate) name_id: String,
-    pref_id: String,
     pub(crate) site_uuid: String,
     pub(crate) site_id: u16,
     password_sha256: String,
@@ -130,7 +110,6 @@ impl NewSiteProperties {
         NewSiteProperties {
             store_id: uuid(),
             name_id: uuid(),
-            pref_id: uuid(),
             // TODO max that can be used ?
             site_id: thread_rng().gen::<u16>(),
             site_uuid: uuid(),
@@ -154,9 +133,7 @@ impl NewSiteProperties {
     // $file:=File("/Users/Drei/Documents/repos/work/msupply/out.txt")
     // $file.create()
     // $file.setText($content; Document with CR)
-
-    // Data without site
-    fn preliminary_data(&self) -> serde_json::Value {
+    fn site_data(&self) -> serde_json::Value {
         json!(
         {
             "name": [
@@ -165,81 +142,6 @@ impl NewSiteProperties {
                     "name": self.name_id
                 }
             ],
-            "store": [
-                {
-                    "ID":  self.store_id,
-                    "name":  self.store_id,
-                    "code":  self.store_id,
-                    "name_ID":  self.name_id,
-                    "sync_id_remote_site": 0,
-                    "store_mode": "store"
-                }
-            ],
-            "pref": [
-                {
-                    "item": "store_preferences",
-                    "user_ID": "",
-                    "ID": self.pref_id,
-                    "network_ID": "",
-                    "store_ID":  self.store_id,
-                    "data": {
-                        "sort_batches_by_VVM_not_expiry": false,
-                        "new_patients_visible_in_this_store_only": true,
-                        "new_names_visible_in_this_store_only": true,
-                        "can_enter_total_distribution_quantities": false,
-                        "round_up_distribute_quantities": false,
-                        "can_pack_items_into_multiple_boxes": false,
-                        "can_issue_in_foreign_currency": false,
-                        "edit_sell_price_on_customer_invoice_lines": false,
-                        "purchase_order_must_be_authorised": false,
-                        "finalise_customer_invoices_automatically": false,
-                        "customer_invoices_must_be_authorised": false,
-                        "customer_invoice_authorisation_needed_only_if_over_budget": false,
-                        "confirm_customer_invoices_automatically": false,
-                        "supplier_invoices_must_be_authorised": false,
-                        "confirm_supplier_invoices_automatically": false,
-                        "goods_received_lines_must_be_authorised": false,
-                        "must_enter_locations_on_goods_received": false,
-                        "can_specify_manufacturer": false,
-                        "show_item_unit_column_while_issuing": false,
-                        "log_editing_transacts": false,
-                        "default_item_packsize_to_one": true,
-                        "shouldAuthoriseResponseRequisition": false,
-                        "includeRequisitionsInSuppliersRemoteAuthorisationProcesses": false,
-                        "canLinkRequistionToSupplierInvoice": false,
-                        "responseRequisitionAutoFillSupplyQuantity": false,
-                        "useExtraFieldsForRequisitions": false,
-                        "CommentFieldToBeShownOnSupplierInvoiceLines": false,
-                        "UseEDDPlaceholderLinesOnSupplierInvoice": false,
-                        "consolidateBatches": false,
-                        "editPrescribedQuantityOnPrescription": false,
-                        "chooseDiagnosisOnPrescription": false,
-                        "useConsumptionAndStockFromCustomersForInternalOrders": false,
-                        "monthlyConsumptionEnforceLookBackPeriod": false,
-                        "usesVaccineModule": false,
-                        "usesDashboardModule": false,
-                        "usesCashRegisterModule": false,
-                        "usesPaymentModule": false,
-                        "usesPatientTypes": false,
-                        "usesHideSnapshotColumn": false,
-                        "good_receipt_finalise_next_action": "supplier_invoice_on_hold",
-                        "stock_transfer_supplier_invoice_is_on_hold": true,
-                        "monthlyConsumptionLookBackPeriod": "0",
-                        "monthsLeadTime": "0",
-                        "usesDispensaryModule": false,
-                        "monthsOverstock": 6 as u32,
-                        "monthsUnderstock": 3 as u32,
-                        "monthsItemsExpire": 3 as u32,
-                    }
-                }
-            ]
-        })
-    }
-
-    // Data with site (relies on preliminary data already inserted)
-    fn site_data(&self) -> serde_json::Value {
-        json!(
-        {
             "site": [
                 {
                     "ID":  self.site_uuid,
