675c86c654d36b754517a8a4f1a097ee4760dd19
Clemens Zeidler <clemens@msupply.foundation>
2022-07-18 13:35:28 +1200

    Fix spelling

--- client/packages/system/src/Patient/DetailView/DetailView.tsx
+++ client/packages/system/src/Patient/DetailView/DetailView.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useMemo } from 'react';
 import {
   DetailViewSkeleton,
-  SaveDocumentMuation,
+  SaveDocumentMutation,
   useJsonForms,
   useUrlQuery,
 } from '@openmsupply-client/common';
@@ -9,7 +9,7 @@ import { usePatient } from '../api';
 import { useCreatePatientStore } from '../hooks';
 import { AppBarButtons } from './AppBarButtons';
 
-const useUpsertPatient = (): SaveDocumentMuation => {
+const useUpsertPatient = (): SaveDocumentMutation => {
   const { mutateAsync: insertPatient } = usePatient.document.insert();
   const { mutateAsync: updatePatient } = usePatient.document.update();
   return async (jsonData: unknown, formSchemaId: string, parent?: string) => {
