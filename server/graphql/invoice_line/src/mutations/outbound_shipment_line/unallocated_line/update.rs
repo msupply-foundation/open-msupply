884ff496bdffb8799073bb9b5dc1a5ec632cf2ea
roxy-dao <roxy@msupply.foundation>
2022-07-20 12:25:24 +1200

    Add back UpdatedLineDoesNotExist error
    Source: https://github.com/msupply-foundation/open-msupply/commit/eefc31f2606246d4fbb2ee4d5d691fff4c9b5df1

--- server/graphql/invoice_line/src/mutations/outbound_shipment_line/unallocated_line/update.rs
+++ server/graphql/invoice_line/src/mutations/outbound_shipment_line/unallocated_line/update.rs
@@ -95,6 +95,7 @@ fn map_error(error: ServiceError) -> Result<UpdateErrorInterface> {
         // Standard Graphql Errors
         ServiceError::LineIsNotUnallocatedLine => BadUserInput(formatted_error),
         ServiceError::DatabaseError(_) => InternalError(formatted_error),
+        ServiceError::UpdatedLineDoesNotExist => InternalError(formatted_error),
     };
 
     Err(graphql_error.extend())
