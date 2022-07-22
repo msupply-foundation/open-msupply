305c6ebb5d83475f3bb106c84b6ac93bfbcbff92
Clemens Zeidler <clemens@msupply.foundation>
2022-07-22 17:08:12 +1200

    Update program table when program is upserted
    Source: https://github.com/msupply-foundation/open-msupply/commit/e6a1818d7499e15bc7273bebe61251885f479e9f

---
+++ server/service/src/document/program/program_updated.rs
@@ -0,0 +1,39 @@
+use chrono::DateTime;
+use repository::{
+    Document, ProgramRepository, ProgramRow, ProgramRowRepository, StorageConnection,
+};
+use util::uuid::uuid;
+
+use super::{program_schema::SchemaProgram, UpsertProgramError};
+
+/// Callback called when the document has been updated
+pub(crate) fn program_updated(
+    con: &StorageConnection,
+    patient_id: &str,
+    document: &Document,
+    program: SchemaProgram,
+) -> Result<ProgramRow, UpsertProgramError> {
+    let enrolment_datetime = DateTime::parse_from_rfc3339(&program.enrolment_datetime)
+        .map_err(|err| {
+            UpsertProgramError::InternalError(format!("Invalid enrolment datetime format: {}", err))
+        })?
+        .naive_utc();
+
+    let repo = ProgramRepository::new(con);
+    let program_row = repo.find_one_by_type_and_patient(&document.r#type, patient_id)?;
+    let id = match program_row {
+        Some(program_row) => program_row.id,
+        None => uuid(),
+    };
+    let program_row = ProgramRow {
+        id,
+        r#type: document.r#type.clone(),
+        name: document.name.clone(),
+        patient_id: patient_id.to_string(),
+        enrolment_datetime: enrolment_datetime,
+        program_patient_id: program.patient_id,
+    };
+    ProgramRowRepository::new(con).upsert_one(&program_row)?;
+
+    Ok(program_row)
+}
