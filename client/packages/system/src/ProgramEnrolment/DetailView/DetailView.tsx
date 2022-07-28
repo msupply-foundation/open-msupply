a043eb6b3f4b6b8c3783f052b963a6846d7762a7
Clemens Zeidler <clemens@msupply.foundation>
2022-07-28 13:39:15 +1200

    Remove unnecessary DetailViewSkeleton options
    Source: https://github.com/msupply-foundation/open-msupply/commit/e675a2b73a35bec041e24a8b878a3a6d221637e5

--- client/packages/system/src/ProgramEnrolment/DetailView/DetailView.tsx
+++ client/packages/system/src/ProgramEnrolment/DetailView/DetailView.tsx
@@ -45,12 +45,10 @@ export const ProgramDetailView: FC = () => {
   const handleSave = useUpsertProgramEnrolment(patientId, type);
   const { JsonForm, loading } = useJsonForms(doc, { handleSave });
 
-  if (loading) return <DetailViewSkeleton  />;
+  if (loading) return <DetailViewSkeleton />;
 
   return (
-    <React.Suspense
-      fallback={<DetailViewSkeleton hasGroupBy={true} hasHold={true} />}
-    >
+    <React.Suspense fallback={<DetailViewSkeleton />}>
       <AppBarButtons />
       {JsonForm}
     </React.Suspense>
