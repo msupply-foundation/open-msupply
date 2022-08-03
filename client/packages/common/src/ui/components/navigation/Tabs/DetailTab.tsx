ae2394eb96c4ddf81bab1f5f892424a3ace3cb87
Mark Prins <mark@malcolmprins.net>
2022-08-03 18:01:59 +1200

    outbound tabs

---
+++ client/packages/common/src/ui/components/navigation/Tabs/DetailTab.tsx
@@ -0,0 +1,30 @@
+import React, { FC, PropsWithChildren } from 'react';
+import { Box, styled } from '@mui/material';
+import { TabPanel } from './Tabs';
+
+interface DetailTabProps {
+  padding?: number;
+  value: string;
+}
+
+const StyledTabPanel = styled(TabPanel)({
+  height: '100%',
+  flex: 1,
+  padding: 0,
+});
+
+const StyledTabContainer = styled(Box)(({ theme }) => ({
+  borderColor: theme.palette.divider,
+  flexDirection: 'row',
+  display: 'flex',
+}));
+
+export const DetailTab: FC<PropsWithChildren<DetailTabProps>> = ({
+  children,
+  padding,
+  value,
+}) => (
+  <StyledTabPanel value={value} sx={{ padding }}>
+    <StyledTabContainer>{children}</StyledTabContainer>
+  </StyledTabPanel>
+);
