8622cacce86db83bccf8664a5dcc90cb39a43d3b
Mark Prins <mark@malcolmprins.net>
2022-08-08 15:03:31 +1200

    tidy form widths

--- client/packages/common/src/ui/forms/JsonForms/components/DateOfBirth.tsx
+++ client/packages/common/src/ui/forms/JsonForms/components/DateOfBirth.tsx
@@ -66,15 +66,10 @@ const UIComponent = (props: ControlProps) => {
       style={{ minWidth: 300 }}
       marginTop={1}
     >
-      <Box
-        flex={1}
-        style={{ textAlign: 'end' }}
-        flexBasis={FORM_LABEL_COLUMN_WIDTH}
-      >
+      <Box style={{ textAlign: 'end' }} flexBasis={FORM_LABEL_COLUMN_WIDTH}>
         <FormLabel sx={{ fontWeight: 'bold' }}>{label}:</FormLabel>
       </Box>
       <Box
-        flex={1}
         flexBasis={FORM_INPUT_COLUMN_WIDTH}
         display="flex"
         alignItems="center"
