6e53f08f40a0126b867941bec742b1b7faa8153d
Mark Prins <mark@malcolmprins.net>
2022-08-17 10:09:34 +1200

    unrefactor

--- client/packages/system/src/Patient/ProgramEnrolment/api/hooks/document/index.ts
+++ client/packages/system/src/Patient/ProgramEnrolment/api/hooks/document/index.ts
@@ -1,11 +1,9 @@
 import { useInsertProgramEnrolment } from './useInsertProgramEnrolment';
 import { useProgramEnrolments } from './useProgramEnrolments';
-import { useProgramEnrolmentsAll } from './useProgramEnrolmentsAll';
 import { useUpdateProgramEnrolment } from './useUpdateProgramEnrolment';
 
 export const Document = {
   useProgramEnrolments,
-  useProgramEnrolmentsAll,
   useInsertProgramEnrolment,
   useUpdateProgramEnrolment,
 };
