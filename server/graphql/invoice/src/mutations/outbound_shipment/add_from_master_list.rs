191c685738b0dcfae638c64f92057c2df7897053
roxy-dao <roxy@msupply.foundation>
2022-08-18 12:41:49 +1200

    Remove commented out assert in graphql

--- server/graphql/invoice/src/mutations/outbound_shipment/add_from_master_list.rs
+++ server/graphql/invoice/src/mutations/outbound_shipment/add_from_master_list.rs
@@ -291,7 +291,6 @@ mod test {
 
         // Success
         let test_service = TestService(Box::new(|input| {
-            // assert_eq!(store_id, "store_a");
             assert_eq!(
                 input,
                 ServiceInput {
--- server/graphql/invoice_line/src/mutations/outbound_shipment_line/service_line/insert.rs
+++ server/graphql/invoice_line/src/mutations/outbound_shipment_line/service_line/insert.rs
@@ -339,7 +339,6 @@ mod test {
 
         // Success
         let test_service = TestService(Box::new(|input| {
-            // assert_eq!(store_id, "store_a");
             assert_eq!(
                 input,
                 ServiceInput {
