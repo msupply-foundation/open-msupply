35240b2be74f75bb002ad779799a7fc6ffbb8e0b
Clemens Zeidler <clemens@msupply.foundation>
2022-08-23 11:12:30 +1200

    Valid custom JSONForms control options
    Source: https://github.com/msupply-foundation/open-msupply/commit/930a500c06cdfb56c06f2e9ae9bf13279003bbdd

---
+++ client/packages/system/src/Patient/JsonForms/useZodOptionsValidation.ts
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import { z, ZodError, ZodIssue } from 'zod';
+
+const formatMessage = (issue: ZodIssue): string => {
+  switch (issue.code) {
+    case 'invalid_type': {
+      return `${issue.expected} ${issue.message}`;
+    }
+    case 'invalid_union': {
+      const messages = issue.unionErrors.map(it => {
+        const issue = it.issues[0];
+        if (!issue) {
+          return '';
+        } else {
+          return formatMessage(issue);
+        }
+      });
+      return `${messages.join(' OR ')}`;
+    }
+  }
+  return issue.message;
+};
+
+const formatIssue = (objectVarName: string, issue: ZodIssue): string => {
+  const path = issue.path
+    .map((part, i) => {
+      if (typeof part === 'number') {
+        return `[${part}]`;
+      }
+      if (i === 0) return part;
+      return `.${part}`;
+    })
+    .join('');
+  const message = formatMessage(issue);
+  return `\"${objectVarName}.${path}\" => ${message}`;
+};
+
+const formatError = (objectVarName: string, error: ZodError): string => {
+  return `${error.issues.map(it => formatIssue(objectVarName, it)).join(', ')}`;
+};
+
+export const useZodOptionsValidation = <T>(
+  TypeDef: z.ZodType<T, z.ZodTypeDef, T>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  options?: Record<string, any>
+): { errors?: string; options?: T } => {
+  const [errors, setErrors] = useState<string | undefined>();
+  const [parsedOptions, setOptions] = useState<T | undefined>();
+  useEffect(() => {
+    if (!options) {
+      setErrors('Bad control config: options are required');
+      return;
+    }
+
+    const result = TypeDef.safeParse(options);
+    if (result.success) {
+      setOptions(result.data);
+    } else {
+      // It gets very messy showing more than one issue...
+      setErrors(`Bad control config: ${formatError('options', result.error)}`);
+    }
+  }, [options]);
+  return { errors, options: parsedOptions };
+};
