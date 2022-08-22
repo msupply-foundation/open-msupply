eb26c9358552f13d5ad5ec5f98007eac87f6603a
Clemens Zeidler <clemens@msupply.foundation>
2022-08-22 15:07:52 +1200

    Extend the encounter fragment in the list view to show an "effective" status
    Source: https://github.com/msupply-foundation/open-msupply/commit/d0aab9b6a87dc5949694098fbfb3ce057d88823e

--- client/packages/system/src/Patient/Encounter/ListView.tsx
+++ client/packages/system/src/Patient/Encounter/ListView.tsx
@@ -9,12 +9,61 @@ import {
   useFormatDateTime,
   useUrlQueryParams,
   ColumnAlign,
+  EncounterNodeStatus,
+  useTranslation,
+  LocaleKey,
+  TypedTFunction,
 } from '@openmsupply-client/common';
 import { EncounterFragment, useEncounter } from './api';
 import { usePatientModalStore } from '../hooks';
 import { PatientModal } from '../PatientView';
 
-type EncounterFragmentWithId = { id: string } & EncounterFragment;
+const effectiveStatus = (
+  encounter: EncounterFragment,
+  t: TypedTFunction<LocaleKey>
+) => {
+  const status = encounter.status;
+  if (!status) {
+    return '';
+  }
+  switch (status) {
+    case EncounterNodeStatus.Cancelled:
+      return t('label.encounter-status-cancelled');
+    case EncounterNodeStatus.Done:
+      return t('label.encounter-status-done');
+    case EncounterNodeStatus.Scheduled:
+      if (new Date(encounter.startDatetime).getTime() < Date.now()) {
+        return t('label.encounter-status-missed');
+      }
+      return t('label.encounter-status-scheduled');
+    default:
+      ((_: never) => {
+        // exhaustive check
+        _;
+      })(status);
+  }
+  return '';
+};
+
+type EncounterFragmentExt = {
+  id: string;
+  effectiveStatus: string;
+} & EncounterFragment;
+
+const useExtendEncounterFragment = (
+  nodes?: EncounterFragment[]
+): EncounterFragmentExt[] | undefined => {
+  const t = useTranslation('common');
+  return useMemo(
+    () =>
+      nodes?.map(node => ({
+        id: node.name,
+        effectiveStatus: effectiveStatus(node, t),
+        ...node,
+      })),
+    [nodes]
+  );
+};
 
 const EncounterListComponent: FC = () => {
   const {
@@ -23,19 +72,13 @@ const EncounterListComponent: FC = () => {
     queryParams: { sortBy, page, first, offset },
   } = useUrlQueryParams();
   const { data, isError, isLoading } = useEncounter.document.list();
-  const dataWithId: EncounterFragmentWithId[] | undefined = useMemo(
-    () =>
-      data?.nodes.map(node => ({
-        id: node.name,
-        ...node,
-      })),
-    [data]
-  );
+  const dataWithId: EncounterFragmentExt[] | undefined =
+    useExtendEncounterFragment(data?.nodes);
   const pagination = { page, first, offset };
   const { localisedDateTime } = useFormatDateTime();
   const { setCurrent, setDocument, setProgramType } = usePatientModalStore();
 
-  const columns = useColumns<EncounterFragmentWithId>(
+  const columns = useColumns<EncounterFragmentExt>(
     [
       {
         key: 'type',
@@ -58,7 +101,7 @@ const EncounterListComponent: FC = () => {
           dateString ? localisedDateTime((dateString as string) || '') : '',
       },
       {
-        key: 'status',
+        key: 'effectiveStatus',
         label: 'label.status',
         align: ColumnAlign.Right,
         width: 175,
@@ -90,7 +133,7 @@ const EncounterListComponent: FC = () => {
 export const EncounterListView: FC = () => (
   <TableProvider
     createStore={createTableStore}
-    queryParamsStore={createQueryParamsStore<EncounterFragmentWithId>({
+    queryParamsStore={createQueryParamsStore<EncounterFragmentExt>({
       initialSortBy: { key: 'type' },
     })}
   >
