b4fffc075d6c5978730aab36b0c31cb4ae98634f
James Brunskill <james@msupply.foundation>
2022-08-26 16:23:54 +1200

    Concurrency fix for next_number()

--- server/repository/migrations/postgres/2021-12-10T10-00_number_table/up.sql
+++ server/repository/migrations/postgres/2021-12-10T10-00_number_table/up.sql
@@ -1,17 +1,20 @@
 CREATE TYPE number_type AS ENUM (
-    'INBOUND_SHIPMENT',
-    'OUTBOUND_SHIPMENT',
-    'INVENTORY_ADJUSTMENT',
-    'STOCKTAKE',
-    'REQUEST_REQUISITION',
-    'RESPONSE_REQUISITION'
+  'INBOUND_SHIPMENT',
+  'OUTBOUND_SHIPMENT',
+  'INVENTORY_ADJUSTMENT',
+  'STOCKTAKE',
+  'REQUEST_REQUISITION',
+  'RESPONSE_REQUISITION'
 );
 
 -- Numbering table holding a list of typed counters
-CREATE TABLE number (
+CREATE TABLE
+  number (
     id TEXT NOT NULL PRIMARY KEY,
     -- current counter value
     value BIGINT NOT NULL,
     store_id TEXT NOT NULL REFERENCES store(id),
     type number_type NOT NULL
-)
+  );
+
+CREATE UNIQUE INDEX ix_number_store_type_unique ON number(store_id, type);
