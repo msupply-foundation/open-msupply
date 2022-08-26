c74f90ab507808a149e27a743f3ab53b0d96c34d
James Brunskill <james@msupply.foundation>
2022-08-26 12:02:09 +1200

    Added test that fails for postgres test_concurrent_next_number()
    Source: https://github.com/msupply-foundation/open-msupply/commit/dab8c255c4d6a78f8bf4218a3886641ee3d9e605

--- server/service/src/number.rs
+++ server/service/src/number.rs
@@ -11,8 +11,9 @@ pub fn next_number(
     // Should be done in transaction
     let next_number = connection.transaction_sync(|connection_tx| {
         let repo = NumberRowRepository::new(&connection_tx);
-
-        let updated_number_row = match repo.find_one_by_type_and_store(r#type, store_id)? {
+        let current = repo.find_one_by_type_and_store(r#type, store_id)?;
+        //This should compile as a no-op in production code, only used during tests
+        let updated_number_row = match current {
             Some(mut row) => {
                 // update existing counter
                 row.value = row.value + 1;
@@ -43,9 +44,11 @@ mod test {
             mock_inbound_shipment_number_store_a, mock_outbound_shipment_number_store_a,
             MockDataInserts,
         },
-        test_db::setup_all,
-        NumberRowType,
+        test_db::{self, setup_all},
+        NumberRowType, RepositoryError, TransactionError,
     };
+
+    const TEST_SLEEP_TIME: u64 = 100;
 
     use crate::number::next_number;
 
@@ -74,5 +77,71 @@ mod test {
 
         let result = next_number(&connection, &NumberRowType::OutboundShipment, "store_b").unwrap();
         assert_eq!(result, 2);
+    }
+
+    #[actix_rt::test]
+    async fn test_concurrent_next_number() {
+        let (_, _, connection_manager, _) = test_db::setup_all(
+            "test_concurrent_numbers",
+            MockDataInserts::none().names().stores(),
+        )
+        .await;
+        /*
+            Test Scenario
+
+            Process A starts a transaction, and gets the next number, then waits before commiting the transaction
+            Concurrently Process B tries to get the next number
+            (Note: This test did fail with previous implementation of next number on postgres)
+        */
+
+        //Part 1: Concurrent up date (first row) e.g. this will require an insert and an update for these processes...
+
+        let manager_a = connection_manager.clone();
+        let process_a = std::thread::spawn(move || {
+            let connection = manager_a.connection().unwrap();
+            let result: Result<i64, TransactionError<RepositoryError>> = connection
+                .transaction_sync(|con| {
+                    let num = next_number(con, &NumberRowType::Stocktake, "store_a")?;
+                    std::thread::sleep(core::time::Duration::from_millis(TEST_SLEEP_TIME));
+                    Ok(num)
+                });
+            result.unwrap()
+        });
+
+        let manager_b = connection_manager.clone();
+        let process_b = std::thread::spawn(move || {
+            let connection = manager_b.connection().unwrap();
+            next_number(&connection, &NumberRowType::Stocktake, "store_a").unwrap()
+        });
+
+        let a = process_a.join().unwrap();
+        let b = process_b.join().unwrap();
+        println!("next_number (INSERT) results : a={} b={}", a, b);
+        assert!(a != b);
+
+        let manager_a = connection_manager.clone();
+        let process_a = std::thread::spawn(move || {
+            let connection = manager_a.connection().unwrap();
+            let result: Result<i64, TransactionError<RepositoryError>> = connection
+                .transaction_sync(|con| {
+                    let num = next_number(con, &NumberRowType::Stocktake, "store_a")?;
+                    std::thread::sleep(core::time::Duration::from_millis(TEST_SLEEP_TIME));
+                    Ok(num)
+                });
+            result.unwrap()
+        });
+
+        //Part 2: Concurrent up date both doing updates
+        let manager_b = connection_manager.clone();
+        let process_b = std::thread::spawn(move || {
+            let connection = manager_b.connection().unwrap();
+            next_number(&connection, &NumberRowType::Stocktake, "store_a").unwrap()
+        });
+
+        let a = process_a.join().unwrap();
+        let b = process_b.join().unwrap();
+
+        println!("next_number (UPDATE) results : a={} b={}", a, b);
+        assert!(a != b);
     }
 }
