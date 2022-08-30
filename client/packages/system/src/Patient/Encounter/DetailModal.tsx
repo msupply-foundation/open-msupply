6e2a9befab88072bd7f89acd154c1c6ccb896b31
Mark Prins <mark@malcolmprins.net>
2022-08-30 15:10:39 +1200

    create encounter
    Source: https://github.com/msupply-foundation/open-msupply/commit/772602e61b75c8dfcd336944c89d4272bfb56f6c

--- client/packages/system/src/Patient/Encounter/DetailModal.tsx
+++ client/packages/system/src/Patient/Encounter/DetailModal.tsx
@@ -3,87 +3,123 @@ import {
   AlertIcon,
   BasicSpinner,
   Box,
+  DatePickerInput,
   DialogButton,
   InputWithLabelRow,
+  RouteBuilder,
   Stack,
+  TimePickerInput,
   Typography,
   useDialog,
+  useNavigate,
+  useNotification,
 } from '@openmsupply-client/common';
-import { useTranslation } from '@common/intl';
+import { DateUtils, useTranslation } from '@common/intl';
 import { useEncounter } from '../../Encounter';
 import { usePatientModalStore } from '../hooks';
 import { PatientModal } from '../PatientView';
 import { ProgramRowFragmentWithId, usePatient } from '../api';
-import { CreateDocument, useJsonForms } from '../JsonForms';
 import { ProgramEnrolmentSearchInput } from '../Components';
+import {
+  EncounterFragment,
+  EncounterDocumentRegistryFragment,
+} from '../../Encounter/api/operations.generated';
+import { AppRoute } from 'packages/config/src';
+
+type Encounter = Pick<EncounterFragment, 'startDatetime' | 'endDatetime'>;
 
 export const EncounterDetailModal: FC = () => {
   const patientId = usePatient.utils.id();
   const t = useTranslation('patients');
-  const { current, reset, document } = usePatientModalStore();
+  const { current, setCurrent } = usePatientModalStore();
   const [program, setProgram] = useState<ProgramRowFragmentWithId | null>(null);
-  const [error, setError] = useState(false);
-  const { mutate: fetch, isLoading, data } = useEncounter.registry.byProgram();
-  const isCreate = !document?.name;
-  const [createDoc, setCreateDoc] = useState<CreateDocument | undefined>(
-    undefined
-  );
+  const [isError, setIsError] = useState(false);
+  const {
+    mutate: fetch,
+    isLoading,
+    data: registryData,
+  } = useEncounter.registry.byProgram();
+  const [documentRegistry, setDocumentRegistry] = useState<
+    EncounterDocumentRegistryFragment | undefined
+  >(undefined);
+
+  const [data, setData] = useState<Encounter | undefined>(undefined);
+  const navigate = useNavigate();
+  const { error } = useNotification();
 
   const handleSave = useEncounter.document.upsert(
     patientId,
     program?.type ?? '',
-    createDoc?.documentRegistry?.documentType ?? ''
-  );
-  const { JsonForm, saveData, isDirty, validationError } = useJsonForms(
-    document?.name,
-    {
-      handleSave,
-    },
-    createDoc
+    documentRegistry?.documentType ?? ''
   );
 
-  const onClose = () => {
-    reset();
+  const reset = () => {
+    setCurrent(undefined);
     setProgram(null);
-    setCreateDoc(undefined);
-    setError(false);
+    setDocumentRegistry(undefined);
+    setData(undefined);
+    setIsError(false);
   };
 
   const { Modal } = useDialog({
     isOpen: current === PatientModal.Encounter,
-    onClose,
+    onClose: reset,
   });
 
   const onChangeProgram = (program: ProgramRowFragmentWithId) => {
     setProgram(program);
-    setError(false);
+    setIsError(false);
     fetch(program.document.documentRegistry?.id || '');
   };
 
   useEffect(() => {
-    if (data && data?.totalCount > 0) {
-      const documentRegistry = data.nodes[0];
+    if (registryData && registryData?.totalCount > 0) {
+      const documentRegistry = registryData.nodes[0];
       if (documentRegistry) {
-        setCreateDoc({ data: {}, documentRegistry });
-        setError(false);
+        setDocumentRegistry(documentRegistry);
+        setIsError(false);
         return;
       }
     } else {
-      setError(true);
+      setIsError(true);
     }
-    setCreateDoc(undefined);
-  }, [data]);
+    setDocumentRegistry(undefined);
+  }, [registryData]);
+
+  const setStartDatetime = (date: Date | null): void => {
+    const startDatetime = date ? DateUtils.formatRFC3339(date) : undefined;
+    if (!startDatetime) return;
+    setData({ ...data, startDatetime });
+  };
+
+  const setEndDatetime = (date: Date | null): void => {
+    const endDatetime = date ? DateUtils.formatRFC3339(date) : null;
+    if (endDatetime && data?.startDatetime) setData({ ...data, endDatetime });
+  };
 
   return (
     <Modal
-      title={t(isCreate ? 'label.new-encounter' : 'label.edit-encounter')}
-      cancelButton={<DialogButton variant="cancel" onClick={onClose} />}
+      title={t('label.new-encounter')}
+      cancelButton={<DialogButton variant="cancel" onClick={reset} />}
       okButton={
         <DialogButton
-          variant={isCreate ? 'create' : 'ok'}
-          disabled={!isDirty || !!validationError}
+          variant={'create'}
+          disabled={data === undefined || isLoading}
           onClick={async () => {
-            await saveData();
+            if (documentRegistry !== undefined) {
+              const { id } = await handleSave(
+                data,
+                documentRegistry.formSchemaId
+              );
+              if (!!id)
+                navigate(
+                  RouteBuilder.create(AppRoute.Dispensary)
+                    .addPart(AppRoute.Encounter)
+                    .addPart(id)
+                    .build()
+                );
+              else error(t('error.encounter-not-created'))();
+            }
             reset();
           }}
         />
@@ -91,24 +127,56 @@ export const EncounterDetailModal: FC = () => {
       width={700}
     >
       <React.Suspense fallback={<div />}>
-        <Stack alignItems="center">
-          {isCreate && (
-            <InputWithLabelRow
-              label={t('label.program')}
-              Input={
-                <ProgramEnrolmentSearchInput
-                  onChange={onChangeProgram}
-                  value={program}
-                />
-              }
-            />
-          )}
+        <Stack alignItems="flex-start" gap={1} sx={{ paddingLeft: '20px' }}>
+          <InputWithLabelRow
+            label={t('label.program')}
+            Input={
+              <ProgramEnrolmentSearchInput
+                onChange={onChangeProgram}
+                value={program}
+              />
+            }
+          />
           <RenderForm
-            Form={JsonForm}
-            isCreate={isCreate}
-            isError={error}
+            isError={isError}
             isLoading={isLoading}
             isProgram={!!program}
+            form={
+              <>
+                <InputWithLabelRow
+                  label={t('label.visit-date')}
+                  Input={
+                    <DatePickerInput
+                      value={DateUtils.getDateOrNull(
+                        data?.startDatetime ?? null
+                      )}
+                      onChange={setStartDatetime}
+                    />
+                  }
+                />
+                <InputWithLabelRow
+                  label={t('label.visit-start')}
+                  Input={
+                    <TimePickerInput
+                      value={DateUtils.getDateOrNull(
+                        data?.startDatetime ?? null
+                      )}
+                      onChange={setStartDatetime}
+                    />
+                  }
+                />
+                <InputWithLabelRow
+                  label={t('label.visit-end')}
+                  Input={
+                    <TimePickerInput
+                      value={DateUtils.getDateOrNull(data?.endDatetime ?? null)}
+                      disabled={data?.startDatetime === undefined}
+                      onChange={setEndDatetime}
+                    />
+                  }
+                />
+              </>
+            }
           />
         </Stack>
       </React.Suspense>
@@ -117,20 +185,18 @@ export const EncounterDetailModal: FC = () => {
 };
 
 const RenderForm = ({
-  Form,
-  isCreate,
+  form,
   isError,
   isLoading,
   isProgram,
 }: {
-  Form: React.ReactNode;
-  isCreate: boolean;
+  form: React.ReactNode;
   isError: boolean;
   isLoading: boolean;
   isProgram: boolean;
 }) => {
   const t = useTranslation('common');
-  if (!isProgram && isCreate) return null;
+  if (!isProgram) return null;
   if (isError)
     return (
       <Box display="flex" gap={1} padding={3}>
@@ -140,5 +206,5 @@ const RenderForm = ({
     );
   if (isLoading) return <BasicSpinner />;
 
-  return <>{Form}</>;
+  return <>{form}</>;
 };
