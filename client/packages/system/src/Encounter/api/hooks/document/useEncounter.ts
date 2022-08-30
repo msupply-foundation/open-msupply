772602e61b75c8dfcd336944c89d4272bfb56f6c
Mark Prins <mark@malcolmprins.net>
2022-08-30 15:10:39 +1200

    create encounter

--- client/packages/system/src/Encounter/api/hooks/document/useEncounter.ts
+++ client/packages/system/src/Encounter/api/hooks/document/useEncounter.ts
@@ -7,6 +7,15 @@ export const useEncounter = () => {
   const id = useEncounterId();
 
   return {
-    ...useQuery(api.keys.detail(id), () => api.get.byId(id)),
+    ...useQuery(
+      api.keys.detail(id),
+      () => api.get.byId(id),
+      // Don't refetch when the edit modal opens, for example. But, don't cache data when this query
+      // is inactive. For example, when navigating away from the page and back again, refetch.
+      {
+        refetchOnMount: false,
+        cacheTime: 0,
+      }
+    ),
   };
 };
