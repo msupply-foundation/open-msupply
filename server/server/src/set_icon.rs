99ea47a61891ccd251e2b29b9796d33a45052391
raviSussol <ravi.shrestha@sussol.net>
2022-08-30 14:32:06 +0545

    #22 Fixed build error for unix based platforms

--- server/server/src/set_icon.rs
+++ server/server/src/set_icon.rs
@@ -7,3 +7,9 @@ fn main() {
     res.set_icon("omSupply.ico");
     res.compile().unwrap();
 }
+
+// Just to skip build error as the build dependencies for "winres" is being used for "windows" targeted
+// This way the build time for the unix based platform gets saved
+#[cfg(unix)]
+fn main() {
+}
