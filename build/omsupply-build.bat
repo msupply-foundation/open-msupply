a891add7e97bf437feed6f806f95b5a647b92b88
raviSussol <ravi.shrestha@sussol.net>
2022-08-30 15:57:30 +0545

    #22 Added setup factory build scripts for windows installers

---
+++ build/omsupply-build.bat
@@ -0,0 +1,26 @@
+@ECHO ##### Removing previous builds #####
+SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\omSupplyMain - installers\workspace\omSupply
+@rmdir "%installerWorkspace%\omSupply Web Server" /s /q
+@rmdir "%installerWorkspace%\omSupply Desktop Server" /s /q
+@rmdir "%installerWorkspace%\build" /s /q
+
+@ECHO ##### Starting omsupply builds #####
+mkdir "%installerWorkspace%\omSupply Web Server"
+xcopy "server\configuration" "%installerWorkspace%\omSupply Web Server\configuration" /e /h /c /i
+
+mkdir "%installerWorkspace%\omSupply Desktop Server"
+mkdir "%installerWorkspace%\omSupply Desktop Server\server"
+xcopy "server\configuration" "%installerWorkspace%\omSupply Desktop Server\server\configuration" /e /h /c /i
+
+copy "server\server\src\omSupply.ico" "build\omSupply.ico"
+xcopy "build" "%installerWorkspace%\build" /e /h /c /i
+copy "version.txt" "%installerWorkspace%\version.txt"
+
+@cd "build"
+start /b /wait omsupply-sqlite-build.bat
+
+@cd "build"
+start /b /wait omsupply-postgres-build.bat
+
+@cd "build"
+start /b /wait omsupply-desktop-build.bat
