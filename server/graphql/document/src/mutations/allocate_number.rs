721c71a3ca6b050bc26b47bd53c4107614d31b4f
Clemens Zeidler <clemens@msupply.foundation>
2022-08-31 13:21:53 +1200

    Make use of the new number service and update endpoint name
    Source: https://github.com/msupply-foundation/open-msupply/commit/81a1acb8d37e881d4ced19a952ec877be4406d41

--- server/graphql/document/src/mutations/allocate_number.rs
+++ server/graphql/document/src/mutations/allocate_number.rs
@@ -1,40 +1,54 @@
 #[derive(InputObject)]
-pub struct AllocateNumberInput {
+pub struct AllocateProgramNumberInput {
     number_name: String,
 }
 
 use async_graphql::*;
-use graphql_core::standard_graphql_error::validate_auth;
-use service::auth::{Resource, ResourceAccessRequest};
+use graphql_core::{standard_graphql_error::validate_auth, ContextExt};
+use repository::NumberRowType;
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    number::next_number,
+};
 
 pub struct NumberNode {
-    pub number: u64,
+    pub number: i64,
 }
 
 #[Object]
 impl NumberNode {
-    pub async fn number(&self) -> u64 {
+    pub async fn number(&self) -> i64 {
         self.number
     }
 }
 
 #[derive(Union)]
-pub enum AllocateNumberResponse {
+pub enum AllocateProgramNumberResponse {
     Response(NumberNode),
 }
 
-pub fn allocate_number(
+pub fn allocate_program_number(
     ctx: &Context<'_>,
     store_id: String,
-    _input: AllocateNumberInput,
-) -> Result<AllocateNumberResponse> {
+    input: AllocateProgramNumberInput,
+) -> Result<AllocateProgramNumberResponse> {
     validate_auth(
         ctx,
         &ResourceAccessRequest {
-            resource: Resource::AllocateNumber,
-            store_id: Some(store_id),
+            resource: Resource::MutateProgram,
+            store_id: Some(store_id.clone()),
         },
     )?;
 
-    Ok(AllocateNumberResponse::Response(NumberNode { number: 42 }))
+    let service_provider = ctx.service_provider();
+    let context = service_provider.basic_context()?;
+
+    let number = next_number(
+        &context.connection,
+        &NumberRowType::Program(input.number_name),
+        &store_id,
+    )?;
+    Ok(AllocateProgramNumberResponse::Response(NumberNode {
+        number,
+    }))
 }
