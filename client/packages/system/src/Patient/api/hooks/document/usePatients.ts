83ec176cfac24b42bfafd3dc1d69c9713e289fa4
Clemens Zeidler <clemens@msupply.foundation>
2022-09-05 14:07:49 +1200

    Use url query params for the patient list + fix patient encounter list

--- client/packages/system/src/Patient/api/hooks/document/usePatients.ts
+++ client/packages/system/src/Patient/api/hooks/document/usePatients.ts
@@ -1,22 +1,12 @@
-import { useQuery, useQueryParamsStore } from '@openmsupply-client/common';
+import { useQuery, useUrlQueryParams } from '@openmsupply-client/common';
 import { usePatientApi } from '../utils/usePatientApi';
 
 export const usePatients = () => {
   const api = usePatientApi();
-  const {
-    sort: { sortBy },
-    filter: { filterBy },
-    pagination: { page, first, offset },
-  } = useQueryParamsStore();
-
-  const queryParams = {
-    page,
-    first,
-    offset,
-    sortBy,
-    filterBy,
-  };
-
+  const { queryParams } = useUrlQueryParams({
+    filterKey: 'lastName',
+    initialSort: { key: 'code', dir: 'asc' },
+  });
   return {
     ...useQuery(api.keys.paramList(queryParams), () =>
       api.get.list(queryParams)
