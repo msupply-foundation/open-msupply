6b368a3cc109b72b7890eb21d6ec011fab51e1b9
Andrei <andrei.evguenov@gmail.com>
2022-09-07 12:29:33 +1200

    Add name_id and store_id to changelog when invoice or invoice_line or requisition or requisition_line are mutated
    Source: https://github.com/msupply-foundation/open-msupply/commit/74aea0be4d33a412372ee95c01b7487ba605a37f

--- server/repository/migrations/postgres/2022-02-23T14-00_create_requisition_line_trigger/up.sql
+++ server/repository/migrations/postgres/2022-02-23T14-00_create_requisition_line_trigger/up.sql
@@ -1,3 +1,29 @@
-CREATE TRIGGER requisition_line_trigger
-  AFTER INSERT OR UPDATE OR DELETE ON requisition_line
-  FOR EACH ROW EXECUTE PROCEDURE update_changelog();
+CREATE FUNCTION upsert_requisition_line_changelog()
+RETURNS trigger AS
+$$
+  BEGIN
+    INSERT INTO changelog (table_name, record_id, row_action, name_id, store_id)
+      SELECT 'requisition_line', NEW.id, 'UPSERT', name_id, store_id FROM requisition WHERE id = NEW.requisition_id;
+    -- Ignored for AFTER trigger
+    RETURN NULL;
+  END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION delete_requisition_line_changelog()
+RETURNS trigger AS
+$$
+  BEGIN
+    INSERT INTO changelog (table_name, record_id, row_action, name_id, store_id)
+      SELECT 'requisition_line', OLD.id, 'DELETE', name_id, store_id FROM requisition WHERE id = OLD.requisition_id;
+    -- Ignored for AFTER trigger
+    RETURN NULL;
+  END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER requisition_line_upsert_trigger
+  AFTER INSERT OR UPDATE ON requisition_line
+  FOR EACH ROW EXECUTE FUNCTION upsert_requisition_line_changelog();
+
+CREATE TRIGGER requisition_line_delete_trigger
+  AFTER DELETE ON requisition_line
+  FOR EACH ROW EXECUTE FUNCTION delete_requisition_line_changelog();
