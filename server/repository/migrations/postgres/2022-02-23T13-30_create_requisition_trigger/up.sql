74aea0be4d33a412372ee95c01b7487ba605a37f
Andrei <andrei.evguenov@gmail.com>
2022-09-07 12:29:33 +1200

    Add name_id and store_id to changelog when invoice or invoice_line or requisition or requisition_line are mutated

--- server/repository/migrations/postgres/2022-02-23T13-30_create_requisition_trigger/up.sql
+++ server/repository/migrations/postgres/2022-02-23T13-30_create_requisition_trigger/up.sql
@@ -1,3 +1,29 @@
-CREATE TRIGGER requisition_trigger
-  AFTER INSERT OR UPDATE OR DELETE ON requisition
-  FOR EACH ROW EXECUTE PROCEDURE update_changelog();
+CREATE FUNCTION upsert_requisition_changelog()
+RETURNS trigger AS
+$$
+  BEGIN
+    INSERT INTO changelog (table_name, record_id, row_action, name_id, store_id)
+      VALUES ('requisition', NEW.id, 'UPSERT', NEW.name_id, NEW.store_id);
+    -- Ignored for AFTER trigger
+    RETURN NULL;
+  END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION delete_requisition_changelog()
+RETURNS trigger AS
+$$
+  BEGIN
+    INSERT INTO changelog (table_name, record_id, row_action, name_id, store_id)
+      VALUES ('requisition', OLD.id, 'DELETE', OLD.name_id, OLD.store_id);
+    -- Ignored for AFTER trigger
+    RETURN NULL;
+  END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER requisition_upsert_trigger
+  AFTER INSERT OR UPDATE ON requisition
+  FOR EACH ROW EXECUTE FUNCTION upsert_requisition_changelog();
+
+CREATE TRIGGER requisition_delete_trigger
+  AFTER DELETE ON requisition
+  FOR EACH ROW EXECUTE FUNCTION delete_requisition_changelog();
