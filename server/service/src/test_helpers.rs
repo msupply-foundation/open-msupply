52337f712ef9bcf89c2f74edfa6efb836e89f53a
Andrei <andrei.evguenov@gmail.com>
2022-09-07 12:36:51 +1200

    Add a test helper for creating processor driver and service provider
    Source: https://github.com/msupply-foundation/open-msupply/commit/be5fc5f77f98fded58024b83782eacc136398399

---
+++ server/service/src/test_helpers.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use actix_rt::task::JoinHandle;
+use repository::{
+    mock::{MockData, MockDataInserts},
+    test_db::setup_all_with_data,
+    StorageConnection,
+};
+
+use crate::{processors::Processors, service_provider::ServiceProvider};
+
+pub(crate) struct ServiceTestContext {
+    pub(crate) connection: StorageConnection,
+    pub(crate) service_provider: Arc<ServiceProvider>,
+    pub(crate) processors_task: JoinHandle<()>,
+}
+
+// TODO use this method in service tests
+pub(crate) async fn setup_all_with_data_and_service_provider(
+    db_name: &str,
+    inserts: MockDataInserts,
+    extra_mock_data: MockData,
+) -> ServiceTestContext {
+    let (_, connection, connection_manager, _) =
+        setup_all_with_data(db_name, inserts, extra_mock_data).await;
+
+    let (processors_trigger, processors) = Processors::init();
+    let service_provider = Arc::new(ServiceProvider::new_with_processors(
+        connection_manager.clone(),
+        "",
+        processors_trigger,
+    ));
+
+    let processors_task = processors.spawn(service_provider.clone());
+
+    ServiceTestContext {
+        connection,
+        service_provider,
+        processors_task,
+    }
+}
+
+// TODO use this method in service tests
+pub(crate) async fn setup_all_and_service_provider(
+    db_name: &str,
+    inserts: MockDataInserts,
+) -> ServiceTestContext {
+    setup_all_with_data_and_service_provider(db_name, inserts, MockData::default()).await
+}
