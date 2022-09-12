eade61da0aefd578ce75dd2beadc8e41b15a688b
Andrei <andrei.evguenov@gmail.com>
2022-09-12 17:23:51 +1200

    Rename latest_changelog to latest_cursor and add comment about it's use
    Source: https://github.com/msupply-foundation/open-msupply/commit/a0cf103995c3e1e285d97ccde1bf0c1ea2c248c4

--- server/service/src/sync/remote_data_synchroniser.rs
+++ server/service/src/sync/remote_data_synchroniser.rs
@@ -89,7 +89,7 @@ impl RemoteDataSynchroniser {
         // Update push cursor after initial sync, i.e. set it to the end of the just received data
         // so we only push new data to the central server
         let cursor = ChangelogRepository::new(connection)
-            .latest_changelog()
+            .latest_cursor()
             .map_err(SetInitialisedError)? as u32;
         remote_sync_state
             .update_push_cursor(cursor + 1)
