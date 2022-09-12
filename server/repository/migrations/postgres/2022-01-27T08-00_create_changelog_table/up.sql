a0ec6c0e8725ecd43128f703f09c9c417b8f6e9a
Andrei <andrei.evguenov@gmail.com>
2022-09-12 17:30:10 +1200

    Add documentation in database schema for name_id and store_id on changelog table
    Source: https://github.com/msupply-foundation/open-msupply/commit/5f34a8066c85a8934e947555a35cc1ab92b6d5e9

--- server/repository/migrations/postgres/2022-01-27T08-00_create_changelog_table/up.sql
+++ server/repository/migrations/postgres/2022-01-27T08-00_create_changelog_table/up.sql
@@ -26,7 +26,7 @@ CREATE TABLE changelog (
     -- Sqlite only fires INSERT when doing an upsert (it does a delete + insert) for this reason
     -- use UPSERT.
     row_action row_action_type NOT NULL,
-    -- TODO comment
+    -- Below fields are extracted from associated record where it's deemed necessary (see changelog/README.md)
     name_id TEXT,
     store_id TEXT
 );
