aade28bc041cd71cfd77fa9ffac7bfdc75d190d5
Mark Prins <mark@malcolmprins.net>
2022-09-13 11:56:39 +1200

    desktop installer
    Source: https://github.com/msupply-foundation/open-msupply/commit/792a949ffd54022343eaff3de303fc5a8aa9b3ed

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -2,8 +2,7 @@
 @rmdir "installers" /s /q
 
 @ECHO ##### Copying bin and certs folders #####
-xcopy "omSupply\bin" "omSupply\omSupply Web Server\bin" /e /h /c /i
-xcopy "omSupply\bin" "omSupply\omSupply Desktop Server\server\bin" /e /h /c /i
+xcopy "omSupply\bin" "omSupply\omSupply Server\bin" /e /h /c /i
 
 @ECHO ##### Adjusting SUFS #####
 SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\omSupplyMain - installers\workspace\omSupply
@@ -19,5 +18,5 @@ node "%SUFlocation%\adjustSUFs.js"
 @ECHO ##### Creating installers #####
 start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_server.suf"
 start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_server_upgrader.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop_server.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop_server_upgrader.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop_upgrader.suf"
--- build/windows/omsupply-sqlite-build.bat
+++ build/windows/omsupply-sqlite-build.bat
@@ -1,3 +1,3 @@
 @ECHO ##### Building omsupply for the sqlite #####
 SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\omSupplyMain - installers\workspace\omSupply
-cd "..\..\server" && cargo build --release && cd .. && @ECHO ##### Copying build artifacts to the installer folder ##### && copy "server\target\release\remote_server.exe" "%installerWorkspace%\omSupply Web Server\omSupply-sqlite.exe" && copy "server\target\release\remote_server.exe" "%installerWorkspace%\omSupply Desktop Server\server\omSupply-sqlite.exe"
+cd "..\..\server" && cargo build --release && cd .. && @ECHO ##### Copying build artifacts to the installer folder ##### && copy "server\target\release\remote_server.exe" "%installerWorkspace%\omSupply Server\omSupply-sqlite.exe"
