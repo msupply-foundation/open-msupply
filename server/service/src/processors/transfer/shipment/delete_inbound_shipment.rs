3533456305c70a716105017c8937ab4b115880ac
Andrei <andrei.evguenov@gmail.com>
2022-09-13 22:16:50 +1200

    Update variable names in shipment transfer processors
    Source: https://github.com/msupply-foundation/open-msupply/commit/28c2376e4cf7974ea5c060d679692c15f40aa773

--- server/service/src/processors/transfer/shipment/delete_inbound_shipment.rs
+++ server/service/src/processors/transfer/shipment/delete_inbound_shipment.rs
@@ -3,10 +3,9 @@ use repository::{
     RepositoryError, StorageConnection,
 };
 
-use super::{
-    common::get_lines_for_invoice, Operation, ShipmentTransferProcessor,
-    ShipmentTransferProcessorRecord,
-};
+use crate::invoice::common::get_lines_for_invoice;
+
+use super::{Operation, ShipmentTransferProcessor, ShipmentTransferProcessorRecord};
 
 const DESCRIPTION: &'static str =
     "Delete inbound shipment when source outbound shipment is deleted";
@@ -24,10 +23,10 @@ impl ShipmentTransferProcessor for DeleteInboundShipmentProcessor {
     /// 2. Operation is delete
     /// 3. Linked shipment exists
     /// 4. Linked shipment is InboundShipment
-    /// 5. Linked shipment is Picked (Inbound shipment can only be deleted before it turns to Shipped status)
+    /// 5. Linked inbound shipment is Picked (Inbound shipment can only be deleted before it turns to Shipped status)
     ///
     /// Only runs once:
-    /// 6. Because linked shipment is deleted `3.` will never be true again
+    /// 6. Because linked inbound shipment is deleted `3.` will never be true again
     fn try_process_record(
         &self,
         connection: &StorageConnection,
@@ -40,35 +39,37 @@ impl ShipmentTransferProcessor for DeleteInboundShipmentProcessor {
             _ => return Ok(None),
         };
         // 3.
-        let linked_shipment = match &linked_shipment {
+        let inbound_shipment = match &linked_shipment {
             Some(linked_shipment) => linked_shipment,
             None => return Ok(None),
         };
         // 4.
-        if linked_shipment.invoice_row.r#type != InvoiceRowType::InboundShipment {
+        if inbound_shipment.invoice_row.r#type != InvoiceRowType::InboundShipment {
             return Ok(None);
         }
         // 5.
-        if linked_shipment.invoice_row.status != InvoiceRowStatus::Picked {
+        if inbound_shipment.invoice_row.status != InvoiceRowStatus::Picked {
             return Ok(None);
         }
 
         // Execute
-        let deleted_invoice_id = &linked_shipment.invoice_row.id;
-        let deleted_invoice_lines = get_lines_for_invoice(connection, deleted_invoice_id)?;
+        let deleted_inbound_shipment_id = &inbound_shipment.invoice_row.id;
+        let deleted_inbound_lines = get_lines_for_invoice(connection, deleted_inbound_shipment_id)?;
 
         let invoice_line_repository = InvoiceLineRowRepository::new(connection);
 
-        for line in deleted_invoice_lines.iter() {
-            invoice_line_repository.delete(&line.id)?;
+        for line in deleted_inbound_lines.iter() {
+            invoice_line_repository.delete(&line.invoice_line_row.id)?;
         }
         // 6.
-        InvoiceRowRepository::new(connection).delete(deleted_invoice_id)?;
+        InvoiceRowRepository::new(connection).delete(deleted_inbound_shipment_id)?;
 
         let result = format!(
             "shipment ({}) lines ({:?})",
-            deleted_invoice_id,
-            deleted_invoice_lines.into_iter().map(|r| r.id),
+            deleted_inbound_shipment_id,
+            deleted_inbound_lines
+                .into_iter()
+                .map(|r| r.invoice_line_row.id),
         );
 
         Ok(Some(result))
--- server/service/src/processors/transfer/shipment/update_outbound_shipment_status.rs
+++ server/service/src/processors/transfer/shipment/update_outbound_shipment_status.rs
@@ -1,5 +1,6 @@
 use repository::{
-    InvoiceRowRepository, InvoiceRowStatus, InvoiceRowType, RepositoryError, StorageConnection,
+    InvoiceRow, InvoiceRowRepository, InvoiceRowStatus, InvoiceRowType, RepositoryError,
+    StorageConnection,
 };
 
 use crate::processors::transfer::shipment::Operation;
@@ -20,11 +21,11 @@ impl ShipmentTransferProcessor for UpdateOutboundShipmentStatusProcessor {
     /// 1. Source shipment name_id is for a store that is active on current site (transfer processor driver guarantees this)
     /// 2. Source shipment is Inbound shipment
     /// 3. Linked shipment exists (the outbound shipment)
-    /// 4. Linked shipment invoice status is not Verified (this is the last status possible)
-    /// 5. Linked shipment status is not source shipment status
+    /// 4. Linked outbound shipment status is not Verified (this is the last status possible)
+    /// 5. Linked outbound shipment status is not source inbound shipment status
     ///
     /// Can only run two times (one for Delivered and one for Verified status):
-    /// 6. Because linked shipment status will be updated to source shipment status and `5.` will never be true again
+    /// 6. Because linked outbound shipment status will be updated to source inbound shipment status and `5.` will never be true again
     ///    and business rules guarantee that Inbound shipment can only change status to Delivered and Verified
     ///    and status cannot be changed backwards
     fn try_process_record(
@@ -33,7 +34,7 @@ impl ShipmentTransferProcessor for UpdateOutboundShipmentStatusProcessor {
         record_for_processing: &ShipmentTransferProcessorRecord,
     ) -> Result<Option<String>, RepositoryError> {
         // Check can execute
-        let (source_shipment, linked_shipment) = match &record_for_processing.operation {
+        let (inbound_shipment, linked_shipment) = match &record_for_processing.operation {
             Operation::Upsert {
                 shipment,
                 linked_shipment,
@@ -41,39 +42,39 @@ impl ShipmentTransferProcessor for UpdateOutboundShipmentStatusProcessor {
             _ => return Ok(None),
         };
         // 2.
-        if source_shipment.invoice_row.r#type != InvoiceRowType::InboundShipment {
+        if inbound_shipment.invoice_row.r#type != InvoiceRowType::InboundShipment {
             return Ok(None);
         }
         // 3.
-        let linked_shipment = match &linked_shipment {
+        let outbound_shipment = match &linked_shipment {
             Some(linked_shipment) => linked_shipment,
             None => return Ok(None),
         };
         // 4.
-        if linked_shipment.invoice_row.status == InvoiceRowStatus::Verified {
+        if outbound_shipment.invoice_row.status == InvoiceRowStatus::Verified {
             return Ok(None);
         }
         // 5.
-        if linked_shipment.invoice_row.status == source_shipment.invoice_row.status {
+        if outbound_shipment.invoice_row.status == inbound_shipment.invoice_row.status {
             return Ok(None);
         }
 
         // Execute
-        let mut updated_linked_shipment = linked_shipment.invoice_row.clone();
-        // 6.
-        updated_linked_shipment.status = source_shipment.invoice_row.status.clone();
-        updated_linked_shipment.delivered_datetime =
-            source_shipment.invoice_row.delivered_datetime.clone();
-        updated_linked_shipment.verified_datetime =
-            source_shipment.invoice_row.verified_datetime.clone();
+        let updated_outbound_shipment = InvoiceRow {
+            // 6.
+            status: inbound_shipment.invoice_row.status.clone(),
+            delivered_datetime: inbound_shipment.invoice_row.delivered_datetime.clone(),
+            verified_datetime: inbound_shipment.invoice_row.verified_datetime.clone(),
+            ..outbound_shipment.invoice_row.clone()
+        };
 
-        InvoiceRowRepository::new(connection).upsert_one(&updated_linked_shipment)?;
+        InvoiceRowRepository::new(connection).upsert_one(&updated_outbound_shipment)?;
 
         let result = format!(
             "shipment ({}) source shipment {}) status ({:?})",
-            updated_linked_shipment.id,
-            source_shipment.invoice_row.id,
-            updated_linked_shipment.status
+            updated_outbound_shipment.id,
+            inbound_shipment.invoice_row.id,
+            updated_outbound_shipment.status
         );
 
         Ok(Some(result))
