164e8eb8fdfec219921d4abe18f3ac3466ac3522
Mark Prins <mark@malcolmprins.net>
2022-09-16 10:23:46 +1200

    update suf paths and versions
    Source: https://github.com/msupply-foundation/open-msupply/commit/6d717a2836e47ce377962e5a8835be9145e1bb9c

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -2,18 +2,16 @@
 @rmdir "installers" /s /q
 
 @ECHO ##### Adjusting SUFS #####
-SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\omSupplyMain - installers\workspace\omSupply
-SET SUFlocation=%installerWorkspace%
 FOR /F "delims=*" %%i in ('more omSupply\version.txt') do SET versionTag=%%i
 @ECHO "current tag = %versionTag%"
-SET installersOutputFolder=C:\Program Files (x86)\Jenkins\jobs\omSupplyMain - installers\workspace\installers
+SET installersOutputFolder=%WORKSPACE%\installers
 
 @cd omSupply
-node "%SUFlocation%\adjustSUFs.js"
+node "adjustSUFs.js"
 @cd ..
 
 @ECHO ##### Creating installers #####
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_server.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_server_upgrader.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop.suf"
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\omsupply_desktop_upgrader.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omsupply_server.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omsupply_server_upgrader.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omsupply_desktop.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omsupply_desktop_upgrader.suf"
