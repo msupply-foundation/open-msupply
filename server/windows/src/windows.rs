af25bdc7e678a25bb4681bd5d378a4745775ea35
Mark Prins <mark@malcolmprins.net>
2022-09-15 12:52:14 +1200

    server stopping and logging
    Source: https://github.com/msupply-foundation/open-msupply/commit/98d65e6c821744a787a038f7b449a2789477f17e

--- server/windows/src/windows.rs
+++ server/windows/src/windows.rs
@@ -12,6 +12,7 @@ fn main() {
 
 #[cfg(windows)]
 mod omsupply_service {
+    use log::{error, info};
     use server::{configuration, logging_init, start_server};
     use service::settings::Settings;
     use std::{
@@ -26,26 +27,30 @@ mod omsupply_service {
             ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
             ServiceType,
         },
-        service_control_handler::{self, ServiceControlHandlerResult},
+        service_control_handler::{self, ServiceControlHandlerResult, ServiceStatusHandle},
         service_dispatcher, Result,
     };
 
+    // used internally by the service control handler - the actual service name can differ
     const SERVICE_NAME: &str = "omsupply_server";
     const SERVICE_TYPE: ServiceType = ServiceType::OWN_PROCESS;
 
     pub fn run() -> Result<()> {
-        // Register generated `ffi_service_main` with the system and start the service, blocking
-        // this thread until the service is stopped.
+        // register generated `ffi_service_main` with the system and start the service
+        // this thread is blocked until the service is stopped
         service_dispatcher::start(SERVICE_NAME, ffi_service_main)
     }
 
-    // Generate the windows service boilerplate.
+    // Generate the windows service boilerplate
     define_windows_service!(ffi_service_main, omsupply_service_main);
 
     // Service entry function which is called on background thread by the system with service
     // parameters. There is no stdout or stderr at this point so make sure to configure the log
     // output to file if needed.
     pub fn omsupply_service_main(_arguments: Vec<OsString>) {
+        // the current dir is used by the configuration module to find the config files
+        // and also by the logging module for the log file location
+        // when run in the service context, the current dir is the windows service directory
         let executable_path = current_exe().unwrap();
         let executable_directory = executable_path.parent().unwrap();
         set_current_dir(&executable_directory).unwrap();
@@ -53,11 +58,27 @@ mod omsupply_service {
             configuration::get_configuration().expect("Failed to parse configuration settings");
         logging_init(settings.logging.clone());
 
-        log::info!("Starting service");
+        info!("Starting service");
 
         if let Err(_e) = run_service(settings) {
-            log::error!("Unable to start service");
+            error!("Unable to start service");
         }
+    }
+
+    fn set_status(
+        status_handle: ServiceStatusHandle,
+        current_state: ServiceState,
+        controls_accepted: ServiceControlAccept,
+    ) -> Result<()> {
+        status_handle.set_service_status(ServiceStatus {
+            service_type: SERVICE_TYPE,
+            current_state,
+            controls_accepted,
+            exit_code: ServiceExitCode::Win32(0),
+            checkpoint: 0,
+            wait_hint: Duration::default(),
+            process_id: None,
+        })
     }
 
     pub fn run_service(settings: Settings) -> Result<()> {
@@ -73,8 +94,20 @@ mod omsupply_service {
 
                 // Handle stop
                 ServiceControl::Stop => {
-                    let _result = futures::executor::block_on(shutdown_tx.send(()));
-                    log::info!("Stopping service");
+                    // update status to StopPending because actix_web can take a long time to stop
+                    // as it has to wait for all threads to stop processing
+                    let event_handler = move |_| -> ServiceControlHandlerResult {
+                        ServiceControlHandlerResult::NotImplemented
+                    };
+                    let status_handle =
+                        service_control_handler::register(SERVICE_NAME, event_handler).unwrap();
+                    let _ = set_status(
+                        status_handle,
+                        ServiceState::StopPending,
+                        ServiceControlAccept::empty(),
+                    );
+
+                    let _ = futures::executor::block_on(shutdown_tx.send(()));
                     ServiceControlHandlerResult::NoError
                 }
 
@@ -82,35 +115,28 @@ mod omsupply_service {
             }
         };
 
-        // Register system service event handler.
-        // The returned status handle should be used to report service status changes to the system.
+        // Register system service event handler
+        // The returned status handle is used to report service status changes to the system
         let status_handle = service_control_handler::register(SERVICE_NAME, event_handler)?;
 
-        // Tell the system that service is running
-        status_handle.set_service_status(ServiceStatus {
-            service_type: SERVICE_TYPE,
-            current_state: ServiceState::Running,
-            controls_accepted: ServiceControlAccept::STOP,
-            exit_code: ServiceExitCode::Win32(0),
-            checkpoint: 0,
-            wait_hint: Duration::default(),
-            process_id: None,
-        })?;
+        // the start_server future only completes when the server is stopped
+        set_status(
+            status_handle,
+            ServiceState::Running,
+            ServiceControlAccept::STOP,
+        )?;
 
-        let _result = futures::executor::block_on(start_server(settings, shutdown_rx));
+        <::actix_web::rt::System>::new().block_on(async {
+            if let Err(e) = start_server(settings, shutdown_rx).await {
+                error!("Error! {:#?}", e);
+            }
+        });
 
-        log::info!("Telling the server that we've stopped");
-
-        // Tell the system that service has stopped.
-        status_handle.set_service_status(ServiceStatus {
-            service_type: SERVICE_TYPE,
-            current_state: ServiceState::Stopped,
-            controls_accepted: ServiceControlAccept::empty(),
-            exit_code: ServiceExitCode::Win32(0),
-            checkpoint: 0,
-            wait_hint: Duration::default(),
-            process_id: None,
-        })?;
+        set_status(
+            status_handle,
+            ServiceState::Stopped,
+            ServiceControlAccept::empty(),
+        )?;
 
         Ok(())
     }
