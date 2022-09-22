412e72ffc43f6e5218d9bc3958fbf699d46e4316
Andrei <andrei.evguenov@gmail.com>
2022-09-22 12:27:52 +1200

    Remove server restart mutation
    Source: https://github.com/msupply-foundation/open-msupply/commit/b972e134b4a88dc8c505db3b4de0680d66febb7a

--- client/packages/host/src/api/hooks/utils/useServerRestart.ts
+++
@@ -1,7 +0,0 @@
-import { useMutation } from '@openmsupply-client/common';
-import { useHostApi } from './useHostApi';
-
-export const useServerRestart = () => {
-  const api = useHostApi();
-  return useMutation<string, unknown, void, unknown>(api.update.restart);
-};
