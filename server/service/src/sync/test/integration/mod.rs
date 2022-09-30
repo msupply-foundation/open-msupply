50f35dcbea549239ef526250e26d6597ec8f6151
Andrei <andrei.evguenov@gmail.com>
2022-09-30 14:49:57 +1300

    Add a comment to delay in integration test site creation

--- server/service/src/sync/test/integration/mod.rs
+++ server/service/src/sync/test/integration/mod.rs
@@ -89,12 +89,12 @@ where
 {
     let mut retries = 0;
     loop {
+        // Reduce chance concurrent operations runing at the same time (when tests all start at once)
         random_delay(10, 50).await;
         let error = match f().await {
             Ok(result) => return Ok(result),
             Err(error) => error,
         };
-
         let error_string = format!("{}", error);
 
         if error_string.contains("Site record locked preventing authentication update")
