caa737ba75a183de681a1945479e7956962f7538
Andrei <andrei.evguenov@gmail.com>
2022-09-30 14:45:38 +1300

    Add mapping of sync error and use it in initialisation
    Source: https://github.com/msupply-foundation/open-msupply/commit/bd8561747977d316ecfa3f36d3857d506c4bc849

--- client/packages/host/src/api/api.ts
+++ client/packages/host/src/api/api.ts
@@ -1,11 +1,18 @@
-import { SyncSettingsInput } from '@common/types';
-import { Sdk } from './operations.generated';
+import {
+  LocaleKey,
+  TypedTFunction,
+  SyncSettingsInput,
+  ErrorWithDetailsProps,
+  SyncErrorVariant,
+} from '@openmsupply-client/common';
+
+import { Sdk, SyncErrorsFragment } from './operations.generated';
 
 export const getHostQueries = (sdk: Sdk) => ({
   get: {
     syncSettings: async () => {
       const result = await sdk.syncSettings();
-      return result?.syncSettings;
+      return result.syncSettings;
     },
     initialisationStatus: async () => {
       const result = await sdk.initialisationStatus();
@@ -22,8 +29,57 @@ export const getHostQueries = (sdk: Sdk) => ({
   },
   initialise: async (settings: SyncSettingsInput) => {
     const result = await sdk.initialiseSite({
-      syncSettings: settings,
+      syncSettings: cleanSyncSettings(settings),
     });
-    return result?.initialiseSite;
+    return result.initialiseSite;
+  },
+  update: async (settings: SyncSettingsInput) => {
+    const result = await sdk.updateSyncSettings({
+      syncSettings: cleanSyncSettings(settings),
+    });
+    return result?.updateSyncSettings;
   },
 });
+
+// In typescript it's allowed to have excess properties on object
+// to avoid errors thrown in mutation, we should remove any excess properties for input
+function cleanSyncSettings({
+  username,
+  password,
+  url,
+  intervalSeconds,
+}: SyncSettingsInput): SyncSettingsInput {
+  return { username, password, url, intervalSeconds };
+}
+
+export function mapSyncError(
+  t: TypedTFunction<LocaleKey>,
+  error: SyncErrorsFragment,
+  defaultKey?: LocaleKey
+): ErrorWithDetailsProps {
+  if (error.__typename === 'UnknownSyncError') {
+    return {
+      error: defaultKey ? t(defaultKey) : t('error.unknown-sync-error'),
+      details: error.fullError,
+    };
+  }
+
+  const errorMapping: { [key in SyncErrorVariant]: LocaleKey } = {
+    [SyncErrorVariant.ConnectionError]: 'error.connection-error',
+    [SyncErrorVariant.SiteUuidIsBeingChanged]:
+      'error.site-uuid-is-being-changed',
+    [SyncErrorVariant.HardwareIdMismatch]: 'error.site-incorrect-hardware-id',
+    [SyncErrorVariant.IncorrectPassword]: 'error.site-incorrect-password',
+    [SyncErrorVariant.SiteAuthTimeout]: 'error.site-auth-timeout',
+    [SyncErrorVariant.SiteHasNoStore]: 'error.site-has-no-store',
+    [SyncErrorVariant.SiteNameNotFound]: 'error.site-name-not-found',
+    [SyncErrorVariant.IntegrationTimeoutReached]:
+      'error.integration-timeout-reached',
+    [SyncErrorVariant.InvalidUrl]: 'error.invalid-url',
+  };
+
+  return {
+    error: t(errorMapping[error.errorVariant]),
+    details: error.fullError,
+  };
+}
