2a130ab65537b34ae4f08f4dfcab42dded2b0b5c
Andrei <andrei.evguenov@gmail.com>
2022-10-05 09:04:14 +1300

    Remove keys from mutations
    Source: https://github.com/msupply-foundation/open-msupply/commit/70a49f5ee7ffbd237340e0ed8fbac838c5d9afc8

--- client/packages/host/src/api/hooks/sync/useInitialiseSite.ts
+++ client/packages/host/src/api/hooks/sync/useInitialiseSite.ts
@@ -1,12 +1,7 @@
-import { useMutation, useQueryClient } from '@openmsupply-client/common';
+import { useMutation } from '@openmsupply-client/common';
 import { useHostApi } from '../utils/useHostApi';
 
 export const useInitialiseSite = () => {
-  const queryClient = useQueryClient();
   const api = useHostApi();
-  return useMutation(api.initialise, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(api.keys.initialiseSite());
-    },
-  });
+  return useMutation(api.initialise);
 };
