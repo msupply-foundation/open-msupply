2e1e3dde63de979965fd70df69fc723632477a4c
roxy-dao <roxy@msupply.foundation>
2022-10-06 09:59:13 +1300

    Rename log in FE
    Source: https://github.com/msupply-foundation/open-msupply/commit/0845333ca74a582f2723c47acbbcc49ec30d51fd

--- client/packages/system/src/Log/api/hooks/document/useLogsByRecord.ts
+++ client/packages/system/src/Log/api/hooks/document/useLogsByRecord.ts
@@ -1,10 +1,10 @@
 import { SortBy, useQuery } from '@openmsupply-client/common';
-import { LogRowFragment } from '../../operations.generated';
+import { ActivityLogRowFragment } from '../../operations.generated';
 import { useLogApi } from '../utils/useLogApi';
 
 export const useLogsByRecord = (
   recordId: string,
-  sortBy?: SortBy<LogRowFragment>
+  sortBy?: SortBy<ActivityLogRowFragment>
 ) => {
   const api = useLogApi();
   const result = useQuery(api.keys.sortedListByRecord(recordId, sortBy), () =>
