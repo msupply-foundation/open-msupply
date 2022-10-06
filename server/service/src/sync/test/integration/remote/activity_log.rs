b3b3ad6d8f89edf22fa7d885e15fd40471c4c2f0
roxy-dao <roxy@msupply.foundation>
2022-10-06 14:55:39 +1300

    Add another log entry to integrated test

--- server/service/src/sync/test/integration/remote/activity_log.rs
+++ server/service/src/sync/test/integration/remote/activity_log.rs
@@ -25,11 +25,20 @@ impl SyncRecordTester for ActivityLogRecordTester {
             datetime: NaiveDate::from_ymd(2020, 1, 1).and_hms(0, 0, 0),
         };
 
+        let log_2 = inline_edit(&row, |mut l| {
+            l.id = uuid();
+            l.r#type = ActivityLogType::InvoiceCreated;
+            l.store_id = Some(store_id.to_string());
+            l.record_id = Some("outbound_shipment_a");
+            d
+        });
+
         result.push(TestStepData {
             central_upsert: json!({}),
             central_delete: json!({}),
             integration_records: IntegrationRecords::from_upserts(vec![
                 PullUpsertRecord::ActivityLog(row.clone()),
+                PullUpsertRecord::ActivityLog(log_2.clone()),
             ]),
         });
         result
