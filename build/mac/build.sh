771e27ff931fecc7b01c1698dbd4759f6dfabc2e
Andrei <andrei.evguenov@gmail.com>
2022-10-11 16:07:24 +1300

    Fix mac build script and readme

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# From repo root directory, make sure 
+# On M1 mac below would be DESTINATION="arm64_mac_sqlite"
 DESTINATION="$(uname -m)_mac_sqlite"
 
 # Buid (on Mac)
@@ -11,8 +11,6 @@ rm -rf $DESTINATION && mkdir $DESTINATION&& mkdir $DESTINATION/bin && mv server/
 mkdir $DESTINATION/configuration && cp -R server/configuration/base.yaml $DESTINATION/configuration/
 # Local file should be present
 touch $DESTINATION/configuration/local.yaml
-# Empty out local.yaml
-rm $DESTINATION/configuration/local.yaml
 
 # Copy launch script
 cp build/mac/open_msupply_server.sh $DESTINATION/
