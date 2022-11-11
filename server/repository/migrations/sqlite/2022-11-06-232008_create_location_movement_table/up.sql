d43e219c87166c7c77f2a8e19f81b3c8688b22e3
roxy-dao <roxy@msupply.foundation>
2022-11-11 16:27:26 +1300

    Migrations for location movement table
    Source: https://github.com/msupply-foundation/open-msupply/commit/5b5bdd4055c5516333cf41f3c5abb1e574e8d6d4

---
+++ server/repository/migrations/sqlite/2022-11-06-232008_create_location_movement_table/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE location_movement (
+    id TEXT NOT NULL PRIMARY KEY,
+    store_id TEXT REFERENCES store(id),
+    location_id TEXT REFERENCES location(id),
+    stock_line_id TEXT REFERENCES stock_line(id),
+    enter_datetime TIMESTAMP NOT NULL,
+    exit_datetime TIMESTAMP
+)
