72a971d20fa440a6449cdcf67e9fc019cab9f9eb
Andrei <andrei.evguenov@gmail.com>
2022-11-14 16:58:21 +1300

    Use dns sd in electron and add go back to discovery binding
    Source: https://github.com/msupply-foundation/open-msupply/commit/a5b92c156cf6fe6cc2184df488d9246e30f28e68

--- client/packages/electron/src/shared.ts
+++ client/packages/electron/src/shared.ts
@@ -1,6 +1,7 @@
 export const IPC_MESSAGES = {
-  SERVER_DISCOVERED: 'server-discovered',
+  DISCOVERED_SERVERS: 'discovered-servers',
   START_SERVER_DISCOVERY: 'start-server-discovery',
   CONNECT_TO_SERVER: 'connect-to-server',
   CONNECTED_SERVER: 'connected-server',
+  GO_BACK_TO_DISCOVERY: 'go-back-to-discovery',
 };
