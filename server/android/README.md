c83cee3a5b5807864ec1b7cecdd6a62ffd72f8cb
Andrei <andrei.evguenov@gmail.com>
2022-11-14 17:14:48 +1300

    Simplify android lib and build
    Add base android app

---
+++ server/android/README.md
@@ -0,0 +1,29 @@
+ # Android build
+
+ # Mac
+
+* Install Android Studio
+* Install NDK (as per screenshot below)  **important to use version 22**
+
+![omSupply Android NDK](./doc/omSupply_android_ndk.png)
+
+Next we need to tell rust compiler to use pre built linked for target Android ABI.
+The two main (supported by devices) ABI architectures are: 
+
+| rust target             | android ABI |
+|-------------------------|-------------|
+| aarch64-linux-android   | arm64-v8a   |
+| armv7-linux-androideabi | armeabi-v7a |
+
+* Add targets to to rust `rustup target add aarch64-linux-android && rustup target add armv7-linux-androideabi`
+* Tell rust to use pre built linker from NDK. This is defined in .cargo/config.toml, underd [target.*]. We need to add prebuild Android ABI from NDK to PATH so that linkers/ar files can be found. 
+   * Find your NDK location, my one is `~/Library/Android/sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/darwin-x86_64/bin/`
+   * Permenantly add NDK_BIN env variable `echo "export NDK_BIN=~/Library/Android/sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/darwin-x86_64/bin/" >> ~/.zshrc`
+   * Open new terminal and validate $NDK_BIN is set correctly: `ls $NDK_BIN` (should give a list of files starting with aarch64 and armv7a)
+   * Now we can add `$NDK_BIN` to path when buiding: `PATH=$PATH:$NDK_BIN cargo build --release`
+
+`cargo build --release` from this directory will build for both ABI mentioned above, it's quicker to build one when doing a debug build `cargo build --target aarch64-linux-android --release`
+
+# Move to abi
+
+Link to how it connects
--- server/android/src/android.rs
+++ server/android/src/android.rs
@@ -2,167 +2,89 @@
 #[allow(non_snake_case)]
 pub mod android {
     extern crate jni;
-
-    use std::io::Write;
     use std::path::PathBuf;
+    use std::sync::Mutex;
     use std::thread::{self, JoinHandle};
-    use std::{collections::HashMap, sync::Mutex};
 
-    use jni::sys::{jchar, jshort};
-    use rcgen::generate_simple_self_signed;
+    use jni::sys::jchar;
     use repository::database_settings::DatabaseSettings;
-
-    use server::certs::{PRIVATE_CERT_FILE, PUBLIC_CERT_FILE};
     use server::start_server;
     use service::settings::{ServerSettings, Settings};
     use tokio::sync::mpsc;
 
     use self::jni::objects::{JClass, JString};
-    use self::jni::sys::{jlong, jstring};
     use self::jni::JNIEnv;
 
     use android_logger::Config;
     use log::Level;
 
-    use once_cell::sync::Lazy;
-
-    /// Handle for a running remote_server instance
-    struct RunningServerHandle {
-        /// Channel to signal the remote_server to exit
+    struct ServerBucket {
         off_switch: mpsc::Sender<()>,
-        /// JoinHandle for the running remote_server thread
         thread: JoinHandle<()>,
     }
 
-    /// Manages a set of RunningServerHandle
-    /// Each running server is associated with an integer handle which can be passed to the Java
-    /// world, e.g. the handle can be used to shutdown the server from Java.
-    struct ServerBucket {
-        // next handle ready to be used
-        next_handle: i64,
-        map: HashMap<i64, RunningServerHandle>,
-    }
-
-    impl ServerBucket {
-        /// Returns an integer handle to the running server
-        fn insert(&mut self, server: RunningServerHandle) -> i64 {
-            let handle = self.next_handle;
-            self.next_handle = handle + 1;
-            self.map.insert(handle, server);
-            handle
-        }
-    }
-
-    static SERVER_BUCKET: Lazy<Mutex<ServerBucket>> = Lazy::new(|| {
-        Mutex::new(ServerBucket {
-            next_handle: 1,
-            map: HashMap::new(),
-        })
-    });
-
-    fn generate_certs(cert_dir: &PathBuf) {
-        let subject_alt_names = vec!["localhost".to_string()];
-        let cert = generate_simple_self_signed(subject_alt_names).unwrap();
-
-        std::fs::create_dir_all(cert_dir).unwrap();
-        let mut file = std::fs::File::create(cert_dir.join(PRIVATE_CERT_FILE)).unwrap();
-        file.write_all(cert.serialize_private_key_pem().as_bytes())
-            .unwrap();
-
-        let mut file = std::fs::File::create(cert_dir.join(PUBLIC_CERT_FILE)).unwrap();
-        file.write_all(cert.serialize_pem().unwrap().as_bytes())
-            .unwrap();
-    }
-
-    /// Bindings test method. TODO: remove when not needed anymore
-    #[no_mangle]
-    pub unsafe extern "C" fn Java_org_openmsupply_client_RemoteServer_rustHelloWorld(
-        env: JNIEnv,
-        _: JClass,
-        name: JString,
-    ) -> jstring {
-        let name: String = env.get_string(name).unwrap().into();
-        let response = format!("Hello {}!", name);
-        env.new_string(response).unwrap().into_inner()
-    }
+    static SERVER_BUCKET: Mutex<Option<ServerBucket>> = Mutex::new(None);
 
     #[no_mangle]
-    pub unsafe extern "C" fn Java_org_openmsupply_client_RemoteServer_startServer(
+    pub unsafe extern "C" fn Java_org_openmsupply_mobile_RemoteServer_startServer(
         env: JNIEnv,
         _: JClass,
         port: jchar,
         files_dir: JString,
         cache_dir: JString,
         android_id: JString,
-    ) -> jlong {
-        android_logger::init_once(Config::default().with_min_level(Level::Trace));
+    ) {
+        android_logger::init_once(Config::default().with_min_level(Level::Info));
+        log_panics::init();
 
         let (off_switch, off_switch_receiver) = mpsc::channel(1);
         let files_dir: String = env.get_string(files_dir).unwrap().into();
         let files_dir = PathBuf::from(&files_dir);
         let android_id: String = env.get_string(android_id).unwrap().into();
         let db_path = files_dir.join("omsupply-database");
-
         let cache_dir: String = env.get_string(cache_dir).unwrap().into();
 
-        let cert_path = files_dir.join("certs");
-        if !cert_path.join(PRIVATE_CERT_FILE).exists() {
-            generate_certs(&cert_path);
-        }
+        let settings = Settings {
+            server: ServerSettings {
+                port,
+                danger_allow_http: false,
+                debug_no_access_control: false,
+                cors_origins: vec!["http://localhost".to_string()],
+                base_dir: Some(files_dir.to_str().unwrap().to_string()),
+                machine_uid: Some(android_id),
+            },
+            database: DatabaseSettings {
+                username: "n/a".to_string(),
+                password: "n/a".to_string(),
+                port: 0,
+                host: "n/a".to_string(),
+                database_name: db_path.to_string_lossy().to_string(),
+                // See https://github.com/openmsupply/remote-server/issues/1076
+                init_sql: Some(format!("PRAGMA temp_store_directory = '{}';", cache_dir)),
+            },
+            // sync settings need to be configured at runtime
+            sync: None,
+            logging: None,
+        };
 
         // run server in background thread
         let thread = thread::spawn(move || {
-            actix_web::rt::System::new().block_on(async move {
-                let settings = Settings {
-                    server: ServerSettings {
-                        port,
-                        danger_allow_http: true,
-                        debug_no_access_control: false,
-                        cors_origins: vec!["http://localhost".to_string()],
-                        base_dir: Some(files_dir.to_str().unwrap().to_string()),
-                        machine_uid: Some(android_id),
-                    },
-                    database: DatabaseSettings {
-                        username: "n/a".to_string(),
-                        password: "n/a".to_string(),
-                        port: 0,
-                        host: "n/a".to_string(),
-                        database_name: db_path.to_string_lossy().to_string(),
-                        // See https://github.com/openmsupply/remote-server/issues/1076
-                        init_sql: Some(format!("PRAGMA temp_store_directory = '{}';", cache_dir)),
-                    },
-                    // sync settings need to be configured at runtime
-                    sync: None,
-                    logging: None,
-                };
-                let _ = start_server(settings, off_switch_receiver).await;
-            });
+            actix_web::rt::System::new()
+                .block_on(start_server(settings, off_switch_receiver))
+                .unwrap();
         });
 
         let mut bucket = SERVER_BUCKET.lock().unwrap();
-        bucket.insert(RunningServerHandle { thread, off_switch })
+        *bucket = Some(ServerBucket { off_switch, thread });
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn Java_org_openmsupply_client_RemoteServer_stopServer(
+    pub unsafe extern "C" fn Java_org_openmsupply_mobile_RemoteServer_stopServer(
         _: JNIEnv,
         _: JClass,
-        handle: jlong,
-    ) -> jshort {
-        let mut bucket = SERVER_BUCKET.lock().unwrap();
-        let handle = match bucket.map.remove(&handle) {
-            Some(handle) => handle,
-            None => return -1,
-        };
-        match futures::executor::block_on(handle.off_switch.send(())) {
-            Ok(_) => {}
-            Err(_) => return -1,
-        }
-        match handle.thread.join() {
-            Ok(_) => {}
-            Err(_) => return -1,
-        }
-
-        0
+    ) {
+        let ServerBucket { off_switch, thread } = SERVER_BUCKET.lock().unwrap().take().unwrap();
+        futures::executor::block_on(off_switch.send(())).unwrap();
+        thread.join().unwrap();
     }
 }
