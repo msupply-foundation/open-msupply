0cdc1d5df2767032ba66b184eed9d73cd3a11ba8
roxy-dao <roxy@msupply.foundation>
2022-11-16 11:13:56 +1300

    Log stock location change

--- server/service/src/stock_line/update.rs
+++ server/service/src/stock_line/update.rs
@@ -1,7 +1,9 @@
-use chrono::NaiveDate;
+use chrono::{NaiveDate, Utc};
 use repository::{
-    RepositoryError, StockLine, StockLineRow, StockLineRowRepository, StorageConnection,
+    LocationMovementRow, LocationMovementRowRepository, RepositoryError, StockLine, StockLineRow,
+    StockLineRowRepository, StorageConnection,
 };
+use util::uuid::uuid;
 
 use crate::{
     service_provider::ServiceContext,
@@ -41,8 +43,15 @@ pub fn update_stock_line(
         .connection
         .transaction_sync(|connection| {
             let existing = validate(connection, &ctx.store_id, &input)?;
-            let new_stock_line = generate(existing, input);
+            let (new_stock_line, location_movements) =
+                generate(ctx.store_id.clone(), existing, input)?;
             StockLineRowRepository::new(&connection).upsert_one(&new_stock_line)?;
+
+            if let Some(location_movements) = location_movements {
+                for movement in location_movements {
+                    LocationMovementRowRepository::new(connection).upsert_one(&movement)?;
+                }
+            }
 
             get_stock_line(ctx, new_stock_line.id).map_err(|error| match error {
                 SingleRecordError::DatabaseError(error) => DatabaseError(error),
@@ -75,6 +84,7 @@ fn validate(
 }
 
 fn generate(
+    store_id: String,
     mut existing: StockLineRow,
     UpdateStockLine {
         id: _,
@@ -85,14 +95,38 @@ fn generate(
         batch,
         on_hold,
     }: UpdateStockLine,
-) -> StockLineRow {
-    existing.location_id = location_id.or(existing.location_id);
+) -> Result<(StockLineRow, Option<Vec<LocationMovementRow>>), UpdateStockLineError> {
+    existing.location_id = location_id.clone().or(existing.location_id);
     existing.batch = batch.or(existing.batch);
     existing.cost_price_per_pack = cost_price_per_pack.unwrap_or(existing.cost_price_per_pack);
     existing.sell_price_per_pack = sell_price_per_pack.unwrap_or(existing.sell_price_per_pack);
     existing.expiry_date = expiry_date.or(existing.expiry_date);
     existing.on_hold = on_hold.unwrap_or(existing.on_hold);
-    existing
+
+    let location_movements = if location_id != existing.location_id {
+        Some(vec![
+            LocationMovementRow {
+                id: uuid(),
+                store_id: store_id.clone(),
+                stock_line_id: existing.id.clone(),
+                location_id: existing.location_id.clone(),
+                enter_datetime: None,
+                exit_datetime: Some(Utc::now().naive_utc()),
+            },
+            LocationMovementRow {
+                id: uuid(),
+                store_id: store_id,
+                stock_line_id: existing.id.clone(),
+                location_id,
+                enter_datetime: Some(Utc::now().naive_utc()),
+                exit_datetime: None,
+            },
+        ])
+    } else {
+        None
+    };
+
+    Ok((existing, location_movements))
 }
 
 impl From<RepositoryError> for UpdateStockLineError {
