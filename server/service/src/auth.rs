4780029f91d4b493b2b46cdbf217f642b0a7aee0
roxy-dao <roxy@msupply.foundation>
2022-11-24 07:56:51 +1300

    Graphql endpoint for response requisition modal
    Source: https://github.com/msupply-foundation/open-msupply/commit/86c9c46671134aa72ffdef645dcc300a4e263fc7

--- server/service/src/auth.rs
+++ server/service/src/auth.rs
@@ -47,6 +47,7 @@ pub enum Resource {
     QueryRequisition,
     MutateRequisition,
     RequisitionChart,
+    RequisitionStats,
     // stock take line
     InsertStocktakeLine,
     UpdateStocktakeLine,
@@ -179,7 +180,13 @@ fn all_permissions() -> HashMap<Resource, PermissionDSL> {
             PermissionDSL::HasPermission(Permission::RequisitionQuery),
         ]),
     );
-
+    map.insert(
+        Resource::RequisitionStats,
+        PermissionDSL::And(vec![
+            PermissionDSL::HasStoreAccess,
+            PermissionDSL::HasPermission(Permission::RequisitionQuery),
+        ]),
+    );
     // invoice
     map.insert(
         Resource::QueryInvoice,
