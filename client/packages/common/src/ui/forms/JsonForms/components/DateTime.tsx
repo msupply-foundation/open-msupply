54fd74a05759374499bdc14d4fd704f1b305ff57
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2022-11-30 10:31:17 +1300

    Implement "Date only" option for "DateTime" widget

--- client/packages/common/src/ui/forms/JsonForms/components/DateTime.tsx
+++ client/packages/common/src/ui/forms/JsonForms/components/DateTime.tsx
@@ -7,7 +7,11 @@ import {
   TextFieldProps,
   StandardTextFieldProps,
 } from '@mui/material';
-import { BasicTextInput } from '@openmsupply-client/common';
+import {
+  BasicTextInput,
+  BaseDatePickerInput,
+  useFormatDateTime,
+} from '@openmsupply-client/common';
 import {
   FORM_LABEL_COLUMN_WIDTH,
   FORM_INPUT_COLUMN_WIDTH,
@@ -45,10 +49,16 @@ export const datetimeTester = rankWith(5, isDateTimeControl);
 
 const UIComponent = (props: ControlProps) => {
   const [error, setError] = React.useState('');
-  const { data, handleChange, label, path } = props;
+  const { data, handleChange, label, path, uischema } = props;
   if (!props.visible) {
     return null;
   }
+  const dateFormatter = useFormatDateTime().customDate;
+  if (!props.visible) {
+    return null;
+  }
+
+  const dateOnly = uischema.options?.['dateOnly'] ?? false;
 
   return (
     <Box
@@ -63,22 +73,39 @@ const UIComponent = (props: ControlProps) => {
         <FormLabel sx={{ fontWeight: 'bold' }}>{label}:</FormLabel>
       </Box>
       <Box flexBasis={FORM_INPUT_COLUMN_WIDTH}>
-        <BaseDateTimePickerInput
-          // undefined is displayed as "now" and null as unset
-          value={data ?? null}
-          onChange={e => {
-            try {
-              setError('');
-              if (e) handleChange(path, e.toISOString());
-            } catch (err) {
-              setError((err as Error).message);
-              console.error(err);
-            }
-          }}
-          inputFormat="dd/MM/yyyy hh:mm"
-          error={error || props.errors}
-          // readOnly={!!props.uischema.options?.['readonly']}
-        />
+        {!dateOnly ? (
+          <BaseDateTimePickerInput
+            // undefined is displayed as "now" and null as unset
+            value={data ?? null}
+            onChange={e => {
+              try {
+                setError('');
+                if (e) handleChange(path, e.toISOString());
+              } catch (err) {
+                setError((err as Error).message);
+                console.error(err);
+              }
+            }}
+            inputFormat="dd/MM/yyyy hh:mm"
+            error={error || props.errors}
+            // readOnly={!!props.uischema.options?.['readonly']}
+          />
+        ) : (
+          <BaseDatePickerInput
+            // undefined is displayed as "now" and null as unset
+            value={data ?? null}
+            onChange={e => {
+              if (e)
+                handleChange(
+                  path,
+                  // By default, will set current date-time. However, if a date
+                  // is selected, the time will be considered "midnight"
+                  dateFormatter(e, 'yyyy-MM-dd') + ' 00:00:00'
+                );
+            }}
+            inputFormat="dd/MM/yyyy"
+          />
+        )}
       </Box>
     </Box>
   );
