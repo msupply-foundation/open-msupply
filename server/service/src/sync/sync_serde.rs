de699fff146b5c84d6d953577654d1a28e78b5b8
Chris Petty <chris@sussol.net>
2022-11-30 17:04:16 +1300

    create `empty_str_as_option_datetime`

--- server/service/src/sync/sync_serde.rs
+++ server/service/src/sync/sync_serde.rs
@@ -1,9 +1,23 @@
 use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{
+    de::{value::StrDeserializer, IntoDeserializer},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 
 pub fn empty_str_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<String>, D::Error> {
     let s: Option<String> = Option::deserialize(d)?;
     Ok(s.filter(|s| !s.is_empty()))
+}
+
+pub fn empty_str_as_option_datetime<'de, D: Deserializer<'de>>(
+    d: D,
+) -> Result<Option<NaiveDateTime>, D::Error> {
+    let s: Option<String> = empty_str_as_option(d)?;
+
+    let Some(s) = s else { return Ok(None)};
+
+    let str_d: StrDeserializer<D::Error> = s.as_str().into_deserializer();
+    Ok(Some(NaiveDateTime::deserialize(str_d)?))
 }
 
 pub fn zero_date_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<NaiveDate>, D::Error> {
