63f509e2919831f8676c5d73090a141f4f66a97c
Mark Prins <mark@malcolmprins.net>
2022-11-30 19:47:33 +1300

    desktop barcode scanning
    Source: https://github.com/msupply-foundation/open-msupply/commit/b0312ce10e1c25f72215f6d5f7f86dd1bae60b60

--- client/packages/electron/src/shared.ts
+++ client/packages/electron/src/shared.ts
@@ -3,4 +3,7 @@ export const IPC_MESSAGES = {
   START_SERVER_DISCOVERY: 'start-server-discovery',
   CONNECT_TO_SERVER: 'connect-to-server',
   CONNECTED_SERVER: 'connected-server',
+  START_BARCODE_SCAN: 'start-barcode-scan',
+  STOP_BARCODE_SCAN: 'stop-barcode-scan',
+  BARCODE_SCANNED: 'barcode-scanned',
 };
