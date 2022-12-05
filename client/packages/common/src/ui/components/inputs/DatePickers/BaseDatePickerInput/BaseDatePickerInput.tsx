6df51d35d2d872ee0f96fda171b66972ad975286
roxy-dao <roxy@msupply.foundation>
2022-12-06 12:53:05 +1300

    Fix MUI css overwrite
    Source: https://github.com/msupply-foundation/open-msupply/commit/43459ee8dbaf4d772b1f0ab08eb9b04d862e9c2a

--- client/packages/common/src/ui/components/inputs/DatePickers/BaseDatePickerInput/BaseDatePickerInput.tsx
+++ client/packages/common/src/ui/components/inputs/DatePickers/BaseDatePickerInput/BaseDatePickerInput.tsx
@@ -2,36 +2,42 @@ import React, { FC } from 'react';
 import DatePicker, { DatePickerProps } from '@mui/lab/DatePicker';
 import { BasicTextInput } from '../../TextInput/BasicTextInput';
 import { StandardTextFieldProps, TextFieldProps } from '@mui/material';
+import { useAppTheme } from '@common/styles';
 
 export const BaseDatePickerInput: FC<
   Omit<DatePickerProps<Date>, 'renderInput'>
 > = props => {
+  const theme = useAppTheme();
+  
   return (
     <DatePicker
       disabled={props.disabled}
       PopperProps={{
         sx: {
           '& .MuiTypography-root.Mui-selected': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}`,
+          },
+          '& .MuiTypography-root.Mui-selected:hover': {
+            backgroundColor: `${theme.palette.secondary.main}`,
           },
           '& .Mui-selected:focus': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}`,
           },
           '& .MuiPickersDay-root.Mui-selected': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}`,
           },
         },
       }}
       PaperProps={{
         sx: {
           '& .Mui-selected': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}!important`,
           },
           '& .Mui-selected:focus': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}`,
           },
           '& .Mui-selected:hover': {
-            backgroundColor: 'secondary.main',
+            backgroundColor: `${theme.palette.secondary.main}`,
           },
         },
       }}
