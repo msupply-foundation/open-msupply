2a43d12d1470439d68456c7090fcdcedcb5db606
roxy-dao <roxy@msupply.foundation>
2022-12-05 15:11:15 +1300

    Don't include id in class name + fix text being hidden by background colour

--- client/packages/common/src/ui/forms/JsonForms/components/Array.tsx
+++ client/packages/common/src/ui/forms/JsonForms/components/Array.tsx
@@ -114,24 +114,38 @@ const EnumArrayComponent: FC<EnumArrayControlCustomProps> = ({
           <Autocomplete
             multiple
             sx={{
-              '.rtl-w7osp1-MuiInputBase-root-MuiInput-root:before': {
-                border: 'none',
+              '& .MuiInput-root': {
                 borderRadius: '8px',
                 height: '100%',
-                backgroundColor: theme.palette.background.drawer,
-             },
-              '.rtl-1fjcdrw-MuiButtonBase-root-MuiChip-root': {
+                backgroundColor: theme.palette.background.menu,
+                padding: '5px',
+              },
+              '& .MuiInput-root:before': {
+                border: 'none'
+              },
+              '& .MuiInput-root:after': {
+                color: theme.palette.gray.dark,
+              },
+              '& .MuiInput-root:focus:before': {
+                borderRadius: '8px 8px 0px 0px',
+              },
+              '& .MuiInput-root:hover:before': {
+                borderRadius: '8px 8px 0px 0px',
+              },
+              '& .MuiChip-root': {
                 backgroundColor: theme.palette.secondary.light,
                 height: 'inherit',
                 color: theme.typography.login.color,
               },
-              '.rtl-1fjcdrw-MuiButtonBase-root-MuiChip-root .MuiChip-deleteIcon': {
-                color: theme.palette.background.white,
+              '& .MuiChip-deleteIcon': {
+                color: `${theme.palette.background.white} !important`,
               },
             }}
             value={data}
             options={schema.enum ?? []}
-            renderInput={params => <TextField {...params} variant="standard" />}
+            renderInput={params => (
+              <TextField {...params} variant="standard" color="secondary" />
+            )}
             onChange={(_, value) => {
               if (value.length - 1 === data.length) {
                 addItem(path, value[value.length - 1])();
