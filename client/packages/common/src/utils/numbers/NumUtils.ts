4783b6322af78b8f1a37da19479d43f6059085e9
Clemens Zeidler <clemens@msupply.foundation>
2022-12-15 10:05:50 +1300

    Define some constants in NumUtils

--- client/packages/common/src/utils/numbers/NumUtils.ts
+++ client/packages/common/src/utils/numbers/NumUtils.ts
@@ -1,6 +1,11 @@
 const constrain = (value: number, min: number, max: number) =>
   Math.min(Math.max(value, min), max);
 
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
 export const NumUtils = {
   constrain,
   isPositive: (num: number): boolean => {
@@ -12,4 +17,19 @@ export const NumUtils = {
 
     return constrain(parsed, min, max);
   },
+  /**
+   * This constant should be used for values that are potentially send to a backend API that expects
+   * an unsigned 32 bit integer and thus would reject Number.MAX_SAFE_INTEGER.
+   * For example, JS number max size is `2^53 - 1` while the Rust u32 size is `2^32 - 1`.
+   */
+  MAX_SAFE_API_INTEGER: 999999999,
+
+  /** Number of milliseconds in one second, i.e. SECOND = 1000*/
+  SECOND,
+  /** Number of milliseconds in one minute */
+  MINUTE,
+  /** Number of milliseconds in one hour */
+  HOUR,
+  /** Number of milliseconds in one day */
+  DAY,
 };
