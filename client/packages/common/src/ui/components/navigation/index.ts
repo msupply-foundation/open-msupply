d54864e9357ec5439750ddc04f70f17beabf2113
Mark Prins <mark@malcolmprins.net>
2022-12-21 11:58:31 +1300

    highlight app nav sections

--- client/packages/common/src/ui/components/navigation/index.ts
+++ client/packages/common/src/ui/components/navigation/index.ts
@@ -1,4 +1,5 @@
-export * from './Breadcrumbs';
 export * from './AppNavLink';
+export * from './AppNavSection';
+export * from './Breadcrumbs';
 export * from './ExternalNavLink';
 export * from './Tabs';
