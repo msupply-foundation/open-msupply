e850acafa47a20805e1cdc8959e77c6dd60a2d0a
Andrei <andrei.evguenov@gmail.com>
2023-01-16 14:24:54 +1300

    Data migrations for two inventory adjustments in stocktake
    Source: https://github.com/msupply-foundation/open-msupply/commit/9d8706a923b291c16570e7d9c788e2adf8f4cb72

--- server/repository/src/migrations/v1_01_01/mod.rs
+++ server/repository/src/migrations/v1_01_01/mod.rs
@@ -1,8 +1,9 @@
 mod create_indexes;
 #[cfg(not(feature = "postgres"))]
 mod remove_sqlite_check;
+mod split_inventory_adjustment;
 
-use super::{version::Version, Migration};
+use super::{sql, version::Version, Migration};
 use crate::StorageConnection;
 
 pub(crate) struct V1_01_01;
@@ -13,11 +14,11 @@ impl Migration for V1_01_01 {
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
-        // Remove Check
         #[cfg(not(feature = "postgres"))]
         remove_sqlite_check::migrate(connection)?;
 
-        // Indexes
+        split_inventory_adjustment::migrate(connection)?;
+
         create_indexes::migrate(connection)?;
 
         // Remove self-referencing name_store_joins
@@ -30,6 +31,42 @@ impl Migration for V1_01_01 {
 
         Ok(())
     }
+}
+
+#[cfg(test)]
+async fn setup_data_migration(name: &str) -> StorageConnection {
+    use crate::{migrations::templates::add_data_from_sync_buffer::V1_00_08, test_db::*};
+
+    // Migrate to version - 1
+    let SetupResult { connection, .. } = setup_test(SetupOption {
+        db_name: name,
+        version: Some(V1_00_08.version()),
+        ..Default::default()
+    })
+    .await;
+    // Common data
+    sql!(
+        &connection,
+        r#"
+        INSERT INTO name 
+        (id, type, is_customer, is_supplier, code, name) 
+        VALUES 
+        ('name_id', 'STORE', false, false, '', '');
+    "#
+    )
+    .unwrap();
+
+    sql!(
+        &connection,
+        r#"
+        INSERT INTO store 
+        (id, name_id, site_id, code) 
+        VALUES 
+        ('store_id', 'name_id', 1, '');
+    "#
+    )
+    .unwrap();
+    connection
 }
 
 #[cfg(test)]
