7b72c800f412b0e85c1a580df4f5c19b9cbc4090
Clemens Zeidler <clemens@msupply.foundation>
2023-01-17 10:47:43 +1300

    Add an ReportArgumentsModel for reports with an argument schema

---
+++ client/packages/system/src/Report/components/ReportArgumentsModal.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState } from 'react';
+
+import { JsonData, JsonForm } from '@openmsupply-client/programs';
+import { ReportRowFragment } from '../api';
+import { useDialog } from '@common/hooks';
+import { DialogButton } from '@common/components';
+
+export type ReportArgumentsModalProps = {
+  /** Modal is shown if there is an argument schema present */
+  report: ReportRowFragment | undefined;
+  onReset: () => void;
+  onArgumentsSelected: (report: ReportRowFragment, args: JsonData) => void;
+};
+
+export const ReportArgumentsModal: FC<ReportArgumentsModalProps> = ({
+  report,
+  onReset,
+  onArgumentsSelected,
+}) => {
+  const [data, setData] = useState<JsonData>({});
+  const [error, setError] = useState<string | false>(false);
+
+  // clean up when modal is closed
+  const cleanUp = () => {
+    setData({});
+    onReset();
+  };
+
+  const { Modal } = useDialog({
+    isOpen: !!report?.argumentSchema,
+    onClose: cleanUp,
+  });
+
+  if (!report?.argumentSchema) {
+    return null;
+  }
+
+  return (
+    <Modal
+      title="Report arguments"
+      cancelButton={<DialogButton variant="cancel" onClick={cleanUp} />}
+      okButton={
+        <DialogButton
+          variant="ok"
+          disabled={!!error}
+          onClick={async () => {
+            onArgumentsSelected(report, data);
+            cleanUp();
+          }}
+        />
+      }
+    >
+      <JsonForm
+        data={data}
+        jsonSchema={report.argumentSchema.jsonSchema}
+        uiSchema={report.argumentSchema.uiSchema}
+        isError={false}
+        isLoading={false}
+        setError={err => setError(err)}
+        updateData={(newData: JsonData) => {
+          setData(newData);
+        }}
+      />
+    </Modal>
+  );
+};
--- client/packages/system/src/Report/components/ReportSelector.tsx
+++ client/packages/system/src/Report/components/ReportSelector.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, useCallback, useState } from 'react';
 import { Box, ReportContext, Typography } from '@openmsupply-client/common';
 import { AlertIcon } from '@common/icons';
 import { useTranslation } from '@common/intl';
@@ -9,10 +9,13 @@ import {
   usePaperClickPopover,
 } from '@common/components';
 import { ReportRowFragment, useReport } from '../api';
+import { ReportArgumentsModal } from './ReportArgumentsModal';
+import { JsonData } from 'packages/programs/src';
 
 interface ReportSelectorProps {
   context?: ReportContext;
-  onClick: (report: ReportRowFragment) => void;
+  context2?: string;
+  onClick: (report: ReportRowFragment, args: JsonData | undefined) => void;
 }
 
 const NoReports = ({ hasPermission }: { hasPermission: boolean }) => {
@@ -35,19 +38,36 @@ const NoReports = ({ hasPermission }: { hasPermission: boolean }) => {
 
 export const ReportSelector: FC<PropsWithChildren<ReportSelectorProps>> = ({
   context,
+  context2,
   children,
   onClick,
 }) => {
   const { hide, PaperClickPopover } = usePaperClickPopover();
-  const { data, isLoading } = useReport.document.list(context);
+  const { data, isLoading } = useReport.document.list(context, context2);
   const t = useTranslation('app');
+  const [reportWithArgs, setReportWithArgs] = useState<
+    ReportRowFragment | undefined
+  >();
+  const onReportSelected = useCallback(
+    (report: ReportRowFragment | undefined) => {
+      if (report === undefined) {
+        return;
+      }
+      if (report.argumentSchema) {
+        setReportWithArgs(report);
+      } else {
+        onClick(report, undefined);
+      }
+    },
+    []
+  );
 
   const reportButtons = data?.nodes?.map(report => (
     <FlatButton
       label={report.name}
       onClick={() => {
         hide();
-        onClick(report);
+        onReportSelected(report);
       }}
       key={report.id}
       sx={{ textAlign: 'left', justifyContent: 'left' }}
@@ -57,42 +77,52 @@ export const ReportSelector: FC<PropsWithChildren<ReportSelectorProps>> = ({
   const hasPermission = !isLoading && data !== undefined;
   const noReports = !isLoading && !data?.nodes?.length;
   const oneReport =
-    !isLoading && data?.nodes?.length === 1 ? data.nodes[0] : null;
+    !isLoading && data?.nodes?.length === 1 ? data.nodes[0] : undefined;
 
-  return !!oneReport ? (
-    <div onClick={() => onClick(oneReport)}>{children}</div>
-  ) : (
-    <PaperClickPopover
-      placement="bottom"
-      width={350}
-      Content={
-        <PaperPopoverSection
-          label={t('select-report')}
-          labelStyle={{ width: '100%' }}
-          alignItems="center"
-        >
-          {isLoading ? (
-            <CircularProgress size={12} />
-          ) : (
-            <Box
-              style={{
-                maxHeight: '200px',
-                overflowY: 'auto',
-              }}
-              display="flex"
-              flexDirection="column"
+  return (
+    <>
+      {!!oneReport ? (
+        <div onClick={() => onReportSelected(oneReport)}>{children}</div>
+      ) : (
+        <PaperClickPopover
+          placement="bottom"
+          width={350}
+          Content={
+            <PaperPopoverSection
+              label={t('select-report')}
+              labelStyle={{ width: '100%' }}
+              alignItems="center"
             >
-              {noReports ? (
-                <NoReports hasPermission={hasPermission} />
+              {isLoading ? (
+                <CircularProgress size={12} />
               ) : (
-                reportButtons
+                <Box
+                  style={{
+                    maxHeight: '200px',
+                    overflowY: 'auto',
+                  }}
+                  display="flex"
+                  flexDirection="column"
+                >
+                  {noReports ? (
+                    <NoReports hasPermission={hasPermission} />
+                  ) : (
+                    reportButtons
+                  )}
+                </Box>
               )}
-            </Box>
-          )}
-        </PaperPopoverSection>
-      }
-    >
-      {children}
-    </PaperClickPopover>
+            </PaperPopoverSection>
+          }
+        >
+          {children}
+        </PaperClickPopover>
+      )}
+
+      <ReportArgumentsModal
+        report={reportWithArgs}
+        onReset={() => setReportWithArgs(undefined)}
+        onArgumentsSelected={(report, args) => onClick(report, args)}
+      ></ReportArgumentsModal>
+    </>
   );
 };
