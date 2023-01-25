0120e46d821041fc5b768efd00178dff7d6f1dd5
roxy-dao <roxy@msupply.foundation>
2023-01-26 12:18:49 +1300

    Query to get inventory adjustment reasons
    Source: https://github.com/msupply-foundation/open-msupply/commit/50e2ced853094a4d0070fa5b1fc898a4466ef4f0

---
+++ client/packages/system/src/InventoryAdjustmentReason/api/hooks/document/useInventoryAdjustmentReason.ts
@@ -0,0 +1,14 @@
+import { SortBy, useQuery } from '@openmsupply-client/common';
+import { InventoryAdjustmentReasonRowFragment } from '../../operations.generated';
+import { useInventoryAdjustmentReasonApi } from '../utils/useInventoryAdjustmentReasonApi';
+
+export const useInventoryAdjustmentReason = (
+  sortBy?: SortBy<InventoryAdjustmentReasonRowFragment>
+) => {
+  const api = useInventoryAdjustmentReasonApi();
+  const result = useQuery(api.keys.sortedList( sortBy), () =>
+    api.get.listAllActive({ sortBy })
+  );
+
+  return { ...result };
+};
