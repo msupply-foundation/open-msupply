6009acbc95e67297d03a436df9608d7c7856fa18
anildahalsussol <anil.dahal@sussol.net>
2023-02-03 14:42:47 +0545

    No need of filtration
    No need of filtration
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/4c87324e53759b2a176f35554f521b87e55d5161

--- server/graphql/types/src/types/store.rs
+++ server/graphql/types/src/types/store.rs
@@ -47,7 +47,9 @@ impl StoreNode {
     pub async fn site_id(&self) -> i32 {
         self.row().site_id
     }
-
+    
+    /// Returns the associated store logo.
+    /// The logo is returned as a data URL schema, e.g. "data:image/png;base64,..."
     pub async fn logo(&self) -> &str {
         &self.row().logo
     }
