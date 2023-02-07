13c9f64e09ff4ae2357b4dac28d2b28d02b1b593
Clemens Zeidler <clemens@msupply.foundation>
2023-02-07 15:29:01 +1300

    Update encounter event FE API

--- client/packages/programs/src/api/operations.generated.ts
+++ client/packages/programs/src/api/operations.generated.ts
@@ -286,7 +286,7 @@ export const EncounterRowFragmentDoc = gql`
     lastName
     name
   }
-  events(at: $eventTime) {
+  events(at: $eventTime, filter: {isCurrentEncounter: true}) {
     ...ProgramEvent
   }
 }
