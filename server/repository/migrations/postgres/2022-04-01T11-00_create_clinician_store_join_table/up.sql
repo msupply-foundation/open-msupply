5274382099cc9acacac961e26ab1ec7b4976dd12
Clemens Zeidler <clemens@msupply.foundation>
2023-02-08 12:04:13 +1300

    Reorder clinician migrations (need to be before the encounter table)
    Source: https://github.com/msupply-foundation/open-msupply/commit/30f455a7648e4c9eb851ac4a7fa435044560a8fe

--- server/repository/migrations/postgres/2022-04-01T11-00_create_clinician_store_join_table/up.sql
+++ server/repository/migrations/postgres/2022-04-01T11-00_create_clinician_store_join_table/up.sql
@@ -1,5 +1,4 @@
-CREATE TABLE clinician_store_join
-(
+CREATE TABLE clinician_store_join (
     id TEXT NOT NULL PRIMARY KEY,
     clinician_id TEXT NOT NULL REFERENCES clinician(id),
     store_id TEXT NOT NULL REFERENCES store(id)
