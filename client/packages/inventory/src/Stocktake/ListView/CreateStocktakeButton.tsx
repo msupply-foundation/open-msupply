8a9950802a81869dc489fbba40068e0cc8d395d8
Mark Prins <mark@malcolmprins.net>
2023-02-15 10:42:01 +1300

    wire up locations
    Source: https://github.com/msupply-foundation/open-msupply/commit/27b028e7ce1b39f9e963fc49877320af04e54911

--- client/packages/inventory/src/Stocktake/ListView/CreateStocktakeButton.tsx
+++ client/packages/inventory/src/Stocktake/ListView/CreateStocktakeButton.tsx
@@ -11,7 +11,7 @@ import { PlusCircleIcon } from '@common/icons';
 import { useFormatDateTime, useTranslation } from '@common/intl';
 import { ToggleState, useDialog } from '@common/hooks';
 import { useStocktake } from '../api';
-import { useMasterList } from '@openmsupply-client/system';
+import { useMasterList, useLocation } from '@openmsupply-client/system';
 import {
   Box,
   FnUtils,
@@ -39,6 +39,11 @@ export const CreateStocktakeButton: React.FC<{
     isLoading: isLoadingMasterLists,
     mutate: fetchMasterLists,
   } = useMasterList.document.listAll({ key: 'name', direction: 'asc' });
+  const {
+    data: locationData,
+    isLoading: isLoadingLocations,
+    mutate: fetchLocations,
+  } = useLocation.document.listAll({ key: 'name', direction: 'asc' });
   const { user } = useAuthContext();
   const { localisedDate } = useFormatDateTime();
   const [createStocktakeArgs, setCreateStocktakeArgs] =
@@ -56,6 +61,10 @@ export const CreateStocktakeButton: React.FC<{
         createStocktakeArgs.masterListId === 'undefined'
           ? undefined
           : createStocktakeArgs.masterListId,
+      locationId:
+        createStocktakeArgs.locationId === 'undefined'
+          ? undefined
+          : createStocktakeArgs.locationId,
     };
     await mutateAsync(input);
   };
@@ -69,27 +78,47 @@ export const CreateStocktakeButton: React.FC<{
     onClose,
     disableBackdrop: true,
   });
-  const isLoading = isLoadingMasterLists;
+  const isLoading = isLoadingMasterLists || isLoadingLocations;
   const handleMasterListChange: ChangeEventHandler<
     HTMLInputElement | HTMLTextAreaElement
   > = event => {
     setCreateStocktakeArgs({
-      ...createStocktakeArgs,
+      locationId: 'undefined',
       masterListId: event.target.value?.toString(),
+    });
+  };
+  const handleLocationChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = event => {
+    setCreateStocktakeArgs({
+      locationId: event.target.value?.toString(),
+      masterListId: 'undefined',
     });
   };
   const masterLists = [
     { label: t('label.please-select'), value: 'undefined' },
     ...(masterListData
       ? masterListData.nodes.map(list => ({
-          label: list.name,
+          label: `${list.name} (${list.lines.totalCount} ${t('label.item', {
+            count: list.lines.totalCount,
+          })})`,
           value: list.id,
+        }))
+      : []),
+  ];
+  const locations = [
+    { label: t('label.please-select'), value: 'undefined' },
+    ...(locationData
+      ? locationData.nodes.map(location => ({
+          label: location.name,
+          value: location.id,
         }))
       : []),
   ];
 
   useEffect(() => {
     fetchMasterLists();
+    fetchLocations();
   }, []);
 
   return (
@@ -132,10 +161,21 @@ export const CreateStocktakeButton: React.FC<{
                       onChange={handleMasterListChange}
                       options={masterLists}
                       value={createStocktakeArgs.masterListId}
-                      // renderOption={renderOption}
                     />
                   }
                   label={t('label.master-list')}
+                />
+                <Box sx={{ height: 16 }} />
+                <InputWithLabelRow
+                  Input={
+                    <Select
+                      fullWidth
+                      onChange={handleLocationChange}
+                      options={locations}
+                      value={createStocktakeArgs.locationId}
+                    />
+                  }
+                  label={t('label.location')}
                 />
               </Box>
             ) : (
