e5e151ef2765847ffd34a2ee0f663ed28cbe2ce0
Clemens Zeidler <clemens@msupply.foundation>
2023-03-09 14:01:32 +1300

    Add document registry pull translation
    Source: https://github.com/msupply-foundation/open-msupply/commit/3b8bec750014e6fe392a62a9212cd288ebfb4fd4

---
+++ server/service/src/sync/translations/document_registry.rs
@@ -0,0 +1,70 @@
+use repository::{DocumentContext, DocumentRegistryRow, StorageConnection, SyncBufferRow};
+use serde::Deserialize;
+
+use super::{IntegrationRecords, LegacyTableName, PullUpsertRecord, SyncTranslation};
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+enum LegacyDocumentContext {
+    Patient,
+    Program,
+    Encounter,
+    Custom,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+struct LegacyDocumentRegistryRow {
+    pub id: String,
+    pub document_type: String,
+    pub context: LegacyDocumentContext,
+    pub name: Option<String>,
+    pub parent_id: Option<String>,
+    pub form_schema_id: Option<String>,
+    pub config: Option<String>,
+}
+
+fn match_pull_table(sync_record: &SyncBufferRow) -> bool {
+    sync_record.table_name == LegacyTableName::DOCUMENT_REGISTRY
+}
+
+pub(crate) struct DocumentRegistryTranslation {}
+impl SyncTranslation for DocumentRegistryTranslation {
+    fn try_translate_pull_upsert(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<Option<IntegrationRecords>, anyhow::Error> {
+        if !match_pull_table(sync_record) {
+            return Ok(None);
+        }
+        let LegacyDocumentRegistryRow {
+            id,
+            document_type,
+            context,
+            name,
+            parent_id,
+            form_schema_id,
+            config,
+        } = serde_json::from_str::<LegacyDocumentRegistryRow>(&sync_record.data)?;
+
+        let result = DocumentRegistryRow {
+            id,
+            document_type,
+            context: match context {
+                LegacyDocumentContext::Patient => DocumentContext::Patient,
+                LegacyDocumentContext::Program => DocumentContext::Program,
+                LegacyDocumentContext::Encounter => DocumentContext::Encounter,
+                LegacyDocumentContext::Custom => DocumentContext::Custom,
+            },
+            name,
+            parent_id,
+            form_schema_id,
+            config,
+        };
+
+        Ok(Some(IntegrationRecords::from_upsert(
+            PullUpsertRecord::DocumentRegistry(result),
+        )))
+    }
+}
