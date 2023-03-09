e206866d45d9ec54b8a31a64344c6d96a6ef7af6
roxy-dao <roxy@msupply.foundation>
2023-03-09 13:49:09 +1300

    Remove name store join delete trigger
    Source: https://github.com/msupply-foundation/open-msupply/commit/c28e34f4680af30251bd982be7ec3e80c67cb5df

--- server/repository/migrations/sqlite/2023-03-05-223004_create_name_store_join_trigger/up.sql
+++ server/repository/migrations/sqlite/2023-03-05-223004_create_name_store_join_trigger/up.sql
@@ -11,10 +11,3 @@ CREATE TRIGGER name_store_join_update_trigger
     INSERT INTO changelog (table_name, record_id, row_action, store_id)
       VALUES ('name_store_join', NEW.id, 'UPSERT', NEW.store_id);
   END;
-
-CREATE TRIGGER name_store_join_delete_trigger
-  AFTER DELETE ON name_store_join
-  BEGIN
-    INSERT INTO changelog (table_name, record_id, row_action, store_id)
-      VALUES ('name_store_join', OLD.id, 'DELETE', OLD.store_id);
-  END;
