8ef1d67f474cb1f48eacadef82f9a3bd58d3adcd
Mark Prins <mark@malcolmprins.net>
2023-03-10 16:20:56 +1300

    no refresh on syncInfo
    Source: https://github.com/msupply-foundation/open-msupply/commit/a001e1dc85546e8c4ae6c699fac484b4455ac701

--- client/packages/common/src/authentication/api/hooks/useRefreshToken.ts
+++ client/packages/common/src/authentication/api/hooks/useRefreshToken.ts
@@ -2,26 +2,30 @@ import {
   DateUtils,
   getAuthCookie,
   setAuthCookie,
+  useGql,
 } from '@openmsupply-client/common';
 import { useGetRefreshToken } from './useGetRefreshToken';
 import { DefinitionNode, DocumentNode, OperationDefinitionNode } from 'graphql';
 
-const isRefreshRequest = (definitionNode: DefinitionNode) => {
+const ignoredQueries = ['refreshToken', 'syncInfo'];
+
+const shouldIgnoreQuery = (definitionNode: DefinitionNode) => {
   const operationNode = definitionNode as OperationDefinitionNode;
   if (!operationNode) return false;
   if (operationNode.operation !== 'query') return false;
 
-  return operationNode.name?.value === 'refreshToken';
+  return ignoredQueries.indexOf(operationNode.name?.value ?? '') !== -1;
 };
 
 export const useRefreshToken = () => {
   const { mutateAsync } = useGetRefreshToken();
+  const { setHeader } = useGql();
 
   const refreshToken = (documentNode?: DocumentNode) => {
     if (!documentNode) return;
 
     // prevent an infinite loop - don't request a refresh token if we are currently requesting one
-    if (documentNode.definitions.some(isRefreshRequest)) return;
+    if (documentNode.definitions.some(shouldIgnoreQuery)) return;
 
     const authCookie = getAuthCookie();
     // authCookie.expires reports as Date but is actually a string
@@ -35,8 +39,10 @@ export const useRefreshToken = () => {
 
     if (expiresIn === 1) {
       mutateAsync().then(data => {
-        const newCookie = { ...authCookie, token: data?.token ?? '' };
+        const token = data?.token ?? '';
+        const newCookie = { ...authCookie, token };
         setAuthCookie(newCookie);
+        setHeader('Authorization', `Bearer ${token}`);
       });
     }
   };
