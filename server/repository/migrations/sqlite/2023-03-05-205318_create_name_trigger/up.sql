a668af8bde86b699d7000678ec28188442953a39
roxy-dao <roxy@msupply.foundation>
2023-03-14 10:47:10 +1300

    Remove delete trigger from name

--- server/repository/migrations/sqlite/2023-03-05-205318_create_name_trigger/up.sql
+++ server/repository/migrations/sqlite/2023-03-05-205318_create_name_trigger/up.sql
@@ -11,10 +11,3 @@ CREATE TRIGGER name_update_trigger
     INSERT INTO changelog (table_name, record_id, row_action)
       VALUES ('name', NEW.id, 'UPSERT');
   END;
-
-CREATE TRIGGER name_delete_trigger
-  AFTER DELETE ON name
-  BEGIN
-    INSERT INTO changelog (table_name, record_id, row_action)
-      VALUES ('name', OLD.id, 'DELETE');
-  END;
