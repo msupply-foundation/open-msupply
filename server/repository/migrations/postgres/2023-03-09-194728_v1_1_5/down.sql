8bafcbc7c3f660d530567a45c3b0bf62bf59902c
roxy-dao <roxy@msupply.foundation>
2023-03-16 10:03:05 +1300

    Fix postgres tests
    Source: https://github.com/msupply-foundation/open-msupply/commit/e8ab17322acb36e12bb85c784fbd265717528968

--- server/repository/migrations/postgres/2023-03-09-194728_v1_1_5/down.sql
+++ server/repository/migrations/postgres/2023-03-09-194728_v1_1_5/down.sql
@@ -3,4 +3,3 @@ ALTER TABLE changelog DROP COLUMN is_sync_update;
 ALTER TABLE clinician DROP COLUMN is_sync_update;
 ALTER TABLE clinician_store_join DROP COLUMN is_sync_update;
 ALTER TABLE name_store_join DROP COLUMN is_sync_update;
-ALTER TABLE document DROP COLUMN is_sync_update;
--- server/repository/migrations/postgres/2023-03-09-194728_v1_1_5/up.sql
+++ server/repository/migrations/postgres/2023-03-09-194728_v1_1_5/up.sql
@@ -3,7 +3,6 @@ ALTER TABLE changelog ADD is_sync_update BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE clinician ADD is_sync_update BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE clinician_store_join ADD is_sync_update BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE name_store_join ADD is_sync_update BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE document ADD is_sync_update BOOLEAN NOT NULL DEFAULT FALSE;
 
 /* View wasn't being dropped in down.sql, so have moved it here to recreate view */
 DROP VIEW IF EXISTS changelog_deduped;
@@ -20,24 +19,3 @@ CREATE VIEW changelog_deduped AS
                     from changelog t2
                     where t2.record_id = t1.record_id)
     ORDER BY t1.cursor;
-
-CREATE OR REPLACE FUNCTION update_changelog()
-RETURNS trigger AS
-$$
-     DECLARE
-     BEGIN
-        IF (TG_OP = 'DELETE') THEN
-            INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
-              VALUES (TG_TABLE_NAME::changelog_table_name, OLD.id, 'DELETE', OLD.is_sync_update);
-            RETURN OLD;
-        ELSIF (TG_OP = 'UPDATE') THEN
-            INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
-              VALUES (TG_TABLE_NAME::changelog_table_name, NEW.id, 'UPSERT', NEW.is_sync_update);
-            RETURN NEW;
-        ELSIF (TG_OP = 'INSERT') THEN
-            INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
-              VALUES (TG_TABLE_NAME::changelog_table_name, NEW.id, 'UPSERT', NEW.is_sync_update);
-            RETURN NEW;
-        END IF;
-     END;
-$$ LANGUAGE 'plpgsql';
