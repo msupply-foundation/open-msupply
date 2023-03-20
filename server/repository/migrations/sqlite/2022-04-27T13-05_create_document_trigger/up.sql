45eee0e5c5815d0cdfd3de5f3bbcebd9591a2130
Clemens Zeidler <clemens@msupply.foundation>
2023-03-20 16:03:15 +1300

    Add document is_sync_update to sqlite triggers
    Source: https://github.com/msupply-foundation/open-msupply/commit/aebb77315178db8b58736c5642781131d851bc61

--- server/repository/migrations/sqlite/2022-04-27T13-05_create_document_trigger/up.sql
+++ server/repository/migrations/sqlite/2022-04-27T13-05_create_document_trigger/up.sql
@@ -1,15 +1,15 @@
 CREATE TRIGGER document_insert_trigger
   AFTER INSERT ON document
   BEGIN
-    INSERT INTO changelog (table_name, record_id, row_action)
-      VALUES ('document', NEW.id, 'UPSERT');
+    INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
+      VALUES ('document', NEW.id, 'UPSERT', NEW.is_sync_update);
   END;
 
 CREATE TRIGGER document_update_trigger
   AFTER UPDATE ON document
   BEGIN
-    INSERT INTO changelog (table_name, record_id, row_action)
-      VALUES ('document', NEW.id, 'UPSERT');
+    INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
+      VALUES ('document', NEW.id, 'UPSERT', NEW.is_sync_update);
   END;
 
 CREATE TRIGGER document_delete_trigger
