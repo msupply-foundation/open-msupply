05aaa1bf5f1bf90b4cabb6538a4168a131a6172d
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-03-21 12:00:02 +1300

    Allow DatePicker to handle manual entry and show validation
    Source: https://github.com/msupply-foundation/open-msupply/commit/719b1b3da13c4b6af5d2ed2c36ad9c701e45ca7d

--- client/packages/common/src/ui/components/inputs/DatePickers/BaseDatePickerInput/BaseDatePickerInput.tsx
+++ client/packages/common/src/ui/components/inputs/DatePickers/BaseDatePickerInput/BaseDatePickerInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
   DatePicker,
   DatePickerProps,
@@ -6,11 +6,37 @@ import {
 import { BasicTextInput } from '../../TextInput/BasicTextInput';
 import { StandardTextFieldProps, TextFieldProps } from '@mui/material';
 import { useAppTheme } from '@common/styles';
+import { DateUtils } from '@common/intl';
+import { useDebounceCallback } from '@common/hooks';
 
 export const BaseDatePickerInput: FC<
-  Omit<DatePickerProps<Date, Date>, 'renderInput'>
+  Omit<DatePickerProps<Date, Date>, 'renderInput' | 'value'> & {
+    value: Date | string | null;
+  }
 > = props => {
   const theme = useAppTheme();
+  const [internalValue, setInternalValue] = useState<Date | null>(null);
+
+  useEffect(() => {
+    // This sets the internal state from parent when first loading (i.e. when
+    // the internal date is still empty)
+    if (props.value && internalValue === null)
+      setInternalValue(DateUtils.getDateOrNull(props.value));
+  }, [props.value]);
+
+  const isInvalid = (value: Date | string | number | null | undefined) => {
+    const dateValue =
+      typeof value === 'string' ? DateUtils.getDateOrNull(value) : value;
+    return !!value && !DateUtils.isValid(dateValue);
+  };
+
+  const debouncedOnChange = useDebounceCallback(
+    value => {
+      // Only run the parent onChange method when the internal date is valid
+      if (DateUtils.isValid(value)) props.onChange(value);
+    },
+    [props.onChange]
+  );
 
   return (
     <DatePicker
@@ -50,10 +76,19 @@ export const BaseDatePickerInput: FC<
           variant: 'standard',
         };
         return (
-          <BasicTextInput disabled={!!props.disabled} {...textInputProps} />
+          <BasicTextInput
+            disabled={!!props.disabled}
+            {...textInputProps}
+            error={isInvalid(internalValue)}
+          />
         );
       }}
       {...props}
+      onChange={d => {
+        setInternalValue(d);
+        debouncedOnChange(d);
+      }}
+      value={internalValue}
     />
   );
 };
