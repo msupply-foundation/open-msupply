a19aa82eca5d0ff42920ab96fce298196f236c21
roxy-dao <roxy@msupply.foundation>
2023-03-27 10:37:31 +1300

    In progress permission integration test
    Source: https://github.com/msupply-foundation/open-msupply/commit/1ccd5df37c4574e9ac5e4d82df29932c71d1ce3c

--- server/service/src/sync/test/integration/central/user_permission.rs
+++ server/service/src/sync/test/integration/central/user_permission.rs
@@ -5,7 +5,7 @@ use crate::sync::{
     translations::{IntegrationRecords, PullDeleteRecord, PullDeleteRecordTable, PullUpsertRecord},
 };
 
-use repository::{Permission, UserPermissionRow};
+use repository::{Language, Permission, UserAccountRow, UserPermissionRow};
 use serde_json::json;
 use util::uuid::uuid;
 
@@ -17,9 +17,37 @@ impl SyncRecordTester for UserPermissionTester {
         // STEP 1 - insert
         let store_id = new_site_properties.store_id.clone();
 
+        let user = UserAccountRow {
+            id: "1234-56789-0000".to_string(),
+            username: "test_user".to_string(),
+            hashed_password: "d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1"
+                .to_string(),
+            email: None,
+            language: Language::English,
+        };
+
+        let user_json = json!({
+            "ID": user.id,
+            "name": user.username,
+            "active": true,
+            "password": "pass",
+            "date_created": "2023-03-24",
+            "type": {
+                "types": "desktop"
+            }
+        });
+
+        let user_store_json = json!({
+            "ID": uuid(),
+            "user_ID": user.id,
+            "store_ID": store_id,
+            "can_login": true,
+            "store_default": false
+        });
+
         let user_permission_row_1 = UserPermissionRow {
             id: uuid(),
-            user_id: "user_account_a".to_string(),
+            user_id: user.id.clone(),
             store_id: Some(store_id.clone()),
             permission: Permission::DocumentMutate,
             context: Some("some program".to_string()),
@@ -29,12 +57,12 @@ impl SyncRecordTester for UserPermissionTester {
             "user_ID": user_permission_row_1.user_id,
             "store_ID": user_permission_row_1.store_id,
             "permission": "DocumentMutate",
-            "context_ID": user_permission_row_1.context,
+            "context_ID": user_permission_row_1.context
         });
 
         let user_permission_row_2 = UserPermissionRow {
             id: uuid(),
-            user_id: "user_account_a".to_string(),
+            user_id: user.id.clone(),
             store_id: Some(store_id),
             permission: Permission::DocumentQuery,
             context: Some("some program".to_string()),
@@ -44,12 +72,14 @@ impl SyncRecordTester for UserPermissionTester {
             "user_ID": user_permission_row_2.user_id,
             "store_ID": user_permission_row_2.store_id,
             "permission": "DocumentQuery",
-            "context_ID": user_permission_row_2.context,
+            "context_ID": user_permission_row_2.context
         });
 
         result.push(TestStepData {
             central_upsert: json!({
-                "user_permission": [user_permission_row_1_json, user_permission_row_2_json],
+                "user": [user_json],
+                "user_store": [user_store_json],
+                "om_user_permission": [user_permission_row_1_json, user_permission_row_2_json],
             }),
             central_delete: json!({}),
             integration_records: IntegrationRecords::from_upserts(vec![
@@ -59,16 +89,16 @@ impl SyncRecordTester for UserPermissionTester {
         });
 
         // STEP 2 - deletes
-        result.push(TestStepData {
-            central_upsert: json!({}),
-            central_delete: json!({
-                "user_permission": [user_permission_row_1_json],
-            }),
-            integration_records: IntegrationRecords::from_deletes(vec![PullDeleteRecord {
-                id: user_permission_row_1.id,
-                table: PullDeleteRecordTable::UserPermission,
-            }]),
-        });
+        // result.push(TestStepData {
+        //     central_upsert: json!({}),
+        //     central_delete: json!({
+        //         "om_user_permission": [user_permission_row_1_json],
+        //     }),
+        //     integration_records: IntegrationRecords::from_deletes(vec![PullDeleteRecord {
+        //         id: user_permission_row_1.id,
+        //         table: PullDeleteRecordTable::UserPermission,
+        //     }]),
+        // });
         result
     }
 }
