8113a1142c8ccedd7d1090ad1641500a67f01ed0
roxy-dao <roxy@msupply.foundation>
2023-03-27 12:37:37 +1300

    Refactor permission test since user id has been removed as an fk
    Source: https://github.com/msupply-foundation/open-msupply/commit/987a341c42e21322a30c0e86bec4962008fd9b09

--- server/service/src/sync/test/integration/central/user_permission.rs
+++ server/service/src/sync/test/integration/central/user_permission.rs
@@ -17,37 +17,9 @@ impl SyncRecordTester for UserPermissionTester {
         // STEP 1 - insert
         let store_id = new_site_properties.store_id.clone();
 
-        let user = UserAccountRow {
-            id: "1234-56789-0000".to_string(),
-            username: "test_user".to_string(),
-            hashed_password: "d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1"
-                .to_string(),
-            email: None,
-            language: Language::English,
-        };
-
-        let user_json = json!({
-            "ID": user.id,
-            "name": user.username,
-            "active": true,
-            "password": "pass",
-            "date_created": "2023-03-24",
-            "type": {
-                "types": "desktop"
-            }
-        });
-
-        let user_store_json = json!({
-            "ID": uuid(),
-            "user_ID": user.id,
-            "store_ID": store_id,
-            "can_login": true,
-            "store_default": false
-        });
-
         let user_permission_row_1 = UserPermissionRow {
             id: uuid(),
-            user_id: user.id.clone(),
+            user_id: "test_user".to_string(),
             store_id: Some(store_id.clone()),
             permission: Permission::DocumentMutate,
             context: Some("some program".to_string()),
@@ -62,7 +34,7 @@ impl SyncRecordTester for UserPermissionTester {
 
         let user_permission_row_2 = UserPermissionRow {
             id: uuid(),
-            user_id: user.id.clone(),
+            user_id: "test_user".to_string(),
             store_id: Some(store_id),
             permission: Permission::DocumentQuery,
             context: Some("some program".to_string()),
@@ -77,8 +49,6 @@ impl SyncRecordTester for UserPermissionTester {
 
         result.push(TestStepData {
             central_upsert: json!({
-                "user": [user_json],
-                "user_store": [user_store_json],
                 "om_user_permission": [user_permission_row_1_json, user_permission_row_2_json],
             }),
             central_delete: json!({}),
@@ -89,16 +59,16 @@ impl SyncRecordTester for UserPermissionTester {
         });
 
         // STEP 2 - deletes
-        // result.push(TestStepData {
-        //     central_upsert: json!({}),
-        //     central_delete: json!({
-        //         "om_user_permission": [user_permission_row_1_json],
-        //     }),
-        //     integration_records: IntegrationRecords::from_deletes(vec![PullDeleteRecord {
-        //         id: user_permission_row_1.id,
-        //         table: PullDeleteRecordTable::UserPermission,
-        //     }]),
-        // });
+        result.push(TestStepData {
+            central_upsert: json!({}),
+            central_delete: json!({
+                "om_user_permission": [user_permission_row_1_json],
+            }),
+            integration_records: IntegrationRecords::from_deletes(vec![PullDeleteRecord {
+                id: user_permission_row_1.id,
+                table: PullDeleteRecordTable::UserPermission,
+            }]),
+        });
         result
     }
 }
