86bf17ee196da670ed962c443e3dc339113b62f1
Clemens Zeidler <clemens@msupply.foundation>
2023-03-28 09:06:48 +1300

    Remove document delete trigger
    Source: https://github.com/msupply-foundation/open-msupply/commit/e2a5bee9361ae5f7754edc98ad5c218afdd183e1

--- server/repository/migrations/sqlite/2022-04-27T13-05_create_document_trigger/up.sql
+++ server/repository/migrations/sqlite/2022-04-27T13-05_create_document_trigger/up.sql
@@ -11,10 +11,3 @@ CREATE TRIGGER document_update_trigger
     INSERT INTO changelog (table_name, record_id, row_action, is_sync_update)
       VALUES ('document', NEW.id, 'UPSERT', NEW.is_sync_update);
   END;
-
-CREATE TRIGGER document_delete_trigger
-  AFTER DELETE ON document
-  BEGIN
-    INSERT INTO changelog (table_name, record_id, row_action)
-      VALUES ('document', OLD.id, 'DELETE');
-  END;
