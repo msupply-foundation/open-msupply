80f69af5fe431f36c1370fba6c830b5acfca84e3
roxy-dao <roxy@msupply.foundation>
2023-03-30 08:06:21 +1300

    Update use document registries hook

--- client/packages/programs/src/api/hooks/document/useDocumentRegistries.ts
+++ client/packages/programs/src/api/hooks/document/useDocumentRegistries.ts
@@ -3,22 +3,13 @@ import { DocumentRegistryParams } from '../../api';
 
 import { useDocumentRegistryApi } from '../utils/useDocumentRegistryApi';
 
-export const useDocumentRegistries = (enabled?: boolean) => {
+export const useDocumentRegistries = () => {
   const api = useDocumentRegistryApi();
   const params: DocumentRegistryParams = {
     sortBy: { key: 'context', direction: 'asc' },
   };
 
-  return useQuery(
-    api.keys.documentRegistries(params),
-    () => api.get.documentRegistries(params),
-    // Don't refetch when the edit modal opens, for example. But, don't cache
-    // data when this query is inactive. For example, when navigating away from
-    // the page and back again, refetch.
-    {
-      refetchOnMount: false,
-      cacheTime: 0,
-      enabled,
-    }
+  return useQuery(api.keys.documentRegistries(params), () =>
+    api.get.documentRegistries(params)
   );
 };
