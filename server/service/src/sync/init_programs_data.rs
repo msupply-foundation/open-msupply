8413e7a468ea059829b900983f2c3cd8cc1be412
Clemens Zeidler <clemens@msupply.foundation>
2023-04-04 14:17:19 +1200

    Fix build

--- server/service/src/sync/init_programs_data.rs
+++ server/service/src/sync/init_programs_data.rs
@@ -45,6 +45,7 @@ mod hiv_testing_program {
                 referred_from: Default::default(),
                 partner_hiv_status: Default::default(),
                 mother: Default::default(),
+                notes: Default::default(),
             }
         }
     }
@@ -75,6 +76,7 @@ mod hiv_care_program {
                 previous_clinic_art_start_date: Default::default(),
                 date_transferred_in: Default::default(),
                 previous_clinic_id: Default::default(),
+                notes: Default::default(),
             }
         }
     }
