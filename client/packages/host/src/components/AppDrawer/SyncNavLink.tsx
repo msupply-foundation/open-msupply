13b15fa3da342620355813c83ed599808348a349
Mark Prins <mark@malcolmprins.net>
2023-04-06 09:28:39 +1200

    review response
    Source: https://github.com/msupply-foundation/open-msupply/commit/62a93ab0be5c3a9addaa9ac964d46bf57d993914

--- client/packages/host/src/components/AppDrawer/SyncNavLink.tsx
+++ client/packages/host/src/components/AppDrawer/SyncNavLink.tsx
@@ -6,7 +6,7 @@ import {
   useTranslation,
 } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { useHost } from '../../api/hooks';
 
 const POLLING_INTERVAL_IN_MILLISECONDS = 60 * 1000;
@@ -20,12 +20,8 @@ export const SyncNavLink = () => {
 
   // the Badge does not show if the content is 0
   // somehow though the numberOfRecordsInPushQueue can be '0' which does show
-  const badgeContent = (
-    !numberOfRecordsInPushQueue
-      ? 0
-      : // parse, after casting to string to satisfy ts
-        Number.parseFloat(`${numberOfRecordsInPushQueue}`)
-  ) as React.ReactNode;
+  const syncCount = Number(numberOfRecordsInPushQueue);
+  const badgeContent = Number.isNaN(syncCount) ? 0 : (syncCount as ReactNode);
 
   const badgeProps = {
     badgeContent,
