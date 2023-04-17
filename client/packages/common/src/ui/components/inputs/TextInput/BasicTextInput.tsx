b577484fe339eb8d06b5c73c6c861ce8545adb36
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-04-18 09:19:34 +1200

    Update BasicTextInput.tsx

--- client/packages/common/src/ui/components/inputs/TextInput/BasicTextInput.tsx
+++ client/packages/common/src/ui/components/inputs/TextInput/BasicTextInput.tsx
@@ -16,55 +16,53 @@ export type BasicTextInputProps = StandardTextFieldProps & {
  */
 
 export const BasicTextInput: FC<BasicTextInputProps> = React.forwardRef(
-  ({ sx, style, InputProps, error, required, textAlign, ...props }, ref) => {
-    return (
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-        alignItems="center"
-        flexBasis={style?.flexBasis}
-      >
-        <TextField
-          ref={ref}
-          color="secondary"
-          sx={{
-            '& .MuiInput-underline:before': { borderBottomWidth: 0 },
-            '& .MuiInput-input': { color: 'gray.dark', textAlign },
-            ...sx,
-          }}
-          variant="standard"
-          size="small"
-          InputProps={{
-            disableUnderline: error ? true : false,
-            ...InputProps,
-            sx: {
-              border: theme =>
-                error ? `2px solid ${theme.palette.error.main}` : 'none',
-              backgroundColor: theme =>
-                props.disabled
-                  ? theme.palette.background.toolbar
-                  : theme.palette.background.menu,
-              borderRadius: '8px',
-              padding: '4px 8px',
-              ...InputProps?.sx,
-            },
-          }}
-          {...props}
-        />
-        <Box width={2}>
-          {required && (
-            <Typography
-              sx={{
-                color: 'primary.light',
-                fontSize: '17px',
-                marginRight: 0.5,
-              }}
-            >
-              *
-            </Typography>
-          )}
-        </Box>
+  ({ sx, style, InputProps, error, required, textAlign, ...props }, ref) => (
+    <Box
+      display="flex"
+      justifyContent="flex-end"
+      alignItems="center"
+      // flexBasis={style?.flexBasis}
+    >
+      <TextField
+        ref={ref}
+        color="secondary"
+        sx={{
+          '& .MuiInput-underline:before': { borderBottomWidth: 0 },
+          '& .MuiInput-input': { color: 'gray.dark', textAlign },
+          ...sx,
+        }}
+        variant="standard"
+        size="small"
+        InputProps={{
+          disableUnderline: error ? true : false,
+          ...InputProps,
+          sx: {
+            border: theme =>
+              error ? `2px solid ${theme.palette.error.main}` : 'none',
+            backgroundColor: theme =>
+              props.disabled
+                ? theme.palette.background.toolbar
+                : theme.palette.background.menu,
+            borderRadius: '8px',
+            padding: '4px 8px',
+            ...InputProps?.sx,
+          },
+        }}
+        {...props}
+      />
+      <Box width={2}>
+        {required && (
+          <Typography
+            sx={{
+              color: 'primary.light',
+              fontSize: '17px',
+              marginRight: 0.5,
+            }}
+          >
+            *
+          </Typography>
+        )}
       </Box>
-    );
-  }
+    </Box>
+  )
 );
