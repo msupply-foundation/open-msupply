5293db3a40073208f247b9ab6f70ca467aeca5a8
Mark Prins <mark@malcolmprins.net>
2023-04-17 12:56:38 +1200

    triggers

--- server/repository/src/migrations/v1_01_12/barcode.rs
+++ server/repository/src/migrations/v1_01_12/barcode.rs
@@ -20,5 +20,62 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
         r#"ALTER TABLE public.invoice_line ADD barcode_id text NULL REFERENCES barcode(id);"#
     )?;
 
+    sql!(
+        connection,
+        r#"ALTER TYPE changelog_table_name ADD VALUE IF NOT EXISTS 'barcode';"#
+    )?;
+
+    sql!(
+        connection,
+        r#"CREATE OR REPLACE FUNCTION public.upsert_barcode_changelog()
+        RETURNS trigger
+        LANGUAGE plpgsql
+       AS $function$
+         BEGIN
+           INSERT INTO changelog (table_name, record_id, row_action)
+                 VALUES ('barcode', NEW.id, 'UPSERT');
+           -- The return value is required, even though it is ignored for a row-level AFTER trigger
+           RETURN NULL;
+         END;
+       $function$
+       ;"#
+    )?;
+
+    sql!(
+        connection,
+        r#"CREATE OR REPLACE FUNCTION public.delete_barcode_changelog()
+        RETURNS trigger
+        LANGUAGE plpgsql
+       AS $function$
+         BEGIN
+           INSERT INTO changelog (table_name, record_id, row_action, name_id)
+                 VALUES ('barcode', OLD.id, 'DELETE', OLD.name_id);
+           -- The return value is required, even though it is ignored for a row-level AFTER trigger
+           RETURN NULL;
+         END;
+       $function$
+       ;"#
+    )?;
+
+    sql!(
+        connection,
+        r#"create trigger barcode_upsert_trigger after
+        insert
+            or
+        update
+            on
+            barcode for each row execute function upsert_barcode_changelog();
+        "#
+    )?;
+    sql!(
+        connection,
+        r#"create trigger barcode_delete_trigger after
+        delete
+            on
+            barcode for each row execute function delete_barcode_changelog();
+        
+        "#
+    )?;
+
     Ok(())
 }
