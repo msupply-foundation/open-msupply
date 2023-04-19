2a3de4c2f97e3e972ea5404027a4712add4db40d
James Brunskill <james@msupply.foundation>
2023-04-19 17:25:38 +1200

    API for creating program requisitions

---
+++ server/graphql/requisition/src/mutations/request_requisition/insert_program.rs
@@ -0,0 +1,386 @@
+use async_graphql::*;
+use chrono::NaiveDate;
+use graphql_core::{
+    simple_generic_errors::{OtherPartyNotASupplier, OtherPartyNotVisible},
+    standard_graphql_error::validate_auth,
+    standard_graphql_error::StandardGraphqlError,
+    ContextExt,
+};
+use graphql_types::types::RequisitionNode;
+use repository::Requisition;
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    requisition::request_requisition::{
+        InsertProgramRequestRequisition, InsertRequestRequisitionError,
+    },
+};
+use util::{constants::expected_delivery_date_offset, date_now_with_offset};
+
+use crate::mutations::request_requisition::InsertErrorInterface;
+
+use super::{InsertError, InsertResponse};
+
+#[derive(InputObject)]
+#[graphql(name = "InsertProgramRequestRequisitionInput")]
+pub struct InsertProgramRequestRequisitionInput {
+    pub id: String,
+    pub other_party_id: String,
+    pub colour: Option<String>,
+    pub their_reference: Option<String>,
+    pub comment: Option<String>,
+    pub program_order_type_id: String,
+    pub period_id: String,
+    /// Defaults to 2 weeks from now
+    pub expected_delivery_date: Option<NaiveDate>,
+}
+
+pub fn insert_program(
+    ctx: &Context<'_>,
+    store_id: &str,
+    input: InsertProgramRequestRequisitionInput,
+) -> Result<InsertResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::MutateRequisition,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    map_response(
+        service_provider
+            .requisition_service
+            .insert_program_request_requisition(&service_context, input.to_domain()),
+    )
+}
+
+pub fn map_response(
+    from: Result<Requisition, InsertRequestRequisitionError>,
+) -> Result<InsertResponse> {
+    let result = match from {
+        Ok(requisition) => InsertResponse::Response(RequisitionNode::from_domain(requisition)),
+        Err(error) => InsertResponse::Error(InsertError {
+            error: map_error(error)?,
+        }),
+    };
+
+    Ok(result)
+}
+
+impl InsertProgramRequestRequisitionInput {
+    pub fn to_domain(self) -> InsertProgramRequestRequisition {
+        let InsertProgramRequestRequisitionInput {
+            id,
+            other_party_id,
+            colour,
+            their_reference,
+            comment,
+            expected_delivery_date,
+            program_order_type_id,
+            period_id,
+        } = self;
+
+        InsertProgramRequestRequisition {
+            id,
+            other_party_id,
+            colour,
+            their_reference,
+            comment,
+            expected_delivery_date: expected_delivery_date
+                .or(Some(date_now_with_offset(expected_delivery_date_offset()))),
+            program_order_type_id,
+            period_id,
+        }
+    }
+}
+
+fn map_error(error: InsertRequestRequisitionError) -> Result<InsertErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Structured Errors
+        InsertRequestRequisitionError::OtherPartyNotASupplier => {
+            return Ok(InsertErrorInterface::OtherPartyNotASupplier(
+                OtherPartyNotASupplier,
+            ))
+        }
+        InsertRequestRequisitionError::OtherPartyNotVisible => {
+            return Ok(InsertErrorInterface::OtherPartyNotVisible(
+                OtherPartyNotVisible,
+            ))
+        }
+        // Standard Graphql Errors
+        InsertRequestRequisitionError::RequisitionAlreadyExists => BadUserInput(formatted_error),
+        InsertRequestRequisitionError::OtherPartyDoesNotExist => BadUserInput(formatted_error),
+
+        InsertRequestRequisitionError::OtherPartyIsNotAStore => BadUserInput(formatted_error),
+        InsertRequestRequisitionError::NewlyCreatedRequisitionDoesNotExist => {
+            InternalError(formatted_error)
+        }
+        InsertRequestRequisitionError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
+}
+
+#[cfg(test)]
+mod test {
+    use async_graphql::EmptyMutation;
+    use chrono::NaiveDate;
+    use graphql_core::{
+        assert_graphql_query, assert_standard_graphql_error, test_helpers::setup_graphl_test,
+    };
+    use repository::{
+        mock::{mock_request_draft_requisition, MockDataInserts},
+        Requisition, StorageConnectionManager,
+    };
+    use serde_json::json;
+    use service::{
+        requisition::{
+            request_requisition::{InsertProgramRequestRequisition, InsertRequestRequisitionError},
+            RequisitionServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+    use util::{date_now, inline_init};
+
+    use crate::RequisitionMutations;
+
+    type InsertLineMethod = dyn Fn(InsertProgramRequestRequisition) -> Result<Requisition, InsertRequestRequisitionError>
+        + Sync
+        + Send;
+
+    pub struct TestService(pub Box<InsertLineMethod>);
+
+    impl RequisitionServiceTrait for TestService {
+        fn insert_program_request_requisition(
+            &self,
+            _: &ServiceContext,
+            input: InsertProgramRequestRequisition,
+        ) -> Result<Requisition, InsertRequestRequisitionError> {
+            self.0(input)
+        }
+    }
+
+    fn service_provider(
+        test_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.requisition_service = Box::new(test_service);
+        service_provider
+    }
+
+    fn empty_variables() -> serde_json::Value {
+        json!({
+          "input": {
+            "id": "n/a",
+            "otherPartyId": "n/a",
+            "maxMonthsOfStock": 0,
+            "minMonthsOfStock": 0
+          },
+          "storeId": "n/a"
+        })
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_insert_request_requisition_errors() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            RequisitionMutations,
+            "test_graphql_insert_program_program_request_requisition_structured_errors",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: InsertProgramRequestRequisitionInput!, $storeId: String) {
+            insertRequestRequisition(storeId: $storeId, input: $input) {
+              ... on InsertRequestRequisitionError {
+                error {
+                  __typename
+                }
+              }
+            }
+          }
+        "#;
+
+        // OtherPartyNotASupplier
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertRequestRequisitionError::OtherPartyNotASupplier)
+        }));
+
+        let expected = json!({
+            "insertRequestRequisition": {
+              "error": {
+                "__typename": "OtherPartyNotASupplier"
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // RequisitionAlreadyExists
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertRequestRequisitionError::RequisitionAlreadyExists)
+        }));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // OtherPartyDoesNotExist
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertRequestRequisitionError::OtherPartyDoesNotExist)
+        }));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // OtherPartyIsNotAStore
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertRequestRequisitionError::OtherPartyIsNotAStore)
+        }));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // NewlyCreatedRequisitionDoesNotExist
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertRequestRequisitionError::NewlyCreatedRequisitionDoesNotExist)
+        }));
+        let expected_message = "Internal error";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_insert_program_request_requisition_success() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            RequisitionMutations,
+            "test_graphql_insert_program_request_requisition_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($storeId: String, $input: InsertProgramRequestRequisitionInput!) {
+            insertRequestRequisition(storeId: $storeId, input: $input) {
+                ... on RequisitionNode {
+                    id
+                }
+            }
+          }
+        "#;
+
+        // Success
+        let test_service = TestService(Box::new(|input| {
+            assert_eq!(
+                input,
+                InsertProgramRequestRequisition {
+                    id: "id input".to_string(),
+                    other_party_id: "other party input".to_string(),
+                    colour: Some("colour input".to_string()),
+                    their_reference: Some("reference input".to_string()),
+                    comment: Some("comment input".to_string()),
+                    expected_delivery_date: Some(NaiveDate::from_ymd_opt(2022, 01, 03).unwrap()),
+                    program_order_type_id: "program_order_type_id".to_string(),
+                    period_id: "period_id".to_string(),
+                }
+            );
+            Ok(inline_init(|r: &mut Requisition| {
+                r.requisition_row = mock_request_draft_requisition()
+            }))
+        }));
+
+        let variables = json!({
+          "input": {
+            "id": "id input",
+            "otherPartyId": "other party input",
+            "maxMonthsOfStock": 1,
+            "minMonthsOfStock": 2,
+            "colour": "colour input",
+            "theirReference": "reference input",
+            "comment": "comment input",
+            "expectedDeliveryDate": "2022-01-03"
+          },
+          "storeId": "store_a"
+        });
+
+        let expected = json!({
+            "insertRequestRequisition": {
+                "id": mock_request_draft_requisition().id
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Default expected_delivery_date
+        let test_service = TestService(Box::new(|input| {
+            let now = date_now();
+            let expected = input.expected_delivery_date.unwrap();
+            assert_eq!((expected - now), chrono::Duration::weeks(2));
+
+            Ok(inline_init(|r: &mut Requisition| {
+                r.requisition_row = mock_request_draft_requisition()
+            }))
+        }));
+
+        let expected = json!({
+            "insertRequestRequisition": {
+                "id": mock_request_draft_requisition().id
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+}
---
+++ server/service/src/requisition/request_requisition/insert_program.rs
@@ -0,0 +1,317 @@
+use crate::{
+    activity_log::activity_log_entry,
+    number::next_number,
+    requisition::{common::check_requisition_exists, query::get_requisition},
+    service_provider::ServiceContext,
+    validate::{check_other_party, CheckOtherPartyType, OtherPartyErrors},
+};
+use chrono::{NaiveDate, Utc};
+use repository::{
+    requisition_row::{RequisitionRow, RequisitionRowStatus, RequisitionRowType},
+    ActivityLogType, NumberRowType, ProgramRequisitionOrderTypeRowRepository,
+    ProgramRequisitionSettingsRowRepository, ProgramRowRepository, RepositoryError, Requisition,
+    RequisitionRowRepository, StorageConnection,
+};
+
+use super::InsertRequestRequisitionError;
+
+#[derive(Debug, PartialEq, Clone, Default)]
+pub struct InsertProgramRequestRequisition {
+    pub id: String,
+    pub other_party_id: String,
+    pub colour: Option<String>,
+    pub their_reference: Option<String>,
+    pub comment: Option<String>,
+    pub expected_delivery_date: Option<NaiveDate>,
+    pub program_order_type_id: String,
+    pub period_id: String,
+}
+
+type OutError = InsertRequestRequisitionError;
+
+pub fn insert_program_request_requisition(
+    ctx: &ServiceContext,
+    input: InsertProgramRequestRequisition,
+) -> Result<Requisition, OutError> {
+    let requisition = ctx
+        .connection
+        .transaction_sync(|connection| {
+            validate(connection, &ctx.store_id, &input)?;
+            let new_requisition = generate(connection, &ctx.store_id, &ctx.user_id, input)?;
+            RequisitionRowRepository::new(&connection).upsert_one(&new_requisition)?;
+
+            activity_log_entry(
+                &ctx,
+                ActivityLogType::RequisitionCreated,
+                Some(new_requisition.id.to_owned()),
+                None,
+            )?;
+
+            get_requisition(ctx, None, &new_requisition.id)
+                .map_err(|error| OutError::DatabaseError(error))?
+                .ok_or(OutError::NewlyCreatedRequisitionDoesNotExist)
+        })
+        .map_err(|error| error.to_inner_error())?;
+
+    Ok(requisition)
+}
+
+fn validate(
+    connection: &StorageConnection,
+    store_id: &str,
+    input: &InsertProgramRequestRequisition,
+) -> Result<(), OutError> {
+    if let Some(_) = check_requisition_exists(connection, &input.id)? {
+        return Err(OutError::RequisitionAlreadyExists);
+    }
+
+    let other_party = check_other_party(
+        connection,
+        store_id,
+        &input.other_party_id,
+        CheckOtherPartyType::Supplier,
+    )
+    .map_err(|e| match e {
+        OtherPartyErrors::OtherPartyDoesNotExist => OutError::OtherPartyDoesNotExist {},
+        OtherPartyErrors::OtherPartyNotVisible => OutError::OtherPartyNotVisible,
+        OtherPartyErrors::TypeMismatched => OutError::OtherPartyNotASupplier,
+        OtherPartyErrors::DatabaseError(repository_error) => {
+            OutError::DatabaseError(repository_error)
+        }
+    })?;
+
+    other_party
+        .store_id()
+        .ok_or(OutError::OtherPartyIsNotAStore)?;
+
+    Ok(())
+}
+
+fn generate(
+    connection: &StorageConnection,
+    store_id: &str,
+    user_id: &str,
+    InsertProgramRequestRequisition {
+        id,
+        other_party_id,
+        colour,
+        comment,
+        their_reference,
+        expected_delivery_date,
+        program_order_type_id,
+        period_id,
+    }: InsertProgramRequestRequisition,
+) -> Result<RequisitionRow, RepositoryError> {
+    // Lookup order type
+    let order_type = ProgramRequisitionOrderTypeRowRepository::new(connection)
+        .find_one_by_id(&program_order_type_id)?;
+    let order_type = match order_type {
+        Some(order_type) => order_type,
+        None => return Err(RepositoryError::NotFound),
+    };
+
+    // Lookup program requisition settings
+    let program_requisition_settings = ProgramRequisitionSettingsRowRepository::new(connection)
+        .find_one_by_id(&order_type.program_requisition_settings_id)?;
+    let program_requisition_settings = match program_requisition_settings {
+        Some(program_requisition_settings) => program_requisition_settings,
+        None => return Err(RepositoryError::NotFound),
+    };
+
+    // Lookup program
+    let program = ProgramRowRepository::new(connection)
+        .find_one_by_id(&program_requisition_settings.program_id)?;
+    let program = match program {
+        Some(program) => program,
+        None => return Err(RepositoryError::NotFound),
+    };
+
+    let result = RequisitionRow {
+        id,
+        user_id: Some(user_id.to_string()),
+        requisition_number: next_number(connection, &NumberRowType::RequestRequisition, &store_id)?,
+        name_id: other_party_id,
+        store_id: store_id.to_owned(),
+        r#type: RequisitionRowType::Request,
+        status: RequisitionRowStatus::Draft,
+        created_datetime: Utc::now().naive_utc(),
+        colour,
+        comment,
+        expected_delivery_date,
+        their_reference,
+        max_months_of_stock: order_type.max_mos,
+        min_months_of_stock: order_type.threshold_mos,
+        // Default
+        sent_datetime: None,
+        approval_status: None,
+        finalised_datetime: None,
+        linked_requisition_id: None,
+        is_sync_update: false,
+        program_id: Some(program.id),
+        period_id: Some(period_id),
+        order_type: Some(order_type.name),
+    };
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod test_insert {
+    use crate::{
+        requisition::request_requisition::{
+            InsertRequestRequisition, InsertRequestRequisitionError as ServiceError,
+        },
+        service_provider::ServiceProvider,
+    };
+    use chrono::{NaiveDate, Utc};
+    use repository::{
+        mock::{
+            mock_name_a, mock_name_store_b, mock_name_store_c, mock_request_draft_requisition,
+            mock_store_a, mock_user_account_a, MockData, MockDataInserts,
+        },
+        test_db::{setup_all, setup_all_with_data},
+        NameRow, RequisitionRowRepository,
+    };
+    use util::{inline_edit, inline_init};
+
+    #[actix_rt::test]
+    async fn insert_request_requisition_errors() {
+        fn not_visible() -> NameRow {
+            inline_init(|r: &mut NameRow| {
+                r.id = "not_visible".to_string();
+            })
+        }
+
+        let (_, _, connection_manager, _) = setup_all_with_data(
+            "insert_request_requisition_errors",
+            MockDataInserts::all(),
+            inline_init(|r: &mut MockData| {
+                r.names = vec![not_visible()];
+            }),
+        )
+        .await;
+
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider
+            .context(mock_store_a().id, "".to_string())
+            .unwrap();
+        let service = service_provider.requisition_service;
+
+        // RequisitionAlreadyExists
+        assert_eq!(
+            service.insert_request_requisition(
+                &context,
+                inline_init(|r: &mut InsertRequestRequisition| {
+                    r.id = mock_request_draft_requisition().id;
+                }),
+            ),
+            Err(ServiceError::RequisitionAlreadyExists)
+        );
+
+        let name_store_b = mock_name_store_b();
+        // OtherPartyNotASupplier
+        assert_eq!(
+            service.insert_request_requisition(
+                &context,
+                inline_init(|r: &mut InsertRequestRequisition| {
+                    r.id = "new_request_requisition".to_owned();
+                    r.other_party_id = name_store_b.id.clone();
+                }),
+            ),
+            Err(ServiceError::OtherPartyNotASupplier)
+        );
+
+        // OtherPartyNotVisible
+        assert_eq!(
+            service.insert_request_requisition(
+                &context,
+                inline_init(|r: &mut InsertRequestRequisition| {
+                    r.id = "new_id".to_string();
+                    r.other_party_id = not_visible().id;
+                })
+            ),
+            Err(ServiceError::OtherPartyNotVisible)
+        );
+
+        // OtherPartyDoesNotExist
+        assert_eq!(
+            service.insert_request_requisition(
+                &context,
+                inline_init(|r: &mut InsertRequestRequisition| {
+                    r.id = "new_request_requisition".to_owned();
+                    r.other_party_id = "invalid".to_owned();
+                }),
+            ),
+            Err(ServiceError::OtherPartyDoesNotExist)
+        );
+
+        // OtherPartyIsNotAStore
+        assert_eq!(
+            service.insert_request_requisition(
+                &context,
+                inline_init(|r: &mut InsertRequestRequisition| {
+                    r.id = "new_request_requisition".to_owned();
+                    r.other_party_id = mock_name_a().id;
+                }),
+            ),
+            Err(ServiceError::OtherPartyIsNotAStore)
+        );
+    }
+
+    #[actix_rt::test]
+    async fn insert_request_requisition_success() {
+        let (_, connection, connection_manager, _) =
+            setup_all("insert_request_requisition_success", MockDataInserts::all()).await;
+
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider
+            .context(mock_store_a().id, mock_user_account_a().id)
+            .unwrap();
+        let service = service_provider.requisition_service;
+
+        let before_insert = Utc::now().naive_utc();
+
+        let result = service
+            .insert_request_requisition(
+                &context,
+                InsertRequestRequisition {
+                    id: "new_request_requisition".to_owned(),
+                    other_party_id: mock_name_store_c().id,
+                    colour: Some("new colour".to_owned()),
+                    their_reference: Some("new their_reference".to_owned()),
+                    comment: Some("new comment".to_owned()),
+                    max_months_of_stock: 1.0,
+                    min_months_of_stock: 0.5,
+                    expected_delivery_date: Some(NaiveDate::from_ymd_opt(2022, 01, 03).unwrap()),
+                },
+            )
+            .unwrap();
+
+        let after_insert = Utc::now().naive_utc();
+
+        let new_row = RequisitionRowRepository::new(&connection)
+            .find_one_by_id(&result.requisition_row.id)
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(
+            new_row,
+            inline_edit(&new_row, |mut u| {
+                u.id = "new_request_requisition".to_owned();
+                u.user_id = Some(mock_user_account_a().id);
+                u.name_id = mock_name_store_c().id;
+                u.colour = Some("new colour".to_owned());
+                u.their_reference = Some("new their_reference".to_owned());
+                u.comment = Some("new comment".to_owned());
+                u.max_months_of_stock = 1.0;
+                u.min_months_of_stock = 0.5;
+                u.expected_delivery_date = Some(NaiveDate::from_ymd_opt(2022, 01, 03).unwrap());
+                u
+            })
+        );
+
+        assert!(
+            new_row.created_datetime > before_insert && new_row.created_datetime < after_insert
+        );
+    }
+}
