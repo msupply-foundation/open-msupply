27e8560ff006a3ef00011e2edaec9adc86eb1db7
Andrei <andrei.evguenov@gmail.com>
2023-04-21 01:04:09 +1200

    Add action modify build script and add documentation for demo mac build action

--- build/mac/README.md
+++ build/mac/README.md
@@ -3,22 +3,23 @@
 `NOTE` -> this is for demo and testing purposes only, not to be used in production
 
 ```bash
-./build/mac/build.sh
+# for Intel mac
+./build/mac/build.sh intel 
+# or for Arm mac (with demo data)
+./build/mac/build.sh arm true
 ```
 
-Above will build and `bundle` files in `{..}_mac_sqlite` folder where `{..}` is replaced with architecture of the build
+Above will build and `bundle` files in `omSupply_mac_{ARCHITECHTURE}_{VERSION}_{COMMIT_DAY_MONTH}_{COMMIT_HOUR_AND_SECOND}`
 
-In finder, right click on `{..}_mac_sqlite/open_msupply_server.sh`, then
-* `Open With`
-* `Other`
-* Select `All Appliations` for `Enable` drop down
-* Select `terminal` from `Utilities
-
-You can zip the contents of `{..}_mac_sqlite` folder now and share with testers or for demo purposes.
+You can zip the contents of that folder now and share with testers or for demo purposes. (they would need to double click on open_msupply_server.sh, and allow it in their mac security settings)
 
 To include some data:
 
-## Add data
+## Add demo data
+
+Add 'true' as last argument (after intel or mac)
+
+## Add other data
 
 * Click on open_msupply_server.sh from finder
 * After 3 seconds initialisation screen should open in browser
@@ -26,4 +27,4 @@ To include some data:
 * Log in with all of the users that will need access in the demo data
 * cmd + c out of terminal that was opened when `open_msupply_server.sh` was clicked
 
-You can zip the contents of `{..}_mac_sqlite` folder now and share with testers or for demo purposes.
+Now zipping `omSupply_mac_{ARCHITECHTURE}_{VERSION}_{COMMIT_DAY_MONTH}_{COMMIT_HOUR_AND_SECOND}` should save the data as well
--- build/mac/build.sh
+++ build/mac/build.sh
@@ -1,16 +1,65 @@
 #!/bin/bash
 
-# On M1 mac below would be DESTINATION="arm64_mac_sqlite"
-DESTINATION="$(uname -m)_mac_sqlite"
+# Current directory
+DIR="$(cd "$(dirname "$0")" && pwd)"
+# intel or arm
+ARCHITECHTURE=$1
+SHOULD_INCLUDE_DEMO_DATA=$2
+DESTINATION=$("${DIR}"/get_name.sh $ARCHITECHTURE)
+
+echo "destination: ${DESTINATION}"
+
+# Select target
+if [ "$ARCHITECHTURE" == "intel" ]; then
+    TARGET="x86_64-apple-darwin"
+elif [ "$ARCHITECHTURE" == "arm" ]; then
+    TARGET="aarch64-apple-darwin"
+else
+    echo "Error: first argument must be 'intel' or 'arm'"
+    exit 1
+fi
+
+# Add target
+rustup target add $TARGET
 
 # Buid (on Mac)
-cd client && yarn install && yarn build && cd ../server && cargo clean && cargo build --release --bin remote_server && cd ../
+cd client
+yarn install
+yarn build
+
+cd ../server
+cargo build --release --bin remote_server --bin remote_server_cli --target $TARGET
+cd ../
+
 # Copy binaries to $DESTINATION
-rm -rf $DESTINATION && mkdir $DESTINATION&& mkdir $DESTINATION/bin && mv server/target/release/remote_server $DESTINATION/bin 
+rm -rf $DESTINATION
+mkdir $DESTINATION
+mkdir $DESTINATION/bin
+cp "server/target/${TARGET}/release/remote_server" $DESTINATION/bin 
+cp "server/target/${TARGET}/release/remote_server_cli" $DESTINATION/bin 
+
 # Copy configurations
-mkdir $DESTINATION/configuration && cp -R server/configuration/base.yaml $DESTINATION/configuration/
+mkdir $DESTINATION/configuration
+cp -R server/configuration/base.yaml $DESTINATION/configuration/
 # Local file should be present
 touch $DESTINATION/configuration/local.yaml
 
+# Initialise demo data
+if [ "$SHOULD_INCLUDE_DEMO_DATA" == "true" ]; then
+    cp -R server/data $DESTINATION
+    cd $DESTINATION
+    ./bin/remote_server_cli initialise-from-export -n reference1
+    cd ../
+fi
+
 # Copy launch script
 cp build/mac/open_msupply_server.sh $DESTINATION/
+
+# This would set openWith = terminal for mac (you can manually set openWith = terminal then do 'xattr -px com.apple.LaunchServices.OpenWith open_msupply_server.sh' to see this hash)
+xattr -wx com.apple.LaunchServices.OpenWith 62706C6973743030D30102030405065776657273696F6E54706174685F101062756E646C656964656E74696669657210005F102B2F53797374656D2F4170706C69636174696F6E732F5574696C69746965732F5465726D696E616C2E6170705F1012636F6D2E6170706C652E5465726D696E616C080F171C2F315F0000000000000101000000000000000700000000000000000000000000000074 $DESTINATION/open_msupply_server.sh
+chmod +x $DESTINATION/open_msupply_server.sh
+
+# Copy instructions
+cp build/mac/instructions.txt $DESTINATION/
+# Write hash
+echo echo $(git log -1) > $DESTINATION/sha.txt
