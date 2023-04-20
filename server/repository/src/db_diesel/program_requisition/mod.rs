b6f819b2005b6e349dae8664258d8624f49f5bfb
Andrei <andrei.evguenov@gmail.com>
2023-04-21 00:12:49 +1200

    Add remaining change for program requisition settings api
    Source: https://github.com/msupply-foundation/open-msupply/commit/17cf5a346abb035e788b8064528c7e35d892f2f6

--- server/repository/src/db_diesel/program_requisition/mod.rs
+++ server/repository/src/db_diesel/program_requisition/mod.rs
@@ -1,7 +1,13 @@
 pub mod program_requisition_order_type_row;
+pub mod program_requisition_settings;
 pub mod program_requisition_settings_row;
 pub mod program_row;
+pub mod program_supplier;
+pub mod requisitions_in_period;
 
 pub use program_requisition_order_type_row::*;
+pub use program_requisition_settings::*;
 pub use program_requisition_settings_row::*;
 pub use program_row::*;
+pub use program_supplier::*;
+pub use requisitions_in_period::*;
