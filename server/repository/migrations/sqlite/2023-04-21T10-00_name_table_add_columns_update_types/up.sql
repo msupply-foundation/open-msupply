0dcae214fe23426b31494ac1aa55af6a3083bbc0
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-04-24 14:25:37 +1200

    Leave "type" column as it was (no change to constraint)

--- server/repository/migrations/sqlite/2023-04-21T10-00_name_table_add_columns_update_types/up.sql
+++ server/repository/migrations/sqlite/2023-04-21T10-00_name_table_add_columns_update_types/up.sql
@@ -3,11 +3,6 @@
 -- earlier migrations had already run.
 
 -- 2021-08-15T10-00_create_name_table
-ALTER TABLE name ADD COLUMN type_new TEXT NOT NULL DEFAULT "FACILITY";
-UPDATE name SET type_new =  type;
-ALTER TABLE name DROP COLUMN type;
-ALTER TABLE name RENAME COLUMN type_new TO type;
-
 ALTER TABLE name ADD COLUMN gender_new TEXT;
 UPDATE name SET gender_new =  gender;
 ALTER TABLE name DROP COLUMN gender;
