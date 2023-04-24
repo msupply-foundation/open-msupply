7bf61754170c11cb11167fc180d27cefd697b718
roxy-dao <roxy@msupply.foundation>
2023-04-24 12:56:36 +1200

    Endpoint for Response Requisition Count
    Source: https://github.com/msupply-foundation/open-msupply/commit/0e15de352c5f06fdf65795f3247da43fa4cce5ee

--- server/graphql/general/src/queries/mod.rs
+++ server/graphql/general/src/queries/mod.rs
@@ -35,6 +35,8 @@ pub mod item_counts;
 pub use self::item_counts::*;
 pub mod store_preference;
 pub use self::store_preference::*;
+pub mod requisition_counts;
+pub use self::requisition_counts::*;
 
 #[cfg(test)]
 mod tests;
