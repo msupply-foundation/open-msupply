1a6388e24fbdb07e6f78692a4b18e7047545982f
Mark Prins <mark@malcolmprins.net>
2023-05-01 11:32:54 +1200

    Revert "remove v12 migration"
    This reverts commit da8d89186fed36d65a1acc1e7c7f29f05547192b.
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/dd804c3a95ab361ba3fad88730d35cc8f84f7116

--- server/repository/src/migrations/mod.rs
+++ server/repository/src/migrations/mod.rs
@@ -5,6 +5,7 @@ mod v1_01_02;
 mod v1_01_03;
 mod v1_01_05;
 mod v1_01_11;
+mod v1_01_12;
 mod version;
 pub(crate) use self::types::*;
 use self::v1_00_04::V1_00_04;
@@ -66,6 +67,7 @@ pub fn migrate(
         Box::new(V1_01_03),
         Box::new(v1_01_05::V1_01_05),
         Box::new(v1_01_11::V1_01_11),
+        Box::new(v1_01_12::V1_01_12),
     ];
 
     // Historic diesel migrations
