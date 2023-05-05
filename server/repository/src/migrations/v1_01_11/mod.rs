de72b3529d1cd6ebeb274988949df84a027c9267
Clemens Zeidler <clemens@msupply.foundation>
2023-05-05 12:18:56 +1200

    Move location_movement_trigger migration to v1_10_12
    Source: https://github.com/msupply-foundation/open-msupply/commit/6a658520b95226b16ade5bb2a10f8900749276c4

--- server/repository/src/migrations/v1_01_11/mod.rs
+++ server/repository/src/migrations/v1_01_11/mod.rs
@@ -2,7 +2,6 @@ use super::{version::Version, Migration};
 mod activity_log;
 mod barcode;
 mod is_sync_updated_for_requisition;
-mod location_movement_triggers;
 mod name_tags;
 mod period_and_period_schedule;
 mod program_requisition;
@@ -32,7 +31,6 @@ impl Migration for V1_01_11 {
 
         barcode::migrate(connection)?;
 
-        location_movement_triggers::migrate(connection)?;
         Ok(())
     }
 }
