eea591040df262cdcbff31398e1eba8eb1d1364d
roxy-dao <roxy@msupply.foundation>
2023-05-08 09:01:12 +1200

    Add new types to invoice + number tables

---
+++ server/repository/src/migrations/v1_01_12/mod.rs
@@ -0,0 +1,36 @@
+use super::{version::Version, Migration};
+
+use crate::StorageConnection;
+pub(crate) struct V1_01_12;
+mod invoice_and_number_type;
+
+impl Migration for V1_01_12 {
+    fn version(&self) -> Version {
+        Version::from_str("1.1.12")
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        invoice_and_number_type::migrate(connection)?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+#[actix_rt::test]
+async fn migration_1_01_12() {
+    use crate::migrations::*;
+    use crate::test_db::*;
+
+    let version = V1_01_12.version();
+
+    // This test allows checking sql syntax
+    let SetupResult { connection, .. } = setup_test(SetupOption {
+        db_name: &format!("migration_{version}"),
+        version: Some(version.clone()),
+        ..Default::default()
+    })
+    .await;
+
+    assert_eq!(get_database_version(&connection), version);
+}
