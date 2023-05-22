884a6d850e756f70576ba2247014df8810562a56
roxy-dao <roxy@msupply.foundation>
2023-05-23 10:30:55 +1200

    Endpoint to get repacks by stock line id
    Source: https://github.com/msupply-foundation/open-msupply/commit/7ff9a751099ffd141bfad5fda0d58e47e23df6af

--- server/graphql/repack/query.rs
+++ server/graphql/repack/query.rs
@@ -2,7 +2,7 @@ use async_graphql::*;
 use graphql_core::{
     simple_generic_errors::NodeError, standard_graphql_error::validate_auth, ContextExt,
 };
-use graphql_types::types::RepackNode;
+use graphql_types::types::{RepackConnector, RepackNode};
 use service::auth::{Resource, ResourceAccessRequest};
 
 #[derive(Union)]
@@ -31,6 +31,30 @@ pub async fn get_repack(
     let repack = repack_service.get_repack(&service_context, invoice_id)?;
 
     let response = RepackResponse::Response(RepackNode::from_domain(repack));
+
+    Ok(response)
+}
+
+pub async fn get_repacks_by_stock_line(
+    ctx: &Context<'_>,
+    store_id: String,
+    stock_line_id: &str,
+) -> Result<RepackConnector> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::QueryStockLine,
+            store_id: Some(store_id.clone()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.clone(), user.user_id)?;
+    let repack_service = &service_provider.repack_service;
+
+    let repacks = repack_service.get_repacks_by_stock_line(&service_context, stock_line_id)?;
+
+    let response = RepackConnector::from_vec(repacks);
 
     Ok(response)
 }
