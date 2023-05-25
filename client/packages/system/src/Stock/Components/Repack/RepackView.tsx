9ca83c824a0b9f778dfa0ba89561c9a4db093e57
roxy-dao <roxy@msupply.foundation>
2023-05-25 16:14:19 +1200

    Add new button to repack view
    Source: https://github.com/msupply-foundation/open-msupply/commit/e5384218d96e02df2c45c06b214227f40f308fe3

--- client/packages/system/src/Stock/Components/Repack/RepackView.tsx
+++ client/packages/system/src/Stock/Components/Repack/RepackView.tsx
@@ -1,25 +1,55 @@
-import React, { FC } from 'react';
-import { useTranslation, DataTable, Box } from '@openmsupply-client/common';
-import { useStock } from '@openmsupply-client/system';
+import React, { FC, useState } from 'react';
+import {
+  useTranslation,
+  DataTable,
+  Box,
+  BaseButton,
+} from '@openmsupply-client/common';
+import { StockLineFragment, useStock } from '@openmsupply-client/system';
+import { RepackFragment } from '../../api';
 import { useRepackColumns } from './column';
 
-export const RepackView: FC<{ recordId: string }> = ({ recordId }) => {
+interface RepackViewProps {
+  stockLine: StockLineFragment | null;
+}
+
+export const RepackView: FC<RepackViewProps> = ({ stockLine }) => {
   const t = useTranslation('inventory');
-  const { data, isError, isLoading } = useStock.repack.list(recordId);
+  const { data, isError, isLoading } = useStock.repack.list(
+    stockLine?.id ?? ''
+  );
   const { columns } = useRepackColumns();
+  const [_invoiceId, setInvoiceId] = useState<string | undefined>(undefined);
+  const [_repackId, setRepackId] = useState<string | undefined>(undefined);
+  const [_isNew, setIsNew] = useState<boolean>(false);
+
+  const onRowClick = (rowData: RepackFragment) => {
+    setInvoiceId(rowData.id);
+    setRepackId(rowData.repackId);
+    setIsNew(false);
+  };
+
+  const onNewClick = () => {
+    setInvoiceId('');
+    setIsNew(true);
+  };
 
   return (
     <Box display={'flex'}>
-      <Box width={'600px'}>
-        <DataTable
-          id="repack-list"
-          columns={columns}
-          data={data?.nodes}
-          isLoading={isLoading}
-          isError={isError}
-          noDataMessage={t('messages.no-repacks')}
-          overflowX="auto"
-        />
+      <Box display={'flex'} flexDirection={'column'} width={'500px'}>
+        <Box paddingBottom={2}>
+          <DataTable
+            id="repack-list"
+            columns={columns}
+            data={data?.nodes}
+            isLoading={isLoading}
+            isError={isError}
+            noDataMessage={t('messages.no-repacks')}
+            overflowX="auto"
+            onRowClick={onRowClick}
+          />
+        </Box>
+        <BaseButton onClick={onNewClick}>New</BaseButton>
       </Box>
     </Box>
   );
