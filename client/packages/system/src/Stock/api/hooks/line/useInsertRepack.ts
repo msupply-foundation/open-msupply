828deae605b6a86018d07749e6bf4757505cf455
roxy-dao <roxy@msupply.foundation>
2023-05-31 14:52:24 +1200

    Invalidate list repacks by stock line on repack insert
    Source: https://github.com/msupply-foundation/open-msupply/commit/eccf1bc8225d01a345e23cebec812d1662dd3fa7

--- client/packages/system/src/Stock/api/hooks/line/useInsertRepack.ts
+++ client/packages/system/src/Stock/api/hooks/line/useInsertRepack.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from 'packages/common/src';
 import { useStockApi } from '../utils/useStockApi';
 
-export const useInsertRepack = () => {
+export const useInsertRepack = (stockLineId: string) => {
   const queryClient = useQueryClient();
   const api = useStockApi();
 
   return useMutation(api.insertRepack, {
     onSuccess: () => {
       queryClient.invalidateQueries(api.keys.list());
+      queryClient.invalidateQueries(
+        api.keys.listRepackByStockLine(stockLineId)
+      );
     },
   });
 };
