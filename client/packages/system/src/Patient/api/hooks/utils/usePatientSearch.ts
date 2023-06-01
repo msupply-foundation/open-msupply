8725ac09caadf24dbadf908687fd38094afaf8aa
Clemens Zeidler <clemens@msupply.foundation>
2023-06-02 11:18:52 +1200

    Update patient FE API
    Source: https://github.com/msupply-foundation/open-msupply/commit/da8e66cc5d60c17fe19cef864448b7e97d9bccbb

--- client/packages/system/src/Patient/api/hooks/utils/usePatientSearch.ts
+++ client/packages/system/src/Patient/api/hooks/utils/usePatientSearch.ts
@@ -1,9 +1,14 @@
-import { PatientSearchInput, useMutation } from '@openmsupply-client/common';
+import { PatientSearchInput, useQuery } from '@openmsupply-client/common';
 import { usePatientApi } from '../utils/usePatientApi';
 
-export const usePatientSearch = () => {
+export const usePatientSearch = (
+  params: PatientSearchInput,
+  enabled?: boolean
+) => {
   const api = usePatientApi();
   return {
-    ...useMutation((input: PatientSearchInput) => api.get.search(input)),
+    ...useQuery(api.keys.search(params), () => api.get.search(params), {
+      enabled,
+    }),
   };
 };
