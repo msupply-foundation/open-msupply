bc3063894a898c02b361decea1778eb93267275f
Clemens Zeidler <clemens@msupply.foundation>
2023-06-02 15:26:46 +1200

    Add support for enabling/disabling the sync status query
    Source: https://github.com/msupply-foundation/open-msupply/commit/32280082d95d96cbd743d0dc68845a700dc0d195

--- client/packages/system/src/Sync/api/hooks/utils/useSyncStatus.ts
+++ client/packages/system/src/Sync/api/hooks/utils/useSyncStatus.ts
@@ -1,11 +1,15 @@
 import { useQuery } from '@openmsupply-client/common';
 import { useHostApi } from './useHostApi';
 
-export const useSyncStatus = (refetchInterval: number | false = false) => {
+export const useSyncStatus = (
+  refetchInterval: number | false = false,
+  enabled?: boolean
+) => {
   const api = useHostApi();
 
   return useQuery(api.keys.syncStatus(), api.get.syncStatus, {
     cacheTime: 0,
     refetchInterval,
+    enabled,
   });
 };
