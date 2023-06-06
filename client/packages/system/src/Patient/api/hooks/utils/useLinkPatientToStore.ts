d7648270bb21477493d5407f78bd0048c3699f46
Clemens <88299195+clemens-msupply@users.noreply.github.com>
2023-06-06 14:12:20 +1200

    Update client/packages/system/src/Patient/api/hooks/utils/useLinkPatientToStore.ts
    Co-authored-by: Mark Prins <9192912+mark-prins@users.noreply.github.com>
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/25d8951e63df3d18365e60d70495a30a1bf95354

--- client/packages/system/src/Patient/api/hooks/utils/useLinkPatientToStore.ts
+++ client/packages/system/src/Patient/api/hooks/utils/useLinkPatientToStore.ts
@@ -3,7 +3,5 @@ import { usePatientApi } from '../utils/usePatientApi';
 
 export const useLinkPatientToStore = () => {
   const api = usePatientApi();
-  return {
-    ...useMutation((nameId: string) => api.linkPatientToStore(nameId)),
-  };
+  return useMutation((nameId: string) => api.linkPatientToStore(nameId));
 };
