ff0d547c9967887911a3b97d3cc813b0e172c134
roxy-dao <roxy@msupply.foundation>
2023-06-12 15:04:49 +1200

    Add log level to key value store

---
+++ server/repository/src/migrations/v1_01_16/key_value_store_update.rs
@@ -0,0 +1,15 @@
+use crate::StorageConnection;
+
+#[cfg(feature = "postgres")]
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    use crate::migrations::sql;
+
+    sql!(connection, r#"ALTER TYPE key_type ADD VALUE 'LOG_LEVEL';"#)?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "postgres"))]
+pub(crate) fn migrate(_connection: &StorageConnection) -> anyhow::Result<()> {
+    Ok(())
+}
