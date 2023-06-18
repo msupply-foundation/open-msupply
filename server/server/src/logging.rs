eceb167d736879414feca0236ccaef90c61d94de
roxy-dao <roxy@msupply.foundation>
2023-06-19 10:54:35 +1200

    Change log crate
    Source: https://github.com/msupply-foundation/open-msupply/commit/e97589a4972e30213b4ee77ec523cb0913484e26

--- server/server/src/logging.rs
+++ server/server/src/logging.rs
@@ -1,12 +1,7 @@
 use std::env;
 
-use fast_log::{
-    consts::LogSize,
-    plugin::{file_split::RollingType, packer::LogPacker},
-    Config as LogConfig,
-};
-use log::LevelFilter;
 use service::settings::{Level, LogMode, LoggingSettings};
+use simple_log::{LogConfig, LogConfigBuilder};
 
 pub fn logging_init(
     settings: Option<LoggingSettings>,
@@ -17,23 +12,32 @@ pub fn logging_init(
         LogMode::Console,
         service::settings::Level::Info,
     ));
+
+    let log_level = level.unwrap_or(settings.level.clone());
     let mut config = match settings.mode {
-        LogMode::File => file_logger(&settings),
-        LogMode::Console => LogConfig::new().console(),
-        LogMode::All => file_logger(&settings).console(),
+        LogMode::File => file_logger(&settings)
+            .level(log_level.to_string())
+            .output_file()
+            .build(),
+        LogMode::Console => LogConfigBuilder::builder()
+            .level(log_level.to_string())
+            .output_console()
+            .build(),
+        LogMode::All => file_logger(&settings)
+            .level(log_level.to_string())
+            .output_console()
+            .output_file()
+            .build(),
     };
 
     if let Some(apply_config) = apply_config {
         config = apply_config(config);
     }
 
-    let log_level = level.unwrap_or(settings.level.clone());
-
-    fast_log::init(config.level(LevelFilter::from(log_level)))
-        .expect("Unable to initialise logger");
+    simple_log::new(config).expect("Unable to initialise logger");
 }
 
-fn file_logger(settings: &LoggingSettings) -> LogConfig {
+fn file_logger(settings: &LoggingSettings) -> LogConfigBuilder {
     let default_log_file = "remote_server.log".to_string();
     let default_log_dir = "log".to_string();
     let default_max_file_count = 10;
@@ -55,18 +59,8 @@ fn file_logger(settings: &LoggingSettings) -> LogConfig {
     let max_file_count = settings.max_file_count.unwrap_or(default_max_file_count);
     let max_file_size = settings.max_file_size.unwrap_or(default_max_file_size);
 
-    // file_loop will append to the specified log file until the max size is reached,
-    // then create a new log file with the same name, with date and time appended
-    // file_split will split the temp file when the max file size is reached
-    // and retain the max number of files while the server is running
-    // Note: when the server is started, the temp files are removed. The main log file is
-    // appended to, but only to the max size limit. Only one additional main log is created
-    LogConfig::new()
-        .file_split(
-            &log_dir,
-            LogSize::MB(max_file_size),
-            RollingType::KeepNum(max_file_count),
-            LogPacker {},
-        )
-        .file_loop(&log_file, LogSize::MB(max_file_size))
+    LogConfigBuilder::builder()
+        .path(&log_file)
+        .size(max_file_size as u64)
+        .roll_count(max_file_count as u32)
 }
