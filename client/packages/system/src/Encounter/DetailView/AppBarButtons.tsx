3a21d635d982abb870bfb9ab2ed0930c6d9551c4
roxy-dao <roxy@msupply.foundation>
2023-06-27 17:12:49 +1200

    Load appointment status on encounter detail view

--- client/packages/system/src/Encounter/DetailView/AppBarButtons.tsx
+++ client/packages/system/src/Encounter/DetailView/AppBarButtons.tsx
@@ -2,15 +2,25 @@ import React, { FC } from 'react';
 import {
   AppBarButtonsPortal,
   Grid,
+  Typography,
   useDetailPanel,
 } from '@openmsupply-client/common';
 
-export const AppBarButtonsComponent: FC = () => {
+interface AppBarButtonsProp {
+  status?: string | null;
+}
+
+export const AppBarButtonsComponent: FC<AppBarButtonsProp> = ({ status }) => {
   const { OpenButton } = useDetailPanel();
 
   return (
     <AppBarButtonsPortal>
       <Grid container gap={1}>
+        {!!status && (
+          <Typography color={'secondary.main'} padding={1} fontWeight={'bold'}>
+            {status}
+          </Typography>
+        )}
         {OpenButton}
       </Grid>
     </AppBarButtonsPortal>
