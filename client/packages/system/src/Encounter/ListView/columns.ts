a9884ef66b6ab57ab2bd27fb968fff5d474bb600
roxy-dao <roxy@msupply.foundation>
2023-06-30 12:45:11 +1200

    Refactor + move functions
    Source: https://github.com/msupply-foundation/open-msupply/commit/500ad64af89a16ac4d991c747b5b1ecf586045fb

--- client/packages/system/src/Encounter/ListView/columns.ts
+++ client/packages/system/src/Encounter/ListView/columns.ts
@@ -4,22 +4,43 @@ import {
   Column,
   ColumnDescription,
   SortBy,
+  ColumnDataAccessor,
+  EncounterNodeStatus,
 } from '@openmsupply-client/common';
-import { useFormatDateTime } from '@common/intl';
+import { useFormatDateTime, useTranslation } from '@common/intl';
 import {
   EncounterRowFragment,
   useDocumentRegistry,
 } from '@openmsupply-client/programs';
-import {
-  getAdditionalInformationColumn,
-  getEncounterEventCell,
-} from './AdditionalInformationColumn';
+import { getAdditionalInformationColumn } from './AdditionalInformationColumn';
+import { useLogicalStatus } from '../utils';
 
 interface useEncounterListColumnsProps {
   onChangeSortBy: (column: Column<any>) => void;
   sortBy: SortBy<EncounterRowFragment>;
   includePatient?: boolean;
 }
+
+export const encounterAdditionalInfoAccessor: ColumnDataAccessor<
+  EncounterRowFragment
+> = ({ rowData }): string[] => {
+  const t = useTranslation();
+  const additionalInfo = [];
+
+  if (rowData?.events[0]?.data) {
+    additionalInfo.push(rowData.events[0].data);
+  }
+
+  if (rowData?.status === EncounterNodeStatus.Pending) {
+    const startDatetime = new Date(rowData?.startDatetime);
+    const status = useLogicalStatus(startDatetime, t);
+    if (status) {
+      additionalInfo.push(status);
+    }
+  }
+
+  return additionalInfo;
+};
 
 export const useEncounterListColumns = ({
   onChangeSortBy,
@@ -75,7 +96,9 @@ export const useEncounterListColumns = ({
       label: 'label.patient',
       accessor: ({ rowData }) => rowData?.patient?.name,
     });
-  columnList.push(getAdditionalInformationColumn(getEncounterEventCell));
+  columnList.push(
+    getAdditionalInformationColumn(encounterAdditionalInfoAccessor)
+  );
   columnList.push({
     key: 'effectiveStatus',
     label: 'label.status',
