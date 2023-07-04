cd916e7a1a340378912aa9c8039229fb6572de7e
roxy-dao <roxy@msupply.foundation>
2023-07-04 16:08:07 +1200

    Add Prescription number type
    Source: https://github.com/msupply-foundation/open-msupply/commit/7a191abaf15c4c29e1c43b996dd7d6ed7c1c00ce

---
+++ server/repository/src/migrations/v1_02_01/number_type.rs
@@ -0,0 +1,18 @@
+use crate::StorageConnection;
+
+#[cfg(feature = "postgres")]
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    use crate::migrations::sql;
+
+    sql!(
+        connection,
+        r#"ALTER TYPE number_type ADD VALUE 'PRESCRIPTION';"#
+    )?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "postgres"))]
+pub(crate) fn migrate(_connection: &StorageConnection) -> anyhow::Result<()> {
+    Ok(())
+}
