8bebc9249fb0a63f48a02e8e50b2218fc81dd257
roxy-dao <roxy@msupply.foundation>
2023-07-05 12:46:33 +1200

    Tests for insert prescription
    Source: https://github.com/msupply-foundation/open-msupply/commit/ce6d3747d332c5500472b42a165b805c4546a0fe

--- server/repository/src/mock/test_name_store_id.rs
+++ server/repository/src/mock/test_name_store_id.rs
@@ -1,6 +1,6 @@
 use util::inline_init;
 
-use crate::{NameRow, NameStoreJoinRow, StoreRow};
+use crate::{NameRow, NameStoreJoinRow, NameType, StoreRow};
 
 use super::MockData;
 
@@ -26,6 +26,15 @@ pub fn mock_test_name_store_id() -> MockData {
     result
         .name_store_joins
         .push(mock_name_not_linked_to_store_join_a());
+    result.names.push(mock_patient_linked_to_store());
+    result.stores.push(mock_store_linked_to_patient());
+    result
+        .name_store_joins
+        .push(mock_patient_linked_to_store_join());
+    result.names.push(mock_patient_not_linked_to_store());
+    result
+        .name_store_joins
+        .push(mock_patient_not_linked_to_store_join());
 
     result
 }
@@ -124,6 +133,58 @@ pub fn mock_name_not_linked_to_store_join_a() -> NameStoreJoinRow {
         id: "name_not_linked_to_store_a_id".to_owned(),
         name_id: "name_not_linked_to_store_a_id".to_owned(),
         store_id: "store_c".to_owned(),
+        name_is_customer: true,
+        name_is_supplier: true,
+        is_sync_update: false,
+    }
+}
+
+pub fn mock_patient_linked_to_store() -> NameRow {
+    inline_init(|r: &mut NameRow| {
+        r.id = "patient_linked_to_store_id".to_owned();
+        r.name = "Patient linked to store".to_owned();
+        r.code = "mock_patient_linked_to_store_code".to_owned();
+        r.is_customer = true;
+        r.is_supplier = true;
+        r.r#type = NameType::Patient;
+    })
+}
+
+pub fn mock_store_linked_to_patient() -> StoreRow {
+    inline_init(|s: &mut StoreRow| {
+        s.id = "patient_store_id".to_string();
+        s.name_id = "patient_linked_to_store_id".to_string();
+        s.code = "patient_store_code".to_string();
+    })
+}
+
+pub fn mock_patient_linked_to_store_join() -> NameStoreJoinRow {
+    NameStoreJoinRow {
+        id: "patient_linked_to_store_id".to_owned(),
+        name_id: "patient_linked_to_store_id".to_owned(),
+        store_id: "store_a".to_owned(),
+        name_is_customer: true,
+        name_is_supplier: true,
+        is_sync_update: false,
+    }
+}
+
+pub fn mock_patient_not_linked_to_store() -> NameRow {
+    inline_init(|r: &mut NameRow| {
+        r.id = "patient_not_linked_to_store_id".to_owned();
+        r.name = "Patient not linked to store".to_owned();
+        r.code = "patient_not_linked_to_store_code".to_owned();
+        r.is_customer = true;
+        r.is_supplier = true;
+        r.r#type = NameType::Patient;
+    })
+}
+
+pub fn mock_patient_not_linked_to_store_join() -> NameStoreJoinRow {
+    NameStoreJoinRow {
+        id: "patient_not_linked_to_store_id".to_owned(),
+        name_id: "patient_not_linked_to_store_id".to_owned(),
+        store_id: "store_a".to_owned(),
         name_is_customer: true,
         name_is_supplier: true,
         is_sync_update: false,
