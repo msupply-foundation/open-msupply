41ff128d0b739707c1b1f530dd99e4cfafe2d270
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-07-18 16:47:52 +1200

    Update BMI.tsx
    Source: https://github.com/msupply-foundation/open-msupply/commit/47b79de7ab4a7df4167bbf04da9957d8a290b05f

--- client/packages/programs/src/JsonForms/components/BMI.tsx
+++ client/packages/programs/src/JsonForms/components/BMI.tsx
@@ -8,19 +8,57 @@ import {
 import { withJsonFormsControlProps } from '@jsonforms/react';
 import { DetailInputWithLabelRow, NumUtils } from '@openmsupply-client/common';
 import { FORM_LABEL_WIDTH } from '../common';
+import { useEncounter, useProgramEvents } from '../../api';
 
 export const bmiTester = rankWith(10, uiTypeIs('BMI'));
 
 const round = (value: number) => Math.round(value * 100) / 100;
 
+const usePreviousHeight = (
+  formData: any | undefined,
+  eventType: string | undefined
+) => {
+  // fetch current encounter
+  const encounterId = useEncounter.utils.idFromUrl();
+  const { data: currentEncounter } = useEncounter.document.byId(encounterId);
+  const { data: events } = useProgramEvents.document.list(
+    {
+      // at: beforeDate,
+      filter: {
+        patientId: { equalTo: currentEncounter?.patient?.id ?? '' },
+        type: {
+          equalTo: 'physicalExaminationHeight',
+        },
+        documentType: {
+          equalTo: currentEncounter?.type,
+        },
+      },
+      page: {
+        first: 1,
+      },
+    },
+    !!currentEncounter && !!eventType
+  );
+
+  if (formData && formData.height) return formData.height;
+
+  const event = events?.nodes[0];
+  if (event?.data === undefined || event?.data === null) {
+    return undefined;
+  }
+  return Number.parseFloat(event.data);
+};
+
 const UIComponent = (props: ControlProps) => {
   const { data, handleChange, label, path } = props;
-  const { height, weight } = data ?? {};
+  const { weight } = data ?? {};
+
+  const height = usePreviousHeight(data, 'physicalExaminationHeight');
 
   useEffect(() => {
-    if (!data) return;
+    if (!height || !weight) return;
 
-    const h = NumUtils.parseString(height);
+    const h = height;
     const w = NumUtils.parseString(weight);
 
     if (!handleChange || !w) return;
