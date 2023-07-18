7968eb57b6b7e32a918d9ae7edae91ab782bf5c9
Mark Prins <mark@malcolmprins.net>
2023-07-19 11:45:03 +1200

    test call rather than start /wait
    Source: https://github.com/msupply-foundation/open-msupply/commit/bf7c8db45cf3aa5ddf991ee2e7ac83f19e072173

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -14,18 +14,18 @@ xcopy "build\windows\demo" "omSupply\demo" /c /y /i
 copy "version.txt" "omSupply\version.txt"
 
 @cd "build\windows"
-start /b /wait omsupply-prepare.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM start /b /wait omsupply-prepare.bat
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-start /b /wait omsupply-sqlite-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+call omsupply-sqlite-build.bat
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-start /b /wait omsupply-postgres-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM start /b /wait omsupply-postgres-build.bat
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-start /b /wait omsupply-desktop-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM start /b /wait omsupply-desktop-build.bat
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd "..\..\server"
-start /wait cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-if %errorlevel% neq 0 exit /b %errorlevel%
+@REM cd "..\..\server"
+@REM start /wait cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
