1e78bf175fe420ee4d411f501f46558706be4e6c
Mark Prins <mark@malcolmprins.net>
2023-07-19 10:37:52 +1200

    surface errorlevel from batches
    Source: https://github.com/msupply-foundation/open-msupply/commit/181d387d3fd9b40c46a3d733edca9e72c9f26db1

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -15,8 +15,17 @@ copy "version.txt" "omSupply\version.txt"
 
 @cd "build\windows"
 start /b /wait omsupply-prepare.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 start /b /wait omsupply-sqlite-build.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 start /b /wait omsupply-postgres-build.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 start /b /wait omsupply-desktop-build.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 cd "..\..\server"
 start /wait cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/omsupply-postgres-build.bat
+++ build/windows/omsupply-postgres-build.bat
@@ -1,2 +1,3 @@
 @ECHO ##### Building omsupply for the postgres #####
 cd "..\..\server" && cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+if %errorlevel% neq 0 exit /b %errorlevel%
