8e723c3362da8cabee9a5d04ecdb5bd0d24a346f
Mark Prins <mark@malcolmprins.net>
2023-07-19 12:31:51 +1200

    remove sub batch files
    Source: https://github.com/msupply-foundation/open-msupply/commit/287b2436e2bc7317b8dc3cb684254c1185307090

--- build/windows/omsupply-desktop-build.bat
+++
@@ -1,3 +0,0 @@
-@ECHO ##### Building omSupply for the desktop #####
-cd "..\..\client" && yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
-if %errorlevel% neq 0 exit /b %errorlevel%
