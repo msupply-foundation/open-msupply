287b2436e2bc7317b8dc3cb684254c1185307090
Mark Prins <mark@malcolmprins.net>
2023-07-19 12:31:51 +1200

    remove sub batch files

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -13,19 +13,21 @@ xcopy "build\windows\*.*" "omSupply" /c
 xcopy "build\windows\demo" "omSupply\demo" /c /y /i
 copy "version.txt" "omSupply\version.txt"
 
-@cd "build\windows"
-call omsupply-prepare.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Prepare omsupply build #####
+cd "client" && yarn install --force --frozen-lockfile && yarn build
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-call omsupply-sqlite-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omsupply for the sqlite #####
+cd "server" && cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-call omsupply-postgres-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omsupply for the postgres #####
+cd "server" && cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-call omsupply-desktop-build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omSupply for the desktop #####
+cd "client" && yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd "..\..\server"
-call cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-if %errorlevel% neq 0 exit /b %errorlevel%
+cd "server" && cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
--- build/windows/omsupply-prepare.bat
+++
@@ -1,3 +0,0 @@
-@ECHO ##### Prepare omsupply build #####
-cd "..\..\client" && yarn install --force --frozen-lockfile && yarn build
-if %errorlevel% neq 0 exit /b %errorlevel%
