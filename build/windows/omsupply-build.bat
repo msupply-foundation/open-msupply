10aa89930c91b9322e4345927e4ff5dac44ec4f5
Mark Prins <mark@malcolmprins.net>
2023-07-19 12:38:03 +1200

    remove prepare step

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -13,9 +13,9 @@ xcopy "build\windows\*.*" "omSupply" /c
 xcopy "build\windows\demo" "omSupply\demo" /c /y /i
 copy "version.txt" "omSupply\version.txt"
 
-@ECHO ##### Prepare omsupply build #####
-cd "client" && yarn install --force --frozen-lockfile && yarn build
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Prepare omsupply build #####
+@REM cd "client" && yarn install --force --frozen-lockfile && yarn build
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omsupply for the sqlite #####
 cd "server" && cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
