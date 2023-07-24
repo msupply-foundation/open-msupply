c5974144a4c0eb8ba2d22897d7d888bcf6853fa9
Mark Prins <mark@malcolmprins.net>
2023-07-24 17:24:56 +1200

    fix batch
    Source: https://github.com/msupply-foundation/open-msupply/commit/da207b1ac960afad6d06e2ffe9cc200fd94af46e

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -17,16 +17,16 @@ start /wait /b build\windows\omsupply-prepare.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omsupply for the sqlite #####
-cd "server" && cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+cd cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omsupply for the postgres #####
-cd "server" && cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+cd cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omSupply for the desktop #####
-cd "client" && yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
+cd "..\client" && yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd "server" && cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+cd "..\server" && cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
 @if %errorlevel% neq 0 exit /b %errorlevel%
