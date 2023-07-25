5aa4ee021e11508fc8a223533e6c54a3cb353349
adrianwb <adrian@sussol.net>
2023-07-25 17:42:38 +0100

    New sensor service module

---
+++ server/service/src/sensor/mod.rs
@@ -0,0 +1,67 @@
+use self::{
+    delete::{delete_sensor, DeleteSensor, DeleteSensorError},
+    insert::{insert_sensor, InsertSensor, InsertSensorError},
+    query::{get_sensor, get_sensors},
+    update::{update_sensor, UpdateSensor, UpdateSensorError},
+};
+
+use super::{ListError, ListResult};
+use crate::{service_provider::ServiceContext, SingleRecordError};
+use repository::sensor::{Sensor, SensorFilter, SensorSort};
+use repository::PaginationOption;
+
+pub mod delete;
+pub mod insert;
+pub mod query;
+pub mod update;
+mod validate;
+
+pub trait SensorServiceTrait: Sync + Send {
+    fn get_sensors(
+        &self,
+        ctx: &ServiceContext,
+        pagination: Option<PaginationOption>,
+        filter: Option<SensorFilter>,
+        sort: Option<SensorSort>,
+    ) -> Result<ListResult<Sensor>, ListError> {
+        get_sensors(ctx, pagination, filter, sort)
+    }
+
+    fn get_sensor(
+        &self,
+        ctx: &ServiceContext,
+        id: String,
+    ) -> Result<Sensor, SingleRecordError> {
+        get_sensor(ctx, id)
+    }
+
+    fn delete_sensor(
+        &self,
+        ctx: &ServiceContext,
+        input: DeleteSensor,
+    ) -> Result<String, DeleteSensorError> {
+        delete_sensor(ctx, input)
+    }
+
+    fn insert_sensor(
+        &self,
+        ctx: &ServiceContext,
+        input: InsertSensor,
+    ) -> Result<Sensor, InsertSensorError> {
+        insert_sensor(ctx, input)
+    }
+
+    fn update_sensor(
+        &self,
+        ctx: &ServiceContext,
+        input: UpdateSensor,
+    ) -> Result<Sensor, UpdateSensorError> {
+        update_sensor(ctx, input)
+    }
+}
+
+pub struct SensorService {}
+impl SensorServiceTrait for SensorService {}
+
+#[cfg(test)]
+mod tests;
