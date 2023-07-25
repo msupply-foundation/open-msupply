cc551d11f89c85b9b9a95d8e48e49f35f508cba0
adrianwb <adrian@sussol.net>
2023-07-25 12:30:52 +0100

    GraphQL changes for sensor
    Source: https://github.com/msupply-foundation/open-msupply/commit/d07fc5d0e7c5a50d6cfd14a22ee0bffe029d6acf

---
+++ server/graphql/sensor/src/lib.rs
@@ -0,0 +1,407 @@
+mod mutations;
+use self::mutations::*;
+
+use async_graphql::*;
+use graphql_core::{
+    pagination::PaginationInput,
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+use graphql_types::types::*;
+use repository::{sensor::sensorFilter, EqualFilter, PaginationOption};
+use service::auth::{Resource, ResourceAccessRequest};
+
+#[derive(Default, Clone)]
+pub struct SensorQueries;
+
+#[Object]
+impl SensorQueries {
+    /// Query omSupply "sensor" entries
+    pub async fn sensors(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        #[graphql(desc = "Pagination option (first and offset)")] page: Option<PaginationInput>,
+        #[graphql(desc = "Filter option")] filter: Option<SensorFilterInput>,
+        #[graphql(desc = "Sort options (only first sort input is evaluated for this endpoint)")]
+        sort: Option<Vec<SensorSortInput>>,
+    ) -> Result<SensorsResponse> {
+        let user = validate_auth(
+            ctx,
+            &ResourceAccessRequest {
+                resource: Resource::QuerySensor,
+                store_id: Some(store_id.clone()),
+            },
+        )?;
+
+        let service_provider = ctx.service_provider();
+        let service_context = service_provider.context(store_id.clone(), user.user_id)?;
+
+        // always filter by store_id
+        let filter = filter
+            .map(SensorFilter::from)
+            .unwrap_or(SensorFilter::new())
+            .store_id(EqualFilter::equal_to(&store_id));
+
+        let sensors = service_provider
+            .sensor_service
+            .get_sensors(
+                &service_context,
+                page.map(PaginationOption::from),
+                Some(filter),
+                // Currently only one sort option is supported, use the first from the list.
+                sort.and_then(|mut sort_list| sort_list.pop())
+                    .map(|sort| sort.to_domain()),
+            )
+            .map_err(StandardGraphqlError::from_list_error)?;
+
+        Ok(SensorsResponse::Response(SensorConnector::from_domain(
+            sensors,
+        )))
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct SensorMutations;
+
+#[Object]
+impl SensorMutations {
+    async fn insert_sensor(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        input: InsertSensorInput,
+    ) -> Result<InsertSensorResponse> {
+        insert_sensor(ctx, &store_id, input)
+    }
+
+    async fn update_sensor(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        input: UpdateSensorInput,
+    ) -> Result<UpdateSensorResponse> {
+        update_sensor(ctx, &store_id, input)
+    }
+
+    async fn delete_sensor(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        input: DeleteSensorInput,
+    ) -> Result<DeleteSensorResponse> {
+        delete_sensor(ctx, &store_id, input)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use async_graphql::EmptyMutation;
+    use graphql_core::assert_graphql_query;
+    use graphql_core::test_helpers::setup_graphl_test;
+    use repository::mock::mock_sensors;
+    use repository::{
+        sensor::{Sensor, SensorFilter, SensorSort, SensorSortField},
+        mock::MockDataInserts,
+        SensorRow, StorageConnectionManager,
+    };
+    use repository::{EqualFilter, PaginationOption, Sort};
+    use serde_json::json;
+
+    use service::{
+        sensor::SensorServiceTrait,
+        service_provider::{ServiceContext, ServiceProvider},
+        ListError, ListResult,
+    };
+
+    use crate::SensorQueries;
+
+    type GetSensors = dyn Fn(
+            Option<PaginationOption>,
+            Option<SensorFilter>,
+            Option<SensorSort>,
+        ) -> Result<ListResult<Sensor>, ListError>
+        + Sync
+        + Send;
+
+    pub struct TestService(pub Box<GetSensors>);
+
+    impl SensorServiceTrait for TestService {
+        fn get_sensors(
+            &self,
+            _: &ServiceContext,
+            pagination: Option<PaginationOption>,
+            filter: Option<SensorFilter>,
+            sort: Option<SensorSort>,
+        ) -> Result<ListResult<Sensor>, ListError> {
+            (self.0)(pagination, filter, sort)
+        }
+    }
+
+    pub fn service_provider(
+        sensor_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.sensor_service = Box::new(sensor_service);
+        service_provider
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_locations_success() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            SensorQueries,
+            EmptyMutation,
+            "test_graphql_sensors_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let query = r#"
+        query {
+            sensors(storeId: \"store_a\") {
+              ... on SensorConnector {
+                nodes {
+                  id
+                  name
+                  serial
+                  is_active
+                }
+                totalCount
+              }
+            }
+        }
+        "#;
+
+        // Test single record
+        let test_service = TestService(Box::new(|_, _, _| {
+            Ok(ListResult {
+                rows: vec![Sensor {
+                    sensor_row: SensorRow {
+                        id: "active_sensor".to_owned(),
+                        name: "test_name".to_owned(),
+                        serial: "test_serial".to_owned(),
+                        is_active: true,
+                        store_id: "store_a".to_owned(),
+                    },
+                }],
+                count: 1,
+            })
+        }));
+
+        let expected = json!({
+              "sensors": {
+                  "nodes": [
+                      {
+                          "id": "active_sensor",
+                          "name": "test_name",
+                          "serial": "test_serial",
+                          "is_active": true,
+                      },
+                  ],
+                  "totalCount": 1
+              }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            query,
+            &None,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Test no records
+
+        let test_service = TestService(Box::new(|_, _, _| {
+            Ok(ListResult {
+                rows: Vec::new(),
+                count: 0,
+            })
+        }));
+
+        let expected = json!({
+              "sensors": {
+                  "nodes": [
+
+                  ],
+                  "totalCount": 0
+              }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            query,
+            &None,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_locations_inputs() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            sensorQueries,
+            EmptyMutation,
+            "test_graphql_sensor_inputs",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let query = r#"
+        query(
+            $sort: [SensorSortInput]
+            $filter: SensorFilterInput
+          ) {
+            sensors(sort: $sort, filter: $filter, storeId: \"store_a\") {
+              __typename
+            }
+          }
+
+        "#;
+
+        let expected = json!({
+              "sensors": {
+                  "__typename": "SensorConnector"
+              }
+          }
+        );
+
+        // Test sort by name no desc
+        let test_service = TestService(Box::new(|_, _, sort| {
+            assert_eq!(
+                sort,
+                Some(Sort {
+                    key: SensorSortField::Name,
+                    desc: None
+                })
+            );
+            Ok(ListResult::empty())
+        }));
+
+        let variables = json!({
+          "sort": [{
+            "key": "name",
+          }]
+        });
+
+        assert_graphql_query!(
+            &settings,
+            query,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Test sort by code with desc
+        let test_service = TestService(Box::new(|_, _, sort| {
+            assert_eq!(
+                sort,
+                Some(Sort {
+                    key: SensorSortField::Serial,
+                    desc: Some(true)
+                })
+            );
+            Ok(ListResult::empty())
+        }));
+
+        let variables = json!({
+          "sort": [{
+            "key": "serial",
+            "desc": true
+          }]
+        });
+
+        assert_graphql_query!(
+            &settings,
+            query,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Test filter
+        let test_service = TestService(Box::new(|_, filter, _| {
+            assert_eq!(
+                filter,
+                Some(
+                    SensorFilter::new()
+                        .store_id(EqualFilter::equal_to("store_a"))
+                        .name(EqualFilter::equal_to("match_name"))
+                )
+            );
+            Ok(ListResult::empty())
+        }));
+
+        let variables = json!({
+          "filter": {
+            "name": { "equalTo": "match_name"},
+          }
+        });
+
+        assert_graphql_query!(
+            &settings,
+            query,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_sensors_always_filtered_by_store() {
+        let (_, _, _, settings) = setup_graphl_test(
+            SensorQueries,
+            EmptyMutation,
+            "test_sensors_always_filtered_by_store",
+            MockDataInserts::none().stores().locations().sensors(),
+        )
+        .await;
+
+        let count_store_a = mock_sensors()
+            .iter()
+            .filter(|v| v.store_id == "store_a")
+            .count();
+        let count_store_b = mock_sensors()
+            .iter()
+            .filter(|v| v.store_id == "store_b")
+            .count();
+        assert!(count_store_a != count_store_b);
+
+        let query = r#"
+        query {
+            sensors(storeId: \"store_a\") {
+              ... on SensorConnector {
+                totalCount
+              }
+            }
+        }
+        "#;
+        let expected = json!({
+              "sensors": {
+                  "totalCount": count_store_a
+              }
+          }
+        );
+        assert_graphql_query!(&settings, query, &None, &expected, None);
+
+        let query = r#"
+        query {
+            sensors(storeId: \"store_b\") {
+              ... on SensorConnector {
+                totalCount
+              }
+            }
+        }
+        "#;
+        let expected = json!({
+              "sensors": {
+                  "totalCount": count_store_b
+              }
+          }
+        );
+        assert_graphql_query!(&settings, query, &None, &expected, None);
+    }
+}
