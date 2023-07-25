d07fc5d0e7c5a50d6cfd14a22ee0bffe029d6acf
adrianwb <adrian@sussol.net>
2023-07-25 12:30:52 +0100

    GraphQL changes for sensor

---
+++ server/graphql/sensor/src/mutations/delete.rs
@@ -0,0 +1,328 @@
+use async_graphql::*;
+use graphql_core::{
+    simple_generic_errors::{DatabaseError, RecordBelongsToAnotherStore, RecordNotFound},
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+
+//use graphql_types::types::{DeleteResponse, InvoiceLineConnector, StockLineConnector};
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    sensor::delete::{DeleteSensor, DeleteSensorError as ServiceError},
+};
+
+pub fn delete_sensor(
+    ctx: &Context<'_>,
+    store_id: &str,
+    input: DeleteSensorInput,
+) -> Result<DeleteSensorResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::MutateSensor,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    match service_provider
+        .sensor_service
+        .delete_sensor(&service_context, input.into())
+    {
+        Ok(sensor_id) => Ok(DeleteSensorResponse::Response(DeleteResponse(
+            sensor_id,
+        ))),
+        Err(error) => Ok(DeleteSensorResponse::Error(DeleteSensorError {
+            error: map_error(error)?,
+        })),
+    }
+}
+
+#[derive(InputObject)]
+pub struct DeleteSensorInput {
+    pub id: String,
+}
+
+impl From<DeleteSensorInput> for DeleteSensor {
+    fn from(DeleteSensorInput { id }: DeleteSensorInput) -> Self {
+        DeleteSensor { id }
+    }
+}
+
+#[derive(SimpleObject)]
+pub struct DeleteSensorError {
+    pub error: DeleteSensorErrorInterface,
+}
+
+#[derive(Union)]
+pub enum DeleteSensorResponse {
+    Error(DeleteSensorError),
+    Response(DeleteResponse),
+}
+
+#[derive(Interface)]
+#[graphql(field(name = "serial", type = "String"))]
+pub enum DeleteSensorErrorInterface {
+    SensorNotFound(RecordNotFound),
+    RecordBelongsToAnotherStore(RecordBelongsToAnotherStore),
+    SensorInUse(SensorInUse),
+    DatabaseError(DatabaseError),
+}
+
+fn map_error(error: ServiceError) -> Result<DeleteSensorErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Structured Errors
+        ServiceError::SensorInUse(sensor_in_use) => {
+            return Ok(DeleteSensorErrorInterface::SensorInUse(SensorInUse {
+                //stock_lines: StockLineConnector::from_vec(location_in_use.stock_lines),
+                //invoice_lines: InvoiceLineConnector::from_vec(location_in_use.invoice_lines),
+            }));
+        }
+
+        // Standard Graphql Errors
+        ServiceError::LocationDoesNotExist => BadUserInput(formatted_error),
+        ServiceError::LocationDoesNotBelongToCurrentStore => BadUserInput(formatted_error),
+        ServiceError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
+}
+
+pub struct SensorInUse {
+//    stock_lines: StockLineConnector,
+//    invoice_lines: InvoiceLineConnector,
+}
+
+#[Object]
+impl SensorInUse {
+    pub async fn serial(&self) -> &'static str {
+        "Sensor in use"
+    }
+
+    //pub async fn stock_lines(&self) -> &StockLineConnector {
+    //    &self.stock_lines
+    //}
+
+    //pub async fn invoice_lines(&self) -> &InvoiceLineConnector {
+    //    &self.invoice_lines
+    //}
+}
+
+#[cfg(test)]
+mod test {
+    use async_graphql::EmptyMutation;
+    use graphql_core::{
+        assert_graphql_query, assert_standard_graphql_error, test_helpers::setup_graphl_test,
+    };
+    use repository::{
+        mock::{
+            //mock_item_a, mock_outbound_shipment_a, mock_outbound_shipment_a_invoice_lines,
+            MockDataInserts,
+        },
+        //InvoiceLine, StockLine, 
+        StorageConnectionManager,
+    };
+    use serde_json::json;
+
+    use service::{
+        sensor::{
+            delete::{DeleteSensor, DeleteSensorError, SensorInUse},
+            SensorServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+
+    use crate::SensorMutations;
+
+    type DeleteSensorMethod =
+        dyn Fn(DeleteSensor) -> Result<String, DeleteSensorError> + Sync + Send;
+
+    pub struct TestService(pub Box<DeleteSensorMethod>);
+
+    impl SensorServiceTrait for TestService {
+        fn delete_sensor(
+            &self,
+            _: &ServiceContext,
+            input: DeleteSensor,
+        ) -> Result<String, DeleteSensorError> {
+            (self.0)(input)
+        }
+    }
+
+    pub fn service_provider(
+        sensor_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.sensor_service = Box::new(sensor_service);
+        service_provider
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_delete_sensor_errors() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            SensorMutations,
+            "test_graphql_delete_sensor_errors",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: DeleteSensorInput!) {
+            deleteSensor(input: $input, storeId: \"store_a\") {
+              ... on DeleteSensorError {
+                error {
+                  __typename
+                }
+              }
+            }
+          }
+        "#;
+
+        let variables = Some(json!({
+          "input": {
+            "id": "n/a",
+          }
+        }));
+
+        // Record Not Found
+        let test_service =
+            TestService(Box::new(|_| Err(DeleteSensorError::SensorDoesNotExist)));
+        let expected_message = "Bad user input";
+
+        assert_standard_graphql_error!(
+            &settings,
+            mutation,
+            &variables,
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Not current store location
+        let test_service = TestService(Box::new(|_| {
+            Err(DeleteSensorError::SensorDoesNotBelongToCurrentStore)
+        }));
+        let expected_message = "Bad user input";
+
+        assert_standard_graphql_error!(
+            &settings,
+            mutation,
+            &variables,
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Location in use
+        let mutation = r#"
+        mutation ($input: DeleteSensorInput!) {
+            deleteSensor(input: $input, storeId: \"store_a\") {
+              ... on DeleteSensorError {
+                error {
+                  __typename
+                }
+              }
+            }
+          }
+        "#;
+
+        //pub fn successfull_invoice_line() -> InvoiceLine {
+        //    InvoiceLine {
+        //        invoice_line_row: mock_outbound_shipment_a_invoice_lines()[0].clone(),
+        //        invoice_row: mock_outbound_shipment_a(),
+        //        location_row_option: None,
+        //        stock_line_option: None,
+        //    }
+        //}
+
+        let test_service = TestService(Box::new(|_| {
+            Err(DeleteLocationError::LocationInUse(LocationInUse {
+                stock_lines: vec![StockLine {
+                    stock_line_row: mock_stock_line_a(),
+                    item_row: mock_item_a(),
+                    location_row: None,
+                    name_row: None,
+                    barcode_row: None,
+                }],
+                invoice_lines: vec![successfull_invoice_line()],
+            }))
+        }));
+
+        // let invoice_line_ids = stock_lines.iter();
+        let out_line = successfull_invoice_line();
+        let expected = json!({
+            "deleteLocation": {
+              "error": {
+                "__typename": "LocationInUse",
+                "stockLines": {
+                  "nodes": [{"id": mock_stock_line_a().id}]
+                },
+                "invoiceLines": {
+                  "nodes": [{"id": out_line.invoice_line_row.id}]
+                }
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &variables,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_delete_location_success() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            LocationMutations,
+            "test_graphql_delete_location_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: DeleteLocationInput!) {
+            deleteLocation(input: $input, storeId: \"store_a\") {
+              ... on DeleteResponse {
+                id
+              }
+            }
+          }
+        "#;
+
+        let variables = Some(json!({
+          "input": {
+            "id": "n/a",
+
+          }
+        }));
+
+        let test_service = TestService(Box::new(|_| Ok("deleted".to_owned())));
+
+        let expected = json!({
+            "deleteLocation": {
+                "id": "deleted",
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &variables,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+}
---
+++ server/graphql/sensor/src/mutations/insert.rs
@@ -0,0 +1,317 @@
+use async_graphql::*;
+use graphql_core::{
+    simple_generic_errors::{
+        DatabaseError, InternalError, RecordAlreadyExist, UniqueValueViolation,
+    },
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+use graphql_types::types::SensorNode;
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    sensor::insert::{InsertSensor, InsertSensorError as ServiceError},
+};
+
+pub fn insert_sensor(
+    ctx: &Context<'_>,
+    store_id: &str,
+    input: InsertSensorInput,
+) -> Result<InsertSensorResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::MutateSensor,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    match service_provider
+        .sensor_service
+        .insert_sensor(&service_context, input.into())
+    {
+        Ok(sensor) => Ok(InsertSensorResponse::Response(SensorNode::from_domain(
+            sensor,
+        ))),
+        Err(error) => Ok(InsertSensorResponse::Error(InsertSensorError {
+            error: map_error(error)?,
+        })),
+    }
+}
+
+#[derive(InputObject)]
+pub struct InsertSensorInput {
+    pub id: String,
+    pub serial: String,
+    pub name: String,
+    pub is_active: bool,
+}
+
+impl From<InsertSensorInput> for InsertSensor {
+    fn from(
+        InsertSensorInput {
+            id,
+            serial,
+            name,
+            is_active,
+        }: InsertSensorInput,
+    ) -> Self {
+        InsertSensor {
+            id,
+            serial,
+            name,
+            is_active,
+        }
+    }
+}
+#[derive(SimpleObject)]
+pub struct InsertSensorError {
+    pub error: InsertSensorErrorInterface,
+}
+
+#[derive(Union)]
+pub enum InsertSensorResponse {
+    Error(InsertSensorError),
+    Response(SensorNode),
+}
+
+#[derive(Interface)]
+#[graphql(field(name = "serial", type = "String"))]
+pub enum InsertSensorErrorInterface {
+    SensorAlreadyExists(RecordAlreadyExist),
+    UniqueValueViolation(UniqueValueViolation),
+    InternalError(InternalError),
+    DatabaseError(DatabaseError),
+}
+
+fn map_error(error: ServiceError) -> Result<InsertSensorErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Standard Graphql Errors
+        ServiceError::SensorAlreadyExists => BadUserInput(formatted_error),
+        ServiceError::SensorWithSerialAlreadyExists => BadUserInput(formatted_error),
+        ServiceError::CreatedRecordNotFound => InternalError(formatted_error),
+        ServiceError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
+}
+
+#[cfg(test)]
+mod test {
+
+    use async_graphql::EmptyMutation;
+    use graphql_core::{
+        assert_graphql_query, assert_standard_graphql_error, test_helpers::setup_graphl_test,
+    };
+    use repository::{
+        sensor::Sensor, mock::MockDataInserts, SensorRow, StorageConnectionManager,
+    };
+    use serde_json::json;
+
+    use service::{
+        sensor::{
+            insert::{InsertSensor, InsertSensorError},
+            SensorServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+
+    use crate::SensorMutations;
+
+    type InsertSensorMethod =
+        dyn Fn(InsertSensor) -> Result<Sensor, InsertSensorError> + Sync + Send;
+
+    pub struct TestService(pub Box<InsertSensorMethod>);
+
+    impl SensorServiceTrait for TestService {
+        fn insert_sensor(
+            &self,
+            _: &ServiceContext,
+            input: InsertSensor,
+        ) -> Result<Sensor, InsertSensorError> {
+            (self.0)(input)
+        }
+    }
+
+    pub fn service_provider(
+        sensor_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.sensor_service = Box::new(sensor_service);
+        service_provider
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_insert_sensor_errors() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            SensorMutations,
+            "test_graphql_insert_sensor_errors",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: InsertSensorInput!) {
+            insertSensor(input: $input, storeId: \"store_a\") {
+              ... on InsertSensorError {
+                error {
+                  __typename
+                }
+              }
+            }
+          }
+        "#;
+
+        let variables = Some(json!({
+          "input": {
+            "id": "n/a",
+            "serial": "n/a",
+            "name": "n/a",
+          }
+        }));
+
+        // Record Already Exists
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertSensorError::SensorAlreadyExists)
+        }));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            mutation,
+            &variables,
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Unique serial violation
+        let mutation = r#"
+              mutation ($input: InsertSensorInput!) {
+                  insertSensor(input: $input, storeId: \"store_a\") {
+                    ... on InsertLocationError {
+                      error {
+                        ... on UniqueValueViolation {
+                            __typename
+                            field
+                        }
+                      }
+                    }
+                  }
+                }
+              "#;
+
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertSensorError::SensorWithSerialAlreadyExists)
+        }));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            mutation,
+            &variables,
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // Created record does not exist (this shouldn't happen, but want to test internal error)
+        let mutation = r#"
+         mutation ($input: InsertSensorInput!) {
+             insertSensor(input: $input, storeId: \"store_a\") {
+               ... on InsertSensorError {
+                 error {
+                   ... on InternalError {
+                       __typename
+                       description
+                       fullError
+                   }
+                 }
+               }
+             }
+           }
+         "#;
+
+        let test_service = TestService(Box::new(|_| {
+            Err(InsertSensorError::CreatedRecordNotFound)
+        }));
+        let expected_message = "Internal error";
+        assert_standard_graphql_error!(
+            &settings,
+            mutation,
+            &variables,
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_insert_sensor_success() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            SensorMutations,
+            "test_graphql_insert_sensor_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: InsertSensorInput!) {
+            insertSensor(input: $input, storeId: \"store_a\") {
+              ... on SensorNode {
+                id
+                serial
+                name
+                is_active
+              }
+            }
+          }
+        "#;
+
+        let variables = Some(json!({
+          "input": {
+            "id": "n/a",
+            "serial": "n/a",
+            "name": "n/a",
+          }
+        }));
+
+        // Record Already Exists
+        let test_service = TestService(Box::new(|_| {
+            Ok(Sensor {
+                sensor_row: SensorRow {
+                    id: "id".to_owned(),
+                    name: "name".to_owned(),
+                    serial: "serial".to_owned(),
+                    is_active: true,
+                    store_id: "store_a".to_owned(),
+                },
+            })
+        }));
+
+        let expected = json!({
+            "insertSensor": {
+                "id": "id",
+                "name": "name",
+                "serial": "serial",
+                "is_active": true
+
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &variables,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+}
