cb50c5802bdfcae5ec3f25d48617ccd05e8d2ed7
roxy-dao <roxy@msupply.foundation>
2023-08-10 13:21:52 +1200

    Remove polling, change title, and fix pagination bug :sweat_smile:

--- server/repository/src/migrations/v1_03_00/user.rs
+++ server/repository/src/migrations/v1_03_00/user.rs
@@ -1,9 +1,16 @@
 use crate::{migrations::sql, StorageConnection};
 
 pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    #[cfg(not(feature = "postgres"))]
     sql!(
         connection,
         r#"ALTER TABLE user_account ADD last_successful_sync TIMESTAMP NOT NULL DEFAULT 0;"#,
+    )?;
+
+    #[cfg(feature = "postgres")]
+    sql!(
+        connection,
+        r#"ALTER TABLE user_account ADD last_successful_sync TIMESTAMP NOT NULL DEFAULT 'epoch';"#,
     )?;
 
     Ok(())
