e2f0812b1f83b6d154b52aca46f8bb49de84dce4
Clemens Zeidler <clemens@msupply.foundation>
2023-08-14 09:09:00 +1200

    Move contact trace creation to the DetailView
    DetailView now handles creation and editing.

--- client/packages/system/src/ContactTrace/Service/Service.tsx
+++ client/packages/system/src/ContactTrace/Service/Service.tsx
@@ -1,18 +1,33 @@
 import React, { FC } from 'react';
-import { RouteBuilder, Routes, Route } from '@openmsupply-client/common';
+import {
+  RouteBuilder,
+  Routes,
+  Route,
+  useSearchParams,
+} from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
 import { ContactTraceListView } from '../ListView';
 import { DetailView } from '../DetailView';
-
 export const Service: FC = () => {
   const contactTracesRoute = RouteBuilder.create(AppRoute.ContactTrace).build();
+
   const contactTraceRoute = RouteBuilder.create(AppRoute.ContactTrace)
     .addPart(':id')
     .build();
+
+  const [searchParams] = useSearchParams();
   return (
     <Routes>
       <Route path={contactTracesRoute} element={<ContactTraceListView />} />
-      <Route path={contactTraceRoute} element={<DetailView />} />
+      <Route
+        path={contactTraceRoute}
+        element={
+          <DetailView
+            createType={searchParams.get('type')}
+            createPatientId={searchParams.get('patient')}
+          />
+        }
+      />
     </Routes>
   );
 };
--- client/packages/system/src/Patient/ContactTrace/Components/CreateContactTraceModal.tsx
+++ client/packages/system/src/Patient/ContactTrace/Components/CreateContactTraceModal.tsx
@@ -1,34 +1,20 @@
 import React, { FC, useState } from 'react';
 import {
-  AlertIcon,
-  BasicSpinner,
-  Box,
   DialogButton,
   InputWithLabelRow,
   RouteBuilder,
-  Stack,
-  Typography,
   useDialog,
   useNavigate,
-  useNotification,
-  DatePickerInput,
-  ContactTraceNodeStatus,
 } from '@openmsupply-client/common';
-import { DateUtils, useTranslation } from '@common/intl';
+import { useTranslation } from '@common/intl';
 import {
   PatientModal,
   usePatientModalStore,
   DocumentRegistryFragment,
-  useContactTraces,
 } from '@openmsupply-client/programs';
 import { AppRoute } from '@openmsupply-client/config';
 import { ContactTraceSearchInput } from './ContactTraceSearchInput';
 import { usePatient } from '../../api';
-
-interface ContactTrace {
-  status?: ContactTraceNodeStatus;
-  datetime?: string;
-}
 
 export const CreateContactTraceModal: FC = () => {
   const patientId = usePatient.utils.id();
@@ -37,22 +23,11 @@ export const CreateContactTraceModal: FC = () => {
   const [traceRegistry, setTraceRegistry] = useState<
     DocumentRegistryFragment | undefined
   >();
-  const [dataError, setDataError] = useState(false);
-  const [draft, setDraft] = useState<ContactTrace | undefined>(undefined);
   const navigate = useNavigate();
-  const { error } = useNotification();
-  const [datetimeError, setDatetimeError] = useState(false);
-
-  const handleSave = useContactTraces.document.upsert(
-    patientId,
-    traceRegistry?.documentType ?? ''
-  );
 
   const reset = () => {
     selectModal(undefined);
     setTraceRegistry(undefined);
-    setDraft(undefined);
-    setDataError(false);
   };
 
   const { Modal } = useDialog({
@@ -61,52 +36,31 @@ export const CreateContactTraceModal: FC = () => {
   });
 
   const onChangeContactTrace = (entry: DocumentRegistryFragment) => {
-    setDataError(false);
     setTraceRegistry(entry);
   };
 
-  const currentOrNewDraft = (): ContactTrace => {
-    return (
-      draft ?? {
-        datetime: DateUtils.formatRFC3339(DateUtils.addCurrentTime(new Date())),
-        status: ContactTraceNodeStatus.Pending,
-      }
-    );
-  };
-  const setDatetime = (date: Date | null): void => {
-    const datetime = DateUtils.formatRFC3339(DateUtils.addCurrentTime(date));
-    setDraft({
-      ...currentOrNewDraft(),
-      datetime,
-    });
-    setDatetimeError(false);
-  };
-
-  const canSubmit = () =>
-    draft !== undefined && draft.datetime && !datetimeError;
+  const canSubmit = () => traceRegistry !== undefined;
 
   return (
     <Modal
-      title={t('label.new-encounter')}
+      title={t('label.new-contact-trace')}
       cancelButton={<DialogButton variant="cancel" onClick={reset} />}
       okButton={
         <DialogButton
-          variant={'save'}
+          variant={'ok'}
           disabled={!canSubmit()}
           onClick={async () => {
             if (traceRegistry !== undefined) {
-              const { id } = await handleSave(
-                draft,
-                traceRegistry.formSchemaId
+              navigate(
+                RouteBuilder.create(AppRoute.Dispensary)
+                  .addPart(AppRoute.ContactTrace)
+                  .addPart('new')
+                  .addQuery({
+                    type: traceRegistry.documentType,
+                    patient: patientId,
+                  })
+                  .build()
               );
-              if (!!id)
-                navigate(
-                  RouteBuilder.create(AppRoute.Dispensary)
-                    .addPart(AppRoute.ContactTrace)
-                    .addPart(id)
-                    .build()
-                );
-              else error(t('error.contact-trace-not-created'))();
             }
             reset();
           }}
@@ -115,64 +69,17 @@ export const CreateContactTraceModal: FC = () => {
       width={700}
     >
       <React.Suspense fallback={<div />}>
-        <Stack alignItems="flex-start" gap={1} sx={{ paddingLeft: '20px' }}>
-          <InputWithLabelRow
-            label={t('label.contact-trace-type')}
-            Input={
-              <ContactTraceSearchInput
-                onChange={onChangeContactTrace}
-                value={null}
-                width={250}
-              />
-            }
-          />
-          <RenderForm
-            isError={dataError}
-            isLoading={false}
-            isProgram={!!traceRegistry}
-            form={
-              <>
-                <InputWithLabelRow
-                  label={t('label.visit-date')}
-                  Input={
-                    <DatePickerInput
-                      value={DateUtils.getDateOrNull(draft?.datetime)}
-                      onChange={setDatetime}
-                      onError={() => setDatetimeError(true)}
-                      width={250}
-                    />
-                  }
-                />
-              </>
-            }
-          />
-        </Stack>
+        <InputWithLabelRow
+          label={t('label.contact-trace-type')}
+          Input={
+            <ContactTraceSearchInput
+              onChange={onChangeContactTrace}
+              value={null}
+              width={250}
+            />
+          }
+        />
       </React.Suspense>
     </Modal>
   );
-};
-
-const RenderForm = ({
-  form,
-  isError,
-  isLoading,
-  isProgram,
-}: {
-  form: React.ReactNode;
-  isError: boolean;
-  isLoading: boolean;
-  isProgram: boolean;
-}) => {
-  const t = useTranslation('common');
-  if (!isProgram) return null;
-  if (isError)
-    return (
-      <Box display="flex" gap={1} padding={3}>
-        <AlertIcon color="error" />
-        <Typography color="error">{t('error.unable-to-load-data')}</Typography>
-      </Box>
-    );
-  if (isLoading) return <BasicSpinner />;
-
-  return <>{form}</>;
 };
