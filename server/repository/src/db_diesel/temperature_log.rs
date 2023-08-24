20d8f05fdffb3e233b4ef43682594d0a02d0b00a
adrianwb <adrian@sussol.net>
2023-08-24 14:22:27 +0100

    Add missing id filter
    Source: https://github.com/msupply-foundation/open-msupply/commit/aa52c3f8406ee3f98a95b01050d5875195b06517

--- server/repository/src/db_diesel/temperature_log.rs
+++ server/repository/src/db_diesel/temperature_log.rs
@@ -112,6 +112,11 @@ impl TemperatureLogFilter {
         }
     }
 
+    pub fn id(mut self, filter: EqualFilter<String>) -> Self {
+        self.id = Some(filter);
+        self
+    }
+    
     pub fn sensor_id(mut self, filter: EqualFilter<String>) -> Self {
         self.sensor_id = Some(filter);
         self
