43e4d21d519dbd2fecbedff2d0cdabb1bff5553b
Mark Prins <mark@malcolmprins.net>
2023-08-31 18:21:24 +1200

    update comments
    Source: https://github.com/msupply-foundation/open-msupply/commit/2baf7901f42f76be010269ff0aa117b89e655d76

--- client/packages/common/src/plugins/types.ts
+++ client/packages/common/src/plugins/types.ts
@@ -11,6 +11,17 @@ export enum PluginType {
   Stocktake,
 }
 
+/**
+ * Plugin interface
+ * area: the area of the app which the plugin component will be rendered in
+ * data: is context specific data which is passed to the plugin, for example a plugin
+ * module: the name of the exposed react component
+ * name: the name of the plugin
+ * path: file path for the plugin, possibly unnecessary now
+ * on the InboundShipment DetailView will have an type of `InvoiceNode`
+ * scope: the name of the scope used by webpack module federation
+ * type: defines the type of the data to be passed in
+ */
 export interface Plugin<T> {
   area: PluginArea;
   data?: T;
