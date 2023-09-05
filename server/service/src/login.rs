6c05025f8a382d0dfdac00db9bd5c13f37a1e461
roxy-dao <roxy@msupply.foundation>
2023-09-05 12:56:21 +1200

    Update AddPatient mapping
    Source: https://github.com/msupply-foundation/open-msupply/commit/6a846b67168acba8c0af47d34f33fdf666c5924e

--- server/service/src/login.rs
+++ server/service/src/login.rs
@@ -380,6 +380,7 @@ fn permissions_to_domain(permissions: Vec<Permissions>) -> HashSet<Permission> {
             }
             // patient
             Permissions::AddPatients => {
+                output.insert(Permission::PatientQuery);
                 output.insert(Permission::PatientMutate);
             }
             Permissions::EditPatientDetails => {
