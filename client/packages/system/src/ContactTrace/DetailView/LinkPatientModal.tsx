f458db5f99f69adac61697dc18da67078c3e28b3
Mark Prins <mark@malcolmprins.net>
2023-09-09 10:40:07 +1200

    suggested change
    Source: https://github.com/msupply-foundation/open-msupply/commit/0a831f9efdff932b4f86974e9265030f5e2cf0f4

--- client/packages/system/src/ContactTrace/DetailView/LinkPatientModal.tsx
+++ client/packages/system/src/ContactTrace/DetailView/LinkPatientModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
   Box,
   DataTable,
@@ -26,51 +26,54 @@ type Filter = {
   identifier?: string;
 };
 
-const useFilterBar = (
-  onFilterChanged: (filter: Partial<Filter>) => void,
-  firstName: string | undefined,
-  lastName: string | undefined
-): { filter: Filter; FilterBar: FC } => {
+const FilterBar = ({
+  filter,
+  onChange,
+}: {
+  filter: Filter;
+  onChange: (patch: Partial<Filter>) => void;
+}) => {
   const t = useTranslation('dispensary');
-  const [filter, setFilter] = useState<Filter>({ firstName, lastName });
-  useEffect(() => onFilterChanged(filter), [onFilterChanged, filter]);
 
-  const FilterBar = () => (
+  return (
     <Box display="flex" gap={1}>
       <SearchBar
         placeholder={t('placeholder.search-by-first-name')}
         value={filter.firstName ?? ''}
-        onChange={(newValue: string) => {
-          setFilter({
-            ...filter,
-            firstName: newValue,
-          });
-        }}
+        onChange={newValue => onChange({ firstName: newValue })}
       />
       <SearchBar
         placeholder={t('placeholder.search-by-last-name')}
         value={filter.lastName ?? ''}
-        onChange={(newValue: string) => {
-          setFilter({ ...filter, lastName: newValue });
-        }}
+        onChange={newValue => onChange({ lastName: newValue })}
       />
       <SearchBar
         placeholder={t('placeholder.search-by-identifier')}
         value={filter.identifier ?? ''}
-        onChange={(newValue: string) => {
-          setFilter({ ...filter, identifier: newValue });
+        onChange={newValue => {
+          onChange({ identifier: newValue });
         }}
       />
     </Box>
   );
-
-  return { filter, FilterBar };
 };
 
 type ModalContentProps = {
   documentData: ContactTrace;
   onPatientLinked: (patientId: string) => void;
   hideDialog: () => void;
+};
+
+const useFilter = ({ contact }: ContactTrace) => {
+  const [filter, setFilter] = useState<Filter>({
+    firstName: contact?.firstName,
+    lastName: contact?.lastName,
+  });
+  const [patch, setPatch] = useState<Partial<Filter>>({});
+
+  useEffect(() => setFilter({ ...filter, ...patch }), [patch]);
+
+  return { filter, onChange: setPatch };
 };
 
 const ModalContent: FC<ModalContentProps> = ({
@@ -80,17 +83,13 @@ const ModalContent: FC<ModalContentProps> = ({
 }) => {
   const t = useTranslation('dispensary');
   const { localisedDate } = useFormatDateTime();
-  const onFilterChanged = useCallback(() => {}, []);
-  const { filter, FilterBar } = useFilterBar(
-    onFilterChanged,
-    documentData.contact?.firstName,
-    documentData.contact?.lastName
-  );
+  const { filter, onChange } = useFilter(documentData);
 
   const searchEnabled =
     (filter.firstName?.length ?? 0) > 0 ||
     (filter.lastName?.length ?? 0) > 0 ||
     (filter.identifier?.length ?? 0) > 0;
+
   const { data: localSearchData } = usePatient.document.list(
     {
       filterBy: {
@@ -98,9 +97,14 @@ const ModalContent: FC<ModalContentProps> = ({
         lastName: filter.lastName ? { like: filter.lastName } : null,
         identifier: filter.identifier ? { like: filter.identifier } : null,
       },
+      sortBy: {
+        key: 'name',
+        direction: 'asc',
+      },
     },
     searchEnabled
   );
+
   const matchingPatients = !searchEnabled ? [] : localSearchData?.nodes;
 
   const columns = useColumns<PatientRowFragment>([
@@ -140,7 +144,7 @@ const ModalContent: FC<ModalContentProps> = ({
           gap={2}
           width="100%"
         >
-          <FilterBar />
+          <FilterBar onChange={onChange} filter={filter} />
           <DataTable
             dense
             id="create-patient-duplicates"
