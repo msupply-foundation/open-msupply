b193ed496a39256f6b39b31bdb808a131516ac29
roxy-dao <roxy@msupply.foundation>
2023-09-11 11:28:54 +1200

    Display birth date in patient search

--- client/packages/system/src/Patient/Components/PatientOptionRenderer/PatientOptionRenderer.tsx
+++ client/packages/system/src/Patient/Components/PatientOptionRenderer/PatientOptionRenderer.tsx
@@ -8,23 +8,25 @@ import {
 import { SearchInputPatient } from '../../utils';
 
 export const getPatientOptionRenderer =
-  (): AutocompleteOptionRenderer<SearchInputPatient> => (props, item) =>
-    (
-      <DefaultAutocompleteItemOption {...props} key={item.id}>
-        <Box display="flex" alignItems="flex-end" gap={1} height={25}>
-          <Box display="flex" flexDirection="row" gap={1} width={110}>
-            <Typography
-              overflow="hidden"
-              fontWeight="bold"
-              textOverflow="ellipsis"
-              sx={{
-                whiteSpace: 'nowrap',
-              }}
-            >
-              {item.code}
-            </Typography>
-          </Box>
-          <Typography>{item.name}</Typography>
+  (): AutocompleteOptionRenderer<SearchInputPatient> => (props, item) => (
+    <DefaultAutocompleteItemOption {...props} key={item.id}>
+      <Box display="flex" alignItems="flex-end" gap={1} height={25}>
+        <Box display="flex" flexDirection="row" gap={1} width={110}>
+          <Typography
+            overflow="hidden"
+            fontWeight="bold"
+            textOverflow="ellipsis"
+            sx={{
+              whiteSpace: 'nowrap',
+            }}
+          >
+            {item.code}
+          </Typography>
         </Box>
-      </DefaultAutocompleteItemOption>
-    );
+        <Box display="flex" flexDirection="row" gap={1} width={80}>
+          <Typography>{item.dateOfBirth}</Typography>
+        </Box>
+        <Typography>{item.name}</Typography>
+      </Box>
+    </DefaultAutocompleteItemOption>
+  );
