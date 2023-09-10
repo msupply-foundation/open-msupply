82431066bccdd8ca5be2fd7ff71c719a075609be
roxy-dao <roxy@msupply.foundation>
2023-09-11 10:54:39 +1200

    Query/insert/update plugin data

--- server/service/src/service_provider.rs
+++ server/service/src/service_provider.rs
@@ -23,14 +23,15 @@ use crate::{
     master_list::{MasterListService, MasterListServiceTrait},
     missing_program::create_missing_master_list_and_program,
     name::get_names,
+    plugin_data::{PluginDataService, PluginDataServiceTrait},
     processors::ProcessorsTrigger,
-    repack::{RepackService, RepackServiceTrait},
     programs::{
         encounter::{EncounterService, EncounterServiceTrait},
         patient::{PatientService, PatientServiceTrait},
         program_enrolment::{ProgramEnrolmentService, ProgramEnrolmentServiceTrait},
         program_event::{ProgramEventService, ProgramEventServiceTrait},
     },
+    repack::{RepackService, RepackServiceTrait},
     report::report_service::{ReportService, ReportServiceTrait},
     requisition::{RequisitionService, RequisitionServiceTrait},
     requisition_line::{RequisitionLineService, RequisitionLineServiceTrait},
@@ -104,6 +105,8 @@ pub struct ServiceProvider {
     pub barcode_service: Box<dyn BarcodeServiceTrait>,
     // Log
     pub log_service: Box<dyn LogServiceTrait>,
+    // Plugin
+    pub plugin_data_service: Box<dyn PluginDataServiceTrait>,
 }
 
 pub struct ServiceContext {
@@ -172,6 +175,7 @@ impl ServiceProvider {
             barcode_service: Box::new(BarcodeService {}),
             repack_service: Box::new(RepackService {}),
             log_service: Box::new(LogService {}),
+            plugin_data_service: Box::new(PluginDataService {}),
         }
     }
 
