45084643f14d1cc136f8a64f1bdae2649cdaa84a
roxy-dao <roxy@msupply.foundation>
2023-09-20 14:51:50 +1200

    Cell for items that have multiple batches
    Source: https://github.com/msupply-foundation/open-msupply/commit/9452c93272fe1537b525a3df751474b3b89b6e04

--- client/packages/system/src/Item/Components/ItemVariant/index.ts
+++ client/packages/system/src/Item/Components/ItemVariant/index.ts
@@ -2,3 +2,4 @@ export * from './PackUnitCell';
 export * from './PackUnitSelectCell';
 export * from './PackUnitQuantityCell';
 export * from './PackUnitEntryCell';
+export * from './PackUnitMultipleCell';
