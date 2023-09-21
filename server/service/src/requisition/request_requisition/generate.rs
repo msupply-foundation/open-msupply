6b20aee30ce9ac4971bedc47ec0c1cd3d2a31325
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-09-21 12:45:50 +1200

    default_min_months_of_stock refactor
    Co-authored-by: roxy-dao <roxy@msupply.foundation>
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/c62331fe165f3744cd709122021152cd3b5e0495

--- server/service/src/requisition/request_requisition/generate.rs
+++ server/service/src/requisition/request_requisition/generate.rs
@@ -25,13 +25,11 @@ pub fn generate_suggested_quantity(
     }
     let months_of_stock = available_stock_on_hand as f64 / average_monthly_consumption as f64;
 
-    let default_min_months_of_stock: f64;
-
-    if min_months_of_stock == 0.0 {
-        default_min_months_of_stock = max_months_of_stock
+    let default_min_months_of_stock = if min_months_of_stock == 0.0 {
+        max_months_of_stock
     } else {
-        default_min_months_of_stock = min_months_of_stock
-    }
+        min_months_of_stock
+    };
 
     if max_months_of_stock == 0.0 || (months_of_stock > default_min_months_of_stock) {
         return 0;
