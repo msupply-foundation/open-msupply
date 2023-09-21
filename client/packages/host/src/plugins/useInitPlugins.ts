f9b761a9db1f5c084345c03ae9e6c53cca1b6c66
Mark Prins <mark@malcolmprins.net>
2023-09-22 10:30:27 +1200

    dependency in useEffect
    Source: https://github.com/msupply-foundation/open-msupply/commit/68e4dabb0cc40298d86e724d284f319f824c54cb

--- client/packages/host/src/plugins/useInitPlugins.ts
+++ client/packages/host/src/plugins/useInitPlugins.ts
@@ -40,6 +40,12 @@ interface PluginConfig {
   dependencies: PluginDependency;
 }
 
+interface InitPluginProps {
+  data?: PluginNode[];
+  addColumnPlugin: (plugin: ColumnPlugin) => void;
+  addComponentPlugin: (plugin: ComponentPlugin) => void;
+}
+
 const mapPlugin = (plugin: PluginNode): PluginDefinition | null => {
   const { config, path } = plugin;
   try {
@@ -84,77 +90,86 @@ const mapPlugin = (plugin: PluginNode): PluginDefinition | null => {
   return null;
 };
 
+const initRemotePlugins = ({
+  data,
+  addColumnPlugin,
+  addComponentPlugin,
+}: InitPluginProps) => {
+  (
+    (data?.map(mapPlugin).filter(plugin => plugin !== null) ??
+      []) as PluginDefinition[]
+  ).forEach(({ columnPlugins, componentPlugins }) => {
+    columnPlugins.forEach(plugin => addColumnPlugin(plugin));
+    componentPlugins.forEach(plugin => addComponentPlugin(plugin));
+  });
+};
+
+// For hot reloading in dev mode plugins will be loaded from ./plugin folder
+const initLocalPlugins = async ({
+  addColumnPlugin,
+  addComponentPlugin,
+}: InitPluginProps) => {
+  for (const plugin of LOCAL_PLUGINS ?? []) {
+    const plugins = mapPlugin(plugin);
+    const handleImportError = (e: Error) =>
+      console.error(`Unable to load plugin ${plugin.name}: ${e.message}`);
+
+    plugins?.componentPlugins.forEach(mapped => {
+      const module = mapped.localModule ?? mapped.module;
+      import(
+        // Using the localModule property to load the inner component
+        // when loading directly like this, the component has access to app context
+        // which it does not when loading async via webpack module federation
+        // Webpack will actually try to load everything in plugins directory
+        // which causes issues
+        /* webpackExclude: /node_modules/ */
+        /* webpackExclude: /operations.graphql/ */
+        `../../../plugins/${plugin.path}/src/${module}`
+      )
+        .then(module => {
+          mapped.component = <T>() =>
+            new Promise<PluginModule<T>>((resolve, reject) => {
+              if (module.default) {
+                resolve(module);
+              } else {
+                reject(
+                  new Error(
+                    `Unable to load plugin ${mapped.module}. Check that the module has a default export.`
+                  )
+                );
+              }
+            });
+          addComponentPlugin(mapped);
+        })
+        .catch(handleImportError);
+    });
+    plugins?.columnPlugins.forEach(mapped => {
+      import(
+        // Webpack will actually try to load everything in plugins directory
+        // which causes issues
+        /* webpackExclude: /node_modules/ */
+        /* webpackExclude: /operations.graphql/ */
+        `../../../plugins/${plugin.path}/src/${mapped.module}`
+      )
+        .then(module => {
+          mapped.column = <T extends RecordWithId>() =>
+            new Promise<ColumnDefinition<T>>(resolve =>
+              resolve(module.default)
+            );
+          addColumnPlugin(mapped);
+        })
+        .catch(handleImportError);
+    });
+  }
+};
+
 export const useInitPlugins = () => {
   const { addComponentPlugin, addColumnPlugin } = usePluginProvider();
   const { data } = useHost.plugins.list();
-  const initRemotePlugins = (data?: PluginNode[]) => {
-    (
-      (data?.map(mapPlugin).filter(plugin => plugin !== null) ??
-        []) as PluginDefinition[]
-    ).forEach(({ columnPlugins, componentPlugins }) => {
-      columnPlugins.forEach(plugin => addColumnPlugin(plugin));
-      componentPlugins.forEach(plugin => addComponentPlugin(plugin));
-    });
-  };
-
-  // For hot reloading in dev mode plugins will be loaded from ./plugin folder
-  const initLocalPlugins = async () => {
-    for (const plugin of LOCAL_PLUGINS ?? []) {
-      const plugins = mapPlugin(plugin);
-      const handleImportError = (e: Error) =>
-        console.error(`Unable to load plugin ${plugin.name}: ${e.message}`);
-
-      plugins?.componentPlugins.forEach(mapped => {
-        const module = mapped.localModule ?? mapped.module;
-        import(
-          // Using the localModule property to load the inner component
-          // when loading directly like this, the component has access to app context
-          // which it does not when loading async via webpack module federation
-          // Webpack will actually try to load everything in plugins directory
-          // which causes issues
-          /* webpackExclude: /node_modules/ */
-          /* webpackExclude: /operations.graphql/ */
-          `../../../plugins/${plugin.path}/src/${module}`
-        )
-          .then(module => {
-            mapped.component = <T>() =>
-              new Promise<PluginModule<T>>((resolve, reject) => {
-                if (module.default) {
-                  resolve(module);
-                } else {
-                  reject(
-                    new Error(
-                      `Unable to load plugin ${mapped.module}. Check that the module has a default export.`
-                    )
-                  );
-                }
-              });
-            addComponentPlugin(mapped);
-          })
-          .catch(handleImportError);
-      });
-      plugins?.columnPlugins.forEach(mapped => {
-        import(
-          // Webpack will actually try to load everything in plugins directory
-          // which causes issues
-          /* webpackExclude: /node_modules/ */
-          /* webpackExclude: /operations.graphql/ */
-          `../../../plugins/${plugin.path}/src/${mapped.module}`
-        )
-          .then(module => {
-            mapped.column = <T extends RecordWithId>() =>
-              new Promise<ColumnDefinition<T>>(resolve =>
-                resolve(module.default)
-              );
-            addColumnPlugin(mapped);
-          })
-          .catch(handleImportError);
-      });
-    }
-  };
 
   useEffect(() => {
-    if (process.env['NODE_ENV'] === 'production') initRemotePlugins(data);
-    else initLocalPlugins();
-  }, [data]);
+    if (process.env['NODE_ENV'] === 'production')
+      initRemotePlugins({ addColumnPlugin, addComponentPlugin, data });
+    else initLocalPlugins({ addColumnPlugin, addComponentPlugin });
+  }, [data, addColumnPlugin, addComponentPlugin]);
 };
