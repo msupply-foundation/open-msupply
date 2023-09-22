71c0c2854e7bed92c78a7f5e76b8baf9becde493
Mark Prins <mark@malcolmprins.net>
2023-09-22 18:27:05 +1200

    remove plugin
    Source: https://github.com/msupply-foundation/open-msupply/commit/9f2260660454ec6779ac1c768d5c1db054a2dd2f

--- client/packages/plugins/StockDonor/src/api/hooks/statistics/index.ts
+++
@@ -1,7 +0,0 @@
-import { useInboundCounts } from './useInboundCounts';
-import { useRequisitionCounts } from './useRequisitionCounts';
-
-export const Statistics = {
-  useInboundCounts,
-  useRequisitionCounts,
-};
--- client/packages/plugins/StockDonor/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './StockDonorEdit';
-export * from './StockDonorColumn';
