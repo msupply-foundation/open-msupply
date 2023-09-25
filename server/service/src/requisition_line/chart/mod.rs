dafb30614da2245431569419d127d1a251be66ba
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-09-25 13:54:13 +1300

    Update for legibility

--- server/service/src/requisition_line/chart/mod.rs
+++ server/service/src/requisition_line/chart/mod.rs
@@ -148,17 +148,17 @@ impl From<RepositoryError> for OutError {
 
 impl SuggestedQuantityCalculation {
     pub fn from_requisition_line(from: &RequisitionLine) -> Self {
+        let threshold = if from.requisition_row.min_months_of_stock == 0.0 {
+            from.requisition_row.max_months_of_stock as f64
+        } else {
+            from.requisition_row.min_months_of_stock as f64
+        };
         SuggestedQuantityCalculation {
             average_monthly_consumption: from.requisition_line_row.average_monthly_consumption
                 as f64,
             stock_on_hand: from.requisition_line_row.available_stock_on_hand as u32,
-            minimum_stock_on_hand: from.requisition_line_row.average_monthly_consumption as f64 * {
-                if from.requisition_row.min_months_of_stock == 0.0 {
-                    from.requisition_row.max_months_of_stock as f64
-                } else {
-                    from.requisition_row.min_months_of_stock as f64
-                }
-            },
+            minimum_stock_on_hand: from.requisition_line_row.average_monthly_consumption as f64
+                * threshold,
             maximum_stock_on_hand: from.requisition_line_row.average_monthly_consumption as f64
                 * from.requisition_row.max_months_of_stock as f64,
             suggested: from.requisition_line_row.suggested_quantity as u32,
