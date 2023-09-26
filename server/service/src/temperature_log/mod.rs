bf46017c5bcad349e91328729266f798542ea583
adrianwb <adrian@sussol.net>
2023-09-26 17:22:54 +0100

    Service temperature log added

---
+++ server/service/src/temperature_log/mod.rs
@@ -0,0 +1,45 @@
+use self::{
+    // no delete/update: temperature logs can only be inserted & queried
+    insert::{insert_temperature_log, InsertTemperatureLog, InsertTemperatureLogError},
+    query::{get_temperature_log, get_temperature_logs},
+};
+
+use super::{ListError, ListResult};
+use crate::{service_provider::ServiceContext, SingleRecordError};
+use repository::temperature_log::{TemperatureLog, TemperatureLogFilter, TemperatureLogSort};
+use repository::PaginationOption;
+
+pub mod insert;
+pub mod query;
+mod validate;
+
+pub trait TemperatureLogServiceTrait: Sync + Send {
+    fn get_temperature_logs(
+        &self,
+        ctx: &ServiceContext,
+        pagination: Option<PaginationOption>,
+        filter: Option<TemperatureLogFilter>,
+        sort: Option<TemperatureLogSort>,
+    ) -> Result<ListResult<TemperatureLog>, ListError> {
+        get_temperature_logs(ctx, pagination, filter, sort)
+    }
+
+    fn get_temperature_log(&self, ctx: &ServiceContext, id: String) -> Result<TemperatureLog, SingleRecordError> {
+        get_temperature_log(ctx, id)
+    }
+
+    fn insert_temperature_log(
+        &self,
+        ctx: &ServiceContext,
+        input: InsertTemperatureLog,
+    ) -> Result<TemperatureLog, InsertTemperatureLogError> {
+        insert_temperature_log(ctx, input)
+    }
+
+}
+
+pub struct TemperatureLogService {}
+impl TemperatureLogServiceTrait for TemperatureLogService {}
+
+#[cfg(test)]
+mod tests;
---
+++ server/service/src/temperature_log/tests/insert.rs
@@ -0,0 +1,117 @@
+#[cfg(test)]
+mod query {
+    use repository::mock::mock_store_a;
+    use repository::EqualFilter;
+    use repository::{
+        mock::MockDataInserts,
+        temperature_log::{TemperatureLog, TemperatureLogFilter, TemperatureLogRepository},
+        test_db::setup_all,
+        TemperatureLogRow,
+    };
+
+    use crate::{
+        temperature_log::insert::{InsertTemperatureLog, InsertTemperatureLogError},
+        service_provider::ServiceProvider,
+    };
+
+    use chrono::{NaiveDate, Duration};
+
+    #[actix_rt::test]
+    async fn insert_temperature_log_service_errors() {
+        let (mock_data, _, connection_manager, _) =
+            setup_all("insert_temperature_log_service_errors", MockDataInserts::all()).await;
+
+        let connection = connection_manager.connection().unwrap();
+        let temperature_log_repository = TemperatureLogRepository::new(&connection);
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider
+            .context(mock_store_a().id, "".to_string())
+            .unwrap();
+        let service = service_provider.temperature_log_service;
+
+        let temperature_logs_in_store = temperature_log_repository
+            .query_by_filter(TemperatureLogFilter::new().store_id(EqualFilter::equal_to("store_a")))
+            .unwrap();
+
+        assert_eq!(
+            service.insert_temperature_log(
+                &context,
+                InsertTemperatureLog {
+                    id: mock_data["base"].temperature_logs[0].id.clone(),
+                    sensor_id: "invalid".to_owned(),
+                    timestamp: None,
+                },
+            ),
+            Err(InsertTemperatureLogError::TemperatureLogAlreadyExists)
+        );
+
+        assert_eq!(
+            service.insert_temperature_log(
+                &context,
+                InsertTemperatureLog {
+                    id: "new_id".to_owned(),
+                    sensor_id: temperature_logs_in_store[0].temperature_log_row.sensor_id.clone(),
+                    timestamp: temperature_logs_in_store[0].temperature_log_row.timestamp.clone(),
+                },
+            ),
+            Err(InsertTemperatureLogError::TemperatureLogNotUnique)
+        );
+    }
+
+    #[actix_rt::test]
+    async fn insert_temperature_log_service_success() {
+        let (_, _, connection_manager, _) =
+            setup_all("insert_temperature_log_service_success", MockDataInserts::all()).await;
+
+        let connection = connection_manager.connection().unwrap();
+        let temperature_log_repository = TemperatureLogRepository::new(&connection);
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider
+            .context(mock_store_a().id, "".to_string())
+            .unwrap();
+        let service = service_provider.temperature_log_service;
+
+        let result_temperature_log = TemperatureLog {
+            temperature_log_row: TemperatureLogRow {
+                id: "new_id".to_owned(),
+                sensor_id: "sensor_1".to_owned(),
+                temperature: 2.4,
+                store_id: Some("store_a".to_owned()),
+                location_id: None,
+                timestamp: NaiveDate::from_ymd_opt(2022, 7, 1)
+                    .unwrap()
+                    .and_hms_opt(0, 0, 0)
+                    .unwrap()
+                    + Duration::seconds(47046),
+            },
+        };
+
+        assert_eq!(
+            service.insert_temperature_log(
+                &context,
+                InsertTemperatureLog {
+                    id: "new_id".to_owned(),
+                    sensor_id: "sensor_1".to_owned(),
+                    timestamp: NaiveDate::from_ymd_opt(2022, 7, 1)
+                        .unwrap()
+                        .and_hms_opt(0, 0, 0)
+                        .unwrap()
+                        + Duration::seconds(47046),
+                },
+            ),
+            Ok(result_temperature_log.clone())
+        );
+
+        assert_eq!(
+            temperature_log_repository
+                .query_by_filter(
+                    TemperatureLogFilter::new()
+                        .id(EqualFilter::equal_to("new_id"))
+                        .store_id(EqualFilter::equal_to("store_a",))
+                )
+                .unwrap(),
+            vec![result_temperature_log]
+        );
+
+    }
+}
