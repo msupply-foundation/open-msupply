438cca6cc9007c27d1fef3dd5f785740359803c5
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-10-04 08:52:34 +1300

    Re use circle icon :)
    Source: https://github.com/msupply-foundation/open-msupply/commit/0a717bbe63453c75acedc09c2595c208e0dc1448

--- client/packages/common/src/ui/layout/tables/components/Cells/DotCell/DotCell.tsx
+++ client/packages/common/src/ui/layout/tables/components/Cells/DotCell/DotCell.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import { CellProps } from '../../../columns';
-import { FiberManualRecordIcon } from '@openmsupply-client/common';
+import { CircleIcon } from '@openmsupply-client/common';
 import { RecordWithId } from '@common/types';
-import { useBufferState } from '@common/hooks';
 
 export const DotCell = <T extends RecordWithId>({
   rowData,
   column,
 }: CellProps<T>): React.ReactElement<CellProps<T>> => {
-  const [buffer] = useBufferState(column.accessor({ rowData }));
-
   return (
-    <>{buffer ? <FiberManualRecordIcon sx={{ color: 'black' }} /> : null}</>
+    <>
+      {column.accessor({ rowData }) === true && (
+        <CircleIcon sx={{ color: 'black', transform: 'scale(0.5)' }} />
+      )}
+    </>
   );
 };
