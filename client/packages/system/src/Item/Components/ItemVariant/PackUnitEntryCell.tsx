588253e41647c1dfb1734822b9d841770b921095
Andrei <andrei.evguenov@gmail.com>
2023-10-04 01:13:55 +1300

    Refine UI to enter pack size manually
    Source: https://github.com/msupply-foundation/open-msupply/commit/27002339d6d0aa8d47ff4584e04453b7317570e1

--- client/packages/system/src/Item/Components/ItemVariant/PackUnitEntryCell.tsx
+++ client/packages/system/src/Item/Components/ItemVariant/PackUnitEntryCell.tsx
@@ -6,8 +6,12 @@ import {
   PositiveNumberInput,
   useDebounceCallback,
   Box,
+  useTranslation,
+  InnerBasicCell,
 } from '@openmsupply-client/common';
 import { useUnitVariant } from '../../context';
+
+const ENTER_PACK_SIZE = -1;
 
 // This cell displays a packSize number input and unit pack drop down if unit pack variants exist
 export const getPackUnitEntryCell =
@@ -23,7 +27,8 @@ export const getPackUnitEntryCell =
       getItemId(rowData),
       getUnitName(rowData)
     );
-
+    const t = useTranslation();
+    const [isEnterPackSize, setIsEnterPackSize] = useState(false);
     const [packSize, setPackSize] = useState(
       Number(column.accessor({ rowData }))
     );
@@ -31,43 +36,67 @@ export const getPackUnitEntryCell =
     const updater = useDebounceCallback(column.setter, [column.setter], 250);
 
     // This is shared between input with drop down and without drop down
-    const numberInput = (
-      <PositiveNumberInput
-        value={packSize}
-        // Should PositiveNumberInput ever return undefined ?
-        onChange={newValue => {
-          setPackSize(newValue || 1);
-          updater({ ...rowData, [column.key]: newValue });
-        }}
-      />
-    );
-
-    if (!variantsControl) {
-      // If no variants exist, then default to just pack size entry
-      return numberInput;
-    }
-
-    const { variants } = variantsControl;
-    const isManuallyEntered =
-      variants.find(v => v.packSize === packSize) === undefined;
-
-    return (
-      <Box display="flex" flexDirection="row">
-        {(packSize === 1 || isManuallyEntered) && numberInput}
-        <Select
-          sx={{ flexGrow: 1, marginLeft: '-2px' }}
-          options={variants.map(v => ({
-            label: v.shortName,
-            value: v.packSize,
-          }))}
-          value={isManuallyEntered ? 1 : packSize}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newValue = Number(e.target.value);
-
-            setPackSize(newValue);
+    const numberInput = () => {
+      return (
+        <PositiveNumberInput
+          focusOnRender={isEnterPackSize}
+          value={packSize}
+          onChange={newValue => {
+            setPackSize(newValue || 1);
             updater({ ...rowData, [column.key]: newValue });
           }}
         />
+      );
+    };
+
+    if (!variantsControl) {
+      // If no variants exist, then default to just pack size entry
+      return numberInput();
+    }
+
+    const { variants } = variantsControl;
+
+    const options = [
+      ...variants.map(v => ({
+        label: v.shortName,
+        value: v.packSize,
+      })),
+      {
+        label: t('label.enter-pack-size'),
+        value: ENTER_PACK_SIZE,
+      },
+    ];
+
+    return (
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <Select
+          sx={{ flexGrow: 1, marginLeft: '-2px' }}
+          options={options}
+          value={isEnterPackSize ? ENTER_PACK_SIZE : packSize}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const newValue = Number(e.target.value);
+
+            // When manually entered pack size is selected, turn on manual entry
+            // and set pack size to 1
+            const isEnterPackSizeSelected = newValue === ENTER_PACK_SIZE;
+            const newPackSize = isEnterPackSizeSelected ? 1 : newValue;
+
+            setPackSize(newPackSize);
+            setIsEnterPackSize(isEnterPackSizeSelected);
+            updater({ ...rowData, [column.key]: newPackSize });
+          }}
+        />
+
+        <InnerBasicCell value={'/'} />
+
+        {
+          /* Allo input only when manually entering pack size */
+          isEnterPackSize ? (
+            numberInput()
+          ) : (
+            <InnerBasicCell value={String(packSize)} />
+          )
+        }
       </Box>
     );
   };
