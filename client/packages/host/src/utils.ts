78decd6730365af28318f8e71c08cd0fe9eb9c9f
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-10-04 09:46:31 +1300

    Add check for touch screen in env utils
    I think I am understanding correctly that this is what you (Carl and Mark) had in mind
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/0f4eb2c98e54d11eebf3ab4ca78a2c8bf389f6d0

--- client/packages/host/src/utils.ts
+++ client/packages/host/src/utils.ts
@@ -1,24 +1,7 @@
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode } from 'react';
 
 export const getBadgeProps = (value?: number) => ({
   badgeContent: (value ?? 0) as ReactNode,
   max: 99,
   color: 'primary' as 'primary' | 'default',
 });
-
-export const useDeviceDetect = () => {
-  const [isMobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    const userAgent =
-      typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-    const mobile = Boolean(
-      userAgent.match(
-        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-      )
-    );
-    setMobile(mobile);
-  }, []);
-
-  return { isMobile };
-};
