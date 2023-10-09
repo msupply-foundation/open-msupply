bec90a205d771c2eb14323bd3c7cddebb9a846c6
raviSussol <ravi.shrestha@sussol.net>
2023-10-09 12:23:56 +0545

    2276 Fix sortby in printReport function

--- client/packages/inventory/src/Stocktake/DetailView/AppBarButtons.tsx
+++ client/packages/inventory/src/Stocktake/DetailView/AppBarButtons.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import {
   AppBarButtonsPortal,
   ButtonWithIcon,
@@ -10,7 +10,6 @@ import {
   LoadingButton,
   PrinterIcon,
   useUrlQueryParams,
-  DataSortInput,
 } from '@openmsupply-client/common';
 import { useStocktake } from '../api';
 import {
@@ -33,43 +32,22 @@ export const AppBarButtonsComponent: FC<AppBarButtonProps> = ({
   const { data } = useStocktake.document.get();
   const { print, isPrinting } = useReport.utils.print();
 
-  const [isReportPrinting, setIsReportPrinting] = useState(false);
-  const [report, setReport] = useState<ReportRowFragment>();
-  const [args, setArgs] = useState<JsonData>();
   const {
     queryParams: { sortBy },
   } = useUrlQueryParams({ initialSort: { key: 'itemName', dir: 'asc' } });
 
   const printReport = (
     report: ReportRowFragment,
-    args: JsonData | undefined,
-    sortBy?: DataSortInput
+    args: JsonData | undefined
   ) => {
     if (!data) return;
     print({
       reportId: report.id,
       dataId: data?.id,
       args,
-      sortBy,
+      sortBy: { key: sortBy.key, desc: sortBy.isDesc },
     });
   };
-
-  // Note: The `sortBy` from the `useUrlQueryParams` doesn't seem to be updating
-  // when it is used as directly or being passed as an argument to the `printReport` function.
-  // It is always the same value as when the component was first rendered,
-  // even when the value changes when the user clicks on the table header.
-  // It seems to behave as a side effect of the `useUrlQueryParams` hook.
-  // So needed to handle it in the `useEffect` below and then update it here.
-  useEffect(() => {
-    let isMounted = true;
-    if (isMounted && isReportPrinting && report) {
-      printReport(report, args, { key: sortBy.key, desc: sortBy.isDesc });
-      setIsReportPrinting(false);
-    }
-    return () => {
-      isMounted = false;
-    };
-  }, [isReportPrinting, report, args]);
 
   return (
     <AppBarButtonsPortal>
@@ -80,14 +58,7 @@ export const AppBarButtonsComponent: FC<AppBarButtonProps> = ({
           Icon={<PlusCircleIcon />}
           onClick={() => onAddItem(true)}
         />
-        <ReportSelector
-          context={ReportContext.Stocktake}
-          onPrint={(report, args) => {
-            setReport(report);
-            setArgs(args);
-            setIsReportPrinting(true);
-          }}
-        >
+        <ReportSelector context={ReportContext.Stocktake} onPrint={printReport}>
           <LoadingButton
             variant="outlined"
             startIcon={<PrinterIcon />}
