95c87a885ff9af00c7282fe70c67af06eacbce15
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-10-11 12:47:12 +1300

    Improvementsx

--- client/packages/common/src/ui/components/inputs/Filters/EnumFilter.tsx
+++ client/packages/common/src/ui/components/inputs/Filters/EnumFilter.tsx
@@ -1,8 +1,11 @@
 import React, { FC, useState } from 'react';
 import { useUrlQuery } from '@common/hooks';
 import { Select } from '@common/components';
-// import { useTranslation } from '@common/intl';
-import { EndAdornment, FilterDefinitionCommon } from './FilterRoot';
+import {
+  EndAdornment,
+  FILTER_WIDTH,
+  FilterDefinitionCommon,
+} from './FilterRoot';
 
 export interface EnumFilterDefinition extends FilterDefinitionCommon {
   type: 'enum';
@@ -15,106 +18,46 @@ export const EnumFilter: FC<{
   filterDefinition: EnumFilterDefinition;
   remove: () => void;
 }> = ({ filterDefinition, remove }) => {
-  const { urlParameter, options, name, placeholder } = filterDefinition;
-  const [loading, setLoading] = useState(false);
+  const { urlParameter, options, name } = filterDefinition;
   const { urlQuery, updateQuery } = useUrlQuery();
   const [value, setValue] = useState<EnumOption | undefined | ''>(
     options.find(option => option.value === urlQuery[urlParameter]) ?? undefined
   );
 
-  const handleChange = (option: EnumOption | null | '') => {
-    setLoading(true);
-    if (option === null || option === '') {
-      setValue('');
-      updateQuery({ [urlParameter]: '' });
-    } else {
-      setValue(option);
-      updateQuery({ [urlParameter]: option.value });
-    }
-    setLoading(false);
+  const handleChange = (selection: string) => {
+    const option = options.find(opt => opt.value === selection);
+    if (!option) return;
+
+    setValue(option);
+    updateQuery({ [urlParameter]: option.value });
   };
 
   return (
-    <>
-      <Select
-        options={options}
-        placeholder={name}
-        InputProps={{
-          endAdornment: (
-            <EndAdornment
-              isLoading={loading}
-              hasValue={!!value}
-              onClear={remove}
-            />
-          ),
-          sx: {
-            width: '220px',
-          },
-          placeholder,
-        }}
-        sx={{
-          '& .MuiInputLabel-root': {
-            zIndex: 100,
-            top: '4px',
-            left: '8px',
-            color: 'gray.main',
-          },
-          '& .MuiInputLabel-root.Mui-focused': {
-            color: 'secondary.main',
-          },
-        }}
-        label={name}
-        value={value}
-        onChange={e =>
-          handleChange(options.find(opt => opt.value === e.target.value) ?? '')
-        }
-      />
-      {/* <Autocomplete
-        options={options}
-        placeholder={name}
-        onChange={(_, option) => handleChange(option)}
-        width="220"
-        // value={value}
-        inputProps={{
-          InputProps: {
-            // endAdornment: (
-            //   <EndAdornment
-            //     isLoading={loading}
-            //     hasValue={!!value}
-            //     onClear={() => handleChange('')}
-            //   />
-            // ),
-          },
-        }}
-        // renderInput={params => (
-        //   <BasicTextInput
-        //     InputProps={{
-        //       endAdornment: (
-        //         <EndAdornment
-        //           isLoading={loading}
-        //           hasValue={!!value}
-        //           onClear={() => handleChange('')}
-        //         />
-        //       ),
-        //       sx: { width: '220px' },
-        //     }}
-        //     // value={value}
-        //     onChange={e => {}}
-        //     label={'Test'}
-        //     sx={{
-        //       '& .MuiInputLabel-root': {
-        //         zIndex: 100,
-        //         top: '4px',
-        //         left: '8px',
-        //         color: 'gray.main',
-        //       },
-        //       '& .MuiInputLabel-root.Mui-focused': {
-        //         color: 'secondary.main',
-        //       },
-        //     }}
-        //   />
-        // )}
-      /> */}
-    </>
+    <Select
+      options={options}
+      placeholder={name}
+      InputProps={{
+        endAdornment: (
+          <EndAdornment isLoading={false} hasValue={!!value} onClear={remove} />
+        ),
+        sx: {
+          width: FILTER_WIDTH,
+        },
+      }}
+      sx={{
+        '& .MuiInputLabel-root': {
+          zIndex: 100,
+          top: '4px',
+          left: '8px',
+          color: 'gray.main',
+        },
+        '& .MuiInputLabel-root.Mui-focused': {
+          color: 'secondary.main',
+        },
+      }}
+      label={name}
+      value={value}
+      onChange={e => handleChange(e.target.value)}
+    />
   );
 };
