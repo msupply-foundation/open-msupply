7bad908f285e806a7bd2ae56bc46f718f4beff55
Mark Prins <mark@malcolmprins.net>
2023-10-12 15:34:07 +1300

    validate access & return cookie

--- server/server/src/cold_chain.rs
+++ server/server/src/cold_chain.rs
@@ -1,4 +1,5 @@
 use actix_web::{
+    cookie::Cookie,
     guard,
     http::header,
     web::{self, Data},
@@ -7,17 +8,22 @@ use actix_web::{
 use mime_guess::mime;
 use repository::RepositoryError;
 use service::{
+    auth::{
+        validate_auth, AuthDeniedKind, AuthError, Resource, ResourceAccessRequest, ValidatedUser,
+    },
     auth_data::AuthData,
     login::{LoginInput, LoginService},
-    service_provider::ServiceProvider,
+    service_provider::{ServiceContext, ServiceProvider},
+    user_account::UserAccountService,
 };
 
 // Fixed login response time in case of an error (see service)
 const MIN_ERR_RESPONSE_TIME_SEC: u64 = 6;
+const URL_PATH: &str = "/coldchain/v1";
 
 pub fn config_cold_chain(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::resource("/coldchain/v1/login")
+        web::resource(format!("{}/login", URL_PATH))
             .guard(guard::Post())
             .to(login),
     );
@@ -46,7 +52,7 @@ async fn do_login(
         .settings
         .sync_settings(&service_context)?
         .unwrap();
-    let response = match LoginService::local_login(
+    let response = match LoginService::login(
         &service_provider,
         &auth_data,
         LoginInput {
@@ -58,15 +64,30 @@ async fn do_login(
     )
     .await
     {
-        // TODO: validate user permission
-        // TODO: create JWT and return cookie
-        Ok(_) => HttpResponse::Ok()
-            .append_header(header::ContentType(mime::APPLICATION_JSON))
-            .body(
-                r#"{
-        "success": true
-    }"#,
-            ),
+        Ok(token) => {
+            match validate_access(
+                &service_provider,
+                &service_context,
+                &auth_data,
+                Some(token.token.clone()),
+            ) {
+                Ok(_) => {
+                    let cookie = Cookie::build("coldchain", token.token)
+                        .path(URL_PATH)
+                        .secure(true)
+                        .http_only(true)
+                        .finish();
+                    HttpResponse::Ok()
+                        .append_header(header::ContentType(mime::APPLICATION_JSON))
+                        .cookie(cookie)
+                        .body(r#"{ "success": true }"#)
+                }
+                Err(error) => {
+                    let formatted_error = format!("{:#?}", error);
+                    HttpResponse::Unauthorized().body(formatted_error)
+                }
+            }
+        }
         Err(error) => {
             let formatted_error = format!("{:#?}", error);
             HttpResponse::Unauthorized().body(formatted_error)
@@ -74,6 +95,44 @@ async fn do_login(
     };
 
     Ok(response)
+}
+
+/// Validates current user is authenticated and authorized
+pub fn validate_access(
+    service_provider: &ServiceProvider,
+    service_context: &ServiceContext,
+    auth_data: &AuthData,
+    token: Option<String>,
+) -> Result<ValidatedUser, AuthError> {
+    let user_service = UserAccountService::new(&service_context.connection);
+    let validated_user = validate_auth(auth_data, &token)?;
+    let store_id = match user_service.find_user(&validated_user.user_id)? {
+        Some(user) => {
+            let store_id = match user.default_store() {
+                Some(store) => Some(store.store_row.id.clone()),
+                None => None,
+            };
+            store_id
+        }
+        None => {
+            return Err(AuthError::Denied(AuthDeniedKind::NotAuthenticated(
+                "No default store".to_string(),
+            )))
+        }
+    };
+
+    let access_request = ResourceAccessRequest {
+        // TODO: Implement new resource
+        resource: Resource::CreateRepack,
+        store_id,
+    };
+
+    service_provider.validation_service.validate(
+        service_context,
+        auth_data,
+        &token,
+        &access_request,
+    )
 }
 
 #[derive(serde::Deserialize)]
