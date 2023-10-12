09009d975535a58892f20ad98af1225f2b401fd6
roxy-dao <roxy@msupply.foundation>
2023-10-12 14:39:31 +1300

    Rename item_variant and end point to get pack units

---
+++ server/graphql/general/src/queries/pack_unit.rs
@@ -0,0 +1,32 @@
+use async_graphql::*;
+use graphql_core::{standard_graphql_error::validate_auth, ContextExt};
+use graphql_types::types::PackUnitNode;
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    pack_unit::get_pack_units,
+};
+
+#[derive(SimpleObject)]
+pub struct PackUnitConnector {
+    total_count: u32,
+    nodes: Vec<PackUnitNode>,
+}
+
+pub fn pack_units(ctx: &Context<'_>, store_id: &str) -> Result<PackUnitConnector> {
+    validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::QueryItems,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let connection = ctx.get_connection_manager().connection()?;
+
+    let pack_units = get_pack_units(&connection, store_id)?;
+
+    Ok(PackUnitConnector {
+        total_count: pack_units.len() as u32,
+        nodes: PackUnitNode::from_vec(pack_units),
+    })
+}
