5bb216b56b4e1fdf4c12bcace006efcfd22a5ea9
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-10-17 13:18:34 +1300

    urlQuery to handle ranges

--- client/packages/common/src/hooks/useUrlQuery/useUrlQuery.ts
+++ client/packages/common/src/hooks/useUrlQuery/useUrlQuery.ts
@@ -1,13 +1,22 @@
 import { useSearchParams } from 'react-router-dom';
 
 export interface UrlQueryObject {
-  [key: string]: string | number | boolean;
+  [key: string]: string | number | boolean | RangeObject;
 }
 
+interface RangeObject {
+  from?: string | number;
+  to?: string | number;
+}
+
+// CONSTANTS
+const RANGE_SPLIT_CHAR = '_';
+
 interface useUrlQueryProps {
-  // an array of keys - the values of which should not be parsed before returning
-  // by default the value of parameters will be coerced to a number if !isNaN
-  // and to boolean if 'true' or 'false'. Specify keys here if you wish to opt out of this
+  // an array of keys - the values of which should not be parsed before
+  // returning by default the value of parameters will be coerced to a number if
+  // !isNaN and to boolean if 'true' or 'false'. Specify keys here if you wish
+  // to opt out of this
   skipParse?: string[];
 }
 export const useUrlQuery = ({ skipParse = [] }: useUrlQueryProps = {}) => {
@@ -20,7 +29,15 @@ export const useUrlQuery = ({ skipParse = [] }: useUrlQueryProps = {}) => {
 
     Object.entries(values).forEach(([key, value]) => {
       if (!value) delete newQueryObject[key];
-      else newQueryObject[key] = String(value);
+      else {
+        if (typeof value === 'object' && ('from' in value || 'to' in value)) {
+          const range = parseRangeString(newQueryObject[key]) as RangeObject;
+          const { from, to } = value;
+          if (from) range.from = from;
+          if (to) range.to = to;
+          newQueryObject[key] = stringifyRange(range);
+        } else newQueryObject[key] = String(value);
+      }
     });
 
     setSearchParams(newQueryObject, { replace: true });
@@ -29,6 +46,7 @@ export const useUrlQuery = ({ skipParse = [] }: useUrlQueryProps = {}) => {
   return {
     urlQuery: parseSearchParams(searchParams, skipParse),
     updateQuery,
+    parseRangeString,
   };
 };
 
@@ -36,13 +54,32 @@ export const useUrlQuery = ({ skipParse = [] }: useUrlQueryProps = {}) => {
 const parseSearchParams = (
   searchParams: URLSearchParams,
   skipParse: string[]
-) =>
+): Record<string, string | number | boolean | undefined> =>
   Object.fromEntries(
     Array.from(searchParams.entries()).map(([key, value]) => {
       if (skipParse.includes(key)) return [key, value];
-      if (!isNaN(Number(value))) return [key, Number(value)];
-      if (value === 'true') return [key, true];
-      if (value === 'false') return [key, false];
-      return [key, value];
+      return [key, unStringify(value)];
     })
   );
+
+// Coerce a string (from url) to a value of the correct data type
+const unStringify = (input: string | undefined) => {
+  if (!isNaN(Number(input))) return Number(input);
+  if (input === 'true') return true;
+  if (input === 'false') return false;
+  return input;
+};
+
+// Split a range string (e.g. "low:high") into a range object( {from: low, to:
+// high} )
+const parseRangeString = (value: string | undefined) => {
+  if (!value) return { from: undefined, to: undefined };
+  const values = value.split(RANGE_SPLIT_CHAR);
+  return { from: unStringify(values[0]), to: unStringify(values[1]) };
+};
+
+const stringifyRange = (range: RangeObject) => {
+  const { from, to } = range;
+  if (!from && !to) return '';
+  return `${from ?? ''}${RANGE_SPLIT_CHAR}${to ?? ''}`;
+};
