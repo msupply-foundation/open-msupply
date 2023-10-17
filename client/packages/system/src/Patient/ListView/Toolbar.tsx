f9ecf1b6a90bf6d250e7b851fd45ed9ec2c1f9e4
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2023-10-18 12:26:37 +1300

    Revert patient toolbar
    Source: https://github.com/msupply-foundation/open-msupply/commit/95aa177bc268d535055bd05dfb421d2567a10616

--- client/packages/system/src/Patient/ListView/Toolbar.tsx
+++ client/packages/system/src/Patient/ListView/Toolbar.tsx
@@ -22,25 +22,17 @@ export const Toolbar: FC<{ filter: FilterController }> = () => {
       <Box display="flex" gap={1}>
         <FilterMenu
           filters={[
-            // Can instantiate multiple related filters at once using this
-            // "group" option
             {
-              type: 'group',
-              name: 'Name',
-              elements: [
-                {
-                  type: 'text',
-                  name: t('label.first-name'),
-                  urlParameter: 'firstName',
-                  placeholder: t('placeholder.search-by-first-name'),
-                },
-                {
-                  type: 'text',
-                  name: t('label.last-name'),
-                  urlParameter: 'lastName',
-                  placeholder: t('placeholder.search-by-last-name'),
-                },
-              ],
+              type: 'text',
+              name: t('label.first-name'),
+              urlParameter: 'firstName',
+              placeholder: t('placeholder.search-by-first-name'),
+            },
+            {
+              type: 'text',
+              name: t('label.last-name'),
+              urlParameter: 'lastName',
+              placeholder: t('placeholder.search-by-last-name'),
             },
             {
               type: 'text',
@@ -48,67 +40,15 @@ export const Toolbar: FC<{ filter: FilterController }> = () => {
               urlParameter: 'identifier',
               placeholder: t('placeholder.search-by-identifier'),
             },
-            // A standalone "Date" input
-            {
-              type: 'date',
-              name: 'Date',
-              urlParameter: 'date',
-            },
-            // A standalone "Date/Time" input
-            {
-              type: 'dateTime',
-              name: 'Date/Time',
-              urlParameter: 'dateTime',
-            },
-            // Grouping two date inputs to form a "range". We define which
-            // element is which end of the range using the "range" property.
-            // Note they both update the same urlParameter
-            {
-              type: 'group',
-              name: 'Date Range',
-              elements: [
-                {
-                  type: 'date',
-                  name: 'From date',
-                  urlParameter: 'dateRange',
-                  range: 'from',
-                },
-                {
-                  type: 'date',
-                  name: 'To date',
-                  urlParameter: 'dateRange',
-                  range: 'to',
-                },
-              ],
-            },
-            // Same as above but using date/time
-            {
-              type: 'group',
-              name: 'Date/Time Range',
-              elements: [
-                {
-                  type: 'dateTime',
-                  name: 'From time',
-                  urlParameter: 'dateTimeRange',
-                  range: 'from',
-                },
-                {
-                  type: 'dateTime',
-                  name: 'To time',
-                  urlParameter: 'dateTimeRange',
-                  range: 'to',
-                },
-              ],
-            },
-            {
-              type: 'enum',
-              name: t('label.gender'),
-              urlParameter: 'gender',
-              options: [
-                { label: 'Male', value: 'MALE' },
-                { label: 'Female', value: 'FEMALE' },
-              ],
-            },
+            // {
+            //   type: 'enum',
+            //   name: t('label.gender'),
+            //   urlParameter: 'gender',
+            //   options: [
+            //     { label: 'Male', value: 'MALE' },
+            //     { label: 'Female', value: 'FEMALE' },
+            //   ],
+            // },
           ]}
         />
       </Box>
