e564f6bf7693e77fd9c33d1ce6f11cce33e81712
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-10-19 16:51:48 +1300

    Import UseDraftSensorControl from SensorEditModal component
    And change to non editable name
    And optional onSave for borrowed interface
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/5230b33bbca07f0af2ab7f665350981a91502b7d

--- client/packages/coldchain/src/Sensor/Components/SensorLineForm.tsx
+++ client/packages/coldchain/src/Sensor/Components/SensorLineForm.tsx
@@ -1,16 +1,11 @@
 import React, { FC } from 'react';
-import { SensorFragment } from '../api';
 import { LocationSearchInput } from '@openmsupply-client/system/src';
 import { useTranslation } from '@common/intl';
-import { BasicTextInput, InputWithLabelRow } from '@common/components';
+import { InputWithLabelRow } from '@common/components';
 import { Box, Formatter, TextWithLabelRow } from '@openmsupply-client/common';
+import { UseDraftSensorControl } from './SensorEditModal';
 
-interface SensorLineFormProps {
-  draft: SensorFragment;
-  onUpdate: (patch: Partial<SensorFragment>) => void;
-}
-
-export const SensorLineForm: FC<SensorLineFormProps> = ({
+export const SensorLineForm: FC<UseDraftSensorControl> = ({
   draft,
   onUpdate,
 }) => {
@@ -21,17 +16,11 @@ export const SensorLineForm: FC<SensorLineFormProps> = ({
 
   return (
     <Box display="flex" flexDirection="column" gap={2}>
-      <InputWithLabelRow
+      <TextWithLabelRow
+        sx={textSx}
         label={t('label.sensor-name')}
-        labelProps={inputTextAlign}
-        Input={
-          <BasicTextInput
-            value={draft.name ?? ''}
-            onChange={e => onUpdate({ name: e.target.value })}
-            disabled={!!draft.name}
-          />
-        }
-      />
+        text={draft.name}
+      ></TextWithLabelRow>
       <InputWithLabelRow
         label={t('label.location')}
         labelProps={inputTextAlign}
