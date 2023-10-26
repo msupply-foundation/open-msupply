9c5d31d39ac5fc1670a1178285631961d7df27cd
roxy-dao <roxy@msupply.foundation>
2023-10-27 06:39:40 +1300

    Fix tests + inventory alphabetical ordering

--- client/packages/common/src/utils/formatters/formatters.ts
+++ client/packages/common/src/utils/formatters/formatters.ts
@@ -13,7 +13,8 @@ export const Formatter = {
     else return null;
   },
   naiveDateTime: (date?: Date | null): string | null => {
-    if (date && isValid(date)) return format(date, "yyyy-MM-dd'T'HH:mm:ss");
+    if (date && isValid(date))
+      return format(date, "yyyy-MM-dd'T'HH:mm:ss+00:00");
     else return null;
   },
   expiryDate: (date?: Date | null): string | null => {
