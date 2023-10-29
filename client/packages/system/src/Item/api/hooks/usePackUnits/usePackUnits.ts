dc6d1061cc192a826c39907bec22fc78a7ccd0a1
Clemens Zeidler <clemens@msupply.foundation>
2023-10-30 10:47:05 +1300

    Fetch pack units at startup and tweak cache times
    Source: https://github.com/msupply-foundation/open-msupply/commit/253be71e8edd80cd0ef980d69c47740faab2bd17

--- client/packages/system/src/Item/api/hooks/usePackUnits/usePackUnits.ts
+++ client/packages/system/src/Item/api/hooks/usePackUnits/usePackUnits.ts
@@ -4,11 +4,12 @@ import { useItemApi } from '../useItemApi';
 export const usePackUnits = () => {
   const api = useItemApi();
 
-  // Assume pack units don't change often, i.e. don't do an API calls every time you need them
-  const cacheTime = 60 * 60 * 1000; // 1h
+  // Always use previously fetched values
+  const cacheTime = 10 * 365 * 24 * 60 * 60 * 1000; // 10 years
+  // Assume pack units don't change often, i.e. only refetch every once a while
+  const staleTime = 60 * 60 * 1000; // 1h
   return useQuery(api.keys.packUnits(), api.get.packUnits, {
     cacheTime,
-    // don't refetch
-    staleTime: cacheTime,
+    staleTime,
   });
 };
