0ca5dfbc7a050fa5ce11d07c2629329aa8ca22c7
roxy-dao <roxy@msupply.foundation>
2023-10-31 10:33:33 +1300

    Temperature log filters

--- client/packages/coldchain/src/Monitoring/api/TemperatureLog/hooks/document/useTemperatureLogs.ts
+++ client/packages/coldchain/src/Monitoring/api/TemperatureLog/hooks/document/useTemperatureLogs.ts
@@ -1,20 +1,11 @@
-import { useUrlQueryParams } from '@common/hooks';
 import { useTemperatureLogApi } from '../utils/useTemperatureLogApi';
 import { useQuery } from '@openmsupply-client/common';
 import { ListParams } from '../../api';
 
-export const useTemperatureLogs = () => {
-  const { queryParams } = useUrlQueryParams({
-    initialSort: { key: 'datetime', dir: 'desc' },
-    filters: [{ key: 'datetime' }],
-  });
-
+export const useTemperatureLogs = (query: ListParams) => {
   const api = useTemperatureLogApi();
 
   return {
-    ...useQuery(
-      api.keys.paramList(queryParams as ListParams),
-      api.get.list(queryParams as ListParams)
-    ),
+    ...useQuery(api.keys.paramList(query), api.get.list(query)),
   };
 };
