5e86ba6a98f9425295fc65297da6f8fc294a48d7
Mark Prins <mark@malcolmprins.net>
2023-11-02 13:15:41 +1300

    make remove optional for locations; fix sensor location removal
    Source: https://github.com/msupply-foundation/open-msupply/commit/c9fbf5810c17ab2773a7b15fd4c82026c5739ea1

--- client/packages/common/src/utils/functions/ApiUtils.tsx
+++ client/packages/common/src/utils/functions/ApiUtils.tsx
@@ -2,9 +2,9 @@ export const setNullableInput = <NODE, KEY extends keyof NODE>(
   key: KEY,
   entity?: NODE | null | undefined
 ) => {
-  if (!entity) return undefined;
-
   if (entity == null) return { value: undefined };
+
+  if (!entity) return undefined;
 
   return { value: entity[key] };
 };
