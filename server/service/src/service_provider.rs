1b92710b8d46f3f77b19de2feef4fb6f847d23e8
roxy-dao <roxy@msupply.foundation>
2023-11-03 02:34:37 +1300

    Delete hard coded missing master list
    Source: https://github.com/msupply-foundation/open-msupply/commit/b16ebd3f78d212ff3728584e28253ca27abe7d29

--- server/service/src/service_provider.rs
+++ server/service/src/service_provider.rs
@@ -21,7 +21,6 @@ use crate::{
     location::{LocationService, LocationServiceTrait},
     log_service::{LogService, LogServiceTrait},
     master_list::{MasterListService, MasterListServiceTrait},
-    missing_program::create_missing_master_list_and_program,
     name::get_names,
     processors::ProcessorsTrigger,
     programs::{
@@ -254,14 +253,6 @@ pub trait GeneralServiceTrait: Sync + Send {
         service_provider: &ServiceProvider,
     ) -> Result<(), RepositoryError> {
         create_system_user(service_provider)
-    }
-
-    // TODO: Delete when soft delete for master list is implemented
-    fn create_missing_master_list_and_program(
-        &self,
-        service_provider: &ServiceProvider,
-    ) -> Result<(), RepositoryError> {
-        create_missing_master_list_and_program(service_provider)
     }
 }
 
