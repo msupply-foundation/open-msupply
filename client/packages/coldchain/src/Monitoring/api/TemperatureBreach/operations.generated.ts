280d487ca9737107a35ffa057c3b4d59c53cd6c3
roxy-dao <roxy@msupply.foundation>
2023-11-02 21:19:58 +1300

    Generate
    Source: https://github.com/msupply-foundation/open-msupply/commit/0ed8c2b3d8d5390cc9c72f95d94af61b68b29f80

--- client/packages/coldchain/src/Monitoring/api/TemperatureBreach/operations.generated.ts
+++ client/packages/coldchain/src/Monitoring/api/TemperatureBreach/operations.generated.ts
@@ -3,124 +3,66 @@ import * as Types from '@openmsupply-client/common';
 import { GraphQLClient } from 'graphql-request';
 import { GraphQLClientRequestHeaders } from 'graphql-request/build/cjs/types';
 import gql from 'graphql-tag';
-import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw';
-export type TemperatureBreachFragment = {
-  __typename: 'TemperatureBreachNode';
-  id: string;
-  acknowledged: boolean;
-  durationMilliseconds: number;
-  endDatetime?: string | null;
-  startDatetime: string;
-  type: Types.TemperatureBreachNodeType;
-  maxOrMinTemperature?: number | null;
-  sensor?: { __typename: 'SensorNode'; id: string; name: string } | null;
-  location?: { __typename: 'LocationNode'; name: string } | null;
-};
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
+export type TemperatureBreachFragment = { __typename: 'TemperatureBreachNode', id: string, acknowledged: boolean, durationMilliseconds: number, endDatetime?: string | null, startDatetime: string, type: Types.TemperatureBreachNodeType, maxOrMinTemperature?: number | null, sensor?: { __typename: 'SensorNode', id: string, name: string } | null, location?: { __typename: 'LocationNode', name: string } | null };
 
 export type Temperature_BreachesQueryVariables = Types.Exact<{
   page?: Types.InputMaybe<Types.PaginationInput>;
-  sort?: Types.InputMaybe<
-    Array<Types.TemperatureBreachSortInput> | Types.TemperatureBreachSortInput
-  >;
+  sort?: Types.InputMaybe<Array<Types.TemperatureBreachSortInput> | Types.TemperatureBreachSortInput>;
   filter?: Types.InputMaybe<Types.TemperatureBreachFilterInput>;
   storeId: Types.Scalars['String']['input'];
 }>;
 
-export type Temperature_BreachesQuery = {
-  __typename: 'Queries';
-  temperatureBreaches: {
-    __typename: 'TemperatureBreachConnector';
-    totalCount: number;
-    nodes: Array<{
-      __typename: 'TemperatureBreachNode';
-      id: string;
-      acknowledged: boolean;
-      durationMilliseconds: number;
-      endDatetime?: string | null;
-      startDatetime: string;
-      type: Types.TemperatureBreachNodeType;
-      maxOrMinTemperature?: number | null;
-      sensor?: { __typename: 'SensorNode'; id: string; name: string } | null;
-      location?: { __typename: 'LocationNode'; name: string } | null;
-    }>;
-  };
-};
+
+export type Temperature_BreachesQuery = { __typename: 'Queries', temperatureBreaches: { __typename: 'TemperatureBreachConnector', totalCount: number, nodes: Array<{ __typename: 'TemperatureBreachNode', id: string, acknowledged: boolean, durationMilliseconds: number, endDatetime?: string | null, startDatetime: string, type: Types.TemperatureBreachNodeType, maxOrMinTemperature?: number | null, sensor?: { __typename: 'SensorNode', id: string, name: string } | null, location?: { __typename: 'LocationNode', name: string } | null }> } };
 
 export const TemperatureBreachFragmentDoc = gql`
-  fragment TemperatureBreach on TemperatureBreachNode {
-    __typename
+    fragment TemperatureBreach on TemperatureBreachNode {
+  __typename
+  id
+  acknowledged
+  durationMilliseconds
+  endDatetime
+  startDatetime
+  type
+  maxOrMinTemperature
+  sensor {
     id
-    acknowledged
-    durationMilliseconds
-    endDatetime
-    startDatetime
-    type
-    maxOrMinTemperature
-    sensor {
-      id
-      name
-    }
-    location {
-      name
-    }
+    name
   }
-`;
+  location {
+    name
+  }
+}
+    `;
 export const Temperature_BreachesDocument = gql`
-  query temperature_breaches(
-    $page: PaginationInput
-    $sort: [TemperatureBreachSortInput!]
-    $filter: TemperatureBreachFilterInput
-    $storeId: String!
+    query temperature_breaches($page: PaginationInput, $sort: [TemperatureBreachSortInput!], $filter: TemperatureBreachFilterInput, $storeId: String!) {
+  temperatureBreaches(
+    page: $page
+    sort: $sort
+    filter: $filter
+    storeId: $storeId
   ) {
-    temperatureBreaches(
-      page: $page
-      sort: $sort
-      filter: $filter
-      storeId: $storeId
-    ) {
-      ... on TemperatureBreachConnector {
-        totalCount
-        nodes {
-          ...TemperatureBreach
-        }
+    ... on TemperatureBreachConnector {
+      totalCount
+      nodes {
+        ...TemperatureBreach
       }
     }
   }
-  ${TemperatureBreachFragmentDoc}
-`;
+}
+    ${TemperatureBreachFragmentDoc}`;
 
-export type SdkFunctionWrapper = <T>(
-  action: (requestHeaders?: Record<string, string>) => Promise<T>,
-  operationName: string,
-  operationType?: string
-) => Promise<T>;
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
 
-const defaultWrapper: SdkFunctionWrapper = (
-  action,
-  _operationName,
-  _operationType
-) => action();
 
-export function getSdk(
-  client: GraphQLClient,
-  withWrapper: SdkFunctionWrapper = defaultWrapper
-) {
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationType) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
-    temperature_breaches(
-      variables: Temperature_BreachesQueryVariables,
-      requestHeaders?: GraphQLClientRequestHeaders
-    ): Promise<Temperature_BreachesQuery> {
-      return withWrapper(
-        wrappedRequestHeaders =>
-          client.request<Temperature_BreachesQuery>(
-            Temperature_BreachesDocument,
-            variables,
-            { ...requestHeaders, ...wrappedRequestHeaders }
-          ),
-        'temperature_breaches',
-        'query'
-      );
-    },
+    temperature_breaches(variables: Temperature_BreachesQueryVariables, requestHeaders?: GraphQLClientRequestHeaders): Promise<Temperature_BreachesQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<Temperature_BreachesQuery>(Temperature_BreachesDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'temperature_breaches', 'query');
+    }
   };
 }
 export type Sdk = ReturnType<typeof getSdk>;
@@ -136,14 +78,8 @@ export type Sdk = ReturnType<typeof getSdk>;
  *   )
  * })
  */
-export const mockTemperatureBreachesQuery = (
-  resolver: ResponseResolver<
-    GraphQLRequest<Temperature_BreachesQueryVariables>,
-    GraphQLContext<Temperature_BreachesQuery>,
-    any
-  >
-) =>
+export const mockTemperatureBreachesQuery = (resolver: ResponseResolver<GraphQLRequest<Temperature_BreachesQueryVariables>, GraphQLContext<Temperature_BreachesQuery>, any>) =>
   graphql.query<Temperature_BreachesQuery, Temperature_BreachesQueryVariables>(
     'temperature_breaches',
     resolver
-  );
+  )
