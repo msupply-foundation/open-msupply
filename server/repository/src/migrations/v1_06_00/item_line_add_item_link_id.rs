96d65d19cf7913414bc21ceea0d774f342b0aa2d
Chris Petty <chris@sussol.net>
2023-11-17 12:55:48 +1300

    Add stock_line.item_link_id

--- server/repository/src/migrations/v1_06_00/item_line_add_item_link_id.rs
+++ server/repository/src/migrations/v1_06_00/item_line_add_item_link_id.rs
@@ -4,8 +4,11 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
         r#"
-        ALTER TABLE stock_line
-        ADD COLUMN item_link_id TEXT NOT NULL REFERENCES item_link(id);
+        PRAGMA foreign_keys = OFF;
+        ALTER TABLE stock_line ADD COLUMN item_link_id TEXT NOT NULL REFERENCES item_link(id);
+        UPDATE stock_line SET item_link_id = item_id;   
+        PRAGMA foreign_keys = ON;
+        ALTER TABLE stock_line DROP COLUMN item_id;
         "#,
     )?;
 
