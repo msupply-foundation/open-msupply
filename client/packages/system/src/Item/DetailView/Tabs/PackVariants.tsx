406aa1f9d063477e3b96fef44c2a8589cfc72f2e
roxy-dao <roxy@msupply.foundation>
2023-11-17 10:00:15 +1300

    Add button to create pack variant and add onRow click
    Source: https://github.com/msupply-foundation/open-msupply/commit/e77c9045cc8654c722a260f5e22f928225c9aec9

--- client/packages/system/src/Item/DetailView/Tabs/PackVariants.tsx
+++ client/packages/system/src/Item/DetailView/Tabs/PackVariants.tsx
@@ -8,12 +8,28 @@ import {
   createTableStore,
   NothingHere,
   useTranslation,
+  ButtonWithIcon,
+  PlusCircleIcon,
+  Grid,
+  useEditModal,
+  useAuthContext,
+  useNotification,
+  UserPermission,
 } from '@openmsupply-client/common';
 import { usePackVariant } from '../../context';
+import { PackVariantEditModal } from '../../Components/PackVariantEditModal';
 
 const PackVariantTable: FC<{ itemId?: string }> = ({ itemId }) => {
-  const { variantsControl } = usePackVariant(itemId ?? '', null);
   const t = useTranslation('catalogue');
+  const { variantsControl } = usePackVariant(itemId ?? '', null);
+  const { isOpen, entity, mode, onClose, onOpen } =
+    useEditModal<VariantFragment>();
+  const { info } = useNotification();
+  const { userHasPermission } = useAuthContext();
+  const infoSnack = info(t('auth.permission-denied'));
+  const hasPermission = userHasPermission(
+    UserPermission.ItemNamesCodesAndUnitsMutate
+  );
 
   const columns = useColumns<VariantFragment>([
     'packSize',
@@ -32,12 +48,31 @@ const PackVariantTable: FC<{ itemId?: string }> = ({ itemId }) => {
   ]);
 
   return (
-    <DataTable
-      id="item-variants-detail"
-      data={variantsControl?.variants ?? []}
-      columns={columns}
-      noDataElement={<NothingHere body={t('error.no-item-variants')} />}
-    />
+    <>
+      {isOpen && (
+        <PackVariantEditModal
+          mode={mode}
+          isOpen={isOpen}
+          onClose={onClose}
+          packVariant={entity}
+          itemId={itemId ?? ''}
+        />
+      )}
+      <Box display="flex" justifyContent="flex-end" paddingBottom={2}>
+        <ButtonWithIcon
+          Icon={<PlusCircleIcon />}
+          label={t('label.new-pack-variant')}
+          onClick={hasPermission ? () => onOpen() : () => infoSnack()}
+        />
+      </Box>
+      <DataTable
+        id="item-variants-detail"
+        data={variantsControl?.variants ?? []}
+        columns={columns}
+        noDataElement={<NothingHere body={t('error.no-item-variants')} />}
+        onRowClick={hasPermission ? onOpen : infoSnack}
+      />
+    </>
   );
 };
 
@@ -48,11 +83,11 @@ interface PackVariantsTabProps {
 export const PackVariantsTab: FC<PackVariantsTabProps> = ({ itemId }) => {
   return (
     <Box justifyContent="center" display="flex" flex={1} paddingTop={3}>
-      <Box flex={1} display="flex" style={{ maxWidth: 1000 }}>
+      <Grid container gap={1} maxWidth={1000} display="block">
         <TableProvider createStore={createTableStore}>
           <PackVariantTable itemId={itemId} />
         </TableProvider>
-      </Box>
+      </Grid>
     </Box>
   );
 };
