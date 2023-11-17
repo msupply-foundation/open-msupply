fabb43d8c312e0bbeb20ff857150dbb9ca406a65
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-11-17 12:06:18 +0900

    Update dashboard spacing

--- client/packages/common/src/ui/components/domain/Dashboard/StatsPanel.tsx
+++ client/packages/common/src/ui/components/domain/Dashboard/StatsPanel.tsx
@@ -23,10 +23,15 @@ export interface StatsPanelProps {
 const Statistic: FC<Stat> = ({ label, value, link }) => {
   const t = useTranslation();
   return (
-    <Grid container alignItems="center" style={{ marginTop: '8px' }}>
-      <Grid item>
+    <Grid container alignItems="center" sx={{ marginTop: 1 }}>
+      <Grid
+        item
+        sx={{ minWidth: '30px', display: 'flex', justifyContent: 'flex-end' }}
+      >
         {value ? (
-          <Typography style={{ fontSize: 24, fontWeight: 'bold' }}>
+          <Typography
+            style={{ fontSize: 24, fontWeight: 'bold', lineHeight: 1.2 }}
+          >
             {value}
           </Typography>
         ) : (
