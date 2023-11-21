c0470ff93494ed1f2527890bf76d85781ffee39f
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2023-11-22 06:46:05 +0900

    Add graphql calls
    Also rename log to activity log
    And add api

---
+++ client/packages/host/src/Admin/WebAppSettings.tsx
@@ -0,0 +1,18 @@
+// import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+// import {
+//   Typography,
+//   useNavigate,
+//   useTranslation,
+// } from '@openmsupply-client/common';
+
+// import { AppRoute } from '@openmsupply-client/config';
+
+// import { Setting } from './Setting';
+
+export const WebAppSettings = () => {
+  //   navigate = useNavigate();
+  //   const t = useTranslation('common');
+  return <></>;
+};
