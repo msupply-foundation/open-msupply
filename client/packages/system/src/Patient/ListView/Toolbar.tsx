8e7eb1cacdec62ed33a39947736d3583124e04ea
roxy-dao <roxy@msupply.foundation>
2023-11-28 09:40:44 +1300

    Add default filter option

--- client/packages/system/src/Patient/ListView/Toolbar.tsx
+++ client/packages/system/src/Patient/ListView/Toolbar.tsx
@@ -28,18 +28,21 @@ export const Toolbar: FC<{ filter: FilterController }> = () => {
               name: t('label.first-name'),
               urlParameter: 'firstName',
               placeholder: t('placeholder.search-by-first-name'),
+              isDefault: true,
             },
             {
               type: 'text',
               name: t('label.last-name'),
               urlParameter: 'lastName',
               placeholder: t('placeholder.search-by-last-name'),
+              isDefault: true,
             },
             {
               type: 'text',
               name: t('label.patient-id'),
               urlParameter: 'identifier',
               placeholder: t('placeholder.search-by-identifier'),
+              isDefault: true,
             },
             {
               type: 'date',
