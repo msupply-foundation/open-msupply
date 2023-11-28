c4b5bb2607f13d1430d814b72d3c133c88aa0585
roxy-dao <roxy@msupply.foundation>
2023-11-29 08:52:34 +1300

    Set error for blood pressure
    Source: https://github.com/msupply-foundation/open-msupply/commit/bd3284c2e8bb8b25ecd4165d1b61a66e54911019

--- client/packages/programs/src/JsonForms/components/BloodPressure.tsx
+++ client/packages/programs/src/JsonForms/components/BloodPressure.tsx
@@ -3,6 +3,7 @@ import {
   ControlProps,
   composePaths,
   rankWith,
+  subErrorsAt,
   uiTypeIs,
 } from '@jsonforms/core';
 import {
@@ -10,20 +11,39 @@ import {
   DetailInputWithLabelRow,
   NumUtils,
   NumericTextInput,
+  Typography,
+  useTranslation,
 } from '@openmsupply-client/common';
 import { FORM_LABEL_WIDTH } from '../common';
-import { withJsonFormsControlProps } from '@jsonforms/react';
+import { useJsonForms, withJsonFormsControlProps } from '@jsonforms/react';
+import { useJSONFormsCustomError } from '../common/hooks/useJSONFormsCustomError';
 
 export const bloodPressureTester = rankWith(10, uiTypeIs('BloodPressure'));
 
 export const UIComponent = (props: ControlProps) => {
-  const { data, handleChange, label, path, errors, schema } = props;
+  const t = useTranslation('programs');
+  const { data, handleChange, label, path, schema } = props;
+  const { core } = useJsonForms();
   const [systolic, setSystolic] = React.useState<number | undefined>(undefined);
   const [diastolic, setDiastolic] = React.useState<number | undefined>(
     undefined
   );
   const systolicPath = composePaths(path, 'systolic');
   const diastolicPath = composePaths(path, 'diastolic');
+  const { customError, setCustomError } = useJSONFormsCustomError(
+    path,
+    'BloodPressure'
+  );
+
+  console.log('core ', core?.errors);
+
+  useEffect(() => {
+    if (core) {
+      const getChildErrors = subErrorsAt(path, schema);
+      const errors = getChildErrors(core);
+      setCustomError(errors[0]?.message);
+    }
+  }, [core]);
 
   if (!props.visible) {
     return null;
@@ -56,10 +76,10 @@ export const UIComponent = (props: ControlProps) => {
 
   const inputProps = {
     type: 'number',
-    error: !!errors,
-    helperText: errors,
     min: schema.minimum ?? 0,
     max: schema.maximum ?? NumUtils.MAX_SAFE_API_INTEGER,
+    error: !!customError,
+    InputLabelProps: { shrink: true },
   };
 
   return (
@@ -67,20 +87,32 @@ export const UIComponent = (props: ControlProps) => {
       label={label}
       labelWidthPercentage={FORM_LABEL_WIDTH}
       inputAlignment="start"
-      sx={{ paddingTop: 0.5 }}
+      sx={{ paddingTop: 1 }}
       Input={
-        <Box display="flex" flexDirection="row" paddingLeft={0.5}>
-          <NumericTextInput
-            onChange={onChangeSystolic}
-            value={systolic}
-            {...inputProps}
-          />
-          <Box mx={1}>/</Box>
-          <NumericTextInput
-            onChange={onChangeDiastolic}
-            value={diastolic}
-            {...inputProps}
-          />
+        <Box display="flex" flexDirection="column">
+          <Box display="flex" flexDirection="row" paddingLeft={0.5}>
+            <NumericTextInput
+              onChange={onChangeSystolic}
+              value={systolic}
+              label={t('label.systolic')}
+              {...inputProps}
+            />
+            <Typography margin={1} paddingTop={2}>
+              /
+            </Typography>
+            <NumericTextInput
+              onChange={onChangeDiastolic}
+              value={diastolic}
+              label={t('label.diastolic')}
+              width={100}
+              {...inputProps}
+            />
+          </Box>
+          <Box display="flex" flexDirection="row" alignSelf="center">
+            {customError && (
+              <Typography variant="caption">{customError}</Typography>
+            )}
+          </Box>
         </Box>
       }
     />
