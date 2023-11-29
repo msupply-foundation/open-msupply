1ccd24d228c2411ebe87b228a288c1976e94798f
roxy-dao <roxy@msupply.foundation>
2023-11-30 01:51:29 +1300

    PR suggestions
    Source: https://github.com/msupply-foundation/open-msupply/commit/8ede126ef1c0447bdeabe0cc7bcf27b81b1c1042

--- client/packages/host/src/Admin/LogTextDisplay.tsx
+++ client/packages/host/src/Admin/LogTextDisplay.tsx
@@ -2,38 +2,32 @@ import { Tooltip, Typography } from '@common/components';
 import React from 'react';
 
 export const LogTextDisplay = ({ logText }: { logText: string[] | string }) => {
-  if (Array.isArray(logText)) {
-    return (
-      <>
-        {logText.map((logLine, i) => (
-          <Tooltip key={i} title={logLine}>
-            <Typography
-              sx={{
-                overflow: 'ellipsis',
-                textOverflow: 'ellipsis',
-                whiteSpace: 'pre',
-                maxWidth: '100%',
-              }}
-              noWrap
-              component="div"
-              key={i}
-            >
-              {`${logLine}`}
-            </Typography>
-          </Tooltip>
-        ))}
-      </>
-    );
-  } else {
-    return (
-      <Tooltip title={logText}>
-        <Typography
-          sx={{ overflow: 'scroll', whiteSpace: 'pre' }}
-          component="div"
-        >
-          {`${logText}`}
-        </Typography>
-      </Tooltip>
-    );
-  }
+  return Array.isArray(logText) ? (
+    <>
+      {logText.map((logLine, i) => (
+        <Tooltip key={i} title={logLine}>
+          <Typography
+            sx={{
+              whiteSpace: 'pre',
+              maxWidth: '100%',
+            }}
+            noWrap
+            component="div"
+            key={i}
+          >
+            {`${logLine}`}
+          </Typography>
+        </Tooltip>
+      ))}
+    </>
+  ) : (
+    <Tooltip title={logText}>
+      <Typography
+        sx={{ overflow: 'scroll', whiteSpace: 'pre' }}
+        component="div"
+      >
+        {`${logText}`}
+      </Typography>
+    </Tooltip>
+  );
 };
--- client/packages/host/src/Admin/WebAppLogFileModal.tsx
+++ client/packages/host/src/Admin/WebAppLogFileModal.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { useDialog, useWebClient } from '@common/hooks';
+import React, { useEffect, useState } from 'react';
+import { useDialog, useNotification, useWebClient } from '@common/hooks';
 import { useTranslation } from '@common/intl';
 import {
   BasicSpinner,
   DialogButton,
   DropdownMenu,
   DropdownMenuItem,
-  LoadingButton,
   Typography,
-} from '@common/components';
+  Box,
+} from '@openmsupply-client/common';
 import { useLog } from '@openmsupply-client/system';
-import { Box, CopyIcon, SaveIcon } from 'packages/common/src';
 import { LogTextDisplay } from './LogTextDisplay';
 
 export const LogDisplay = ({
@@ -20,27 +19,32 @@ export const LogDisplay = ({
   fileName: string;
   setLogContent: (content: string[]) => void;
 }) => {
-  const { data, isLoading } = useLog.document.logContentsByFileName(fileName);
+  const { mutateAsync, data, isLoading } =
+    useLog.document.logContentsByFileName();
 
-  if (data?.fileContent !== undefined && data?.fileContent !== null) {
-    setLogContent(data?.fileContent);
-  }
+  useEffect(() => {
+    if (fileName) {
+      mutateAsync(fileName);
+    }
+  }, [fileName]);
 
-  if (isLoading) {
-    return (
-      <>
-        <BasicSpinner></BasicSpinner>
-      </>
-    );
-  } else {
-    return (
-      <>
-        {Array.isArray(data?.fileContent) && data?.fileContent != undefined ? (
+  useEffect(() => {
+    if (!!data?.fileContent) {
+      setLogContent(data.fileContent);
+    }
+  }, [data]);
+
+  return isLoading ? (
+    <BasicSpinner />
+  ) : (
+    <>
+      {Array.isArray(data?.fileContent) && data?.fileContent != undefined ? (
+        <Box paddingTop={2} maxHeight={400}>
           <LogTextDisplay logText={data?.fileContent}></LogTextDisplay>
-        ) : null}
-      </>
-    );
-  }
+        </Box>
+      ) : null}
+    </>
+  );
 };
 
 export const WebAppLogFileModal = ({
@@ -50,29 +54,37 @@ export const WebAppLogFileModal = ({
   isOpen: boolean;
   onClose: () => void;
 }) => {
-  const t = useTranslation('common');
+  const t = useTranslation();
+  const { success, warning } = useNotification();
   const [logToRender, setLogToRender] = useState('');
   const [logContent, setLogContent] = useState<string[]>([]);
   const { Modal } = useDialog({ isOpen });
   const { saveFile } = useWebClient();
   const [isSaving, setIsSaving] = useState(false);
+  const noLog = logContent.length === 0;
 
-  const {
-    data: logFiles,
-    isError,
-    isLoading: logListLoading,
-  } = useLog.document.listFileNames();
+  const { data, isError, isLoading } = useLog.document.listFileNames();
 
   const saveLog = async () => {
-    setIsSaving(true);
-    await saveFile({
-      content: logContent.toString(),
-      filename: `${logToRender}-exported-log.txt`,
-    });
-    setIsSaving(false);
+    if (noLog) {
+      warning(t('message.nothing-to-save'))();
+    } else if (isSaving) {
+      warning(t('message.already-saving'))();
+    } else {
+      setIsSaving(true);
+      saveFile({
+        content: logContent.toString(),
+        filename: logToRender,
+      });
+      setIsSaving(false);
+    }
   };
 
-  // should add error condition for second hook
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(logContent.toString()).then(() => {
+      success(t('message.copy-success'))();
+    });
+  };
 
   if (isError) {
     return (
@@ -80,10 +92,8 @@ export const WebAppLogFileModal = ({
         title={t('heading.server-log')}
         okButton={<DialogButton variant="ok" onClick={onClose} />}
       >
-        <Box sx={{ padding: 2 }}>
-          <Typography sx={{ color: 'error.main' }}>
-            {t('error.unable-to-load-data')}
-          </Typography>
+        <Box sx={{ padding: 2 }} textAlign="center">
+          <Typography>{t('error.unable-to-server-log')}</Typography>
         </Box>
       </Modal>
     );
@@ -92,54 +102,44 @@ export const WebAppLogFileModal = ({
   return (
     <Modal
       title={t('heading.server-log')}
-      copyContent={
-        <>
-          <LoadingButton
-            isLoading={logListLoading}
-            onClick={() => {
-              navigator.clipboard.writeText(logContent.toString());
-            }}
-            disabled={logContent.length === 0}
-            startIcon={<CopyIcon />}
-          >
-            {t('link.copy-to-clipboard')}
-          </LoadingButton>
-          <LoadingButton
-            startIcon={<SaveIcon />}
-            onClick={saveLog}
-            disabled={logContent.length === 0 || isSaving}
-            isLoading={false}
-          >
-            {t('button.save-log')}
-          </LoadingButton>
-          <Box marginLeft="8px">
-            <DialogButton variant="ok" onClick={onClose} />
-          </Box>
-        </>
+      okButton={<DialogButton variant="ok" onClick={onClose} />}
+      width={850}
+      height={700}
+      copyButton={
+        <DialogButton
+          variant="copy"
+          onClick={
+            noLog
+              ? () => warning(t('message.nothing-to-copy'))()
+              : copyToClipboard
+          }
+          color="primary"
+        />
+      }
+      saveButton={
+        <DialogButton variant="save" onClick={saveLog} color="primary" />
       }
     >
-      <>
-        {!logListLoading ? (
-          <>
-            <DropdownMenu
-              label={logToRender ? logToRender : t('label.server-log')}
-            >
-              {logFiles?.fileNames?.map((fileName, i) => (
-                <DropdownMenuItem
-                  key={i}
-                  onClick={() => {
-                    setLogToRender(fileName);
-                  }}
-                >{`${fileName}`}</DropdownMenuItem>
-              ))}
-            </DropdownMenu>
+      {!isLoading ? (
+        <>
+          <DropdownMenu
+            label={logToRender ? logToRender : t('label.server-log')}
+          >
+            {data?.fileNames?.map((fileName, i) => (
+              <DropdownMenuItem
+                key={i}
+                onClick={() => {
+                  setLogToRender(fileName);
+                }}
+              >{`${fileName}`}</DropdownMenuItem>
+            ))}
+          </DropdownMenu>
 
-            <LogDisplay fileName={logToRender} setLogContent={setLogContent} />
-          </>
-        ) : (
-          <BasicSpinner />
-        )}
-      </>
+          <LogDisplay fileName={logToRender} setLogContent={setLogContent} />
+        </>
+      ) : (
+        <BasicSpinner />
+      )}
     </Modal>
   );
 };
