b2116c678524c794b64d0d3ba5917021742945bd
roxy-dao <roxy@msupply.foundation>
2023-12-01 21:48:51 +1300

    Fix merge error and add postgres migrations

--- server/repository/src/migrations/v1_06_00/stocktake_line_add_item_link_id.rs
+++ server/repository/src/migrations/v1_06_00/stocktake_line_add_item_link_id.rs
@@ -1,27 +1,46 @@
 use crate::{migrations::sql, StorageConnection};
 
 pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
-    sql!(
+    if cfg!(feature = "postgres") {
+        sql!(
+            connection,
+            r#"
+            -- Adding stocktake_line.item_link_id
+            ALTER TABLE stocktake_line
+            ADD COLUMN item_link_id TEXT NOT NULL DEFAULT 'temp_for_migration';
+        
+            UPDATE stocktake_line
+            SET item_link_id = item_id;
+        
+            ALTER TABLE stocktake_line ADD CONSTRAINT stocktake_line_item_link_id_fkey FOREIGN KEY (item_link_id) REFERENCES item_link(id);
+       "#,
+        )?;
+    } else {
+        sql!(
+            connection,
+            r#"
+            -- Adding stocktake_line.item_link_id
+            PRAGMA foreign_keys = OFF;
+
+            ALTER TABLE stocktake_line
+            ADD COLUMN item_link_id TEXT NOT NULL REFERENCES item_link (id) DEFAULT 'temp_for_migration'; 
+
+            UPDATE stocktake_line SET item_link_id = item_id;
+
+            PRAGMA foreign_keys = ON;
+            "#,
+        )?;
+    }
+
+    sql! {
         connection,
         r#"
-        -- Adding stocktake_line.item_link_id
-        PRAGMA foreign_keys = OFF;
-
-        ALTER TABLE stocktake_line
-        ADD COLUMN item_link_id TEXT NOT NULL REFERENCES item_link (id) DEFAULT 'temp_for_migration'; 
-
-        UPDATE stocktake_line
-        SET item_link_id = item_id;
-
-        PRAGMA foreign_keys = ON;
-
         DROP INDEX IF EXISTS index_stocktake_line_item_id_fkey;
-        ALTER TABLE stocktake_line
-        DROP COLUMN item_id;
-        
+        ALTER TABLE stocktake_line DROP COLUMN item_id;
+
         CREATE INDEX "index_stocktake_line_item_link_id_fkey" ON "stocktake_line" ("item_link_id");
-        "#,
-    )?;
+        "#
+    }?;
 
     Ok(())
 }
