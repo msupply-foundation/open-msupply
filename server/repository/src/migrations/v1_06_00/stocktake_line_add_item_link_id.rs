146e08d7aea11607a43f1df8d694f86ed74875cb
roxy-dao <roxy@msupply.foundation>
2023-12-01 22:22:23 +1300

    Move create index sql statement
    Source: https://github.com/msupply-foundation/open-msupply/commit/5701311d742ff5d5eb36b3a0a4fba2df4f8f75d4

--- server/repository/src/migrations/v1_06_00/stocktake_line_add_item_link_id.rs
+++ server/repository/src/migrations/v1_06_00/stocktake_line_add_item_link_id.rs
@@ -28,6 +28,8 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
             UPDATE stocktake_line SET item_link_id = item_id;
 
             PRAGMA foreign_keys = ON;
+
+            CREATE INDEX "index_stocktake_line_item_link_id_fkey" ON "stocktake_line" ("item_link_id");
             "#,
         )?;
     }
@@ -37,8 +39,6 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
         r#"
         DROP INDEX IF EXISTS index_stocktake_line_item_id_fkey;
         ALTER TABLE stocktake_line DROP COLUMN item_id;
-
-        CREATE INDEX "index_stocktake_line_item_link_id_fkey" ON "stocktake_line" ("item_link_id");
         "#
     }?;
 
