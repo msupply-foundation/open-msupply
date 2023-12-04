76ae0f963b96b4b9551cd99000d433ec3905bd6b
roxy-dao <roxy@msupply.foundation>
2023-12-05 08:54:20 +1300

    Name link table
    Source: https://github.com/msupply-foundation/open-msupply/commit/1a40a334ce42fd0d0a4e3ee66bcc731427e655d0

---
+++ server/repository/src/db_diesel/name_link_row.rs
@@ -0,0 +1,63 @@
+use super::{name_link_row::name_link::dsl::*, name_row::name, StorageConnection};
+
+use crate::repository_error::RepositoryError;
+
+use diesel::prelude::*;
+
+table! {
+    name_link (id) {
+        id -> Text,
+        name_id -> Text,
+    }
+}
+joinable!(name_link -> name (name_id));
+
+#[derive(Queryable, Insertable, Clone, Debug, PartialEq, AsChangeset, Eq)]
+#[table_name = "name_link"]
+pub struct NameLinkRow {
+    pub id: String,
+    pub name_id: String,
+}
+
+pub struct NameLinkRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> NameLinkRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        NameLinkRepository { connection }
+    }
+
+    #[cfg(feature = "postgres")]
+    pub fn upsert_one(&self, row: &NameLinkRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(name_link)
+            .values(row)
+            .on_conflict(id)
+            .do_update()
+            .set(row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    #[cfg(not(feature = "postgres"))]
+    pub fn upsert_one(&self, row: &NameLinkRow) -> Result<(), RepositoryError> {
+        diesel::replace_into(name_link)
+            .values(row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    pub async fn insert_one(&self, row: &NameLinkRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(name_link)
+            .values(row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    pub async fn find_one_by_id(&self, item_link_id: &str) -> Result<NameLinkRow, RepositoryError> {
+        let result = name_link
+            .filter(name_link::id.eq(item_link_id))
+            .first::<NameLinkRow>(&self.connection.connection)?;
+        Ok(result)
+    }
+}
