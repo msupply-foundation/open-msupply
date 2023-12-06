1605f48e6e081aaf7efc3080a93b10cdef06b804
Mark Prins <mark@malcolmprins.net>
2023-12-07 10:18:13 +1300

    edit sensor and other messaging updates

--- client/packages/common/src/types/schema.ts
+++ client/packages/common/src/types/schema.ts
@@ -3754,6 +3754,7 @@ export type Queries = {
   syncSettings?: Maybe<SyncSettingsNode>;
   /** Query omSupply "temperature_breach" entries */
   temperatureBreaches: TemperatureBreachesResponse;
+  temperatureChart: TemperatureChartResponse;
   /** Query omSupply "temperature_log" entries */
   temperatureLogs: TemperatureLogsResponse;
 };
@@ -4153,6 +4154,15 @@ export type QueriesTemperatureBreachesArgs = {
 };
 
 
+export type QueriesTemperatureChartArgs = {
+  filter?: InputMaybe<TemperatureLogFilterInput>;
+  fromDatetime: Scalars['DateTime']['input'];
+  numberOfDataPoints: Scalars['Int']['input'];
+  storeId: Scalars['String']['input'];
+  toDatetime: Scalars['DateTime']['input'];
+};
+
+
 export type QueriesTemperatureLogsArgs = {
   filter?: InputMaybe<TemperatureLogFilterInput>;
   page?: InputMaybe<PaginationInput>;
@@ -4524,6 +4534,12 @@ export type ResponseStoreStatsNode = {
   requestedQuantity: Scalars['Int']['output'];
   stockOnHand: Scalars['Float']['output'];
   stockOnOrder: Scalars['Int']['output'];
+};
+
+export type SensorAxisNode = {
+  __typename: 'SensorAxisNode';
+  points: Array<TemperaturePointNode>;
+  sensor?: Maybe<SensorNode>;
 };
 
 export type SensorConnector = {
@@ -5038,6 +5054,13 @@ export type TemperatureBreachSortInput = {
 
 export type TemperatureBreachesResponse = TemperatureBreachConnector;
 
+export type TemperatureChartNode = {
+  __typename: 'TemperatureChartNode';
+  sensors: Array<SensorAxisNode>;
+};
+
+export type TemperatureChartResponse = TemperatureChartNode;
+
 export type TemperatureLogConnector = {
   __typename: 'TemperatureLogConnector';
   nodes: Array<TemperatureLogNode>;
@@ -5079,6 +5102,13 @@ export type TemperatureLogSortInput = {
 };
 
 export type TemperatureLogsResponse = TemperatureLogConnector;
+
+export type TemperaturePointNode = {
+  __typename: 'TemperaturePointNode';
+  breachIds?: Maybe<Array<Scalars['String']['output']>>;
+  midPoint: Scalars['DateTime']['output'];
+  temperature?: Maybe<Scalars['Float']['output']>;
+};
 
 export type TokenExpired = RefreshTokenErrorInterface & {
   __typename: 'TokenExpired';
