10d132742c5e83af4c900419efc23b73d9eef6bd
Andrei <andrei.evguenov@gmail.com>
2023-12-11 12:16:34 +1300

    Add sync buffer migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/c3a556ed47ff9505687c5ac53f3873ea6a59c40b

--- server/repository/src/migrations/v1_05_04/mod.rs
+++ server/repository/src/migrations/v1_05_04/mod.rs
@@ -10,14 +10,16 @@ impl Migration for V1_05_04 {
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        // Update integartion_datetime on facility/store type name records in sync_buffer
+        // when server start, or on next sync these will be re-integrated
         sql!(
             connection,
             r#"
                 ALTER TABLE name ADD COLUMN custom_data TEXT DEFAULT NULL;
+                UPDATE sync_buffer SET integration_datetime = NULL 
+                    WHERE record_id IN (SELECT id FROM name where name."type" IN ('FACILITY', 'STORE'));
             "#
         )?;
-
-        // Re-sync (so custom data is available)
 
         Ok(())
     }
