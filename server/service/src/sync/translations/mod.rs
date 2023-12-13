4dfdd9401162313fb826c2de0e3269bf396547dc
Chris Petty <chris@sussol.net>
2023-12-13 18:45:24 +1300

    Merging implemented properly and tests
    Source: https://github.com/msupply-foundation/open-msupply/commit/aafec6f5f6b8ffdae3242cccf13195de471c95f2

--- server/service/src/sync/translations/mod.rs
+++ server/service/src/sync/translations/mod.rs
@@ -87,6 +87,7 @@ pub(crate) fn all_translators() -> SyncTranslators {
         Box::new(document::DocumentTranslation {}),
         // Special translations
         Box::new(special::NameToNameStoreJoinTranslation {}),
+        Box::new(special::ItemMergeTranslation {}),
     ]
 }
 
@@ -316,6 +317,14 @@ pub(crate) trait SyncTranslation {
     }
 
     fn try_translate_pull_delete(
+        &self,
+        _: &StorageConnection,
+        _: &SyncBufferRow,
+    ) -> Result<Option<IntegrationRecords>, anyhow::Error> {
+        Ok(None)
+    }
+
+    fn try_translate_pull_merge(
         &self,
         _: &StorageConnection,
         _: &SyncBufferRow,
