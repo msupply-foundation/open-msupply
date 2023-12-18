707bb0a1357e742cc451d1cc97ca5e3adf693609
Clemens Zeidler <clemens@msupply.foundation>
2023-12-19 08:23:43 +1300

    Move encounter deleted filtering to the backend
    Source: https://github.com/msupply-foundation/open-msupply/commit/5514e8e692eb7de4bc4d82786557566cd081267f

--- client/packages/programs/src/api/hooks/document/useEncounters.ts
+++ client/packages/programs/src/api/hooks/document/useEncounters.ts
@@ -1,23 +1,8 @@
-import {
-  EncounterFilterInput,
-  EncounterNodeStatus,
-  useQuery,
-} from '@openmsupply-client/common';
+import { useQuery } from '@openmsupply-client/common';
 import { EncounterListParams, useEncounterApi } from '../utils/useEncounterApi';
 
 export const useEncounters = (params: EncounterListParams) => {
   const api = useEncounterApi();
 
-  const filterBy: EncounterFilterInput = {
-    ...params.filterBy,
-    status: {
-      ...params.filterBy?.status,
-      notEqualTo: EncounterNodeStatus.Deleted,
-    },
-  };
-  const p = {
-    ...params,
-    filterBy,
-  };
-  return useQuery(api.keys.paramList(p), () => api.list(p));
+  return useQuery(api.keys.paramList(params), () => api.list(params));
 };
