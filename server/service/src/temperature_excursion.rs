72f158bf077ad6ee681782a8ab4244eb9f1d4777
Mark Prins <mark@malcolmprins.net>
2023-12-22 10:28:00 +1300

    temperature excursions
    Source: https://github.com/msupply-foundation/open-msupply/commit/6c56712c5525caa3ae0123800ae5d27a8ee76e02

---
+++ server/service/src/temperature_excursion.rs
@@ -0,0 +1,21 @@
+use chrono::{Days, Utc};
+use repository::{
+    DatetimeFilter, EqualFilter, RepositoryError, StorageConnection,
+    TemperatureExcursionRepository, TemperatureExcursionRow, TemperatureLogFilter,
+};
+
+pub fn get_excursions(
+    connection: &StorageConnection,
+    store_id: &str,
+) -> Result<Vec<TemperatureExcursionRow>, RepositoryError> {
+    let filter = TemperatureLogFilter::new()
+        .store_id(EqualFilter::equal_to(store_id))
+        .datetime(DatetimeFilter::after_or_equal_to(
+            Utc::now()
+                .naive_utc()
+                .checked_sub_days(Days::new(7))
+                .unwrap(),
+        ));
+
+    TemperatureExcursionRepository::new(&connection).query(filter)
+}
