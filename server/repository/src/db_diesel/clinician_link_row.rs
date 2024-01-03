a72ac9c43adf7b86f0c86c39a19c21677d70f0de
roxy-dao <roxy@msupply.foundation>
2024-01-04 03:01:51 +1300

    Allow in the same query and change clinician_id to clinician_link_id for invoice
    Source: https://github.com/msupply-foundation/open-msupply/commit/e1d9f4f1e9f3093b4d2b0f1aed06afecc493b62a

--- server/repository/src/db_diesel/clinician_link_row.rs
+++ server/repository/src/db_diesel/clinician_link_row.rs
@@ -1,4 +1,7 @@
-use super::{clinician_row::clinician, StorageConnection};
+use super::{
+    clinician_row::clinician, invoice_line_row::invoice_line, invoice_row::invoice, name_row::name,
+    store_row::store, StorageConnection,
+};
 use crate::repository_error::RepositoryError;
 
 use self::clinician_link::dsl as clinician_link_dsl;
@@ -12,6 +15,10 @@ table! {
 }
 
 joinable!(clinician_link -> clinician (clinician_id));
+allow_tables_to_appear_in_same_query!(clinician_link, name);
+allow_tables_to_appear_in_same_query!(clinician_link, store);
+allow_tables_to_appear_in_same_query!(clinician_link, invoice);
+allow_tables_to_appear_in_same_query!(clinician_link, invoice_line);
 
 #[derive(Clone, Insertable, Queryable, Debug, PartialEq, AsChangeset, Eq)]
 #[table_name = "clinician_link"]
