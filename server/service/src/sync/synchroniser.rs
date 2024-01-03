adc05fc3d4275146b71617ac07ecf6c68e0d215c
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-01-03 14:54:31 +0900

    Add testing timer
    Source: https://github.com/msupply-foundation/open-msupply/commit/609738b1747e221e7ea8f710a426beb1d8a535a3

--- server/service/src/sync/synchroniser.rs
+++ server/service/src/sync/synchroniser.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use log::warn;
 use repository::{RepositoryError, StorageConnection, SyncBufferAction};
-use std::{convert::TryInto, sync::Arc};
+use std::{convert::TryInto, sync::Arc, thread, time};
 use thiserror::Error;
 use util::format_error;
 
@@ -266,6 +266,9 @@ pub async fn integrate_and_translate_sync_buffer<'a>(
             })?;
 
         let current_progress = 0;
+
+        let delay = time::Duration::from_millis(50);
+        thread::sleep(delay);
 
         let upsert_integration_result = translation_and_integration
             .translate_and_integrate_sync_records(
