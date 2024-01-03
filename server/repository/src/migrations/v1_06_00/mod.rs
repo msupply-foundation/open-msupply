f366f80627a2be6246fa7ba0b00205c068dcd7ea
roxy-dao <roxy@msupply.foundation>
2024-01-04 03:01:10 +1300

    Add clinician_link_id to invoice migration

--- server/repository/src/migrations/v1_06_00/mod.rs
+++ server/repository/src/migrations/v1_06_00/mod.rs
@@ -8,6 +8,7 @@ mod contact_trace;
 mod encounter_add_clinician_link_id;
 mod encounter_status;
 mod indexes;
+mod invoice_add_clinician_link_id;
 mod invoice_add_name_link_id;
 mod invoice_line_add_item_link_id;
 mod item_add_is_active;
@@ -60,6 +61,7 @@ impl Migration for V1_06_00 {
         clinician_link::migrate(connection)?;
         clinician_store_join_add_clinician_link_id::migrate(connection)?;
         encounter_add_clinician_link_id::migrate(connection)?;
+        invoice_add_clinician_link_id::migrate(connection)?;
 
         program_enrolment_status::migrate(connection)?;
         indexes::migrate(connection)?;
