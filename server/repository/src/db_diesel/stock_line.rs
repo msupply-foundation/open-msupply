9c569e4f1dfb5c9b26d5ed0d3b0b6127f025663a
roxy-dao <roxy@msupply.foundation>
2024-01-04 03:52:38 +1300

    Add location code sort field to stock line
    Source: https://github.com/msupply-foundation/open-msupply/commit/1a2c98aed3f822c13f3553095ca04e7104e0d4a3

--- server/repository/src/db_diesel/stock_line.rs
+++ server/repository/src/db_diesel/stock_line.rs
@@ -40,6 +40,7 @@ pub enum StockLineSortField {
     Batch,
     PackSize,
     SupplierName,
+    LocationCode,
 }
 
 #[derive(Debug, Clone)]
@@ -135,6 +136,9 @@ impl<'a> StockLineRepository<'a> {
                 }
                 StockLineSortField::SupplierName => {
                     apply_sort_no_case!(query, sort, name_dsl::name_);
+                }
+                StockLineSortField::LocationCode => {
+                    apply_sort_no_case!(query, sort, location_dsl::code);
                 }
             }
         } else {
