a0a37b4ae71710d45a11c7511a34f786d40a9d85
Mark Prins <mark@malcolmprins.net>
2024-01-10 15:03:30 +1300

    refetch notifications
    Source: https://github.com/msupply-foundation/open-msupply/commit/5594735cec64b7e569be50c2f97e89aabe99a42f

--- client/packages/coldchain/src/Monitoring/api/TemperatureBreach/hooks/document/useUpdateTemperatureBreach.ts
+++ client/packages/coldchain/src/Monitoring/api/TemperatureBreach/hooks/document/useUpdateTemperatureBreach.ts
@@ -1,11 +1,16 @@
+import { useTemperatureNotificationApi } from '../../../TemperatureNotification/hooks/utils/useTemperatureNotificationApi';
 import { useTemperatureBreachApi } from '../utils/useTemperatureBreachesApi';
 import { useMutation, useQueryClient } from '@openmsupply-client/common';
 
 export const useUpdateTemperatureBreach = () => {
   const queryClient = useQueryClient();
   const api = useTemperatureBreachApi();
+  const notificationApi = useTemperatureNotificationApi();
 
   return useMutation(api.update, {
-    onSuccess: () => queryClient.invalidateQueries(api.keys.base()),
+    onSuccess: () => {
+      queryClient.invalidateQueries(api.keys.base());
+      queryClient.invalidateQueries(notificationApi.keys.base());
+    },
   });
 };
