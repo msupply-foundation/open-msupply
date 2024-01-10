81aee74ca43855aab4dfa16405c2e728ca6c7c43
Mark Prins <mark@malcolmprins.net>
2024-01-11 09:18:41 +1300

    change loader colour

--- client/packages/system/src/ContactTrace/DetailView/Footer.tsx
+++ client/packages/system/src/ContactTrace/DetailView/Footer.tsx
@@ -74,6 +74,7 @@ export const Footer: FC<FooterProps> = ({
             />
             <LoadingButton
               color="secondary"
+              loadingStyle={{ iconColor: 'secondary.main' }}
               variant="outlined"
               disabled={isDisabled || isSaving}
               isLoading={isSaving}
