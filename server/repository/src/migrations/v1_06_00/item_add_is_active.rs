a9dd1803f0dc8ea0be1d2d8f9913acfa34aba6d4
Chris Petty <chris@sussol.net>
2024-01-11 17:32:11 +1300

    Update item_add_is_active.rs
    Source: https://github.com/msupply-foundation/open-msupply/commit/84849b8f79ad61427cc3979de04e1952a6800b51

--- server/repository/src/migrations/v1_06_00/item_add_is_active.rs
+++ server/repository/src/migrations/v1_06_00/item_add_is_active.rs
@@ -4,7 +4,8 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
         r#"
-        ALTER TABLE item ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT TRUE;
+            ALTER TABLE item ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT TRUE;
+            CREATE INDEX "index_item_is_active" ON "item" ("is_active");
         "#,
     )?;
 
