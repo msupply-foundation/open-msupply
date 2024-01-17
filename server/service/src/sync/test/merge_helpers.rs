08f5a17470ecfc7b3fa3a564cd3fc8194532ef66
roxy-dao <roxy@msupply.foundation>
2024-01-18 04:01:55 +1300

    Helper to merge all names into name_a
    Source: https://github.com/msupply-foundation/open-msupply/commit/cca70182cd3da9419cf6ee366665b426caaf1876

---
+++ server/service/src/sync/test/merge_helpers.rs
@@ -0,0 +1,26 @@
+use repository::{
+    mock::{mock_name_link_from_name, MockDataCollection},
+    NameLinkRowRepository, NameRow, RepositoryError, StorageConnection,
+};
+
+pub fn name_links_merged(
+    connection: &StorageConnection,
+    mock_data: &MockDataCollection,
+) -> Result<(), RepositoryError> {
+    let name_link_repo = NameLinkRowRepository::new(&connection);
+
+    let names: Vec<NameRow> = mock_data
+        .data
+        .iter()
+        .map(|(_, mock)| mock.names.clone())
+        .flatten()
+        .collect();
+
+    for name in names {
+        let mut name_link = mock_name_link_from_name(&name);
+        name_link.name_id = "name_a".to_string();
+        name_link_repo.upsert_one(&name_link)?;
+    }
+
+    Ok(())
+}
