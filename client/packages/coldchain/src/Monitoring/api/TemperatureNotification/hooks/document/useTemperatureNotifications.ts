df727fb9f281d370f5f55a50b4b6f6639496364d
Mark Prins <mark@malcolmprins.net>
2024-01-19 14:19:55 +1300

    refetch notifications

--- client/packages/coldchain/src/Monitoring/api/TemperatureNotification/hooks/document/useTemperatureNotifications.ts
+++ client/packages/coldchain/src/Monitoring/api/TemperatureNotification/hooks/document/useTemperatureNotifications.ts
@@ -6,14 +6,24 @@ import {
 } from '@openmsupply-client/common';
 import { ListParams } from '../../api';
 
+const POLLING_INTERVAL_IN_MINUTES = 5;
+
 export const useTemperatureNotifications = (queryParams: ListParams) => {
   const api = useTemperatureNotificationApi();
   const { warning } = useNotification();
   const t = useTranslation('coldchain');
 
-  return useQuery(api.keys.paramList(queryParams), () =>
-    api.get
-      .list(queryParams)()
-      .catch(e => warning(`${t('error.fetch-notifications')}: ${e.message}`)())
+  return useQuery(
+    api.keys.paramList(queryParams),
+    () =>
+      api.get
+        .list(queryParams)()
+        .catch(e =>
+          warning(`${t('error.fetch-notifications')}: ${e.message}`)()
+        ),
+    {
+      cacheTime: 0,
+      refetchInterval: 1000 * 60 * POLLING_INTERVAL_IN_MINUTES,
+    }
   );
 };
