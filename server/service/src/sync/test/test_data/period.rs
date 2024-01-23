75788737fce87a13b3d741baca4e931dbd2e1472
Andrei <andrei.evguenov@gmail.com>
2024-01-24 00:21:12 +1300

    Fix sync test data
    Source: https://github.com/msupply-foundation/open-msupply/commit/9e6c77ccb9163a5757c27bca9a108316a88f2c8f

--- server/service/src/sync/test/test_data/period.rs
+++ server/service/src/sync/test/test_data/period.rs
@@ -1,10 +1,9 @@
 use chrono::NaiveDate;
 use repository::PeriodRow;
 
-use crate::sync::{
-    test::TestSyncPullRecord,
-    translations::{LegacyTableName, PullUpsertRecord},
-};
+use crate::sync::test::TestSyncPullRecord;
+
+const TABLE_NAME: &'static str = "period";
 
 const PERIOD_1: (&'static str, &'static str) = (
     "period_1",
@@ -55,48 +54,48 @@ const PERIOD_4: (&'static str, &'static str) = (
 pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncPullRecord> {
     vec![
         TestSyncPullRecord::new_pull_upsert(
-            LegacyTableName::PERIOD,
+            TABLE_NAME,
             PERIOD_1,
-            PullUpsertRecord::Period(PeriodRow {
+            PeriodRow {
                 id: "period_1".to_string(),
                 period_schedule_id: "period_schedule_1".to_string(),
                 name: "Jan Wk01 2023".to_string(),
                 start_date: NaiveDate::from_ymd_opt(2023, 01, 01).unwrap(),
                 end_date: NaiveDate::from_ymd_opt(2023, 01, 07).unwrap(),
-            }),
+            },
         ),
         TestSyncPullRecord::new_pull_upsert(
-            LegacyTableName::PERIOD,
+            TABLE_NAME,
             PERIOD_2,
-            PullUpsertRecord::Period(PeriodRow {
+            PeriodRow {
                 id: "period_2".to_string(),
                 period_schedule_id: "period_schedule_2".to_string(),
                 name: "2023".to_string(),
                 start_date: NaiveDate::from_ymd_opt(2023, 01, 01).unwrap(),
                 end_date: NaiveDate::from_ymd_opt(2023, 12, 31).unwrap(),
-            }),
+            },
         ),
         TestSyncPullRecord::new_pull_upsert(
-            LegacyTableName::PERIOD,
+            TABLE_NAME,
             PERIOD_3,
-            PullUpsertRecord::Period(PeriodRow {
+            PeriodRow {
                 id: "641A3560C84A44BC9E6DDC01F3D75923".to_string(),
                 period_schedule_id: "597074CBCCC24166B8C1F82553DACC2F".to_string(),
                 name: "2020_Q2".to_string(),
                 start_date: NaiveDate::from_ymd_opt(2020, 04, 01).unwrap(),
                 end_date: NaiveDate::from_ymd_opt(2020, 06, 30).unwrap(),
-            }),
+            },
         ),
         TestSyncPullRecord::new_pull_upsert(
-            LegacyTableName::PERIOD,
+            TABLE_NAME,
             PERIOD_4,
-            PullUpsertRecord::Period(PeriodRow {
+            PeriodRow {
                 id: "772B3984DBA14A5F941ED0EF857FDB31".to_string(),
                 period_schedule_id: "597074CBCCC24166B8C1F82553DACC2F".to_string(),
                 name: "2020_Q3".to_string(),
                 start_date: NaiveDate::from_ymd_opt(2020, 07, 01).unwrap(),
                 end_date: NaiveDate::from_ymd_opt(2020, 09, 30).unwrap(),
-            }),
+            },
         ),
     ]
 }
--- server/service/src/sync/test/test_data/stocktake_line.rs
+++ server/service/src/sync/test/test_data/stocktake_line.rs
@@ -1,10 +1,9 @@
 use super::TestSyncPushRecord;
-use crate::sync::{
-    test::TestSyncPullRecord,
-    translations::{stocktake_line::LegacyStocktakeLineRow, LegacyTableName, PullUpsertRecord},
-};
+use crate::sync::{test::TestSyncPullRecord, translations::stocktake_line::LegacyStocktakeLineRow};
 use repository::StocktakeLineRow;
 use serde_json::json;
+
+const TABLE_NAME: &'static str = "Stock_take_lines";
 
 const STOCKTAKE_LINE_1: (&'static str, &'static str) = (
     "0a3de900f0d211eb8dddb54df6d741bc",
@@ -33,9 +32,9 @@ const STOCKTAKE_LINE_1: (&'static str, &'static str) = (
 );
 fn stocktake_line_pull_record() -> TestSyncPullRecord {
     TestSyncPullRecord::new_pull_upsert(
-        LegacyTableName::STOCKTAKE_LINE,
+        TABLE_NAME,
         STOCKTAKE_LINE_1,
-        PullUpsertRecord::StocktakeLine(StocktakeLineRow {
+        StocktakeLineRow {
             id: STOCKTAKE_LINE_1.0.to_string(),
             stocktake_id: "stocktake_a".to_string(),
             stock_line_id: Some("item_c_line_a".to_string()),
@@ -51,12 +50,12 @@ fn stocktake_line_pull_record() -> TestSyncPullRecord {
             sell_price_per_pack: Some(15.0),
             note: None,
             inventory_adjustment_reason_id: None,
-        }),
+        },
     )
 }
 fn stocktake_line_push_record() -> TestSyncPushRecord {
     TestSyncPushRecord {
-        table_name: LegacyTableName::STOCKTAKE_LINE.to_string(),
+        table_name: TABLE_NAME.to_string(),
         record_id: STOCKTAKE_LINE_1.0.to_string(),
         push_data: json!(LegacyStocktakeLineRow {
             ID: STOCKTAKE_LINE_1.0.to_string(),
@@ -107,9 +106,9 @@ const STOCKTAKE_LINE_OM_FIELDS: (&'static str, &'static str) = (
 );
 fn stocktake_line_om_field_pull_record() -> TestSyncPullRecord {
     TestSyncPullRecord::new_pull_upsert(
-        LegacyTableName::STOCKTAKE_LINE,
+        TABLE_NAME,
         STOCKTAKE_LINE_OM_FIELDS,
-        PullUpsertRecord::StocktakeLine(StocktakeLineRow {
+        StocktakeLineRow {
             id: STOCKTAKE_LINE_OM_FIELDS.0.to_string(),
             stocktake_id: "stocktake_a".to_string(),
             stock_line_id: Some("item_c_line_a".to_string()),
@@ -125,12 +124,12 @@ fn stocktake_line_om_field_pull_record() -> TestSyncPullRecord {
             sell_price_per_pack: Some(15.0),
             note: Some("om note".to_string()),
             inventory_adjustment_reason_id: None,
-        }),
+        },
     )
 }
 fn stocktake_line_om_field_push_record() -> TestSyncPushRecord {
     TestSyncPushRecord {
-        table_name: LegacyTableName::STOCKTAKE_LINE.to_string(),
+        table_name: TABLE_NAME.to_string(),
         record_id: STOCKTAKE_LINE_OM_FIELDS.0.to_string(),
         push_data: json!(LegacyStocktakeLineRow {
             ID: STOCKTAKE_LINE_OM_FIELDS.0.to_string(),
