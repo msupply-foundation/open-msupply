d870c8d7f65c6cb23afbbfc451742692e3c85037
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-02-01 12:02:01 +1300

    Refactor numeric input to use standard text input
    Source: https://github.com/msupply-foundation/open-msupply/commit/d71b9e9ae7c843fa174373f40fe991e38f6b46e3

--- client/packages/common/src/intl/number/IntlNumber.ts
+++ client/packages/common/src/intl/number/IntlNumber.ts
@@ -1,16 +1,41 @@
+import { useCurrency } from '../currency';
 import { useIntlUtils } from '../utils';
 
 export const useFormatNumber = () => {
   const { currentLanguage } = useIntlUtils();
+  const {
+    options: { decimal },
+  } = useCurrency();
 
   return {
-    format: (value: number, options?: Intl.NumberFormatOptions) =>
-      new Intl.NumberFormat(currentLanguage, options).format(value),
+    format: (
+      value: number,
+      options?: Intl.NumberFormatOptions & { locale?: string }
+    ) => {
+      const locale = options?.locale ?? currentLanguage;
+      return new Intl.NumberFormat(locale, options).format(value);
+    },
     round: (value?: number, dp?: number): string => {
       const intl = new Intl.NumberFormat(currentLanguage, {
         maximumFractionDigits: dp ?? 0,
       });
       return intl.format(value ?? 0);
+    },
+    parse: (numberString: string, decimalChar: string = decimal) => {
+      const negative = numberString.startsWith('-') ? -1 : 1;
+      const separatorRegex = new RegExp(
+        `[${decimalChar === '.' ? '\\.' : decimalChar}](\\d+)$`
+      );
+
+      const num = numberString
+        // Convert separator to standard decimal point
+        .replace(separatorRegex, '.$1')
+        // Remove all other characters
+        .replace(/[^\d\.]/g, '');
+
+      if (num === '') return NaN;
+
+      return Number(num) * negative;
     },
   };
 };
--- client/packages/common/src/ui/components/inputs/TextInput/NumericTextInput.tsx
+++ client/packages/common/src/ui/components/inputs/TextInput/NumericTextInput.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
-import { StandardTextFieldProps } from '@mui/material';
+import React, { FC, useState } from 'react';
+import { StandardTextFieldProps } from '@common/components';
 import { BasicTextInput } from './BasicTextInput';
 import { NumUtils } from '@common/utils';
+import { useFormatNumber, useCurrency } from '@common/intl';
 export interface NumericTextInputProps
   extends Omit<StandardTextFieldProps, 'onChange'> {
   onChange?: (value: number | undefined) => void;
@@ -11,6 +12,9 @@ export interface NumericTextInputProps
   min?: number;
   max?: number;
   precision?: number;
+  step?: number;
+  multiplier?: number;
+  value: number | undefined;
 }
 
 export const NumericTextInput: FC<NumericTextInputProps> = React.forwardRef(
@@ -19,16 +23,31 @@ export const NumericTextInput: FC<NumericTextInputProps> = React.forwardRef(
       sx,
       InputProps,
       width = 75,
-      onChange,
+      onChange = () => {},
       defaultValue,
       allowNegative,
       min = allowNegative ? -NumUtils.MAX_SAFE_API_INTEGER : 0,
       max = NumUtils.MAX_SAFE_API_INTEGER,
       precision = 0,
+      step = 1,
+      multiplier = 10,
+      value,
       ...props
     },
     ref
   ) => {
+    const { format, parse } = useFormatNumber();
+    const {
+      options: { separator, decimal },
+    } = useCurrency();
+    const [textValue, setTextValue] = useState<string>(
+      format((value as number) ?? defaultValue ?? '')
+    );
+
+    const inputRegex = new RegExp(
+      `^-?\\d*${decimal === '.' ? '\\.' : decimal}?\\d*$`
+    );
+
     return (
       <BasicTextInput
         ref={ref}
@@ -38,23 +57,57 @@ export const NumericTextInput: FC<NumericTextInputProps> = React.forwardRef(
         }}
         InputProps={InputProps}
         onChange={e => {
-          if (
-            (e.target.value === '' || e.target.value === undefined) &&
-            !!onChange
-          ) {
-            onChange(defaultValue);
+          const input = e.target.value
+            // Remove separators
+            .replace(separator, '')
+            // Remove negative if not allowed
+            .replace(min <= 0 ? '-' : '', '');
+
+          if (input === '') {
+            setTextValue('');
+            onChange(undefined);
             return;
           }
-          const parsed = Number(e.target.value);
-          if (!Number.isNaN(parsed) && !!onChange)
-            onChange(
-              NumUtils.constrain(NumUtils.round(parsed, precision), min, max)
-            );
+
+          // Prevent illegal characters from being entered
+          if (inputRegex.test(input)) setTextValue(input);
+          else return;
+
+          if (input.endsWith(decimal)) return;
+
+          const parsed = parse(input);
+
+          if (Number.isNaN(parsed)) return;
+
+          const constrained = constrain(parsed, precision, min, max);
+
+          setTextValue(format(constrained));
+          onChange(constrained);
+        }}
+        onKeyDown={e => {
+          if (e.key !== 'ArrowUp' && e.key !== 'ArrowDown') return;
+
+          e.preventDefault();
+          const change =
+            (e.key === 'ArrowUp' ? step : -step) *
+            (e.shiftKey ? multiplier : 1);
+
+          const newNum = constrain(
+            (value ?? Math.max(min, 0)) + change,
+            precision,
+            min,
+            max
+          );
+          setTextValue(format(newNum));
+          onChange(newNum);
         }}
         onFocus={e => e.target.select()}
-        type="number"
         {...props}
+        value={textValue}
       />
     );
   }
 );
+
+const constrain = (value: number, decimals: number, min: number, max: number) =>
+  NumUtils.constrain(NumUtils.round(value, decimals), min, max);
