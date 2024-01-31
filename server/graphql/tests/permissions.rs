a3c9fbf589eb10f1adc85d0a0ed1ab23f3635f3d
Andrei <andrei.evguenov@gmail.com>
2024-01-31 14:18:44 +1300

    Add isCentralServer query and restrict central server mutations
    Source: https://github.com/msupply-foundation/open-msupply/commit/da18221085ad14e00781b364f6b550ca0f0a7ffb

--- server/graphql/tests/permissions.rs
+++ server/graphql/tests/permissions.rs
@@ -16,7 +16,7 @@ mod permission_tests {
     use graphql_invoice::{InvoiceMutations, InvoiceQueries};
     use graphql_invoice_line::InvoiceLineMutations;
     use graphql_location::{LocationMutations, LocationQueries};
-    use graphql_pack_variant::{PackVariantMutations, PackVariantQueries};
+    use graphql_pack_variant::PackVariantQueries;
     use graphql_reports::ReportQueries;
     use graphql_requisition::{RequisitionMutations, RequisitionQueries};
     use graphql_requisition_line::RequisitionLineMutations;
@@ -57,7 +57,6 @@ mod permission_tests {
         pub RequisitionMutations,
         pub RequisitionLineMutations,
         pub GeneralMutations,
-        pub PackVariantMutations,
     );
 
     pub fn full_query() -> FullQuery {
@@ -89,7 +88,6 @@ mod permission_tests {
             RequisitionMutations,
             RequisitionLineMutations,
             GeneralMutations,
-            PackVariantMutations,
         )
     }
 
