6cccea0a86e361fbdff00b05fc98fd5d724140a6
Andrei <andrei.evguenov@gmail.com>
2024-02-02 15:26:22 +1300

    Add explanation about NameToNameStoreJoinTranslator dependencies
    Source: https://github.com/msupply-foundation/open-msupply/commit/2469f09b424be834042bd7999f12e0caa898e63a

--- server/service/src/sync/translations/special/name_to_name_store_join.rs
+++ server/service/src/sync/translations/special/name_to_name_store_join.rs
@@ -28,11 +28,14 @@ pub(crate) fn boxed() -> Box<dyn SyncTranslation> {
 // NOTE Translator should be removed when central server configures these properties on name_store_join
 pub(super) struct NameToNameStoreJoinTranslation;
 impl SyncTranslation for NameToNameStoreJoinTranslation {
-    // TODO would this even work ? (would it not have a dependnecy to ?)
     fn table_name(&self) -> &'static str {
         NameTranslation.table_name()
     }
 
+    // Even though we are making changes to name_store_joins, no dependencies required
+    // this translator is special translator to update existing names_store_joins when
+    // name.is_customer or name.is_supplier are toggled
+    // Btw, when name_store_joins are upserted, we check name for is_customer,is_supplier
     fn pull_dependencies(&self) -> Vec<&'static str> {
         vec![]
     }
