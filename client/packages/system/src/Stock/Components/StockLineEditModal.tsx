d78ba6efe0938625a7570a7429d85fb150165b57
Lache Melvin <lache.melvin@gmail.com>
2024-02-08 09:08:27 +1300

    revert use of OkKeyBindings in StockLineEditModal

--- client/packages/system/src/Stock/Components/StockLineEditModal.tsx
+++ client/packages/system/src/Stock/Components/StockLineEditModal.tsx
@@ -11,7 +11,6 @@ import {
   usePluginEvents,
   usePluginElements,
   PluginEventListener,
-  OkKeyBindings,
 } from '@openmsupply-client/common';
 import { StockLineRowFragment, useStock } from '../api';
 import { ActivityLogList } from '../../ActivityLog';
@@ -100,28 +99,27 @@ export const StockLineEditModal: FC<StockLineEditModalProps> = ({
     return () => removeEventListener(listener);
   }, [addEventListener, removeEventListener]);
 
-  const onOk = () =>
-    getConfirmation({
-      onConfirm: async () => {
-        await onSave();
-        dispatchEvent('onSaveStockEditForm', new Event(draft.id));
-        onClose();
-      },
-      onCancel: () => onClose(),
-    });
-
-  const okDisabled = ObjUtils.isEqual(draft, stockLine) && !hasChanged;
-
   return (
     <>
-      <OkKeyBindings onOk={onOk} okDisabled={okDisabled} />
       <Modal
         width={700}
         height={575}
         slideAnimation={false}
         title={t('title.stock-line-details')}
         okButton={
-          <DialogButton variant="ok" disabled={okDisabled} onClick={onOk} />
+          <DialogButton
+            variant="ok"
+            disabled={ObjUtils.isEqual(draft, stockLine) && !hasChanged}
+            onClick={() =>
+              getConfirmation({
+                onConfirm: async () => {
+                  await onSave();
+                  dispatchEvent('onSaveStockEditForm', new Event(draft.id));
+                  onClose();
+                },
+              })
+            }
+          />
         }
         cancelButton={<DialogButton variant="cancel" onClick={onClose} />}
       >
