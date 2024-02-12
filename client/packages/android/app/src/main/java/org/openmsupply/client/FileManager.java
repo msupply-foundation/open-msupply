dbb7cd8eb6d84648c51e0c1dc8fdc38716695259
James Brunskill <james@sussol.net>
2024-02-12 18:21:50 +1300

    Use native download in Android
    Source: https://github.com/msupply-foundation/open-msupply/commit/d9251f8bee00237a9109b800b8a3e029fe7d4656

--- client/packages/android/app/src/main/java/org/openmsupply/client/FileManager.java
+++ client/packages/android/app/src/main/java/org/openmsupply/client/FileManager.java
@@ -7,16 +7,31 @@ import android.net.Uri;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class FileManager {
     private static final int SAVE_FILE_REQUEST = 12321;
+    private static final int SAVE_DATABASE_REQUEST = 12322;
     private Activity activity;
     private String content;
+    private File dbFile;
 
     public FileManager(Activity activity) {
         this.activity = activity;
+    }
+
+    public void SaveDatabase(File file) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/x-sqlite3");
+        intent.putExtra(Intent.EXTRA_TITLE, "openmsupply.sqlite");
+        activity.startActivityForResult(intent, SAVE_DATABASE_REQUEST);
+
     }
 
     public void Save(String filename, String content) {
@@ -45,6 +60,43 @@ public class FileManager {
                 bw.close();
             } catch (Exception e) {
                 Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        if (requestCode == SAVE_DATABASE_REQUEST && resultCode == Activity.RESULT_OK && data != null
+                && dbFile != null) {
+            Uri uri = data.getData();
+            Context context = activity.getApplicationContext();
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                inputStream = new FileInputStream(dbFile);
+                outputStream = context.getContentResolver().openOutputStream(uri);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception e) {
+                Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
