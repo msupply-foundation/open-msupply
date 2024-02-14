5858b515e9bf194490aaadf88c4e28529ec286d4
Lache Melvin <lache.melvin@gmail.com>
2024-02-14 15:37:42 +1300

    selectedIds -> stockLineIds

--- client/packages/invoices/src/Returns/api/hooks/line/useNewSupplierReturnLines.ts
+++ client/packages/invoices/src/Returns/api/hooks/line/useNewSupplierReturnLines.ts
@@ -5,20 +5,20 @@ import {
 } from '@openmsupply-client/common';
 import { useReturnsApi } from '../utils/useReturnsApi';
 
-export const useNewSupplierReturnLines = (selectedIds: string[]) => {
+export const useNewSupplierReturnLines = (stockLineIds: string[]) => {
   const t = useTranslation('replenishment');
   const { info, error } = useNotification();
 
   const api = useReturnsApi();
 
   const { refetch } = useQuery(
-    api.keys.newReturns(selectedIds),
-    () => api.get.newSupplierReturnLines(selectedIds),
+    api.keys.newReturns(stockLineIds),
+    () => api.get.newSupplierReturnLines(stockLineIds),
     { enabled: false } // disable autofetch
   );
 
   return async () => {
-    if (!selectedIds.length) {
+    if (!stockLineIds.length) {
       const selectLinesSnack = info(t('messages.select-rows-to-return'));
       selectLinesSnack();
     } else {
