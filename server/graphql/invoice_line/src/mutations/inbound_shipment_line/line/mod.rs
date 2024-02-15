9293a76445bc37a4b73784e2e43e13c51793ec67
roxy-dao <roxy@msupply.foundation>
2024-02-16 09:20:26 +1300

    GraphQL endpoint to zero inbound line quantity
    Source: https://github.com/msupply-foundation/open-msupply/commit/a7c91edfee86b34040cdb3e283ab7f89ba897f81

--- server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/mod.rs
+++ server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/mod.rs
@@ -3,11 +3,20 @@ use async_graphql::*;
 pub mod delete;
 pub mod insert;
 pub mod update;
+pub mod zero_line_quantities;
 
 pub struct BatchIsReserved;
 #[Object]
 impl BatchIsReserved {
     pub async fn description(&self) -> &'static str {
         "Batch is already reserved/issued"
+    }
+}
+
+pub struct InvoiceWasCreatedAfterStore;
+#[Object]
+impl InvoiceWasCreatedAfterStore {
+    pub async fn description(&self) -> &'static str {
+        "Invoice was created after store"
     }
 }
