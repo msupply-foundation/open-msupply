4a3fce3d77517d600648461306102ceeaa20afd4
roxy-dao <roxy@msupply.foundation>
2024-02-15 15:41:23 +1300

    Migration to add new activity log type for 'deleting' invoices created before store
    Source: https://github.com/msupply-foundation/open-msupply/commit/e4215e58fc327fac3c8fd68adee2cea125acf86c

---
+++ server/repository/src/migrations/v1_07_00/activity_log_add_clear_invoice_created_before_store.rs
@@ -0,0 +1,20 @@
+use crate::StorageConnection;
+
+#[cfg(feature = "postgres")]
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    use crate::migrations::sql;
+
+    sql!(
+        connection,
+        r#"ALTER TYPE activity_log_type ADD VALUE 'CLEAR_INVOICE_CREATED_BEFORE_STORE';
+
+        "#
+    )?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "postgres"))]
+pub(crate) fn migrate(_connection: &StorageConnection) -> anyhow::Result<()> {
+    Ok(())
+}
