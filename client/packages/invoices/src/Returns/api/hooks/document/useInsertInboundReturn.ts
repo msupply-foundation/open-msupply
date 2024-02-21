a8afa1f50c8989faeb45b47af7200c09473ae759
Lache Melvin <lache.melvin@gmail.com>
2024-02-21 13:00:47 +1300

    add new inbound return from list view
    Source: https://github.com/msupply-foundation/open-msupply/commit/d124df44797d82b176fbc6f9383b097a9fa1ba06

--- client/packages/invoices/src/Returns/api/hooks/document/useInsertInboundReturn.ts
+++ client/packages/invoices/src/Returns/api/hooks/document/useInsertInboundReturn.ts
@@ -1,19 +1,23 @@
-import { useQueryClient, useMutation } from '@openmsupply-client/common';
+import {
+  useQueryClient,
+  useMutation,
+  useNavigate,
+  RouteBuilder,
+} from '@openmsupply-client/common';
 import { useReturnsApi } from '../utils/useReturnsApi';
+import { AppRoute } from 'packages/config/src';
 
 export const useInsertInboundReturn = () => {
   const queryClient = useQueryClient();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const api = useReturnsApi();
   return useMutation(api.insertInboundReturn, {
-    onSuccess: () => {
-      // TODO: redirect to details page
-      // onSuccess: invoiceNumber => {
-      // const route = RouteBuilder.create(AppRoute.Replenishment)
-      //   .addPart(AppRoute.InboundShipment)
-      //   .addPart(String(invoiceNumber))
-      //   .build();
-      // navigate(route, { replace: true });
+    onSuccess: invoiceNumber => {
+      const route = RouteBuilder.create(AppRoute.Distribution)
+        .addPart(AppRoute.InboundReturn)
+        .addPart(String(invoiceNumber))
+        .build();
+      navigate(route);
       return queryClient.invalidateQueries(api.keys.base());
     },
   });
