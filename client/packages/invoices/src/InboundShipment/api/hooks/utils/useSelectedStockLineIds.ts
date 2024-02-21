e83bd187c1f0a49f09ae641f385cd3d9d0c4ccaf
Lache Melvin <lache.melvin@gmail.com>
2024-02-21 14:15:47 +1300

    outboundShipmentLineIds -> stockLineIds

---
+++ client/packages/invoices/src/InboundShipment/api/hooks/utils/useSelectedStockLineIds.ts
@@ -0,0 +1,23 @@
+import { useTableStore } from 'packages/common/src';
+import { useInboundRows } from '../line/useInboundRows';
+import { isString } from 'lodash';
+
+export const useSelectedStockLineIds = () => {
+  const { items, lines } = useInboundRows();
+
+  const selectedIds =
+    useTableStore(state => {
+      const { isGrouped } = state;
+
+      return isGrouped
+        ? items
+            ?.filter(({ id }) => state.rowState[id]?.isSelected)
+            .map(({ lines }) => lines.flat())
+            .flat()
+        : lines?.filter(({ id }) => state.rowState[id]?.isSelected);
+    })
+      ?.map(({ stockLine }) => stockLine?.id)
+      .filter(isString) || [];
+
+  return selectedIds;
+};
