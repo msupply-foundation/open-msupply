28a672314408675a881e9fbfcff84145568d7357
James Brunskill <james@sussol.net>
2024-02-22 15:04:55 +1300

    Asset Table

--- server/repository/src/db_diesel/assets/mod.rs
+++ server/repository/src/db_diesel/assets/mod.rs
@@ -1,6 +1,8 @@
+pub mod asset;
 pub mod asset_category;
 pub mod asset_category_row;
 pub mod asset_class;
 pub mod asset_class_row;
+pub mod asset_row;
 pub mod asset_type;
 pub mod asset_type_row;
---
+++ server/repository/src/migrations/v1_08_00/assets/asset.rs
@@ -0,0 +1,42 @@
+use crate::migrations::DATE;
+use crate::migrations::DATETIME;
+use crate::{migrations::sql, StorageConnection};
+
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    sql!(
+        connection,
+        r#"
+        CREATE TABLE asset (
+            id TEXT NOT NULL PRIMARY KEY,
+            serial_number TEXT NOT NULL, 
+            asset_category_id TEXT REFERENCES asset_category (id),
+            asset_type_id TEXT REFERENCES asset_type (id),
+            catalogue_item_id, -- TODO: after merge https://github.com/msupply-foundation/open-msupply/pull/3036 TEXT REFERENCES catalogue_item (id)
+            installation_date {DATE},
+            replacement_date {DATE},
+            deleted_datetime {DATETIME},
+            UNIQUE (serial_number, deleted_datetime) --If something doesn't have a serial number, one can be generated?
+        );
+        CREATE INDEX asset_category_id ON asset (asset_category_id);
+        CREATE INDEX asset_type_id ON asset (asset_type_id);
+        CREATE INDEX asset_catalogue_item_id ON asset (catalogue_item_id);
+        CREATE INDEX asset_serial_number ON asset (serial_number);
+        CREATE INDEX asset_deleted_datetime ON asset (deleted_datetime);
+        "#,
+    )?;
+
+    sql!(
+        connection,
+        r#"
+        CREATE TABLE asset_location (
+            id TEXT NOT NULL PRIMARY KEY,
+            asset_id TEXT NOT NULL REFERENCES asset (id),
+            location_id TEXT NOT NULL REFERENCES location (id),
+            UNIQUE (asset_id, location_id)
+        );
+        CREATE INDEX asset_location_asset_id ON asset_location (asset_id);
+        "#,
+    )?;
+
+    Ok(())
+}
