124820734d54af82693a6bff76f802e0065c6bf8
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-02-23 10:26:44 +1300

    Add query types

---
+++ server/service/src/catalogue/query_types.rs
@@ -0,0 +1,37 @@
+use repository::{
+    assets::asset_type::{AssetType, AssetTypeFilter, AssetTypeRepository, AssetTypeSort},
+    EqualFilter, PaginationOption, StorageConnection,
+};
+
+use crate::{
+    get_default_pagination, i64_to_u32, service_provider::ServiceContext, ListError, ListResult,
+    SingleRecordError,
+};
+
+pub const MAX_LIMIT: u32 = 1000;
+pub const MIN_LIMIT: u32 = 1;
+
+pub fn get_asset_types(
+    connection: &StorageConnection,
+    pagination: Option<PaginationOption>,
+    filter: Option<AssetTypeFilter>,
+    sort: Option<AssetTypeSort>,
+) -> Result<ListResult<AssetType>, ListError> {
+    let pagination = get_default_pagination(pagination, MAX_LIMIT, MIN_LIMIT)?;
+    let repository = AssetTypeRepository::new(&connection);
+    Ok(ListResult {
+        rows: repository.query(pagination, filter.clone(), sort)?,
+        count: i64_to_u32(repository.count(filter)?),
+    })
+}
+
+pub fn get_asset_type(ctx: &ServiceContext, id: String) -> Result<AssetType, SingleRecordError> {
+    let repository = AssetTypeRepository::new(&ctx.connection);
+    let mut result =
+        repository.query_by_filter(AssetTypeFilter::new().id(EqualFilter::equal_to(&id)))?;
+    if let Some(record) = result.pop() {
+        Ok(record)
+    } else {
+        Err(SingleRecordError::NotFound(id))
+    }
+}
