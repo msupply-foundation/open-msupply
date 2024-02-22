d2bfc67cd261f69899d186799c49b0b970ab96d9
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-02-23 09:56:09 +1300

    Add service layer query class

---
+++ server/service/src/catalogue/query_classes.rs
@@ -0,0 +1,37 @@
+use repository::{
+    assets::asset_class::{AssetClass, AssetClassFilter, AssetClassRepository, AssetClassSort},
+    EqualFilter, PaginationOption, StorageConnection,
+};
+
+use crate::{
+    get_default_pagination, i64_to_u32, service_provider::ServiceContext, ListError, ListResult,
+    SingleRecordError,
+};
+
+pub const MAX_LIMIT: u32 = 1000;
+pub const MIN_LIMIT: u32 = 1;
+
+pub fn get_asset_classes(
+    connection: &StorageConnection,
+    pagination: Option<PaginationOption>,
+    filter: Option<AssetClassFilter>,
+    sort: Option<AssetClassSort>,
+) -> Result<ListResult<AssetClass>, ListError> {
+    let pagination = get_default_pagination(pagination, MAX_LIMIT, MIN_LIMIT)?;
+    let repository = AssetClassRepository::new(&connection);
+    Ok(ListResult {
+        rows: repository.query(pagination, filter.clone(), sort)?,
+        count: i64_to_u32(repository.count(filter)?),
+    })
+}
+
+pub fn get_asset_class(ctx: &ServiceContext, id: String) -> Result<AssetClass, SingleRecordError> {
+    let repository = AssetClassRepository::new(&ctx.connection);
+    let mut result =
+        repository.query_by_filter(AssetClassFilter::new().id(EqualFilter::equal_to(&id)))?;
+    if let Some(record) = result.pop() {
+        Ok(record)
+    } else {
+        Err(SingleRecordError::NotFound(id))
+    }
+}
