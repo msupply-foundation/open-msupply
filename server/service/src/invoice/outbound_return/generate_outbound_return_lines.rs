057bff45d2e4c596b24d25e47aef96be5b32c4fa
Lache Melvin <lache.melvin@gmail.com>
2024-02-22 17:35:07 +1300

    leave myself some todo notes

--- server/service/src/invoice/outbound_return/generate_outbound_return_lines.rs
+++ server/service/src/invoice/outbound_return/generate_outbound_return_lines.rs
@@ -8,12 +8,40 @@ pub struct OutboundReturnLine {
     pub comment: Option<String>,
     pub number_of_packs: u32,
     pub stock_line: StockLineRow,
+    // TODO: should Item be here or should we make a new join query on stockline??
     pub item: ItemRow,
+}
+
+pub struct OutboundReturnLinesInput {
+    pub stock_line_ids: Vec<String>,
+    pub item_id: Option<String>,
+    pub return_id: Option<String>,
 }
 
 pub fn generate_outbound_return_lines(
     ctx: &ServiceContext,
+    input: OutboundReturnLinesInput,
 ) -> Result<ListResult<OutboundReturnLine>, ListError> {
+    // validate
+    // - stock line ids exist?
+    // - item_id exists?
+    // - return_id exists?
+    // wondering if we do need the inbound shipment id - don't I need to validate its delivered/verified?
+    // what else does backend need to check to determine stock is returnable?
+    // maybe not much - if quantity available is 0, then can't do anything after this
+
+    /*
+     THEN!
+    v1:
+    query for stock lines
+    query for items based on ids in stock lines, or consider join here
+
+    v2
+    if item id, query stock lines by item id?
+
+    v3
+    if return_id, query for return lines by return id
+     */
     Ok(ListResult {
         count: 1,
         rows: vec![OutboundReturnLine {
