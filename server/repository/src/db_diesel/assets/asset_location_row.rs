dbf9ad8938f9e188af5f146ca0b5393629f66e2e
James Brunskill <james@sussol.net>
2024-02-22 15:11:04 +1300

    asset_location_row
    Source: https://github.com/msupply-foundation/open-msupply/commit/317860961bdf65db838e9929a4ed828d54aebea0

---
+++ server/repository/src/db_diesel/assets/asset_location_row.rs
@@ -0,0 +1,96 @@
+use super::asset_location_row::asset_location::dsl::*;
+
+use crate::RepositoryError;
+use crate::StorageConnection;
+
+use diesel::prelude::*;
+
+table! {
+    asset_location (id) {
+        id -> Text,
+        asset_id -> Text,
+        location_id -> Text,
+    }
+}
+
+#[derive(Clone, Insertable, Queryable, Debug, PartialEq, AsChangeset, Eq)]
+#[table_name = "asset_location"]
+pub struct AssetLocationRow {
+    pub id: String,
+    pub asset_id: String,
+    pub location_id: String,
+}
+
+pub struct AssetLocationRowRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> AssetLocationRowRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        AssetLocationRowRepository { connection }
+    }
+
+    #[cfg(feature = "postgres")]
+    pub fn upsert_one(&self, asset_location_row: &AssetLocationRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(asset_location)
+            .values(asset_location_row)
+            .on_conflict(id)
+            .do_update()
+            .set(asset_location_row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    #[cfg(not(feature = "postgres"))]
+    pub fn upsert_one(&self, asset_location_row: &AssetLocationRow) -> Result<(), RepositoryError> {
+        diesel::replace_into(asset_location)
+            .values(asset_location_row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    pub fn insert_one(&self, asset_location_row: &AssetLocationRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(asset_location)
+            .values(asset_location_row)
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+
+    pub fn find_all_by_location(
+        &self,
+        some_location_id: String,
+    ) -> Result<Vec<AssetLocationRow>, RepositoryError> {
+        let result = asset_location
+            .filter(location_id.eq(some_location_id))
+            .load(&self.connection.connection);
+        Ok(result?)
+    }
+
+    pub fn find_all_by_asset(
+        &self,
+        some_asset_id: String,
+    ) -> Result<Vec<AssetLocationRow>, RepositoryError> {
+        let result = asset_location
+            .filter(asset_id.eq(some_asset_id))
+            .load(&self.connection.connection);
+        Ok(result?)
+    }
+
+    pub fn find_one_by_id(
+        &self,
+        asset_location_id: &str,
+    ) -> Result<Option<AssetLocationRow>, RepositoryError> {
+        let result = asset_location
+            .filter(id.eq(asset_location_id))
+            .first(&self.connection.connection)
+            .optional()?;
+        Ok(result)
+    }
+
+    pub fn delete(&self, asset_location_id: &str) -> Result<(), RepositoryError> {
+        diesel::delete(asset_location)
+            .filter(id.eq(asset_location_id))
+            .execute(&self.connection.connection)?;
+        Ok(())
+    }
+}
