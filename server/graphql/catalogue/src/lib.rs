4e25c9a3c5bf8192c7191b4583a2cdce343ca663
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-02-23 16:47:31 +1300

    Add asset_catalogue_item_queries
    Source: https://github.com/msupply-foundation/open-msupply/commit/5c82fb08f37a7c10651fb3ae8b8307163f550def

--- server/graphql/catalogue/src/lib.rs
+++ server/graphql/catalogue/src/lib.rs
@@ -1,28 +1,32 @@
 use async_graphql::*;
-use graphql_core::{
-    pagination::PaginationInput,
-    standard_graphql_error::{validate_auth, StandardGraphqlError},
-    ContextExt,
-};
+use graphql_core::pagination::PaginationInput;
 use graphql_types::types::*;
-use repository::{
-    asset_catalogue_item::AssetCatalogueItemFilter, asset_category::AssetCategoryFilter,
-    asset_class::AssetClassFilter, asset_type::AssetTypeFilter, EqualFilter, PaginationOption,
-};
-use service::auth::{Resource, ResourceAccessRequest};
+
+pub mod asset_catalogue_item_queries;
+use self::asset_catalogue_item_queries::*;
 
 #[derive(Default, Clone)]
-pub struct AssetCatalogueQueries;
-
+pub struct AssetCatalogueItemQueries;
 #[Object]
-impl AssetCatalogueQueries {
-
+impl AssetCatalogueItemQueries {
     pub async fn asset_catalogue_items(
         &self,
         ctx: &Context<'_>,
+        store_id: String,
         #[graphql(desc = "Pagination option (first and offset)")] page: Option<PaginationInput>,
-        #[graphql(desc = "Filter option")] filter: Option<TemperatureLogFilterInput>,
+        #[graphql(desc = "Filter option")] filter: Option<AssetCatalogueItemFilterInput>,
         #[graphql(desc = "Sort options (only first sort input is evaluated for this endpoint)")]
-    sort: Option<Vec<AssetCatalogueSortInput>>,    
-)
+        sort: Option<Vec<AssetCatalogueItemSortInput>>,
+    ) -> Result<AssetCatalogueItemsResponse> {
+        asset_catalogue_items(ctx, store_id, page, filter, sort)
+    }
+
+    pub async fn asset_catalogue_item(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        #[graphql(desc = "id of the asset catalogue item")] id: String,
+    ) -> Result<AssetCatalogueItemResponse> {
+        asset_catalogue_item(ctx, store_id, id)
+    }
 }
