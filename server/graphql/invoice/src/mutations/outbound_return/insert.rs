f1040f947cec31b299c5899fd4e482eb9deedd13
Lache Melvin <lache.melvin@gmail.com>
2024-02-23 17:42:29 +1300

    start graphql layer
    Source: https://github.com/msupply-foundation/open-msupply/commit/dab810f78d874002714400e15d9d7307d9c1b93a

--- server/graphql/invoice/src/mutations/outbound_return/insert.rs
+++ server/graphql/invoice/src/mutations/outbound_return/insert.rs
@@ -1,8 +1,16 @@
 use async_graphql::*;
 
-use graphql_core::simple_generic_errors::{OtherPartyNotASupplier, OtherPartyNotVisible};
+use graphql_core::{
+    simple_generic_errors::{OtherPartyNotASupplier, OtherPartyNotVisible},
+    standard_graphql_error::validate_auth,
+    ContextExt,
+};
 use graphql_types::types::InvoiceNode;
 use repository::Invoice;
+use service::auth::{Resource, ResourceAccessRequest};
+use service::invoice::outbound_return::insert::{
+    InsertOutboundReturn as ServiceInput, InsertOutboundReturnLine,
+};
 
 #[derive(InputObject)]
 #[graphql(name = "OutboundReturnInput")]
@@ -34,7 +42,23 @@ pub enum InsertResponse {
     Response(InvoiceNode),
 }
 
-pub fn insert(_ctx: &Context<'_>, _input: InsertInput) -> Result<InsertResponse> {
+pub fn insert(ctx: &Context<'_>, store_id: &str, input: InsertInput) -> Result<InsertResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            // resource: Resource::MutateOutboundReturn, // TODO
+            resource: Resource::MutateInboundShipment,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    let result = service_provider
+        .invoice_service
+        .insert_outbound_return(&service_context, input);
+
     Ok(InsertResponse::Response(InvoiceNode::from_domain(
         Invoice {
             ..Default::default()
@@ -48,4 +72,43 @@ pub fn insert(_ctx: &Context<'_>, _input: InsertInput) -> Result<InsertResponse>
 pub enum InsertErrorInterface {
     OtherPartyNotVisible(OtherPartyNotVisible),
     OtherPartyNotASupplier(OtherPartyNotASupplier),
+}
+
+impl InsertInput {
+    pub fn to_domain(self) -> ServiceInput {
+        let InsertInput {
+            id,
+            supplier_id,
+            outbound_return_lines,
+        }: InsertInput = self;
+
+        ServiceInput {
+            id,
+            other_party_id: supplier_id,
+            outbound_return_lines: outbound_return_lines
+                .into_iter()
+                .map(|line| line.to_domain())
+                .collect(),
+        }
+    }
+}
+
+impl OutboundReturnLineInput {
+    pub fn to_domain(self) -> InsertOutboundReturnLine {
+        let OutboundReturnLineInput {
+            id,
+            stock_line_id,
+            number_of_packs_to_return,
+            reason_id,
+            comment,
+        }: OutboundReturnLineInput = self;
+
+        InsertOutboundReturnLine {
+            id,
+            stock_line_id,
+            number_of_packs: number_of_packs_to_return,
+            reason_id,
+            note: comment,
+        }
+    }
 }
