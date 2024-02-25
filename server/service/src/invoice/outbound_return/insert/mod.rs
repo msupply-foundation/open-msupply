c46a97165dcf4112c1ccf47ff491ae0e92e182df
Lache Melvin <lache.melvin@gmail.com>
2024-02-26 12:20:52 +1300

    implement generate

--- server/service/src/invoice/outbound_return/insert/mod.rs
+++ server/service/src/invoice/outbound_return/insert/mod.rs
@@ -1,11 +1,13 @@
 use repository::{
-    ActivityLogType, Invoice, InvoiceRow, InvoiceRowRepository, RepositoryError, TransactionError,
+    ActivityLogType, Invoice, InvoiceRowRepository, RepositoryError, TransactionError,
 };
 
 use crate::{
     activity_log::activity_log_entry, invoice::get_invoice, service_provider::ServiceContext,
 };
+pub mod generate;
 pub mod validate;
+use generate::generate;
 use validate::validate;
 
 #[derive(Clone, Debug, Default, PartialEq)]
@@ -44,11 +46,13 @@ pub fn insert_outbound_return(
         .connection
         .transaction_sync(|connection| {
             let other_party = validate(connection, &ctx.store_id, &input)?;
-            // let new_invoice = generate(connection, &ctx.store_id, &ctx.user_id, input, other_party)?;
-
-            let new_invoice = InvoiceRow {
-                ..Default::default()
-            };
+            let new_invoice = generate(
+                connection,
+                &ctx.store_id,
+                &ctx.user_id,
+                input.clone(),
+                other_party,
+            )?;
 
             InvoiceRowRepository::new(&connection).upsert_one(&new_invoice)?;
 
@@ -60,11 +64,10 @@ pub fn insert_outbound_return(
                 None,
             )?;
 
-            for line in input.outbound_return_lines {
+            for _line in input.outbound_return_lines {
                 // insert them liiiines
             }
 
-            // TODO: do we want to respond with invoice lines as well?
             get_invoice(ctx, None, &new_invoice.id)
                 .map_err(|error| InsertOutboundReturnError::DatabaseError(error))?
                 .ok_or(InsertOutboundReturnError::NewlyCreatedInvoiceDoesNotExist)
@@ -97,11 +100,13 @@ impl From<TransactionError<InsertOutboundReturnError>> for InsertOutboundReturnE
 #[cfg(test)]
 mod test {
     use repository::{
-        mock::{mock_outbound_return_a, mock_store_a, MockData, MockDataInserts},
+        mock::{
+            mock_outbound_return_a, mock_store_a, mock_user_account_a, MockData, MockDataInserts,
+        },
         test_db::setup_all_with_data,
-        NameRow, NameStoreJoinRow,
+        InvoiceRowRepository, NameRow, NameStoreJoinRow,
     };
-    use util::inline_init;
+    use util::{inline_edit, inline_init};
 
     use crate::{
         invoice::outbound_return::insert::{InsertOutboundReturn, InsertOutboundReturnError},
@@ -194,5 +199,63 @@ mod test {
         );
 
         // TODO: line error?
+    }
+
+    #[actix_rt::test]
+    async fn test_insert_outbound_return_success() {
+        fn supplier() -> NameRow {
+            inline_init(|r: &mut NameRow| {
+                r.id = "supplier".to_string();
+            })
+        }
+
+        fn supplier_join() -> NameStoreJoinRow {
+            inline_init(|r: &mut NameStoreJoinRow| {
+                r.id = "supplier_join".to_string();
+                r.name_link_id = supplier().id;
+                r.store_id = mock_store_a().id;
+                r.name_is_supplier = true;
+            })
+        }
+
+        let (_, connection, connection_manager, _) = setup_all_with_data(
+            "test_insert_outbound_return_success",
+            MockDataInserts::all(),
+            inline_init(|r: &mut MockData| {
+                r.names = vec![supplier()];
+                r.name_store_joins = vec![supplier_join()];
+            }),
+        )
+        .await;
+
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider
+            .context(mock_store_a().id, mock_user_account_a().id)
+            .unwrap();
+
+        service_provider
+            .invoice_service
+            .insert_outbound_return(
+                &context,
+                inline_init(|r: &mut InsertOutboundReturn| {
+                    r.id = "new_outbound_return_id".to_string();
+                    r.other_party_id = supplier().id;
+                }),
+            )
+            .unwrap();
+
+        let invoice = InvoiceRowRepository::new(&connection)
+            .find_one_by_id("new_outbound_return_id")
+            .unwrap();
+
+        assert_eq!(invoice.id, "new_outbound_return_id");
+        assert_eq!(
+            invoice,
+            inline_edit(&invoice, |mut u| {
+                u.name_link_id = supplier().id;
+                u.user_id = Some(mock_user_account_a().id);
+                u
+            })
+        );
     }
 }
