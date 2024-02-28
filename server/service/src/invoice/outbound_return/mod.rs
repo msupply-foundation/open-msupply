882cec2a66f7786fe0bde2fc373eb5458d139fb1
Lache Melvin <lache.melvin@gmail.com>
2024-02-28 14:42:21 +1300

    make outbound return input a shared type

--- server/service/src/invoice/outbound_return/mod.rs
+++ server/service/src/invoice/outbound_return/mod.rs
@@ -1,3 +1,12 @@
 pub mod generate_outbound_return_lines;
 pub mod insert;
 pub mod update;
+
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct OutboundReturnLineInput {
+    pub id: String,
+    pub stock_line_id: String,
+    pub number_of_packs: f64,
+    pub reason_id: String,
+    pub note: String,
+}
