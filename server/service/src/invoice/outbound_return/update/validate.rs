17fdc71167f712ed820512521709788afccd4891
Lache Melvin <lache.melvin@gmail.com>
2024-02-29 14:45:24 +1300

    add status check to validate
    Source: https://github.com/msupply-foundation/open-msupply/commit/1873d834df40ff0674d4f8b4a31a9f96e5140ef7

--- server/service/src/invoice/outbound_return/update/validate.rs
+++ server/service/src/invoice/outbound_return/update/validate.rs
@@ -1,19 +1,21 @@
 use repository::{InvoiceRow, InvoiceRowType, StorageConnection};
 
 use crate::invoice::{
-    check_invoice_exists, check_invoice_is_editable, check_invoice_type, check_store,
+    check_invoice_exists, check_invoice_is_editable, check_invoice_status, check_invoice_type,
+    check_status_change, check_store, InvoiceRowStatusError,
 };
 
-use super::UpdateOutboundReturnError;
+use super::{UpdateOutboundReturn, UpdateOutboundReturnError};
 
 pub fn validate(
     connection: &StorageConnection,
     store_id: &str,
-    id: &str,
+    input: &UpdateOutboundReturn,
 ) -> Result<(InvoiceRow, bool), UpdateOutboundReturnError> {
     use UpdateOutboundReturnError::*;
 
-    let return_row = check_invoice_exists(id, connection)?.ok_or(ReturnDoesNotExist)?;
+    let return_row =
+        check_invoice_exists(&input.outbound_return_id, connection)?.ok_or(ReturnDoesNotExist)?;
 
     if !check_store(&return_row, store_id) {
         return Err(ReturnDoesNotBelongToCurrentStore);
@@ -25,5 +27,18 @@ pub fn validate(
         return Err(NotAnOutboundReturn);
     }
 
-    Ok((return_row, false))
+    // Status check
+    let status_changed = check_status_change(&return_row, input.full_status());
+    if status_changed {
+        // TODO: on_hold?
+        check_invoice_status(&return_row, input.full_status(), &Some(false)).map_err(
+            |e| match e {
+                InvoiceRowStatusError::CannotChangeStatusOfInvoiceOnHold => {
+                    CannotChangeStatusOfInvoiceOnHold
+                }
+                InvoiceRowStatusError::CannotReverseInvoiceStatus => CannotReverseInvoiceStatus,
+            },
+        )?;
+    }
+    Ok((return_row, status_changed))
 }
