6b22b643000204ca97209e3d0355c9170859cfe9
Mark Prins <mark@malcolmprins.net>
2024-03-01 13:21:35 +1300

    migration fixes
    Source: https://github.com/msupply-foundation/open-msupply/commit/b1c2d49fc110efc195f17247d64ca882d0880a80

--- server/repository/src/migrations/v1_06_00/mod.rs
+++ server/repository/src/migrations/v1_06_00/mod.rs
@@ -6,6 +6,7 @@ mod changelog_deduped;
 mod contact_trace;
 mod encounter_status;
 mod indexes;
+mod is_sync_update;
 mod master_list;
 mod patient_id_indices;
 mod plugin_data;
@@ -29,6 +30,7 @@ impl Migration for V1_06_00 {
         indexes::migrate(connection)?;
         encounter_status::migrate(connection)?;
         changelog_deduped::migrate(connection)?;
+        is_sync_update::migrate(connection)?;
         Ok(())
     }
 }
