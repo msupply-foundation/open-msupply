b7f9892be659e18ab465d89a2bcb3c131566d5fa
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-03-01 14:18:57 +1300

    Add asset log single record and pagination tests

---
+++ server/service/src/asset/tests/query_log.rs
@@ -0,0 +1,78 @@
+#[cfg(test)]
+
+mod query {
+    use repository::{
+        assets::asset_log::{AssetLogFilter, AssetLogSort, AssetLogSortField},
+        mock::{
+            asset_log::{mock_asset_log_a, mock_asset_log_b, mock_asset_log_c},
+            MockDataInserts,
+        },
+        test_db::setup_all,
+    };
+    use repository::{EqualFilter, PaginationOption};
+
+    use crate::{service_provider::ServiceProvider, ListError, SingleRecordError};
+
+    #[actix_rt::test]
+
+    async fn asset_log_service_pagination() {
+        let (_, connection, connection_manager, _) = setup_all(
+            "test_asset_log_service_pagination",
+            MockDataInserts::none().asset_logs(),
+        )
+        .await;
+
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let service = service_provider.asset_service;
+
+        assert_eq!(
+            service.get_asset_logs(
+                &connection,
+                Some(PaginationOption {
+                    limit: Some(2000),
+                    offset: None,
+                }),
+                None,
+                None
+            ),
+            Err(ListError::LimitAboveMax(1000))
+        );
+
+        assert_eq!(
+            service.get_asset_logs(
+                &connection,
+                Some(PaginationOption {
+                    limit: Some(0),
+                    offset: None
+                }),
+                None,
+                None
+            ),
+            Err(ListError::LimitBelowMin(1))
+        )
+    }
+
+    #[actix_rt::test]
+    async fn asset_log_service_single_record() {
+        let (_, _, connection_manager, _) = setup_all(
+            "test_asset_log_single_record",
+            MockDataInserts::none().asset_logs(),
+        )
+        .await;
+
+        let service_provider = ServiceProvider::new(connection_manager, "app_data");
+        let context = service_provider.basic_context().unwrap();
+        let service = service_provider.asset_service;
+
+        assert_eq!(
+            service.get_asset_log(&context, "invalid_id".to_owned()),
+            Err(SingleRecordError::NotFound("invalid_id".to_owned()))
+        );
+
+        let result = service
+            .get_asset_log(&context, mock_asset_log_a().id)
+            .unwrap();
+
+        assert_eq!(result.id, mock_asset_log_a().id);
+    }
+}
