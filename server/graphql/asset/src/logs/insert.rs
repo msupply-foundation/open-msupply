c6aaa8c6ba821c01b089bdc712438832e2a2ffb8
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-03-05 12:12:22 +1300

    Add insert asset_log graphql test

--- server/graphql/asset/src/logs/insert.rs
+++ server/graphql/asset/src/logs/insert.rs
@@ -102,3 +102,103 @@ fn map_error(error: ServiceError) -> Result<InsertAssetLogErrorInterface> {
 
     Err(graphql_error.extend())
 }
+
+#[cfg(test)]
+
+mod test {
+    use async_graphql::EmptyMutation;
+    use graphql_core::{assert_graphql_query, test_helpers::setup_graphl_test};
+    use repository::{
+        assets::asset_log::AssetLog, mock::MockDataInserts, StorageConnectionManager,
+    };
+    use serde_json::json;
+
+    use service::{
+        asset::{
+            insert_log::{InsertAssetLog, InsertAssetLogError},
+            AssetServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+
+    use crate::AssetLogs;
+
+    type InsertAssetLogMethod =
+        dyn Fn(InsertAssetLog) -> Result<AssetLog, InsertAssetLogError> + Sync + Send;
+
+    pub struct TestService(pub Box<InsertAssetLogMethod>);
+    impl AssetServiceTrait for TestService {
+        fn insert_asset_log(
+            &self,
+            _: &ServiceContext,
+            input: InsertAssetLog,
+        ) -> Result<AssetLog, InsertAssetLogError> {
+            (self.0)(input)
+        }
+    }
+
+    pub fn service_provider(
+        asset_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.asset_service = Box::new(asset_service);
+        service_provider
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_insert_asset_log_success() {
+        let (_, _, connection_manager, settings) = setup_graphl_test(
+            EmptyMutation,
+            AssetLogs,
+            "test_graphql_insert_asset_log_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: InsertAssetLogInput!) {
+            insertAssetLog(input: $input, storeId: \"store_a\") {
+                ... on AssetLogNode {
+                    id
+                    assetId
+                    status
+                }
+            }
+        }
+        "#;
+
+        let variables = Some(json!({
+            "input": {
+                "id": "n/a",
+                "assetId": "asset_a",
+                "status": "status",
+            }
+        }));
+
+        // Record already exists
+        let test_service = TestService(Box::new(|_| {
+            Ok(AssetLog {
+                id: "id".to_owned(),
+                asset_id: "asset_a".to_owned(),
+                status: Some("status".to_owned()),
+                ..Default::default()
+            })
+        }));
+
+        let expected = json!({
+            "insertAssetLog": {
+                "id": "id",
+                "assetId": "asset_a",
+                "status": "status",
+            }
+        });
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &variables,
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+}
