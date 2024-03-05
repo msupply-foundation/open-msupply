8e08870d7885d396c78304bed3593ac5290b018a
roxy-dao <roxy@msupply.foundation>
2024-03-05 17:04:56 +1300

    Add box sx to paper popover section
    Source: https://github.com/msupply-foundation/open-msupply/commit/f5b2edf02fe9f837347081c56fcdc3345290560f

--- client/packages/common/src/ui/components/popover/PaperPopover/PaperPopoverSection.tsx
+++ client/packages/common/src/ui/components/popover/PaperPopover/PaperPopoverSection.tsx
@@ -1,22 +1,25 @@
 import React, { FC, PropsWithChildren } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { SxProps, Theme } from '@mui/material';
 
 export interface PaperPopoverSectionProps {
   label?: string;
   labelStyle?: React.CSSProperties;
   alignItems?: 'center' | 'flex-start' | 'flex-end' | 'stretch';
+  sx?: SxProps<Theme>;
 }
 
 export const PaperPopoverSection: FC<
   PropsWithChildren<PaperPopoverSectionProps>
-> = ({ children, label, labelStyle, alignItems }) => (
+> = ({ children, label, labelStyle, alignItems, sx }) => (
   <Box
     gap={2}
     p={3}
     flexDirection="column"
     display="flex"
     alignItems={alignItems}
+    sx={sx}
   >
     <Typography fontWeight="700" style={labelStyle}>
       {label}
