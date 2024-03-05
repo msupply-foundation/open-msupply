96fdea0a75eee76f23ad8ec98c904f722daebc39
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-03-05 16:59:59 +1300

    Basic Toolbar component

---
+++ client/packages/invoices/src/Returns/DetailView/Footer/StatusChangeButton.tsx
@@ -0,0 +1,195 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import {
+  ArrowRightIcon,
+  useTranslation,
+  useNotification,
+  InvoiceNodeStatus,
+  SplitButton,
+  SplitButtonOption,
+  useConfirmationModal,
+} from '@openmsupply-client/common';
+import { getNextOutboundStatus, getStatusTranslation } from '../../../utils';
+import { useReturns } from '../../api';
+
+const getStatusOptions = (
+  currentStatus: InvoiceNodeStatus,
+  getButtonLabel: (status: InvoiceNodeStatus) => string
+): SplitButtonOption<InvoiceNodeStatus>[] => {
+  const options: [
+    SplitButtonOption<InvoiceNodeStatus>,
+    SplitButtonOption<InvoiceNodeStatus>,
+    SplitButtonOption<InvoiceNodeStatus>,
+    SplitButtonOption<InvoiceNodeStatus>,
+    SplitButtonOption<InvoiceNodeStatus>,
+    SplitButtonOption<InvoiceNodeStatus>,
+  ] = [
+    {
+      value: InvoiceNodeStatus.New,
+      label: getButtonLabel(InvoiceNodeStatus.New),
+      isDisabled: true,
+    },
+    {
+      value: InvoiceNodeStatus.Allocated,
+      label: getButtonLabel(InvoiceNodeStatus.Allocated),
+      isDisabled: true,
+    },
+    {
+      value: InvoiceNodeStatus.Picked,
+      label: getButtonLabel(InvoiceNodeStatus.Picked),
+      isDisabled: true,
+    },
+    {
+      value: InvoiceNodeStatus.Shipped,
+      label: getButtonLabel(InvoiceNodeStatus.Shipped),
+      isDisabled: true,
+    },
+    {
+      value: InvoiceNodeStatus.Delivered,
+      label: getButtonLabel(InvoiceNodeStatus.Delivered),
+      isDisabled: true,
+    },
+    {
+      value: InvoiceNodeStatus.Verified,
+      label: getButtonLabel(InvoiceNodeStatus.Verified),
+      isDisabled: true,
+    },
+  ];
+
+  if (currentStatus === InvoiceNodeStatus.New) {
+    options[1].isDisabled = false;
+    options[2].isDisabled = false;
+    options[3].isDisabled = false;
+  }
+
+  if (currentStatus === InvoiceNodeStatus.Allocated) {
+    options[2].isDisabled = false;
+    options[3].isDisabled = false;
+  }
+
+  if (currentStatus === InvoiceNodeStatus.Picked) {
+    options[3].isDisabled = false;
+  }
+
+  return options;
+};
+
+const getNextStatusOption = (
+  status: InvoiceNodeStatus,
+  options: SplitButtonOption<InvoiceNodeStatus>[]
+): SplitButtonOption<InvoiceNodeStatus> | null => {
+  if (!status) return options[0] ?? null;
+
+  const nextStatus = getNextOutboundStatus(status);
+  const nextStatusOption = options.find(o => o.value === nextStatus);
+  return nextStatusOption || null;
+};
+
+const getButtonLabel =
+  (t: ReturnType<typeof useTranslation>) =>
+  (invoiceStatus: InvoiceNodeStatus): string => {
+    return t('button.save-and-confirm-status', {
+      status: t(getStatusTranslation(invoiceStatus)),
+    });
+  };
+
+const useStatusChangeButton = () => {
+  // const { lines, status, onHold, update } = useOutbound.document.fields([
+  //   'status',
+  //   'onHold',
+  //   'lines',
+  // ]);
+
+  const { success, error } = useNotification();
+  const t = useTranslation('distribution');
+  const { data } = useReturns.document.outboundReturn();
+
+  // TEMP until "fields" hook available:
+  const status = data?.status ?? InvoiceNodeStatus.New;
+  const update = async (_: unknown) => true;
+  const onHold = false;
+  const lines: any = [];
+
+  const hasLinesToPrune =
+    data?.status === InvoiceNodeStatus.New &&
+    (data?.lines?.nodes ?? []).some(line => line.numberOfPacks === 0);
+
+  const options = useMemo(
+    () => getStatusOptions(status, getButtonLabel(t)),
+    [status, getButtonLabel]
+  );
+
+  const [selectedOption, setSelectedOption] =
+    useState<SplitButtonOption<InvoiceNodeStatus> | null>(() =>
+      getNextStatusOption(status, options)
+    );
+
+  const onConfirmStatusChange = async () => {
+    if (!selectedOption) return null;
+    try {
+      await update({ status: selectedOption.value });
+      success(t('messages.shipment-saved'))();
+    } catch (e) {
+      error(t('messages.error-saving-shipment'))();
+    }
+  };
+
+  const getConfirmation = useConfirmationModal({
+    title: t('heading.are-you-sure'),
+    message: hasLinesToPrune
+      ? t('messages.confirm-zero-quantity-status')
+      : t('messages.confirm-status-as', {
+          status: selectedOption?.value
+            ? getStatusTranslation(selectedOption?.value)
+            : '',
+        }),
+    onConfirm: onConfirmStatusChange,
+  });
+
+  // When the status of the invoice changes (after an update), set the selected option to the next status.
+  // It would be set to the current status, which is now a disabled option.
+  useEffect(() => {
+    setSelectedOption(() => getNextStatusOption(status, options));
+  }, [status, options]);
+
+  return {
+    options,
+    selectedOption,
+    setSelectedOption,
+    getConfirmation,
+    onHold,
+    lines,
+  };
+};
+
+export const StatusChangeButton = () => {
+  const {
+    options,
+    selectedOption,
+    setSelectedOption,
+    getConfirmation,
+    onHold,
+    lines,
+  } = useStatusChangeButton();
+  // const isDisabled = useOutbound.utils.isDisabled();
+  const t = useTranslation();
+  const noLines = lines?.totalCount === 0;
+
+  if (!selectedOption) return null;
+  // if (isDisabled) return null;
+
+  const onStatusClick = () => {
+    return getConfirmation();
+  };
+
+  return (
+    <SplitButton
+      label={noLines ? t('messages.no-lines') : ''}
+      isDisabled={noLines || onHold}
+      options={options}
+      selectedOption={selectedOption}
+      onSelectOption={setSelectedOption}
+      Icon={<ArrowRightIcon />}
+      onClick={onStatusClick}
+    />
+  );
+};
--- client/packages/invoices/src/Returns/api/operations.generated.ts
+++ client/packages/invoices/src/Returns/api/operations.generated.ts
@@ -4,7 +4,7 @@ import { GraphQLClient } from 'graphql-request';
 import { GraphQLClientRequestHeaders } from 'graphql-request/build/cjs/types';
 import gql from 'graphql-tag';
 import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
-export type OutboundReturnRowFragment = { __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string };
+export type OutboundReturnRowFragment = { __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string, allocatedDatetime?: string | null, pickedDatetime?: string | null, shippedDatetime?: string | null, deliveredDatetime?: string | null, verifiedDatetime?: string | null };
 
 export type InboundReturnRowFragment = { __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string, deliveredDatetime?: string | null };
 
@@ -20,7 +20,7 @@ export type OutboundReturnsQueryVariables = Types.Exact<{
 }>;
 
 
-export type OutboundReturnsQuery = { __typename: 'Queries', invoices: { __typename: 'InvoiceConnector', totalCount: number, nodes: Array<{ __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string }> } };
+export type OutboundReturnsQuery = { __typename: 'Queries', invoices: { __typename: 'InvoiceConnector', totalCount: number, nodes: Array<{ __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string, allocatedDatetime?: string | null, pickedDatetime?: string | null, shippedDatetime?: string | null, deliveredDatetime?: string | null, verifiedDatetime?: string | null }> } };
 
 export type InboundReturnsQueryVariables = Types.Exact<{
   first?: Types.InputMaybe<Types.Scalars['Int']['input']>;
@@ -56,7 +56,7 @@ export type OutboundReturnByNumberQueryVariables = Types.Exact<{
 }>;
 
 
-export type OutboundReturnByNumberQuery = { __typename: 'Queries', invoiceByNumber: { __typename: 'InvoiceNode', id: string, invoiceNumber: number, otherPartyName: string, lines: { __typename: 'InvoiceLineConnector', nodes: Array<{ __typename: 'InvoiceLineNode', id: string, itemCode: string, itemName: string, batch?: string | null, expiryDate?: string | null, numberOfPacks: number, packSize: number, sellPricePerPack: number }> }, otherPartyStore?: { __typename: 'StoreNode', code: string } | null } | { __typename: 'NodeError' } };
+export type OutboundReturnByNumberQuery = { __typename: 'Queries', invoiceByNumber: { __typename: 'InvoiceNode', id: string, otherPartyName: string, status: Types.InvoiceNodeStatus, invoiceNumber: number, colour?: string | null, createdDatetime: string, allocatedDatetime?: string | null, pickedDatetime?: string | null, shippedDatetime?: string | null, deliveredDatetime?: string | null, verifiedDatetime?: string | null, lines: { __typename: 'InvoiceLineConnector', nodes: Array<{ __typename: 'InvoiceLineNode', id: string, itemCode: string, itemName: string, batch?: string | null, expiryDate?: string | null, numberOfPacks: number, packSize: number, sellPricePerPack: number }> }, otherPartyStore?: { __typename: 'StoreNode', code: string } | null } | { __typename: 'NodeError' } };
 
 export type InsertOutboundReturnMutationVariables = Types.Exact<{
   storeId: Types.Scalars['String']['input'];
@@ -99,6 +99,12 @@ export const OutboundReturnRowFragmentDoc = gql`
   invoiceNumber
   colour
   createdDatetime
+  createdDatetime
+  allocatedDatetime
+  pickedDatetime
+  shippedDatetime
+  deliveredDatetime
+  verifiedDatetime
 }
     `;
 export const InboundReturnRowFragmentDoc = gql`
@@ -215,20 +221,20 @@ export const OutboundReturnByNumberDocument = gql`
     ... on InvoiceNode {
       __typename
       id
-      invoiceNumber
       lines {
         nodes {
           ...OutboundReturnDetailRow
         }
       }
-      otherPartyName
       otherPartyStore {
         code
       }
+      ...OutboundReturnRow
     }
   }
 }
-    ${OutboundReturnDetailRowFragmentDoc}`;
+    ${OutboundReturnDetailRowFragmentDoc}
+${OutboundReturnRowFragmentDoc}`;
 export const InsertOutboundReturnDocument = gql`
     mutation insertOutboundReturn($storeId: String!, $input: OutboundReturnInput!) {
   insertOutboundReturn(storeId: $storeId, input: $input) {
