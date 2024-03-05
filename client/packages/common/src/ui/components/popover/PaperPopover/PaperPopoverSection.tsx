3885b0e165bdae493733f68154836032ef3ef5c4
roxy-dao <roxy@msupply.foundation>
2024-03-05 18:12:28 +1300

    Move sx straight to paper popover component
    Source: https://github.com/msupply-foundation/open-msupply/commit/e515cec5242a7d2ec763be400ee4409b7a3d2259

--- client/packages/common/src/ui/components/popover/PaperPopover/PaperPopoverSection.tsx
+++ client/packages/common/src/ui/components/popover/PaperPopover/PaperPopoverSection.tsx
@@ -1,25 +1,23 @@
 import React, { FC, PropsWithChildren } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { SxProps, Theme } from '@mui/material';
 
 export interface PaperPopoverSectionProps {
   label?: string;
   labelStyle?: React.CSSProperties;
   alignItems?: 'center' | 'flex-start' | 'flex-end' | 'stretch';
-  sx?: SxProps<Theme>;
 }
 
 export const PaperPopoverSection: FC<
   PropsWithChildren<PaperPopoverSectionProps>
-> = ({ children, label, labelStyle, alignItems, sx }) => (
+> = ({ children, label, labelStyle, alignItems }) => (
   <Box
     gap={2}
     p={3}
     flexDirection="column"
     display="flex"
     alignItems={alignItems}
-    sx={sx}
+    sx={{ wordBreak: 'break-word' }}
   >
     <Typography fontWeight="700" style={labelStyle}>
       {label}
