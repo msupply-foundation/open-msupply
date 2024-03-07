4e7fcba8db7ab6c5f0ad80777ba963c1e17293ed
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-03-08 11:22:17 +1300

    Add enum for status
    Source: https://github.com/msupply-foundation/open-msupply/commit/c5a489d5dcb8c06cbb5d5fb27b16061598984488

--- server/repository/src/db_diesel/assets/asset_log_row.rs
+++ server/repository/src/db_diesel/assets/asset_log_row.rs
@@ -15,7 +15,7 @@ table! {
         id -> Text,
         asset_id -> Text,
         user_id -> Text,
-        status -> Nullable<Text>,
+        status -> Nullable<crate::db_diesel::assets::asset_log_row::StatusMapping>,
         comment -> Nullable<Text>,
         #[sql_name = "type"] type_ -> Nullable<Text>,
         reason -> Nullable<crate::db_diesel::assets::asset_log_row::ReasonMapping>,
@@ -54,6 +54,31 @@ impl Reason {
     }
 }
 
+#[derive(DbEnum, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
+
+pub enum Status {
+    NotInUse,
+    Functioning,
+    FunctioningButNeedsAttention,
+    NotFunctioning,
+    Decomissioned,
+}
+
+impl Status {
+    pub fn equal_to(&self) -> EqualFilter<Status> {
+        EqualFilter {
+            equal_to: Some(self.clone()),
+            not_equal_to: None,
+            equal_any: None,
+            not_equal_all: None,
+            equal_any_or_null: None,
+            is_null: None,
+        }
+    }
+}
+
 #[derive(Clone, Insertable, Queryable, Debug, PartialEq, AsChangeset, Eq, Default)]
 #[changeset_options(treat_none_as_null = "true")]
 #[table_name = "asset_log"]
@@ -62,7 +87,7 @@ pub struct AssetLogRow {
     pub id: String,
     pub asset_id: String,
     pub user_id: String,
-    pub status: Option<String>,
+    pub status: Option<Status>,
     pub comment: Option<String>,
     #[column_name = "type_"]
     pub r#type: Option<String>,
