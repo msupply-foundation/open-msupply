efb296f7041d1badf4564e85fe6dae2114d3d517
Lache Melvin <lache.melvin@gmail.com>
2024-03-08 08:53:48 +1300

    handle no stock line for new inbound return lines

--- server/graphql/types/src/types/inbound_return_line.rs
+++ server/graphql/types/src/types/inbound_return_line.rs
@@ -1,47 +1,45 @@
 use async_graphql::*;
 use chrono::NaiveDate;
-use repository::{ItemRow, StockLineRow};
+use repository::ItemRow;
 use service::{invoice::inbound_return::generate_lines::InboundReturnLine, ListResult};
 
 #[derive(SimpleObject)]
-pub struct InboundReturnLineConnector {
+pub struct GeneratedInboundReturnLineConnector {
     total_count: u32,
-    nodes: Vec<InboundReturnLineNode>,
+    nodes: Vec<GeneratedInboundReturnLineNode>,
 }
 
-impl InboundReturnLineConnector {
-    pub fn from_domain(return_lines: ListResult<InboundReturnLine>) -> InboundReturnLineConnector {
-        InboundReturnLineConnector {
+impl GeneratedInboundReturnLineConnector {
+    pub fn from_domain(
+        return_lines: ListResult<InboundReturnLine>,
+    ) -> GeneratedInboundReturnLineConnector {
+        GeneratedInboundReturnLineConnector {
             total_count: return_lines.count,
             nodes: return_lines
                 .rows
                 .into_iter()
-                .map(InboundReturnLineNode::from_domain)
+                .map(GeneratedInboundReturnLineNode::from_domain)
                 .collect(),
         }
     }
 }
 
-pub struct InboundReturnLineNode {
+pub struct GeneratedInboundReturnLineNode {
     pub return_line: InboundReturnLine,
 }
 
-impl InboundReturnLineNode {
-    pub fn from_domain(return_line: InboundReturnLine) -> InboundReturnLineNode {
-        InboundReturnLineNode { return_line }
+impl GeneratedInboundReturnLineNode {
+    pub fn from_domain(return_line: InboundReturnLine) -> GeneratedInboundReturnLineNode {
+        GeneratedInboundReturnLineNode { return_line }
     }
 
     pub fn item_row(&self) -> &ItemRow {
         &self.return_line.item_row
     }
-
-    pub fn stock_line_row(&self) -> &StockLineRow {
-        &self.return_line.stock_line_row
-    }
 }
 
 #[Object]
-impl InboundReturnLineNode {
+impl GeneratedInboundReturnLineNode {
     pub async fn id(&self) -> &str {
         &self.return_line.id
     }
@@ -62,27 +60,31 @@ impl InboundReturnLineNode {
         &self.return_line.packs_issued
     }
 
+    pub async fn stock_line_id(&self) -> &Option<String> {
+        &self.return_line.stock_line_id
+    }
+
+    pub async fn batch(&self) -> &Option<String> {
+        &self.return_line.batch
+    }
+
+    pub async fn expiry_date(&self) -> &Option<NaiveDate> {
+        &self.return_line.expiry_date
+    }
+
+    pub async fn pack_size(&self) -> &i32 {
+        &self.return_line.pack_size
+    }
+
+    pub async fn item_id(&self) -> &str {
+        &self.item_row().id
+    }
+
     pub async fn item_code(&self) -> &str {
         &self.item_row().code
     }
 
     pub async fn item_name(&self) -> &str {
         &self.item_row().name
-    }
-
-    pub async fn stock_line_id(&self) -> &str {
-        &self.stock_line_row().id
-    }
-
-    pub async fn batch(&self) -> &Option<String> {
-        &self.stock_line_row().batch
-    }
-
-    pub async fn expiry_date(&self) -> &Option<NaiveDate> {
-        &self.stock_line_row().expiry_date
-    }
-
-    pub async fn pack_size(&self) -> &i32 {
-        &self.stock_line_row().pack_size
     }
 }
