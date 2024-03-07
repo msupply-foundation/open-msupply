efef88b1c6466c3ee3d051e600e51937a6209441
Lache Melvin <lache.melvin@gmail.com>
2024-03-07 15:13:24 +1300

    add generate prefix
    Source: https://github.com/msupply-foundation/open-msupply/commit/aedb17644d7ae85871c3a036fc867434f4d35d39

--- client/packages/invoices/src/Returns/api/hooks/line/useGenerateInboundReturnLines.ts
+++ client/packages/invoices/src/Returns/api/hooks/line/useGenerateInboundReturnLines.ts
@@ -1,7 +1,9 @@
 import { useQuery } from '@openmsupply-client/common';
 import { useReturnsApi } from '../utils/useReturnsApi';
 
-export const useInboundReturnLines = (outboundShipmentLineIds: string[]) => {
+export const useGenerateInboundReturnLines = (
+  outboundShipmentLineIds: string[]
+) => {
   const api = useReturnsApi();
 
   const { data } = useQuery(api.keys.newReturns(), () =>
