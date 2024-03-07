3ed66640dd5aa748a23afa1659457778ae7331ce
roxy-dao <roxy@msupply.foundation>
2024-03-07 14:17:42 +1300

    Update master list and master list line hooks
    Source: https://github.com/msupply-foundation/open-msupply/commit/ede8eb3a8c2d123a025ab1a06fd1bd6b5737d3fd

--- client/packages/system/src/MasterList/api/hooks/line/useMasterListLines.ts
+++ client/packages/system/src/MasterList/api/hooks/line/useMasterListLines.ts
@@ -1,21 +1,16 @@
-import { useMemo } from 'react';
-import { SortUtils } from '@openmsupply-client/common';
-import { useMasterListColumns } from '../../../DetailView/columns';
-import { useMasterListFields } from '../document/useMasterListFields';
+import { useQuery, useUrlQueryParams } from '@openmsupply-client/common';
+import { useMasterListApi } from '../utils/useMasterListApi';
+import { useMasterListId } from '../document/useMasterList';
 
 export const useMasterListLines = () => {
-  const { columns, onChangeSortBy, sortBy } = useMasterListColumns();
-  const { lines } = useMasterListFields();
+  const { queryParams } = useUrlQueryParams({
+    initialSort: { key: 'name', dir: 'asc' },
+  });
 
-  const sorted = useMemo(() => {
-    const currentColumn = columns.find(({ key }) => key === sortBy.key);
-    const { getSortValue } = currentColumn ?? {};
-    return getSortValue
-      ? lines?.nodes.sort(
-          SortUtils.getColumnSorter(getSortValue, !!sortBy.isDesc)
-        )
-      : lines?.nodes;
-  }, [sortBy.key, sortBy.isDesc, lines]);
+  const api = useMasterListApi();
+  const masterListId = useMasterListId();
 
-  return { lines: sorted, sortBy, onChangeSortBy, columns };
+  return useQuery(api.keys.lines(masterListId, queryParams), () =>
+    api.get.lines(masterListId, queryParams)
+  );
 };
