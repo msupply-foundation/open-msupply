5f1a6257c917f68f6ee4ba1f30b8ccc46a7b775e
roxy-dao <roxy@msupply.foundation>
2024-03-08 17:19:29 +1300

    Refactor to zero quantities with update endpoint
    Source: https://github.com/msupply-foundation/open-msupply/commit/48a4e3005606bc2ed6eb8a48ac927e76fdd8a178

--- client/packages/invoices/src/InboundShipment/api/api.ts
+++ client/packages/invoices/src/InboundShipment/api/api.ts
@@ -287,11 +287,8 @@ export const getInboundQueries = (sdk: Sdk, storeId: string) => ({
         .map(inboundParsers.toInsertLine),
       updateInboundShipmentLines: draftInboundLine
         .filter(
-          ({ type, isCreated, isUpdated, numberOfPacks }) =>
-            !isCreated &&
-            isUpdated &&
-            type === InvoiceLineNodeType.StockIn &&
-            numberOfPacks > 0
+          ({ type, isCreated, isUpdated }) =>
+            !isCreated && isUpdated && type === InvoiceLineNodeType.StockIn
         )
         .map(inboundParsers.toUpdateLine),
       deleteInboundShipmentLines: draftInboundLine
