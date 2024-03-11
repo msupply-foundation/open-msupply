2d18a1e58664c89496e88bb4962dff1dc73d95a3
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-03-12 09:37:25 +1300

    Basic upload structure
    Source: https://github.com/msupply-foundation/open-msupply/commit/c835a743464ca6fa0314b069f076665a6675ddcd

---
+++ client/packages/coldchain/src/Equipment/ImportAsset/Upload.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Paper } from '@mui/material';
+
+export interface UploadProps {}
+
+export const Upload: FC<UploadProps> = ({}) => {
+  return <Paper>upload</Paper>;
+};
--- client/packages/coldchain/src/Equipment/ImportAsset/index.ts
+++ client/packages/coldchain/src/Equipment/ImportAsset/index.ts
@@ -1,1 +1,3 @@
 export * from './ImportAssetModal';
+export * from './UploadTab';
+export * from './Upload';
