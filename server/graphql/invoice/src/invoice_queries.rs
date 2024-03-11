2a5d5ae740ad0d0d04237548ab047c8c3cfb3913
Lache Melvin <lache.melvin@gmail.com>
2024-03-11 17:29:36 +1300

    remove dummy queries
    Source: https://github.com/msupply-foundation/open-msupply/commit/003d8bbe081e31ffb578dde339821ba9a4fa3f2b

--- server/graphql/invoice/src/invoice_queries.rs
+++ server/graphql/invoice/src/invoice_queries.rs
@@ -11,9 +11,8 @@ use graphql_core::{
 };
 use graphql_types::types::{InvoiceConnector, InvoiceNode, InvoiceNodeStatus, InvoiceNodeType};
 use repository::{
-    DatetimeFilter, EqualFilter, Invoice, InvoiceFilter, InvoiceLineRow, InvoiceLineRowRepository,
-    InvoiceRepository, InvoiceRow, InvoiceRowRepository, InvoiceSort, InvoiceSortField,
-    PaginationOption, StringFilter,
+    DatetimeFilter, EqualFilter, InvoiceFilter, InvoiceSort, InvoiceSortField, PaginationOption,
+    StringFilter,
 };
 use service::auth::{Resource, ResourceAccessRequest};
 
@@ -132,20 +131,6 @@ pub fn get_invoices(
     filter: Option<InvoiceFilterInput>,
     sort: Option<Vec<InvoiceSortInput>>,
 ) -> Result<InvoicesResponse> {
-    if let Some(inv_type) = filter.clone().map(|filter| {
-        filter
-            .r#type
-            .map(|t| t.equal_to)
-            .flatten()
-            .unwrap_or_else(|| InvoiceNodeType::InboundShipment) // ..default to something that isn't a return
-    }) {
-        if let Some(invoice) = inbound_return(ctx, &inv_type) {
-            return Ok(InvoicesResponse::Response(InvoiceConnector::from_vec(
-                vec![invoice],
-            )));
-        }
-    };
-
     let user = validate_auth(
         ctx,
         &ResourceAccessRequest {
@@ -175,72 +160,12 @@ pub fn get_invoices(
     )))
 }
 
-fn inbound_return(ctx: &Context<'_>, r#type: &InvoiceNodeType) -> Option<Invoice> {
-    if *r#type != InvoiceNodeType::InboundReturn {
-        return None;
-    }
-
-    let service_provider = ctx.service_provider();
-    let context = service_provider.basic_context().unwrap();
-
-    InvoiceRowRepository::new(&context.connection)
-        .upsert_one(&InvoiceRow {
-            id: "inbound_return_1".to_string(),
-            name_link_id: "8251E89A4B7E6D47AFA01E2805C7DDAD".to_string(),
-            name_store_id: Some("0AF30FF38285394284E5701F005BAD58".to_string()),
-            store_id: "D77F67339BF8400886D009178F4962E1".to_string(),
-            user_id: Some("0763E2E3053D4C478E1E6B6B03FEC207".to_string()),
-            invoice_number: 3,
-            ..Default::default()
-        })
-        .unwrap();
-
-    InvoiceLineRowRepository::new(&context.connection)
-        .upsert_one(&InvoiceLineRow {
-            id: "inbound_return_line1".to_string(),
-            invoice_id: "inbound_return_1".to_string(),
-            item_link_id: "E43D125F51DE4355AE1233DA449ED08A".to_string(),
-            item_name: "Amoxicillin 250mg tabs".to_string(),
-            item_code: "030453".to_string(),
-            stock_line_id: Some("68118a4f-1f4a-4469-ba35-1f30e2b1bb77".to_string()),
-            pack_size: 20,
-            number_of_packs: 1000.0,
-            ..Default::default()
-        })
-        .unwrap();
-    InvoiceLineRowRepository::new(&context.connection)
-        .upsert_one(&InvoiceLineRow {
-            id: "inbound_return_line2".to_string(),
-            invoice_id: "inbound_return_1".to_string(),
-            item_link_id: "E43D125F51DE4355AE1233DA449ED08A".to_string(),
-            item_name: "Amoxicillin 250mg tabs".to_string(),
-            item_code: "030453".to_string(),
-            stock_line_id: Some("050e7e63-d63d-4e9e-b4b1-dff288e6bd40".to_string()),
-            pack_size: 20,
-            number_of_packs: 1000.0,
-            ..Default::default()
-        })
-        .unwrap();
-
-    Some(
-        InvoiceRepository::new(&context.connection)
-            .query_one(InvoiceFilter::by_id("inbound_return_1"))
-            .unwrap()
-            .unwrap(),
-    )
-}
-
 pub fn get_invoice_by_number(
     ctx: &Context<'_>,
     store_id: String,
     invoice_number: u32,
     r#type: InvoiceNodeType,
 ) -> Result<InvoiceResponse> {
-    if r#type == InvoiceNodeType::InboundReturn {
-        if let Some(invoice) = inbound_return(ctx, &r#type) {
-            return Ok(InvoiceResponse::Response(InvoiceNode::from_domain(invoice)));
-        }
-    }
     let user = validate_auth(
         ctx,
         &ResourceAccessRequest {
