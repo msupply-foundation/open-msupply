1b99552e91b39f02e0fc259cb0f69ac0a6b5de39
Mark Prins <mark@malcolmprins.net>
2024-03-12 14:30:22 +1300

    postgres-migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/e5ffb4c65afd77d21b1e38ce8e781fe803a18a54

---
+++ server/repository/src/migrations/v1_08_00/assets/activity_log.rs
@@ -0,0 +1,17 @@
+use crate::{migrations::sql, StorageConnection};
+
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    if cfg!(feature = "postgres") {
+        sql!(
+            connection,
+            r#"
+            ALTER TYPE activity_log_type ADD VALUE 'ASSET_CREATED';
+            ALTER TYPE activity_log_type ADD VALUE 'ASSET_UPDATED';
+            ALTER TYPE activity_log_type ADD VALUE 'ASSET_DELETED';
+            ALTER TYPE activity_log_type ADD VALUE 'ASSET_LOG_CREATED';
+            "#
+        )?;
+    }
+
+    Ok(())
+}
--- server/repository/src/migrations/v1_08_00/assets/mod.rs
+++ server/repository/src/migrations/v1_08_00/assets/mod.rs
@@ -1,5 +1,6 @@
 use crate::StorageConnection;
 
+pub mod activity_log;
 pub mod asset;
 pub mod asset_catalogue_data;
 pub mod asset_catalogue_item;
@@ -12,5 +13,6 @@ pub(crate) fn migrate_assets(connection: &StorageConnection) -> anyhow::Result<(
     asset_catalogue_data::migrate(connection)?;
     asset::migrate(connection)?;
     asset_log::migrate(connection)?;
+    activity_log::migrate(connection)?;
     Ok(())
 }
