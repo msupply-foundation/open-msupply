0a0fa03751cbb6b3357b0f6c95dbd960d9f6d480
Mark Prins <mark@malcolmprins.net>
2024-03-12 18:22:53 +1300

    use view for the latest log
    Source: https://github.com/msupply-foundation/open-msupply/commit/741036573eb06e33e5e342ce90d8a1ea66867b43

--- server/repository/src/migrations/v1_08_00/assets/mod.rs
+++ server/repository/src/migrations/v1_08_00/assets/mod.rs
@@ -4,6 +4,7 @@ pub mod asset;
 pub mod asset_catalogue_data;
 pub mod asset_catalogue_item;
 pub mod asset_log;
+pub mod latest_asset_log;
 pub mod reference_data;
 
 pub(crate) fn migrate_assets(connection: &StorageConnection) -> anyhow::Result<()> {
@@ -12,5 +13,6 @@ pub(crate) fn migrate_assets(connection: &StorageConnection) -> anyhow::Result<(
     asset_catalogue_data::migrate(connection)?;
     asset::migrate(connection)?;
     asset_log::migrate(connection)?;
+    latest_asset_log::migrate(connection)?;
     Ok(())
 }
