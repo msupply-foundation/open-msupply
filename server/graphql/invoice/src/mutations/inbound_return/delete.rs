4bb07f0533540c03133b6ce205d930959687c2a1
Lache Melvin <lache.melvin@gmail.com>
2024-03-14 11:21:19 +1300

    update delete graphql layer
    Source: https://github.com/msupply-foundation/open-msupply/commit/074bfd77b125bfab5045f2e376b709f499a2b461

--- server/graphql/invoice/src/mutations/inbound_return/delete.rs
+++ server/graphql/invoice/src/mutations/inbound_return/delete.rs
@@ -1,15 +1,14 @@
 use async_graphql::*;
 use graphql_core::simple_generic_errors::CannotEditInvoice;
 use graphql_core::simple_generic_errors::RecordNotFound;
+use graphql_core::standard_graphql_error::validate_auth;
+use graphql_core::standard_graphql_error::StandardGraphqlError;
+use graphql_core::ContextExt;
 use graphql_types::generic_errors::CannotDeleteInvoiceWithLines;
 use graphql_types::types::DeleteResponse as GenericDeleteResponse;
-
-#[derive(InputObject)]
-#[graphql(name = "DeleteInboundReturnInput")]
-pub struct DeleteInput {
-    // TODO: convert to accept singular ID
-    pub ids: Vec<String>,
-}
+use service::auth::Resource;
+use service::auth::ResourceAccessRequest;
+use service::invoice::inbound_return::delete::DeleteInboundReturnError as ServiceError;
 
 #[derive(SimpleObject)]
 #[graphql(name = "DeleteInboundReturnError")]
@@ -24,10 +23,35 @@ pub enum DeleteResponse {
     Response(GenericDeleteResponse),
 }
 
-pub fn delete(_ctx: &Context<'_>, _store_id: &str, _input: DeleteInput) -> Result<DeleteResponse> {
-    Ok(DeleteResponse::Response(GenericDeleteResponse(
-        "deleted_id".to_string(),
-    )))
+pub fn delete(ctx: &Context<'_>, store_id: &str, id: String) -> Result<DeleteResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            // resource: Resource::MutateInboundReturn, // TODO
+            resource: Resource::MutateOutboundShipment,
+            store_id: Some(store_id.to_string()),
+        },
+    )?;
+
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    map_response(
+        service_provider
+            .invoice_service
+            .delete_inbound_return(&service_context, id),
+    )
+}
+
+pub fn map_response(from: Result<String, ServiceError>) -> Result<DeleteResponse> {
+    let result = match from {
+        Ok(id) => DeleteResponse::Response(GenericDeleteResponse(id)),
+        Err(error) => DeleteResponse::Error(DeleteError {
+            error: map_error(error)?,
+        }),
+    };
+
+    Ok(result)
 }
 
 #[derive(Interface)]
@@ -37,4 +61,23 @@ pub enum DeleteErrorInterface {
     RecordNotFound(RecordNotFound),
     CannotEditInvoice(CannotEditInvoice),
     CannotDeleteInvoiceWithLines(CannotDeleteInvoiceWithLines),
+}
+
+fn map_error(error: ServiceError) -> Result<DeleteErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Standard Graphql Errors
+        ServiceError::InvoiceDoesNotExist
+        | ServiceError::CannotEditFinalised
+        | ServiceError::NotAnInboundReturn
+        | ServiceError::NotThisStoreInvoice => BadUserInput(formatted_error),
+
+        ServiceError::DatabaseError(_) | ServiceError::LineDeleteError { .. } => {
+            InternalError(formatted_error)
+        }
+    };
+
+    Err(graphql_error.extend())
 }
