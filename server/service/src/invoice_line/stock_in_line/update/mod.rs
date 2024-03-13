2bc15c37ca9a041b2c4a9c4272f0b63965d2da50
Lache Melvin <lache.melvin@gmail.com>
2024-03-14 09:28:58 +1300

    add note to update stock in
    Source: https://github.com/msupply-foundation/open-msupply/commit/531a286184fcf8aa02e119565aeb11fed4f037a8

--- server/service/src/invoice_line/stock_in_line/update/mod.rs
+++ server/service/src/invoice_line/stock_in_line/update/mod.rs
@@ -24,6 +24,7 @@ pub struct UpdateStockInLine {
     pub location: Option<NullableUpdate<String>>,
     pub pack_size: Option<u32>,
     pub batch: Option<String>,
+    pub note: Option<String>,
     pub cost_price_per_pack: Option<f64>,
     pub sell_price_per_pack: Option<f64>,
     pub expiry_date: Option<NaiveDate>,
