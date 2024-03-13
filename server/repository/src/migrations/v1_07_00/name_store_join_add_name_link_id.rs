8dbeaf0e743f5201c06a034e0f1ab13378a35d87
roxy-dao <roxy@msupply.foundation>
2024-03-14 11:42:40 +1300

    Update trigger

--- server/repository/src/migrations/v1_07_00/name_store_join_add_name_link_id.rs
+++ server/repository/src/migrations/v1_07_00/name_store_join_add_name_link_id.rs
@@ -39,15 +39,15 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
         CREATE TRIGGER name_store_join_insert_trigger
           AFTER INSERT ON name_store_join
           BEGIN
-            INSERT INTO changelog (table_name, record_id, row_action, store_id, is_sync_update)
-              VALUES ('name_store_join', NEW.id, 'UPSERT', NEW.store_id, NEW.is_sync_update);
+            INSERT INTO changelog (table_name, record_id, row_action, store_id, name_link_id, is_sync_update)
+              VALUES ('name_store_join', NEW.id, 'UPSERT', NEW.store_id, NEW.name_link_id, NEW.is_sync_update);
           END;
 
         CREATE TRIGGER name_store_join_update_trigger
           AFTER UPDATE ON name_store_join
           BEGIN
-            INSERT INTO changelog (table_name, record_id, row_action, store_id, is_sync_update)
-              VALUES ('name_store_join', NEW.id, 'UPSERT', NEW.store_id, NEW.is_sync_update);
+            INSERT INTO changelog (table_name, record_id, row_action, store_id, name_link_id, is_sync_update)
+              VALUES ('name_store_join', NEW.id, 'UPSERT', NEW.store_id, NEW.name_link_id, NEW.is_sync_update);
           END;
         "#
     )?;
