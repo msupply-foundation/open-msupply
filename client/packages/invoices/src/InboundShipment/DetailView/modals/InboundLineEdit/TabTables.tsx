9073ae61a5e83081498ed015a37a44a1e8da2fd2
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-03-15 09:11:07 +1300

    Revert "Add min=0 to Inbound line pack count"
    This reverts commit c17a5a5aa8322afb514c174caeaa6393431879c9.
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/531c9ce014d67c525a56b7b25fcb9e1f5414d271

--- client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/TabTables.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/TabTables.tsx
@@ -68,7 +68,6 @@ const NumberOfPacksCell: React.FC<CellProps<DraftInboundLine>> = ({
 }) => (
   <NumberInputCell
     {...props}
-    min={0}
     isRequired={rowData.numberOfPacks === 0}
     rowData={rowData}
   />
