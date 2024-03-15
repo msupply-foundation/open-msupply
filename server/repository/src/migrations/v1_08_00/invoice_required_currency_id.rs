2b3ab8028b36638c149c3d1439a87dbc1c254b52
roxy-dao <roxy@msupply.foundation>
2024-03-15 14:47:45 +1300

    PR changes

--- server/repository/src/migrations/v1_08_00/invoice_required_currency_id.rs
+++ server/repository/src/migrations/v1_08_00/invoice_required_currency_id.rs
@@ -9,7 +9,7 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
             ALTER TABLE invoice RENAME COLUMN currency_id TO old_currency_id;
             ALTER TABLE invoice ADD COLUMN currency_id TEXT REFERENCES currency(id) NOT NULL;
             UPDATE invoice SET currency_id = old_currency_id;
-            UPDATE invoice SET currency_id = (SELECT id FROM currency WHERE is_home_currency = true) AND currency_id = '';
+            UPDATE invoice SET currency_id = (SELECT id FROM currency WHERE is_home_currency = true) WHERE currency_id = '';
             ALTER TABLE invoice DROP COLUMN old_currency_id;
         "#
     )?;
