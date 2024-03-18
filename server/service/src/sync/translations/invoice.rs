ed51122274d0730933791cf39eb63258f9f54c27
roxy-dao <roxy@msupply.foundation>
2024-03-18 14:56:29 +1300

    Refactor query currency in invoice translation

--- server/service/src/sync/translations/invoice.rs
+++ server/service/src/sync/translations/invoice.rs
@@ -259,10 +259,18 @@ impl SyncTranslation for InvoiceTranslation {
             format!("Unsupported invoice type: {:?}", data._type),
         ))?;
         let mapping = map_legacy(&invoice_type, &data);
-        let default_currency = CurrencyRepository::new(connection)
-            .query_by_filter(CurrencyFilter::new().is_home_currency(true))?
-            .pop()
-            .ok_or(anyhow::Error::msg("Default currency not found"))?;
+
+        let currency_id = match data.currency_id {
+            Some(currency_id) => currency_id,
+            None => {
+                CurrencyRepository::new(connection)
+                    .query_by_filter(CurrencyFilter::new().is_home_currency(true))?
+                    .pop()
+                    .ok_or(anyhow::Error::msg("Default currency not found"))?
+                    .currency_row
+                    .id
+            }
+        };
 
         let result = InvoiceRow {
             id: data.ID,
@@ -277,7 +285,7 @@ impl SyncTranslation for InvoiceTranslation {
             comment: data.comment,
             their_reference: data.their_ref,
             tax: data.tax,
-            currency_id: data.currency_id.unwrap_or(default_currency.currency_row.id),
+            currency_id: currency_id,
             currency_rate: data.currency_rate.unwrap_or(1.0),
             clinician_link_id: data.clinician_id,
 
