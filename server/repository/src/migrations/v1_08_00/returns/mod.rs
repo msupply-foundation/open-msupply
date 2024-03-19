39eb067472521c0645b2129c220178724dc37944
Lache Melvin <lache.melvin@gmail.com>
2024-03-20 11:47:20 +1300

    add returns migrations folder
    Source: https://github.com/msupply-foundation/open-msupply/commit/21c53b540c9e248aa27fe3f9a555a664457e8da6

---
+++ server/repository/src/migrations/v1_08_00/returns/mod.rs
@@ -0,0 +1,12 @@
+use crate::StorageConnection;
+
+pub mod return_context_types;
+pub mod return_invoice_types;
+pub mod return_reasons;
+
+pub(crate) fn migrate_returns(connection: &StorageConnection) -> anyhow::Result<()> {
+    return_reasons::migrate(connection)?;
+    return_invoice_types::migrate(connection)?;
+    return_context_types::migrate(connection)?;
+    Ok(())
+}
