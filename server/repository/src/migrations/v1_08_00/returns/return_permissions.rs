c68795cce9d8dd5af6092b565ab11f63c06a81a1
Lache Melvin <lache.melvin@gmail.com>
2024-03-20 11:49:42 +1300

    add return permissions migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/5505114d018aa4374e857114ec6a70af7529cf9e

---
+++ server/repository/src/migrations/v1_08_00/returns/return_permissions.rs
@@ -0,0 +1,15 @@
+use crate::{migrations::sql, StorageConnection};
+
+pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    if cfg!(feature = "postgres") {
+        sql!(
+            connection,
+            r#"
+            ALTER TYPE permission_type ADD VALUE 'OUTBOUND_RETURN_MUTATE';
+            ALTER TYPE permission_type ADD VALUE 'OUTBOUND_RETURN_QUERY';
+            "#
+        )?;
+    }
+
+    Ok(())
+}
