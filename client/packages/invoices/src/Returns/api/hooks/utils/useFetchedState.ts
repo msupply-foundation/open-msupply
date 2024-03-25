ca49be7a7413af9adda133427eb495c851799278
Lache Melvin <lache.melvin@gmail.com>
2024-03-25 16:32:33 +1300

    remove useFetchedState

--- client/packages/invoices/src/Returns/api/hooks/utils/useFetchedState.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react';
-
-export const useFetchedBufferState = <T>(input: T, isFetched: boolean) => {
-  const [state, setState] = useState<T>(input);
-
-  useEffect(() => setState(input), [isFetched]);
-
-  return [state, setState] as const;
-};
