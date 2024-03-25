4b8b99ce41e96ac2dd6f545c701934b9433ede35
Lache Melvin <lache.melvin@gmail.com>
2024-03-25 18:03:27 +1300

    Revert "remove newlines"
    This reverts commit d6e042e50eaad3b01bbe0b130797603b70aa9292.
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/eed14181613dda76cd64b20238b0ea79ba85346a

--- server/service/src/sync/test/test_data/invoice.rs
+++ server/service/src/sync/test/test_data/invoice.rs
@@ -348,6 +348,7 @@ fn transact_2_push_record() -> TestSyncOutgoingRecord {
             om_colour: None,
             tax: Some(0.0),
             clinician_id: None,
+
             currency_id: Some("AUSTRALIAN_DOLLARS".to_string()),
             currency_rate: 1.0,
         }),
@@ -569,6 +570,7 @@ fn transact_om_fields_push_record() -> TestSyncOutgoingRecord {
             om_colour: Some("SomeColour".to_string()),
             tax: Some(0.0),
             clinician_id: None,
+
             currency_id: None,
             currency_rate: 1.0,
         }),
@@ -753,6 +755,7 @@ fn inventory_addition_push_record() -> TestSyncOutgoingRecord {
             requisition_ID: None,
             linked_transaction_id: None,
             clinician_id: None,
+
             currency_id: None,
             currency_rate: 1.0
         }),
@@ -937,6 +940,7 @@ fn inventory_reduction_push_record() -> TestSyncOutgoingRecord {
             requisition_ID: None,
             linked_transaction_id: None,
             clinician_id: None,
+
             currency_id: None,
             currency_rate: 1.0,
         }),
@@ -1116,6 +1120,7 @@ fn prescription_1_push_record() -> TestSyncOutgoingRecord {
             om_colour: None,
             tax: Some(0.0),
             clinician_id: None,
+
             currency_id: None,
             currency_rate: 1.0,
         }),
