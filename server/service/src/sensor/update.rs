b4e1d728e5be8fd428d90a363b0f1841be62fcea
Andrei <andrei.evguenov@gmail.com>
2024-03-26 12:52:12 +1300

    Simplified association of breaches with logs
    Source: https://github.com/msupply-foundation/open-msupply/commit/0cc4e034a56d5474b9dfcaebec671ffd00cd52b1

--- server/service/src/sensor/update.rs
+++ server/service/src/sensor/update.rs
@@ -1,16 +1,15 @@
 use super::{
-    query::{get_sensor, get_sensor_logs_for_breach},
+    query::{get_sensor, get_sensor_logs_filter_for_breach},
     validate::check_sensor_exists,
 };
 use crate::{
     activity_log::activity_log_entry, service_provider::ServiceContext, NullableUpdate,
     SingleRecordError,
 };
-use chrono::{Duration, NaiveDateTime, NaiveTime};
+
 use repository::{
-    ActivityLogType, RepositoryError, Sensor, SensorRow, SensorRowRepository, StorageConnection,
-    TemperatureBreachRowRepository, TemperatureBreachRowType, TemperatureLog,
-    TemperatureLogRowRepository,
+    ActivityLogType, EqualFilter, RepositoryError, Sensor, SensorRow, SensorRowRepository,
+    StorageConnection, TemperatureBreachRow, TemperatureLogRepository, TemperatureLogRowRepository,
 };
 
 #[derive(PartialEq, Debug)]
@@ -60,98 +59,6 @@ pub fn update_sensor(
     Ok(sensor)
 }
 
-pub fn update_sensor_logs_for_breach(
-    connection: &StorageConnection,
-    breach_id: &String,
-) -> Result<Vec<TemperatureLog>, RepositoryError> {
-    let mut temperature_logs: Vec<TemperatureLog> = Vec::new();
-
-    let breach_result =
-        TemperatureBreachRowRepository::new(connection).find_one_by_id(breach_id)?;
-
-    if let Some(mut breach_record) = breach_result {
-        let is_cumulative_breach = (breach_record.r#type
-            == TemperatureBreachRowType::ColdCumulative)
-            | (breach_record.r#type == TemperatureBreachRowType::HotCumulative);
-        let logs = get_sensor_logs_for_breach(connection, breach_id)?; //sorted by date/time
-        let mut log_interval = 0;
-        let zero_time = NaiveTime::parse_from_str("00:00", "%H:%M").unwrap(); // hard-coded -> should always work!
-
-        if let Some(sensor) = SensorRowRepository::new(connection).find_one_by_id(&breach_record.sensor_id)? {
-            if let Some(interval) = sensor.log_interval {
-                log_interval = interval;
-            }
-        }
-        
-        if is_cumulative_breach {
-            // Update breach start/end from first/last logs if it has changed
-            if let Some(first_log) = logs.first() {
-                // If within log_interval seconds of the start of the day,
-                // use the start of the day as first log time
-                let mut first_breach_datetime = NaiveDateTime::new(first_log.temperature_log_row.datetime.date(), zero_time);
-                let first_log_datetime = first_breach_datetime + Duration::seconds(log_interval.into());
-
-                if first_log_datetime < first_log.temperature_log_row.datetime {
-                    first_breach_datetime = first_log.temperature_log_row.datetime;
-                }
-
-                // Use the first log time if it's either earlier than the calculated breach end
-                // or if the calculated breach start is less than the sensor log interval before the first log time
-                if (breach_record.start_datetime > first_breach_datetime) | (log_interval > 0) & (breach_record.start_datetime < first_breach_datetime - Duration::seconds(log_interval.into())) {
-                    log::info!(
-                        "Updating cumulative breach start for {:?} to {:?}",
-                        breach_record,
-                        first_breach_datetime
-                    );
-                    breach_record.start_datetime = first_breach_datetime;
-                    TemperatureBreachRowRepository::new(connection).upsert_one(&breach_record)?;
-                }
-            }
-            if let Some(last_log) = logs.last() {
-                // If within log_interval seconds of the end of the day,
-                // use the end of the day as last log time
-                let mut last_breach_datetime = NaiveDateTime::new(last_log.temperature_log_row.datetime.date(), zero_time) + Duration::days(1);
-                let last_log_datetime = last_breach_datetime - Duration::seconds(log_interval.into());
-
-                if last_log_datetime > last_log.temperature_log_row.datetime {
-                    last_breach_datetime = last_log.temperature_log_row.datetime;
-                }
-                
-                // Use the last log time if it's either later than the calculated breach end
-                // or if the calculated breach end is more than the sensor log interval after the last log time
-                if (breach_record.end_datetime < Some(last_breach_datetime)) | (log_interval > 0) & (breach_record.end_datetime > Some(last_breach_datetime + Duration::seconds(log_interval.into()))) {
-                    log::info!(
-                        "Updating cumulative breach end for {:?} to {:?}",
-                        breach_record,
-                        last_breach_datetime
-                    );
-                    breach_record.end_datetime = Some(last_breach_datetime);
-                    TemperatureBreachRowRepository::new(connection).upsert_one(&breach_record)?;
-                }
-            }
-        }
-
-        for mut temperature_log in logs {
-            if let Some(_breach_id) = &temperature_log.temperature_log_row.temperature_breach_id {
-                if is_cumulative_breach {
-                    // Skip as cumulative breach can only update unassigned temperature logs
-                    continue;
-                }
-            };
-
-            temperature_log.temperature_log_row.temperature_breach_id = Some(breach_id.to_string());
-            TemperatureLogRowRepository::new(connection)
-                .upsert_one(&temperature_log.temperature_log_row)?;
-            temperature_logs.push(temperature_log.clone());
-        }
-        log::info!("Temperature logs assigned for breach {:?}", breach_record);
-
-        Ok(temperature_logs)
-    } else {
-        Err(RepositoryError::NotFound)
-    }
-}
-
 pub fn validate(
     connection: &StorageConnection,
     store_id: &str,
@@ -190,6 +97,27 @@ pub fn generate(
     sensor_row.log_interval = log_interval.or(sensor_row.log_interval);
     sensor_row.battery_level = battery_level.or(sensor_row.battery_level);
     sensor_row
+}
+
+pub fn update_sensor_logs_for_breach(
+    connection: &StorageConnection,
+    breach: &TemperatureBreachRow,
+) -> Result<(), RepositoryError> {
+    let Some(temperature_log_filter) = get_sensor_logs_filter_for_breach(&breach) else {
+        // End time is not set on breach
+        return Ok(());
+    };
+
+    // And temperature log is not associated with any breaches
+    let temperature_log_filter =
+        temperature_log_filter.temperature_breach_id(EqualFilter::is_null(true));
+
+    let logs =
+        TemperatureLogRepository::new(&connection).query_by_filter(temperature_log_filter)?;
+
+    let log_ids = logs.into_iter().map(|l| l.temperature_log_row.id).collect();
+
+    TemperatureLogRowRepository::new(&connection).update_breach_id(&breach.id, &log_ids)
 }
 
 impl From<RepositoryError> for UpdateSensorError {
