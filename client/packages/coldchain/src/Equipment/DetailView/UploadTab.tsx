2ede2987412335985f911d6e4b5326f72e57489e
Mark Prins <mark@malcolmprins.net>
2024-03-27 11:57:19 +1300

    add upload to status update

--- client/packages/coldchain/src/Equipment/DetailView/UploadTab.tsx
+++ client/packages/coldchain/src/Equipment/DetailView/UploadTab.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { AssetLogPanel } from '../Components';
+import { Upload } from '@common/components';
 
-export const UploadTab = ({ draft, value }: AssetLogPanel) => {
+const FileList = ({ files }: { files?: File[] }) =>
+  files === undefined ? null : (
+    <div>{files?.map((file, i) => <div key={i}>{file.name}</div>)}</div>
+  );
+
+export const UploadTab = ({ draft, onChange, value }: AssetLogPanel) => {
+  const onUpload = (files: File[]) => {
+    onChange({ files });
+  };
+
   return (
-    <AssetLogPanel value={value} draft={draft}>
-      TODO: implement the upload panel
+    <AssetLogPanel value={value} draft={draft} onChange={onChange}>
+      <Upload onUpload={onUpload} />
+      <FileList files={draft.files} />
     </AssetLogPanel>
   );
 };
