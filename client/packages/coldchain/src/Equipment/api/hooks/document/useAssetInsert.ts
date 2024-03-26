ba3d9a25dcc1f512c743e0bc59f7507b4253cd1a
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-03-26 16:50:03 +1300

    Update generated files

--- client/packages/coldchain/src/Equipment/api/hooks/document/useAssetInsert.ts
+++ client/packages/coldchain/src/Equipment/api/hooks/document/useAssetInsert.ts
@@ -1,13 +1,14 @@
 import { useMutation, useQueryClient } from '@openmsupply-client/common';
 import { useAssetApi } from '../utils/useAssetApi';
 import { AssetFragment } from '../../operations.generated';
+import { LocationIds } from '../../../DetailView';
 
 export const useAssetInsert = () => {
   const queryClient = useQueryClient();
   const api = useAssetApi();
 
   return useMutation(
-    async (asset: Partial<AssetFragment>) => api.insert(asset),
+    async (asset: Partial<AssetFragment & LocationIds>) => api.insert(asset),
     {
       onSettled: () => queryClient.invalidateQueries(api.keys.base()),
       onError: e => {
