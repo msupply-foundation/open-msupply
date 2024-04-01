20fac01c2515769ced28f8561c09d56e55bb2833
roxy-dao <roxy@msupply.foundation>
2024-04-02 08:40:52 +1300

    Revert default currency id
    Source: https://github.com/msupply-foundation/open-msupply/commit/1a0c5729adb5e88748e46419b4e76ec7e023cef1

--- server/repository/src/db_diesel/invoice_row.rs
+++ server/repository/src/db_diesel/invoice_row.rs
@@ -39,7 +39,7 @@ table! {
         requisition_id -> Nullable<Text>,
         linked_invoice_id -> Nullable<Text>,
         tax -> Nullable<Double>,
-        currency_id -> Text,
+        currency_id -> Nullable<Text>,
         currency_rate -> Double,
         clinician_link_id -> Nullable<Text>,
     }
@@ -107,7 +107,7 @@ pub struct InvoiceRow {
     pub requisition_id: Option<String>,
     pub linked_invoice_id: Option<String>,
     pub tax: Option<f64>,
-    pub currency_id: String,
+    pub currency_id: Option<String>,
     pub currency_rate: f64,
     pub clinician_link_id: Option<String>,
 }
