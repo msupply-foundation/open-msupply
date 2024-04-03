39cfa60c849ac6be158d58078ade06f8d1071e6f
Lache Melvin <lache.melvin@gmail.com>
2024-04-04 08:24:56 +1300

    wip update inputs
    Source: https://github.com/msupply-foundation/open-msupply/commit/547754d250e39243d65e5ce5ed93160c5290c347

--- client/packages/system/src/Stock/Components/InventoryAdjustmentForm.tsx
+++ client/packages/system/src/Stock/Components/InventoryAdjustmentForm.tsx
@@ -1,17 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
   TextWithLabelRow,
   InputWithLabelRow,
   InputWithLabelRowProps,
   useTranslation,
-  useDebounceCallback,
+  // useDebounceCallback,
   Box,
   BasicTextInput,
   Autocomplete,
+  NumericTextInput,
 } from '@openmsupply-client/common';
 import { StockLineRowFragment } from '../api';
 import {
   Adjustment,
+  InventoryAdjustmentReasonRowFragment,
   InventoryAdjustmentReasonSearchInput,
   usePackVariant,
 } from '../..';
@@ -36,9 +38,20 @@ interface InventoryAdjustmentFormProps {
 }
 export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
   draft,
-  onUpdate,
+  // onUpdate,
 }) => {
   const t = useTranslation('inventory');
+
+  const [direction, setDirection] = useState<Adjustment>(Adjustment.None);
+  const [state, setState] = useState<{
+    reason: InventoryAdjustmentReasonRowFragment | null;
+    adjustBy: number;
+    newNumPacks: number;
+  }>({
+    reason: null,
+    adjustBy: 0,
+    newNumPacks: draft.totalNumberOfPacks,
+  });
 
   const { asPackVariant } = usePackVariant(
     draft.itemId,
@@ -46,11 +59,16 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
   );
   const packUnit = asPackVariant(draft.packSize);
 
-  const debouncedUpdate = useDebounceCallback(
-    (patch: Partial<StockLineRowFragment>) => onUpdate(patch),
-    [onUpdate],
-    500
-  );
+  const options = [
+    { label: t('label.addition'), value: Adjustment.Addition },
+    { label: t('label.reduction'), value: Adjustment.Reduction },
+  ];
+
+  // const debouncedUpdate = useDebounceCallback(
+  //   (patch: Partial<StockLineRowFragment>) => onUpdate(patch),
+  //   [onUpdate],
+  //   500
+  // );
 
   return (
     <Box display="flex">
@@ -65,6 +83,8 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
           Input={
             <Box display="flex" style={{ width: 160 }}>
               <Autocomplete
+                options={options}
+                clearable={false}
                 renderInput={p => (
                   <BasicTextInput
                     {...p}
@@ -74,29 +94,26 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
                     }}
                   />
                 )}
-                options={[
-                  { label: t('label.addition'), value: Adjustment.Addition },
-                  { label: t('label.reduction'), value: Adjustment.Reduction },
-                ]}
-                // value={draft.adjustment}
-                // onChange={(_, value) => debouncedUpdate({ adjustment: value })}
+                value={
+                  options.find(option => option.value === direction) ?? null
+                }
+                // TODO: on change, update new num packs, adjust by to max
+                onChange={(_, direction) =>
+                  direction && setDirection(direction.value)
+                }
               />
             </Box>
           }
-        />{' '}
+        />
         <StyledInputRow
           label={t('label.reason')}
           Input={
             <Box display="flex" style={{ width: 160 }}>
               <InventoryAdjustmentReasonSearchInput
                 width={160}
-                onChange={
-                  () => {}
-                  // reason =>
-                  // debouncedUpdate({ barcode: e.target.value })
-                }
-                value={null}
-                adjustment={Adjustment.Addition}
+                onChange={reason => setState(state => ({ ...state, reason }))}
+                value={state.reason}
+                adjustment={direction}
               />
             </Box>
           }
@@ -118,23 +135,60 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
         <StyledInputRow
           label={t('label.adjust-by')}
           Input={
-            <Box display="flex" style={{ width: 160 }}>
-              <BasicTextInput
-                defaultValue={draft.barcode ?? ''}
-                onChange={e => debouncedUpdate({ barcode: e.target.value })}
-              />
-            </Box>
+            <NumericTextInput
+              disabled={direction === Adjustment.None}
+              width={160}
+              max={
+                direction === Adjustment.Reduction
+                  ? draft.totalNumberOfPacks
+                  : undefined
+              }
+              value={state.adjustBy}
+              onChange={adjustBy =>
+                setState(state => ({
+                  ...state,
+                  adjustBy: adjustBy ?? 0,
+                  newNumPacks:
+                    direction === Adjustment.Addition
+                      ? draft.totalNumberOfPacks + (adjustBy ?? 0)
+                      : draft.totalNumberOfPacks - (adjustBy ?? 0),
+                }))
+              }
+            />
           }
         />
         <StyledInputRow
           label={t('label.new-num-packs')}
           Input={
-            <Box display="flex" style={{ width: 160 }}>
-              <BasicTextInput
-                defaultValue={draft.barcode ?? ''}
-                onChange={e => debouncedUpdate({ barcode: e.target.value })}
-              />
-            </Box>
+            // TODO max based on direction!
+            <NumericTextInput
+              disabled={direction === Adjustment.None}
+              width={160}
+              value={state.newNumPacks}
+              // TODO: is this weird UX? instead have it change the direction??
+              // LOL NOT THIS - maybe error state! (disable ok? or show error message)
+              // reset to max on lose focus?
+              max={
+                direction === Adjustment.Reduction
+                  ? draft.totalNumberOfPacks
+                  : undefined
+              }
+              min={
+                direction === Adjustment.Addition
+                  ? draft.totalNumberOfPacks
+                  : undefined
+              }
+              onChange={newNumPacks =>
+                // tODO: onchange, udpate adjustby
+                setState(state => ({
+                  ...state,
+                  newNumPacks: newNumPacks ?? 0,
+                  adjustBy: Math.abs(
+                    draft.totalNumberOfPacks - (newNumPacks ?? 0)
+                  ),
+                }))
+              }
+            />
           }
         />
       </Box>
