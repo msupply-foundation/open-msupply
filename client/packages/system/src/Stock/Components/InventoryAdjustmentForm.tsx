76376f597358aaf000395be5e1036e041309d8dc
Lache Melvin <lache.melvin@gmail.com>
2024-04-03 17:42:21 +1300

    add fields

--- client/packages/system/src/Stock/Components/InventoryAdjustmentForm.tsx
+++ client/packages/system/src/Stock/Components/InventoryAdjustmentForm.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import {
-  Grid,
   TextWithLabelRow,
   InputWithLabelRow,
   InputWithLabelRowProps,
@@ -8,9 +7,14 @@ import {
   useDebounceCallback,
   Box,
   BasicTextInput,
+  Autocomplete,
 } from '@openmsupply-client/common';
 import { StockLineRowFragment } from '../api';
-import { usePackVariant } from '../..';
+import {
+  Adjustment,
+  InventoryAdjustmentReasonSearchInput,
+  usePackVariant,
+} from '../..';
 
 const StyledInputRow = ({ label, Input }: InputWithLabelRowProps) => (
   <InputWithLabelRow
@@ -49,40 +53,72 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
   );
 
   return (
-    <Grid
-      display="flex"
-      flex={1}
-      container
-      paddingTop={2}
-      paddingBottom={2}
-      width="100%"
-    >
-      <Grid
-        container
+    <Box display="flex">
+      <Box display="flex" flexDirection="column" padding={2} gap={1} flex={1}>
+        <TextWithLabelRow
+          label={t('label.pack')}
+          text={String(packUnit)}
+          textProps={{ textAlign: 'end' }}
+        />
+        <StyledInputRow
+          label={t('label.direction')}
+          Input={
+            <Box display="flex" style={{ width: 160 }}>
+              <Autocomplete
+                renderInput={p => (
+                  <BasicTextInput
+                    {...p}
+                    InputProps={{
+                      style: { width: 160 },
+                      ...p.InputProps,
+                    }}
+                  />
+                )}
+                options={[
+                  { label: t('label.addition'), value: Adjustment.Addition },
+                  { label: t('label.reduction'), value: Adjustment.Reduction },
+                ]}
+                // value={draft.adjustment}
+                // onChange={(_, value) => debouncedUpdate({ adjustment: value })}
+              />
+            </Box>
+          }
+        />{' '}
+        <StyledInputRow
+          label={t('label.reason')}
+          Input={
+            <Box display="flex" style={{ width: 160 }}>
+              <InventoryAdjustmentReasonSearchInput
+                width={160}
+                onChange={
+                  () => {}
+                  // reason =>
+                  // debouncedUpdate({ barcode: e.target.value })
+                }
+                value={null}
+                adjustment={Adjustment.Addition}
+              />
+            </Box>
+          }
+        />
+      </Box>
+      <Box
         display="flex"
-        flex={1}
-        flexBasis="50%"
         flexDirection="column"
         gap={1}
+        paddingTop={2}
+        flex={1}
       >
         <TextWithLabelRow
           label={t('label.num-packs')}
           text={String(draft.totalNumberOfPacks)}
           textProps={{ textAlign: 'end' }}
+          labelProps={{ sx: { textWrap: 'wrap' } }}
         />
-      </Grid>
-      <Grid
-        container
-        display="flex"
-        flex={1}
-        flexBasis="50%"
-        flexDirection="column"
-        gap={1}
-      >
         <StyledInputRow
-          label={t('label.barcode')}
+          label={t('label.adjust-by')}
           Input={
-            <Box display="flex" style={{ width: 162 }}>
+            <Box display="flex" style={{ width: 160 }}>
               <BasicTextInput
                 defaultValue={draft.barcode ?? ''}
                 onChange={e => debouncedUpdate({ barcode: e.target.value })}
@@ -90,12 +126,18 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
             </Box>
           }
         />
-        <TextWithLabelRow
-          label={t('label.pack')}
-          text={String(packUnit)}
-          textProps={{ textAlign: 'end' }}
+        <StyledInputRow
+          label={t('label.new-num-packs')}
+          Input={
+            <Box display="flex" style={{ width: 160 }}>
+              <BasicTextInput
+                defaultValue={draft.barcode ?? ''}
+                onChange={e => debouncedUpdate({ barcode: e.target.value })}
+              />
+            </Box>
+          }
         />
-      </Grid>
-    </Grid>
+      </Box>
+    </Box>
   );
 };
