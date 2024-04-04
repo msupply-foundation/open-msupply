4a4aed25d46107bf95ef56002e7140ce69afd96a
Lache Melvin <lache.melvin@gmail.com>
2024-04-04 16:38:30 +1300

    remove unused code
    Source: https://github.com/msupply-foundation/open-msupply/commit/a4e0323776989282cf4f12dfdf1d1c0350e7af5b

--- server/graphql/inventory_adjustment/lib.rs
+++ server/graphql/inventory_adjustment/lib.rs
@@ -1,31 +1,19 @@
 pub mod mutations;
-pub mod query;
 use async_graphql::*;
-// use graphql_types::types::RepackConnector;
 use mutations::{create_inventory_adjustment, CreateInventoryAdjustmentInput, InsertResponse};
-// use query::{get_repack, get_repacks_by_stock_line, RepackResponse};
 
 // #[derive(Default, Clone)]
 // pub struct InventoryAdjustmentQueries;
 
 // #[Object]
 // impl InventoryAdjustmentQueries {
-// pub async fn repack(
+// pub async fn inventory_adjustment(
 //     &self,
 //     ctx: &Context<'_>,
 //     store_id: String,
 //     invoice_id: String,
-// ) -> Result<RepackResponse> {
-//     get_repack(ctx, store_id, &invoice_id).await
-// }
-
-// pub async fn repacks_by_stock_line(
-//     &self,
-//     ctx: &Context<'_>,
-//     store_id: String,
-//     stock_line_id: String,
-// ) -> Result<RepackConnector> {
-//     get_repacks_by_stock_line(ctx, store_id, &stock_line_id).await
+// ) -> Result<InventoryAdjustmentResponse> {
+//     inventory_adjustment(ctx, store_id, &invoice_id).await
 // }
 // }
 
