e6ba021321b3459fb341c9b7c52fe4be6027059d
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-04-04 16:35:11 +1300

    Update latest_asset_log view to include status and reason

--- server/repository/src/migrations/v1_08_00/assets/asset_log.rs
+++ server/repository/src/migrations/v1_08_00/assets/asset_log.rs
@@ -2,28 +2,6 @@ use crate::migrations::DATETIME;
 use crate::{migrations::sql, StorageConnection};
 
 pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
-    // asset status (configurable)
-    sql!(
-        connection,
-        r#"
-        CREATE TABLE asset_log_status (
-            id TEXT NOT NULL PRIMARY KEY,
-            status TEXT NOT NULL,
-          );
-        "#,
-    )?;
-    // asset reason (configurable)
-    sql!(
-        connection,
-        r#"
-        CREATE TABLE asset_log_reason (
-            id TEXT NOT NULL PRIMARY KEY,
-            status_id TEXT NOT NULL REFERENCES asset_log_status(id)
-            reason TEXT NOT NULL,
-          );
-        "#,
-    )?;
-
     sql!(
         connection,
         r#"
@@ -31,10 +9,8 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
             id TEXT NOT NULL PRIMARY KEY,
             asset_id TEXT NOT NULL REFERENCES asset(id),
             user_id TEXT NOT NULL REFERENCES user_account(id),
-            status TEXT,
             comment TEXT,
             type TEXT,
-            reason TEXT,
             log_datetime {DATETIME} NOT NULL
           );
         "#,
