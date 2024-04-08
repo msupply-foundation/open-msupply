45c1c5a4e683420bbd323760b10c6584c534d144
Lache Melvin <lache.melvin@gmail.com>
2024-04-08 17:27:57 +1200

    disable new number of packs input
    Source: https://github.com/msupply-foundation/open-msupply/commit/1f5ed57cf845c7c26fa4f9eef65ad29822d30caa

--- client/packages/system/src/Stock/Components/InventoryAdjustment/InventoryAdjustmentForm.tsx
+++ client/packages/system/src/Stock/Components/InventoryAdjustment/InventoryAdjustmentForm.tsx
@@ -49,7 +49,7 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
 
   return (
     <Box display="flex">
-      <Box display="flex" flexDirection="column" padding={2} gap={1} flex={1}>
+      <Box display="flex" flexDirection="column" padding={2} gap={2} flex={1}>
         <TextWithLabelRow
           label={t('label.pack')}
           text={packUnit}
@@ -87,7 +87,7 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
       <Box
         display="flex"
         flexDirection="column"
-        gap={1}
+        gap={2}
         paddingTop={2}
         flex={1}
       >
@@ -126,32 +126,8 @@ export const InventoryAdjustmentForm: FC<InventoryAdjustmentFormProps> = ({
           Input={
             <NumericTextInput
               width={160}
+              disabled={true}
               value={draft.newNumberOfPacks}
-              max={
-                draft.direction === Adjustment.Reduction
-                  ? stockLine.totalNumberOfPacks
-                  : undefined
-              }
-              // // TODO: minimum new # packs when in addition mode
-              // // `min` field doesn't really work... e.g. if current/min is 5,
-              // // user wants to type 20, so starts by typing 2, it will be reset to 5!
-              // // I want a debounced min field or something lol
-              // // maybe error state? (disable ok? or show error message)
-              // // reset to min on lose focus?
-              // min={
-              //   direction === Adjustment.Addition
-              //     ? draft.totalNumberOfPacks
-              //     : undefined
-              // }
-              onChange={newNumPacks =>
-                setDraft(state => ({
-                  ...state,
-                  newNumberOfPacks: newNumPacks ?? 0,
-                  adjustBy: Math.abs(
-                    stockLine.totalNumberOfPacks - (newNumPacks ?? 0)
-                  ),
-                }))
-              }
             />
           }
         />
