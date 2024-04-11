dac4c73f6ad83f65827c1fc3dedbd7168604da49
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-04-11 14:02:38 +1200

    Table formatting, localisation

--- client/packages/system/src/Stock/Components/Ledger/useLedgerColumns.ts
+++ client/packages/system/src/Stock/Components/Ledger/useLedgerColumns.ts
@@ -2,13 +2,24 @@ import {
   ColumnFormat,
   useColumns,
   useFormatDateTime,
+  useTranslation,
 } from '@openmsupply-client/common';
-import { RepackFragment } from '../../api/operations.generated';
+import { LedgerLine } from '../../api/hooks/useStockLedger';
+
+type MovementType =
+  | 'INBOUND_SHIPMENT'
+  | 'OUTBOUND_SHIPMENT'
+  | 'INBOUND_RETURN'
+  | 'OUTBOUND_RETURN'
+  | 'PRESCRIPTION'
+  | 'INVENTORY_ADDITION'
+  | 'INVENTORY_REDUCTION';
 
 export const useLedgerColumns = () => {
+  const t = useTranslation('app');
   const { localisedTime } = useFormatDateTime();
 
-  const columns = useColumns<RepackFragment>([
+  const columns = useColumns<LedgerLine>([
     {
       key: 'datetime',
       label: 'label.date',
@@ -30,6 +41,8 @@ export const useLedgerColumns = () => {
     {
       key: 'type',
       label: 'label.type',
+      accessor: ({ rowData }) =>
+        t(getLocalisationKey(rowData.type as MovementType)),
     },
     {
       key: 'reason',
@@ -38,4 +51,23 @@ export const useLedgerColumns = () => {
   ]);
 
   return { columns };
+};
+
+const getLocalisationKey = (type: MovementType) => {
+  switch (type) {
+    case 'INBOUND_SHIPMENT':
+      return 'inbound-shipment';
+    case 'OUTBOUND_SHIPMENT':
+      return 'outbound-shipment';
+    case 'INBOUND_RETURN':
+      return 'inbound-return';
+    case 'OUTBOUND_RETURN':
+      return 'outbound-return';
+    case 'PRESCRIPTION':
+      return 'prescription';
+    case 'INVENTORY_ADDITION':
+      return 'inventory-addition';
+    case 'INVENTORY_REDUCTION':
+      return 'inventory-reduction';
+  }
 };
--- client/packages/system/src/Stock/api/hooks/useStockLedger.ts
+++ client/packages/system/src/Stock/api/hooks/useStockLedger.ts
@@ -3,9 +3,9 @@ import { StockLineRowFragment } from '../../..';
 import { LEDGER } from './keys';
 // import { useStockGraphQL } from '../useStockGraphQL';
 
-// Replace with auto-gen type from graphql codegen
+// TO-DO:Replace with auto-gen type from graphql codegen
 export interface LedgerLine {
-  invoiceLineId: string;
+  id: string;
   itemId: string;
   storeId: string;
   quantity: number;
@@ -20,9 +20,9 @@ export function useStockLedger(stockLine: StockLineRowFragment) {
 
   const queryKey = [LEDGER, stockLine.id];
   const queryFn = async () => {
-    return [
+    const exampleResult = [
       {
-        invoiceLineId: 'AAAA',
+        id: 'AAAA',
         itemId: '042B38856AB04B318FDBAEABADE932C1',
         storeId: '7F9C518F2EBE4D96A84CE25D4D2D6131',
         quantity: 100,
@@ -32,7 +32,7 @@ export function useStockLedger(stockLine: StockLineRowFragment) {
         reason: null,
       },
       {
-        invoiceLineId: 'BBBB',
+        id: 'BBBB',
         itemId: 'BBBB',
         storeId: 'BBBB',
         quantity: -546,
@@ -42,7 +42,7 @@ export function useStockLedger(stockLine: StockLineRowFragment) {
         reason: 'Lost',
       },
       {
-        invoiceLineId: 'CCCC',
+        id: 'CCCC',
         itemId: 'CCCC',
         storeId: 'CCCC',
         quantity: 100,
@@ -52,6 +52,7 @@ export function useStockLedger(stockLine: StockLineRowFragment) {
         reason: 'Found',
       },
     ] as LedgerLine[];
+    return exampleResult;
   };
 
   const query = useQuery({ queryKey, queryFn });
