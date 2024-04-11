a47d078527a332f1aff49e844c0fee86d91fb386
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-04-11 12:34:33 +1200

    Finish tests

--- server/service/src/invoice/inventory_adjustment/insert.rs
+++ server/service/src/invoice/inventory_adjustment/insert.rs
@@ -96,12 +96,13 @@ impl From<RepositoryError> for InsertInventoryAdjustmentError {
 mod test {
     use repository::{
         mock::{
-            mock_stock_line_a, mock_store_a, mock_store_b, mock_user_account_a, MockData,
-            MockDataInserts,
+            mock_stock_line_a, mock_stock_line_b, mock_store_a, mock_store_b, mock_user_account_a,
+            MockData, MockDataInserts,
         },
         test_db::{setup_all, setup_all_with_data},
-        InventoryAdjustmentReasonRow, InventoryAdjustmentReasonType, InvoiceRowRepository,
-        InvoiceRowStatus, StockLine,
+        EqualFilter, InventoryAdjustmentReasonRow, InventoryAdjustmentReasonType,
+        InvoiceRowRepository, InvoiceRowStatus, StockLineFilter, StockLineRepository,
+        StockLineRowRepository,
     };
     use util::inline_edit;
 
@@ -186,14 +187,15 @@ mod test {
                 &context,
                 InsertInventoryAdjustment {
                     stock_line_id: mock_stock_line_a().id,
+                    adjustment: 2.0,
                     inventory_adjustment_reason_id: Some("invalid".to_string()),
                     ..Default::default()
                 }
             ),
-            Err(ServiceError::InvalidAdjustment)
+            Err(ServiceError::AdjustmentReasonNotValid)
         );
 
-        // Reduce stock below zero
+        // Invalid adjustment (adjustment = 0)
         assert_eq!(
             service.insert_inventory_adjustment(
                 &context,
@@ -207,22 +209,29 @@ mod test {
         );
 
         // Reduce stock below zero
-        // assert_eq!(
-        //     service.insert_inventory_adjustment(
-        //         &context,
-        //         InsertInventoryAdjustment {
-        //             stock_line_id: mock_stock_line_a().id,
-        //             adjustment_type:
-        //                 crate::invoice::inventory_adjustment::AdjustmentType::Reduction,
-        //             adjustment: 50.0,
-        //             ..Default::default()
-        //         }
-        //     ),
-        //     Err(ServiceError::StockLineReducedBelowZero(StockLine {
-        //         stock_line_row: mock_stock_line_a(),
-        //         ..Default::default()
-        //     }))
-        // );
+        let stock_line = StockLineRepository::new(&context.connection)
+            .query_by_filter(
+                StockLineFilter::new().id(EqualFilter::equal_to(&mock_stock_line_a().id)),
+                Some(mock_store_a().id.clone()),
+            )
+            .unwrap()
+            .pop()
+            .unwrap();
+
+        assert_eq!(
+            service.insert_inventory_adjustment(
+                &context,
+                InsertInventoryAdjustment {
+                    stock_line_id: mock_stock_line_a().id,
+                    adjustment_type:
+                        crate::invoice::inventory_adjustment::AdjustmentType::Reduction,
+                    adjustment: 50.0,
+                    inventory_adjustment_reason_id: Some(reduction_reason().id),
+                    ..Default::default()
+                }
+            ),
+            Err(ServiceError::StockLineReducedBelowZero(stock_line))
+        );
     }
 
     #[actix_rt::test]
@@ -236,7 +245,19 @@ mod test {
             .unwrap();
         let service = service_provider.invoice_service;
 
-        // Success
+        // Check *no* error when reasons not defined and not provided
+        let result = service.insert_inventory_adjustment(
+            &context,
+            InsertInventoryAdjustment {
+                stock_line_id: mock_stock_line_a().id,
+                adjustment: 1.0,
+                ..Default::default()
+            },
+        );
+
+        assert!(result.is_ok());
+
+        // Positive adjustment
         let created_invoice = service
             .insert_inventory_adjustment(
                 &context,
@@ -252,6 +273,10 @@ mod test {
             .find_one_by_id(&created_invoice.invoice_row.id)
             .unwrap();
 
+        let updated_stockline = StockLineRowRepository::new(&connection)
+            .find_one_by_id(&mock_stock_line_a().id)
+            .unwrap();
+
         assert_eq!(
             retrieved_invoice,
             inline_edit(&retrieved_invoice, |mut u| {
@@ -259,6 +284,55 @@ mod test {
                 u.status = InvoiceRowStatus::Verified;
                 u
             })
+        );
+
+        assert_eq!(
+            updated_stockline.available_number_of_packs,
+            mock_stock_line_a().available_number_of_packs + 3.0
+        );
+
+        assert_eq!(
+            updated_stockline.total_number_of_packs,
+            mock_stock_line_a().total_number_of_packs + 3.0
+        );
+
+        // Negative adjustment
+        let created_invoice = service
+            .insert_inventory_adjustment(
+                &context,
+                InsertInventoryAdjustment {
+                    stock_line_id: mock_stock_line_b().id,
+                    adjustment: -10.5,
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+        let retrieved_invoice = InvoiceRowRepository::new(&connection)
+            .find_one_by_id(&created_invoice.invoice_row.id)
+            .unwrap();
+
+        let updated_stockline = StockLineRowRepository::new(&connection)
+            .find_one_by_id(&mock_stock_line_b().id)
+            .unwrap();
+
+        assert_eq!(
+            retrieved_invoice,
+            inline_edit(&retrieved_invoice, |mut u| {
+                u.id = created_invoice.invoice_row.id;
+                u.status = InvoiceRowStatus::Verified;
+                u
+            })
+        );
+
+        assert_eq!(
+            updated_stockline.available_number_of_packs,
+            mock_stock_line_b().available_number_of_packs - 10.5
+        );
+
+        assert_eq!(
+            updated_stockline.total_number_of_packs,
+            mock_stock_line_b().total_number_of_packs - 10.5
         );
     }
 }
