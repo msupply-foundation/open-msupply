8ea22ce2794731388bade1d70559d951ec13b2ee
Mark Prins <mark@malcolmprins.net>
2024-04-11 15:54:37 +1200

    infinite scroll
    Source: https://github.com/msupply-foundation/open-msupply/commit/7928a14959e89e941ced54b5c30cbd497b09255a

--- client/packages/system/src/Asset/api/hooks/document/index.ts
+++ client/packages/system/src/Asset/api/hooks/document/index.ts
@@ -2,10 +2,12 @@ import { useAsset } from './useAsset';
 import { useAssetFields } from './useAssetFields';
 import { useAssets } from './useAssets';
 import { useAssetsAll } from './useAssetsAll';
+import { useInfiniteAssets } from './useInfiniteAssets';
 
 export const Document = {
   useAsset,
   useAssetFields,
   useAssets,
   useAssetsAll,
+  useInfiniteAssets,
 };
