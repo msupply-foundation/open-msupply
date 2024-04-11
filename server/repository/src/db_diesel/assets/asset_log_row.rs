33d56a581e5a7b5ad7cf1b258231c2fe0d9d5a54
James Brunskill <james@sussol.net>
2024-04-11 16:16:25 +1200

    Use defaults when creating changelog rows

--- server/repository/src/db_diesel/assets/asset_log_row.rs
+++ server/repository/src/db_diesel/assets/asset_log_row.rs
@@ -175,7 +175,7 @@ impl<'a> AssetLogRowRepository<'a> {
             record_id: asset_log_id,
             row_action: action,
             store_id: store_id,
-            name_link_id: None,
+            ..Default::default()
         };
 
         ChangelogRepository::new(self.connection).insert(&row)
