f8fdfbb66002044839dba859724c6ba85adffd5d
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-04-12 17:41:33 +1200

    Add basic modal for creating log reason

--- client/packages/system/src/Asset/AssetLogReasons/AppBarButtons.tsx
+++ client/packages/system/src/Asset/AssetLogReasons/AppBarButtons.tsx
@@ -1,44 +1,33 @@
-// import React from 'react';
-// import {
-//   DownloadIcon,
-//   useNotification,
-//   AppBarButtonsPortal,
-//   Grid,
-//   useTranslation,
-//   FileUtils,
-//   LoadingButton,
-//   EnvUtils,
-//   Platform,
-//   InfoIcon,
-//   useIsCentralServerApi,
-//   RouteBuilder,
-//   useNavigate,
-// } from '@openmsupply-client/common';
-// import { AppRoute } from '@openmsupply-client/config';
-// import { useAssetData } from '../api';
-// import { assetCategoryListItemsToCsv } from '../utils';
+import React, { FC } from 'react';
+import {
+  AppBarButtonsPortal,
+  Grid,
+  useTranslation,
+  IconButton,
+  CloseIcon,
+} from '@openmsupply-client/common';
 
-// export const AppBarButtonsComponent = () => {
-//   const t = useTranslation(['catalogue']);
-//   const navigate = useNavigate();
+interface AppBarButtonsProps {
+  onCreate: () => void;
+}
 
-//   const isCentralServer = useIsCentralServerApi();
+export const AppBarButtonsComponent: FC<AppBarButtonsProps> = ({
+  onCreate,
+}) => {
+  const t = useTranslation('coldchain');
 
-//   const path = RouteBuilder.create(AppRoute.Catalogue)
-//     .addPart(AppRoute.Assets)
-//     .addPart(AppRoute.LogReasons)
-//     .build();
+  return (
+    <AppBarButtonsPortal>
+      <Grid container gap={1}>
+        <IconButton
+          color="primary"
+          onClick={onCreate}
+          icon={<CloseIcon />}
+          label={t('button.create-log-reason')}
+        />{' '}
+      </Grid>
+    </AppBarButtonsPortal>
+  );
+};
 
-//   return (
-//     <AppBarButtonsPortal>
-//       <Grid container gap={1}>
-//         {isCentralServer && <></>}
-//         {/* {isCentralServer && ( */}
-
-//         {/* )} */}
-//       </Grid>
-//     </AppBarButtonsPortal>
-//   );
-// };
-
-// export const AppBarButtons = React.memo(AppBarButtonsComponent);
+export const AppBarButtons = React.memo(AppBarButtonsComponent);
