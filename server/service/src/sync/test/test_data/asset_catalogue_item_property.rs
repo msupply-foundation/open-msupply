68673ba892c83af39e3ab1dc3b41e74dd209086b
Mark Prins <mark@malcolmprins.net>
2024-04-15 17:13:13 +1200

    add sync
    Source: https://github.com/msupply-foundation/open-msupply/commit/3f12237f186d452fb2f3f6da465581dd9689e41c

---
+++ server/service/src/sync/test/test_data/asset_catalogue_item_property.rs
@@ -0,0 +1,44 @@
+use repository::asset_catalogue_item_property_row::AssetCatalogueItemPropertyRow;
+use serde_json::json;
+
+use super::{TestSyncIncomingRecord, TestSyncOutgoingRecord};
+
+const TABLE_NAME: &str = "asset_catalogue_item_property";
+
+const ASSET_CATALOGUE_ITEM_PROPERTY1: (&str, &str) = (
+    "639e728b-f64b-4eef-9fd6-a1874bafb8a4",
+    r#"{
+        "id": "639e728b-f64b-4eef-9fd6-a1874bafb8a4",
+        "asset_catalogue_item_id": "0dda9346-b79f-4f0f-a375-ae778240043a",
+        "asset_catalogue_property_id": "7613ef45-6410-41dc-a50a-c8fabf80cf71",
+        "value_string": "Electricity",
+    }"#,
+);
+
+fn asset_catalogue_item_property1() -> AssetCatalogueItemPropertyRow {
+    AssetCatalogueItemPropertyRow {
+        id: ASSET_CATALOGUE_ITEM_PROPERTY1.0.to_string(),
+        catalogue_item_id: "0dda9346-b79f-4f0f-a375-ae778240043a".to_string(),
+        catalogue_property_id: "7613ef45-6410-41dc-a50a-c8fabf80cf71".to_string(),
+        value_string: Some("Electricity".to_string()),
+        value_int: None,
+        value_float: None,
+        value_bool: None,
+    }
+}
+
+pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
+    vec![TestSyncIncomingRecord::new_pull_upsert(
+        TABLE_NAME,
+        ASSET_CATALOGUE_ITEM_PROPERTY1,
+        asset_catalogue_item_property1(),
+    )]
+}
+
+pub(crate) fn test_v6_central_push_records() -> Vec<TestSyncOutgoingRecord> {
+    vec![TestSyncOutgoingRecord {
+        table_name: TABLE_NAME.to_string(),
+        record_id: ASSET_CATALOGUE_ITEM_PROPERTY1.0.to_string(),
+        push_data: json!(asset_catalogue_item_property1()),
+    }]
+}
---
+++ server/service/src/sync/translations/asset_catalogue_item_property.rs
@@ -0,0 +1,110 @@
+use repository::{
+    asset_catalogue_item_property_row::{
+        AssetCatalogueItemPropertyRow, AssetCatalogueItemPropertyRowRepository,
+    },
+    ChangelogRow, ChangelogTableName, StorageConnection, SyncBufferRow,
+};
+
+use crate::sync::translations::asset_category::AssetCategoryTranslation;
+use crate::sync::translations::asset_class::AssetClassTranslation;
+use crate::sync::translations::asset_type::AssetTypeTranslation;
+
+use super::{
+    PullTranslateResult, PushTranslateResult, SyncTranslation, ToSyncRecordTranslationType,
+};
+
+// Needs to be added to all_translators()
+#[deny(dead_code)]
+pub(crate) fn boxed() -> Box<dyn SyncTranslation> {
+    Box::new(AssetCatalogueItemPropertyTranslation)
+}
+
+struct AssetCatalogueItemPropertyTranslation;
+
+impl SyncTranslation for AssetCatalogueItemPropertyTranslation {
+    fn table_name(&self) -> &str {
+        "asset_catalogue_item_property"
+    }
+
+    fn pull_dependencies(&self) -> Vec<&str> {
+        vec![
+            AssetClassTranslation.table_name(),
+            AssetCategoryTranslation.table_name(),
+            AssetTypeTranslation.table_name(),
+        ]
+    }
+
+    fn try_translate_from_upsert_sync_record(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        Ok(PullTranslateResult::upsert(serde_json::from_str::<
+            AssetCatalogueItemPropertyRow,
+        >(&sync_record.data)?))
+    }
+
+    fn change_log_type(&self) -> Option<ChangelogTableName> {
+        Some(ChangelogTableName::AssetCatalogueItemProperty)
+    }
+
+    // Only translating and pulling from central server
+    fn should_translate_to_sync_record(
+        &self,
+        row: &ChangelogRow,
+        r#type: &ToSyncRecordTranslationType,
+    ) -> bool {
+        match r#type {
+            ToSyncRecordTranslationType::PullFromOmSupplyCentral => {
+                self.change_log_type().as_ref() == Some(&row.table_name)
+            }
+            _ => false,
+        }
+    }
+
+    fn try_translate_to_upsert_sync_record(
+        &self,
+        connection: &StorageConnection,
+        changelog: &ChangelogRow,
+    ) -> Result<PushTranslateResult, anyhow::Error> {
+        let row = AssetCatalogueItemPropertyRowRepository::new(connection)
+            .find_one_by_id(&changelog.record_id)?
+            .ok_or(anyhow::Error::msg(format!(
+                "AssetCatalogueItemProperty row ({}) not found",
+                changelog.record_id
+            )))?;
+
+        Ok(PushTranslateResult::upsert(
+            changelog,
+            self.table_name(),
+            serde_json::to_value(row)?,
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use repository::{mock::MockDataInserts, test_db::setup_all};
+
+    #[actix_rt::test]
+    async fn test_asset_catalogue_item_property_translation() {
+        use crate::sync::test::test_data::asset_catalogue_item_property as test_data;
+        let translator = AssetCatalogueItemPropertyTranslation;
+
+        let (_, connection, _, _) = setup_all(
+            "test_asset_catalogue_item_property_translation",
+            MockDataInserts::none(),
+        )
+        .await;
+
+        for record in test_data::test_pull_upsert_records() {
+            assert!(translator.should_translate_from_sync_record(&record.sync_buffer_row));
+            let translation_result = translator
+                .try_translate_from_upsert_sync_record(&connection, &record.sync_buffer_row)
+                .unwrap();
+
+            assert_eq!(translation_result, record.translated_record);
+        }
+    }
+}
