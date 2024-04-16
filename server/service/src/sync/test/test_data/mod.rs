8ddc513ecf537f8e8abd442c26a27848e1fcb390
roxy-dao <roxy@msupply.foundation>
2024-04-17 10:05:21 +1200

    Test data for currency sync deletion
    Source: https://github.com/msupply-foundation/open-msupply/commit/ab439c052b6167819a650dabfdef14d10bd577d5

--- server/service/src/sync/test/test_data/mod.rs
+++ server/service/src/sync/test/test_data/mod.rs
@@ -106,6 +106,7 @@ pub(crate) fn get_all_pull_delete_central_test_records() -> Vec<TestSyncIncoming
     test_records.append(&mut user_permission::test_pull_delete_records());
     test_records.append(&mut unit::test_pull_delete_records());
     test_records.append(&mut item::test_pull_delete_records());
+    test_records.append(&mut currency::test_pull_delete_records());
     test_records.append(&mut master_list_name_join::test_pull_delete_records());
     test_records.append(&mut report::test_pull_delete_records());
     test_records.append(&mut store::test_pull_delete_records());
