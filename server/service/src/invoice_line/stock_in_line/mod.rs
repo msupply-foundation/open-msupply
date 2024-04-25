fb1e5f34dce39c03edfbe96ba25f020a590540c0
Lache Melvin <lache.melvin@gmail.com>
2024-04-26 11:16:31 +1200

    share generate batch

--- server/service/src/invoice_line/stock_in_line/mod.rs
+++ server/service/src/invoice_line/stock_in_line/mod.rs
@@ -1,4 +1,5 @@
-use repository::InvoiceRowType;
+use repository::{InvoiceLineRow, InvoiceRowType, StockLineRow};
+use util::uuid::uuid;
 
 pub mod delete;
 pub mod insert;
@@ -20,5 +21,60 @@ impl StockInType {
             StockInType::InboundReturn => InvoiceRowType::InboundReturn,
             StockInType::InventoryAddition => InvoiceRowType::InventoryAddition,
         }
+    }
+}
+
+pub struct StockLineInput {
+    pub keep_existing_batch: bool,
+    pub store_id: String,
+    pub on_hold: bool,
+    pub barcode_id: Option<String>,
+    pub supplier_link_id: String,
+}
+
+pub fn generate_batch(
+    InvoiceLineRow {
+        stock_line_id,
+        item_link_id,
+        pack_size,
+        batch,
+        expiry_date,
+        sell_price_per_pack,
+        cost_price_per_pack,
+        number_of_packs,
+        location_id,
+        note,
+        ..
+    }: InvoiceLineRow,
+    StockLineInput {
+        keep_existing_batch,
+        store_id,
+        on_hold,
+        barcode_id,
+        supplier_link_id,
+    }: StockLineInput,
+) -> StockLineRow {
+    // Generate new id if requested via parameter or if stock_line_id is not already set on line
+    let stock_line_id = match (stock_line_id, keep_existing_batch) {
+        (Some(stock_line_id), true) => stock_line_id,
+        _ => uuid(),
+    };
+
+    StockLineRow {
+        id: stock_line_id,
+        item_link_id,
+        store_id,
+        location_id,
+        batch,
+        pack_size,
+        cost_price_per_pack,
+        sell_price_per_pack,
+        available_number_of_packs: number_of_packs,
+        total_number_of_packs: number_of_packs,
+        expiry_date,
+        note,
+        supplier_link_id: Some(supplier_link_id),
+        on_hold,
+        barcode_id,
     }
 }
