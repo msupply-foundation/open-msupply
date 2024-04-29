7d1cf5a3a4a40389fe8b4856f9766da5bf20abda
Clemens Zeidler <clemens@msupply.foundation>
2024-04-30 11:15:34 +1200

    Move pg enum migrations to v2.1.0
    Source: https://github.com/msupply-foundation/open-msupply/commit/92ca93dd82a47ace3fcd378e98a37078549dd68f

---
+++ server/repository/src/migrations/v2_01_00/mod.rs
@@ -0,0 +1,37 @@
+use super::{version::Version, Migration};
+
+use crate::StorageConnection;
+
+mod pg_enums;
+
+pub(crate) struct V2_01_00;
+
+impl Migration for V2_01_00 {
+    fn version(&self) -> Version {
+        Version::from_str("2.1.0")
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        pg_enums::migrate(connection)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+#[actix_rt::test]
+async fn migration_2_01_00() {
+    use crate::migrations::*;
+    use crate::test_db::*;
+
+    let version = V2_01_00.version();
+
+    // This test allows checking sql syntax
+    let SetupResult { connection, .. } = setup_test(SetupOption {
+        db_name: &format!("migration_{version}"),
+        version: Some(version.clone()),
+        ..Default::default()
+    })
+    .await;
+
+    assert_eq!(get_database_version(&connection), version);
+}
