f4fb81a08ecdff57d2cdc931e0944f8bb1da592c
Clemens Zeidler <clemens@msupply.foundation>
2024-04-30 11:43:14 +1200

    Add missing postgres enums

--- server/repository/src/migrations/v2_00_00/assets/asset_log.rs
+++ server/repository/src/migrations/v2_00_00/assets/asset_log.rs
@@ -2,6 +2,26 @@ use crate::migrations::DATETIME;
 use crate::{migrations::sql, StorageConnection};
 
 pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    #[cfg(feature = "postgres")]
+    sql!(
+        connection,
+        r#"
+          CREATE TYPE asset_log_status AS ENUM (
+            'NOT_IN_USE',
+            'FUNCTIONING',
+            'FUNCTIONING_BUT_NEEDS_ATTENTION',
+            'NOT_FUNCTIONING',
+            'DECOMMISSIONED'
+          );
+        "#
+    )?;
+
+    const ASSET_LOG_STATUS_ENUM_TYPE: &str = if cfg!(feature = "postgres") {
+        "asset_log_status"
+    } else {
+        "TEXT"
+    };
+
     sql!(
         connection,
         r#"
@@ -9,7 +29,7 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
             id TEXT NOT NULL PRIMARY KEY,
             asset_id TEXT NOT NULL REFERENCES asset(id),
             user_id TEXT NOT NULL REFERENCES user_account(id),
-            status TEXT,
+            status {ASSET_LOG_STATUS_ENUM_TYPE},
             reason_id TEXT REFERENCES asset_log_reason(id),
             comment TEXT,
             type TEXT,
