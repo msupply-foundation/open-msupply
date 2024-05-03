869ca28f8c204baa210c862a3849bb6868345d7a
Clemens Zeidler <clemens@msupply.foundation>
2024-05-03 14:32:49 +1200

    Use cached Postgres database templates in tests

--- server/repository/src/test_db/constants.rs
+++ server/repository/src/test_db/constants.rs
@@ -1,4 +1,7 @@
 #[allow(dead_code)]
 pub(crate) const TEST_OUTPUT_DIR: &str = "test_output";
+#[cfg(all(not(feature = "postgres"), not(feature = "memory")))]
 #[allow(dead_code)]
 pub(crate) const TEMPLATE_MARKER_FILE: &str = "___template_needs_update.marker";
+#[cfg(feature = "postgres")]
+pub(crate) const TEMPLATE_MARKER_FILE: &'static str = "___template_needs_update_pg.marker";
