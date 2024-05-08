6c9dd59fa03def4a6679f7c58bfba9d829eda8bb
roxy-dao <roxy@msupply.foundation>
2024-05-08 14:27:55 +1200

    Don't sort item unit
    Source: https://github.com/msupply-foundation/open-msupply/commit/59001368abe80d63b190c09ee2f5fdfd9a63e671

--- client/packages/system/src/Item/ListView/ListView.tsx
+++ client/packages/system/src/Item/ListView/ListView.tsx
@@ -50,6 +50,7 @@ const ItemListComponent: FC = () => {
           getUnitName: r => r.unitName || null,
         }),
         width: 130,
+        sortable: false,
       },
       [
         'stockOnHand',
