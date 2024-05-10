861794a68cdb1c2b15978ee6b70d515c9e54ee87
Lache Melvin <lache.melvin@gmail.com>
2024-05-10 12:03:25 +1200

    add hint to tooltip
    Source: https://github.com/msupply-foundation/open-msupply/commit/b89f818742b8eb4910e3e19058ff0b9bfd701d23

--- client/packages/common/src/ui/components/errors/ErrorWithDetails.tsx
+++ client/packages/common/src/ui/components/errors/ErrorWithDetails.tsx
@@ -10,28 +10,52 @@ import {
 export type ErrorWithDetailsProps = {
   error: string;
   details: string;
+  hint?: string;
 };
 
 export const ErrorWithDetails: React.FC<ErrorWithDetailsProps> = ({
   error,
   details,
-}) => (
-  <Tooltip title={details}>
-    <Box
-      display="flex"
-      sx={{ color: 'error.main' }}
-      gap={1}
-      justifyContent="center"
-    >
-      <Box display="flex" flexWrap="wrap" alignContent="center">
-        <AlertIcon />
+  hint,
+}) => {
+  const sx = { color: 'inherit', fontSize: 'inherit' };
+  const TooltipContents = () => {
+    if (!details && !hint) {
+      return null;
+    }
+
+    return (
+      <Box display="flex" flexDirection="column" gap={1}>
+        {!!hint && <Typography sx={sx}>{hint}</Typography>}
+
+        {!!details && (
+          <Box>
+            <Typography sx={sx}>Error Details:</Typography>
+            <Typography sx={sx}>{details}</Typography>
+          </Box>
+        )}
       </Box>
-      <Box sx={{ '& > div': { display: 'inline-block' } }}>
-        <Typography sx={{ color: 'inherit' }} component="span">
-          {error}
-        </Typography>
-        {details && <InfoIcon fontSize="inherit" />}
+    );
+  };
+
+  return (
+    <Tooltip title={<TooltipContents />}>
+      <Box
+        display="flex"
+        sx={{ color: 'error.main' }}
+        gap={1}
+        justifyContent="center"
+      >
+        <Box display="flex" flexWrap="wrap" alignContent="center">
+          <AlertIcon />
+        </Box>
+        <Box sx={{ '& > div': { display: 'inline-block' } }}>
+          <Typography sx={{ color: 'inherit' }} component="span">
+            {error}
+          </Typography>
+          {!!(details || hint) && <InfoIcon fontSize="inherit" />}
+        </Box>
       </Box>
-    </Box>
-  </Tooltip>
-);
+    </Tooltip>
+  );
+};
