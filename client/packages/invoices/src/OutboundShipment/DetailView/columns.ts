cffbda679c03e3d222dddd3d8892d4ea96b4bc57
roxy-dao <roxy@msupply.foundation>
2024-05-15 10:25:46 +1200

    Readjust column width
    Source: https://github.com/msupply-foundation/open-msupply/commit/72e005d3268f3b1071d23a3617854162ae0f95d6

--- client/packages/invoices/src/OutboundShipment/DetailView/columns.ts
+++ client/packages/invoices/src/OutboundShipment/DetailView/columns.ts
@@ -149,6 +149,7 @@ export const useOutboundColumns = ({
             { path: ['lines', 'location', 'code'] },
             { path: ['location', 'code'], default: '' },
           ]),
+        width: 100,
       },
     ],
   ];
