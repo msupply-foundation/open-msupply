62f6eae498f7339fc7b6e09ad5b5efe0d2cdb0f5
James Brunskill <james@sussol.net>
2024-05-16 12:48:22 +1200

    Sync asset_property
    Source: https://github.com/msupply-foundation/open-msupply/commit/068e54ba58095bb1180560bc962681488fda189f

--- server/service/src/sync/test/test_data/asset_property.rs
+++ server/service/src/sync/test/test_data/asset_property.rs
@@ -1,45 +1,47 @@
-use repository::{
-    asset_catalogue_property_row::AssetCataloguePropertyRow, types::PropertyValueType,
-};
+use repository::{asset_property_row::AssetPropertyRow, types::PropertyValueType};
 use serde_json::json;
 
 use super::{TestSyncIncomingRecord, TestSyncOutgoingRecord};
 
-const TABLE_NAME: &str = "asset_catalogue_property";
+const TABLE_NAME: &str = "asset_property";
 
-const ASSET_CATALOGUE_PROPERTY1: (&str, &str) = (
-    "854d8e25-d265-4884-aea3-8f13de3b55fb",
+const ASSET_PROPERTY1: (&str, &str) = (
+    "59d20a10-0555-4e65-9c36-2c86e50e5abc",
     r#"{
-        "id": "854d8e25-d265-4884-aea3-8f13de3b55fb",
-        "category_id": "02cbea92-d5bf-4832-863b-c04e093a7760",
-        "name": "Energy source",
+        "id": "59d20a10-0555-4e65-9c36-2c86e50e5abc",
+        "asset_category_id": "02cbea92-d5bf-4832-863b-c04e093a7760", 
+        "name": "favourite_colour",
+        "description": "Your favourite colour",
         "value_type": "STRING",
-        "allowed_values": "Electricity,Solar,Passive,Kerosene,Gas"
+        "allowed_values": "Blue,Yellow"
     }"#,
 );
 
-fn asset_catalogue_property1() -> AssetCataloguePropertyRow {
-    AssetCataloguePropertyRow {
-        id: ASSET_CATALOGUE_PROPERTY1.0.to_string(),
-        category_id: "02cbea92-d5bf-4832-863b-c04e093a7760".to_string(),
-        name: "Energy source".to_string(),
+fn asset_property1() -> AssetPropertyRow {
+    AssetPropertyRow {
+        id: ASSET_PROPERTY1.0.to_string(),
+        asset_class_id: None,
+        asset_category_id: Some("02cbea92-d5bf-4832-863b-c04e093a7760".to_string()), // Refrigerators and freezers
+        asset_type_id: None,
+        name: "favourite_colour".to_string(),
+        description: "Your favourite colour".to_string(),
         value_type: PropertyValueType::String,
-        allowed_values: Some("Electricity,Solar,Passive,Kerosene,Gas".to_string()),
+        allowed_values: Some("Blue,Yellow".to_string()),
     }
 }
 
 pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
     vec![TestSyncIncomingRecord::new_pull_upsert(
         TABLE_NAME,
-        ASSET_CATALOGUE_PROPERTY1,
-        asset_catalogue_property1(),
+        ASSET_PROPERTY1,
+        asset_property1(),
     )]
 }
 
 pub(crate) fn test_v6_central_push_records() -> Vec<TestSyncOutgoingRecord> {
     vec![TestSyncOutgoingRecord {
         table_name: TABLE_NAME.to_string(),
-        record_id: ASSET_CATALOGUE_PROPERTY1.0.to_string(),
-        push_data: json!(asset_catalogue_property1()),
+        record_id: ASSET_PROPERTY1.0.to_string(),
+        push_data: json!(asset_property1()),
     }]
 }
