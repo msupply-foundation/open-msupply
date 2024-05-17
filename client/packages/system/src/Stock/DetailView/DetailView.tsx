fdcb0cb33a7a89cd82fabfec5b965b13eea88688
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2024-05-17 16:23:03 +1200

    WIP
    Source: https://github.com/msupply-foundation/open-msupply/commit/dbddccf6370b3265f118c8d28e801adc6859a58f

--- client/packages/system/src/Stock/DetailView/DetailView.tsx
+++ client/packages/system/src/Stock/DetailView/DetailView.tsx
@@ -23,9 +23,9 @@ import {
   ActivityLogList,
   StockLineRowFragment,
 } from '@openmsupply-client/system';
-// import { Toolbar } from './Toolbar';
-// import { Footer } from './Footer';
-// import { AppBarButtons } from './AppBarButtons';
+import { Toolbar } from './Toolbar';
+import { Footer } from './Footer';
+import { AppBarButtons } from './AppBarButtons';
 // import { SidePanel } from './SidePanel';
 // import { StocktakeSummaryItem } from '../../types';
 // import { StocktakeLineEdit } from './modal/StocktakeLineEdit';
@@ -38,40 +38,6 @@ import { StockLineRowFragment, useStock, useStockLine } from '../api';
 import { StockLineForm } from '../Components/StockLineForm';
 import { InventoryAdjustmentForm } from '../Components/InventoryAdjustment';
 import { LedgerForm } from '../Components/Ledger';
-
-// const StocktakeTabs = ({
-//   id,
-//   isDisabled,
-//   onOpen,
-// }: {
-//   id: string | undefined;
-//   isDisabled: boolean;
-//   onOpen: (item?: ItemRowFragment | null | undefined) => void;
-// }) => {
-//   const onRowClick = useCallback(
-//     (item: StocktakeLineFragment | StocktakeSummaryItem) => {
-//       if (item.item) onOpen(item.item);
-//     },
-//     [onOpen]
-//   );
-
-//   const tabs = [
-//     {
-//       Component: (
-//         <ContentArea
-//           onRowClick={!isDisabled ? onRowClick : null}
-//           onAddItem={() => onOpen()}
-//         />
-//       ),
-//       value: 'Details',
-//     },
-//     {
-//       Component: <ActivityLogList recordId={id ?? ''} />,
-//       value: 'Log',
-//     },
-//   ];
-//   return <DetailTabs tabs={tabs} />;
-// };
 
 const DetailViewComponent = ({
   //   stocktake,
@@ -87,53 +53,24 @@ const DetailViewComponent = ({
   return (
     <>
       <HighlightStyles />
-      {/* <AppBarButtons onAddItem={() => onOpen()} />
+      {/* <AppBarButtons onAddItem={() => onOpen()} /> */}
 
-      <Footer />
-      <SidePanel />
+      <Footer
+        isSaving={false}
+        showSaveConfirmation={() => {}}
+        showCancelConfirmation={() => {}}
+      />
+      {/* <SidePanel /> */}
 
-      <Toolbar />
+      {/* <Toolbar /> */}
 
-      <StocktakeTabs
+      {/* <StocktakeTabs
         id={stocktake?.id}
         onOpen={onOpen}
         isDisabled={isDisabled}
       /> */}
     </>
   );
-};
-
-interface UseDraftStockLineControl {
-  draft: StockLineRowFragment;
-  onUpdate: (patch: Partial<StockLineRowFragment>) => void;
-  onSave: () => Promise<void>;
-  isLoading: boolean;
-}
-
-const useDraftStockLine = (
-  seed: StockLineRowFragment
-): UseDraftStockLineControl => {
-  const [stockLine, setStockLine] = useState<StockLineRowFragment>({ ...seed });
-  const { mutate, isLoading } = useStock.line.update();
-
-  useEffect(() => {
-    setStockLine(seed);
-  }, [seed]);
-
-  const onUpdate = (patch: Partial<StockLineRowFragment>) => {
-    const newStockLine = { ...stockLine, ...patch };
-    if (ObjUtils.isEqual(stockLine, newStockLine)) return;
-    setStockLine(newStockLine);
-  };
-
-  const onSave = async () => mutate(stockLine);
-
-  return {
-    draft: stockLine,
-    onUpdate,
-    onSave,
-    isLoading,
-  };
 };
 
 interface StockLineEditProps {
@@ -146,18 +83,20 @@ export const StockLineDetailView: React.FC<StockLineEditProps> = ({
   const { id } = useParams();
   const {
     query: { data, isLoading },
-  } = useStockLine();
-  //   const { draft, onUpdate, onSave } = useDraftStockLine(stockLine);
+    draft,
+    updatePatch,
+  } = useStockLine(id);
+  const { HighlightStyles } = useRowHighlight();
   const { dispatchEvent, addEventListener, removeEventListener } =
     usePluginEvents();
   const [hasChanged, setHasChanged] = useState(false);
 
   const t = useTranslation('inventory');
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
   const { setSuffix } = useBreadcrumbs();
 
-  React.useEffect(() => {
-    setSuffix(data?.name ?? '');
+  useEffect(() => {
+    setSuffix(data?.item.name ?? '');
   }, [data]);
 
   const plugins = usePluginElements({
@@ -165,29 +104,32 @@ export const StockLineDetailView: React.FC<StockLineEditProps> = ({
     data: stockLine,
   });
 
-  console.log('data', data);
-
   const tabs = [
-    // {
-    //   Component: (
-    //     <StockLineForm draft={draft} onUpdate={onUpdate} plugins={plugins} />
-    //   ),
-    //   value: 'label.details',
-    // },
-    // {
-    //   Component: (
-    //     <InventoryAdjustmentForm stockLine={draft} onUpdate={onUpdate} />
-    //   ),
-    //   value: 'label.adjust',
-    // },
-    // {
-    //   Component: <ActivityLogList recordId={draft?.id ?? ''} />,
-    //   value: 'label.log',
-    // },
-    // {
-    //   Component: <LedgerForm stockLine={draft} />,
-    //   value: 'label.ledger',
-    // },
+    {
+      Component: (
+        <StockLineForm
+          loading={isLoading}
+          draft={draft}
+          onUpdate={updatePatch}
+          plugins={plugins}
+        />
+      ),
+      value: t('label.details'),
+    },
+    {
+      Component: (
+        <InventoryAdjustmentForm stockLine={draft} onUpdate={updatePatch} />
+      ),
+      value: t('label.adjust'),
+    },
+    {
+      Component: <ActivityLogList recordId={data?.id ?? ''} />,
+      value: t('label.log'),
+    },
+    {
+      Component: <LedgerForm stockLine={draft} />,
+      value: t('label.ledger'),
+    },
   ];
 
   const onChange = () => setHasChanged(true);
@@ -203,46 +145,22 @@ export const StockLineDetailView: React.FC<StockLineEditProps> = ({
     return () => removeEventListener(listener);
   }, [addEventListener, removeEventListener]);
 
-  return <DetailTabs tabs={tabs} />;
-  //   <StocktakeLineErrorProvider></StocktakeLineErrorProvider>>
-  //   const { isOpen, entity, onOpen, onClose, mode } =
-  //     useEditModal<ItemRowFragment>();
+  return (
+    <>
+      <HighlightStyles />
+      <AppBarButtons onAddItem={() => {}} />
+      {/* <Toolbar /> */}
 
-  //   if (isLoading) return <DetailViewSkeleton hasGroupBy={true} hasHold={true} />;
+      <Footer
+        isSaving={false}
+        showSaveConfirmation={() => {}}
+        showCancelConfirmation={() => {}}
+      />
+      {/* <SidePanel /> */}
 
-  //   if (!stocktake?.lines || !stocktake)
-  //     return (
-  //       <AlertModal
-  //         open={true}
-  //         onOk={() =>
-  //           navigate(
-  //             RouteBuilder.create(AppRoute.Inventory)
-  //               .addPart(AppRoute.Stocktakes)
-  //               .build()
-  //           )
-  //         }
-  //         title={t('error.stocktake-not-found')}
-  //         message={t('messages.click-to-return')}
-  //       />
-  //     );
-
-  //   return (
-  //   <StocktakeLineErrorProvider>
-  //       <TableProvider createStore={createTableStore}>
-  //         <DetailViewComponent
-  //           stocktake={stocktake}
-  //           onOpen={onOpen}
-  //           isDisabled={isDisabled}
-  //         />
-  //         {isOpen && (
-  //           <StocktakeLineEdit
-  //             isOpen={isOpen}
-  //             onClose={onClose}
-  //             mode={mode}
-  //             item={entity}
-  //           />
-  //         )}
-  //       </TableProvider>
-  //   </StocktakeLineErrorProvider>
-  //   );
+      <TableProvider createStore={createTableStore}>
+        <DetailTabs tabs={tabs} />
+      </TableProvider>
+    </>
+  );
 };
---
+++ client/packages/system/src/Stock/DetailView/Toolbar.tsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import {
+  AppBarContentPortal,
+  Grid,
+  DropdownMenu,
+  DropdownMenuItem,
+  DeleteIcon,
+  useTranslation,
+  BufferedTextInput,
+  useBufferState,
+  InputWithLabelRow,
+  DateTimePickerInput,
+  Formatter,
+  SearchBar,
+  Box,
+  Switch,
+  DateUtils,
+  Alert,
+  useIsGrouped,
+  useUrlQuery,
+  RewindIcon,
+  useEditModal,
+  useNotification,
+  ArrowRightIcon,
+} from '@openmsupply-client/common';
+// import { useStocktake } from '../api';
+import { ReduceLinesToZeroConfirmationModal } from './ReduceLinesToZeroModal';
+import { ChangeLocationConfirmationModal } from './ChangeLocationModal';
+
+export const Toolbar = () => {
+  const { info } = useNotification();
+  const { isGrouped, toggleIsGrouped } = useIsGrouped('stocktake');
+  const [localIsGrouped, setLocalIsGrouped] = React.useState(isGrouped);
+  //   const isDisabled = useStocktake.utils.isDisabled();
+  //   const t = useTranslation('inventory');
+  //   const { isLocked, stocktakeDate, description, update } =
+  //     useStocktake.document.fields(['isLocked', 'description', 'stocktakeDate']);
+  //   const onDelete = useStocktake.line.deleteSelected();
+  //   const [descriptionBuffer, setDescriptionBuffer] = useBufferState(description);
+
+  //   const infoMessage = isLocked
+  //     ? t('messages.on-hold-stock-take')
+  //     : t('messages.finalised-stock-take');
+  //   const onChangeIsGrouped = () => {
+  //     setLocalIsGrouped(!localIsGrouped);
+  //     // when the render of the dependent component is slow
+  //     // separate the render of the switch change from the wider state change
+  //     // otherwise the switch doesn't render until the slow component completes
+  //     setTimeout(toggleIsGrouped, 100);
+  //   };
+  const { urlQuery, updateQuery } = useUrlQuery({
+    skipParse: ['itemCodeOrName'],
+  });
+  const itemFilter = (urlQuery['itemCodeOrName'] as string) ?? '';
+  const setItemFilter = (itemFilter: string) =>
+    updateQuery({ itemCodeOrName: itemFilter });
+
+  const reduceModal = useEditModal();
+  const changeLocationModal = useEditModal();
+
+  //   const selectedRows = useStocktake.utils.selectedRows();
+
+  //   const checkSelected = () => {
+  //     if (!selectedRows.length) {
+  //       const selectRowsSnack = info(t('messages.no-lines-selected'));
+  //       selectRowsSnack();
+  //       return;
+  //     }
+  //     if (isDisabled) {
+  //       const isLockedSnack = info(t('error.is-locked'));
+  //       isLockedSnack();
+  //       return;
+  //     }
+  //     return true;
+  //   };
+
+  //   const openReduceToZeroModal = () => {
+  //     if (checkSelected()) reduceModal.onOpen();
+  //   };
+
+  //   const openChangeLocationModal = () => {
+  //     if (checkSelected()) changeLocationModal.onOpen();
+  //   };
+
+  return (
+    <AppBarContentPortal sx={{ display: 'flex', flex: 1, marginBottom: 1 }}>
+      <Grid
+        container
+        flexDirection="row"
+        display="flex"
+        flex={1}
+        alignItems="flex-end"
+      >
+        {reduceModal.isOpen && (
+          <ReduceLinesToZeroConfirmationModal
+            isOpen={reduceModal.isOpen}
+            onCancel={reduceModal.onClose}
+          />
+        )}
+        {changeLocationModal.isOpen && (
+          <ChangeLocationConfirmationModal
+            isOpen={changeLocationModal.isOpen}
+            onCancel={changeLocationModal.onClose}
+          />
+        )}
+        <Grid item display="flex" flex={1} flexDirection="column" gap={1}>
+          {/* <InputWithLabelRow
+            label={t('heading.description')}
+            Input={
+              <BufferedTextInput
+                disabled={isDisabled}
+                size="small"
+                sx={{ width: 220 }}
+                value={descriptionBuffer ?? ''}
+                onChange={event => {
+                  setDescriptionBuffer(event.target.value);
+                  update({ description: event.target.value });
+                }}
+              />
+            }
+          />
+          <InputWithLabelRow
+            label={t('label.stocktake-date')}
+            Input={
+              <DateTimePickerInput
+                disabled={true}
+                value={DateUtils.getDateOrNull(stocktakeDate)}
+                onChange={date => {
+                  if (DateUtils.isValid(date))
+                    update({ stocktakeDate: Formatter.naiveDate(date) });
+                }}
+              />
+            }
+          />
+          {isDisabled && <Alert severity="info">{infoMessage}</Alert>}
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          gap={1}
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          <SearchBar
+            placeholder={t('placeholder.filter-items')}
+            value={itemFilter}
+            onChange={newValue => {
+              setItemFilter(newValue);
+            }}
+          />
+          <Box sx={{ marginRight: 2 }}>
+            <Switch
+              label={t('label.group-by-item')}
+              onChange={onChangeIsGrouped}
+              checked={localIsGrouped}
+              size="small"
+              color="secondary"
+            />
+          </Box>
+          <DropdownMenu disabled={isDisabled} label={t('label.actions')}>
+            <>
+              <DropdownMenuItem
+                IconComponent={ArrowRightIcon}
+                onClick={openChangeLocationModal}
+              >
+                {t('button.change-location')}
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                IconComponent={RewindIcon}
+                onClick={openReduceToZeroModal}
+              >
+                {t('button.reduce-lines-to-zero')}
+              </DropdownMenuItem>
+            </>
+            <DropdownMenuItem IconComponent={DeleteIcon} onClick={onDelete}>
+              {t('button.delete-lines')}
+            </DropdownMenuItem>
+          </DropdownMenu> */}
+        </Grid>
+      </Grid>
+    </AppBarContentPortal>
+  );
+};
