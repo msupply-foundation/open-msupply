6462df8385ba5a01646b274a28b086cf27208035
roxy-dao <roxy@msupply.foundation>
2024-05-21 08:31:57 +1200

    Change more anyhow to ignored

--- server/service/src/sync/translations/requisition.rs
+++ server/service/src/sync/translations/requisition.rs
@@ -332,12 +332,15 @@ impl SyncTranslation for RequisitionTranslation {
             name_ID: name_row.id,
             store_ID: store_id.clone(),
             r#type: to_legacy_type(&r#type),
-            status: to_legacy_status(&r#type, &status, has_outbound_shipment).ok_or(
-                anyhow::Error::msg(format!(
-                    "Unexpected row requisition status {:?} (type: {:?}), row id:{}",
-                    status, r#type, changelog.record_id
-                )),
-            )?,
+            status: match to_legacy_status(&r#type, &status, has_outbound_shipment) {
+                Some(status) => status,
+                None => {
+                    return Ok(PushTranslateResult::Ignored(format!(
+                        "Unsupported requisition status: {:?}",
+                        status
+                    )))
+                }
+            },
             om_status: Some(status),
             date_entered: date_from_date_time(&created_datetime),
             created_datetime: Some(created_datetime),
