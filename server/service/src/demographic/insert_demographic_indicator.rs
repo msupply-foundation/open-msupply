1ce74a4dc15b7e320c317ea5aba92bd5b1540b3c
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-05-21 11:09:54 +1200

    Add insert demographics
    Source: https://github.com/msupply-foundation/open-msupply/commit/8748e0429f68b59868861e4322e103e2cad7ac7d

---
+++ server/service/src/demographic/insert_demographic_indicator.rs
@@ -0,0 +1,97 @@
+use crate::{
+    activity_log::activity_log_entry, service_provider::ServiceContext, SingleRecordError,
+};
+use repository::{
+    DemographicIndicatorRow, DemographicIndicatorRowRepository, RepositoryError, StorageConnection,
+};
+
+use super::query_demographic_indicator::get_demographic_indicator;
+
+#[derive(PartialEq, Debug)]
+pub enum InsertDemographicIndicatorError {
+    AssetAlreadyExists,
+    CreatedRecordNotFound,
+    DatabaseError(RepositoryError),
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct InsertDemographicIndicator {
+    pub id: String,
+    pub name: Option<String>,
+    pub population_percentage: Option<f64>,
+    pub year_1_projection: Option<f64>,
+    pub year_2_projection: Option<f64>,
+    pub year_3_projection: Option<f64>,
+    pub year_4_projection: Option<f64>,
+    pub year_5_projection: Option<f64>,
+}
+
+pub fn insert_demographic_indicator(
+    ctx: &ServiceContext,
+    input: InsertDemographicIndicator,
+) -> Result<DemographicIndicatorRow, InsertDemographicIndicatorError> {
+    let demographic_indicator = ctx
+        .connection
+        .transaction_sync(|connection| {
+            validate(&input, connection)?;
+
+            let new_demographic_indicator = generate(input);
+            DemographicIndicatorRowRepository::new(connection)
+                .upsert_one(&new_demographic_indicator)?;
+
+            // TODO add activity log entry
+
+            get_demographic_indicator(ctx, new_demographic_indicator.id)
+                .map_err(InsertDemographicIndicatorError::from)
+        })
+        .map_err(|error| error.to_inner_error())?;
+    Ok(demographic_indicator.demographic_indicator_row)
+}
+
+pub fn validate(
+    _input: &InsertDemographicIndicator,
+    _connection: &StorageConnection,
+) -> Result<(), InsertDemographicIndicatorError> {
+    // TODO add validation functionality if requirtedd
+    Ok(())
+}
+
+pub fn generate(
+    InsertDemographicIndicator {
+        id,
+        name,
+        population_percentage,
+        year_1_projection,
+        year_2_projection,
+        year_3_projection,
+        year_4_projection,
+        year_5_projection,
+    }: InsertDemographicIndicator,
+) -> DemographicIndicatorRow {
+    DemographicIndicatorRow {
+        id,
+        name: name.unwrap_or_default(),
+        population_percentage: population_percentage.unwrap_or_default(),
+        year_1_projection: year_1_projection.unwrap_or_default(),
+        year_2_projection: year_2_projection.unwrap_or_default(),
+        year_3_projection: year_3_projection.unwrap_or_default(),
+        year_4_projection: year_4_projection.unwrap_or_default(),
+        year_5_projection: year_5_projection.unwrap_or_default(),
+    }
+}
+
+impl From<RepositoryError> for InsertDemographicIndicatorError {
+    fn from(error: RepositoryError) -> Self {
+        InsertDemographicIndicatorError::DatabaseError(error)
+    }
+}
+
+impl From<SingleRecordError> for InsertDemographicIndicatorError {
+    fn from(error: SingleRecordError) -> Self {
+        use InsertDemographicIndicatorError::*;
+        match error {
+            SingleRecordError::DatabaseError(error) => DatabaseError(error),
+            SingleRecordError::NotFound(_) => CreatedRecordNotFound,
+        }
+    }
+}
---
+++ server/service/src/demographic/insert_demographic_projection.rs
@@ -0,0 +1,91 @@
+use repository::{
+    DemographicProjectionRow, DemographicProjectionRowRepository, RepositoryError,
+    StorageConnection,
+};
+
+use crate::{service_provider::ServiceContext, SingleRecordError};
+
+use super::query_demographic_projection::get_demographic_projection;
+
+#[derive(PartialEq, Debug)]
+pub enum InsertDemographicProjectionError {
+    AssetAlreadyExists,
+    CreatedRecordNotFound,
+    DatabaseError(RepositoryError),
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct InsertDemographicProjection {
+    pub id: String,
+    pub year_1: Option<f64>,
+    pub year_2: Option<f64>,
+    pub year_3: Option<f64>,
+    pub year_4: Option<f64>,
+    pub year_5: Option<f64>,
+}
+
+pub fn insert_demographic_projection(
+    ctx: &ServiceContext,
+    input: InsertDemographicProjection,
+) -> Result<DemographicProjectionRow, InsertDemographicProjectionError> {
+    let demographic_projection = ctx
+        .connection
+        .transaction_sync(|connection| {
+            validate(&input, connection)?;
+
+            let new_demographic_projection = generate(input);
+            DemographicProjectionRowRepository::new(connection)
+                .upsert_one(&new_demographic_projection)?;
+
+            // TODO add activity log entry
+
+            get_demographic_projection(ctx, new_demographic_projection.id)
+                .map_err(InsertDemographicProjectionError::from)
+        })
+        .map_err(|error| error.to_inner_error())?;
+    Ok(demographic_projection.demographic_projection_row)
+}
+
+pub fn validate(
+    _input: &InsertDemographicProjection,
+    _connection: &StorageConnection,
+) -> Result<(), InsertDemographicProjectionError> {
+    // TODO add validation functionality if requirtedd
+    Ok(())
+}
+
+pub fn generate(
+    InsertDemographicProjection {
+        id,
+        year_1,
+        year_2,
+        year_3,
+        year_4,
+        year_5,
+    }: InsertDemographicProjection,
+) -> DemographicProjectionRow {
+    DemographicProjectionRow {
+        id,
+        year_1: year_1.unwrap_or_default(),
+        year_2: year_2.unwrap_or_default(),
+        year_3: year_3.unwrap_or_default(),
+        year_4: year_4.unwrap_or_default(),
+        year_5: year_5.unwrap_or_default(),
+    }
+}
+
+impl From<RepositoryError> for InsertDemographicProjectionError {
+    fn from(error: RepositoryError) -> Self {
+        InsertDemographicProjectionError::DatabaseError(error)
+    }
+}
+
+impl From<SingleRecordError> for InsertDemographicProjectionError {
+    fn from(error: SingleRecordError) -> Self {
+        use InsertDemographicProjectionError::*;
+        match error {
+            SingleRecordError::DatabaseError(error) => DatabaseError(error),
+            SingleRecordError::NotFound(_) => CreatedRecordNotFound,
+        }
+    }
+}
