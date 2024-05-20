00fa4beed1f8ebfa4c8e9fc838bbc2049cf3b8b8
roxy-dao <roxy@msupply.foundation>
2024-05-21 08:43:22 +1200

    improve ignored message
    Source: https://github.com/msupply-foundation/open-msupply/commit/1c36959af9024fcbeb0910dab6ba6772951b9936

--- server/service/src/sync/translations/invoice_line.rs
+++ server/service/src/sync/translations/invoice_line.rs
@@ -145,10 +145,14 @@ impl SyncTranslation for InvoiceLineTranslation {
                 }
             });
 
-
         let line_type = match to_invoice_line_type(&r#type) {
             Some(line_type) => line_type,
-            None => return Ok(PullTranslateResult::Ignored("Unsupported line type".to_string())),
+            None => {
+                return Ok(PullTranslateResult::Ignored(format!(
+                    "Unsupported line type {:?}",
+                    r#type
+                )))
+            }
         };
 
         let (item_code, tax_percentage, total_before_tax, total_after_tax) = match item_code {
