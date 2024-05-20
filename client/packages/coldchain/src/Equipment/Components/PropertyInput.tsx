c7dde910e5091ef58406724e23e244845b11e568
James Brunskill <james@sussol.net>
2024-05-20 16:40:05 +1200

    Allow negative numbers, and show auto complete for allowed values
    Source: https://github.com/msupply-foundation/open-msupply/commit/f2ba81a24333ca058438af4c69b6de461efd4222

--- client/packages/coldchain/src/Equipment/Components/PropertyInput.tsx
+++ client/packages/coldchain/src/Equipment/Components/PropertyInput.tsx
@@ -1,23 +1,29 @@
-import { BasicTextInput, NumericTextInput } from '@common/components';
+import {
+  Autocomplete,
+  BasicTextInput,
+  Checkbox,
+  NumericTextInput,
+} from '@common/components';
 import { PropertyNodeValueType } from '@common/types';
 import React, { FC } from 'react';
 
 export type PropertyInput = {
   valueType: PropertyNodeValueType;
+  allowedValues?: string[];
   value: string | number | boolean | undefined | null;
   onChange: (value: string | number | boolean | undefined) => void;
 };
 
 export const PropertyInput: FC<PropertyInput> = ({
   valueType,
+  allowedValues,
   value,
   onChange,
 }) => {
   switch (valueType) {
     case PropertyNodeValueType.Boolean:
       return (
-        <input
-          type="checkbox"
+        <Checkbox
           checked={value as boolean}
           onChange={e => onChange(e.target.checked)}
         />
@@ -29,6 +35,7 @@ export const PropertyInput: FC<PropertyInput> = ({
         <NumericTextInput
           value={valueInt ?? 0}
           fullWidth
+          allowNegative
           onChange={n => {
             onChange(n);
           }}
@@ -36,6 +43,26 @@ export const PropertyInput: FC<PropertyInput> = ({
         />
       );
     case PropertyNodeValueType.String:
+      if (allowedValues && allowedValues.length > 0) {
+        const valueOption = {
+          label: value as string,
+          id: value as string,
+          value: value as string,
+        };
+        return (
+          <Autocomplete
+            options={allowedValues.map(value => ({
+              label: value,
+              id: value,
+              value,
+            }))}
+            value={valueOption}
+            onChange={(_, value) => {
+              onChange(value?.value);
+            }}
+          />
+        );
+      }
       return (
         <BasicTextInput
           value={value}
