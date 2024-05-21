27a603f41d56a7b04a638f29445dcf1f837ddb4f
James Brunskill <james@sussol.net>
2024-05-21 18:51:44 +1200

    Fix tests (Don't create changelog records)

--- server/repository/src/migrations/v2_01_00/assets/mod.rs
+++ server/repository/src/migrations/v2_01_00/assets/mod.rs
@@ -1,4 +1,4 @@
-use crate::StorageConnection;
+use crate::{migrations::helpers::run_without_change_log_updates, StorageConnection};
 
 pub mod asset;
 pub mod asset_catalogue_data;
@@ -6,9 +6,12 @@ pub mod asset_property;
 pub mod asset_property_data;
 
 pub(crate) fn migrate_assets(connection: &StorageConnection) -> anyhow::Result<()> {
-    asset::migrate(connection)?;
-    asset_catalogue_data::migrate(connection)?;
-    asset_property::migrate(connection)?;
-    asset_property_data::migrate(connection)?;
+    run_without_change_log_updates(connection, |connection| {
+        asset::migrate(connection)?;
+        asset_catalogue_data::migrate(connection)?;
+        asset_property::migrate(connection)?;
+        asset_property_data::migrate(connection)?;
+        Ok(())
+    })?;
     Ok(())
 }
