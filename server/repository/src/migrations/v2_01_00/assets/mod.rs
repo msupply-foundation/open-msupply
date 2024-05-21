88c3fd1d0e56a89c4c46891962c24e26c28bc176
James Brunskill <james@sussol.net>
2024-05-21 18:22:03 +1200

    Cold&Freezer Rooms Data

--- server/repository/src/migrations/v2_01_00/assets/mod.rs
+++ server/repository/src/migrations/v2_01_00/assets/mod.rs
@@ -1,11 +1,13 @@
 use crate::StorageConnection;
 
 pub mod asset;
+pub mod asset_catalogue_data;
 pub mod asset_property;
 pub mod asset_property_data;
 
 pub(crate) fn migrate_assets(connection: &StorageConnection) -> anyhow::Result<()> {
     asset::migrate(connection)?;
+    asset_catalogue_data::migrate(connection)?;
     asset_property::migrate(connection)?;
     asset_property_data::migrate(connection)?;
     Ok(())
