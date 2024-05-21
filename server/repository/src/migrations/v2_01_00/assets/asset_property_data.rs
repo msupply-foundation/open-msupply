d5464bcbfee7f05197ee2aea78d94780c59ce42b
James Brunskill <james@sussol.net>
2024-05-21 18:22:03 +1200

    Cold&Freezer Rooms Data
    Source: https://github.com/msupply-foundation/open-msupply/commit/88c3fd1d0e56a89c4c46891962c24e26c28bc176

--- server/repository/src/migrations/v2_01_00/assets/asset_property_data.rs
+++ server/repository/src/migrations/v2_01_00/assets/asset_property_data.rs
@@ -5,8 +5,18 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
         r#"
-        INSERT INTO asset_property (id, key, name, value_type, allowed_values) VALUES ('external_dimensions', 'external_dimensions', 'External dimensions - WxDxH (in cm)', 'STRING', NULL);
+        INSERT INTO asset_property (id, key, name, value_type, allowed_values, asset_class_id) VALUES ('external_dimensions', 'external_dimensions', 'External dimensions - WxDxH (in cm)', 'STRING', NULL, 'fad280b6-8384-41af-84cf-c7b6b4526ef0');
         "#,
     )?;
+
+    // Properties for Cold/Freezer rooms
+
+    sql!(
+        connection,
+        r#"
+        -- TODO
+        "#,
+    )?;
+
     Ok(())
 }
