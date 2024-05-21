7752488b9daaa46e82ab3e0b2b8a0a90cb77b0ff
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-05-22 10:46:47 +1200

    Add feature flag for demographic indicators

--- client/packages/config/src/config.ts
+++ client/packages/config/src/config.ts
@@ -1,5 +1,6 @@
 declare const API_HOST: string;
 declare const FEATURE_PACK_VARIANTS: boolean;
+declare const FEATURE_DEMOGRAPHIC_INDICATORS: boolean;
 
 // For production, API is on the same domain/ip and port as web app, available through sub-route
 // i.e. web app is on https://my.openmsupply.com/, then graphql will be available https://my.openmsupply.com/graphql
@@ -44,6 +45,11 @@ export const Environment = {
     typeof FEATURE_PACK_VARIANTS === 'undefined'
       ? false
       : FEATURE_PACK_VARIANTS,
+
+  FEATURE_DEMOGRAPHIC_INDICATORS:
+    typeof FEATURE_DEMOGRAPHIC_INDICATORS === 'undefined'
+      ? false
+      : FEATURE_DEMOGRAPHIC_INDICATORS,
 };
 
 export default Environment;
