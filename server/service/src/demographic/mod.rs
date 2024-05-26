1d522c49a4df3c4335dd7cd3151cc09df72d00b6
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-05-27 11:41:13 +1200

    Add service layer mutations
    Source: https://github.com/msupply-foundation/open-msupply/commit/3b79df8b38ad145fd7b4c482016ed5ef2cc89c56

--- server/service/src/demographic/mod.rs
+++ server/service/src/demographic/mod.rs
@@ -1,4 +1,17 @@
+use self::insert_demographic_indicator::{
+    insert_demographic_indicator, InsertDemographicIndicator, InsertDemographicIndicatorError,
+};
+
+use self::insert_demographic_projection::{
+    insert_demographic_projection, InsertDemographicProjection, InsertDemographicProjectionError,
+};
 use self::query_demographic_indicator::{get_demographic_indicator, get_demographic_indicators};
+use self::update_demographic_indicator::{
+    update_demographic_indicator, UpdateDemographicIndicator, UpdateDemographicIndicatorError,
+};
+use self::update_demographic_projection::{
+    update_demographic_projection, UpdateDemographicProjection, UpdateDemographicProjectionError,
+};
 
 use super::{ListError, ListResult};
 use crate::{service_provider::ServiceContext, SingleRecordError};
@@ -6,7 +19,9 @@ use crate::{service_provider::ServiceContext, SingleRecordError};
 use repository::demographic_indicator::{
     DemographicIndicator, DemographicIndicatorFilter, DemographicIndicatorSort,
 };
-use repository::{PaginationOption, StorageConnection};
+use repository::{
+    DemographicIndicatorRow, DemographicProjectionRow, PaginationOption, StorageConnection,
+};
 
 pub mod insert_demographic_indicator;
 pub mod insert_demographic_projection;
@@ -57,6 +72,37 @@ pub trait DemographicServiceTrait: Sync + Send {
         id: String,
     ) -> Result<DemographicProjection, SingleRecordError> {
         get_demographic_projection(ctx, id)
+    }
+
+    fn insert_demographic_indicator(
+        &self,
+        ctx: &ServiceContext,
+        input: InsertDemographicIndicator,
+    ) -> Result<DemographicIndicatorRow, InsertDemographicIndicatorError> {
+        insert_demographic_indicator(ctx, input)
+    }
+    fn insert_demographic_projection(
+        &self,
+        ctx: &ServiceContext,
+        input: InsertDemographicProjection,
+    ) -> Result<DemographicProjectionRow, InsertDemographicProjectionError> {
+        insert_demographic_projection(ctx, input)
+    }
+
+    fn update_demographic_indicator(
+        &self,
+        ctx: &ServiceContext,
+        input: UpdateDemographicIndicator,
+    ) -> Result<DemographicIndicatorRow, UpdateDemographicIndicatorError> {
+        update_demographic_indicator(ctx, input)
+    }
+
+    fn update_demographic_projection(
+        &self,
+        ctx: &ServiceContext,
+        input: UpdateDemographicProjection,
+    ) -> Result<DemographicProjectionRow, UpdateDemographicProjectionError> {
+        update_demographic_projection(ctx, input)
     }
 }
 
