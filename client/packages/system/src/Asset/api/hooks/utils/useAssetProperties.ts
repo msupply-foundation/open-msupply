e71dd1e4b80710f4bbf81ff70a0ec6cbe997bfd6
James Brunskill <james@sussol.net>
2024-05-28 09:44:29 +1200

    Tweak properties filter

--- client/packages/system/src/Asset/api/hooks/utils/useAssetProperties.ts
+++ client/packages/system/src/Asset/api/hooks/utils/useAssetProperties.ts
@@ -1,9 +1,7 @@
 import { AssetPropertyFilterInput, useQuery } from '@openmsupply-client/common';
 import { useAssetApi } from './useAssetApi';
 
-export const useAssetProperties = (
-  filter: AssetPropertyFilterInput | undefined
-) => {
+export const useAssetProperties = (filter?: AssetPropertyFilterInput) => {
   const api = useAssetApi();
   return useQuery(api.keys.properties(filter), () =>
     api.get.properties(filter)
