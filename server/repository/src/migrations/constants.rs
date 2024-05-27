ae8f7a61bc47da22a760ccedc60d91cddf3fe92b
James Brunskill <james@sussol.net>
2024-05-27 19:25:04 +1200

    use constants in coldchain test

--- server/repository/src/migrations/constants.rs
+++ server/repository/src/migrations/constants.rs
@@ -1,4 +1,4 @@
-pub static COLD_CHAIN_EQUIPMENT_UUID: &str = "fad280b6-8384-41af-84cf-c7b6b4526ef0";
-pub static INSULATED_CONTAINERS_UUID: &str = "b7eea921-5a14-44cc-b5e0-ea59f2e9cb8d";
-pub static REFRIGERATORS_AND_FREEZERS_UUID: &str = "02cbea92-d5bf-4832-863b-c04e093a7760";
-pub static COLD_ROOMS_AND_FREEZER_ROOMS_UUID: &str = "7db32eb6-5929-4dd1-a5e9-01e36baa73ad";
+pub const COLD_CHAIN_EQUIPMENT_UUID: &str = "fad280b6-8384-41af-84cf-c7b6b4526ef0";
+pub const INSULATED_CONTAINERS_UUID: &str = "b7eea921-5a14-44cc-b5e0-ea59f2e9cb8d";
+pub const REFRIGERATORS_AND_FREEZERS_UUID: &str = "02cbea92-d5bf-4832-863b-c04e093a7760";
+pub const COLD_ROOMS_AND_FREEZER_ROOMS_UUID: &str = "7db32eb6-5929-4dd1-a5e9-01e36baa73ad";
