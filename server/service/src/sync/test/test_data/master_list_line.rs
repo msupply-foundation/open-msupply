233f85ca05a5b028db19e5b9c6736dd86182380a
roxy-dao <roxy@msupply.foundation>
2024-05-29 10:29:53 +1200

    fix test

--- server/service/src/sync/test/test_data/master_list_line.rs
+++ server/service/src/sync/test/test_data/master_list_line.rs
@@ -6,7 +6,7 @@ const MASTER_LIST_LINE_1: (&str, &str) = (
     "9B02D0770B544BD1AC7DB99BB85FCDD5",
     r#"{
     "ID": "9B02D0770B544BD1AC7DB99BB85FCDD5",
-    "item_master_ID": "87027C44835B48E6989376F42A58F7E3",
+    "item_master_ID": "item_query_test1",
     "item_ID": "8F252B5884B74888AAB73A0D42C09E7F",
     "imprest_quan": 0,
     "order_number": 1,
@@ -21,7 +21,7 @@ pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
         MasterListLineRow {
             id: "9B02D0770B544BD1AC7DB99BB85FCDD5".to_owned(),
             item_link_id: "8F252B5884B74888AAB73A0D42C09E7F".to_owned(),
-            master_list_id: "87027C44835B48E6989376F42A58F7E3".to_owned(),
+            master_list_id: "item_query_test1".to_owned(),
         },
     )]
 }
