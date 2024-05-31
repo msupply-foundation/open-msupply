c985859bf2db8b7a560c13f701f7196e2a56e313
roxy-dao <roxy@msupply.foundation>
2024-05-31 15:35:55 +1200

    Add test for missing master list

--- server/service/src/sync/test/test_data/master_list_line.rs
+++ server/service/src/sync/test/test_data/master_list_line.rs
@@ -1,12 +1,25 @@
-use repository::MasterListLineRow;
+use repository::{MasterListLineRow, SyncBufferRow};
+use util::inline_init;
 
-use crate::sync::test::TestSyncIncomingRecord;
+use crate::sync::{test::TestSyncIncomingRecord, translations::PullTranslateResult};
 
 const MASTER_LIST_LINE_1: (&str, &str) = (
     "9B02D0770B544BD1AC7DB99BB85FCDD5",
     r#"{
     "ID": "9B02D0770B544BD1AC7DB99BB85FCDD5",
     "item_master_ID": "item_query_test1",
+    "item_ID": "item_a",
+    "imprest_quan": 0,
+    "order_number": 1,
+    "price": 0
+  }"#,
+);
+
+const MASTER_LIST_LINE_2: (&str, &str) = (
+    "orphan",
+    r#"{
+    "ID": "9B02D0770B544BD1AC7DB99BB85FCDD5",
+    "item_master_ID": "orphan",
     "item_ID": "8F252B5884B74888AAB73A0D42C09E7F",
     "imprest_quan": 0,
     "order_number": 1,
@@ -14,14 +27,30 @@ const MASTER_LIST_LINE_1: (&str, &str) = (
   }"#,
 );
 
-pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
-    vec![TestSyncIncomingRecord::new_pull_upsert(
+fn master_list_line_a() -> TestSyncIncomingRecord {
+    TestSyncIncomingRecord::new_pull_upsert(
         "list_master_line",
         MASTER_LIST_LINE_1,
         MasterListLineRow {
             id: "9B02D0770B544BD1AC7DB99BB85FCDD5".to_owned(),
-            item_link_id: "8F252B5884B74888AAB73A0D42C09E7F".to_owned(),
+            item_link_id: "item_a".to_owned(),
             master_list_id: "item_query_test1".to_owned(),
         },
-    )]
+    )
+}
+
+fn master_list_line_b() -> TestSyncIncomingRecord {
+    TestSyncIncomingRecord {
+        translated_record: PullTranslateResult::Ignored("Missing master list".to_string()),
+        sync_buffer_row: inline_init(|r: &mut SyncBufferRow| {
+            r.table_name = "list_master_line".to_owned();
+            r.record_id = MASTER_LIST_LINE_2.0.to_owned();
+            r.data = MASTER_LIST_LINE_2.1.to_owned();
+        }),
+        extra_data: None,
+    }
+}
+
+pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
+    vec![master_list_line_a(), master_list_line_b()]
 }
