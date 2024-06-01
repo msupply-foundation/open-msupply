bcb1f405feea1ad2083550710a2c2f7c031aa701
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-01 21:02:35 +1200

    Add list view columns

--- client/packages/system/src/Immunisation/Service/Service.tsx
+++ client/packages/system/src/Immunisation/Service/Service.tsx
@@ -1,19 +1,21 @@
 import React, { FC } from 'react';
 import { RouteBuilder, Routes, Route } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
+import { ImmunisationsListView } from '../ListView';
+import { ImmunisationDetailView } from '../DetailView';
 
 export const ImmunisationService: FC = () => {
   const immunisationsRoute = RouteBuilder.create(
     AppRoute.Immunisations
   ).build();
-  //   const immunisationRoute = RouteBuilder.create(AppRoute.Immunisations)
-  //     .addPart(':id')
-  //     .build();
+  const immunisationRoute = RouteBuilder.create(AppRoute.Immunisations)
+    .addPart(':id')
+    .build();
 
   return (
     <Routes>
-      <Route path={immunisationsRoute} element={<></>} />
-      {/* <Route path={masterListRoute} element={<ImmunisationDetailView />} /> */}
+      <Route path={immunisationsRoute} element={<ImmunisationsListView />} />
+      <Route path={immunisationRoute} element={<ImmunisationDetailView />} />
     </Routes>
   );
 };
