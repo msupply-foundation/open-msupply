13774f8ddc5d16d23b6ee54bff37f0341d95ca13
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-01 21:02:35 +1200

    Add list view columns

--- client/packages/system/src/Immunisation/ListView/ListView.tsx
+++ client/packages/system/src/Immunisation/ListView/ListView.tsx
@@ -2,27 +2,59 @@ import React, { FC } from 'react';
 import {
   TableProvider,
   DataTable,
-  TableStore,
+  useColumns,
+  useUrlQueryParams,
+  useNavigate,
+  NothingHere,
+  useTranslation,
+  createTableStore,
+  createQueryParamsStore,
 } from '@openmsupply-client/common';
 import { Toolbar } from './Toolbar';
 import { AppBarButtons } from './AppBarButtons';
-import { UseBoundStore, StoreApi } from 'zustand';
 
 const ImmunisationsListComponent: FC = () => {
+  const {
+    updateSortQuery,
+    updatePaginationQuery,
+    queryParams: { sortBy, page, first, offset },
+  } = useUrlQueryParams({ filters: [{ key: 'name' }] });
+  const pagination = { page, first, offset };
+  const navigate = useNavigate();
+  const t = useTranslation('catalogue');
+
+  const columns = useColumns(
+    ['name', 'description'],
+    {
+      onChangeSortBy: updateSortQuery,
+      sortBy,
+    },
+    [updateSortQuery, sortBy]
+  );
+
   return (
     <>
       <Toolbar />
       <AppBarButtons />
-      <DataTable columns={[]} id={''} />
+      <DataTable
+        id={'immunisation-list'}
+        pagination={{ ...pagination }}
+        onChangePage={updatePaginationQuery}
+        columns={columns}
+        isLoading={false}
+        onRowClick={row => navigate(row.id)}
+        noDataElement={<NothingHere body={t('error.no-master-lists')} />}
+      />
     </>
   );
 };
 
 export const ImmunisationsListView: FC = () => (
   <TableProvider
-    createStore={function (): UseBoundStore<StoreApi<TableStore>> {
-      throw new Error('Function not implemented.');
-    }}
+    createStore={createTableStore}
+    queryParamsStore={createQueryParamsStore({
+      initialSortBy: { key: 'name' },
+    })}
   >
     <ImmunisationsListComponent />
   </TableProvider>
