fc7abbe55966f8be1d98a81371d78882cd0f7a40
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-06 10:48:10 +1200

    Add new programs nav
    Source: https://github.com/msupply-foundation/open-msupply/commit/aa3a009b59f25f4f9d871d4c85ceccd536155328

---
+++ client/packages/host/src/components/Navigation/ProgramNav.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import {
+  Collapse,
+  List,
+  useTranslation,
+  RouteBuilder,
+  AppNavLink,
+  AppNavSection,
+  useIsCentralServerApi,
+} from '@openmsupply-client/common';
+import { AppRoute } from '@openmsupply-client/config';
+import { useNestedNav } from './useNestedNav';
+import { PowerIcon } from '@openmsupply-client/common/src/ui/icons/Power';
+
+export const ProgramsNav: FC = () => {
+  const { isActive } = useNestedNav(
+    RouteBuilder.create(AppRoute.Programs).addWildCard().build()
+  );
+  const t = useTranslation('app');
+  const isCentralServer = useIsCentralServerApi();
+  const visible = isCentralServer;
+
+  return (
+    <AppNavSection isActive={isActive} to={AppRoute.Programs}>
+      <AppNavLink
+        visible={visible}
+        end={false}
+        to={AppRoute.Programs}
+        icon={<PowerIcon color="primary" fontSize="small" />}
+        text={t('programs')}
+        inactive
+      />
+      <Collapse in={isActive}>
+        <List>
+          <AppNavLink
+            visible={visible}
+            end
+            to={RouteBuilder.create(AppRoute.Programs)
+              .addPart(AppRoute.Immunisations)
+              .build()}
+            text={t('immunisations')}
+          />
+        </List>
+      </Collapse>
+    </AppNavSection>
+  );
+};
