997ddb60911d077208aadf792d697415bc431d51
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-10 11:07:17 +1200

    Add generated files for vaccine course queries and insert
    Also add new hook format
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/f385fc89bdf390802b9cd67f8515e28cf3d23072

---
+++ client/packages/system/src/Immunisation/api/hooks/useVaccineCourse.ts
@@ -0,0 +1,118 @@
+import { useState } from 'react';
+import {
+  FnUtils,
+  VaccineCourseSortFieldInput,
+  isEqual,
+  useMutation,
+  useQuery,
+} from '@openmsupply-client/common';
+import { VACCINECOURSES } from './keys';
+import { useImmunisationGraphQL } from '../useImmunisationGraphQL';
+import { VaccineCourseFragment } from '../operations.generated';
+
+export interface DraftVaccineCourse extends VaccineCourseFragment {}
+
+const defaultDraftVaccineCourse: DraftVaccineCourse = {
+  __typename: 'VaccineCourseNode',
+  id: '',
+  name: '',
+  programId: '',
+  demographicIndicatorId: '',
+};
+
+export function useVaccineCourseProgram(id?: string) {
+  const [patch, setPatch] = useState<Partial<DraftVaccineCourse>>({});
+  const [isDirty, setIsDirty] = useState(false);
+  const { data, isLoading, error } = useGet(id ?? '');
+  const {
+    mutateAsync: createMutation,
+    isLoading: isCreating,
+    error: createError,
+  } = useCreate();
+
+  const draft: DraftVaccineCourse = data
+    ? { ...defaultDraftVaccineCourse, ...data, ...patch }
+    : { ...defaultDraftVaccineCourse, ...patch };
+
+  const updatePatch = (newData: Partial<DraftVaccineCourse>) => {
+    const newPatch = { ...patch, ...newData };
+    setPatch(newPatch);
+
+    // Ensures that UI doesn't show in "dirty" state if nothing actually
+    // different from the saved data
+    const updatedData = { ...data, ...newPatch };
+    if (isEqual(data, updatedData)) setIsDirty(false);
+    else setIsDirty(true);
+    return;
+  };
+
+  const resetDraft = () => {
+    if (data) {
+      setPatch({});
+      setIsDirty(false);
+    }
+  };
+
+  const create = async () => {
+    const result = await createMutation(draft);
+    setIsDirty(false);
+    return result;
+  };
+
+  return {
+    query: { data: data, isLoading, error },
+    create: { create, isCreating, createError },
+    draft,
+    resetDraft,
+    isDirty,
+    updatePatch,
+  };
+}
+
+const useGet = (id: string) => {
+  const { api, storeId } = useImmunisationGraphQL();
+
+  const queryFn = async () => {
+    const result = await api.vaccineCourses({
+      storeId,
+      first: 1,
+      offset: 0,
+      key: VaccineCourseSortFieldInput.Name,
+      desc: false,
+      filter: { id: { equalTo: id } },
+    });
+
+    if (result.vaccineCourses.__typename === 'VaccineCourseConnector') {
+      return result.vaccineCourses.nodes[0];
+    }
+  };
+
+  const query = useQuery({
+    queryKey: [VACCINECOURSES, id],
+    queryFn,
+    enabled: id !== '',
+  });
+
+  return query;
+};
+
+const useCreate = () => {
+  const { api, storeId, queryClient } = useImmunisationGraphQL();
+
+  const mutationFn = async ({ name }: DraftVaccineCourse) => {
+    return await api.insertVaccineCourse({
+      storeId,
+      input: {
+        id: FnUtils.generateUUID(),
+        name,
+        demographicIndicatorId: '',
+        programId: '',
+      },
+    });
+  };
+
+  return useMutation({
+    mutationFn,
+    onSuccess: () => queryClient.invalidateQueries([VACCINECOURSES]),
+  });
+};
