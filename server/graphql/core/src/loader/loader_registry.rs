badacd39d0feea2b7ff111ef48a762c36c322531
James Brunskill <james@sussol.net>
2024-06-10 18:02:28 +1200

    Demographic Indicator Loader

--- server/graphql/core/src/loader/loader_registry.rs
+++ server/graphql/core/src/loader/loader_registry.rs
@@ -334,5 +334,12 @@ pub async fn get_loaders(
         },
         async_std::task::spawn,
     ));
+
+    loaders.insert(DataLoader::new(
+        DemographicIndicatorLoader {
+            connection_manager: connection_manager.clone(),
+        },
+        async_std::task::spawn,
+    ));
     loaders
 }
