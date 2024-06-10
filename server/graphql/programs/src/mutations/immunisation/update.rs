694aac4802832e37b29974b87b2770b6b8427463
James Brunskill <james@sussol.net>
2024-06-10 12:30:46 +1200

    Add update_immunisation graphql
    Source: https://github.com/msupply-foundation/open-msupply/commit/a2acb1147379565fdc37312354b00de051bb3d5f

---
+++ server/graphql/programs/src/mutations/immunisation/update.rs
@@ -0,0 +1,92 @@
+use async_graphql::*;
+
+use graphql_core::{
+    simple_generic_errors::{DatabaseError, RecordAlreadyExist, UniqueValueViolation},
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+use graphql_types::types::program_node::ProgramNode;
+use service::{
+    auth::{Resource, ResourceAccessRequest},
+    program::update_immunisation::{
+        UpdateImmunisationProgram, UpdateImmunisationProgramError as ServiceError,
+    },
+};
+
+#[derive(InputObject)]
+pub struct UpdateImmunisationProgramInput {
+    pub id: String,
+    pub name: String,
+}
+
+impl From<UpdateImmunisationProgramInput> for UpdateImmunisationProgram {
+    fn from(input: UpdateImmunisationProgramInput) -> Self {
+        Self {
+            id: input.id,
+            name: input.name,
+        }
+    }
+}
+
+#[derive(SimpleObject)]
+pub struct UpdateImmunisationProgramError {
+    pub error: UpdateImmunisationProgramErrorInterface,
+}
+
+#[derive(Interface)]
+#[graphql(field(name = "description", type = "String"))]
+pub enum UpdateImmunisationProgramErrorInterface {
+    ProgramAlreadyExists(RecordAlreadyExist),
+    DuplicateName(UniqueValueViolation),
+    DatabaseError(DatabaseError),
+}
+
+fn map_error(error: ServiceError) -> Result<UpdateImmunisationProgramErrorInterface> {
+    use StandardGraphqlError::*;
+    let formatted_error = format!("{:#?}", error);
+
+    let graphql_error = match error {
+        // Standard Graphql Errors
+        ServiceError::ImmunisationProgramAlreadyExists => BadUserInput(formatted_error),
+        ServiceError::CreatedRecordNotFound => InternalError(formatted_error),
+        ServiceError::DatabaseError(_) => InternalError(formatted_error),
+    };
+
+    Err(graphql_error.extend())
+}
+
+#[derive(Union)]
+pub enum UpdateImmunisationProgramResponse {
+    Response(ProgramNode),
+    Error(UpdateImmunisationProgramError),
+}
+
+pub fn update_immunisation_program(
+    ctx: &Context<'_>,
+    store_id: String,
+    input: UpdateImmunisationProgramInput,
+) -> Result<UpdateImmunisationProgramResponse> {
+    let user = validate_auth(
+        ctx,
+        &ResourceAccessRequest {
+            resource: Resource::MutateProgram,
+            store_id: Some(store_id.clone()),
+        },
+    )?;
+    let service_provider = ctx.service_provider();
+    let service_context = service_provider.context(store_id.to_string(), user.user_id)?;
+
+    match service_provider
+        .program_service
+        .update_immunisation_program(&service_context, input.into())
+    {
+        Ok(row) => Ok(UpdateImmunisationProgramResponse::Response(ProgramNode {
+            program_row: row,
+        })),
+        Err(error) => Ok(UpdateImmunisationProgramResponse::Error(
+            UpdateImmunisationProgramError {
+                error: map_error(error)?,
+            },
+        )),
+    }
+}
