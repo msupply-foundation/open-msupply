fbbd2b6c20c210f0c610a78179117672ba2cd3d1
mark-prins <mark@malcolmprins.net>
2024-06-12 14:27:44 +1200

    fix error; handle errors

--- server/service/src/vaccine_course/validate.rs
+++ server/service/src/vaccine_course/validate.rs
@@ -1,7 +1,10 @@
 use repository::{
     vaccine_course::vaccine_course_row::{VaccineCourseRow, VaccineCourseRowRepository},
-    DemographicIndicatorRow, DemographicIndicatorRowRepository, ProgramRow, ProgramRowRepository,
-    RepositoryError, StorageConnection,
+    // DemographicIndicatorRow, DemographicIndicatorRowRepository,
+    ProgramRow,
+    ProgramRowRepository,
+    RepositoryError,
+    StorageConnection,
 };
 
 pub fn check_vaccine_course_exists(
@@ -18,9 +21,9 @@ pub fn check_program_exists(
     ProgramRowRepository::new(connection).find_one_by_id(id)
 }
 
-pub fn check_demographic_indicator_exists(
-    id: &str,
-    connection: &StorageConnection,
-) -> Result<Option<DemographicIndicatorRow>, RepositoryError> {
-    DemographicIndicatorRowRepository::new(connection).find_one_by_id(id)
-}
+// pub fn check_demographic_indicator_exists(
+//     id: &str,
+//     connection: &StorageConnection,
+// ) -> Result<Option<DemographicIndicatorRow>, RepositoryError> {
+//     DemographicIndicatorRowRepository::new(connection).find_one_by_id(id)
+// }
