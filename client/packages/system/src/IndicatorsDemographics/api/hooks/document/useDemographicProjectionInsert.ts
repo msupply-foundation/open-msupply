f62f3a2c0978c0f3a3b91a5ac71ca330d95ab07a
mark-prins <mark@malcolmprins.net>
2024-06-12 13:35:17 +1200

    missed from previous commit?
    Source: https://github.com/msupply-foundation/open-msupply/commit/ffd028dc61847e7084baf6bb864f88de4b0cf4f8

--- client/packages/system/src/IndicatorsDemographics/api/hooks/document/useDemographicProjectionInsert.ts
+++ client/packages/system/src/IndicatorsDemographics/api/hooks/document/useDemographicProjectionInsert.ts
@@ -1,23 +1,13 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation } from 'react-query';
 import { DemographicProjectionFragment } from '../../operations.generated';
 import { useDemographicsApi } from '../utils/useDemographicApi';
 
 export const useDemographicProjectionInsert = () => {
-  const queryClient = useQueryClient();
   const api = useDemographicsApi();
 
-  const invalidateQueries = () =>
-    queryClient.invalidateQueries(api.keys.baseProjection());
-  const { mutateAsync: insertDemographicProjection } = useMutation(
-    async (demographicProjection: DemographicProjectionFragment) =>
-      api.insertProjection(demographicProjection),
-    {
-      onSettled: () => queryClient.invalidateQueries(api.keys.baseProjection()),
-      onError: e => {
-        console.error(e);
-      },
-    }
+  return useMutation(
+    async (
+      demographicProjection: Omit<DemographicProjectionFragment, '__typename'>
+    ) => api.insertProjection(demographicProjection)
   );
-
-  return { insertDemographicProjection, invalidateQueries };
 };
