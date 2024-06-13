ce2958e8cd9a89108d5780ad235075507fd5ff86
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-13 15:17:30 +1200

    Add vaccine course item loader

--- server/graphql/types/src/types/vaccine_course_item.rs
+++ server/graphql/types/src/types/vaccine_course_item.rs
@@ -1,6 +1,11 @@
 use async_graphql::*;
+use dataloader::DataLoader;
+
+use graphql_core::{loader::ItemLoader, standard_graphql_error::StandardGraphqlError, ContextExt};
 
 use repository::vaccine_course::vaccine_course_item_row::VaccineCourseItemRow;
+
+use super::ItemNode;
 
 #[derive(PartialEq, Debug)]
 pub struct VaccineCourseItemNode {
@@ -13,9 +18,21 @@ impl VaccineCourseItemNode {
         &self.row().id
     }
 
-    pub async fn item_id(&self) -> &str {
-        // TODO Look up item_id
-        &self.row().item_link_id
+    // pub async fn item_id(&self) -> &str {
+    //     // TODO Look up item_id
+    //     &self.row().item_link_id
+    // }
+
+    pub async fn item(&self, ctx: &Context<'_>) -> Result<ItemNode> {
+        let loader = ctx.get_loader::<DataLoader<ItemLoader>>();
+        let item_option = loader.load_one(self.row().item_link_id.clone()).await?;
+
+        let item = item_option.ok_or(
+            StandardGraphqlError::InternalError(format!("Cannot find item {}", self.row().id,))
+                .extend(),
+        )?;
+
+        Ok(ItemNode::from_domain(item))
     }
 }
 
