6c693909cee84058e93baa765dcd7b64398d134a
James Brunskill <james@sussol.net>
2024-06-13 13:19:32 +1200

    Re-translate items on the next sync...

--- server/repository/src/migrations/v2_01_00/item_add_is_vaccine.rs
+++ server/repository/src/migrations/v2_01_00/item_add_is_vaccine.rs
@@ -3,11 +3,17 @@ use crate::{migrations::sql, StorageConnection};
 pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
-        // TODO migrate across from legacy field
         r#"
             ALTER TABLE item ADD COLUMN is_vaccine BOOLEAN NOT NULL DEFAULT FALSE;
-            
             CREATE INDEX "index_item_is_vaccine" ON "item" ("is_vaccine");
+        "#,
+    )?;
+
+    // Reset translate all items on the next sync
+    sql!(
+        connection,
+        r#"
+            UPDATE sync_buffer SET integration_datetime = NULL WHERE table_name = 'item';
         "#,
     )?;
 
