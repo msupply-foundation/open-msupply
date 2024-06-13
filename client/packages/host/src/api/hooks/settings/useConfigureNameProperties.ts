08dd89d0a1d18f5822994758a76e6c078be0c06f
Lache Melvin <lache.melvin@gmail.com>
2024-06-14 08:58:04 +1200

    invalidate name properties after configuring
    Source: https://github.com/msupply-foundation/open-msupply/commit/54306d17581963e6d93c0444d65bf032778867a6

--- client/packages/host/src/api/hooks/settings/useConfigureNameProperties.ts
+++ client/packages/host/src/api/hooks/settings/useConfigureNameProperties.ts
@@ -1,8 +1,13 @@
-import { useMutation } from '@openmsupply-client/common';
+import { useMutation, useQueryClient } from '@openmsupply-client/common';
+import { NAME_PROPERTIES_KEY } from '@openmsupply-client/system';
 import { useHostApi } from '../utils/useHostApi';
 import { gapsNameProperties } from './namePropertyData';
 
 export const useConfigureNameProperties = () => {
   const api = useHostApi();
-  return useMutation(() => api.configureNameProperties(gapsNameProperties));
+  const queryClient = useQueryClient();
+
+  return useMutation(() => api.configureNameProperties(gapsNameProperties), {
+    onSuccess: () => queryClient.invalidateQueries(NAME_PROPERTIES_KEY),
+  });
 };
