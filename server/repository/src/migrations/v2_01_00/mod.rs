7e0f0f433f2d17e99b158ad0445ade68178c087a
Lache Melvin <lache.melvin@gmail.com>
2024-06-13 15:21:12 +1200

    remove name_property_data migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/81a4f97bca545b255d847f34a479322b2c06b2be

--- server/repository/src/migrations/v2_01_00/mod.rs
+++ server/repository/src/migrations/v2_01_00/mod.rs
@@ -8,7 +8,7 @@ mod decimal_pack_size;
 mod decimal_requisition_quantities;
 mod demographics;
 mod ledger;
-mod name_properties;
+mod name_property;
 mod pg_enums;
 mod program;
 mod property;
@@ -33,7 +33,7 @@ impl Migration for V2_01_00 {
         assets::migrate_assets(connection)?;
         v6_sync_api_error_code::migrate(connection)?;
         property::migrate(connection)?;
-        name_properties::migrate_name_properties(connection)?;
+        name_property::migrate(connection)?;
         demographics::migrate(connection)?;
         vaccine_course::migrate(connection)?;
         program::migrate(connection)?;
