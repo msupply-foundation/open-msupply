0ab3696f00846fbf583fe311b33af3117887589a
roxy-dao <roxy@msupply.foundation>
2024-06-14 17:42:02 +1200

    Fix typo

--- server/server/src/certs.rs
+++ server/server/src/certs.rs
@@ -122,7 +122,7 @@ impl Certificates {
 
         let cert_file = cert_dir.join(PUBLIC_CERT_FILE);
         let mut file = std::fs::File::create(&cert_file)?;
-        let public_cert_buffer = cert.cert.pems();
+        let public_cert_buffer = cert.cert.pem();
         file.write_all(public_cert_buffer.as_bytes())?;
 
         Ok(CertFiles {
