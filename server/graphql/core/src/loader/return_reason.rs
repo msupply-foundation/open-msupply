c54e1b3824d5d8b32d1d2d9dc39647ccc9a1b860
roxy-dao <roxy@msupply.foundation>
2024-06-18 21:48:30 +1200

    Loader for return reason
    Source: https://github.com/msupply-foundation/open-msupply/commit/c676288761aa91b9aa0d4883d4faaccebaacf2e6

---
+++ server/graphql/core/src/loader/return_reason.rs
@@ -0,0 +1,32 @@
+use repository::{
+    return_reason::{ReturnReason, ReturnReasonFilter, ReturnReasonRepository},
+    EqualFilter, RepositoryError, StorageConnectionManager,
+};
+
+use async_graphql::dataloader::*;
+use async_graphql::*;
+use std::collections::HashMap;
+
+pub struct ReturnReasonLoader {
+    pub connection_manager: StorageConnectionManager,
+}
+
+#[async_trait::async_trait]
+impl Loader<String> for ReturnReasonLoader {
+    type Value = ReturnReason;
+    type Error = RepositoryError;
+
+    async fn load(&self, ids: &[String]) -> Result<HashMap<String, Self::Value>, Self::Error> {
+        let connection = self.connection_manager.connection()?;
+        let repo = ReturnReasonRepository::new(&connection);
+
+        let result = repo.query_by_filter(
+            ReturnReasonFilter::new().id(EqualFilter::equal_any(ids.to_owned())),
+        )?;
+
+        Ok(result
+            .into_iter()
+            .map(|return_reason| (return_reason.return_reason_row.id.clone(), return_reason))
+            .collect())
+    }
+}
