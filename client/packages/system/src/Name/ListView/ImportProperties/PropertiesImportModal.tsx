c419897806697db23e57ee2fcf123eec5808ce5e
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-06-21 10:38:45 +1200

    Bring in import tab

--- client/packages/system/src/Name/ListView/ImportProperties/PropertiesImportModal.tsx
+++ client/packages/system/src/Name/ListView/ImportProperties/PropertiesImportModal.tsx
@@ -14,6 +14,7 @@ import { UploadTab } from './UploadTab';
 import { ReviewTab } from './ReviewTab';
 import { useNameProperties } from '../../api/hooks/document/useNameProperties';
 import { FacilityNameRowFragment } from '../../api/operations.generated';
+import { ImportTab } from './ImportTab';
 
 interface PropertiesImportModalProps {
   isOpen: boolean;
@@ -50,6 +51,9 @@ export const PropertiesImportModal: FC<PropertiesImportModalProps> = ({
   const { Modal } = useDialog({ isOpen, onClose });
 
   const [errorMessage, setErrorMessage] = useState<string>(() => '');
+  const [importProgress] = useState(0);
+  const [importErrorCount] = useState(0);
+
   const [warningMessage] = useState<string>(() => '');
   const { data: properties } = useNameProperties();
 
@@ -154,6 +158,11 @@ export const PropertiesImportModal: FC<PropertiesImportModalProps> = ({
               tab={Tabs.Review}
               uploadedRows={bufferedFacilityProperties}
               properties={properties}
+            />
+            <ImportTab
+              tab={Tabs.Import}
+              importProgress={importProgress}
+              importErrorCount={importErrorCount}
             />
           </Grid>
         </TabContext>
