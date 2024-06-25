00bf4897852d86c0b27f85a0a5085a99e4cfa864
mark-prins <mark@malcolmprins.net>
2024-06-25 15:13:07 +1200

    format the numbers

--- client/packages/system/src/IndicatorsDemographics/DetailView/IndicatorsDemographics.tsx
+++ client/packages/system/src/IndicatorsDemographics/DetailView/IndicatorsDemographics.tsx
@@ -8,6 +8,7 @@ import {
   TableProvider,
   createTableStore,
   useColumns,
+  useFormatNumber,
   useNotification,
   useTranslation,
   useUrlQueryParams,
@@ -39,6 +40,7 @@ const IndicatorsDemographicsComponent = () => {
   const [headerDraft, setHeaderDraft] = useState<HeaderData>();
   const [indexPopulation, setIndexPopulation] = useState(0);
   const [isDirty, setIsDirty] = useState(false);
+  const formatNumber = useFormatNumber();
 
   const { error, success } = useNotification();
   const t = useTranslation();
@@ -178,11 +180,11 @@ const IndicatorsDemographicsComponent = () => {
       [nameColumn(), { setter }],
       [percentageColumn(), { setter }],
       [populationColumn(), { setter: handlePopulationChange }],
-      yearColumn(1),
-      yearColumn(2),
-      yearColumn(3),
-      yearColumn(4),
-      yearColumn(5),
+      yearColumn(1, formatNumber.format),
+      yearColumn(2, formatNumber.format),
+      yearColumn(3, formatNumber.format),
+      yearColumn(4, formatNumber.format),
+      yearColumn(5, formatNumber.format),
     ],
     { sortBy, onChangeSortBy: updateSortQuery },
     [draft, indexPopulation, sortBy]
@@ -235,11 +237,28 @@ export const IndicatorsDemographics = () => (
   </TableProvider>
 );
 
-const yearColumn = (year: number) => ({
+const yearColumn = (year: number, format: (n: number) => string) => ({
   key: String(year),
   width: 150,
   align: ColumnAlign.Right,
   label: undefined,
   labelProps: { defaultValue: currentYear + year },
   sortable: false,
+  accessor: ({ rowData }: { rowData: Row }) => {
+    // using a switch to appease typescript
+    switch (year) {
+      case 1:
+        return format(rowData[1]);
+      case 2:
+        return format(rowData[2]);
+      case 3:
+        return format(rowData[3]);
+      case 4:
+        return format(rowData[4]);
+      case 5:
+        return format(rowData[5]);
+      default:
+        return '';
+    }
+  },
 });
