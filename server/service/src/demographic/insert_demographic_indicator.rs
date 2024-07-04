4c2cd288cc65eb63d2439df20a107fb32124399b
roxy-dao <roxy@msupply.foundation>
2024-07-05 02:52:46 +1200

    Remove extra enum

--- server/service/src/demographic/insert_demographic_indicator.rs
+++ server/service/src/demographic/insert_demographic_indicator.rs
@@ -14,7 +14,6 @@ pub enum InsertDemographicIndicatorError {
     DemographicIndicatorAlreadyExistsForThisYear,
     DemographicIndicatorHasNoName,
     CreatedRecordNotFound,
-    DemographicIndicatorHasNoName,
     DatabaseError(RepositoryError),
 }
 
