7d73206b18caf1f8caf92a546317e77be0344ba6
roxy-dao <roxy@msupply.foundation>
2024-07-09 20:04:19 +1200

    Remove number of shipments from node and use shipment total count

--- client/packages/common/src/types/schema.ts
+++ client/packages/common/src/types/schema.ts
@@ -6144,7 +6144,6 @@ export type RequisitionNode = {
   maxMonthsOfStock: Scalars['Float']['output'];
   /** Minimum quantity to have for stock to be ordered, used to deduce calculated quantity for each line, see calculated in requisition line */
   minMonthsOfStock: Scalars['Float']['output'];
-  numberOfShipments: Scalars['Int']['output'];
   orderType?: Maybe<Scalars['String']['output']>;
   /**
    * Request Requisition: Supplying store (store that is supplying stock)
