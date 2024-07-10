24686b9e0698145cc09e241fae049fa6aabb70c5
roxy-dao <roxy@msupply.foundation>
2024-07-10 20:24:47 +1200

    Add translations for reports

--- client/packages/common/src/intl/locales/index.ts
+++ client/packages/common/src/intl/locales/index.ts
@@ -12,6 +12,7 @@ import * as programs from './en/programs.json';
 import * as coldchain from './en/coldchain.json';
 import * as plugin from './en/plugin.json';
 import * as manage from './en/manage.json';
+import * as reports from './en/reports.json';
 
 // Normalize single namespace
 type WithOrWithoutPlural<K> = TypeOptions['jsonFormat'] extends 'v4'
@@ -40,4 +41,5 @@ export type LocaleKey =
   | WithOrWithoutPlural<keyof typeof programs>
   | WithOrWithoutPlural<keyof typeof coldchain>
   | WithOrWithoutPlural<keyof typeof plugin>
-  | WithOrWithoutPlural<keyof typeof manage>;
+  | WithOrWithoutPlural<keyof typeof manage>
+  | WithOrWithoutPlural<keyof typeof reports>;
