f3bfde7040422a900f9538eba48407942cae720e
roxy-dao <roxy@msupply.foundation>
2024-07-15 08:01:26 +1200

    Allow icon to be displayed in widget

--- client/packages/common/src/ui/components/domain/Dashboard/Widget.tsx
+++ client/packages/common/src/ui/components/domain/Dashboard/Widget.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from 'react';
-import { Box, Card, CircularProgress, Typography } from '@mui/material';
+import { Box, Card, CircularProgress, Grid, Typography } from '@mui/material';
+import { SvgIconProps } from '@common/icons';
 
 const Loading = () => (
   <Box display="flex" flex={1} justifyContent="center" alignItems="center">
@@ -10,12 +11,14 @@ const Loading = () => (
 interface WidgetProps {
   height?: number | string;
   title: string;
+  Icon?: (props: SvgIconProps & { stroke?: string }) => JSX.Element;
 }
 
 export const Widget: React.FC<PropsWithChildren<WidgetProps>> = ({
   children,
   height = '100%',
   title,
+  Icon,
 }) => (
   <Card
     sx={{
@@ -29,9 +32,21 @@ export const Widget: React.FC<PropsWithChildren<WidgetProps>> = ({
       boxShadow: theme => theme.shadows[2],
     }}
   >
-    <Typography sx={{ fontSize: '14px', fontWeight: 'bold' }}>
-      {title}
-    </Typography>
+    <Grid container alignItems="center">
+      {Icon && (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ width: 48 }}
+        >
+          <Icon color="primary" />
+        </Box>
+      )}
+      <Typography sx={{ fontSize: '14px', fontWeight: 'bold' }}>
+        {title}
+      </Typography>
+    </Grid>
 
     <React.Suspense fallback={<Loading />}>{children}</React.Suspense>
   </Card>
