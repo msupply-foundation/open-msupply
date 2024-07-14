2c74bf11bce6dd3bb121825ac97c7cb5ae406f7c
roxy-dao <roxy@msupply.foundation>
2024-07-15 08:06:25 +1200

    Add stock & expiring widgets
    Source: https://github.com/msupply-foundation/open-msupply/commit/4ebc4a9c64ed8884e59a6fe0166df854ca02f483

--- client/packages/reports/src/ListView/ListView.tsx
+++ client/packages/reports/src/ListView/ListView.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Grid } from '@openmsupply-client/common';
 import { AppBarButtons } from './AppBarButton';
 import { SidePanel } from './SidePanel';
+import { StockAndItemWidget } from './StockAndItemWidget';
+import { useReport } from '@openmsupply-client/system';
+import { ExpiringWidget } from './ExpiringWidget';
 
 export const ListView = () => {
+  const { data } = useReport.document.list({});
+  const stockAndItemReports = data?.nodes?.filter(
+    report => report?.subContext === 'StockAndItems'
+  );
+  const expiringReports = data?.nodes?.filter(
+    report => report?.subContext === 'Expiring'
+  );
+
   return (
     <>
+      <Grid
+        container
+        sx={{
+          backgroundColor: 'background.toolbar',
+          paddingBottom: '32px',
+        }}
+        justifyContent="space-evenly"
+      >
+        <StockAndItemWidget reports={stockAndItemReports} />
+        <ExpiringWidget reports={expiringReports} />
+      </Grid>
+
       <AppBarButtons />
       <SidePanel />
     </>
