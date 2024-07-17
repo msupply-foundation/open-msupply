e5faa1533f887d9482b2c0434b3f264fa091a382
roxy-dao <roxy@msupply.foundation>
2024-07-17 20:56:52 +1200

    Remove old report hooks
    Source: https://github.com/msupply-foundation/open-msupply/commit/ef9039d47fcb8a7c549995590f0c9544f31954d6

---
+++ client/packages/system/src/Report/api/hooks/useDisplayReport.ts
@@ -0,0 +1,31 @@
+import { PrintFormat } from '@common/types';
+import { useReportGraphQL } from '../useReportGraphQL';
+import { PrintReportParams } from './usePrintReport';
+import { useQuery } from '@openmsupply-client/common';
+import { REPORT } from './keys';
+export const useDisplayReport = (params: PrintReportParams) => {
+  const { reportApi, storeId } = useReportGraphQL();
+
+  const queryKey = [REPORT, storeId];
+  const queryFn = async () => {
+    const { dataId, reportId, args, sort } = params;
+    const result = await reportApi.printReport({
+      dataId,
+      reportId,
+      storeId,
+      format: PrintFormat.Html,
+      arguments: args,
+      sort,
+    });
+    if (result?.printReport?.__typename === 'PrintReportNode') {
+      return result.printReport.fileId;
+    }
+
+    return null;
+  };
+
+  return useQuery({
+    queryKey,
+    queryFn,
+  });
+};
