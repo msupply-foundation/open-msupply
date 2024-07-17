22e6fe7727d8be332efc7f24d122d00c681b87cd
roxy-dao <roxy@msupply.foundation>
2024-07-17 21:10:50 +1200

    Rename report service
    Source: https://github.com/msupply-foundation/open-msupply/commit/2e0fe53bcc188698083e17dfce18df9eff99d38b

--- client/packages/reports/src/ReportsService.tsx
+++ client/packages/reports/src/ReportsService.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
 import { Routes, Route } from '@openmsupply-client/common';
 import { ListView } from './ListView';
-import { DetailView } from './DetailView';
 
 const ReportsService: FC = () => {
   return (
     <Routes>
-      <Route path="/:id" element={<DetailView />} />
       <Route path="/" element={<ListView />} />
     </Routes>
   );
