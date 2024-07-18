dd28627bd8af091899d417e3c8597181e81e2473
roxy-dao <roxy@msupply.foundation>
2024-07-18 19:30:47 +1200

    Add display key + refactor report state
    Source: https://github.com/msupply-foundation/open-msupply/commit/2a5e352662aa995f3c149f468399f07c62e053e7

--- client/packages/system/src/Report/api/hooks/useReportStore.ts
+++ client/packages/system/src/Report/api/hooks/useReportStore.ts
@@ -1,18 +1,21 @@
 import { create } from '@openmsupply-client/common';
 import { JsonData } from '@openmsupply-client/programs';
-import { ReportRowFragment } from '../operations.generated';
 
 interface ReportStoreState {
-  report: ReportRowFragment;
-  setReport: (report: ReportRowFragment) => void;
-  args: JsonData | undefined;
-  setArgs: (args: JsonData | undefined) => void;
+  id: string;
+  setId: (id: string) => void;
+  args: Map<string, JsonData | undefined>;
+  setArgs: (id: string, value: JsonData | undefined) => void;
 }
 
 export const useReportStore = create<ReportStoreState>(set => ({
-  report: {} as ReportRowFragment,
-  setReport: (report: ReportRowFragment) =>
-    set(state => ({ ...state, report })),
-  args: undefined,
-  setArgs: (args: JsonData | undefined) => set(state => ({ ...state, args })),
+  id: '',
+  setId: (id: string) => set(state => ({ ...state, id })),
+  args: new Map(),
+  setArgs: (id: string, value: JsonData | undefined) =>
+    set(state => {
+      const args = new Map(state.args);
+      args.set(id, value);
+      return { ...state, args };
+    }),
 }));
