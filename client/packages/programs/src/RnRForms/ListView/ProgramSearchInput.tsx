e67426ac4ff38cea05d249fa7efe37ac2e572c3c
Lache Melvin <lache.melvin@gmail.com>
2024-07-19 10:20:24 +1200

    create modal

--- client/packages/programs/src/RnRForms/ListView/ProgramSearchInput.tsx
+++ client/packages/programs/src/RnRForms/ListView/ProgramSearchInput.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
-import {
-  Autocomplete,
-} from '@openmsupply-client/common';
+import { Autocomplete } from '@openmsupply-client/common';
 import { ProgramFragment } from '../../api/operations.generated';
 import { useProgramList } from '../../api';
 
 type ProgramSearchInputProps = {
   value: ProgramFragment | null;
   onChange: (newProgram: ProgramFragment) => void;
+  width?: string;
 };
-
 
 export const ProgramSearchInput = ({
   value,
   onChange,
-}: ProgramSearchInputProps) => { 
- const { data, isLoading } = useProgramList();
+  width,
+}: ProgramSearchInputProps) => {
+  const { data, isLoading } = useProgramList();
 
   return (
     <Autocomplete
-    width='100%'
+      width={width}
       loading={isLoading}
       options={data?.nodes ?? []}
-      optionKey='name'
+      optionKey="name"
       onChange={(_, value) => value && onChange(value)}
       value={value ? { label: value.name, ...value } : null}
       isOptionEqualToValue={(option, value) => option.id === value.id}
     />
   );
- };
+};
