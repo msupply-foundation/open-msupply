cc55fea1350a60dbc0f8480eb01238fbcb1cbb65
roxy-dao <roxy@msupply.foundation>
2024-07-22 09:39:26 +1200

    wip
    Source: https://github.com/msupply-foundation/open-msupply/commit/4d6a95c967c2b316c21dba4ee1303a5df76d6b90

--- client/packages/reports/src/DetailView/DetailView.tsx
+++ client/packages/reports/src/DetailView/DetailView.tsx
@@ -1,26 +1,65 @@
-import React, { useEffect } from 'react';
-import { NothingHere, useBreadcrumbs } from '@openmsupply-client/common';
+import React, { useEffect, useState } from 'react';
 import {
-  useDisplayReport,
-  useReport,
-  useReportStore,
-} from '@openmsupply-client/system';
+  BasicSpinner,
+  NothingHere,
+  useBreadcrumbs,
+  useParams,
+  useUrlQueryParams,
+} from '@openmsupply-client/common';
+import { useGenerateReport, useReport } from '@openmsupply-client/system';
 import { Environment } from '@openmsupply-client/config';
+import { AppBarButtons } from './AppBarButton';
 
 export const DetailView = () => {
-  const { id, args } = useReportStore();
+  const { id } = useParams();
   const { setCustomBreadcrumbs } = useBreadcrumbs();
-  const { data } = useReport(id);
-  const { data: fileId } = useDisplayReport({
-    reportId: id,
-    args: args.get(id),
-  });
-
-  const url = `${Environment.FILE_URL}${fileId}`;
+  const { data } = useReport(id ?? '');
+  const { mutateAsync, isLoading } = useGenerateReport();
+  const [fileId, setFileId] = useState<string | undefined>();
+  const { urlQuery } = useUrlQueryParams();
 
   useEffect(() => {
     setCustomBreadcrumbs({ 0: data?.name ?? '' });
   }, [data?.name]);
 
-  return fileId ? <iframe src={url} width="100%" /> : <NothingHere />;
+  const report = async () => {
+    if (!data) return;
+
+    const reportArgs = urlQuery['reportArgs']
+      ? JSON.parse(urlQuery['reportArgs'].toString())
+      : undefined;
+
+    const fileId = await mutateAsync({
+      reportId: data.id,
+      args: reportArgs,
+      dataId: '',
+    });
+    setFileId(fileId);
+  };
+
+  useEffect(() => {
+    if (id) {
+      report();
+    }
+  }, [id]);
+
+  const url = `${Environment.FILE_URL}${fileId}`;
+
+  if (!data) {
+    return <NothingHere />;
+  }
+
+  return (
+    <>
+      {isLoading && <BasicSpinner />}
+      {fileId ? (
+        <>
+          <iframe src={url} width="100%" />
+          <AppBarButtons report={data} isDisabled={!!data.argumentSchema} />
+        </>
+      ) : (
+        <NothingHere />
+      )}
+    </>
+  );
 };
