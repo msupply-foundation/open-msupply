64f4d27415741315baff45c03c4278eb9f7ac07b
Lache Melvin <lache.melvin@gmail.com>
2024-07-30 14:14:45 +1200

    editable fields
    Source: https://github.com/msupply-foundation/open-msupply/commit/c271c04bbe342c8bfcb79bad6f1a7f18241c2ff6

--- client/packages/programs/src/RnRForms/DetailView/ContentArea.tsx
+++ client/packages/programs/src/RnRForms/DetailView/ContentArea.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  BasicTextInput,
   Box,
+  Checkbox,
+  DatePicker,
+  Formatter,
   NothingHere,
+  NumericTextInput,
   Table,
   useTheme,
   useTranslation,
@@ -24,78 +29,175 @@ export const ContentArea = ({ data }: ContentAreaProps) => {
         sx={{
           '& th': {
             textAlign: 'left',
-            color: 'text',
             padding: 1,
             fontWeight: 'bold',
             border: '1px solid lightgray',
           },
           '& td': {
-            padding: 1,
+            padding: '2px',
             border: '1px solid lightgray',
           },
         }}
       >
         <thead>
-          <th>{t('label.code')}</th>
-          <th>{t('label.name')}</th>
-          <th>{t('label.strength')}</th>
-          <th>{t('label.unit')}</th>
-          <th>{t('label.ven')}</th>
-          <th>{t('label.rnr-initial-balance')}</th>
-          <th>{t('label.rnr-received')}</th>
-          <th>{t('label.rnr-consumed')}</th>
-          <th>{t('label.rnr-losses')}</th>
-          <th>{t('label.rnr-stock-out-duration')}</th>
-          <th>{t('label.rnr-consumed-adjusted')}</th>
-          <th>{t('label.rnr-final-balance')}</th>
-          <th>{t('label.amc')}</th>
-          <th>{t('label.rnr-maximum-quantity')}</th>
-          <th>{t('label.expiry')}</th>
-          <th>{t('label.requested-quantity')}</th>
-          <th>{t('label.comment')}</th>
+          <tr>
+            <th style={{ width: '80px' }}>{t('label.code')}</th>
+            <th style={{ minWidth: '300px' }}>{t('label.name')}</th>
+            <th>{t('label.strength')}</th>
+            <th>{t('label.unit')}</th>
+            <th>{t('label.ven')}</th>
+            <th>{t('label.rnr-initial-balance')}</th>
+            <th>{t('label.rnr-received')}</th>
+            <th>{t('label.rnr-consumed')}</th>
+            <th>{t('label.rnr-adjustments')}</th>
+            <th>{t('label.rnr-stock-out-duration')}</th>
+            <th>{t('label.rnr-consumed-adjusted')}</th>
+            <th>{t('label.rnr-final-balance')}</th>
+            <th>{t('label.amc')}</th>
+            <th>{t('label.rnr-maximum-quantity')}</th>
+            <th>{t('label.expiry')}</th>
+            <th>{t('label.requested-quantity')}</th>
+            <th>{t('label.comment')}</th>
+            <th>{t('label.confirmed')}</th>
+          </tr>
         </thead>
 
-        {data.map((line, index) => (
-          <RnRFormLine key={index} line={line} />
-        ))}
+        <tbody>
+          {data.map((line, index) => (
+            <RnRFormLine key={index} line={line} />
+          ))}
+        </tbody>
       </Table>
     </Box>
   );
 };
 
 export const RnRFormLine = ({ line }: { line: RnRFormLineFragment }) => {
-  // const t = useTranslation('replenishment');
   const theme = useTheme();
 
-  const venCategory =
-    line.item.venCategory === VenCategoryType.NotAssigned
-      ? ''
-      : line.item.venCategory;
+  const [patch, setPatch] = useState<Partial<RnRFormLineFragment>>({});
+  const draft = { ...line, ...patch };
 
-  const disabledSx = {
+  const venCategory =
+    draft.item.venCategory === VenCategoryType.NotAssigned
+      ? ''
+      : draft.item.venCategory;
+
+  const readOnlyColumn = {
     backgroundColor: theme.palette.background.drawer,
-    color: theme.palette.text.disabled,
+    padding: '5px',
   };
 
   return (
     <tr>
-      <td style={disabledSx}>{line.item.code}</td>
-      <td style={disabledSx}>{line.item.name}</td>
-      <td style={disabledSx}>{line.item.strength}</td>
-      <td style={disabledSx}>{line.item.unitName}</td>
-      <td style={disabledSx}>{venCategory}</td>
-      <td>{line.initialBalance}</td>
-      <td>{line.quantityReceived}</td>
-      <td>{line.quantityConsumed}</td>
-      <td>{line.adjustments}</td>
-      <td>{line.stockOutDuration}</td>
-      <td style={disabledSx}>{line.adjustedQuantityConsumed}</td>
-      <td>{line.finalBalance}</td>
-      <td style={disabledSx}>{line.averageMonthlyConsumption}</td>
-      <td>{line.maximumQuantity}</td>
-      <td>{line.expiryDate}</td>
-      <td>{line.requestedQuantity}</td>
-      <td>{line.comment}</td>
+      <td style={readOnlyColumn}>{draft.item.code}</td>
+      <td style={readOnlyColumn}>{draft.item.name}</td>
+      <td style={readOnlyColumn}>{draft.item.strength}</td>
+      <td style={readOnlyColumn}>{draft.item.unitName}</td>
+      <td style={{ ...readOnlyColumn, textAlign: 'center' }}>{venCategory}</td>
+      <RnRNumberCell
+        value={draft.initialBalance}
+        onChange={val => setPatch({ ...patch, initialBalance: val })}
+      />
+      <RnRNumberCell
+        value={draft.quantityReceived}
+        onChange={val => setPatch({ ...patch, quantityReceived: val })}
+      />
+      <RnRNumberCell
+        value={draft.quantityConsumed}
+        onChange={val => setPatch({ ...patch, quantityConsumed: val })}
+      />
+      <RnRNumberCell
+        value={draft.adjustments}
+        onChange={val => setPatch({ ...patch, adjustments: val })}
+      />
+      <RnRNumberCell
+        value={draft.stockOutDuration}
+        onChange={val => setPatch({ ...patch, stockOutDuration: val })}
+      />
+      <RnRNumberCell
+        disabled
+        value={draft.adjustedQuantityConsumed}
+        onChange={() => {}}
+      />
+      <RnRNumberCell disabled value={draft.finalBalance} onChange={() => {}} />
+      <RnRNumberCell
+        disabled
+        value={draft.averageMonthlyConsumption}
+        onChange={() => {}}
+      />
+      <RnRNumberCell
+        disabled
+        value={draft.maximumQuantity}
+        onChange={() => {}}
+      />
+      <td>
+        <DatePicker
+          sx={{ width: '160px', '& fieldset': { border: 'none' } }}
+          value={draft.expiryDate ? new Date(draft.expiryDate) : null}
+          onChange={date =>
+            setPatch({ ...patch, expiryDate: Formatter.naiveDate(date) })
+          }
+        />
+      </td>
+      <RnRNumberCell
+        value={draft.requestedQuantity}
+        onChange={val => setPatch({ ...patch, requestedQuantity: val })}
+      />
+      <td>
+        <BasicTextInput
+          multiline
+          sx={{ width: '200px' }}
+          InputProps={{
+            sx: {
+              backgroundColor: theme.palette.background.default,
+            },
+          }}
+          value={draft.comment ?? ''}
+          onChange={e => setPatch({ ...patch, comment: e.target.value })}
+        />
+      </td>
+      <td style={{ textAlign: 'center' }}>
+        <Checkbox
+          checked={!!draft.confirmed}
+          size="medium"
+          onClick={() => {
+            // TODO: save here!
+            setPatch({ ...patch, confirmed: !draft.confirmed });
+          }}
+        />
+      </td>
     </tr>
+  );
+};
+
+const RnRNumberCell = ({
+  value,
+  disabled,
+  onChange,
+}: {
+  value: number;
+  disabled?: boolean;
+  onChange: (val: number) => void;
+}) => {
+  const theme = useTheme();
+  const disabledStyle = {
+    backgroundColor: theme.palette.background.drawer,
+    // color: theme.palette.text.disabled,
+  };
+
+  return (
+    <td style={disabled ? disabledStyle : {}}>
+      <NumericTextInput
+        InputProps={{
+          sx: {
+            backgroundColor: disabled ? disabledStyle.backgroundColor : 'white',
+          },
+        }}
+        value={value}
+        disabled={disabled}
+        onChange={val => onChange(val ?? 0)}
+      />
+    </td>
   );
 };
