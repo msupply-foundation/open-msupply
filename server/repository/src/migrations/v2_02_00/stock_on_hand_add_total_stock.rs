f5e96a03a17b6253ea75071cee2cb031561627ba
James Brunskill <james@sussol.net>
2024-08-02 12:18:41 +1200

    Postgres fixes

--- server/repository/src/migrations/v2_02_00/stock_on_hand_add_total_stock.rs
+++ server/repository/src/migrations/v2_02_00/stock_on_hand_add_total_stock.rs
@@ -5,7 +5,13 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
         r#"
+        DROP VIEW IF EXISTS stock_on_hand;
         DROP VIEW IF EXISTS store_items;
+        "#,
+    )?;
+    sql!(
+        connection,
+        r#"
         CREATE VIEW store_items AS
         SELECT i.id as item_id, sl.store_id, sl.pack_size, sl.available_number_of_packs, sl.total_number_of_packs
         FROM
@@ -18,8 +24,6 @@ pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
     sql!(
         connection,
         r#"
-        DROP VIEW stock_on_hand;
-
         CREATE VIEW stock_on_hand AS
         SELECT
           'n/a' AS id,
