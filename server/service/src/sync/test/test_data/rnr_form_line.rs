9c4389f2e5d7c1e86305ba7f89a8f55bb3e22e30
roxy-dao <roxy@msupply.foundation>
2024-08-06 16:22:42 +1200

    remove extra p.amc
    Source: https://github.com/msupply-foundation/open-msupply/commit/4665b43a1dd48d7b7f4f200ea0807fb91a6c7413

--- server/service/src/sync/test/test_data/rnr_form_line.rs
+++ server/service/src/sync/test/test_data/rnr_form_line.rs
@@ -50,7 +50,6 @@ fn rnr_form_line_1() -> RnRFormLineRow {
         requested_quantity: 0.0,
         comment: None,
         confirmed: false,
-        previous_average_monthly_consumption: 0.0,
     }
 }
 
