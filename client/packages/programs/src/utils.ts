460d059042b6826ca04b176424505f523e7914fa
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-08-07 11:21:53 +1200

    Add basic status to RnR list view

--- client/packages/programs/src/utils.ts
+++ client/packages/programs/src/utils.ts
@@ -1,4 +1,6 @@
+import { RnRFormNodeStatus } from '@common/types';
 import { ProgramEventFragment } from './api';
+import { RnRFormFragment } from './api/operations.generated';
 
 /**
  * Finds all events that are status events and return their data
@@ -8,3 +10,7 @@ export const getStatusEventData = (events: ProgramEventFragment[]): string[] =>
     .filter(e => e.type?.startsWith('status'))
     .map(e => e.data)
     .filter((data): data is string => !!data);
+
+export const isRnRFormDisabled = (form: RnRFormFragment): boolean => {
+  return form.status === RnRFormNodeStatus.Finalised;
+};
