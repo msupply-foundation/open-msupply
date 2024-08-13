df758ad2821a31c99b7058a639903e3c27bb2e05
roxy-dao <roxy@msupply.foundation>
2024-08-14 09:06:31 +1200

    css props to make inputs prettier
    Source: https://github.com/msupply-foundation/open-msupply/commit/7a3db7dff2b9058c34468ab2951c993bdcc9a9b7

--- client/packages/programs/src/JsonForms/common/components/Number.tsx
+++ client/packages/programs/src/JsonForms/common/components/Number.tsx
@@ -8,17 +8,32 @@ import {
   useDebounceCallback,
 } from '@openmsupply-client/common';
 import { FORM_LABEL_WIDTH, DefaultFormRowSx } from '../styleConstants';
+import { z } from 'zod';
+import { useZodOptionsValidation } from '../hooks/useZodOptionsValidation';
 
 export const numberTester = rankWith(3, schemaTypeIs('number'));
 
+const Options = z
+  .object({
+    inputAlignment: z.enum(['start', 'end']).optional(),
+    paddingRight: z.number().optional(),
+  })
+  .optional();
+
+type Options = z.infer<typeof Options>;
+
 const UIComponent = (props: ControlProps) => {
-  const { data, handleChange, label, path, errors, schema } = props;
+  const { data, handleChange, label, path, errors, schema, uischema } = props;
   const [localData, setLocalData] = useState<number | undefined>(data);
   const onChange = useDebounceCallback(
     (value: number | undefined) => handleChange(path, value),
     [path]
   );
-  const error = !!errors;
+  const { errors: zErrors, options } = useZodOptionsValidation(
+    Options,
+    uischema.options
+  );
+  const error = !!errors || !!zErrors;
 
   if (!props.visible) {
     return null;
@@ -38,12 +53,17 @@ const UIComponent = (props: ControlProps) => {
     helperText: errors,
     value: localData,
   };
+
+  const inputAlignment = options?.inputAlignment ?? 'start';
+  const paddingRight = options?.paddingRight ?? 0;
+
   return (
     <DetailInputWithLabelRow
       sx={DefaultFormRowSx}
       label={label}
       labelWidthPercentage={FORM_LABEL_WIDTH}
-      inputAlignment="start"
+      inputAlignment={inputAlignment}
+      inputSx={{ paddingRight: `${paddingRight}px` }}
       Input={
         <NumericTextInput
           {...inputProps}
