9de62ded2168aff499925f5714eec572103a3dd0
Lache Melvin <lache.melvin@gmail.com>
2024-08-13 15:09:18 +1200

    update only for this requisition!

--- server/service/src/processors/transfer/requisition/update_request_requisition_approved_quantities.rs
+++ server/service/src/processors/transfer/requisition/update_request_requisition_approved_quantities.rs
@@ -73,6 +73,7 @@ impl RequisitionTransferProcessor for UpdateRequestRequisitionApprovedQuantities
         // Update approved quantities on request requisition lines
         for line in response_lines.iter() {
             requisition_line_row_repository.update_approved_quantity_by_item_id(
+                &request_requisition.requisition_row.id,
                 &line.requisition_line_row.item_link_id,
                 line.requisition_line_row.approved_quantity,
             )?;
