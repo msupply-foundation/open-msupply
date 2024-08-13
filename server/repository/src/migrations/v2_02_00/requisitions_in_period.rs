e50d9a6067abdd694127af0d3877e093ed83aadf
Andrei <andrei.evguenov@gmail.com>
2024-08-13 20:21:03 +1200

    Use fragment migrations for requisitions in period view
    Source: https://github.com/msupply-foundation/open-msupply/commit/431af7f7cd1364fb4d397a6db50ed5bf470dbd0f

--- server/repository/src/migrations/v2_02_00/requisitions_in_period.rs
+++ server/repository/src/migrations/v2_02_00/requisitions_in_period.rs
@@ -1,19 +1,27 @@
-use crate::{migrations::sql, StorageConnection};
+use crate::migrations::*;
 
-pub(crate) fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
-    // View to help figure out how many requisitions were done for a program and order type in a period
+pub(crate) struct Migrate;
 
-    // This migration adds the not null constraint to order_type
-    sql!(
-        connection,
-        r#"
-        DROP VIEW IF EXISTS requisitions_in_period;
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "requisitions_in_period"
+    }
 
-        CREATE VIEW requisitions_in_period AS
-        SELECT 'n/a' as id, program_id, period_id, store_id, order_type, type, count(*) as count FROM requisition WHERE order_type IS NOT NULL
-            GROUP BY 1,2,3,4,5,6;
-    "#
-    )?;
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        // View to help figure out how many requisitions were done for a program and order type in a period
 
-    Ok(())
+        // This migration adds the not null constraint to order_type
+        sql!(
+            connection,
+            r#"
+                DROP VIEW IF EXISTS requisitions_in_period;
+
+                CREATE VIEW requisitions_in_period AS
+                SELECT 'n/a' as id, program_id, period_id, store_id, order_type, type, count(*) as count FROM requisition WHERE order_type IS NOT NULL
+                    GROUP BY 1,2,3,4,5,6;            
+            "#
+        )?;
+
+        Ok(())
+    }
 }
