23d17b3a56add261cf84b1493d5109daf11ced07
roxy-dao <roxy@msupply.foundation>
2024-08-19 09:01:45 +1200

    Move rename returns to v2.3
    Source: https://github.com/msupply-foundation/open-msupply/commit/cab6d0c3d6d9b1b0c3f29a4a85de375b3b3b4dfa

--- server/repository/src/migrations/v2_03_00/return_types_rename.rs
+++ server/repository/src/migrations/v2_03_00/return_types_rename.rs
@@ -1,16 +1,10 @@
 use crate::{migrations::*, StorageConnection};
 
-pub(crate) struct Migrate;
-impl MigrationFragment for Migrate {
-    fn identifier(&self) -> &'static str {
-        "return_types_rename"
-    }
-
-    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
-        if cfg!(feature = "postgres") {
-            sql!(
-                connection,
-                r#"
+pub fn migrate(connection: &StorageConnection) -> anyhow::Result<()> {
+    if cfg!(feature = "postgres") {
+        sql!(
+            connection,
+            r#"
                  ALTER TYPE context_type RENAME VALUE 'INBOUND_RETURN' TO 'CUSTOMER_RETURN';
                  ALTER TYPE context_type RENAME VALUE 'OUTBOUND_RETURN' TO 'SUPPLIER_RETURN';
                  ALTER TYPE invoice_type RENAME VALUE 'INBOUND_RETURN' TO 'CUSTOMER_RETURN';
@@ -23,9 +17,8 @@ impl MigrationFragment for Migrate {
                  ALTER TYPE permission_type RENAME VALUE 'INBOUND_RETURN_MUTATE' TO 'CUSTOMER_RETURN_MUTATE';
      
                  "#,
-            )?;
-        }
-
-        Ok(())
+        )?;
     }
+
+    Ok(())
 }
