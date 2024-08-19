7474f03d7c068c7b55cf1d40e57e02aeb09f8477
mark-prins <mark@malcolmprins.net>
2024-08-19 18:10:10 +1200

    adjust translation namespace calls

--- client/packages/invoices/src/Returns/InboundDetailView/AppBarButtons.tsx
+++ client/packages/invoices/src/Returns/InboundDetailView/AppBarButtons.tsx
@@ -30,7 +30,7 @@ export const AppBarButtonsComponent: FC<AppBarButtonProps> = ({
   const isDisabled = useReturns.utils.inboundIsDisabled();
   const { data } = useReturns.document.inboundReturn();
   const { OpenButton } = useDetailPanel();
-  const t = useTranslation('common');
+  const t = useTranslation('reports'); // note: using 'reports' due to issue #4616
   const { print, isPrinting } = usePrintReport();
   const {
     queryParams: { sortBy },
