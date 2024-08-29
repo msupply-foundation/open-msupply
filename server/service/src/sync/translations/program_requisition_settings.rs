689c66fafd4c7494365c7590aecfd0f1c074a333
Lache Melvin <lache.melvin@gmail.com>
2024-08-29 18:23:11 +1200

    translate is_immunisation field from msupply

--- server/service/src/sync/translations/program_requisition_settings.rs
+++ server/service/src/sync/translations/program_requisition_settings.rs
@@ -26,6 +26,7 @@ pub struct LegacyListMasterRow {
     is_program: bool,
     #[serde(rename = "programSettings")]
     program_settings: Option<LegacyProgramSettings>,
+    is_immunisation: bool,
 }
 
 #[derive(Deserialize, Clone)]
@@ -180,7 +181,7 @@ fn generate_requisition_program(
         master_list_id: Some(master_list.id.clone()),
         name: master_list.description.clone(),
         context_id: context_row.id.clone(),
-        is_immunisation: false,
+        is_immunisation: master_list.is_immunisation.clone(),
         deleted_datetime: None,
     };
 
