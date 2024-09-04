2ca603fa1f5122d5b8472c8d4d91dbf418cadfd8
mark-prins <mark@malcolmprins.net>
2024-09-05 10:00:47 +1200

    build apk
    Source: https://github.com/msupply-foundation/open-msupply/commit/0637af5e75de94ee72ed35f74e282a678499fbf8

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -17,26 +17,36 @@ start /wait /b build\windows\omsupply-prepare.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omsupply for sqlite #####
-cd server && cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@cd server 
+@REM cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omsupply for postgres #####
-cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building sqlite omsupply server #####
+@REM cd cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omSupply cli #####
-cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
-cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building omsupply for postgres #####
+@REM cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omSupply cli #####
-cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
-cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building omSupply cli #####
+@REM cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
+@REM cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @ECHO ##### Building connection test utility #####
+@REM cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
+@REM cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omSupply for the desktop #####
-cd "..\client" && yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@cd "..\client" 
+@REM yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd "..\server" && cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omSupply apk #####
+REM copy the keystore and local.properties for apk signing
+copy c:\android\local.properties packages\android\ /y
+copy c:\android\release.keystore packages\android\app\ /y
+copy packages\android\app\build\outputs\apk\release\*.apk ..\omSupply
+
