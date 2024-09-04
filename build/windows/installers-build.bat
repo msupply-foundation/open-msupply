f181069c2749c2417fdaa8fa681fb403b8f94b7a
mark-prins <mark@malcolmprins.net>
2024-09-05 10:14:11 +1200

    rollback testing changes
    Source: https://github.com/msupply-foundation/open-msupply/commit/4d2087ee41be731e2b668d3fb271f8321a6e55cb

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -11,9 +11,8 @@ node "adjustSUFs.js"
 @cd ..
 
 @ECHO ##### Creating installers #####
-@REM start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_server.suf"
-@REM start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_desktop.suf"
-@REM start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_demo.suf"
-@REM start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_standalone.suf"
-mkdir installers
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_server.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_desktop.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_demo.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "omSupply\omsupply_standalone.suf"
 copy omSupply\open-msupply-*.apk installers\Open_mSupply_Android_%versionTag%.apk
--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -18,35 +18,35 @@ start /wait /b build\windows\omsupply-prepare.bat
 
 @ECHO ##### Building omsupply for sqlite #####
 @cd server 
-@REM cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM @ECHO ##### Building sqlite omsupply server #####
-@REM cd cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building sqlite omsupply server #####
+cd cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM @ECHO ##### Building omsupply for postgres #####
-@REM cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omsupply for postgres #####
+cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM @ECHO ##### Building omSupply cli #####
-@REM cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
-@REM cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building omSupply cli #####
+cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
+cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM @ECHO ##### Building connection test utility #####
-@REM cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
-@REM cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+@ECHO ##### Building connection test utility #####
+cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
+cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omSupply for the desktop #####
 @cd "..\client" 
-@REM yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
+@if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omSupply apk #####
-REM copy the keystore and local.properties for apk signing
-copy c:\android\local.properties packages\android\ /y
-copy c:\android\release.keystore packages\android\app\ /y
+@REM copy the keystore and local.properties for apk signing
+copy c:\android\local.properties packages\android\ /y && copy c:\android\release.keystore packages\android\app\ /y
+yarn android:build:release
 copy packages\android\app\build\outputs\apk\release\*.apk ..\omSupply
-
+@if %errorlevel% neq 0 exit /b %errorlevel%
