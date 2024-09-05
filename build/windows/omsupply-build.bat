39f5e21bfe5ecb469c3909606060ec25230a3787
mark-prins <mark@malcolmprins.net>
2024-09-05 14:44:52 +1200

    disable build for testing
    Source: https://github.com/msupply-foundation/open-msupply/commit/ba85203c90033178edab7fce3256ad4eb0f040f0

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -16,33 +16,33 @@ copy "version.txt" "omSupply\version.txt"
 start /wait /b build\windows\omsupply-prepare.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omsupply for sqlite #####
-@cd server 
-cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building omsupply for sqlite #####
+@REM @cd server 
+@REM cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building sqlite omsupply server #####
-cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building sqlite omsupply server #####
+@REM cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omsupply for postgres #####
-cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building omsupply for postgres #####
+@REM cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omSupply cli #####
-cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
-cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building omSupply cli #####
+@REM cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
+@REM cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building connection test utility #####
-cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
-cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @ECHO ##### Building connection test utility #####
+@REM cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
+@REM cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building omSupply for the desktop #####
+@REM @ECHO ##### Building omSupply for the desktop #####
 @cd "..\client" 
-yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@REM yarn electron:build && xcopy "packages\electron\out\open mSupply-win32-x64\**" "..\omSupply\Desktop\" /e /h /c /i
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @ECHO ##### Building omSupply apk #####
 @REM copy the keystore and local.properties for apk signing
