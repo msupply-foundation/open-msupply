a16fcebf57d225ba2d03e21529eb50e90a4d7be8
mark-prins <mark@malcolmprins.net>
2024-09-06 10:54:06 +1200

    another test

--- build/windows/omsupply-android.bat
+++ build/windows/omsupply-android.bat
@@ -1,7 +1,5 @@
 @ECHO ##### Building omSupply apk #####
 @REM copy the keystore and local.properties for apk signing
 copy c:\android\local.properties client\packages\android\ /y && copy c:\android\release.keystore client\packages\android\app\ /y
-cd "client" && yarn android:build:release
-@if %errorlevel% neq 0 exit /b %errorlevel%
-copy client\packages\android\app\build\outputs\apk\release\*.apk omSupply
-@exit /b 0
+cd "client" && yarn android:build:release && copy packages\android\app\build\outputs\apk\release\*.apk ..\omSupply
+@exit /b %errorlevel%
