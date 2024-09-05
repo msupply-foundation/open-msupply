75880612b93bf251440ac138e0393db1c74afd9b
mark-prins <mark@malcolmprins.net>
2024-09-06 10:23:33 +1200

    exit on batch files

--- build/windows/omsupply-android.bat
+++ build/windows/omsupply-android.bat
@@ -4,3 +4,4 @@ copy c:\android\local.properties client\packages\android\ /y && copy c:\android\
 cd "client" && yarn android:build:release
 @if %errorlevel% neq 0 exit /b %errorlevel%
 copy client\packages\android\app\build\outputs\apk\release\*.apk omSupply
+@exit /b 0
--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -13,37 +13,38 @@ xcopy "build\windows\*.*" "omSupply" /c
 xcopy "build\windows\demo" "omSupply\demo" /c /y /i
 copy "version.txt" "omSupply\version.txt"
 
-start /wait /b build\windows\omsupply-prepare.bat
+@REM note: the /b must come first, otherwise the command is not waited
+start /b /wait build\windows\omsupply-prepare.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@cd server 
+@REM @cd server 
 
-@ECHO ##### Building omsupply for sqlite #####
-cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@REM @ECHO ##### Building omsupply for sqlite #####
+@REM cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @ECHO ##### Building sqlite omsupply server #####
+@REM cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @ECHO ##### Building omsupply for postgres #####
+@REM cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @ECHO ##### Building omSupply cli #####
+@REM cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
+@REM cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @ECHO ##### Building connection test utility #####
+@REM cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
+@REM cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
+@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+
+@REM @cd..
+
+start /b /wait build\windows\omsupply-android.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-@ECHO ##### Building sqlite omsupply server #####
-cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
-
-@ECHO ##### Building omsupply for postgres #####
-cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
-
-@ECHO ##### Building omSupply cli #####
-cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
-cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
-
-@ECHO ##### Building connection test utility #####
-cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
-cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
-
-@cd..
-
-start /wait /b build\windows\omsupply-android.bat
-@if %errorlevel% neq 0 exit /b %errorlevel%
-
-start /wait /b build\windows\omsupply-electron.bat
+start /b /wait build\windows\omsupply-electron.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
