588e173b22c516fa012c1e891d89924fc73da693
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-09-06 16:40:15 +1200

    add mock data

---
+++ server/repository/src/mock/encounter.rs
@@ -0,0 +1,15 @@
+use crate::EncounterRow;
+
+use super::mock_program_a;
+
+pub fn mock_encounter_a() -> EncounterRow {
+    EncounterRow {
+        id: "encounter_a".to_string(),
+        program_id: mock_program_a().id,
+        ..Default::default()
+    }
+}
+
+pub fn mock_encounters() -> Vec<EncounterRow> {
+    vec![mock_encounter_a()]
+}
