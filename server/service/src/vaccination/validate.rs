3bd8d5ba87d3385d5496de1ebfb1f57aa811a116
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-09-09 10:59:48 +1200

    more validation steps
    Source: https://github.com/msupply-foundation/open-msupply/commit/fab9075fa27b6db894161ae9db375c8c318dcc95

--- server/service/src/vaccination/validate.rs
+++ server/service/src/vaccination/validate.rs
@@ -1,6 +1,10 @@
 use repository::{
-    vaccine_course::vaccine_course::{VaccineCourseFilter, VaccineCourseRepository},
-    EqualFilter, RepositoryError, StorageConnection, StringFilter, VaccinationRow,
+    vaccine_course::{
+        vaccine_course::{VaccineCourseFilter, VaccineCourseRepository},
+        vaccine_course_dose_row::{VaccineCourseDoseRow, VaccineCourseDoseRowRepository},
+    },
+    EncounterRow, EncounterRowRepository, EqualFilter, ProgramEnrolmentFilter,
+    ProgramEnrolmentRepository, RepositoryError, StorageConnection, StringFilter, VaccinationRow,
     VaccinationRowRepository,
 };
 
@@ -9,6 +13,35 @@ pub fn check_vaccination_exists(
     connection: &StorageConnection,
 ) -> Result<Option<VaccinationRow>, RepositoryError> {
     VaccinationRowRepository::new(connection).find_one_by_id(id)
+}
+
+pub fn check_encounter_exists(
+    id: &str,
+    connection: &StorageConnection,
+) -> Result<Option<EncounterRow>, RepositoryError> {
+    EncounterRowRepository::new(connection).find_one_by_id(id)
+}
+
+pub fn check_program_enrolment_exists(
+    encounter: &EncounterRow,
+    connection: &StorageConnection,
+) -> Result<bool, RepositoryError> {
+    let result = ProgramEnrolmentRepository::new(connection)
+        .query_by_filter(
+            ProgramEnrolmentFilter::new()
+                .program_id(EqualFilter::equal_to(&encounter.program_id))
+                .patient_id(EqualFilter::equal_to(&encounter.patient_link_id)),
+        )?
+        .pop();
+
+    Ok(result.is_some())
+}
+
+pub fn check_vaccine_course_dose_exists(
+    id: &str,
+    connection: &StorageConnection,
+) -> Result<Option<VaccineCourseDoseRow>, RepositoryError> {
+    VaccineCourseDoseRowRepository::new(connection).find_one_by_id(id)
 }
 
 pub fn check_vaccine_course_name_exists_for_program(
