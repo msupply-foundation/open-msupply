fab9075fa27b6db894161ae9db375c8c318dcc95
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-09-09 10:59:48 +1200

    more validation steps

--- server/service/src/vaccination/insert/validate.rs
+++ server/service/src/vaccination/insert/validate.rs
@@ -1,6 +1,9 @@
 use repository::StorageConnection;
 
-use crate::vaccination::validate::check_vaccination_exists;
+use crate::vaccination::validate::{
+    check_encounter_exists, check_program_enrolment_exists, check_vaccination_exists,
+    check_vaccine_course_dose_exists,
+};
 
 use super::{InsertVaccination, InsertVaccinationError};
 
@@ -11,9 +14,15 @@ pub fn validate(
     if check_vaccination_exists(&input.id, connection)?.is_some() {
         return Err(InsertVaccinationError::VaccinationAlreadyExists);
     }
-    // if check_vaccine_course_exists(&input.id, connection)?.is_some() {
-    //     return Err(InsertVaccineCourseError::VaccineCourseAlreadyExists);
-    // }
+    let encounter = check_encounter_exists(&input.encounter_id, connection)?
+        .ok_or(InsertVaccinationError::EncounterDoesNotExist)?;
+
+    if !check_program_enrolment_exists(&encounter, connection)? {
+        return Err(InsertVaccinationError::ProgramEnrolmentDoesNotExist);
+    }
+    if !check_vaccine_course_dose_exists(&input.vaccine_course_dose_id, connection)?.is_none() {
+        return Err(InsertVaccinationError::VaccineCourseDoseDoesNotExist);
+    }
 
     Ok(())
 }
