a5b538f7fea0a9409e0ca83b921745fa76e29e54
James Brunskill <james@sussol.net>
2024-09-10 10:30:29 +1200

    Tidy up naming
    Source: https://github.com/msupply-foundation/open-msupply/commit/4b7cdf9042b08931d0a9f245037a6975c88e4826

--- client/packages/invoices/src/OutboundShipment/api/hooks/utils/useGetItemPricing.ts
+++ client/packages/invoices/src/OutboundShipment/api/hooks/utils/useGetItemPricing.ts
@@ -1,20 +1,18 @@
 import { useGql, useAuthContext, useQuery } from '@openmsupply-client/common';
 import { getSdk } from '../../operations.generated';
 
-// Should only fire when nameId is not null (userQuery has parameter for that)
-// Consumer to also handle error ?
 export const useGetItemPricing = ({
   nameId,
   itemId,
 }: {
-  nameId: string;
+  nameId?: string;
   itemId: string;
 }) => {
   const { client } = useGql();
   const sdk = getSdk(client);
   const { storeId } = useAuthContext();
 
-  const result = useQuery(`discount${storeId}${nameId}${itemId}`, () =>
+  const result = useQuery(`pricing${storeId}${nameId}${itemId}`, () =>
     sdk.getItemPricing({ storeId, input: { nameId, itemId } })
   );
 
