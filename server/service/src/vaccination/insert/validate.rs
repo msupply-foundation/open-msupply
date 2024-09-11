7a6f8fffc533b5e89ce9c0c6f89a43ba8c13c215
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-09-11 14:52:03 +1200

    check doses is not 0

--- server/service/src/vaccination/insert/validate.rs
+++ server/service/src/vaccination/insert/validate.rs
@@ -66,6 +66,14 @@ pub fn validate(
 
             let stock_line = check_stock_line_exists(connection, store_id, stock_line_id)?;
 
+            // This shouldn't be possible (mSupply ensures doses is at least 1 for vaccine items)
+            // but if it happens, we should catch it - otherwise we'll dispense infinity!
+            if stock_line.item_row.vaccine_doses == 0 {
+                return Err(InsertVaccinationError::InternalError(
+                    "Item has no doses defined".to_string(),
+                ));
+            }
+
             if !check_item_belongs_to_vaccine_course(
                 &stock_line.stock_line_row.item_link_id,
                 &vaccine_course_dose
