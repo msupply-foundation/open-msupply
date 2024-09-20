c12633dc88ca6296aae239f1a1f8353a791cfc0c
mark-prins <mark@malcolmprins.net>
2024-09-20 13:09:58 +1200

    remove comment
    Source: https://github.com/msupply-foundation/open-msupply/commit/29b30b00cbeebc3dce28c59270b141b4d4cfbe82

--- server/service/src/report/qr_code.rs
+++ server/service/src/report/qr_code.rs
@@ -14,28 +14,3 @@ pub fn qr_code_as_html_img_src(data: &str) -> String {
 
     format!("{}", image)
 }
-
-// pub fn generate_qr_code(data: &str) -> Vec<u8> {
-//     let code = QrCode::new(data.as_bytes()).unwrap();
-//     let image = code.render::<Luma<u8>>().build();
-//     let mut buf = vec![];
-//     {
-//         let mut cursor = Cursor::new(&mut buf);
-//         let result = image.write_to(&mut cursor, image::ImageFormat::Png);
-
-//         if let Err(e) = result {
-//             log::error!("Failed to write QR code to buffer: {}", e);
-//         }
-//     }
-//     buf
-// }
-
-// pub fn qr_code_as_base64(data: &str) -> String {
-//     let qr_code = generate_qr_code(data);
-//     STANDARD.encode(&qr_code)
-// }
-
-// pub fn qr_code_as_html_img_src(data: &str) -> String {
-//     let base64 = qr_code_as_base64(data);
-//     format!("data:image/png;base64,{}", base64)
-// }
