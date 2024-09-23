76d927ef5a311ccdc52731f1ad55e9f138e07078
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2024-09-23 17:05:46 +1200

    Update server/service/src/sync/translations/report.rs
    Co-authored-by: James Brunskill <james@sussol.net>

--- server/service/src/sync/translations/report.rs
+++ server/service/src/sync/translations/report.rs
@@ -121,9 +121,9 @@ impl SyncTranslation for ReportTranslation {
             comment,
             sub_context,
             argument_schema_id,
-            is_custom: todo!(),
-            version: todo!(),
-            code: todo!(),
+            is_custom: true,
+            version: 1.0.to_string(),
+            code: id,
         };
 
         Ok(PullTranslateResult::upsert(result))
