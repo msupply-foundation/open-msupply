875d51f9060ab69309c4122433fded35b83dbab8
roxy-dao <roxy@msupply.foundation>
2024-09-24 13:17:08 +1200

    Move stocktake line delete into respective folder/file
    Source: https://github.com/msupply-foundation/open-msupply/commit/1ccbb1d8a60f25edf53b99ccde85012314f4c511

--- server/service/src/stocktake_line/delete/mod.rs
+++ server/service/src/stocktake_line/delete/mod.rs
@@ -1,3 +1,6 @@
+mod validate;
+pub use validate::*;
+
 use repository::{
     RepositoryError, StocktakeLineRowRepository, StorageConnection, TransactionError,
 };
@@ -17,38 +20,6 @@ pub enum DeleteStocktakeLineError {
     StocktakeLineDoesNotExist,
     CannotEditFinalised,
     StocktakeIsLocked,
-}
-
-fn validate(
-    connection: &StorageConnection,
-    store_id: &str,
-    stocktake_line_id: &str,
-) -> Result<(), DeleteStocktakeLineError> {
-    let line = match check_stocktake_line_exist(connection, stocktake_line_id)? {
-        Some(line) => line.line,
-        None => return Err(DeleteStocktakeLineError::StocktakeLineDoesNotExist),
-    };
-    let stocktake = match check_stocktake_exist(connection, &line.stocktake_id)? {
-        Some(stocktake) => stocktake,
-        None => {
-            return Err(DeleteStocktakeLineError::InternalError(format!(
-                "Stocktake is missing: {}",
-                line.stocktake_id
-            )))
-        }
-    };
-
-    if stocktake.is_locked {
-        return Err(DeleteStocktakeLineError::StocktakeIsLocked);
-    }
-
-    if !check_stocktake_not_finalised(&stocktake.status) {
-        return Err(DeleteStocktakeLineError::CannotEditFinalised);
-    }
-    if !check_store_id_matches(store_id, &stocktake.store_id) {
-        return Err(DeleteStocktakeLineError::InvalidStore);
-    }
-    Ok(())
 }
 
 /// Returns the id of the deleted stocktake_line
