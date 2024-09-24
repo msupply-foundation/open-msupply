99bc45e9e33dbc87c9c7cba7fe5075aed1b61be5
Fergus Roache <fergus@msupply.foundation>
2024-09-25 09:06:01 +1200

    Fix PR to include real values
    Source: https://github.com/msupply-foundation/open-msupply/commit/58f4aff6d292803de84c9a88becd7876b176e75d

--- server/service/src/sync/test/test_data/report.rs
+++ server/service/src/sync/test/test_data/report.rs
@@ -39,9 +39,9 @@ pub(crate) fn test_pull_upsert_records() -> Vec<TestSyncIncomingRecord> {
             comment: Some("Test comment".to_string()),
             sub_context: None,
             argument_schema_id: None,
-            is_custom: false,
+            is_custom: true,
             version: "1.0".to_string(),
-            code: None,
+            code: Some(REPORT_1.0.to_string()),
         },
     )]
 }
