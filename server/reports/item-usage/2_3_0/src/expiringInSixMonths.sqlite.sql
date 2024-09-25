7b1c78774a7ed17586724e6c89eba1ff99230f54
Fergus Roache <fergus@msupply.foundation>
2024-09-25 14:07:11 +1200

    Add standard report src to oms repo
    Source: https://github.com/msupply-foundation/open-msupply/commit/45621ec9e474111c33eacbbeaa562f5b7d6f3dbf

---
+++ server/reports/item-usage/2_3_0/src/expiringInSixMonths.sqlite.sql
@@ -0,0 +1,15 @@
+WITH 
+    this_month AS (
+        SELECT date('now') AS this_month
+    ),
+    six_months AS (
+        SELECT date('now', '+6 months') AS six_months
+    )
+SELECT 
+    stock_line.id,
+    i.item_id,
+    SUM(stock_line.available_number_of_packs) AS quantity
+FROM stock_line, six_months, this_month
+INNER JOIN item_link i ON i.id = stock_line.item_link_id
+WHERE stock_line.expiry_date >= this_month AND stock_line.expiry_date < six_months AND stock_line.store_id = $storeId
+GROUP BY i.item_id
