6045971aaada5f667b1d9e36d0ac3164909bfe96
roxy-dao <roxy@msupply.foundation>
2024-10-15 11:21:54 +1300

    Revert "Add check for if report query params have changed before setting is loading state"
    This reverts commit a98db46f97eb4b11d99f105e9b96daf3e0c0c6f5.
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/5721f3f9ce2d0acd3cb3c09df8bf062e3c1f9a08

--- client/packages/system/src/Report/components/ReportArgumentsModal.tsx
+++ client/packages/system/src/Report/components/ReportArgumentsModal.tsx
@@ -11,19 +11,16 @@ export type ReportArgumentsModalProps = {
   report: ReportRowFragment | undefined;
   onReset: () => void;
   onArgumentsSelected: (report: ReportRowFragment, args: JsonData) => void;
-  onSetLoading?: (value: React.SetStateAction<boolean>) => void
 };
 
 export const ReportArgumentsModal: FC<ReportArgumentsModalProps> = ({
   report,
   onReset,
   onArgumentsSelected,
-  onSetLoading,
 }) => {
   const { store } = useAuthContext();
   const { urlQuery } = useUrlQuery();
   const t = useTranslation();
-  const [isDirty, setIsDirty] = useState(false);
 
   const {
     monthlyConsumptionLookBackPeriod,
@@ -44,9 +41,6 @@ export const ReportArgumentsModal: FC<ReportArgumentsModalProps> = ({
   // clean up when modal is closed
   const cleanUp = () => {
     onReset();
-    if (isDirty) {
-      onSetLoading;
-    }
   };
 
   const { Modal } = useDialog({
@@ -87,10 +81,7 @@ export const ReportArgumentsModal: FC<ReportArgumentsModalProps> = ({
           isLoading={false}
           setError={err => setError(err)}
           updateData={(newData: JsonData) => {
-            if (newData != data) {
             setData(newData);
-            setIsDirty(true);
-            } 
           }}
         />
       </>
