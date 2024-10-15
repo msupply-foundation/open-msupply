057641717edc2053a41b1547d2e318d2e22a7138
Fergus Roache <fergus@msupply.foundation>
2024-10-15 16:57:21 +1300

    Add formatting
    Source: https://github.com/msupply-foundation/open-msupply/commit/9bb8a0b29266591432aa18e1c44e787f6fc5b898

--- server/reports/expiring-items/2_3_0/convert_data_js/convert_data.js
+++ server/reports/expiring-items/2_3_0/convert_data_js/convert_data.js
@@ -6,58 +6,74 @@ function convert_data() {
 }
 
 const processStockLines = (nodes) => {
-    nodes.forEach((line) => {
-        line.daysUntilExpired = calculateDaysUntilExpired(line.expiryDate);
-        line.expectedUsage = addExpectedUsage(line);
-        line.stockAtRisk = addStockAtRisk(line);
-        line.daysUntilExpired = roundDaysToInteger(line.daysUntilExpired)
-    });
-    return nodes;
-}
+  nodes.forEach((line) => {
+    line.daysUntilExpired = calculateDaysUntilExpired(line.expiryDate);
+    line.expectedUsage = calculateExpectedUsage(
+      line.daysUntilExpired,
+      line.averageMonthlyConsumption
+    );
+    line.stockAtRisk = addStockAtRisk(line);
+    line.daysUntilExpired = roundDaysToInteger(line.daysUntilExpired);
+  });
+  return nodes;
+};
 
 const calculateDaysUntilExpired = (expiryDateString) => {
-    let daysUntilExpired = undefined;
-    if (!!expiryDateString) {
-        let now = Date.now();
-        daysUntilExpired = (new Date(expiryDateString) - now) / 1000 / 60 / 60 / 24;
-    } 
-    return daysUntilExpired
-}
+  let daysUntilExpired = undefined;
+  if (!!expiryDateString) {
+    let now = Date.now();
+    daysUntilExpired = (new Date(expiryDateString) - now) / 1000 / 60 / 60 / 24;
+  }
+  return daysUntilExpired;
+};
 
-const addExpectedUsage = (line) => {
-    return undefined;
-}
+const calculateExpectedUsage = (
+  daysUntilExpired,
+  averageMonthlyConsumption
+) => {
+  return undefined;
+};
 
 const addStockAtRisk = (line) => {
-    return undefined;
-}
+  return undefined;
+};
 
 const calculateStockAtRisk = (line) => {
-    if (line.item.stats.averageMonthlyConsumption && !!line.expiryDate) {
-        if (line.daysUntilExpired > 0) {
-            line.expectedUsage = line.item.stats.averageMonthlyConsumption * (line.daysUntilExpired / 30);
-            line.stockAtRisk = line.totalNumberOfPacks * line.packSize - line.expectedUsage;
-        } else {
-            line.stockAtRisk = line.totalNumberOfPacks * line.packSize;
-        }
+  if (line.item.stats.averageMonthlyConsumption && !!line.expiryDate) {
+    if (line.daysUntilExpired > 0) {
+      line.expectedUsage =
+        line.item.stats.averageMonthlyConsumption *
+        (line.daysUntilExpired / 30);
+      line.stockAtRisk =
+        line.totalNumberOfPacks * line.packSize - line.expectedUsage;
+    } else {
+      line.stockAtRisk = line.totalNumberOfPacks * line.packSize;
     }
-    return line
-}
-const calculateStockAtRiskIfNoMonthlyConsumption = (line) => {        
-    if (line && line.expiryDate && !line.item.stats.averageMonthlyConsumption) {
-        if (line.daysUntilExpired && line.daysUntilExpired < 0) {
-            line.stockAtRisk = line.totalNumberOfPacks * line.packSize;
-        }
-    } ;
-    return line
-}
+  }
+  return line;
+};
+const calculateStockAtRiskIfNoMonthlyConsumption = (line) => {
+  if (line && line.expiryDate && !line.item.stats.averageMonthlyConsumption) {
+    if (line.daysUntilExpired && line.daysUntilExpired < 0) {
+      line.stockAtRisk = line.totalNumberOfPacks * line.packSize;
+    }
+  }
+  return line;
+};
 const roundDaysToInteger = (daysUntilExpired) => {
-    let rounded = undefined;
-    if (!!daysUntilExpired) {
-        rounded = Math.round(daysUntilExpired);
-    }
-    return rounded
-}
+  let rounded = undefined;
+  if (!!daysUntilExpired) {
+    rounded = Math.round(daysUntilExpired);
+  }
+  return rounded;
+};
 
-module.exports = { convert_data, processStockLines, calculateDaysUntilExpired, calculateStockAtRisk, calculateStockAtRiskIfNoMonthlyConsumption, roundDaysToInteger };
-
+module.exports = {
+  calculateExpectedUsage,
+  convert_data,
+  processStockLines,
+  calculateDaysUntilExpired,
+  calculateStockAtRisk,
+  calculateStockAtRiskIfNoMonthlyConsumption,
+  roundDaysToInteger,
+};
