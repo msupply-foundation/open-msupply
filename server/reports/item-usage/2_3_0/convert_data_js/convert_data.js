becd2fe34e5a2162a1d8711e88c0c1a88c4dd10c
Fergus Roache <fergus@msupply.foundation>
2024-10-17 12:50:51 +1300

    Add end to end testing

--- server/reports/item-usage/2_3_0/convert_data_js/convert_data.js
+++ server/reports/item-usage/2_3_0/convert_data_js/convert_data.js
@@ -5,10 +5,32 @@ function convert_data() {
 }
 
 const processItemLines = (res) => {
-  res.items.nodes.forEach((line) => {
-    line.monthConsumption = calculateQuantity(res.monthConsumption, item.id);
-  })
-}
+  res.items.nodes.forEach((item) => {
+    item.monthConsumption = calculateQuantity(res.thisMonthConsumption, item.id);
+    item.lastMonthConsumption = calculateQuantity(
+      res.lastMonthConsumption,
+      item.id
+    );
+    item.twoMonthsAgoConsumption = calculateQuantity(
+      res.twoMonthsAgoConsumption,
+      item.id
+    );
+    item.expiringInSixMonths = calculateQuantity(
+      res.expiringInSixMonths,
+      item.id
+    );
+    item.expiringInTwelveMonths = calculateQuantity(
+      res.expiringInTwelveMonths,
+      item.id
+    );
+    item.stockOnOrder = calculateQuantity(res.stockOnOrder, item.id);
+    item.AMCTwelve = calculateQuantity(res.AMCTwelve, item.id);
+    item.AMCTwentyFour = calculateQuantity(res.AMCTwentyFour, item.id);
+    item.SOH = calculateStatValue(item.stats.availableStockOnHand);
+    item.MOS = calculateStatValue(item.stats.availableMonthsOfStockOnHand);
+  });
+  return res.items.nodes;
+};
 
 // function adds month consumption to data  (either this or last month)
 const calculateQuantity = (queryResult, id) => {
@@ -18,17 +40,20 @@ const calculateQuantity = (queryResult, id) => {
     quantity = node?.quantity ? node.quantity : 0;
   }
   return quantity;
-}
+};
 
-calculateStatValue = (value) => {
+const calculateStatValue = (value) => {
   let returnValue = 0;
   if (!!value) {
     // round to 1 decimal
     returnValue = Math.round(value * 10) / 10;
   }
   return returnValue;
-}
+};
 
-
-
-module.exports = { convert_data, calculateQuantity, calculateStatValue };
+module.exports = {
+  convert_data,
+  calculateQuantity,
+  calculateStatValue,
+  processItemLines,
+};
