5bc6dc03d91a0c9b8b640c8baf6da867c16c651c
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-10-16 13:08:33 +1300

    add rnr form edit fields to side panel

--- client/packages/requisitions/src/RnRForms/DetailView/SidePanel.tsx
+++ client/packages/requisitions/src/RnRForms/DetailView/SidePanel.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {
+  BufferedTextArea,
+  BufferedTextInput,
   CopyIcon,
   DetailPanelAction,
   DetailPanelPortal,
@@ -8,22 +10,30 @@ import {
   PanelField,
   PanelLabel,
   PanelRow,
+  RnRFormNodeStatus,
   useFormatDateTime,
   useNotification,
   useTranslation,
 } from '@openmsupply-client/common';
-import { RnRFormFragment } from '../api';
+import { useRnRForm } from '../api';
 
-export const SidePanel = ({ data }: { data: RnRFormFragment }) => {
+export const SidePanel = ({ rnrFormId }: { rnrFormId: string }) => {
   const { success } = useNotification();
   const t = useTranslation('distribution');
   const { localisedDate } = useFormatDateTime();
+  const {
+    query: { data },
+    bufferedDetails,
+    updateRnRForm,
+  } = useRnRForm({ rnrFormId });
 
   const copyToClipboard = () => {
     navigator.clipboard
-      .writeText(JSON.stringify(data, null, 4) ?? '')
+      .writeText(JSON.stringify(data ?? {}, null, 4) ?? '')
       .then(() => success(t('message.copy-success'))());
   };
+
+  if (!data) return null;
 
   return (
     <DetailPanelPortal
@@ -52,6 +62,31 @@ export const SidePanel = ({ data }: { data: RnRFormFragment }) => {
           <PanelRow>
             <PanelLabel>{t('label.created')}</PanelLabel>
             <PanelField>{localisedDate(data.createdDatetime)}</PanelField>
+          </PanelRow>
+          <PanelRow>
+            <PanelLabel flex={0.4}>{t('heading.reference')}</PanelLabel>
+            <PanelField>
+              <BufferedTextInput
+                disabled={data.status === RnRFormNodeStatus.Finalised}
+                onChange={e =>
+                  updateRnRForm({ theirReference: e.target.value })
+                }
+                value={bufferedDetails?.theirReference ?? ''}
+                InputProps={{
+                  sx: { backgroundColor: 'white' },
+                }}
+              />
+            </PanelField>
+          </PanelRow>
+          <PanelRow>
+            <PanelLabel flex={0.4}>{t('heading.comment')}</PanelLabel>
+            <PanelField>
+              <BufferedTextArea
+                disabled={data.status === RnRFormNodeStatus.Finalised}
+                onChange={e => updateRnRForm({ comment: e.target.value })}
+                value={bufferedDetails?.comment ?? ''}
+              />
+            </PanelField>
           </PanelRow>
         </Grid>
       </DetailPanelSection>
