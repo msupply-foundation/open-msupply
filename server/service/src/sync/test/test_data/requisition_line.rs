ff09c15d944022ba30aab960ec8dcd07a2c89d6a
Chris Petty <chris@sussol.net>
2024-10-17 17:28:14 +1300

    translation and tests
    Source: https://github.com/msupply-foundation/open-msupply/commit/535d7a4edca9758cb1e5a9ba053af5b76876c5c3

--- server/service/src/sync/test/test_data/requisition_line.rs
+++ server/service/src/sync/test/test_data/requisition_line.rs
@@ -41,7 +41,9 @@ const REQUISITION_LINE_1: (&str, &str) = (
         "requestedPackSize": 0,
         "approved_quantity": 0,
         "authoriser_comment": "",
-        "om_snapshot_datetime": ""
+        "om_snapshot_datetime": "",
+        "available": 0,
+        "MOS": 0
     }"#,
 );
 fn requisition_line_request_pull_record() -> TestSyncIncomingRecord {
@@ -62,6 +64,16 @@ fn requisition_line_request_pull_record() -> TestSyncIncomingRecord {
             approved_quantity: 0.0,
             approval_comment: None,
             item_name: "Ibuprofen 200mg tablets".to_string(),
+            initial_stock_on_hand_units: 0.0,
+            incoming_units: 0.0,
+            outgoing_units: 0.0,
+            loss_in_units: 0.0,
+            addition_in_units: 0.0,
+            available_units: 0.0,
+            expiring_units: 0.0,
+            days_out_of_stock: 0.0,
+            months_of_stock: 0.0,
+            option_id: None,
         },
     )
 }
@@ -82,7 +94,17 @@ fn requisition_line_request_push_record() -> TestSyncOutgoingRecord {
             snapshot_datetime: None,
             approved_quantity: 0.0,
             approval_comment: None,
-            item_name: "Ibuprofen 200mg tablets".to_string()
+            item_name: "Ibuprofen 200mg tablets".to_string(),
+            cust_prev_stock_balance: 0.0,
+            cust_stock_received: 0.0,
+            cust_stock_issued: 0.0,
+            stock_losses: 0.0,
+            stock_additions: 0.0,
+            available: 0.0,
+            stock_expiring: 0.0,
+            days_out_or_new_demand: 0.0,
+            months_of_stock: 0.0,
+            option_id: None
         }),
     }
 }
@@ -120,7 +142,9 @@ const REQUISITION_LINE_OM_FIELD: (&str, &str) = (
         "requestedPackSize": 0,
         "approved_quantity": 0,
         "authoriser_comment": "approval comment",
-        "om_snapshot_datetime": "2022-04-04T14:48:11"
+        "om_snapshot_datetime": "2022-04-04T14:48:11",
+        "available": 0,
+        "MOS": 0
     }"#,
 );
 fn requisition_line_om_fields_pull_record() -> TestSyncIncomingRecord {
@@ -146,6 +170,16 @@ fn requisition_line_om_fields_pull_record() -> TestSyncIncomingRecord {
                     .unwrap(),
             ),
             item_name: "Ibuprofen 200mg tablets".to_string(),
+            initial_stock_on_hand_units: 0.0,
+            incoming_units: 0.0,
+            outgoing_units: 0.0,
+            loss_in_units: 0.0,
+            addition_in_units: 0.0,
+            available_units: 0.0,
+            expiring_units: 0.0,
+            days_out_of_stock: 0.0,
+            months_of_stock: 0.0,
+            option_id: None,
         },
     )
 }
@@ -172,6 +206,16 @@ fn requisition_line_om_fields_push_record() -> TestSyncOutgoingRecord {
                     .and_hms_opt(14, 48, 11)
                     .unwrap()
             ),
+            cust_prev_stock_balance: 0.0,
+            cust_stock_received: 0.0,
+            cust_stock_issued: 0.0,
+            stock_losses: 0.0,
+            stock_additions: 0.0,
+            available: 0.0,
+            stock_expiring: 0.0,
+            days_out_or_new_demand: 0.0,
+            months_of_stock: 0.0,
+            option_id: None
         }),
     }
 }
