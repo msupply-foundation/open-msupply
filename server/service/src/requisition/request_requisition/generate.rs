8b8dfab0d5be2a9d92573442f748cf956c70d9d8
Chris Petty <chris@sussol.net>
2024-10-17 16:55:02 +1300

    update various usage of struct
    Source: https://github.com/msupply-foundation/open-msupply/commit/a0b1bd1d41958a6630b78de8a750fa4322b1353c

--- server/service/src/requisition/request_requisition/generate.rs
+++ server/service/src/requisition/request_requisition/generate.rs
@@ -78,6 +78,16 @@ pub fn generate_requisition_lines(
                 requested_quantity: 0.0,
                 approved_quantity: 0.0,
                 approval_comment: None,
+                initial_stock_on_hand_units: 0.0,
+                incoming_units: 0.0,
+                outgoing_units: 0.0,
+                loss_in_units: 0.0,
+                addition_in_units: 0.0,
+                available_units: 0.0,
+                expiring_units: 0.0,
+                days_out_of_stock: 0.0,
+                months_of_stock: 0.0,
+                option_id: None,
             }
         })
         .collect();
