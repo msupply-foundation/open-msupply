34395914eb54af98e72b34b97c8b6e87df3af717
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-10-21 12:53:26 +1300

    migrate new R&R form columns
    Source: https://github.com/msupply-foundation/open-msupply/commit/6c91f5c9f442ab9e6a99ab77ab9aa88bc146cfb9

---
+++ server/repository/src/migrations/v2_03_01/add_rnr_columns.rs
@@ -0,0 +1,22 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_rnr_columns"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            // Note: We don't have a `snapshot_losses` - not required for system to pre-calculate this column at this stage
+            r#"
+                ALTER TABLE rnr_form_line ADD COLUMN entered_losses {DOUBLE} DEFAULT 0;
+                ALTER TABLE rnr_form_line ADD COLUMN minimum_quantity {DOUBLE} NOT NULL DEFAULT 0;
+            "#
+        )?;
+
+        Ok(())
+    }
+}
