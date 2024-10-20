e8d7d3e738cf949e1812e8211668381681050354
Fergus Roache <fergus@msupply.foundation>
2024-10-21 12:37:06 +1300

    Fix item-usage convert data to handle empty values
    Source: https://github.com/msupply-foundation/open-msupply/commit/1af66e95bcd96b91cd17caa18c048611f1352fa6

--- server/reports/item-usage/2_3_0/convert_data_js/convert_data.js
+++ server/reports/item-usage/2_3_0/convert_data_js/convert_data.js
@@ -6,6 +6,10 @@ function convert_data() {
 
 const processItemLines = (res) => {
   res.items.nodes.forEach((item) => {
+    // don't add default values if empty object added
+    if (Object.keys(item).length == 0) {
+      return;
+    }
     item.monthConsumption = calculateQuantity(
       res.thisMonthConsumption,
       item.id
@@ -29,10 +33,11 @@ const processItemLines = (res) => {
     item.stockOnOrder = calculateQuantity(res.stockOnOrder, item.id);
     item.AMC12 = calculateQuantity(res.AMCTwelve, item.id);
     item.AMC24 = calculateQuantity(res.AMCTwentyFour, item.id);
-    item.SOH = calculateStatValue(item.stats.availableStockOnHand);
-    item.MOS = calculateStatValue(item.stats.availableMonthsOfStockOnHand);
+    item.SOH = calculateStatValue(item?.stats?.availableStockOnHand);
+    item.MOS = calculateStatValue(item?.stats?.availableMonthsOfStockOnHand);
   });
-  return res.items.nodes;
+  let cleanNodes = cleanUpNodes(res.items.nodes);
+  return cleanNodes;
 };
 
 // function adds month consumption to data  (either this or last month)
@@ -54,9 +59,37 @@ const calculateStatValue = (value) => {
   return returnValue;
 };
 
+const cleanUpNodes = (nodes) => {
+  let cleanNodes = [];
+  nodes.forEach((node) => {
+    if (Object.keys(node).length != 0) {
+      cleanNodes.push(cleanUpObject(node));
+    }
+  });
+  return cleanNodes;
+};
+
+const cleanUpObject = (node) => {
+  let newNode = {};
+  // remove empty keys which will fail to parse
+  Object.keys(node).forEach(function (key) {
+    if (node[key] !== "" && node[key] !== undefined && node[key] !== null) {
+      if (typeof node[key] === "object") {
+        // recursively remove empty strings or undefined from graphql query
+        newNode[key] = cleanUpObject(node[key]);
+      } else {
+        newNode[key] = node[key];
+      }
+    }
+  });
+  return newNode;
+};
+
 module.exports = {
   convert_data,
   calculateQuantity,
   calculateStatValue,
   processItemLines,
+  cleanUpObject,
+  cleanUpNodes,
 };
