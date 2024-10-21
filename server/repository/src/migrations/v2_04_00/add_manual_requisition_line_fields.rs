ccde3684ae610be1ab4773d9f0ae364ab2569421
Chris Petty <chris@sussol.net>
2024-10-21 16:40:54 +1300

    shouldn't have been only migrating for pgsql!
    Source: https://github.com/msupply-foundation/open-msupply/commit/95b4539ae996aff6262f1283817b58749a440816

--- server/repository/src/migrations/v2_04_00/add_manual_requisition_line_fields.rs
+++ server/repository/src/migrations/v2_04_00/add_manual_requisition_line_fields.rs
@@ -8,10 +8,9 @@ impl MigrationFragment for Migrate {
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
-        if cfg!(feature = "postgres") {
-            sql!(
-                connection,
-                r#"
+        sql!(
+            connection,
+            r#"
                 ALTER TABLE requisition_line ADD initial_stock_on_hand_units {DOUBLE} NOT NULL DEFAULT 0;
                 ALTER TABLE requisition_line ADD incoming_units {DOUBLE} NOT NULL DEFAULT 0;
                 ALTER TABLE requisition_line ADD outgoing_units {DOUBLE} NOT NULL DEFAULT 0;
@@ -23,8 +22,7 @@ impl MigrationFragment for Migrate {
                 ALTER TABLE requisition_line ADD months_of_stock {DOUBLE} NOT NULL DEFAULT 0;
                 ALTER TABLE requisition_line ADD option_ID REFERENCES return_reason(id);
                 "#
-            )?;
-        }
+        )?;
 
         Ok(())
     }
