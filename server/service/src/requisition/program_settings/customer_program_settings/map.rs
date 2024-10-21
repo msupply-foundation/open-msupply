7d3313f70b061da7c0c7c7f0b51167e4cd0005bc
roxy-dao <roxy@msupply.foundation>
2024-10-22 12:18:13 +1300

    Add tests for customer program settings

--- server/service/src/requisition/program_settings/customer_program_settings/map.rs
+++ server/service/src/requisition/program_settings/customer_program_settings/map.rs
@@ -59,3 +59,25 @@ pub(super) fn map_customer_program_settings(
         })
         .collect()
 }
+
+#[test]
+fn test_reduce_and_sort_periods() {
+    use repository::PeriodRow;
+
+    fn make_date(offset: &i32) -> PeriodRow {
+        PeriodRow {
+            id: offset.to_string(),
+            start_date: util::date_now_with_offset(chrono::Duration::days(*offset as i64)),
+            ..PeriodRow::default()
+        }
+    }
+
+    let periods: Vec<PeriodRow> = [3, -10, -2, -5, 10, 11, 2, 4, -4, -10, 20, 21]
+        .iter()
+        .map(make_date)
+        .collect();
+
+    let result: Vec<PeriodRow> = [-4, -2, 2, 3].iter().map(make_date).collect();
+
+    assert_eq!(reduce_and_sort_periods(periods), result)
+}
