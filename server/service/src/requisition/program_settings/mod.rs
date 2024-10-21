87b28c5c519293a183d5ee6bb3ea023ddea6e422
roxy-dao <roxy@msupply.foundation>
2024-10-22 11:16:03 +1300

    Function to get customer program settings

--- server/service/src/requisition/program_settings/mod.rs
+++ server/service/src/requisition/program_settings/mod.rs
@@ -1,4 +1,6 @@
 mod common;
+pub mod customer_program_settings;
 pub mod supplier_program_settings;
 
+pub use customer_program_settings::get_customer_program_requisition_settings;
 pub use supplier_program_settings::get_supplier_program_requisition_settings;
