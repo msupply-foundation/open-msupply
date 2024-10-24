2445b96846341301d23978d33e2b99efe0dd2387
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-10-25 10:30:08 +1300

    manufacturer search input
    Source: https://github.com/msupply-foundation/open-msupply/commit/27a0fa549928a6734743f27d83b69d1716ad16da

---
+++ client/packages/system/src/Name/Components/ManufacturerSearchInput/ManufacturerSearchInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Autocomplete,
+  useBufferState,
+  useTranslation,
+} from '@openmsupply-client/common';
+import { useName } from '../../api';
+import {
+  basicFilterOptions,
+  filterByNameAndCode,
+  NameSearchInputProps,
+} from '../../utils';
+import { getNameOptionRenderer } from '../NameOptionRenderer';
+
+export const ManufacturerSearchInput = ({
+  onChange,
+  width = 250,
+  value,
+  disabled = false,
+}: NameSearchInputProps) => {
+  // TODO: currently using supplier list, should be manufacturers!
+  const { data, isLoading } = useName.document.suppliers();
+  const [buffer, setBuffer] = useBufferState(value);
+  const t = useTranslation();
+  const NameOptionRenderer = getNameOptionRenderer(t('label.on-hold'));
+
+  return (
+    <Autocomplete
+      disabled={disabled}
+      clearable={false}
+      value={buffer && { ...buffer, label: buffer.name }}
+      filterOptionConfig={basicFilterOptions}
+      filterOptions={filterByNameAndCode}
+      loading={isLoading}
+      onChange={(_, name) => {
+        setBuffer(name);
+        name && onChange(name);
+      }}
+      options={data?.nodes ?? []}
+      renderOption={NameOptionRenderer}
+      width={`${width}px`}
+      popperMinWidth={width}
+      isOptionEqualToValue={(option, value) => option?.id === value?.id}
+      getOptionDisabled={option => option.isOnHold}
+    />
+  );
+};
