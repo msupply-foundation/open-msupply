cb8a32c4d02cf177529beffe8e75db9666aa56c8
Chris Petty <chris@sussol.net>
2024-10-25 08:22:44 +1300

    testing boilerplate :X
    Source: https://github.com/msupply-foundation/open-msupply/commit/67af9f84ee3f655cf7cd33236fdc2c3e4c459985

---
+++ server/repository/src/mock/program_indicator.rs
@@ -0,0 +1,36 @@
+use crate::ProgramIndicatorRow;
+
+use super::{mock_program_a, mock_program_b};
+
+pub fn mock_program_indicator_a() -> ProgramIndicatorRow {
+    ProgramIndicatorRow {
+        id: "program_indicator_a".to_string(),
+        program_id: mock_program_a().id,
+        code: "program indicator a".to_string(),
+        is_active: true,
+    }
+}
+pub fn mock_program_indicator_b() -> ProgramIndicatorRow {
+    ProgramIndicatorRow {
+        id: "program_indicator_b".to_string(),
+        program_id: mock_program_a().id,
+        code: "program indicator a".to_string(),
+        is_active: true,
+    }
+}
+pub fn mock_program_indicator_c() -> ProgramIndicatorRow {
+    ProgramIndicatorRow {
+        id: "program_indicator_c".to_string(),
+        program_id: mock_program_b().id,
+        code: "program indicator a".to_string(),
+        is_active: false,
+    }
+}
+
+pub fn mock_program_indicators() -> Vec<ProgramIndicatorRow> {
+    vec![
+        mock_program_indicator_a(),
+        mock_program_indicator_b(),
+        mock_program_indicator_c(),
+    ]
+}
