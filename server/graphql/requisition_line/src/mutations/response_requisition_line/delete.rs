32c14fb4494fc7e72804a12ca10e200a7f3a946e
Fergus Roache <fergus@msupply.foundation>
2024-10-24 14:40:32 +1300

    Add tests to graphql layer

--- server/graphql/requisition_line/src/mutations/response_requisition_line/delete.rs
+++ server/graphql/requisition_line/src/mutations/response_requisition_line/delete.rs
@@ -93,6 +93,11 @@ fn map_error(error: ServiceError) -> Result<DeleteErrorInterface> {
                 ForeignKeyError(ForeignKey::RequisitionId),
             ))
         }
+        ServiceError::CannotEditRequisition => {
+            return Ok(DeleteErrorInterface::CannotEditRequisition(
+                CannotEditRequisition {},
+            ))
+        }
         // Standard Graphql Errors
         ServiceError::NotThisStoreRequisition => BadUserInput(formatted_error),
         ServiceError::NotAResponseRequisition => BadUserInput(formatted_error),
@@ -100,4 +105,220 @@ fn map_error(error: ServiceError) -> Result<DeleteErrorInterface> {
     };
 
     Err(graphql_error.extend())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::RequisitionLineMutations;
+    use async_graphql::EmptyMutation;
+    use graphql_core::{
+        assert_graphql_query, assert_standard_graphql_error, test_helpers::setup_graphql_test,
+    };
+    use repository::{mock::MockDataInserts, StorageConnectionManager};
+    use serde_json::json;
+
+    use service::{
+        requisition_line::{
+            response_requisition_line::{
+                DeleteResponseRequisitionLine as ServiceInput,
+                DeleteResponseRequisitionLineError as ServiceError,
+            },
+            RequisitionLineServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+
+    type DeleteLineMethod = dyn Fn(ServiceInput) -> Result<String, ServiceError> + Sync + Send;
+
+    pub struct TestService(pub Box<DeleteLineMethod>);
+
+    impl RequisitionLineServiceTrait for TestService {
+        fn delete_response_requisition_line(
+            &self,
+            _: &ServiceContext,
+            input: ServiceInput,
+        ) -> Result<String, ServiceError> {
+            self.0(input)
+        }
+    }
+
+    fn service_provider(
+        test_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone(), "app_data");
+        service_provider.requisition_line_service = Box::new(test_service);
+        service_provider
+    }
+
+    fn empty_variables() -> serde_json::Value {
+        json!({
+          "input": {
+            "id": "n/a",
+          },
+          "storeId": "n/a"
+        })
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_update_response_requisition_line_errors() {
+        let (_, _, connection_manager, settings) = setup_graphql_test(
+            EmptyMutation,
+            RequisitionLineMutations,
+            "test_graphql_delete_response_requisition_line_structured_errors",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: DeleteResponseRequisitionLineInput!, $storeId: String) {
+            deleteResponseRequisitionLine(storeId: $storeId, input: $input) {
+              ... on DeleteResponseRequisitionLineError {
+                error {
+                  __typename
+                }
+              }
+            }
+          }
+        "#;
+
+        // RecordNotFound
+        let test_service =
+            TestService(Box::new(|_| Err(ServiceError::RequisitionLineDoesNotExist)));
+
+        let expected = json!({
+            "deleteResponseRequisitionLine": {
+              "error": {
+                "__typename": "RecordNotFound"
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // RequisitionDoesNotExist
+        let test_service = TestService(Box::new(|_| Err(ServiceError::RequisitionDoesNotExist)));
+
+        let expected = json!({
+            "deleteResponseRequisitionLine": {
+              "error": {
+                "__typename": "ForeignKeyError"
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // CannotEditRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::CannotEditRequisition)));
+
+        let expected = json!({
+            "deleteResponseRequisitionLine": {
+              "error": {
+                "__typename": "CannotEditRequisition"
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // NotThisStoreRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::NotThisStoreRequisition)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // NotAResponseRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::NotAResponseRequisition)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_delete_response_requisition_line_success() {
+        let (_, _, connection_manager, settings) = setup_graphql_test(
+            EmptyMutation,
+            RequisitionLineMutations,
+            "test_graphql_delete_response_requisition_line_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($storeId: String, $input: DeleteResponseRequisitionLineInput!) {
+            deleteResponseRequisitionLine(storeId: $storeId, input: $input) {
+                ... on DeleteResponse {
+                    id
+                }
+            }
+          }
+        "#;
+
+        // Success
+        let test_service = TestService(Box::new(|input| {
+            assert_eq!(
+                input,
+                ServiceInput {
+                    id: "delete line id input".to_string(),
+                }
+            );
+            Ok("deleted id".to_owned())
+        }));
+
+        let variables = json!({
+          "input": {
+            "id": "delete line id input",
+          },
+          "storeId": "store_a"
+        });
+
+        let expected = json!({
+            "deleteResponseRequisitionLine": {
+                "id": "deleted id"
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
 }
