ffe604a32bb6c436a16e579f1b8ba58ad3671217
James Brunskill <james@sussol.net>
2024-10-25 17:15:03 +1300

    Remove pack variant from the integration tests

--- server/service/src/sync/test/integration/omsupply_central/test.rs
+++ server/service/src/sync/test/integration/omsupply_central/test.rs
@@ -1,15 +1,8 @@
 #[cfg(test)]
 mod tests {
     use crate::sync::test::integration::omsupply_central::{
-        asset::AssetTester, pack_variant::PackVariantTester, test_omsupply_central_records,
-        test_omsupply_central_remote_records,
+        asset::AssetTester, test_omsupply_central_records, test_omsupply_central_remote_records,
     };
-
-    // DISABLED: pack_variant has been removed, however keeping it here as a reference for OMS Central Integration tests
-    // #[actix_rt::test]
-    // async fn integration_sync_omsupply_central_sync_pack_variant() {
-    //     test_omsupply_central_records("pack_variant", &PackVariantTester).await;
-    // }
 
     #[actix_rt::test]
     async fn integration_sync_omsupply_central_sync_asset() {
