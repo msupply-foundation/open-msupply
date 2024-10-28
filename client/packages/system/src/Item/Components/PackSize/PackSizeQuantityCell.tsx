ca0b84db4bf9e09c865e13d0541cc87ccf667a94
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-10-29 12:14:35 +1300

    quantity input suggestion
    Source: https://github.com/msupply-foundation/open-msupply/commit/be37b069dd4d59aa24c5f7a9d460dc2d4e511839

--- client/packages/system/src/Item/Components/PackSize/PackSizeQuantityCell.tsx
+++ client/packages/system/src/Item/Components/PackSize/PackSizeQuantityCell.tsx
@@ -9,30 +9,25 @@ import {
   NumUtils,
 } from '@openmsupply-client/common';
 
-// Adjust quantity to reflect selected pack variant
-export const PackSizeQuantityCell =
-  <T extends RecordWithId>({
-    getPackSize,
-    getQuantity,
-  }: {
-    getPackSize: (row: T) => number;
-    getQuantity: (row: T) => number;
-  }) =>
-  ({ isError, rowData }: CellProps<T>): ReactElement => {
-    const formatNumber = useFormatNumber();
-    const quantity = getQuantity(rowData);
-    const packQuantity = quantity / getPackSize(rowData);
-    const roundedPackQuantity = formatNumber.round(packQuantity, 2);
+export const PackQuantityCell = <T extends RecordWithId>({
+  isError,
+  rowData,
+  column,
+}: CellProps<T>): ReactElement => {
+  const formatNumber = useFormatNumber();
+  const quantity = column.accessor({ rowData });
+  const packQuantity = Number(quantity ?? 0);
+  const roundedPackQuantity = formatNumber.round(packQuantity, 2);
 
-    return (
-      <BasicCellLayout isError={isError}>
-        <Tooltip title={String(packQuantity)}>
-          <Typography>
-            {!!NumUtils.hasMoreThanTwoDp(packQuantity)
-              ? `${roundedPackQuantity}...`
-              : roundedPackQuantity}
-          </Typography>
-        </Tooltip>
-      </BasicCellLayout>
-    );
-  };
+  return (
+    <BasicCellLayout isError={isError}>
+      <Tooltip title={String(packQuantity)}>
+        <Typography>
+          {!!NumUtils.hasMoreThanTwoDp(packQuantity)
+            ? `${roundedPackQuantity}...`
+            : roundedPackQuantity}
+        </Typography>
+      </Tooltip>
+    </BasicCellLayout>
+  );
+};
