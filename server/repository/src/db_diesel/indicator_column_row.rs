1130aaa012d48341d16a057b83db36a28157c5fa
Chris Petty <chris@sussol.net>
2024-10-31 14:45:50 +1300

    indicator_column repository
    Source: https://github.com/msupply-foundation/open-msupply/commit/5da2abb93e861e1934bb70d4cad194c2a0f4d28e

---
+++ server/repository/src/db_diesel/indicator_column_row.rs
@@ -0,0 +1,75 @@
+use super::StorageConnection;
+
+use crate::{repository_error::RepositoryError, Upsert};
+
+use diesel::prelude::*;
+
+table! {
+    indicator_column (id) {
+        id -> Text,
+        program_indicator_id -> Text,
+        index -> BigInt,
+        header ->Text,
+        value_type -> Text,
+        default_value -> Text,
+        is_active -> Bool,
+    }
+}
+
+#[derive(Clone, Insertable, Queryable, Debug, PartialEq, AsChangeset, Default)]
+#[diesel(table_name = indicator_column)]
+pub struct IndicatorColumnRow {
+    pub id: String,
+    pub program_indicator_id: String,
+    pub index: i64,
+    pub header: String,
+    pub value_type: String,
+    pub default_value: String,
+    pub is_active: bool,
+}
+
+pub struct IndicatorRowRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> IndicatorRowRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        IndicatorRowRepository { connection }
+    }
+
+    pub fn upsert_one(&self, row: &IndicatorColumnRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(indicator_column::table)
+            .values(row)
+            .on_conflict(indicator_column::id)
+            .do_update()
+            .set(row)
+            .execute(self.connection.lock().connection())?;
+        Ok(())
+    }
+
+    pub fn find_one_by_id(
+        &self,
+        record_id: &str,
+    ) -> Result<Option<IndicatorColumnRow>, RepositoryError> {
+        let result = indicator_column::table
+            .filter(indicator_column::id.eq(record_id))
+            .first(self.connection.lock().connection())
+            .optional()?;
+        Ok(result)
+    }
+}
+
+impl Upsert for IndicatorColumnRow {
+    fn upsert(&self, con: &StorageConnection) -> Result<Option<i64>, RepositoryError> {
+        IndicatorRowRepository::new(con).upsert_one(self)?;
+        Ok(None) // Table not in Changelog
+    }
+
+    // Test only
+    fn assert_upserted(&self, con: &StorageConnection) {
+        assert_eq!(
+            IndicatorRowRepository::new(con).find_one_by_id(&self.id),
+            Ok(Some(self.clone()))
+        )
+    }
+}
