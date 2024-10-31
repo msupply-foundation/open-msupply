8ef9c22f0ee0e25c51366b16a3da81da5f678986
Chris Petty <chris@sussol.net>
2024-10-31 15:29:32 +1300

    translator for indicator attribute/row/column

---
+++ server/service/src/sync/translations/indicator_attribute.rs
@@ -0,0 +1,126 @@
+use anyhow::anyhow;
+use repository::{IndicatorColumnRow, IndicatorLineRow, StorageConnection, SyncBufferRow};
+
+use serde::Deserialize;
+
+use crate::sync::translations::program_indicator::ProgramIndicatorTranslation;
+
+use super::{PullTranslateResult, SyncTranslation};
+
+#[derive(Deserialize)]
+
+enum Axis {
+    Column,
+    Row,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize)]
+pub struct LegacyIndicatorAttribute {
+    #[serde(rename = "ID")]
+    id: String,
+    #[serde(rename = "indicator_ID")]
+    program_indicator_id: String,
+    description: String,
+    code: String,
+    index: i64,
+    is_required: bool,
+    value_type: String,
+    axis: Axis,
+    is_active: bool,
+    default_value: String,
+}
+
+// Needs to be added to all_translators()
+#[deny(dead_code)]
+pub(crate) fn boxed() -> Box<dyn SyncTranslation> {
+    Box::new(IndicatorAttribute)
+}
+pub(super) struct IndicatorAttribute;
+impl SyncTranslation for IndicatorAttribute {
+    fn table_name(&self) -> &str {
+        "indicator_attribute"
+    }
+
+    fn pull_dependencies(&self) -> Vec<&str> {
+        vec![ProgramIndicatorTranslation.table_name()]
+    }
+
+    fn try_translate_from_upsert_sync_record(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        let LegacyIndicatorAttribute {
+            id,
+            program_indicator_id,
+            description,
+            code,
+            index,
+            is_required,
+            value_type,
+            axis,
+            is_active,
+            default_value,
+        } = serde_json::from_str::<LegacyIndicatorAttribute>(&sync_record.data)?;
+        Ok(match axis {
+            Axis::Column => PullTranslateResult::upsert(IndicatorColumnRow {
+                id,
+                program_indicator_id,
+                index,
+                header: description,
+                value_type,
+                default_value,
+                is_active,
+            }),
+            Axis::Row => PullTranslateResult::upsert(IndicatorLineRow {
+                id,
+                program_indicator_id,
+                index,
+                description,
+                code,
+                is_required,
+                is_active,
+            }),
+        })
+    }
+
+    fn try_translate_from_delete_sync_record(
+        &self,
+        _: &StorageConnection,
+        _: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        Err(anyhow!(
+            "Delete not supported for program_indicator records"
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use repository::{mock::MockDataInserts, test_db::setup_all};
+
+    #[actix_rt::test]
+    async fn test_program_indicator_translation() {
+        use crate::sync::test::test_data::program_indicator;
+        let translator = IndicatorAttribute;
+
+        let (_, connection, _, _) = setup_all(
+            "test_program_indicator_translation",
+            MockDataInserts::none(),
+        )
+        .await;
+
+        program_indicator::test_pull_upsert_records()
+            .into_iter()
+            .for_each(|record| {
+                assert!(translator.should_translate_from_sync_record(&record.sync_buffer_row));
+                let translation_result = translator
+                    .try_translate_from_upsert_sync_record(&connection, &record.sync_buffer_row)
+                    .unwrap();
+
+                assert_eq!(translation_result, record.translated_record);
+            });
+    }
+}
