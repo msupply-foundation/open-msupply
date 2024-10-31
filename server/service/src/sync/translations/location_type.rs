3058f215ea9da647f588a3347a1869ebe1de81c9
James Brunskill <james@sussol.net>
2024-10-31 16:07:40 +1300

    Fix tests
    Source: https://github.com/msupply-foundation/open-msupply/commit/884e05a721ea4d26c9f198e1e96ec24774c1bc46

--- server/service/src/sync/translations/location_type.rs
+++ server/service/src/sync/translations/location_type.rs
@@ -1,10 +1,7 @@
-use repository::{
-    ChangelogRow, ChangelogTableName, ColdStorageTypeRow, ColdStorageTypeRowRepository,
-    StorageConnection, SyncBufferRow,
-};
+use repository::{ChangelogTableName, ColdStorageTypeRow, StorageConnection, SyncBufferRow};
 use serde::{Deserialize, Serialize};
 
-use super::{PullTranslateResult, PushTranslateResult, SyncTranslation};
+use super::{PullTranslateResult, SyncTranslation};
 
 #[derive(Deserialize, Serialize)]
 pub struct LegacyLocationTypeRow {
@@ -56,39 +53,9 @@ impl SyncTranslation for LocationTypeTranslation {
             min_temperature: temperature_min,
             max_temperature: temperature_max,
         };
+        println!("Resulting row {:?}", result);
 
         Ok(PullTranslateResult::upsert(result))
-    }
-
-    fn try_translate_to_upsert_sync_record(
-        &self,
-        connection: &StorageConnection,
-        changelog: &ChangelogRow,
-    ) -> Result<PushTranslateResult, anyhow::Error> {
-        let ColdStorageTypeRow {
-            id,
-            name,
-            min_temperature,
-            max_temperature,
-        } = ColdStorageTypeRowRepository::new(connection)
-            .find_one_by_id(&changelog.record_id)?
-            .ok_or(anyhow::Error::msg(format!(
-                "Cold Storage row ({}) not found",
-                changelog.record_id
-            )))?;
-
-        let legacy_row = LegacyLocationTypeRow {
-            id: id.clone(),
-            description: name,
-            temperature_min: min_temperature,
-            temperature_max: max_temperature,
-        };
-
-        Ok(PushTranslateResult::upsert(
-            changelog,
-            self.table_name(),
-            serde_json::to_value(legacy_row)?,
-        ))
     }
 }
 
@@ -98,22 +65,18 @@ mod tests {
     use repository::{mock::MockDataInserts, test_db::setup_all};
 
     #[actix_rt::test]
-    async fn test_location_translation() {
-        use crate::sync::test::test_data::location_movement as test_data;
+    async fn test_location_type_translation() {
+        use crate::sync::test::test_data::location_type as test_data;
         let translator = LocationTypeTranslation {};
 
-        let (_, connection, _, _) = setup_all(
-            "test_location_movement_translation",
-            MockDataInserts::none(),
-        )
-        .await;
+        let (_, connection, _, _) =
+            setup_all("test_location_type_translation", MockDataInserts::none()).await;
 
         for record in test_data::test_pull_upsert_records() {
             assert!(translator.should_translate_from_sync_record(&record.sync_buffer_row));
             let translation_result = translator
                 .try_translate_from_upsert_sync_record(&connection, &record.sync_buffer_row)
                 .unwrap();
-
             assert_eq!(translation_result, record.translated_record);
         }
     }
