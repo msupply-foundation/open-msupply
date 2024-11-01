301a7e8babfef0660393721c2f898d95ec278b96
mark-prins <mark@malcolmprins.net>
2024-11-01 13:07:38 +1300

    use modal
    Source: https://github.com/msupply-foundation/open-msupply/commit/23ded2deac310fd0d700bad337950f7017b357b9

--- client/packages/common/src/hooks/useConfirmOnLeaving/useConfirmOnLeaving.ts
+++ client/packages/common/src/hooks/useConfirmOnLeaving/useConfirmOnLeaving.ts
@@ -1,32 +1,42 @@
-import { useCallback } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { BlockerFunction, useBeforeUnload, useBlocker } from 'react-router-dom';
 import { useTranslation } from '@common/intl';
+import { ConfirmationModalContext } from '@openmsupply-client/common'; // '@common/components';
 
 /** useConfirmOnLeaving is a hook that will prompt the user if they try to navigate away from,
  *  or refresh the page, when there are unsaved changes.
  * */
 export const useConfirmOnLeaving = (isUnsaved?: boolean) => {
   const t = useTranslation();
-  const confirmMessage = `${t('heading.are-you-sure')}\n${t('messages.confirm-cancel-generic')}`;
-
   const customConfirm = (onOk: () => void) => {
-    if (confirm(confirmMessage)) {
-      onOk();
-    }
+    setOnConfirm(onOk);
+    showConfirmation();
   };
 
-  useBlocker(
-    useCallback<BlockerFunction>(
-      ({ currentLocation, nextLocation }) => {
-        if (!!isUnsaved && currentLocation.pathname !== nextLocation.pathname) {
-          return !confirm(confirmMessage);
-        }
-        return false;
-      },
-      [isUnsaved]
-    )
+  const { setOpen, setMessage, setOnConfirm, setTitle } = useContext(
+    ConfirmationModalContext
   );
 
+  const showConfirmation = useCallback(() => {
+    setMessage(t('heading.are-you-sure'));
+    setTitle(t('messages.confirm-cancel-generic'));
+    setOpen(true);
+  }, [setMessage, setTitle, setOpen]);
+
+  const shouldBlock = useCallback<BlockerFunction>(
+    ({ currentLocation, nextLocation }) => {
+      if (!!isUnsaved && currentLocation.pathname !== nextLocation.pathname) {
+        showConfirmation();
+        return true;
+      }
+      return false;
+    },
+    [isUnsaved, showConfirmation]
+  );
+
+  const blocker = useBlocker(shouldBlock);
+
+  // handle page refresh events
   useBeforeUnload(
     useCallback(
       event => {
@@ -37,6 +47,20 @@ export const useConfirmOnLeaving = (isUnsaved?: boolean) => {
     ),
     { capture: true }
   );
+
+  // Reset the blocker if the dirty state changes
+  useEffect(() => {
+    if (blocker.state === 'blocked' && !isUnsaved) {
+      blocker.reset();
+    }
+  }, [blocker, isUnsaved]);
+
+  // update the onConfirm function when the blocker changes
+  useEffect(() => {
+    setOnConfirm(() => {
+      blocker?.proceed?.();
+    });
+  }, [blocker]);
 
   return { showConfirmation: customConfirm };
 };
