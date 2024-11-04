239dfec4f98323bfbe1b5bdf5c4755507d3f0083
Fergus Roache <fergus@msupply.foundation>
2024-11-05 12:19:10 +1300

    Remove mutations changes
    Source: https://github.com/msupply-foundation/open-msupply/commit/72cfd65c36729d70c62ad10236aac255f419a96f

--- server/graphql/programs/src/mutations/program_indicator/insert.rs
+++
@@ -1,8 +0,0 @@
-use async_graphql::*;
-use graphql_core::{
-    standard_graphql_error::{validate_auth, StandardGraphqlError},
-    ContextExt,
-};
-use graphql_types::types::program_indicator::ProgramIndicatorNode;
-
-#[derive(InputObject)]
