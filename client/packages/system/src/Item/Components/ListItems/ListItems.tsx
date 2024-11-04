a6ce13df3729b6e96945a5bbd0cdb62e802d3204
roxy-dao <roxy@msupply.foundation>
2024-11-05 11:57:25 +1300

    Don't pass in icons as props

--- client/packages/system/src/Item/Components/ListItems/ListItems.tsx
+++ client/packages/system/src/Item/Components/ListItems/ListItems.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import {
   Tooltip,
   ListOptions,
-  ChevronDownIcon,
   RouteBuilder,
   useNavigate,
-  CheckIcon,
 } from '@openmsupply-client/common';
 import { ItemRowFragment } from '../../api';
 
@@ -28,18 +26,6 @@ export const ListItems = ({
   return (
     <Tooltip title={value?.name}>
       <ListOptions
-        startIcon={
-          <CheckIcon
-            style={{
-              backgroundColor: '#33A901',
-              borderRadius: '50%',
-              padding: '2px',
-              color: 'white',
-              height: 18,
-              width: 18,
-            }}
-          />
-        }
         currentId={value?.id}
         onClick={id => {
           navigate(route.addPart(id).build(), { replace: true });
@@ -49,11 +35,6 @@ export const ListItems = ({
             id,
             value: name,
           })) ?? []
-        }
-        endIcon={
-          <ChevronDownIcon
-            style={{ width: 17, height: 17, transform: 'rotate(-90deg)' }}
-          />
         }
         enteredLineIds={enteredLineIds}
       />
