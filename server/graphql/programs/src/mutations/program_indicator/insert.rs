b1606dc926f36c3b8574ef9c156ad244317ae7b9
Fergus Roache <fergus@msupply.foundation>
2024-11-05 09:34:37 +1300

    Add basic scaffold for mutations
    Source: https://github.com/msupply-foundation/open-msupply/commit/70b8721ac2898dcf4f4a9f1dbc87a3b82a38ed24

---
+++ server/graphql/programs/src/mutations/program_indicator/insert.rs
@@ -0,0 +1,8 @@
+use async_graphql::*;
+use graphql_core::{
+    standard_graphql_error::{validate_auth, StandardGraphqlError},
+    ContextExt,
+};
+use graphql_types::types::program_indicator::ProgramIndicatorNode;
+
+#[derive(InputObject)]
