a2dd85c3f33a343038c8a9d8f4c18a806928d82c
roxy-dao <roxy@msupply.foundation>
2024-11-06 08:28:57 +1300

    Change response store stats UI

--- client/packages/requisitions/src/ResponseRequisition/DetailView/ReponseStats/ResponseStoreStats.tsx
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/ReponseStats/ResponseStoreStats.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useTranslation } from '@common/intl';
-import { Box, Typography, ValueBar } from '@openmsupply-client/common';
+import { Box, Typography, NewValueBar } from '@openmsupply-client/common';
+import { ItemRowFragment } from 'packages/system/src';
 
 export interface ResponseStoreStatsProps {
+  item?: ItemRowFragment;
   stockOnHand: number;
   incomingStock: number;
   stockOnOrder: number;
@@ -11,6 +13,7 @@ export interface ResponseStoreStatsProps {
 }
 
 export const ResponseStoreStats: React.FC<ResponseStoreStatsProps> = ({
+  item,
   stockOnHand,
   incomingStock,
   stockOnOrder,
@@ -34,13 +37,35 @@ export const ResponseStoreStats: React.FC<ResponseStoreStatsProps> = ({
       : '100%';
 
   return (
-    <>
+    <Box sx={{ height: 400, width: 500 }}>
+      <Box display="flex" flexDirection="column" paddingTop={2} paddingLeft={4}>
+        <Box display="flex">
+          <Typography fontSize={12} fontWeight={700}>
+            {t('label.name')}:&nbsp;
+          </Typography>
+          <Typography fontSize={12}>{item?.name ?? ''}</Typography>
+        </Box>
+        <Box display="flex" gap={5}>
+          <Box display="flex">
+            <Typography fontSize={12} fontWeight={700}>
+              {t('label.code')}:&nbsp;
+            </Typography>
+            <Typography fontSize={12}>{item?.code ?? ''}</Typography>
+          </Box>
+          <Box display="flex">
+            <Typography fontSize={12} fontWeight={700}>
+              {t('label.unit')}:&nbsp;
+            </Typography>
+            <Typography fontSize={12}>{item?.unitName ?? ''}</Typography>
+          </Box>
+        </Box>
+      </Box>
       <Box
         flex={1}
         sx={{
           paddingLeft: 4,
           paddingRight: 4,
-          paddingTop: 4,
+          paddingTop: 2,
           paddingBottom: 2,
         }}
       >
@@ -52,57 +77,171 @@ export const ResponseStoreStats: React.FC<ResponseStoreStatsProps> = ({
             </span>
           </Typography>
         ) : (
-          <Box
-            display="flex"
-            alignItems="flex-start"
-            width={predictedStockPercent}
-          >
-            <ValueBar
-              value={stockOnHand}
-              total={predictedStockLevels}
-              label={t('label.stock-on-hand')}
-              colour="gray.dark"
-              startDivider
-            />
-            <ValueBar
-              value={incomingStock}
-              total={predictedStockLevels}
-              label={t('label.incoming-stock')}
-              colour="gray.main"
-            />
-            <ValueBar
-              value={stockOnOrder}
-              total={predictedStockLevels}
-              label={t('label.stock-on-order')}
-              colour="gray.light"
-            />
-          </Box>
+          <>
+            <Box>
+              <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                {t('label.our-stock')}
+              </Typography>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="flex-start"
+              flexDirection="column"
+              width={predictedStockPercent}
+            >
+              <Box display="flex" width="100%">
+                <NewValueBar
+                  value={stockOnHand}
+                  total={predictedStockLevels}
+                  colour="gray.dark"
+                />
+                <NewValueBar
+                  value={incomingStock}
+                  total={predictedStockLevels}
+                  colour="gray.main"
+                />
+                <NewValueBar
+                  value={stockOnOrder}
+                  total={predictedStockLevels}
+                  colour="gray.light"
+                />
+              </Box>
+              <Box paddingTop={1}>
+                {!!stockOnHand && (
+                  <Box display="flex" alignItems="center" gap={1}>
+                    <Box
+                      sx={{
+                        backgroundColor: 'gray.dark',
+                        height: 10,
+                        width: 10,
+                        minWidth: 10,
+                        minHeight: 10,
+                      }}
+                    />
+                    <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                      {t('label.stock-on-hand')}
+                    </Typography>
+                    <Typography fontWeight={800} fontSize={12}>
+                      {stockOnHand}
+                    </Typography>
+                  </Box>
+                )}
+                {!!incomingStock && (
+                  <Box display="flex" alignItems="center" gap={1}>
+                    <Box
+                      sx={{
+                        backgroundColor: 'gray.main',
+                        height: 10,
+                        width: 10,
+                        minWidth: 10,
+                        minHeight: 10,
+                      }}
+                    />
+                    <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                      {t('label.incoming-stock')}
+                    </Typography>
+                    <Typography fontWeight={800} fontSize={12}>
+                      {incomingStock}
+                    </Typography>
+                  </Box>
+                )}
+                {!!stockOnOrder && (
+                  <Box display="flex" alignItems="center" gap={1}>
+                    <Box
+                      sx={{
+                        backgroundColor: 'gray.light',
+                        height: 10,
+                        width: 10,
+                        minWidth: 10,
+                        minHeight: 10,
+                      }}
+                    />
+                    <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                      {t('label.stock-on-order')}
+                    </Typography>
+                    <Typography fontWeight={800} fontSize={12}>
+                      {stockOnOrder}
+                    </Typography>
+                  </Box>
+                )}
+              </Box>
+            </Box>
+          </>
         )}
       </Box>
       <Box
         sx={{
           paddingLeft: 4,
           paddingRight: 4,
-          paddingTop: 4,
+          paddingTop: 2,
           paddingBottom: 2,
         }}
       >
-        <Box display="flex" alignItems="flex-start" width={requestedPercent}>
-          <ValueBar
-            value={requestedQuantity}
-            total={totalRequested}
-            label={t('label.requested-quantity')}
-            colour="primary.main"
-            startDivider
-          />
-          <ValueBar
-            value={otherRequestedQuantity}
-            total={totalRequested}
-            label={t('label.all-requested-quantity')}
-            colour="primary.light"
-          />
+        <Box>
+          <Typography fontSize={12} style={{ textAlign: 'start' }}>
+            {t('label.requested')}
+          </Typography>
+        </Box>
+        <Box
+          display="flex"
+          alignItems="flex-start"
+          flexDirection="column"
+          width={requestedPercent}
+        >
+          <Box display="flex" width="100%">
+            <NewValueBar
+              value={requestedQuantity}
+              total={totalRequested}
+              colour="primary.main"
+            />
+            <NewValueBar
+              value={otherRequestedQuantity}
+              total={totalRequested}
+              colour="primary.light"
+            />
+          </Box>
+          <Box paddingTop={1}>
+            {!!requestedQuantity && (
+              <Box display="flex" alignItems="center" gap={1}>
+                <Box
+                  sx={{
+                    backgroundColor: 'primary.main',
+                    height: 10,
+                    width: 10,
+                    minWidth: 10,
+                    minHeight: 10,
+                  }}
+                />
+                <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                  {t('label.requested')}
+                </Typography>
+                <Typography fontWeight={800} fontSize={12}>
+                  {requestedQuantity}
+                </Typography>
+              </Box>
+            )}
+            {!!otherRequestedQuantity && (
+              <Box display="flex" alignItems="center" gap={1}>
+                <Box
+                  sx={{
+                    backgroundColor: 'primary.light',
+                    height: 10,
+                    width: 10,
+                    minWidth: 10,
+                    minHeight: 10,
+                  }}
+                />
+                <Typography fontSize={12} style={{ textAlign: 'start' }}>
+                  {t('label.other-requested-quantity')}
+                </Typography>
+                <Typography fontWeight={800} fontSize={12}>
+                  {otherRequestedQuantity}
+                </Typography>
+              </Box>
+            )}
+          </Box>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
