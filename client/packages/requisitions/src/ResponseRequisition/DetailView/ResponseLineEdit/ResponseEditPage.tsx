8af9e3570036cd1f87a5f1b2ac648e5ac3563a58
roxy-dao <roxy@msupply.foundation>
2024-11-05 17:07:26 +1300

    Show available stock on hand if not program instead of initial stock on hand

--- client/packages/requisitions/src/ResponseRequisition/DetailView/ResponseLineEdit/ResponseEditPage.tsx
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/ResponseLineEdit/ResponseEditPage.tsx
@@ -89,6 +89,7 @@ export const ResponseLineEditPage = () => {
               next={next}
               hasPrevious={hasPrevious}
               previous={previous}
+              isProgram={!!data?.programName}
             />
           </>
         }
