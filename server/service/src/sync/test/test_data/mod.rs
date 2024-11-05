3a537c4d858426573d467efb242e49608c6ae489
Chris Petty <chris@sussol.net>
2024-11-05 15:20:50 +1300

    remove dead code codes

--- server/service/src/sync/test/test_data/mod.rs
+++ server/service/src/sync/test/test_data/mod.rs
@@ -149,7 +149,7 @@ pub(crate) fn get_all_pull_delete_central_test_records() -> Vec<TestSyncIncoming
     test_records.append(&mut report::test_pull_delete_records());
     test_records.append(&mut store::test_pull_delete_records());
     test_records.append(&mut unit::test_pull_delete_records());
-    test_records.append(&mut indicator_value::test_pull_delete_records());
+
     // Central but site specific
     test_records.append(&mut name_store_join::test_pull_delete_records());
 
