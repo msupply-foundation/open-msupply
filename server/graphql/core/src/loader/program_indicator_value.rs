779cf0781ffef14c8ff1eaf18cd114dce6027e37
Fergus Roache <fergus@msupply.foundation>
2024-11-07 14:29:14 +1300

    Tidy and rename params to match db
    Source: https://github.com/msupply-foundation/open-msupply/commit/cc14c00ac8bc4caa2782412c3fee661495e0a063

--- server/graphql/core/src/loader/program_indicator_value.rs
+++ server/graphql/core/src/loader/program_indicator_value.rs
@@ -14,7 +14,7 @@ use super::IdPair;
 pub struct IndicatorValuePayload {
     pub period_id: String,
     pub supplier_store_id: String,
-    pub customer_name_id: String,
+    pub customer_name_link_id: String,
 }
 
 pub type IndicatorValueLoaderInput = IdPair<IndicatorValuePayload>;
@@ -43,21 +43,21 @@ impl Loader<IndicatorValueLoaderInput> for IndicatorValueLoader {
     ) -> Result<HashMap<IndicatorValueLoaderInput, Self::Value>, Self::Error> {
         let service_context = self.service_provider.basic_context()?;
 
-        let (period_id, supplier_store_id, customer_name_id) =
+        let (period_id, supplier_store_id, customer_name_link_id) =
             // TODO replace with logic to not assume only one combination queried at any time.
             if let Some(loader_input) = loader_inputs.first() {
                 (
                     loader_input.payload.period_id.clone(),
                     loader_input.payload.supplier_store_id.clone(),
-                    loader_input.payload.customer_name_id.clone(),
+                    loader_input.payload.customer_name_link_id.clone(),
                 )
             } else {
                 return Ok(HashMap::new());
             };
 
         let filter = IndicatorValueFilter::new()
-            .store_id(EqualFilter::equal_to(&supplier_store_id))
-            .facility_id(EqualFilter::equal_to(&customer_name_id))
+            .supplier_store_id(EqualFilter::equal_to(&supplier_store_id))
+            .customer_name_link_id(EqualFilter::equal_to(&customer_name_link_id))
             .period_id(EqualFilter::equal_to(&period_id));
 
         let values =
@@ -66,7 +66,7 @@ impl Loader<IndicatorValueLoaderInput> for IndicatorValueLoader {
         let payload = IndicatorValuePayload {
             period_id,
             supplier_store_id,
-            customer_name_id,
+            customer_name_link_id,
         };
 
         Ok(values
