c21e97e7eff3ed152285ed299c4ac4bdf4d332a3
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-11-13 10:30:13 +1300

    cleanups
    Source: https://github.com/msupply-foundation/open-msupply/commit/81a5d53b279b66efc9e22fa66eabcc5a916a6b38

--- client/packages/system/src/Item/api/hooks/useItemVariant/useIsItemVariantsEnabled.ts
+++ client/packages/system/src/Item/api/hooks/useItemVariant/useIsItemVariantsEnabled.ts
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query';
 import { useItemGraphQL } from '../useItemApi';
 
-// Item variant inputs (i.e. in Inbound Shipment) should not be available if no item variants are configured
+// Item variant inputs (e.g. in Inbound Shipment) should not be available if no item variants are configured
 export const useIsItemVariantsEnabled = () => {
   const { api, storeId } = useItemGraphQL();
   const { data } = useQuery({
