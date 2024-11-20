e7a86fa67a912eee12cc4c808647debeca2495d3
roxy-dao <roxy@msupply.foundation>
2024-11-20 16:53:35 +1300

    Fix infinite fetching
    Source: https://github.com/msupply-foundation/open-msupply/commit/48159aaf07ad77dc8b8d2176a7743147e6013019

--- client/packages/requisitions/src/ResponseRequisition/api/hooks/document/useIndicators.ts
+++ client/packages/requisitions/src/ResponseRequisition/api/hooks/document/useIndicators.ts
@@ -1,12 +1,14 @@
-import { useQueryClient, useQuery } from '@openmsupply-client/common';
+import { useQuery } from '@openmsupply-client/common';
 import { useResponseApi } from '../utils/useResponseApi';
 
 export const useIndicators = (customerNameLinkId: string, periodId: string) => {
-  const queryClient = useQueryClient();
   const api = useResponseApi();
-  return useQuery(api.keys.indicators(), ()=> api.getIndicators(customerNameLinkId, periodId), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(api.keys.indicators());
-    },
-  });
+  return useQuery(
+    api.keys.indicators(),
+    () => api.getIndicators(customerNameLinkId, periodId),
+    {
+      refetchOnMount: false,
+      cacheTime: 0,
+    }
+  );
 };
