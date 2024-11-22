3202f9131a8b465a03dcd3adcfdcbeb85611c454
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2024-11-22 16:57:27 +1300

    add pagination to stores api calls

--- client/packages/system/src/Store/api/hooks/document/useStores.ts
+++ client/packages/system/src/Store/api/hooks/document/useStores.ts
@@ -1,13 +1,23 @@
-import { useQueryParamsStore, useQuery } from '@openmsupply-client/common';
+import { useQuery, useQueryParamsStore } from '@openmsupply-client/common';
 import { useStoreApi } from '../utils/useStoreApi';
 
-export const useStores = () => {
-  const api = useStoreApi();
+export const useStores = (first?: number, offset?: number) => {
   const queryParams = useQueryParamsStore();
   const { filter } = queryParams;
   const { filterBy } = filter;
 
-  return useQuery(api.keys.paramList(filterBy), async () =>
-    api.get.list(filterBy)
+  const params = {
+    first: first ?? 100,
+    offset: offset ?? 0,
+    filterBy,
+    sortBy: { key: 'name', isDesc: false, direction: 'asc' as 'asc' | 'desc' },
+  };
+
+  const api = useStoreApi();
+
+  return useQuery(
+    api.keys.paramList(params),
+    async () => api.get.list(params),
+    { refetchOnWindowFocus: false, cacheTime: 0 }
   );
 };
