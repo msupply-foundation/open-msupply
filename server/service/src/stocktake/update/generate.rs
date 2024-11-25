92dc88f93a656748ceb158345dc87d3370add61b
James Brunskill <james@sussol.net>
2024-11-26 11:51:41 +1300

    Update comment

--- server/service/src/stocktake/update/generate.rs
+++ server/service/src/stocktake/update/generate.rs
@@ -103,6 +103,10 @@ fn generate_stock_in_out_or_update(
         .sell_price_per_pack
         .unwrap_or(stock_line_row.sell_price_per_pack);
 
+    // If item_variant_id is null on the stocktake_line, we need to set the stock_line item_variant_id to null too.
+    // Without this, we'd wouldn't be able to clear it...
+    let item_variant_id = stocktake_line.line.item_variant_id.clone();
+
     log_stock_changes(ctx, stock_line_row.clone(), row.clone())?;
 
     // If no change in stock quantity, we just update the stock line (no inventory adjustment)
@@ -114,7 +118,7 @@ fn generate_stock_in_out_or_update(
             cost_price_per_pack,
             sell_price_per_pack,
             expiry_date,
-            item_variant_id: stocktake_line.line.item_variant_id.clone(), // This needs to be a copy of the item variant id, not only on update, otherwise we can't clear the item_variant
+            item_variant_id,
             ..stock_line_row
         }
         .to_owned();
