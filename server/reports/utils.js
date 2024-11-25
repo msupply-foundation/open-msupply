cd81a3e46d3f0346a5f0f39fc088ab4157ee6e7f
Fergus Roache <fergus@msupply.foundation>
2024-11-25 15:30:33 +1300

    Sorting now ignores caps
    Source: https://github.com/msupply-foundation/open-msupply/commit/2da39d932f4bfd4b645d12f75a0a7b1168ee5f14

--- server/reports/utils.js
+++ server/reports/utils.js
@@ -32,8 +32,16 @@ const getNestedValue = (node, key) => {
 const sortNodes = (nodes, sort, dir) => {
   if (!!sort) {
     nodes.sort((a, b) => {
-      const valueA = getNestedValue(a, sort);
-      const valueB = getNestedValue(b, sort);
+      let valueA = getNestedValue(a, sort);
+
+      if (typeof valueA == "string") {
+        valueA = valueA.toLocaleLowerCase();
+      }
+
+      let valueB = getNestedValue(b, sort);
+      if (typeof valueB == "string") {
+        valueB = valueB.toLocaleLowerCase();
+      }
 
       if (valueA == null && valueB == null) {
         return 0;
