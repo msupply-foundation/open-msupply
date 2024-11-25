ac50bdca453bf19b43afc78a5672eaa94807ecf9
James Brunskill <james@sussol.net>
2024-11-26 12:23:27 +1300

    Populate item_variant on stock_take_line when creating from stock_line
    Source: https://github.com/msupply-foundation/open-msupply/commit/6bf227193ace8efa5baea5dd23e0cd1a2a01e7bc

--- server/service/src/stocktake_line/insert/generate.rs
+++ server/service/src/stocktake_line/insert/generate.rs
@@ -24,11 +24,22 @@ pub fn generate(
         item_variant_id,
     }: InsertStocktakeLine,
 ) -> StocktakeLineRow {
-    let snapshot_number_of_packs = if let Some(stock_line) = stock_line {
+    let snapshot_number_of_packs = if let Some(stock_line) = stock_line.clone() {
         stock_line.stock_line_row.total_number_of_packs
     } else {
         0.0
     };
+
+    // Get any existing item_variant_id from stock_line, if not provided as input for the new stocktake_line
+    // This should make it safe to assume that the item_variant_id is always present in the stocktake record if it was set on the previous stock_line
+    let item_variant_id = match item_variant_id {
+        Some(id) => Some(id),
+        None => match stock_line {
+            Some(stock_line) => stock_line.stock_line_row.item_variant_id,
+            None => None,
+        },
+    };
+
     StocktakeLineRow {
         id,
         stocktake_id,
