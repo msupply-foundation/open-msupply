9e3529206c874ce8ebb3b860f3d9f38a91c497ac
Chris Petty <chris@sussol.net>
2024-11-25 14:30:05 +1300

    hide/show indicator buttons as required
    Source: https://github.com/msupply-foundation/open-msupply/commit/387ce3c14d18ec1807607df05aebbade34d7423c

--- client/packages/requisitions/src/ResponseRequisition/DetailView/IndicatorsTab.tsx
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/IndicatorsTab.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Box,
   ButtonWithIcon,
+  DataTableSkeleton,
   IndicatorLineRowNode,
   NothingHere,
   PlusCircleIcon,
@@ -28,41 +29,48 @@ export const IndicatorsTab = ({
 }: IndicatorTabProps) => {
   const t = useTranslation();
   if (isLoading) {
+    return <DataTableSkeleton />;
+  }
+  if (!indicators || indicators.length === 0) {
     return <NothingHere body={t('error.no-indicators')} />;
   }
-  const hivIndicators = indicators?.filter(
-    indicator => indicator?.code === 'HIV'
+  const regimenIndicators = indicators.filter(
+    indicator => indicator.code === 'REGIMEN'
   );
-  const regimenIndicators = indicators?.filter(
-    indicator => indicator?.code === 'REGIMEN'
+  const hivIndicators = indicators.filter(
+    indicator => indicator.code === 'HIV'
   );
 
   return (
     <Box display="flex" flexDirection="column" padding={2} gap={2}>
-      <ButtonWithIcon
-        // disabled={disableAddButton}
-        label={t('button.regimen')}
-        Icon={<PlusCircleIcon />}
-        onClick={() =>
-          onClick(
-            regimenIndicators?.[0],
-            regimenIndicators?.[0]?.lineAndColumns[0]?.line,
-            response
-          )
-        }
-      />
-      <ButtonWithIcon
-        // disabled={disableAddButton}
-        label={t('button.hiv')}
-        Icon={<PlusCircleIcon />}
-        onClick={() =>
-          onClick(
-            hivIndicators?.[0],
-            hivIndicators?.[0]?.lineAndColumns[0]?.line,
-            response
-          )
-        }
-      />
+      {regimenIndicators.length >= 0 && (
+        <ButtonWithIcon
+          // disabled={disableAddButton}
+          label={t('button.regimen')}
+          Icon={<PlusCircleIcon />}
+          onClick={() =>
+            onClick(
+              regimenIndicators[0],
+              regimenIndicators[0]?.lineAndColumns[0]?.line,
+              response
+            )
+          }
+        />
+      )}
+      {hivIndicators.length >= 0 && (
+        <ButtonWithIcon
+          // disabled={disableAddButton}
+          label={t('button.hiv')}
+          Icon={<PlusCircleIcon />}
+          onClick={() =>
+            onClick(
+              hivIndicators[0],
+              hivIndicators[0]?.lineAndColumns[0]?.line,
+              response
+            )
+          }
+        />
+      )}
     </Box>
   );
 };
