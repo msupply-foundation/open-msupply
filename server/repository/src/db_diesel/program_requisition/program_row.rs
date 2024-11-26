63846a65123a4564db4c97c3bf4ed385ce8123cb
roxy-dao <roxy@msupply.foundation>
2024-11-27 08:33:32 +1300

    Remove dup customers for program

--- server/repository/src/db_diesel/program_requisition/program_row.rs
+++ server/repository/src/db_diesel/program_requisition/program_row.rs
@@ -26,7 +26,7 @@ joinable!(program -> context (context_id));
 allow_tables_to_appear_in_same_query!(program, document);
 allow_tables_to_appear_in_same_query!(program, name_link);
 
-#[derive(Clone, Queryable, Insertable, AsChangeset, Debug, PartialEq, Default)]
+#[derive(Clone, Queryable, Insertable, AsChangeset, Debug, PartialEq, Eq, Default)]
 #[diesel(table_name = program)]
 pub struct ProgramRow {
     pub id: String, // Master list id
