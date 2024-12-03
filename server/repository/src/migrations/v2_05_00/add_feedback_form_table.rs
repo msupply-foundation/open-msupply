b5b578f647b69ba89a093008dfe7b895b3ba7149
noel-yeldos <noelyeldos@gmail.com>
2024-12-03 16:38:19 +1300

    create migration for feedbackform
    Source: https://github.com/msupply-foundation/open-msupply/commit/c9c177eda686692a27fb8eb47eea0efff9cf8be2

---
+++ server/repository/src/migrations/v2_05_00/add_feedback_form_table.rs
@@ -0,0 +1,38 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_feedback_form_table"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+                CREATE TABLE feedback_form (
+                    id TEXT NOT NULL PRIMARY KEY,
+                    reply_email TEXT NOT NULL,
+                    body TEXT NOT NULL,
+                    created_datetime {DATETIME} NOT NULL,
+                    user_id TEXT NOT NULL REFERENCES user_account(id),
+                    store_id TEXT NOT NULL REFERENCES store(id),
+                    site_id TEXT NOT NULL
+                );
+            "#
+        )?;
+
+        if cfg!(feature = "postgres") {
+            // Postgres changelog variant
+            sql!(
+                connection,
+                r#"
+                    ALTER TYPE changelog_table_name ADD VALUE IF NOT EXISTS 'feedback_form';
+                "#
+            )?;
+        }
+
+        Ok(())
+    }
+}
