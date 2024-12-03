da9cfd1022555bddc45dfab47fe62181f4b277af
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2024-12-03 14:05:35 +1300

    removed accordian from user guide
    Source: https://github.com/msupply-foundation/open-msupply/commit/a844128fbf35725f8f4cd70e68370b4345a3cb1e

--- client/packages/host/src/Admin/UserGuide.tsx
+++ client/packages/host/src/Admin/UserGuide.tsx
@@ -1,7 +1,7 @@
-import { ExternalNavLink } from '@common/components';
+import { ExternalNavLink, Typography } from '@common/components';
 import { useTranslation } from '@common/intl';
 import { EnvUtils } from '@common/utils';
-import { ExternalURL, useExternalUrl } from 'packages/config/src';
+import { ExternalURL, useExternalUrl } from '@openmsupply-client/config';
 import { useLocation } from '@openmsupply-client/common';
 
 import React from 'react';
@@ -16,6 +16,9 @@ export const UserGuide: React.FC = () => {
 
   return (
     <>
+      <Typography variant="h5" style={{ paddingBottom: 10 }}>
+        {t('heading.user-guide')}
+      </Typography>
       <ExternalNavLink
         to={docsUrl}
         text={t('label.user-guide')}
--- client/packages/host/src/Help/Help.tsx
+++ client/packages/host/src/Help/Help.tsx
@@ -1,6 +1,5 @@
 import {
   AppBarButtonsPortal,
-  BookIcon,
   Box,
   useInitialisationStatus,
 } from '@openmsupply-client/common/src';
@@ -8,29 +7,13 @@ import React from 'react';
 import { AppVersion } from '../components';
 import { SiteInfo } from '../components/SiteInfo';
 import { UserGuide } from '../Admin/UserGuide';
-import { HelpSection } from './HelpSection';
 
 export const Help: React.FC = () => {
   const { data: initStatus } = useInitialisationStatus();
 
-  const [activeSection, setActiveSection] = React.useState<number | null>(null);
-
-  const toggleSection = (index: number) => () =>
-    setActiveSection(activeSection === index ? null : index);
-
   return (
     <Box flex={1} padding={4} sx={{ maxWidth: 800 }}>
-      <div>
-        <p>Hello</p>
-      </div>
-      <HelpSection
-        Icon={BookIcon}
-        titleKey="heading.user-guide"
-        expanded={activeSection === 5}
-        onChange={toggleSection(5)}
-      >
-        <UserGuide />
-      </HelpSection>
+      <UserGuide />
       <AppBarButtonsPortal>
         <AppVersion SiteInfo={<SiteInfo siteName={initStatus?.siteName} />} />
       </AppBarButtonsPortal>
