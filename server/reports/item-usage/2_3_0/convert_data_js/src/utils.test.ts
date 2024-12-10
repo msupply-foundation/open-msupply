d64a175fbc3fc5c1675d96b8788e727e44ae7617
Fergus Roache <fergus@msupply.foundation>
2024-12-10 13:05:30 +1300

    Add totalStockOnHand to item with stats query
    Source: https://github.com/msupply-foundation/open-msupply/commit/0ba25670454e4dea80c2a9debf4b6b0accd54bc5

--- server/reports/item-usage/2_3_0/convert_data_js/src/utils.test.ts
+++ server/reports/item-usage/2_3_0/convert_data_js/src/utils.test.ts
@@ -132,7 +132,7 @@ describe("calculate SOH", () => {
   });
   it("returns rounded value if value exists", () => {
     expect(
-      calculateStatValue(inputData.items.nodes[0].stats.availableStockOnHand)
+      calculateStatValue(inputData.items.nodes[0].stats.totalStockOnHand)
     ).toBe(300.9);
   });
 });
