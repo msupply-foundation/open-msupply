f0b0cb89e4d17962916a52ed7e16c75c28fdeafe
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-12-11 08:34:47 +1300

    pass through inputMode prop
    Source: https://github.com/msupply-foundation/open-msupply/commit/359aeff72d42ace8c1745495c34fd5be2e1d62b6

--- client/packages/common/src/ui/components/inputs/TextInput/NumericTextInput.tsx
+++ client/packages/common/src/ui/components/inputs/TextInput/NumericTextInput.tsx
@@ -195,6 +195,7 @@ export const NumericTextInput = React.forwardRef<
       noFormatting = false,
       fullWidth,
       endAdornment,
+      inputMode,
       ...props
     },
     ref
@@ -265,8 +266,6 @@ export const NumericTextInput = React.forwardRef<
 
     return (
       <BasicTextInput
-        // NOTE: not setting input mode as numeric here, because on Samsung tablets,
-        // the numeric keyboard doesn't allow entering negative numbers!
         ref={ref}
         sx={{
           '& .MuiInput-input': {
@@ -275,6 +274,7 @@ export const NumericTextInput = React.forwardRef<
           },
           ...sx,
         }}
+        inputMode={inputMode ?? 'numeric'}
         InputProps={{
           endAdornment: endAdornment ? (
             <InputAdornment position="end" sx={{ paddingBottom: '2px' }}>
