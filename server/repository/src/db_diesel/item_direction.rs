03e03d92cfda5398b424c64f50d96688d56dbee4
Chris Petty <chris@sussol.net>
2024-12-11 12:13:44 +1300

    item direction repo

---
+++ server/repository/src/db_diesel/item_direction.rs
@@ -0,0 +1,77 @@
+use super::{item_direction_row::item_direction, ItemDirectionRow, StorageConnection};
+
+use crate::{diesel_macros::apply_equal_filter, StringFilter};
+
+use crate::{repository_error::RepositoryError, DBType, EqualFilter};
+use diesel::{dsl::IntoBoxed, prelude::*};
+
+pub type ItemDirection = ItemDirectionRow;
+
+#[derive(Clone, Default, PartialEq, Debug)]
+pub struct ItemDirectionFilter {
+    pub id: Option<EqualFilter<String>>,
+}
+
+pub struct ItemDirectionRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> ItemDirectionRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        ItemDirectionRepository { connection }
+    }
+
+    pub fn count(&self, filter: Option<ItemDirectionFilter>) -> Result<i64, RepositoryError> {
+        let query = Self::create_filtered_query(filter);
+
+        Ok(query
+            .count()
+            .get_result(self.connection.lock().connection())?)
+    }
+
+    pub fn query_by_filter(
+        &self,
+        filter: ItemDirectionFilter,
+    ) -> Result<Vec<ItemDirection>, RepositoryError> {
+        self.query(Some(filter))
+    }
+
+    pub fn query(
+        &self,
+        filter: Option<ItemDirectionFilter>,
+    ) -> Result<Vec<ItemDirection>, RepositoryError> {
+        let query = Self::create_filtered_query(filter);
+
+        let result = query.load::<ItemDirection>(self.connection.lock().connection())?;
+
+        Ok(result)
+    }
+
+    pub fn create_filtered_query(filter: Option<ItemDirectionFilter>) -> BoxedItemDirectionQuery {
+        let mut query = item_direction::table.into_boxed();
+
+        if let Some(filter) = filter {
+            apply_equal_filter!(query, filter.id, item_direction::id);
+        }
+
+        query
+    }
+}
+
+type BoxedItemDirectionQuery = IntoBoxed<'static, item_direction::table, DBType>;
+
+impl ItemDirectionFilter {
+    pub fn new() -> ItemDirectionFilter {
+        Self::default()
+    }
+
+    pub fn id(mut self, filter: EqualFilter<String>) -> Self {
+        self.id = Some(filter);
+        self
+    }
+
+    pub fn key(mut self, filter: StringFilter) -> Self {
+        self.text = Some(filter);
+        self
+    }
+}
