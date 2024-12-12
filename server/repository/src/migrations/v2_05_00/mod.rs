08d31daccabdfb9c83aeb279a8d2423611fe6b39
John Geronimo <johnageronimo1@gmail.com>
2024-12-12 14:55:09 +1300

    remove contact forms side id
    Source: https://github.com/msupply-foundation/open-msupply/commit/29b548c76f1e012e6dc3a49dc80c5d0e5d34bb11

--- server/repository/src/migrations/v2_05_00/mod.rs
+++ server/repository/src/migrations/v2_05_00/mod.rs
@@ -4,6 +4,7 @@ mod abbreviation_create_table;
 mod add_contact_form_table;
 mod add_emergency_orders;
 mod new_store_preferences;
+mod remove_contact_form_site_id;
 mod remove_unique_description_on_tmp_breach;
 
 use crate::StorageConnection;
@@ -26,6 +27,7 @@ impl Migration for V2_05_00 {
             Box::new(remove_unique_description_on_tmp_breach::Migrate),
             Box::new(add_emergency_orders::Migrate),
             Box::new(abbreviation_create_table::Migrate),
+            Box::new(remove_contact_form_site_id::Migrate),
         ]
     }
 }
