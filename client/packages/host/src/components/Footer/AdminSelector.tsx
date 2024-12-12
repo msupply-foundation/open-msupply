fb1e0e4b36d7d20f4466dc963f6e839ed7592576
noel-yeldos <noelyeldos@gmail.com>
2024-12-12 13:09:22 +1300

    Move logout from app drawer to user footer
    Source: https://github.com/msupply-foundation/open-msupply/commit/0314a66e1f7c9fc9c515fef2d46ae07f0d8629ef

---
+++ client/packages/host/src/components/Footer/AdminSelector.tsx
@@ -0,0 +1,83 @@
+import React, { FC } from 'react';
+import {
+  Box,
+  CircularProgress,
+  FlatButton,
+  PaperPopoverSection,
+  useAuthContext,
+  usePaperClickPopover,
+  useTranslation,
+  useNavigate,
+  useUserDetails,
+  useConfirmationModal,
+  RouteBuilder,
+  PowerIcon,
+  Typography,
+} from '@openmsupply-client/common';
+import { AppRoute } from '@openmsupply-client/config';
+import { PropsWithChildrenOnly } from '@common/types';
+
+export const AdminSelector: FC<PropsWithChildrenOnly> = ({ children }) => {
+  const { logout, user, token } = useAuthContext();
+  const navigate = useNavigate();
+  const { hide, PaperClickPopover } = usePaperClickPopover();
+  const { isLoading } = useUserDetails(token);
+  const t = useTranslation();
+
+  const handleLogout = () => {
+    navigate(RouteBuilder.create(AppRoute.Login).build());
+    logout();
+  };
+
+  const showConfirmation = useConfirmationModal({
+    onConfirm: handleLogout,
+    message: t('messages.logout-confirm'),
+    title: t('heading.logout-confirm'),
+  });
+
+  const logoutButton = (
+    <FlatButton
+      startIcon={<PowerIcon fontSize="small" color="primary" />}
+      label={t('logout')}
+      disabled={false}
+      onClick={async () => {
+        hide();
+        showConfirmation();
+      }}
+      key={null}
+      sx={{
+        whiteSpace: 'nowrap',
+        overflowX: 'hidden',
+        overflowY: 'visible',
+        textOverflow: 'ellipsis',
+      }}
+    />
+  );
+  return (
+    <PaperClickPopover
+      placement="top"
+      width={300}
+      Content={
+        <PaperPopoverSection label={`${user?.firstName} ${user?.lastName}`}>
+          {isLoading ? (
+            <CircularProgress size={12} />
+          ) : (
+            <Box
+              style={{
+                overflowY: 'auto',
+                maxHeight: 300,
+                gap: 10,
+              }}
+            >
+              <Typography>Username: {user?.name}</Typography>
+              <Typography>Email: {user?.email}</Typography>
+            </Box>
+          )}
+          {logoutButton}
+        </PaperPopoverSection>
+      }
+    >
+      {children}
+    </PaperClickPopover>
+  );
+};
