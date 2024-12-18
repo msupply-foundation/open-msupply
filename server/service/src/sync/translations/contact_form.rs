da0c302a9534586e587f6de663d829b31dce7c18
John Geronimo <johnageronimo1@gmail.com>
2024-12-19 10:02:33 +1300

    add translate to upsert sync test

--- server/service/src/sync/translations/contact_form.rs
+++ server/service/src/sync/translations/contact_form.rs
@@ -76,7 +76,16 @@ impl SyncTranslation for ContactFormTranslation {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use repository::{mock::MockDataInserts, test_db::setup_all};
+    use crate::sync::{
+        test::merge_helpers::merge_all_name_links, translations::ToSyncRecordTranslationType,
+    };
+    use repository::{
+        contact_form_row::ContactFormRow,
+        mock::{mock_contact_form_a, MockData, MockDataInserts},
+        test_db::{setup_all, setup_all_with_data},
+        RowActionType,
+    };
+    use serde_json::json;
 
     #[actix_rt::test]
     async fn test_contact_form_translation() {
@@ -94,5 +103,59 @@ mod tests {
 
             assert_eq!(translation_result, record.translated_record);
         }
+    }
+
+    #[actix_rt::test]
+    async fn test_try_translate_to_upsert_sync_record() {
+        let (mock_data, connection, _, _) = setup_all_with_data(
+            "test_contact_form_push_merged",
+            MockDataInserts::all(),
+            MockData {
+                contact_form: vec![ContactFormRow {
+                    id: mock_contact_form_a().id,
+                    reply_email: mock_contact_form_a().reply_email,
+                    body: mock_contact_form_a().body,
+                    created_datetime: mock_contact_form_a().created_datetime,
+                    user_id: mock_contact_form_a().user_id,
+                    store_id: mock_contact_form_a().store_id,
+                    contact_type: mock_contact_form_a().contact_type,
+                }],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        merge_all_name_links(&connection, &mock_data).unwrap();
+
+        let changelog = ChangelogRow {
+            cursor: 1,
+            table_name: ChangelogTableName::ContactForm,
+            record_id: "contact_id".to_string(),
+            row_action: RowActionType::Upsert,
+            name_id: None,
+            store_id: None,
+            is_sync_update: false,
+            source_site_id: None,
+        };
+
+        let translator = ContactFormTranslation {};
+        assert!(translator.should_translate_to_sync_record(
+            &changelog,
+            &ToSyncRecordTranslationType::PushToOmSupplyCentral
+        ));
+        let translated = translator
+            .try_translate_to_upsert_sync_record(&connection, &changelog)
+            .unwrap();
+
+        assert!(matches!(translated, PushTranslateResult::PushRecord(_)));
+
+        let PushTranslateResult::PushRecord(translated) = translated else {
+            panic!("Test fail, should translate")
+        };
+
+        assert_eq!(
+            translated[0].record.record_data["user_id"],
+            json!("user_account_a")
+        );
     }
 }
