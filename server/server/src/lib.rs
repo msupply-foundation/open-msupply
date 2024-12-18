c82e37f510e412c4f2249b1cbc4c571d7a0d2807
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2024-12-19 11:29:57 +1300

    add email service
    Source: https://github.com/msupply-foundation/open-msupply/commit/4de64fd97dc88576a32652b289c2df667104b807

--- server/server/src/lib.rs
+++ server/server/src/lib.rs
@@ -106,6 +106,7 @@ pub async fn start_server(
         processors_trigger,
         sync_trigger,
         site_is_initialise_trigger,
+        settings.mail.clone(),
     ));
     let loaders = get_loaders(&connection_manager, service_provider.clone()).await;
     let certificates = Certificates::try_load(&settings.server).unwrap();
