f1aba927dce280ee494ab49f21cebac48f34640d
roxy-dao <roxy@msupply.foundation>
2024-12-24 16:09:46 +1300

    Remove FC

--- client/packages/requisitions/src/ResponseRequisition/DetailView/IndicatorEdit/Footer.tsx
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/IndicatorEdit/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import {
   Box,
   AppFooterPortal,
@@ -18,13 +18,13 @@ interface FooterProps {
   requisitionNumber?: number;
 }
 
-export const Footer: FC<FooterProps> = ({
+export const Footer = ({
   hasNext,
   next,
   hasPrevious,
   previous,
   requisitionNumber,
-}) => {
+}: FooterProps) => {
   const navigate = useNavigate();
   const { programIndicatorCode } = useParams();
 
