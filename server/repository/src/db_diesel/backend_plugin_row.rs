cac7a0a9859af121aa1ca4f6f62f51df0bbd986f
Andrei <andrei.evguenov@gmail.com>
2024-12-24 14:29:22 +1300

    Add backend plugin table with translations
    Source: https://github.com/msupply-foundation/open-msupply/commit/c39ded70fe6256c2e32bfde4629aa35a44694772

---
+++ server/repository/src/db_diesel/backend_plugin_row.rs
@@ -0,0 +1,191 @@
+use super::{
+    backend_plugin_row::backend_plugin::dsl as backend_plugin_dsl, ChangeLogInsertRow,
+    ChangelogRepository, ChangelogTableName, RowActionType, StorageConnection,
+};
+
+use crate::{repository_error::RepositoryError, Delete, Upsert};
+use diesel::prelude::*;
+use diesel_derive_enum::DbEnum;
+use serde::{Deserialize, Serialize};
+
+#[derive(DbEnum, Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[cfg_attr(test, derive(strum::EnumIter))]
+pub enum PluginType {
+    #[default]
+    Amc,
+}
+
+#[derive(DbEnum, Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[DbValueStyle = "SCREAMING_SNAKE_CASE"]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[cfg_attr(test, derive(strum::EnumIter))]
+pub enum PluginVariantType {
+    #[default]
+    BoaJs,
+}
+
+table! {
+  backend_plugin (id) {
+      id -> Text,
+      code -> Text,
+      bundle_base64 -> Text,
+      #[sql_name = "type"] type_ -> crate::db_diesel::backend_plugin_row::PluginTypeMapping,
+      variant_type  -> crate::db_diesel::backend_plugin_row::PluginVariantTypeMapping,
+  }
+}
+
+#[derive(
+    Clone, Insertable, Default, Queryable, Debug, PartialEq, Eq, AsChangeset, Serialize, Deserialize,
+)]
+#[diesel(table_name = backend_plugin)]
+pub struct BackendPluginRow {
+    pub id: String,
+    pub code: String,
+    pub bundle_base64: String,
+    #[diesel(column_name = type_)]
+    pub r#type: PluginType,
+    pub variant_type: PluginVariantType,
+}
+
+pub struct BackendPluginRowRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> BackendPluginRowRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        BackendPluginRowRepository { connection }
+    }
+
+    pub fn find_one_by_id(&self, id: &str) -> Result<Option<BackendPluginRow>, RepositoryError> {
+        let result = backend_plugin_dsl::backend_plugin
+            .filter(backend_plugin_dsl::id.eq(id))
+            .first(self.connection.lock().connection())
+            .optional()?;
+        Ok(result)
+    }
+
+    pub fn find_one_by_code(
+        &self,
+        code: &str,
+    ) -> Result<Option<BackendPluginRow>, RepositoryError> {
+        let result = backend_plugin_dsl::backend_plugin
+            .filter(backend_plugin_dsl::code.eq(code))
+            .first(self.connection.lock().connection())
+            .optional()?;
+        Ok(result)
+    }
+
+    pub fn upsert_one(&self, row: &BackendPluginRow) -> Result<i64, RepositoryError> {
+        diesel::insert_into(backend_plugin_dsl::backend_plugin)
+            .values(row)
+            .on_conflict(backend_plugin_dsl::id)
+            .do_update()
+            .set(row)
+            .execute(self.connection.lock().connection())?;
+        self.insert_changelog(&row.id, RowActionType::Upsert)
+    }
+
+    fn insert_changelog(&self, uid: &str, action: RowActionType) -> Result<i64, RepositoryError> {
+        let row = ChangeLogInsertRow {
+            table_name: ChangelogTableName::BackendPlugin,
+            record_id: uid.to_string(),
+            row_action: action,
+            store_id: None,
+            name_link_id: None,
+        };
+
+        ChangelogRepository::new(self.connection).insert(&row)
+    }
+
+    pub fn delete(&self, id: &str) -> Result<Option<i64>, RepositoryError> {
+        let old_row = self.find_one_by_id(id)?;
+        let change_log_id = match old_row {
+            Some(_) => self.insert_changelog(id, RowActionType::Delete)?,
+            None => {
+                return Ok(None);
+            }
+        };
+
+        diesel::delete(backend_plugin_dsl::backend_plugin.filter(backend_plugin_dsl::id.eq(id)))
+            .execute(self.connection.lock().connection())?;
+        Ok(Some(change_log_id))
+    }
+}
+
+impl Upsert for BackendPluginRow {
+    fn upsert(&self, con: &StorageConnection) -> Result<Option<i64>, RepositoryError> {
+        let change_log = BackendPluginRowRepository::new(con).upsert_one(self)?;
+        Ok(Some(change_log))
+    }
+
+    // Test only
+    fn assert_upserted(&self, con: &StorageConnection) {
+        assert_eq!(
+            BackendPluginRowRepository::new(con).find_one_by_id(&self.id),
+            Ok(Some(self.clone()))
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+// Most central data will be soft deleted (via upsert), and this trait will not be implemented
+// backend_plugins don't have referencial relations to any other tables so it's ok to delete as an example
+pub struct BackendPluginRowDelete(pub String);
+impl Delete for BackendPluginRowDelete {
+    fn delete(&self, con: &StorageConnection) -> Result<Option<i64>, RepositoryError> {
+        let change_log_id = BackendPluginRowRepository::new(con).delete(&self.0)?;
+        Ok(change_log_id)
+    }
+    // Test only
+    fn assert_deleted(&self, con: &StorageConnection) {
+        assert_eq!(
+            BackendPluginRowRepository::new(con).find_one_by_id(&self.0),
+            Ok(None)
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{mock::MockDataInserts, test_db::setup_all};
+
+    use super::*;
+    use strum::IntoEnumIterator;
+    use util::assert_variant;
+
+    #[actix_rt::test]
+    async fn backend_plugin_row_enum() {
+        let (_, connection, _, _) =
+            setup_all("backend_plugin_row_enum", MockDataInserts::none()).await;
+
+        let repo = BackendPluginRowRepository::new(&connection);
+        // Try upsert all variants of Language, confirm that diesel enums match postgres
+        for variant in PluginType::iter() {
+            let id = format!("{:?}", variant);
+            let result = repo.upsert_one(&BackendPluginRow {
+                id: id.clone(),
+                r#type: variant.clone(),
+                ..Default::default()
+            });
+            let _ = assert_variant!(result, Ok(_) => {});
+
+            let result = repo.find_one_by_id(&id).unwrap().unwrap();
+            assert_eq!(result.r#type, variant);
+        }
+
+        for variant in PluginVariantType::iter() {
+            let id = format!("{:?}", variant);
+            let result = repo.upsert_one(&BackendPluginRow {
+                id: id.clone(),
+                variant_type: variant.clone(),
+                ..Default::default()
+            });
+            let _ = assert_variant!(result, Ok(_) => {});
+
+            let result = repo.find_one_by_id(&id).unwrap().unwrap();
+            assert_eq!(result.variant_type, variant);
+        }
+    }
+}
---
+++ server/service/src/sync/translations/backend_plugin.rs
@@ -0,0 +1,107 @@
+use repository::{
+    BackendPluginRow, BackendPluginRowDelete, BackendPluginRowRepository, ChangelogRow,
+    ChangelogTableName, StorageConnection, SyncBufferRow,
+};
+
+use super::{
+    PullTranslateResult, PushTranslateResult, SyncTranslation, ToSyncRecordTranslationType,
+};
+
+// Needs to be added to all_translators()
+#[deny(dead_code)]
+pub(crate) fn boxed() -> Box<dyn SyncTranslation> {
+    Box::new(BackendPluginTranslator)
+}
+
+pub(crate) struct BackendPluginTranslator;
+
+impl SyncTranslation for BackendPluginTranslator {
+    fn table_name(&self) -> &str {
+        "backend_plugin"
+    }
+
+    fn pull_dependencies(&self) -> Vec<&str> {
+        vec![]
+    }
+
+    fn try_translate_from_upsert_sync_record(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        Ok(PullTranslateResult::upsert(serde_json::from_str::<
+            BackendPluginRow,
+        >(&sync_record.data)?))
+    }
+
+    fn change_log_type(&self) -> Option<ChangelogTableName> {
+        Some(ChangelogTableName::BackendPlugin)
+    }
+
+    // Only translating and pulling from central server
+    fn should_translate_to_sync_record(
+        &self,
+        row: &ChangelogRow,
+        r#type: &ToSyncRecordTranslationType,
+    ) -> bool {
+        match r#type {
+            ToSyncRecordTranslationType::PullFromOmSupplyCentral => {
+                self.change_log_type().as_ref() == Some(&row.table_name)
+            }
+            _ => false,
+        }
+    }
+
+    fn try_translate_to_upsert_sync_record(
+        &self,
+        connection: &StorageConnection,
+        changelog: &ChangelogRow,
+    ) -> Result<PushTranslateResult, anyhow::Error> {
+        let row = BackendPluginRowRepository::new(connection)
+            .find_one_by_id(&changelog.record_id)?
+            .ok_or(anyhow::Error::msg(format!(
+                "backend_plugin row ({}) not found",
+                changelog.record_id
+            )))?;
+
+        Ok(PushTranslateResult::upsert(
+            changelog,
+            self.table_name(),
+            serde_json::to_value(row)?,
+        ))
+    }
+
+    fn try_translate_from_delete_sync_record(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        Ok(PullTranslateResult::delete(BackendPluginRowDelete(
+            sync_record.record_id.clone(),
+        )))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use repository::{mock::MockDataInserts, test_db::setup_all};
+
+    #[actix_rt::test]
+    async fn test_backend_plugin_translation() {
+        use crate::sync::test::test_data::backend_plugin as test_data;
+        let translator = BackendPluginTranslator;
+
+        let (_, connection, _, _) =
+            setup_all("test_backend_plugin_translation", MockDataInserts::none()).await;
+
+        for record in test_data::test_pull_upsert_records() {
+            assert!(translator.should_translate_from_sync_record(&record.sync_buffer_row));
+            let translation_result = translator
+                .try_translate_from_upsert_sync_record(&connection, &record.sync_buffer_row)
+                .unwrap();
+
+            assert_eq!(translation_result, record.translated_record);
+        }
+    }
+}
