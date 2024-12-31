a97a2d9dba9426a51da83a9353d20585e10f72db
roxy-dao <roxy@msupply.foundation>
2024-12-31 13:58:20 +1300

    Insert loaders
    Source: https://github.com/msupply-foundation/open-msupply/commit/b95cfe269d930f0f73fd9f023eed7d6da30b1b8b

--- server/graphql/core/src/loader/loader_registry.rs
+++ server/graphql/core/src/loader/loader_registry.rs
@@ -448,6 +448,18 @@ pub async fn get_loaders(
         },
         async_std::task::spawn,
     ));
+    loaders.insert(DataLoader::new(
+        RequisitionIndicatorInfoLoader {
+            service_provider: service_provider.clone(),
+        },
+        async_std::task::spawn,
+    ));
+    loaders.insert(DataLoader::new(
+        IndicatorColumnByIdLoader {
+            service_provider: service_provider.clone(),
+        },
+        async_std::task::spawn,
+    ));
 
     loaders
 }
