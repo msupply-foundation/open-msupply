d7c2d9fde78669a8e3df811912bf46408c11f068
roxy-dao <roxy@msupply.foundation>
2025-01-06 10:02:24 +1300

    Add back indicator column loader to register
    Source: https://github.com/msupply-foundation/open-msupply/commit/2300305f931e8b3377661aa656e2dd971befb269

--- server/graphql/core/src/loader/loader_registry.rs
+++ server/graphql/core/src/loader/loader_registry.rs
@@ -460,6 +460,12 @@ pub async fn get_loaders(
         },
         async_std::task::spawn,
     ));
+    loaders.insert(DataLoader::new(
+        IndicatorColumnByIdLoader {
+            service_provider: service_provider.clone(),
+        },
+        async_std::task::spawn,
+    ));
 
     loaders
 }
