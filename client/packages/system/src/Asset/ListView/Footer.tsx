25d6be662eb4da46845d86ca5d4816deca3fa33f
noel-yeldos <noelyeldos@gmail.com>
2025-01-06 12:41:43 +1300

    Update actions footer list view assets

---
+++ client/packages/system/src/Asset/ListView/Footer.tsx
@@ -0,0 +1,41 @@
+import React, { FC, memo } from 'react';
+import {
+  Action,
+  ActionsFooter,
+  DeleteIcon,
+  useTranslation,
+  AppFooterPortal,
+  useIsCentralServerApi,
+} from '@openmsupply-client/common';
+import { useAssetData } from '../api/hooks';
+
+export const FooterComponent: FC = () => {
+  const t = useTranslation();
+  const isCentralServer = useIsCentralServerApi();
+
+  const { selectedRows, ...onDelete } = useAssetData.document.delete();
+  const actions: Action[] = [
+    {
+      label: t('button.delete-lines'),
+      icon: <DeleteIcon />,
+      onClick: onDelete.confirmAndDelete,
+    },
+  ];
+
+  return (
+    <AppFooterPortal
+      Content={
+        <>
+          {selectedRows.length !== 0 && isCentralServer && (
+            <ActionsFooter
+              actions={actions}
+              selectedRowCount={selectedRows.length}
+            />
+          )}
+        </>
+      }
+    />
+  );
+};
+
+export const Footer = memo(FooterComponent);
