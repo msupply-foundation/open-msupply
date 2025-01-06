d8bc10213c622ad63d6b2ed9fca1e5a402516b61
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-01-07 10:49:26 +1300

    add task interval to config
    Source: https://github.com/msupply-foundation/open-msupply/commit/532aa0e50ef60811d30793367cad21251ac75b57

--- server/server/src/scheduled_tasks.rs
+++ server/server/src/scheduled_tasks.rs
@@ -2,16 +2,19 @@ use service::service_provider::ServiceProvider;
 use std::{sync::Arc, time::Duration};
 use tokio::task::JoinHandle;
 
-static TASK_INTERVAL: Duration = Duration::from_secs(10);
+pub fn spawn_scheduled_task_runner(
+    service_provider: Arc<ServiceProvider>,
+    interval_secs: u64,
+) -> JoinHandle<()> {
+    let period: Duration = Duration::from_secs(interval_secs);
 
-pub fn spawn_scheduled_task_runner(service_provider: Arc<ServiceProvider>) -> JoinHandle<()> {
     tokio::spawn(async move {
-        scheduled_task_runner(service_provider).await;
+        scheduled_task_runner(service_provider, period).await;
     })
 }
 
-async fn scheduled_task_runner(service_provider: Arc<ServiceProvider>) {
-    let mut interval = actix_web::rt::time::interval(TASK_INTERVAL);
+async fn scheduled_task_runner(service_provider: Arc<ServiceProvider>, period: Duration) {
+    let mut interval = actix_web::rt::time::interval(period);
     let service_context = service_provider.basic_context().unwrap();
 
     loop {
