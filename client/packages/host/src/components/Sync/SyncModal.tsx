2582673c3c52aaeca216db86bf88039b9d5da2d5
mark-prins <mark@malcolmprins.net>
2025-01-08 17:05:04 +1300

    move the ServerInfo to the settings page

--- client/packages/host/src/components/Sync/SyncModal.tsx
+++ client/packages/host/src/components/Sync/SyncModal.tsx
@@ -16,7 +16,6 @@ import {
 } from '@openmsupply-client/common';
 import { useSync } from '@openmsupply-client/system';
 import { SyncProgress } from '../SyncProgress';
-import { ServerInfo } from './ServerInfo';
 import { BasicModal, IconButton } from '@common/components';
 
 const STATUS_POLLING_INTERVAL = 1000;
@@ -128,7 +127,6 @@ export const SyncModal = ({
           label={t('button.close')}
         />
 
-        <ServerInfo />
         <Grid
           container
           flexDirection="column"
