57012cb99bc0c65832713a877c240ccbbcf3c1df
John Geronimo <johnageronimo1@gmail.com>
2025-01-10 11:51:50 +1300

    remove console log
    Source: https://github.com/msupply-foundation/open-msupply/commit/aec8050364f09404ffcddfdc3e5ebcc2d6d543f9

--- client/packages/requisitions/src/ResponseRequisition/ListView/ListView.tsx
+++ client/packages/requisitions/src/ResponseRequisition/ListView/ListView.tsx
@@ -64,7 +64,6 @@ export const ResponseRequisitionListView: FC = () => {
   });
   const pagination = { page, first, offset };
   const queryParams = { ...filter, sortBy, page, first, offset };
-  console.log('params at list view', queryParams);
   const { data, isError, isLoading } = useResponse.document.list(queryParams);
   const { authoriseResponseRequisitions } = useResponse.utils.preferences();
   useDisableResponseRows(data?.nodes);
