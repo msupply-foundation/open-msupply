e4533ed67acc72682dd7f0d29f5ec50ff3e7483a
Andrei <andrei.evguenov@gmail.com>
2025-01-09 15:25:13 +1300

    Refactor aggregate indicator info

--- server/graphql/core/src/loader/loader_registry.rs
+++ server/graphql/core/src/loader/loader_registry.rs
@@ -462,12 +462,6 @@ pub async fn get_loaders(
         async_std::task::spawn,
     ));
     loaders.insert(DataLoader::new(
-        IndicatorColumnByIdLoader {
-            service_provider: service_provider.clone(),
-        },
-        async_std::task::spawn,
-    ));
-    loaders.insert(DataLoader::new(
         DiagnosisLoader {
             connection_manager: connection_manager.clone(),
         },
