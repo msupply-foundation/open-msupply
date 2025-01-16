7f7232eebfb6df3a68d2e21b764fb84d1cd7daad
Andrei <andrei.evguenov@gmail.com>
2025-01-17 11:08:21 +1300

    Example of overrides

--- client/packages/host/src/App.tsx
+++ client/packages/host/src/App.tsx
@@ -1,6 +1,82 @@
+// @ts-nocheck
 import React, { FC } from 'react';
 import Host from './Host';
 import '@fontsource-variable/inter';
+
+const error = false;
+
+const check = slotProps => {
+  const error = false;
+  const props = {
+    disable: false,
+  };
+
+  console.log({
+    input: {
+      disableInjectingGlobalStyles: true,
+      disableUnderline: error ? true : false,
+      sx: {
+        border: theme =>
+          error ? `2px solid ${theme.palette.error.main}` : 'none',
+
+        backgroundColor: theme =>
+          props.disabled
+            ? theme.palette.background.toolbar
+            : theme.palette.background.menu,
+        borderRadius: 1,
+        padding: 0.5,
+      },
+      ...slotProps?.input,
+    },
+    htmlInput: {
+      style: props?.disabled ? { textOverflow: 'ellipsis' } : {},
+      inputMode: props?.disabled ? undefined : props.inputMode,
+      sx: { padding: 0.5 },
+      ...slotProps?.htmlInput,
+    },
+    ...slotProps,
+  });
+};
+
+console.log(check({ input: { sx: { shouldNotOverrideAll: 'but it does ' } } }));
+
+const check2 = slotProps => {
+  const error = false;
+  const props = {
+    disable: false,
+  };
+
+  console.log({
+    ...slotProps,
+    input: {
+      disableInjectingGlobalStyles: true,
+      disableUnderline: error ? true : false,
+      ...slotProps?.input,
+      sx: {
+        border: theme =>
+          error ? `2px solid ${theme.palette.error.main}` : 'none',
+
+        backgroundColor: theme =>
+          props.disabled
+            ? theme.palette.background.toolbar
+            : theme.palette.background.menu,
+        borderRadius: 1,
+        padding: 0.5,
+        ...slotProps?.input?.sx,
+      },
+    },
+    htmlInput: {
+      style: props?.disabled ? { textOverflow: 'ellipsis' } : {},
+      inputMode: props?.disabled ? undefined : props.inputMode,
+      ...slotProps?.htmlInput,
+      sx: { padding: 0.5, ...slotProps?.htmlInput?.sx },
+    },
+  });
+};
+
+console.log(
+  check2({ input: { sx: { shouldNotOverrideAll: 'it doesnt', padding: 0.6 } } })
+);
 
 const App: FC = () => <Host />;
 
