ef640d982e20f01d6d94610b0e795494af90dfca
Fergus Roache <fergus@msupply.foundation>
2025-01-17 15:05:19 +1300

    Standardise stock on hand in standard reports
    Source: https://github.com/msupply-foundation/open-msupply/commit/f681e90e957acc8b50039c79fb8ab8b9e4cbfe4d

---
+++ server/reports/item-usage/2_5_0/convert_data_js/src/utils.js
@@ -0,0 +1,69 @@
+import { cleanUpNodes, sortNodes } from "../../../../utils";
+
+const processItemLines = (data, sort, dir) => {
+  data.items.nodes.forEach((item) => {
+    // don't add default values if empty object added
+    if (Object.keys(item).length == 0) {
+      return;
+    }
+    item.monthConsumption = calculateQuantity(
+      data.thisMonthConsumption,
+      item.id
+    );
+    item.lastMonthConsumption = calculateQuantity(
+      data.lastMonthConsumption,
+      item.id
+    );
+    item.twoMonthsAgoConsumption = calculateQuantity(
+      data.twoMonthsAgoConsumption,
+      item.id
+    );
+    item.expiringInSixMonths = calculateQuantity(
+      data.expiringInSixMonths,
+      item.id
+    );
+    item.expiringInTwelveMonths = calculateQuantity(
+      data.expiringInTwelveMonths,
+      item.id
+    );
+    item.stockOnOrder = calculateQuantity(data.stockOnOrder, item.id);
+    item.AMC12 = calculateQuantity(data.AMCTwelve, item.id);
+    item.AMC24 = calculateQuantity(data.AMCTwentyFour, item.id);
+    item.SOH = calculateStatValue(item?.stats?.stockOnHand);
+    item.MOS = calculateStatValue(item?.stats?.availableMonthsOfStockOnHand);
+  });
+  let cleanNodes = cleanUpNodes(data.items.nodes);
+  let sortedNodes = sortNodes(cleanNodes, sort, dir);
+  return sortedNodes;
+};
+
+// function adds month consumption to data  (either this or last month)
+const calculateQuantity = (queryResult, id) => {
+  let quantity = 0;
+  if (!!queryResult && !!id) {
+    const node = queryResult.find((element) => element.item_id == id);
+    quantity = node?.quantity ? node.quantity : 0;
+  }
+  // return 0 if quantity is less than 0. This covers use cases such as stock on order which can be negative if invoice line stock is greater than requested stock.
+  if (quantity < 0) {
+    return 0;
+  }
+  return quantity;
+};
+
+const calculateStatValue = (value) => {
+  let returnValue = 0;
+  if (!!value) {
+    // round to 1 decimal
+    returnValue = Math.round(value * 10) / 10;
+  }
+  return returnValue;
+};
+
+module.exports = {
+  calculateQuantity,
+  calculateStatValue,
+  processItemLines,
+  cleanUpNodes,
+  sortNodes,
+};
