e8211c5bb8272695811cbfd459dab04b5197ce7a
Fergus Roache <fergus@msupply.foundation>
2025-01-21 14:23:59 +1300

    Add front end error handling
    Source: https://github.com/msupply-foundation/open-msupply/commit/21d2cb3063e6164b5397dde3e0ea82bf69319417

--- client/packages/requisitions/src/RequestRequisition/api/hooks/document/useInsertProgramRequest.ts
+++ client/packages/requisitions/src/RequestRequisition/api/hooks/document/useInsertProgramRequest.ts
@@ -1,12 +1,34 @@
-import { useQueryClient, useMutation } from '@openmsupply-client/common';
+import { useQueryClient, useMutation, useTranslation, useNotification, InsertProgramRequestRequisitionInput } from '@openmsupply-client/common';
 import { useRequestApi } from '../utils/useRequestApi';
 
 export const useInsertProgramRequest = () => {
   const queryClient = useQueryClient();
   const api = useRequestApi();
-  return useMutation(api.insertProgram, {
+  const t = useTranslation();
+  const { error } = useNotification();
+
+  const { mutateAsync } = useMutation(api.insertProgram, {
     onSuccess: () => {
       queryClient.invalidateQueries(api.keys.base());
     },
   });
+
+  const insert = async (input: InsertProgramRequestRequisitionInput) => {
+    const result = await mutateAsync(input);
+    if (result.__typename == "InsertProgramRequestRequisitionError") {
+      switch (result.error.__typename) {
+        case "MaxOrdersReachedForPeriod": {
+          error(t('error.max-orders-reached-for-period'))()
+          break;
+        }
+        default:
+          error(t('error.unable-to-create-requisition'))()
+      }
+    }
+    return result
+  }
+
+  return {
+    insert
+  }
 };
