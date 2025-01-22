8db605184609cbaa32f16e056bf2c4262ba17c13
roxy-dao <roxy@msupply.foundation>
2025-01-23 12:54:53 +1300

    Remove naive date conversion

--- client/packages/programs/src/JsonForms/components/DateOfBirth.tsx
+++ client/packages/programs/src/JsonForms/components/DateOfBirth.tsx
@@ -61,7 +61,6 @@ const UIComponent = (props: ControlProps) => {
 
   useEffect(() => {
     if (!data) return;
-    const dob = DateUtils.getNaiveDate(data.dateOfBirth);
     setDoB(dob);
     if (dob === null) {
       setAge(undefined);
