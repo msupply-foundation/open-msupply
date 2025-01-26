b5a2b0b840bda39bb3c609265162539f78197a60
Martin Falconer <mcfalconer@gmail.com>
2025-01-27 10:29:23 +1300

    modify update status modal

--- client/packages/coldchain/src/Equipment/DetailView/StatusTab.tsx
+++ client/packages/coldchain/src/Equipment/DetailView/StatusTab.tsx
@@ -3,6 +3,7 @@ import {
   Autocomplete,
   BasicTextInput,
   InputWithLabelRow,
+  Typography,
 } from '@common/components';
 import { LocaleKey, useTranslation } from '@common/intl';
 import {
@@ -15,32 +16,56 @@ import {
 import { AssetLogPanel } from '../Components';
 import { parseLogStatus } from '../utils';
 import { useAssetData } from '@openmsupply-client/system';
+import { useIsGapsStoreOnly } from '@openmsupply-client/common';
 
 const Row = ({
   children,
   label,
+  isGaps,
 }: {
   children: React.ReactNode;
   label: string;
-}) => (
-  <Box paddingTop={1.5}>
-    <InputWithLabelRow
-      labelWidth="200px"
-      label={label}
-      labelProps={{
-        sx: { fontSize: '16px', paddingRight: 2, textAlign: 'right' },
-      }}
-      Input={
-        <Box sx={{}} flex={1}>
-          {children}
-        </Box>
-      }
-    />
-  </Box>
-);
+  isGaps: boolean;
+}) => {
+
+  if (!isGaps) return (
+    <Box paddingTop={1.5}>
+      <InputWithLabelRow
+        labelWidth="160px"
+        label={label}
+        labelProps={{
+          sx: {
+            fontSize: '16px',
+            paddingRight: 2,
+            textAlign: 'right',
+          },
+        }}
+        Input={
+          <Box sx={{}} flex={1}>
+            {children}
+          </Box>
+        }
+      />
+    </Box>);
+
+  return (
+    <Box paddingTop={1.5}>
+      <Typography
+        sx={{
+          fontSize: '1em',
+          fontWeight: 'bold',
+        }}
+      >
+        {label}
+      </Typography>
+      {children}
+    </Box>
+  )
+};
 
 export const StatusTab = ({ draft, onChange, value }: AssetLogPanel) => {
   const t = useTranslation();
+  const isGaps = useIsGapsStoreOnly();
   const debouncedOnChange = useDebounceCallback(
     (patch: Partial<InsertAssetLogInput>) => onChange(patch),
     [onChange],
@@ -65,8 +90,8 @@ export const StatusTab = ({ draft, onChange, value }: AssetLogPanel) => {
   const { data } = useAssetData.log.listReasons(
     draft.status
       ? {
-          assetLogStatus: { equalTo: draft.status },
-        }
+        assetLogStatus: { equalTo: draft.status },
+      }
       : undefined
   );
 
@@ -81,7 +106,7 @@ export const StatusTab = ({ draft, onChange, value }: AssetLogPanel) => {
   return (
     <AssetLogPanel value={value} draft={draft} onChange={onChange}>
       <Box display="flex" flexDirection="column" sx={{ width: '100%' }}>
-        <Row label={t('label.new-functional-status')}>
+        <Row label={t('label.new-functional-status')} isGaps={isGaps}>
           <Autocomplete
             isOptionEqualToValue={option => option?.value === draft.status}
             onChange={(_e, selected) =>
@@ -94,7 +119,7 @@ export const StatusTab = ({ draft, onChange, value }: AssetLogPanel) => {
             width="100%"
           />
         </Row>
-        <Row label={t('label.reason')}>
+        <Row label={t('label.reason')} isGaps={isGaps}>
           <Autocomplete
             disabled={reasons.length === 0}
             options={reasons}
@@ -106,7 +131,7 @@ export const StatusTab = ({ draft, onChange, value }: AssetLogPanel) => {
             value={reasons.find(r => r?.value === draft.reasonId) ?? null}
           />
         </Row>
-        <Row label={t('label.observations')}>
+        <Row label={t('label.observations')} isGaps={isGaps}>
           <BasicTextInput
             multiline
             rows={4}
