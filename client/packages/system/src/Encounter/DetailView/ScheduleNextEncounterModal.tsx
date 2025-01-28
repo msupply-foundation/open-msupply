f21363d282365f260d70c494ffa241b56f48529c
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-01-28 16:49:21 +1300

    add schedule next encounter modal
    Source: https://github.com/msupply-foundation/open-msupply/commit/3714965bce70df0e6f9c6a95c023b318bbdc1763

---
+++ client/packages/system/src/Encounter/DetailView/ScheduleNextEncounterModal.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import {
+  DialogButton,
+  EncounterNodeStatus,
+  RouteBuilder,
+  Stack,
+  useDialog,
+  useNavigate,
+  useNotification,
+  useAuthContext,
+} from '@openmsupply-client/common';
+import { useTranslation } from '@common/intl';
+import {
+  useEncounter,
+  EncounterSchema,
+  DocumentRegistryFragment,
+} from '@openmsupply-client/programs';
+import { AppRoute } from '@openmsupply-client/config';
+import { PatientTabValue } from '../../Patient/PatientView/PatientView';
+import { CreateEncounterForm } from '../../Patient/Encounter';
+
+export const ScheduleNextEncounterModal = ({
+  patientId,
+  encounterConfig,
+  onClose,
+}: {
+  patientId: string;
+  encounterConfig: DocumentRegistryFragment;
+  onClose: () => void;
+}) => {
+  const { user, storeId } = useAuthContext();
+  const t = useTranslation();
+  const [draft, setDraft] = useState<EncounterSchema>({
+    createdDatetime: new Date().toISOString(),
+    createdBy: { id: user?.id ?? '', username: user?.name ?? '' },
+    status: EncounterNodeStatus.Pending,
+    location: {
+      storeId,
+    },
+  });
+  const navigate = useNavigate();
+  const { error } = useNotification();
+  const [hasFormError, setHasFormError] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const handleSave = useEncounter.document.upsert(
+    patientId,
+    encounterConfig.documentType
+  );
+
+  const { Modal } = useDialog({
+    isOpen: true,
+    onClose: onClose,
+  });
+
+  const canSubmit = () =>
+    draft !== undefined && draft.startDatetime && !hasFormError;
+
+  return (
+    <Modal
+      title={t('label.schedule-next-encounter')}
+      cancelButton={<DialogButton variant="cancel" onClick={onClose} />}
+      okButton={
+        <DialogButton
+          variant={'save'}
+          disabled={!canSubmit() || isCreating}
+          onClick={async () => {
+            setIsCreating(true);
+            const { id } = await handleSave(
+              draft,
+              encounterConfig.formSchemaId
+            );
+
+            if (!id) {
+              setIsCreating(false);
+              error(t('error.encounter-not-created'))();
+              return;
+            }
+            navigate(
+              RouteBuilder.create(AppRoute.Dispensary)
+                .addPart(AppRoute.Patients)
+                .addPart(patientId)
+                .addQuery({ tab: PatientTabValue.Encounters })
+                .build()
+            );
+
+            onClose();
+          }}
+        />
+      }
+      width={700}
+    >
+      <React.Suspense fallback={<div />}>
+        <Stack gap={1}>
+          <CreateEncounterForm
+            draft={draft}
+            setDraft={setDraft}
+            setHasFormError={setHasFormError}
+          />
+        </Stack>
+      </React.Suspense>
+    </Modal>
+  );
+};
