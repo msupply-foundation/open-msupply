34bbb72bda37c2f6b56d5a9577316307418892ec
noel-yeldos <noelyeldos@gmail.com>
2025-01-28 15:03:27 +1300

    Add migration for postgres

---
+++ server/repository/src/migrations/v2_06_00/add_create_invoice_from_requisition_permission.rs
@@ -0,0 +1,22 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_create_invoice_from_requisition_permission"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        if cfg!(feature = "postgres") {
+            sql!(
+                connection,
+                r#"
+                    ALTER TABLE user_permission ADD VALUE IF NOT EXISTS 'REQUISITION_CREATE_OUTBOUND_SHIPMENT';
+                "#
+            )?;
+        }
+
+        Ok(())
+    }
+}
