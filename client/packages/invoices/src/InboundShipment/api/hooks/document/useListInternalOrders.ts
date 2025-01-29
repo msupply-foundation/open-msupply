0dfe489d99025c0011412961eb4ab712c3de57a2
roxy-dao <roxy@msupply.foundation>
2025-01-29 14:57:04 +1300

    Create invoice if pref not on

--- client/packages/invoices/src/InboundShipment/api/hooks/document/useListInternalOrders.ts
+++ client/packages/invoices/src/InboundShipment/api/hooks/document/useListInternalOrders.ts
@@ -1,12 +1,18 @@
 import { useQuery } from '@openmsupply-client/common';
 import { useInboundApi } from '../utils/useInboundApi';
 
+const MILLISECONDS_PER_MINUTE = 60 * 1000;
+const POLLING_INTERVAL_MS = 3 * MILLISECONDS_PER_MINUTE;
+const STALE_TIME_MS = 1 * MILLISECONDS_PER_MINUTE;
+
 export const useListInternalOrders = (otherPartyId: string) => {
   const api = useInboundApi();
 
   return {
-    ...useQuery(api.keys.listInternalOrders(otherPartyId), () =>
-      api.get.listInternalOrders(otherPartyId)
+    ...useQuery(
+      api.keys.listInternalOrders(otherPartyId),
+      () => api.get.listInternalOrders(otherPartyId),
+      { cacheTime: POLLING_INTERVAL_MS, staleTime: STALE_TIME_MS }
     ),
   };
 };
