72e9b920e3c4dcdff4ba7ff604f3d624a1aa09ab
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-01-30 12:27:37 +1300

    fix background colour

--- client/packages/common/src/ui/components/inputs/Autocomplete/AutocompleteWithPagination.tsx
+++ client/packages/common/src/ui/components/inputs/Autocomplete/AutocompleteWithPagination.tsx
@@ -201,7 +201,10 @@ export function AutocompleteWithPagination<T extends RecordWithId>({
       onChange={onChange}
       getOptionLabel={getOptionLabel || defaultGetOptionLabel}
       sx={{
-        background: theme => theme.palette.background.drawer,
+        background: theme =>
+          disabled
+            ? theme.palette.background.toolbar
+            : theme.palette.background.drawer,
         borderRadius: 2,
         paddingTop: 0.5,
         paddingBottom: 0.5,
