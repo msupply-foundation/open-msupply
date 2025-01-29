3b87bba93aa7d1ba72635afaecc8f5c09e4de4b0
mark-prins <mark@malcolmprins.net>
2025-01-29 13:15:38 +1300

    test commit

--- .github/workflows/sort-json.sh
+++ .github/workflows/sort-json.sh
@@ -1,9 +1,9 @@
 
-for FILE in `git diff --relative --name-only | grep "locales.*json"`; do
+for FILE in `find . -type f -name "*.json" | grep locales`; do
   echo "Sorting translations in $FILE"
 
   cat "$FILE" | jq --sort-keys >> output.tmp && mv output.tmp "$FILE"
-  yarn prettier --write $FILE
+  # yarn prettier --write $FILE
   git add $FILE
 
 #   yarn "Object.keys(o).sort().reduce((r, k) => (r[k] = o[k], r), {})" $FILE > .husky/tmp.json
