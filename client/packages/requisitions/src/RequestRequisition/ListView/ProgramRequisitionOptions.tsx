489d0af5b6b83775994c3333fed78e4613591e13
roxy-dao <roxy@msupply.foundation>
2025-01-30 13:36:22 +1300

    Message for max periods shown

--- client/packages/requisitions/src/RequestRequisition/ListView/ProgramRequisitionOptions.tsx
+++ client/packages/requisitions/src/RequestRequisition/ListView/ProgramRequisitionOptions.tsx
@@ -205,7 +205,20 @@ export const ProgramRequisitionOptions = ({
       />
       <LabelAndOptions {...suppliers} optionKey="name" />
       <LabelAndOptions {...orderTypes} optionKey="name" />
-      <LabelAndOptions {...periods} optionKey="name" />
+      <Grid item width={'100%'}>
+        <Typography
+          sx={{
+            fontStyle: 'italic',
+            color: 'gray.main',
+            fontSize: '12px',
+            paddingLeft: 20,
+            marginBottom: 0,
+          }}
+        >
+          {t('message.program-period')}
+        </Typography>
+        <LabelAndOptions {...periods} optionKey="name" />
+      </Grid>
       <Grid item>
         <ButtonWithIcon
           Icon={<PlusCircleIcon />}
