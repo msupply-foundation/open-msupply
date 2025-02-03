c27cbe12c753277baf8236060096d27ac17b5322
Martin Falconer <mcfalconer@gmail.com>
2025-02-04 09:09:43 +1300

    undo commenting out of discovery.rs

--- server/server/src/discovery.rs
+++ server/server/src/discovery.rs
@@ -9,20 +9,20 @@ const HARDWARE_ID_KEY: &str = "hardware_id";
 const CLIENT_VERSION: &str = "unspecified";
 
 pub(crate) fn start_discovery(protocol: Protocol, port: u16, hardware_id: String) {
-    // tokio::task::spawn(async move {
-    //     let mut text_record = HashMap::<String, String>::new();
-    //     text_record.insert(HARDWARE_ID_KEY.to_string(), hardware_id.to_string());
-    //     text_record.insert(CLIENT_VERSION_KEY.to_string(), CLIENT_VERSION.to_string());
-    //     text_record.insert(PROTOCOL_KEY.to_string(), protocol.to_string());
+    tokio::task::spawn(async move {
+        let mut text_record = HashMap::<String, String>::new();
+        text_record.insert(HARDWARE_ID_KEY.to_string(), hardware_id.to_string());
+        text_record.insert(CLIENT_VERSION_KEY.to_string(), CLIENT_VERSION.to_string());
+        text_record.insert(PROTOCOL_KEY.to_string(), protocol.to_string());
 
-    //     let registration_result = DNSServiceBuilder::new(SERVICE_NAME, port)
-    //         .with_txt_record(text_record.clone())
-    //         .with_name(NAME)
-    //         .register();
+        let registration_result = DNSServiceBuilder::new(SERVICE_NAME, port)
+            .with_txt_record(text_record.clone())
+            .with_name(NAME)
+            .register();
 
-    //     match registration_result {
-    //         Ok(_service_handle) => futures::future::pending::<()>().await,
-    //         Err(e) => log::error!("Error registering discovery: {:?}", e),
-    //     }
-    // });
+        match registration_result {
+            Ok(_service_handle) => futures::future::pending::<()>().await,
+            Err(e) => log::error!("Error registering discovery: {:?}", e),
+        }
+    });
 }
