6a4073e4e00811bdbf845a38df02e64c8cfba480
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-02-04 14:53:42 +1300

    update to optional filter

--- server/service/src/abbreviation.rs
+++ server/service/src/abbreviation.rs
@@ -5,12 +5,12 @@ use repository::{
 
 pub fn get_all_abbreviations(
     connection_manager: &StorageConnectionManager,
-    filter: AbbreviationFilter,
+    filter: Option<AbbreviationFilter>,
 ) -> Result<Vec<Abbreviation>, RepositoryError> {
     let connection = connection_manager.connection()?;
     let repository = AbbreviationRepository::new(&connection);
 
-    let rows = repository.query_by_filter(filter.clone())?;
+    let rows = repository.query(filter.clone())?;
 
     Ok(rows)
 }
