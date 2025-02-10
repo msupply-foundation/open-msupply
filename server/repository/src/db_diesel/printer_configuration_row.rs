3e7e0bb0c677f274f6dae196c32e2298a3e05783
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-02-10 17:09:49 +1300

    create printer configuration table
    Source: https://github.com/msupply-foundation/open-msupply/commit/a27a51ab40cdfb98a9032470f3a75697073fdfbb

---
+++ server/repository/src/db_diesel/printer_configuration_row.rs
@@ -0,0 +1,42 @@
+use super::printer_configuration_row::printer_configuration::dsl::*;
+use crate::RepositoryError;
+use crate::StorageConnection;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+
+table! {
+    printer_configuration (id) {
+    id -> Text,
+    address -> Text,
+    port -> Integer,
+    label_width -> Integer,
+    label_height -> Integer,
+    }
+}
+
+#[derive(
+    Clone, Insertable, Queryable, Debug, PartialEq, AsChangeset, Eq, Serialize, Deserialize,
+)]
+#[diesel(table_name = printer_configuration)]
+pub struct PrinterConfigurationRow {
+    pub id: String,
+    pub address: String,
+    pub port: i32,
+    pub label_width: i32,
+    pub label_height: i32,
+}
+
+pub struct PrinterConfigurationRowRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+impl<'a> PrinterConfigurationRowRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        PrinterConfigurationRowRepository { connection }
+    }
+
+    pub fn find_all(&self) -> Result<Vec<PrinterConfigurationRow>, RepositoryError> {
+        let result = printer_configuration.load(self.connection.lock().connection())?;
+        Ok(result)
+    }
+}
---
+++ server/repository/src/migrations/v2_06_00/printer_configuration_create_table.rs
@@ -0,0 +1,26 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "printer_configuration_create_table"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+            CREATE TABLE printer_configuration (
+                id TEXT NOT NULL PRIMARY KEY,
+                address TEXT NOT NULL,
+                port INTEGER NOT NULL
+                label_width INTEGER NOT NULL
+                label_height INTEGER NOT NULL
+            );
+        "#
+        )?;
+
+        Ok(())
+    }
+}
