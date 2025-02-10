7348138f77d80747a8130130f97d7a4e1c1956ab
mark-prins <mark@malcolmprins.net>
2025-02-10 20:47:46 +0100

    refine the keyboard shortcuts
    Source: https://github.com/msupply-foundation/open-msupply/commit/7f65a442e4b8180c7f54c985896dc7ff1a14c58f

--- client/packages/host/src/Help/Help.tsx
+++ client/packages/host/src/Help/Help.tsx
@@ -1,20 +1,30 @@
+import React from 'react';
 import {
   AppBarButtonsPortal,
   Box,
+  Typography,
   useInitialisationStatus,
-} from '@openmsupply-client/common/src';
-import React from 'react';
+} from '@openmsupply-client/common';
+import { useTranslation } from '@common/intl';
 import { AppVersion } from '../components';
 import { SiteInfo } from '../components/SiteInfo';
 import { ContactFormSection } from './ContactFormSection';
-import { UserGuide } from '../Admin/UserGuide';
+import { UserGuide } from './UserGuide';
 
-export const Help: React.FC = () => {
+export const Help = () => {
   const { data: initStatus } = useInitialisationStatus();
+  const t = useTranslation();
 
   return (
     <Box flex={1} padding={4} sx={{ maxWidth: 800 }}>
       <UserGuide />
+      <Typography variant="h5" paddingTop={4} paddingBottom={1}>
+        {t('heading.keyboard-shortcuts')}
+      </Typography>
+      <Typography style={{ paddingBottom: 10 }}>
+        {t('message.keyboard-shortcuts')}
+      </Typography>
+
       <ContactFormSection />
       <AppBarButtonsPortal>
         <AppVersion SiteInfo={<SiteInfo siteName={initStatus?.siteName} />} />
