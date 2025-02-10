9832e7b7141569caed0a11d828a22d0f62c6d357
Andrei <andrei.evguenov@gmail.com>
2025-02-10 17:04:50 +1300

    Use manifest description

--- server/cli/src/plugins.rs
+++ server/cli/src/plugins.rs
@@ -1,7 +1,7 @@
 use std::{
     ffi::OsStr,
     fs,
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::{Command, ExitStatus, Stdio},
 };
 
@@ -10,7 +10,7 @@ use cli::{queries_mutations::INSTALL_PLUGINS, Api, ApiError};
 use log::{info, warn};
 use repository::{
     BackendPluginRow, BackendPluginTypes, BackendPluginVariantType, FrontendPluginFile,
-    FrontendPluginFiles, FrontendPluginRow,
+    FrontendPluginFiles, FrontendPluginRow, FrontendPluginTypes,
 };
 use reqwest::Url;
 use serde::Deserialize;
@@ -18,6 +18,7 @@ use serde_json::json;
 use service::backend_plugin::plugin_provider::PluginBundle;
 
 use thiserror::Error as ThisError;
+use util::format_error;
 #[derive(ThisError, Debug)]
 pub(super) enum Error {
     #[error("Failed to read dir {0}")]
@@ -26,8 +27,6 @@ pub(super) enum Error {
     FailedToGetFileOrDir(PathBuf, #[source] std::io::Error),
     #[error("Failed to open manifest file {0}")]
     CannotOpenManifestFile(PathBuf, #[source] std::io::Error),
-    #[error("Failed to parse manifest file {0}")]
-    CannotReadManifestFile(PathBuf, #[source] serde_json::Error),
     #[error("Path does not have parent {0}")]
     PathDoesNotHaveParent(PathBuf),
     #[error("Failed to read bundle file {0}")]
@@ -89,42 +88,42 @@ pub(super) struct GenerateAndInstallPluginBundle {
     #[clap(long)]
     password: String,
 }
-#[derive(Deserialize)]
-struct BackendManifestJson {
-    code: String,
-    types: BackendPluginTypes,
-    variant_type: BackendPluginVariantType,
-    bundle_path: PathBuf,
-}
 
+#[derive(Deserialize, Debug)]
+#[serde(tag = "target")]
+enum PluginDescription {
+    #[serde(rename = "backend")]
+    Backend {
+        types: BackendPluginTypes,
+        variant_type: BackendPluginVariantType,
+    },
+    #[serde(rename = "frontend")]
+    FrontEnd { types: FrontendPluginTypes },
+}
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct FrontendManifestJson {
+struct ManifestJson {
     #[serde(rename = "name")]
     code: String,
-    is_open_msupply_plugin: bool,
-}
-
-struct ManifestNames<'a> {
-    backend: &'a OsStr,
-    frontend: &'a OsStr,
+    om_supply_plugin: PluginDescription,
 }
 
 pub(crate) fn generate_plugin_bundle(
     GeneratePluginBundle { in_dir, out_file }: GeneratePluginBundle,
 ) -> Result<(), Error> {
-    let ignore_paths = vec![OsStr::new("node_modules"), OsStr::new("target")];
-    let manifest_names = ManifestNames {
-        backend: OsStr::new("plugin_manifest.json"),
-        frontend: OsStr::new("package.json"),
-    };
+    let ignore_paths = vec![
+        OsStr::new("node_modules"),
+        OsStr::new("dist"),
+        OsStr::new("target"),
+    ];
+    let manifest_name = OsStr::new("package.json");
 
     let mut bundle = PluginBundle {
         backend_plugins: Vec::new(),
         frontend_plugins: Vec::new(),
     };
 
-    generate_bundle_recursive(&mut bundle, &ignore_paths, &manifest_names, &in_dir)?;
+    generate_bundle_recursive(&mut bundle, &ignore_paths, manifest_name, &in_dir)?;
     fs::write(
         &out_file,
         serde_json::to_string_pretty(&bundle).map_err(Error::FailedToSerializeBundle)?,
@@ -137,19 +136,15 @@ pub(crate) fn generate_plugin_bundle(
 fn generate_bundle_recursive(
     bundle: &mut PluginBundle,
     ignore_paths: &Vec<&OsStr>,
-    manifest_names: &ManifestNames,
+    manifest_name: &OsStr,
     path: &PathBuf,
 ) -> Result<(), Error> {
     if let Some(_) = ignore_paths.iter().find(|p| Some(**p) == path.file_name()) {
         return Ok(());
     }
 
-    if path.is_file() && Some(manifest_names.backend) == path.file_name() {
-        return process_backend_manifest(bundle, path);
-    }
-
-    if path.is_file() && Some(manifest_names.frontend) == path.file_name() {
-        return process_frontend_manifest(bundle, path);
+    if path.is_file() && Some(manifest_name) == path.file_name() {
+        return process_manifest(bundle, path);
     }
 
     if !path.is_dir() {
@@ -163,65 +158,34 @@ fn generate_bundle_recursive(
         let next_path = file_or_folder
             .map_err(|e| Error::FailedToGetFileOrDir(path.clone(), e))?
             .path();
-        generate_bundle_recursive(bundle, &ignore_paths, manifest_names, &next_path)?;
+        generate_bundle_recursive(bundle, &ignore_paths, manifest_name, &next_path)?;
     }
 
     Ok(())
 }
 
-fn process_backend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Result<(), Error> {
-    let BackendManifestJson {
+fn process_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Result<(), Error> {
+    let ManifestJson {
         code,
-        types,
-        variant_type,
-        bundle_path,
-    } = serde_json::from_reader(
+        om_supply_plugin,
+    } = match serde_json::from_reader(
         fs::File::open(path).map_err(|e| Error::CannotOpenManifestFile(path.clone(), e))?,
-    )
-    .map_err(|e| Error::CannotReadManifestFile(path.clone(), e))?;
-
-    let bundle_path = path
-        .parent()
-        .ok_or(Error::PathDoesNotHaveParent(path.clone()))?
-        .join(bundle_path);
-
-    let bundle_base64 = BASE64_STANDARD
-        .encode(fs::read(bundle_path).map_err(|e| Error::FailedToReadBundleFile(path.clone(), e))?);
-
-    bundle.backend_plugins.push(BackendPluginRow {
-        // TODO for now id = code in the future id = code + version (similar to reports)
-        id: code.clone(),
-        bundle_base64: bundle_base64,
-        variant_type,
-        types,
-        code,
-    });
-
-    Ok(())
-}
-
-fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Result<(), Error> {
-    let Ok(FrontendManifestJson {
-        code,
-        is_open_msupply_plugin,
-    }) = serde_json::from_reader(
-        fs::File::open(path).map_err(|e| Error::CannotOpenManifestFile(path.clone(), e))?,
-    )
-    else {
-        warn!("Ignoring manifest file: isOpenMsupplyPlugin is missing from {path:#?}");
-        return Ok(());
+    ) {
+        Ok(d) => d,
+        Err(e) => {
+            warn!(
+                "Ignoring manifest file, failed to parse as manifest file {path:#?} {}",
+                format_error(&e)
+            );
+            return Ok(());
+        }
     };
-
-    if !is_open_msupply_plugin {
-        warn!("Ignoring manifest file: isOpenMsupplyPlugin flag is false in {path:#?}");
-        return Ok(());
-    }
 
     let plugin_root = path
         .parent()
         .ok_or(Error::PathDoesNotHaveParent(path.clone()))?;
 
-    info!("Building plugin: {path:#?}");
+    info!("Building plugin: {plugin_root:#?}");
 
     // Yarn install
     run_command_with_error(
@@ -240,6 +204,54 @@ fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Resul
     )
     .map_err(|e| Error::FailedToBuildPlugin(plugin_root.to_path_buf(), e))?;
 
+    match om_supply_plugin {
+        PluginDescription::Backend {
+            types,
+            variant_type,
+        } => bundle_backend_plugin(bundle, code, types, variant_type, plugin_root)?,
+        PluginDescription::FrontEnd { types } => {
+            bundle_frontend_plugin(bundle, code, types, plugin_root)?
+        }
+    }
+
+    Ok(())
+}
+
+fn bundle_backend_plugin(
+    bundle: &mut PluginBundle,
+    code: String,
+    types: BackendPluginTypes,
+    variant_type: BackendPluginVariantType,
+    plugin_root: &Path,
+) -> Result<(), Error> {
+    // Backend plugin bundle will be located in {plugindir}/dist/plugin.js
+    let bundle_base64 = BASE64_STANDARD.encode(
+        fs::read(plugin_root.join("dist").join("plugin.js"))
+            .map_err(|e| Error::FailedToReadBundleFile(plugin_root.to_path_buf(), e))?,
+    );
+
+    bundle.backend_plugins.push(BackendPluginRow {
+        // TODO for now id = code in the future id = code + version (similar to reports)
+        id: code.clone(),
+        bundle_base64: bundle_base64,
+        variant_type,
+        types,
+        code,
+    });
+
+    Ok(())
+}
+
+fn bundle_frontend_plugin(
+    bundle: &mut PluginBundle,
+    code: String,
+    types: FrontendPluginTypes,
+    plugin_root: &Path,
+) -> Result<(), Error> {
+    // Frontend plugin bundle will be in {plugindir}/dist/ folder, consisting of one or many files
+    // with entry point starting with plugin code. Any files starting with 'main' or having 'LICENSE' in their name
+    // will be removed (main file is something generate by webpack as executable entrypoint for the application), vs module like
+    // library export that we are expecting for the plugin bundle
     let dist_folder = plugin_root.join("dist");
 
     let dist_files =
@@ -250,8 +262,9 @@ fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Resul
 
     for file_or_folder in dist_files {
         let next_path = file_or_folder
-            .map_err(|e| Error::FailedToGetFileOrDir(path.clone(), e))?
+            .map_err(|e| Error::FailedToGetFileOrDir(dist_folder.clone(), e))?
             .path();
+
         if !next_path.is_file() {
             continue;
         }
@@ -261,12 +274,10 @@ fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Resul
         if file_name.starts_with("main") {
             continue;
         }
-
+        // Ingore any licence files
         if file_name.contains("LICENSE") {
             continue;
         }
-
-        info!("bundling files {file_name}");
 
         let file = FrontendPluginFile {
             file_name,
@@ -276,6 +287,7 @@ fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Resul
             ),
         };
 
+        // Set the entry if start with plugin code
         if file.file_name.starts_with(&code) {
             entry = Some(file.file_name.clone())
         }
@@ -295,6 +307,7 @@ fn process_frontend_manifest(bundle: &mut PluginBundle, path: &PathBuf) -> Resul
         code,
         entry_point,
         files: FrontendPluginFiles(files),
+        types,
     });
 
     Ok(())
