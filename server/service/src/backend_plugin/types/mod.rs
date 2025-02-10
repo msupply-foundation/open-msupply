c07c512593c6e33e747e03c0431caaeb532d03e8
Andrei <andrei.evguenov@gmail.com>
2025-02-10 18:16:30 +1300

    Bundle plugins together

--- server/service/src/backend_plugin/types/mod.rs
+++ server/service/src/backend_plugin/types/mod.rs
@@ -1,1 +1,20 @@
+use ts_rs::TS;
+
 pub mod amc;
+
+#[derive(TS)]
+#[allow(unused)]
+struct Function<I: TS, O: TS> {
+    input: I,
+    output: O,
+}
+#[derive(TS)]
+#[allow(unused)]
+struct PluginTypes {
+    average_monthly_consumption: Function<amc::Input, amc::Output>,
+}
+
+#[test]
+fn export_plugin_typescript() {
+    PluginTypes::export_all_to("../../client/packages/plugins/backendTypes/generated").unwrap();
+}
