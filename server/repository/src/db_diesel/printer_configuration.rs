ecfc1af3857ba08c5718175b4ad579dc3c18838c
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-02-12 14:29:48 +1300

    add upsert and filter to repo

--- server/repository/src/db_diesel/printer_configuration.rs
+++ server/repository/src/db_diesel/printer_configuration.rs
@@ -1,20 +1,41 @@
 use super::{
-    printer_configuration_row::printer_configuration, PrinterConfigurationRow, StorageConnection,
+    printer_configuration_row::printer_configuration, EqualFilter, PrinterConfigurationRow,
+    StorageConnection, StringFilter,
 };
 
-use crate::{repository_error::RepositoryError, DBType};
+use crate::{
+    diesel_macros::{apply_equal_filter, apply_string_filter},
+    repository_error::RepositoryError,
+    DBType,
+};
 use diesel::{dsl::IntoBoxed, prelude::*};
 
 pub type PrinterConfiguration = PrinterConfigurationRow;
 
 #[derive(Clone, Default, PartialEq, Debug)]
 pub struct PrinterConfigurationFilter {
-    pub id: String,
-    pub description: String,
-    pub address: String,
-    pub port: u16,
-    pub label_width: i32,
-    pub label_height: i32,
+    pub id: Option<EqualFilter<String>>,
+    pub description: Option<StringFilter>,
+    pub address: Option<EqualFilter<String>>,
+}
+
+impl PrinterConfigurationFilter {
+    pub fn new() -> PrinterConfigurationFilter {
+        Self::default()
+    }
+
+    pub fn id(mut self, filter: EqualFilter<String>) -> Self {
+        self.id = Some(filter);
+        self
+    }
+    pub fn description(mut self, filter: EqualFilter<String>) -> Self {
+        self.id = Some(filter);
+        self
+    }
+    pub fn address(mut self, filter: EqualFilter<String>) -> Self {
+        self.id = Some(filter);
+        self
+    }
 }
 
 pub struct PrinterConfigurationRepository<'a> {
@@ -26,18 +47,52 @@ impl<'a> PrinterConfigurationRepository<'a> {
         PrinterConfigurationRepository { connection }
     }
 
-    pub fn query(&self) -> Result<Vec<PrinterConfiguration>, RepositoryError> {
-        let query = create_query();
+    pub fn count(
+        &self,
+        filter: Option<PrinterConfigurationFilter>,
+    ) -> Result<i64, RepositoryError> {
+        let query = Self::create_filtered_query(filter);
+
+        Ok(query
+            .count()
+            .get_result(self.connection.lock().connection())?)
+    }
+
+    pub fn query_by_filter(
+        &self,
+        filter: PrinterConfigurationFilter,
+    ) -> Result<Vec<PrinterConfiguration>, RepositoryError> {
+        self.query(Some(filter))
+    }
+
+    pub fn query(
+        &self,
+        filter: Option<PrinterConfigurationFilter>,
+    ) -> Result<Vec<PrinterConfiguration>, RepositoryError> {
+        let query = Self::create_filtered_query(filter);
 
         let result = query.load::<PrinterConfiguration>(self.connection.lock().connection())?;
 
         Ok(result)
     }
+
+    pub fn create_filtered_query(
+        filter: Option<PrinterConfigurationFilter>,
+    ) -> BoxedPrinterConfigurationQuery {
+        let mut query = printer_configuration::table.into_boxed();
+
+        if let Some(filter) = filter {
+            apply_equal_filter!(query, filter.id, printer_configuration::id);
+            apply_string_filter!(
+                query,
+                filter.description,
+                printer_configuration::description
+            );
+            apply_equal_filter!(query, filter.address, printer_configuration::address);
+        }
+
+        query
+    }
 }
+
 type BoxedPrinterConfigurationQuery = IntoBoxed<'static, printer_configuration::table, DBType>;
-
-fn create_query() -> BoxedPrinterConfigurationQuery {
-    let query = printer_configuration::table.into_boxed();
-
-    query
-}
--- server/repository/src/db_diesel/printer_configuration_row.rs
+++ server/repository/src/db_diesel/printer_configuration_row.rs
@@ -5,14 +5,14 @@ use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
 table! {
-    printer_configuration (id) {
-    id -> Text,
-    description -> Text,
-    address -> Text,
-    port -> Integer,
-    label_width -> Integer,
-    label_height -> Integer,
-    }
+printer_configuration (id) {
+id -> Text,
+description -> Text,
+address -> Text,
+port -> Integer,
+label_width -> Integer,
+label_height -> Integer,
+}
 }
 
 #[derive(
@@ -35,6 +35,29 @@ pub struct PrinterConfigurationRowRepository<'a> {
 impl<'a> PrinterConfigurationRowRepository<'a> {
     pub fn new(connection: &'a StorageConnection) -> Self {
         PrinterConfigurationRowRepository { connection }
+    }
+
+    pub fn upsert_one(&self, row: &PrinterConfigurationRow) -> Result<(), RepositoryError> {
+        diesel::insert_into(printer_configuration::table)
+            .values(row)
+            .on_conflict(printer_configuration::id)
+            .do_update()
+            .set(row)
+            .execute(self.connection.lock().connection())?;
+
+        Ok(())
+    }
+
+    pub fn find_one_by_id(
+        &self,
+        printer_configuration_id: &str,
+    ) -> Result<Option<PrinterConfigurationRow>, RepositoryError> {
+        let result: Option<PrinterConfigurationRow> = printer_configuration::table
+            .filter(printer_configuration::id.eq(printer_configuration_id))
+            .first(self.connection.lock().connection())
+            .optional()?;
+
+        Ok(result)
     }
 
     pub fn find_all(&self) -> Result<Vec<PrinterConfigurationRow>, RepositoryError> {
