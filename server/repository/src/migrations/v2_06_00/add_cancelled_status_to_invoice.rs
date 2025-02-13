ee8886a12315b79dd67490a0d14aba180443bbb0
mSupplyZachariah <zachariah@msupply.foundation>
2025-02-14 06:55:57 +1300

    remove unnecessary parts of migration now that cancelled_datetime is not required

---
+++ server/repository/src/migrations/v2_06_00/add_cancelled_status_to_invoice.rs
@@ -0,0 +1,20 @@
+use crate::migrations::*;
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_cancelled_status_to_invoice"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        if cfg!(feature = "postgres") {
+            sql!(
+                connection,
+                r#"
+                ALTER TYPE invoice_status ADD VALUE IF NOT EXISTS 'CANCELLED';
+                "#,
+            )?;
+        }
+        Ok(())
+    }
+}
