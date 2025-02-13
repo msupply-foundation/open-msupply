3027bdc9c350bcfd7822fca6e328cdc8cfbe2ba3
John Geronimo <johnageronimo1@gmail.com>
2025-02-13 16:10:49 +1300

    remove use patient insurances
    Source: https://github.com/msupply-foundation/open-msupply/commit/aa4d0205b3d4eca3162f422c8097ce46a06eae80

--- client/packages/system/src/Patient/api/hooks/document/usePatientInsurances.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@openmsupply-client/common';
-import { usePatientApi } from '../utils/usePatientApi';
-
-export const usePatientInsurances = () => {
-  const api = usePatientApi();
-  return useQuery(api.keys.getPatientInsurances(), () =>
-    api.getPatientInsurances()
-  );
-};
