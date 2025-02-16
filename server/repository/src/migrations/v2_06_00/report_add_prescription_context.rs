4da0325cb9f3a706969da8f79b5ddb0b2652b3ba
noel-yeldos <noelyeldos@gmail.com>
2025-02-17 12:30:42 +1300

    Update migration to have correct name

--- server/repository/src/migrations/v2_06_00/report_add_prescription_context.rs
+++ server/repository/src/migrations/v2_06_00/report_add_prescription_context.rs
@@ -4,7 +4,7 @@ pub(crate) struct Migrate;
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
-        "report_add_prescriptions_context"
+        "report_add_prescription_context"
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
@@ -12,7 +12,7 @@ impl MigrationFragment for Migrate {
             sql!(
                 connection,
                 r#"
-            ALTER TYPE context_type ADD VALUE 'PRESCRIPTIONS';
+            ALTER TYPE context_type ADD VALUE 'PRESCRIPTION';
             "#
             )?;
         }
