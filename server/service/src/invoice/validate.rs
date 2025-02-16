5d21db4e8ae8af02990cd4159d06f7480597ac1c
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-02-17 11:33:32 +1300

    Improve back-end validation logic

--- server/service/src/invoice/validate.rs
+++ server/service/src/invoice/validate.rs
@@ -48,10 +48,7 @@ pub fn check_invoice_is_editable(invoice: &InvoiceRow) -> bool {
         InvoiceType::Prescription => {
             matches!(
                 status,
-                InvoiceStatus::New
-                    | InvoiceStatus::Allocated
-                    | InvoiceStatus::Picked
-                    | InvoiceStatus::Verified
+                InvoiceStatus::New | InvoiceStatus::Allocated | InvoiceStatus::Picked
             )
         }
         InvoiceType::InventoryAddition | InvoiceType::InventoryReduction => {
