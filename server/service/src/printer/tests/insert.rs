f541ef1a463d62041e75951ed6e00b69c7d066cd
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-02-17 14:14:25 +1300

    extra validation and tests

--- server/service/src/printer/tests/insert.rs
+++ server/service/src/printer/tests/insert.rs
@@ -6,13 +6,15 @@ mod test {
     };
     use repository::{
         mock::{mock_printer_a, MockDataInserts},
+        printer::{PrinterFilter, PrinterRepository},
         test_db::setup_all,
+        EqualFilter, PrinterRow,
     };
 
     #[actix_rt::test]
-    async fn printer_service_update_errors() {
+    async fn printer_insert_errors() {
         let (_, _, connection_manager, _) =
-            setup_all("printer_service_update_errors", MockDataInserts::all()).await;
+            setup_all("printer_insert_errors", MockDataInserts::all()).await;
 
         let service_provider = ServiceProvider::new(connection_manager);
         let context = service_provider
@@ -20,7 +22,7 @@ mod test {
             .unwrap();
         let service = service_provider.printer_service;
 
-        //Printer does not exist
+        //Printer id already exists
         let result = service.insert_printer(
             &context,
             InsertPrinter {
@@ -29,6 +31,73 @@ mod test {
             },
         );
 
-        assert_eq!(result, Err(InsertPrinterError::DuplicatePrinter));
+        assert_eq!(result, Err(InsertPrinterError::DuplicatePrinterId));
+
+        //Printer description already exists
+        let result = service.insert_printer(
+            &context,
+            InsertPrinter {
+                description: "Room one".to_string(),
+                ..Default::default()
+            },
+        );
+
+        assert_eq!(result, Err(InsertPrinterError::DuplicatePrinterDescription));
+
+        //Printer address already exists
+        let result = service.insert_printer(
+            &context,
+            InsertPrinter {
+                address: "111.222.1.222".to_string(),
+                ..Default::default()
+            },
+        );
+
+        assert_eq!(result, Err(InsertPrinterError::DuplicatePrinterAddress));
+    }
+
+    #[actix_rt::test]
+    async fn insert_printer_success() {
+        let (_, _, connection_manager, _) =
+            setup_all("insert_printer_success", MockDataInserts::all()).await;
+
+        let connection = connection_manager.connection().unwrap();
+        let printer_repository = PrinterRepository::new(&connection);
+        let service_provider = ServiceProvider::new(connection_manager);
+        let context = service_provider
+            .context(mock_printer_a().id, "".to_string())
+            .unwrap();
+        let service = service_provider.printer_service;
+
+        let result_printer = PrinterRow {
+            id: "new_id".to_owned(),
+            description: "new_description".to_owned(),
+            address: "new_address".to_owned(),
+            port: 8000.to_owned(),
+            label_width: 50.to_owned(),
+            label_height: 70.to_owned(),
+        };
+
+        assert_eq!(
+            service.insert_printer(
+                &context,
+                InsertPrinter {
+                    id: "new_id".to_owned(),
+                    description: "new_description".to_owned(),
+                    address: "new_address".to_owned(),
+                    port: 8000.to_owned(),
+                    label_width: 50.to_owned(),
+                    label_height: 70.to_owned(),
+                },
+            ),
+            Ok(result_printer.clone())
+        );
+
+        assert_eq!(
+            printer_repository
+                .query_by_filter(PrinterFilter::new().id(EqualFilter::equal_to("new_id")))
+                .unwrap(),
+            vec![result_printer]
+        );
     }
 }
--- server/service/src/printer/tests/query.rs
+++ server/service/src/printer/tests/query.rs
@@ -2,10 +2,9 @@
 mod test {
     use crate::service_provider::ServiceProvider;
     use repository::{
-        mock::{mock_printer_a, MockData, MockDataInserts},
+        mock::{mock_printer_a, mock_printer_b, MockData, MockDataInserts},
         printer::PrinterFilter,
         test_db::setup_all_with_data,
-        PrinterRow,
     };
 
     #[actix_rt::test]
@@ -14,14 +13,7 @@ mod test {
             "test_printer_query",
             MockDataInserts::all(),
             MockData {
-                printer: vec![PrinterRow {
-                    id: mock_printer_a().id,
-                    description: mock_printer_a().description,
-                    address: mock_printer_a().address,
-                    port: mock_printer_a().port,
-                    label_width: mock_printer_a().label_width,
-                    label_height: mock_printer_a().label_height,
-                }],
+                printer: vec![mock_printer_a(), mock_printer_b()],
                 ..Default::default()
             },
         )
@@ -30,13 +22,13 @@ mod test {
         let service_provider = ServiceProvider::new(connection_manager);
         let service = service_provider.printer_service;
 
-        let printer = mock_data["base"].printer.clone();
+        let printers = mock_data["base"].printer.clone();
 
         //Get All Printers
         let result = service
             .get_printers(&connection, Some(PrinterFilter::new()))
             .unwrap();
 
-        assert_eq!(result, printer);
+        assert_eq!(result, printers);
     }
 }
