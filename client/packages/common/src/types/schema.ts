b20b39a723a1ce7b41f0a9693c26369a51943414
Ethan McQuarrie <ethan.m@msupply.foundation>
2025-02-19 12:28:44 +1300

    Regenerate schema
    Source: https://github.com/msupply-foundation/open-msupply/commit/5b6cc1d9de374dd9aeebf40c759a61d01da03e71

--- client/packages/common/src/types/schema.ts
+++ client/packages/common/src/types/schema.ts
@@ -6952,6 +6952,7 @@ export enum ReportContext {
 export type ReportFilterInput = {
   context?: InputMaybe<EqualFilterReportContextInput>;
   id?: InputMaybe<EqualFilterStringInput>;
+  isActive?: InputMaybe<Scalars['Boolean']['input']>;
   name?: InputMaybe<StringFilterInput>;
   subContext?: InputMaybe<EqualFilterStringInput>;
 };
@@ -6961,6 +6962,7 @@ export type ReportNode = {
   argumentSchema?: Maybe<FormSchemaNode>;
   context: ReportContext;
   id: Scalars['String']['output'];
+  isActive: Scalars['Boolean']['output'];
   isCustom: Scalars['Boolean']['output'];
   /** Human readable name of the report */
   name: Scalars['String']['output'];
