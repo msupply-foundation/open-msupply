b232e5fc62318d17577b778e656f68b57d3316f9
Andrei <andrei.evguenov@gmail.com>
2025-02-20 18:08:27 +1300

    Add plugin data integration test

--- server/service/src/sync/test/integration/omsupply_central/test.rs
+++ server/service/src/sync/test/integration/omsupply_central/test.rs
@@ -1,11 +1,23 @@
 #[cfg(test)]
 mod tests {
     use crate::sync::test::integration::omsupply_central::{
-        asset::AssetTester, test_omsupply_central_records, test_omsupply_central_remote_records,
+        asset::AssetTester,
+        plugin_data::{PluginDataCentral, PluginDataRemote},
+        test_omsupply_central_records, test_omsupply_central_remote_records,
     };
 
     #[actix_rt::test]
-    async fn integration_sync_omsupply_central_sync_asset() {
+    async fn integration_sync_omsupply_central_sync_plugin_data() {
+        test_omsupply_central_records("plugin_data", &PluginDataCentral).await;
+    }
+
+    #[actix_rt::test]
+    async fn integration_sync_omsupply_remote_sync_plugin_data() {
+        test_omsupply_central_remote_records("asset", &PluginDataRemote).await;
+    }
+
+    #[actix_rt::test]
+    async fn integration_sync_omsupply_remote_sync_asset() {
         test_omsupply_central_remote_records("asset", &AssetTester).await;
     }
 }
