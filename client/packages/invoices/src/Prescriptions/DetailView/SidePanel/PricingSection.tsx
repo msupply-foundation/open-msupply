165669e5bc767109491598b77f01a40fd7823f41
mSupplyZachariah <zachariah@msupply.foundation>
2025-02-26 10:35:47 +1300

    wip add insurance section
    Source: https://github.com/msupply-foundation/open-msupply/commit/0a274af46a75070ee0cb4d6ce7156c09e59dacfe

--- client/packages/invoices/src/Prescriptions/DetailView/SidePanel/PricingSection.tsx
+++ client/packages/invoices/src/Prescriptions/DetailView/SidePanel/PricingSection.tsx
@@ -7,6 +7,7 @@ import {
   PanelRow,
   useTranslation,
   useFormatCurrency,
+  InfoTooltipIcon,
 } from '@openmsupply-client/common';
 import { useInsurance, usePrescription } from '../../api';
 import { usePrescriptionGraphQL } from '../../api/usePrescriptionGraphQL';
@@ -16,23 +17,53 @@ export const PricingSectionComponent = () => {
   const c = useFormatCurrency();
 
   const {
-    query: { data: prescriptionData}
+    query: { data: prescriptionData },
   } = usePrescription();
   const pricing = prescriptionData?.pricing;
   if (!pricing) return null;
 
   const insuranceId = prescriptionData.nameInsuranceJoinId;
-  const { storeId } = usePrescriptionGraphQL()
+  const { storeId } = usePrescriptionGraphQL();
   const {
-    query: {data: insuranceData}
-   } = useInsurance(insuranceId, storeId);
+    query: { data: insuranceData },
+  } = useInsurance(insuranceId, storeId);
 
   return (
     <DetailPanelSection title={t('heading.pricing')}>
       <Grid container gap={0.5}>
-        
         {insuranceData && (
-        <>
+          <>
+            <PanelRow>
+              <InfoTooltipIcon
+                title={t('messages.insurance-description')}
+              />
+              <PanelLabel fontWeight="bold">
+                {t('heading.insurance')}
+              </PanelLabel>
+            </PanelRow>
+
+
+            <PanelRow sx={{ marginLeft: '10px' }}>
+              <PanelLabel>{t('label.provider-name')}</PanelLabel>
+              <PanelField>{insuranceData.insuranceProviders?.providerName}</PanelField>
+            </PanelRow>
+
+            <PanelRow sx={{ marginLeft: '10px' }}>
+              <PanelLabel>{t('label.policy-number')}</PanelLabel>
+              <PanelField>{insuranceData.policyNumber}</PanelField>
+            </PanelRow>
+
+            <PanelRow sx={{ marginLeft: '10px' }}>
+              <PanelLabel>{t('label.discount-amount')}</PanelLabel>
+              <PanelField>{c(prescriptionData.insuranceDiscountAmount ?? 0)}</PanelField>
+            </PanelRow>
+
+            <PanelRow sx={{ marginLeft: '10px' }}>
+              <PanelLabel>{t('label.discount-percentage')}</PanelLabel>
+              <PanelField>{insuranceData.discountPercentage}%</PanelField>
+            </PanelRow>
+
+            {/*         
         <PanelRow>
           <PanelLabel>{t('label.insurance-provider-name')}</PanelLabel>
           <PanelField>
@@ -57,8 +88,8 @@ export const PricingSectionComponent = () => {
         <PanelRow>
           <PanelLabel>{t('label.insurance-discount-percentage')}</PanelLabel>
           <PanelField>{insuranceData.discountPercentage}% </PanelField>
-        </PanelRow>
-        </>
+        </PanelRow> */}
+          </>
         )}
 
         <PanelRow>
