cafe814d6ca1b2cd6295b1efcd157163951f8ef9
roxy-dao <roxy@msupply.foundation>
2025-02-28 11:44:54 +1300

    Right align

--- client/packages/common/src/ui/components/footer/ActionsFooter.tsx
+++ client/packages/common/src/ui/components/footer/ActionsFooter.tsx
@@ -44,12 +44,6 @@ export const ActionsFooter: FC<ActionsFooterProps> = ({
     );
   };
 
-  actions.push({
-    label: t('label.clear-selection'),
-    icon: <MinusCircleIcon />,
-    onClick: clearSelected,
-  });
-
   return (
     <Stack
       direction="row"
@@ -60,40 +54,49 @@ export const ActionsFooter: FC<ActionsFooterProps> = ({
         p: 4,
         mx: '-20px',
         boxShadow: theme => `0 -5px 10px -5px ${theme.palette.grey[400]}`,
+        justifyContent: 'space-between',
       }}
     >
-      <Typography
-        sx={{
-          pr: 1,
-          fontWeight: 'bold',
-        }}
-      >
-        {selectedRowCount} {t('label.selected')}
-      </Typography>
-      {actions.map(
-        ({
-          label,
-          icon,
-          onClick,
-          disabled,
-          shouldShrink,
-          disabledToastMessage,
-        }) => (
-          <div
-            key={label}
-            onClick={handleDisabledClick(disabled, disabledToastMessage)}
-          >
-            <FlatButton
-              startIcon={icon}
-              label={label}
-              disabled={disabled}
-              onClick={onClick}
-              // Flatbutton doesn't shrink by default but we want it to in actions footer
-              shouldShrink={shouldShrink ?? true}
-            />
-          </div>
-        )
-      )}
+      <Stack direction="row" alignItems="center" gap={4}>
+        <Typography
+          sx={{
+            pr: 1,
+            fontWeight: 'bold',
+          }}
+        >
+          {selectedRowCount} {t('label.selected')}
+        </Typography>
+        {actions.map(
+          ({
+            label,
+            icon,
+            onClick,
+            disabled,
+            shouldShrink,
+            disabledToastMessage,
+          }) => (
+            <div
+              key={label}
+              onClick={handleDisabledClick(disabled, disabledToastMessage)}
+            >
+              <FlatButton
+                startIcon={icon}
+                label={label}
+                disabled={disabled}
+                onClick={onClick}
+                // Flatbutton doesn't shrink by default but we want it to in actions footer
+                shouldShrink={shouldShrink ?? true}
+              />
+            </div>
+          )
+        )}
+      </Stack>
+      <FlatButton
+        startIcon={<MinusCircleIcon />}
+        label={t('label.clear-selection')}
+        onClick={clearSelected}
+        shouldShrink={true}
+      />
     </Stack>
   );
 };
