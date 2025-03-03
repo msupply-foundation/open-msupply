a295a6404fed688acaebe70bf52206f2ab995034
roxy-dao <roxy@msupply.foundation>
2025-03-04 10:24:59 +1300

    Use screen width to calculate Indicator Line/Item info table

--- client/packages/requisitions/src/RequestRequisition/DetailView/IndicatorEdit/IndicatorLineEdit.tsx
+++ client/packages/requisitions/src/RequestRequisition/DetailView/IndicatorEdit/IndicatorLineEdit.tsx
@@ -10,6 +10,7 @@ import {
   useAuthContext,
   useNotification,
   useTranslation,
+  useWindowDimensions,
 } from '@openmsupply-client/common';
 import {
   IndicatorLineRowFragment,
@@ -119,6 +120,7 @@ export const IndicatorLineEdit = ({
   const showInfo =
     store?.preferences.useConsumptionAndStockFromCustomersForInternalOrders &&
     !!currentLine?.customerIndicatorInfo;
+  const { width } = useWindowDimensions();
 
   return (
     <>
@@ -133,7 +135,7 @@ export const IndicatorLineEdit = ({
         ))}
       </Box>
       {showInfo && (
-        <Box paddingTop={1} maxHeight={200} width="100%" display="flex">
+        <Box paddingTop={1} maxHeight={200} width={width * 0.48} display="flex">
           <CustomerIndicatorInfoView
             columns={columns}
             customerInfos={currentLine?.customerIndicatorInfo}
