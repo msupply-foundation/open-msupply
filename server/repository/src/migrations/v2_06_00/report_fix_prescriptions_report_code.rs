7d858513dda2957b133c3a6d0402608f1b15e3ea
James Brunskill <james@sussol.net>
2025-03-05 12:46:44 +1300

    Remove report changelogs
    Source: https://github.com/msupply-foundation/open-msupply/commit/1783e61b4c9e572f1870900c8b807787404f1855

--- server/repository/src/migrations/v2_06_00/report_fix_prescriptions_report_code.rs
+++ server/repository/src/migrations/v2_06_00/report_fix_prescriptions_report_code.rs
@@ -4,7 +4,7 @@ pub(crate) struct Migrate;
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
-        "report_fix_prescriptions_report_code"
+        "report_fix_prescriptions_report_code_updated"
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
@@ -14,6 +14,15 @@ impl MigrationFragment for Migrate {
             r#"
             UPDATE report SET code = 'prescription-receipt' WHERE code = 'prescriptions';
             UPDATE report SET id = 'prescription-receipt_2_6_0_false' where id = 'prescriptions_2_6_0_false';
+            "#
+        )?;
+
+        // remove records of report from changelog to make sure we don't have issues trying to sync missing reports
+        // This is safe to do (this time) as we're removing all reports from the system in the reinitialise_reports migration
+        sql!(
+            connection,
+            r#"
+                DELETE FROM changelog WHERE table_name = 'report';
             "#
         )?;
 
