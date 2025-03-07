f001361aecb6fc874adcc38a9d32062c003b957f
mSupplyZachariah <zachariah@msupply.foundation>
2025-03-07 16:15:40 +1300

    change initial settings, results in tiny code

--- server/service/src/print/label.rs
+++ server/service/src/print/label.rs
@@ -6,7 +6,7 @@ use super::jetdirect::{Jetdirect, Mode};
 
 const LINE_HEIGHT_IN_DOTS: i32 = 50;
 
-pub fn print_qr_code(
+pub fn print_2d_code(
     settings: LabelPrinterSettingNode,
     code: String,
     message: Option<String>,
@@ -36,7 +36,7 @@ pub fn print_qr_code(
         ^FX - encoding (28 = UTF-8)
         ^CI28
         ^FO50,{}
-        ^BQN,2,4
+        ^BXN
         ^FDMA,{}^FS        
         {}
         ^XZ"#,
@@ -45,6 +45,49 @@ pub fn print_qr_code(
     let printer = Jetdirect::new(settings.address, settings.port);
     printer.send_string(payload, Mode::Print)
 }
+
+// Below is the qr code printing function before it was modified to print a 2d datamatrix rather than a QR code.
+// It is preserved here for future use, as requested in issue 6848, which you'll find at this url:
+// https://github.com/orgs/msupply-foundation/projects/21/views/7?pane=issue&itemId=100558540&issue=msupply-foundation%7Copen-msupply%7C6848
+
+// pub fn print_qr_code(
+//     settings: LabelPrinterSettingNode,
+//     code: String,
+//     message: Option<String>,
+// ) -> Result<String> {
+//     let qr_height = 133; // approx height in dots for the magnification factor of 4 when printing a uuid
+//     let vertical_offset = (settings.label_height - qr_height) / 2;
+//     let formatted_message = match message {
+//         Some(msg) => {
+//             // adding max to ensure that the y is not negative
+//             let mut y = (vertical_offset - LINE_HEIGHT_IN_DOTS).max(0);
+//             msg.split('\n')
+//                 .map(|line| {
+//                     y += LINE_HEIGHT_IN_DOTS;
+//                     format!("^FO200,{}^A0,32,25^FD{}^FS", y, line)
+//                 })
+//                 .collect::<Vec<_>>()
+//                 .join("\n")
+//         }
+//         None => "".to_string(),
+//     };
+//
+//     let payload = format!(
+//         r#"
+//         ^XA
+//         ^FX CI command parameters:
+//         ^FX - encoding (28 = UTF-8)
+//         ^CI28
+//         ^FO50,{}
+//         ^BQN,2,4
+//         ^FDMA,{}^FS
+//         {}
+//         ^XZ"#,
+//         vertical_offset, code, formatted_message
+//     );
+//     let printer = Jetdirect::new(settings.address, settings.port);
+//     printer.send_string(payload, Mode::Print)
+// }
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
