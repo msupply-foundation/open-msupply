8a9ea7fe01723541b2289d04a9ee1388edda2700
noel-yeldos <noelyeldos@gmail.com>
2025-03-10 11:25:40 +1300

    Add prefs table migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/cf7712efb93b500d80bf9b9f49a8497011fed296

---
+++ server/repository/src/migrations/v2_07_00/add_prefs_table.rs
@@ -0,0 +1,25 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_prefs_table"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+                CREATE TABLE prefs (
+                    id TEXT NOT NULL PRIMARY KEY,
+                    key TEXT NOT NULL,
+                    value TEXT NOT NULL,
+                    store_id TEXT NOT NULL REFERENCES store(id)
+                );
+            "#
+        )?;
+
+        Ok(())
+    }
+}
