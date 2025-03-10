612eab4d2dd9bd142be9fb180d73c5096539103f
Martin Falconer <mcfalconer@gmail.com>
2025-03-11 11:15:34 +1300

    remove '[null]' display from equip log details card

--- client/packages/coldchain/src/Mobile/Equipment/LogCardListView.tsx
+++ client/packages/coldchain/src/Mobile/Equipment/LogCardListView.tsx
@@ -1,18 +1,19 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 import {
   NothingHere,
   useTranslation,
   BasicSpinner,
   useFormatDateTime,
   Formatter,
-} from "@openmsupply-client/common";
-import { Box, Typography, Card, CardContent } from "@mui/material";
-import { SimpleLabelDisplay } from "../Components/SimpleLabelDisplay";
+} from '@openmsupply-client/common';
+import { Box, Typography, Card, CardContent } from '@mui/material';
+import { SimpleLabelDisplay } from '../Components/SimpleLabelDisplay';
 import { useActivityLog } from '@openmsupply-client/system/src/ActivityLog/api';
 
 export const LogCardListView: FC<{ recordId: string }> = ({ recordId }) => {
   const t = useTranslation();
-  const { data, isError, isLoading } = useActivityLog.document.listByRecord(recordId);
+  const { data, isError, isLoading } =
+    useActivityLog.document.listByRecord(recordId);
   const { localisedDate, localisedTime } = useFormatDateTime();
 
   if (isLoading) return <BasicSpinner />;
@@ -28,61 +29,76 @@ export const LogCardListView: FC<{ recordId: string }> = ({ recordId }) => {
   }
 
   if (data?.totalCount === 0 || !data) {
-    return <NothingHere body={t('error.no-items-to-display')} />
+    return <NothingHere body={t('error.no-items-to-display')} />;
   }
 
-  return (
-    <Box sx={{
-      width: '100%',
-      flex: 1,
-    }}>
-      <Box sx={{
-        display: 'flex',
-        flexDirection: "column",
-        alignItems: 'center',
-        padding: '0px 5px 10px 0px',
-        gap: 1,
-        overflow: 'auto',
-        overflowY: 'scroll',
-      }}>
+  console.log(data.nodes);
 
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        flex: 1,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '0px 5px 10px 0px',
+          gap: 1,
+          overflow: 'auto',
+          overflowY: 'scroll',
+        }}
+      >
         {data.nodes.map(l => (
-          <Card key={l.id} sx={{
-            minWidth: '90%',
-            maxWidth: '450px',
-            padding: '10px 5px',
-            border: '1px solid',
-            borderColor: '#eee',
-            borderRadius: '16px',
-          }}
+          <Card
+            key={l.id}
+            sx={{
+              minWidth: '90%',
+              maxWidth: '450px',
+              padding: '10px 5px',
+              border: '1px solid',
+              borderColor: '#eee',
+              borderRadius: '16px',
+            }}
           >
             <CardContent>
-              <Typography sx={{
-                fontSize: '1.2rem!important',
-                fontWeight: 'bold',
-              }}>
-                {localisedDate(l.datetime) || "n/a"}
+              <Typography
+                sx={{
+                  fontSize: '1.2rem!important',
+                  fontWeight: 'bold',
+                }}
+              >
+                {localisedDate(l.datetime) || 'n/a'}
               </Typography>
               <SimpleLabelDisplay
                 label="Time"
-                value={localisedTime(l.datetime) || "n/a"}
+                value={localisedTime(l.datetime) || 'n/a'}
               />
               <SimpleLabelDisplay
                 label="User"
-                value={l.user?.username || "n/a"}
+                value={l.user?.username || 'n/a'}
               />
               <SimpleLabelDisplay
                 label="Event"
-                value={t(Formatter.logTypeTranslation(l.type), {
-                  defaultValue: l.type,
-                }) || "n/a"}
+                value={
+                  t(Formatter.logTypeTranslation(l.type), {
+                    defaultValue: l.type,
+                  }) || 'n/a'
+                }
               />
               <SimpleLabelDisplay
                 label="Details"
-                value={l?.from && l.to ?
-                  `[${l.from}] ${t('log.changed-to')} [${l.to}]`
-                  :
-                  `${t('log.changed-from')} [${l.from}]`
+                value={
+                  l?.from && l.to
+                    ? `[${l.from}] ${t('log.changed-to')} [${l.to}]`
+                    : l?.from
+                      ? `${t('log.changed-from')} [${l.from}]`
+                      : l?.to
+                        ? `${t('log.changed-to')} [${l.to}]`
+                        : ''
                 }
               />
             </CardContent>
@@ -90,5 +106,5 @@ export const LogCardListView: FC<{ recordId: string }> = ({ recordId }) => {
         ))}
       </Box>
     </Box>
-  )
-}
+  );
+};
