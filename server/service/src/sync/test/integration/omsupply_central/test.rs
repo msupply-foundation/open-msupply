25d6e1a6bb85eab6833205e7ba86af9cf04736b1
roxy-dao <roxy@msupply.foundation>
2025-03-12 11:42:06 +1300

    Integration test fixes
    Source: https://github.com/msupply-foundation/open-msupply/commit/7edbb2919ebb810630cea2af4128031df07fcc47

--- server/service/src/sync/test/integration/omsupply_central/test.rs
+++ server/service/src/sync/test/integration/omsupply_central/test.rs
@@ -1,9 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::sync::test::integration::omsupply_central::{
-        asset::AssetTester,
-        plugin_data::{PluginDataCentral, PluginDataRemote},
-        test_omsupply_central_records, test_omsupply_central_remote_records,
+        asset::AssetTester, plugin_data::{PluginDataCentral, PluginDataRemote}, report::ReportTester, test_omsupply_central_records, test_omsupply_central_remote_records
     };
 
     #[actix_rt::test]
