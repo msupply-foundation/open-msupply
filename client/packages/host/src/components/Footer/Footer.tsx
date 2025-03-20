03f53dfbaa93583df37560dddeb06a9b8d8e169b
John Geronimo <johnageronimo1@gmail.com>
2025-03-20 17:08:51 +1300

    refactor footer

--- client/packages/host/src/components/Footer/Footer.tsx
+++ client/packages/host/src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, ReactNode } from 'react';
 import {
   Box,
   HomeIcon,
@@ -15,42 +15,90 @@ import {
   useEditModal,
   EditIcon,
   useTheme,
+  useIsSmallScreen,
+  SxProps,
 } from '@openmsupply-client/common';
 import { StoreSelector } from './StoreSelector';
 import { LanguageSelector } from './LanguageSelector';
 import { FacilityEditModal, useName } from '@openmsupply-client/system';
 import { UserDetails } from './UserDetails';
 
-export const Footer: React.FC = () => {
-  const { user, store } = useAuthContext();
+interface PaddedCellProps {
+  sx?: SxProps;
+  text?: string;
+  icon: ReactNode;
+  textSx?: SxProps;
+  tooltip?: string;
+  onClick?: () => void;
+}
+
+const PaddedCell: FC<PaddedCellProps> = ({
+  sx,
+  text,
+  icon,
+  textSx,
+  tooltip,
+  onClick,
+}) => {
+  const isSmallScreen = useIsSmallScreen();
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      onClick={onClick}
+      sx={{ cursor: onClick ? 'pointer' : 'inherit', ...sx }}
+    >
+      {icon}
+      {!isSmallScreen && text && (
+        <Tooltip title={tooltip || ''}>
+          <Typography
+            sx={{
+              color: 'inherit',
+              fontSize: '12px',
+              marginInlineStart: '8px',
+              ...textSx,
+            }}
+          >
+            {text}
+          </Typography>
+        </Tooltip>
+      )}
+    </Box>
+  );
+};
+
+export const Footer: FC = () => {
+  const theme = useTheme();
   const t = useTranslation();
+  const isSmallScreen = useIsSmallScreen();
+  const { user, store } = useAuthContext();
   const { currentLanguageName, getLocalisedFullName } = useIntlUtils();
+
   const isCentralServer = useIsCentralServerApi();
   const { isOpen, onClose, onOpen } = useEditModal();
-  const theme = useTheme();
   const { data: nameProperties } = useName.document.properties();
 
-  const PaddedCell = styled(Box)({ display: 'flex' });
   const Divider = styled(Box)({
     width: '1px',
     height: '24px',
     backgroundColor: isCentralServer ? '#fff' : theme.palette.gray.main,
   });
-  const iconStyles = { color: 'inherit', height: '16px', width: '16px' };
-  const textStyles = {
+
+  const iconStyles = {
     color: 'inherit',
-    fontSize: '12px',
-    marginInlineStart: '8px',
+    height: isSmallScreen ? 24 : 16,
+    width: isSmallScreen ? 24 : 16,
   };
 
   return (
     <Box
       gap={2}
-      display="flex"
       flex={1}
+      display="flex"
       alignItems="center"
-      paddingY={0.75}
-      paddingX={0}
+      px={0}
+      py={isSmallScreen ? 1.5 : 0.75}
+      justifyContent={isSmallScreen ? 'space-evenly' : 'inherit'}
     >
       {isOpen && (
         <FacilityEditModal
@@ -60,61 +108,50 @@ export const Footer: React.FC = () => {
         />
       )}
       <StoreSelector>
-        <PaddedCell>
-          <HomeIcon sx={iconStyles} />
-          <Tooltip title={t('store-details', { ...store })}>
-            <Typography sx={textStyles}>{store?.name}</Typography>
-          </Tooltip>
-        </PaddedCell>
+        <PaddedCell
+          icon={<HomeIcon sx={iconStyles} />}
+          text={store?.name}
+          tooltip={t('store-details', { ...store })}
+        />
       </StoreSelector>
       {!!nameProperties?.length && (
-        <PaddedCell onClick={onOpen} sx={{ cursor: 'hand' }}>
-          <EditIcon sx={iconStyles} />
-          <Tooltip title={t('label.edit-store-properties')}>
-            <Typography sx={textStyles}>{t('label.edit')}</Typography>
-          </Tooltip>
-        </PaddedCell>
+        <PaddedCell
+          icon={<EditIcon sx={iconStyles} />}
+          text={t('label.edit')}
+          tooltip={t('label.edit-store-properties')}
+          onClick={onOpen}
+        />
       )}
       {user ? (
         <>
           <Divider />
           <UserDetails>
-            <PaddedCell>
-              <UserIcon sx={iconStyles} />
-              <Tooltip
-                title={getLocalisedFullName(user.firstName, user.lastName)}
-              >
-                <Typography sx={textStyles}>{user.name}</Typography>
-              </Tooltip>
-            </PaddedCell>
+            <PaddedCell
+              icon={<UserIcon sx={iconStyles} />}
+              text={user.name}
+              tooltip={getLocalisedFullName(user.firstName, user.lastName)}
+            />
           </UserDetails>
         </>
       ) : null}
       <Divider />
       <LanguageSelector>
-        <PaddedCell>
-          <TranslateIcon sx={iconStyles} />
-          <Tooltip title={t('select-language', { ...store })}>
-            <Typography sx={textStyles}>{currentLanguageName}</Typography>
-          </Tooltip>
-        </PaddedCell>
+        <PaddedCell
+          icon={<TranslateIcon sx={iconStyles} />}
+          text={currentLanguageName}
+          tooltip={t('select-language', { ...store })}
+        />
       </LanguageSelector>
+
+      {isSmallScreen && <Divider />}
       {isCentralServer ? (
-        <Box
-          flex={1}
-          justifyContent="flex-end"
-          display="flex"
-          alignItems="center"
-          paddingX={2}
-        >
-          <CentralIcon />
-          <Typography
-            variant="caption"
-            sx={{ color: 'inherit', whiteSpace: 'nowrap' }}
-          >
-            {t('label.central-server')}
-          </Typography>
-        </Box>
+        <PaddedCell
+          icon={<CentralIcon />}
+          text={t('label.central-server')}
+          tooltip={t('select-language', { ...store })}
+          sx={{ ml: isSmallScreen ? 0 : 'auto' }}
+          textSx={{ marginInlineStart: 0 }}
+        />
       ) : null}
     </Box>
   );
