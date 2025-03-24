e81af9c10c48ea0f3e75e4eb316cc619e9e6b0bc
Andrei <andrei.evguenov@gmail.com>
2025-03-25 10:19:21 +1300

    Fix plugin sync in cli and remove plugins in 2.6.1 migration

---
+++ server/repository/src/migrations/v2_06_01/remove_plugins.rs
@@ -0,0 +1,22 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    // Need to remove plugins since we've changed id in cli to have backend/frontend prefix
+    fn identifier(&self) -> &'static str {
+        "remove_plugins"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+            DELETE FROM backend_plugin;
+            DELETE FROM frontend_plugin;
+            "#,
+        )?;
+
+        Ok(())
+    }
+}
