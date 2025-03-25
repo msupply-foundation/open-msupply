cd2ef2c4a8f6d9e66bd98aa87690e9fb7c3395e5
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-03-26 07:58:51 +1300

    add graphql layer

--- server/graphql/types/src/types/preferences.rs
+++ server/graphql/types/src/types/preferences.rs
@@ -1,14 +1,35 @@
 use async_graphql::*;
 use service::preference::preferences::{complex_pref::ComplexPref, Preferences};
 
+pub struct PreferenceDescriptionNode {
+    pub key: String,
+    pub global_only: bool,
+    pub json_forms_input_type: String,
+}
+
+#[Object]
+impl PreferenceDescriptionNode {
+    pub async fn key(&self) -> &String {
+        &self.key
+    }
+
+    pub async fn global_only(&self) -> &bool {
+        &self.global_only
+    }
+
+    pub async fn json_forms_input_type(&self) -> &String {
+        &self.json_forms_input_type
+    }
+}
+
 pub struct PreferencesNode {
     pub preferences: Preferences,
 }
 
 #[Object]
 impl PreferencesNode {
-    pub async fn use_payments_in_prescriptions(&self) -> &bool {
-        &self.preferences.use_payments_in_prescriptions
+    pub async fn show_contact_tracing(&self) -> &bool {
+        &self.preferences.show_contact_tracing
     }
 
     pub async fn complex_pref(&self) -> ComplexPrefNode {
