496b266d9b27ea8416ace7673e0bb7fa022fadec
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-03-25 14:34:41 +1300

    add preferences page

--- client/packages/system/src/Manage/Service/Service.tsx
+++ client/packages/system/src/Manage/Service/Service.tsx
@@ -2,11 +2,14 @@ import React, { FC } from 'react';
 import { RouteBuilder, Routes, Route } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
 import { IndicatorsDemographics } from '../IndicatorsDemographics/DetailView/IndicatorsDemographics';
+import { PreferencesLineEdit } from '../Preferences/LineEditView';
 
 export const ManageService: FC = () => {
   const indicatorsDemographicsRoute = RouteBuilder.create(
     AppRoute.IndicatorsDemographics
   ).build();
+
+  const preferencesRoute = RouteBuilder.create(AppRoute.Preferences).build();
 
   return (
     <Routes>
@@ -14,6 +17,7 @@ export const ManageService: FC = () => {
         path={indicatorsDemographicsRoute}
         element={<IndicatorsDemographics />}
       />
+      <Route path={preferencesRoute} element={<PreferencesLineEdit />} />
     </Routes>
   );
 };
