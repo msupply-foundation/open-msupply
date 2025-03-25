36c9b1bbaa4d8c8a22e70f8b3431d9dc92d511e9
roxy-dao <roxy@msupply.foundation>
2025-03-25 16:33:39 +1300

    Revert translating label

--- client/packages/system/src/Report/components/ReportSelector.tsx
+++ client/packages/system/src/Report/components/ReportSelector.tsx
@@ -1,7 +1,7 @@
 import React, { FC, PropsWithChildren, useState } from 'react';
 import { Box, ReportContext, Typography } from '@openmsupply-client/common';
 import { AlertIcon } from '@common/icons';
-import { LocaleKey, useTranslation } from '@common/intl';
+import { useTranslation } from '@common/intl';
 import {
   CircularProgress,
   FlatButton,
@@ -72,7 +72,7 @@ export const ReportSelector: FC<PropsWithChildren<ReportSelectorProps>> = ({
 
   const reportButtons = data?.nodes?.map(report => (
     <FlatButton
-      label={t(report.name as LocaleKey)}
+      label={report.name}
       onClick={() => {
         hide();
         onReportSelected(report);
