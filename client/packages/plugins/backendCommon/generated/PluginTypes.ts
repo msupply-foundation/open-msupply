f298480ca3c94ceea640745a3286891c37b364d9
Andrei <andrei.evguenov@gmail.com>
2025-03-27 09:58:59 +1300

    Relocate backend plugin common add utils and new graphql query type
    Source: https://github.com/msupply-foundation/open-msupply/commit/6bdf49dc85cb445164c4728e7a47b6765da4f976

--- client/packages/plugins/backendCommon/generated/PluginTypes.ts
+++ client/packages/plugins/backendCommon/generated/PluginTypes.ts
@@ -2,10 +2,12 @@
 import type { AverageMonthlyConsumptionInput } from "./AverageMonthlyConsumptionInput";
 import type { AverageMonthlyConsumptionItem } from "./AverageMonthlyConsumptionItem";
 import type { Function } from "./Function";
+import type { GraphqlQueryInput } from "./GraphqlQueryInput";
+import type { JsonValue } from "./serde_json/JsonValue";
 import type { PluginDataFilter } from "./PluginDataFilter";
 import type { PluginDataRow } from "./PluginDataRow";
 import type { StorePreferenceRow } from "./StorePreferenceRow";
 import type { TransformRequestRequisitionLineInput } from "./TransformRequestRequisitionLineInput";
 import type { TransformRequestRequisitionLineOutput } from "./TransformRequestRequisitionLineOutput";
 
-export type PluginTypes = { average_monthly_consumption: Function<AverageMonthlyConsumptionInput, { [key in string]?: AverageMonthlyConsumptionItem }>, transform_request_requisition_lines: Function<TransformRequestRequisitionLineInput, TransformRequestRequisitionLineOutput>, get_store_preferences: StorePreferenceRow, get_plugin_data: Function<PluginDataFilter, Array<PluginDataRow>>, };
+export type PluginTypes = { average_monthly_consumption: Function<AverageMonthlyConsumptionInput, { [key in string]?: AverageMonthlyConsumptionItem }>, transform_request_requisition_lines: Function<TransformRequestRequisitionLineInput, TransformRequestRequisitionLineOutput>, graphql_query: Function<GraphqlQueryInput, JsonValue>, get_store_preferences: StorePreferenceRow, get_plugin_data: Function<PluginDataFilter, Array<PluginDataRow>>, };
--- client/packages/plugins/backendCommon/types.ts
+++ client/packages/plugins/backendCommon/types.ts
@@ -7,10 +7,14 @@ export type BackendPlugins = {
   transform_request_requisition_lines?: (
     _: PluginTypes['transform_request_requisition_lines']['input']
   ) => PluginTypes['transform_request_requisition_lines']['output'];
+  graphql_query?: (
+    _: PluginTypes['graphql_query']['input']
+  ) => PluginTypes['graphql_query']['output'];
 };
 
 declare global {
   var sql: (_: string) => [Record<string, any>];
+  var sql_type: () => 'postgres' | 'sqlite';
   var log: (_: any) => void;
   var get_store_preferences: (
     _: string
