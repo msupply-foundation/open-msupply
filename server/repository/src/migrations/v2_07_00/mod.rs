0a035eb51fee8239d3cf05c012fadfbfe79ad8c1
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-03-31 12:13:14 +1300

    add migration for adding patient link id to vaccination

--- server/repository/src/migrations/v2_07_00/mod.rs
+++ server/repository/src/migrations/v2_07_00/mod.rs
@@ -2,6 +2,7 @@ use super::{version::Version, Migration, MigrationFragment};
 use crate::StorageConnection;
 
 mod add_linked_invoice_id_to_invoice_line;
+mod add_patient_link_id_to_vaccination;
 mod add_preference_table;
 mod new_stocktake_fields;
 
@@ -21,6 +22,7 @@ impl Migration for V2_07_00 {
             Box::new(add_preference_table::Migrate),
             Box::new(add_linked_invoice_id_to_invoice_line::Migrate),
             Box::new(new_stocktake_fields::Migrate),
+            Box::new(add_patient_link_id_to_vaccination::Migrate),
         ]
     }
 }
