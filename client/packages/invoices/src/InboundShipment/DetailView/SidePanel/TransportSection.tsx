523117116768ac8a5e4f8b24ed5bd5145e29ec92
roxy-dao <roxy@msupply.foundation>
2025-04-01 08:43:07 +1300

    Read only expected delivery date in IS side panel

--- client/packages/invoices/src/InboundShipment/DetailView/SidePanel/TransportSection.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/SidePanel/TransportSection.tsx
@@ -6,17 +6,22 @@ import {
   useTranslation,
   useBufferState,
   BufferedTextInput,
+  PanelRow,
+  useFormatDateTime,
 } from '@openmsupply-client/common';
 import { useInbound } from '../../api';
 
 export const TransportSectionComponent: FC = () => {
   const t = useTranslation();
-  const { transportReference, update } = useInbound.document.fields([
-    'transportReference',
-  ]);
+  const { transportReference, expectedDeliveryDatetime, update } =
+    useInbound.document.fields([
+      'transportReference',
+      'expectedDeliveryDatetime',
+    ]);
   const [referenceBuffer, setReferenceBuffer] = useBufferState(
     transportReference ?? ''
   );
+  const { localisedDate } = useFormatDateTime();
 
   // the text input is read-only, as the transport reference is only populated
   // as part of the transfer creation process,
@@ -25,24 +30,46 @@ export const TransportSectionComponent: FC = () => {
   return (
     <DetailPanelSection title={t('heading.transport-details')}>
       <Grid container gap={0.5} key="transport-details">
-        <PanelLabel display="flex" alignItems="center">
-          {t('heading.reference')}
-        </PanelLabel>
-        <BufferedTextInput
-          disabled={true}
-          onChange={e => {
-            setReferenceBuffer(e.target.value);
-            update({ transportReference: e.target.value });
-          }}
-          value={referenceBuffer}
-          slotProps={{
-            input: {
-              style: {
-                backgroundColor: 'white',
+        <PanelRow>
+          <PanelLabel display="flex" alignItems="center">
+            {t('label.expected-delivery-date')}
+          </PanelLabel>
+          <BufferedTextInput
+            disabled={true}
+            value={
+              expectedDeliveryDatetime
+                ? localisedDate(expectedDeliveryDatetime)
+                : ''
+            }
+            slotProps={{
+              input: {
+                style: {
+                  backgroundColor: 'white',
+                },
               },
-            },
-          }}
-        />
+            }}
+          />
+        </PanelRow>
+        <PanelRow>
+          <PanelLabel display="flex" alignItems="center">
+            {t('heading.reference')}
+          </PanelLabel>
+          <BufferedTextInput
+            disabled={true}
+            onChange={e => {
+              setReferenceBuffer(e.target.value);
+              update({ transportReference: e.target.value });
+            }}
+            value={referenceBuffer}
+            slotProps={{
+              input: {
+                style: {
+                  backgroundColor: 'white',
+                },
+              },
+            }}
+          />
+        </PanelRow>
       </Grid>
     </DetailPanelSection>
   );
