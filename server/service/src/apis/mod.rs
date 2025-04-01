413c0f5cda9089bbcd88b933f58dfac5c42654f6
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-04-02 08:56:54 +1300

    add oms_central api consumer

--- server/service/src/apis/mod.rs
+++ server/service/src/apis/mod.rs
@@ -1,4 +1,5 @@
 pub mod api_on_central;
 pub mod login_v4;
+pub mod oms_central;
 pub mod patient_v4;
 pub mod permissions;
---
+++ server/service/src/apis/oms_central.rs
@@ -0,0 +1,52 @@
+use reqwest::{Client, StatusCode, Url};
+
+use super::api_on_central::NameStoreJoinParams;
+
+// Non-sync related APIs on the OMS Central server
+pub struct OmsCentral {
+    server_url: Url,
+    client: Client,
+    /// Username to authenticate with the central server. For the backend this is usually the site
+    /// name.
+    username: String,
+    /// For example, the site password which is also used for sync.
+    password_sha256: String,
+}
+
+#[derive(Debug)]
+pub enum OmsCentralError {
+    AuthenticationFailed,
+    InvalidResponse(serde_json::Error),
+    ConnectionError(reqwest::Error),
+}
+
+impl OmsCentral {
+    pub fn new(client: Client, server_url: Url, username: &str, password_sha256: &str) -> Self {
+        OmsCentral {
+            server_url,
+            client,
+            username: username.to_string(),
+            password_sha256: password_sha256.to_string(),
+        }
+    }
+
+    /// Creates/updates a name_store_join
+    pub async fn name_store_join(&self, body: NameStoreJoinParams) -> Result<(), OmsCentralError> {
+        let response = self
+            .client
+            .post(self.server_url.join("/central/name-store-join").unwrap())
+            .json(&body)
+            .basic_auth(&self.username, Some(&self.password_sha256))
+            .send()
+            .await
+            .map_err(OmsCentralError::ConnectionError)?;
+
+        if response.status() == StatusCode::UNAUTHORIZED {
+            return Err(OmsCentralError::AuthenticationFailed);
+        }
+        response
+            .json()
+            .await
+            .map_err(OmsCentralError::ConnectionError)
+    }
+}
