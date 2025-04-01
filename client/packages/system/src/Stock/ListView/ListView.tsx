8101d7c5c92995fd032d076f3446c90550ae2082
noel-yeldos <noelyeldos@gmail.com>
2025-04-02 11:30:14 +1300

    Update label and description
    Source: https://github.com/msupply-foundation/open-msupply/commit/cc1b4aaffcb6c76bf8915d1bb7df35582788b281

--- client/packages/system/src/Stock/ListView/ListView.tsx
+++ client/packages/system/src/Stock/ListView/ListView.tsx
@@ -132,6 +132,8 @@ const StockListComponent: FC = () => {
     [
       'availableStockOnHand',
       {
+        label: 'label.available-soh',
+        description: 'description.available-soh',
         accessor: ({ rowData }) =>
           rowData.availableNumberOfPacks * rowData.packSize,
         sortable: false,
