d9f7baf15e6fe5683bc480773d2d4c7a830d6956
roxy-dao <roxy@msupply.foundation>
2025-04-03 08:47:58 +1300

    Remove refactored hooks from api
    Source: https://github.com/msupply-foundation/open-msupply/commit/9f5bb04e70f9fe2cfbe0148398d6deb4a7e6f745

--- client/packages/system/src/Asset/api/api.ts
+++ client/packages/system/src/Asset/api/api.ts
@@ -6,9 +6,6 @@ import {
   AssetTypeSortFieldInput,
   AssetCategoryFilterInput,
   AssetTypeFilterInput,
-  InsertAssetLogReasonInput,
-  AssetLogStatusInput,
-  AssetLogReasonFilterInput,
   AssetPropertyFilterInput,
 } from '@openmsupply-client/common';
 import { Sdk } from './operations.generated';
@@ -20,35 +17,7 @@ export type ListParams<T> = {
   filterBy?: FilterByWithBoolean | null;
 };
 
-const logReasonParsers = {
-  toLogReasonInsert: (
-    input: Partial<InsertAssetLogReasonInput>
-  ): InsertAssetLogReasonInput => ({
-    id: input.id ?? '',
-    // default enum of NotInUse will never be used as it will fail the checkStatus check first
-    // and throw an error.
-    assetLogStatus: input.assetLogStatus ?? AssetLogStatusInput.NotInUse,
-    reason: input.reason ?? '',
-  }),
-  checkStatus: (status: string): boolean => {
-    switch (status) {
-      case AssetLogStatusInput.Decommissioned:
-        return true;
-      case AssetLogStatusInput.Functioning:
-        return true;
-      case AssetLogStatusInput.FunctioningButNeedsAttention:
-        return true;
-      case AssetLogStatusInput.NotFunctioning:
-        return true;
-      case AssetLogStatusInput.NotInUse:
-        return true;
-      default:
-        return false;
-    }
-  },
-};
-
-export const getAssetQueries = (sdk: Sdk, currentStoreId: string) => ({
+export const getAssetQueries = (sdk: Sdk) => ({
   get: {
     categories: async (filter: AssetCategoryFilterInput | undefined) => {
       const result = await sdk.assetCategories({
@@ -87,39 +56,6 @@ export const getAssetQueries = (sdk: Sdk, currentStoreId: string) => ({
 
       throw new Error('Unable to fetch properties');
     },
-    logReasons: async (filter: AssetLogReasonFilterInput | undefined) => {
-      const result = await sdk.assetLogReasons({
-        filter,
-        storeId: currentStoreId,
-      });
-      return result?.assetLogReasons;
-    },
-  },
-  insertLogReason: async (input: InsertAssetLogReasonInput) => {
-    if (!logReasonParsers.checkStatus(input.assetLogStatus ?? '')) {
-      throw new Error('Cannot parse status');
-    }
-    const result = await sdk.insertAssetLogReason({
-      input: logReasonParsers.toLogReasonInsert(input),
-    });
-    if (
-      result.centralServer.logReason.insertAssetLogReason.__typename ===
-      'AssetLogReasonNode'
-    ) {
-      return result.centralServer.logReason.insertAssetLogReason;
-    }
-
-    throw new Error('Could not insert reason');
-  },
-  deleteLogReason: async (reasonId: string) => {
-    const result = await sdk.deleteLogReason({ reasonId });
-    if (
-      result.centralServer.logReason.deleteLogReason.__typename ===
-      'DeleteResponse'
-    ) {
-      return result.centralServer.logReason;
-    }
-    throw new Error('Could not delete reason');
   },
 });
 
