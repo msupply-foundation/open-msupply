a2224fc854da3d8dfc6db2b4786c97f96e48c456
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-04-02 14:07:03 +1300

    Update Payment modal logic

--- client/packages/invoices/src/Prescriptions/DetailView/Payments/PaymentsModal.tsx
+++ client/packages/invoices/src/Prescriptions/DetailView/Payments/PaymentsModal.tsx
@@ -34,7 +34,6 @@ export const PaymentsModal: FC<PaymentsModalProps> = ({
   const { Modal } = useDialog({ isOpen, onClose, disableBackdrop: true });
 
   const [insuranceId, setInsuranceId] = useState<string | null>();
-  const [pluginError, setPluginError] = useState<string>();
 
   const {
     query: { data: prescriptionData },
@@ -55,8 +54,12 @@ export const PaymentsModal: FC<PaymentsModalProps> = ({
   const selectedInsurance = insuranceData?.find(({ id }) => id === insuranceId);
 
   const { plugins } = usePluginProvider();
-  const pluginEvents = usePluginEvents({
+  const pluginEvents = usePluginEvents<{
+    isDirty: boolean;
+    errorMessage: string | null;
+  }>({
     isDirty: false,
+    errorMessage: null,
   });
 
   const totalAfterTax = prescriptionData?.pricing.totalAfterTax ?? 0;
@@ -85,14 +88,18 @@ export const PaymentsModal: FC<PaymentsModalProps> = ({
       );
       onClose();
     } catch (error) {
-      setPluginError((error as Error).message);
+      pluginEvents.setState({
+        ...pluginEvents.state,
+        errorMessage: (error as Error).message,
+      });
     }
   };
 
   useEffect(() => {
     // Reset plugin error when modal is closed
-    setPluginError(undefined);
-  }, [isOpen]);
+    if (pluginEvents.state.errorMessage)
+      pluginEvents.setState({ ...pluginEvents.state, errorMessage: null });
+  }, [isOpen, pluginEvents]);
 
   return (
     <Modal
@@ -189,9 +196,9 @@ export const PaymentsModal: FC<PaymentsModalProps> = ({
             ) : null
           )}
         </Grid>
-        {pluginError && (
+        {pluginEvents.state.errorMessage && (
           <Box sx={{ pt: 4, display: 'flex', justifyContent: 'center' }}>
-            <Alert severity="error">{pluginError}</Alert>
+            <Alert severity="error">{pluginEvents.state.errorMessage}</Alert>
           </Box>
         )}
       </>
