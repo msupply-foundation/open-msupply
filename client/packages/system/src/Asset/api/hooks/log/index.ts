570a8df0e84b36140f288aa943c0d3ee2167f1db
roxy-dao <roxy@msupply.foundation>
2025-04-03 08:04:56 +1300

    Remove get asset log reason hook

--- client/packages/system/src/Asset/api/hooks/log/index.ts
+++ client/packages/system/src/Asset/api/hooks/log/index.ts
@@ -1,9 +1,7 @@
 import { useAssetLogReasonInsert } from './useAssetLogReasonInsert';
-import { useAssetLogReasons } from './useAssetLogReasons';
 import { useAssetLogReasonsDelete } from './useAssetLogReasonsDelete';
 
 export const Log = {
   useAssetLogReasonInsert,
-  useAssetLogReasons,
   useAssetLogReasonsDelete,
 };
