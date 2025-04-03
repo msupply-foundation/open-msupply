c7321e22982509e89e8074ae8dd0da941ff0d632
noel-yeldos <noelyeldos@gmail.com>
2025-04-04 10:44:23 +1300

    Testing android build

--- client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
+++ client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
@@ -69,6 +69,8 @@ const PlaceholderRow = ({ line }: { line?: DraftStockOutLine }) => {
   );
 };
 
+// commment for android build
+
 const TotalRow = ({ allocatedQuantity }: { allocatedQuantity: number }) => {
   const t = useTranslation();
   const formattedValue = useFormatNumber().round(allocatedQuantity, 2);
