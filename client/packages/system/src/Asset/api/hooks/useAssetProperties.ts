71cbcaa81eb569987e4aa118960ec18e13f061f9
roxy-dao <roxy@msupply.foundation>
2025-04-04 10:28:12 +1300

    Allow properties to be filtered
    Source: https://github.com/msupply-foundation/open-msupply/commit/eac2534fbaaea5966d56a0e516df4c0848aacc9d

--- client/packages/system/src/Asset/api/hooks/useAssetProperties.ts
+++ client/packages/system/src/Asset/api/hooks/useAssetProperties.ts
@@ -1,13 +1,15 @@
-import { useQuery } from '@openmsupply-client/common';
+import { FilterByWithBoolean, useQuery } from '@openmsupply-client/common';
 import { useAssetGraphQL } from '../useAssetGraphQL';
 import { ASSET, PROPERTIES } from './keys';
 
-export const useAssetProperties = () => {
+export const useAssetProperties = (filterBy?: FilterByWithBoolean | null) => {
   const { assetApi, storeId } = useAssetGraphQL();
   const queryKey = [ASSET, storeId, PROPERTIES];
 
   const queryFn = async () => {
-    const query = await assetApi.assetProperties();
+    const query = await assetApi.assetProperties({
+      filter: filterBy,
+    });
 
     if (query?.assetProperties?.__typename === 'AssetPropertyConnector') {
       return query?.assetProperties?.nodes;
