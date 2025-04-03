76b8035483e1bfb01bce0f0816486f0f47d009ad
roxy-dao <roxy@msupply.foundation>
2025-04-03 15:01:21 +1300

    refactor asset properties hook

--- client/packages/system/src/Asset/api/hooks/index.ts
+++ client/packages/system/src/Asset/api/hooks/index.ts
@@ -5,11 +5,11 @@ export * from './useAssetLogReasonList';
 export * from './useAssetLogReason';
 export * from './useAssetCategories';
 export * from './useAssetClasses';
+export * from './useAssetProperties';
 
 export const useAssetData = {
   utils: {
     types: Utils.useAssetTypes,
-    properties: Utils.useAssetProperties,
   },
 
   line: {},
