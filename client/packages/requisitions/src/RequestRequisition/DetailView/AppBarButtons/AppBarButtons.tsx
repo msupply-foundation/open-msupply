74467c722381e4e625f3b24696af180d9894d9cc
roxy-dao <roxy@msupply.foundation>
2025-04-08 09:35:47 +1200

    Use correct customer name id

--- client/packages/requisitions/src/RequestRequisition/DetailView/AppBarButtons/AppBarButtons.tsx
+++ client/packages/requisitions/src/RequestRequisition/DetailView/AppBarButtons/AppBarButtons.tsx
@@ -9,6 +9,7 @@ import {
   PrinterIcon,
   LoadingButton,
   ReportContext,
+  useAuthContext,
 } from '@openmsupply-client/common';
 import {
   ReportRowFragment,
@@ -32,6 +33,7 @@ export const AppBarButtonsComponent: FC<AppBarButtonProps> = ({
   showIndicators = false,
 }) => {
   const t = useTranslation();
+    const { store } = useAuthContext();
   const isProgram = useRequest.utils.isProgram();
   const { OpenButton } = useDetailPanel();
   const { data } = useRequest.document.get();
@@ -68,7 +70,7 @@ export const AppBarButtonsComponent: FC<AppBarButtonProps> = ({
               ? {
                   periodId: data?.period?.id,
                   programId: data?.program?.id,
-                  customerNameId: data?.otherPartyId,
+                  customerNameId: store?.nameId,
                 }
               : undefined
           }
