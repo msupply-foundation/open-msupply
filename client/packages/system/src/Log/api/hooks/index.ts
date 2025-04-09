48cad1738f59a681a0cc75a97e5d4da55970aaf4
roxy-dao <roxy@msupply.foundation>
2025-04-09 14:54:30 +1200

    Refactor get log content hook

--- client/packages/system/src/Log/api/hooks/index.ts
+++ client/packages/system/src/Log/api/hooks/index.ts
@@ -1,12 +1,1 @@
-import { Utils } from './utils';
-import { Document } from './document';
 export * from './useLog';
-
-export const useLogOld = {
-  document: {
-    logContentsByFileName: Document.useLogContentsByFileName,
-  },
-  utils: {
-    api: Utils.useLogApi,
-  },
-};
