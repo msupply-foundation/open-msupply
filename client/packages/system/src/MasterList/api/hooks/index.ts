8a68a859863c28dbef07c6861035dc6125dad5c7
roxy-dao <roxy@msupply.foundation>
2025-04-10 11:49:22 +1200

    Refactor master list line hooks
    Source: https://github.com/msupply-foundation/open-msupply/commit/b7c6390bdd49c5fb3307a7a66498d677a25523fa

--- client/packages/system/src/MasterList/api/hooks/index.ts
+++ client/packages/system/src/MasterList/api/hooks/index.ts
@@ -1,12 +1,1 @@
-import { Lines } from './line';
-import { Utils } from './utils';
 export { useMasterLists } from './useMasterLists';
-
-export const useMasterListOld = {
-  line: {
-    rows: Lines.useMasterListLines,
-  },
-  utils: {
-    api: Utils.useMasterListApi,
-  },
-};
