b678243aff8b0d4a5a931e7a78c07a6d36041a2b
zachariah-at-msupply <zachariah@msupply.foundation>
2025-04-18 16:21:38 +1200

    Fix SQL code

--- server/repository/src/migrations/v2_07_00/change_vaccination_date_to_not_nullable.rs
+++ server/repository/src/migrations/v2_07_00/change_vaccination_date_to_not_nullable.rs
@@ -12,13 +12,12 @@ impl MigrationFragment for Migrate {
             sql!(
                 connection,
                 r#"
-                    -- prepare to make the vaccination_date column not null by deleting 
-                    -- all records with null vaccination_dates and their changelogs.
-                    DELETE FROM changelog WHERE record_id IS (
-                        SELECT id FROM vaccination WHERE vaccination_date IS NULL
+                    DELETE FROM changelog AS c WHERE c.record_id IN (
+                            SELECT v.id FROM vaccination AS v WHERE v.vaccination_date IS NULL
                     );
+
                     DELETE FROM vaccination WHERE vaccination_date IS NULL;
-                    -- make the vaccination_date column not null
+
                     ALTER TABLE vaccination ALTER COLUMN vaccination_date SET NOT NULL;
                 "#,
             )?;
@@ -26,74 +25,78 @@ impl MigrationFragment for Migrate {
             sql!(
                 connection,
                 r#"
-                -- prepare to make the vaccination_date column not null by deleting 
-                -- all records with null vaccination_dates and their changelogs.
-                DELETE FROM changelog WHERE record_id IS (
-                    SELECT id FROM vaccination WHERE vaccination_date IS NULL
-                );
-                DELETE FROM vaccination WHERE vaccination_date IS NULL;
-                -- make the vaccination_date column not null
-                PRAGMA foreign_keys = OFF;
-                ALTER TABLE vaccination RENAME TO vaccination_old;
-                CREATE TABLE vaccination (
-                    id TEXT NOT NULL PRIMARY KEY,
-                    store_id TEXT NOT NULL,
-                    program_enrolment_id TEXT NOT NULL,
-                    patient_link_id TEXT NOT NULL DEFAULT '',
-                    encounter_id TEXT NOT NULL,
-                    user_id TEXT NOT NULL,
-                    vaccine_course_dose_id TEXT NOT NULL REFERENCES vaccine_course_dose(id),
-                    created_datetime {DATETIME} NOT NULL,
-                    facility_name_link_id TEXT,
-                    facility_free_text TEXT,
-                    invoice_id TEXT,
-                    stock_line_id TEXT,
-                    clinician_link_id TEXT,
-                    vaccination_date {DATE} NOT NULL,
-                    given BOOLEAN NOT NULL,
-                    not_given_reason TEXT,
-                    comment TEXT
-                );
-                INSERT INTO vaccination (
-                    id,
-                    store_id,
-                    program_enrolment_id,
-                    patient_link_id,
-                    encounter_id,
-                    user_id,
-                    vaccine_course_dose_id,
-                    created_datetime,
-                    facility_name_link_id,
-                    facility_free_text,
-                    invoice_id,
-                    stock_line_id,
-                    clinician_link_id,
-                    vaccination_date,
-                    given,
-                    not_given_reason,
-                    comment
-                )
-                SELECT
-                    id,
-                    store_id,
-                    program_enrolment_id,
-                    patient_link_id,
-                    encounter_id,
-                    user_id,
-                    vaccine_course_dose_id,
-                    created_datetime,
-                    facility_name_link_id,
-                    facility_free_text,
-                    invoice_id,
-                    stock_line_id,
-                    clinician_link_id,
-                    vaccination_date,
-                    given,
-                    not_given_reason,
-                    comment
-                FROM vaccination_old;
-                DROP TABLE vaccination_old;
-                PRAGMA foreign_keys = ON;
+                    DELETE FROM changelog AS c WHERE c.record_id IN (
+                            SELECT v.id FROM vaccination AS v WHERE v.vaccination_date IS NULL
+                    );
+
+                    DELETE FROM vaccination WHERE vaccination_date IS NULL;
+
+                    PRAGMA foreign_keys = OFF;
+
+                    ALTER TABLE vaccination RENAME TO vaccination_old;
+                    
+                    CREATE TABLE vaccination (
+                        id TEXT NOT NULL PRIMARY KEY,
+                        store_id TEXT NOT NULL,
+                        program_enrolment_id TEXT NOT NULL,
+                        patient_link_id TEXT NOT NULL DEFAULT '',
+                        encounter_id TEXT NOT NULL,
+                        user_id TEXT NOT NULL,
+                        vaccine_course_dose_id TEXT NOT NULL REFERENCES vaccine_course_dose(id),
+                        created_datetime {DATETIME} NOT NULL,
+                        facility_name_link_id TEXT,
+                        facility_free_text TEXT,
+                        invoice_id TEXT,
+                        stock_line_id TEXT,
+                        clinician_link_id TEXT,
+                        vaccination_date {DATE} NOT NULL,
+                        given BOOLEAN NOT NULL,
+                        not_given_reason TEXT,
+                        comment TEXT
+                    );
+
+                    INSERT INTO vaccination (
+                        id,
+                        store_id,
+                        program_enrolment_id,
+                        patient_link_id,
+                        encounter_id,
+                        user_id,
+                        vaccine_course_dose_id,
+                        created_datetime,
+                        facility_name_link_id,
+                        facility_free_text,
+                        invoice_id,
+                        stock_line_id,
+                        clinician_link_id,
+                        vaccination_date,
+                        given,
+                        not_given_reason,
+                        comment
+                    )
+                    SELECT
+                        id,
+                        store_id,
+                        program_enrolment_id,
+                        patient_link_id,
+                        encounter_id,
+                        user_id,
+                        vaccine_course_dose_id,
+                        created_datetime,
+                        facility_name_link_id,
+                        facility_free_text,
+                        invoice_id,
+                        stock_line_id,
+                        clinician_link_id,
+                        vaccination_date,
+                        given,
+                        not_given_reason,
+                        comment
+                    FROM vaccination_old;
+
+                    DROP TABLE vaccination_old;
+
+                    PRAGMA foreign_keys = ON;
                 "#,
             )?;
         };
