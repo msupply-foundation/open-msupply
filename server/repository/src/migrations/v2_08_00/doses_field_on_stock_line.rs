d2ccbb8fa0383b47be4585ebe6ae3721b31228f0
roxy-dao <roxy@msupply.foundation>
2025-04-22 12:29:30 +1200

    Update stock line with current item dose values

--- server/repository/src/migrations/v2_08_00/doses_field_on_stock_line.rs
+++ server/repository/src/migrations/v2_08_00/doses_field_on_stock_line.rs
@@ -12,6 +12,18 @@ impl MigrationFragment for Migrate {
             connection,
             r#"
                 ALTER TABLE stock_line ADD COLUMN vaccine_doses INTEGER;
+
+                UPDATE stock_line
+                SET vaccine_doses = (
+                    SELECT 
+                        CASE 
+                            WHEN item.vaccine_doses = 0 THEN NULL 
+                            ELSE item.vaccine_doses 
+                        END
+                    FROM item
+                    INNER JOIN item_link ON item_link.id = stock_line.item_link_id
+                    WHERE item.id = item_link.item_id
+                );
             "#
         )?;
 
