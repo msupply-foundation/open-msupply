c2d7e52880972414262cfbd2319e3301329aa3a4
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2025-04-29 15:59:54 +1200

    7131 refactor useIsScreen now uses useMediaQuery (#7408)
    * refactor useIsScreen now uses useMediaQuery
    
    * fix failing tests

--- client/packages/host/src/components/Login/LoginIcon.tsx
+++ client/packages/host/src/components/Login/LoginIcon.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import {
   MSupplyGuy,
   useLocalStorage,
-  useTheme,
+  useAppTheme,
   RegexUtils,
-  useIsSmallScreen,
+  useMediaQuery,
+  Breakpoints,
 } from '@openmsupply-client/common';
 
 export const LoginIcon = ({ small = false }: { small?: boolean }) => {
+  const theme = useAppTheme();
   const [customLogo] = useLocalStorage('/theme/logo');
-  const isSmallScreen = useIsSmallScreen() || small;
+  const isSmallScreen =
+    useMediaQuery(theme.breakpoints.down(Breakpoints.md)) || small;
   const logoStyle = isSmallScreen
     ? { width: 155, height: 90 }
     : { width: 285, height: 180 };
-  const theme = useTheme();
 
   if (!customLogo) return <MSupplyGuy style={logoStyle} />;
 
