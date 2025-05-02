79a6e4155f7049b98c5befac9388d96979cba129
James Brunskill <james@sussol.net>
2025-05-02 16:20:12 +1200

    7511 Fix unable to save stocktake line for an item (#7562)
    * Use the current item when querying lines to display for a stock take on an item
    
    * Update graphql schema
    
    * Add item_id filter to stocktakelines
    
    * Use item_id to select the correct rows from stock take lines

--- client/packages/inventory/src/Stocktake/api/hooks/line/useStocktakeLines.ts
+++ client/packages/inventory/src/Stocktake/api/hooks/line/useStocktakeLines.ts
@@ -2,11 +2,23 @@ import { useQuery, useUrlQueryParams } from '@openmsupply-client/common';
 import { useStocktakeApi } from '../utils/useStocktakeApi';
 import { useStocktakeNumber } from '../document/useStocktake';
 
-export const useStocktakeLines = (id: string) => {
+export const useStocktakeLines = (id: string, itemId?: string) => {
   const { queryParams } = useUrlQueryParams({
     initialSort: { key: 'itemName', dir: 'asc' },
     filters: [{ key: 'itemCodeOrName' }],
   });
+
+  if (itemId) {
+    queryParams.filterBy = {
+      ...queryParams.filterBy,
+      itemId: { equalTo: itemId },
+    };
+    // We use itemCode when we want to get all the lines for a specific item
+    // Get 1000 lines back, assuming that there's never more than 1000 lines for a single item
+    queryParams.first = 1000;
+    queryParams.offset = 0;
+  }
+
   const api = useStocktakeApi();
   const stocktakeNumber = useStocktakeNumber();
 
