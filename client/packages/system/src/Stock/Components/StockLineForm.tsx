b24d6d847199ef3e6646e4c01a941f6d47cd3a06
noel-yeldos <noelyeldos@gmail.com>
2025-05-06 15:42:29 +1200

    Remove available packs input

--- client/packages/system/src/Stock/Components/StockLineForm.tsx
+++ client/packages/system/src/Stock/Components/StockLineForm.tsx
@@ -120,20 +120,6 @@ export const StockLineForm: FC<StockLineFormProps> = ({
             }
           />
           <StyledInputRow
-            label={t('label.available-packs')}
-            Input={
-              <NumericTextInput
-                autoFocus
-                disabled={!packEditable}
-                width={160}
-                value={parseFloat(draft.availableNumberOfPacks.toFixed(2))}
-                onChange={availableNumberOfPacks =>
-                  onUpdate({ availableNumberOfPacks })
-                }
-              />
-            }
-          />
-          <StyledInputRow
             label={t('label.cost-price')}
             Input={
               <CurrencyInput
