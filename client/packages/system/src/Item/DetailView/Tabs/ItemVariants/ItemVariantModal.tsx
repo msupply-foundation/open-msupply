9a1b29e7ef669198e63da3cbaf4e6f3c36b0be50
roxy-dao <roxy@msupply.foundation>
2025-05-07 15:17:35 +1200

    Doses per unit and vvm type on item variant modal

--- client/packages/system/src/Item/DetailView/Tabs/ItemVariants/ItemVariantModal.tsx
+++ client/packages/system/src/Item/DetailView/Tabs/ItemVariants/ItemVariantModal.tsx
@@ -11,6 +11,7 @@ import {
   QueryParamsProvider,
   createQueryParamsStore,
   useNotification,
+  NumericTextInput,
 } from '@openmsupply-client/common';
 import { ItemPackagingVariantsTable } from './ItemPackagingVariantsTable';
 import {
@@ -151,6 +152,34 @@ const ItemVariantForm = ({
                 }
               />
             </Box>
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.doses-per-unit')}
+          labelWidth="200"
+          Input={
+            <NumericTextInput
+              fullWidth
+              disabled={!variant.item?.isVaccine}
+              value={variant.dosesPerUnit}
+              onChange={dosesPerUnit => {
+                updateVariant({ dosesPerUnit });
+              }}
+            />
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.vvm-type')}
+          labelWidth="200"
+          Input={
+            <BasicTextInput
+              disabled={!variant.item?.isVaccine}
+              value={variant.vvmType}
+              onChange={event => {
+                updateVariant({ vvmType: event.target.value });
+              }}
+              fullWidth
+            />
           }
         />
       </Box>
