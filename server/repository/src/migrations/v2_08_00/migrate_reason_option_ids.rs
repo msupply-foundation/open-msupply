1135443a5502d6dd3f5f662473d5382b4bcc96ed
noel-yeldos <noelyeldos@gmail.com>
2025-05-07 16:17:33 +1200

    Update migration for part 1

--- server/repository/src/migrations/v2_08_00/migrate_reason_option_ids.rs
+++ server/repository/src/migrations/v2_08_00/migrate_reason_option_ids.rs
@@ -12,6 +12,19 @@ impl MigrationFragment for Migrate {
         sql!(
             connection,
             r#"
+            INSERT INTO reason_option(id, type) 
+            SELECT 
+                id,
+                CASE
+                    WHEN type = 'POSITIVE' THEN 'POSITIVE_INVENTORY_ADJUSTMENT'
+                    WHEN type = 'NEGATIVE' THEN 'NEGATIVE_INVENTORY_ADJUSTMENT'
+                END as type
+            FROM inventory_adjustment_reason
+            WHERE type IN ('POSITIVE', 'NEGATIVE')
+            UNION ALL
+            SELECT id, 'RETURN_REASON' as type
+            FROM return_reason;         
+
             ALTER TABLE stocktake_line ADD COLUMN reason_option_id TEXT REFERENCES reason_option(id);
             ALTER TABLE invoice_line ADD COLUMN reason_option_id TEXT REFERENCES reason_option(id);
 
@@ -26,6 +39,10 @@ impl MigrationFragment for Migrate {
             UPDATE stocktake_line
             SET reason_option_id = inventory_adjustment_reason_id
             WHERE inventory_adjustment_reason_id IS NOT NULL;
+
+            UPDATE sync_buffer
+            SET integration_datetime = NULL
+            WHERE table_name = reason_option;   
             "#
         )?;
 
