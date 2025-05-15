e31c67ed779f061183b7c871f43b1e2fb78c0954
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2025-05-16 05:43:14 +0545

    7710 (7380 b) add donor select front end (msupply-foundation/open-msupply#7754)
    * Add donor and donor_id exposed in graphql to inbound shipment node
    
    * Add donor front end edit scaffold
    
    * Update migrations to be in series
    
    * Add mapping of donor and fix donor test
    
    * Add status change in modal
    
    * Update to unique test identifier
    
    * Fix tests via migration change
    
    * Remove duplicate donor id
    
    * Update donor method check to only show warning if changes will be made
    
    * Update to donor name
    
    * Update error message.
    
    And update variable naming to be a bit easier to understand
    
    * cleanups
    
    * simplify donor edit modal
    
    * merge fixes
    
    * update types
    
    ---------
    
    Co-authored-by: Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/3d29de1bb80ccdd7836feb54736c20a2167b751d

--- client/packages/invoices/src/InboundShipment/api/api.ts
+++ client/packages/invoices/src/InboundShipment/api/api.ts
@@ -20,6 +20,7 @@ import {
   RequisitionNodeType,
   InsertInboundShipmentLineFromInternalOrderLineInput,
   RequisitionNodeStatus,
+  UpdateDonorInput,
 } from '@openmsupply-client/common';
 import { DraftInboundLine } from './../../types';
 import { isA } from '../../utils';
@@ -75,12 +76,18 @@ const inboundParsers = {
     }
   },
   toUpdate: (
-    patch: RecordPatch<InboundFragment> | RecordPatch<InboundRowFragment>
+    patch:
+      | RecordPatch<InboundFragment>
+      | RecordPatch<InboundRowFragment>
+      | {
+          id: string;
+          defaultDonor: UpdateDonorInput;
+        }
   ): UpdateInboundShipmentInput => {
     return {
       id: patch.id,
-      colour: patch.colour,
-      comment: patch.comment,
+      colour: 'colour' in patch ? patch.colour : undefined,
+      comment: 'comment' in patch ? patch.comment : undefined,
       status: inboundParsers.toStatus(patch),
       onHold: 'onHold' in patch ? patch.onHold : undefined,
       otherPartyId: 'otherParty' in patch ? patch.otherParty?.id : undefined,
@@ -92,6 +99,7 @@ const inboundParsers = {
           : undefined,
       currencyId: 'currency' in patch ? patch.currency?.id : undefined,
       currencyRate: 'currencyRate' in patch ? patch.currencyRate : undefined,
+      defaultDonor: 'defaultDonor' in patch ? patch.defaultDonor : undefined,
     };
   },
   toInsertLine: (line: DraftInboundLine): InsertInboundShipmentLineInput => {
@@ -282,7 +290,10 @@ export const getInboundQueries = (sdk: Sdk, storeId: string) => ({
     throw new Error(insertInboundShipment.error.description);
   },
   update: async (
-    patch: RecordPatch<InboundFragment> | RecordPatch<InboundRowFragment>
+    patch:
+      | RecordPatch<InboundFragment>
+      | RecordPatch<InboundRowFragment>
+      | { id: string; defaultDonor: UpdateDonorInput }
   ) =>
     sdk.updateInboundShipment({
       input: inboundParsers.toUpdate(patch),
