4cd3154133751cd4181a9f80c7239bea7edb302d
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-05-16 09:11:57 +1200

    simplify donor edit modal

--- client/packages/invoices/src/InboundShipment/DetailView/modals/Donor/DonorEditModal.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/modals/Donor/DonorEditModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   Autocomplete,
   Box,
@@ -6,14 +6,10 @@ import {
   DialogButton,
   useNotification,
   useTranslation,
-  InputWithLabelRow,
   UpdateDonorMethodInput,
-  Typography,
-  LocaleKey,
-  useConfirmationModal,
+  Select,
 } from '@openmsupply-client/common';
 import { useName } from 'packages/system/src';
-import { getDonorUpdateKeys } from 'packages/system/src/utils';
 import { useInbound } from '../../../api';
 
 interface DonorEditModalProps {
@@ -29,12 +25,6 @@ interface DonorOption {
   id: string | null;
 }
 
-interface MethodOption {
-  label: string;
-  value: UpdateDonorMethodInput;
-  message: LocaleKey;
-}
-
 export const DonorEditModal = ({
   invoiceId,
   donorId,
@@ -43,89 +33,59 @@ export const DonorEditModal = ({
 }: DonorEditModalProps) => {
   const { error } = useNotification();
   const t = useTranslation();
-  const { Modal } = useDialog({ isOpen, onClose });
+  const { Modal } = useDialog({
+    isOpen,
+    onClose,
+    disableMobileFullScreen: true,
+  });
   const { mutateAsync } = useInbound.document.update();
-  const defaultMethod = {
-    label: t(getDonorUpdateKeys(UpdateDonorMethodInput.None).label),
-    value: UpdateDonorMethodInput.None,
-    message: getDonorUpdateKeys(UpdateDonorMethodInput.None).message,
-  };
 
-  const [donor, setDonor] = useState<DonorOption | undefined>();
-  const [method, setMethod] = useState<MethodOption>(defaultMethod);
+  const [donor, setDonor] = useState<DonorOption | null>(null);
+  const [isDirty, setIsDirty] = useState(false);
+  const [method, setMethod] = useState<UpdateDonorMethodInput>();
 
   const width = '350px';
 
-  const getConfirmation = useConfirmationModal({
-    title: t('heading.are-you-sure'),
-    message: t('messages.update-all-children-lines'),
-  });
-
   const { data } = useName.document.donors();
 
-  const donorOptions: DonorOption[] =
-    data?.nodes.map(node => ({
-      label: node.name,
-      value: node.id,
-      id: node.id,
-    })) ?? [];
-
-  const nullOption = {
-    label: t('label.set-to-none'),
-    value: null,
-    id: null,
-  };
-
-  donorOptions.push(nullOption);
-
-  type IsMethod = keyof typeof UpdateDonorMethodInput;
-  const methodOptions: MethodOption[] = (
-    Object.keys(UpdateDonorMethodInput) as Array<IsMethod>
-  ).map(key => {
-    const valueKeys = getDonorUpdateKeys(UpdateDonorMethodInput[key]);
-    return {
-      label: t(valueKeys.label),
-      value: UpdateDonorMethodInput[key],
-      message: valueKeys.message,
-    };
-  });
+  const donorOptions: DonorOption[] = useMemo(
+    () =>
+      data?.nodes.map(node => ({
+        label: node.name,
+        value: node.id,
+        id: node.id,
+      })) ?? [],
+    [data]
+  );
 
   useEffect(() => {
-    const donorFound = donorOptions?.find(donor => (donor.id = donorId));
-    donorFound ? setDonor(donorFound) : setDonor(nullOption);
-  }, [donorId, data]);
+    const donorFound = donorOptions?.find(donor => donor.id === donorId);
+    if (donorFound) {
+      setDonor(donorFound);
+      setIsDirty(false);
+    }
+  }, [donorId, donorOptions]);
 
-  const confirm = () => {
+  const confirm = () =>
     mutateAsync({
       id: invoiceId,
       defaultDonorId: donor?.id,
-      updateDonorMethod: method.value,
+      updateDonorMethod: method,
     });
-  };
-
-  const handleSave = () => {
-    // only prompt confirm if we are making updates to children lines
-    method.value != UpdateDonorMethodInput.None
-      ? getConfirmation({
-          onConfirm: () => {
-            confirm();
-          },
-        })
-      : confirm();
-  };
 
   return (
     <Modal
-      title={t('heading.donor-changes')}
+      title={t('heading.donor-update')}
       width={600}
       sx={{ '& .MuiDialogContent-root': { paddingTop: 0 } }}
       cancelButton={<DialogButton variant="cancel" onClick={onClose} />}
       okButton={
         <DialogButton
           variant="ok"
+          disabled={!isDirty}
           onClick={async () => {
             try {
-              await handleSave();
+              await confirm();
               onClose();
             } catch {
               error(t('error.failed-to-save-donor-charges'))();
@@ -136,65 +96,55 @@ export const DonorEditModal = ({
     >
       <Box display="flex" width="100%" justifyContent="center" padding={3}>
         <Box display="flex" flexDirection="column" gap={2}>
-          <InputWithLabelRow
-            labelWidth="160px"
-            label={t('label.donor')}
-            labelProps={{
-              sx: {
-                fontSize: '16px',
-                paddingRight: 2,
-                textAlign: 'right',
-              },
+          <Autocomplete
+            value={donor}
+            options={donorOptions ?? []}
+            fullWidth={true}
+            onChange={(_e, value) => {
+              setDonor(value);
+              setIsDirty(true);
             }}
-            Input={
-              <Autocomplete
-                value={donor}
-                options={donorOptions ?? []}
-                fullWidth={true}
-                onChange={(_e, value) => {
-                  value && setDonor(value);
-                }}
-                clearable={false}
-                width={width}
-              />
-            }
+            clearable
+            width={width}
+            inputProps={{
+              placeholder: t('label.no-donor-selected'),
+            }}
           />
-          <InputWithLabelRow
-            labelWidth="160px"
-            label={t('label.donor-line-update')}
-            labelProps={{
-              sx: {
-                fontSize: '16px',
-                paddingRight: 2,
-                textAlign: 'right',
+          <Select
+            label={t('label.apply-donor-which-lines')}
+            value={method ?? ''}
+            options={[
+              {
+                label: t('label.all-lines'),
+                value: UpdateDonorMethodInput.All,
               },
+              {
+                label: t('label.apply-donor-existing'),
+                value: UpdateDonorMethodInput.Existing,
+              },
+              {
+                label: t('label.apply-donor-unassigned'),
+                value: UpdateDonorMethodInput.Unspecified,
+              },
+              {
+                label: t('label.none'),
+                value: UpdateDonorMethodInput.None,
+              },
+            ]}
+            onChange={e => {
+              const newMethod = e.target.value as UpdateDonorMethodInput;
+              setMethod(newMethod);
+
+              // If not applying change to any lines, is dirty should only
+              // get set if the shipment donor changes
+              if (newMethod !== UpdateDonorMethodInput.None) {
+                setIsDirty(true);
+              }
             }}
-            Input={
-              <Autocomplete
-                options={methodOptions ?? []}
-                fullWidth={true}
-                defaultValue={method}
-                onChange={(_e, value) => {
-                  value && setMethod(value);
-                }}
-                // on clear, we set default of no changes
-                onInputChange={(_e, _value, reason) => {
-                  if (reason === 'clear') {
-                    setMethod(defaultMethod);
-                  }
-                }}
-                width={width}
-              />
-            }
+            fullWidth
+            clearable
+            slotProps={{ inputLabel: { shrink: true } }}
           />
-          <Typography
-            sx={{
-              fontSize: '1em',
-              fontWeight: 'bold',
-            }}
-          >
-            {t(method.message, { donor: donor?.label })}
-          </Typography>
         </Box>
       </Box>
     </Modal>
