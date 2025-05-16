3674aeeac27247303674ddac8797332e3b345258
roxy-dao <roxy@msupply.foundation>
2025-05-16 19:49:29 +1200

    7607 add created datetime and created by to item variant (#7717)
    * Migrations to add created_datetime / by to item variants and add item variant types to activity log
    * Validation checks for manufacturer + return cold chain type and manufacturer
    * Change variant return type on validate result
    * Return name_row on ItemVariant struct
    * Return cold storage type row on Item Variant struct
    * Log changes to item variant
    * Update error/enum mappings
    * Item variant delete logs

--- server/repository/src/migrations/v2_08_00/mod.rs
+++ server/repository/src/migrations/v2_08_00/mod.rs
@@ -1,10 +1,12 @@
 use super::{version::Version, Migration, MigrationFragment};
 use crate::StorageConnection;
 
+mod add_created_fields_to_item_variant;
 mod add_donor_id_to_invoice_and_invoice_lines;
 mod add_donor_id_to_stock_lines;
 mod add_doses_columns_to_item_variant;
 mod add_initial_stocktake_field;
+mod add_item_variant_enums_to_activity_log;
 mod add_view_and_edit_vvm_status_permission;
 mod add_vvm_status_id_to_stock_line;
 mod add_vvm_status_log_change_log_table_name;
@@ -28,8 +30,10 @@ impl Migration for V2_08_00 {
             Box::new(add_vvm_status_table::Migrate),
             Box::new(add_vvm_status_log_table::Migrate),
             Box::new(add_doses_columns_to_item_variant::Migrate),
-            Box::new(add_vvm_status_log_change_log_table_name::Migrate),
             Box::new(add_initial_stocktake_field::Migrate),
+            Box::new(add_created_fields_to_item_variant::Migrate),
+            Box::new(add_item_variant_enums_to_activity_log::Migrate),
+            Box::new(add_vvm_status_log_change_log_table_name::Migrate),
             Box::new(add_view_and_edit_vvm_status_permission::Migrate),
             Box::new(add_donor_id_to_invoice_and_invoice_lines::Migrate),
             Box::new(add_vvm_status_log_update_to_activity_log::Migrate),
