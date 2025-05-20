39b0fa19d105fa3a421f0786b9d4146db87c95e1
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-05-20 13:44:00 +1200

    #7397 Simplified mobile columns (for Internal Order) (#7792)
    * Mechanism to hide columns on Mobile
    * Update useColumnDisplayState.ts
    * Update useSimplifiedTabletUI.ts

--- client/packages/common/src/ui/layout/tables/hooks/useColumnDisplayState.ts
+++ client/packages/common/src/ui/layout/tables/hooks/useColumnDisplayState.ts
@@ -12,7 +12,19 @@ export const useColumnDisplayState = <T extends RecordWithId>(
 ) => {
   const [hiddenColsStorage, setHiddenColsStorage] =
     useLocalStorage('/columns/hidden');
-  const hiddenColKeys = hiddenColsStorage?.[tableId] ?? [];
+
+  const simplifiedMobileView = useSimplifiedTabletUI();
+
+  const hiddenColKeys =
+    hiddenColsStorage?.[tableId] ??
+    // If "Simplified Mobile View" is enabled, hide designated columns by
+    // default, but only if no manual column hide/show statuses have already
+    // been set.
+    (simplifiedMobileView
+      ? initialColumns
+          .filter(col => col.defaultHideOnMobile)
+          .map(col => col.key as string)
+      : []);
 
   // TO-DO Implement for column display...
   const simplifiedTabletView = useSimplifiedTabletUI();
