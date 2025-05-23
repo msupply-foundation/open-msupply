d66a88852cb500024250af2c5992a7f94b3a2e36
Fergus Roache <fergus@msupply.foundation>
2025-05-23 14:28:07 +1200

    Add default hide batch column

--- client/packages/invoices/src/OutboundShipment/DetailView/columns.ts
+++ client/packages/invoices/src/OutboundShipment/DetailView/columns.ts
@@ -118,6 +118,7 @@ export const useOutboundColumns = ({
             { path: ['lines', 'batch'] },
             { path: ['batch'] },
           ]),
+          defaultHideOnMobile: true,
       },
     ],
     [
