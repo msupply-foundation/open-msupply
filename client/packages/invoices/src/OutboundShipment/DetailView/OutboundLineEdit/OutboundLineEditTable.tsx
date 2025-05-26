ad8cf0d6cf3abc12a6e9ab297210c6a3e0eabfd2
Fergus Roache <fergus@msupply.foundation>
2025-05-27 10:38:16 +1200

    Amend max modal height on tablet

--- client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
+++ client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
@@ -207,7 +207,7 @@ export const OutboundLineEditTable = ({
       <Divider margin={10} />
       <Box
         style={{
-          maxHeight: useSimplifiedTabletView ? 290 : 325,
+          maxHeight: useSimplifiedTabletView ? 255 : 325,
           display: 'flex',
           flexDirection: 'column',
           overflowX: 'hidden',
