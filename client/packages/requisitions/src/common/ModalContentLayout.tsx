f56e65c82f1420c782f3ab761cf6338736beb4d8
roxy-dao <roxy@msupply.foundation>
2025-05-26 14:27:41 +1200

    InputRow common component

--- client/packages/requisitions/src/common/ModalContentLayout.tsx
+++ client/packages/requisitions/src/common/ModalContentLayout.tsx
@@ -1,6 +1,10 @@
 import React, { useMemo } from 'react';
 import {
+  Box,
   Grid,
+  InputWithLabelRow,
+  NumericTextInput,
+  NumUtils,
   SxProps,
   Theme,
   Typography,
@@ -139,5 +143,98 @@ export const ValueInfoRow = ({
       packagingDisplay={packagingDisplay}
       sx={sx}
     />
+  );
+};
+
+interface InputRowProps {
+  label: string;
+  value: number;
+  onChange?: (value?: number) => void;
+  disabled: boolean;
+  autoFocus?: boolean;
+  representation: RepresentationValue;
+  defaultPackSize: number;
+  unitName: string;
+  sx?: SxProps<Theme>;
+}
+
+export const InputRow = ({
+  label,
+  value,
+  onChange,
+  disabled,
+  autoFocus = false,
+  representation,
+  defaultPackSize,
+  unitName,
+  sx,
+}: InputRowProps) => {
+  const t = useTranslation();
+  const { getPlural } = useIntlUtils();
+
+  const valueInUnitsOrPacks = getValueInUnitsOrPacks(
+    representation,
+    defaultPackSize,
+    value
+  );
+
+  const packagingDisplay = useMemo(() => {
+    if (representation === Representation.PACKS) {
+      return getPlural(t('label.pack').toLowerCase(), valueInUnitsOrPacks);
+    }
+    return getPlural(unitName.toLowerCase(), valueInUnitsOrPacks);
+  }, [representation, unitName]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 1,
+        px: 1,
+        ...sx,
+      }}
+    >
+      <Box sx={{ flex: 1 }}>
+        <InputWithLabelRow
+          Input={
+            <NumericTextInput
+              sx={{
+                '& .MuiInputBase-input': {
+                  p: '3px 4px',
+                  backgroundColor: theme =>
+                    disabled
+                      ? theme.palette.background.toolbar
+                      : theme.palette.background.white,
+                },
+              }}
+              slotProps={{
+                input: {
+                  sx: {
+                    background: theme =>
+                      disabled
+                        ? theme.palette.background.toolbar
+                        : theme.palette.background.white,
+                  },
+                },
+              }}
+              min={0}
+              width={145}
+              value={NumUtils.round(valueInUnitsOrPacks, 2)}
+              onChange={onChange}
+              disabled={disabled}
+              autoFocus={autoFocus}
+              decimalLimit={2}
+            />
+          }
+          label={label}
+          sx={{
+            justifyContent: 'space-between',
+          }}
+        />
+      </Box>
+      <Typography sx={{ pl: 0.5 }}>{packagingDisplay}</Typography>
+    </Box>
   );
 };
