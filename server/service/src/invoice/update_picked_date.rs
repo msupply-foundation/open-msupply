10f47d179a7662c506b8baf918e9ca4b0b23f033
James Brunskill <james@sussol.net>
2025-05-26 17:09:02 +1200

    Fix picked status

--- server/service/src/invoice/update_picked_date.rs
+++ server/service/src/invoice/update_picked_date.rs
@@ -1,5 +1,5 @@
 use repository::{
-    InvoiceRow, InvoiceRowRepository, InvoiceStatus, InvoiceType, RepositoryError,
+    Invoice, InvoiceRow, InvoiceRowRepository, InvoiceStatus, InvoiceType, RepositoryError,
     StorageConnection,
 };
 
@@ -7,14 +7,27 @@ use super::common::get_invoice_status_datetime;
 
 const MIN_PICKED_DATE_UPDATE_INTERVAL_SECONDS: i64 = 60;
 
-pub fn should_auto_pick(invoice: &InvoiceRow) -> bool {
+fn auto_pick_invoice(
+    connection: &StorageConnection,
+    invoice: InvoiceRow,
+) -> Result<InvoiceRow, RepositoryError> {
     // If the invoice is a prescription and it's in the new status, we should automatically pick it
     if invoice.r#type == InvoiceType::Prescription && invoice.status == InvoiceStatus::New {
-        return true;
-    }
+        println!(
+            "Automatically picking prescription invoice with ID: {}",
+            invoice.id
+        );
+        let update = InvoiceRow {
+            status: InvoiceStatus::Picked,
+            picked_datetime: Some(get_invoice_status_datetime(&invoice)),
+            ..invoice
+        };
 
-    // For other types of invoices, we don't do anything
-    false
+        // Update the invoice status to picked
+        let _result = InvoiceRowRepository::new(connection).upsert_one(&update)?;
+        return Ok(update);
+    }
+    Ok(invoice)
 }
 
 /// This function is called when a line is updated on an invoice. It will update the picked date if appropriate.
@@ -22,8 +35,13 @@ pub fn update_picked_date(
     connection: &StorageConnection,
     invoice: &InvoiceRow,
 ) -> Result<(), RepositoryError> {
+    println!("Updating picked date for invoice with ID: {}", invoice.id);
+
+    // Some invoices such as prescriptions should be automatically picked when lines are added
+    let invoice = auto_pick_invoice(connection, invoice.clone())?;
+
     // We only want to update the picked date if the invoice is in the picked status
-    if invoice.status != InvoiceStatus::Picked && !should_auto_pick(invoice) {
+    if invoice.status != InvoiceStatus::Picked {
         return Ok(());
     };
 
@@ -35,7 +53,7 @@ pub fn update_picked_date(
         {
             return Ok(());
         }
-    };
+    }
 
     // Use the invoice's backdated datetime if it's set, otherwise set the status to now
     let status_datetime = get_invoice_status_datetime(&invoice);
