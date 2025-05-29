41782ad1fcb37b02607c605435b37ba6783bf35a
Fergus Roache <fergus@msupply.foundation>
2025-05-29 16:54:03 +1200

    Tidy up add report upload modal

--- client/packages/system/src/Manage/Reports/ListView/ReportUploadModal.tsx
+++ client/packages/system/src/Manage/Reports/ListView/ReportUploadModal.tsx
@@ -18,7 +18,6 @@ export const ReportUploadModal = ({
   const t = useTranslation();
   const [draft, setDraft] = useState<{ id?: string; files?: File[] }>({});
   const { error, success } = useNotification();
-  const [errorMessage, setErrorMessage] = useState<string>(() => '');
 
   const { Modal } = useDialog({ isOpen, onClose, disableBackdrop: true });
 
@@ -27,51 +26,46 @@ export const ReportUploadModal = ({
   };
 
   const onUpload = (files: File[]) => {
-    files.forEach(file => {
-      if (!file.name.endsWith('json')) {
-        setErrorMessage(t('messages.invalid-file'));
-      }
-    });
-
-    if (errorMessage != '') {
+    if (files.filter(f => !f.name.endsWith('json')).length > 0) {
+      error('error.report-invalid-file')();
+    } else {
       setDraft({ files });
-      setErrorMessage('');
     }
   };
 
   const onOk = async () => {
-    const uploadPromise = () => {
-      if (!draft.files?.length)
-        return new Promise(resolve => resolve('no files'));
+    if (!draft.files?.length)
+      return new Promise(resolve => resolve('no files'));
 
-      // create new json file id
-      const id = FnUtils.generateUUID();
-      const url = `${Environment.SYNC_FILES_URL}/report-data/${id}`;
-      const formData = new FormData();
-      draft.files?.forEach(file => {
-        formData.append('files', file);
-      });
-
-      return fetch(url, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-        },
-        credentials: 'include',
-        body: formData,
-      });
-    };
-
-    uploadPromise()
-      .then(id => {
-        // TODO add install uploaded plugin end point here
-        throw error;
-      })
-      .then(() => {
-        success(t('messages.log-saved-successfully'))();
-        onClose();
-      })
-      .catch(e => error(`${t('error.unable-to-save-log')}: ${e.message}`)());
+    // create new json file id
+    const id = FnUtils.generateUUID();
+    const url = `${Environment.SYNC_FILES_URL}/report-data/${id}`;
+    try {
+      if (draft.files) {
+        for (const file of draft.files) {
+          const formData = new FormData();
+          formData.append('files', file);
+          const fileId = await fetch(url, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+            },
+            credentials: 'include',
+            body: formData,
+          });
+          if (!fileId) {
+            throw error;
+          }
+          console.log('fileId', fileId.json());
+          // TODO do something with fileId
+        }
+      }
+      success(t('messsages.reports-installed-successfully'))();
+      onClose();
+    } catch (e) {
+      const message = e instanceof Error ? e.message : String(e);
+      error(`${t('error.unable-to-install-reports')}: ${message}`)();
+    }
   };
 
   return (
@@ -110,7 +104,6 @@ export const ReportUploadModal = ({
             removeFile={removeFile}
           />
         </Box>
-        <Typography>{errorMessage}</Typography>
       </DetailContainer>
     </Modal>
   );
