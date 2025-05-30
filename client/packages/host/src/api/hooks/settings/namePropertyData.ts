7c2c7781e086bb967d9d512b64a05ef42dceeea6
James Brunskill <james@sussol.net>
2025-05-30 16:47:32 +1200

    Fix translations
    Source: https://github.com/msupply-foundation/open-msupply/commit/e30b0b01bbf3f537adf02ef2ed7940477c9d40e4

--- client/packages/host/src/api/hooks/settings/namePropertyData.ts
+++ client/packages/host/src/api/hooks/settings/namePropertyData.ts
@@ -28,7 +28,7 @@ export type LocalisedNamePropertyConfig = Partial<
   en: ConfigureNamePropertyInput[];
 };
 
-const enTranslations: Record<string, string> | null = {
+const enTranslations: Record<string, string> = {
   POPULATION_SERVED_KEY: 'Population Served',
   LATITUDE_KEY: 'Latitude',
   LONGITUDE_KEY: 'Longitude',
@@ -47,7 +47,7 @@ const enTranslations: Record<string, string> | null = {
 };
 
 // French Translations
-const frTranslations: Record<string, string> | null = {
+const frTranslations: Record<string, string> = {
   POPULATION_SERVED_KEY: 'Population Desservie',
   LATITUDE_KEY: 'Latitude',
   LONGITUDE_KEY: 'Longitude',
@@ -93,15 +93,18 @@ const frTranslations: Record<string, string> | null = {
   Low: 'Faible',
 };
 
-const populationServed: ConfigureNamePropertyInput = {
-  id: '7716cecc-7d62-4f1b-93fa-a55a275079b4',
-  propertyId: POPULATION_SERVED_KEY,
-  key: POPULATION_SERVED_KEY,
-  name: 'Population Served',
-  valueType: PropertyNodeValueType.Float,
-  allowedValues: null,
-  remoteEditable: true,
-};
+function getPopulationServedForLanguage(language: string) {
+  const translations = language === 'fr' ? frTranslations : enTranslations;
+  return {
+    id: '7716cecc-7d62-4f1b-93fa-a55a275079b4',
+    propertyId: POPULATION_SERVED_KEY,
+    key: POPULATION_SERVED_KEY,
+    name: translations['POPULATION_SERVED_KEY'] ?? 'Population Served',
+    valueType: PropertyNodeValueType.Float,
+    allowedValues: null,
+    remoteEditable: true,
+  };
+}
 
 function translateAllowedValues(
   allowedValues: string | null,
@@ -122,7 +125,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '0ed01a18-c9ac-4b51-bb56-d5fea4f15feb',
       propertyId: LATITUDE_KEY,
       key: LATITUDE_KEY,
-      name: translations?.[LATITUDE_KEY] ?? 'Latitude',
+      name: translations['LATITUDE_KEY'] ?? 'Latitude',
       valueType: PropertyNodeValueType.Float,
       allowedValues: null,
       remoteEditable: false,
@@ -131,7 +134,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '9d595b3e-2eca-4b1a-983e-77aa34b14e62',
       propertyId: LONGITUDE_KEY,
       key: LONGITUDE_KEY,
-      name: translations?.[LONGITUDE_KEY] ?? 'Longitude',
+      name: translations['LONGITUDE_KEY'] ?? 'Longitude',
       valueType: PropertyNodeValueType.Float,
       allowedValues: null,
       remoteEditable: false,
@@ -140,7 +143,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '3285c231-ffc2-485b-9a86-5ccafed9a5c5',
       propertyId: SUPPLY_LEVEL_KEY,
       key: SUPPLY_LEVEL_KEY,
-      name: translations?.[SUPPLY_LEVEL_KEY] ?? 'Supply Level',
+      name: translations['SUPPLY_LEVEL_KEY'] ?? 'Supply Level',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'Primary,Sub-National,Lowest Distribution,Service Point',
@@ -152,7 +155,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '0e6fa1d3-4762-4b19-a832-1fe8a391e75b',
       propertyId: FACILITY_TYPE_KEY,
       key: FACILITY_TYPE_KEY,
-      name: translations?.[FACILITY_TYPE_KEY] ?? 'Facility Type',
+      name: translations['FACILITY_TYPE_KEY'] ?? 'Facility Type',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'National Vaccine Store,Regional Vaccine Store,Referral Hospital,Municipal Warehouse,Maternal Clinic',
@@ -164,7 +167,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '098d1c23-1257-451a-a449-500ab3907337',
       propertyId: OWNERSHIP_TYPE_KEY,
       key: OWNERSHIP_TYPE_KEY,
-      name: translations?.[OWNERSHIP_TYPE_KEY] ?? 'Ownership Type',
+      name: translations['OWNERSHIP_TYPE_KEY'] ?? 'Ownership Type',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'Government,NGO,Private,Faith-based',
@@ -176,7 +179,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '4396d231-ffc2-485b-9a86-5ccafed0b6d6',
       propertyId: BUFFER_STOCK_KEY,
       key: BUFFER_STOCK_KEY,
-      name: translations?.[BUFFER_STOCK_KEY] ?? 'Stock Safety Buffer (months)',
+      name: translations['BUFFER_STOCK_KEY'] ?? 'Stock Safety Buffer (months)',
       valueType: PropertyNodeValueType.Integer,
       allowedValues: null,
       remoteEditable: false,
@@ -186,7 +189,7 @@ function getGapsPropertiesForLanguage(language: string) {
       propertyId: SUPPLY_INTERVAL_KEY,
       key: SUPPLY_INTERVAL_KEY,
       name:
-        translations?.[SUPPLY_INTERVAL_KEY] ??
+        translations['SUPPLY_INTERVAL_KEY'] ??
         'Supply Interval (Months between deliveries)',
       valueType: PropertyNodeValueType.Integer,
       allowedValues: null,
@@ -196,7 +199,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: 'c5e363fc-40c9-4m1c-b29a-76d74534b077',
       propertyId: PACKAGING_LEVEL_KEY,
       key: PACKAGING_LEVEL_KEY,
-      name: translations?.[PACKAGING_LEVEL_KEY] ?? 'Packaging Level',
+      name: translations['PACKAGING_LEVEL_KEY'] ?? 'Packaging Level',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'Primary (1),Secondary (2),Tertiary (3)',
@@ -204,16 +207,13 @@ function getGapsPropertiesForLanguage(language: string) {
       ),
       remoteEditable: true,
     },
-    {
-      ...populationServed,
-      name: translations?.[POPULATION_SERVED_KEY] ?? 'Population Served',
-    },
+    getPopulationServedForLanguage(language),
     {
       id: 'd700e86a-28c9-40a9-830c-f8a9793c63a0',
       propertyId: ELECTRICITY_AVAILABILITY_KEY,
       key: ELECTRICITY_AVAILABILITY_KEY,
       name:
-        translations?.[ELECTRICITY_AVAILABILITY_KEY] ??
+        translations['ELECTRICITY_AVAILABILITY_KEY'] ??
         'Electricity Availability',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
@@ -226,7 +226,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: 'cbb104cd-c5f7-4c7a-af5e-ef4ad1b428e0',
       propertyId: SOLAR_AVAILABILITY_KEY,
       key: SOLAR_AVAILABILITY_KEY,
-      name: translations?.[SOLAR_AVAILABILITY_KEY] ?? 'Solar Availability',
+      name: translations['SOLAR_AVAILABILITY_KEY'] ?? 'Solar Availability',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         '> 16 hours,8-16 hours,< 8 hours,No availability,Unknown',
@@ -238,7 +238,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '633f4741-91ad-46a6-b302-8d1979eb3be4',
       propertyId: GAS_AVAILABILITY_KEY,
       key: GAS_AVAILABILITY_KEY,
-      name: translations?.[GAS_AVAILABILITY_KEY] ?? 'Gas Availability',
+      name: translations['GAS_AVAILABILITY_KEY'] ?? 'Gas Availability',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'No availability,Available,Irregular,Unknown',
@@ -251,7 +251,7 @@ function getGapsPropertiesForLanguage(language: string) {
       propertyId: KEROSENE_AVAILABILITY_KEY,
       key: KEROSENE_AVAILABILITY_KEY,
       name:
-        translations?.[KEROSENE_AVAILABILITY_KEY] ?? 'Kerosene Availability',
+        translations['KEROSENE_AVAILABILITY_KEY'] ?? 'Kerosene Availability',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'No availability,Available,Irregular,Unknown',
@@ -263,7 +263,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '86cb041d-96d3-40f1-874e-4189f4796790',
       propertyId: PENTA_3_KEY,
       key: PENTA_3_KEY,
-      name: translations?.[PENTA_3_KEY] ?? 'Penta-3 Coverage',
+      name: translations['PENTA_3_KEY'] ?? 'Penta-3 Coverage',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'High,Medium,Low,Unknown',
@@ -275,7 +275,7 @@ function getGapsPropertiesForLanguage(language: string) {
       id: '9cc3ac59-061e-4e3f-af13-d2b6d9a52dea',
       propertyId: ZERO_DOSE_KEY,
       key: ZERO_DOSE_KEY,
-      name: translations?.[ZERO_DOSE_KEY] ?? 'Zero Dose Coverage',
+      name: translations['ZERO_DOSE_KEY'] ?? 'Zero Dose Coverage',
       valueType: PropertyNodeValueType.String,
       allowedValues: translateAllowedValues(
         'High,Medium,Low,Unknown',
@@ -292,5 +292,6 @@ export const gapsNameProperties: LocalisedNamePropertyConfig = {
 };
 
 export const populationNameProperties: LocalisedNamePropertyConfig = {
-  en: [populationServed],
+  en: [getPopulationServedForLanguage('en')],
+  fr: [getPopulationServedForLanguage('fr')],
 };
