eba68a7bd5070441747af2ce6b6b5f75fc560cb2
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-06-03 16:26:57 +1200

    Add migration

---
+++ server/repository/src/migrations/v2_08_00/add_population_percentage_to_demographic.rs
@@ -0,0 +1,25 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_population_percentage_to_demographic"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+                ALTER TABLE demographic ADD COLUMN population_percentage {DOUBLE} NOT NULL DEFAULT 0;
+
+                UPDATE demographic SET population_percentage = (
+                    SELECT population_percentage FROM demographic_indicator
+                    WHERE demographic_id = demographic.id
+                );
+            "#,
+        )?;
+
+        Ok(())
+    }
+}
