a0cb4193733478b2f20e129d1a4e4672852c11d2
James Brunskill <james@sussol.net>
2025-06-05 12:56:06 +1200

    Re-integrate the demographic for remote stores

--- server/repository/src/migrations/v2_08_00/add_population_percentage_to_demographic.rs
+++ server/repository/src/migrations/v2_08_00/add_population_percentage_to_demographic.rs
@@ -30,13 +30,24 @@ impl MigrationFragment for Migrate {
         )?;
 
         // Create changelog entries for all the existing demographic records, so they'll be synced with their new population_percentage
-        // We limit it to records that have a corresponding demographic_indicator, so this actually only affects the central server, otherwise we sync 0 records to central!
+        // We limit it to records that have a corresponding demographic_indicator, so this actually only affects the central server, otherwise we might accidentally sync 0 records to central!
         sql!(
                 connection,
                 "INSERT INTO changelog (table_name, record_id, row_action) SELECT 'demographic', id, 'UPSERT' FROM demographic WHERE id in (
                     SELECT demographic_id FROM demographic_indicator
                 );"
             )?;
+
+        // If we've update and synced the records on central, the remote site might not have had that column yet.
+        // So we need to re-integrate the records on the remote site on upgrade, this should be safe as only the remote site should have data in the sync_buffer.
+        // Would be nice to limit this so Central server doesn't run this code at all but it doesn't know it's central till first sync.
+        sql!(
+            connection,
+            r#"
+                UPDATE sync_buffer SET integration_datetime = NULL 
+                    WHERE table_name = 'demographic';
+            "#
+        )?;
 
         Ok(())
     }
