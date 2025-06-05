075ea3b859a1238b28dd0fdc3593f38565abfaa3
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2025-06-05 14:22:23 +1200

    use getNaiveDate to render dates (msupply-foundation/open-msupply#8029)
    Source: https://github.com/msupply-foundation/open-msupply/commit/420bd8c7040c275c0e5cde065b41d146e6909c12

--- client/packages/system/src/Manage/Campaigns/ListView/ListView.tsx
+++ client/packages/system/src/Manage/Campaigns/ListView/ListView.tsx
@@ -14,6 +14,7 @@ import {
   useTableStore,
   useDeleteConfirmation,
   useNotification,
+  DateUtils,
 } from '@openmsupply-client/common';
 import { Footer } from './Footer';
 import { CampaignEditModal } from './CampaignEditModal';
@@ -103,6 +104,7 @@ const CampaignsComponent = () => {
         width: 150,
         format: ColumnFormat.Date,
         sortable: false,
+        accessor: ({ rowData }) => DateUtils.getNaiveDate(rowData.startDate),
       },
       {
         key: 'endDate',
@@ -110,6 +112,7 @@ const CampaignsComponent = () => {
         width: 150,
         format: ColumnFormat.Date,
         sortable: false,
+        accessor: ({ rowData }) => DateUtils.getNaiveDate(rowData.endDate),
       },
     ],
     {
