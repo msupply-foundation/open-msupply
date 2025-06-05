6b05e6b696ed0b1cdaebce44b9fe98bf22923c7e
James Brunskill <james@sussol.net>
2025-06-05 13:53:12 +1200

    Add comment

--- client/packages/host/src/api/hooks/settings/namePropertyData.ts
+++ client/packages/host/src/api/hooks/settings/namePropertyData.ts
@@ -29,6 +29,8 @@ export type LocalisedNamePropertyConfig = Partial<
 };
 
 // English keys are fall-backs, all other languages should implement the same keys
+// NOTE: If we need to add a third language or are doing refactoring here, we should probaly use the usual t() style translation function
+// Perhaps with a different .json file to so these translations are easy to find and not accidentally removed...
 type TranslationKey = keyof typeof frTranslations;
 
 const enTranslations = {
