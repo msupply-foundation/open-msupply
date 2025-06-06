bb49ff150d3bec6e6e622234c3f5f0cf0ab26832
Fergus Roache <90807420+fergie-nz@users.noreply.github.com>
2025-06-06 07:45:08 +0545

    7876 add report list view (#7920)
    * Add backend endpoint for all reports
    
    * Add list view and front end api connection
    
    * Amend to correct query showing all reports
    
    * Add pagination to reports query
    
    Turns out this was necessary because we are already exceeding viewable page limits.
    
    * Change error display
    
    * Add requested changes
    
    * Add requested changes

---
+++ client/packages/system/src/Manage/Reports/ListView/ListView.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import {
+  TableProvider,
+  DataTable,
+  useColumns,
+  createTableStore,
+  NothingHere,
+  useUrlQueryParams,
+  useTranslation,
+  TooltipTextCell,
+} from '@openmsupply-client/common';
+import { AppBarButtons } from './AppBarButtons';
+import { useAllReportVersionsList } from '../api/hooks/useAllReportVersionsList';
+import { ReportRowFragment } from 'packages/system/src/Report';
+
+const ReportsComponent = () => {
+  const t = useTranslation();
+  const {
+    updateSortQuery,
+    updatePaginationQuery,
+    queryParams: { sortBy, page, first, offset, filterBy },
+  } = useUrlQueryParams({ initialSort: { key: 'code', dir: 'asc' } });
+
+  const queryParams = { sortBy, first, offset, filterBy };
+  const {
+    query: { data, isError, isLoading },
+  } = useAllReportVersionsList({
+    queryParams,
+  });
+
+  const pagination = { page, first, offset };
+
+  const columns = useColumns<ReportRowFragment>(
+    [
+      {
+        key: 'name',
+        label: 'label.name',
+        width: 150,
+        sortable: true,
+        Cell: TooltipTextCell,
+      },
+      {
+        key: 'code',
+        label: 'label.code',
+        width: 150,
+        sortable: true,
+        Cell: TooltipTextCell,
+      },
+      {
+        key: 'version',
+        label: 'label.version',
+        width: 150,
+        sortable: true,
+      },
+      {
+        label: 'label.status',
+        key: 'isActive',
+        accessor: ({ rowData }) => {
+          const { isActive } = rowData;
+          return t(isActive ? 'label.active' : 'label.inactive');
+        },
+        width: 150,
+        sortable: false,
+      },
+      {
+        label: 'label.is_custom',
+        key: 'isCustom',
+        width: 150,
+        sortable: false,
+      },
+    ],
+    {
+      sortBy,
+      onChangeSortBy: updateSortQuery,
+    },
+    [sortBy]
+  );
+
+  return (
+    <>
+      <AppBarButtons />
+      <DataTable
+        id="report-list"
+        pagination={{ ...pagination, total: data?.totalCount ?? 0 }}
+        onChangePage={updatePaginationQuery}
+        columns={columns}
+        data={data?.nodes}
+        isLoading={isLoading}
+        isError={isError}
+        noDataElement={<NothingHere body={t('error.no-reports')} />}
+      />
+    </>
+  );
+};
+
+export const ReportsList = () => (
+  <TableProvider createStore={createTableStore}>
+    <ReportsComponent />
+  </TableProvider>
+);
---
+++ client/packages/system/src/Manage/Reports/api/hooks/useAllReportVersionsList.ts
@@ -0,0 +1,100 @@
+import {
+  useQuery,
+  SortBy,
+  ReportSortFieldInput,
+  ReportFilterInput,
+  useIntlUtils,
+  useTranslation,
+  useNotification,
+  LIST_KEY,
+} from '@openmsupply-client/common';
+import {  ALLREPORTVERSIONS } from './keys';
+import { ReportRowFragment } from 'packages/system/src/Report/index.js';
+import { useAllReportVersionsGraphQL } from '../../api/useReportsGraphQL';
+
+export interface ReportListParams {
+  filterBy: ReportFilterInput | null;
+  sortBy?: SortBy<ReportRowFragment>;
+  first?: number;
+  offset?: number;
+};
+
+export const useAllReportVersionsList = ({
+  queryParams,
+}: {
+  queryParams?: ReportListParams;
+}) => {
+    // QUERY
+    const { data, isLoading, isError } = useGetList(queryParams);
+
+    return {
+        query: {data, isLoading, isError},
+    };
+};
+
+const useGetList = (queryParams?: ReportListParams) => {
+  const t = useTranslation();
+  const { reportApi, storeId } = useAllReportVersionsGraphQL();
+  const { currentLanguage: language } = useIntlUtils();
+  const { error } = useNotification();
+
+  const {
+    filterBy,
+    sortBy = {
+      key: 'code',
+      direction: 'asc',
+    },
+    offset,
+    first,
+  } = queryParams ?? {};
+    const queryKey = [ALLREPORTVERSIONS, storeId, LIST_KEY, sortBy, filterBy, offset, first];
+
+  const queryFn = async () => {
+    try {
+      const query = await reportApi.allReportVersions({
+        filter: {
+          ...filterBy,
+        },
+        key: sortBy.key as ReportSortFieldInput,
+        desc: sortBy.isDesc,
+        storeId,
+        userLanguage: language,
+        first,
+        offset,
+      });
+
+      if (query?.allReportVersions?.__typename == 'ReportConnector') {
+        return {
+          nodes: query.allReportVersions.nodes,
+          totalCount: query.allReportVersions.totalCount,
+        };
+      }
+      if (query?.allReportVersions.__typename == 'QueryReportsError') {
+        let errorMessage;
+        switch (query.allReportVersions.error.__typename) {
+          case 'FailedTranslation':
+            errorMessage = t('report.error-translating', {
+              key: query.allReportVersions.error.description,
+            });
+            break;
+          // TODO add never exhaustive error handling if adding more error types to QueryReportError
+          // default:
+          //   noOtherVariants(query.reports.error.__typename);
+        }
+        error(errorMessage)();
+        console.error(errorMessage);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return useQuery({
+    queryKey,
+    queryFn,
+    onError: (e: Error) => {
+      if (/HasPermission\(Report\)/.test(e.message)) return null;
+      return [];
+    },
+  });
+};
