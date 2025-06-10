1751cf9fe2f02e9ff6926436225e9f6a0d341c94
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-06-10 13:59:21 +1200

    Add initial migration fragments

---
+++ server/repository/src/migrations/v2_09_00/add_purchase_order_tables.rs
@@ -0,0 +1,81 @@
+use crate::migrations::*;
+
+pub(crate) struct Migrate;
+
+impl MigrationFragment for Migrate {
+    fn identifier(&self) -> &'static str {
+        "add_purchase_order_tables"
+    }
+
+    fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        sql!(
+            connection,
+            r#"
+                CREATE TABLE purchase_order (
+                    id TEXT NOT NULL PRIMARY KEY,
+                    created_datetime {DATETIME},
+                    confirmed_datetime {DATETIME},
+                    delivery_datetime {DATETIME},
+                    status TEXT,
+                    target_months {DOUBLE},
+                    comment TEXT,
+                    supplier_id TEXT REFERENCES name(id),
+                    supplier_discount_percentage {DOUBLE},
+                    supplier_discount_amount {DOUBLE},
+                    donor_link_id TEXT REFERENCES name_link(id),
+                    reference TEXT,
+                    currency_id TEXT REFERENCES currency(id),
+                    foreign_exchange_rate {DOUBLE},
+                    shipping_method TEXT,
+                    sent_datetime {DATETIME},
+                    contract_signed_datetime {DATETIME},
+                    advance_paid_datetime {DATETIME},
+                    received_at_port_datetime {DATETIME},
+                    expected_delivery_datetime {DATETIME},
+                    supplier_agent TEXT,
+                    authorising_officer_1 TEXT,
+                    authorising_officer_2 TEXT,
+                    additional_instructions TEXT,
+                    heading_message TEXT
+                    -- agent_commission TEXT,
+                    -- document_charge TEXT,
+                    -- communications_charge TEXT,
+                    -- insurance_charge TEXT,
+                    -- freight_charge TEXT,
+                    -- freight_conditions TEXT
+                );
+            "#
+        )?;
+
+        sql!(
+            connection,
+            r#"
+                CREATE TABLE purchase_order_line (
+                    id TEXT NOT NULL PRIMARY KEY,
+                    item_code TEXT NOT NULL,
+                    item_name TEXT,
+                    number_of_packs {DOUBLE},
+                    pack_size {DOUBLE},
+                    original_quantity {DOUBLE},
+                    adjusted_quantity {DOUBLE},
+                    total_received {DOUBLE},
+                    -- stock_on_hand {DOUBLE},
+                    on_order BOOLEAN,
+                    -- price_extension TEXT,
+                    requested_delivery_date {DATE},
+                    expected_delivery_date {DATE}
+                );
+            "#
+        )?;
+
+        sql!(
+            connection,
+            r#"
+                ALTER TABLE invoice ADD COLUMN purchase_order_id TEXT REFERENCES purchase_order(id);
+                ALTER TABLE invoice_line ADD COLUMN purchase_order_line_id TEXT REFERENCES purchase_order(id);
+            "#
+        )?;
+
+        Ok(())
+    }
+}
