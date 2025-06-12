fd018b1060143148420622d2b5fa944537e781f5
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-06-13 11:39:41 +1200

    8061 create clinician when creating prescription (msupply-foundation/open-msupply#8159)
    * sync clinician store join
    
    * fix datetime picker props
    
    * add create button to clinician search input
    
    * fix new prescription modal centering
    
    * add slide in modal option
    
    * add create clinician modal
    
    * add create clinician draft state hook
    
    * save the clinician!
    
    * clear on cancel; show loading on save
    
    * make asSidePanel an optin
    
    * prevent duplicate key issues
    
    * populate modal with newly created clinician
    
    * add confirmation modal if existing clinician code is found
    
    * type fix
    
    * add quit to desktop translations
    
    * uppercase codes
    
    ---------
    
    Co-authored-by: Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/52beddde3189d5a58e5fdfb155dd4c0411e15fb9

--- client/packages/system/src/Clinician/index.ts
+++ client/packages/system/src/Clinician/index.ts
@@ -1,2 +1,6 @@
+import { InsertClinicianInput } from '@common/types';
+
 export * from './utils';
 export * from './ClinicianSearchInput';
+
+export type DraftClinician = Omit<InsertClinicianInput, 'id'>;
