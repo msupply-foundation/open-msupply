31c80baa504d0957aa3792c75be1c21985089b38
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-06-13 12:47:45 +1200

    remove non-required accessors

--- client/packages/system/src/Clinician/ListView/columns.ts
+++ client/packages/system/src/Clinician/ListView/columns.ts
@@ -12,22 +12,18 @@ export const useClinicianListColumns = () => {
     {
       key: 'code',
       label: 'label.code',
-      accessor: ({ rowData }) => rowData?.code,
     },
     {
       key: 'firstName',
       label: 'label.first-name',
-      accessor: ({ rowData }) => rowData?.firstName,
     },
     {
       key: 'lastName',
       label: 'label.last-name',
-      accessor: ({ rowData }) => rowData?.lastName,
     },
     {
       key: 'initials',
       label: 'label.initials',
-      accessor: ({ rowData }) => rowData?.initials,
     },
     {
       key: 'gender',
