c01c06486fe5e1035a0ffa76064f483b9b53efa9
roxy-dao <roxy@msupply.foundation>
2025-06-17 15:58:23 +1200

    Recreate view don't nest

--- server/repository/src/migrations/views/mod.rs
+++ server/repository/src/migrations/views/mod.rs
@@ -154,6 +154,49 @@ pub(crate) fn rebuild_views(connection: &StorageConnection) -> anyhow::Result<()
         invoice.status AS invoice_status,
         invoice_line_stock_movement.total_before_tax AS total_before_tax,
         invoice_line_stock_movement.pack_size as pack_size,
+        invoice_line_stock_movement.number_of_packs as number_of_packs
+    FROM
+        invoice_line_stock_movement
+        LEFT JOIN reason_option ON invoice_line_stock_movement.reason_option_id = reason_option.id
+        LEFT JOIN stock_line ON stock_line.id = invoice_line_stock_movement.stock_line_id
+        JOIN invoice ON invoice.id = invoice_line_stock_movement.invoice_id
+        JOIN name_link ON invoice.name_link_id = name_link.id
+        JOIN name ON name_link.name_id = name.id
+    )
+    SELECT * FROM all_movements
+    WHERE datetime IS NOT NULL;
+
+  CREATE VIEW item_ledger AS
+    WITH all_movements AS (
+      SELECT
+        invoice_line_stock_movement.id AS id,
+        quantity_movement AS movement_in_units,
+        invoice_line_stock_movement.item_link_id AS item_id,
+        invoice.store_id as store_id,
+        CASE WHEN invoice.type IN (
+          'OUTBOUND_SHIPMENT', 'SUPPLIER_RETURN', 'PRESCRIPTION'
+        ) THEN picked_datetime
+          WHEN invoice.type IN (
+            'INBOUND_SHIPMENT', 'CUSTOMER_RETURN'
+        ) THEN delivered_datetime
+          WHEN invoice.type IN (
+            'INVENTORY_ADDITION', 'INVENTORY_REDUCTION', 'REPACK'
+        ) THEN verified_datetime
+        ELSE NULL
+        END AS datetime,
+        name,
+        invoice.type AS invoice_type,
+        invoice.invoice_number AS invoice_number,
+        invoice.id AS invoice_id,
+        reason_option.reason AS reason,
+        stock_line_id,
+        invoice_line_stock_movement.expiry_date AS expiry_date,
+        invoice_line_stock_movement.batch AS batch,
+        invoice_line_stock_movement.cost_price_per_pack AS cost_price_per_pack,
+        invoice_line_stock_movement.sell_price_per_pack AS sell_price_per_pack,
+        invoice.status AS invoice_status,
+        invoice_line_stock_movement.total_before_tax AS total_before_tax,
+        invoice_line_stock_movement.pack_size as pack_size,
         invoice_line_stock_movement.number_of_packs as number_of_packs,
         CASE
           WHEN invoice.type IN ('INBOUND_SHIPMENT', 'CUSTOMER_RETURN', 'INVENTORY_ADDITION') THEN 1
@@ -168,37 +211,15 @@ pub(crate) fn rebuild_views(connection: &StorageConnection) -> anyhow::Result<()
         JOIN name_link ON invoice.name_link_id = name_link.id
         JOIN name ON name_link.name_id = name.id
     )
-    SELECT * FROM all_movements
-    WHERE datetime IS NOT NULL;
-
-  CREATE VIEW item_ledger AS
-    SELECT    
-      id,
-      quantity,
-      item_id,
-      store_id,
-      datetime,
-      name,
-      invoice_type,
-      invoice_number,
-      invoice_id,
-      reason,
-      expiry_date,
-      batch,
-      cost_price_per_pack,
-      sell_price_per_pack,
-      invoice_status,
-      total_before_tax,
-      pack_size,
-      number_of_packs,
-      type_precedence,
-      SUM(quantity) OVER (
-        PARTITION BY stock_movement.store_id, stock_movement.item_id
-        ORDER BY stock_movement.datetime, stock_movement.id, type_precedence
+    SELECT *,
+      SUM(movement_in_units) OVER (
+        PARTITION BY store_id, item_id
+        ORDER BY datetime, id, type_precedence
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
       ) AS running_balance
-  FROM stock_movement
-  ORDER BY stock_movement.datetime, stock_movement.id, type_precedence;
+    FROM all_movements
+    WHERE datetime IS NOT NULL  
+    ORDER BY datetime, id, type_precedence;
 
   CREATE VIEW replenishment AS
     SELECT
