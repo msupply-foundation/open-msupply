e4992dbf27bc5cd45409eddac890a9d32b4d9a70
noel-yeldos <noelyeldos@gmail.com>
2025-06-18 10:38:29 +1200

    Remove default comment

--- server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/update.rs
+++ server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/update.rs
@@ -130,8 +130,7 @@ impl UpdateInput {
             }),
             r#type: StockInType::InboundShipment,
             vvm_status_id,
-            note: note.map(|note| NullableUpdate { value: note.value }),
-            // Default
+            note,
             donor_id: donor_id.map(|donor_id| NullableUpdate {
                 value: donor_id.value,
             }),
