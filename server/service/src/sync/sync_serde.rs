3c68531d8ad9f4b480334452ccce4021bc9ffb66
Fergus Roache <fergus@msupply.foundation>
2025-06-23 12:34:14 +1200

    Refactor empty str as option without non string failure

--- server/service/src/sync/sync_serde.rs
+++ server/service/src/sync/sync_serde.rs
@@ -1,6 +1,6 @@
 use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime};
 use serde::{
-    de::{value::StrDeserializer, Error, IntoDeserializer},
+    de::{Error, IntoDeserializer},
     Deserialize, Deserializer, Serialize, Serializer,
 };
 use serde_yaml::Value;
@@ -16,12 +16,22 @@ pub fn empty_str_as_option_string<'de, D: Deserializer<'de>>(
 pub fn empty_str_as_option<'de, T: Deserialize<'de>, D: Deserializer<'de>>(
     d: D,
 ) -> Result<Option<T>, D::Error> {
-    let s: Option<String> = empty_str_as_option_string(d)?;
-
-    let Some(s) = s else { return Ok(None) };
-
-    let str_d: StrDeserializer<D::Error> = s.as_str().into_deserializer();
-    Ok(Some(T::deserialize(str_d)?))
+    let value: Value = Value::deserialize(d)?;
+    match value {
+        Value::String(ref s) if s.is_empty() => Ok(None),
+        Value::Null => Ok(None),
+        _ => {
+            let value_str = format!("{:?}", value);
+            let t: T = T::deserialize(value).map_err(|e| {
+                Error::custom(format!(
+                    "Failed to deserialize value: {}. Error: {}",
+                    value_str,
+                    format_error(&e)
+                ))
+            })?;
+            Ok(Some(t))
+        }
+    }
 }
 
 pub fn zero_date_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<NaiveDate>, D::Error> {
@@ -30,12 +40,25 @@ pub fn zero_date_as_option<'de, D: Deserializer<'de>>(d: D) -> Result<Option<Nai
         .and_then(|s| NaiveDate::parse_from_str(&s, "%Y-%m-%d").ok()))
 }
 
-pub fn zero_datetime_as_option<'de, D: Deserializer<'de>>(
+pub fn object_fields_as_option<'de, T: Deserialize<'de>, D: Deserializer<'de>>(
     d: D,
-) -> Result<Option<NaiveDateTime>, D::Error> {
-    let s: Option<String> = Option::deserialize(d)?;
-    Ok(s.filter(|s| s != "0000-00-00 00:00:00")
-        .and_then(|s| NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").ok()))
+) -> Result<Option<T>, D::Error> {
+    // error if cannot deserialise into a Value (which includes null, empty string or empty object)
+    let value: Value = Value::deserialize(d)?;
+    return match value {
+        Value::Null => Ok(None),
+        Value::String(s) if s.is_empty() => Ok(None),
+        // check if values as an empty object `{}`
+        Value::Sequence(ref map) if map.is_empty() => Ok(None),
+        Value::Mapping(ref map) if map.is_empty() => Ok(None),
+        _ => {
+            // if value is not null, empty string or empty object, extract struct T from value
+            let result: Result<Option<T>, D::Error> = T::deserialize(value.into_deserializer())
+                .map(Some)
+                .map_err(Error::custom);
+            result
+        }
+    };
 }
 
 pub fn date_and_time_to_datetime(date: NaiveDate, seconds: i64) -> NaiveDateTime {
@@ -63,13 +86,6 @@ where
 {
     x.map(|date| date.and_hms_opt(0, 0, 0).unwrap())
         .serialize(s)
-}
-
-pub fn datetime_option_to_isostring<S>(x: &Option<NaiveDateTime>, s: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    x.map(|dt| dt).serialize(s)
 }
 
 /// Currently v5 returns times in sec and v3 expects a time string when posting. To make it more
@@ -119,5 +135,73 @@ pub fn zero_f64_as_none<'de, D: Deserializer<'de>>(d: D) -> Result<Option<f64>, 
         _ => Err(Error::custom(
             "zero_f64_as_none Expected a string or number",
         )),
+    }
+}
+
+// test deserialise empty_str_as_option
+
+#[cfg(test)]
+mod test {
+    use crate::sync::sync_serde::empty_str_as_option;
+    use serde::{Deserialize, Serialize};
+
+    #[allow(non_snake_case)]
+    #[derive(Deserialize, Serialize, Debug, PartialEq)]
+    pub struct LegcacyRowWithOptionNonString {
+        #[serde(rename = "ID")]
+        pub id: String,
+        #[serde(default)]
+        #[serde(deserialize_with = "empty_str_as_option")]
+        pub option_t: Option<i64>,
+    }
+
+    #[test]
+    fn test_handle_some_translation() {
+        // case with populated fields
+        const LEGACY_ROW_1: (&str, &str) = (
+            "LEGACY_ROW_1",
+            r#"{
+                "ID": "LEGACY_ROW_1",
+                "option_t": 12
+            }"#,
+        );
+        let a = serde_json::from_str::<LegcacyRowWithOptionNonString>(&LEGACY_ROW_1.1);
+        assert!(a.is_ok());
+        assert_eq!(a.unwrap().option_t, Some(12));
+
+        // case with empty string
+        const LEGACY_ROW_2: (&str, &str) = (
+            "LEGACY_ROW_2",
+            r#"{
+                "ID": "LEGACY_ROW_2",
+                "option_t": ""
+            }"#,
+        );
+        let b = serde_json::from_str::<LegcacyRowWithOptionNonString>(&LEGACY_ROW_2.1);
+        assert!(b.is_ok());
+        assert_eq!(b.unwrap().option_t, None);
+
+        // case with null
+        const LEGACY_ROW_3: (&str, &str) = (
+            "LEGACY_ROW_3",
+            r#"{
+                "ID": "LEGACY_ROW_3",
+                "option_t": null
+            }"#,
+        );
+        let c = serde_json::from_str::<LegcacyRowWithOptionNonString>(&LEGACY_ROW_3.1);
+        assert!(c.is_ok());
+        assert_eq!(c.unwrap().option_t, None);
+
+        // case with no value
+        const LEGACY_ROW_4: (&str, &str) = (
+            "LEGACY_ROW_4",
+            r#"{
+                "ID": "LEGACY_ROW_4"            
+            }"#,
+        );
+        let d = serde_json::from_str::<LegcacyRowWithOptionNonString>(&LEGACY_ROW_4.1);
+        assert!(d.is_ok());
+        assert_eq!(d.unwrap().option_t, None);
     }
 }
