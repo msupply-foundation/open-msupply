8b074ef562a8412e902431b9aed99aaac86e6c70
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-06-25 10:36:28 +1200

    refactor to own fns

--- server/service/src/report/convert_to_excel.rs
+++ server/service/src/report/convert_to_excel.rs
@@ -5,10 +5,10 @@ use scraper::{ElementRef, Html, Selector};
 use umya_spreadsheet::{
     helper::coordinate::{column_index_from_string, coordinate_from_index, index_from_coordinate},
     writer::xlsx,
-    Cell, FontSize, Worksheet,
+    Cell, FontSize, Spreadsheet, Worksheet,
 };
 
-use crate::static_files::{StaticFileCategory, StaticFileService};
+use crate::static_files::{StaticFile, StaticFileCategory, StaticFileService};
 
 use super::report_service::{GeneratedReport, ReportError};
 
@@ -19,8 +19,28 @@ pub fn export_html_report_to_excel(
     report_name: String,
     template_as_buffer: &Option<Vec<u8>>,
 ) -> Result<String, ReportError> {
-    let sheet_name = "Report";
-    // Save the file to the tmp directory
+    let reserved_file = reserve_file(base_dir, &report_name)?;
+    let mut book = get_workbook(template_as_buffer, &reserved_file.path)?;
+
+    // We work with the first sheet in the book
+    let sheet = book
+        .get_sheet_mut(&0)
+        .ok_or(ReportError::DocGenerationError(
+            "Couldn't find Excel sheet".to_string(),
+        ))?;
+
+    // Parse HTML report and apply it to the sheet
+    apply_report(sheet, report);
+
+    // Save the report to tmp dir, for download
+    xlsx::write(&book, reserved_file.path)
+        .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
+
+    Ok(reserved_file.id)
+}
+
+/// Hold a file in the temporary directory
+fn reserve_file(base_dir: &Option<String>, report_name: &str) -> Result<StaticFile, ReportError> {
     let now: DateTime<Utc> = SystemTime::now().into();
     let file_service = StaticFileService::new(base_dir)
         .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
@@ -33,48 +53,72 @@ pub fn export_html_report_to_excel(
         )
         .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
 
-    let mut book = match template_as_buffer {
+    Ok(reserved_file)
+}
+
+/// Generates excel spreadsheet from a template or creates a new one
+fn get_workbook(
+    template_as_buffer: &Option<Vec<u8>>,
+    path: &str,
+) -> Result<Spreadsheet, ReportError> {
+    let book = match template_as_buffer {
         Some(template) => {
             // Save a copy of the template to the reserved file path
-            fs::write(&reserved_file.path, template)
+            fs::write(path, template)
                 .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
 
-            // Read the template as a mutable XLSX book
-            umya_spreadsheet::reader::xlsx::read(&reserved_file.path)
+            // Read in the template as a mutable XLSX book
+            umya_spreadsheet::reader::xlsx::read(path)
                 .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?
         }
         None => {
             // Create a new xlsx file if no template is provided
             let mut book = umya_spreadsheet::new_file();
-            book.set_sheet_name(0, sheet_name)
+            book.set_sheet_name(0, "Report")
                 .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
             book
         }
     };
-
-    let sheet = book
-        .get_sheet_by_name_mut(sheet_name)
-        .ok_or(ReportError::DocGenerationError(format!(
-            "Couldn't find Excel sheet: {}",
-            sheet_name
-        )))?;
-
-    // Parse HTML report and apply it to the sheet
-    apply_report(sheet, report);
-
-    xlsx::write(&book, reserved_file.path)
-        .map_err(|err| ReportError::DocGenerationError(format!("{}", err)))?;
-
-    Ok(reserved_file.id)
+    Ok(book)
 }
 
+/// Maps a generated HTML report to an Excel worksheet
 fn apply_report(sheet: &mut Worksheet, report: GeneratedReport) -> () {
     let mut row_idx: u32 = 1;
 
     // HEADER
-    let header = Selectors::new(&report.header.unwrap_or_default());
-    let header_cells = header.excel_cells();
+    if let Some(header_html) = &report.header {
+        let rows_used = apply_header(sheet, header_html);
 
+        if rows_used > 0 {
+            // If there is header content, adjust the row index
+            // IMPORTANT: +1 is to leave a row before the main data table
+            // which is needed to support pivot tables/post processing in Excel
+            row_idx += rows_used + 1;
+        }
+    }
+
+    // MAIN DATA
+    let body = Selectors::new(&report.document);
+
+    // Table headers
+    let index_to_column_map = apply_data_table_headers(&body, sheet, row_idx);
+    // Data rows
+    // remove _ when idx needed for footer
+    let _row_idx = apply_data_rows(&body, sheet, row_idx + 1, &index_to_column_map);
+
+    // FOOTER
+    // Currently not implemented, but could be added later
+}
+
+/// Applies the header HTML to the worksheet
+fn apply_header(sheet: &mut Worksheet, header_html: &str) -> u32 /* rows used */ {
+    let header_selectors = Selectors::new(header_html);
+    let header_cells = header_selectors.excel_cells();
+
+    let mut used_rows: u32 = 0; // Start from the first row
+
+    // Apply any header content to the specified coordinates
     for (coordinate, el) in header_cells.into_iter() {
         let sheet_cell = sheet.get_cell_mut(coordinate);
 
@@ -87,21 +131,21 @@ fn apply_report(sheet: &mut Worksheet, report: GeneratedReport) -> () {
         let (_, row, _, _) = index_from_coordinate(coordinate);
         let row_index = row.unwrap_or(0);
 
-        // Keep track of which rows are used by the header section, data rows should be after
-        if row_idx < row_index {
-            row_idx = row_index + 1;
+        if used_rows < row_index {
+            used_rows = row_index;
         }
     }
+    used_rows
+}
 
-    // IMPORTANT: Leave a row before the main data table
-    // Needed to support pivot tables/post processing in Excel
-    row_idx += 1;
+/// Applies the data table header row to the worksheet
+fn apply_data_table_headers(
+    body: &Selectors,
+    sheet: &mut Worksheet,
+    row_idx: u32,
+) -> HashMap<u32, u32> {
+    let mut index_to_column_map = HashMap::new();
 
-    // MAIN DATA
-    let body = Selectors::new(&report.document);
-    let mut index_to_column_index_map: HashMap<u32, u32> = HashMap::new();
-
-    // Header row
     let data_header_row = body.data_headers();
 
     let has_custom_columns = data_header_row
@@ -120,16 +164,26 @@ fn apply_report(sheet: &mut Worksheet, report: GeneratedReport) -> () {
             .unwrap_or(html_index + 1);
 
         // Store the mapping from HTML index to column index - used for data rows
-        index_to_column_index_map.insert(html_index, column_index);
+        index_to_column_map.insert(html_index, column_index);
 
         let cell = sheet.get_cell_mut((column_index, row_idx));
 
         cell.set_value(header);
         cell.get_style_mut().get_font_mut().set_bold(true);
     }
-    row_idx += 1; // Next row
 
-    // Data rows
+    index_to_column_map
+}
+
+/// Maps each row of data to the worksheet
+fn apply_data_rows(
+    body: &Selectors,
+    sheet: &mut Worksheet,
+    row_index: u32,
+    index_to_column_index_map: &HashMap<u32, u32>,
+) -> u32 {
+    let mut row_idx = row_index;
+
     for row in body.rows_and_cells().into_iter() {
         // Duplicate any formulae/formatting to the next row before populating
         for col in 0..sheet.get_highest_column() {
@@ -152,8 +206,7 @@ fn apply_report(sheet: &mut Worksheet, report: GeneratedReport) -> () {
         row_idx += 1; // Next row
     }
 
-    // FOOTER
-    // Currently not implemented, but could be added later
+    row_idx
 }
 
 struct Selectors {
@@ -242,18 +295,71 @@ mod report_to_excel_test {
     use scraper::Html;
 
     #[test]
-    fn test_generate_excel() {
+    fn test_generate_excel_no_attributes() {
         let report: GeneratedReport = GeneratedReport {
             document: r#"
-               <table>
-              <thead
-                <tr>
-                  <th excel-column="B">Item</th>
-                  <th>Unit</th>
-                  <th excel-column="C">Consumed</th>
-                </tr>
-              </thead>
-              <tbody>
+          <table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Unit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Acetylsalicylic Acid 100mg tabs</td>
+                <td>Tablets</td>
+              </tr>
+              <tr>
+                <td>Ibuprofen 200mg tabs</td>
+                <td>Tablets</td>
+              </tr>
+            </tbody>
+          </table>
+        "#
+            .to_string(),
+            header: Some(
+                r#"
+                <div>Something here but with no excel-cell attribute</div>
+            "#
+                .to_string(),
+            ),
+            footer: None,
+        };
+
+        let mut book = umya_spreadsheet::new_file();
+        book.set_sheet_name(0, "test").unwrap();
+        let sheet = book.get_sheet_by_name_mut("test").unwrap();
+
+        apply_report(sheet, report);
+
+        let get_value = |coord: &str| {
+            sheet
+                .get_cell(coord)
+                .map(|c| c.get_raw_value().to_string())
+                .unwrap_or_default()
+        };
+
+        // Header is ignored, data table headers are in the first row
+        assert_eq!(get_value("A1"), "Item");
+        // both headers are in the first row, in order
+        assert_eq!(get_value("B1"), "Unit");
+        assert_eq!(get_value("A3"), "Ibuprofen 200mg tabs");
+    }
+
+    #[test]
+    fn test_generate_excel_with_attributes() {
+        let report: GeneratedReport = GeneratedReport {
+            document: r#"
+          <table>
+            <thead>
+              <tr>
+                <th excel-column="C">Item</th>
+                <th>Unit</th>
+                <th excel-column="B">Consumed</th>
+              </tr>
+            </thead>
+            <tbody>
               <tr>
                 <td>Acetylsalicylic Acid 100mg tabs</td>
                 <td>Tablets</td>
@@ -270,13 +376,12 @@ mod report_to_excel_test {
                 <td>5</td>
               </tr>
             </tbody>
-            </table>
-</table>
+          </table>
         "#
             .to_string(),
             header: Some(
                 r#"
-                <div excel-cell="A2"></div>
+                <div excel-cell="A2">Title Here</div>
             "#
                 .to_string(),
             ),
@@ -297,10 +402,18 @@ mod report_to_excel_test {
                 .unwrap_or_default()
         };
 
-        assert_eq!(get_value("B4"), "Item");
-        assert_eq!(get_value("C4"), "Consumed");
+        // Custom header cells are populated
+        assert_eq!(get_value("A2"), "Title Here");
+
+        // Header takes 2 rows, plus one empty row before the data table
+        assert_eq!(get_value("B3"), "");
+        // Data table headers start from row 4, using custom specified columns
         assert_eq!(get_value("A4"), "");
-        assert_eq!(get_value("B6"), "Ibuprofen 200mg tabs");
+        assert_eq!(get_value("B4"), "Consumed");
+        assert_eq!(get_value("C4"), "Item");
+        // Data also mapped to the right columns
+        assert_eq!(get_value("C6"), "Ibuprofen 200mg tabs");
+        assert_eq!(get_value("A6"), "");
     }
 
     #[test]
@@ -332,7 +445,7 @@ mod report_to_excel_test {
     fn test_excel_attribute_selector() {
         let selectors = Selectors::new(
             r#"
-          <span excel-cell="B2">First</span>
+          <td excel-cell="B2">First</td>
           <span>Second</span>
           <span excel-cell="A1">Third</span>
         "#,
@@ -378,6 +491,7 @@ mod report_to_excel_test {
 
         assert_eq!(
             selectors.data_headers(),
+            // Some() where custom column is specified
             vec![(None, "First Header"), (Some("A"), "Second Header")]
         );
 
