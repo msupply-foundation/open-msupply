d2875557a2faf09a520a8774ea127bcf25c14344
James Brunskill <james@sussol.net>
2025-06-30 15:38:28 +1200

    Update label
    Source: https://github.com/msupply-foundation/open-msupply/commit/4ba54248355a6abad0b92ced0a112db92ad0a452

--- client/packages/invoices/src/utils.ts
+++ client/packages/invoices/src/utils.ts
@@ -88,7 +88,7 @@ const statusTranslation: Record<InvoiceNodeStatus, LocaleKey> = {
   PICKED: 'label.picked',
   SHIPPED: 'label.shipped',
   DELIVERED: 'label.delivered',
-  RECEIVED: 'label.inbound-received',
+  RECEIVED: 'label.received',
   NEW: 'label.new',
   VERIFIED: 'label.verified',
   CANCELLED: 'label.cancelled',
