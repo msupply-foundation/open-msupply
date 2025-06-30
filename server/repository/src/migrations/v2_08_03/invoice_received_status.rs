741d8b9910a5402b6020fdf4d67240316a861b1a
James Brunskill <james@sussol.net>
2025-06-30 17:12:53 +1200

    Update migration

--- server/repository/src/migrations/v2_08_03/invoice_received_status.rs
+++ server/repository/src/migrations/v2_08_03/invoice_received_status.rs
@@ -16,10 +16,10 @@ impl MigrationFragment for Migrate {
             sql!(
                 connection,
                 r#"
-                ALTER TYPE invoice_status ADD VALUE IF NOT EXISTS 'RECEIVED';
+                ALTER TYPE invoice_status ADD VALUE IF NOT EXISTS 'DELIVERED_NO_STOCK';
                 ALTER TYPE activity_log_type
                 ADD VALUE IF NOT EXISTS
-                    'PRESCRIPTION_STATUS_RECEIVED';
+                    'INVOICE_STATUS_DELIVERED_NO_STOCK';
                 "#,
             )?;
         }
@@ -29,24 +29,6 @@ impl MigrationFragment for Migrate {
             r#"
                 -- Add received_datetime column to invoice table
                 ALTER TABLE invoice ADD COLUMN received_datetime {DATETIME};
-
-                -- Set a received_datetime for all existing invoices that were Delivered before (have a delivered_datetime)
-                UPDATE invoice
-                SET status = 'RECEIVED',
-                received_datetime = delivered_datetime
-                WHERE delivered_datetime is not null;
-
-                -- Set all existing invoices for this site that were Delivered to Received
-                UPDATE invoice
-                SET status = 'RECEIVED'
-                WHERE status = 'DELIVERED';
-
-                --Create changelogs to resync all the invoices that were Delivered before and are for this sync site
-                INSERT INTO changelog (record_id, table_name, row_action, store_id)
-                SELECT id,'invoice', 'UPSERT', store_id
-                FROM invoice
-                WHERE received_datetime IS NOT NULL
-                AND store_id in (SELECT store_id FROM store WHERE site_id in (SELECT value_int FROM key_value_store WHERE id = 'SETTINGS_SYNC_SITE_ID'))
             "#
         )?;
 
