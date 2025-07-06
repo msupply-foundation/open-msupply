12ca7bd2721e659120515e5f6b617a338a00398a
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-07-07 10:58:09 +1200

    Reformat

--- client/packages/system/src/Patient/Insurance/InsuranceModal.tsx
+++ client/packages/system/src/Patient/Insurance/InsuranceModal.tsx
@@ -106,189 +106,145 @@ export const InsuranceModal = (): ReactElement => {
         '& .MuiDialogContent-root': { display: 'flex', alignItems: 'center' },
       }}
     >
-      <Stack gap={8} flexDirection="row">
-        <Box display="flex" flexDirection="column" gap={2}>
-          <InputWithLabelRow
-            label={t('label.policy-number-family')}
-            Input={
-              <BasicTextInput
-                {...getErrorProps({
-                  code: t('label.policy-number-family'),
-                  value: draft.policyNumberFamily,
-                  required: !draft.policyNumberPerson,
-                })}
-                required={draft.policyNumberPerson === ''}
-                disabled={haveInsuranceId}
-                // value={draft.policyNumberFamily}
-                onChange={event => {
-                  updatePatch({
-                    policyNumberFamily: event.target.value,
-                  });
-                }}
-              />
-            }
-          />
-          <InputWithLabelRow
-            label={t('label.policy-number-person')}
-            Input={
-              <BasicTextInput
-                required={draft.policyNumberFamily === ''}
-                disabled={haveInsuranceId}
-                value={draft.policyNumberPerson}
-                onChange={event => {
-                  updatePatch({
-                    policyNumberPerson: event.target.value,
-                  });
-                }}
-              />
-            }
-          />
-          <InsurancePolicySelect
-            policyType={draft.policyType}
-            onChange={value =>
-              updatePatch({
-                policyType: value,
-              })
-            }
-          />
-          <InputWithLabelRow
-            label={t('label.insurance-active')}
-            Input={
-              <Switch
-                onChange={() => updatePatch({ isActive: !draft.isActive })}
-                checked={draft.isActive}
-                switchSx={{ left: '-13px' }}
-                color="secondary"
-              />
-            }
-          />
-        </Box>
-        <Box display="flex" flexDirection="column" gap={2}>
-          <InputWithLabelRow
-            label={t('label.insurance-expiry-date')}
-            Input={
-              <DateTimePickerInput
-                required
-                value={DateUtils.getNaiveDate(draft.expiryDate)}
-                onChange={date => {
-                  if (date)
+      <>
+        <Stack gap={8} flexDirection="row">
+          <Box display="flex" flexDirection="column" gap={2}>
+            <InputWithLabelRow
+              label={t('label.policy-number-family')}
+              Input={
+                <BasicTextInput
+                  {...getErrorProps({
+                    code: t('label.policy-number-family'),
+                    value: draft.policyNumberFamily,
+                    required: !draft.policyNumberPerson,
+                  })}
+                  required={draft.policyNumberPerson === ''}
+                  disabled={haveInsuranceId}
+                  // value={draft.policyNumberFamily}
+                  onChange={event => {
                     updatePatch({
                       policyNumberFamily: event.target.value,
                     });
-                }}
-              />
-            }
-          />
-          <InputWithLabelRow
-            label={t('label.policy-number-person')}
-            Input={
-              <BasicTextInput
-                {...getErrorProps({
-                  code: t('label.policy-number-person'),
-                  value: draft.policyNumberPerson,
-                  required: !draft.policyNumberFamily,
-                  customValidation: () => draft.policyNumberPerson !== '666',
-                  customErrorMessage:
-                    'That is the devils number and is not allowed',
-                })}
-                disabled={haveInsuranceId}
-                // value={draft.policyNumberPerson}
-                onChange={event => {
-                  updatePatch({
-                    policyNumberPerson: event.target.value,
-                  });
-                }}
-              />
-            }
-          />
-          <InsurancePolicySelect
-            {...getErrorProps({
-              code: t('label.policy-type'),
-              value: draft.policyType,
-              required: true,
-            })}
-            policyType={draft.policyType}
-            onChange={value =>
-              updatePatch({
-                policyType: value,
-              })
-            }
-          />
-          <InputWithLabelRow
-            label={t('label.insurance-active')}
-            Input={
-              <Switch
-                onChange={() => updatePatch({ isActive: !draft.isActive })}
-                checked={draft.isActive}
-                switchSx={{ left: '-13px' }}
-                color="secondary"
-              />
-            }
-          />
-        </Box>
-        <Box display="flex" flexDirection="column" gap={2}>
-          <InputWithLabelRow
-            label={t('label.expiry-date')}
-            Input={
-              <BaseDatePickerInput
-                {...getErrorProps({
-                  code: t('label.expiry-date'),
-                  value: DateUtils.getNaiveDate(draft.expiryDate),
-                  required: true,
-                })}
-                value={DateUtils.getNaiveDate(draft.expiryDate)}
-                onChange={date => {
-                  if (date)
+                  }}
+                />
+              }
+            />
+            <InputWithLabelRow
+              label={t('label.policy-number-person')}
+              Input={
+                <BasicTextInput
+                  {...getErrorProps({
+                    code: t('label.policy-number-person'),
+                    value: draft.policyNumberPerson,
+                    required: !draft.policyNumberFamily,
+                    customValidation: () => draft.policyNumberPerson !== '666',
+                    customErrorMessage:
+                      'That is the devils number and is not allowed',
+                  })}
+                  required={draft.policyNumberFamily === ''}
+                  disabled={haveInsuranceId}
+                  // value={draft.policyNumberPerson}
+                  onChange={event => {
                     updatePatch({
-                      expiryDate: formatDateTime.customDate(date, 'yyyy-MM-dd'),
+                      policyNumberPerson: event.target.value,
                     });
-                }}
-              />
-            }
-          />
-          <InsuranceProvidersSelect
-            {...getErrorProps({
-              code: t('label.provider-name'),
-              value: draft.insuranceProviderId,
-              required: true,
-            })}
-            insuranceProviderId={draft.insuranceProviderId}
-            onChange={value => {
-              updatePatch({
-                insuranceProviderId: value,
-              });
-            }}
-          />
-          <InputWithLabelRow
-            label={t('label.discount-rate')}
-            Input={
-              <NumericTextInput
-                {...getErrorProps({
-                  code: t('label.discount-rate'),
-                  value: draft.discountPercentage,
-                  required: true,
-                  customErrorMessage:
-                    draft.discountPercentage >= 110
-                      ? 'Waaaay to big!'
-                      : undefined,
-                })}
-                min={0}
-                max={100}
-                decimalLimit={2}
-                value={draft.discountPercentage ?? 0}
-                onChange={value => {
-                  if (value) {
-                    updatePatch({
-                      discountPercentage: value,
-                    });
-                  }
-                }}
-              />
-            }
-          />
-        </Box>
-      </Stack>
-      <ErrorDisplay />
+                  }}
+                />
+              }
+            />
+            <InsurancePolicySelect
+              {...getErrorProps({
+                code: t('label.policy-type'),
+                value: draft.policyType,
+                required: true,
+              })}
+              policyType={draft.policyType}
+              onChange={value =>
+                updatePatch({
+                  policyType: value,
+                })
+              }
+            />
+            <InputWithLabelRow
+              label={t('label.insurance-active')}
+              Input={
+                <Switch
+                  onChange={() => updatePatch({ isActive: !draft.isActive })}
+                  checked={draft.isActive}
+                  switchSx={{ left: '-13px' }}
+                  color="secondary"
+                />
+              }
+            />
+          </Box>
+          <Box display="flex" flexDirection="column" gap={2}>
+            <InputWithLabelRow
+              label={t('label.insurance-expiry-date')}
+              Input={
+                <DateTimePickerInput
+                  {...getErrorProps({
+                    code: t('label.expiry-date'),
+                    value: DateUtils.getNaiveDate(draft.expiryDate),
+                    required: true,
+                  })}
+                  required
+                  value={DateUtils.getNaiveDate(draft.expiryDate)}
+                  onChange={date => {
+                    if (date)
+                      updatePatch({
+                        expiryDate: formatDateTime.customDate(
+                          date,
+                          'yyyy-MM-dd'
+                        ),
+                      });
+                  }}
+                />
+              }
+            />
+            <InsuranceProvidersSelect
+              {...getErrorProps({
+                code: t('label.provider-name'),
+                value: draft.insuranceProviderId,
+                required: true,
+              })}
+              insuranceProviderId={draft.insuranceProviderId}
+              onChange={value => {
+                updatePatch({
+                  insuranceProviderId: value,
+                });
+              }}
+            />
+            <InputWithLabelRow
+              label={t('label.discount-rate')}
+              Input={
+                <NumericTextInput
+                  {...getErrorProps({
+                    code: t('label.discount-rate'),
+                    value: draft.discountPercentage,
+                    required: true,
+                    customErrorMessage:
+                      draft.discountPercentage >= 110
+                        ? 'Waaaay to big!'
+                        : undefined,
+                  })}
+                  min={0}
+                  max={100}
+                  decimalLimit={2}
+                  value={draft.discountPercentage ?? 0}
+                  onChange={value => {
+                    if (value) {
+                      updatePatch({
+                        discountPercentage: value,
+                      });
+                    }
+                  }}
+                />
+              }
+            />
+          </Box>
+        </Stack>
+        <ErrorDisplay />
+      </>
     </Modal>
   );
 };
