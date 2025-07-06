a8db9d2a32043960ea19fae5a516c5351c798763
James Brunskill <james@sussol.net>
2025-07-07 11:13:53 +1200

    Fix B2 commands

--- .github/workflows/mac_backblaze_upload.sh
+++ .github/workflows/mac_backblaze_upload.sh
@@ -6,15 +6,21 @@
 # python3 -m pip install b2
 #
 # TODO fix path for b2 cli? Make it a nicer path?
+B2_PATH=/Users/tmfmacmini/Library/Python/3.9/bin/b2
+B2_BUCKET_NAME=msupply-releases
 
-echo "GITHUB_REF = ${GITHUB_REF}"
+GITHUB_REF_NAME=$(echo "${GITHUB_REF}" | sed -e 's,.*/,,')
+echo "GITHUB_REF = ${GITHUB_REF_NAME}"
 
-ARM64_FILENAME=$(ls ./client/packages/android/app/build/outputs/apk/arm64/release/);
+# Get the full path and extract just the filename
+ARM64_FILE_PATH=$(ls ./client/packages/android/app/build/outputs/apk/arm64/release/*.apk)
+ARM64_FILENAME=$(basename "$ARM64_FILE_PATH")
 echo "Found file $ARM64_FILENAME to upload"
-/Users/tmfmacmini/Library/Python/3.9/bin/b2 file upload msupply-releases ./client/packages/android/app/build/outputs/apk/arm64/release/${ARM64_FILENAME} ${GITHUB_REF}/${ARM64_FILENAME};
+$B2_PATH file upload $B2_BUCKET_NAME "$ARM64_FILE_PATH" "${GITHUB_REF_NAME}/${ARM64_FILENAME}"
 
-UNIVERSAL_FILENAME=$(ls ./client/packages/android/app/build/outputs/apk/universal/release/);
+UNIVERSAL_FILE_PATH=$(ls ./client/packages/android/app/build/outputs/apk/universal/release/*.apk)
+UNIVERSAL_FILENAME=$(basename "$UNIVERSAL_FILE_PATH")
 echo "Found file $UNIVERSAL_FILENAME to upload"
-/Users/tmfmacmini/Library/Python/3.9/bin/b2 file upload msupply-releases ./client/packages/android/app/build/outputs/apk/universal/release/${UNIVERSAL_FILENAME} ${GITHUB_REF}/${UNIVERSAL_FILENAME};
+$B2_PATH file upload $B2_BUCKET_NAME "$UNIVERSAL_FILE_PATH" "${GITHUB_REF_NAME}/${UNIVERSAL_FILENAME}"
 
-python3 ./send-telegram-notification.py $ARM64_FILENAME $UNIVERSAL_FILENAME
+python3 ./send-telegram-notification.py "$ARM64_FILENAME" "$UNIVERSAL_FILENAME"
