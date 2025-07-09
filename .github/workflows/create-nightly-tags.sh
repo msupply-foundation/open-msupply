4882b3a802cd9a4f196d1c2a332063a2af8247e4
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2025-07-10 11:48:45 +1200

    Upload apk to backblaze (#8421)
    * initial upload apk to backblaze logic
    
    * side quest needs python on our runners
    
    * remove python install
    
    * Update to RC version
    
    * Upload to back blaze and send telegram notification on android build
    
    * Fix B2 commands
    
    * Fix paths
    
    * fix path again
    
    * Add env to right step
    
    * no-progress update
    
    * Remove unnecessary call.
    
    * Include filenames in send-telegram notification
    
    * Update to send the notification with correct url
    
    * Pass tag name and build status in upload step
    
    * Escape filename
    
    * Fix dowload link formatting
    
    * update package version for tag

--- .github/workflows/create-nightly-tags.sh
+++ .github/workflows/create-nightly-tags.sh
@@ -10,6 +10,10 @@ AFFECTED_BRANCHES=()
 # - Prepend 'v' to the package version if it doesn't exist
 # - Format tag as: v<package_version>-<commit_date>
 # Example: v1.2.3-20250702
+# - If tag already exists, skip creating it
+# - Updates package.json version field to new tag version
+# - Commit the change to package.json but NOT push it - commit lets GH pass the workflow
+# - AGAIN DONT MAKE CHANGES TO PUSH THE COMMIT unless requirement changes
 
 create_tag_for_branch() {
     local branch_name=$1
@@ -20,16 +24,23 @@ create_tag_for_branch() {
     COMMIT_DATE=$(git log -1 --format=%cd --date=format:%m%d%H%M)
     
     if [[ -n "$PKG_VERSION" && -n "$COMMIT_DATE" ]]; then
+        NEW_VERSION="$PKG_VERSION-$COMMIT_DATE"
+
         if [[ "$PKG_VERSION" == v* ]]; then
-            TAG_NAME="$PKG_VERSION-$COMMIT_DATE"
+            TAG_NAME="$NEW_VERSION"
         else
-            TAG_NAME="v$PKG_VERSION-$COMMIT_DATE"
+            TAG_NAME="v$NEW_VERSION"
         fi
 
         if git ls-remote --tags origin | grep -q "refs/tags/${TAG_NAME}$"; then
             echo "Tag $TAG_NAME already exists for branch $branch_name (version: $PKG_VERSION, latest commit date: $COMMIT_DATE), skipping..."
             return 
         fi
+
+        sed 's/"version":[ \t]*"[^"]*"/"version": "'"$NEW_VERSION"'"/' ./package.json > ./package.json.tmp && mv ./package.json.tmp ./package.json
+
+        git add ./package.json
+        git commit -m "Update package.json version to $NEW_VERSION for branch $branch_name"
 
         echo "Creating tag for $branch_name: $TAG_NAME"
         git tag "$TAG_NAME"
