d3da7674214be4ce40696366c9dcc56caa732735
roxy-dao <roxy@msupply.foundation>
2025-07-10 11:42:32 +1200

    Refactor add from master list button

--- client/packages/requisitions/src/RequestRequisition/DetailView/AppBarButtons/AddFromMasterListButton.tsx
+++ client/packages/requisitions/src/RequestRequisition/DetailView/AppBarButtons/AddFromMasterListButton.tsx
@@ -1,38 +1,29 @@
 import React from 'react';
-import {
-  ButtonWithIcon,
-  useTranslation,
-  useToggle,
-  PlusCircleIcon,
-  useAuthContext,
-} from '@openmsupply-client/common';
+import { useAuthContext } from '@openmsupply-client/common';
 import { MasterListSearchModal } from '@openmsupply-client/system';
 import { useRequest } from '../../api';
 
-export const AddFromMasterListButtonComponent = () => {
-  const t = useTranslation();
-  const isDisabled = useRequest.utils.isDisabled();
-  const isProgram = useRequest.utils.isProgram();
+export const AddFromMasterListButtonComponent = ({
+  isOn,
+  toggleOff,
+}: {
+  isOn: boolean;
+  toggleOff: () => void;
+}) => {
   const { addFromMasterList } = useRequest.utils.addFromMasterList();
-  const modalController = useToggle();
   const { storeId } = useAuthContext();
+  const filter = { isProgram: false, existsForStoreId: { equalTo: storeId } };
 
   return (
     <>
       <MasterListSearchModal
-        open={modalController.isOn}
-        onClose={modalController.toggleOff}
+        open={isOn}
+        onClose={toggleOff}
         onChange={masterList => {
-          modalController.toggleOff();
+          toggleOff();
           addFromMasterList(masterList);
         }}
-        filterBy={{ isProgram: false, existsForStoreId: { equalTo: storeId } }}
-      />
-      <ButtonWithIcon
-        disabled={isDisabled || isProgram}
-        Icon={<PlusCircleIcon />}
-        label={t('button.add-from-master-list')}
-        onClick={modalController.toggleOn}
+        filterBy={filter}
       />
     </>
   );
