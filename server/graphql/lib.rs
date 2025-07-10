3fd3823c5cb7f7dde9e85b0890f06c592aed8b72
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-07-10 15:16:55 +1200

    add graphql logger

--- server/graphql/lib.rs
+++ server/graphql/lib.rs
@@ -3,6 +3,9 @@
 #[cfg(test)]
 mod tests;
 
+mod logger;
+use logger::{GraphQLRequestLogger, QueryLogInfo};
+
 use std::sync::Mutex;
 
 use actix_web::web::{self, Data};
@@ -322,6 +325,7 @@ impl GraphqlSchema {
                 .data(auth.clone())
                 .data(settings.clone())
                 .data(validated_plugins.clone())
+                .extension(GraphQLRequestLogger)
                 .finish();
         // Self requester does not need loggers
 
@@ -335,7 +339,8 @@ impl GraphqlSchema {
                 .data(settings.clone())
                 .data(validated_plugins.clone())
                 // Add self requester to operational
-                .data(Data::new(SelfRequestImpl::new_boxed(self_requester_schema)));
+                .data(Data::new(SelfRequestImpl::new_boxed(self_requester_schema)))
+                .extension(GraphQLRequestLogger);
 
         // Initialisation schema should ony need service_provider
         let initialisation_builder = InitialisationSchema::build(
@@ -343,7 +348,8 @@ impl GraphqlSchema {
             InitialisationMutations,
             EmptySubscription,
         )
-        .data(service_provider.clone());
+        .data(service_provider.clone())
+        .extension(GraphQLRequestLogger);
 
         GraphqlSchema {
             operational: operational_builder.finish(),
@@ -357,7 +363,9 @@ impl GraphqlSchema {
     }
 
     async fn execute(&self, http_req: HttpRequest, req: GraphQLRequest) -> Response {
-        let req = req.into_inner();
+        let mut req = req.into_inner();
+        req = req.data(QueryLogInfo::new());
+
         if *self.is_operational.read().await {
             // auth_data is only available in schema in operational mode
             let user_data = auth_data_from_request(&http_req);
