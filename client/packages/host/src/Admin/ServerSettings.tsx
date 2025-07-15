948f17b26273956ad7b51a3f128021bd8bc7a0ce
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-07-15 15:16:43 +1200

    re-add server settings components

--- client/packages/host/src/Admin/ServerSettings.tsx
+++ client/packages/host/src/Admin/ServerSettings.tsx
@@ -1,29 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
+  Typography,
+  NativeMode,
+  RouteBuilder,
+  Switch,
   useToggle,
   BaseButton,
+  getPreference,
+  setPreference,
+  removePreference,
+  useNavigate,
   useTranslation,
   DownloadIcon,
+  useNativeClient,
+  LoadingButton,
   DatabaseType,
   Tooltip,
 } from '@openmsupply-client/common';
-import { Environment } from '@openmsupply-client/config';
+import { Capacitor } from '@capacitor/core';
+import { AppRoute, Environment } from '@openmsupply-client/config';
 
 import { Setting } from './Setting';
-import { LogFileModal } from './LogFileModal';
 import { useDatabaseSettings } from '../api/hooks/settings/useDatabaseSettings';
+import { LogFileModal } from './LogFileModal';
 
 export const ServerSettings = () => {
+  const [nativeMode, setNativeMode] = useState(NativeMode.None);
+  const navigate = useNavigate();
+  const { saveDatabase } = useNativeClient();
   const { data: databaseSettings } = useDatabaseSettings();
+  const [isDownloading, setIsDownloading] = useState(false);
   const t = useTranslation();
   const {
     isOn: isLogShown,
     toggleOn: showLog,
     toggleOff: hideLog,
   } = useToggle();
+  const toggleNativeMode = () => {
+    const mode =
+      nativeMode === NativeMode.Server ? NativeMode.Client : NativeMode.Server;
 
-  return (
+    (async () => {
+      await removePreference('previousServer');
+      await setPreference('mode', mode);
+      navigate(RouteBuilder.create(AppRoute.Android).build());
+    })();
+  };
+
+  useEffect(() => {
+    getPreference('mode', 'none').then(setNativeMode);
+  }, []);
+  return Capacitor.isNativePlatform() ? (
+    <>
+      <Typography variant="h5" color="primary" style={{ paddingBottom: 25 }}>
+        {t('heading.settings-android')}
+      </Typography>
+      <Setting
+        title={t('label.mode')}
+        component={
+          <>
+            <Switch
+              label={t('label.client')}
+              onChange={toggleNativeMode}
+              checked={nativeMode === NativeMode.Server}
+            />
+            <Typography
+              component="div"
+              sx={{
+                alignItems: 'center',
+                display: 'inline-flex',
+                fontSize: '14px',
+                paddingLeft: 1,
+              }}
+            >
+              {t('label.server')}
+            </Typography>
+          </>
+        }
+      />
+      <Setting
+        title={t('label.server-log')}
+        component={
+          <>
+            <LogFileModal onClose={hideLog} isOpen={isLogShown} />
+            <BaseButton onClick={showLog}>{t('button.view')}</BaseButton>
+          </>
+        }
+      />
+
+      <Setting
+        title={t('label.download-database')}
+        component={
+          <Tooltip
+            title={
+              databaseSettings?.databaseType !== DatabaseType.SqLite
+                ? t('message.database-not-sqlite')
+                : nativeMode !== NativeMode.Server
+                  ? t('message.database-not-local')
+                  : t('label.download-database')
+            }
+          >
+            <span>
+              <LoadingButton
+                disabled={
+                  databaseSettings?.databaseType !== DatabaseType.SqLite ||
+                  nativeMode !== NativeMode.Server
+                }
+                isLoading={isDownloading}
+                startIcon={<DownloadIcon />}
+                onClick={async () => {
+                  setIsDownloading(true);
+                  const vacuum = await fetch(
+                    `${Environment.API_HOST}/support/vacuum`,
+                    {
+                      method: 'POST',
+                    }
+                  );
+                  if (vacuum.ok) {
+                    await saveDatabase();
+                  }
+                  setIsDownloading(false);
+                }}
+                label={t('button.download')}
+              />
+            </span>
+          </Tooltip>
+        }
+      />
+    </>
+  ) : (
     <>
       <Setting
         title={t('label.server-log')}
