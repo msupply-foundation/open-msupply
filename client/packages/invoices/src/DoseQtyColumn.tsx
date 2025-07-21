dccf644d85e1721d86aef385edc60043efa9b8e5
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-07-22 10:07:54 +1200

    round doses column display

--- client/packages/invoices/src/DoseQtyColumn.tsx
+++ client/packages/invoices/src/DoseQtyColumn.tsx
@@ -21,18 +21,19 @@ export const getDosesQuantityColumn = <
   width: 100,
   align: ColumnAlign.Right,
   accessor: ({ rowData }) => {
+    // Doses always shown in whole numbers
     if ('lines' in rowData) {
       const { lines } = rowData;
 
       const isVaccine = lines[0]?.item?.isVaccine ?? false;
       const unitQuantity = ArrayUtils.getUnitQuantity(lines);
       return isVaccine
-        ? unitQuantity * (lines[0]?.item?.doses ?? 1)
+        ? Math.round(unitQuantity * (lines[0]?.item?.doses ?? 1))
         : UNDEFINED_STRING_VALUE;
     } else {
       const unitQty = (rowData?.numberOfPacks ?? 0) * (rowData?.packSize ?? 1);
       return rowData.item && rowData.item.isVaccine
-        ? unitQty * (rowData.item.doses ?? 1)
+        ? Math.round(unitQty * (rowData.item.doses ?? 1))
         : UNDEFINED_STRING_VALUE;
     }
   },
