6aa2ab705680fc1d1408d114e992514ab1b74ba1
roxy-dao <roxy@msupply.foundation>
2025-07-21 12:06:56 +1200

    Delete translations for rnr form and lines

--- server/service/src/sync/translations/rnr_form.rs
+++ server/service/src/sync/translations/rnr_form.rs
@@ -1,6 +1,6 @@
 use repository::{
     rnr_form_row::{RnRFormRow, RnRFormRowRepository},
-    ChangelogRow, ChangelogTableName, StorageConnection, SyncBufferRow,
+    ChangelogRow, ChangelogTableName, RnRFormDelete, StorageConnection, SyncBufferRow,
 };
 
 use crate::sync::translations::{
@@ -84,6 +84,16 @@ impl SyncTranslation for RnRFormTranslation {
             self.table_name(),
             serde_json::to_value(row)?,
         ))
+    }
+
+    fn try_translate_from_delete_sync_record(
+        &self,
+        _: &StorageConnection,
+        sync_record: &SyncBufferRow,
+    ) -> Result<PullTranslateResult, anyhow::Error> {
+        Ok(PullTranslateResult::delete(RnRFormDelete(
+            sync_record.record_id.clone(),
+        )))
     }
 }
 
