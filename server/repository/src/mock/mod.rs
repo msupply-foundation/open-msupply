dbf55fb771d1f7aa632da3b0c50153cf6b5a90a0
James Brunskill <james@sussol.net>
2025-07-22 13:01:15 +1200

    Restore more purchase orders stuff
    Source: https://github.com/msupply-foundation/open-msupply/commit/628b7c06419ff558e7b2b8aebfdf0b219ae2a340

--- server/repository/src/mock/mod.rs
+++ server/repository/src/mock/mod.rs
@@ -110,6 +110,8 @@ pub use program_indicator::*;
 pub use program_order_types::*;
 pub use program_requisition_settings::*;
 pub use property::*;
+pub use purchase_order::*;
+pub use purchase_order_line::*;
 pub use reason_option::*;
 pub use reports::*;
 pub use rnr_form::*;
@@ -243,6 +245,8 @@ pub struct MockData {
     pub reason_options: Vec<ReasonOptionRow>,
     pub vvm_statuses: Vec<VVMStatusRow>,
     pub campaigns: Vec<CampaignRow>,
+    pub purchase_order: Vec<PurchaseOrderRow>,
+    pub purchase_order_line: Vec<PurchaseOrderLineRow>,
 }
 
 impl MockData {
@@ -331,6 +335,8 @@ pub struct MockDataInserts {
     pub reason_options: bool,
     pub vvm_statuses: bool,
     pub campaigns: bool,
+    pub purchase_order: bool,
+    pub purchase_order_line: bool,
 }
 
 impl MockDataInserts {
@@ -408,6 +414,8 @@ impl MockDataInserts {
             store_preferences: true,
             reason_options: true,
             campaigns: true,
+            purchase_order: true,
+            purchase_order_line: true,
         }
     }
 
@@ -757,6 +765,24 @@ impl MockDataInserts {
         self.vvm_statuses = true;
         self
     }
+
+    pub fn purchase_order(mut self) -> Self {
+        self.names = true;
+        self.stores = true;
+        self.currencies = true;
+        self.purchase_order = true;
+        self
+    }
+    pub fn purchase_order_line(mut self) -> Self {
+        self.names = true;
+        self.units = true;
+        self.items = true;
+        self.stores = true;
+        self.currencies = true;
+        self.purchase_order = true;
+        self.purchase_order_line = true;
+        self
+    }
 }
 
 #[derive(Default)]
@@ -853,6 +879,8 @@ pub(crate) fn all_mock_data() -> MockDataCollection {
             printer: mock_printers(),
             vvm_statuses: mock_vvm_statuses(),
             campaigns: mock_campaigns(),
+            purchase_order: mock_purchase_orders(),
+            purchase_order_line: mock_purchase_order_lines(),
             ..Default::default()
         },
     );
@@ -1410,6 +1438,18 @@ pub fn insert_mock_data(
                 repo.upsert_one(row).unwrap();
             }
         }
+        if inserts.purchase_order {
+            let repo = PurchaseOrderRowRepository::new(connection);
+            for row in &mock_data.purchase_order {
+                repo.upsert_one(row).unwrap();
+            }
+        }
+        if inserts.purchase_order_line {
+            let repo = PurchaseOrderLineRowRepository::new(connection);
+            for row in &mock_data.purchase_order_line {
+                repo.upsert_one(row).unwrap();
+            }
+        }
     }
     mock_data
 }
@@ -1490,6 +1530,8 @@ impl MockData {
             mut reason_options,
             mut vvm_statuses,
             mut campaigns,
+            mut purchase_order,
+            mut purchase_order_line,
         } = other;
 
         self.user_accounts.append(&mut user_accounts);
@@ -1564,6 +1606,8 @@ impl MockData {
         self.store_preferences.append(&mut store_preferences);
         self.vvm_statuses.append(&mut vvm_statuses);
         self.campaigns.append(&mut campaigns);
+        self.purchase_order.append(&mut purchase_order);
+        self.purchase_order_line.append(&mut purchase_order_line);
         self
     }
 }
