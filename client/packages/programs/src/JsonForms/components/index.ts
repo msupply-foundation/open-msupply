8f901708ace97958f72e329f8edf457eaa395fef
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-07-22 15:35:07 +1200

    Delete old "PreviousEncounterField" component

--- client/packages/programs/src/JsonForms/components/index.ts
+++ client/packages/programs/src/JsonForms/components/index.ts
@@ -4,7 +4,6 @@ export * from './DateOfBirth';
 export * from './DecisionTree/DecisionTree';
 export * from './EncounterLineChart';
 export * from './IdGenerator';
-export * from './PreviousEncounterField';
 export * from './QuantityDispensed';
 export * from './Search/Search';
 export * from './ProgramEvent';
