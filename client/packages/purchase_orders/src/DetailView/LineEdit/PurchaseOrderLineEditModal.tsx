cb107925cb553774707cd779875af7deeebd59fe
Fergus Roache <fergus@msupply.foundation>
2025-07-24 15:05:54 +1200

    Add basic purchase order line edit modal
    Source: https://github.com/msupply-foundation/open-msupply/commit/af3d922fea4df2549004db0db7d73daca6eb4b3a

--- client/packages/purchase_orders/src/DetailView/LineEdit/PurchaseOrderLineEditModal.tsx
+++ client/packages/purchase_orders/src/DetailView/LineEdit/PurchaseOrderLineEditModal.tsx
@@ -1,12 +1,12 @@
 import { ModalMode, useDialog, useNotification } from '@common/hooks';
-import { PurchaseOrderFragment, PurchaseOrderLineFragment } from '../../api';
+import { PurchaseOrderFragment } from '../../api';
 import { DialogButton, InlineSpinner } from '@common/components';
 import { useTranslation, Box } from '@openmsupply-client/common';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState } from 'react';
 import { PurchaseOrderLineEdit } from './PurchaseOrderLineEdit';
-
-type PurchaseOrderLineItem = PurchaseOrderLineFragment['item'];
-
+import { usePurchaseOrderLine } from '../../api/hooks/usePurchaseOrderLine';
+import { ItemWithStatsFragment } from '@openmsupply-client/system';
+import { createDraftPurchaseOrderLine } from './utils';
 interface PurchaseOrderLineEditModalProps {
   itemId: string | null;
   purchaseOrder: PurchaseOrderFragment;
@@ -25,21 +25,69 @@ export const PurchaseOrderLineEditModal = ({
   const t = useTranslation();
   const { error } = useNotification();
 
-  const lines = useMemo(
-    () =>
-      purchaseOrder.lines.nodes
-        .slice()
-        .sort((a, b) => a.item.name.localeCompare(b.item.name)) ?? [],
-    [purchaseOrder.lines.nodes]
+  const lines = purchaseOrder.lines.nodes;
+
+  const [currentItem, setCurrentItem] = useState(
+    lines.find(line => line.item.id === itemId) ?? undefined
   );
 
-  const [currentItem, setCurrentItem] = useState<PurchaseOrderLineItem | null>(
-    lines.find(line => line.item.id === itemId)?.item ?? null
-  );
+  const {
+    create: { create, isCreating },
+    draft,
+    updatePatch,
+  } = usePurchaseOrderLine(currentItem?.item.id);
+
+  const deletePreviousLine = () => {
+    const shouldDelete = shouldDeleteLine(mode, currentItem?.id, false);
+    if (currentItem?.id && shouldDelete) {
+      // TODO implement line delete
+    }
+  };
+
+  const onChangeItem = (item: ItemWithStatsFragment) => {
+    if (mode === ModalMode.Create) {
+      deletePreviousLine();
+    }
+    const draftLine = createDraftPurchaseOrderLine(item, purchaseOrder.id);
+    item &&
+      updatePatch({
+        ...draftLine,
+      });
+    setCurrentItem({
+      ...draftLine,
+      __typename: 'PurchaseOrderLineNode',
+      item: item,
+    });
+  };
+
+  const handleSave = async () => {
+    try {
+      await create();
+      updatePatch(draft);
+      return true;
+      // TODO add proper error handling by returning type errors from API
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        error(e.message)();
+      } else {
+        error('unknown error')();
+      }
+      return false;
+    }
+  };
+
+  // TODO handle next item workflow
+  // const onSave = async () => {
+  //   const success = await handleSave();
+  //   if (!success) return false;
+  //   // if (mode === ModalMode.Update && false setCurrentItem(next);
+  //   // else if (mode === ModalMode.Create) setCurrentItem(undefined);
+  //   // else onClose();
+  //   onClose();
+  //   return true;
+  // };
 
   const { Modal } = useDialog({ isOpen, onClose, disableBackdrop: true });
-
-  const isLoading = false;
 
   return (
     <Modal
@@ -49,28 +97,23 @@ export const PurchaseOrderLineEditModal = ({
           : t('heading.edit-item')
       }
       cancelButton={<DialogButton variant="cancel" onClick={onClose} />}
-      nextButton={
-        <DialogButton
-          variant="next-and-ok"
-          onClick={async () => {
-            console.log('clicked');
-          }}
-        />
-      }
+      // TODO add next button functionality
+      // nextButton={<DialogButton variant="next-and-ok" onClick={onSave} />}
       okButton={
         <DialogButton
           variant="ok"
           disabled={!currentItem}
           onClick={async () => {
-            console.log('clicked');
+            const success = await handleSave();
+            if (success) onClose();
           }}
         />
       }
       height={700}
       width={1200}
-      enableAutocomplete /* Required for previously entered batches to be remembered and suggested in future shipments */
+      enableAutocomplete
     >
-      {isLoading ? (
+      {isCreating ? (
         <Box
           display="flex"
           flex={1}
@@ -82,9 +125,22 @@ export const PurchaseOrderLineEditModal = ({
         </Box>
       ) : (
         <PurchaseOrderLineEdit
-          isExternalSupplier={false}
+          currentItem={currentItem}
+          isUpdateMode={mode === ModalMode.Update}
+          lines={lines}
+          onChangeItem={onChangeItem}
         ></PurchaseOrderLineEdit>
       )}
     </Modal>
   );
+};
+
+export const shouldDeleteLine = (
+  mode: ModalMode | null,
+  draftId?: string,
+  isDisabled?: boolean
+): boolean => {
+  if (mode === ModalMode.Create) return true;
+  if (!draftId || isDisabled || mode === ModalMode.Update) return false;
+  return false;
 };
