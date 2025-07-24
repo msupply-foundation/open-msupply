dcb7eb4118e30c39ae5c9e7034732229ff8adece
Fergus Roache <fergus@msupply.foundation>
2025-07-24 15:06:34 +1200

    Add create draft utils function
    Source: https://github.com/msupply-foundation/open-msupply/commit/67843cdc0d8a69c27141455d42622fdc20f0b7fd

---
+++ client/packages/purchase_orders/src/DetailView/LineEdit/utils.ts
@@ -0,0 +1,14 @@
+import { ItemWithStatsFragment } from '@openmsupply-client/system';
+import { DraftPurchaseOrderLine } from "../../api/hooks/usePurchaseOrderLine";
+import { FnUtils } from "@common/utils";
+
+export const createDraftPurchaseOrderLine = (
+  item: ItemWithStatsFragment,
+  purchaseOrderId: string
+): DraftPurchaseOrderLine => {
+  return {
+    id: FnUtils.generateUUID(),
+    purchaseOrderId,
+    itemId: item.id,
+  };
+}
