9a673c648cdfb6a5ccc5aafff5425630e35909ae
James Brunskill <james@sussol.net>
2025-07-28 10:54:09 +1200

    Remove number_type as not used any more

--- server/repository/src/migrations/v2_10_00/delete_unused_number_type.rs
+++ server/repository/src/migrations/v2_10_00/delete_unused_number_type.rs
@@ -4,15 +4,17 @@ pub(crate) struct Migrate;
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
-        "add_purchase_order_line_to_number_type"
+        "delete_unused_number_type"
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
+        // The number_type in postgres was used, but not we have a custom string mapping
+        // Removing this to avoid future confusion
         if cfg!(feature = "postgres") {
             sql!(
                 connection,
                 r#"
-                    ALTER TYPE number_type ADD VALUE 'PURCHASE_ORDER_LINE';
+                    DROP TYPE IF EXISTS number_row_type;
                 "#
             )?;
         }
