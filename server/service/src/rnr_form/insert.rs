27bf399ecb2c3d31ad1ff41d61bb775696c3074d
Chris Petty <chris@msupply.foundation>
2025-07-28 14:35:29 +1200

    Update server/service/src/rnr_form/insert.rs
    Co-authored-by: roxy-dao <roxy@msupply.foundation>

--- server/service/src/rnr_form/insert.rs
+++ server/service/src/rnr_form/insert.rs
@@ -180,7 +180,7 @@ fn validate(
         }
 
         // If the the previous form was from many periods ago, don't use it in subsequent calculations
-        if this_period as isize - previous_period as isize > 1 {
+        if this_period.saturating_sub(previous_period) > 1 {
             return Ok((None, period, master_list_id));
         }
     }
