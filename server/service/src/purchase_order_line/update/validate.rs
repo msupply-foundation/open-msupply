9964c344177ff5a1a79bf5f47baf42c723d3dd7e
James Brunskill <james@sussol.net>
2025-07-28 16:12:35 +1200

    Fix po validation when editing a line
    Source: https://github.com/msupply-foundation/open-msupply/commit/d855630da51ffc6019bfa751246db6060dc044bf

--- server/service/src/purchase_order_line/update/validate.rs
+++ server/service/src/purchase_order_line/update/validate.rs
@@ -14,7 +14,16 @@ pub fn validate(
     input: &UpdatePurchaseOrderLineInput,
     connection: &StorageConnection,
 ) -> Result<PurchaseOrderLineRow, UpdatePurchaseOrderLineInputError> {
-    let purchase_order = PurchaseOrderRowRepository::new(connection).find_one_by_id(&input.id)?;
+    let purchase_order_line =
+        PurchaseOrderLineRowRepository::new(connection).find_one_by_id(&input.id)?;
+
+    let purchase_order_line = match purchase_order_line {
+        Some(purchase_order_line) => purchase_order_line,
+        None => return Err(UpdatePurchaseOrderLineInputError::PurchaseOrderLineNotFound),
+    };
+
+    let purchase_order = PurchaseOrderRowRepository::new(connection)
+        .find_one_by_id(&purchase_order_line.purchase_order_id)?;
     let purchase_order = match purchase_order {
         Some(purchase_order) => purchase_order,
         None => return Err(UpdatePurchaseOrderLineInputError::PurchaseOrderDoesNotExist),
@@ -23,14 +32,6 @@ pub fn validate(
     if !purchase_order_is_editable(&purchase_order) {
         return Err(UpdatePurchaseOrderLineInputError::CannotEditPurchaseOrder);
     }
-
-    let purchase_order_line =
-        PurchaseOrderLineRowRepository::new(connection).find_one_by_id(&input.id)?;
-
-    let purchase_order_line = match purchase_order_line {
-        Some(purchase_order_line) => purchase_order_line,
-        None => return Err(UpdatePurchaseOrderLineInputError::PurchaseOrderLineNotFound),
-    };
 
     Ok(purchase_order_line)
 }
