a7c8931edd4c77b7040d8f385728ff1e102ef4c3
James Brunskill <james@sussol.net>
2025-07-28 14:30:13 +1200

    Improve mocks

--- server/repository/src/mock/purchase_order_line.rs
+++ server/repository/src/mock/purchase_order_line.rs
@@ -1,27 +1,44 @@
 use util::inline_init;
 
-use crate::{mock::mock_purchase_order_a, PurchaseOrderLineRow};
+use crate::{
+    mock::{mock_item_a, mock_item_b, mock_purchase_order_a, mock_purchase_order_b_confirmed},
+    PurchaseOrderLineRow,
+};
 
-pub fn mock_purchase_order_line_a() -> PurchaseOrderLineRow {
+pub fn mock_purchase_order_a_line_1() -> PurchaseOrderLineRow {
     inline_init(|r: &mut PurchaseOrderLineRow| {
-        r.id = "test_purchase_order_line_a".to_string();
+        r.id = "test_purchase_order_a_line_1".to_string();
         r.purchase_order_id = mock_purchase_order_a().id;
         r.store_id = "store_a".to_string();
         r.line_number = 1;
-        r.item_link_id = "item_a".to_string();
+        r.item_link_id = mock_item_a().id;
     })
 }
 
-pub fn mock_purchase_order_line_b() -> PurchaseOrderLineRow {
+pub fn mock_purchase_order_a_line_2() -> PurchaseOrderLineRow {
     inline_init(|r: &mut PurchaseOrderLineRow| {
-        r.id = "test_purchase_order_line_b".to_string();
+        r.id = "test_purchase_order_a_line_2".to_string();
         r.store_id = "store_a".to_string();
         r.purchase_order_id = mock_purchase_order_a().id;
         r.line_number = 1;
-        r.item_link_id = "item_a".to_string();
+        r.item_link_id = mock_item_b().id;
+    })
+}
+
+pub fn mock_purchase_order_b_line_1() -> PurchaseOrderLineRow {
+    inline_init(|r: &mut PurchaseOrderLineRow| {
+        r.id = "test_purchase_order_b_line_1".to_string();
+        r.store_id = "store_a".to_string();
+        r.purchase_order_id = mock_purchase_order_b_confirmed().id;
+        r.line_number = 1;
+        r.item_link_id = mock_item_a().id;
     })
 }
 
 pub fn mock_purchase_order_lines() -> Vec<PurchaseOrderLineRow> {
-    vec![mock_purchase_order_line_a(), mock_purchase_order_line_b()]
+    vec![
+        mock_purchase_order_a_line_1(),
+        mock_purchase_order_a_line_2(),
+        mock_purchase_order_b_line_1(),
+    ]
 }
