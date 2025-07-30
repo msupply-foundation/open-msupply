f627f1176e9fc8b6b6925af08a9d814270fe4c0d
roxy-dao <roxy@msupply.foundation>
2025-07-30 12:03:18 +1200

    Default for service lines, set prescribed quantity and generating from master list / Internal Order

--- server/service/src/invoice_line/inbound_shipment_from_internal_order_lines/generate.rs
+++ server/service/src/invoice_line/inbound_shipment_from_internal_order_lines/generate.rs
@@ -92,5 +92,6 @@ fn generate_line(
         reason_option_id: None,
         campaign_id: None,
         shipped_number_of_packs: None,
+        volume_per_pack: 0.0,
     }
 }
--- server/service/src/invoice_line/stock_out_line/set_prescribed_quantity/generate.rs
+++ server/service/src/invoice_line/stock_out_line/set_prescribed_quantity/generate.rs
@@ -41,6 +41,7 @@ pub fn generate(
         reason_option_id: None,
         campaign_id: None,
         shipped_number_of_packs: None,
+        volume_per_pack: 0.0,
     };
 
     Ok(invoice_line)
