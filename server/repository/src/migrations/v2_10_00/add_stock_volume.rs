8bab7916b0cf20d96f8b66583bc0a175416aa761
roxy-dao <roxy@msupply.foundation>
2025-07-31 09:02:45 +1200

    Add tests for partial updates from sync buffer

--- server/repository/src/migrations/v2_10_00/add_stock_volume.rs
+++ server/repository/src/migrations/v2_10_00/add_stock_volume.rs
@@ -123,3 +123,150 @@ impl MigrationFragment for Migrate {
         Ok(())
     }
 }
+
+#[cfg(test)]
+#[actix_rt::test]
+async fn migration_stock_volume() {
+    use crate::migrations::*;
+    use crate::test_db::*;
+    use diesel::{sql_query, sql_types::Timestamp, RunQueryDsl};
+    use util::*;
+
+    let previous_version = v2_09_01::V2_09_01.version();
+    let version = v2_10_00::V2_10_00.version();
+    let SetupResult { connection, .. } = setup_test(SetupOption {
+        db_name: &format!("migration_{version}_stock_volume"),
+        version: Some(previous_version.clone()),
+        ..Default::default()
+    })
+    .await;
+
+    sql!(
+        &connection,
+        r#"
+        INSERT INTO unit (id, name, "index", is_active) VALUES ('unit_id', 'Unit', 1, true);
+        INSERT INTO item (id, name, code, default_pack_size, type, is_active, legacy_record) VALUES ('item_id', 'Item', 'ITEM', 1, 'STOCK', true, 'het is leuk');
+        INSERT INTO item_link (id, item_id) VALUES ('item_link_id', 'item_id');
+        INSERT INTO name (id, type, is_customer, is_supplier, code, name) VALUES ('name_id', 'STORE', false, false, '', '');
+        INSERT INTO name_link (id, name_id) VALUES ('name_link_id', 'name_id');
+        INSERT INTO store (id, name_link_id, site_id, code) VALUES
+        ('store_id', 'name_link_id', 1, '');
+
+        INSERT INTO stock_line (id, item_link_id, store_id, pack_size, total_number_of_packs, available_number_of_packs, cost_price_per_pack, sell_price_per_pack, on_hold) VALUES
+        ('stock_line_id', 'item_link_id', 'store_id', 1, 10.0, 5.0, 0.0, 0.0, false);
+        INSERT INTO invoice (id, name_link_id, store_id, invoice_number, type, status, on_hold, created_datetime) VALUES
+        ('invoice_id', 'name_link_id', 'store_id', 1, 'INBOUND_SHIPMENT', 'NEW', false, '2023-01-01 00:00:00');
+        INSERT INTO invoice_line (id, invoice_id, item_link_id, pack_size, number_of_packs, item_name, item_code, sell_price_per_pack, total_before_tax, total_after_tax, cost_price_per_pack, type) VALUES
+        ('invoice_line_id', 'invoice_id', 'item_link_id', 1, 2.0, 'Item', 'ITEM', 0.0, 0.0, 0.0, 0.0, 'STOCK_IN');
+        INSERT INTO stocktake (id, store_id, user_id, stocktake_number, status, created_datetime, is_locked) VALUES
+        ('stocktake_id', 'store_id', 'user_id', 1, 'NEW', '2023-01-01 00:00:00', false);
+        INSERT INTO stocktake_line (id, stocktake_id, item_link_id, pack_size, counted_number_of_packs, item_name, snapshot_number_of_packs) VALUES
+        ('stocktake_line_id', 'stocktake_id', 'item_link_id', 1, 3.0, 'Item', 5.0);
+    "#
+    )
+    .unwrap();
+
+    let stock_line_sync_buffer_data = r#"{
+        "ID": "stock_line_id",
+        "item_ID": "item_id",
+        "name_ID": "name_id",
+        "pack_size": 1,
+        "available": 5.0,
+        "quantity": 10.0,
+        "store_ID": "store_id",
+        "total_volume": 100.0,
+        "volume_per_pack": 50.0
+    }"#;
+
+    let invoice_line_sync_buffer_data = r#"{
+        "ID": "invoice_line_id",
+        "transaction_ID": "invoice_id",
+        "item_ID": "item_id",
+        "name_ID": "name_id",
+        "pack_size": 1,
+        "quantity": 2.0,
+        "volume_per_pack": 50.0
+    }"#;
+
+    let stocktake_line_sync_buffer_data = r#"{
+        "ID": "stocktake_line_id",
+        "stock_take_ID": "stocktake_id",
+        "item_ID": "item_id",
+        "name_ID": "name_id",
+        "pack_size": 1,
+        "stock_take_qty": 3.0,
+        "volume_per_pack": 50.0
+    }"#;
+
+    execute_sql_with_error(
+        &connection,
+        sql_query(format!(
+            r#"
+            INSERT INTO sync_buffer
+            (record_id, received_datetime, table_name, action, data)
+            VALUES
+            ('stock_line_id', $1, 'item_line', 'UPSERT', '{stock_line_sync_buffer_data}');
+        "#
+        ))
+        .bind::<Timestamp, _>(Defaults::naive_date_time()),
+    )
+    .unwrap();
+
+    execute_sql_with_error(
+        &connection,
+        sql_query(format!(
+            r#"
+            INSERT INTO sync_buffer
+            (record_id, received_datetime, table_name, action, data)
+            VALUES
+            ('invoice_line_id', $1, 'trans_line', 'UPSERT', '{invoice_line_sync_buffer_data}');
+        "#
+        ))
+        .bind::<Timestamp, _>(Defaults::naive_date_time()),
+    )
+    .unwrap();
+
+    execute_sql_with_error(
+        &connection,
+        sql_query(format!(
+            r#"
+            INSERT INTO sync_buffer
+            (record_id, received_datetime, table_name, action, data)
+            VALUES
+            ('stocktake_line_id', $1, 'Stock_take_lines', 'UPSERT', '{stocktake_line_sync_buffer_data}');
+        "#
+        ))
+        .bind::<Timestamp, _>(Defaults::naive_date_time()),
+    )
+    .unwrap();
+
+    migrate(&connection, Some(version.clone())).unwrap();
+
+    let stock_lines = stock_line::table
+        .select((
+            stock_line::id,
+            stock_line::total_volume,
+            stock_line::volume_per_pack,
+        ))
+        .load::<(String, f64, f64)>(connection.lock().connection())
+        .unwrap();
+    assert_eq!(
+        stock_lines,
+        vec![("stock_line_id".to_string(), 100.0, 50.0)]
+    );
+
+    let invoice_lines = invoice_line::table
+        .select((invoice_line::id, invoice_line::volume_per_pack))
+        .load::<(String, f64)>(connection.lock().connection())
+        .unwrap();
+    assert_eq!(invoice_lines, vec![("invoice_line_id".to_string(), 50.0)]);
+
+    let stocktake_lines = stocktake_line::table
+        .select((stocktake_line::id, stocktake_line::volume_per_pack))
+        .load::<(String, f64)>(connection.lock().connection())
+        .unwrap();
+    assert_eq!(
+        stocktake_lines,
+        vec![("stocktake_line_id".to_string(), 50.0)]
+    );
+}
