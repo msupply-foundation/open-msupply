4883cff1176c113e4457026b764ad49ddcf15c2a
roxy-dao <roxy@msupply.foundation>
2025-07-31 16:34:43 +1200

    Update existing shipped num packs through sync buffer

--- server/repository/src/migrations/v2_09_01/add_invoice_line_shipped_pack_size.rs
+++ server/repository/src/migrations/v2_09_01/add_invoice_line_shipped_pack_size.rs
@@ -1,6 +1,25 @@
-use crate::migrations::*;
+use crate::migrations::{
+    templates::add_data_from_sync_buffer::{sync_buffer, SyncAction},
+    *,
+};
+use anyhow::Context;
+use diesel::prelude::*;
+use serde::Deserialize;
 
 pub(crate) struct Migrate;
+
+table! {
+    invoice_line (id) {
+        id -> Text,
+        shipped_pack_size -> Nullable<Double>,
+    }
+}
+
+#[derive(Deserialize)]
+pub struct LegacyTransLineRow {
+    #[serde(rename = "sent_pack_size")]
+    pub shipped_pack_size: Option<f64>,
+}
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
@@ -14,6 +33,25 @@ impl MigrationFragment for Migrate {
                 ALTER TABLE invoice_line ADD COLUMN shipped_pack_size {DOUBLE};
             "#
         )?;
+
+        let sync_buffer_rows = sync_buffer::table
+            .select((sync_buffer::record_id, sync_buffer::data))
+            .filter(
+                sync_buffer::action
+                    .eq(SyncAction::Upsert)
+                    .and(sync_buffer::table_name.eq("trans_line")),
+            )
+            .load::<(String, String)>(connection.lock().connection())?;
+
+        for (id, data) in sync_buffer_rows {
+            let legacy_row = serde_json::from_str::<LegacyTransLineRow>(&data)
+                .with_context(|| format!("Cannot parse sync buffer row data: {data}"))?;
+
+            diesel::update(invoice_line::table)
+                .filter(invoice_line::id.eq(id))
+                .set(invoice_line::shipped_pack_size.eq(legacy_row.shipped_pack_size))
+                .execute(connection.lock().connection())?;
+        }
 
         Ok(())
     }
