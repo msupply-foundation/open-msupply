92247a1f07308c14fb85690576f900c09757476c
Fergus Roache <fergus@msupply.foundation>
2025-08-01 12:42:34 +1200

    Remove batch mutation

--- server/graphql/batch_mutations/src/lib.rs
+++ server/graphql/batch_mutations/src/lib.rs
@@ -1,7 +1,6 @@
 mod batch_inbound_shipment;
 mod batch_outbound_shipment;
 mod batch_prescription;
-mod batch_purchase_order;
 mod batch_request_requisition;
 mod batch_response_requisition;
 mod batch_stocktake;
@@ -64,15 +63,6 @@ impl BatchMutations {
         input: batch_prescription::BatchInput,
     ) -> Result<batch_prescription::BatchResponse> {
         batch_prescription::batch(ctx, &store_id, input)
-    }
-
-    async fn batch_purchase_order(
-        &self,
-        ctx: &Context<'_>,
-        store_id: String,
-        input: batch_purchase_order::BatchInput,
-    ) -> Result<batch_purchase_order::BatchResponse> {
-        batch_purchase_order::batch(ctx, &store_id, input)
     }
 }
 
