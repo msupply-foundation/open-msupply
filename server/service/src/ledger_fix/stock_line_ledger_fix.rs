aa55cd7c17a932311431805a051be1d83488398a
Andrei <andrei.evguenov@gmail.com>
2025-08-03 09:26:26 +1200

    Use faster stock line ledger check
    Source: https://github.com/msupply-foundation/open-msupply/commit/43d9fc0b32e111d77eb826fcf5ef10563b0f5d0c

--- server/service/src/ledger_fix/stock_line_ledger_fix.rs
+++ server/service/src/ledger_fix/stock_line_ledger_fix.rs
@@ -1,8 +1,10 @@
-use crate::ledger_fix::find_ledger_discrepancies::{
-    find_stock_line_ledger_discrepancies, FindStockLineLedgerDiscrepanciesError,
-};
+use crate::ledger_fix::find_ledger_discrepancies::FindStockLineLedgerDiscrepanciesError;
 
-use repository::{RepositoryError, StorageConnection};
+use repository::{
+    stock_line_ledger::{StockLineLedgerFilter, StockLineLedgerRepository},
+    EqualFilter, InvoiceLineFilter, InvoiceLineRepository, InvoiceLineType, InvoiceStatus,
+    RepositoryError, StockLineRowRepository, StorageConnection,
+};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -42,9 +44,51 @@ pub(super) fn stock_line_ledger_fix(
 fn is_ledger_fixed(
     connection: &StorageConnection,
     stock_line_id: &str,
-) -> Result<bool, FindStockLineLedgerDiscrepanciesError> {
-    let discrepancies = find_stock_line_ledger_discrepancies(connection, Some(stock_line_id))?;
-    Ok(discrepancies.is_empty())
+) -> Result<bool, RepositoryError> {
+    let ledger_lines = StockLineLedgerRepository::new(connection).query_by_filter(
+        StockLineLedgerFilter::new().stock_line_id(EqualFilter::equal_to(stock_line_id)),
+    )?;
+
+    if ledger_lines.iter().any(|line| line.running_balance < 0.0) {
+        return Ok(false);
+    }
+
+    let reserved_not_picked = InvoiceLineRepository::new(connection).query_by_filter(
+        InvoiceLineFilter::new()
+            .stock_line_id(EqualFilter::equal_to(stock_line_id))
+            .r#type(InvoiceLineType::StockOut.equal_to())
+            .invoice_status(InvoiceStatus::equal_any(vec![
+                InvoiceStatus::Allocated,
+                InvoiceStatus::New,
+            ])),
+    )?;
+
+    let reserved_not_picked_summed = reserved_not_picked
+        .iter()
+        .map(|line| line.invoice_line_row.number_of_packs * line.invoice_line_row.pack_size)
+        .sum::<f64>();
+
+    // Unwrap is safe stock_line must exist at this pont
+    let stock_line = StockLineRowRepository::new(connection)
+        .find_one_by_id(stock_line_id)?
+        .unwrap();
+
+    let available = stock_line.available_number_of_packs * stock_line.pack_size;
+    let total = stock_line.total_number_of_packs * stock_line.pack_size;
+
+    if available > 0.0 || total > 0.0 && ledger_lines.is_empty() {
+        return Ok(false);
+    }
+
+    match ledger_lines.last() {
+        Some(last_line) => {
+            return Ok(available + reserved_not_picked_summed == total
+                && total == last_line.running_balance);
+        }
+        None => {
+            return Ok(available == 0.0 || total == 0.0);
+        }
+    }
 }
 
 fn fix_ledger_1(
