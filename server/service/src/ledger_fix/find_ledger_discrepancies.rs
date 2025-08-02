80daf5f9a56e2ff57ab6c6490632526abbc62ab9
Andrei <andrei.evguenov@gmail.com>
2025-08-03 11:14:41 +1200

    Adjust historic incoming invoices
    Source: https://github.com/msupply-foundation/open-msupply/commit/fdb5dad2a203b1b9b79828c251b5161340261f7d

--- server/service/src/ledger_fix/find_ledger_discrepancies.rs
+++ server/service/src/ledger_fix/find_ledger_discrepancies.rs
@@ -49,71 +49,14 @@ pub(super) fn find_stock_line_ledger_discrepancies(
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::test_helpers::{setup_all_with_data_and_service_provider, ServiceTestContext};
-    use chrono::Utc;
+    use crate::test_helpers::{
+        make_movements, setup_all_with_data_and_service_provider, ServiceTestContext,
+    };
     use repository::{
-        mock::{mock_item_a, mock_name_a, mock_store_a, mock_store_b, MockData, MockDataInserts},
-        InvoiceLineRow, InvoiceLineType, InvoiceRow, InvoiceStatus, InvoiceType,
-        KeyValueStoreRepository, StockLineRow,
+        mock::{mock_item_a, mock_store_a, mock_store_b, MockData, MockDataInserts},
+        InvoiceStatus, KeyValueStoreRepository, StockLineRow,
     };
     use util::inline_edit;
-
-    fn make_movements(stock_line: StockLineRow, date_quantity: Vec<(i64, i64)>) -> MockData {
-        let (invoices, invoice_lines) = date_quantity
-            .into_iter()
-            .map(|(date, quantity)| {
-                let invoice_id = format!("invoice_{}_{}_{}", stock_line.id, date, quantity);
-                let date = Utc::now().naive_utc() + chrono::Duration::days(date - 30);
-
-                (
-                    InvoiceRow {
-                        id: invoice_id.clone(),
-                        store_id: stock_line.store_id.clone(),
-                        name_link_id: mock_name_a().id.clone(),
-                        r#type: if quantity > 0 {
-                            InvoiceType::InboundShipment
-                        } else {
-                            InvoiceType::OutboundShipment
-                        },
-                        status: if quantity > 0 {
-                            InvoiceStatus::Verified
-                        } else {
-                            InvoiceStatus::Shipped
-                        },
-                        created_datetime: date,
-                        allocated_datetime: Some(date),
-                        picked_datetime: Some(date),
-                        shipped_datetime: Some(date),
-                        delivered_datetime: Some(date),
-                        received_datetime: Some(date),
-                        verified_datetime: Some(date),
-                        ..Default::default()
-                    },
-                    InvoiceLineRow {
-                        id: format!("line_{}", invoice_id),
-                        invoice_id,
-                        item_link_id: stock_line.item_link_id.clone(),
-                        stock_line_id: Some(stock_line.id.clone()),
-                        pack_size: stock_line.pack_size,
-                        number_of_packs: quantity.abs() as f64,
-                        r#type: if quantity > 0 {
-                            InvoiceLineType::StockIn
-                        } else {
-                            InvoiceLineType::StockOut
-                        },
-                        ..Default::default()
-                    },
-                )
-            })
-            .unzip();
-
-        MockData {
-            invoices,
-            invoice_lines,
-
-            ..Default::default()
-        }
-    }
 
     fn mock_data() -> MockData {
         let negative_balance = StockLineRow {
--- server/service/src/ledger_fix/stock_line_ledger_fix.rs
+++ server/service/src/ledger_fix/stock_line_ledger_fix.rs
@@ -1,26 +1,25 @@
-use crate::ledger_fix::find_ledger_discrepancies::FindStockLineLedgerDiscrepanciesError;
-
-use repository::{
-    stock_line_ledger::{StockLineLedgerFilter, StockLineLedgerRepository},
-    EqualFilter, InvoiceLineFilter, InvoiceLineRepository, InvoiceLineType, InvoiceStatus,
-    RepositoryError, StockLineRowRepository, StorageConnection,
-};
+use repository::{RepositoryError, StorageConnection};
 use thiserror::Error;
 
+use crate::ledger_fix::{
+    fixes::{adjust_historic_incoming_invoices, LedgerFixError},
+    is_ledger_fixed,
+};
+
 #[derive(Error, Debug)]
-enum StockLineLedgerFixError {
+pub(crate) enum StockLineLedgerFixError {
     #[error(transparent)]
     DatabaseError(#[from] RepositoryError),
     #[error(transparent)]
-    FindStockLineLedgerDiscrepanciesError(#[from] FindStockLineLedgerDiscrepanciesError),
+    LedgerFixError(#[from] LedgerFixError),
 }
 
 pub(super) fn stock_line_ledger_fix(
     connection: &StorageConnection,
     operation_log: &mut String,
     stock_line_id: &str,
-) -> Result</* fixed fully */ bool, FindStockLineLedgerDiscrepanciesError> {
-    fix_ledger_1(connection, operation_log, stock_line_id)?;
+) -> Result</* fixed fully */ bool, StockLineLedgerFixError> {
+    adjust_historic_incoming_invoices(connection, operation_log, stock_line_id)?;
 
     if is_ledger_fixed(connection, stock_line_id)? {
         return Ok(true);
@@ -41,70 +40,11 @@ pub(super) fn stock_line_ledger_fix(
     Ok(false)
 }
 
-fn is_ledger_fixed(
-    connection: &StorageConnection,
-    stock_line_id: &str,
-) -> Result<bool, RepositoryError> {
-    let ledger_lines = StockLineLedgerRepository::new(connection).query_by_filter(
-        StockLineLedgerFilter::new().stock_line_id(EqualFilter::equal_to(stock_line_id)),
-    )?;
-
-    if ledger_lines.iter().any(|line| line.running_balance < 0.0) {
-        return Ok(false);
-    }
-
-    let reserved_not_picked = InvoiceLineRepository::new(connection).query_by_filter(
-        InvoiceLineFilter::new()
-            .stock_line_id(EqualFilter::equal_to(stock_line_id))
-            .r#type(InvoiceLineType::StockOut.equal_to())
-            .invoice_status(InvoiceStatus::equal_any(vec![
-                InvoiceStatus::Allocated,
-                InvoiceStatus::New,
-            ])),
-    )?;
-
-    let reserved_not_picked_summed = reserved_not_picked
-        .iter()
-        .map(|line| line.invoice_line_row.number_of_packs * line.invoice_line_row.pack_size)
-        .sum::<f64>();
-
-    // Unwrap is safe stock_line must exist at this pont
-    let stock_line = StockLineRowRepository::new(connection)
-        .find_one_by_id(stock_line_id)?
-        .unwrap();
-
-    let available = stock_line.available_number_of_packs * stock_line.pack_size;
-    let total = stock_line.total_number_of_packs * stock_line.pack_size;
-
-    if available > 0.0 || total > 0.0 && ledger_lines.is_empty() {
-        return Ok(false);
-    }
-
-    match ledger_lines.last() {
-        Some(last_line) => {
-            return Ok(available + reserved_not_picked_summed == total
-                && total == last_line.running_balance);
-        }
-        None => {
-            return Ok(available == 0.0 || total == 0.0);
-        }
-    }
-}
-
-fn fix_ledger_1(
-    _connection: &StorageConnection,
-    operation_log: &mut String,
-    _stock_line_id: &str,
-) -> Result<(), RepositoryError> {
-    operation_log.push_str("Starting fix_ledger_1\n");
-    Ok(())
-}
-
 fn fix_ledger_2(
     _connection: &StorageConnection,
     operation_log: &mut String,
     _stock_line_id: &str,
-) -> Result<(), RepositoryError> {
+) -> Result<(), LedgerFixError> {
     operation_log.push_str("Starting fix_ledger_2\n");
     Ok(())
 }
@@ -113,7 +53,7 @@ fn fix_ledger_3(
     _connection: &StorageConnection,
     operation_log: &mut String,
     _stock_line_id: &str,
-) -> Result<(), RepositoryError> {
+) -> Result<(), LedgerFixError> {
     operation_log.push_str("Starting fix_ledger_3\n");
     Ok(())
 }
