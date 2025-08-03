6d1199d87de289fce325a7fcdbc16e0cd5c6d6f8
Andrei <andrei.evguenov@gmail.com>
2025-08-04 11:01:31 +1200

    8688 Find ledger discrepancies (#8699)
    * Create scheduler for ledger fix
    
    * Add datetime that failed to parse to error
    
    * Find ledger discrepancies

---
+++ server/repository/src/db_diesel/stock_line_ledger_discrepancy.rs
@@ -0,0 +1,73 @@
+use super::StorageConnection;
+
+use crate::{RepositoryError, StockLineFilter};
+
+use crate::{db_diesel::stock_line_row::stock_line, DBType, StockLineRepository};
+
+use diesel::dsl::IntoBoxed;
+use diesel::prelude::*;
+
+table! {
+    stock_line_ledger_discrepancy (stock_line_id) {
+        stock_line_id -> Text,
+    }
+}
+
+joinable!(stock_line_ledger_discrepancy -> stock_line (stock_line_id));
+allow_tables_to_appear_in_same_query!(stock_line, stock_line_ledger_discrepancy);
+#[derive(Clone, Queryable, Debug, PartialEq)]
+#[diesel(table_name = stock_line_ledger_discrepancy)]
+pub struct StockLineLedgerDiscrepancy {
+    pub id: String,
+}
+
+pub struct StockLineLedgerDiscrepancyRepository<'a> {
+    connection: &'a StorageConnection,
+}
+
+pub struct StockLineLedgerDiscrepancyFilter {
+    pub stock_line: Option<StockLineFilter>,
+}
+
+impl<'a> StockLineLedgerDiscrepancyRepository<'a> {
+    pub fn new(connection: &'a StorageConnection) -> Self {
+        StockLineLedgerDiscrepancyRepository { connection }
+    }
+
+    pub fn query(
+        &self,
+        filter: Option<StockLineLedgerDiscrepancyFilter>,
+    ) -> Result<Vec<StockLineLedgerDiscrepancy>, RepositoryError> {
+        let query = create_filtered_query(filter);
+
+        // Debug diesel query
+        // println!(
+        //     "{}",
+        //     diesel::debug_query::<DBType, _>(&query).to_string()
+        // );
+
+        let result =
+            query.load::<StockLineLedgerDiscrepancy>(self.connection.lock().connection())?;
+
+        Ok(result)
+    }
+}
+
+fn create_filtered_query(
+    filter: Option<StockLineLedgerDiscrepancyFilter>,
+) -> IntoBoxed<'static, stock_line_ledger_discrepancy::table, DBType> {
+    let mut query = stock_line_ledger_discrepancy::table.into_boxed();
+
+    let Some(StockLineLedgerDiscrepancyFilter { stock_line }) = filter else {
+        return query;
+    };
+
+    if let Some(stock_line_filter) = stock_line {
+        let stock_line_ids = StockLineRepository::create_filtered_query(Some(stock_line_filter))
+            .select(stock_line::id);
+
+        query = query.filter(stock_line_ledger_discrepancy::stock_line_id.eq_any(stock_line_ids));
+    };
+
+    query
+}
--- server/service/src/ledger_fix/mod.rs
+++ server/service/src/ledger_fix/mod.rs
@@ -1,1 +1,2 @@
+pub mod find_ledger_discrepancies;
 pub mod ledger_fix_driver;
