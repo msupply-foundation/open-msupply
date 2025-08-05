6612cffc545b9435392e097a4c261f703aa841e9
James Brunskill <james@sussol.net>
2025-08-05 16:04:03 +1200

    Add Goods Received To Nav

--- client/packages/purchase_orders/src/PurchaseOrderService.tsx
+++ client/packages/purchase_orders/src/PurchaseOrderService.tsx
@@ -4,6 +4,16 @@ import { AppRoute } from '@openmsupply-client/config';
 import { PurchaseOrderListView } from './ListView/ListView';
 import { PurchaseOrderDetailView } from './DetailView';
 
+// --- New Goods Received placeholder components ---
+const GoodsReceivedListView: FC = () => (
+  <div>Goods Received List View (to be implemented)</div>
+);
+
+const GoodsReceivedDetailView: FC = () => (
+  <div>Goods Received Detail View (to be implemented)</div>
+);
+// ------------------------------------------------
+
 const PurchaseOrderService: FC = () => {
   const purchaseOrdersRoute = RouteBuilder.create(
     AppRoute.PurchaseOrder
@@ -13,10 +23,25 @@ const PurchaseOrderService: FC = () => {
     .addPart(':purchaseOrderId')
     .build();
 
+  // New routes for Goods Received
+  const goodsReceivedRoute = RouteBuilder.create(
+    AppRoute.GoodsReceived
+  ).build();
+
+  const goodsReceivedDetailRoute = RouteBuilder.create(AppRoute.GoodsReceived)
+    .addPart(':goodsReceivedId')
+    .build();
+
   return (
     <Routes>
       <Route path={purchaseOrdersRoute} element={<PurchaseOrderListView />} />
       <Route path={purchaseOrderRoute} element={<PurchaseOrderDetailView />} />
+      {/* New Goods Received routes */}
+      <Route path={goodsReceivedRoute} element={<GoodsReceivedListView />} />
+      <Route
+        path={goodsReceivedDetailRoute}
+        element={<GoodsReceivedDetailView />}
+      />
     </Routes>
   );
 };
