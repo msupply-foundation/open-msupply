e064b3c0951e5325d080f8c5da74a55cc2bd69bc
Fergus Roache <fergus@msupply.foundation>
2025-08-07 09:07:35 +1200

    Memoize styling

--- client/packages/common/src/ui/components/panels/ImportPanel.tsx
+++ client/packages/common/src/ui/components/panels/ImportPanel.tsx
@@ -1,17 +1,6 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, useMemo } from 'react';
 
 import { styled, TabPanel, Box } from '@openmsupply-client/common';
-
-const StyledTabPanel = styled(TabPanel)({
-  height: '100%',
-  width: '100%',
-});
-
-const StyledTabContainer = styled(Box)(({ theme }) => ({
-  borderColor: theme.palette.divider,
-  flexDirection: 'column',
-  display: 'flex',
-}));
 
 export interface ImportPanel {
   tab: string;
@@ -20,8 +9,28 @@ export interface ImportPanel {
 export const ImportPanel: FC<PropsWithChildren<ImportPanel>> = ({
   tab,
   children,
-}) => (
-  <StyledTabPanel value={tab}>
-    <StyledTabContainer>{children}</StyledTabContainer>
-  </StyledTabPanel>
-);
+}) => {
+  const StyledTabPanel = useMemo(
+    () =>
+      styled(TabPanel)({
+        height: '100%',
+        width: '100%',
+      }),
+    []
+  );
+
+  const StyledTabContainer = useMemo(
+    () =>
+      styled(Box)(({ theme }) => ({
+        borderColor: theme.palette.divider,
+        flexDirection: 'column',
+        display: 'flex',
+      })),
+    []
+  );
+  return (
+    <StyledTabPanel value={tab}>
+      <StyledTabContainer>{children}</StyledTabContainer>
+    </StyledTabPanel>
+  );
+};
