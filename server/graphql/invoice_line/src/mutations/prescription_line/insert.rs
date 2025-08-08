bd4a8bbad3168fc2ae008e2f06e0e8f65839f623
roxy-dao <roxy@msupply.foundation>
2025-08-08 14:02:41 +1200

    Missed
    Source: https://github.com/msupply-foundation/open-msupply/commit/fcc4367ffa0466ad3b61da4a597558a5c0ab3317

--- server/graphql/invoice_line/src/mutations/prescription_line/insert.rs
+++ server/graphql/invoice_line/src/mutations/prescription_line/insert.rs
@@ -173,6 +173,8 @@ impl InsertInput {
             cost_price_per_pack: None,
             sell_price_per_pack: None,
             campaign_id: None,
+            item_variant_id: None,
+            donor_id: None,
         }
     }
 }
