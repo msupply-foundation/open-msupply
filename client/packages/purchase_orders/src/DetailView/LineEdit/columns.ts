5411095d3d2d2ba9b986921068158ae3799ba4cc
Fergus Roache <fergus@msupply.foundation>
2025-08-08 13:05:20 +1200

    Linting fixes

--- client/packages/purchase_orders/src/DetailView/LineEdit/columns.ts
+++ client/packages/purchase_orders/src/DetailView/LineEdit/columns.ts
@@ -1,53 +1,74 @@
-import { useMemo } from "react";
-import { DraftPurchaseOrderLine } from "../../api/hooks/usePurchaseOrderLine";
-import { ColumnDescription,  DateInputCell,    DateUtils,    Formatter,    NumberInputCell, useColumns } from "@openmsupply-client/common/src";
+import { useMemo } from 'react';
+import { DraftPurchaseOrderLine } from '../../api/hooks/usePurchaseOrderLine';
+import {
+  ColumnDescription,
+  DateInputCell,
+  DateUtils,
+  Formatter,
+  NumberInputCell,
+  useColumns,
+} from '@openmsupply-client/common/src';
 
 export const usePurchaseOrderLineEditColumns = ({
-    draft,
-    updatePatch,
+  draft,
+  updatePatch,
 }: {
-    draft?: DraftPurchaseOrderLine | null;
-    updatePatch: (patch: Partial<DraftPurchaseOrderLine>) => void;
+  draft?: DraftPurchaseOrderLine | null;
+  updatePatch: (patch: Partial<DraftPurchaseOrderLine>) => void;
 }) => {
-    const columnDefinitions: ColumnDescription<DraftPurchaseOrderLine>[] = useMemo(
-        () => [
-            {
-                Cell: NumberInputCell,
-                key: 'requestedPackSize',
-                label: 'label.pack-size',
-                setter: patch => {
-                    updatePatch({ ...patch });
-                },
-            },
-            {   
-                Cell: NumberInputCell,
-                key: 'requestedNumberOfUnits',
-                label: 'label.requested-quantity',
-                setter: patch => {
-                    updatePatch({ ...patch });
-                },
-            },
-            {
-                Cell: DateInputCell,
-                key: 'requestedDeliveryDate',
-                label: 'Requested delivery date',
-                setter: ({id, requestedDeliveryDate}) => {
-                    updatePatch({ id, requestedDeliveryDate: Formatter.naiveDate(DateUtils.getNaiveDate(requestedDeliveryDate)) });
-                },
-            },
-            {   
-                Cell: DateInputCell,
-                key: 'expectedDeliveryDate',
-                label: 'Expected Delivery Date',
-                setter: ({id, expectedDeliveryDate}) => {
-                    updatePatch({ id, expectedDeliveryDate: Formatter.naiveDate(DateUtils.getNaiveDate(expectedDeliveryDate)) });
-                },
-            },
-        ],
-        [updatePatch]
+  const columnDefinitions: ColumnDescription<DraftPurchaseOrderLine>[] =
+    useMemo(
+      () => [
+        {
+          Cell: NumberInputCell,
+          key: 'requestedPackSize',
+          label: 'label.pack-size',
+          setter: patch => {
+            updatePatch({ ...patch });
+          },
+        },
+        {
+          Cell: NumberInputCell,
+          key: 'requestedNumberOfUnits',
+          label: 'label.requested-quantity',
+          setter: patch => {
+            updatePatch({ ...patch });
+          },
+        },
+        {
+          Cell: DateInputCell,
+          key: 'requestedDeliveryDate',
+          label: 'Requested delivery date',
+          setter: ({ id, requestedDeliveryDate }) => {
+            updatePatch({
+              id,
+              requestedDeliveryDate: Formatter.naiveDate(
+                DateUtils.getNaiveDate(requestedDeliveryDate)
+              ),
+            });
+          },
+        },
+        {
+          Cell: DateInputCell,
+          key: 'expectedDeliveryDate',
+          label: 'Expected Delivery Date',
+          setter: ({ id, expectedDeliveryDate }) => {
+            updatePatch({
+              id,
+              expectedDeliveryDate: Formatter.naiveDate(
+                DateUtils.getNaiveDate(expectedDeliveryDate)
+              ),
+            });
+          },
+        },
+      ],
+      [updatePatch]
     );
 
-    const columns = useColumns<DraftPurchaseOrderLine>(columnDefinitions, {}, [updatePatch, draft]);
-    
-    return columns;
-}
+  const columns = useColumns<DraftPurchaseOrderLine>(columnDefinitions, {}, [
+    updatePatch,
+    draft,
+  ]);
+
+  return columns;
+};
