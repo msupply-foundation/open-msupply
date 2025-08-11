41819f19b6709d5b32be417c523316252c78cb51
Mark Prins <9192912+mark-prins@users.noreply.github.com>
2025-08-11 13:36:58 +1200

    Rewrite test url in connection checker (msupply-foundation/open-msupply#8762)
    * rewrite url
    
    * fix login fail
    
    Source: https://github.com/msupply-foundation/open-msupply/commit/27d629ce94469d54bdc8ce8b007d55fcce4e6a94

--- server/cli/src/test_connection.rs
+++ server/cli/src/test_connection.rs
@@ -226,7 +226,7 @@ impl Test for LoginTest {
             username,
             password,
             login_type: LoginUserTypeV4::User,
-            site_name: None,
+            site_name: Some("null".to_string()),
         };
 
         let _info = login_api
@@ -512,7 +512,7 @@ impl TestState {
 }
 
 fn get_url(config: &service::settings::Settings) -> Result<Url> {
-    let address = config.server.address();
+    let address = config.server.address().replace("0.0.0.0", "localhost");
     let scheme = match config.server.danger_allow_http | is_develop() {
         true => "http",
         false => "https",
