ef96cdd4e67f39e00ac71c320b82915cba60272b
James Brunskill <james@sussol.net>
2025-08-11 17:12:21 +1200

    Create button frontend
    Source: https://github.com/msupply-foundation/open-msupply/commit/db4b765999605a394c15a4559503c2771256c6c7

--- client/packages/purchasing/src/goods_received/ListView/AppBarButtons.tsx
+++ client/packages/purchasing/src/goods_received/ListView/AppBarButtons.tsx
@@ -3,23 +3,49 @@ import {
   AppBarButtonsPortal,
   ButtonWithIcon,
   DownloadIcon,
+  PlusCircleIcon,
+  Grid,
   useTranslation,
+  useToggle,
 } from '@openmsupply-client/common';
+import { PurchaseOrderSearchModal } from '../../purchase_order/Components';
+import { PurchaseOrderRowFragment } from '../../purchase_order/api';
 
 export const AppBarButtons: React.FC = () => {
   const t = useTranslation();
-  // Placeholder for actions
+  const modalController = useToggle();
+
   const handleExport = () => {
     // eslint-disable-next-line
     console.log('TO-DO: Export goods received...');
   };
+
+  const handlePurchaseOrderSelected = (selected: PurchaseOrderRowFragment) => {
+    // TODO: Create goods received from purchase order
+    // eslint-disable-next-line no-console
+    console.log('Selected purchase order:', selected);
+    modalController.toggleOff();
+  };
+
   return (
     <AppBarButtonsPortal>
-      <ButtonWithIcon
-        Icon={<DownloadIcon />}
-        label={t('button.export')}
-        onClick={handleExport}
-      />
+      <Grid container gap={1}>
+        <ButtonWithIcon
+          Icon={<PlusCircleIcon />}
+          label={t('button.new-goods-received')}
+          onClick={modalController.toggleOn}
+        />
+        <ButtonWithIcon
+          Icon={<DownloadIcon />}
+          label={t('button.export')}
+          onClick={handleExport}
+        />
+        <PurchaseOrderSearchModal
+          open={modalController.isOn}
+          onClose={modalController.toggleOff}
+          onChange={handlePurchaseOrderSelected}
+        />
+      </Grid>
     </AppBarButtonsPortal>
   );
 };
