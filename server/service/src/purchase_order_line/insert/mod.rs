230387b8a83c89e6809446a10cd09a5fe2044087
roxy-dao <roxy@msupply.foundation>
2025-08-14 15:23:02 +1200

    Return item in validation method
    Source: https://github.com/msupply-foundation/open-msupply/commit/77605dec898bfe2097b720fb34b8adf49a94ff02

--- server/service/src/purchase_order_line/insert/mod.rs
+++ server/service/src/purchase_order_line/insert/mod.rs
@@ -1,14 +1,14 @@
 use crate::{activity_log::activity_log_entry, service_provider::ServiceContext};
 use chrono::NaiveDate;
 use repository::{
-    ActivityLogType, ItemRowRepository, PurchaseOrderLineRow, PurchaseOrderLineRowRepository,
-    RepositoryError, TransactionError,
+    ActivityLogType, PurchaseOrderLineRow, PurchaseOrderLineRowRepository, RepositoryError,
+    TransactionError,
 };
+use util::uuid;
 
 mod generate;
-use generate::{generate, generate_from_csv, GenerateFromCSVInput};
+use generate::{generate, generate_from_csv};
 mod validate;
-use util::uuid;
 use validate::validate;
 mod test;
 
@@ -51,24 +51,17 @@ pub fn insert_purchase_order_line(
             let validate_input = validate::ValidateInput {
                 id: input.id.clone(),
                 purchase_order_id: input.purchase_order_id.clone(),
-                item_id: input.item_id.clone(),
+                item_id: Some(input.item_id.clone()),
+                item_code: None,
                 // TODO amend default value if we extend standard insert line input
                 requested_pack_size: input.requested_pack_size.unwrap_or_default(), // Default value
             };
             validate(&ctx.store_id.clone(), &validate_input, connection)?;
-
-            activity_log_entry(
-                &ctx,
-                ActivityLogType::PurchaseOrderLineCreated,
-                Some(input.purchase_order_id.clone()),
-                None,
-                None,
-            )?;
-
             let new_purchase_order_line =
                 generate(connection, &ctx.store_id.clone(), input.clone())?;
+
             activity_log_entry(
-                &ctx,
+                ctx,
                 ActivityLogType::PurchaseOrderLineCreated,
                 Some(new_purchase_order_line.purchase_order_id.clone()),
                 None,
@@ -105,34 +98,26 @@ pub fn insert_purchase_order_line_from_csv(
         .connection
         .transaction_sync(|connection| {
             // first validate that we can find the item
-            let item_option =
-                ItemRowRepository::new(connection).find_one_by_code(&input.item_code)?;
-
-            let item = match item_option {
-                Some(item) => item,
-                None => {
-                    return Err(InsertPurchaseOrderLineError::CannotFindItemByCode(
-                        input.item_code.clone(),
-                    ));
-                }
-            };
 
             let id = uuid::uuid();
             let validate_input = validate::ValidateInput {
                 id: id.clone(),
                 purchase_order_id: input.purchase_order_id.clone(),
-                item_id: item.id.clone(),
+                item_id: None,
+                item_code: Some(input.item_code.clone()),
                 requested_pack_size: input.requested_pack_size.unwrap_or(0.0), // Default value which can be edited in UI
             };
 
-            validate(&ctx.store_id.clone(), &validate_input, connection)?;
+            let item = validate(&ctx.store_id.clone(), &validate_input, connection)?;
 
-            let generate_input = GenerateFromCSVInput {
+            let generate_input = InsertPurchaseOrderLineInput {
                 id,
                 purchase_order_id: input.purchase_order_id,
                 item_id: item.id,
                 requested_pack_size: input.requested_pack_size,
                 requested_number_of_units: input.requested_number_of_units,
+                requested_delivery_date: None,
+                expected_delivery_date: None,
             };
 
             let new_purchase_order_line =
