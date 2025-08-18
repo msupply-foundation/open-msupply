b3372356d792f46884239d29f0b4ac9a6a033d1e
John Geronimo <johnageronimo1@gmail.com>
2025-08-19 09:55:00 +1200

    fix draft create

--- client/packages/purchasing/src/goods_received/DetailView/LineEdit/utils.ts
+++ client/packages/purchasing/src/goods_received/DetailView/LineEdit/utils.ts
@@ -11,6 +11,18 @@ export const createDraftGoodsReceivedLine = (
     id: FnUtils.generateUUID(),
     goodsReceivedId,
     purchaseOrderLineId,
+    item: {
+      __typename: 'ItemNode',
+      id: item.id,
+      name: item.name,
+    },
+    batch: '',
+    comment: '',
+    lineNumber: 0,
+    expiryDate: null,
+    manufacturerLinkId: '',
+    numberOfPacksReceived: 0,
+    receivedPackSize: 0,
     itemId: item.id,
   };
 };
