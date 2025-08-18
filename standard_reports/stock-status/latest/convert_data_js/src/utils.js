6cd3d7c4955e078b4b00de5777eee030bf0dcdd8
James Brunskill <james@sussol.net>
2025-08-18 14:50:54 +1200

    Update stock-status report to handle blank item code

--- standard_reports/stock-status/latest/convert_data_js/src/utils.js
+++ standard_reports/stock-status/latest/convert_data_js/src/utils.js
@@ -1,9 +1,22 @@
-import { cleanUpNodes, sortNodes } from '../../../../utils';
+import { cleanUpNodes, sortNodes } from "../../../../utils";
 
 const processItemLines = (nodes, sort, dir) => {
   let cleanNodes = cleanUpNodes(nodes);
+  let removeBlankCodes = removeItemsWithBlankCodes(cleanNodes);
   let sortedNodes = sortNodes(cleanNodes, sort, dir);
   return sortedNodes;
+};
+
+const removeItemsWithBlankCodes = (itemNodes) => {
+  let cleanNodes = [];
+
+  itemNodes.forEach((node) => {
+    // If we have any items without a valid code, we'll exclude them from the list
+    if (Object.keys(node).length != 0 && node.code) {
+      cleanNodes.push(cleanUpObject(node));
+    }
+  });
+  return cleanNodes;
 };
 
 export { processItemLines };
