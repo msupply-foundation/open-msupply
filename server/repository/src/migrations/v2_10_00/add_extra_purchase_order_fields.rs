4a1586ead6b509dbc2399452a42f5a849cf3c1af
roxy-dao <roxy@msupply.foundation>
2025-08-19 15:21:23 +1200

    rename migration
    Source: https://github.com/msupply-foundation/open-msupply/commit/e25bd09a68be5b242cc7e71671c0d15cb696dc5a

--- server/repository/src/migrations/v2_10_00/add_extra_purchase_order_fields.rs
+++ server/repository/src/migrations/v2_10_00/add_extra_purchase_order_fields.rs
@@ -4,14 +4,14 @@ pub(crate) struct Migrate;
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
-        "add_purchase_order_line_manufacturer_and_notes"
+        "add_extra_purchase_order_fields"
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
         sql!(
             connection,
             r#"
-            ALTER TABLE purchase_order_line ADD COLUMN manufacturer_link_id REFERENCES name_link(id);
+            ALTER TABLE purchase_order_line ADD COLUMN manufacturer_link_id TEXT REFERENCES name_link(id);
             ALTER TABLE purchase_order_line ADD COLUMN note TEXT;
             ALTER TABLE purchase_order_line ADD COLUMN unit_of_packs TEXT;
             "#
