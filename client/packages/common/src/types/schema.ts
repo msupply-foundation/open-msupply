a6ab76678d8407825c14393dcf0c739f3ab98d95
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-08-21 15:29:05 +1200

    apply nullable update for expiry dates to frontend
    Source: https://github.com/msupply-foundation/open-msupply/commit/c9afb72e8ab03718334e5f4ff3ffd602d473c8ab

--- client/packages/common/src/types/schema.ts
+++ client/packages/common/src/types/schema.ts
@@ -9976,7 +9976,7 @@ export type UpdateInboundShipmentLineInput = {
   campaignId?: InputMaybe<NullableStringUpdate>;
   costPricePerPack?: InputMaybe<Scalars['Float']['input']>;
   donorId?: InputMaybe<NullableStringUpdate>;
-  expiryDate?: InputMaybe<Scalars['NaiveDate']['input']>;
+  expiryDate?: InputMaybe<NullableDateUpdate>;
   id: Scalars['String']['input'];
   itemId?: InputMaybe<Scalars['String']['input']>;
   itemVariantId?: InputMaybe<NullableStringUpdate>;
@@ -10690,7 +10690,7 @@ export type UpdateStocktakeLineInput = {
   costPricePerPack?: InputMaybe<Scalars['Float']['input']>;
   countedNumberOfPacks?: InputMaybe<Scalars['Float']['input']>;
   donorId?: InputMaybe<NullableStringUpdate>;
-  expiryDate?: InputMaybe<Scalars['NaiveDate']['input']>;
+  expiryDate?: InputMaybe<NullableDateUpdate>;
   id: Scalars['String']['input'];
   /** @deprecated Since 2.8.0. Use reason_option_id */
   inventoryAdjustmentReasonId?: InputMaybe<Scalars['String']['input']>;
