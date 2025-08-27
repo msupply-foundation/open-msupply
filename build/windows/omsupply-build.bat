19707a0034728e50c498a22375b8748e4c83fa97
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-08-28 09:28:02 +1200

    wip telegram notifications on jenkins build

--- build/windows/omsupply-build.bat
+++ build/windows/omsupply-build.bat
@@ -15,36 +15,47 @@ copy "version.txt" "omSupply\version.txt"
 
 @REM note: the /b must come first, otherwise the command is not waited
 start /b /wait build\windows\omsupply-prepare.bat
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=Client Preparation & goto :error)
 
 @cd server 
 
 @ECHO ##### Building omsupply service::sqlite #####
 cargo build --release --bin omsupply_service && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=SQLite Build & goto :error)
 
 @ECHO ##### Building sqlite omsupply server::sqlite #####
 cargo build --release && copy "target\release\remote_server.exe" "..\omSupply\Server\omSupply-server-sqlite.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=SQLite Server Build & goto :error)
 
 @ECHO ##### Building omsupply service::postgres #####
 cargo build --release --bin omsupply_service --features postgres && copy "target\release\omsupply_service.exe" "..\omSupply\Server\omSupply-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=Postgres Build & goto :error)
 
 @ECHO ##### Building omSupply cli #####
 cargo build --release --bin remote_server_cli && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-sqlite.exe"
 cargo build --release --bin remote_server_cli --features postgres && copy "target\release\remote_server_cli.exe" "..\omSupply\Server\omSupply-cli-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=CLI Build & goto :error)
 
 @ECHO ##### Building connection test utility #####
 cargo build --release --bin test_connection && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-sqlite.exe"
 cargo build --release --bin test_connection --features postgres && copy "target\release\test_connection.exe" "..\omSupply\Server\test-connection-postgres.exe"
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=Connection Test Build & goto :error)
 
 @cd..
 
 @REM start /b /wait build\windows\omsupply-android.bat
-@REM @if %errorlevel% neq 0 exit /b %errorlevel%
+@REM @if %errorlevel% neq 0 goto :error
 
 start /b /wait build\windows\omsupply-electron.bat
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (set ERROR_STEP=Electron Build & goto :error)
+
+@REM Success - pass "success" as argument
+python .github\scripts\send-telegram-notification.py success
+goto :end
+
+:error
+@REM Failure - pass "failure" as argument  
+python .github\scripts\send-telegram-notification.py failure "%ERROR_STEP%"
+exit /b %errorlevel%
+
+:end
