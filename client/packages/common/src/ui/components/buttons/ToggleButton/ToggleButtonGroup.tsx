aeb96ad4b49c97fb42d8a23cf4f0147080a7cbab
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-03 10:15:12 +1200

    add new toggle button group

---
+++ client/packages/common/src/ui/components/buttons/ToggleButton/ToggleButtonGroup.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import MuiToggleButton from '@mui/material/ToggleButton';
+import MuiToggleButtonGroup, {
+  ToggleButtonGroupProps as MuiProps,
+} from '@mui/material/ToggleButtonGroup';
+
+interface ToggleButtonGroupProps<T> {
+  value: T | null;
+  onChange: (value: T) => void;
+  options: { id: string; value: T; icon: React.ReactNode; label: string }[];
+  color?: MuiProps['color'];
+}
+
+export const ToggleButtonGroup = <T extends string>({
+  value,
+  onChange,
+  options,
+  color = 'secondary',
+}: ToggleButtonGroupProps<T>) => (
+  <MuiToggleButtonGroup
+    color={color}
+    value={value}
+    exclusive
+    onChange={(_, value) => onChange(value as T)}
+  >
+    {options.map(option => (
+      <MuiToggleButton
+        key={option.id}
+        value={option.value}
+        aria-label={option.label}
+      >
+        {option.icon}
+      </MuiToggleButton>
+    ))}
+  </MuiToggleButtonGroup>
+);
