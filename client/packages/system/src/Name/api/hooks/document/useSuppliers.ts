2c460065af4861757be5f67420024d99756d9191
John Geronimo <johnageronimo1@gmail.com>
2025-09-03 10:12:45 +1200

    refactor useSupplier hook to support returning external suppliers

--- client/packages/system/src/Name/api/hooks/document/useSuppliers.ts
+++ client/packages/system/src/Name/api/hooks/document/useSuppliers.ts
@@ -1,11 +1,14 @@
 import { useQuery, useQueryParamsStore } from '@openmsupply-client/common';
 import { useNameApi } from '../utils/useNameApi';
 
-export const useSuppliers = () => {
+export const useSuppliers = (external?: boolean) => {
   const api = useNameApi();
   const queryParams = useQueryParamsStore();
 
   return useQuery(api.keys.paramList(queryParams.paramList()), () =>
-    api.get.suppliers(queryParams.paramList())
+    api.get.suppliers({
+      ...queryParams.paramList(),
+      external,
+    })
   );
 };
