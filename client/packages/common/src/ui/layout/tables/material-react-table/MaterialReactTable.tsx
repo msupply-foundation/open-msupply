bb66050e7b5faa664a0d04e87c14f27c69b63d68
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-08 11:48:19 +1200

    fix table overflow

--- client/packages/common/src/ui/layout/tables/material-react-table/MaterialReactTable.tsx
+++
@@ -1,4 +0,0 @@
-import { MaterialReactTable } from 'material-react-table';
-
-// Re-exporting so all imports come from common folder
-export { MaterialReactTable as MaterialTable };
--- client/packages/common/src/ui/layout/tables/material-react-table/index.ts
+++ client/packages/common/src/ui/layout/tables/material-react-table/index.ts
@@ -1,4 +1,6 @@
-export * from './MaterialReactTable';
 export * from './usePaginatedMaterialTable';
 export * from './useNonPaginatedMaterialTable';
 export * from './useMaterialTableColumns';
+
+// Re-exporting so all imports come from common folder
+export { MaterialReactTable as MaterialTable } from 'material-react-table';
