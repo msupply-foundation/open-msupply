f723bdc081d9baccd8b06d49fcf692a3c5ff6de8
roxy-dao <roxy@msupply.foundation>
2025-09-09 17:09:53 +1200

    Fix tests

--- server/repository/src/mock/item_store_join.rs
+++ server/repository/src/mock/item_store_join.rs
@@ -1,5 +1,5 @@
 use crate::{
-    mock::{mock_item_a, mock_store_a},
+    mock::{mock_item_a, mock_store_a, mock_store_b},
     ItemStoreJoinRow,
 };
 
@@ -17,7 +17,7 @@ pub fn mock_item_query_test1_join_store_() -> ItemStoreJoinRow {
     ItemStoreJoinRow {
         id: "item_query_test1_store_a".to_string(),
         item_link_id: "item_query_test1".to_string(),
-        store_id: mock_store_a().id.clone(),
+        store_id: mock_store_b().id.clone(),
         default_sell_price_per_pack: 50.0,
         ignore_for_orders: false,
     }
