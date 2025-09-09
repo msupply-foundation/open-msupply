94684f20efaee98270946cfa3675bb67d3d823ab
roxy-dao <roxy@msupply.foundation>
2025-09-09 14:26:16 +1200

    Add store join mock for item query item1

--- server/repository/src/mock/item_store_join.rs
+++ server/repository/src/mock/item_store_join.rs
@@ -13,6 +13,19 @@ pub fn mock_item_a_join_store_a() -> ItemStoreJoinRow {
     }
 }
 
+pub fn mock_item_query_test1_join_store_() -> ItemStoreJoinRow {
+    ItemStoreJoinRow {
+        id: "item_query_test1_store_a".to_string(),
+        item_link_id: "item_query_test1".to_string(),
+        store_id: mock_store_a().id.clone(),
+        default_sell_price_per_pack: 50.0,
+        ignore_for_orders: false,
+    }
+}
+
 pub fn mock_item_store_joins() -> Vec<ItemStoreJoinRow> {
-    vec![mock_item_a_join_store_a()]
+    vec![
+        mock_item_a_join_store_a(),
+        mock_item_query_test1_join_store_(),
+    ]
 }
