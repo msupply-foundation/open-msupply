925263db8debd6869de151e652e784533267c86d
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-11 10:59:32 +1200

    move manual filters to own file
    Source: https://github.com/msupply-foundation/open-msupply/commit/8efc731568168aaa4b2157ac5180d768d5dfd0b2

---
+++ client/packages/common/src/ui/layout/tables/material-react-table/useManualTableFilters.ts
@@ -0,0 +1,78 @@
+/**
+ * Hook to provide "filterUpdater" functions, to correctly update the URL query
+ * based on the filter type; and "getFilterState" function, which converts the
+ * current URL query into the filter state required by MRT.
+ */
+import { useMemo } from 'react';
+import { MRT_RowData } from 'material-react-table';
+import {
+  DateUtils,
+  useFormatDateTime,
+  useUrlQuery,
+} from '@openmsupply-client/common';
+
+import { ColumnDef } from './types';
+
+export const useManualTableFilters = <T extends MRT_RowData>(
+  columns: ColumnDef<T>[]
+) => {
+  const { urlQuery, updateQuery } = useUrlQuery();
+  const { customDate, urlQueryDateTime } = useFormatDateTime();
+
+  const filterUpdaters = useMemo(() => {
+    const filterUpdaters: Record<string, (value: any) => void> = {};
+
+    columns.forEach(({ filterVariant, ...mrtProperties }) => {
+      const filterKey = (mrtProperties.accessorKey ||
+        mrtProperties.id) as string;
+
+      switch (filterVariant) {
+        case 'text':
+        case 'select':
+          filterUpdaters[filterKey] = (value: string) => {
+            updateQuery({ [filterKey]: value });
+          };
+          break;
+
+        case 'date-range':
+          filterUpdaters[filterKey] = ([date1, date2]: [
+            Date | '',
+            Date | '',
+          ]) => {
+            updateQuery({
+              [filterKey]: {
+                from: date1 ? customDate(date1, urlQueryDateTime) : '',
+                to: date2 ? customDate(date2, urlQueryDateTime) : '',
+              },
+            });
+          };
+          break;
+      }
+    });
+
+    return filterUpdaters;
+  }, [columns]);
+
+  const getFilterState = () => {
+    return Object.entries(urlQuery).map(([id, val]) => {
+      // Date range
+      if (typeof val === 'object' && ('to' in val || 'from' in val))
+        return {
+          id,
+          value: [
+            val.from ? DateUtils.getDateOrNull(val.from as string) : '',
+            val.to ? DateUtils.getDateOrNull(val.to as string) : '',
+          ],
+        };
+
+      // TO-DO: Implement filter state for other types
+
+      return {
+        id,
+        value: val,
+      };
+    });
+  };
+
+  return { filterUpdaters, getFilterState };
+};
