d3e63800b5dae369b1543c5f6250a3ea5941a693
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-12 09:40:44 +1200

    add colour setter to name column

--- client/packages/common/src/ui/layout/tables/columns/NameAndColorColumn.tsx
+++ client/packages/common/src/ui/layout/tables/columns/NameAndColorColumn.tsx
@@ -4,6 +4,9 @@ import { RecordWithId } from '@common/types';
 import { ColumnDefinition } from '../columns/types';
 import { ColorSelectButton } from '@common/components';
 import { Link } from 'react-router-dom';
+import { MRT_RowData } from 'material-react-table';
+import { ColumnDef } from '..';
+import { LocaleKey, TypedTFunction } from '@common/intl';
 
 interface RecordWithIdWithRequiredFields extends RecordWithId {
   colour?: string | null;
@@ -64,4 +67,43 @@ export const getNameAndColorColumn = <
     </Box>
   ),
   minWidth: 400,
+});
+
+export const getNameAndColorSetterColumn = <
+  T extends MRT_RowData & {
+    id: string;
+    otherPartyName: string;
+    colour?: string | null;
+  },
+>(
+  onColorChange: (patch: { id: string; colour: string }) => void,
+  getIsDisabled: (row: T) => boolean,
+  t: TypedTFunction<LocaleKey>
+): ColumnDef<T> => ({
+  id: 'otherPartyName',
+  header: t('label.name'),
+  accessorKey: 'otherPartyName',
+  size: 400,
+  Cell: ({ row }) => (
+    <Box
+      sx={{
+        flexDirection: 'row',
+        borderBottom: 'none',
+        alignItems: 'center',
+        display: 'flex',
+        width: '100%',
+      }}
+    >
+      <ColorSelectButton
+        disabled={getIsDisabled(row.original)}
+        onChange={color =>
+          onColorChange({ id: row.original.id, colour: color.hex })
+        }
+        color={row.original.colour}
+      />
+      <Box ml={1} />
+      {/* TODO: implement row click as link */}
+      <Box>{row.original.otherPartyName}</Box>
+    </Box>
+  ),
 });
