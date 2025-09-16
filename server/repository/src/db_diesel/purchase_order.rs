e232ead44377d75ada5da0f9291fd79bc8d06313
roxy-dao <roxy@msupply.foundation>
2025-09-17 09:56:43 +1200

    Add purchase order filter to purchase order lines
    Source: https://github.com/msupply-foundation/open-msupply/commit/786c287874b1e0a9e0253110d2993ae236fdd0c3

--- server/repository/src/db_diesel/purchase_order.rs
+++ server/repository/src/db_diesel/purchase_order.rs
@@ -56,7 +56,7 @@ impl<'a> PurchaseOrderRepository<'a> {
     }
 
     pub fn count(&self, filter: Option<PurchaseOrderFilter>) -> Result<i64, RepositoryError> {
-        let query = create_filtered_query(filter);
+        let query = Self::create_filtered_query(filter);
         Ok(query
             .count()
             .get_result(self.connection.lock().connection())?)
@@ -75,7 +75,7 @@ impl<'a> PurchaseOrderRepository<'a> {
         filter: Option<PurchaseOrderFilter>,
         sort: Option<PurchaseOrderSort>,
     ) -> Result<Vec<PurchaseOrder>, RepositoryError> {
-        let mut query = create_filtered_query(filter);
+        let mut query = Self::create_filtered_query(filter);
         if let Some(sort) = sort {
             match sort.key {
                 PurchaseOrderSortField::Number => {
@@ -105,6 +105,49 @@ impl<'a> PurchaseOrderRepository<'a> {
 
         Ok(result.into_iter().map(to_domain).collect())
     }
+
+    pub fn create_filtered_query(filter: Option<PurchaseOrderFilter>) -> BoxedPurchaseOrderQuery {
+        let mut query = purchase_order::table
+            .left_join(purchase_order_stats::table)
+            .into_boxed();
+
+        if let Some(f) = filter {
+            let PurchaseOrderFilter {
+                id,
+                store_id,
+                status,
+                supplier,
+                confirmed_datetime,
+                requested_delivery_date,
+                sent_datetime,
+            } = f;
+            apply_equal_filter!(query, id, purchase_order::id);
+            apply_equal_filter!(query, store_id, purchase_order::store_id);
+            apply_equal_filter!(query, status, purchase_order::status);
+            if let Some(supplier_string) = supplier {
+                let mut sub_query = name_link::table
+                    .inner_join(name::table)
+                    .select(name_link::id)
+                    .into_boxed();
+                apply_string_filter!(sub_query, Some(supplier_string), name::name_);
+
+                query = query.filter(purchase_order::supplier_name_link_id.eq_any(sub_query));
+            }
+            apply_date_time_filter!(
+                query,
+                confirmed_datetime,
+                purchase_order::confirmed_datetime
+            );
+            apply_date_filter!(
+                query,
+                requested_delivery_date,
+                purchase_order::requested_delivery_date
+            );
+            apply_date_time_filter!(query, sent_datetime, purchase_order::sent_datetime);
+        }
+
+        query
+    }
 }
 
 fn to_domain((purchase_order, purchase_order_stats): PurchaseOrderJoin) -> PurchaseOrder {
@@ -116,49 +159,6 @@ fn to_domain((purchase_order, purchase_order_stats): PurchaseOrderJoin) -> Purch
 
 type BoxedPurchaseOrderQuery =
     IntoBoxed<'static, LeftJoin<purchase_order::table, purchase_order_stats::table>, DBType>;
-
-fn create_filtered_query(filter: Option<PurchaseOrderFilter>) -> BoxedPurchaseOrderQuery {
-    let mut query = purchase_order::table
-        .left_join(purchase_order_stats::table)
-        .into_boxed();
-
-    if let Some(f) = filter {
-        let PurchaseOrderFilter {
-            id,
-            store_id,
-            status,
-            supplier,
-            confirmed_datetime,
-            requested_delivery_date,
-            sent_datetime,
-        } = f;
-        apply_equal_filter!(query, id, purchase_order::id);
-        apply_equal_filter!(query, store_id, purchase_order::store_id);
-        apply_equal_filter!(query, status, purchase_order::status);
-        if let Some(supplier_string) = supplier {
-            let mut sub_query = name_link::table
-                .inner_join(name::table)
-                .select(name_link::id)
-                .into_boxed();
-            apply_string_filter!(sub_query, Some(supplier_string), name::name_);
-
-            query = query.filter(purchase_order::supplier_name_link_id.eq_any(sub_query));
-        }
-        apply_date_time_filter!(
-            query,
-            confirmed_datetime,
-            purchase_order::confirmed_datetime
-        );
-        apply_date_filter!(
-            query,
-            requested_delivery_date,
-            purchase_order::requested_delivery_date
-        );
-        apply_date_time_filter!(query, sent_datetime, purchase_order::sent_datetime);
-    }
-
-    query
-}
 
 impl PurchaseOrderFilter {
     pub fn new() -> PurchaseOrderFilter {
@@ -192,5 +192,14 @@ impl PurchaseOrderFilter {
     pub fn sent_datetime(mut self, filter: DatetimeFilter) -> Self {
         self.sent_datetime = Some(filter);
         self
+    }
+}
+
+impl PurchaseOrderStatus {
+    pub fn equal_to(&self) -> EqualFilter<Self> {
+        EqualFilter {
+            equal_to: Some(self.clone()),
+            ..Default::default()
+        }
     }
 }
