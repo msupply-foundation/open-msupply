abb036261d98856ba7e9bff25b5de39d962a0488
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-09-16 15:34:21 +1200

    authorise statuses based on auth

--- server/service/src/purchase_order/validate.rs
+++ server/service/src/purchase_order/validate.rs
@@ -21,15 +21,28 @@ pub(crate) fn can_edit_requested_quantity(purchase_order: &PurchaseOrderRow) -> 
     }
 }
 
-// adjusted quantity is updated at all statuses except finalised
-// even though the user can only update adjusted at CONFIRMED and SENT statuses
-pub(crate) fn can_edit_adjusted_quantity(purchase_order: &PurchaseOrderRow) -> bool {
-    match purchase_order.status {
-        PurchaseOrderStatus::New
-        | PurchaseOrderStatus::RequestApproval
-        | PurchaseOrderStatus::Confirmed
-        | PurchaseOrderStatus::Sent => true,
-        PurchaseOrderStatus::Finalised => false,
+pub(crate) fn can_edit_adjusted_quantity(
+    purchase_order: &PurchaseOrderRow,
+    user_has_permission: bool,
+) -> bool {
+    if user_has_permission == true {
+        // User can only update ADJUSTED QUANTITY field at CONFIRMED and SENT statuses and requires permissions
+        match purchase_order.status {
+            PurchaseOrderStatus::New
+            | PurchaseOrderStatus::RequestApproval
+            | PurchaseOrderStatus::Confirmed
+            | PurchaseOrderStatus::Sent => true,
+            PurchaseOrderStatus::Finalised => false,
+        }
+    } else {
+        // Adjusted quantity is updated from REQUESTED QUANTITY in New and Request Approval statuses
+        // All users need to be able to update it in these statuses
+        match purchase_order.status {
+            PurchaseOrderStatus::New | PurchaseOrderStatus::RequestApproval => true,
+            PurchaseOrderStatus::Confirmed
+            | PurchaseOrderStatus::Sent
+            | PurchaseOrderStatus::Finalised => false,
+        }
     }
 }
 
