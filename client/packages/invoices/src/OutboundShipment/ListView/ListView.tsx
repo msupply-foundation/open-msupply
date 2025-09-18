f663dbd5aff261dea39c27f862bbce9fce52f84e
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-19 11:26:25 +1200

    remove old OS list view
    Source: https://github.com/msupply-foundation/open-msupply/commit/c509abf2f238f30ca141631cd3e3116b8ff3521d

--- client/packages/invoices/src/OutboundShipment/ListView/ListView.tsx
+++ client/packages/invoices/src/OutboundShipment/ListView/ListView.tsx
@@ -1,27 +1,17 @@
-import React, { FC, useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import {
   useNavigate,
-  DataTable,
-  useColumns,
-  getNameAndColorColumn,
-  TableProvider,
-  createTableStore,
   useTranslation,
   InvoiceNodeStatus,
-  useTableStore,
-  NothingHere,
   useToggle,
   useUrlQueryParams,
-  TooltipTextCell,
-  GenericColumnKey,
-  getCommentPopoverColumn,
+  TextWithTooltipCell,
   useSimplifiedTabletUI,
-  useFeatureFlags,
   MaterialTable,
   usePaginatedMaterialTable,
   ColumnDef,
-  getNameAndColorSetterColumn,
   ColumnType,
+  NameAndColorSetterCell,
 } from '@openmsupply-client/common';
 import { getStatusTranslator, isOutboundDisabled } from '../../utils';
 import { Toolbar } from './Toolbar';
@@ -30,23 +20,15 @@ import { useOutbound } from '../api';
 import { OutboundRowFragment } from '../api/operations.generated';
 import { Footer } from './Footer';
 
-const useDisableOutboundRows = (rows?: OutboundRowFragment[]) => {
-  const { setDisabledRows } = useTableStore();
-  useEffect(() => {
-    const disabledRows = rows?.filter(isOutboundDisabled).map(({ id }) => id);
-    if (disabledRows) setDisabledRows(disabledRows);
-  }, [rows, setDisabledRows]);
-};
-
-const OutboundShipmentListViewComponent: FC = () => {
-  const { tableUsabilityImprovements } = useFeatureFlags();
-  const { mutate: onUpdate } = useOutbound.document.update();
+export const OutboundShipmentListView = () => {
   const t = useTranslation();
+  const navigate = useNavigate();
+  const modalController = useToggle();
+  const simplifiedTabletView = useSimplifiedTabletUI();
+
   const {
-    updateSortQuery,
-    updatePaginationQuery,
     filter,
-    queryParams: { sortBy, page, first, offset },
+    queryParams: { sortBy, first, offset },
   } = useUrlQueryParams({
     initialSort: { key: 'invoiceNumber', dir: 'desc' },
     filters: [
@@ -58,75 +40,35 @@ const OutboundShipmentListViewComponent: FC = () => {
       { key: 'invoiceNumber', condition: 'equalTo', isNumber: true },
     ],
   });
-  const navigate = useNavigate();
-  const modalController = useToggle();
-  const pagination = { page, first, offset };
   const queryParams = { ...filter, sortBy, first, offset };
-  const simplifiedTabletView = useSimplifiedTabletUI();
 
-  const { data, isError, isLoading } = useOutbound.document.list(queryParams);
-  useDisableOutboundRows(data?.nodes);
-
-  const columns = useColumns<OutboundRowFragment>(
-    [
-      GenericColumnKey.Selection,
-      [
-        getNameAndColorColumn(),
-        { setter: onUpdate, defaultHideOnMobile: true },
-      ],
-      [
-        'status',
-        {
-          formatter: status =>
-            getStatusTranslator(t)(status as InvoiceNodeStatus),
-        },
-      ],
-      [
-        'invoiceNumber',
-        { description: 'description.invoice-number', width: 150 },
-      ],
-      'createdDatetime',
-      {
-        description: 'description.customer-reference',
-        key: 'theirReference',
-        label: 'label.reference',
-        Cell: TooltipTextCell,
-        width: 175,
-        defaultHideOnMobile: true,
-      },
-      getCommentPopoverColumn(),
-      [
-        'totalAfterTax',
-        {
-          accessor: ({ rowData }) => rowData.pricing.totalAfterTax,
-          width: 125,
-          defaultHideOnMobile: true,
-        },
-      ],
-    ],
-    { onChangeSortBy: updateSortQuery, sortBy },
-    [sortBy]
-  );
+  const { data, isLoading } = useOutbound.document.list(queryParams);
+  const { mutate: onUpdate } = useOutbound.document.update();
 
   const mrtColumns = useMemo(
     (): ColumnDef<OutboundRowFragment>[] => [
       {
         header: t('label.name'),
         accessorKey: 'otherPartyName',
-        ...getNameAndColorSetterColumn<OutboundRowFragment>(
-          onUpdate,
-          isOutboundDisabled
-        ),
+        size: 400,
         enableColumnFilter: true,
         enableSorting: true,
         defaultHideOnMobile: true,
+        Cell: ({ row }) => (
+          <NameAndColorSetterCell
+            onColorChange={onUpdate}
+            getIsDisabled={isOutboundDisabled}
+            row={row.original}
+          />
+        ),
       },
       {
-        accessorFn: row => getStatusTranslator(t)(row.status),
         id: 'status',
         header: t('label.status'),
+        accessorFn: row => getStatusTranslator(t)(row.status),
         size: 140,
         enableSorting: true,
+        enableColumnFilter: true,
         filterVariant: 'select',
         filterSelectOptions: [
           { value: InvoiceNodeStatus.New, label: t('label.new') },
@@ -158,6 +100,7 @@ const OutboundShipmentListViewComponent: FC = () => {
         description: t('description.customer-reference'),
         size: 175,
         defaultHideOnMobile: true,
+        Cell: TextWithTooltipCell,
       },
 
       {
@@ -177,7 +120,7 @@ const OutboundShipmentListViewComponent: FC = () => {
 
   const { table, selectedRows, resetRowSelection } =
     usePaginatedMaterialTable<OutboundRowFragment>({
-      tableId: 'outbound-shipment-list-view',
+      tableId: 'outbound-shipment-list',
       isLoading,
       onRowClick: row => navigate(row.id),
       columns: mrtColumns,
@@ -186,6 +129,12 @@ const OutboundShipmentListViewComponent: FC = () => {
       initialSort: { key: 'invoiceNumber', dir: 'desc' },
       getIsRestrictedRow: isOutboundDisabled,
     });
+  //     noDataElement={
+  //   <NothingHere
+  //     body={t('error.no-outbound-shipments')}
+  //     onCreate={modalController.toggleOn}
+  //   />
+  // }
 
   return (
     <>
@@ -194,29 +143,8 @@ const OutboundShipmentListViewComponent: FC = () => {
         modalController={modalController}
         simplifiedTabletView={simplifiedTabletView}
       />
-      {tableUsabilityImprovements ? (
-        <MaterialTable table={table} />
-      ) : (
-        <DataTable
-          id="outbound-list"
-          enableColumnSelection
-          pagination={{ ...pagination, total: data?.totalCount ?? 0 }}
-          onChangePage={updatePaginationQuery}
-          columns={columns}
-          data={data?.nodes ?? []}
-          isError={isError}
-          isLoading={isLoading}
-          noDataElement={
-            <NothingHere
-              body={t('error.no-outbound-shipments')}
-              onCreate={modalController.toggleOn}
-            />
-          }
-          onRowClick={row => {
-            navigate(row.id);
-          }}
-        />
-      )}
+      <MaterialTable table={table} />
+
       <Footer
         selectedRows={selectedRows}
         resetRowSelection={resetRowSelection}
@@ -224,9 +152,3 @@ const OutboundShipmentListViewComponent: FC = () => {
     </>
   );
 };
-
-export const OutboundShipmentListView: FC = () => (
-  <TableProvider createStore={createTableStore}>
-    <OutboundShipmentListViewComponent />
-  </TableProvider>
-);
