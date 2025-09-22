4603e3573f00dedac83a1555901fad7485df419f
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-22 15:32:14 +1200

    fix exports
    Source: https://github.com/msupply-foundation/open-msupply/commit/c49740b7841169ea611f0410fea07fcfc3eae214

--- client/packages/common/src/ui/layout/tables/material-react-table/index.ts
+++ client/packages/common/src/ui/layout/tables/material-react-table/index.ts
@@ -5,7 +5,8 @@ export * from './useMaterialTableColumns';
 export * from './utils';
 export * from './types';
 export * from './useGetColumnDefDefaults';
-export * from './components';
+// Not exporting all yet, some conflict with old cell names
+export { NameAndColorSetterCell, TextWithTooltipCell } from './components';
 
 // Re-exporting so all imports come from common folder
 export { MaterialReactTable as MaterialTable } from 'material-react-table';
