40d34dcbef94b1d8c05426cc7e5ad01b8708c7b2
roxy-dao <roxy@msupply.foundation>
2025-09-23 14:41:25 +1200

    Remove redundant rounding
    Source: https://github.com/msupply-foundation/open-msupply/commit/9f0eeefac8b93409c4c5d4294352ae541cf250e2

--- client/packages/purchasing/src/common/Inputs.tsx
+++ client/packages/purchasing/src/common/Inputs.tsx
@@ -149,13 +149,11 @@ export const NumInputRow = ({
   isVerticalScreen: boolean;
   label: string;
 }) => {
-  const roundedValue = NumUtils.round(value ?? 0, decimalLimit ?? 2);
-
   const handleChange = (newValue?: number) => {
-    if (!onChange || newValue === roundedValue) return;
+    if (!onChange || newValue === value) return;
 
-    const value = newValue === undefined ? 0 : newValue;
-    onChange(value);
+    const v = newValue === undefined ? 0 : newValue;
+    onChange(v);
   };
 
   return (
@@ -174,7 +172,7 @@ export const NumInputRow = ({
             }}
             slotProps={inputSlotProps(disabled)}
             min={0}
-            value={roundedValue}
+            value={value}
             onChange={handleChange}
             disabled={disabled}
             max={max}
