d0c74036e9884208f6ca322643687939377b60ef
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-23 17:18:23 +1200

    placeholder to bottom

--- client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
+++ client/packages/invoices/src/OutboundShipment/DetailView/OutboundLineEdit/OutboundLineEditTable.tsx
@@ -1,15 +1,12 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import {
   Divider,
   Box,
   useTranslation,
-  TableCell,
-  styled,
   useFormatNumber,
   Tooltip,
   NumUtils,
   Typography,
-  useTableStore,
   usePreferences,
   MaterialTable,
   useSimpleMaterialTable,
@@ -23,103 +20,11 @@ import {
   DraftStockOutLineFragment,
 } from '../../../StockOut';
 import { min } from 'lodash';
-import { useDisableVvmRows } from '../../../useDisableVvmRows';
 
 export interface OutboundLineEditTableProps {
   currency?: CurrencyRowFragment | null;
   isExternalSupplier: boolean;
 }
-
-const PlaceholderCell = styled(TableCell)(({ theme }) => ({
-  fontSize: 12,
-  padding: '4px 20px 4px 12px',
-  color: theme.palette.secondary.main,
-}));
-
-const TotalCell = styled(TableCell)(({ theme }) => ({
-  fontSize: 14,
-  padding: '8px 12px 4px 12px',
-  fontWeight: 'bold',
-  position: 'sticky',
-  bottom: 0,
-  background: theme.palette.background.white,
-  borderTop: `1px solid ${theme.palette.divider}`,
-}));
-
-const PlaceholderRow = ({
-  quantity,
-  extraColumnOffset,
-  dosesPerUnit,
-}: {
-  quantity: number | null;
-  extraColumnOffset: number;
-  dosesPerUnit?: number;
-}) => {
-  const t = useTranslation();
-
-  const formattedValue = useFormatNumber().round(quantity ?? 0, 2);
-  const tooltip = useFormatNumber().round(quantity ?? 0, 10);
-
-  // TODO - maybe should be editable? Can't clear when manually allocating..
-  return quantity === null ? null : (
-    <tr>
-      <PlaceholderCell
-        colSpan={5 + extraColumnOffset}
-        sx={{ color: 'secondary.main' }}
-      >
-        {t('label.placeholder')}
-      </PlaceholderCell>
-      <PlaceholderCell style={{ textAlign: 'right', paddingRight: '14px' }}>
-        1
-      </PlaceholderCell>
-      {!!dosesPerUnit && (
-        <PlaceholderCell style={{ textAlign: 'right', paddingRight: '14px' }}>
-          {dosesPerUnit}
-        </PlaceholderCell>
-      )}
-      <PlaceholderCell colSpan={dosesPerUnit ? 2 : 3}></PlaceholderCell>
-      <Tooltip title={tooltip}>
-        <PlaceholderCell style={{ textAlign: 'right' }}>
-          {!!NumUtils.hasMoreThanTwoDp(quantity)
-            ? `${formattedValue}...`
-            : formattedValue}
-        </PlaceholderCell>
-      </Tooltip>
-    </tr>
-  );
-};
-
-const TotalRow = ({
-  allocatedQuantity,
-  extraColumnOffset,
-}: {
-  allocatedQuantity: number;
-  extraColumnOffset: number;
-}) => {
-  const t = useTranslation();
-  const formattedValue = useFormatNumber().round(allocatedQuantity, 2);
-  const tooltip = useFormatNumber().round(allocatedQuantity, 10);
-
-  return (
-    <tr>
-      <TotalCell colSpan={3}>{t('label.total-quantity')}</TotalCell>
-      <TotalCell colSpan={6 + extraColumnOffset}></TotalCell>
-      <Tooltip title={tooltip}>
-        <TotalCell
-          style={{
-            textAlign: 'right',
-            paddingRight: 20,
-          }}
-        >
-          {!!NumUtils.hasMoreThanTwoDp(allocatedQuantity)
-            ? `${formattedValue}...`
-            : formattedValue}
-        </TotalCell>
-      </Tooltip>
-      <TotalCell colSpan={2} />
-    </tr>
-  );
-};
 
 export const OutboundLineEditTable = ({
   currency,
@@ -127,7 +32,6 @@ export const OutboundLineEditTable = ({
 }: OutboundLineEditTableProps) => {
   const t = useTranslation();
   const { format } = useFormatNumber();
-  const tableStore = useTableStore();
   const prefs = usePreferences();
 
   const {
@@ -161,8 +65,11 @@ export const OutboundLineEditTable = ({
   const getIsDisabled = useCallback(
     (row: DraftStockOutLineFragment) => {
       if (nonAllocatableLines.some(line => line.id === row.id)) return true;
+
+      // For Outbound Shipments, we also don't allow allocating bad VVM status stock
       if (prefs.manageVvmStatusForStock && item?.isVaccine)
         return !!row.vvmStatus?.unusable;
+
       return false;
     },
     [nonAllocatableLines, prefs, item]
@@ -195,51 +102,12 @@ export const OutboundLineEditTable = ({
     () => [...draftLines, ...nonAllocatableLines],
     [draftLines, nonAllocatableLines]
   );
-  // But disable the non-allocatable ones
-  useEffect(() => {
-    tableStore.setDisabledRows(nonAllocatableLines.map(({ id }) => id));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useDisableVvmRows({ rows: lines, isVaccine: item?.isVaccine });
-
-  // Null means we aren't using placeholder
-  // todo - not this
-
-  let extraColumnOffset = 0;
-  if (
-    item?.isVaccine &&
-    (prefs.manageVvmStatusForStock || prefs.sortByVvmStatusThenExpiry)
-  ) {
-    extraColumnOffset += 1;
-  }
-  if (prefs.allowTrackingOfStockByDonor) {
-    extraColumnOffset += 1;
-  }
-
-  const additionalRows = [
-    <PlaceholderRow
-      // Only show a 0 placeholder if we have no stock lines to show
-      quantity={
-        placeholderQuantity === 0 && lines.length ? null : placeholderQuantity
-      }
-      extraColumnOffset={extraColumnOffset}
-      dosesPerUnit={item?.doses}
-      key="placeholder-row"
-    />,
-    <TotalRow
-      key="total-row"
-      allocatedQuantity={allocatedQuantity + (placeholderQuantity ?? 0)}
-      extraColumnOffset={extraColumnOffset}
-    />,
-  ];
 
   const table = useSimpleMaterialTable<DraftStockOutLineFragment>({
     tableId: 'outbound-line-edit',
     columns,
     data: lines,
     getIsRestrictedRow: row => getIsDisabled(row),
-    // no surely?
     bottomToolbarContent: (
       <Box
         sx={{
@@ -248,18 +116,23 @@ export const OutboundLineEditTable = ({
           justifyContent: 'flex-end',
         }}
       >
-        TODO: TOTAL
-        {/* {additionalRows} */}
+        <PlaceholderAndTotal
+          allocatedQuantity={allocatedQuantity + (placeholderQuantity ?? 0)}
+          inDoses={allocateIn.type === AllocateInType.Doses}
+          placeholderQuantity={
+            placeholderQuantity === 0 && lines.length
+              ? null
+              : placeholderQuantity
+          }
+        />
       </Box>
     ),
-  });
-
-  if (!lines.length && placeholderQuantity === null)
-    return (
+    renderEmptyRowsFallback: () => (
       <Box sx={{ margin: 'auto' }}>
         <Typography>{t('messages.no-stock-available')}</Typography>
       </Box>
-    );
+    ),
+  });
 
   return (
     <Box style={{ width: '100%' }}>
@@ -276,5 +149,63 @@ export const OutboundLineEditTable = ({
         <MaterialTable table={table} />
       </Box>
     </Box>
+  );
+};
+
+const PlaceholderAndTotal = ({
+  allocatedQuantity,
+  placeholderQuantity,
+  inDoses,
+}: {
+  allocatedQuantity: number;
+  placeholderQuantity: number | null;
+  inDoses: boolean;
+}) => {
+  const t = useTranslation();
+  const formattedValue = useFormatNumber().round(allocatedQuantity, 2);
+  const tooltip = useFormatNumber().round(allocatedQuantity, 10);
+
+  return (
+    <>
+      {placeholderQuantity !== null && (
+        <Box
+          sx={{
+            display: 'flex',
+            gap: '10px',
+            fontSize: 12,
+            padding: '4px 20px 4px 12px',
+            color: 'secondary.main',
+          }}
+        >
+          {t('label.placeholder')}
+          <Tooltip title={tooltip}>
+            <span
+              style={{
+                textAlign: 'right',
+                paddingRight: 20,
+              }}
+            >
+              {placeholderQuantity}
+            </span>
+          </Tooltip>
+        </Box>
+      )}
+
+      <div style={{ display: 'flex', gap: 20, fontWeight: 'bold' }}>
+        {inDoses ? t('label.total-doses') : t('label.total-units')}
+        <Tooltip title={tooltip}>
+          <span
+            style={{
+              textAlign: 'right',
+              paddingRight: 20,
+            }}
+          >
+            {!!NumUtils.hasMoreThanTwoDp(allocatedQuantity)
+              ? `${formattedValue}...`
+              : formattedValue}
+          </span>
+        </Tooltip>
+      </div>
+    </>
   );
 };
