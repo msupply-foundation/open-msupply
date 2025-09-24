d9d91077edb4db5873691a886d62ec4061ea409e
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-09-24 16:55:41 +1200

    Apply to Detail view, set virtualization threshold

--- client/packages/common/src/ui/layout/tables/material-react-table/useNonPaginatedMaterialTable.ts
+++ client/packages/common/src/ui/layout/tables/material-react-table/useNonPaginatedMaterialTable.ts
@@ -1,14 +1,21 @@
 import { MRT_RowData } from 'material-react-table';
 import { BaseTableConfig, useBaseMaterialTable } from './useBaseMaterialTable';
 
+const VIRTUALIZATION_THRESHOLD = 250;
+
 interface NonPaginatedTableConfig<T extends MRT_RowData>
   extends BaseTableConfig<T> {}
 
 export const useNonPaginatedMaterialTable = <T extends MRT_RowData>({
   ...tableOptions
 }: NonPaginatedTableConfig<T>) => {
+  // On initial load, "data" can be undefined, which causes the first render to
+  // not use virtualization, which is really slow, so we default to true
+  const shouldVirtualize = tableOptions?.data
+    ? tableOptions?.data.length > VIRTUALIZATION_THRESHOLD
+    : true;
   const table = useBaseMaterialTable<T>({
-    enableRowVirtualization: true,
+    enableRowVirtualization: shouldVirtualize,
     ...tableOptions,
   });
 
