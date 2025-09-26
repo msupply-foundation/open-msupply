37f3b6e1053c5119117a395530dc14def79cbb9b
roxy-dao <roxy@msupply.foundation>
2025-09-26 12:53:24 +1200

    Tests & error handling
    Source: https://github.com/msupply-foundation/open-msupply/commit/27528448eef0085ac656cc9f856e4999d78a2834

--- server/graphql/requisition/src/mutations/response_requisition/add_from_master_list.rs
+++ server/graphql/requisition/src/mutations/response_requisition/add_from_master_list.rs
@@ -113,3 +113,228 @@ pub fn map_error(error: ServiceError) -> Result<ResponseAddFromMasterListErrorIn
 
     Err(graphql_error.extend())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::RequisitionMutations;
+    use async_graphql::EmptyMutation;
+    use graphql_core::{
+        assert_graphql_query, assert_standard_graphql_error, test_helpers::setup_graphql_test,
+    };
+    use repository::{
+        mock::{
+            mock_item_a, mock_new_response_requisition_for_update_test,
+            mock_new_response_requisition_for_update_test_line, MockDataInserts,
+        },
+        RequisitionLine, StorageConnectionManager,
+    };
+    use serde_json::json;
+    use service::{
+        requisition::{
+            response_requisition::{
+                ResponseAddFromMasterList as ServiceInput,
+                ResponseAddFromMasterListError as ServiceError,
+            },
+            RequisitionServiceTrait,
+        },
+        service_provider::{ServiceContext, ServiceProvider},
+    };
+
+    type AddFromMasterListMethod =
+        dyn Fn(ServiceInput) -> Result<Vec<RequisitionLine>, ServiceError> + Sync + Send;
+
+    pub struct TestService(pub Box<AddFromMasterListMethod>);
+
+    impl RequisitionServiceTrait for TestService {
+        fn response_add_from_master_list(
+            &self,
+            _: &ServiceContext,
+            input: ServiceInput,
+        ) -> Result<Vec<RequisitionLine>, ServiceError> {
+            self.0(input)
+        }
+    }
+
+    fn service_provider(
+        test_service: TestService,
+        connection_manager: &StorageConnectionManager,
+    ) -> ServiceProvider {
+        let mut service_provider = ServiceProvider::new(connection_manager.clone());
+        service_provider.requisition_service = Box::new(test_service);
+        service_provider
+    }
+
+    fn empty_variables() -> serde_json::Value {
+        json!({
+          "input": {
+            "responseRequisitionId": "n/a",
+            "masterListId": "n/a"
+          },
+          "storeId": "n/a"
+        })
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_response_add_from_master_list_errors() {
+        let (_, _, connection_manager, settings) = setup_graphql_test(
+            EmptyMutation,
+            RequisitionMutations,
+            "test_graphql_response_add_from_master_list_errors",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: ResponseAddFromMasterListInput!, $storeId: String) {
+            responseAddFromMasterList(storeId: $storeId, input: $input) {
+              ... on ResponseAddFromMasterListError {
+                error {
+                  __typename
+                  ... on MasterListNotFoundForThisStore {
+                    description
+                  }
+                }
+              }
+            }
+          }
+        "#;
+
+        // MasterListNotFoundForThisStore
+        let test_service = TestService(Box::new(|_| {
+            Err(ServiceError::MasterListNotFoundForThisStore)
+        }));
+
+        let expected = json!({
+            "responseAddFromMasterList": {
+              "error": {
+                "__typename": "MasterListNotFoundForThisStore",
+                "description": "Master list not found (might not be visible to this store)"
+              }
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(empty_variables()),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // RequisitionDoesNotExist
+        let test_service = TestService(Box::new(|_| Err(ServiceError::RequisitionDoesNotExist)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // CannotEditRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::CannotEditRequisition)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // NotThisStoreRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::NotThisStoreRequisition)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+
+        // NotAResponseRequisition
+        let test_service = TestService(Box::new(|_| Err(ServiceError::NotAResponseRequisition)));
+        let expected_message = "Bad user input";
+        assert_standard_graphql_error!(
+            &settings,
+            &mutation,
+            &Some(empty_variables()),
+            &expected_message,
+            None,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_graphql_response_add_from_master_list_success() {
+        let (_, _, connection_manager, settings) = setup_graphql_test(
+            EmptyMutation,
+            RequisitionMutations,
+            "test_graphql_response_add_from_master_list_success",
+            MockDataInserts::all(),
+        )
+        .await;
+
+        let mutation = r#"
+        mutation ($input: ResponseAddFromMasterListInput!, $storeId: String) {
+            responseAddFromMasterList(storeId: $storeId, input: $input) {
+              ... on RequisitionLineConnector {
+                nodes {
+                  id
+                }
+                totalCount
+              }
+            }
+          }
+        "#;
+
+        // Success
+        let test_service = TestService(Box::new(|input| {
+            assert_eq!(
+                input,
+                ServiceInput {
+                    response_requisition_id: "id".to_string(),
+                    master_list_id: "master list".to_string(),
+                }
+            );
+            Ok(vec![RequisitionLine {
+                requisition_line_row: mock_new_response_requisition_for_update_test_line(),
+                requisition_row: mock_new_response_requisition_for_update_test(),
+                item_row: mock_item_a(),
+            }])
+        }));
+
+        let variables = json!({
+          "input": {
+            "responseRequisitionId": "id",
+            "masterListId": "master list"
+          },
+          "storeId": "store_a"
+        });
+
+        let expected = json!({
+            "responseAddFromMasterList": {
+              "nodes": [
+                {
+                  "id": mock_new_response_requisition_for_update_test_line().id
+                }
+              ]
+            }
+          }
+        );
+
+        assert_graphql_query!(
+            &settings,
+            mutation,
+            &Some(variables),
+            &expected,
+            Some(service_provider(test_service, &connection_manager))
+        );
+    }
+}
