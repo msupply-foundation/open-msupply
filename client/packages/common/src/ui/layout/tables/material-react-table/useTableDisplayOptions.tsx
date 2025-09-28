0516c9664fd65bd7d7e325408151525704f2d588
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-29 11:09:41 +1300

    add isGrouped to table
    Source: https://github.com/msupply-foundation/open-msupply/commit/492894124c7cc567570256d8f75cbb9c62f532bc

--- client/packages/common/src/ui/layout/tables/material-react-table/useTableDisplayOptions.tsx
+++ client/packages/common/src/ui/layout/tables/material-react-table/useTableDisplayOptions.tsx
@@ -11,6 +11,7 @@ import {
   CheckboxCheckedIcon,
   CheckboxEmptyIcon,
   CheckboxIndeterminateIcon,
+  MedicineIcon,
   RefreshIcon,
 } from '@common/icons';
 import { MenuItem, Typography } from '@mui/material';
@@ -19,13 +20,21 @@ import { IconButton } from '@common/components';
 import { useTranslation } from '@common/intl';
 import { hasSavedState } from './tableState/utils';
 
-export const useTableDisplayOptions = <T extends MRT_RowData>(
-  tableId: string,
-  resetTableState: () => void,
-  onRowClick?: (row: T) => void,
-  getIsPlaceholderRow: (row: T) => boolean = () => false,
-  getIsRestrictedRow: (row: T) => boolean = () => false
-): Partial<MRT_TableOptions<T>> => {
+export const useTableDisplayOptions = <T extends MRT_RowData>({
+  tableId,
+  resetTableState,
+  onRowClick,
+  toggleGrouped,
+  getIsPlaceholderRow = () => false,
+  getIsRestrictedRow = () => false,
+}: {
+  tableId: string;
+  resetTableState: () => void;
+  onRowClick?: (row: T) => void;
+  toggleGrouped?: () => void;
+  getIsPlaceholderRow?: (row: T) => boolean;
+  getIsRestrictedRow?: (row: T) => boolean;
+}): Partial<MRT_TableOptions<T>> => {
   const t = useTranslation();
   return {
     // Add description to column menu
@@ -54,6 +63,14 @@ export const useTableDisplayOptions = <T extends MRT_RowData>(
     // Add reset state button to toolbar
     renderToolbarInternalActions: ({ table }) => (
       <>
+        {toggleGrouped && (
+          <IconButton
+            icon={<MedicineIcon />}
+            onClick={toggleGrouped}
+            label={t('label.group-by-item')}
+            sx={iconButtonProps}
+          />
+        )}
         <MRT_ToggleFiltersButton table={table} />
         <MRT_ToggleDensePaddingButton table={table} />
         <MRT_ShowHideColumnsButton table={table} />
@@ -62,11 +79,7 @@ export const useTableDisplayOptions = <T extends MRT_RowData>(
           onClick={resetTableState}
           label={t('label.reset-table-defaults')}
           disabled={!hasSavedState(tableId)}
-          sx={{
-            width: '40px',
-            height: '40px',
-            '& svg': { fontSize: '1.2rem' },
-          }}
+          sx={iconButtonProps}
         />
         <MRT_ToggleFullScreenButton table={table} />
       </>
@@ -256,4 +269,10 @@ export const useTableDisplayOptions = <T extends MRT_RowData>(
       },
     },
   };
+};
+
+const iconButtonProps = {
+  width: '40px',
+  height: '40px',
+  '& svg': { fontSize: '1.2rem' },
 };
