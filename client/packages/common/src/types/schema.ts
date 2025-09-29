4b2b2290e978a322884ea83927f1d56e3e3e14d9
Laché Melvin <55115239+lache-melvin@users.noreply.github.com>
2025-09-30 11:38:13 +1300

    count out of stock items for estimated count

--- client/packages/common/src/types/schema.ts
+++ client/packages/common/src/types/schema.ts
@@ -4092,6 +4092,7 @@ export type InsertStocktakeInput = {
   expiresBefore?: InputMaybe<Scalars['NaiveDate']['input']>;
   id: Scalars['String']['input'];
   includeAllMasterListItems?: InputMaybe<Scalars['Boolean']['input']>;
+  isAllItemsStocktake?: InputMaybe<Scalars['Boolean']['input']>;
   isInitialStocktake?: InputMaybe<Scalars['Boolean']['input']>;
   locationId?: InputMaybe<Scalars['String']['input']>;
   masterListId?: InputMaybe<Scalars['String']['input']>;
