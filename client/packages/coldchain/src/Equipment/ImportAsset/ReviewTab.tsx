ab8835e28654cba7ff8316729b0b0a9bd8c3715b
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-10-10 12:37:09 +1300

    Replace import table with new table UI
    Source: https://github.com/msupply-foundation/open-msupply/commit/0a3f02dec2fdd94ddbd6cd935274b960270e281e

--- client/packages/coldchain/src/Equipment/ImportAsset/ReviewTab.tsx
+++ client/packages/coldchain/src/Equipment/ImportAsset/ReviewTab.tsx
@@ -6,17 +6,21 @@ import { ImportPanel } from '@common/components';
 interface EquipmentReviewTabProps {
   uploadedRows: ImportRow[];
   showWarnings: boolean;
+  hasError: boolean;
 }
 
 export const EquipmentReviewTab: FC<ImportPanel & EquipmentReviewTabProps> = ({
   showWarnings,
   tab,
   uploadedRows,
+  hasError,
 }) => (
   <ImportPanel tab={tab}>
-    <ImportReviewDataTable
-      importRows={uploadedRows}
-      showWarnings={showWarnings}
-    />
+    {!hasError && (
+      <ImportReviewDataTable
+        importRows={uploadedRows}
+        showWarnings={showWarnings}
+      />
+    )}
   </ImportPanel>
 );
