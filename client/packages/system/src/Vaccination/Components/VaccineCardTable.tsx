d74dd1e413af33883a846e78f010949cf0f4e39f
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-10-15 12:51:27 +1300

    Basic table config

--- client/packages/system/src/Vaccination/Components/VaccineCardTable.tsx
+++ client/packages/system/src/Vaccination/Components/VaccineCardTable.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import {
   TableProvider,
   createTableStore,
@@ -11,9 +11,14 @@ import {
   useFormatDateTime,
   useRowStyle,
   useTheme,
-  StatusCell,
+  StatusCell as StatusCellOld,
   VaccinationCardItemNodeStatus,
+  useNonPaginatedMaterialTable,
+  MaterialTable,
+  ColumnDef,
+  ColumnType,
 } from '@openmsupply-client/common';
+import { StatusCell } from '@openmsupply-client/common/src/ui/layout/tables/material-react-table/components';
 import {
   VaccinationCardFragment,
   VaccinationCardItemFragment,
@@ -138,7 +143,94 @@ const VaccinationCardComponent = ({
     return monthsLabel;
   };
 
-  const columns = useColumns<VaccinationCardItemFragment>(
+  const columns = useMemo(
+    (): ColumnDef<VaccinationCardItemFragment>[] => [
+      {
+        id: 'age',
+        header: t('label.age'),
+        size: 160,
+        accessorFn: row => {
+          const index = data?.items.findIndex(item => item.id === row.id) ?? 0;
+
+          const sameAsPrev =
+            row.minAgeMonths === data?.items?.[index - 1]?.minAgeMonths;
+
+          // Only show age label for first of each "block", when repeated
+          return sameAsPrev ? null : getAgeLabel(row);
+        },
+        Cell: ({ renderedCellValue }) => (
+          <span style={{ fontWeight: 'bold', paddingLeft: 8 }}>
+            {renderedCellValue}
+          </span>
+        ),
+        enableHiding: false,
+        enableColumnOrdering: false,
+      },
+      {
+        accessorKey: 'label',
+        header: t('label.dose'),
+        size: 100,
+        enableHiding: false,
+      },
+      {
+        accessorKey: 'status',
+        header: t('label.status'),
+        size: 140,
+        Cell: ({ cell }) => (
+          <StatusCell
+            cell={cell}
+            statusMap={{
+              [VaccinationCardItemNodeStatus.Given]: {
+                color: theme.palette.vaccinationStatus.given,
+                label: t('label.status-given'),
+              },
+              [VaccinationCardItemNodeStatus.NotGiven]: {
+                color: theme.palette.vaccinationStatus.notGiven,
+                label: t('label.status-not-given'),
+              },
+              [VaccinationCardItemNodeStatus.Pending]: {
+                color: theme.palette.vaccinationStatus.pending,
+                label: t('label.status-pending'),
+              },
+              [VaccinationCardItemNodeStatus.Late]: {
+                color: theme.palette.vaccinationStatus.late,
+                label: t('label.status-late'),
+              },
+            }}
+          />
+        ),
+        enableHiding: false,
+      },
+      {
+        id: 'suggestedDate',
+        header: t('label.suggested-date'),
+        size: 120,
+        accessorFn: row => row.suggestedDate ?? '',
+        columnType: ColumnType.Date,
+      },
+      {
+        id: 'dateGiven',
+        header: t('label.date-given'),
+        size: 120,
+        accessorFn: row =>
+          row.status === VaccinationCardItemNodeStatus.Given
+            ? (row.vaccinationDate ?? '')
+            : '',
+        columnType: ColumnType.Date,
+      },
+      {
+        accessorKey: 'batch',
+        header: t('label.batch'),
+      },
+      {
+        accessorKey: 'facilityName',
+        header: t('label.facility'),
+      },
+    ],
+    [data]
+  );
+
+  const oldColumns = useColumns<VaccinationCardItemFragment>(
     [
       {
         key: 'age',
@@ -167,7 +259,7 @@ const VaccinationCardComponent = ({
         label: 'label.status',
         accessor: ({ rowData }) => rowData.status,
         Cell: ({ ...props }) => (
-          <StatusCell
+          <StatusCellOld
             {...props}
             statusMap={{
               [VaccinationCardItemNodeStatus.Given]: {
@@ -221,18 +313,37 @@ const VaccinationCardComponent = ({
     [data]
   );
 
+  const { table } = useNonPaginatedMaterialTable<VaccinationCardItemFragment>({
+    tableId: 'vaccine-card-table',
+    data: data?.items ?? [],
+    columns,
+    enableRowSelection: false,
+    onRowClick: row => {
+      if (canClickRow(isEncounter, row, data?.items, canSkipDose))
+        openModal(row);
+    },
+    // muiTableBodyRowProps: ({ row }) => ({
+    //   sx: {
+    //     cursor: canClickRow(isEncounter, row.original, data?.items, canSkipDose)
+    //       ? 'pointer'
+    //       : 'default',
+    //   },
+    // }),
+  });
+
   return (
     <>
-      <DataTable
+      {/* <DataTable
         id={'Vaccine Course List'}
-        columns={columns}
+        columns={oldColumns}
         data={data?.items ?? []}
         onRowClick={row => {
           if (canClickRow(isEncounter, row, data?.items, canSkipDose))
             openModal(row);
         }}
         noDataElement={<NothingHere body={t('error.no-items')} />}
-      />
+      /> */}
+      <MaterialTable table={table} />
     </>
   );
 };
