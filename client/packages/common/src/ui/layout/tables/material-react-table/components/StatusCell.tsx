714dcecaf2d083a8b9329b28a2c67e0c940ce60f
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-10-15 12:51:27 +1300

    Basic table config
    Source: https://github.com/msupply-foundation/open-msupply/commit/d74dd1e413af33883a846e78f010949cf0f4e39f

---
+++ client/packages/common/src/ui/layout/tables/material-react-table/components/StatusCell.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MRT_Cell, MRT_RowData } from 'material-react-table';
+import { StatusChip } from 'packages/common/src/ui/components/panels/StatusChip';
+
+export const StatusCell = <T extends MRT_RowData>({
+  cell,
+  statusMap,
+}: {
+  cell: MRT_Cell<T>;
+  statusMap: Record<
+    string,
+    { label: string; color: string; bgColor?: string }
+  > | null;
+}) => {
+  const status = cell.getValue<string>();
+
+  const { label, color, bgColor } = statusMap?.[status] ?? {};
+  return <StatusChip label={label} color={color} bgColor={bgColor} />;
+};
