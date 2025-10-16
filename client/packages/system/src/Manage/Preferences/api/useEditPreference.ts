1216710a9a0ba3a53789efeea860ef3f3a9c25c2
John Geronimo <johnageronimo1@gmail.com>
2025-10-16 14:04:32 +1300

    improve support for validation

--- client/packages/system/src/Manage/Preferences/api/useEditPreference.ts
+++ client/packages/system/src/Manage/Preferences/api/useEditPreference.ts
@@ -1,6 +1,6 @@
+import { Dispatch, SetStateAction } from 'react';
 import {
   isEmpty,
-  PreferenceKey,
   PreferenceNodeType,
   PREFERENCES_QUERY_KEY,
   UpsertPreferencesInput,
@@ -11,10 +11,12 @@ import {
 import { usePreferencesGraphQL } from './usePreferencesGraphQL';
 import { useAdminPrefsList } from './useAdminPrefsList';
 import { PREFERENCE_DESCRIPTION_QUERY_KEY } from './keys';
+import { inputValidation } from './utils';
 
 export const useEditPreferences = (
   prefType: PreferenceNodeType,
-  storeId?: string
+  storeId?: string,
+  setActionValid?: Dispatch<SetStateAction<boolean>>
 ) => {
   const t = useTranslation();
   const { error, warning } = useNotification();
@@ -22,47 +24,10 @@ export const useEditPreferences = (
   const { data } = useAdminPrefsList(prefType, storeId);
   const { mutateAsync } = useUpsertPref();
 
-  const processThreshold = (
-    input: Partial<UpsertPreferencesInput>
-  ): boolean => {
-    const inputFirstThreshold =
-      input?.firstThresholdForExpiringItems?.[0]?.value;
-    const inputSecondThreshold =
-      input?.secondThresholdForExpiringItems?.[0]?.value;
-
-    // Second threshold should not exceed 30 days
-    if (inputSecondThreshold && inputSecondThreshold > 30) {
-      warning(t('label.second-threshold-exceeds-days'))();
-      return false;
-    }
-
-    const existingFirstThreshold = data?.find(
-      pref => pref.key === PreferenceKey.FirstThresholdForExpiringItems
-    )?.value;
-    const existingSecondThreshold = data?.find(
-      pref => pref.key === PreferenceKey.SecondThresholdForExpiringItems
-    )?.value;
-
-    const firstThreshold = inputFirstThreshold ?? existingFirstThreshold;
-    const secondThreshold = inputSecondThreshold ?? existingSecondThreshold;
-
-    // Second threshold should not be less than first threshold
-    if (
-      firstThreshold != null &&
-      secondThreshold != null &&
-      secondThreshold !== 0 &&
-      secondThreshold < firstThreshold
-    ) {
-      warning(t('label.second-threshold-is-less-than-first-threshold'))();
-      return false;
-    }
-    return true;
-  };
-
   const update = async (
     input: Partial<UpsertPreferencesInput>
   ): Promise<boolean /* wasSuccessful */> => {
-    if (!processThreshold(input)) return false;
+    if (!inputValidation(input, t, warning, data, setActionValid)) return false;
 
     try {
       await mutateAsync(input);
