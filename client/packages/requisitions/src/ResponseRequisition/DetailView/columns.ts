bac1ad74813fe8acf6a0ed22bc9c03668bd2b870
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2025-10-21 09:40:31 +1300

    Implement new table
    Source: https://github.com/msupply-foundation/open-msupply/commit/412fa8fcf0c547839b33d824b1cd99a0295bb26b

--- client/packages/requisitions/src/ResponseRequisition/DetailView/columns.ts
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/columns.ts
@@ -11,12 +11,19 @@ import {
   getLinesFromRow,
   UNDEFINED_STRING_VALUE,
   usePreferences,
+  ColumnDef,
+  useTranslation,
+  ColumnType,
+  UnitsAndDosesCell,
 } from '@openmsupply-client/common';
 import { ResponseLineFragment, useResponse } from './../api';
 import { PackQuantityCell } from '@openmsupply-client/system';
 import { useResponseRequisitionLineErrorContext } from '../context';
+import { useMemo } from 'react';
 
 export const useResponseColumns = () => {
+  const t = useTranslation();
+
   const { getError } = useResponseRequisitionLineErrorContext();
   const { manageVaccinesInDoses } = usePreferences();
 
@@ -27,6 +34,237 @@ export const useResponseColumns = () => {
   const { store } = useAuthContext();
   const { isRemoteAuthorisation } = useResponse.utils.isRemoteAuthorisation();
   const { programName } = useResponse.document.fields(['programName']);
+
+  const showExtraProgramColumns =
+    !!programName && store?.preferences?.extraFieldsInRequisition;
+  // const isRemoteAuthorisation = true;
+
+  const newColumns = useMemo(
+    (): ColumnDef<ResponseLineFragment>[] => [
+      {
+        accessorKey: 'comment',
+        header: t('label.comment'),
+        columnType: ColumnType.Comment,
+        pin: 'left',
+      },
+      {
+        accessorKey: 'item.code',
+        header: t('label.code'),
+        size: 100,
+      },
+      {
+        accessorKey: 'item.name',
+        header: t('label.name'),
+        // Cell: TextWithTooltipCell,
+        size: 250,
+      },
+      {
+        accessorKey: 'item.unitName',
+        header: t('label.unit'),
+        enableColumnFilter: true,
+        size: 130,
+      },
+      {
+        id: 'dosesPerUnit',
+        accessorFn: row =>
+          row.item?.isVaccine ? row.item.doses : UNDEFINED_STRING_VALUE,
+        header: t('label.doses-per-unit'),
+        enableColumnFilter: true,
+        size: 100,
+        columnType: ColumnType.Number,
+        includeColumn: manageVaccinesInDoses,
+      },
+      {
+        accessorKey: 'itemStats.stockOnHand',
+        header: t('label.our-soh'),
+        description: t('description.our-soh'),
+        size: 150,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        enableSorting: true,
+      },
+      {
+        accessorKey: 'availableStockOnHand',
+        header: t('label.customer-soh'),
+        description: t('description.customer-soh'),
+        size: 150,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        enableSorting: true,
+        includeColumn: !programName,
+      },
+      // TODO: Global pref to show/hide the next columns
+      {
+        accessorKey: 'initialStockOnHandUnits',
+        header: t('label.initial-stock-on-hand'),
+        description: t('description.initial-stock-on-hand'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'incomingUnits',
+        header: t('label.incoming'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'outgoingUnits',
+        header: t('label.outgoing'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'lossInUnits',
+        header: t('label.losses'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'additionInUnits',
+        header: t('label.additions'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        id: 'availableUnits',
+        accessorFn: row => {
+          const stockOnHand = row.initialStockOnHandUnits;
+
+          const incomingStock = row.incomingUnits + row.additionInUnits;
+          const outgoingStock = row.lossInUnits + row.outgoingUnits;
+
+          return stockOnHand + incomingStock - outgoingStock;
+        },
+        header: t('label.available'),
+        description: t('description.available-stock'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'expiringUnits',
+        header: t('label.short-expiry'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'daysOutOfStock',
+        header: t('label.days-out-of-stock'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'averageMonthlyConsumption',
+        header: t('label.amc'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        id: 'mos',
+        accessorFn: row => {
+          const stockOnHand = row.initialStockOnHandUnits;
+          const incomingStock = row.incomingUnits + row.additionInUnits;
+          const outgoingStock = row.lossInUnits + row.outgoingUnits;
+
+          const available = stockOnHand + incomingStock - outgoingStock;
+
+          const averageMonthlyConsumption = row.averageMonthlyConsumption;
+
+          return averageMonthlyConsumption !== 0
+            ? available / averageMonthlyConsumption
+            : 0;
+        },
+        header: t('label.months-of-stock'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        includeColumn: showExtraProgramColumns,
+      },
+      {
+        accessorKey: 'suggestedQuantity',
+        header: t('label.suggested'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+      },
+      {
+        accessorKey: 'requestedQuantity',
+        header: t('label.requested'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        enableSorting: true,
+      },
+      {
+        accessorKey: 'approvedQuantity',
+        header: t('label.approved-quantity'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+        enableSorting: true,
+        includeColumn: isRemoteAuthorisation,
+      },
+      {
+        accessorKey: 'approvalComment',
+        header: t('label.approval-comment'),
+        includeColumn: isRemoteAuthorisation,
+      },
+      {
+        accessorKey: 'supplyQuantity',
+        header: t('label.supply-quantity'),
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+      },
+      // TODO: Global pref to show/hide column
+      {
+        id: 'reason',
+        header: t('label.reason'),
+        accessorFn: row => row.reason?.reason ?? '',
+        includeColumn: showExtraProgramColumns,
+        // SHOW ERROR STATE?
+      },
+      {
+        accessorKey: 'alreadyIssued',
+        header: t('label.already-issued'),
+        description: t('description.already-issued'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+      },
+      {
+        accessorKey: 'remainingQuantityToSupply',
+        header: t('label.remaining-to-supply'),
+        description: t('description.remaining-to-supply'),
+        size: 100,
+        columnType: ColumnType.Number,
+        Cell: UnitsAndDosesCell,
+      },
+    ],
+    [
+      showExtraProgramColumns,
+      isRemoteAuthorisation,
+      t,
+      manageVaccinesInDoses,
+      programName,
+    ]
+  );
 
   const columnDefinitions: ColumnDescription<ResponseLineFragment>[] = [
     [
@@ -310,5 +548,5 @@ export const useResponseColumns = () => {
     [updateSortQuery, sortBy]
   );
 
-  return { columns, sortBy, onChangeSortBy: updateSortQuery };
+  return { columns, sortBy, onChangeSortBy: updateSortQuery, newColumns };
 };
