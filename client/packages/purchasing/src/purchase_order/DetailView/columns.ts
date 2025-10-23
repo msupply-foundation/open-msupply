777ed5120e928f46761a51cfd3697d4046346e37
roxy-dao <roxy@msupply.foundation>
2025-10-24 10:12:56 +1300

    Handle selectedRows for purchase orders

--- client/packages/purchasing/src/purchase_order/DetailView/columns.ts
+++ client/packages/purchasing/src/purchase_order/DetailView/columns.ts
@@ -1,156 +1,99 @@
 import {
-  ColumnAlign,
-  ColumnDescription,
-  ColumnFormat,
-  CurrencyCell,
-  GenericColumnKey,
+  ColumnDef,
+  ColumnType,
   getLinesFromRow,
-  TooltipTextCell,
-  useColumns,
-  useUrlQueryParams,
+  TextWithTooltipCell,
 } from '@openmsupply-client/common';
-import { PackQuantityCell } from '@openmsupply-client/system';
 import { PurchaseOrderLineFragment } from '../api';
 import { usePurchaseOrderLineErrorContext } from '../context';
+import { useMemo } from 'react';
 
 export const usePurchaseOrderColumns = () => {
-  const {
-    updateSortQuery,
-    queryParams: { sortBy },
-  } = useUrlQueryParams({ initialSort: { key: 'itemName', dir: 'asc' } });
   const { getError } = usePurchaseOrderLineErrorContext();
 
-  const columnDefinitions: ColumnDescription<PurchaseOrderLineFragment>[] = [
-    GenericColumnKey.Selection,
-    {
-      key: 'lineNumber',
-      label: 'label.line-number',
-      align: ColumnAlign.Right,
-      width: 100,
-      accessor: ({ rowData }) => rowData.lineNumber,
-    },
-    [
-      'itemCode',
+  const columns = useMemo((): ColumnDef<PurchaseOrderLineFragment>[] => {
+    return [
       {
-        width: 130,
-        accessor: ({ rowData }) => rowData.item.code,
-        getSortValue: rowData => rowData.item.code,
+        accessorKey: 'lineNumber',
+        header: 'label.line-number',
+        columnType: ColumnType.Number,
+      },
+      {
+        accessorKey: 'item.code',
+        header: 'label.item-code',
+        size: 130,
         getIsError: row =>
           getLinesFromRow(row).some(
             r => getError(r)?.__typename === 'ItemCannotBeOrdered'
           ),
       },
-    ],
-    [
-      'itemName',
       {
-        Cell: TooltipTextCell,
-        width: 350,
-        accessor: ({ rowData }) => rowData.item.name,
-        getSortValue: rowData => rowData.item.name,
+        accessorKey: 'item.name',
+        header: 'label.item-name',
+        Cell: TextWithTooltipCell,
+        size: 350,
       },
-    ],
-    {
-      key: 'numberOfPacks',
-      label: 'label.num-packs',
-      align: ColumnAlign.Right,
-      width: 150,
-      Cell: PackQuantityCell,
-      accessor: ({ rowData }) => {
-        const numUnits =
-          rowData.adjustedNumberOfUnits ?? rowData.requestedNumberOfUnits;
-        return Math.ceil(numUnits / rowData.requestedPackSize);
+      {
+        accessorKey: 'numberOfPacks',
+        header: 'label.num-packs',
+        columnType: ColumnType.Number,
       },
-    },
-    {
-      key: 'packSize',
-      label: 'label.pack-size',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData }) => rowData.requestedPackSize,
-      getSortValue: rowData => rowData.requestedPackSize ?? 1,
-      defaultHideOnMobile: true,
-    },
-    {
-      key: 'requestedNumberOfUnits',
-      label: 'label.requested-quantity',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData }) => rowData.requestedNumberOfUnits,
-      getSortValue: rowData => rowData.requestedNumberOfUnits ?? 0,
-    },
-    {
-      key: 'authorisedNumberOfUnits',
-      label: 'label.adjusted-units',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData }) => rowData.adjustedNumberOfUnits,
-      getSortValue: rowData => rowData.adjustedNumberOfUnits ?? 0,
-    },
-    {
-      key: 'totalReceived',
-      label: 'label.total-received',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData: _ }) => '',
-      // TODO: GOOD RECEIVED CALC
-      // rowData.totalReceived,
-      // getSortValue: rowData =>  //rowData.totalReceived ?? 0,
-    },
-    {
-      key: 'stockOnHand',
-      label: 'label.soh',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData }) => rowData.item.stats.stockOnHand,
-      getSortValue: rowData => rowData.item.stats.stockOnHand ?? 0,
-      defaultHideOnMobile: true,
-    },
-    {
-      key: 'onOrder',
-      label: 'label.on-order',
-      align: ColumnAlign.Right,
-      accessor: ({ rowData }) => rowData.unitsOrderedInOthers,
-    },
-    {
-      key: 'totalCost',
-      label: 'label.total-cost',
-      align: ColumnAlign.Right,
-      Cell: CurrencyCell,
-      accessor: ({ rowData }) => {
-        const units =
-          rowData.adjustedNumberOfUnits ?? rowData.requestedNumberOfUnits ?? 0;
-        const packSize = rowData.requestedPackSize || 1;
-        return (rowData.pricePerPackAfterDiscount ?? 0) * (units / packSize);
+      {
+        accessorKey: 'packSize',
+        header: 'label.pack-size',
+        columnType: ColumnType.Number,
+        defaultHideOnMobile: true,
       },
-      getSortValue: rowData => {
-        const units =
-          rowData.adjustedNumberOfUnits ?? rowData.requestedNumberOfUnits ?? 0;
-        const packSize = rowData.requestedPackSize || 1;
-        return (rowData.pricePerPackAfterDiscount ?? 0) * (units / packSize);
+      {
+        accessorKey: 'requestedNumberOfUnits',
+        header: 'label.requested-quantity',
+        columnType: ColumnType.Number,
       },
-    },
-    {
-      key: 'requestedDeliveryDate',
-      label: 'label.requested-delivery-date',
-      align: ColumnAlign.Right,
-      format: ColumnFormat.Date,
-      accessor: ({ rowData }) => rowData.requestedDeliveryDate,
-      getSortValue: rowData => rowData.requestedDeliveryDate ?? '1000-01-01',
-    },
-    {
-      key: 'expectedDeliveryDate',
-      label: 'label.expected-delivery-date',
-      align: ColumnAlign.Right,
-      format: ColumnFormat.Date,
-      accessor: ({ rowData }) => rowData.expectedDeliveryDate,
-      getSortValue: rowData => rowData.expectedDeliveryDate ?? '1000-01-01',
-    },
-  ];
+      {
+        accessorKey: 'authorisedNumberOfUnits',
+        header: 'label.adjusted-units',
+        columnType: ColumnType.Number,
+      },
+      {
+        // TODO: Goods received calculation
+        accessorKey: 'totalReceived',
+        header: 'label.total-received',
+        columnType: ColumnType.Number,
+      },
+      {
+        accessorKey: 'stockOnHand',
+        header: 'label.soh',
+        columnType: ColumnType.Number,
+        defaultHideOnMobile: true,
+      },
+      {
+        accessorKey: 'onOrder',
+        header: 'label.on-order',
+        columnType: ColumnType.Number,
+      },
+      {
+        accessorKey: 'totalCost',
+        header: 'label.total-cost',
+        columnType: ColumnType.Currency,
+        accessorFn: row => {
+          const units =
+            row.adjustedNumberOfUnits ?? row.requestedNumberOfUnits ?? 0;
+          const packSize = row.requestedPackSize || 1;
+          return (row.pricePerPackAfterDiscount ?? 0) * (units / packSize);
+        },
+      },
+      {
+        accessorKey: 'requestedDeliveryDate',
+        header: 'label.requested-delivery-date',
+        columnType: ColumnType.Date,
+      },
+      {
+        accessorKey: 'expectedDeliveryDate',
+        header: 'label.expected-delivery-date',
+        columnType: ColumnType.Date,
+      },
+    ];
+  }, [getError]);
 
-  const columns = useColumns<PurchaseOrderLineFragment>(
-    columnDefinitions,
-    {
-      onChangeSortBy: updateSortQuery,
-      sortBy,
-    },
-    [updateSortQuery, sortBy]
-  );
-
-  return { columns, sortBy, onChangeSortBy: updateSortQuery };
+  return { columns };
 };
