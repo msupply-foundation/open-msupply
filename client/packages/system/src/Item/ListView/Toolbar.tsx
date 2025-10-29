12199c12300ca357b0940dc0f57d66e09b55f6dd
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2025-10-29 14:28:05 +1300

    9519 out of stock products (#9589)
    * seperate stock widget panels to their own components
    
    * out of stock products and refactor a few things
    
    * add products at risk of being out of stock section
    
    * add proper filtering
    
    * added new filters to item page and clean up item count
    
    * add missing arg

--- client/packages/system/src/Item/ListView/Toolbar.tsx
+++ client/packages/system/src/Item/ListView/Toolbar.tsx
@@ -4,10 +4,16 @@ import {
   useTranslation,
   FilterMenu,
   Box,
+  usePreferences,
 } from '@openmsupply-client/common';
 
 export const Toolbar: FC = () => {
   const t = useTranslation();
+  const {
+    numberOfMonthsToCheckForConsumptionWhenCalculatingOutOfStockProducts:
+      numMonthsConsumption,
+  } = usePreferences();
+
   return (
     <AppBarContentPortal
       sx={{
@@ -56,6 +62,40 @@ export const Toolbar: FC = () => {
               minValue: 0,
               decimalLimit: 0,
             },
+            ...(numMonthsConsumption
+              ? [
+                  {
+                    type: 'enum' as const,
+                    name: t('label.out-of-stock-products'),
+                    urlParameter: 'outOfStockProducts',
+                    options: [
+                      {
+                        label: t('label.show-out-of-stock-products'),
+                        value: 'true',
+                      },
+                      {
+                        label: t('label.show-in-stock-products'),
+                        value: 'false',
+                      },
+                    ],
+                  },
+                  {
+                    type: 'enum' as const,
+                    name: t('label.products-at-risk-of-being-out-of-stock'),
+                    urlParameter: 'productsAtRiskOfBeingOutOfStock',
+                    options: [
+                      {
+                        label: t('label.show-products-at-risk'),
+                        value: 'true',
+                      },
+                      {
+                        label: t('label.show-products-not-at-risk'),
+                        value: 'false',
+                      },
+                    ],
+                  },
+                ]
+              : []),
           ]}
         />
       </Box>
