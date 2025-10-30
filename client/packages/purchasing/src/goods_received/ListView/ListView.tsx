72ef9cd2a5577f3323ea4352b960f4ba01461a82
roxy-dao <roxy@msupply.foundation>
2025-10-30 14:31:55 +1300

    Move deleted rows for GR to footer component
    Source: https://github.com/msupply-foundation/open-msupply/commit/54e344979c6af29f017422ea67ff1d318c6526b7

--- client/packages/purchasing/src/goods_received/ListView/ListView.tsx
+++ client/packages/purchasing/src/goods_received/ListView/ListView.tsx
@@ -10,7 +10,6 @@ import {
   useUrlQueryParams,
   GenericColumnKey,
   ColumnFormat,
-  ColumnType,
 } from '@openmsupply-client/common';
 import { useGoodsReceivedList } from '../api';
 import { GoodsReceivedRowFragment } from '../api/operations.generated';
@@ -121,7 +120,7 @@ const ListView = () => {
           navigate(row.id);
         }}
       />
-      <Footer />
+      <Footer data={data?.nodes} />
     </>
   );
 };
