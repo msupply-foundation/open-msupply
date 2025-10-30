2a31ca8cbbd2c658380179c1ed0924feec02a4aa
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-10-30 15:47:06 +1300

    use seperate components for sell price
    Source: https://github.com/msupply-foundation/open-msupply/commit/64afd1cc8ba6da75f50f2f33a83ec32e63f2484f

--- client/packages/system/src/Item/DetailView/Tabs/Store.tsx
+++ client/packages/system/src/Item/DetailView/Tabs/Store.tsx
@@ -7,56 +7,74 @@ import {
   Grid,
   CurrencyInput,
   Checkbox,
+  useIsCentralServerApi,
+  usePluginProvider,
 } from '@openmsupply-client/common';
 import { ItemFragment } from '../../api';
 
 export const StoreTab = ({ item }: { item: ItemFragment }) => {
   const t = useTranslation();
   const isDisabled = true;
+  const isCentralServer = useIsCentralServerApi();
+  const { plugins } = usePluginProvider();
+
+  const showPlugin = isCentralServer && !!plugins.itemProperties?.ItemSellPrice;
 
   return (
-    <DetailContainer>
-      <Grid
-        container
-        flex={1}
-        flexDirection="column"
-        style={{ maxWidth: 500 }}
-        gap={4}
-      >
-        <DetailSection title={t('title.pricing')}>
-          <DetailInputWithLabelRow
-            label={t('label.default-sell-price-per-pack')}
-            Input={
-              <CurrencyInput
-                value={item?.itemStoreProperties?.defaultSellPricePerPack}
-                disabled={isDisabled}
-                onChangeNumber={() => {}}
-                width={'100%'}
+    <>
+      <DetailContainer>
+        <Grid
+          container
+          flex={1}
+          flexDirection="column"
+          style={{ maxWidth: 500 }}
+          gap={4}
+        >
+          <DetailSection title={t('title.pricing')}>
+            {!!showPlugin ? (
+              plugins.itemProperties?.ItemSellPrice?.map((Plugin, index) => (
+                <Plugin key={index} item={item} />
+              ))
+            ) : (
+              <DetailInputWithLabelRow
+                label={t('label.default-sell-price-per-pack')}
+                Input={
+                  <CurrencyInput
+                    value={item?.itemStoreProperties?.defaultSellPricePerPack}
+                    disabled={isDisabled}
+                    onChangeNumber={() => {}}
+                    width={'100%'}
+                  />
+                }
               />
-            }
-          />
-        </DetailSection>
-      </Grid>
+            )}
+          </DetailSection>
+        </Grid>
 
-      <Grid
-        container
-        flex={1}
-        flexDirection="column"
-        style={{ maxWidth: 500 }}
-        gap={4}
-      >
-        <DetailSection title={t('title.ordering')}>
-          <DetailInputWithLabelRow
-            label={t('label.ignore-for-orders')}
-            Input={
-              <Checkbox
-                disabled={isDisabled}
-                checked={item?.itemStoreProperties?.ignoreForOrders}
-              />
-            }
-          />
-        </DetailSection>
-      </Grid>
-    </DetailContainer>
+        <Grid
+          container
+          flex={1}
+          flexDirection="column"
+          style={{ maxWidth: 500 }}
+          gap={4}
+        >
+          <DetailSection title={t('title.ordering')}>
+            <DetailInputWithLabelRow
+              label={t('label.ignore-for-orders')}
+              Input={
+                <Checkbox
+                  disabled={isDisabled}
+                  checked={item?.itemStoreProperties?.ignoreForOrders}
+                />
+              }
+            />
+          </DetailSection>
+        </Grid>
+      </DetailContainer>
+      {showPlugin &&
+        plugins.itemProperties?.ItemFooter?.map((Plugin, index) => (
+          <Plugin key={index} item={item} />
+        ))}
+    </>
   );
 };
