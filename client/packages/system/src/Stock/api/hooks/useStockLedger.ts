70f2cb271bdb4a973be6cd6eb1f6f02bae58b26f
roxy-dao <roxy@msupply.foundation>
2025-11-04 14:28:51 +1300

    Refactor stock ledger

--- client/packages/system/src/Stock/api/hooks/useStockLedger.ts
+++ client/packages/system/src/Stock/api/hooks/useStockLedger.ts
@@ -11,7 +11,7 @@ import { ColumnKey } from '../../Components/Ledger/useLedgerColumns';
 
 export function useStockLedger(
   stockLine: StockLineRowFragment,
-  sortBy: SortBy<LedgerRowFragment>
+  sortBy?: SortBy<LedgerRowFragment>
 ) {
   const { stockApi, storeId } = useStockGraphQL();
 
@@ -24,8 +24,8 @@ export function useStockLedger(
 
     const query = await stockApi.ledger({
       storeId,
-      key: getSortKey(sortBy.key),
-      desc: sortBy.direction === 'desc',
+      key: getSortKey(sortBy?.key || ColumnKey.DateTime),
+      desc: sortBy?.direction === 'desc',
       filter,
     });
     const { nodes, totalCount } = query?.ledger;
