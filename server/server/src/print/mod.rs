f6a97e31d5e558887408edfb18729c649d4e2c8f
mark-prins <mark@malcolmprins.net>
2025-11-05 18:23:35 +1300

    test commit

--- server/server/src/print/mod.rs
+++ server/server/src/print/mod.rs
@@ -1,7 +1,7 @@
 use actix_web::web;
 
 mod label;
-use label::{print_label_prescription, print_label_asset};
+use label::{get_label_asset, print_label_asset, print_label_prescription};
 
 use self::label::test_printer;
 
@@ -11,6 +11,10 @@ pub fn config_print(cfg: &mut web::ServiceConfig) {
     cfg.route(
         &format!("{}/label-qr", URL_PATH),
         web::post().to(print_label_asset),
+    );
+    cfg.route(
+        &format!("{}/label-qr", URL_PATH),
+        web::get().to(get_label_asset),
     );
     cfg.route(
         &format!("{}/label-prescription", URL_PATH),
