199d1cfd15727ce39215bd1ce8d40ab4497ab812
roxy-dao <roxy@msupply.foundation>
2025-11-13 08:52:22 +0100

    Endpoint to query if customers have programs configured

--- server/service/src/requisition/program_settings/customer_program_settings/mod.rs
+++ server/service/src/requisition/program_settings/customer_program_settings/mod.rs
@@ -4,7 +4,10 @@ pub mod prepare;
 use prepare::{
     prepare_program_requisition_settings_by_customer, CustomerProgramRequisitionSetting,
 };
-use repository::{ProgramCustomer, ProgramRequisitionSettings, RepositoryError};
+use repository::{
+    EqualFilter, MasterListFilter, NameTagFilter, ProgramCustomer, ProgramRequisitionSettings,
+    ProgramRequisitionSettingsFilter, ProgramRequisitionSettingsRepository, RepositoryError,
+};
 
 use crate::service_provider::ServiceContext;
 
@@ -21,4 +24,21 @@ pub fn get_program_requisition_settings_by_customer(
 ) -> Result<CustomerProgramRequisitionSetting, RepositoryError> {
     let prepared = prepare_program_requisition_settings_by_customer(ctx, customer_name_id)?;
     Ok(prepared)
+}
+
+pub fn has_customer_program_requisition_settings(
+    ctx: &ServiceContext,
+    customer_name_ids: &[String],
+) -> Result<bool, RepositoryError> {
+    let filter = ProgramRequisitionSettingsFilter::new()
+        .master_list(
+            MasterListFilter::new()
+                .exists_for_name_id(EqualFilter::equal_any(customer_name_ids.to_vec())),
+        )
+        .name_tag(NameTagFilter::new().name_id(EqualFilter::equal_any(customer_name_ids.to_vec())));
+
+    let query =
+        ProgramRequisitionSettingsRepository::new(&ctx.connection).query(Some(filter.clone()))?;
+
+    Ok(!query.is_empty())
 }
