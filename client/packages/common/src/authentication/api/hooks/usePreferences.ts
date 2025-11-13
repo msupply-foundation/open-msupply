b3c7f6223b6ff786aee325912251c4059553cb8f
James Brunskill <james@sussol.net>
2025-11-13 14:36:04 +1300

    Change to manual query for preferences
    Source: https://github.com/msupply-foundation/open-msupply/commit/ac4b191e2df46bbf275a16951515209699b44540

--- client/packages/common/src/authentication/api/hooks/usePreferences.ts
+++ client/packages/common/src/authentication/api/hooks/usePreferences.ts
@@ -1,59 +1,22 @@
-import { gql } from 'graphql-tag';
 import {
-  PreferenceKey,
   PREFERENCES_QUERY_KEY,
   PreferencesNode,
   useAuthContext,
   useGql,
   useQuery,
 } from '@openmsupply-client/common';
-
-type PreferencesQuery = {
-  __typename: 'Queries';
-  preferences: PreferencesNode;
-};
-
-// Map of complex preference fields that require subfield selection,
-// If they are a complex type, they must be included in the query or we get an error from the graphql server
-// These must be kept in sync with the GraphQL schema.
-// If a new complex preference type is added, add it here with its subfields.
-const COMPLEX_PREFERENCE_FIELDS: Record<string, string> = {
-  warnWhenMissingRecentStocktake: `
-    enabled
-    maxAge
-    minItems
-  `,
-};
+import { getSdk } from '../operations.generated';
 
 /** Fields undefined until query has loaded */
 export const usePreferences = (): Partial<PreferencesNode> => {
   const { client } = useGql();
   const { storeId } = useAuthContext();
-
-  // Build field list with subfields for complex types
-  const fields = Object.values(PreferenceKey)
-    .map(key => {
-      const subfields = COMPLEX_PREFERENCE_FIELDS[key];
-      return subfields ? `${key} { ${subfields} }` : key;
-    })
-    .join('\n');
-
-  // Custom query, rather than using generated one, so new preferences
-  // will be included without needing to update the generated types.
-  const PreferencesDocument = gql`
-  query preferences {
-    preferences(storeId: "${storeId}") {
-      ${fields}
-    }
-  }
-`;
+  const sdk = getSdk(client);
 
   const { data } = useQuery({
     queryKey: [PREFERENCES_QUERY_KEY, storeId],
     queryFn: async () => {
-      const result = await client.request<PreferencesQuery, undefined>(
-        PreferencesDocument
-      );
+      const result = await sdk.preferences({ storeId });
       return result.preferences;
     },
     // Only refetch when explicitly invalidated (on sync/updating preferences)
