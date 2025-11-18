bc54655182b7dff0218f92e8384d6844b424e676
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2025-11-19 08:32:19 +1300

    simplify threshold validation to only check number of days
    Source: https://github.com/msupply-foundation/open-msupply/commit/7aad635b0c2d4ef8662ecf8aba34e4efdb7cbb8c

--- client/packages/system/src/Manage/Preferences/api/utils.ts
+++ client/packages/system/src/Manage/Preferences/api/utils.ts
@@ -1,20 +1,17 @@
 import {
   LocaleKey,
-  PreferenceKey,
   TypedTFunction,
   UpsertPreferencesInput,
 } from '@openmsupply-client/common';
-import { AdminPreferenceFragment } from './operations.generated';
 import { Dispatch, SetStateAction } from 'react';
 
 export const inputValidation = (
   input: Partial<UpsertPreferencesInput>,
   t: TypedTFunction<LocaleKey>,
   warning: (msg: string) => () => void,
-  preferences?: AdminPreferenceFragment[],
   setActionValid?: Dispatch<SetStateAction<boolean>>
 ): boolean => {
-  const thresholdResult = thresholdValidation(input, t, warning, preferences);
+  const thresholdResult = thresholdValidation(input, t, warning);
 
   // Combine results
   const isValid = thresholdResult; // && otherResult
@@ -27,37 +24,18 @@ export const inputValidation = (
 const thresholdValidation = (
   input: Partial<UpsertPreferencesInput>,
   t: TypedTFunction<LocaleKey>,
-  warning: (msg: string) => () => void,
-  preferences?: AdminPreferenceFragment[]
+  warning: (msg: string) => () => void
 ) => {
   const inputFirstThreshold = input?.firstThresholdForExpiringItems?.[0]?.value;
   const inputSecondThreshold =
     input?.secondThresholdForExpiringItems?.[0]?.value;
 
   // Second threshold should not exceed 30 days
-  if (inputSecondThreshold && inputSecondThreshold > 30) {
-    warning(t('label.second-threshold-exceeds-days'))();
-    return false;
-  }
-
-  const existingFirstThreshold = preferences?.find(
-    pref => pref.key === PreferenceKey.FirstThresholdForExpiringItems
-  )?.value;
-  const existingSecondThreshold = preferences?.find(
-    pref => pref.key === PreferenceKey.SecondThresholdForExpiringItems
-  )?.value;
-
-  const firstThreshold = inputFirstThreshold ?? existingFirstThreshold;
-  const secondThreshold = inputSecondThreshold ?? existingSecondThreshold;
-
-  // Second threshold should not be less than first threshold
   if (
-    firstThreshold != null &&
-    secondThreshold != null &&
-    secondThreshold !== 0 &&
-    secondThreshold < firstThreshold
+    (inputSecondThreshold && inputSecondThreshold > 30) ||
+    (inputFirstThreshold && inputFirstThreshold > 30)
   ) {
-    warning(t('label.second-threshold-is-less-than-first-threshold'))();
+    warning(t('label.threshold-exceeds-days'))();
     return false;
   }
 
