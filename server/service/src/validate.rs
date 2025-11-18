fb89e932d2dab8de4e44bcab5582458c15fa52e4
James Brunskill <james@sussol.net>
2025-11-19 10:50:56 +1300

    Check for name store join when checking other party

--- server/service/src/validate.rs
+++ server/service/src/validate.rs
@@ -38,10 +38,25 @@ pub fn get_other_party(
     store_id: &str,
     other_party_id: &str,
 ) -> Result<Option<Name>, RepositoryError> {
-    NameRepository::new(connection).query_one(
+    let mut results = NameRepository::new(connection).query_by_filter(
         store_id,
         NameFilter::new().id(EqualFilter::equal_to(other_party_id)),
-    )
+    )?;
+
+    if results.is_empty() {
+        return Ok(None);
+    }
+
+    // If names have been merged, there could be multiple results, if so we need to return the one with the name_store_join if it exists
+    let name_with_join = results
+        .iter()
+        .find(|name| name.name_store_join_row.is_some());
+
+    if let Some(name) = name_with_join {
+        return Ok(Some(name.to_owned()));
+    }
+
+    Ok(results.pop())
 }
 
 pub fn check_patient_exists(
