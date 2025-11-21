9a4208f0da0a161baecf960c9f7e5364dae4f185
Chris Petty <chris@msupply.foundation>
2025-11-21 15:01:48 +1300

    silly inconsequential move of ref syntax
    Source: https://github.com/msupply-foundation/open-msupply/commit/d978dfc28f44bedc5af9c6c16462f6430d4e267f

--- server/repository/build.rs
+++ server/repository/build.rs
@@ -10,15 +10,15 @@ fn main() {
     // when migrations are changing mark the template DBs to be recreated
     // Only search in specific directories, in particular avoiding ../target/ which has temporary files that may cause glob to error
     let patterns = [
-        &format!("../{TEST_OUTPUT_DIR}"),
-        &format!("../repository/**/{TEST_OUTPUT_DIR}"),
-        &format!("../cli/**/{TEST_OUTPUT_DIR}"),
-        &format!("../graphql/**/{TEST_OUTPUT_DIR}"),
-        &format!("../service/**/{TEST_OUTPUT_DIR}"),
+        format!("../{TEST_OUTPUT_DIR}"),
+        format!("../repository/**/{TEST_OUTPUT_DIR}"),
+        format!("../cli/**/{TEST_OUTPUT_DIR}"),
+        format!("../graphql/**/{TEST_OUTPUT_DIR}"),
+        format!("../service/**/{TEST_OUTPUT_DIR}"),
     ];
 
     for pattern in patterns {
-        for entry in glob(pattern).expect("Failed to read glob pattern") {
+        for entry in glob(&pattern).expect("Failed to read glob pattern") {
             match entry {
                 Ok(path) => {
                     if !path.to_string_lossy().contains("/target/") {
