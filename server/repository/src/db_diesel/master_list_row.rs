73e8c3d925e1b73bb72222df98c8818e694eb048
Andrei <andrei.evguenov@gmail.com>
2025-11-21 17:15:14 +1300

    Remove comment about table not in changelog
    Source: https://github.com/msupply-foundation/open-msupply/commit/85bd143d6ba90c26b96633818d6c8211909c0b97

--- server/repository/src/db_diesel/master_list_row.rs
+++ server/repository/src/db_diesel/master_list_row.rs
@@ -82,7 +82,7 @@ impl<'a> MasterListRowRepository<'a> {
 impl Upsert for MasterListRow {
     fn upsert(&self, con: &StorageConnection) -> Result<Option<i64>, RepositoryError> {
         let change_log_id = MasterListRowRepository::new(con).upsert_one(self)?;
-        Ok(Some(change_log_id)) // Table not in Changelog
+        Ok(Some(change_log_id))
     }
 
     // Test only
