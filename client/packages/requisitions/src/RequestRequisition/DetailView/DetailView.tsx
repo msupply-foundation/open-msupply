e50215c46b50002a150abf17b623a169a93b32e1
raviSussol <ravi.shrestha@sussol.net>
2025-11-24 16:56:49 +0545

    9698 move upload button to appbar container
    Source: https://github.com/msupply-foundation/open-msupply/commit/eef60a0b29ac8d22f2edd086d5f88fee8a5d197b

--- client/packages/requisitions/src/RequestRequisition/DetailView/DetailView.tsx
+++ client/packages/requisitions/src/RequestRequisition/DetailView/DetailView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import {
   DetailViewSkeleton,
   AlertModal,
@@ -10,12 +10,18 @@ import {
   useAuthContext,
   useBreadcrumbs,
   useEditModal,
+  useToggle,
   useNonPaginatedMaterialTable,
   usePluginProvider,
   NothingHere,
   MaterialTable,
+  useUrlQuery,
 } from '@openmsupply-client/common';
-import { ActivityLogList } from '@openmsupply-client/system';
+import {
+  ActivityLogList,
+  DocumentsTable,
+  UploadDocumentModal,
+} from '@openmsupply-client/system';
 import { RequestLineFragment, useHideOverStocked, useRequest } from '../api';
 import { Toolbar } from './Toolbar';
 import { Footer } from './Footer';
@@ -23,11 +29,12 @@ import { AppBarButtons } from './AppBarButtons';
 import { SidePanel } from './SidePanel';
 import { AppRoute } from '@openmsupply-client/config';
 import { RequestRequisitionLineErrorProvider } from '../context';
-import { IndicatorsTab, Documents } from './Tabs';
+import { IndicatorsTab } from './IndicatorsTab';
 import { buildIndicatorEditRoute } from './utils';
 import { RequestLineEditModal } from './RequestLineEdit';
 import { useRequestColumns } from './columns';
 import { isRequestLinePlaceholderRow } from '../../utils';
+import { InternalOrderDetailTabs } from './types';
 
 export const DetailView = () => {
   const t = useTranslation();
@@ -44,6 +51,7 @@ export const DetailView = () => {
 
   const { data, isLoading, invalidateQueries } = useRequest.document.get();
   const isDisabled = useRequest.utils.isDisabled();
+  const uploadDocumentController = useToggle();
   const { data: programIndicators, isLoading: isProgramIndicatorsLoading } =
     useRequest.document.indicators(
       store?.nameId ?? '',
@@ -51,6 +59,17 @@ export const DetailView = () => {
       data?.program?.id ?? '',
       !!data
     );
+  const { urlQuery, updateQuery } = useUrlQuery();
+  const tab = urlQuery['tab'] ?? InternalOrderDetailTabs.Details;
+
+  const deletableDocumentIds = useMemo(() => {
+    if (data?.status === RequisitionNodeStatus.Finalised) {
+      return new Set<string>();
+    }
+    // Request requisition can't have documents linked to response requisition.
+    // So all documents linked to request requisition are deletable.
+    return undefined;
+  }, [data?.status]);
 
   const onRowClick = useCallback(
     (line: RequestLineFragment) => onOpen(line.item.id),
@@ -81,6 +100,12 @@ export const DetailView = () => {
   }, [setCustomBreadcrumbs, data?.requisitionNumber]);
 
   const onAddItem = () => onOpen();
+  const onOpenUploadModal = () => {
+    uploadDocumentController.toggleOn();
+    if (tab !== InternalOrderDetailTabs.Documents) {
+      updateQuery({ tab: InternalOrderDetailTabs.Documents });
+    }
+  };
 
   const { lines, itemFilter, isError, isFetching } = useRequest.line.list();
   const { on } = useHideOverStocked();
@@ -148,10 +173,12 @@ export const DetailView = () => {
     },
     {
       Component: (
-        <Documents
-          data={data}
-          disable={data.status === RequisitionNodeStatus.Finalised}
+        <DocumentsTable
+          recordId={data?.id ?? ''}
+          documents={data?.documents?.nodes ?? []}
+          tableName="requisition"
           invalidateQueries={invalidateQueries}
+          deletableDocumentIds={deletableDocumentIds}
         />
       ),
       value: t('label.documents'),
@@ -187,6 +214,7 @@ export const DetailView = () => {
       <AppBarButtons
         isDisabled={!data || isDisabled}
         onAddItem={onAddItem}
+        openUploadModal={onOpenUploadModal}
         showIndicators={showIndicatorTab}
       />
       <Toolbar />
@@ -206,6 +234,16 @@ export const DetailView = () => {
           onClose={onClose}
           mode={mode}
           store={store}
+        />
+      )}
+
+      {uploadDocumentController.isOn && (
+        <UploadDocumentModal
+          isOn={uploadDocumentController.isOn}
+          toggleOff={uploadDocumentController.toggleOff}
+          recordId={data?.id ?? ''}
+          tableName="requisition"
+          invalidateQueries={invalidateQueries}
         />
       )}
     </RequestRequisitionLineErrorProvider>
