561ab61d241ce2d9bf0d734f798e78fd6d82e1a5
roxy-dao <roxy@msupply.foundation>
2025-11-27 15:16:37 +0100

    Rest of list views
    Source: https://github.com/msupply-foundation/open-msupply/commit/30eb986388e6217079a91a139a2ce52e082d7bb5

--- client/packages/system/src/Asset/ListView/AppBarButtons.tsx
+++ client/packages/system/src/Asset/ListView/AppBarButtons.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  DownloadIcon,
   useNotification,
   AppBarButtonsPortal,
   Grid,
@@ -13,9 +12,9 @@ import {
   useNavigate,
   BaseButton,
   EditIcon,
-  useExportCSV,
 } from '@openmsupply-client/common';
 import { AppRoute } from '@openmsupply-client/config';
+import { ExportSelector } from '@openmsupply-client/system';
 import { AssetCatalogueItemFragment } from '../api';
 import { assetCatalogueItemsListToCsv } from '../utils';
 
@@ -30,21 +29,19 @@ export const AppBarButtonsComponent = ({
   const isCentralServer = useIsCentralServerApi();
   const { error } = useNotification();
   const navigate = useNavigate();
-  const exportCSV = useExportCSV();
 
-  const csvExport = async () => {
-    if (!assets || !assets?.length) {
-      error(t('error.no-data'))();
-      return;
-    }
-
-    const csv = assetCatalogueItemsListToCsv(assets, t);
-    exportCSV(csv, t('filename.asset-categories'));
-  };
   const path = RouteBuilder.create(AppRoute.Catalogue)
     .addPart(AppRoute.Assets)
     .addPart(AppRoute.LogReasons)
     .build();
+
+  const getCsvData = async () => {
+    if (!assets?.length) {
+      error(t('error.no-data'))();
+      return null;
+    }
+    return assetCatalogueItemsListToCsv(assets, t);
+  };
 
   return (
     <AppBarButtonsPortal>
@@ -56,13 +53,10 @@ export const AppBarButtonsComponent = ({
             onClick={importModalController.toggleOn}
           />
         )}
-        <BaseButton
-          startIcon={<DownloadIcon />}
-          variant="outlined"
-          onClick={csvExport}
-        >
-          {t('button.export')}
-        </BaseButton>
+        <ExportSelector
+          getCsvData={getCsvData}
+          filename={t('filename.asset-categories')}
+        />
         {isCentralServer && (
           <BaseButton
             startIcon={<EditIcon />}
