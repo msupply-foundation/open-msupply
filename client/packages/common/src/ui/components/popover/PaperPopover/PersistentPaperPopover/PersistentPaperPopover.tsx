d0ba68261ce135fbe46880f7f1ac05aabb2be4a1
roxy-dao <roxy@msupply.foundation>
2025-12-02 13:47:07 +0100

    Use ref to close popover on selection

--- client/packages/common/src/ui/components/popover/PaperPopover/PersistentPaperPopover/PersistentPaperPopover.tsx
+++ client/packages/common/src/ui/components/popover/PaperPopover/PersistentPaperPopover/PersistentPaperPopover.tsx
@@ -1,59 +1,68 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useImperativeHandle } from 'react';
 import Paper from '@mui/material/Paper';
 import { PaperPopoverProps, usePopover } from '../..';
 import { CloseIcon } from '@common/icons';
 import { Box, ClickAwayListener, IconButton } from '@mui/material';
 
-export function PersistentPaperPopover({
-  children,
-  Content,
-  paperProps,
-  width,
-  height,
-  placement = 'left',
-}: PropsWithChildren<PaperPopoverProps>) {
-  const { show, hide, Popover } = usePopover({
-    hideDebounceDelay: 0,
-  });
-
-  return (
-    <>
-      <div
-        style={{
-          cursor: 'pointer',
-          // prevents the blue highlight on click on mobile browser
-          WebkitTapHighlightColor: 'transparent',
-        }}
-        onClick={show}
-      >
-        {children}
-      </div>
-      <Popover placement={placement}>
-        <Box>
-          <IconButton
-            color="primary"
-            onClick={hide}
-            size="small"
-            style={{ position: 'absolute', right: 4, top: 4 }}
-          >
-            <CloseIcon />
-          </IconButton>
-          <ClickAwayListener onClickAway={hide}>
-            <Paper
-              sx={{
-                height: height ? `${height}px` : 'auto',
-                width: width ? `${width}px` : 'auto',
-                borderRadius: '16px',
-                boxShadow: theme => theme.shadows[7],
-                ...paperProps?.sx,
-              }}
-              {...paperProps}
-            >
-              {Content}
-            </Paper>
-          </ClickAwayListener>
-        </Box>
-      </Popover>
-    </>
-  );
+export interface PersistentPaperPopoverRef {
+  hide: () => void;
 }
+
+export const PersistentPaperPopover = React.forwardRef<
+  PersistentPaperPopoverRef,
+  PropsWithChildren<PaperPopoverProps>
+>(
+  (
+    { children, Content, paperProps, width, height, placement = 'left' },
+    ref
+  ) => {
+    const { show, hide, Popover } = usePopover({
+      hideDebounceDelay: 0,
+    });
+
+    useImperativeHandle(ref, () => ({
+      hide,
+    }));
+
+    return (
+      <>
+        <div
+          style={{
+            cursor: 'pointer',
+            // prevents the blue highlight on click on mobile browser
+            WebkitTapHighlightColor: 'transparent',
+          }}
+          onClick={show}
+        >
+          {children}
+        </div>
+        <Popover placement={placement}>
+          <Box>
+            <IconButton
+              color="primary"
+              onClick={hide}
+              size="small"
+              style={{ position: 'absolute', right: 4, top: 4 }}
+            >
+              <CloseIcon />
+            </IconButton>
+            <ClickAwayListener onClickAway={hide}>
+              <Paper
+                sx={{
+                  height: height ? `${height}px` : 'auto',
+                  width: width ? `${width}px` : 'auto',
+                  borderRadius: '16px',
+                  boxShadow: theme => theme.shadows[7],
+                  ...paperProps?.sx,
+                }}
+                {...paperProps}
+              >
+                {Content}
+              </Paper>
+            </ClickAwayListener>
+          </Box>
+        </Popover>
+      </>
+    );
+  }
+);
