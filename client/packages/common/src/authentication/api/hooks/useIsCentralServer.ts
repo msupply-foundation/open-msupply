0a1472b1d6b95ef8011c16faf741d147586d67f4
John Geronimo <johnageronimo1@gmail.com>
2025-12-16 16:20:21 +1300

    fix suspense and init
    Source: https://github.com/msupply-foundation/open-msupply/commit/1667d82fba054c70a8e14fac42dfcd5b2048aea0

--- client/packages/common/src/authentication/api/hooks/useIsCentralServer.ts
+++ client/packages/common/src/authentication/api/hooks/useIsCentralServer.ts
@@ -2,9 +2,11 @@ import { useQuery } from 'react-query';
 import { useAuthApi } from './useAuthApi';
 import { useNotification } from '@common/hooks';
 import { useTranslation } from '@common/intl';
+import { useAuthContext } from '../../AuthContext';
 
 export const useIsCentralServerApi = () => {
   const api = useAuthApi();
+  const { storeId } = useAuthContext();
   // api.keys.isCentralServer and "refetchOnMount: false" should guarantee that this is called just once, on page load
   const { data } = useQuery(
     api.keys.isCentralServer,
@@ -14,6 +16,7 @@ export const useIsCentralServerApi = () => {
       cacheTime: Infinity,
       staleTime: Infinity,
       suspense: true,
+      enabled: !!storeId,
     }
   );
   return !!data;
