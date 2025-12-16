e78553bb90a5de4bc049afc77c4d0477a2a6881e
Chris Petty <chris@msupply.foundation>
2025-12-16 16:52:31 +1300

    show default price per unit
    Source: https://github.com/msupply-foundation/open-msupply/commit/590cf3274912e605409ee538b6018c221259031a

--- client/packages/requisitions/src/ResponseRequisition/DetailView/columns.tsx
+++ client/packages/requisitions/src/ResponseRequisition/DetailView/columns.tsx
@@ -216,6 +216,13 @@ export const useResponseColumns = () => {
         enableSorting: true,
       },
       {
+        header: t('label.default-price-per-unit'),
+        description: t('description.default-price-per-unit'),
+        accessorKey: 'pricePerUnit',
+        columnType: ColumnType.Currency,
+        includeColumn: showIndicativePriceInRequisitions,
+      },
+      {
         header: t('label.indicative-price'),
         description: t('description.indicative-price'),
         accessorFn: row => row.requestedQuantity * (row?.pricePerUnit || 0),
