b266364d183a28a0f787786a3d3e6db0e78b5992
Brian <bk.dev@icloud.com>
2025-12-17 15:14:56 +1300

    update translation input table

--- client/packages/common/src/ui/layout/tables/material-react-table/components/TextInputCell.tsx
+++ client/packages/common/src/ui/layout/tables/material-react-table/components/TextInputCell.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BasicTextInput } from '@common/components';
 import { MRT_Cell, MRT_RowData } from 'material-react-table';
 import { useBufferState } from '@common/hooks';
+import { SxProps, Theme } from '@common/styles';
 
 interface TextInputCell<T extends MRT_RowData> {
   cell: MRT_Cell<T>;
   updateFn: (value: string) => void;
   disabled?: boolean;
   autoFocus?: boolean;
+  multiline?: boolean;
+  sx?: SxProps<Theme>;
 }
 
 export const TextInputCell = <T extends MRT_RowData>({
@@ -15,6 +18,8 @@ export const TextInputCell = <T extends MRT_RowData>({
   updateFn,
   disabled = false,
   autoFocus = false,
+  multiline = false,
+  sx,
 }: TextInputCell<T>) => {
   const value = cell.getValue<string>();
   const [buffer, setBuffer] = useBufferState(value);
@@ -29,8 +34,9 @@ export const TextInputCell = <T extends MRT_RowData>({
         updateFn(newValue);
       }}
       autoFocus={autoFocus}
+      multiline={multiline}
       fullWidth
-
+      sx={sx}
       // Enable these as required?
       // required={isRequired}
       // multiline={multiline}
