71f8ff55bc438ce1638c93123fa3c9428f3d42b8
roxy-dao <roxy@msupply.foundation>
2025-12-19 13:34:17 +0100

    Remove clean nodes from PO form

--- standard_forms/purchase-order/latest/convert_data_js/src/convert_data.js
+++ standard_forms/purchase-order/latest/convert_data_js/src/convert_data.js
@@ -1,24 +1,23 @@
-import { cleanUpNodes, sortNodes } from "../../../../../standard_reports/utils";
+import { sortNodes } from '../../../../../standard_reports/utils';
 
 function convert_data(queryResponse) {
   let nodes = queryResponse.data.purchaseOrder.lines.nodes;
   for (let node in nodes) {
-    let pricePerPack = nodes[node]["pricePerPackAfterDiscount"];
-    let packSize = nodes[node]["requestedPackSize"];
-    let unitQuantity = nodes[node]["requestedNumberOfUnits"];
+    let pricePerPack = nodes[node]['pricePerPackAfterDiscount'];
+    let packSize = nodes[node]['requestedPackSize'];
+    let unitQuantity = nodes[node]['requestedNumberOfUnits'];
     let numberOfPacks = packSize > 0 ? unitQuantity / packSize : 0;
     let lineCost = pricePerPack * numberOfPacks;
-    nodes[node]["lineCost"] = lineCost.toFixed(2);
+    nodes[node]['lineCost'] = lineCost.toFixed(2);
 
-    nodes[node]["requestedNumberOfPacks"] = numberOfPacks;
+    nodes[node]['requestedNumberOfPacks'] = numberOfPacks;
   }
 
-  // Clean and sort each line:
-  let cleanNodes = cleanUpNodes(queryResponse.data.purchaseOrder.lines.nodes);
+  // Sort each line:
   queryResponse.data.purchaseOrder.lines.nodes = sortNodes(
-    cleanNodes,
-    "lineNumber",
-    "asc"
+    nodes,
+    'lineNumber',
+    'asc'
   );
 
   return queryResponse;
