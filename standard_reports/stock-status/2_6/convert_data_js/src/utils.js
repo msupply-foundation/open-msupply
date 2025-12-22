4a927ddd126a9d855dbe64ac1ca3fcdd93ad935d
roxy-dao <roxy@msupply.foundation>
2025-12-22 08:42:52 +0100

    Bump patch

--- standard_reports/stock-status/2_6/convert_data_js/src/utils.js
+++
@@ -1,8 +0,0 @@
-import { sortNodes } from '../../../../utils';
-
-const processItemLines = (nodes, sort, dir) => {
-  let sortedNodes = sortNodes(nodes, sort, dir);
-  return sortedNodes;
-};
-
-export { processItemLines };
