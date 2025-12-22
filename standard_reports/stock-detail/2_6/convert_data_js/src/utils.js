82ddb714e80964b769de82d19a529f0797365d7c
roxy-dao <roxy@msupply.foundation>
2025-12-22 08:42:52 +0100

    Bump patch
    Source: https://github.com/msupply-foundation/open-msupply/commit/4a927ddd126a9d855dbe64ac1ca3fcdd93ad935d

--- standard_reports/stock-detail/2_6/convert_data_js/src/utils.js
+++
@@ -1,8 +0,0 @@
-import { sortNodes } from '../../../../utils';
-
-const processStockLines = (nodes, sort, dir) => {
-  let sortedNodes = sortNodes(nodes, sort, dir);
-  return sortedNodes;
-};
-
-export { processStockLines };
