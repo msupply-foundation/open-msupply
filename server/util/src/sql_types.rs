0d9376d86f9190e15da56a41efa5a49dbf1a3fea
Andrei <andrei.evguenov@gmail.com>
2025-12-23 15:25:04 +1300

    Remove timezone casting to localtime in postgres
    Source: https://github.com/msupply-foundation/open-msupply/commit/35e822be9bdc8475146745a89b39776d72d2a1dd

--- server/util/src/sql_types.rs
+++ server/util/src/sql_types.rs
@@ -1,6 +1,6 @@
 pub fn sql_utc_datetime_as_local_date(is_postgres: bool, column: &str) -> String {
     if is_postgres {
-        format!("date({column} AT TIME ZONE 'UTC' AT TIME ZONE localtime)")
+        format!("date({column} AT TIME ZONE 'UTC')")
     } else {
         format!("date({column}, 'localtime')")
     }
