13713a25e37c82bfff6f9ed9c1433c1f538ec312
Brian <bk.dev@icloud.com>
2026-01-06 12:25:05 +1300

    add default version

--- server/repository/src/migrations/v2_16_00/add_version_to_plugins.rs
+++ server/repository/src/migrations/v2_16_00/add_version_to_plugins.rs
@@ -10,10 +10,9 @@ impl MigrationFragment for Migrate {
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
         sql!(
             connection,
-            // TODO migration fails if theres any installed plugins, is that a problem?
             r#"
-                ALTER TABLE backend_plugin ADD COLUMN version TEXT NOT NULL;
-                ALTER TABLE frontend_plugin ADD COLUMN version TEXT NOT NULL;
+                ALTER TABLE backend_plugin ADD COLUMN version TEXT NOT NULL DEFAULT '1.0.0';
+                ALTER TABLE frontend_plugin ADD COLUMN version TEXT NOT NULL DEFAULT '1.0.0';
             "#
         )?;
 
