ddc1d92a2a382426abddef3002418277c2f2983e
mark-prins <mark@malcolmprins.net>
2026-01-06 13:13:24 +1300

    fix logout issue
    Source: https://github.com/msupply-foundation/open-msupply/commit/5accc9370e149c3cd842ab10a8c6fa2e9f7bb69c

--- client/packages/common/src/authentication/api/hooks/usePreferences.ts
+++ client/packages/common/src/authentication/api/hooks/usePreferences.ts
@@ -10,11 +10,13 @@ import { getSdk } from '../operations.generated';
 /** Fields undefined until query has loaded */
 export const usePreferences = (): Partial<PreferencesNode> => {
   const { client } = useGql();
-  const { storeId } = useAuthContext();
+  const { storeId, token } = useAuthContext();
   const sdk = getSdk(client);
 
   const { data } = useQuery({
-    queryKey: [PREFERENCES_QUERY_KEY, storeId],
+    // Adding the token in as this query will fail and log the user out if the token is incorrect
+    // Am having issues with an old token being used here on android after a logout/login cycle
+    queryKey: [PREFERENCES_QUERY_KEY, storeId, token],
     queryFn: async () => {
       const result = await sdk.preferences({ storeId });
       return result.preferences;
