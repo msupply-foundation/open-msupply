cf7b2ccacb0a03947a85e4eea43f1bac0cfecd47
Karim Sawaneh <sakjrn@gmail.com>
2026-01-07 14:35:17 +0100

    Fixes msupply-foundation/open-msupply#4918: avoid bogus ellipsis for float noise
    Source: https://github.com/msupply-foundation/open-msupply/commit/20eefc83cad1b199e790fe6d71d504f8cc559461

--- client/packages/common/src/utils/numbers/NumUtils.test.ts
+++ client/packages/common/src/utils/numbers/NumUtils.test.ts
@@ -20,4 +20,25 @@ describe('NumUtils', () => {
     expect(NumUtils.parseString('40', 1, 10)).toBe(10);
     expect(NumUtils.parseString('4.56')).toBe(4.56);
   });
+
+  it('hasMoreThanTwoDp', () => {
+    expect(NumUtils.hasMoreThanTwoDp(4.4)).toBe(false);
+    expect(NumUtils.hasMoreThanTwoDp(4.40000000000006)).toBe(false);
+    expect(NumUtils.hasMoreThanTwoDp(4.401)).toBe(true);
+    expect(NumUtils.hasMoreThanTwoDp(0.01)).toBe(false);
+    expect(NumUtils.hasMoreThanTwoDp(0.001)).toBe(true);
+  });
+
+  it('hasMoreThanDp', () => {
+    expect(NumUtils.hasMoreThanDp(4, 0)).toBe(false);
+    expect(NumUtils.hasMoreThanDp(4.4, 0)).toBe(true);
+
+    expect(NumUtils.hasMoreThanDp(4.4, 2)).toBe(false);
+    expect(NumUtils.hasMoreThanDp(4.40000000000006, 2)).toBe(false);
+    expect(NumUtils.hasMoreThanDp(4.41, 2)).toBe(false);
+    expect(NumUtils.hasMoreThanDp(4.411, 2)).toBe(true);
+
+    expect(NumUtils.hasMoreThanDp(0.1234, 3)).toBe(true);
+    expect(NumUtils.hasMoreThanDp(0.123, 3)).toBe(false);
+  });
 });
