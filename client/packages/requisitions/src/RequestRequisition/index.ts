3193c6069c58a6c5877684a1cafb13ce25e87061
roxy-dao <roxy@msupply.foundation>
2026-01-09 12:14:36 +0100

    Allow program orders from order now button
    Source: https://github.com/msupply-foundation/open-msupply/commit/aa02d41827b1478a47fb6776a117dc3e742412c7

--- client/packages/requisitions/src/RequestRequisition/index.ts
+++ client/packages/requisitions/src/RequestRequisition/index.ts
@@ -1,3 +1,4 @@
 export { useRequest } from './api';
 export * from './ListView';
 export * from './DetailView';
+export { CreateRequisitionModal as RequestRequisitionCreateModal } from './ListView/CreateRequisitionModal';
--- client/packages/requisitions/src/index.ts
+++ client/packages/requisitions/src/index.ts
@@ -1,12 +1,14 @@
 export {
   ListView as RequestRequisitionListView,
   DetailView as RequestRequisitionDetailView,
+  RequestRequisitionCreateModal,
   useRequest,
 } from './RequestRequisition';
 export {
   ListView as ResponseRequisitionListView,
   DetailView as ResponseRequisitionDetailView,
 } from './ResponseRequisition';
+export { NewRequisitionType } from './types';
 export * from './RnRForms/api';
 
 export { RequisitionService } from './RequisitionService';
