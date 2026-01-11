1a3b2c0481bebefbc3428ceafd6584d9c5acb419
Andrei <andrei.evguenov@gmail.com>
2026-01-12 10:59:55 +1300

    Error mapping

--- server/repository/src/migrations/mod.rs
+++ server/repository/src/migrations/mod.rs
@@ -177,19 +177,17 @@ pub fn migrate(
     ];
 
     // Check if the database has been initialised, if not run the base sql to kick start the process
-    if is_empty_db(connection).map_err(|e| MigrationError::InitializeDatabaseError(e.into()))? {
+    if is_empty_db(connection)? {
         log::info!("Empty database detected, creating base schema...");
 
         if to_version.is_some() {
             log::info!("Target version specified, initializing earliest base schema");
             // We always use the earliest base schema when migrating to a specific version
             // This is the easiest way to makes sure migration tests can still run.
-            initialize_earliest_db(connection)
-                .map_err(|e| MigrationError::InitializeDatabaseError(e.into()))?;
+            initialize_earliest_db(connection)?;
         } else {
             log::info!("No target version specified, initializing latest base schema");
-            initialize_latest_db(connection)
-                .map_err(|e| MigrationError::InitializeDatabaseError(e.into()))?;
+            initialize_latest_db(connection)?;
         }
         log::info!("Base schema...installed");
     }
