d247e6698ba6aa0e3c698d2b4362700d409f0b73
raviSussol <ravi.shrestha@sussol.net>
2026-01-20 08:50:59 +0545

    build test v2.15.2

--- build/windows/omsupply-prepare.bat
+++ build/windows/omsupply-prepare.bat
@@ -1,4 +1,10 @@
 @ECHO ##### Prepare omsupply build #####
+
+@REM Clean up previous build artifacts
+@if exist "client\packages\host\dist" (
+    rd /s /q "client\packages\host\dist" 2>nul
+)
+
 cd "client" && yarn install --force --frozen-lockfile && yarn build
 @if %errorlevel% neq 0 (
     @ECHO ERROR: Failed to prepare client
