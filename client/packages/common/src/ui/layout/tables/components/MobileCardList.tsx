93bfeb616d7e66d5f0960fbfdfb730b16b08a371
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2026-01-22 15:40:42 +1300

    Basic functionality
    Source: https://github.com/msupply-foundation/open-msupply/commit/90106f417f54048c6a7d73d23de4e0ffcb0b6ed2

---
+++ client/packages/common/src/ui/layout/tables/components/MobileCardList.tsx
@@ -0,0 +1,54 @@
+import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
+import {
+  flexRender,
+  MRT_Row,
+  MRT_RowData,
+  MRT_TableInstance,
+  MRT_TopToolbar,
+} from 'material-react-table';
+import React from 'react';
+
+const TableCard = <T extends MRT_RowData>({ row }: { row: MRT_Row<T> }) => {
+  const cells = row.getVisibleCells();
+  return (
+    <Card sx={{ overflow: 'visible', maxWidth: 400, width: '100%' }}>
+      <CardContent>
+        <Stack spacing={0.5}>
+          {cells.map(cell => {
+            // Don't show "Select" checkbox in mobile card view
+            if (cell.column.id === 'mrt-row-select') return null;
+
+            const content = cell.column.columnDef.Cell
+              ? flexRender(cell.column.columnDef.Cell, cell.getContext())
+              : cell.renderValue();
+            return (
+              <Box key={cell.id} display="flex" justifyContent="space-between">
+                <Typography color="text.secondary">
+                  {flexRender(cell.column.columnDef.header, cell.getContext())}
+                </Typography>
+                <Box>{content}</Box>
+              </Box>
+            );
+          })}
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const MobileCardList = <T extends MRT_RowData>({
+  table,
+}: {
+  table: MRT_TableInstance<T>;
+}) => {
+  return (
+    <>
+      <Stack spacing={2} sx={{ width: '100%', alignItems: 'center', m: 2 }}>
+        <MRT_TopToolbar table={table} />
+        {table.getRowModel().rows.map(row => (
+          <TableCard key={row.id} row={row} />
+        ))}
+      </Stack>
+    </>
+  );
+};
