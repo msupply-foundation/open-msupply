4f7fe64924a351851cf104cfded25cf0afa3056a
aneeshthunga26 <150754185+aneeshthunga26@users.noreply.github.com>
2026-01-23 16:48:44 +1300

    test: trigger marker re-run

--- server/repository/build.rs
+++ server/repository/build.rs
@@ -29,7 +29,9 @@ fn main() {
         }
     }
 
+    
     // Removed cargo:rerun-if-changed directives to ensure marker files are always created on every build
+    println!("cargo:rerun-if-changed=lol.rs");
     // println!("cargo:rerun-if-changed=src/migrations");
     // println!("cargo:rerun-if-changed=src/mock");
 }
