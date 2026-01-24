4a505c42f0d41425b97179e0389232d6245bc941
aneeshthunga26 <150754185+aneeshthunga26@users.noreply.github.com>
2026-01-24 19:37:56 +1300

    fix: simplify build script for marker generation and create test directory

--- server/repository/build.rs
+++ server/repository/build.rs
@@ -1,35 +1,19 @@
-use glob::glob;
 use std::fs;
 use std::path::Path;
 
 #[path = "src/test_db/constants.rs"]
 mod constants;
-use crate::constants::{TEMPLATE_MARKER_FILE_SQLITE, TEMPLATE_MARKER_FILE_POSTGRES, TEST_OUTPUT_DIR};
+use crate::constants::{
+    TEMPLATE_MARKER_FILE_POSTGRES, TEMPLATE_MARKER_FILE_SQLITE, TEST_OUTPUT_DIR,
+};
 
 fn main() {
-    // when migrations are changing mark the template DBs to be recreated
-    // Only search in specific directories, in particular avoiding ../target/ which has temporary files that may cause glob to error
-    let patterns = [
-        format!("../{TEST_OUTPUT_DIR}"),
-        format!("../repository/**/{TEST_OUTPUT_DIR}"),
-        format!("../cli/**/{TEST_OUTPUT_DIR}"),
-        format!("../graphql/**/{TEST_OUTPUT_DIR}"),
-        format!("../service/**/{TEST_OUTPUT_DIR}"),
-    ];
+    let markers_path = format!("../repository/{TEST_OUTPUT_DIR}");
+    fs::create_dir_all(&markers_path).ok();
 
-    for pattern in patterns {
-        for entry in glob(&pattern).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => {
-                    fs::File::create(Path::new(&path).join(TEMPLATE_MARKER_FILE_SQLITE)).unwrap();
-                    fs::File::create(Path::new(&path).join(TEMPLATE_MARKER_FILE_POSTGRES)).unwrap();
-                }
-                Err(e) => println!("cargo:warning={:?}", e),
-            }
-        }
-    }
+    fs::File::create(Path::new(&markers_path).join(TEMPLATE_MARKER_FILE_SQLITE)).unwrap();
+    fs::File::create(Path::new(&markers_path).join(TEMPLATE_MARKER_FILE_POSTGRES)).unwrap();
 
-    
     // Removed cargo:rerun-if-changed directives to ensure marker files are always created on every build
     println!("cargo:rerun-if-changed=/migrations");
     // println!("cargo:rerun-if-changed=src/migrations");
