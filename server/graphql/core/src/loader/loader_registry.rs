6f5d0e30b5295316cffb662893391bf600fb5725
roxy-dao <roxy@msupply.foundation>
2026-01-26 10:10:32 +0100

    Loader to get available volume for requisition lines
    Source: https://github.com/msupply-foundation/open-msupply/commit/422e4b5181586ad1164265d9525895319ba3b6b4

--- server/graphql/core/src/loader/loader_registry.rs
+++ server/graphql/core/src/loader/loader_registry.rs
@@ -578,5 +578,12 @@ pub async fn get_loaders(
         tokio::spawn,
     ));
 
+    loaders.insert(DataLoader::new(
+        AvailableVolumeOnRequisitionLineByTypeLoader {
+            service_provider: service_provider.clone(),
+        },
+        tokio::spawn,
+    ));
+
     loaders
 }
