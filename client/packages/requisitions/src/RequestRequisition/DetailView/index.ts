a5c606d7b0012111dd7dfd7e6a2843892b1edd7e
roxy-dao <roxy@msupply.foundation>
2026-01-29 13:35:35 +0100

    Hide if plugin wants to hide

--- client/packages/requisitions/src/RequestRequisition/DetailView/index.ts
+++ client/packages/requisitions/src/RequestRequisition/DetailView/index.ts
@@ -1,2 +1,7 @@
 export { DetailView } from './DetailView';
 export { IndicatorEditPage } from './IndicatorEdit/IndicatorEditPage';
+export {
+  STOCK_DISTRIBUTION_INFO,
+  CONSUMPTION_HISTORY_INFO,
+  STOCK_EVOLUTION_INFO,
+} from './utils';
