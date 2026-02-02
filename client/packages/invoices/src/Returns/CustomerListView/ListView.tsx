3099c7d2596295c7571087a61134cbafe08df1c5
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2026-02-02 16:13:53 +1300

    customer returns list view filters
    Source: https://github.com/msupply-foundation/open-msupply/commit/00b94ab1cd3dbd730b9e60edfd4637bc56300402

--- client/packages/invoices/src/Returns/CustomerListView/ListView.tsx
+++ client/packages/invoices/src/Returns/CustomerListView/ListView.tsx
@@ -21,6 +21,7 @@ import {
 import { getStatusTranslator, isInboundListItemDisabled } from '../../utils';
 import { AppBarButtons } from './AppBarButtons';
 import { CustomerReturnRowFragment, useReturns } from '../api';
+import { Toolbar } from './Toolbar';
 import { Footer } from './Footer';
 
 export const CustomerReturnListView = () => {
@@ -148,6 +149,7 @@ export const CustomerReturnListView = () => {
 
   return (
     <>
+      <Toolbar />
       <AppBarButtons modalController={modalController} onNew={handleClick} />
 
       <MaterialTable table={table} />
