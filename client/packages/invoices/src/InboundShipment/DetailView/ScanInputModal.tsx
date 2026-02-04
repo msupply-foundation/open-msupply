02ef7f1dc653e112d9ea0d170e4f1423c5a817c6
Carl Smith <5456533+CarlosNZ@users.noreply.github.com>
2026-02-04 13:17:30 +1300

    Improvements
    Source: https://github.com/msupply-foundation/open-msupply/commit/f57abe0c82d8888ffb1321b0d381ba4e87cc6d6a

--- client/packages/invoices/src/InboundShipment/DetailView/ScanInputModal.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/ScanInputModal.tsx
@@ -11,15 +11,26 @@ import {
   NumericTextInput,
   DatePicker,
   Alert,
+  useDialog,
+  Box,
+  TypedTFunction,
+  LocaleKey,
 } from '@openmsupply-client/common';
-import { ScanResult, useBarcodeScannerContext } from '@common/utils';
+import { FnUtils, ScanResult, useBarcodeScannerContext } from '@common/utils';
 import React, { useEffect, useRef, useState } from 'react';
 import { useOutbound } from '../../OutboundShipment/api';
-import { BarcodeNode, InvoiceLineNode } from '@common/types';
+import {
+  BarcodeNode,
+  InvoiceLineNode,
+  InvoiceLineNodeType,
+} from '@common/types';
 import {
   ItemStockOnHandFragment,
   StockItemSearchInput,
 } from '@openmsupply-client/system';
+import { useDraftInboundLines } from '../api';
+import { DraftInboundLine } from '../../types';
+import { set } from 'lodash';
 
 interface Message {
   type: 'error' | 'warning' | 'info';
@@ -28,6 +39,7 @@ interface Message {
 
 interface ScanInputModalProps {
   lines: InvoiceLineNode[];
+  invoiceId: string;
 }
 
 interface FormDraftState {
@@ -43,64 +55,71 @@ interface FormDraftState {
   barcodeContent: string;
 }
 
-export const ScanInputModal = ({ lines }: ScanInputModalProps) => {
+const defaultDraftState: FormDraftState = {
+  item: null,
+  batch: '',
+  expiryDate: null,
+  packSize: 1,
+  quantity: 0,
+
+  //
+  isNewLine: true,
+  barcodeContent: '',
+};
+
+export const ScanInputModal = ({ lines, invoiceId }: ScanInputModalProps) => {
   const t = useTranslation();
   const [isOpen, setIsOpen] = useState(false);
-  const { isConnected, isEnabled, isScanning, stopScan, startScanning } =
+  const { isConnected, isEnabled, stopScan, startScanning } =
     useBarcodeScannerContext();
-  const [barcodeItem, setBarcodeItem] = useState<BarcodeNode | null>(null);
-  //   const [message, setMessage] = useState<Message | null>(null);
-  const [draftState, setDraftState] = useState<FormDraftState>({
-    item: null,
-    batch: '',
-    expiryDate: null,
-    packSize: 1,
-    quantity: 0,
+  const [barcodeData, setBarcodeData] = useState<BarcodeNode | null>(null);
+  const [draftState, setDraftState] =
+    useState<FormDraftState>(defaultDraftState);
 
-    //
-    isNewLine: true,
-    barcodeContent: '',
-  });
+  const { saveSingleLine } = useDraftInboundLines(barcodeData?.itemId);
+
+  const { Modal } = useDialog({ isOpen });
 
   const { mutateAsync: getBarcode } = useOutbound.utils.barcode();
+  const { mutateAsync: saveBarcode } = useOutbound.utils.barcodeInsert();
+
+  const existingLine = lines.find(line => line.batch === draftState.batch);
 
   const handleScan = async (barcode: ScanResult) => {
     const newState = { ...draftState };
-    console.log('Scanned barcode:', barcode);
+
     newState.barcodeContent = barcode.content ?? '';
     if (!isOpen) {
       setIsOpen(true);
     }
-    const { content, gtin, batch, expiryDate } = barcode;
+
+    const {
+      content,
+      gtin,
+      batch,
+      expiryDate,
+      // packSize, quantity -- TO-DO
+    } = barcode;
+
     const barcodeOrGtin = gtin ?? content;
-    console.log('Fetching barcode data for:', barcodeOrGtin);
+
     if (barcodeOrGtin) {
       const dbBarcodeData = (await getBarcode(barcodeOrGtin)) as BarcodeNode;
       if (!dbBarcodeData?.id) {
-        newState.newGtin = barcodeOrGtin;
+        // Only save as new GTIN if barcode has one, otherwise ignore (for now)
+        if (gtin) newState.newGtin = barcodeOrGtin;
         newState.isNewLine = true;
-        // setMessage({
-        //   type: 'info',
-        //   text: `GTIN ${barcodeOrGtin} is new and not found in the system.`,
-        // });
-        setBarcodeItem(null);
-      } else setBarcodeItem(dbBarcodeData);
+      } else {
+        setBarcodeData(dbBarcodeData);
+      }
+
       if (batch) newState.batch = batch;
       if (expiryDate) newState.expiryDate = new Date(expiryDate);
       //   if (packSize) newState.packSize = packSize;
       //   if (quantity) newState.quantity = quantity;
-      console.log('newState', newState);
     }
     setDraftState(newState);
-
-    // console.log('Fetched barcode data:', fetchedBarcode);
   };
-
-  console.log('draftState', draftState);
-
-  const existingLine = lines.find(line => line.batch === draftState.batch);
-
-  console.log('existingLine', existingLine);
 
   useEffect(() => {
     if (isEnabled && isConnected) {
@@ -112,106 +131,164 @@ export const ScanInputModal = ({ lines }: ScanInputModalProps) => {
     };
   }, []);
 
-  const onChangeItem = (item: ItemStockOnHandFragment | null) => {};
+  const onChangeItem = (item: ItemStockOnHandFragment | null) => {
+    setDraftState(current => ({
+      ...current,
+      item,
+      packSize: barcodeData?.packSize || item?.defaultPackSize || 1,
+    }));
+  };
 
-  console.log('barcodeItem', barcodeItem);
+  const message: Message = getMessage(barcodeData, draftState, existingLine, t);
 
-  const message: Message = !barcodeItem
-    ? {
-        type: 'warning',
-        text: 'Unknown GTIN or barcode. Select an item to associate with this GTIN',
-      }
-    : !existingLine
-      ? {
-          type: 'warning',
-          text: 'Batch not found in existing lines. A new line will be created.',
-        }
-      : {
-          type: 'info',
-          text: `Batch already exists with a quantity of ${existingLine.numberOfPacks} units. The new quantity will be added to this line.`,
-        };
+  const isFormValid =
+    (!!draftState.item || !!barcodeData) && draftState.quantity > 0;
 
   return (
-    <BasicModal
-      open={isOpen}
-      sx={{
-        padding: 4,
-        display: 'flex',
-        gap: 2,
-      }}
-      width={400}
-      height={200}
+    <Modal
+      title="Scan product"
+      width={500}
+      disableEnforceFocus // Prevents input block in Mock barcode scanner element
+      okButton={
+        <DialogButton
+          variant="ok"
+          disabled={!isFormValid}
+          onClick={async () => {
+            try {
+              const updatedLine: Partial<DraftInboundLine> = {
+                type: InvoiceLineNodeType.StockIn,
+                batch: draftState.batch.trim(),
+                expiryDate: draftState.expiryDate
+                  ? draftState.expiryDate.toISOString().substring(0, 10)
+                  : null,
+                packSize: draftState.packSize,
+                numberOfPacks:
+                  (existingLine?.numberOfPacks || 0) + draftState.quantity,
+              };
+              if (existingLine) {
+                updatedLine.id = existingLine.id;
+                updatedLine.isUpdated = true;
+              } else {
+                updatedLine.id = FnUtils.generateUUID();
+                updatedLine.isCreated = true;
+                updatedLine.invoiceId = invoiceId;
+                updatedLine.sellPricePerPack = 0;
+                updatedLine.costPricePerPack = 0;
+              }
+              if (draftState.item) {
+                updatedLine.item = draftState.item;
+              } else updatedLine.item = { id: barcodeData?.itemId || '' };
+              console.log('Existing line:', existingLine);
+
+              await saveSingleLine(updatedLine);
+
+              if (draftState.newGtin) {
+                // Save new barcode to database
+                await saveBarcode({
+                  input: {
+                    gtin: draftState.newGtin,
+                    itemId: draftState.item?.id!,
+                    packSize: draftState.packSize,
+                  },
+                });
+              }
+              setDraftState(defaultDraftState);
+              setBarcodeData(null);
+              setIsOpen(false);
+            } catch (e) {
+              //   error((e as Error).message)();
+            }
+          }}
+        />
+      }
+      cancelButton={
+        <DialogButton
+          variant="cancel"
+          onClick={() => {
+            setDraftState(defaultDraftState);
+            setBarcodeData(null);
+            setIsOpen(false);
+          }}
+        />
+      }
     >
-      <Typography>
-        <strong>Scan Product</strong>
-      </Typography>
-      <Typography>
-        <strong>Barcode:</strong> {draftState.barcodeContent}
-      </Typography>
-      <Alert severity={message.type}>{message.text}</Alert>
-      <InputWithLabelRow
-        label="Item"
-        Input={
-          <StockItemSearchInput
-            autoFocus={!barcodeItem}
-            // openOnFocus={!barcodeItem}
-            disabled={!!barcodeItem}
-            currentItemId={barcodeItem?.itemId}
-            onChange={newItem => onChangeItem(newItem)}
-            // filter={{ id: { notEqualAll: existingItemIds } }}
-            // A scanned-in item will only have an ID, not a full item object,
-            // so this flag makes the StockItemSearchInput component update the
-            // current item on initial load from the API
-            initialUpdate
-          />
-        }
-      />
-      <InputWithLabelRow
-        label={t('label.batch')}
-        Input={
-          <BasicTextInput
-            value={draftState.batch ?? ''}
-            onChange={e =>
-              setDraftState(current => ({ ...current, batch: e.target.value }))
-            }
-          />
-        }
-      />
-      <InputWithLabelRow
-        label={t('label.expiry-date')}
-        Input={
-          <DatePicker
-            value={draftState.expiryDate}
-            onChange={value =>
-              setDraftState(current => ({ ...current, expiryDate: value }))
-            }
-          />
-        }
-      />
-      <InputWithLabelRow
-        label={t('label.pack-size')}
-        Input={
-          <NumericTextInput
-            value={draftState.packSize ?? ''}
-            onChange={value =>
-              setDraftState(current => ({ ...current, packSize: value || 1 }))
-            }
-          />
-        }
-      />
-      <InputWithLabelRow
-        label={t('label.quantity')}
-        Input={
-          <NumericTextInput
-            value={draftState.quantity ?? ''}
-            onChange={value =>
-              setDraftState(current => ({ ...current, quantity: value || 1 }))
-            }
-          />
-        }
-      />
-      {/* TO-DO: Manufacture Date */}
-      {/* <InputWithLabelRow
+      <Box
+        display="flex"
+        // justifyContent="space-between"
+        // alignItems="center"
+        flexDirection="column"
+        gap={1}
+      >
+        <Typography>
+          <strong>Barcode:</strong> {draftState.barcodeContent}
+        </Typography>
+        <Alert severity={message.type}>{message.text}</Alert>
+        <InputWithLabelRow
+          label="Item"
+          Input={
+            <StockItemSearchInput
+              autoFocus={!barcodeData}
+              // openOnFocus={!barcodeItem}
+              disabled={!!barcodeData}
+              currentItemId={draftState.item?.id || barcodeData?.itemId || null}
+              onChange={newItem => onChangeItem(newItem)}
+              // filter={{ id: { notEqualAll: existingItemIds } }}
+              // A scanned-in item will only have an ID, not a full item object,
+              // so this flag makes the StockItemSearchInput component update the
+              // current item on initial load from the API
+              initialUpdate
+            />
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.batch')}
+          Input={
+            <BasicTextInput
+              value={draftState.batch ?? ''}
+              onChange={e =>
+                setDraftState(current => ({
+                  ...current,
+                  batch: e.target.value,
+                }))
+              }
+            />
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.expiry-date')}
+          Input={
+            <DatePicker
+              value={draftState.expiryDate}
+              onChange={value =>
+                setDraftState(current => ({ ...current, expiryDate: value }))
+              }
+            />
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.pack-size')}
+          Input={
+            <NumericTextInput
+              value={draftState.packSize ?? ''}
+              onChange={value =>
+                setDraftState(current => ({ ...current, packSize: value || 1 }))
+              }
+            />
+          }
+        />
+        <InputWithLabelRow
+          label={t('label.quantity')}
+          Input={
+            <NumericTextInput
+              value={draftState.quantity ?? ''}
+              onChange={value =>
+                setDraftState(current => ({ ...current, quantity: value || 1 }))
+              }
+            />
+          }
+        />
+        {/* TO-DO: Manufacture Date */}
+        {/* <InputWithLabelRow
         label={t('label.quantity')}
         Input={
           <NumericTextInput
@@ -222,6 +299,37 @@ export const ScanInputModal = ({ lines }: ScanInputModalProps) => {
           />
         }
       /> */}
-    </BasicModal>
+      </Box>
+    </Modal>
   );
+};
+
+const getMessage = (
+  barcodeData: BarcodeNode | null,
+  draftState: FormDraftState,
+  existingLine: InvoiceLineNode | undefined,
+  t: TypedTFunction<LocaleKey>
+): Message => {
+  if (!barcodeData && !draftState.newGtin && !draftState.item)
+    return {
+      type: 'error',
+      text: 'Unknown barcode, and does not contain a valid GTIN. This barcode will not be used for future scans. You must manually select an item.',
+    };
+
+  if (!barcodeData && !!draftState.newGtin && !draftState.item)
+    return {
+      type: 'warning',
+      text: 'Unknown GTIN or barcode. Select an item to associate with this GTIN, which will be saved for future scans.',
+    };
+
+  if (!existingLine)
+    return {
+      type: 'warning',
+      text: 'Batch not found in existing lines. A new line will be created.',
+    };
+
+  return {
+    type: 'info',
+    text: `Batch already exists with a quantity of ${existingLine.numberOfPacks} units. The new quantity will be added to this line.`,
+  };
 };
