6656300e29c12ac76c549dcdca1d8f342f44aaea
roxy-dao <roxy@msupply.foundation>
2026-02-10 05:01:48 +0100

    Transfer info to requisition

--- server/service/src/processors/transfer/requisition/create_response_requisition.rs
+++ server/service/src/processors/transfer/requisition/create_response_requisition.rs
@@ -347,9 +347,9 @@ fn generate_response_requisition_lines(
                 price_per_unit,
                 available_volume,
                 location_type_id,
-                forecast_total_doses: _,
-                forecast_total_units: _,
-                vaccine_courses: _,
+                forecast_total_doses,
+                forecast_total_units,
+                vaccine_courses,
             },
         item_row: ItemRow { id: item_id, .. },
         requisition_row: _,
@@ -392,13 +392,13 @@ fn generate_response_requisition_lines(
             available_volume,
             location_type_id,
             price_per_unit,
+            forecast_total_units,
+            forecast_total_doses,
+            vaccine_courses,
             // Default
             supply_quantity: 0.0,
             approved_quantity: 0.0,
             approval_comment: None,
-            forecast_total_units: None,
-            forecast_total_doses: None,
-            vaccine_courses: None,
         });
     }
 
