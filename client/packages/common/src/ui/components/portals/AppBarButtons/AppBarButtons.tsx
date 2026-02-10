25ef308ed3bd4f5f59a1e5070126e37a68df331d
James Brunskill <james@sussol.net>
2026-02-10 17:28:20 +1300

    Improve button /appbar on mobile

--- client/packages/common/src/ui/components/portals/AppBarButtons/AppBarButtons.tsx
+++ client/packages/common/src/ui/components/portals/AppBarButtons/AppBarButtons.tsx
@@ -1,33 +1,16 @@
 import { Box, BoxProps, Portal } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import React, { FC, useEffect, useRef } from 'react';
-import { useHostContext, useIsExtraSmallScreen } from '@common/hooks';
-
-const Container = styled('div')({
-  display: 'flex',
-  flex: 1,
-  justifyContent: 'flex-end',
-});
-
-const MobileContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'flex-end',
-});
+import { useHostContext } from '@common/hooks';
 
 export const AppBarButtons: FC = () => {
   const { setAppBarButtonsRef } = useHostContext();
   const ref = useRef(null);
 
-  const isExtraSmallScreen = useIsExtraSmallScreen();
-
   useEffect(() => {
     setAppBarButtonsRef(ref);
   }, []);
-  if (isExtraSmallScreen) {
-    return <MobileContainer ref={ref} />;
-  }
 
-  return <Container ref={ref} />;
+  return <Box ref={ref} />;
 };
 
 export const AppBarButtonsPortal: FC<BoxProps> = props => {
