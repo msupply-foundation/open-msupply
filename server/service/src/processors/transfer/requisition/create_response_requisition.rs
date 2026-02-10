ae116c96b4055457b2a985af4413a6f6c87e0209
Esme HM <61033090+esmehm@users.noreply.github.com>
2026-02-10 14:46:25 +1300

    configure showing indicative pricing as store pref not global

--- server/service/src/processors/transfer/requisition/create_response_requisition.rs
+++ server/service/src/processors/transfer/requisition/create_response_requisition.rs
@@ -303,7 +303,7 @@ fn generate_response_requisition_lines(
     request_requisition: &RequisitionRow,
 ) -> Result<Vec<RequisitionLineRow>, RepositoryError> {
     let request_lines = get_lines_for_requisition(connection, &request_requisition.id)?;
-    let populate_price_per_unit = get_indicative_price_pref(connection)?;
+    let populate_price_per_unit = get_indicative_price_pref(connection, &response_requisition.store_id)?;
     let price_list = if populate_price_per_unit {
         Some(get_pricing_for_items(
             connection,
