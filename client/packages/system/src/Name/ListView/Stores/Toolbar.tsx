1ab4ba208edfe653e689f9b2dd412a204076abcc
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2026-02-11 13:45:10 +1300

    reinstate code or name filter on stores list

---
+++ client/packages/system/src/Name/ListView/Stores/Toolbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  AppBarContentPortal,
+  useTranslation,
+  SearchBar,
+  FilterRule,
+  useUrlQueryParams,
+} from '@openmsupply-client/common';
+
+export const Toolbar = () => {
+  const t = useTranslation();
+
+  const { filter } = useUrlQueryParams({
+    filters: [{ key: 'codeOrName' }],
+  });
+
+  const filterString =
+    ((filter.filterBy?.['codeOrName'] as FilterRule)?.like as string) || '';
+
+  return (
+    <AppBarContentPortal
+      sx={{
+        paddingBottom: '16px',
+        flex: 1,
+        justifyContent: 'space-between',
+        display: 'flex',
+      }}
+    >
+      <SearchBar
+        placeholder={t('placeholder.enter-code-or-name')}
+        value={filterString ?? ''}
+        onChange={newValue => {
+          if (!newValue) {
+            return filter.onClearFilterRule('codeOrName');
+          }
+          return filter.onChangeStringFilterRule(
+            'codeOrName',
+            'like',
+            newValue
+          );
+        }}
+      />
+    </AppBarContentPortal>
+  );
+};
