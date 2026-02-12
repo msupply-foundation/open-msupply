536dc31a07d1a81728928460eac6fd3e0007c4ec
roxy-dao <roxy@msupply.foundation>
2026-02-12 15:00:24 +0100

    Migration to change item category join item id to item link id
    Source: https://github.com/msupply-foundation/open-msupply/commit/2440003f8b37ecfab8cdc82fb73dbdd4b203f4d6

--- server/repository/src/migrations/v2_16_00/mod.rs
+++ server/repository/src/migrations/v2_16_00/mod.rs
@@ -8,6 +8,7 @@ mod add_shipping_method_id_to_invoice;
 mod add_universal_code_to_item;
 mod add_version_to_plugins;
 mod available_volume_on_requisition_line_and_volume_per_pack_on_item;
+mod item_category_join_add_item_link_id;
 
 pub(crate) struct V2_16_00;
 impl Migration for V2_16_00 {
@@ -28,6 +29,7 @@ impl Migration for V2_16_00 {
             Box::new(available_volume_on_requisition_line_and_volume_per_pack_on_item::Migrate),
             Box::new(add_universal_code_to_item::Migrate),
             Box::new(add_comments_required_to_asset_log_reason::Migrate),
+            Box::new(item_category_join_add_item_link_id::Migrate),
         ]
     }
 }
