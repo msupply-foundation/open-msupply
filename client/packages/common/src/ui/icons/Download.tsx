5c30cc49c37c81b0c8e546366828124305a6093c
Esme HM <61033090+esmehm@users.noreply.github.com>
2026-02-17 14:06:40 +1300

    upload and download icons
    Source: https://github.com/msupply-foundation/open-msupply/commit/71d44f0e863405d72e191c571cee1c3467cfc469

--- client/packages/common/src/ui/icons/Download.tsx
+++ client/packages/common/src/ui/icons/Download.tsx
@@ -16,9 +16,10 @@ export const DownloadIcon = (props: SvgIconProps): JSX.Element => {
 
   return (
     <SvgIcon {...combinedProps} viewBox="0 0 24 24">
-      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-      <polyline points="17 8 12 3 7 8"></polyline>
-      <line x1="12" y1="3" x2="12" y2="15"></line>
+      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+      {/* Arrow DOWN */}
+      <polyline points="7 10 12 15 17 10" />
+      <line x1="12" y1="3" x2="12" y2="15" />
     </SvgIcon>
   );
 };
