a2549e9de14b00fd29e4a65fae609f2fe18d8c2a
Brian <bk.dev@icloud.com>
2026-02-19 16:49:49 +1300

    add back row minHeight

--- client/packages/common/src/ui/layout/tables/useTableDisplayOptions.tsx
+++ client/packages/common/src/ui/layout/tables/useTableDisplayOptions.tsx
@@ -241,7 +241,7 @@ export const useTableDisplayOptions = <T extends MRT_RowData>({
           },
 
     muiTableBodyRowProps: params => {
-      const { row } = params;
+      const { row, table } = params;
       const customProps =
         typeof muiTableBodyRowProps === 'function'
           ? muiTableBodyRowProps(params)
@@ -256,6 +256,7 @@ export const useTableDisplayOptions = <T extends MRT_RowData>({
         },
         sx: {
           backgroundColor: 'inherit',
+          minHeight: table.getState().density === 'compact' ? '32px' : '40px',
           // these two selectors are to change the background color of a selected
           // row from the default which is to use primary.main of the theme
           // with an opacity of 0.2 and 0.4 on hover
