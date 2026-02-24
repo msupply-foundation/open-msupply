84f54ebe5c0c47b8cf813b716a6971d5b38cd531
Brian <bk.dev@icloud.com>
2026-02-25 09:50:12 +1300

    fix server tests
    Source: https://github.com/msupply-foundation/open-msupply/commit/d4121880ded5acb7fc763ad8affd9c640a498ce5

--- server/graphql/invoice/src/mutations/inbound_shipment/add_from_master_list.rs
+++ server/graphql/invoice/src/mutations/inbound_shipment/add_from_master_list.rs
@@ -103,7 +103,8 @@ mod test {
     };
     use repository::{
         mock::{mock_empty_draft_inbound_shipment, mock_item_a, MockDataInserts},
-        InvoiceLine, InvoiceLineRow, InvoiceLineType, StorageConnectionManager,
+        InvoiceLine, InvoiceLineRow, InvoiceLineStatsRow, InvoiceLineType,
+        StorageConnectionManager,
     };
     use serde_json::json;
     use service::{
@@ -114,7 +115,6 @@ mod test {
         },
         service_provider::{ServiceContext, ServiceProvider},
     };
-    
 
     type DeleteLineMethod =
         dyn Fn(ServiceInput) -> Result<Vec<InvoiceLine>, ServiceError> + Sync + Send;
@@ -300,6 +300,7 @@ mod test {
                 },
                 invoice_row: mock_empty_draft_inbound_shipment(),
                 item_row: mock_item_a(),
+                invoice_line_stats_row: InvoiceLineStatsRow::default(),
                 location_row_option: None,
                 stock_line_option: None,
             }])
--- server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/insert.rs
+++ server/graphql/invoice_line/src/mutations/inbound_shipment_line/line/insert.rs
@@ -203,7 +203,7 @@ mod test {
             mock_inbound_shipment_c, mock_inbound_shipment_c_invoice_lines, mock_item_a,
             mock_location_1, MockDataInserts,
         },
-        InvoiceLine, RepositoryError, StorageConnectionManager,
+        InvoiceLine, InvoiceLineStatsRow, RepositoryError, StorageConnectionManager,
     };
     use serde_json::json;
     use service::{
@@ -484,6 +484,7 @@ mod test {
                 invoice_line_row: mock_inbound_shipment_c_invoice_lines()[0].clone(),
                 invoice_row: mock_inbound_shipment_c(),
                 item_row: mock_item_a(),
+                invoice_line_stats_row: InvoiceLineStatsRow::default(),
                 location_row_option: Some(mock_location_1()),
                 stock_line_option: None,
             })
