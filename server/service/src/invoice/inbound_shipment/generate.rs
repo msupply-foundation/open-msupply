aafdc94744883ac0ae6636f3eb1ecb4bfa738345
Brian <bk.dev@icloud.com>
2026-03-02 16:27:54 +1300

    stop 'add from master list' for external inbound shipments
    Source: https://github.com/msupply-foundation/open-msupply/commit/67b937e72112e4983995f6639660da0b10f977d0

--- server/service/src/invoice/inbound_shipment/generate.rs
+++ server/service/src/invoice/inbound_shipment/generate.rs
@@ -51,7 +51,7 @@ pub fn generate_empty_invoice_lines(
                     shipped_number_of_packs: None,
                     volume_per_pack: 0.0,
                     shipped_pack_size: None,
-                    status: None, // TODO: Set status based on authorisation config
+                    status: None, // we know it's an internal order due to add_from_master_list validation, so we can leave status as None
                 });
             }
             Ok(None) => {}
