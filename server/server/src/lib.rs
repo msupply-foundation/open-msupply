902f49a143f7e6ba6aea95f5903cb2f57dc10fbd
James Brunskill <james@sussol.net>
2026-03-02 14:43:32 +1300

    Now that base dir is required, better to make it non-optional
    Source: https://github.com/msupply-foundation/open-msupply/commit/6511e7435895b7ea7847a48ed8e37b0123c80e28

--- server/server/src/lib.rs
+++ server/server/src/lib.rs
@@ -107,9 +107,9 @@ pub async fn start_server(
     }
 
     // CREATE BASE DIRECTORY IF IT DOESN'T EXIST
-    let base_dir = settings.server.base_dir.clone().unwrap_or(".".to_string());
+    let base_dir = &settings.server.base_dir;
     info!("Creating base directory if needed: {}", base_dir);
-    std::fs::create_dir_all(&base_dir)?;
+    std::fs::create_dir_all(base_dir)?;
 
     // INITIALISE DATABASE AND CONNECTION
     let connection_manager = get_storage_connection_manager(&settings.database);
