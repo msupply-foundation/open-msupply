c02e4a49f988e1056d2e6b020f6d8d96235f5940
Esme HM <61033090+esmehm@users.noreply.github.com>
2026-03-06 14:23:16 +1300

    10732 close for receiving changes line status to closed not new
    Source: https://github.com/msupply-foundation/open-msupply/commit/4f8571d2ee77165db0e04d5d4bd02e33362e71dd

--- server/service/src/purchase_order_line/update/mod.rs
+++ server/service/src/purchase_order_line/update/mod.rs
@@ -72,9 +72,17 @@ pub fn update_purchase_order_line(
             let mut updated_input = input.clone();
             let mut line_status_changed_to_new = false;
 
-            if input.adjusted_number_of_units
-                != current_purchase_order_line.adjusted_number_of_units
-            {
+            let adjusted_units_changed = match input.adjusted_number_of_units {
+                // Only consider it changed if explicitly provided and different
+                Some(_) => {
+                    input.adjusted_number_of_units
+                        != current_purchase_order_line.adjusted_number_of_units
+                }
+                // Not provided in input, no change
+                None => false,
+            };
+
+            if adjusted_units_changed {
                 let purchase_order = PurchaseOrderRowRepository::new(connection)
                     .find_one_by_id(&purchase_order_id)?
                     .ok_or(UpdatePurchaseOrderLineInputError::PurchaseOrderDoesNotExist)?;
