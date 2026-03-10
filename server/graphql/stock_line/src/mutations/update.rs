e9ea471ac3a547dd95c0b3856c1070eccb3e935b
roxy-dao <roxy@msupply.foundation>
2026-03-10 11:11:08 +0100

    Manufacturer id on stock line update & validation
    Source: https://github.com/msupply-foundation/open-msupply/commit/d03ba6482e27130518ec8ba6feba7d9dcea86d52

--- server/graphql/stock_line/src/mutations/update.rs
+++ server/graphql/stock_line/src/mutations/update.rs
@@ -32,6 +32,7 @@ pub struct UpdateInput {
     pub campaign_id: Option<NullableUpdateInput<String>>,
     pub program_id: Option<NullableUpdateInput<String>>,
     pub volume_per_pack: Option<f64>,
+    pub manufacturer_id: Option<NullableUpdateInput<String>>,
 }
 
 #[derive(Interface)]
@@ -103,6 +104,7 @@ impl UpdateInput {
             campaign_id,
             program_id,
             volume_per_pack,
+            manufacturer_id,
         } = self;
 
         ServiceInput {
@@ -132,6 +134,9 @@ impl UpdateInput {
                 value: program_id.value,
             }),
             volume_per_pack,
+            manufacturer_id: manufacturer_id.map(|manufacturer_id| NullableUpdate {
+                value: manufacturer_id.value,
+            }),
         }
     }
 }
@@ -152,6 +157,9 @@ fn map_error(error: ServiceError) -> Result<UpdateErrorInterface> {
         ServiceError::DonorDoesNotExist
         | ServiceError::DonorNotVisible
         | ServiceError::DonorIsNotADonor
+        | ServiceError::ManufacturerDoesNotExist
+        | ServiceError::ManufacturerNotVisible
+        | ServiceError::ManufacturerIsNotAManufacturer
         | ServiceError::VVMStatusDoesNotExist
         | ServiceError::StockDoesNotBelongToStore
         | ServiceError::LocationDoesNotExist
