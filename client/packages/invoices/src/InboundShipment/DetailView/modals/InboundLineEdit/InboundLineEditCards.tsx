f9fb5cd2aa75e4cdd6116ff66ef1212e6119ca68
Esme HM <61033090+esmehm@users.noreply.github.com>
2026-03-12 12:38:30 +1300

    simplified tablet view
    Source: https://github.com/msupply-foundation/open-msupply/commit/74b001cdfc4e1070b323ef85d486136d06cd3aba

--- client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/InboundLineEditCards.tsx
+++ client/packages/invoices/src/InboundShipment/DetailView/modals/InboundLineEdit/InboundLineEditCards.tsx
@@ -40,7 +40,7 @@ import {
 } from '@openmsupply-client/system';
 import { PatchDraftLineInput } from '../../../api';
 
-interface TableProps {
+interface CardProps {
   lines: DraftInboundLine[];
   updateDraftLine: (patch: PatchDraftLineInput) => void;
   isDisabled?: boolean;
@@ -53,354 +53,13 @@ interface TableProps {
   restrictedToLocationTypeId?: string | null;
 }
 
-interface QuantityTableProps extends TableProps {
-  removeDraftLine: (id: string) => void;
-}
-
-export const QuantityTable = ({
-  lines,
-  updateDraftLine,
-  removeDraftLine,
-  isDisabled = false,
-  hasItemVariantsEnabled,
-  hasVvmStatusesEnabled,
-  item,
-  setPackRoundingMessage,
-}: QuantityTableProps) => {
-  const t = useTranslation();
-  const { getPlural } = useIntlUtils();
-  const { format } = useFormatNumber();
-  const formatRef = useRef(format);
-  formatRef.current = format;
-  const { manageVaccinesInDoses } = usePreferences();
-
-  const displayInDoses = manageVaccinesInDoses && !!item?.isVaccine;
-  const unitName = Formatter.sentenceCase(
-    item?.unitName ? item.unitName : t('label.unit')
-  );
-  const pluralisedUnitName = getPlural(unitName, 2);
-
-  const columns = useMemo(() => {
-    const cols: ColumnDef<DraftInboundLine>[] = [
-      {
-        accessorKey: 'batch',
-        header: t('label.batch'),
-        size: 100,
-        Cell: ({ row, cell }) => (
-          <TextInputCell
-            cell={cell}
-            updateFn={(batch: string) =>
-              updateDraftLine({ id: row.original.id, batch })
-            }
-            autoFocus={row.index === 0}
-          />
-        ),
-      },
-      {
-        accessorKey: 'expiryDate',
-        header: t('label.expiry-date'),
-        size: 150,
-        columnType: ColumnType.Date,
-        accessorFn: row => DateUtils.getDateOrNull(row.expiryDate),
-        Cell: ({ cell, row }) => {
-          const value = cell.getValue<Date | null>();
-          return (
-            <ExpiryDateInput
-              value={value}
-              disabled={isDisabled}
-              onChange={date =>
-                updateDraftLine({
-                  id: row.original.id,
-                  expiryDate: Formatter.naiveDate(date),
-                })
-              }
-            />
-          );
-        },
-      },
-      {
-        id: 'manufactureDate',
-        header: t('label.manufacture-date'),
-        size: 150,
-        columnType: ColumnType.Date,
-        accessorFn: row => DateUtils.getDateOrNull(row.manufactureDate),
-        Cell: ({ cell, row }) => {
-          const value = cell.getValue<Date | null>();
-          return (
-            <DateTimePickerInput
-              value={value}
-              disabled={isDisabled}
-              onChange={date =>
-                updateDraftLine({
-                  id: row.original.id,
-                  manufactureDate: date ? Formatter.naiveDate(date) : null,
-                })
-              }
-            />
-          );
-        },
-      },
-      {
-        id: 'itemVariant',
-        header: t('label.item-variant'),
-        accessorFn: row => row.itemVariant?.id || '',
-        size: 150,
-        Cell: ({
-          row: {
-            original: { id, packSize, itemVariant, item },
-          },
-        }) => (
-          <ItemVariantInput
-            disabled={isDisabled}
-            selectedId={itemVariant?.id}
-            itemId={item.id}
-            width="100%"
-            onChange={itemVariant =>
-              updateDraftLine({
-                id,
-                itemVariantId: itemVariant?.id,
-                itemVariant,
-                volumePerPack: getVolumePerPackFromVariant({
-                  packSize,
-                  itemVariant,
-                }),
-              })
-            }
-          />
-        ),
-        includeColumn: hasItemVariantsEnabled,
-      },
-      {
-        id: 'itemDoses',
-        header: t('label.doses-per-unit'),
-        columnType: ColumnType.Number,
-        defaultHideOnMobile: true,
-        includeColumn: displayInDoses,
-        accessorFn: row => (row.item.isVaccine ? row.item.doses : undefined),
-      },
-      {
-        id: 'vvmStatus',
-        header: t('label.vvm-status'),
-        size: 150,
-        accessorFn: row => row.vvmStatus || '',
-        Cell: ({
-          row: {
-            original: { id, vvmStatus, stockLine },
-          },
-        }) => (
-          <VVMStatusSearchInput
-            disabled={isDisabled}
-            selected={vvmStatus ?? null}
-            onChange={vvmStatus => updateDraftLine({ id, vvmStatus })}
-            useDefault={!stockLine}
-          />
-        ),
-        includeColumn: hasVvmStatusesEnabled && item?.isVaccine,
-      },
-      {
-        accessorKey: 'shippedPackSize',
-        header: t('label.shipped-pack-size'),
-        size: 120,
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            updateFn={(value: number) => {
-              updateDraftLine({ shippedPackSize: value, id: row.original.id });
-            }}
-            disabled={isDisabled}
-            min={1}
-          />
-        ),
-        defaultHideOnMobile: true,
-      },
-      {
-        accessorKey: 'shippedNumberOfPacks',
-        header: t('label.shipped-number-of-packs'),
-        size: 100,
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            updateFn={(value: number) => {
-              updateDraftLine({
-                shippedNumberOfPacks: value,
-                id: row.original.id,
-              });
-            }}
-            disabled={isDisabled}
-            min={0}
-          />
-        ),
-      },
-      {
-        accessorKey: 'packSize',
-        header: t('label.received-pack-size'),
-        size: 120,
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            updateFn={(value: number) => {
-              const line = row.original;
-              const item = row.original.item;
-              const shouldClearSellPrice =
-                item?.defaultPackSize !== line.packSize &&
-                item?.itemStoreProperties?.defaultSellPricePerPack ===
-                  line.sellPricePerPack;
-
-              updateDraftLine({
-                volumePerPack:
-                  getVolumePerPackFromVariant({
-                    itemVariant: line.itemVariant,
-                    packSize: value,
-                  }) ?? 0,
-                sellPricePerPack: shouldClearSellPrice
-                  ? 0
-                  : line.sellPricePerPack,
-                packSize: value,
-                id: row.original.id,
-              });
-            }}
-            disabled={isDisabled}
-            min={1}
-          />
-        ),
-        defaultHideOnMobile: true,
-      },
-      {
-        accessorKey: 'numberOfPacks',
-        header: t('label.packs-received'),
-        size: 100,
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            updateFn={(value: number) => {
-              const { packSize } = row.original;
-              if (packSize !== undefined) {
-                const packToUnits = packSize * value;
-                setPackRoundingMessage?.('');
-                updateDraftLine({
-                  receivedNumberOfUnits: packToUnits,
-                  id: row.original.id,
-                  numberOfPacks: value,
-                });
-              }
-            }}
-            disabled={isDisabled}
-            min={0}
-          />
-        ),
-      },
-      {
-        accessorKey: 'receivedNumberOfUnits',
-        header: t('label.units-received', {
-          unit: pluralisedUnitName,
-        }),
-        size: 120,
-        defaultHideOnMobile: true,
-        accessorFn: row => {
-          return row.numberOfPacks * row.packSize;
-        },
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            debounceTime={500}
-            updateFn={(value: number) => {
-              const { packSize } = row.original;
-              if (packSize !== undefined) {
-                const unitToPacks = value / packSize;
-                const roundedPacks = Math.ceil(unitToPacks);
-                const actualUnits = roundedPacks * packSize;
-                if (roundedPacks === unitToPacks || roundedPacks === 0) {
-                  setPackRoundingMessage?.('');
-                } else {
-                  setPackRoundingMessage?.(
-                    t('messages.under-allocated', {
-                      receivedQuantity: formatRef.current(NumUtils.round(value, 2)), // round the display value to 2dp
-                      quantity: formatRef.current(actualUnits),
-                    })
-                  );
-                }
-                updateDraftLine({
-                  receivedNumberOfUnits: actualUnits,
-                  numberOfPacks: roundedPacks,
-                  id: row.original.id,
-                });
-                return actualUnits;
-              }
-            }}
-            disabled={isDisabled}
-            min={0}
-          />
-        ),
-      },
-      {
-        accessorKey: 'doseQuantity',
-        header: t('label.doses-received'),
-        size: 100,
-        includeColumn: displayInDoses,
-        accessorFn: row => {
-          const total = row.numberOfPacks * row.packSize;
-          return formatRef.current(total * row.item.doses);
-        },
-      },
-      {
-        accessorKey: 'volumePerPack',
-        header: t('label.volume-per-pack'),
-        size: 140,
-        Cell: ({ row, cell }) => (
-          <NumberInputCell
-            cell={cell}
-            updateFn={(value: number) => {
-              updateDraftLine({ volumePerPack: value, id: row.original.id });
-            }}
-            disabled={isDisabled}
-            decimalLimit={10}
-          />
-        ),
-      },
-      {
-        id: 'delete',
-        header: '',
-        size: 50,
-        Cell: ({ row }) => (
-          <IconButton
-            label={t('button.delete')}
-            onClick={() => removeDraftLine(row.original.id)}
-            icon={<DeleteIcon fontSize="small" />}
-          />
-        ),
-      },
-    ];
-    return cols;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    isDisabled,
-    updateDraftLine,
-    setPackRoundingMessage,
-    unitName,
-    pluralisedUnitName,
-    displayInDoses,
-    hasItemVariantsEnabled,
-    hasVvmStatusesEnabled,
-    item?.isVaccine,
-    removeDraftLine,
-  ]);
-
-  const table = useSimpleMaterialTable<DraftInboundLine>({
-    tableId: 'inbound-line-quantity',
-    columns,
-    data: lines,
-    getIsRestrictedRow: isDisabled ? () => true : undefined,
-  });
-
-  return <MaterialTable table={table} />;
-};
-
-interface InboundLineEditTableProps extends TableProps {
+interface InboundLineEditCardsProps extends CardProps {
   removeDraftLine: (id: string) => void;
   lastCardRef?: React.RefObject<HTMLDivElement>;
+  simplified?: boolean;
 }
 
-export const InboundLineEditTable = ({
+export const InboundLineEditCards = ({
   lines,
   updateDraftLine,
   removeDraftLine,
@@ -413,10 +72,12 @@ export const InboundLineEditTable = ({
   setPackRoundingMessage,
   restrictedToLocationTypeId,
   lastCardRef,
-}: InboundLineEditTableProps) => {
+  simplified,
+}: InboundLineEditCardsProps) => {
   const t = useTranslation();
   const { getPlural } = useIntlUtils();
   const { format } = useFormatNumber();
+  // Ref avoids format in useMemo deps (unstable reference)
   const formatRef = useRef(format);
   formatRef.current = format;
   const { store } = useAuthContext();
@@ -466,6 +127,29 @@ export const InboundLineEditTable = ({
                 updateDraftLine({
                   id: row.original.id,
                   expiryDate: Formatter.naiveDate(date),
+                })
+              }
+            />
+          );
+        },
+      },
+      {
+        id: 'manufactureDate',
+        header: t('label.manufacture-date'),
+        size: 150,
+        columnType: ColumnType.Date,
+        columnGroup: 'general',
+        accessorFn: row => DateUtils.getDateOrNull(row.manufactureDate),
+        Cell: ({ cell, row }) => {
+          const value = cell.getValue<Date | null>();
+          return (
+            <DateTimePickerInput
+              value={value}
+              disabled={isDisabled}
+              onChange={date =>
+                updateDraftLine({
+                  id: row.original.id,
+                  manufactureDate: date ? Formatter.naiveDate(date) : null,
                 })
               }
             />
@@ -706,6 +390,7 @@ export const InboundLineEditTable = ({
         accessorKey: 'costPricePerPack',
         header: t('label.pack-cost-price'),
         columnGroup: 'pricing',
+        defaultHideOnMobile: true,
         Cell: ({ cell, row }) => (
           <CurrencyInputCell
             cell={cell}
@@ -739,6 +424,7 @@ export const InboundLineEditTable = ({
         accessorKey: 'sellPricePerPack',
         header: t('label.pack-sell-price'),
         columnGroup: 'pricing',
+        defaultHideOnMobile: true,
         Cell: ({ cell, row }) => (
           <CurrencyInputCell
             cell={cell}
@@ -771,6 +457,7 @@ export const InboundLineEditTable = ({
         header: t('label.line-total'),
         size: 100,
         columnGroup: 'pricing',
+        defaultHideOnMobile: true,
         accessorFn: row => row.costPricePerPack * row.numberOfPacks,
         Cell: ({ cell }) => (
           <CurrencyInputCell cell={cell} disabled updateFn={() => {}} />
@@ -797,6 +484,7 @@ export const InboundLineEditTable = ({
         id: 'location',
         header: t('label.location'),
         columnGroup: 'general',
+        defaultHideOnMobile: true,
         Cell: ({ row: { original: row } }) => {
           return (
             <LocationSearchInput
@@ -834,6 +522,7 @@ export const InboundLineEditTable = ({
         id: 'donor',
         header: t('label.donor'),
         columnGroup: 'other',
+        defaultHideOnMobile: true,
         Cell: ({ row: { original: row } }) => (
           <DonorSearchInput
             donorId={row?.donor?.id || null}
@@ -854,6 +543,7 @@ export const InboundLineEditTable = ({
         id: 'campaignOrProgram',
         header: t('label.campaign'),
         columnGroup: 'other',
+        defaultHideOnMobile: true,
         Cell: ({ row }) => (
           <CampaignOrProgramCell
             row={row.original}
@@ -906,20 +596,25 @@ export const InboundLineEditTable = ({
     getIsRestrictedRow: isDisabled ? () => true : undefined,
   });
 
+  const groupIcons = simplified
+    ? undefined
+    : {
+        general: <EditIcon />,
+        quantities: <StockIcon />,
+        pricing: <InvoiceIcon />,
+        other: <SlidersIcon />,
+      };
+
   return (
     <>
+      {/* Hidden MRT table required for column visibility state management */}
       <Box sx={{ display: 'none' }}>
         <MaterialTable table={table} />
       </Box>
       <CardList
         table={table}
         lastItemRef={lastCardRef}
-        groupIcons={{
-          general: <EditIcon />,
-          quantities: <StockIcon />,
-          pricing: <InvoiceIcon />,
-          other: <SlidersIcon />,
-        }}
+        groupIcons={groupIcons}
       />
     </>
   );
