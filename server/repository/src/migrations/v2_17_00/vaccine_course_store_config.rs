a1b1c5c7c751958c3a94eb354055740adfd4973f
roxy-dao <roxy@msupply.foundation>
2026-03-16 13:38:44 +0100

    Update migration - change table name & add coverage rate
    Source: https://github.com/msupply-foundation/open-msupply/commit/3187844f7e1af28306d9b2610024772c29403532

--- server/repository/src/migrations/v2_17_00/vaccine_course_store_config.rs
+++ server/repository/src/migrations/v2_17_00/vaccine_course_store_config.rs
@@ -4,18 +4,19 @@ pub(crate) struct Migrate;
 
 impl MigrationFragment for Migrate {
     fn identifier(&self) -> &'static str {
-        "vaccine_course_store_wastage"
+        "vaccine_course_store_config"
     }
 
     fn migrate(&self, connection: &StorageConnection) -> anyhow::Result<()> {
         sql!(
             connection,
             r#"
-                CREATE TABLE vaccine_course_store_wastage (
+                CREATE TABLE vaccine_course_store_config (
                     id TEXT PRIMARY KEY NOT NULL,
                     vaccine_course_id TEXT NOT NULL REFERENCES vaccine_course(id),
                     store_id TEXT NOT NULL REFERENCES store(id),
-                    wastage_rate {DOUBLE}
+                    wastage_rate {DOUBLE},
+                    coverage_rate {DOUBLE}
                 );
             "#
         )?;
@@ -24,7 +25,7 @@ impl MigrationFragment for Migrate {
             sql!(
                 connection,
                 r#"
-                    ALTER TYPE changelog_table_name ADD VALUE IF NOT EXISTS 'vaccine_course_store_wastage';
+                    ALTER TYPE changelog_table_name ADD VALUE IF NOT EXISTS 'vaccine_course_store_config';
                 "#
             )?;
         }
