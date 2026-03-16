e874d678da324a5b9a6aec62809ee0c48d4d7aba
roxy-dao <roxy@msupply.foundation>
2026-03-16 13:39:20 +0100

    Renames in repository layer & add coverage rate

--- server/repository/src/db_diesel/vaccine_course/mod.rs
+++ server/repository/src/db_diesel/vaccine_course/mod.rs
@@ -1,8 +1,8 @@
 pub mod vaccine_course;
 pub mod vaccine_course_dose;
 pub mod vaccine_course_dose_row;
-pub mod vaccine_course_store_wastage;
-pub mod vaccine_course_store_wastage_row;
+pub mod vaccine_course_store_config;
+pub mod vaccine_course_store_config_row;
 pub mod vaccine_course_item;
 pub mod vaccine_course_item_row;
 pub mod vaccine_course_row;
