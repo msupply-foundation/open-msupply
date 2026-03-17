9b80f59676cedf8305060a0b22ceae8abb84745d
Brian <bk.dev@icloud.com>
2026-03-18 12:27:45 +1300

    remove backfill migration

--- server/repository/src/migrations/v2_17_00/add_purchase_order_line_id_to_invoice_line.rs
+++ server/repository/src/migrations/v2_17_00/add_purchase_order_line_id_to_invoice_line.rs
@@ -15,46 +15,6 @@ impl MigrationFragment for Migrate {
             "#
         )?;
 
-        // Backfill from the existing item-based matching logic
-        if cfg!(feature = "postgres") {
-            sql!(
-                connection,
-                r#"
-                    UPDATE invoice_line il
-                    SET purchase_order_line_id = pol.id
-                    FROM invoice i
-                    JOIN item_link il_item_link ON il_item_link.id = il.item_link_id
-                    JOIN purchase_order_line pol ON i.purchase_order_id = pol.purchase_order_id
-                    JOIN item_link pol_item_link ON pol_item_link.id = pol.item_link_id
-                        AND il_item_link.item_id = pol_item_link.item_id
-                    WHERE il.invoice_id = i.id
-                        AND i.purchase_order_id IS NOT NULL;
-                "#
-            )?;
-        } else {
-            sql!(
-                connection,
-                r#"
-                    UPDATE invoice_line
-                    SET purchase_order_line_id = (
-                        SELECT pol.id
-                        FROM invoice i
-                        JOIN item_link il_item_link ON il_item_link.id = invoice_line.item_link_id
-                        JOIN purchase_order_line pol ON i.purchase_order_id = pol.purchase_order_id
-                        JOIN item_link pol_item_link ON pol_item_link.id = pol.item_link_id
-                            AND il_item_link.item_id = pol_item_link.item_id
-                        WHERE invoice_line.invoice_id = i.id
-                            AND i.purchase_order_id IS NOT NULL
-                    )
-                    WHERE EXISTS (
-                        SELECT 1 FROM invoice i
-                        WHERE invoice_line.invoice_id = i.id
-                            AND i.purchase_order_id IS NOT NULL
-                    );
-                "#
-            )?;
-        }
-
         Ok(())
     }
 }
