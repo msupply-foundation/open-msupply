c840ada394f40d9a7baf2a4d0da4551d1f38880e
roxy-dao <roxy@msupply.foundation>
2026-03-20 09:47:50 +0100

    Tests for stock effect
    Source: https://github.com/msupply-foundation/open-msupply/commit/645b99a64862f1f6d60fd314f8074157688b212d

--- server/service/src/invoice/stock_effect.rs
+++ server/service/src/invoice/stock_effect.rs
@@ -70,3 +70,224 @@ pub fn stock_effects(
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn outbound_shipment_status_effects() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Picked,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Shipped,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::Allocated,
+                &InvoiceStatus::Picked,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Allocated,
+            ),
+            StockEffect::None,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::Picked,
+                &InvoiceStatus::Shipped,
+            ),
+            StockEffect::None,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::OutboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::New,
+            ),
+            StockEffect::None,
+        );
+    }
+
+    #[test]
+    fn prescription_status_effects() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::Prescription,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Picked,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::Prescription,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::Prescription,
+                &InvoiceStatus::Verified,
+                &InvoiceStatus::Cancelled,
+            ),
+            StockEffect::ReverseStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::Prescription,
+                &InvoiceStatus::Picked,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::None,
+        );
+    }
+
+    #[test]
+    fn supplier_return_status_effects() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::SupplierReturn,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Picked,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::SupplierReturn,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Shipped,
+            ),
+            StockEffect::ReduceStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::SupplierReturn,
+                &InvoiceStatus::Picked,
+                &InvoiceStatus::Shipped,
+            ),
+            StockEffect::None,
+        );
+    }
+
+    #[test]
+    fn inbound_status_effects() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Received,
+            ),
+            StockEffect::CreateStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InboundShipment,
+                &InvoiceStatus::Shipped,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::CreateStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InboundShipment,
+                &InvoiceStatus::Delivered,
+                &InvoiceStatus::Received,
+            ),
+            StockEffect::CreateStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InboundShipment,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Shipped,
+            ),
+            StockEffect::None,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InboundShipment,
+                &InvoiceStatus::Received,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::None,
+        );
+    }
+
+    #[test]
+    fn customer_return_status_effects() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::CustomerReturn,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Received,
+            ),
+            StockEffect::CreateStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::CustomerReturn,
+                &InvoiceStatus::Shipped,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::CreateStock,
+        );
+
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::CustomerReturn,
+                &InvoiceStatus::Received,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::None,
+        );
+    }
+
+    #[test]
+    fn inventory_addition_no_effect() {
+        assert_eq!(
+            stock_effects(
+                &InvoiceType::InventoryAddition,
+                &InvoiceStatus::New,
+                &InvoiceStatus::Verified,
+            ),
+            StockEffect::None,
+        );
+    }
+}
