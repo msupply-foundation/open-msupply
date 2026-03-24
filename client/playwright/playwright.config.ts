acbac07181610877bbe84f437539746dc58f7f2f
Aneesh T <150754185+aneeshthunga26@users.noreply.github.com>
2026-03-24 16:28:02 +1300

    tests: update playwright config

--- client/playwright/playwright.config.ts
+++ client/playwright/playwright.config.ts
@@ -3,13 +3,19 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  workers: process.env['CI'] ? 1 : 3,
   reporter: 'html',
+  timeout: 60000,
+  expect: {
+    timeout: 15000,
+  },
   use: {
-    baseURL: 'http://localhost:3003',
+    baseURL: process.env['BASE_URL'] || 'http://localhost:3003',
     trace: 'on-first-retry',
+    actionTimeout: 15000,
+    screenshot: 'only-on-failure',
   },
 
   projects: [
