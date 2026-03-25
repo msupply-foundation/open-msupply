173f253b2e7135e29a06b14cd40f1ca55f97cf25
Brian <bk.dev@icloud.com>
2026-03-26 09:56:20 +1300

    use separate column for linked PO/Requisition
    Source: https://github.com/msupply-foundation/open-msupply/commit/de4106fb867f6aba6e30fe13a022c9de6bb1f44a

--- client/packages/invoices/src/InboundShipment/ListView/SupplierCell.tsx
+++ client/packages/invoices/src/InboundShipment/ListView/SupplierCell.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import {
-  useNavigate,
-  RouteBuilder,
   HomeIcon,
   TruckIcon,
   ColorSelectButton,
   InboundNodeType,
+  useTranslation,
+  LocaleKey,
 } from '@openmsupply-client/common';
-import { Box, Link, Tooltip } from '@mui/material';
-import { AppRoute } from '@openmsupply-client/config';
+import { Box, Tooltip } from '@mui/material';
 import { InboundRowFragment } from '../api';
 import { isInboundDisabled } from '../../utils';
+
+export const inboundTypeTranslation: Record<InboundNodeType, LocaleKey> = {
+  [InboundNodeType.FromPurchaseOrder]: 'label.inbound-type-from-purchase-order',
+  [InboundNodeType.FromRequisition]: 'label.inbound-type-from-requisition',
+  [InboundNodeType.ManualExternal]: 'label.inbound-type-manual-external',
+  [InboundNodeType.ManualInternal]: 'label.inbound-type-manual-internal',
+};
+
+export const getInboundColorAndIcon = (inboundType: InboundNodeType) => {
+  const external =
+    inboundType === InboundNodeType.ManualExternal ||
+    inboundType === InboundNodeType.FromPurchaseOrder;
+  return {
+    icon: external ? TruckIcon : HomeIcon,
+    color: external ? 'info.main' : 'primary.main',
+  };
+};
 
 interface SupplierCellProps {
   row: InboundRowFragment;
@@ -18,13 +34,10 @@ interface SupplierCellProps {
 }
 
 export const SupplierCell = ({ row, onColorChange }: SupplierCellProps) => {
-  const navigate = useNavigate();
-  const external =
-    row.inboundType === InboundNodeType.ManualExternal ||
-    row.inboundType === InboundNodeType.FromPurchaseOrder;
-  const KindIcon = external ? TruckIcon : HomeIcon;
-  // Proper colors?
-  const iconColor = external ? 'info.main' : 'primary.main';
+  const t = useTranslation();
+  const { icon: KindIcon, color: iconColor } = getInboundColorAndIcon(
+    row.inboundType
+  );
 
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
@@ -33,62 +46,13 @@ export const SupplierCell = ({ row, onColorChange }: SupplierCellProps) => {
         onChange={color => onColorChange({ id: row.id, colour: color.hex })}
         color={row.colour}
       />
-      {/* Translation? */}
-      <Tooltip title={row.inboundType}>
+      <Tooltip title={t(inboundTypeTranslation[row.inboundType])}>
         <Box sx={{ display: 'flex' }}>
           <KindIcon sx={{ fontSize: 16, color: iconColor }} />
         </Box>
       </Tooltip>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         {row.otherPartyName}
-        {row.purchaseOrder && (
-          <Link
-            component="button"
-            variant="caption"
-            sx={{
-              whiteSpace: 'nowrap',
-              textAlign: 'left',
-              color: iconColor,
-              width: 'fit-content',
-            }}
-            onClick={e => {
-              e.stopPropagation();
-              if (!row.purchaseOrder?.id) return;
-              navigate(
-                RouteBuilder.create(AppRoute.Replenishment)
-                  .addPart(AppRoute.PurchaseOrder)
-                  .addPart(row.purchaseOrder.id)
-                  .build()
-              );
-            }}
-          >
-            PO-{String(row.purchaseOrder?.number).padStart(4, '0')}
-          </Link>
-        )}
-        {row.requisition && (
-          <Link
-            component="button"
-            variant="caption"
-            sx={{
-              whiteSpace: 'nowrap',
-              textAlign: 'left',
-              color: iconColor,
-              width: 'fit-content',
-            }}
-            onClick={e => {
-              e.stopPropagation();
-              if (!row.requisition?.id) return;
-              navigate(
-                RouteBuilder.create(AppRoute.Replenishment)
-                  .addPart(AppRoute.InternalOrder)
-                  .addPart(row.requisition.id)
-                  .build()
-              );
-            }}
-          >
-            REQ-{String(row.requisition.requisitionNumber).padStart(4, '0')}
-          </Link>
-        )}
       </Box>
     </Box>
   );
