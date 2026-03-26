4a635b92730710cb7f7f91a78de8bb3d998dfee9
roxy-dao <roxy@msupply.foundation>
2026-03-26 10:12:19 +0100

    Migration to make comments required for all existing not functioning

---
+++ server/repository/src/migrations/v2_18_00/mod.rs
@@ -0,0 +1,46 @@
+use super::{version::Version, Migration, MigrationFragment};
+use crate::StorageConnection;
+
+mod asset_log_reason_not_functioning_comments_required;
+
+pub(crate) struct V2_18_00;
+impl Migration for V2_18_00 {
+    fn version(&self) -> Version {
+        Version::from_str("2.18.0")
+    }
+
+    fn migrate(&self, _connection: &StorageConnection) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn migrate_fragments(&self) -> Vec<Box<dyn MigrationFragment>> {
+        vec![
+            Box::new(asset_log_reason_not_functioning_comments_required::Migrate),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[actix_rt::test]
+    async fn migration_2_18_00() {
+        use crate::migrations::*;
+        use crate::test_db::*;
+        use v2_17_00::V2_17_00;
+        use v2_18_00::V2_18_00;
+
+        let previous_version = V2_17_00.version();
+        let version = V2_18_00.version();
+
+        let SetupResult { connection, .. } = setup_test(SetupOption {
+            db_name: &format!("migration_{version}"),
+            version: Some(previous_version.clone()),
+            ..Default::default()
+        })
+        .await;
+
+        // Run this migration
+        migrate(&connection, Some(version.clone())).unwrap();
+        assert_eq!(get_database_version(&connection), version);
+    }
+}
