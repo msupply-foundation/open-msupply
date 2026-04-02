0603244613666afea1bbb2b2a5b4336ce4f3eaf4
roxy-dao <roxy@msupply.foundation>
2026-04-02 07:35:55 +0200

    Update under / overstocked to be float instead of int
    Source: https://github.com/msupply-foundation/open-msupply/commit/c8927a9e844be71f327983905afbb13073d0ade9

--- server/graphql/preference/src/upsert.rs
+++ server/graphql/preference/src/upsert.rs
@@ -88,9 +88,9 @@ pub struct UpsertPreferencesInput {
     pub number_of_months_to_check_for_consumption_when_calculating_out_of_stock_products:
         Option<Vec<IntegerStorePrefInput>>,
     pub number_of_months_threshold_to_show_low_stock_alerts_for_products:
-        Option<Vec<IntegerStorePrefInput>>,
+        Option<Vec<FloatStorePrefInput>>,
     pub number_of_months_threshold_to_show_over_stock_alerts_for_products:
-        Option<Vec<IntegerStorePrefInput>>,
+        Option<Vec<FloatStorePrefInput>>,
     pub first_threshold_for_expiring_items: Option<Vec<IntegerStorePrefInput>>,
     pub second_threshold_for_expiring_items: Option<Vec<IntegerStorePrefInput>>,
     pub warn_when_missing_recent_stocktake: Option<Vec<WarnWhenMissingRecentStocktakeInput>>,
