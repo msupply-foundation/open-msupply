1751fb5786c39f1ab63a9af8f0acb2bb8b3dc407
rafaelcamargonz <rafaelcamargonz@gmail.com>
2026-04-08 16:27:29 +0200

    Remove obsolete includes

--- client/packages/host/webpack.config.js
+++ client/packages/host/webpack.config.js
@@ -30,9 +30,6 @@ module.exports = env => {
 
   return {
     entry: './src/index',
-    experiments: {
-      asyncWebAssembly: true,
-    },
     mode: isProduction ? 'production' : 'development',
     devtool: isProduction ? undefined : 'source-map',
     devServer: {
