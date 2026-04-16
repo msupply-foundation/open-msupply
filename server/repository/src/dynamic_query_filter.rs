2effcfdae4d596a6a501796d48ec7f390302f5a3
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2026-04-17 11:13:05 +1200

    pr review changes + rename filter

--- server/repository/src/dynamic_query_filter.rs
+++ server/repository/src/dynamic_query_filter.rs
@@ -1,4 +1,5 @@
-// This is all from dynamic filtering repository tutorial, but i've asked claude to make "create_condition"
+// This is from dynamic filtering repository [tutorial](https://github.com/andreievg/diesel-rs-dynamic-filters/tree/main)
+
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 #[serde(bound = "T: Clone + serde::Serialize + serde::de::DeserializeOwned")]
 pub enum GeneralFilter<T: Clone + serde::Serialize + serde::de::DeserializeOwned> {
@@ -14,25 +15,25 @@ pub enum GeneralFilter<T: Clone + serde::Serialize + serde::de::DeserializeOwned
 macro_rules! general_filter {
     ($filter:ident, $dsl_field:expr ) => {{
         match $filter {
-            crate::dynamic_query::GeneralFilter::Equal(value) => {
+            crate::dynamic_query_filter::GeneralFilter::Equal(value) => {
                 Box::new($dsl_field.eq(value).nullable())
             }
-            crate::dynamic_query::GeneralFilter::NotEqual(value) => {
+            crate::dynamic_query_filter::GeneralFilter::NotEqual(value) => {
                 Box::new($dsl_field.ne(value).nullable())
             }
-            crate::dynamic_query::GeneralFilter::In(value) => {
+            crate::dynamic_query_filter::GeneralFilter::In(value) => {
                 Box::new($dsl_field.eq_any(value).nullable())
             }
-            crate::dynamic_query::GeneralFilter::GreaterThan(value) => {
+            crate::dynamic_query_filter::GeneralFilter::GreaterThan(value) => {
                 Box::new($dsl_field.gt(value).nullable())
             }
-            crate::dynamic_query::GeneralFilter::LowerThan(value) => {
+            crate::dynamic_query_filter::GeneralFilter::LowerThan(value) => {
                 Box::new($dsl_field.lt(value).nullable())
             }
-            crate::dynamic_query::GeneralFilter::IsNull => {
+            crate::dynamic_query_filter::GeneralFilter::IsNull => {
                 Box::new($dsl_field.is_null().nullable())
             }
-            crate::dynamic_query::GeneralFilter::IsNotNull => {
+            crate::dynamic_query_filter::GeneralFilter::IsNotNull => {
                 Box::new($dsl_field.is_not_null().nullable())
             }
         }
@@ -40,30 +41,7 @@ macro_rules! general_filter {
 }
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
-pub enum BooleanFilter {
-    True,
-    False,
-    IsNull,
-    IsNotNull,
-}
-
-macro_rules! boolean_filter {
-    ($filter:ident, $dsl_field:expr ) => {{
-        match $filter {
-            crate::dynamic_query::BooleanFilter::True => Box::new($dsl_field.eq(true).nullable()),
-            crate::dynamic_query::BooleanFilter::False => Box::new($dsl_field.eq(false).nullable()),
-            crate::dynamic_query::BooleanFilter::IsNull => {
-                Box::new($dsl_field.is_null().nullable())
-            }
-            crate::dynamic_query::BooleanFilter::IsNotNull => {
-                Box::new($dsl_field.is_not_null().nullable())
-            }
-        }
-    }};
-}
-
-#[derive(Clone, serde::Serialize, serde::Deserialize)]
-pub enum AndOr {
+pub(crate) enum AndOr {
     And,
     Or,
 }
@@ -93,24 +71,6 @@ pub trait FilterBuilder<T: Clone + serde::Serialize + serde::de::DeserializeOwne
     }
     fn is_not_null() -> Self::Condition {
         Self::make_condition(GeneralFilter::IsNotNull)
-    }
-}
-
-pub trait BooleanFilterBuilder {
-    type Condition;
-    fn make_condition(filter: BooleanFilter) -> Self::Condition;
-
-    fn is_true() -> Self::Condition {
-        Self::make_condition(BooleanFilter::True)
-    }
-    fn is_false() -> Self::Condition {
-        Self::make_condition(BooleanFilter::False)
-    }
-    fn is_null() -> Self::Condition {
-        Self::make_condition(BooleanFilter::IsNull)
-    }
-    fn is_not_null() -> Self::Condition {
-        Self::make_condition(BooleanFilter::IsNotNull)
     }
 }
 
@@ -175,15 +135,15 @@ macro_rules! create_condition {
                                 Some(create_condition!(@filter_macro $filter_kind, f, $dsl_expr))
                             },
                         )+
-                        Inner::And(conditions) => create_filter(conditions, crate::dynamic_query::AndOr::And),
-                        Inner::Or(conditions) => create_filter(conditions, crate::dynamic_query::AndOr::Or),
+                        Inner::And(conditions) => create_filter(conditions, crate::dynamic_query_filter::AndOr::And),
+                        Inner::Or(conditions) => create_filter(conditions, crate::dynamic_query_filter::AndOr::Or),
                         Inner::True => Some(Box::new(true.into_sql::<diesel::sql_types::Bool>().nullable())),
                         Inner::False => Some(Box::new(false.into_sql::<diesel::sql_types::Bool>().nullable())),
                     }
                 }
             }
 
-            fn create_filter(conditions: Vec<Inner>, and_or: crate::dynamic_query::AndOr) -> Option<BoxedCondition> {
+            fn create_filter(conditions: Vec<Inner>, and_or: crate::dynamic_query_filter::AndOr) -> Option<BoxedCondition> {
                 conditions
                     .into_iter()
                     .filter_map::<BoxedCondition, _>(Inner::to_boxed_condition)
@@ -192,8 +152,8 @@ macro_rules! create_condition {
                             None => boxed_condition,
                             Some(bc) =>
                                 match and_or {
-                                    crate::dynamic_query::AndOr::And => Box::new(bc.and(boxed_condition)),
-                                    crate::dynamic_query::AndOr::Or => Box::new(bc.or(boxed_condition)),
+                                    crate::dynamic_query_filter::AndOr::And => Box::new(bc.and(boxed_condition)),
+                                    crate::dynamic_query_filter::AndOr::Or => Box::new(bc.or(boxed_condition)),
 
                                 }
                             })
@@ -203,16 +163,15 @@ macro_rules! create_condition {
     };
 
     // Map filter kind to filter type
-    (@filter_type number) => { crate::dynamic_query::GeneralFilter<i64> };
-    (@filter_type string) => { crate::dynamic_query::GeneralFilter<String> };
-    (@filter_type boolean) => { crate::dynamic_query::BooleanFilter };
-    (@filter_type $custom_type:ty) => { crate::dynamic_query::GeneralFilter<$custom_type> };
+    (@filter_type number) => { crate::dynamic_query_filter::GeneralFilter<i32> };
+    (@filter_type string) => { crate::dynamic_query_filter::GeneralFilter<String> };
+    (@filter_type $custom_type:ty) => { crate::dynamic_query_filter::GeneralFilter<$custom_type> };
 
     // Implement FilterBuilder trait for number fields
     (@impl_trait $variant:ident, number) => {
-        impl crate::dynamic_query::FilterBuilder<i64> for $variant {
+        impl crate::dynamic_query_filter::FilterBuilder<i32> for $variant {
             type Condition = Inner;
-            fn make_condition(filter: crate::dynamic_query::GeneralFilter<i64>) -> Inner {
+            fn make_condition(filter: crate::dynamic_query_filter::GeneralFilter<i32>) -> Inner {
                 Inner::$variant(filter)
             }
         }
@@ -220,19 +179,9 @@ macro_rules! create_condition {
 
     // Implement FilterBuilder trait for string fields
     (@impl_trait $variant:ident, string) => {
-        impl crate::dynamic_query::FilterBuilder<String> for $variant {
+        impl crate::dynamic_query_filter::FilterBuilder<String> for $variant {
             type Condition = Inner;
-            fn make_condition(filter: crate::dynamic_query::GeneralFilter<String>) -> Inner {
-                Inner::$variant(filter)
-            }
-        }
-    };
-
-    // Implement BooleanFilterBuilder trait for boolean fields
-    (@impl_trait $variant:ident, boolean) => {
-        impl crate::dynamic_query::BooleanFilterBuilder for $variant {
-            type Condition = Inner;
-            fn make_condition(filter: crate::dynamic_query::BooleanFilter) -> Inner {
+            fn make_condition(filter: crate::dynamic_query_filter::GeneralFilter<String>) -> Inner {
                 Inner::$variant(filter)
             }
         }
@@ -240,9 +189,9 @@ macro_rules! create_condition {
 
     // Implement FilterBuilder trait for custom type fields
     (@impl_trait $variant:ident, $custom_type:ty) => {
-        impl crate::dynamic_query::FilterBuilder<$custom_type> for $variant {
+        impl crate::dynamic_query_filter::FilterBuilder<$custom_type> for $variant {
             type Condition = Inner;
-            fn make_condition(filter: crate::dynamic_query::GeneralFilter<$custom_type>) -> Inner {
+            fn make_condition(filter: crate::dynamic_query_filter::GeneralFilter<$custom_type>) -> Inner {
                 Inner::$variant(filter)
             }
         }
@@ -250,16 +199,12 @@ macro_rules! create_condition {
 
     // Helper rules - matching on identifier tokens
     (@filter_macro string, $f:ident, $dsl_expr:expr) => {
-        crate::dynamic_query::general_filter!($f, $dsl_expr)
-    };
-    (@filter_macro boolean, $f:ident, $dsl_expr:expr) => {
-        crate::dynamic_query::boolean_filter!($f, $dsl_expr)
+        crate::dynamic_query_filter::general_filter!($f, $dsl_expr)
     };
     (@filter_macro $custom_type:ty, $f:ident, $dsl_expr:expr) => {
-        crate::dynamic_query::general_filter!($f, $dsl_expr)
+        crate::dynamic_query_filter::general_filter!($f, $dsl_expr)
     };
 }
 
-pub(crate) use boolean_filter;
 pub(crate) use create_condition;
 pub(crate) use general_filter;
