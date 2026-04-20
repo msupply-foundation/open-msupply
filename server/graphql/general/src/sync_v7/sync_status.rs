9f99b17e5e6b05e34daa65994aad468c155ddef9
Aimee <163234798+aimee-mcneil-melville@users.noreply.github.com>
2026-04-20 15:24:31 +1200

    add v7 tests

--- server/graphql/general/src/sync_v7/sync_status.rs
+++ server/graphql/general/src/sync_v7/sync_status.rs
@@ -144,3 +144,235 @@ pub fn latest_sync_status_v7(
         last_successful_sync_status,
     )))
 }
+
+#[cfg(test)]
+mod test {
+    use async_graphql::EmptyMutation;
+    use chrono::{Duration, NaiveDate};
+    use graphql_core::{assert_graphql_query, test_helpers::setup_graphql_test_with_data};
+    use repository::{
+        mock::{MockData, MockDataInserts},
+        syncv7::SyncError,
+        SyncLogV7Row,
+    };
+    use serde_json::json;
+
+    use crate::GeneralQueries;
+
+    #[actix_rt::test]
+    async fn graphql_latest_sync_status_v7_empty() {
+        let (_, _, _, settings) = setup_graphql_test_with_data(
+            GeneralQueries,
+            EmptyMutation,
+            "graphql_latest_sync_status_v7_empty",
+            MockDataInserts::none(),
+            MockData::default(),
+        )
+        .await;
+
+        let query = r#"{
+            latestSyncStatusV7 {
+                isSyncing
+                summary { started finished durationInSeconds }
+                error { variant fullError }
+                push { started finished total done }
+                pull { started finished total done }
+                integration { started finished total done }
+                waitingForIntegration { started finished durationInSeconds }
+            }
+        }"#;
+
+        let expected = json!({
+            "latestSyncStatusV7": null
+        });
+        assert_graphql_query!(&settings, query, &None, expected, None);
+    }
+
+    #[actix_rt::test]
+    async fn graphql_latest_sync_status_v7_in_progress() {
+        let started = NaiveDate::from_ymd_opt(2025, 6, 1)
+            .unwrap()
+            .and_hms_opt(10, 0, 0)
+            .unwrap();
+
+        let (_, _, _, settings) = setup_graphql_test_with_data(
+            GeneralQueries,
+            EmptyMutation,
+            "graphql_latest_sync_status_v7_in_progress",
+            MockDataInserts::none(),
+            MockData {
+                sync_logs_v7: vec![SyncLogV7Row {
+                    id: "sync_1".to_string(),
+                    started_datetime: started,
+                    push_started_datetime: Some(started + Duration::seconds(5)),
+                    push_progress_total: Some(10),
+                    push_progress_done: Some(3),
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        let query = r#"{
+            latestSyncStatusV7 {
+                isSyncing
+                summary { started finished }
+                error { variant fullError }
+                push { started finished total done }
+                pull { started finished total done }
+            }
+        }"#;
+
+        let expected = json!({
+            "latestSyncStatusV7": {
+                "isSyncing": true,
+                "summary": {
+                    "started": "2025-06-01T10:00:00+00:00",
+                    "finished": null,
+                },
+                "error": null,
+                "push": {
+                    "started": "2025-06-01T10:00:05+00:00",
+                    "finished": null,
+                    "total": 10,
+                    "done": 3,
+                },
+                "pull": null,
+            }
+        });
+        assert_graphql_query!(&settings, query, &None, expected, None);
+    }
+
+    #[actix_rt::test]
+    async fn graphql_latest_sync_status_v7_error() {
+        let started = NaiveDate::from_ymd_opt(2025, 6, 1)
+            .unwrap()
+            .and_hms_opt(10, 0, 0)
+            .unwrap();
+
+        let (_, _, _, settings) = setup_graphql_test_with_data(
+            GeneralQueries,
+            EmptyMutation,
+            "graphql_latest_sync_status_v7_error",
+            MockDataInserts::none(),
+            MockData {
+                sync_logs_v7: vec![SyncLogV7Row {
+                    id: "sync_1".to_string(),
+                    started_datetime: started,
+                    error: Some(SyncError::ConnectionError {
+                        url: "http://test.com".to_string(),
+                        e: "connection refused".to_string(),
+                    }),
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        let query = r#"{
+            latestSyncStatusV7 {
+                isSyncing
+                error { variant fullError }
+            }
+        }"#;
+
+        let expected = json!({
+            "latestSyncStatusV7": {
+                "isSyncing": false,
+                "error": {
+                    "variant": "CONNECTION_ERROR",
+                    "fullError": "Could not connect to server http://test.com connection refused",
+                },
+            }
+        });
+        assert_graphql_query!(&settings, query, &None, expected, None);
+    }
+
+    #[actix_rt::test]
+    async fn graphql_latest_sync_status_v7_completed() {
+        let started = NaiveDate::from_ymd_opt(2025, 6, 1)
+            .unwrap()
+            .and_hms_opt(10, 0, 0)
+            .unwrap();
+        let finished = started + Duration::seconds(60);
+
+        let (_, _, _, settings) = setup_graphql_test_with_data(
+            GeneralQueries,
+            EmptyMutation,
+            "graphql_latest_sync_status_v7_completed",
+            MockDataInserts::none(),
+            MockData {
+                sync_logs_v7: vec![SyncLogV7Row {
+                    id: "sync_1".to_string(),
+                    started_datetime: started,
+                    finished_datetime: Some(finished),
+                    push_started_datetime: Some(started + Duration::seconds(5)),
+                    push_finished_datetime: Some(started + Duration::seconds(15)),
+                    push_progress_total: Some(10),
+                    push_progress_done: Some(10),
+                    pull_started_datetime: Some(started + Duration::seconds(20)),
+                    pull_finished_datetime: Some(started + Duration::seconds(40)),
+                    pull_progress_total: Some(20),
+                    pull_progress_done: Some(20),
+                    integration_started_datetime: Some(started + Duration::seconds(41)),
+                    integration_finished_datetime: Some(started + Duration::seconds(55)),
+                    integration_progress_total: Some(5),
+                    integration_progress_done: Some(5),
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        let query = r#"{
+            latestSyncStatusV7 {
+                isSyncing
+                summary { started finished durationInSeconds }
+                error { variant fullError }
+                lastSuccessfulSync { started finished durationInSeconds }
+                push { started finished total done }
+                pull { started finished total done }
+                integration { started finished total done }
+            }
+        }"#;
+
+        let expected = json!({
+            "latestSyncStatusV7": {
+                "isSyncing": false,
+                "summary": {
+                    "started": "2025-06-01T10:00:00+00:00",
+                    "finished": "2025-06-01T10:01:00+00:00",
+                    "durationInSeconds": 60,
+                },
+                "error": null,
+                "lastSuccessfulSync": {
+                    "started": "2025-06-01T10:00:00+00:00",
+                    "finished": "2025-06-01T10:01:00+00:00",
+                    "durationInSeconds": 60,
+                },
+                "push": {
+                    "started": "2025-06-01T10:00:05+00:00",
+                    "finished": "2025-06-01T10:00:15+00:00",
+                    "total": 10,
+                    "done": 10,
+                },
+                "pull": {
+                    "started": "2025-06-01T10:00:20+00:00",
+                    "finished": "2025-06-01T10:00:40+00:00",
+                    "total": 20,
+                    "done": 20,
+                },
+                "integration": {
+                    "started": "2025-06-01T10:00:41+00:00",
+                    "finished": "2025-06-01T10:00:55+00:00",
+                    "total": 5,
+                    "done": 5,
+                },
+            }
+        });
+        assert_graphql_query!(&settings, query, &None, expected, None);
+    }
+}
