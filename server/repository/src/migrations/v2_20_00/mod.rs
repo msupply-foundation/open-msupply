c8888bd5a2e0b707a370d1fcefa68bbb8137975c
roxy-dao <roxy@msupply.foundation>
2026-05-21 11:18:08 +0200

    Migration for stocktake edited

---
+++ server/repository/src/migrations/v2_20_00/mod.rs
@@ -0,0 +1,44 @@
+use super::{version::Version, Migration, MigrationFragment};
+use crate::StorageConnection;
+
+mod add_stocktake_edited_activity_log_type;
+
+pub(crate) struct V2_20_00;
+impl Migration for V2_20_00 {
+    fn version(&self) -> Version {
+        Version::from_str("2.20.0")
+    }
+
+    fn migrate(&self, _connection: &StorageConnection) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn migrate_fragments(&self) -> Vec<Box<dyn MigrationFragment>> {
+        vec![Box::new(add_stocktake_edited_activity_log_type::Migrate)]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[actix_rt::test]
+    async fn migration_2_20_00() {
+        use crate::migrations::*;
+        use crate::test_db::*;
+        use v2_19_00::V2_19_00;
+        use v2_20_00::V2_20_00;
+
+        let previous_version = V2_19_00.version();
+        let version = V2_20_00.version();
+
+        let SetupResult { connection, .. } = setup_test(SetupOption {
+            db_name: &format!("migration_{version}"),
+            version: Some(previous_version.clone()),
+            ..Default::default()
+        })
+        .await;
+
+        // Run this migration
+        migrate(&connection, Some(version.clone())).unwrap();
+        assert_eq!(get_database_version(&connection), version);
+    }
+}
