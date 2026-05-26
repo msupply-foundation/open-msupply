ddfb7e6226f5c78e4e4983815c3c250b30fedf98
Aneesh T <150754185+aneeshthunga26@users.noreply.github.com>
2026-05-26 12:38:33 +1200

    feat(sync-v7): backend validation to prevent sites from clearing their own token / hw id
    Source: https://github.com/msupply-foundation/open-msupply/commit/dae4ef1516c36023d2718ea62cbe8999032fc7b0

--- server/graphql/site/src/mutations/clear_token.rs
+++ server/graphql/site/src/mutations/clear_token.rs
@@ -45,6 +45,7 @@ fn map_error(error: ServiceError) -> async_graphql::Error {
 
     let graphql_error = match error {
         ServiceError::SiteDoesNotExist => BadUserInput(formatted_error),
+        ServiceError::SameSite => BadUserInput(formatted_error),
         ServiceError::DatabaseError(_) => InternalError(formatted_error),
     };
 
