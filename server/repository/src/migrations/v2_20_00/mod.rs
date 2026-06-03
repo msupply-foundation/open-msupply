383ab6560c7cacb8d656fc9a30ce32a1a1ad0cb7
roxy-dao <roxy@msupply.foundation>
2026-06-03 08:29:17 +0200

    Migration for stock relocation table & bare min repo layer

--- server/repository/src/migrations/v2_20_00/mod.rs
+++ server/repository/src/migrations/v2_20_00/mod.rs
@@ -4,6 +4,7 @@ use crate::StorageConnection;
 mod add_in_progress_and_error_statuses_sync_message;
 mod add_plugin_data_datetime_field;
 mod add_plugin_data_indexes;
+mod add_stock_relocation_table;
 mod add_stocktake_edited_activity_log_type;
 mod add_support_upload_files_processor_cursor_key_value_store;
 mod add_variant_and_bundle_activity_log_types;
@@ -26,6 +27,7 @@ impl Migration for V2_20_00 {
             Box::new(add_in_progress_and_error_statuses_sync_message::Migrate),
             Box::new(add_variant_and_bundle_activity_log_types::Migrate),
             Box::new(add_stocktake_edited_activity_log_type::Migrate),
+            Box::new(add_stock_relocation_table::Migrate),
         ]
     }
 }
