eb2482406e97bdadbb0bc96176b5e20d4f111efd
roxy-dao <roxy@msupply.foundation>
2026-06-04 11:52:42 +0200

    Fix merge compilation

--- client/packages/common/src/authentication/api/hooks/useIsCentralStandalone.ts
+++ client/packages/common/src/authentication/api/hooks/useIsCentralStandalone.ts
@@ -1,16 +1,14 @@
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useAuthApi } from './useAuthApi';
 
 export const useIsCentralStandalone = (): boolean => {
   const api = useAuthApi();
-  const { data } = useQuery(
-    api.keys.isCentralStandalone,
-    () => api.get.isCentralStandalone(),
-    {
-      refetchOnMount: false,
-      cacheTime: Infinity,
-      staleTime: Infinity,
-    }
-  );
+  const { data } = useQuery({
+    queryKey: api.keys.isCentralStandalone,
+    queryFn: () => api.get.isCentralStandalone(),
+    refetchOnMount: false,
+    gcTime: Infinity,
+    staleTime: Infinity,
+  });
   return !!data;
 };
