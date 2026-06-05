9df6af873d58b52f8fe6994d9cd9cde79b1d4a8a
roxy-dao <roxy@msupply.foundation>
2026-06-05 10:22:10 +0200

    Insert graphQL
    Source: https://github.com/msupply-foundation/open-msupply/commit/7d6d50bf9eb8f4f8c9f0a2019119365a4ec0dc49

--- server/graphql/stock_relocation/src/lib.rs
+++ server/graphql/stock_relocation/src/lib.rs
@@ -1,9 +1,11 @@
 use async_graphql::{Context, Object, Result};
 use graphql_core::pagination::PaginationInput;
 
+pub mod mutations;
 pub mod queries;
 pub mod types;
 
+use mutations::{insert_stock_relocation, InsertInput, InsertResponse};
 use queries::{
     get_stock_relocation, get_stock_relocations, StockRelocationFilterInput,
     StockRelocationResponse, StockRelocationSortInput, StockRelocationsResponse,
@@ -32,5 +34,20 @@ impl StockRelocationQueries {
         sort: Option<Vec<StockRelocationSortInput>>,
     ) -> Result<StockRelocationsResponse> {
         get_stock_relocations(ctx, &store_id, page, filter, sort)
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct StockRelocationMutations;
+
+#[Object]
+impl StockRelocationMutations {
+    pub async fn insert_stock_relocation(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        input: InsertInput,
+    ) -> Result<InsertResponse> {
+        insert_stock_relocation(ctx, &store_id, input)
     }
 }
