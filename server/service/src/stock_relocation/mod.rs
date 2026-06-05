c47e2219ac091d654ec1566d5a3329790d16231a
roxy-dao <roxy@msupply.foundation>
2026-06-05 10:10:46 +0200

    Inserts for stock relocation

--- server/service/src/stock_relocation/mod.rs
+++ server/service/src/stock_relocation/mod.rs
@@ -1,1 +1,44 @@
+use self::insert::{insert_stock_relocation, InsertStockRelocation, InsertStockRelocationError};
+use self::query::{get_stock_relocation, get_stock_relocations};
+use crate::{service_provider::ServiceContext, ListError, ListResult};
+use repository::{
+    PaginationOption, RepositoryError, StockRelocation, StockRelocationFilter, StockRelocationRow,
+    StockRelocationSort,
+};
+
+pub mod insert;
+pub mod query;
+
+pub trait StockRelocationServiceTrait: Sync + Send {
+    fn get_stock_relocations(
+        &self,
+        ctx: &ServiceContext,
+        store_id: Option<&str>,
         pagination: Option<PaginationOption>,
+        filter: Option<StockRelocationFilter>,
+        sort: Option<StockRelocationSort>,
+    ) -> Result<ListResult<StockRelocation>, ListError> {
+        get_stock_relocations(ctx, store_id, pagination, filter, sort)
+    }
+
+    fn get_stock_relocation(
+        &self,
+        ctx: &ServiceContext,
+        store_id: Option<&str>,
+        id: &str,
+    ) -> Result<Option<StockRelocation>, RepositoryError> {
+        get_stock_relocation(ctx, store_id, id)
+    }
+
+    fn insert_stock_relocation(
+        &self,
+        ctx: &ServiceContext,
+        store_id: &str,
+        input: InsertStockRelocation,
+    ) -> Result<Vec<StockRelocationRow>, InsertStockRelocationError> {
+        insert_stock_relocation(ctx, store_id, input)
+    }
+}
+
+pub struct StockRelocationService;
+impl StockRelocationServiceTrait for StockRelocationService {}
