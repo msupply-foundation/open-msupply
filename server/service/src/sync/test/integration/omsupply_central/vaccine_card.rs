fea5e9a7ec618e440f8adc5080a4f903d105d134
Chris Petty <chris@msupply.foundation>
2026-06-09 16:43:06 +1200

    integration tests compile
    Source: https://github.com/msupply-foundation/open-msupply/commit/75f9b7a7ddeda4b211d4918d082d39d2cbf93836

--- server/service/src/sync/test/integration/omsupply_central/vaccine_card.rs
+++ server/service/src/sync/test/integration/omsupply_central/vaccine_card.rs
@@ -104,8 +104,8 @@ pub(super) async fn test_vaccine_card() {
         .expect("Problem inserting central data");
 
     // Sync both sites (only one needed here realy, to get central_server_url)
-    site1.synchroniser.sync(None).await.unwrap();
-    site2.synchroniser.sync(None).await.unwrap();
+    site1.synchroniser.sync().await.unwrap();
+    site2.synchroniser.sync().await.unwrap();
 
     let CentralServerConfig::CentralServerUrl(central_server_url) = CentralServerConfig::get()
     else {
@@ -134,8 +134,8 @@ pub(super) async fn test_vaccine_card() {
     .await;
 
     // Sync all pre requisites
-    site1.synchroniser.sync(None).await.unwrap();
-    site2.synchroniser.sync(None).await.unwrap();
+    site1.synchroniser.sync().await.unwrap();
+    site2.synchroniser.sync().await.unwrap();
 
     // TEST
 
@@ -174,7 +174,7 @@ pub(super) async fn test_vaccine_card() {
     )
     .unwrap();
 
-    site1.synchroniser.sync(None).await.unwrap();
+    site1.synchroniser.sync().await.unwrap();
 
     // 2 - Link patient to sites 2, sync and test data from 1 is present
     let context = site2.context.service_provider.basic_context().unwrap();
@@ -187,11 +187,7 @@ pub(super) async fn test_vaccine_card() {
     .await
     .unwrap();
 
-    site2
-        .synchroniser
-        .sync(Some(patient_one.id.clone()))
-        .await
-        .unwrap();
+    site2.synchroniser.sync().await.unwrap();
 
     check_integrated(&site2.context.connection, &integrations_one);
 
@@ -209,8 +205,8 @@ pub(super) async fn test_vaccine_card() {
     let integrations_two =
         integrate_with_is_sync_reset(&site2.context.connection, integrations_two);
 
-    site2.synchroniser.sync(None).await.unwrap();
-    site1.synchroniser.sync(None).await.unwrap();
+    site2.synchroniser.sync().await.unwrap();
+    site1.synchroniser.sync().await.unwrap();
 
     check_integrated(&site1.context.connection, &integrations_two);
 
@@ -219,8 +215,8 @@ pub(super) async fn test_vaccine_card() {
     let site1 = init_test_context(site1.config, "vaccine_card_site_1_reinit").await;
     let site2 = init_test_context(site2.config, "vaccine_card_site_2_reinit").await;
 
-    site1.synchroniser.sync(None).await.unwrap();
-    site2.synchroniser.sync(None).await.unwrap();
+    site1.synchroniser.sync().await.unwrap();
+    site2.synchroniser.sync().await.unwrap();
 
     check_integrated(&site1.context.connection, &integrations_one);
     check_integrated(&site1.context.connection, &integrations_two);
