7c281acc8de612ccfbc3f49c6420c4b894cccb17
roxy-dao <roxy@msupply.foundation>
2026-06-09 10:58:12 +0200

    Delete stock movement hook
    Source: https://github.com/msupply-foundation/open-msupply/commit/213a4037c8790879116002bb63f123212ceb5839

---
+++ client/packages/inventory/src/StockMovement/api/hooks/useDeleteStockMovement.ts
@@ -0,0 +1,43 @@
+import { useMutation } from '@openmsupply-client/common';
+import { STOCK_MOVEMENT } from './keys';
+import { useStockMovementGraphQL } from '../useStockMovementGraphQL';
+
+export const useDeleteStockMovement = () => {
+  const { stockMovementApi, storeId, queryClient } = useStockMovementGraphQL();
+
+  const mutationFn = async (id: string) => {
+    const result = await stockMovementApi.deleteStockRelocation({
+      storeId,
+      input: { id },
+    });
+    return result.deleteStockRelocation;
+  };
+
+  const { mutateAsync, isPending, error } = useMutation({
+    mutationFn,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: [STOCK_MOVEMENT] }),
+  });
+
+  return { delete: mutateAsync, isDeleting: isPending, error };
+};
+
+export const useDeleteStockMovements = () => {
+  const { stockMovementApi, storeId, queryClient } = useStockMovementGraphQL();
+
+  const mutationFn = async (ids: string[]) => {
+    await Promise.all(
+      ids.map(id =>
+        stockMovementApi.deleteStockRelocation({ storeId, input: { id } })
+      )
+    );
+  };
+
+  const { mutateAsync, isPending, error } = useMutation({
+    mutationFn,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: [STOCK_MOVEMENT] }),
+  });
+
+  return { deleteStockMovements: mutateAsync, isDeleting: isPending, error };
+};
