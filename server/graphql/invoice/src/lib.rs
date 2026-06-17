26cc2b18f33503a5639a8a0044d6b87980528482
roxy-dao <roxy@msupply.foundation>
2026-06-17 11:06:24 +0200

    GraphQL endpoint to duplicate inbound shipment
    Source: https://github.com/msupply-foundation/open-msupply/commit/9dfb4b53feb5bcb0639288939687119afabc3ca4

--- server/graphql/invoice/src/lib.rs
+++ server/graphql/invoice/src/lib.rs
@@ -165,6 +165,15 @@ impl InvoiceMutations {
         )
     }
 
+    async fn duplicate_inbound_shipment(
+        &self,
+        ctx: &Context<'_>,
+        store_id: String,
+        #[graphql(desc = "id of the inbound shipment to duplicate")] id: String,
+    ) -> Result<graphql_types::types::InvoiceNode> {
+        inbound_shipment::duplicate::duplicate(ctx, &store_id, id)
+    }
+
     async fn delete_inbound_shipment_external(
         &self,
         ctx: &Context<'_>,
