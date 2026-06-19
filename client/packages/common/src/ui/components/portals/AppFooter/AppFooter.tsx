f134c8a8486628f408b332ca1bc8f85879b0331b
mark-prins <mark@malcolmprins.net>
2026-06-19 16:29:43 +1200

    move to on page finished
    Source: https://github.com/msupply-foundation/open-msupply/commit/f2bbdfb40d24f30f14d00d44e530143e4ef9864c

--- client/packages/common/src/ui/components/portals/AppFooter/AppFooter.tsx
+++ client/packages/common/src/ui/components/portals/AppFooter/AppFooter.tsx
@@ -25,7 +25,9 @@ export const AppFooter = ({
   textColor,
 }: AppFooterProps) => {
   const setAppFooterRef = useHostContext(s => s.setAppFooterRef);
-  const setAppSessionDetailsRef = useHostContext(s => s.setAppSessionDetailsRef);
+  const setAppSessionDetailsRef = useHostContext(
+    s => s.setAppSessionDetailsRef
+  );
   const fullScreen = useHostContext(s => s.fullScreen);
   const { keyboardIsOpen } = useKeyboard();
   const appFooterRef = useRef(null);
@@ -42,7 +44,7 @@ export const AppFooter = ({
     <Box
       sx={{
         display: hideFooter ? 'none' : undefined,
-        paddingBottom: 'var(--safe-area-inset-bottom)',
+        paddingBottom: 'var(--inset-bottom)',
       }}
     >
       <Container ref={appFooterRef} style={{ flex: 0 }} />
