3c25f7f74e3489a1a70c6b4d728299d6f7d98e89
roxy-dao <roxy@msupply.foundation>
2026-06-24 10:06:18 +0200

    Preference to not print placeholder line labels
    Source: https://github.com/msupply-foundation/open-msupply/commit/367df76bd27e81a940eb13d2e74f8351902f5d76

--- server/service/src/preference/types.rs
+++ server/service/src/preference/types.rs
@@ -55,6 +55,7 @@ pub enum PrefKey {
     WarnWhenMissingRecentStocktake,
     InvoiceStatusOptions,
     ShowIndicativePriceInRequisitions,
+    DoNotPrintPlaceholderLineLabels,
 }
 
 #[derive(Clone, Debug, PartialEq)]
