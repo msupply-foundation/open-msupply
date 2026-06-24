71f711173693b68bab44bc152fff378b11e136a6
Aneesh T <150754185+aneeshthunga26@users.noreply.github.com>
2026-06-25 11:39:40 +1200

    feat(sync-v7): add key value store enum variant for changelog dedup cursor
    Source: https://github.com/msupply-foundation/open-msupply/commit/ac2714ee9199c10caafa4e4a4e72e01c848261d1

--- server/repository/src/db_diesel/key_value_store.rs
+++ server/repository/src/db_diesel/key_value_store.rs
@@ -38,6 +38,7 @@ pub enum KeyType {
     AssignRequisitionNumberProcessorCursor,
     RequisitionAutoFinaliseProcessorCursor,
     MergeSyncMessageProcessorCursor,
+    ChangelogDedupCursor,
     // Nested key value store to store dynamic cursor values as JSON text
     DynamicCursor,
 
