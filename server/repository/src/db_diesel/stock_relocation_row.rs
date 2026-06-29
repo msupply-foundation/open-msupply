d0aacdf59c448a8f15c2fc3111ac4a5c9016154e
roxy-dao <roxy@msupply.foundation>
2026-06-29 10:34:00 +0200

    Simplify stock relocation table
    Source: https://github.com/msupply-foundation/open-msupply/commit/c6fb91ab097efc0c61602bb6db18446ad5504f9c

--- server/repository/src/db_diesel/stock_relocation_row.rs
+++ server/repository/src/db_diesel/stock_relocation_row.rs
@@ -1,4 +1,4 @@
-use super::{item_row::item, stock_line_row::stock_line, StorageConnection};
+use super::StorageConnection;
 
 use crate::Upsert;
 use crate::{repository_error::RepositoryError, Delete};
@@ -12,23 +12,15 @@ use serde::{Deserialize, Serialize};
 table! {
     stock_relocation (id) {
         id -> Text,
-        created_datetime -> Timestamp,
-        finalised_datetime -> Nullable<Timestamp>,
-        from_stock_line_id -> Text,
-        from_location_id -> Nullable<Text>,
-        from_number_of_packs -> Double,
-        to_stock_line_id -> Nullable<Text>,
-        to_location_id -> Nullable<Text>,
-        to_pack_size -> Nullable<Double>,
-        status -> crate::db_diesel::stock_relocation_row::StockRelocationStatusMapping,
         store_id -> Text,
-        user_id -> Text,
+        reference_number -> Text,
+        status -> crate::db_diesel::stock_relocation_row::StockRelocationStatusMapping,
+        created_datetime -> Timestamp,
+        created_by -> Text,
+        finalised_datetime -> Nullable<Timestamp>,
+        comment -> Nullable<Text>,
     }
 }
-
-joinable!(stock_relocation -> stock_line (from_stock_line_id));
-allow_tables_to_appear_in_same_query!(stock_relocation, stock_line);
-allow_tables_to_appear_in_same_query!(stock_relocation, item);
 
 #[derive(DbEnum, Debug, Clone, PartialEq, Eq, Default, Serialize, Deserialize)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
@@ -36,6 +28,7 @@ allow_tables_to_appear_in_same_query!(stock_relocation, item);
 pub enum StockRelocationStatus {
     #[default]
     New,
+    Confirmed,
     Finalised,
 }
 
@@ -45,17 +38,13 @@ pub enum StockRelocationStatus {
 #[diesel(table_name = stock_relocation)]
 pub struct StockRelocationRow {
     pub id: String,
-    pub created_datetime: NaiveDateTime,
-    pub finalised_datetime: Option<NaiveDateTime>,
-    pub from_stock_line_id: String,
-    pub from_location_id: Option<String>,
-    pub from_number_of_packs: f64,
-    pub to_stock_line_id: Option<String>,
-    pub to_location_id: Option<String>,
-    pub to_pack_size: Option<f64>,
-    pub status: StockRelocationStatus,
     pub store_id: String,
-    pub user_id: String,
+    pub reference_number: String,
+    pub status: StockRelocationStatus,
+    pub created_datetime: NaiveDateTime,
+    pub created_by: String,
+    pub finalised_datetime: Option<NaiveDateTime>,
+    pub comment: Option<String>,
 }
 
 pub struct StockRelocationRowRepository<'a> {
