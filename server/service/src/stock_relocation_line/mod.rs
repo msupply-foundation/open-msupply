fa26f42e46644b2a0609c29c845f9782100d86f5
roxy-dao <roxy@msupply.foundation>
2026-06-30 10:22:03 +0200

    batch upserts for stock movement
    Source: https://github.com/msupply-foundation/open-msupply/commit/b88ce5b71e68f31cd72a31a1e372dc1e5f9f584e

--- server/service/src/stock_relocation_line/mod.rs
+++ server/service/src/stock_relocation_line/mod.rs
@@ -1,5 +1,9 @@
+pub mod batch;
 pub mod upsert;
 
+pub use batch::{
+    batch_stock_relocation_line, BatchStockRelocationLine, BatchStockRelocationLineResult,
+};
 pub use upsert::{
     upsert_stock_relocation_line, UpsertStockRelocationLine, UpsertStockRelocationLineError,
 };
