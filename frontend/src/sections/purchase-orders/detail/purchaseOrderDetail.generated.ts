4bf93914b46dae0db416febbf7e3bbc3ccdac363
roxy-dao <roxy@msupply.foundation>
2026-09-23 10:12:43 +0200

    Update purchaseOrderDetail.generated.ts

--- frontend/src/sections/purchase-orders/detail/purchaseOrderDetail.generated.ts
+++ frontend/src/sections/purchase-orders/detail/purchaseOrderDetail.generated.ts
@@ -203,7 +203,7 @@ export type PurchaseOrderLineSetResult = {
 };
 
 export const PurchaseOrderLineSet = {
-  query: "query purchaseOrderLineSet($storeId: String!, $orderId: String!) {\n  purchaseOrderLines(\n    storeId: $storeId\n    filter: {purchaseOrderId: {equalTo: $orderId}}\n    page: {first: 5000}\n  ) {\n    ... on PurchaseOrderLineConnector {\n      __typename\n      nodes {\n        id\n        requestedNumberOfUnits\n        requestedDeliveryDate\n        expectedDeliveryDate\n        outstandingNumberOfUnits\n      }\n    }\n  }\n}",
+  query: "query purchaseOrderLineSet($storeId: String!, $orderId: String!) {\n  purchaseOrderLines(\n    storeId: $storeId\n    filter: {purchaseOrderId: {equalTo: $orderId}}\n    page: {first: 5000}\n  ) {\n    ... on PurchaseOrderLineConnector {\n      __typename\n      nodes {\n        id\n        item {\n          id\n        }\n        requestedNumberOfUnits\n        requestedDeliveryDate\n        expectedDeliveryDate\n        outstandingNumberOfUnits\n      }\n    }\n  }\n}",
 } as TypedDocument<PurchaseOrderLineSetResult, PurchaseOrderLineSetVariables>;
 
 export type UpdatePurchaseOrderVariables = {
